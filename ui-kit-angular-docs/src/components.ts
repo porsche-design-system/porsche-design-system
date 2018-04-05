8f13253997971250f41a27dffe5a79fd9feb3ddf
Stephan Schröter <stephan.schroeter@mhp.com>
2018-04-05 15:45:42 +0200

    Updated docs for h3 | cs / sas | VRS-1509

--- ui-kit-angular-docs/src/components.ts
+++ ui-kit-angular-docs/src/components.ts
@@ -1,9 +1,1 @@
-import {
-    PuiHeadline1Component,
-    PuiHeadline2Component
-} from "@porsche/ui-kit-angular";
-
-export const components = [
-    PuiHeadline1Component,
-    PuiHeadline2Component
-];
+export const components = [];
--- ui-kit-angular-docs/stories/2_atoms/1_text.md
+++ ui-kit-angular-docs/stories/2_atoms/1_text.md
@@ -6,4 +6,5 @@ which can be applied to any element.
 <pui-docs-example-viewer>
     <pui-h1>text-size-1</pui-h1>
     <pui-h2>text-size-2</pui-h2>
+    <pui-h3>text-size-3</pui-h3>
 </pui-docs-example-viewer>
