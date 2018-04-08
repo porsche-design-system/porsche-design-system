b4c478b6bec34644bfb9ad2e7443480c2052cad8
Stephan Schröter <stephan.schroeter@mhp.com>
2018-04-09 00:49:17 +0200

    Updated src folder structure | sas | PUIK

--- core/ui-kit/config.js
+++ core/ui-kit/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   stylesheet: {
     development: {
-      file: './src/porsche-stylesheets.scss',
+      file: './src/index.scss',
       outFile: './dist/porsche-stylesheets.origin.css',
       sourceMap: true,
       sourceMapRoot: `file:///${__dirname}/dist/`
     },
     production: {
-      file: './src/porsche-stylesheets.scss',
+      file: './src/index.scss',
       outFile: './dist/porsche-stylesheets.origin.min.css',
       sourceMap: false,
       outputStyle: 'compressed'
