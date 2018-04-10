481dbeac90207953d62903bd6e61ba9d6a4a3bff
Stephan Schröter <stephan.schroeter@mhp.com>
2018-04-11 01:44:59 +0200

    Updated plugin porsche ui kit synchronizer | sas | PUIK

--- core/tools/plugin-porsche-ui-kit-core-synchronizer/index.js
+++ core/tools/plugin-porsche-ui-kit-core-synchronizer/index.js
@@ -1,53 +1,64 @@
-let package = require('./package');
+let npmPackage = require('./package');
 let Logger = require('./console-logger');
 let rimraf = require('rimraf');
 let ncp = require('ncp').ncp;
 
-let syncPatterns = (folder) => {
-  rimraf('./patternlab/source/_patterns/'+ folder, () => {
-    ncp('./node_modules/@porsche/ui-kit-core/patternlab/source/_patterns/'+ folder, './patternlab/source/_patterns/'+ folder, (err) => {
-      if (err) {
-        return Logger.error(err);
-      }
-      Logger.progress(package.name +': Synced patterns in "'+ folder +'"');
-    });
-  });
-};
+class PorscheUIKitCoreSynchronizer {
 
-let syncData = (file) => {
-  rimraf('./patternlab/source/_data/'+ file, () => {
-    ncp('./node_modules/@porsche/ui-kit-core/patternlab/source/_data/'+ file, './patternlab/source/_data/'+ file, (err) => {
-      if (err) {
-        return Logger.error(err);
-      }
-      Logger.progress(package.name +': Synced data "'+ file +'"');
-    });
-  });
-};
+  constructor() {
+    this.init();
+  }
 
-let syncImages = (folder) => {
-  rimraf('./patternlab/source/images/'+ folder, () => {
-    ncp('./node_modules/@porsche/ui-kit-core/patternlab/source/images/'+ folder, './patternlab/source/images/'+ folder, (err) => {
-      if (err) {
-        return Logger.error(err);
-      }
-      Logger.progress(package.name +': Synced images in "'+ folder +'"');
-    });
-  });
-};
+  init() {
+    ['01-atoms', '02-molecules', '03-organisms', '98-deprecated', '99-design-spec'].forEach(this.syncPatterns);
+    ['porsche-ui-kit-core.json'].forEach(this.syncData);
+    ['porsche-ui-kit-core'].forEach(this.syncImages);
+    ['porsche-ui-kit-core'].forEach(this.syncScripts);
+  }
 
-let syncScripts = (folder) => {
-  rimraf('./patternlab/source/js/'+ folder, () => {
-    ncp('./node_modules/@porsche/ui-kit-core/patternlab/source/js/'+ folder, './patternlab/source/js/'+ folder, (err) => {
-      if (err) {
-        return Logger.error(err);
-      }
-      Logger.progress(package.name +': Synced scripts in "'+ folder +'"');
+  syncPatterns(folder) {
+    rimraf('./patternlab/source/_patterns/'+ folder, () => {
+      ncp('./node_modules/@porsche/ui-kit-core/patternlab/source/_patterns/'+ folder, './patternlab/source/_patterns/'+ folder, (err) => {
+        if (err) {
+          return Logger.error(err);
+        }
+        Logger.progress(npmPackage.name +': Synced patterns in "'+ folder +'"');
+      });
     });
-  });
-};
+  }
 
-['01-atoms', '02-molecules', '03-organisms', '98-deprecated', '99-design-spec'].forEach(syncPatterns);
-['porsche-ui-kit-core.json'].forEach(syncData);
-['porsche-ui-kit-core'].forEach(syncImages);
-['porsche-ui-kit-core'].forEach(syncScripts);
+  syncData(file) {
+    rimraf('./patternlab/source/_data/'+ file, () => {
+      ncp('./node_modules/@porsche/ui-kit-core/patternlab/source/_data/'+ file, './patternlab/source/_data/'+ file, (err) => {
+        if (err) {
+          return Logger.error(err);
+        }
+        Logger.progress(npmPackage.name +': Synced data "'+ file +'"');
+      });
+    });
+  }
+
+  syncImages(folder) {
+    rimraf('./patternlab/source/images/'+ folder, () => {
+      ncp('./node_modules/@porsche/ui-kit-core/patternlab/source/images/'+ folder, './patternlab/source/images/'+ folder, (err) => {
+        if (err) {
+          return Logger.error(err);
+        }
+        Logger.progress(npmPackage.name +': Synced images in "'+ folder +'"');
+      });
+    });
+  }
+
+  syncScripts(folder) {
+    rimraf('./patternlab/source/js/'+ folder, () => {
+      ncp('./node_modules/@porsche/ui-kit-core/patternlab/source/js/'+ folder, './patternlab/source/js/'+ folder, (err) => {
+        if (err) {
+          return Logger.error(err);
+        }
+        Logger.progress(npmPackage.name +': Synced scripts in "'+ folder +'"');
+      });
+    });
+  }
+}
+
+module.exports = PorscheUIKitCoreSynchronizer;
