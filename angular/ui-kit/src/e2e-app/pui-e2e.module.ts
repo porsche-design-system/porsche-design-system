b793dceafab112d3590ab8219759b397a189d438
Oliver Schlegel <oliver.schlegel@aracom.de>
2018-04-26 09:07:38 +0200

    feat(notification-error-inline): PCCK-12453 Initial implementation

--- angular/ui-kit/src/e2e-app/pui-e2e.module.ts
+++ angular/ui-kit/src/e2e-app/pui-e2e.module.ts
@@ -4,22 +4,25 @@ import { NgModule } from '@angular/core';
 import {PuiE2eRoutingModule} from './pui-e2e-routing.module';
 
 import { PuiE2eComponent } from './pui-e2e.component';
-import {PuiTextModule, PuiIconModule} from '../public_api';
+import {PuiTextModule, PuiIconModule, PuiNotificationModule} from '../public_api';
 import {PuiE2eAtomsTextSizeComponent} from './pages/atoms/text/text-size.component';
 import { PuiE2eAtomsIconComponent } from './pages/atoms/icon/icon.component';
+import { PuiE2eOrganismsNotificationComponent } from './pages/organisms/notification/notification.component';
 
 
 @NgModule({
   declarations: [
     PuiE2eComponent,
     PuiE2eAtomsTextSizeComponent,
-    PuiE2eAtomsIconComponent
+    PuiE2eAtomsIconComponent,
+    PuiE2eOrganismsNotificationComponent
   ],
   imports: [
     BrowserModule,
     PuiE2eRoutingModule,
     PuiTextModule,
-    PuiIconModule
+    PuiIconModule,
+    PuiNotificationModule
   ],
   bootstrap: [PuiE2eComponent]
 })
