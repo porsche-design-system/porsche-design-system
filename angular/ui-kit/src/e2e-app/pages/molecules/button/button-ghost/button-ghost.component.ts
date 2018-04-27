3846582231e6fecc01fa9d543d8b214f4c307d3b
Oliver Schlegel <oliver.schlegel@aracom.de>
2018-04-27 15:01:38 +0200

    Linting issues

--- angular/ui-kit/src/e2e-app/pages/molecules/button/button-ghost/button-ghost.component.ts
+++ angular/ui-kit/src/e2e-app/pages/molecules/button/button-ghost/button-ghost.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'pui-e2e-molecules-button-ghost',
--- angular/ui-kit/src/lib/molecules/image/image-cover/image-cover.module.ts
+++ angular/ui-kit/src/lib/molecules/image/image-cover/image-cover.module.ts
@@ -1,6 +1,6 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {PuiImageCoverComponent} from './image-cover.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PuiImageCoverComponent } from './image-cover.component';
 
 @NgModule({
   imports: [
