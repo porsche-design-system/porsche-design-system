16af47d04f465886b32d3b5dc57f95130e6f6918
Oliver Schlegel <oliver.schlegel@aracom.de>
2018-04-27 13:17:24 +0200

    chore(): PCCK-12453 Refactoring and formatting

--- angular/ui-kit/src/lib/shared/pui-base.component.ts
+++ angular/ui-kit/src/lib/shared/pui-base.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, Input, Renderer2, ElementRef } from '@angular/core';
 
-@Component({
-  selector: 'pui-component',
-  template: ''
-})
-export class PuiBaseComponent {
+export abstract class PuiBaseComponent {
   private currentStyleModifier: string;
 
   @Input() set styleModifier(styleModifier: string) {
