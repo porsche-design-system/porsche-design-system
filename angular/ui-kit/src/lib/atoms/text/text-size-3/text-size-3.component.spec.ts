a3afb71b5b60a127602be9cc55d703c8ef8fc272
Nils Nowak <nowaknils@aol.com>
2018-04-30 15:18:44 +0200

    PCCK-12453 naming fixes

--- angular/ui-kit/src/lib/atoms/text/text-size-3/text-size-3.component.spec.ts
+++ angular/ui-kit/src/lib/atoms/text/text-size-3/text-size-3.component.spec.ts
@@ -1,24 +1,23 @@
-import { By } from '@angular/platform-browser';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { PuiTextSize1Component } from './text-size-3.component';
-import { PuiTextSize1Directive } from './text-size-3.directive';
-import { DebugElement } from '@angular/core';
+import {PuiTextSize3Component} from './text-size-3.component';
+import {PuiTextSize3Directive} from './text-size-3.directive';
+import {DebugElement} from '@angular/core';
 
-describe('Text Size 1 Component', () => {
-  let component: PuiTextSize1Component;
-  let fixture: ComponentFixture<PuiTextSize1Component>;
+describe('Text Size 3 Component', () => {
+  let component: PuiTextSize3Component;
+  let fixture: ComponentFixture<PuiTextSize3Component>;
   let textElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PuiTextSize1Component, PuiTextSize1Directive ]
-    })
-      .compileComponents();
+      declarations: [PuiTextSize3Component, PuiTextSize3Directive]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PuiTextSize1Component);
+    fixture = TestBed.createComponent(PuiTextSize3Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
     textElement = fixture.debugElement.query(By.css('h1'));
@@ -29,12 +28,12 @@ describe('Text Size 1 Component', () => {
   });
 
   it('should show element with the correct class', () => {
-    expect(textElement.nativeElement.classList).toContain('-text-size-1-regular');
+    expect(textElement.nativeElement.classList).toContain('-text-size-3-regular');
   });
 
   it('should switch to correct thin class if input thin is set', () => {
     component.thin = true;
     fixture.detectChanges();
-    expect(textElement.nativeElement.classList).toContain('-text-size-1-thin');
+    expect(textElement.nativeElement.classList).toContain('-text-size-3-thin');
   });
 });
