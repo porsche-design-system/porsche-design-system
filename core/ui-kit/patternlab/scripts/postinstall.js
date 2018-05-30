4f0c5590415778cef616664f04d3901abccfddae
tklae <github@klaesener.net>
2018-05-30 13:02:50 +0200

    Add scripts for publishing to multiple repositories (nexus + artifactory) | el/tk | VRS-1592

--- core/ui-kit/patternlab/scripts/postinstall.js
+++ core/ui-kit/patternlab/scripts/postinstall.js
@@ -1,5 +1,14 @@
 "use strict";
+const symlinkOrCopySync = require('symlink-or-copy').sync;
+var fs = require('fs');
+
 console.log('Beginning Pattern Lab Node Gulp postinstall...');
+
+if (!fs.existsSync('./node_modules/plugin-node-sass-dependency-collector')) {
+  console.log('Symlinking "./node_modules/@porsche/plugin-node-sass-dependency-collector" to "./node_modules/plugin-node-sass-dependency-collector" so Patternlab can find it');
+  symlinkOrCopySync('./node_modules/@porsche/plugin-node-sass-dependency-collector', './node_modules/plugin-node-sass-dependency-collector');
+  console.log('Symlink done');
+}
 
 //call the core library postinstall
 var patternlab = require('patternlab-node/core/scripts/postinstall');
