e9f38fc6f06b4d517b47ff51380e317936a5faf3
Oliver Hook <oliver.hook@mhp.com>
2018-06-21 10:30:30 +0200

    added notification component to docs

---
+++ react/packages/@porsche/ui-kit-react-docs/src/examples/organisms/Notification/Types/index.js
@@ -0,0 +1,13 @@
+import ComponentExample from "src/app/Components/ComponentDoc/ComponentExample/index"
+import ExampleSection from "src/app/Components/ComponentDoc/ExampleSection"
+import React from "react"
+
+const Types = () => (
+    <ExampleSection title="Types">
+        <ComponentExample title="Notification" examplePath="organisms/Notification/Types/NotificationExampleBasic">
+            The default notification component
+        </ComponentExample>
+    </ExampleSection>
+)
+
+export default Types
