7711c1da286aa8008e7ad5e0591f6a22bb27ef6c
Mario Ulmer <mario.ulmer@mhp.com>
2018-07-26 09:03:41 +0200

    PFI-2727: rename to wide; add example to override custom container styles

---
+++ react/packages/@porsche/ui-kit-react-docs/src/examples/organisms/Modal/Types/ModalExampleWideCustomContainer.js
@@ -0,0 +1,43 @@
+import "./ModalExampleWideCustomContainer.css"
+import React from "react"
+import { Modal, Button } from "@porsche/ui-kit-react"
+
+class ModalExampleWide extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { modal: false }
+    }
+
+    handleClick = () => {
+        this.setState({
+            modal: true
+        })
+    }
+
+    handleClose = () => {
+        this.setState({
+            modal: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.handleClick}>Show Modal</Button>
+
+                <Modal
+                    wide
+                    containerClass={"custom-container"}
+                    isOpen={this.state.modal}
+                    onRequestClose={this.handleClose}
+                    ariaHideApp={false}
+                >
+                    I want a fixed width...
+                </Modal>
+            </div>
+        )
+    }
+}
+
+export default ModalExampleWide
