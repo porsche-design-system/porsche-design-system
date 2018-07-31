96c6220bee1fee8de6ceefb9984c333dc03b2c40
Mario Ulmer <mario.ulmer@mhp.com>
2018-07-31 16:19:12 +0200

    PFI-2727: add component

---
+++ react/packages/@porsche/ui-kit-react-docs/src/examples/molecules/Link/Types/LinkExampleWithIcon.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Divider, Flex, Flyout, Spacing, Link } from "@porsche/ui-kit-react"
+
+const LinkExampleBasic = () => {
+    return (
+        <React.Fragment>
+            <Link withIcon>Link</Link>
+        </React.Fragment>
+    )
+}
+
+export default LinkExampleBasic
