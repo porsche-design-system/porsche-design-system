da45cd302d6a1975abf99ff43dae61b885bd3723
Oliver Schlegel <oliver.schlegel@aracom.de>
2018-09-03 10:09:30 +0200

    chore(input): PCCK-13792 Remove angular forms

--- angular/ui-kit/src/lib/molecules/form/input/input.module.ts
+++ angular/ui-kit/src/lib/molecules/form/input/input.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { PuiInputComponent } from './input.component';
 import { PuiIconModule } from '../../../atoms/index';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, PuiIconModule],
+  imports: [CommonModule, PuiIconModule],
   declarations: [PuiInputComponent],
   exports: [PuiInputComponent]
 })
