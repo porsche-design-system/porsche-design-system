510d38c3b14981404086dd487b4d8549ee7130d2
Oliver Hook <oliver.hook@mhp.com>
2018-10-01 17:13:47 +0200

    Optimized item rendering

--- react/packages/@porsche/ui-kit-react-docs/src/examples/molecules/Pagination/Types/PaginationExampleBasic.js
+++ react/packages/@porsche/ui-kit-react-docs/src/examples/molecules/Pagination/Types/PaginationExampleBasic.js
@@ -1,31 +1,8 @@
 import React from "react"
 import { Pagination } from "@porsche/ui-kit-react"
 
-class PaginationExampleBasic extends React.Component {
-    state = {
-        activeItem: "1"
-    }
-
-    onClick = (event, pane) => {
-        this.setState({
-            activeItem: pane.key
-        })
-    }
-
-    render() {
-        const paginationItems = [
-            {
-                key: "1",
-                isActive: this.state.activeItem === "1"
-            },
-            {
-                key: "2",
-                isActive: this.state.activeItem === "2"
-            }
-        ]
-
-        return <Pagination items={paginationItems} />
-    }
+const PaginationExampleBasic = () => {
+    return <Pagination pageTotal={30} />
 }
 
 export default PaginationExampleBasic
