c1a94f274043188383b1c0a2d70a3286650b8266
mario ulmer <mario.ulmer@mhp.com>
2018-10-11 11:12:43 +0200

    Separate examples to emphasise CI incompatibility

---
+++ react/packages/@porsche/ui-kit-react-docs/src/examples/atoms/Text/Types/TextExampleBoldTypes.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Text } from "@porsche/ui-kit-react"
+
+const TextExampleBoldTypes = () => {
+    return (
+        <div>
+            <Text type="1-bold">1-bold</Text>
+            <br />
+            <Text type="2-bold">2-bold</Text>
+            <br />
+            <Text type="3-bold">3-bold</Text>
+            <br />
+            <Text type="4-bold">4-bold</Text>
+            <br />
+            <Text type="5-bold">5-bold</Text>
+            <br />
+            <Text type="copy-bold">copy-bold</Text>
+            <Text type="small-bold">small-bold</Text>
+            <Text type="micro-bold">micro-bold</Text>
+        </div>
+    )
+}
+
+export default TextExampleBoldTypes
--- react/packages/@porsche/ui-kit-react-docs/src/examples/atoms/Text/Types/TextExampleTypes.js
+++ react/packages/@porsche/ui-kit-react-docs/src/examples/atoms/Text/Types/TextExampleTypes.js
@@ -8,30 +8,22 @@ const TextExampleTypes = () => {
             <br />
             <Text type="1-regular">1-regular</Text>
             <Text type="1-thin">1-thin</Text>
-            <Text type="1-bold">1-bold</Text>
             <br />
             <Text type="2-regular">2-regular</Text>
             <Text type="2-thin">2-thin</Text>
-            <Text type="2-bold">2-bold</Text>
             <br />
             <Text type="3-regular">3-regular</Text>
             <Text type="3-thin">3-thin</Text>
-            <Text type="3-bold">3-bold</Text>
             <br />
             <Text type="4-regular">4-regular</Text>
             <Text type="4-thin">4-thin</Text>
-            <Text type="4-bold">4-bold</Text>
             <br />
             <Text type="5-regular">5-regular</Text>
             <Text type="5-thin">5-thin</Text>
-            <Text type="5-bold">5-bold</Text>
             <br />
             <Text type="copy">copy</Text>
-            <Text type="copy-bold">copy-bold</Text>
             <Text type="small">small</Text>
-            <Text type="small-bold">small-bold</Text>
             <Text type="micro">micro</Text>
-            <Text type="micro-bold">micro-bold</Text>
         </div>
     )
 }
