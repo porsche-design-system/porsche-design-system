5a82eb6f14a343873018c51f7a5462db9442e6a5
Stephan Schröter <stephan.schroeter@mhp.com>
2018-12-07 19:23:47 +0100

    Update VRT tester | sas | PUIK

--- core/ui-kit/patternlab/source/_patterns/02-molecules/pagination/pagination.spec.ts
+++ core/ui-kit/patternlab/source/_patterns/02-molecules/pagination/pagination.spec.ts
@@ -1,58 +1,51 @@
 import 'jasmine';
-import * as puppeteer from 'puppeteer';
-import { Browser, Page } from 'puppeteer';
-import { VisualRegressionTester } from './../../../../../vrt/visual-regression-tester';
+import { VisualRegressionTester } from '@myporsche/myservices-visual-regression-tester';
+import { getVisualRegressionTester } from '../../../../../vrt/helpers/setup';
 
 describe('Pagination', () => {
-  let browser: Browser, page: Page, visualRegressionTester: VisualRegressionTester;
+  let visualRegressionTester: VisualRegressionTester;
 
   beforeAll(async () => {
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-    visualRegressionTester = new VisualRegressionTester(page);
+    visualRegressionTester = await getVisualRegressionTester();
   });
 
   it('should have no visual regression | variant:many pages | state:default', async () => {
     expect(await visualRegressionTester.test('pagination~default', async () => {
-      await visualRegressionTester.goTo('02-molecules-pagination-pagination-04-many-pages/02-molecules-pagination-pagination-04-many-pages.rendered.html?disableCSSAnimations');
+      await visualRegressionTester.goTo('/02-molecules-pagination-pagination-04-many-pages/02-molecules-pagination-pagination-04-many-pages.rendered.html?disableCSSAnimations');
     })).toBeFalsy();
   });
 
   it('should have no visual regression | variant:many pages | state:hover', async () => {
     expect(await visualRegressionTester.test('pagination~default-hover', async () => {
-      await visualRegressionTester.goTo('02-molecules-pagination-pagination-04-many-pages/02-molecules-pagination-pagination-04-many-pages.rendered.html?disableCSSAnimations');
-      await page.hover('.pagination__item:nth-child(1) .pagination__goto');
+      await visualRegressionTester.goTo('/02-molecules-pagination-pagination-04-many-pages/02-molecules-pagination-pagination-04-many-pages.rendered.html?disableCSSAnimations');
+      await visualRegressionTester.hover('.pagination__item:nth-child(1) .pagination__goto');
     })).toBeFalsy();
   });
 
   it('should have no visual regression | variant:many pages | state:focus', async () => {
     expect(await visualRegressionTester.test('pagination~default-focus', async () => {
-      await visualRegressionTester.goTo('02-molecules-pagination-pagination-04-many-pages/02-molecules-pagination-pagination-04-many-pages.rendered.html?disableCSSAnimations');
-      await page.focus('.pagination__item:nth-child(1) .pagination__goto');
+      await visualRegressionTester.goTo('/02-molecules-pagination-pagination-04-many-pages/02-molecules-pagination-pagination-04-many-pages.rendered.html?disableCSSAnimations');
+      await visualRegressionTester.focus('.pagination__item:nth-child(1) .pagination__goto');
     })).toBeFalsy();
   });
 
   it('should have no visual regression | variant:many pages inverted | state:default', async () => {
     expect(await visualRegressionTester.test('pagination~default-inverted', async () => {
-      await visualRegressionTester.goTo('02-molecules-pagination-pagination-08-many-pages-inverted/02-molecules-pagination-pagination-08-many-pages-inverted.rendered.html?disableCSSAnimations');
+      await visualRegressionTester.goTo('/02-molecules-pagination-pagination-08-many-pages-inverted/02-molecules-pagination-pagination-08-many-pages-inverted.rendered.html?disableCSSAnimations');
     })).toBeFalsy();
   });
 
   it('should have no visual regression | variant:many pages inverted | state:hover', async () => {
     expect(await visualRegressionTester.test('pagination~default-inverted-hover', async () => {
-      await visualRegressionTester.goTo('02-molecules-pagination-pagination-08-many-pages-inverted/02-molecules-pagination-pagination-08-many-pages-inverted.rendered.html?disableCSSAnimations');
-      await page.hover('.pagination__item:nth-child(1) .pagination__goto');
+      await visualRegressionTester.goTo('/02-molecules-pagination-pagination-08-many-pages-inverted/02-molecules-pagination-pagination-08-many-pages-inverted.rendered.html?disableCSSAnimations');
+      await visualRegressionTester.hover('.pagination__item:nth-child(1) .pagination__goto');
     })).toBeFalsy();
   });
 
   it('should have no visual regression | variant:many pages inverted | state:focus', async () => {
     expect(await visualRegressionTester.test('pagination~default-inverted-focus', async () => {
-      await visualRegressionTester.goTo('02-molecules-pagination-pagination-08-many-pages-inverted/02-molecules-pagination-pagination-08-many-pages-inverted.rendered.html?disableCSSAnimations');
-      await page.focus('.pagination__item:nth-child(1) .pagination__goto');
+      await visualRegressionTester.goTo('/02-molecules-pagination-pagination-08-many-pages-inverted/02-molecules-pagination-pagination-08-many-pages-inverted.rendered.html?disableCSSAnimations');
+      await visualRegressionTester.focus('.pagination__item:nth-child(1) .pagination__goto');
     })).toBeFalsy();
-  });
-
-  afterAll(async () => {
-    await browser.close();
   });
 });
