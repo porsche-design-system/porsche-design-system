9adcedcb01d11e413901f6451134213223f1b0f9
Michael Sindlinger <michael@rockstardevelopers.de>
2019-01-16 10:23:23 +0100

    PFI-3761: Radio Component - add RasdioGroup, update styling

--- react/packages/@porsche/ui-kit-react/src/components/molecules/Radio/Radio.test.tsx
+++ react/packages/@porsche/ui-kit-react/src/components/molecules/Radio/Radio.test.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { RadioProps, Radio } from "./Radio"
 import { mount, ReactWrapper } from "enzyme"
+import { RadioGroupProps } from "./RadioGroup"
 
 describe("Radio", () => {
     const createComponent = (props?: any): ReactWrapper => {
@@ -8,16 +9,83 @@ describe("Radio", () => {
     }
 
     describe("default state", () => {
-        //arrange
-        const defaultProps = {
-            name: "radiotest",
-            value: "test"
-        }
-        //act
-        const component = createComponent()
+        it("sets all pass through properties on the input", () => {
+            //arrange
+            const defaultProps = {
+                name: "radiotest",
+                value: "test"
+            }
 
-        //assert
-        const input = component.find("input")
-        expect(input.getDOMNode().nodeValue).toEqual("")
+            //act
+            const component = createComponent(defaultProps)
+
+            //assert
+            const input = component.find("input")
+            expect(input.prop("checked")).toBe(undefined)
+            expect(input.prop("name")).toBe("radiotest")
+            expect(input.prop("value")).toBe("test")
+        })
+    })
+
+    describe("onChange", () => {
+        it("is called with the value of the input on change", () => {
+            //arrange
+            const onChange = jest.fn()
+            const defaultProps = {
+                name: "radiotest",
+                onChange,
+                value: "test"
+            }
+
+            //act
+            const component = createComponent(defaultProps)
+            const input = component.find("input")
+
+            input.simulate("change")
+
+            //assert
+            expect(onChange).toHaveBeenCalledWith("test", expect.anything(), expect.anything())
+        })
+    })
+})
+
+describe("Radio Group", () => {
+    const createComponent = (
+        defaultRadioGroupProps: RadioGroupProps,
+        defaultRadio1Props: any,
+        defaultRadio2Props: any
+    ): ReactWrapper => {
+        return mount(
+            <Radio.Group name={defaultRadioGroupProps.name} onChange={defaultRadioGroupProps.onChange}>
+                <Radio checked={true} value={defaultRadio1Props.value} />
+                <Radio value={defaultRadio2Props.value} />
+            </Radio.Group>
+        )
+    }
+
+    describe("onChange", () => {
+        it("is called with the value of the input on change", () => {
+            //arrange
+            const onChange = jest.fn()
+            const defaultRadioGroupProps = {
+                name: "radiotest",
+                onChange
+            }
+            const defaultRadio1Props = {
+                value: "radio1"
+            }
+            const defaultRadio2Props = {
+                value: "radio2"
+            }
+
+            //act
+            const component = createComponent(defaultRadioGroupProps, defaultRadio1Props, defaultRadio2Props)
+            const input = component.find("input").first()
+
+            input.simulate("change")
+
+            //assert
+            expect(onChange).toHaveBeenCalledWith(defaultRadio1Props.value, expect.anything(), expect.anything())
+        })
     })
 })
--- react/packages/@porsche/ui-kit-react/src/components/molecules/Radio/RadioGroup.tsx
+++ react/packages/@porsche/ui-kit-react/src/components/molecules/Radio/RadioGroup.tsx
@@ -5,13 +5,22 @@ import { prefix } from "../../../lib"
 import { ClassNameProp, ComponentProp } from "../../../lib/props"
 
 import { Flex } from "../../../index"
+import { RadioProps } from "./Radio"
 
 export interface RadioGroupProps extends ClassNameProp, ComponentProp {
     name: string
+
+    /**
+     * Called when the user attempts to change the selected radio.
+     * @param {string} value The proposed value after the change.
+     * @param {SyntheticEvent} event React's original event.
+     * @param {CheckboxProps} data All props of the component.
+     */
+    onChange?: (value: string, event: React.FormEvent<HTMLInputElement>, data: RadioProps) => void
 }
 
 const _RadioGroup: React.StatelessComponent<RadioGroupProps> = (props) => {
-    const { as, className, children, name, ...rest } = props
+    const { as, className, children, name, onChange, ...rest } = props
 
     const classes = cx(prefix("radio-group"), className)
 
@@ -20,7 +29,7 @@ const _RadioGroup: React.StatelessComponent<RadioGroupProps> = (props) => {
             {React.Children.map(children, (child: any, i) => {
                 return (
                     <div key={i} className={prefix("radio-group__radio")}>
-                        {React.cloneElement(child, { name })}
+                        {React.cloneElement(child, { name, onChange })}
                     </div>
                 )
             })}
