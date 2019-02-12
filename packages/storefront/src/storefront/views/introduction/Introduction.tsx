064cce86c8149e411214965e1c90e3f408a70a33
Chris Albert <mail@christophalbert.com>
2019-02-12 16:35:54 +0100

    Migrated Flex component

--- packages/storefront/src/storefront/views/introduction/Introduction.tsx
+++ packages/storefront/src/storefront/views/introduction/Introduction.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import cx from "classnames"
-import { prefix, Logo } from "@porscheui/porsche-ui-kit"
+import { prefix, Flex } from "@porscheui/porsche-ui-kit"
 import Background from "../../assets/img/bg.png"
 import "./introduction.scss"
 
@@ -8,9 +8,7 @@ class Introduction extends Component {
   render() {
     return (
       <div className={cx(prefix("introduction"))}>
-        <header className={cx(prefix("introduction__header"))}>
-          <Logo />
-        </header>
+        <header className={cx(prefix("introduction__header"))} />
         <main className={cx(prefix("introduction__stage"))}>
           <h1>Porsche UI Kit</h1>
           <img className={cx(prefix("introduction__stage-img"))} src={Background} alt="" />
