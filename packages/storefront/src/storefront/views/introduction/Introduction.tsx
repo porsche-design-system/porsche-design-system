fda6a5bc4946e96d36ee73c87f29e72d42076835
Chris Albert <mail@christophalbert.com>
2019-02-13 11:49:47 +0100

    MDX works

--- packages/storefront/src/storefront/views/introduction/Introduction.tsx
+++ packages/storefront/src/storefront/views/introduction/Introduction.tsx
@@ -1,8 +1,12 @@
-import React, { Component } from "react"
+import React, { Component, Suspense, lazy } from "react"
 import cx from "classnames"
 import { prefix, Flex } from "@porscheui/porsche-ui-kit"
 import Background from "../../assets/img/bg.png"
 import "./introduction.scss"
+
+import { Stories } from "../../../stories"
+
+const Story = lazy(() => Stories.Layout.Flex)
 
 class Introduction extends Component {
   render() {
@@ -11,6 +15,9 @@ class Introduction extends Component {
         <header className={cx(prefix("introduction__header"))} />
         <main className={cx(prefix("introduction__stage"))}>
           <h1>Porsche UI Kit</h1>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Story />
+          </Suspense>
           <img className={cx(prefix("introduction__stage-img"))} src={Background} alt="" />
         </main>
       </div>
