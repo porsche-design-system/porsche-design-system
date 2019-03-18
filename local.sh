a981cd0502052042498a39cbe61c5f0cbd1881a3
Stephan Schröter <stephan.schroeter@mhp.com>
2019-03-18 19:47:07 +0100

    Add vrt ci/cd configuration | cs / sas | #14

--- local.sh
+++ local.sh
@@ -3,7 +3,18 @@
 set -o errexit
 set -o pipefail
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 SCRIPT_DIR="$(cd `dirname ${0}` && pwd)"
+
+if ! [ -x "$(command -v local-env-creator)" ]; then
+  (>&2 printf "${RED}'local-env-creator' binary not found, please install it via:${NC}\n")
+  (>&2 printf "${RED}npm i -g @myporsche/myservices-local-env-creator --registry https://porschedev.jfrog.io/porschedev/api/npm/public-npm/${NC}\n")
+  exit 1;
+fi
+
+local-env-creator
 
 export RUN_UID="$(id -u)"
 export RUN_GID="$(id -g)"
