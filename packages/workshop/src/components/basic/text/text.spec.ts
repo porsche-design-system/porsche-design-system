a280588649d555f9df9eab313c9f6e072728e269
Oliver Hook <oliver.hook@mhp.com>
2019-03-26 15:54:40 +0100

    add vrt text | oh | #94

---
+++ packages/workshop/src/components/basic/text/text.spec.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+
+import "jasmine"
+import { VisualRegressionTester } from "@porscheui/visual-regression-tester"
+import { getVisualRegressionTester } from "../../../../../../vrt/helpers/setup"
+
+describe("Component Text", () => {
+  let visualRegressionTester: VisualRegressionTester
+
+  beforeAll(async () => {
+    visualRegressionTester = await getVisualRegressionTester()
+  })
+
+  it("should have no visual regression", async () => {
+    expect(
+      await visualRegressionTester.test("component-text", async () => {
+        await visualRegressionTester.goTo("/vrt/base/text")
+      })
+    ).toBeFalsy()
+  })
+})
