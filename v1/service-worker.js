c22bc7b203e5c5762d5f3a9944bd996a26151c5f
stephanschroeter <stephanschroeter@users.noreply.github.com>
2019-04-01 12:22:17 +0000

    Automated deployment of Porsche UI Kit v1 (Docs, Sketch Libraries + RSS Feed)

--- v1/service-worker.js
+++ v1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08dd8f667db164cfbe3cb226118616dd.js"
+  "/precache-manifest.27941a2dc16a43bd975f524eb6f4a66f.js"
 );
 
 workbox.clientsClaim();
