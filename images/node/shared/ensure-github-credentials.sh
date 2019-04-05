8d8a549b58e5be7d7672fadd89aada8d4e855567
Stephan Schröter <stephan.schroeter@mhp.com>
2019-04-05 16:56:22 +0200

    Bugfix verify deployment task | sas | #101

--- images/node/shared/ensure-github-credentials.sh
+++ images/node/shared/ensure-github-credentials.sh
@@ -12,12 +12,14 @@ fi
 
 cleanup_credentials() {
   local exit_code=$?
+  echo "cleanup_credentials $(date)"
   echo "Cleaning up credentials"
   rm "/root/.ssh/id_rsa"
   exit ${exit_code}
 }
 
 setup_credentials() {
+  echo "setup_credentials $(date)"
   echo "Setting up Git deploy key credentials"
   mkdir -p "/root/.ssh"
   ssh-keyscan -t rsa github.com > "/root/.ssh/known_hosts"
