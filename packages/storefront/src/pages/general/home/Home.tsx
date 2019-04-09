61426c7b41be2af2955a8bdbe5d3f7c8067e3203
Stephan Schröter <stephan.schroeter@mhp.com>
2019-04-09 17:36:46 +0200

    Add first draft of frontpage layout | sas | #106

--- packages/storefront/src/pages/general/home/Home.tsx
+++ packages/storefront/src/pages/general/home/Home.tsx
@@ -1,118 +1,62 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom"
 import styles from "./home.module.scss"
-import { Button } from "@porsche/ui-kit-react"
-import { Flex, Spacing, Text } from "@porscheui/porsche-ui-kit"
-import { Markdown } from "../../../components/markdown/Markdown"
-import Releases from "../../../releases.json"
+import {Button} from "@porsche/ui-kit-react"
+import {Flex, Spacing, Text} from "@porscheui/porsche-ui-kit"
 
 export const Home: React.FunctionComponent = () => {
   return (
     <React.Fragment>
-      <header className={styles["video-teaser"]}>
-        <video src={require("./assets/porsche-ui-kit.mp4")} autoPlay muted />
-      </header>
+
       <header className={styles.teaser}>
-        <div className={styles.text}>
-          <Text className={styles.headline} as="h1" type="1-bold" align="center">
-            Welcome to the
-            <span className={styles["headline-large"]}>Porsche UI Kit</span>
-          </Text>
-          <Text className={styles.subline} as="p" align="center">
-            A Porsche initiative to empower teams and partners to create great digital Porsche experiences, that feel like being designed by one. <br />
-            United around a global design community with shared tools, methods and components.
-          </Text>
-          <Spacing marginTop={32}>
-            <Flex justifyContent="center" gap={12}>
-              <Flex.Item>
-                <Button type="highlight" as={Link} {...{ to: "/design/introduction" }}>
-                  Start Designing
-                </Button>
-              </Flex.Item>
-              <Flex.Item>
-                <Button type="highlight" as={Link} {...{ to: "/code/introduction" }}>
-                  Start Coding
-                </Button>
-              </Flex.Item>
-            </Flex>
-          </Spacing>
+        <div className={styles.cover}>
+          <video src={require("./assets/porsche-ui-kit.mp4")} autoPlay muted/>
         </div>
       </header>
 
-      <Spacing className={styles["mail-teaser"]} marginTop={64} wrap>
-        <Flex gap={24}>
-          <Flex.Item>
-            <Text as="h3" type="3-bold">
-              Always stay informed
-            </Text>
-            <Text>Join our mailing list and get informed about Porsche UI.</Text>
+      <Spacing marginTop={64}>
+        <Flex gap={32}>
+          <Flex.Item width="one-third">
+            <Text as="h1" type="2-bold">Porsche UI Kit</Text>
+            <Text as="h2" type="4-regular">Design System</Text>
           </Flex.Item>
-          <Flex.Item>
-            <Button as="a" {...{ href: "http://eepurl.com/ghVSjH", target: "_blank" }}>
-              Subscribe to mailing list
-            </Button>
+          <Flex.Item width="two-thirds">
+            <Text type="2-regular">The Porsche UI Kit provides the fundamental elements to build aesthetic, quialitative
+              and intuitive designs. Ready to use as designed Sketch Libraries, coded React Components or simple CSS
+              with compiled HTML. Everything built and tested following the Porsche Quality Standards and Corporate
+              Design.</Text>
+            <Spacing marginTop={32}>
+              <Flex gap={12}>
+                <Flex.Item>
+                  <Button type="highlight" as={Link} {...{to: "/design/introduction"}}>Start Designing</Button>
+                </Flex.Item>
+                <Flex.Item>
+                  <Button type="highlight" as={Link} {...{to: "/code/introduction"}}>Start Coding</Button>
+                </Flex.Item>
+              </Flex>
+            </Spacing>
           </Flex.Item>
         </Flex>
       </Spacing>
 
-      <section className={styles.content}>
-        <Markdown path={require("./home.md")} />
-      </section>
-      <Spacing marginTop={32}>
-        <Text as="h3" type="3-bold">
-          Release History
-        </Text>
-        <Flex>
-          <Flex.Item flex="equal">
-            <Text as="h4" type="copy-bold">
-              Version v1
-            </Text>
-            {Releases && (
-              <ul>
-                {Releases.releases.v1.map((item) => {
-                  return (
-                    <li key={item.version}>
-                      <a href={item.link}>{item.version}</a>
-                    </li>
-                  )
-                })}
-              </ul>
-            )}
-          </Flex.Item>
-          <Flex.Item flex="equal">
-            <Text as="h4" type="copy-bold">
-              Version v0 Core (deprecated)
-            </Text>
-            {Releases && (
-              <ul>
-                {Releases.releases.v0Core.map((item) => {
-                  return (
-                    <li key={item.version}>
-                      <a href={item.link}>{item.version}</a>
-                    </li>
-                  )
-                })}
-              </ul>
-            )}
-          </Flex.Item>
-          <Flex.Item flex="equal">
-            <Text as="h4" type="copy-bold">
-              Version v0 React (deprecated)
-            </Text>
-            {Releases && (
-              <ul>
-                {Releases.releases.v0React.map((item) => {
-                  return (
-                    <li key={item.version}>
-                      <a href={item.link}>{item.version}</a>
-                    </li>
-                  )
-                })}
-              </ul>
-            )}
-          </Flex.Item>
-        </Flex>
+      <Spacing marginTop={80}>
+        <article className={styles.newsletter}>
+          <Flex gap={32}>
+            <Flex.Item width="one-third">
+              <Text as="h1" type="2-bold">Always stay informed</Text>
+            </Flex.Item>
+            <Flex.Item width="two-thirds">
+              <Text type="2-regular">Join our mailing list and get informed about Porsche UI.</Text>
+              <Spacing marginTop={8}>
+                <Text>
+                  <a href="http://eepurl.com/ghVSjH" target="_blank">Subscribe to mailing list</a>
+                </Text>
+              </Spacing>
+            </Flex.Item>
+          </Flex>
+        </article>
       </Spacing>
+
     </React.Fragment>
   )
 }
