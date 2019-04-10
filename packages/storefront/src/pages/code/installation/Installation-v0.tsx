c6aa637384f287296eb03cef5232d846ed406e74
Stephan Schröter <stephan.schroeter@mhp.com>
2019-04-10 18:20:42 +0200

    Update code and faq content sites | sas | #66

--- packages/storefront/src/pages/code/installation/Installation-v0.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { Markdown } from "../../../components/markdown/Markdown"
-
-export const InstallationV0: React.FunctionComponent = () => {
-  return <Markdown path={require("./installation-v0.md")} />
-}
