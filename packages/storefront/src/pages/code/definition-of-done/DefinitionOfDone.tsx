a6938484656091cd5d0de92ddab8e8e99e1d6f79
Marcel Bertram <marcel.bertram@porsche.de>
2019-04-12 12:09:56 +0200

    Structural Changes | mb | #66

--- packages/storefront/src/pages/code/definition-of-done/DefinitionOfDone.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import {Markdown} from "../../../components/markdown/Markdown"
-
-export const DefinitionOfDone: React.FunctionComponent = () => {
-  return <Markdown path={require('./definition-of-done.md')}/>
-}
--- packages/storefront/src/views/Storefront.tsx
+++ packages/storefront/src/views/Storefront.tsx
@@ -15,14 +15,11 @@ import { FAQ as GeneralFAQ } from "../pages/help/faq/FAQ"
 import { Updates as GeneralUpdates } from "../pages/news/updates/Updates"
 import { Versioning as GeneralVersioning } from "../pages/news/versioning/Versioning"
 import { Roadmap as GeneralRoadmap } from "../pages/news/roadmap/Roadmap"
+import { BrowserCompatibility as BasicsBrowserCompatibility } from "../pages/basics/browser-compatibility/BrowserCompatibility"
+import { QualityCriteria as BasicsQualityCriteria } from "../pages/basics/quality-criteria/QualityCriteria"
+import { Accessibility as BasicsAccessibility } from "../pages/basics/accessibility/Accessibility"
 import { License as GeneralLicense } from "../pages/general/license/License"
-import { Accessibility as GuidelinesAccessibility } from "../pages/guidelines/accessibility/Accessibility"
-import { Contribution as DesignContribution } from "../pages/design/contribution/Contribution"
 import { SketchPlugins as DesignSketchPlugins } from "../pages/design/sketch-plugins/SketchPlugins"
-import { DefinitionOfDone as CodeDefinitionOfDone } from "../pages/code/definition-of-done/DefinitionOfDone"
-import { CICD as CodeCICD } from "../pages/code/ci-cd/CICD"
-import { BrowserCompatibility as CodeBrowserCompatibility } from "../pages/code/browser-compatibility/BrowserCompatibility"
-import { Contribution as CodeContribution } from "../pages/code/contribution/Contribution"
 import { Markdown } from "../pages/demo/markdown/Markdown"
 import { Footer } from "../components/footer/Footer"
 
@@ -82,13 +79,10 @@ export const Storefront: React.FunctionComponent = () => {
           <Route path="/news/versioning" component={GeneralVersioning} />
           <Route path="/news/roadmap" component={GeneralRoadmap} />
           <Route path="/general/license" component={GeneralLicense} />
-          <Route path="/guidelines/accessibility" component={GuidelinesAccessibility} />
+          <Route path="/basics/quality-criteria" component={BasicsQualityCriteria} />
+          <Route path="/basics/browser-compatibility" component={BasicsBrowserCompatibility} />
+          <Route path="/basics/accessibility" component={BasicsAccessibility} />
           <Route path="/design/sketch-plugins" component={DesignSketchPlugins} />
-          <Route path="/design/contribution" component={DesignContribution} />
-          <Route path="/code/definition-of-done" component={CodeDefinitionOfDone} />
-          <Route path="/code/ci-cd" component={CodeCICD} />
-          <Route path="/code/browser-compatibility" component={CodeBrowserCompatibility} />
-          <Route path="/code/contribution" component={CodeContribution} />
           <Route path="/demo/markdown" component={Markdown} />
           <Route path="/:category/:story" render={(props) => <Story featureV1={featureShowV1} {...props} />} />
         </Switch>
