d8c5a7d3e27fa77c7b3ea670fd961e4f5d3d5e5f
o-hook <o-hook@users.noreply.github.com>
2019-04-12 07:14:03 +0000

    Automated deployment of Porsche UI Kit (v1): 73d89dac723767eeac5bbe90faf9beb7409d198a

--- v1/service-worker.js
+++ v1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cdca11fa35933b85ed4a76847a66f5fc.js"
+  "./precache-manifest.550d2ffec4456f53ff8bbdaed7274c0e.js"
 );
 
 workbox.clientsClaim();
