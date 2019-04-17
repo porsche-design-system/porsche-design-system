17f9b14b01405d88c2e38becf9308fff56f1ac70
Oliver Hook <oliver.hook@mhp.com>
2019-04-17 21:56:00 +0200

    fixed markdown vrt fixtures | oh

--- packages/storefront/src/components/sidebar/Sidebar.tsx
+++ packages/storefront/src/components/sidebar/Sidebar.tsx
@@ -45,91 +45,93 @@ export const Sidebar: React.FunctionComponent<SidebarProps> = (props) => {
 
   return (
     <div className={style.sidebar}>
-      <Header />
-      <Divider spacing="small" />
+      <div className={style.container}>
+        <Header />
+        <Divider spacing="small" />
 
-      {SidebarLinks &&
-        SidebarLinks.categories.map((item: any) => {
-          return (
-            <div key={item.category} className={style.category}>
-              <Text type="copy-bold">{item.category}</Text>
-              <nav>
-                <ul>
-                  {item.links &&
-                    item.links.map((itemLink: any) => {
-                      return (
-                        <React.Fragment key={itemLink.title}>
-                          {!itemLink.featureV1 || (itemLink.featureV1 && props.featureV1) ? (
-                            <li className={style["nav-item"]}>
-                              <SidebarLink
-                                to={{ pathname: itemLink.path, search: props.featureV1 }}
-                                title={itemLink.title}
-                              />
-                              {itemLink.sublinks && (
-                                <ul>
-                                  {itemLink.sublinks.map((itemSubLink: any) => {
-                                    return (
-                                      <React.Fragment key={itemSubLink.title}>
-                                        {!itemSubLink.featureV1 || (itemSubLink.featureV1 && props.featureV1) ? (
-                                          <li className={style["nav-item"]}>
-                                            <SidebarLink
-                                              to={{ pathname: itemSubLink.path, search: props.featureV1 }}
-                                              title={itemSubLink.title}
-                                            />
-                                          </li>
-                                        ) : null}
-                                      </React.Fragment>
-                                    )
-                                  })}
-                                </ul>
-                              )}
-                            </li>
-                          ) : null}
-                        </React.Fragment>
-                      )
-                    })}
-                </ul>
-              </nav>
-            </div>
-          )
-        })}
-
-      {props.featureV1 && (
-        <React.Fragment>
-          <Divider spacing="small" />
-          <Text type="4-bold" as="h2">
-            Components
-          </Text>
-          {categories.map((category) => {
-            const stories = Object.keys((Stories as any)[category])
-            if (!stories) {
-              return <Redirect to="/general/home" />
-            }
+        {SidebarLinks &&
+          SidebarLinks.categories.map((item: any) => {
             return (
-              <SidebarCategory key={category} title={category}>
+              <div key={item.category} className={style.category}>
+                <Text type="copy-bold">{item.category}</Text>
                 <nav>
                   <ul>
-                    {stories.map((story) => {
-                      return (
-                        <li className={style["nav-item"]} key={story}>
-                          <SidebarLink
-                            to={{
-                              pathname: `/${category.toLowerCase()}/${story.toLowerCase()}`,
-                              search: `${props.featureV1}`
-                            }}
-                            title={story}
-                          />
-                        </li>
-                      )
-                    })}
+                    {item.links &&
+                      item.links.map((itemLink: any) => {
+                        return (
+                          <React.Fragment key={itemLink.title}>
+                            {!itemLink.featureV1 || (itemLink.featureV1 && props.featureV1) ? (
+                              <li className={style["nav-item"]}>
+                                <SidebarLink
+                                  to={{ pathname: itemLink.path, search: props.featureV1 }}
+                                  title={itemLink.title}
+                                />
+                                {itemLink.sublinks && (
+                                  <ul>
+                                    {itemLink.sublinks.map((itemSubLink: any) => {
+                                      return (
+                                        <React.Fragment key={itemSubLink.title}>
+                                          {!itemSubLink.featureV1 || (itemSubLink.featureV1 && props.featureV1) ? (
+                                            <li className={style["nav-item"]}>
+                                              <SidebarLink
+                                                to={{ pathname: itemSubLink.path, search: props.featureV1 }}
+                                                title={itemSubLink.title}
+                                              />
+                                            </li>
+                                          ) : null}
+                                        </React.Fragment>
+                                      )
+                                    })}
+                                  </ul>
+                                )}
+                              </li>
+                            ) : null}
+                          </React.Fragment>
+                        )
+                      })}
                   </ul>
                 </nav>
-              </SidebarCategory>
+              </div>
             )
           })}
-        </React.Fragment>
-      )}
-      <Footer />
+
+        {props.featureV1 && (
+          <React.Fragment>
+            <Divider spacing="small" />
+            <Text type="4-bold" as="h2">
+              Components
+            </Text>
+            {categories.map((category) => {
+              const stories = Object.keys((Stories as any)[category])
+              if (!stories) {
+                return <Redirect to="/general/home" />
+              }
+              return (
+                <SidebarCategory key={category} title={category}>
+                  <nav>
+                    <ul>
+                      {stories.map((story) => {
+                        return (
+                          <li className={style["nav-item"]} key={story}>
+                            <SidebarLink
+                              to={{
+                                pathname: `/${category.toLowerCase()}/${story.toLowerCase()}`,
+                                search: `${props.featureV1}`
+                              }}
+                              title={story}
+                            />
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </nav>
+                </SidebarCategory>
+              )
+            })}
+          </React.Fragment>
+        )}
+        <Footer />
+      </div>
     </div>
   )
 }
