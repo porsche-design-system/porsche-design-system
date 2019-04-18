6189acb2a8727be4f84112436b0500244eff7cee
Stephan Schröter <stephan.schroeter@mhp.com>
2019-04-18 10:51:29 +0200

    Add e2e test automation | sas | #119

---
+++ e2e/helpers/setup.ts
@@ -0,0 +1,25 @@
+import 'jasmine'
+import * as puppeteer from 'puppeteer'
+import { Browser, Page } from 'puppeteer'
+
+let browser: Browser
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000
+
+afterAll(async () => {
+  if (browser) {
+    await browser.close()
+  }
+})
+
+export async function getPage(url: string): Promise<Page> {
+  if (!browser) {
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+  }
+  const page = await browser.newPage()
+  await page.goto(`http://localhost:3000${url}`, {waitUntil: 'networkidle0'})
+
+  return page
+}
