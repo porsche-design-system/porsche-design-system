585c1c6b2d0541ae1747f2c544deb3283d8aca2e
Stephan Schröter <stephan.schroeter@mhp.com>
2019-04-24 15:47:21 +0200

    Add semicolons for spec files | sas

--- e2e/helpers/setup.ts
+++ e2e/helpers/setup.ts
@@ -1,26 +1,26 @@
-import 'jasmine'
-import * as puppeteer from 'puppeteer'
-import { Browser, Page } from 'puppeteer'
+import 'jasmine';
+import * as puppeteer from 'puppeteer';
+import { Browser, Page } from 'puppeteer';
 
-let browser: Browser
+let browser: Browser;
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
 
 afterAll(async () => {
   if (browser) {
-    await browser.close()
+    await browser.close();
   }
-})
+});
 
 export async function getPage(url: string): Promise<Page> {
   if (!browser) {
     browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
-    })
+    });
   }
-  const page = await browser.newPage()
-  await page.setDefaultNavigationTimeout(90000)
-  await page.goto(`http://localhost:3000${url}`, {waitUntil: 'networkidle0'})
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(90000);
+  await page.goto(`http://localhost:3000${url}`, {waitUntil: 'networkidle0'});
 
-  return page
+  return page;
 }
