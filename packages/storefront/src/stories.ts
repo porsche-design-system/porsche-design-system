02024f9665dc00f3d1c4e04c33899b2011c9ba4a
Stephan Schröter <stephan.schroeter@mhp.com>
2019-05-29 18:37:52 +0200

    Improve navigation link label generation | sas | #133

--- packages/storefront/src/stories.ts
+++ packages/storefront/src/stories.ts
@@ -13,12 +13,12 @@ export interface Stories {
   };
 }
 export const Stories: Stories = {
-  Basic: {
-    Color: {
+  "Basic": {
+    "Color": {
       design: importMDX("../../ui-kit-js/src/components/basic/color/color.design.mdx"),
       code: importMDX("../../ui-kit-js/src/components/basic/color/color.code.mdx")
     },
-    Typography: {
+    "Typography": {
       design: importMDX("../../ui-kit-js/src/components/basic/typography/typography.design.mdx"),
       code: importMDX("../../ui-kit-js/src/components/basic/typography/typography.code.mdx"),
       vrt: importMDX("../../ui-kit-js/src/components/basic/typography/typography.vrt.mdx"),
@@ -28,44 +28,44 @@ export const Stories: Stories = {
       ]
     }
   },
-  Action: {
-    Buttonregular: {
+  "Action": {
+    "Button Regular": {
       design: importMDX("../../ui-kit-js/src/components/action/button-regular/button-regular.design.mdx"),
       code: importMDX("../../ui-kit-js/src/components/action/button-regular/button-regular.code.mdx"),
       vrt: importMDX("../../ui-kit-js/src/components/action/button-regular/button-regular.vrt.mdx"),
       docs: [importMDX("../../ui-kit-js/src/components/action/button-regular/button-regular.prop.mdx")]
     }
   },
-  Feedback: {
-    Loader: {
+  "Feedback": {
+    "Loader": {
       design: importMDX("../../ui-kit-js/src/components/feedback/loader/loader.design.mdx"),
       code: importMDX("../../ui-kit-js/src/components/feedback/loader/loader.code.mdx"),
       vrt: importMDX("../../ui-kit-js/src/components/feedback/loader/loader.vrt.mdx"),
       docs: [importMDX("../../ui-kit-js/src/components/feedback/loader/loader.prop.mdx")]
     }
   },
-  Icon: {
-    Icon: {
+  "Icon": {
+    "Icon": {
       design: importMDX("../../ui-kit-js/src/components/icon/icon/icon.design.mdx"),
       code: importMDX("../../ui-kit-js/src/components/icon/icon/icon.code.mdx"),
       vrt: importMDX("../../ui-kit-js/src/components/icon/icon/icon.vrt.mdx"),
       docs: [importMDX("../../ui-kit-js/src/components/icon/icon/icon.prop.mdx")]
     }
   },
-  Layout: {
-    Flex: {
+  "Layout": {
+    "Flex": {
       design: importMDX("../../workshop/src/components/layout/flex/flex.design.mdx"),
       code: importMDX("../../workshop/src/components/layout/flex/flex.code.mdx"),
       vrt: importMDX("../../workshop/src/components/layout/flex/flex.vrt.mdx"),
       props: ["Flex", "FlexItem"]
     },
-    Grid: {
+    "Grid": {
       design: importMDX("../../workshop/src/components/layout/grid/grid.design.mdx"),
       code: importMDX("../../workshop/src/components/layout/grid/grid.code.mdx"),
       vrt: importMDX("../../workshop/src/components/layout/grid/grid.vrt.mdx"),
       props: ["Grid", "GridChild"]
     },
-    Spacing: {
+    "Spacing": {
       design: importMDX("../../workshop/src/components/layout/spacing/spacing.design.mdx"),
       code: importMDX("../../workshop/src/components/layout/spacing/spacing.code.mdx"),
       vrt: importMDX("../../workshop/src/components/layout/spacing/spacing.vrt.mdx"),
