42e935578dbc4428447ad7add7875de1a853f977
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-03 11:47:57 +0200

    Stencil test | sas

---
+++ stencil/build/p-vupidygm.entry.js
@@ -0,0 +1,1 @@
+import{d as t,e}from"./p-f34f3f79.js";class r{constructor(e){t(this,e)}getText(){return(this.first||"")+((t=this.middle)?` ${t}`:"")+((e=this.last)?` ${e}`:"");var t,e}render(){return e("div",null,"1Hello, World! I'm ",this.getText())}static get style(){return"div{color:red}"}}export{r as my_component};
