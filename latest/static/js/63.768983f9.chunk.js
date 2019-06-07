2791683cd123446223690d626518a7eec5506f55
o-hook <o-hook@users.noreply.github.com>
2019-06-07 10:33:04 +0000

    Automated deployment of Porsche UI Kit (v1): df8f75688f587f3a6a356ce8f7f97cfd99572406

--- latest/static/js/63.768983f9.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[63],{296:function(n,o,t){"use strict";t.r(o),t.d(o,"p_color",function(){return i});var c=t(72),i=function(){return function(n){Object(c.h)(this,n)}}()}}]);
-//# sourceMappingURL=63.768983f9.chunk.js.map
