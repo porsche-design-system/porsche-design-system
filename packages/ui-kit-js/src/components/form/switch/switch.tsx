f95d9ef542192543ab544abc47c22a76192b34ea
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-11 13:36:02 +0200

    Add select switch docu | sas | #151

---
+++ packages/ui-kit-js/src/components/form/switch/switch.tsx
@@ -0,0 +1,44 @@
+import { JSX, Component, Prop, h } from "@stencil/core";
+import cx from "classnames";
+import { prefix } from "../../../utils";
+
+@Component({
+  tag: "p-switch",
+  styleUrl: "switch.scss",
+  shadow: true
+})
+export class Switch {
+
+  @Prop() name?: string = "";
+
+  @Prop() value?: string = "";
+
+  @Prop() disabled?: boolean = false;
+
+  @Prop() checked?: boolean = false;
+
+  render(): JSX.Element {
+    const switchClasses = cx(prefix("switch"));
+    const checkboxClasses = cx(prefix("switch__checkbox"));
+    const sliderClasses = cx(prefix("switch__slider"));
+    const iconInactiveClasses = cx(prefix("switch__icon"), prefix("switch__icon--inactive"));
+    const iconActiveClasses = cx(prefix("switch__icon"), prefix("switch__icon--active"));
+
+    return (
+      <label class={switchClasses}>
+        <input
+          class={checkboxClasses}
+          name={this.name}
+          value={this.value}
+          type="checkbox"
+          disabled={this.disabled}
+          checked={this.checked}
+        />
+        <span class={sliderClasses}>
+          <p-icon class={iconInactiveClasses} source="minus" />
+          <p-icon class={iconActiveClasses} source="check" />
+        </span>
+      </label>
+    );
+  }
+}
