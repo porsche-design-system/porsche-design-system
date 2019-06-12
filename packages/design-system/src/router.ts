02de2947b4d05231ef5f1b11d36559db06aa5ff4
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-12 20:49:01 +0200

    Add routing | sas | #164

--- packages/design-system/src/router.ts
+++ packages/design-system/src/router.ts
@@ -12,12 +12,14 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/:category/:page',
+      name: 'page',
+      component: () => import('./views/Page.vue'),
+    },
+    {
+      path: '/components/:category/:component',
+      name: 'component',
+      component: () => import('./views/Component.vue'),
     },
   ],
 });
