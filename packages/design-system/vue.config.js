525be14d5802385b5962ae23150191e4ba5186bf
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-12 22:47:07 +0200

    Add mdx loader for VueJS | sas | #164

--- packages/design-system/vue.config.js
+++ packages/design-system/vue.config.js
@@ -1,14 +1,27 @@
 module.exports = {
   publicPath: './',
-  chainWebpack: config => {
-    config.module
-      .rule('markdown')
-      .test(/\.(md)(\?.*)?$/)
-      .use('file-loader')
-      .loader('file-loader')
-      .tap(options => {
-        return {...options, ...{name: 'markdown/[name].[hash:8].[ext]'}};
-      })
-      .end()
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(mdx)(\?.*)?$/,
+          use: [
+            'babel-loader',
+            '@mdx-js/vue-loader'
+          ]
+        },
+        {
+          test: /\.(md)(\?.*)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'markdown/[name].[hash:8].[ext]'
+              }
+            }
+          ]
+        }
+      ]
+    }
   }
 };
