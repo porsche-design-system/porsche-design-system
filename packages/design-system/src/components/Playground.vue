195763d43c611d4c40509045e261805666282847
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-12 22:47:07 +0200

    Add mdx loader for VueJS | sas | #164

---
+++ packages/design-system/src/components/Playground.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="playground">
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+
+  @Component
+  export default class Playground extends Vue {
+  }
+</script>
+
+<style scoped lang="scss">
+  .playground {
+    padding: 30px;
+    border: 1px solid deeppink;
+  }
+</style>
--- packages/design-system/src/views/Home.vue
+++ packages/design-system/src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
     <img alt="Vue logo" src="../assets/logo.png">
+    <Accessibility/>
   </div>
 </template>
+
+<script>
+  import Accessibility from '../pages/basics/accessibility.mdx';
+
+  export default {
+    name: 'app',
+    components: {
+      Accessibility,
+    },
+  };
+</script>
