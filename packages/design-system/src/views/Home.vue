89102b530bbdd4e7b813e0d56be0a5d316fb7b7a
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-12 20:57:46 +0200

    Add markdown pages | sas | #164

--- packages/design-system/src/views/Home.vue
+++ packages/design-system/src/views/Home.vue
@@ -1,18 +1,5 @@
 <template>
-  <div class="home">
+  <div>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
-</script>
