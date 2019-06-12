01b7136e39bc0775baebe2b02cbfb472b030f500
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-12 20:30:51 +0200

    Add custom element support for VueJS | sas | #164

--- packages/design-system/src/main.ts
+++ packages/design-system/src/main.ts
@@ -2,8 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { defineCustomElements } from '@porscheui/ui-kit-js/loader';
 
 Vue.config.productionTip = false;
+Vue.config.ignoredElements = [/p-\w*/];
+
+defineCustomElements(window);
 
 new Vue({
   router,
