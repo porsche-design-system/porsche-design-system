7ac8ec815210f617665e038bdf12b65e60554ab5
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-14 14:32:52 +0200

    tab navigation to design system | sas | #164

--- packages/design-system/src/views/Component.vue
+++ packages/design-system/src/views/Component.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
     <h1>Component</h1>
+    <Tabs>
+      <Tab name="Design" :selected="true">design</Tab>
+      <Tab name="Code">code</Tab>
+      <Tab name="Props">props</Tab>
+    </Tabs>
   </div>
 </template>
+
+<script lang="js">
+  import {Component, Vue} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+  import Tab from '@/components/Tab.vue';
+
+  @Component({
+    components: {
+      Tabs,
+      Tab,
+    },
+  })
+  export default class Page extends Vue {}
+</script>
