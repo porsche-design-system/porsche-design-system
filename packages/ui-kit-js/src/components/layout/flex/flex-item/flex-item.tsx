f45b2133834bc6b8fc2cac681628f28a430c9960
Oliver Hook <oliver.hook@mhp.com>
2019-06-14 11:46:24 +0200

    optimized typings of BreakpointCustomizable type | oh

--- packages/ui-kit-js/src/components/layout/flex/flex-item/flex-item.tsx
+++ packages/ui-kit-js/src/components/layout/flex/flex-item/flex-item.tsx
@@ -12,36 +12,38 @@ export class FlexItem {
    * The width of the flex item. You can also supply values for specific breakpoints, like {base: "full", l: "one-quarter"}. You always need to provide a base value when doing this.
    */
   @Prop() width?: BreakpointCustomizable<
-    "auto" | "one-quarter" | "one-third" | "half" | "two-thirds" | "three-quarters" | "full" | any
-  > = "auto";
+    "auto" | "one-quarter" | "one-third" | "half" | "two-thirds" | "three-quarters" | "full"
+  > &
+    string = "auto";
 
   /**
    * The offset of the column. You can also supply values for specific breakpoints, like {base: "none", l: "one-quarter"}. You always need to provide a base value when doing this.
    */
 
   @Prop() offset?: BreakpointCustomizable<
-    "none" | "one-quarter" | "one-third" | "half" | "two-thirds" | "three-quarters" | any
-  > = "none";
+    "none" | "one-quarter" | "one-third" | "half" | "two-thirds" | "three-quarters"
+  > &
+    string = "none";
   /**
    * Defines how this flex item is aligned along the cross axis. This overwrites the cross axis alignment set by the container. Corresponds to the "alignSelf" css property.
    */
-  @Prop() alignSelf?: BreakpointCustomizable<"auto" | "start" | "end" | "center" | "baseline" | "stretch" | any> =
+  @Prop() alignSelf?: BreakpointCustomizable<"auto" | "start" | "end" | "center" | "baseline" | "stretch"> & string =
     "auto";
 
   /**
    * The ability to allow/disallow the flex child to grow.
    */
-  @Prop() grow?: BreakpointCustomizable<0 | 1 | any> = 0;
+  @Prop() grow?: BreakpointCustomizable<"0" | "1"> & string = "0";
 
   /**
    * The ability to allow/disallow the flex child to shrink.
    */
-  @Prop() shrink?: BreakpointCustomizable<1 | 0 | any> = 1;
+  @Prop() shrink?: BreakpointCustomizable<"1" | "0"> & string = "1";
 
   /**
    * The shorthand property for the combined definition of "shrink", "grow" and "basis"
    */
-  @Prop() flex?: BreakpointCustomizable<"initial" | "auto" | "none" | "equal" | any> = "initial";
+  @Prop() flex?: BreakpointCustomizable<"initial" | "auto" | "none" | "equal"> & string = "initial";
 
   render(): JSX.Element {
     const isJsonString = (str: string) => {
@@ -62,8 +64,8 @@ export class FlexItem {
       this.width !== "auto" && mapBreakpointPropToClasses("flex__item--width", parseProp(this.width)),
       this.offset !== "none" && mapBreakpointPropToClasses("flex__item--offset", parseProp(this.offset)),
       this.alignSelf !== "auto" && mapBreakpointPropToClasses("flex__item--align-self", parseProp(this.alignSelf)),
-      this.grow !== 0 && mapBreakpointPropToClasses("flex__item--grow", parseProp(this.grow)),
-      this.shrink !== 1 && mapBreakpointPropToClasses("flex__item--shrink", parseProp(this.shrink)),
+      this.grow !== "0" && mapBreakpointPropToClasses("flex__item--grow", parseProp(this.grow)),
+      this.shrink !== "1" && mapBreakpointPropToClasses("flex__item--shrink", parseProp(this.shrink)),
       this.flex !== "initial" && mapBreakpointPropToClasses("flex__item-", parseProp(this.flex))
     );
 
--- packages/ui-kit-js/src/components/layout/grid/grid/grid.tsx
+++ packages/ui-kit-js/src/components/layout/grid/grid/grid.tsx
@@ -8,10 +8,10 @@ import { BreakpointCustomizable, mapBreakpointPropToClasses, prefix } from "../.
 })
 export class Grid {
   /** Defines the direction of the main and cross axis. The default "row" defines the main axis as horizontal left to right. Also defines the direction for specific breakpoints, like {"base": "column", "l": "row"}. You always need to provide a base value when doing this. */
-  @Prop() direction?: BreakpointCustomizable<"row" | "row-reverse" | "column" | "column-reverse" | any> = "row";
+  @Prop() direction?: BreakpointCustomizable<"row" | "row-reverse" | "column" | "column-reverse"> & string = "row";
 
   /** Defines the gap between contained children. The value "normal" (default) sets responsive grid spacings that should be used together with Grid.Child. Also defines the gap for specific breakpoints, like {"base": "zero", "l": "normal"}. You always need to provide a base value when doing this. */
-  @Prop() gap?: BreakpointCustomizable<"normal" | "zero" | any> = "normal";
+  @Prop() gap?: BreakpointCustomizable<"normal" | "zero"> & string = "normal";
 
   render(): JSX.Element {
     const isJsonString = (str: string) => {
