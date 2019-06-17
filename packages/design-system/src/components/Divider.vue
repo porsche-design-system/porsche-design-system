ae7151626c89d90b4d3b323d3fddd450681ba352
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-17 13:08:17 +0200

    Add divider component | sas | #164

---
+++ packages/design-system/src/components/Divider.vue
@@ -0,0 +1,13 @@
+<template>
+  <hr class="divider"/>
+</template>
+
+<style scoped lang="scss">
+  @import "~@porscheui/ui-kit-js/src/styles/utility/index";
+
+  .divider {
+    border: 0;
+    height: 1px;
+    background: $p-color-neutral-grey-2;
+  }
+</style>
