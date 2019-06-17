9e1d15d3c6270a6baed94f24ae54394d6bcb56bc
Stephan Schröter <stephan.schroeter@mhp.com>
2019-06-17 11:44:48 +0200

    Update marque component | sas | #164

--- packages/design-system/src/App.vue
+++ packages/design-system/src/App.vue
@@ -27,6 +27,14 @@
   export default class App extends Vue {}
 </script>
 
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
+
 <style scoped lang="scss">
   aside {
     position: fixed;
