b0ee0a8d1dfd240e03ce2c2111467cce9b08510a
Stephan Schröter <stephan.schroeter@mhp.com>
2019-07-01 16:08:24 +0200

    Add license | sas | #164

---
+++ packages/design-system/src/views/License.vue
@@ -0,0 +1,25 @@
+<template>
+  <Markdown>
+    <component :is="license" v-if="license"></component>
+  </Markdown>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  import Markdown from '@/components/Markdown.vue';
+
+  @Component({
+    components: {
+      Markdown,
+    },
+  })
+  export default class License extends Vue {
+
+    private license: any = null;
+
+    private async mounted(): Promise<void> {
+      const empty = '';
+      this.license = (await (() => import(`@/pages/${empty}license.md`))()).default;
+    }
+  }
+</script>
