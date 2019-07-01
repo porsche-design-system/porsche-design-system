c1f5158b8c496af181118daccf5ff9d3dd502a42
Stephan Schröter <stephan.schroeter@mhp.com>
2019-07-01 16:50:25 +0200

    Add unit tests | sas | #164

--- packages/design-system/src/services/utils.ts
+++ packages/design-system/src/services/utils.ts
@@ -1,16 +1,17 @@
 export const encodeUrl = (param: string): string => {
-  return param.toLowerCase().replace(' ', '-');
+  return param.toLowerCase().replace(/\s+/g, '-');
 };
 
 export const decodeUrl = (param: string): string => {
   return param
     .toLowerCase()
-    .replace('-', ' ')
+    .replace(/-+/g, ' ')
     .split(' ')
     .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
     .join(' ');
 };
 
-export const featureToggle = (feature: 'Q2/2019 Components'): boolean => {
+export type Feature = 'Q2/2019 Components';
+export const featureToggle = (feature: Feature): boolean => {
   return localStorage[`Feature: ${feature}`] === 'true';
 };
