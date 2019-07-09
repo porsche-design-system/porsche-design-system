25cc0aac6ce0bd2a18085e87cdc4ddd994f6cb89
o-hook <o-hook@users.noreply.github.com>
2019-07-09 07:45:54 +0000

    Automated deployment of Porsche UI Kit (v1): 07e0d917e82a090d9e7b846f573f4d43a000850f

---
+++ v1/js/chunk-2d21624f.e406ca72.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21624f"],{c0e1:function(e,t,n){"use strict";n.r(t);var c=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"vmark"},[n("h1",[e._v("Text List")]),n("p",[e._v("TBD")])])}],r=n("2be6"),a={},l=Object(r["a"])(a,c,s,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d21624f.e406ca72.js.map
