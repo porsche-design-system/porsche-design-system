3830516ccb5dc05b87707191b84447c25a1fb03e
Oliver Hook <oliver.hook@mhp.com>
2019-07-10 10:36:10 +0200

    fixed IE11 storefront bug | oh

--- packages/design-system/src/App.vue
+++ packages/design-system/src/App.vue
@@ -1,141 +1,143 @@
 <template>
   <div id="app">
-    <aside class="sidebar" :class="{'is-visible': isVisible()}">
-      <Header/>
-      <Divider spacing="small"/>
-      <Sidebar/>
-      <Divider spacing="small"/>
-      <Footer/>
+    <aside class="sidebar" :class="{ 'is-visible': isVisible() }">
+      <Header />
+      <Divider spacing="small" />
+      <Sidebar />
+      <Divider spacing="small" />
+      <Footer />
     </aside>
-    <main class="content" :class="{'is-hidden': isHidden()}">
-      <router-view/>
+    <main class="content" :class="{ 'is-hidden': isHidden() }">
+      <router-view />
     </main>
     <Menu class="menu" @toggle="onToggleMenu"></Menu>
   </div>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from 'vue-property-decorator';
-  import Header from '@/components/Header.vue';
-  import Sidebar from '@/components/Sidebar.vue';
-  import Footer from '@/components/Footer.vue';
-  import Divider from '@/components/Divider.vue';
-  import Menu from '@/components/Menu.vue';
+import { Component, Vue } from 'vue-property-decorator';
+import Header from '@/components/Header.vue';
+import Sidebar from '@/components/Sidebar.vue';
+import Footer from '@/components/Footer.vue';
+import Divider from '@/components/Divider.vue';
+import Menu from '@/components/Menu.vue';
 
-  @Component({
-    components: {
-      Header,
-      Sidebar,
-      Footer,
-      Divider,
-      Menu,
-    },
-  })
-  export default class App extends Vue {
-
-    public isMenuActive: boolean = false;
-
-    public onToggleMenu(isActive: boolean): void {
-      this.isMenuActive = isActive;
-    }
-
-    public isHidden(): boolean {
-      return this.isMenuActive;
-    }
-
-    public isVisible(): boolean {
-      return this.isMenuActive;
-    }
+@Component({
+  components: {
+    Header,
+    Sidebar,
+    Footer,
+    Divider,
+    Menu
   }
+})
+export default class App extends Vue {
+  public isMenuActive: boolean = false;
+
+  public onToggleMenu(isActive: boolean): void {
+    this.isMenuActive = isActive;
+  }
+
+  public isHidden(): boolean {
+    return this.isMenuActive;
+  }
+
+  public isVisible(): boolean {
+    return this.isMenuActive;
+  }
+}
 </script>
 
 <style lang="scss">
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
+main {
+  display: block;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
 
 <style scoped lang="scss">
-  @import "~@porscheui/ui-kit-js/src/styles/utility/index";
+@import '~@porscheui/ui-kit-js/src/styles/utility/index';
 
-  #app {
-    overflow-x: hidden;
-  }
+#app {
+  overflow-x: hidden;
+}
 
-  .sidebar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: rem(280px);
-    height: 100vh;
-    z-index: 10;
-    padding: $p-spacing-40 $p-spacing-32;
-    border-right: 1px solid $p-color-neutral-grey-2;
-    background: $p-color-porsche-light;
-    overflow-x: hidden;
-    overflow-y: auto;
-    -webkit-overflow-scrolling: touch;
+.sidebar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: rem(280px);
+  height: 100vh;
+  z-index: 10;
+  padding: $p-spacing-40 $p-spacing-32;
+  border-right: 1px solid $p-color-neutral-grey-2;
+  background: $p-color-porsche-light;
+  overflow-x: hidden;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
 
-    @include breakpoint("xxs", "s") {
-      transition: transform .3s;
-      transform: translate3d(rem(-140px), 0, 0);
+  @include breakpoint('xxs', 's') {
+    transition: transform 0.3s;
+    transform: translate3d(rem(-140px), 0, 0);
 
-      &.is-visible {
-        transform: translate3d(0, 0, 0);
-      }
+    &.is-visible {
+      transform: translate3d(0, 0, 0);
     }
   }
+}
 
-  .content {
-    position: relative;
-    min-height: 100vh;
-    padding: $p-spacing-32;
-    background: $p-color-porsche-light;
-    z-index: 20;
+.content {
+  position: relative;
+  min-height: 100vh;
+  padding: $p-spacing-32;
+  background: $p-color-porsche-light;
+  z-index: 20;
 
-    @include breakpoint("xxs", "s") {
-      transition: transform .3s;
+  @include breakpoint('xxs', 's') {
+    transition: transform 0.3s;
+
+    &::after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      height: 0;
+      background: $p-color-porsche-light;
+      opacity: 0;
+      transition: opacity 0.3s;
+    }
+
+    &.is-hidden {
+      transform: translate3d(rem(280px), 0, 0);
 
       &::after {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        height: 0;
-        background: $p-color-porsche-light;
-        opacity: 0;
-        transition: opacity .3s;
+        min-height: 100vh;
+        height: 100%;
+        opacity: 0.95;
       }
-
-      &.is-hidden {
-        transform: translate3d(rem(280px), 0, 0);
-
-        &::after {
-          min-height: 100vh;
-          height: 100%;
-          opacity: .95;
-        }
-      }
-    }
-
-    @include breakpoint("s") {
-      padding: $p-spacing-64;
-      margin-left: rem(280px);
     }
   }
 
-  .menu {
-    position: fixed;
-    top: 0;
-    right: 0;
-    z-index: 30;
-    display: none;
-
-    @include breakpoint("xxs", "s") {
-      display: block;
-    }
+  @include breakpoint('s') {
+    padding: $p-spacing-64;
+    margin-left: rem(280px);
   }
+}
+
+.menu {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 30;
+  display: none;
+
+  @include breakpoint('xxs', 's') {
+    display: block;
+  }
+}
 </style>
