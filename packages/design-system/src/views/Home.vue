5173d249a5b426ab7eca15b81b6a6405f8b47503
Stephan Schröter <stephan.schroeter@mhp.com>
2019-07-10 11:09:37 +0200

    Add vrt test setup for design-system | sas | #164

--- packages/design-system/src/views/Home.vue
+++ packages/design-system/src/views/Home.vue
@@ -16,28 +16,28 @@
         <p-grid-child size='{"base": 12, "m": 4}'>
           <p-headline type="headline-3" level="1">Porsche UI Kit</p-headline>
           <p-text type="24">Design System</p-text>
-          <p-text class="p-spacing-mt--8">
+          <p-text class="p-spacing-mt-8">
             <router-link to="/getting-started/about">Learn more about it</router-link>
           </p-text>
         </p-grid-child>
-        <p-grid-child class="p-spacing-mt--24 p-spacing-mt--0-m" size='{"base": 12, "m": 8}'>
+        <p-grid-child class="p-spacing-mt-24 p-spacing-mt-0-min-m" size='{"base": 12, "m": 8}'>
           <p-text type="24">
             The Porsche UI Kit provides the design fundamentals for easily creating aesthetic and qualitative
             digital products. Ready to use as reusable Sketch libraries, coded React components or HTML and CSS
             elements. Everything built and tested following the Porsche quality standards and corporate design
             principles.
           </p-text>
-          <p-text class="p-spacing-mt--16">
+          <p-text class="p-spacing-mt-16">
             While Porsche UI Kit v1 is coming soon as npm package with code examples and design documentation have
             a look at v0 documentation of <a href="https://ui.porsche.com/v0/core/">Porsche UI Kit – Core v0</a>
             and <a href="https://ui.porsche.com/v0/react/">Porsche UI Kit – React v0</a> in the meantime.
           </p-text>
-          <p-flex class="p-spacing-mt--16" gap="16" wrap="wrap">
+          <p-flex class="p-spacing-mt-16" gap="16" wrap="wrap">
             <p-flex-item>
-              <p-button-regular class="p-spacing-mt--16" variant="highlight" href="#/getting-started/start-designing">Start designing</p-button-regular>
+              <p-button-regular class="p-spacing-mt-16" variant="highlight" href="#/getting-started/start-designing">Start designing</p-button-regular>
             </p-flex-item>
             <p-flex-item>
-              <p-button-regular class="p-spacing-mt--16" variant="highlight" href="#/getting-started/start-coding">Start coding</p-button-regular>
+              <p-button-regular class="p-spacing-mt-16" variant="highlight" href="#/getting-started/start-coding">Start coding</p-button-regular>
             </p-flex-item>
           </p-flex>
         </p-grid-child>
@@ -51,7 +51,7 @@
         </p-grid-child>
         <p-grid-child size='{"base": 12, "m": 8}' offset='{"base": 0, "m": 1}'>
           <p-text type="24">Join our mailing list and get informed about relevant updates and topics.</p-text>
-          <p-text class="p-spacing-mt--16">
+          <p-text class="p-spacing-mt-16">
             <a href="http://eepurl.com/ghVSjH" target="_blank" rel="noopener noreferrer">Subscribe to mailing list</a>
           </p-text>
         </p-grid-child>
---
+++ packages/design-system/tests/vrt/helpers/setup.ts
@@ -0,0 +1,35 @@
+import 'jasmine';
+import {VisualRegressionTester, VisualRegressionTestOptions} from '@porscheui/visual-regression-tester';
+import * as puppeteer from 'puppeteer';
+import {Browser} from 'puppeteer';
+
+let browser: Browser;
+let visualRegressionTester: VisualRegressionTester;
+
+const testOptions: VisualRegressionTestOptions = {
+  viewports: [320, 480, 760, 1000, 1300, 1760],
+  fixturesDir: 'tests/vrt/fixtures',
+  resultsDir: 'tests/vrt/results',
+  tolerance: 0,
+  baseUrl: 'http://localhost:8080',
+  timeout: 90000
+};
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
+
+afterAll(async () => {
+  if (browser) {
+    await browser.close();
+  }
+});
+
+export async function getVisualRegressionTester(): Promise<VisualRegressionTester> {
+  if (!visualRegressionTester) {
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+    });
+    visualRegressionTester = new VisualRegressionTester(browser, testOptions);
+  }
+
+  return visualRegressionTester;
+}
