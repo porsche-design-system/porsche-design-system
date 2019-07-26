da3cbfa8d32676f6dc8fc13ba07e147d394d2d22
Stephan Schröter <stephan.schroeter@mhp.com>
2019-07-26 17:30:54 +0200

    Optimize storefront vrt tests | oh / sas | #212

---
+++ packages/design-system/tests/vrt/specs/story.vrt.ts
@@ -0,0 +1,20 @@
+import 'jasmine';
+import { VisualRegressionTester } from '@porscheui/visual-regression-tester';
+import { getVisualRegressionTester } from '../helpers/setup';
+
+describe('Story', () => {
+  let visualRegressionTester: VisualRegressionTester;
+
+  beforeAll(async () => {
+    visualRegressionTester = await getVisualRegressionTester();
+  });
+
+  it('should have no visual regression', async () => {
+    expect(
+      await visualRegressionTester.test('story', async () => {
+        await visualRegressionTester.goTo('/#/components/navigation/pagination#code');
+        await visualRegressionTester.click('.playground:nth-of-type(1) > .tabs p-text:nth-of-type(2) button');
+      }, ['#app > .sidebar > nav'])
+    ).toBeFalsy();
+  });
+});
