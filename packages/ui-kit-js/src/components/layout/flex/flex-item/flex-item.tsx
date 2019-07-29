2f373c4843d0383349c81138b50561525b1be962
Stephan Schröter <stephan.schroeter@mhp.com>
2019-07-29 17:08:27 +0200

    Optimize flex docs | oh / sas | #165

--- packages/ui-kit-js/src/components/layout/flex/flex-item/flex-item.tsx
+++ packages/ui-kit-js/src/components/layout/flex/flex-item/flex-item.tsx
@@ -8,39 +8,28 @@ import { BreakpointCustomizable, mapBreakpointPropToClasses, prefix } from '../.
 })
 export class FlexItem {
   @Element() public host: HTMLDivElement;
-  /**
-   * The width of the flex item. You can also supply values for specific breakpoints, like {base: "full", l: "one-quarter"}. You always need to provide a base value when doing this.
-   */
+
+  /** The width of the flex item. You can also supply values for specific breakpoints, like {base: "full", l: "one-quarter"}. You always need to provide a base value when doing this. */
   @Prop() public width?: BreakpointCustomizable<
     'auto' | 'one-quarter' | 'one-third' | 'half' | 'two-thirds' | 'three-quarters' | 'full'
   > = 'auto';
 
-  /**
-   * The offset of the column. You can also supply values for specific breakpoints, like {base: "none", l: "one-quarter"}. You always need to provide a base value when doing this.
-   */
-
+  /** The offset of the column. You can also supply values for specific breakpoints, like {base: "none", l: "one-quarter"}. You always need to provide a base value when doing this. */
   @Prop() public offset?: BreakpointCustomizable<
     'none' | 'one-quarter' | 'one-third' | 'half' | 'two-thirds' | 'three-quarters'
   > = 'none';
-  /**
-   * Defines how this flex item is aligned along the cross axis. This overwrites the cross axis alignment set by the container. Corresponds to the "alignSelf" css property.
-   */
+
+  /** Defines how this flex item is aligned along the cross axis. This overwrites the cross axis alignment set by the container. Corresponds to the "alignSelf" css property. */
   @Prop() public alignSelf?: BreakpointCustomizable<'auto' | 'start' | 'end' | 'center' | 'baseline' | 'stretch'> =
     'auto';
 
-  /**
-   * The ability to allow/disallow the flex child to grow.
-   */
+  /** The ability to allow/disallow the flex child to grow. */
   @Prop() public grow?: BreakpointCustomizable<0 | 1> = 0;
 
-  /**
-   * The ability to allow/disallow the flex child to shrink.
-   */
+  /** The ability to allow/disallow the flex child to shrink. */
   @Prop() public shrink?: BreakpointCustomizable<1 | 0> = 1;
 
-  /**
-   * The shorthand property for the combined definition of "shrink", "grow" and "basis"
-   */
+  /** The shorthand property for the combined definition of "shrink", "grow" and "basis" */
   @Prop() public flex?: BreakpointCustomizable<'initial' | 'auto' | 'none' | 'equal'> = 'initial';
 
   public render(): JSX.Element {
