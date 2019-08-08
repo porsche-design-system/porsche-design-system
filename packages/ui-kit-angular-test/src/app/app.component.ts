5d0933b6d096629c3587365cbeb7f29f6be1b00e
Stephan Schröter <stephan.schroeter@mhp.com>
2019-08-08 15:21:26 +0200

    Setup angular test project for integration test of ui kit components | sas | #194

---
+++ packages/ui-kit-angular-test/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ui-kit-angular-test';
+
+  onButtonClick() {
+    alert('hello world');
+  }
+}
---
+++ packages/ui-kit-angular-test/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {ComponentLibraryModule} from '@porscheui/ui-kit-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ComponentLibraryModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
