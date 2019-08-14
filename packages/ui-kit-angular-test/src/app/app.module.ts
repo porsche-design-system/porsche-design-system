3e09835c3a0a4ab4b3f03697f298ac1f33fdbad8
Stephan Schröter <stephan.schroeter@mhp.com>
2019-08-14 10:50:37 +0200

    Add first angular wrapper example | sas | #194

--- packages/ui-kit-angular-test/src/app/app.module.ts
+++ packages/ui-kit-angular-test/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ComponentLibraryModule} from "@porscheui/ui-kit-angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
