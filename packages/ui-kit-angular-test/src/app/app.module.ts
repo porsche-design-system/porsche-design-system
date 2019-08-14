5b55b89c67ddac210d7dafa4b7521e01fdbb8824
Stephan Schröter <stephan.schroeter@mhp.com>
2019-08-14 10:17:27 +0200

    Sync angular versions of test and wrapper package | sas | #194

--- packages/ui-kit-angular-test/src/app/app.module.ts
+++ packages/ui-kit-angular-test/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ComponentLibraryModule} from "@porscheui/ui-kit-angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    ComponentLibraryModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
