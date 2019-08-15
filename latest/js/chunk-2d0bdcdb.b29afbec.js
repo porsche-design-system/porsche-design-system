868923ea3a8bd53dc9d139a6d301578399133f37
stephanschroeter <stephanschroeter@users.noreply.github.com>
2019-08-15 13:03:42 +0000

    Automated deployment of Porsche UI Kit (v1): b251aee6f408bf70e1da668d4a8ad46533d34987

--- latest/js/chunk-2d0bdcdb.b29afbec.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bdcdb"],{"2e0b":function(n,c,o){"use strict";o.r(c),o.d(c,"p_color",function(){return u});var t=o("6098"),u=function(){function n(n){Object(t["h"])(this,n)}return n}()}}]);
-//# sourceMappingURL=chunk-2d0bdcdb.b29afbec.js.map
