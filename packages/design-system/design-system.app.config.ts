4927f86d73f7ab8df7771f6e2abe8aacfe791635
Marcel Bertram <marcel.bertram@porsche.de>
2019-08-16 15:11:24 +0200

    Add pages for UI Kit App | mb

--- packages/design-system/design-system.app.config.ts
+++ packages/design-system/design-system.app.config.ts
@@ -10,6 +10,22 @@ export const config: DesignSystemAppConfig = {
     'Getting Started': {
       // @ts-ignore
       About: () => import(`@/pages/app/about.md`),
+      // @ts-ignore
+      'Start Designing': () => import(`@/pages/app/start-designing.md`),
+      // @ts-ignore
+      'Design Workflow': () => import(`@/pages/app/design-workflow.md`)
     },
-  }
+    News: {
+      // @ts-ignore
+      Updates: () => import(`@/pages/app/updates.md`),
+      // @ts-ignore
+      Roadmap: () => import(`@/pages/app/roadmap.md`)
+    },
+    Help: {
+      // @ts-ignore
+      Support: () => import(`@/pages/app/support.md`),
+      // @ts-ignore
+      Faq: () => import(`@/pages/app/faq.md`)
+    }
+  },
 };
