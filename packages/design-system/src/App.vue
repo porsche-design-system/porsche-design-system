4052d207a6eb30b816214e63234d4ac869ea66a9
Stephan Schröter <stephan.schroeter@mhp.com>
2019-08-20 22:08:28 +0200

    Add disclaimer | sas | #190

--- packages/design-system/src/App.vue
+++ packages/design-system/src/App.vue
@@ -1,48 +1,56 @@
 <template>
   <div id="app">
-    <aside class="sidebar" :class="{ 'is-visible': isMenuActive }">
-      <Header/>
-      <Divider spacing="small"/>
-      <Sidebar/>
-      <Divider spacing="small"/>
-      <Footer/>
-    </aside>
-    <main class="content" :class="{ 'is-hidden': isMenuActive }">
-      <router-view class="router-view" :class="{ 'is-loading': isLoading }"/>
-      <p-spinner class="spinner" size="medium" ally-label="Loading page"></p-spinner>
-    </main>
-    <Menu class="menu" @toggle="onToggleMenu"></Menu>
+    <div class="topbar">
+      <Disclaimer/>
+    </div>
+    <div class="content">
+      <aside class="sidebar" :class="{ 'is-menu-active': isMenuActive }">
+        <Header/>
+        <Divider spacing="small"/>
+        <Sidebar/>
+        <Divider spacing="small"/>
+        <Footer/>
+      </aside>
+      <main class="main" :class="{ 'is-menu-active': isMenuActive }">
+        <router-view class="router-view" :class="{ 'is-menu-active': isMenuActive, 'is-loading': isLoading }"/>
+        <p-spinner class="spinner" :class="{ 'is-loading': isLoading }" size="medium"
+                   ally-label="Loading page"></p-spinner>
+      </main>
+      <Menu class="menu" @toggle="onToggleMenu"></Menu>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Header from '@/components/Header.vue';
-import Sidebar from '@/components/Sidebar.vue';
-import Footer from '@/components/Footer.vue';
-import Divider from '@/components/Divider.vue';
-import Menu from '@/components/Menu.vue';
+  import {Component, Vue} from 'vue-property-decorator';
+  import Disclaimer from '@/components/Disclaimer.vue';
+  import Header from '@/components/Header.vue';
+  import Sidebar from '@/components/Sidebar.vue';
+  import Footer from '@/components/Footer.vue';
+  import Divider from '@/components/Divider.vue';
+  import Menu from '@/components/Menu.vue';
 
-@Component({
-  components: {
-    Header,
-    Sidebar,
-    Footer,
-    Divider,
-    Menu
-  }
-})
-export default class App extends Vue {
-  public isMenuActive: boolean = false;
+  @Component({
+    components: {
+      Disclaimer,
+      Header,
+      Sidebar,
+      Footer,
+      Divider,
+      Menu
+    }
+  })
+  export default class App extends Vue {
+    public isMenuActive: boolean = false;
 
-  public get isLoading(): boolean {
-    return this.$store.getters.loading;
-  }
+    public get isLoading(): boolean {
+      return this.$store.getters.loading;
+    }
 
-  public onToggleMenu(isActive: boolean): void {
-    this.isMenuActive = isActive;
+    public onToggleMenu(isActive: boolean): void {
+      this.isMenuActive = isActive;
+    }
   }
-}
 </script>
 
 <style lang="scss">
@@ -61,109 +69,122 @@ export default class App extends Vue {
   @import '~@porscheui/ui-kit-scss-utils/index';
 
   #app {
-    overflow-x: hidden;
-  }
-
-  .sidebar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: rem(280px);
+    width: 100vw;
     height: 100vh;
-    z-index: 10;
-    padding: $p-spacing-40 $p-spacing-32;
-    border-right: 1px solid $p-color-neutral-grey-2;
-    background: $p-color-porsche-light;
-    overflow-x: hidden;
-    overflow-y: auto;
-    -webkit-overflow-scrolling: touch;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
 
-    @include breakpoint('xxs', 's') {
-      padding-bottom: $p-spacing-80;
-      opacity: 0;
-      transition: transform .3s, opacity .3s;
-      transform: translate3d(rem(-140px), 0, 0);
-
-      &.is-visible {
-        opacity: 1;
-        transform: translate3d(0, 0, 0);
-      }
+    .topbar {
+      height: auto;
     }
-  }
 
-  .content {
-    position: relative;
-    min-height: 100vh;
-    background: $p-color-porsche-light;
-    z-index: 20;
+    .content {
+      position: relative;
+      height: 100%;
 
-    @include breakpoint('xxs', 's') {
-      transition: transform 0.3s;
-
-      &::after {
-        content: '';
+      .sidebar {
         position: absolute;
         top: 0;
         left: 0;
-        right: 0;
-        height: 0;
+        bottom: 0;
+        width: rem(280px);
+        padding: $p-spacing-40 $p-spacing-32;
+        border-right: 1px solid $p-color-neutral-grey-2;
         background: $p-color-porsche-light;
-        opacity: 0;
-        transition: opacity 0.3s;
-      }
+        overflow-x: hidden;
+        overflow-y: auto;
+        -webkit-overflow-scrolling: touch;
 
-      &.is-hidden {
-        transform: translate3d(rem(280px), 0, 0);
+        @include breakpoint('xxs', 's') {
+          opacity: 0;
+          transform: translate3d(rem(-140px), 0, 0);
+          transition: transform .3s, opacity .3s;
+        }
 
-        &::after {
-          min-height: 100vh;
-          height: 100%;
-          opacity: 0.95;
+        &.is-menu-active {
+          @include breakpoint('xxs', 's') {
+            opacity: 1;
+            transform: translate3d(0, 0, 0);
+          }
         }
       }
-    }
 
-    @include breakpoint('s') {
-      margin-left: rem(280px);
-    }
-  }
+      .main {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background: $p-color-porsche-light;
+        overflow-x: hidden;
+        overflow-y: auto;
+        -webkit-overflow-scrolling: touch;
 
-  .router-view {
-    position: relative;
-    min-height: 100vh;
-    padding: $p-spacing-32;
-    z-index: 2;
-    background: $p-color-porsche-light;
-    opacity: 1;
-    transition: opacity $p-animation-hover-duration $p-animation-hover-bezier;
+        @include breakpoint('xxs', 's') {
+          transform: translate3d(0, 0, 0);
+          transition: transform 0.3s;
+        }
 
-    @include breakpoint('s') {
-      padding: $p-spacing-64;
-    }
+        @include breakpoint('s') {
+          left: rem(280px);
+        }
 
-    &.is-loading {
-      opacity: 0;
-      pointer-events: none;
-    }
-  }
+        &.is-menu-active {
+          @include breakpoint('xxs', 's') {
+            transform: translate3d(rem(280px), 0, 0);
+          }
+        }
 
-  .spinner {
-    position: absolute;
-    top: 50vh;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 1;
-  }
+        .router-view {
+          position: relative;
+          padding: $p-spacing-32;
+          z-index: 20;
+          background: $p-color-porsche-light;
+          opacity: 1;
+          transition: opacity .3s;
 
-  .menu {
-    position: fixed;
-    top: 0;
-    right: 0;
-    z-index: 30;
-    display: none;
+          @include breakpoint('s') {
+            padding: $p-spacing-64;
+          }
 
-    @include breakpoint('xxs', 's') {
-      display: block;
+          &.is-menu-active {
+            @include breakpoint('xxs', 's') {
+              opacity: 0.05;
+              pointer-events: none;
+            }
+          }
+
+          &.is-loading {
+            opacity: 0;
+            pointer-events: none;
+          }
+        }
+
+        .spinner {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          visibility: hidden;
+          z-index: 10;
+
+          &.is-loading {
+            visibility: visible;
+          }
+        }
+      }
+
+      .menu {
+        position: absolute;
+        top: 0;
+        right: 0;
+        display: none;
+
+        @include breakpoint('xxs', 's') {
+          display: block;
+        }
+      }
     }
   }
 </style>
