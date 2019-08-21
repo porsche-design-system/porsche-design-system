0b5871c997317edf63270730efe8f00477ba0ac5
Stephan Schröter <stephan.schroeter@mhp.com>
2019-08-21 17:05:26 +0200

    Update vrt tests | sas | #190

--- packages/design-system/tests/vrt/specs/story.vrt.ts
+++ packages/design-system/tests/vrt/specs/story.vrt.ts
@@ -1,6 +1,6 @@
 import 'jasmine';
-import { VisualRegressionTester } from '@porscheui/visual-regression-tester';
-import { getVisualRegressionTester } from '../helpers/setup';
+import {VisualRegressionTester} from '@porscheui/visual-regression-tester';
+import {getVisualRegressionTester} from '../helpers/setup';
 
 describe('Story', () => {
   let visualRegressionTester: VisualRegressionTester;
@@ -11,10 +11,14 @@ describe('Story', () => {
 
   it('should have no visual regression', async () => {
     expect(
-      await visualRegressionTester.test('story', async () => {
-        await visualRegressionTester.goTo('/#/web/components/navigation/pagination#code');
-        await visualRegressionTester.click('.playground:nth-of-type(1) > .tabs p-text:nth-of-type(2) button');
-      }, ['#app > .sidebar > nav'])
+      await visualRegressionTester.test(
+        'story',
+        async () => {
+          await visualRegressionTester.goTo('/#/web/components/navigation/pagination#code');
+          await visualRegressionTester.click('.playground:nth-of-type(1) > .tabs p-text:nth-of-type(2) button');
+        },
+        '#app > .content > .main > .router-view'
+      )
     ).toBeFalsy();
   });
 });
