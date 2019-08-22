f31ba6e76089db20d310e6b48c0b50f83bb219da
stephanschroeter <stephanschroeter@users.noreply.github.com>
2019-08-22 16:15:55 +0000

    Automated deployment of Porsche UI Kit (v1): 200a520843038f9ab2663e8808c7237e401caece

--- v1/js/chunk-2d0c0845.f003cf09.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c0845"],{"41b4":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"vmark"},[n("h1",[e._v("Updates")]),n("h2",[e._v("[1.0.0-alpha.1] (2019-08-21)")]),n("h3",[e._v("Features")]),n("ul",[n("li",[e._v("Initial release")])])])}],s=n("2be6"),r={},c=Object(s["a"])(r,a,l,!1,null,null,null);t["default"]=c.exports}}]);
-//# sourceMappingURL=chunk-2d0c0845.f003cf09.js.map
