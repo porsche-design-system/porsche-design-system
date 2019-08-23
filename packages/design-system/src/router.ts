5860abd9e560e092a2148ab1ca8ca2cbcfaa3f2b
Oliver Hook <oliver.hook@mhp.com>
2019-08-23 11:36:18 +0200

    fixed scrollTop behaviour on view router | oh | #238

--- packages/design-system/src/router.ts
+++ packages/design-system/src/router.ts
@@ -81,10 +81,7 @@ const router = new Router({
       path: '*',
       redirect: { name: 'home' }
     }
-  ],
-  scrollBehavior() {
-    return { x: 0, y: 0 };
-  }
+  ]
 });
 
 router.beforeEach(async (to, from, next) => {
@@ -94,6 +91,12 @@ router.beforeEach(async (to, from, next) => {
 
 router.afterEach(async () => {
   await $store.dispatch('toggleLoadingAsync', false);
+  // workaround for open issue(s) to set vue router scroll behaviour to other than standard body element
+  // see: https://github.com/vuejs/vue-router/pull/2780
+  const scrollElement = document.querySelector('.main');
+  if(scrollElement !== null) {
+    scrollElement.scrollTo(0,0);
+  }
 });
 
 export default router;
