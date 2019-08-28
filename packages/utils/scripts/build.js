d66a5acc75d1c9e5dd49cf0721bd3e7cea19222f
Stephan Schröter <stephan.schroeter@mhp.com>
2019-08-28 18:54:19 +0200

    Rename utils vrt styles | sas | #238

--- packages/utils/scripts/build.js
+++ packages/utils/scripts/build.js
@@ -3,8 +3,8 @@ const path = require('path');
 const sass = require('node-sass');
 const tildeImporter = require('node-sass-tilde-importer');
 
-const scssPath = path.resolve(__dirname, '../src/components-overview.scss');
-const targetPath = path.resolve(__dirname, '../dist/components-overview.css');
+const scssPath = path.resolve(__dirname, '../src/visual-regression-test.scss');
+const targetPath = path.resolve(__dirname, '../dist/visual-regression-test.css');
 const result = sass.renderSync({
   file: scssPath,
   importer: tildeImporter,
