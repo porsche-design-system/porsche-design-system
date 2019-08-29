b5ed74af0e871327b471011213809eae2997a163
Chris Albert <mail@christophalbert.com>
2019-08-29 14:19:16 +0200

    Load ui-kit-react icons from cdn

--- react/packages/@porsche/ui-kit-react/src/components/atoms/Icon/icons.ts
+++ react/packages/@porsche/ui-kit-react/src/components/atoms/Icon/icons.ts
@@ -1,219 +1,111 @@
-import Svg111PointsCheck from "./svg/111_points_check.svg"
-import Svg4Wd from "./svg/4WD.svg"
-import SvgAccidentFree from "./svg/accident_free.svg"
-import SvgArrowBothWaysHair from "./svg/arrow_both_ways_hair.svg"
-import SvgArrowDownHair from "./svg/arrow_down_hair.svg"
-import SvgArrowLeftHair from "./svg/arrow_left_hair.svg"
-import SvgArrowLeftHairFirst from "./svg/arrow_left_hair_first.svg"
-import SvgArrowOpenFullDown from "./svg/arrow_open_full_down.svg"
-import SvgArrowOpenFullUp from "./svg/arrow_open_full_up.svg"
-import SvgArrowRightHair from "./svg/arrow_right_hair.svg"
-import SvgArrowRightHairLast from "./svg/arrow_right_hair_last.svg"
-import SvgArrowUp from "./svg/arrow_up.svg"
-import SvgArrowUpHair from "./svg/arrow_up_hair.svg"
-import SvgBin from "./svg/bin.svg"
-import SvgBox from "./svg/box.svg"
-import SvgCalendar from "./svg/calendar.svg"
-import SvgCancel from "./svg/cancel.svg"
-import SvgCheck from "./svg/check.svg"
-import SvgCircle from "./svg/circle.svg"
-import SvgCircleCheck from "./svg/circle_check.svg"
-import SvgCircleCrossedSmall from "./svg/circle_crossed_small.svg"
-import SvgClockTime from "./svg/clock_time.svg"
-import SvgClosedEye from "./svg/closed_eye.svg"
-import SvgCompare from "./svg/compare.svg"
-import SvgCopy from "./svg/copy.svg"
-import SvgCorporateSales from "./svg/corporate_sales.svg"
-import SvgCustomerView from "./svg/customer_view.svg"
-import SvgDeleteEntry from "./svg/delete_entry.svg"
-import SvgDocument from "./svg/document.svg"
-import SvgEdit from "./svg/edit.svg"
-import SvgEditOutline from "./svg/edit_outline.svg"
-import SvgElectricity from "./svg/electricity.svg"
-import SvgExport from "./svg/export.svg"
-import SvgEye from "./svg/eye.svg"
-import SvgFacebook from "./svg/facebook.svg"
-import SvgFilter from "./svg/filter.svg"
-import SvgFuel from "./svg/fuel.svg"
-import SvgFuelOutline from "./svg/fuel_outline.svg"
-import SvgFullServiceHistory from "./svg/full_service_history.svg"
-import SvgGoogle from "./svg/google.svg"
-import SvgGridView from "./svg/grid_view.svg"
-import SvgHelp from "./svg/help.svg"
-import SvgInformation from "./svg/information.svg"
-import SvgInformationOutline from "./svg/information_outline.svg"
-import SvgInstagram from "./svg/instagram.svg"
-import SvgLinkedin from "./svg/linkedin.svg"
-import SvgList from "./svg/list.svg"
-import SvgLoader from "./svg/loader.svg"
-import SvgMagnifyGlass from "./svg/magnify_glass.svg"
-import SvgMaintenance from "./svg/maintenance.svg"
-import SvgMapPin from "./svg/map_pin.svg"
-import SvgMenuDots from "./svg/menu_dots.svg"
-import SvgMenuDotsVertical from "./svg/menu_dots_vertical.svg"
-import SvgMessage from "./svg/message.svg"
-import SvgMessageOutline from "./svg/message_outline.svg"
-import SvgMessageOutlineRequest from "./svg/message_outline_request.svg"
-import SvgMinus from "./svg/minus.svg"
-import SvgMute from "./svg/mute.svg"
-import SvgNavigation from "./svg/navigation.svg"
-import SvgOptionClose from "./svg/option_close.svg"
-import SvgOptionOpen from "./svg/option_open.svg"
-import SvgOptionSort from "./svg/option_sort.svg"
-import SvgOptionView1 from "./svg/option_view_1.svg"
-import SvgOrganizer from "./svg/organizer.svg"
-import SvgOverrevsChecked from "./svg/overrevs_checked.svg"
-import SvgOverview from "./svg/overview.svg"
-import SvgPaintingChecked from "./svg/painting_checked.svg"
-import SvgPause from "./svg/pause.svg"
-import SvgPerson from "./svg/person.svg"
-import SvgPersonAdd from "./svg/person_add.svg"
-import SvgPersonOutline from "./svg/person_outline.svg"
-import SvgPhoneReceiver from "./svg/phone_receiver.svg"
-import SvgPicture from "./svg/picture.svg"
-import SvgPictureImport from "./svg/picture_import.svg"
-import SvgPinterest from "./svg/pinterest.svg"
-import SvgPlay from "./svg/play.svg"
-import SvgPlayCirle from "./svg/play_cirle.svg"
-import SvgPlus from "./svg/plus.svg"
-import SvgPorscheAssistance from "./svg/porsche_assistance.svg"
-import SvgPorscheGenuineParts from "./svg/porsche_genuine_parts.svg"
-import SvgPrint from "./svg/print.svg"
-import SvgProfileDepth3Mm from "./svg/profile_depth_3_mm.svg"
-import SvgPurchased from "./svg/purchased.svg"
-import SvgRadioButton from "./svg/radio_button.svg"
-import SvgReadout from "./svg/readout.svg"
-import SvgReconditionCosts from "./svg/recondition_costs.svg"
-import SvgRentalCar from "./svg/rental_car.svg"
-import SvgReset from "./svg/reset.svg"
-import SvgRestart from "./svg/restart.svg"
-import SvgSaved from "./svg/saved.svg"
-import SvgShare from "./svg/share.svg"
-import SvgSmoking from "./svg/smoking.svg"
-import SvgSmokingForbidden from "./svg/smoking_forbidden.svg"
-import SvgSnapchat from "./svg/snapchat.svg"
-import SvgStar from "./svg/star.svg"
-import SvgStarOutline from "./svg/star_outline.svg"
-import SvgStockView from "./svg/stock_view.svg"
-import SvgSubmenu from "./svg/submenu.svg"
-import SvgTimer from "./svg/timer.svg"
-import SvgTransmission from "./svg/transmission.svg"
-import SvgTwitter from "./svg/twitter.svg"
-import SvgUpdateSave from "./svg/update_save.svg"
-import SvgVehicleReconditioned from "./svg/vehicle_reconditioned.svg"
-import SvgWarningCircle from "./svg/warning_circle.svg"
-import SvgWarningFilled from "./svg/warning_filled.svg"
-import SvgWechat from "./svg/wechat.svg"
-import SvgWhatsapp from "./svg/whatsapp.svg"
-import SvgYoutube from "./svg/youtube.svg"
-
 export const icons = {
-    "111_points_check": Svg111PointsCheck,
-    "4WD": Svg4Wd,
-    accident_free: SvgAccidentFree,
-    arrow_both_ways_hair: SvgArrowBothWaysHair,
-    arrow_down_hair: SvgArrowDownHair,
-    arrow_left_hair: SvgArrowLeftHair,
-    arrow_left_hair_first: SvgArrowLeftHairFirst,
-    arrow_open_full_down: SvgArrowOpenFullDown,
-    arrow_open_full_up: SvgArrowOpenFullUp,
-    arrow_right_hair: SvgArrowRightHair,
-    arrow_right_hair_last: SvgArrowRightHairLast,
-    arrow_up: SvgArrowUp,
-    arrow_up_hair: SvgArrowUpHair,
-    bin: SvgBin,
-    box: SvgBox,
-    calendar: SvgCalendar,
-    cancel: SvgCancel,
-    check: SvgCheck,
-    circle: SvgCircle,
-    circle_check: SvgCircleCheck,
-    circle_crossed_small: SvgCircleCrossedSmall,
-    clock_time: SvgClockTime,
-    closed_eye: SvgClosedEye,
-    compare: SvgCompare,
-    copy: SvgCopy,
-    corporate_sales: SvgCorporateSales,
-    customer_view: SvgCustomerView,
-    delete_entry: SvgDeleteEntry,
-    document: SvgDocument,
-    edit: SvgEdit,
-    edit_outline: SvgEditOutline,
-    electricity: SvgElectricity,
-    export: SvgExport,
-    eye: SvgEye,
-    facebook: SvgFacebook,
-    filter: SvgFilter,
-    fuel: SvgFuel,
-    fuel_outline: SvgFuelOutline,
-    full_service_history: SvgFullServiceHistory,
-    google: SvgGoogle,
-    grid_view: SvgGridView,
-    help: SvgHelp,
-    information: SvgInformation,
-    information_outline: SvgInformationOutline,
-    instagram: SvgInstagram,
-    linkedin: SvgLinkedin,
-    list: SvgList,
-    loader: SvgLoader,
-    magnify_glass: SvgMagnifyGlass,
-    maintenance: SvgMaintenance,
-    map_pin: SvgMapPin,
-    menu_dots: SvgMenuDots,
-    menu_dots_vertical: SvgMenuDotsVertical,
-    message: SvgMessage,
-    message_outline: SvgMessageOutline,
-    message_outline_request: SvgMessageOutlineRequest,
-    minus: SvgMinus,
-    mute: SvgMute,
-    navigation: SvgNavigation,
-    option_close: SvgOptionClose,
-    option_open: SvgOptionOpen,
-    option_sort: SvgOptionSort,
-    option_view_1: SvgOptionView1,
-    organizer: SvgOrganizer,
-    overrevs_checked: SvgOverrevsChecked,
-    overview: SvgOverview,
-    painting_checked: SvgPaintingChecked,
-    pause: SvgPause,
-    person: SvgPerson,
-    person_add: SvgPersonAdd,
-    person_outline: SvgPersonOutline,
-    phone_receiver: SvgPhoneReceiver,
-    picture: SvgPicture,
-    picture_import: SvgPictureImport,
-    pinterest: SvgPinterest,
-    play: SvgPlay,
-    play_cirle: SvgPlayCirle,
-    plus: SvgPlus,
-    porsche_assistance: SvgPorscheAssistance,
-    porsche_genuine_parts: SvgPorscheGenuineParts,
-    print: SvgPrint,
-    profile_depth_3_mm: SvgProfileDepth3Mm,
-    purchased: SvgPurchased,
-    radio_button: SvgRadioButton,
-    readout: SvgReadout,
-    recondition_costs: SvgReconditionCosts,
-    rental_car: SvgRentalCar,
-    reset: SvgReset,
-    restart: SvgRestart,
-    saved: SvgSaved,
-    share: SvgShare,
-    smoking: SvgSmoking,
-    smoking_forbidden: SvgSmokingForbidden,
-    snapchat: SvgSnapchat,
-    star: SvgStar,
-    star_outline: SvgStarOutline,
-    stock_view: SvgStockView,
-    submenu: SvgSubmenu,
-    timer: SvgTimer,
-    transmission: SvgTransmission,
-    twitter: SvgTwitter,
-    update_save: SvgUpdateSave,
-    vehicle_reconditioned: SvgVehicleReconditioned,
-    warning_circle: SvgWarningCircle,
-    warning_filled: SvgWarningFilled,
-    wechat: SvgWechat,
-    whatsapp: SvgWhatsapp,
-    youtube: SvgYoutube
+    "111_points_check": "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/111_points_check.svg",
+    "4WD": "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/4WD.svg",
+    accident_free: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/accident_free.svg",
+    arrow_both_ways_hair: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_both_ways_hair.svg",
+    arrow_down_hair: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_down_hair.svg",
+    arrow_left_hair: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_left_hair.svg",
+    arrow_left_hair_first: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_left_hair_first.svg",
+    arrow_open_full_down: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_open_full_down.svg",
+    arrow_open_full_up: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_open_full_up.svg",
+    arrow_right_hair: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_right_hair.svg",
+    arrow_right_hair_last: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_right_hair_last.svg",
+    arrow_up: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_up.svg",
+    arrow_up_hair: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/arrow_up_hair.svg",
+    bin: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/bin.svg",
+    box: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/box.svg",
+    calendar: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/calendar.svg",
+    cancel: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/cancel.svg",
+    check: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/check.svg",
+    circle: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/circle.svg",
+    circle_check: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/circle_check.svg",
+    circle_crossed_small: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/circle_crossed_small.svg",
+    clock_time: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/clock_time.svg",
+    closed_eye: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/closed_eye.svg",
+    compare: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/compare.svg",
+    copy: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/copy.svg",
+    corporate_sales: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/corporate_sales.svg",
+    customer_view: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/customer_view.svg",
+    delete_entry: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/delete_entry.svg",
+    document: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/document.svg",
+    edit: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/edit.svg",
+    edit_outline: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/edit_outline.svg",
+    electricity: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/electricity.svg",
+    export: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/export.svg",
+    eye: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/eye.svg",
+    facebook: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/facebook.svg",
+    filter: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/filter.svg",
+    fuel: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/fuel.svg",
+    fuel_outline: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/fuel_outline.svg",
+    full_service_history: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/full_service_history.svg",
+    google: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/google.svg",
+    grid_view: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/grid_view.svg",
+    help: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/help.svg",
+    information: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/information.svg",
+    information_outline: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/information_outline.svg",
+    instagram: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/instagram.svg",
+    linkedin: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/linkedin.svg",
+    list: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/list.svg",
+    loader: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/loader.svg",
+    magnify_glass: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/magnify_glass.svg",
+    maintenance: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/maintenance.svg",
+    map_pin: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/map_pin.svg",
+    menu_dots: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/menu_dots.svg",
+    menu_dots_vertical: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/menu_dots_vertical.svg",
+    message: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/message.svg",
+    message_outline: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/message_outline.svg",
+    message_outline_request:
+        "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/message_outline_request.svg",
+    minus: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/minus.svg",
+    mute: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/mute.svg",
+    navigation: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/navigation.svg",
+    option_close: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/option_close.svg",
+    option_open: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/option_open.svg",
+    option_sort: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/option_sort.svg",
+    option_view_1: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/option_view_1.svg",
+    organizer: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/organizer.svg",
+    overrevs_checked: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/overrevs_checked.svg",
+    overview: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/overview.svg",
+    painting_checked: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/painting_checked.svg",
+    pause: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/pause.svg",
+    person: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/person.svg",
+    person_add: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/person_add.svg",
+    person_outline: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/person_outline.svg",
+    phone_receiver: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/phone_receiver.svg",
+    picture: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/picture.svg",
+    picture_import: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/picture_import.svg",
+    pinterest: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/pinterest.svg",
+    play: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/play.svg",
+    play_circle: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/play_cirle.svg",
+    plus: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/plus.svg",
+    porsche_assistance: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/porsche_assistance.svg",
+    porsche_genuine_parts: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/v.svg",
+    print: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/print.svg",
+    profile_depth_3_mm: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/profile_depth_3_mm.svg",
+    purchased: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/purchased.svg",
+    radio_button: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/radio_button.svg",
+    readout: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/readout.svg",
+    recondition_costs: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/recondition_costs.svg",
+    rental_car: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/rental_car.svg",
+    reset: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/reset.svg",
+    restart: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/restart.svg",
+    saved: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/saved.svg",
+    share: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/share.svg",
+    smoking: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/smoking.svg",
+    smoking_forbidden: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/smoking_forbidden.svg",
+    snapchat: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/snapchat.svg",
+    star: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/star.svg",
+    star_outline: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/star_outline.svg",
+    stock_view: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/stock_view.svg",
+    submenu: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/submenu.svg",
+    timer: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/timer.svg",
+    transmission: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/transmission.svg",
+    twitter: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/twitter.svg",
+    update_save: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/update_save.svg",
+    vehicle_reconditioned: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/vehicle_reconditioned.svg",
+    warning_circle: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/warning_circle.svg",
+    warning_filled: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/warning_filled.svg",
+    wechat: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/wechat.svg",
+    whatsapp: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/whatsapp.svg",
+    youtube: "https://cdn.ui.porsche.com/porsche-ui-kit/icon/v0/ui-kit-react/youtube.svg"
 }
