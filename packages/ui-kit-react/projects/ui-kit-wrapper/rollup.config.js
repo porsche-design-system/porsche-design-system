b10ebd346c4f9e5ea8ea235c461ba6258f31af6a
Oliver Hook <oliver.hook@mhp.com>
2019-09-10 17:04:59 +0200

    fix react wrapper build | oh | #housekeeping

---
+++ packages/ui-kit-react/projects/ui-kit-wrapper/rollup.config.js
@@ -0,0 +1,21 @@
+import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve';
+
+export default {
+  input: 'dist/index.js',
+
+  external: ['@porsche-ui/ui-kit-js', '@porsche-ui/ui-kit-js/loader', 'react', 'react-dom', 'classnames'],
+
+  plugins: [resolve()],
+
+  output: [
+    {
+      format: 'cjs',
+      file: pkg.main
+    },
+    {
+      format: 'es',
+      file: pkg.module
+    }
+  ]
+};
