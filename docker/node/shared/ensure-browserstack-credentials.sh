e69eac63d29c883d0ab6ca5870acd7789ac81aae
Oliver Hook <oliver.hook@mhp.com>
2019-09-17 13:59:44 +0200

    add bs credentials to docker env | oh | #206

---
+++ docker/node/shared/ensure-browserstack-credentials.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# DO NOT ADD OTHER TRAPS AFTER SOURCING THIS FILE, BECAUSE IT WOULD OVERRIDE THE CREDENTIAL CLEANUP TRAP
+
+set -o errexit
+set -o pipefail
+
+if [[ -z "${BROWSERSTACK_USER_NAME}" ]]; then
+  echo "Please provide the \$BROWSERSTACK_USER_NAME environment variable."
+  exit 1
+fi
+
+if [[ -z "${BROWSERSTACK_ACCESS_KEY}" ]]; then
+  echo "Please provide the \$BROWSERSTACK_ACCESS_KEY environment variable."
+  exit 1
+fi
+
+cleanup_credentials() {
+  local exit_code=$?
+  echo "task: [$(date)] \"cleanup_credentials\""
+  rm "${HOME}/.env"
+  exit ${exit_code}
+}
+
+setup_credentials() {
+  echo "task: [$(date)] \"setup_credentials\""
+  echo "BROWSERSTACK_USER_NAME=${BROWSERSTACK_USER_NAME}" > "${HOME}/.env"
+  echo "BROWSERSTACK_ACCESS_KEY=${BROWSERSTACK_ACCESS_KEY}" > "${HOME}/.env"
+}
+
+trap cleanup_credentials EXIT
+setup_credentials
