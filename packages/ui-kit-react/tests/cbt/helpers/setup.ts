1016193698f5b2e56eef224b456d6e35d2a862c0
Stephan Schröter <stephan.schroeter@mhp.com>
2019-09-23 18:11:09 +0200

    Add cbt for UI Kit React | oh / sas | #206

---
+++ packages/ui-kit-react/tests/cbt/helpers/setup.ts
@@ -0,0 +1,32 @@
+require('dotenv').config();
+import 'jasmine';
+import { CrossBrowserTester, CrossBrowserTestOptions } from '@porsche-ui/cross-browser-tester';
+
+let crossBrowserTester: CrossBrowserTester;
+
+const options: CrossBrowserTestOptions = {
+  username: process.env.BROWSERSTACK_USER_NAME,
+  accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
+  browserList: [
+    {'os': 'Windows', 'os_version': '10', 'browser_version': '11.0', 'browser': 'ie'}
+  ],
+  local: true,
+  orientation: 'portrait',
+  winRes: '1024x768',
+  quality: 'original',
+  fixturesDir: '../ui-kit-js/tests/cbt/fixtures',
+  resultsDir: 'tests/cbt/results',
+  tolerance: 0,
+  baseUrl: 'http://localhost:3000',
+  timeout: 10
+};
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
+
+export async function getCrossBrowserTester(): Promise<CrossBrowserTester> {
+  if (!crossBrowserTester) {
+    crossBrowserTester = new CrossBrowserTester(options);
+  }
+
+  return crossBrowserTester;
+}
