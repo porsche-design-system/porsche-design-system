4353ce648d1b75ab0a450496d7e3f3af7286894f
Stephan Schröter <stephan.schroeter@mhp.com>
2019-10-07 11:24:46 +0200

    Improve design system configuration | sas | #257

--- packages/design-system/design-system.web.config.ts
+++ packages/design-system/design-system.web.config.ts
@@ -4,139 +4,225 @@ import {DesignSystemConfig} from '@/interface';
 export const config: DesignSystemConfig = {
   pages: {
     'Getting Started': {
-      // @ts-ignore
-      About: () => import(`@/pages/web/about.md`),
-      // @ts-ignore
-      'Start Designing': () => import(`@/pages/web/start-designing.md`),
-      // @ts-ignore
-      'Design Workflow': () => import(`@/pages/web/design-workflow.md`),
-      // @ts-ignore
-      'Sketch Plugins': () => import(`@/pages/web/sketch-plugins.md`),
-      // @ts-ignore
-      'Start Coding': () => import(`@/pages/web/start-coding.md`)
+      About: [
+        // @ts-ignore
+        () => import(`@/pages/web/about.md`),
+      ],
+      'Start Designing': [
+        // @ts-ignore
+        () => import(`@/pages/web/start-designing.md`),
+      ],
+      'Design Workflow': [
+        // @ts-ignore
+        () => import(`@/pages/web/design-workflow.md`),
+      ],
+      'Sketch Plugins': [
+        // @ts-ignore
+        () => import(`@/pages/web/sketch-plugins.md`),
+      ],
+      'Start Coding': [
+        // @ts-ignore
+        () => import(`@/pages/web/start-coding.md`),
+      ],
     },
     News: {
-      // @ts-ignore
-      Updates: () => import(`@/pages/web/updates.md`),
-      // @ts-ignore
-      Versioning: () => import(`@/pages/web/versioning.md`),
-      // @ts-ignore
-      Roadmap: () => import(`@/pages/web/roadmap.md`)
+      Updates: [
+        // @ts-ignore
+        () => import(`@/pages/web/updates.md`),
+      ],
+      Versioning: [
+        // @ts-ignore
+        () => import(`@/pages/web/versioning.md`),
+      ],
+      Roadmap: [
+        // @ts-ignore
+        () => import(`@/pages/web/roadmap.md`),
+      ],
     },
     Help: {
-      // @ts-ignore
-      Support: () => import(`@/pages/web/support.md`),
-      // @ts-ignore
-      Faq: () => import(`@/pages/web/faq.md`)
+      Support: [
+        // @ts-ignore
+        () => import(`@/pages/web/support.md`),
+      ],
+      Faq: [
+        // @ts-ignore
+        () => import(`@/pages/web/faq.md`),
+      ],
     },
     Basics: {
-      // @ts-ignore
-      'Browser Compatibility': () => import(`@/pages/web/browser-compatibility.md`),
-      // @ts-ignore
-      'Quality Criteria': () => import(`@/pages/web/quality-criteria.md`),
-      // @ts-ignore
-      'Accessibility Criteria': () => import(`@/pages/web/accessibility.md`)
+      'Browser Compatibility': [
+        // @ts-ignore
+        () => import(`@/pages/web/browser-compatibility.md`),
+      ],
+      'Quality Criteria': [
+        // @ts-ignore
+        () => import(`@/pages/web/quality-criteria.md`),
+      ],
+      'Accessibility Criteria': [
+        // @ts-ignore
+        () => import(`@/pages/web/accessibility.md`),
+      ],
     },
     'Scss Utils': {
-      // @ts-ignore
-      Introduction: () => import(`@/pages/web/introduction.md`),
-      // @ts-ignore
-      Variables: () => import(`@/pages/web/variables.md`),
-      // @ts-ignore
-      Mixins: () => import(`@/pages/web/mixins.md`),
-      // @ts-ignore
-      Functions: () => import(`@/pages/web/functions.md`),
+      Introduction: [
+        // @ts-ignore
+        () => import(`@/pages/web/introduction.md`),
+      ],
+      Variables: [
+        // @ts-ignore
+        () => import(`@/pages/web/variables.md`),
+      ],
+      Mixins: [
+        // @ts-ignore
+        () => import(`@/pages/web/mixins.md`),
+      ],
+      Functions: [
+        // @ts-ignore
+        () => import(`@/pages/web/functions.md`),
+      ],
     },
   },
   stories: {
     Basic: {
       Color: {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/basic/color/color.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/basic/color/color.code.md`)
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/basic/color/color.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/basic/color/color.code.md`),
+        ],
       },
       Typography: {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/basic/typography/typography.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/basic/typography/typography.code.md`),
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/basic/typography/typography.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/basic/typography/typography.code.md`),
+        ],
         Props: [
           // @ts-ignore
           () => import(`@/../../ui-kit-js/src/components/basic/typography/headline/headline.props.md`),
           // @ts-ignore
-          () => import(`@/../../ui-kit-js/src/components/basic/typography/text/text.props.md`)
+          () => import(`@/../../ui-kit-js/src/components/basic/typography/text/text.props.md`),
         ]
       },
       Marque: {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/basic/marque/marque.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/basic/marque/marque.code.md`),
-        // @ts-ignore
-        Props: () => import(`@/../../ui-kit-js/src/components/basic/marque/marque.props.md`)
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/basic/marque/marque.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/basic/marque/marque.code.md`),
+        ],
+        Props: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/basic/marque/marque.props.md`),
+        ],
       }
     },
     Action: {
       'Button Regular': {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/action/button-regular/button-regular.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/action/button-regular/button-regular.code.md`),
-        // @ts-ignore
-        Props: () => import(`@/../../ui-kit-js/src/components/action/button-regular/button-regular.props.md`)
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/button-regular/button-regular.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/button-regular/button-regular.code.md`),
+        ],
+        Props: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/button-regular/button-regular.props.md`),
+        ],
       },
       'Button Icon': {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/action/button-icon/button-icon.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/action/button-icon/button-icon.code.md`),
-        // @ts-ignore
-        Props: () => import(`@/../../ui-kit-js/src/components/action/button-icon/button-icon.props.md`)
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/button-icon/button-icon.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/button-icon/button-icon.code.md`),
+        ],
+        Props: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/button-icon/button-icon.props.md`),
+        ],
       },
       'Text Link': {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/action/text-link/text-link.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/action/text-link/text-link.code.md`),
-        // @ts-ignore
-        Props: () => import(`@/../../ui-kit-js/src/components/action/text-link/text-link.props.md`)
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/text-link/text-link.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/text-link/text-link.code.md`),
+        ],
+        Props: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/action/text-link/text-link.props.md`),
+        ],
       }
     },
     Content: {
       'Text List': {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/content/text-list/text-list.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/content/text-list/text-list.code.md`),
-        // @ts-ignore
-        Props: () => import(`@/../../ui-kit-js/src/components/content/text-list/text-list/text-list.props.md`)
-      }
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/content/text-list/text-list.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/content/text-list/text-list.code.md`),
+        ],
+        Props: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/content/text-list/text-list/text-list.props.md`),
+        ],
+      },
     },
     Feedback: {
       Spinner: {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/feedback/spinner/spinner.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/feedback/spinner/spinner.code.md`),
-        // @ts-ignore
-        Props: () => import(`@/../../ui-kit-js/src/components/feedback/spinner/spinner.props.md`)
-      }
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/feedback/spinner/spinner.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/feedback/spinner/spinner.code.md`),
+        ],
+        Props: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/feedback/spinner/spinner.props.md`),
+        ],
+      },
     },
     Icon: {
       Icon: {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/icon/icon/icon.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/icon/icon/icon.code.md`),
-        // @ts-ignore
-        Props: () => import(`@/../../ui-kit-js/src/components/icon/icon/icon.props.md`)
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/icon/icon/icon.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/icon/icon/icon.code.md`),
+        ],
+        Props: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/icon/icon/icon.props.md`),
+        ],
       }
     },
     Layout: {
       Flex: {
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/layout/flex/flex.code.md`),
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/layout/flex/flex.code.md`),
+        ],
         Props: [
           // @ts-ignore
           () => import(`@/../../ui-kit-js/src/components/layout/flex/flex/flex.props.md`),
@@ -145,33 +231,47 @@ export const config: DesignSystemConfig = {
         ]
       },
       Grid: {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/layout/grid/grid.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/layout/grid/grid.code.md`),
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/layout/grid/grid.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/layout/grid/grid.code.md`),
+        ],
         Props: [
           // @ts-ignore
           () => import(`@/../../ui-kit-js/src/components/layout/grid/grid/grid.props.md`),
           // @ts-ignore
-          () => import(`@/../../ui-kit-js/src/components/layout/grid/grid-child/grid-child.props.md`)
-        ]
+          () => import(`@/../../ui-kit-js/src/components/layout/grid/grid-child/grid-child.props.md`),
+        ],
       },
       Spacing: {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/layout/spacing/spacing.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/layout/spacing/spacing.code.md`)
-      }
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/layout/spacing/spacing.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/layout/spacing/spacing.code.md`),
+        ],
+      },
     },
     Navigation: {
       Pagination: {
-        // @ts-ignore
-        Design: () => import(`@/../../ui-kit-js/src/components/navigation/pagination/pagination.design.md`),
-        // @ts-ignore
-        Code: () => import(`@/../../ui-kit-js/src/components/navigation/pagination/pagination.code.md`),
-        // @ts-ignore
-        Props: () => import(`@/../../ui-kit-js/src/components/navigation/pagination/pagination.props.md`)
-      }
-    }
+        Design: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/navigation/pagination/pagination.design.md`),
+        ],
+        Code: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/navigation/pagination/pagination.code.md`),
+        ],
+        Props: [
+          // @ts-ignore
+          () => import(`@/../../ui-kit-js/src/components/navigation/pagination/pagination.props.md`),
+        ],
+      },
+    },
   }
 };
