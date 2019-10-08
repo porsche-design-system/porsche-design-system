190ad6559cdcac41c3c344f88ddb0cdc23061585
Stephan Schröter <stephan.schroeter@mhp.com>
2019-10-08 11:26:02 +0200

    Test | cs / sas | #279

--- packages/ui-kit-js/tests/vrt/specs/button-icon.vrt.ts
+++ packages/ui-kit-js/tests/vrt/specs/button-icon.vrt.ts
@@ -1,6 +1,6 @@
 import 'jasmine';
-import { VisualRegressionTester } from '@porsche-ui/visual-regression-tester';
-import { getVisualRegressionTester } from '../helpers/setup';
+import {VisualRegressionTester} from '@porsche-ui/visual-regression-tester';
+import {getVisualRegressionTester} from '../helpers/setup';
 
 describe('Button Icon', () => {
   let vrt: VisualRegressionTester;
@@ -10,22 +10,10 @@ describe('Button Icon', () => {
   });
 
   it('should have no visual regression', async () => {
-    try {
-      expect(
-        await vrt.test('button-icon', async () => {
-          console.log('-------------------------------------> 1');
-          try {
-            console.log('-------------------------------------> 2');
-            await vrt.goTo('/src/components/action/button-icon/button-icon.test.html');
-            console.log('-------------------------------------> 3');
-          } catch (e) {
-            console.log('####################### ERROR A:', e);
-          }
-          console.log('-------------------------------------> 4');
-        })
-      ).toBeFalsy();
-    } catch (e) {
-      console.log('####################### ERROR B:', e);
-    }
+    expect(
+      await vrt.test('button-icon', async () => {
+        await vrt.goTo('/src/components/action/button-icon/button-icon.test.html');
+      })
+    ).toBeFalsy();
   });
 });
