b31fa5045b9b8e6681fb12865e67312f5236f21c
Stephan Schröter <stephan.schroeter@mhp.com>
2019-10-08 10:59:51 +0200

    Disable VRT test | cs / sas

--- packages/ui-kit-js/tests/vrt/specs/overview.vrt.ts
+++ packages/ui-kit-js/tests/vrt/specs/overview.vrt.ts
@@ -13,11 +13,15 @@ describe('Components Overview', () => {
     try {
       expect(
         await vrt.test('overview', async () => {
+          console.log('-------------------------------------> 1');
           try {
+            console.log('-------------------------------------> 2');
             await vrt.goTo('/index.html');
+            console.log('-------------------------------------> 3');
           } catch (e) {
             console.log('####################### ERROR A:', e);
           }
+          console.log('-------------------------------------> 4');
         })
       ).toBeFalsy();
     } catch (e) {
