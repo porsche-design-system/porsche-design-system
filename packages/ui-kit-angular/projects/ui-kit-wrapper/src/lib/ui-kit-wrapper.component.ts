4f1fe4c573fb30da75fe5b9be0ff6607e9192b89
Christoph Stickel <christoph@neochic.de>
2019-10-14 16:01:35 +0200

    temporary modified ui-kit-wrapper components manually to make buildOptimizer builds work with camel case inputs | sas / cs | #300

--- packages/ui-kit-angular/projects/ui-kit-wrapper/src/lib/ui-kit-wrapper.component.ts
+++ packages/ui-kit-angular/projects/ui-kit-wrapper/src/lib/ui-kit-wrapper.component.ts
@@ -3,24 +3,22 @@
 import { Component, ElementRef, ChangeDetectorRef, EventEmitter } from '@angular/core';
 import { fromEvent } from 'rxjs';
 
-function proxyInputs(Cmp: any, inputs: string[]) {
-  const Prototype = Cmp.prototype;
-  inputs.forEach(item => {
-    Object.defineProperty(Prototype, item, {
-      get() { return this.el[item]; },
-      set(val: any) { this.el[item] = val; },
-    });
-  });
-}
-
-function proxyMethods(Cmp: any, methods: string[]) {
-  const Prototype = Cmp.prototype;
-  methods.forEach(methodName => {
-    Prototype[methodName] = function() {
-      const args = arguments;
-      return this.el.componentOnReady().then((el: any) => el[methodName].apply(el, args));
+export function ProxyInputs(inputs: string[]) {
+  const decorator = function <T extends {new(...args:any[])}>(constructor:T) {
+    return class extends constructor {
+      constructor(...args:any[]) {
+        super(...args);
+        const Prototype = constructor.prototype;
+        inputs.forEach((item) => {
+          Object.defineProperty(Prototype, item, {
+            get() { return this.el[item]; },
+            set(val: any) { this.el[item] = val; },
+          });
+        });
+      }
     };
-  });
+  };
+  return decorator;
 }
 
 function proxyOutputs(instance: any, el: any, events: string[]) {
@@ -30,6 +28,7 @@ function proxyOutputs(instance: any, el: any, events: string[]) {
 import { Components } from '@porsche-ui/ui-kit-js'
 
 export declare interface PButtonIcon extends Components.PButtonIcon {}
+@ProxyInputs(['disabled', 'href', 'icon', 'label', 'loading', 'target', 'theme', 'type', 'variant'])
 @Component({ selector: 'p-button-icon', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['disabled', 'href', 'icon', 'label', 'loading', 'target', 'theme', 'type', 'variant'] })
 export class PButtonIcon {
   pClick!: EventEmitter<CustomEvent>;
@@ -42,9 +41,9 @@ export class PButtonIcon {
     proxyOutputs(this, this.el, ['pClick', 'pFocus', 'pBlur']);
   }
 }
-proxyInputs(PButtonIcon, ['disabled', 'href', 'icon', 'label', 'loading', 'target', 'theme', 'type', 'variant']);
 
 export declare interface PButtonRegular extends Components.PButtonRegular {}
+@ProxyInputs(['disabled', 'href', 'icon', 'loading', 'small', 'target', 'theme', 'type', 'variant'])
 @Component({ selector: 'p-button-regular', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['disabled', 'href', 'icon', 'loading', 'small', 'target', 'theme', 'type', 'variant'] })
 export class PButtonRegular {
   pClick!: EventEmitter<CustomEvent>;
@@ -57,9 +56,9 @@ export class PButtonRegular {
     proxyOutputs(this, this.el, ['pClick', 'pFocus', 'pBlur']);
   }
 }
-proxyInputs(PButtonRegular, ['disabled', 'href', 'icon', 'loading', 'small', 'target', 'theme', 'type', 'variant']);
 
 export declare interface PFlex extends Components.PFlex {}
+@ProxyInputs(['alignContent', 'alignItems', 'direction', 'flow', 'gap', 'justifyContent', 'wrap'])
 @Component({ selector: 'p-flex', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['alignContent', 'alignItems', 'direction', 'flow', 'gap', 'justifyContent', 'wrap'] })
 export class PFlex {
   protected el: HTMLElement;
@@ -68,9 +67,9 @@ export class PFlex {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PFlex, ['alignContent', 'alignItems', 'direction', 'flow', 'gap', 'justifyContent', 'wrap']);
 
 export declare interface PFlexItem extends Components.PFlexItem {}
+@ProxyInputs(['alignSelf', 'flex', 'grow', 'offset', 'shrink', 'width'])
 @Component({ selector: 'p-flex-item', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['alignSelf', 'flex', 'grow', 'offset', 'shrink', 'width'] })
 export class PFlexItem {
   protected el: HTMLElement;
@@ -79,9 +78,9 @@ export class PFlexItem {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PFlexItem, ['alignSelf', 'flex', 'grow', 'offset', 'shrink', 'width']);
 
 export declare interface PGrid extends Components.PGrid {}
+@ProxyInputs(['direction', 'gap'])
 @Component({ selector: 'p-grid', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['direction', 'gap'] })
 export class PGrid {
   protected el: HTMLElement;
@@ -90,9 +89,9 @@ export class PGrid {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PGrid, ['direction', 'gap']);
 
 export declare interface PGridChild extends Components.PGridChild {}
+@ProxyInputs(['offset', 'size'])
 @Component({ selector: 'p-grid-child', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['offset', 'size'] })
 export class PGridChild {
   protected el: HTMLElement;
@@ -101,9 +100,9 @@ export class PGridChild {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PGridChild, ['offset', 'size']);
 
 export declare interface PHeadline extends Components.PHeadline {}
+@ProxyInputs(['align', 'color', 'ellipsis', 'tag', 'variant'])
 @Component({ selector: 'p-headline', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['align', 'color', 'ellipsis', 'tag', 'variant'] })
 export class PHeadline {
   protected el: HTMLElement;
@@ -112,9 +111,9 @@ export class PHeadline {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PHeadline, ['align', 'color', 'ellipsis', 'tag', 'variant']);
 
 export declare interface PIcon extends Components.PIcon {}
+@ProxyInputs(['ariaLabel', 'color', 'lazy', 'size', 'source'])
 @Component({ selector: 'p-icon', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['ariaLabel', 'color', 'lazy', 'size', 'source'] })
 export class PIcon {
   protected el: HTMLElement;
@@ -123,9 +122,9 @@ export class PIcon {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PIcon, ['ariaLabel', 'color', 'lazy', 'size', 'source']);
 
 export declare interface PMarque extends Components.PMarque {}
+@ProxyInputs(['trademark'])
 @Component({ selector: 'p-marque', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['trademark'] })
 export class PMarque {
   protected el: HTMLElement;
@@ -134,9 +133,9 @@ export class PMarque {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PMarque, ['trademark']);
 
 export declare interface PPagination extends Components.PPagination {}
+@ProxyInputs(['activePage', 'itemsPerPage', 'label', 'labelNext', 'labelPage', 'labelPrev', 'pageRange', 'theme', 'totalItemsCount'])
 @Component({ selector: 'p-pagination', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['activePage', 'itemsPerPage', 'label', 'labelNext', 'labelPage', 'labelPrev', 'pageRange', 'theme', 'totalItemsCount'] })
 export class PPagination {
   pClick!: EventEmitter<CustomEvent>;
@@ -147,9 +146,9 @@ export class PPagination {
     proxyOutputs(this, this.el, ['pClick']);
   }
 }
-proxyInputs(PPagination, ['activePage', 'itemsPerPage', 'label', 'labelNext', 'labelPage', 'labelPrev', 'pageRange', 'theme', 'totalItemsCount']);
 
 export declare interface PSpinner extends Components.PSpinner {}
+@ProxyInputs(['allyLabel', 'size', 'theme'])
 @Component({ selector: 'p-spinner', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['allyLabel', 'size', 'theme'] })
 export class PSpinner {
   protected el: HTMLElement;
@@ -158,9 +157,9 @@ export class PSpinner {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PSpinner, ['allyLabel', 'size', 'theme']);
 
 export declare interface PText extends Components.PText {}
+@ProxyInputs(['align', 'color', 'ellipsis', 'tag', 'variant'])
 @Component({ selector: 'p-text', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['align', 'color', 'ellipsis', 'tag', 'variant'] })
 export class PText {
   protected el: HTMLElement;
@@ -169,9 +168,9 @@ export class PText {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PText, ['align', 'color', 'ellipsis', 'tag', 'variant']);
 
 export declare interface PTextLink extends Components.PTextLink {}
+@ProxyInputs(['color', 'download', 'href', 'icon', 'rel', 'tag', 'target', 'variant'])
 @Component({ selector: 'p-text-link', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['color', 'download', 'href', 'icon', 'rel', 'tag', 'target', 'variant'] })
 export class PTextLink {
   pClick!: EventEmitter<CustomEvent>;
@@ -182,9 +181,9 @@ export class PTextLink {
     proxyOutputs(this, this.el, ['pClick']);
   }
 }
-proxyInputs(PTextLink, ['color', 'download', 'href', 'icon', 'rel', 'tag', 'target', 'variant']);
 
 export declare interface PTextList extends Components.PTextList {}
+@ProxyInputs(['color', 'listType'])
 @Component({ selector: 'p-text-list', changeDetection: 0, template: '<ng-content></ng-content>', inputs: ['color', 'listType'] })
 export class PTextList {
   protected el: HTMLElement;
@@ -193,7 +192,6 @@ export class PTextList {
     this.el = r.nativeElement;
   }
 }
-proxyInputs(PTextList, ['color', 'listType']);
 
 export declare interface PTextListItem extends Components.PTextListItem {}
 @Component({ selector: 'p-text-list-item', changeDetection: 0, template: '<ng-content></ng-content>' })
