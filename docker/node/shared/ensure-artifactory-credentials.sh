4e67d84bdef774edefc0e37f5b35b20d35e434b8
Oliver Hook <oliver.hook@mhp.com>
2019-10-23 08:36:05 +0200

    remove IONIC REGISTRY TOKEN | oh | #housekeeping

--- docker/node/shared/ensure-artifactory-credentials.sh
+++ docker/node/shared/ensure-artifactory-credentials.sh
@@ -10,11 +10,6 @@ if [[ -z "${PORSCHE_NPM_REGISTRY_TOKEN}" ]]; then
   exit 1
 fi
 
-if [[ -z "${IONIC_NPM_REGISTRY_TOKEN}" ]]; then
-  echo "Please provide the \$IONIC_NPM_REGISTRY_TOKEN environment variable. Have a look at README for more information."
-  exit 1
-fi
-
 cleanup_credentials() {
   local exit_code=$?
   echo "task: [$(date)] \"cleanup_credentials\""
@@ -25,7 +20,6 @@ cleanup_credentials() {
 setup_credentials() {
   echo "task: [$(date)] \"setup_credentials\""
   echo "//porscheui.jfrog.io/porscheui/api/npm/npm/:_authToken=${PORSCHE_NPM_REGISTRY_TOKEN}" > "${HOME}/.npmrc"
-  echo "//registry.ionicframework.com/:_authToken=${IONIC_NPM_REGISTRY_TOKEN}" >> "${HOME}/.npmrc"
 }
 
 trap cleanup_credentials EXIT
