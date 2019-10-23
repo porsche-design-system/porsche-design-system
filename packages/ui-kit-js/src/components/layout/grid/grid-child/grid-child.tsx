c813bc675bde6b7088111385f13fa64ff2c6c318
Stephan Schröter <stephan.schroeter@mhp.com>
2019-10-23 14:31:38 +0200

    Improve typings for BreakpointCustomizable | cs / sas | #288

--- packages/ui-kit-js/src/components/layout/grid/grid-child/grid-child.tsx
+++ packages/ui-kit-js/src/components/layout/grid/grid-child/grid-child.tsx
@@ -8,10 +8,10 @@ import { BreakpointCustomizable, mapBreakpointPropToClasses, prefix } from '../.
 })
 export class GridChild {
   /** The size of the column. Can be between 1 and 12. Also defines the size of the column for specific breakpoints, like {"base": 6, "l": 3}. You always need to provide a base value when doing this. */
-  @Prop() public size?: BreakpointCustomizable<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12> | string = 1;
+  @Prop() public size?: BreakpointCustomizable<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12> = 1;
 
   /** The offset of the column. Can be between 0 and 11. Also defines the offset of the column for specific breakpoints, like {"base": 6, "l": 3}. You always need to provide a base value when doing this. */
-  @Prop() public offset?: BreakpointCustomizable<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11> | string = 0;
+  @Prop() public offset?: BreakpointCustomizable<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11> = 0;
 
   public render(): JSX.Element {
     const isJsonString = (str: string) => {
