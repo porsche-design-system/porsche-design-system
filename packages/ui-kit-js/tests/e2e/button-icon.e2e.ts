d39c23942181b232f11f966aec95b46816d2eb00
Christoph Stickel <christoph@neochic.de>
2019-10-28 14:49:42 +0100

    improved event handling for button-icon | cs

--- packages/ui-kit-js/tests/e2e/button-icon.e2e.ts
+++ packages/ui-kit-js/tests/e2e/button-icon.e2e.ts
@@ -14,13 +14,12 @@ describe('button-icon', () => {
     const button = await page.find('p-button-icon >>> button');
     const host = await page.find('#hostElement');
     const wrapper = await page.find('div');
-    const hostEventSpyPromise = wrapper.spyOnEvent('click');
-    const wrapperEventSpyPromise = wrapper.spyOnEvent('click');
+    const hostEventSpy = await wrapper.spyOnEvent('click');
+    const wrapperEventSpy = await wrapper.spyOnEvent('click');
     await button.click();
     await host.click();
 
-    for (const promise of [hostEventSpyPromise, wrapperEventSpyPromise]) {
-      const spy = await promise;
+    for (const spy of [hostEventSpy, wrapperEventSpy]) {
       expect(spy.length).toBe(2);
       for (const event of spy.events) {
         expect(event.target.id).toBe(host.id);
@@ -35,9 +34,8 @@ describe('button-icon', () => {
     const host = await page.find('p-button-icon');
     const form = await page.find('form');
     for(const triggerElement of [host, button]) {
-      const spyPromise = form.spyOnEvent('submit');
+      const spy = await form.spyOnEvent('submit');
       await triggerElement.click();
-      const spy = await spyPromise;
       expect(spy.length).toBe(1);
     }
   });
@@ -58,9 +56,160 @@ describe('button-icon', () => {
     `);
     const button = await page.find('p-button-icon >>> button');
     const form = await page.find('form');
-    const spyPromise = form.spyOnEvent('submit');
+    const spy = await form.spyOnEvent('submit');
     await button.click();
-    const spy = await spyPromise;
     expect(spy.length).toBe(0);
+  });
+
+  it(`should trigger focus&blur events at the correct time`, async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+          <div id="wrapper">
+            <a href="#" id="before">before</a>
+            <p-button-icon></p-button-icon>
+            <a href="#" id="after">after</a>
+          </div>
+    `);
+    const button = await page.find('p-button-icon');
+    const before = await page.find('#before');
+    const after = await page.find('#after');
+    await before.focus();
+
+    const beforeFocusSpy = await before.spyOnEvent('focus');
+    const focusSpy = await button.spyOnEvent('focus');
+    const focusinSpy = await button.spyOnEvent('focusin');
+    const blurSpy = await button.spyOnEvent('blur');
+    const focusoutSpy = await button.spyOnEvent('focusout');
+    const afterFocusSpy = await after.spyOnEvent('focus');
+    await page.keyboard.press('Tab');
+    expect(focusSpy.length).toBe(1);
+    expect(focusinSpy.length).toBe(1);
+    expect(blurSpy.length).toBe(0);
+    expect(focusoutSpy.length).toBe(0);
+    expect(afterFocusSpy.length).toBe(0);
+
+    await page.keyboard.press('Tab');
+
+    expect(focusSpy.length).toBe(1);
+    expect(focusinSpy.length).toBe(1);
+    expect(blurSpy.length).toBe(1);
+    expect(focusoutSpy.length).toBe(1);
+    expect(afterFocusSpy.length).toBe(1);
+
+    // tab back
+    await page.keyboard.down('ShiftLeft');
+    await page.keyboard.press('Tab');
+    expect(focusSpy.length).toBe(2);
+    expect(focusinSpy.length).toBe(2);
+    expect(blurSpy.length).toBe(1);
+    expect(focusoutSpy.length).toBe(1);
+    expect(beforeFocusSpy.length).toBe(0);
+
+    await page.keyboard.down('ShiftLeft');
+    await page.keyboard.press('Tab');
+
+    expect(focusSpy.length).toBe(2);
+    expect(focusinSpy.length).toBe(2);
+    expect(blurSpy.length).toBe(2);
+    expect(focusoutSpy.length).toBe(2);
+    expect(beforeFocusSpy.length).toBe(1);
+
+    await page.keyboard.up('ShiftLeft');
+  });
+
+  it(`should provide methods to focus&blur the element`, async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+          <div id="wrapper">
+            <a href="#" id="before">before</a>
+            <p-button-icon></p-button-icon>
+          </div>
+    `);
+
+    async function buttonHasFocus() {
+      return await page.evaluate(() => {
+        const buttonElement = document.querySelector('p-button-icon') as HTMLElement;
+        return document.activeElement === buttonElement;
+      });
+    }
+
+    const button = await page.find('p-button-icon');
+    const before = await page.find('#before');
+    await before.focus();
+    expect(await buttonHasFocus()).toBe(false);
+    await button.focus();
+    expect(await buttonHasFocus()).toBe(true);
+    await page.evaluate(() => {
+      const buttonElement = document.querySelector('p-button-icon') as HTMLElement;
+      buttonElement.blur();
+    });
+    expect(await buttonHasFocus()).toBe(false);
+  });
+
+  it(`should be removed from tab index order for pTabindex -1`, async () => {
+    const page = await newE2EPage();
+    await page.setContent(`
+          <div id="wrapper">
+            <a href="#" id="before">before</a>
+            <p-button-icon p-tabindex="-1"></p-button-icon>
+            <a href="#" id="after">after</a>
+          </div>
+    `);
+
+    const button = await page.find('p-button-icon');
+    const before = await page.find('#before');
+    const after = await page.find('#after');
+
+    await before.focus();
+
+    const focusSpy = await button.spyOnEvent('focus');
+    const afterFocusSpy = await after.spyOnEvent('focus');
+
+    await page.keyboard.press('Tab');
+    expect(focusSpy.length).toBe(0);
+    expect(afterFocusSpy.length).toBe(1);
+    await page.keyboard.press('Tab');
+    expect(focusSpy.length).toBe(0);
+    expect(afterFocusSpy.length).toBe(1);
+  });
+
+  it(`should ignore native tab index and show a warning on console`, async () => {
+    const page = await newE2EPage();
+    const consoleLogs = [];
+    page.on('console', msg => {
+      consoleLogs.push({
+        type: msg.type(),
+        text: msg.text()
+      });
+    });
+    await page.setContent(`
+          <div id="wrapper">
+            <a href="#" id="before">before</a>
+            <p-button-icon tabindex="1"></p-button-icon>
+            <a href="#" id="after">after</a>
+          </div>
+    `);
+
+    expect(consoleLogs.length).toBe(1);
+    expect(consoleLogs[0].type).toBe('warning');
+    expect(consoleLogs[0].text).toBe('You can not set the tabindex on the host element of Porsche UI-Kit components. Please use `p-tabindex` instead.');
+
+    const button = await page.find('p-button-icon');
+    const before = await page.find('#before');
+    const after = await page.find('#after');
+
+    const focusSpy = await button.spyOnEvent('focus');
+    const afterFocusSpy = await after.spyOnEvent('focus');
+
+    await before.focus();
+
+    expect(focusSpy.length).toBe(0);
+    expect(afterFocusSpy.length).toBe(0);
+    await page.keyboard.press('Tab');
+    expect(focusSpy.length).toBe(1);
+    expect(afterFocusSpy.length).toBe(0);
+    await page.keyboard.press('Tab');
+    expect(focusSpy.length).toBe(1);
+    expect(afterFocusSpy.length).toBe(1);
   });
 });
