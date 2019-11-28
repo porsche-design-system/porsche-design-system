291e34bcb9de7b7ad7cab6266648b3876dd88d91
skoller <simon.koller@mhp.com>
2019-11-28 14:58:56 +0100

    refactor-docs | update about and disclaimer

--- packages/design-system/src/components/Disclaimer.vue
+++ packages/design-system/src/components/Disclaimer.vue
@@ -2,24 +2,23 @@
   <div class="disclaimer">
     <p-text color="porsche-light">
       A stable version of the Porsche UI Kit 1.0 is not released yet. The available alpha version of Porsche UI Kit 1.0
-      is meant to be used for testing purpose only.
+      is meant to be used for testing purposesw only.
     </p-text>
   </div>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
-  @Component
-  export default class Disclaimer extends Vue {
-  }
+@Component
+export default class Disclaimer extends Vue {}
 </script>
 
 <style scoped lang="scss">
-  @import "~@porsche-ui/ui-kit-scss-utils/index";
+@import '~@porsche-ui/ui-kit-scss-utils/index';
 
-  .disclaimer {
-    padding: $p-spacing-8 $p-spacing-32;
-    background: $p-color-porsche-red;
-  }
+.disclaimer {
+  padding: $p-spacing-8 $p-spacing-32;
+  background: $p-color-porsche-red;
+}
 </style>
