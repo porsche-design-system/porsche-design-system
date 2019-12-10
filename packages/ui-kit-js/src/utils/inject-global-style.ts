38433652334fc55023f5edf077e2a981fec5c27c
Christoph Stickel <christoph@neochic.de>
2019-12-10 19:15:17 +0100

    add disable focus on click hack | cs

---
+++ packages/ui-kit-js/src/utils/inject-global-style.ts
@@ -0,0 +1,7 @@
+export function injectGlobalStyle() {
+  const link = document.createElement('link');
+  link.href = 'https://cdn.ui.porsche.com/porsche-ui-kit/styles/v1/porsche-ui-kit.css';
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  document.getElementsByTagName('head')[0].appendChild(link);
+}
