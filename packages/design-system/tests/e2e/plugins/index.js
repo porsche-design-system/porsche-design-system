ded27837fe10262774ed52c885e395d0b72e7beb
Stephan Schröter <stephan.schroeter@mhp.com>
2019-12-13 19:21:03 +0100

    Fix e2e | cs / oh / sas

--- packages/design-system/tests/e2e/plugins/index.js
+++ packages/design-system/tests/e2e/plugins/index.js
@@ -8,17 +8,36 @@
 /* eslint-disable import/no-extraneous-dependencies, global-require, arrow-body-style */
 // const webpack = require('@cypress/webpack-preprocessor')
 
+const puppeteer = require('puppeteer');
+
 module.exports = (on, config) => {
   // on('file:preprocessor', webpack({
   //  webpackOptions: require('@vue/cli-service/webpack.config'),
   //  watchOptions: {}
   // }))
 
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.family === 'chrome') {
+      args.push('--disable-dev-shm-usage');
+      args.push('--headless');
+    }
+
+    return args
+  });
+
   return Object.assign({}, config, {
     fixturesFolder: 'tests/e2e/fixtures',
     integrationFolder: 'tests/e2e/specs',
     screenshotsFolder: 'tests/e2e/screenshots',
     videosFolder: 'tests/e2e/videos',
-    supportFile: 'tests/e2e/support/index.js'
+    supportFile: 'tests/e2e/support/index.js',
+    browsers: [ {
+        name: 'chrome',
+        family: 'chrome',
+        displayName: 'Chrome',
+        path: puppeteer.executablePath(),
+        version: '79.0.3945.0',
+        majorVersion: 79
+    } ]
   })
 };
