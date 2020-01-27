df79dfa6e1b316d9e666fd9ab941a807d462a73a
Stephan Schröter <stephan.schroeter@mhp.com>
2020-01-27 12:34:21 +0100

    Optimize unit tests | cs / sas | #458

--- packages/components-js/tests/unit/utils/line-height.spec.ts
+++ packages/components-js/tests/unit/utils/line-height.spec.ts
@@ -3,13 +3,15 @@ import { lineHeightFactor } from '../../../src/utils';
 describe('lineHeight', () => {
 
   it('should return correct line-height value based on type scale definition', () => {
-    const result1 = lineHeightFactor(16);
-    const result2 = lineHeightFactor(90);
-    const result3 = lineHeightFactor(33);
 
-    expect(result1).toEqual(1.5);
-    expect(result2).toEqual(1.2);
-    expect(result3).toEqual(1.3333333333333333);
+    expect(lineHeightFactor(14)).toEqual(1.4285714285714286); // => 20px
+    expect(lineHeightFactor(15)).toEqual(1.6); // => 24px
+    expect(lineHeightFactor(16)).toEqual(1.5); // => 24px
+    expect(lineHeightFactor(17)).toEqual(1.411764705882353); // => 24px
+    expect(lineHeightFactor(18)).toEqual(1.5555555555555556); // => 28px
+
+    expect(lineHeightFactor(90)).toEqual(1.2); // => 108px
+
+    expect(lineHeightFactor(33)).toEqual(1.3333333333333333); // => 44px
   });
-
 });
