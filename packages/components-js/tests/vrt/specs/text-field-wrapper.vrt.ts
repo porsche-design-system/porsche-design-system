38470e779f212567cd822807cd2f7faba5b324ba
Oliver Hook <oliver.hook@mhp.com>
2020-02-11 13:07:31 +0100

    add vrt tests | oh | #17

---
+++ packages/components-js/tests/vrt/specs/text-field-wrapper.vrt.ts
@@ -0,0 +1,20 @@
+import 'jasmine';
+import {VisualRegressionTester} from '@porsche-ui/visual-regression-tester';
+import {getVisualRegressionTester} from '../helpers/setup';
+
+describe('Text Field Wrapper', () => {
+  let vrt: VisualRegressionTester;
+
+  beforeAll(async () => {
+    vrt = await getVisualRegressionTester();
+  });
+
+  it('should have no visual regression', async () => {
+    expect(
+      await vrt.test('text-field-wrapper', async () => {
+        await vrt.goTo('/src/components/form/text-field-wrapper/text-field-wrapper.test.html');
+        await vrt.focus('#test-focus-state');
+      })
+    ).toBeFalsy();
+  });
+});
