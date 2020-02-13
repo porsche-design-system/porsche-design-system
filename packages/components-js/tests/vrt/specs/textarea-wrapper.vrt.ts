02a4a85cb81bd58ba0b0e218ce2ab5d56aee0a0a
Oliver Hook <oliver.hook@mhp.com>
2020-02-13 14:28:10 +0100

    Add vrt | oh | #208

---
+++ packages/components-js/tests/vrt/specs/textarea-wrapper.vrt.ts
@@ -0,0 +1,20 @@
+import 'jasmine';
+import {VisualRegressionTester} from '@porsche-ui/visual-regression-tester';
+import {getVisualRegressionTester} from '../helpers/setup';
+
+fdescribe('Textarea Wrapper', () => {
+  let vrt: VisualRegressionTester;
+
+  beforeAll(async () => {
+    vrt = await getVisualRegressionTester();
+  });
+
+  it('should have no visual regression', async () => {
+    expect(
+      await vrt.test('textarea-wrapper', async () => {
+        await vrt.goTo('/src/components/form/textarea-wrapper/textarea-wrapper.test.html');
+        await vrt.focus('#test-focus-state');
+      })
+    ).toBeFalsy();
+  });
+});
