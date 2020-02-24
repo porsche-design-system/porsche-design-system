ecbbeb39334dc43ae18a255afc9351ff1178c06d
Matthias Haegele <matthias.haegele@mhp.com>
2020-02-24 14:39:07 +0100

    Implement Polyfill / CBT test for stencil | mh | housekeeping

--- packages/components-js/tests/cbt/specs/overview.cbt.ts
+++ packages/components-js/tests/cbt/specs/overview.cbt.ts
@@ -9,7 +9,22 @@ describe('Components Overview', () => {
     cbt = await getCrossBrowserTester();
   });
 
-  it('should have no visual regression in IE11', async () => {
-    expect(await cbt.test('overview', '/index.html')).toBeFalsy();
+  it('basic should have no visual regression in IE11', async () => {
+    expect(await cbt.test('overview-basic', '/src/#basic')).toBeFalsy();
+  });
+  it('action should have no visual regression in IE11', async () => {
+    expect(await cbt.test('overview-action', '/src/#action')).toBeFalsy();
+  });
+  it('feedback should have no visual regression in IE11', async () => {
+    expect(await cbt.test('overview-feedback', '/src/#feedback')).toBeFalsy();
+  });
+  it('icon should have no visual regression in IE11', async () => {
+    expect(await cbt.test('overview-icon', '/src/#icon')).toBeFalsy();
+  });
+  it('layout should have no visual regression in IE11', async () => {
+    expect(await cbt.test('overview-layout', '/src/#layout')).toBeFalsy();
+  });
+  it('navigation should have no visual regression in IE11', async () => {
+    expect(await cbt.test('overview-navigation', '/src/#navigation')).toBeFalsy();
   });
 });
