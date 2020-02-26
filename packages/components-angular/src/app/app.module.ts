9c744475556d78de4bdcf0140694a98c1a9774f0
Stephan Schröter <stephan.schroeter@mhp.com>
2020-02-26 14:16:55 +0100

    Optimize angular routing | cs / sas

--- packages/components-angular/src/app/app.module.ts
+++ packages/components-angular/src/app/app.module.ts
@@ -2,23 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { PorscheDesignSystemModule } from '@porsche-design-system/components-angular';
 import { AppComponent } from './app.component';
-import { AppNavigationComponent } from './app-navigation/app-navigation.component';
-import { AppBasicComponent } from './app-basic/app-basic.component';
-import { AppActionComponent } from './app-action/app-action.component';
-import { AppFeedbackComponent } from './app-feedback/app-feedback.component';
-import { AppIconComponent } from './app-icon/app-icon.component';
-import { AppLayoutComponent } from './app-layout/app-layout.component';
-import {AppRoutingModule} from "./app-routing.module";
+import { NavigationComponent } from './pages/navigation/navigation.component';
+import { BasicComponent } from './pages/basic/basic.component';
+import { ActionComponent } from './pages/action/action.component';
+import { FeedbackComponent } from './pages/feedback/feedback.component';
+import { IconComponent } from './pages/icon/icon.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavigationComponent,
-    AppBasicComponent,
-    AppActionComponent,
-    AppFeedbackComponent,
-    AppIconComponent,
-    AppLayoutComponent
+    NavigationComponent,
+    BasicComponent,
+    ActionComponent,
+    FeedbackComponent,
+    IconComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
@@ -28,4 +28,5 @@ import {AppRoutingModule} from "./app-routing.module";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
