62d051f8834d221f718e0cee3b5a6348ed5408fc
Christoph Stickel <christoph@neochic.de>
2020-02-27 07:57:41 +0100

    improve checkbox-wrapper tests | cs | #144

--- packages/components-js/tests/e2e/checkbox-wrapper.e2e.ts
+++ packages/components-js/tests/e2e/checkbox-wrapper.e2e.ts
@@ -1,4 +1,6 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { E2EPage, newE2EPage } from '@stencil/core/testing';
+import { Components } from '../../src';
+import PIcon = Components.PIcon;
 
 describe('checkbox-wrapper', () => {
   it('should render', async () => {
@@ -11,7 +13,7 @@ describe('checkbox-wrapper', () => {
     expect(el).not.toBeNull();
   });
 
-  it('should check checkbox when input is clicked', async () => {
+  it('should toggle checkbox when input is clicked', async () => {
     const page = await newE2EPage();
     await page.setContent(`
       <p-checkbox-wrapper label="Some label">
@@ -32,7 +34,7 @@ describe('checkbox-wrapper', () => {
     expect(iconWrapper).not.toHaveClass('p-checkbox-wrapper__icon-wrapper--checked');
   });
 
-  it('should check checkbox when label text is clicked', async () => {
+  it('should toggle checkbox when label text is clicked', async () => {
     const page = await newE2EPage();
     await page.setContent(`
       <p-checkbox-wrapper label="Some label">
@@ -41,19 +43,23 @@ describe('checkbox-wrapper', () => {
 
     const iconWrapper = await page.find('p-checkbox-wrapper >>> .p-checkbox-wrapper__icon-wrapper');
     const labelText = await page.find('p-checkbox-wrapper >>> .p-checkbox-wrapper__label-text');
+    const input = await page.find('input[type="checkbox"]');
 
     expect(iconWrapper).not.toHaveClass('p-checkbox-wrapper__icon-wrapper--checked');
+    expect(await input.getProperty('checked')).toBe(false);
 
     await labelText.click();
 
     expect(iconWrapper).toHaveClass('p-checkbox-wrapper__icon-wrapper--checked');
+    expect(await input.getProperty('checked')).toBe(true);
 
     await labelText.click();
 
     expect(iconWrapper).not.toHaveClass('p-checkbox-wrapper__icon-wrapper--checked');
+    expect(await input.getProperty('checked')).toBe(false);
   });
 
-  it('should check checkbox when checkbox is changed programmatically', async () => {
+  it('should check/uncheck checkbox when checkbox is changed programmatically', async () => {
     const page = await newE2EPage();
     await page.setContent(`
       <p-checkbox-wrapper label="Some label">
@@ -127,5 +133,105 @@ describe('checkbox-wrapper', () => {
     }
 
     expect(await getIconWrapper()).not.toHaveClass('p-checkbox-wrapper__icon-wrapper--disabled');
+  });
+
+  describe('indeterminate state', () => {
+    const getIconName = async (page: E2EPage) => {
+      return await page.evaluate(() => {
+        const wrapper = document.querySelector('p-checkbox-wrapper');
+        const icon =  wrapper.shadowRoot.querySelector('p-icon') as PIcon;
+        return icon.name;
+      });
+    };
+
+    const setIndeterminate = async (page: E2EPage, value: boolean) => {
+      await page.evaluate((indeterminate: boolean) => {
+        const input = document.querySelector('input[type="checkbox"]') as HTMLInputElement;
+        input.indeterminate = indeterminate;
+      }, value);
+
+      await page.waitForChanges();
+    };
+
+    const setChecked = async (page: E2EPage, value: boolean) => {
+      await page.evaluate((checked: boolean) => {
+        const input = document.querySelector('input[type="checkbox"]') as HTMLInputElement;
+        input.checked = checked;
+      }, value);
+
+      await page.waitForChanges();
+    };
+
+    const showsIcon = async (page: E2EPage) => {
+      const icon = await page.find('p-checkbox-wrapper >>> .p-checkbox-wrapper__icon');
+      const styles = await icon.getComputedStyle();
+      await page.waitFor(parseFloat(styles.transitionDuration) * 1000);
+      return await icon.isVisible();
+    };
+
+    it('should show indeterminate state when checkbox is set to indeterminate', async () => {
+      const page = await newE2EPage();
+      await page.setContent(`
+      <p-checkbox-wrapper label="Some label">
+        <input type="checkbox" name="some-name"/>
+      </p-checkbox-wrapper>`);
+
+      expect(await getIconName(page)).toBe('check');
+      expect(await showsIcon(page)).toBe(false);
+
+      await setIndeterminate(page, true);
+      expect(await getIconName(page)).toBe('subtract');
+      expect(await showsIcon(page)).toBe(true);
+
+      await setIndeterminate(page, false);
+      expect(await getIconName(page)).toBe('check');
+      expect(await showsIcon(page)).toBe(false);
+    });
+
+    it('should remove indeterminate state when checkbox value is changed by the user', async () => {
+      const page = await newE2EPage();
+      await page.setContent(`
+      <p-checkbox-wrapper label="Some label">
+        <input type="checkbox" name="some-name"/>
+      </p-checkbox-wrapper>`);
+
+      const input = await page.find('input[type="checkbox"]');
+
+      await setIndeterminate(page, true);
+      expect(await getIconName(page)).toBe('subtract');
+      expect(await showsIcon(page)).toBe(true);
+
+      await input.click();
+      expect(await getIconName(page)).toBe('check');
+      expect(await showsIcon(page)).toBe(true);
+
+      await setIndeterminate(page, true);
+      expect(await getIconName(page)).toBe('subtract');
+      expect(await showsIcon(page)).toBe(true);
+
+      await input.click();
+      expect(await getIconName(page)).toBe('check');
+      expect(await showsIcon(page)).toBe(false);
+    });
+
+    it('should keep indeterminate state when checkbox value is changed programmatically', async () => {
+      const page = await newE2EPage();
+      await page.setContent(`
+      <p-checkbox-wrapper label="Some label">
+        <input type="checkbox" name="some-name"/>
+      </p-checkbox-wrapper>`);
+
+      await setIndeterminate(page, true);
+      expect(await getIconName(page)).toBe('subtract');
+      expect(await showsIcon(page)).toBe(true);
+
+      await setChecked(page, true);
+      expect(await getIconName(page)).toBe('subtract');
+      expect(await showsIcon(page)).toBe(true);
+
+      await setChecked(page, false);
+      expect(await getIconName(page)).toBe('subtract');
+      expect(await showsIcon(page)).toBe(true);
+    });
   });
 });
