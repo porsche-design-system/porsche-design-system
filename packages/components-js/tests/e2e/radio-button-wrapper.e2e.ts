f13c51d394a58e78c478e98ed160e308aa9ae078
Oliver Hook <oliver.hook@mhp.com>
2020-02-28 14:20:50 +0100

    fix tests | oh | #209

--- packages/components-js/tests/e2e/radio-button-wrapper.e2e.ts
+++ packages/components-js/tests/e2e/radio-button-wrapper.e2e.ts
@@ -7,7 +7,7 @@ describe('radio-button-wrapper', () => {
       <p-radio-button-wrapper label="Some label">
         <input type="radio" name="some-name"/>
       </p-radio-button-wrapper>`);
-    const el = await page.find('p-radio-button-wrapper >>> .p-radio-button-wrapper__fake-radio');
+    const el = await page.find('p-radio-button-wrapper >>> .p-radio-button-wrapper__fake-radio-button');
     expect(el).not.toBeNull();
   });
 
@@ -21,21 +21,21 @@ describe('radio-button-wrapper', () => {
         <input type="radio" name="some-name"/>
       </p-radio-button-wrapper>`);
 
-    const fakeRadio1 = await page.find('#radio-1 >>> .p-radio-button-wrapper__fake-radio');
-    const fakeRadio2 = await page.find('#radio-2 >>> .p-radio-button-wrapper__fake-radio');
+    const fakeRadio1 = await page.find('#radio-1 >>> .p-radio-button-wrapper__fake-radio-button');
+    const fakeRadio2 = await page.find('#radio-2 >>> .p-radio-button-wrapper__fake-radio-button');
     const input1 = await page.find('#radio-1 > input[type="radio"]');
     const input2 = await page.find('#radio-2 > input[type="radio"]');
 
-    expect(fakeRadio1).not.toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(fakeRadio1).not.toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
 
     await input1.click();
 
-    expect(fakeRadio1).toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(fakeRadio1).toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
 
     await input2.click();
 
-    expect(fakeRadio1).not.toHaveClass('p-radio-button-wrapper__fake-radio--checked');
-    expect(fakeRadio2).toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(fakeRadio1).not.toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
+    expect(fakeRadio2).toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
   });
 
   it('should check radio-button when label text is clicked', async () => {
@@ -48,21 +48,21 @@ describe('radio-button-wrapper', () => {
         <input type="radio" name="some-name"/>
       </p-radio-button-wrapper>`);
 
-    const fakeRadio1 = await page.find('#radio-1 >>> .p-radio-button-wrapper__fake-radio');
-    const fakeRadio2 = await page.find('#radio-2 >>> .p-radio-button-wrapper__fake-radio');
+    const fakeRadio1 = await page.find('#radio-1 >>> .p-radio-button-wrapper__fake-radio-button');
+    const fakeRadio2 = await page.find('#radio-2 >>> .p-radio-button-wrapper__fake-radio-button');
     const labelText1 = await page.find('#radio-1 >>> .p-radio-button-wrapper__label-text');
     const labelText2 = await page.find('#radio-2 >>> .p-radio-button-wrapper__label-text');
 
-    expect(fakeRadio1).not.toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(fakeRadio1).not.toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
 
     await labelText1.click();
 
-    expect(fakeRadio1).toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(fakeRadio1).toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
 
     await labelText2.click();
 
-    expect(fakeRadio1).not.toHaveClass('p-radio-button-wrapper__fake-radio--checked');
-    expect(fakeRadio2).toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(fakeRadio1).not.toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
+    expect(fakeRadio2).toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
   });
 
   it('should check radio-button when radio-button is changed programmatically', async () => {
@@ -77,39 +77,39 @@ describe('radio-button-wrapper', () => {
 
     const getFakeRadio1 = async () => {
       const radioButtonWrapper = await page.find('#radio-1');
-      return radioButtonWrapper.shadowRoot.querySelector('.p-radio-button-wrapper__fake-radio');
+      return radioButtonWrapper.shadowRoot.querySelector('.p-radio-button-wrapper__fake-radio-button');
     };
 
     const getFakeRadio2 = async () => {
       const radioButtonWrapper = await page.find('#radio-2');
-      return radioButtonWrapper.shadowRoot.querySelector('.p-radio-button-wrapper__fake-radio');
+      return radioButtonWrapper.shadowRoot.querySelector('.p-radio-button-wrapper__fake-radio-button');
     };
 
     const fakeRadio1Classlist = async () => (await getFakeRadio1()).classList;
     const fakeRadio2Classlist = async () => (await getFakeRadio2()).classList;
 
-    expect(await getFakeRadio1()).not.toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(await getFakeRadio1()).not.toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
 
     await page.evaluate(() => {
       document.querySelectorAll('input')[0].checked = true;
     });
 
     // for some reason we've to requery the iconWrapper each time and .waitForSelector does not work
-    while(!(await fakeRadio1Classlist()).contains('p-radio-button-wrapper__fake-radio--checked')) {
+    while(!(await fakeRadio1Classlist()).contains('p-radio-button-wrapper__fake-radio-button--checked')) {
       await page.waitFor(10);
     }
 
-    expect(await getFakeRadio1()).toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(await getFakeRadio1()).toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
 
     await page.evaluate(() => {
       document.querySelectorAll('input')[1].checked = true;
     });
 
-    while((await fakeRadio2Classlist()).contains('p-radio-button-wrapper__fake-radio--checked')) {
+    while((await fakeRadio2Classlist()).contains('p-radio-button-wrapper__fake-radio-button--checked')) {
       await page.waitFor(10);
     }
 
-    expect(await getFakeRadio2()).not.toHaveClass('p-radio-button-wrapper__fake-radio--checked');
+    expect(await getFakeRadio2()).not.toHaveClass('p-radio-button-wrapper__fake-radio-button--checked');
   });
 
   it('should disable radio-button when radio-button is set disabled programmatically', async () => {
@@ -121,32 +121,32 @@ describe('radio-button-wrapper', () => {
 
     const getFakeRadio1 = async () => {
       const radioButtonWrapper = await page.find('#radio-1');
-      return radioButtonWrapper.shadowRoot.querySelector('.p-radio-button-wrapper__fake-radio');
+      return radioButtonWrapper.shadowRoot.querySelector('.p-radio-button-wrapper__fake-radio-button');
     };
 
     const fakeRadio1Classlist = async () => (await getFakeRadio1()).classList;
 
-    expect(await getFakeRadio1()).not.toHaveClass('p-radio-button-wrapper__fake-radio--disabled');
+    expect(await getFakeRadio1()).not.toHaveClass('p-radio-button-wrapper__fake-radio-button--disabled');
 
     await page.evaluate(() => {
       document.querySelector('input').disabled = true;
     });
 
     // for some reason we've to requery the iconWrapper each time and .waitForSelector does not work
-    while(!(await fakeRadio1Classlist()).contains('p-radio-button-wrapper__fake-radio--disabled')) {
+    while(!(await fakeRadio1Classlist()).contains('p-radio-button-wrapper__fake-radio-button--disabled')) {
       await page.waitFor(10);
     }
 
-    expect(await getFakeRadio1()).toHaveClass('p-radio-button-wrapper__fake-radio--disabled');
+    expect(await getFakeRadio1()).toHaveClass('p-radio-button-wrapper__fake-radio-button--disabled');
 
     await page.evaluate(() => {
       document.querySelector('input').disabled = false;
     });
 
-    while((await fakeRadio1Classlist()).contains('p-radio-button-wrapper__fake-radio--disabled')) {
+    while((await fakeRadio1Classlist()).contains('p-radio-button-wrapper__fake-radio-button--disabled')) {
       await page.waitFor(10);
     }
 
-    expect(await getFakeRadio1()).not.toHaveClass('p-radio-button-wrapper__fake-radio--disabled');
+    expect(await getFakeRadio1()).not.toHaveClass('p-radio-button-wrapper__fake-radio-button--disabled');
   });
 });
