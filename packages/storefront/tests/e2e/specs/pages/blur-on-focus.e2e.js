c227a77a317ed15cbbf2f013c39b3913575e7e45
Matthias Haegele <matthias.haegele@mhp.com>
2020-03-03 11:02:36 +0100

    Optimize folder structure storefront | mh / sas | #519

---
+++ packages/storefront/tests/e2e/specs/pages/blur-on-focus.e2e.js
@@ -0,0 +1,14 @@
+describe('Blur on Focus', () => {
+  it('Should navigate through main navigation and show page title', () => {
+    cy.visit('/#/web');
+
+    cy.get('#app aside nav p-link-pure')
+      .contains('Blur On Focus')
+      .click()
+      .should('have.attr', 'active', 'true');
+
+    cy.get('#app main h1')
+      .should('be.visible')
+      .and('contain', 'Blur on Focus');
+  });
+});
