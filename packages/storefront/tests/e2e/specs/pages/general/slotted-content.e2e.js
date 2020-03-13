77f14f77249e54b73f1cf577b11ff349cc227fca
Oliver Hook <oliver.hook@mhp.com>
2020-03-13 09:16:51 +0100

    Fix e2e tests | oh | #566

--- packages/storefront/tests/e2e/specs/pages/general/slotted-content.e2e.js
+++ packages/storefront/tests/e2e/specs/pages/general/slotted-content.e2e.js
@@ -2,7 +2,7 @@ describe('Slotted Content', () => {
   it('Should navigate through main navigation and show page title', () => {
     cy.visit('/');
 
-    cy.get('#app aside nav p-link-pure')
+    cy.get('#app .sidebar nav p-link-pure')
       .contains('Slotted Content')
       .click()
       .should('have.attr', 'active', 'true');
