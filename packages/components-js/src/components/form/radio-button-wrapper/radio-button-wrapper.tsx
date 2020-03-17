896215f4107cc6b0fdea28df2fed7f1da2125568
Oliver Hook <oliver.hook@mhp.com>
2020-03-17 10:25:46 +0100

    add hover styles | oh | #527

--- packages/components-js/src/components/form/radio-button-wrapper/radio-button-wrapper.tsx
+++ packages/components-js/src/components/form/radio-button-wrapper/radio-button-wrapper.tsx
@@ -66,14 +66,14 @@ export class RadioButtonWrapper {
     return (
       <Host>
         <label class={labelClasses} id={this.state === 'error' && this.labelId}>
-          <span class={fakeRadioButtonClasses}>
-            <slot/>
-          </span>
           {this.isLabelVisible &&
           <p-text class={labelTextClasses} tag='span' color='inherit' onClick={(e: MouseEvent) => this.labelClick(e)}>
             {this.label ? this.label : <span><slot name='label'/></span>}
           </p-text>
           }
+          <span class={fakeRadioButtonClasses}>
+            <slot/>
+          </span>
         </label>
         {this.isMessageVisible &&
         <p-text
