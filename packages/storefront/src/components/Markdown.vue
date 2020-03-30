70443d2094434c72844b0777aaf6421711e4caf3
Oliver Hook <oliver.hook@mhp.com>
2020-03-30 17:23:48 +0200

    fix IE11 Storefront link bug | oh | #544

--- packages/storefront/src/components/Markdown.vue
+++ packages/storefront/src/components/Markdown.vue
@@ -215,7 +215,7 @@
         }
 
         // Links
-        a:not(.p-button-regular):not(.p-button-icon) {
+        a:not(.p-button):not(.p-link) {
           outline: none;
           text-decoration: underline;
           color: $p-color-theme-light-default;
