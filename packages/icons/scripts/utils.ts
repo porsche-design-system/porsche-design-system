95c61a872edfbd51cf1d27cd0da076337b9521cf
Stephan Schröter <stephan.schroeter@mhp.com>
2020-04-03 15:04:35 +0200

    Use tsc for icon package | sas | #550

--- packages/icons/scripts/utils.ts
+++ packages/icons/scripts/utils.ts
@@ -10,7 +10,7 @@ export const toHash = (str: string): string => {
 
 export const toKebabCase = (str: string): string => {
   return str
-    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)!
     .map(x => x.toLowerCase())
     .join('-');
 };
