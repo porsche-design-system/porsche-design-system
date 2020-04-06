bda973611da6b365d5e159c4956d775e482d779c
Matthias Haegele <matthias.haegele@mhp.com>
2020-04-06 12:31:33 +0200

    change mocks | mh | #538

---
+++ packages/components-react/projects/components-wrapper/src/mocks/p-divider-mock.jsx
@@ -0,0 +1,5 @@
+const React = require('react');
+
+module.exports = {
+  PDivider: (props) => <p-divider>{props.children}</p-divider>
+};
---
+++ packages/components-react/projects/components-wrapper/src/mocks/p-textarea-wrapper-mock.jsx
@@ -0,0 +1,5 @@
+const React = require('react');
+
+module.exports = {
+  PTextareaWrapper: (props) => <p-textarea-wrapper>{props.children}</p-textarea-wrapper>
+};
