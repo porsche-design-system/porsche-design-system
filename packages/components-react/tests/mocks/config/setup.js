1877ebfcae50937bd1c0e39452cd1578ddfe7630
Matthias Haegele <matthias.haegele@mhp.com>
2020-04-07 13:45:14 +0200

    Update readme | mh | #538

--- packages/components-react/tests/mocks/config/setup.js
+++ packages/components-react/tests/mocks/config/setup.js
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom/extend-expect';
 
 jest.mock('../../../projects/components-wrapper/src/lib/components', () => {
-  return require('../../../projects/components-wrapper/src/mocks/mock-collection');
+  return require('../../../projects/components-wrapper/src/mocks/all');
 });
