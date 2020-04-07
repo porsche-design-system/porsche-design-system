d99b71b6729b6bdf6b6f9ea38a5b30f8bf885669
Chris Albert <mail@christophalbert.com>
2020-04-07 11:28:32 +0200

    Use actual mocks in mock-collection | ca | #538

--- packages/components-react/projects/components-wrapper/src/mocks/mock-collection.jsx
+++ packages/components-react/projects/components-wrapper/src/mocks/mock-collection.jsx
@@ -1,55 +1,43 @@
+import { PButton } from './p-button-mock';
+import { PButtonPure } from './p-button-pure-mock';
+import { PCheckboxWrapper } from './p-checkbox-wrapper-mock';
+import { PDivider } from './p-divider-mock';
+import { PFlex } from './p-flex-mock';
+import { PFlexItem } from './p-flex-item-mock';
+import { PGrid } from './p-grid-mock';
+import { PGridItem } from './p-grid-item-mock';
+import { PHeadline } from './p-headline-mock';
+import { PIcon } from './p-icon-mock';
+import { PLink } from './p-link-mock';
+import { PLinkPure } from './p-link-pure-mock';
+import { PMarque } from './p-marque-mock';
 import { PPagination } from './p-pagination-mock';
-
-const React = require('react');
+import { PRadioButtonWrapper } from './p-radio-button-wrapper-mock';
+import { PSelectWrapper } from './p-select-wrapper-mock';
+import { PSpinner } from './p-spinner-mock';
+import { PText } from './p-text-mock';
+import { PTextareaWrapper } from './p-textarea-wrapper-mock';
+import { PTextFieldWrapper } from './p-text-field-wrapper-mock';
 
 module.exports = {
-  PHeadline: (props) => <p-headline>{props.children}</p-headline>,
-  PText: (props) => <p-text>{props.children}</p-text>,
-  PIcon: (props) => (
-    <p-icon name={props.name}>
-      <svg />
-    </p-icon>
-  ),
-  PMarque: (props) => (
-    <p-marque>
-      <img />
-    </p-marque>
-  ),
-  PButton: (props) => <p-button onClick={props.onClick}>{props.children}</p-button>,
-  PButtonPure: (props) => <p-button-pure onClick={props.onClick}>{props.children}</p-button-pure>,
-  PDivider: () => <p-divider />,
-  PGrid: (props) => <p-grid>{props.children}</p-grid>,
-  PGridItem: (props) => <p-grid-item>{props.children}</p-grid-item>,
-  PFlex: (props) => <p-flex>{props.children}</p-flex>,
-  PFlexItem: (props) => <p-flex-item>{props.children}</p-flex-item>,
-  PSelectWrapper: (props) => <p-select-wrapper>{props.children}</p-select-wrapper>,
-  PCheckboxWrapper: (props) => <p-checkbox-wrapper>{props.children}</p-checkbox-wrapper>,
-  PRadioButtonWrapper: (props) => <p-radio-button-wrapper>{props.children}</p-radio-button-wrapper>,
-  PTextareaWrapper: (props) => <p-textarea-wrapper>{props.children}</p-textarea-wrapper>,
-  PTextFieldWrapper: (props) => <p-textfield-wrapper>{props.children}</p-textfield-wrapper>,
-  /* To enable testing in wrapped Links we have to check if there is a href on our web-component before we simulate the native behaviour */
-  PLink: (props) => {
-    if (props.href) {
-      return (
-        <p-link>
-          <a href={props.href}>{props.children}</a>
-        </p-link>
-      );
-    }
-    return <p-link>{props.children}</p-link>;
-  },
-  PLinkPure: (props) => {
-    if (props.href) {
-      return (
-        <p-link-pure>
-          <a href={props.href}>{props.children}</a>
-        </p-link-pure>
-      );
-    }
-    return <p-link-pure>{props.children}</p-link-pure>;
-  },
-  PSpinner: () => <p-spinner />,
-  /* PPagination uses the onPageChange Event, which you could test. Unfortunately Jest and JS-Dom have many Restrictions (usage of useState is not allowed)
-    to trigger a Mocked Custom Event. */
+  PButton,
+  PButtonPure,
+  PCheckboxWrapper,
+  PDivider,
+  PFlex,
+  PFlexItem,
+  PGrid,
+  PGridItem,
+  PHeadline,
+  PIcon,
+  PLink,
+  PLinkPure,
+  PMarque,
   PPagination,
+  PRadioButtonWrapper,
+  PSelectWrapper,
+  PSpinner,
+  PText,
+  PTextareaWrapper,
+  PTextFieldWrapper,
 };
