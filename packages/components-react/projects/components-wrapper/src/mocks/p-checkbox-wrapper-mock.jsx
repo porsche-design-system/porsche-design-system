7c691a9c1acc7d3291a23d7e1c466b4956a236a9
Chris Albert <mail@christophalbert.com>
2020-04-09 10:59:29 +0200

    Added label and error message support for form elements to jest mocks

--- packages/components-react/projects/components-wrapper/src/mocks/p-checkbox-wrapper-mock.jsx
+++ packages/components-react/projects/components-wrapper/src/mocks/p-checkbox-wrapper-mock.jsx
@@ -1,5 +1,11 @@
 const React = require('react');
 
 module.exports = {
-  PCheckboxWrapper: (props) => <p-checkbox-wrapper>{props.children}</p-checkbox-wrapper>
+  PCheckboxWrapper: (props) => (
+    <p-checkbox-wrapper>
+      {props.hideLabel !== true && props.label}
+      {props.children}
+      {props.state && props.state !== 'none' && props.message}
+    </p-checkbox-wrapper>
+  ),
 };
--- packages/components-react/tests/mocks/specs/PCheckboxWrapper.test.tsx
+++ packages/components-react/tests/mocks/specs/PCheckboxWrapper.test.tsx
@@ -4,16 +4,45 @@ import { PCheckboxWrapper } from '../../../projects/components-wrapper/src';
 
 describe('PCheckboxWrapper', () => {
   it('should render PCheckboxWrapper input name', () => {
-    const {container} = render(
+    const { container } = render(
       <PCheckboxWrapper>
-        <input type={"checkbox"}/>
+        <input type={'checkbox'} />
       </PCheckboxWrapper>
     );
     expect(container.getElementsByTagName('input')).toBeTruthy();
   });
 
-  it('should render TagName of component', ()=> {
-    const {container} = render(<PCheckboxWrapper/>);
+  it('should render TagName of component', () => {
+    const { container } = render(<PCheckboxWrapper />);
     expect(container.getElementsByTagName('p-checkbox-wrapper')).toBeTruthy();
+  });
+
+  describe('Label', () => {
+    it('should render label', () => {
+      const { getByText } = render(<PCheckboxWrapper label="Label" />);
+      expect(getByText('Label')).toBeDefined();
+    });
+
+    it('should not render label when hide label is set', () => {
+      const { queryByText } = render(<PCheckboxWrapper label="Label" hideLabel={true} />);
+      expect(queryByText('Label')).toBeNull();
+    });
+  });
+
+  describe('Error Message', () => {
+    it('should render message with error state', () => {
+      const { getByText } = render(<PCheckboxWrapper state="error" message="Message" />);
+      expect(getByText('Message')).toBeDefined();
+    });
+
+    it('should render message with success state', () => {
+      const { getByText } = render(<PCheckboxWrapper state="success" message="Message" />);
+      expect(getByText('Message')).toBeDefined();
+    });
+
+    it('should not render label when hide label is set', () => {
+      const { queryByText } = render(<PCheckboxWrapper state="none" message="Message" />);
+      expect(queryByText('Message')).toBeNull();
+    });
   });
 });
