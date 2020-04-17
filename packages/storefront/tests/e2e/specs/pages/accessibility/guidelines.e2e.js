f64658b0a8e9feb8ede6e9a9d82a4e3f724cec4a
Stephan Schröter <stephan.schroeter@mhp.com>
2020-04-17 09:56:26 +0200

    Fix e2e | sas | #524

--- packages/storefront/tests/e2e/specs/pages/accessibility/guidelines.e2e.js
+++ packages/storefront/tests/e2e/specs/pages/accessibility/guidelines.e2e.js
@@ -1,14 +1,14 @@
-describe('Accessibility Compliance', () => {
+describe('Accessibility Guidelines', () => {
   it('Should navigate through main navigation and show page title', () => {
     cy.visit('/');
 
-    cy.get('#app .sidebar nav p-link-pure[href*="accessibility/compliance"]')
-      .contains('Compliance')
+    cy.get('#app .sidebar nav p-link-pure[href*="accessibility/guidelines"]')
+      .contains('Guidelines')
       .click()
       .should('have.attr', 'active', 'true');
 
     cy.get('#app main h1')
       .should('be.visible')
-      .and('contain', 'Compliance');
+      .and('contain', 'Guidelines');
   });
 });
