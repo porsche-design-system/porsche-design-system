08b0a9537d150862ead940f33f6ea6823befada3
Matthias Haegele <matthias.haegele@mhp.com>
2020-04-29 10:22:31 +0200

    Test Jest Puppeteer | mh/bh | #637

---
+++ packages/components-js/tests/e2e/icon.puppeteer.e2e.ts
@@ -0,0 +1,36 @@
+/*
+import 'expect-puppeteer';
+
+*/
+
+describe('p-icon', () => {
+  it('should render', async () => {
+
+    const requestInterceptor = (request) => {
+      if (request.url().indexOf('.svg') >= 0) {
+        console.log('position1');
+        request.respond({
+          status: 200,
+          contentType: 'image/svg+xml',
+          body: '<svg height="100%" viewBox="0 0 48 48" width="100%" xmlns="http://www.w3.org/2000/svg"></svg>'
+        });
+      }
+      request.continue();
+    };
+
+    await page.setRequestInterception(true);
+    console.log('position2');
+    await page.on('request', requestInterceptor);
+    await page.setContent(`
+      <p-icon icon="question"></p-icon>
+    `);
+    console.log('position4');
+    /*    await page.waitForResponse(response => response.url().indexOf('.svg') >= 0 && response.status() === 200);*/
+    await page.waitFor(1000);
+
+    const el = document.body.querySelector('p-icon');
+    expect(el).not.toBe(null);
+  })
+
+});
+
