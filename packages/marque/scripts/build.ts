e7ed51b06a996d022a3d18649ec83a4996e6c47f
Stephan Schröter <stephan.schroeter@mhp.com>
2020-04-30 15:47:47 +0200

    Improve build task for marque | sas | #631

--- packages/marque/scripts/build.ts
+++ packages/marque/scripts/build.ts
@@ -2,23 +2,13 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as crypto from 'crypto';
 import globby from 'globby';
-import sharp from 'sharp';
 import { paramCase, camelCase } from 'change-case';
 
 interface Manifest {
   [name: string]: {
     [size: string]: {
-      '1x': string;
-      '2x': string;
-      '3x': string;
+      [resolution: string]: string;
     };
-  }
-}
-
-interface Config {
-  [size: string]: {
-    w: number;
-    h: number;
   };
 }
 
@@ -29,7 +19,19 @@ const toHash = (str: string): string => {
     .digest('hex');
 };
 
-const createManifestAndOptimizeMarque = async (cdn: string, files: string[], config: Config): Promise<void> => {
+const checkIntegrity = async (manifest: Manifest): Promise<void> => {
+  for (const [name, size] of Object.entries(manifest)) {
+    if (!size.small) throw new Error(`Marque size declaration "small" is missing in manifest for "${name}".`);
+    if (!size.medium) throw new Error(`Marque size declaration "medium" is missing in manifest for "${name}".`);
+    for (const resolution of Object.values(size)) {
+      if (!resolution['1x']) throw new Error(`Marque resolution declaration "1x" is missing in manifest for "${name}".`);
+      if (!resolution['2x']) throw new Error(`Marque resolution declaration "2x" is missing in manifest for "${name}".`);
+      if (!resolution['3x']) throw new Error(`Marque resolution declaration "3x" is missing in manifest for "${name}".`);
+    }
+  }
+};
+
+const createManifestAndCopyMarque = async (cdn: string, files: string[]): Promise<void> => {
   fs.rmdirSync(path.normalize('./dist'), {recursive: true});
   fs.mkdirSync(path.normalize('./dist/marque'), {recursive: true});
 
@@ -38,36 +40,32 @@ const createManifestAndOptimizeMarque = async (cdn: string, files: string[], con
   for (let file of files) {
     const ext = path.extname(file);
     const sourcePath = path.normalize(file);
-    const name = path.basename(sourcePath, ext);
-    const marque = fs.readFileSync(sourcePath, {encoding: null});
+    const info = path.basename(sourcePath, ext).split(/[.@]/g);
+    const name = info[0];
+    const size = info[1];
+    const resolution = info[2];
+    const marque = fs.readFileSync(sourcePath, {encoding: 'binary'});
+    const hash = toHash(marque);
+    const filename = `${paramCase(name)}.${paramCase(size)}.min.${hash}@${paramCase(resolution)}.png`;
+    const targetPath = path.normalize(`./dist/marque/${filename}`);
 
-    for (const [size, dimension] of Object.entries(config)) {
-      for (let i = 1; i <= 3; i++) {
-
-        const width = dimension.w * i;
-        const height = dimension.h * i;
-        const optimizedMarque = await sharp(marque).resize(width, height).png({ quality: 100 }).toBuffer();
-        const hash = toHash(optimizedMarque.toString());
-        const filename = `${paramCase(name)}.min.${hash}@${i}x.png`;
-        const targetPath = path.normalize(`./dist/marque/${filename}`);
-
-        const nameKey = camelCase(name);
-        const sizeKey = camelCase(size);
-        const resolutionKey = camelCase(`${i}x`);
-        manifest[nameKey] = {
-          ...manifest[nameKey],
-          [sizeKey]: {
-            ...manifest[nameKey]?.[sizeKey],
-            [resolutionKey]: filename
-          }
-        };
-
-        fs.writeFileSync(targetPath, optimizedMarque, {encoding: 'utf8'});
-
-        console.log(`Marque "${name}" optimized as "${size}" variant in ${i}x resolution.`);
+    const nameKey = camelCase(name);
+    const sizeKey = camelCase(size);
+    const resolutionKey = camelCase(resolution);
+    manifest[nameKey] = {
+      ...manifest[nameKey],
+      [sizeKey]: {
+        ...manifest[nameKey]?.[sizeKey],
+        [resolutionKey]: filename
       }
-    }
+    };
+
+    fs.writeFileSync(targetPath, marque, {encoding: 'binary'});
+
+    console.log(`Marque "${name}" copied as "${size}" variant in ${resolution} resolution.`);
   }
+
+  await checkIntegrity(manifest);
 
   fs.writeFileSync(path.normalize('./index.ts'),
     `export const cdn = "${cdn}";
@@ -79,13 +77,9 @@ export const marque = ${JSON.stringify(manifest)};`
 
 (async (): Promise<void> => {
   const cdn = 'https://cdn.ui.porsche.com/porsche-design-system/marque';
-  const files = await globby('./src/**/*.svg');
-  const config: Config = {
-    'small': {w: 100, h: 60},
-    'medium': {w: 120, h: 72}
-  };
+  const files = await globby('./src/**/*.png');
 
-  await createManifestAndOptimizeMarque(cdn, files, config).catch(e => {
+  await createManifestAndCopyMarque(cdn, files).catch(e => {
     console.error(e);
     process.exit(1);
   });
