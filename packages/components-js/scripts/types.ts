6cfde69d65eebde0c607a6a263ca356ba5b7f2c8
Matthias Haegele <matthias.haegele@mhp.com>
2020-05-04 15:07:34 +0200

    fix(components-js): change name svg | mh/bh | #637

--- packages/components-js/scripts/types.ts
+++ packages/components-js/scripts/types.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as icons from '@porsche-design-system/icons';
 
-const iconNames = Object.keys(icons.svg).sort().map(icon => `'${icon}'`);
+const iconNames = Object.keys(icons.SVG_MANIFEST).sort().map(icon => `'${icon}'`);
 const file = path.normalize('./src/types.d.ts');
 const types = fs.readFileSync(file, 'utf8').replace(/\s\/\* Auto Generated Below \*\/.*/s, '');
 
