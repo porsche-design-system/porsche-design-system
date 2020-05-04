e1cd357e469c134780885e0a2363b66f918efb0f
Matthias Haegele <matthias.haegele@mhp.com>
2020-05-04 11:00:24 +0200

    perf(components-js): remove duplicate caching and simplify getSvgContent() | mh/bh | #637

--- packages/components-js/src/components/icon/icon/icon-request.ts
+++ packages/components-js/src/components/icon/icon/icon-request.ts
@@ -7,48 +7,24 @@ import { cdn, svg } from '@porsche-design-system/icons';
 import { IconName } from '../../../types';
 import { isUrl } from './icon-helper';
 
-const iconCache = new Map<string, string>();
 const requestCache = new Map<string, Promise<string>>();
 
-export function getSvgContent (url: string): Promise<string> {
-  let req: Promise<string>;
-  if(iconCache.has(url)){
-    req = Promise.resolve(iconCache.get(url));
-  } else {
-    // see if we already have a request for this url
-    const pendingReq: Promise<string> = requestCache.get(url);
-
-    if (!pendingReq) {
-      // we don't already have a request
-      req = fetch(url).then(rsp => {
-        if (rsp.ok) {
-          return rsp.text().then(svgContent => {
-            const validSvgContent = validateContent(svgContent);
-            iconCache.set(url, validSvgContent);
-            return validSvgContent;
-          });
-        }
-        const emptyString = '';
-        iconCache.set(url, emptyString);
-        return emptyString;
-      });
-
-      // cache for the same requests
-      requestCache.set(url, req);
-    }else{
-     req = pendingReq;
-    }
+export const getSvgContent = (url: string): Promise<string> => {
+  let req = requestCache.get(url);
+  if (req === undefined) {
+    req = fetch(url).then(rsp => rsp.ok ? rsp.text().then(svgContent => validateContent(svgContent)) : '');
+    requestCache.set(url, req);
   }
   return req;
-}
+};
 
 export const buildIconUrl = (iconNameOrSource: IconName | string): string => {
-  if (isUrl(iconNameOrSource)){
+  if (isUrl(iconNameOrSource)) {
     return iconNameOrSource;
   } else if (svg[iconNameOrSource]) { // check if IconName exists
     return `${cdn}/${svg[iconNameOrSource]}`;
   }
-
+  // Only occurs if consumer is not using typescript -> necessary?
   console.warn('Please provide either an name property or a source property!');
   return '';
 };
