5a93e21508d87c89b964ec75b2d627c10aac7d9e
stephanschroeter <stephanschroeter@users.noreply.github.com>
2020-05-05 11:16:13 +0000

    Automated deployment of Porsche Design System (issue/653): 412d34f2a45c3779a2f1858e9b5419dc1624a029

---
+++ issue/653/js/chunk-2d21e8ff.428f71b7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e8ff"],{d5a9:function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"vmark"},[t("h1",[e._v("Content Wrapper")])])}],c=t("2877"),s={},l=Object(c["a"])(s,r,a,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d21e8ff.428f71b7.js.map
