a70d338d494fbf8b50fc11fd72d7c07a04c58106
Bernd Hacker <hacker.bernd@gmail.com>
2020-05-06 11:13:29 +0200

    test(components-js): rewrite blur-on-focus e2e tests | bh | #637

---
+++ packages/components-js/tests/e2e/specs/blur-on-focus.puppeteer-e2e.ts
@@ -0,0 +1,101 @@
+import {getActiveElementTagName, selectNode, setContentWithDesignSystem} from "../helpers";
+
+describe('blur on focus', () => {
+  it('should blur element after click', async () => {
+    await setContentWithDesignSystem(`
+        <p-button>Some label</p-button>
+        <button>Other label</button>
+        <span tabindex="0">One more label</span>
+    `);
+    const pButton = await selectNode('p-button');
+    const button = await selectNode('button');
+    const span = await selectNode('span');
+    await pButton.click();
+    expect(await getActiveElementTagName()).toBe('BODY');
+    await button.click();
+    expect(await getActiveElementTagName()).toBe('BODY');
+    await span.click();
+    expect(await getActiveElementTagName()).toBe('BODY');
+  });
+
+  it('should not blur input elements', async () => {
+    await setContentWithDesignSystem(`
+        <input type="text">Some label</input>
+        <select><option>Other label</option></select>
+        <textarea>One more label</span>
+    `);
+    const input = await selectNode('input');
+    const select = await selectNode('select');
+    const textarea = await selectNode('textarea');
+    await input.click();
+    expect(await getActiveElementTagName()).toBe('INPUT');
+    await select.click();
+    expect(await getActiveElementTagName()).toBe('SELECT');
+    await textarea.click();
+    expect(await getActiveElementTagName()).toBe('TEXTAREA');
+  });
+
+  it('should not blur on keyboard navigation', async () => {
+    await setContentWithDesignSystem(`
+        <p-button>Some label</p-button>
+        <button>Other label</button>
+        <span tabindex="0">One more label</span>
+        <input type="text">Some label</input>
+        <select><option>Other label</option></select>
+        <textarea>One more label</span>
+    `);
+    await page.keyboard.press('Tab');
+    expect(await getActiveElementTagName()).toBe('P-BUTTON');
+    await page.keyboard.press('Tab');
+    expect(await getActiveElementTagName()).toBe('BUTTON');
+    await page.keyboard.press('Tab');
+    expect(await getActiveElementTagName()).toBe('SPAN');
+    await page.keyboard.press('Tab');
+    expect(await getActiveElementTagName()).toBe('INPUT');
+    await page.keyboard.press('Tab');
+    expect(await getActiveElementTagName()).toBe('SELECT');
+    await page.keyboard.press('Tab');
+    expect(await getActiveElementTagName()).toBe('TEXTAREA');
+  });
+
+  it('should not blur on programmatic focus set', async () => {
+    await setContentWithDesignSystem(`
+        <p-button>Some label</p-button>
+        <button>Other label</button>
+    `);
+
+    const pButton = await selectNode('p-button');
+    const button = await selectNode('button');
+
+    await pButton.focus();
+    expect(await getActiveElementTagName()).toBe('P-BUTTON');
+    await button.focus();
+    expect(await getActiveElementTagName()).toBe('BUTTON');
+  });
+
+  it('should not blur if exclude class is set to element or parent', async () => {
+    await setContentWithDesignSystem(`
+        <p-button class="p-re-enable-focus-on-click">Some label</p-button>
+        <p-button>Other label</p-button>
+        <div class="p-re-enable-focus-on-click">
+          <button>One more label</button>
+        </div>
+        <div>
+          <button>And another label</button>
+        </div>
+    `);
+    const pButtonExcluded = await selectNode('p-button.p-re-enable-focus-on-click');
+    const pButton = await selectNode('p-button:not(.p-re-enable-focus-on-click)');
+    const buttonExcluded = await selectNode('.p-re-enable-focus-on-click button');
+    const button = await selectNode(':not(.p-re-enable-focus-on-click) > button');
+
+    await pButtonExcluded.click();
+    expect(await getActiveElementTagName()).toBe('P-BUTTON');
+    await pButton.click();
+    expect(await getActiveElementTagName()).toBe('BODY');
+    await buttonExcluded.click();
+    expect(await getActiveElementTagName()).toBe('BUTTON');
+    await button.click();
+    expect(await getActiveElementTagName()).toBe('BODY');
+  });
+});
