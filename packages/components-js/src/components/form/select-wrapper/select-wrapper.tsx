bc8e90fc7c10e665ed02e24a23a540224dbdd7ec
Oliver Hook <oliver.hook@mhp.com>
2020-05-07 10:57:02 +0200

    add additional keyboard navigations | oh | #617

--- packages/components-js/src/components/form/select-wrapper/select-wrapper.tsx
+++ packages/components-js/src/components/form/select-wrapper/select-wrapper.tsx
@@ -249,11 +249,22 @@ export class SelectWrapper {
         }
       }
       if(e.code === 'Enter') {
+        e.preventDefault();
         this.fakeOptionListHidden = true;
         this.setOptionSelected(this.optionHighlighted);
       }
       if(e.code === 'Escape') {
         this.fakeOptionListHidden = true;
+      }
+      if(e.code === 'PageUp') {
+        e.preventDefault();
+        this.optionHighlighted = 0;
+        this.handleScroll();
+      }
+      if(e.code === 'PageDown') {
+        e.preventDefault();
+        this.optionHighlighted = this.options.length-1;
+        this.handleScroll();
       }
     });
   }
@@ -332,6 +343,10 @@ export class SelectWrapper {
       }
     }
 
+    this.handleScroll();
+  };
+
+  private handleScroll(): void {
     if (this.fakeOptionListNode.scrollHeight > this.fakeOptionListNode.clientHeight) {
       this.fakeOptionHighlightedNode = this.fakeOptionListNode.querySelectorAll('div')[this.optionHighlighted];
       const scrollBottom = this.fakeOptionListNode.clientHeight + this.fakeOptionListNode.scrollTop;
@@ -343,7 +358,7 @@ export class SelectWrapper {
         this.fakeOptionListNode.scrollTop = this.fakeOptionHighlightedNode.offsetTop;
       }
     }
-  };
+  }
 
   private setState(): void {
     this.disabled = this.select.disabled;
