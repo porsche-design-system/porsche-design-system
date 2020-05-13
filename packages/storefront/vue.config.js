3200a247df17b901e9725882e0b27db0b08433e6
Oliver Hook <oliver.hook@mhp.com>
2020-05-13 09:11:38 +0200

    move downlodable sketch file to gh-pages | oh | #524

--- packages/storefront/vue.config.js
+++ packages/storefront/vue.config.js
@@ -13,9 +13,6 @@ module.exports = {
     config.module
       .rule('media')
       .test(/\.(vtt|mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)
-    config.module
-      .rule('files')
-      .test(/\.(sketch|zip)(\?.*)?$/)
   },
   configureWebpack: {
     module: {
