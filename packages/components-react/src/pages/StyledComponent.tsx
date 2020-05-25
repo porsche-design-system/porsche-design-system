e1c6b80e3df1e165a2966334bee2e84331a36766
Matthias Haegele <matthias.haegele@mhp.com>
2020-05-25 16:01:00 +0200

    feat(utilities): implement font as js variable | mh | #629

--- packages/components-react/src/pages/StyledComponent.tsx
+++ packages/components-react/src/pages/StyledComponent.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import { color } from '@porsche-design-system/utilities';
+import { font, text } from '@porsche-design-system/utilities';
 
 const Div = styled.div`
   background-color: ${color.darkTheme.background};
 `;
 
+const P = styled.p`
+  color: ${color.darkTheme.default};
+  font-size: ${text.size.xlarge};
+  font-weight: ${font.weight.bold};
+  font-family: ${font.family}
+`;
+
 export function StyledComponent() {
   return (
     <Div>
-      <p>Look at me iam styled</p>
+      <P>Look at me iam styled</P>
     </Div>
   )
 };
---
+++ packages/utilities/js/helper.ts
@@ -0,0 +1,6 @@
+export const remBase = 16;
+/*
+export const rem = (pixel: number) => {
+  return `${pixel / remBase}rem`
+};
+*/
