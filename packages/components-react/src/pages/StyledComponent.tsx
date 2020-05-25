e398ff0e727ad502b919d55501f149ec26b53b1d
Matthias Haegele <matthias.haegele@mhp.com>
2020-05-25 16:28:37 +0200

    feat(utilities): implement spacing/animation as js variable | mh | #629

--- packages/components-react/src/pages/StyledComponent.tsx
+++ packages/components-react/src/pages/StyledComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { color } from '@porsche-design-system/utilities';
+import { color, spacing } from '@porsche-design-system/utilities';
 import { font, text } from '@porsche-design-system/utilities';
 
 const Div = styled.div`
@@ -11,14 +11,20 @@ const P = styled.p`
   color: ${color.darkTheme.default};
   font-size: ${text.size.xlarge};
   font-weight: ${font.weight.bold};
-  font-family: ${font.family}
+  font-family: ${font.family};
+  margin-top: ${spacing.twentyFour};
+  margin-left: ${spacing.sixteen};
 `;
 
 export function StyledComponent() {
   return (
-    <Div>
-      <P>Look at me iam styled</P>
-    </Div>
+    <div>
+      <p>Iam unstyled </p>
+      <Div>
+        <P>Look at me iam styled</P>
+        <P>Second styled Paragraph with spacing</P>
+      </Div>
+    </div>
   )
 };
 
