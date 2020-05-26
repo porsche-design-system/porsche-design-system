85e67b704bfeb98655a0c4aa6893df1d8c19a8fb
Matthias Haegele <matthias.haegele@mhp.com>
2020-05-26 15:40:07 +0200

    feat(utilities): implement tsconfigs | mh / shs | #629

---
+++ packages/utilities/src/js/variables/font.ts
@@ -0,0 +1,39 @@
+import { rem } from './helper';
+
+export const font = {
+
+  family: `"Porsche Next", "Arial Narrow", Arial, sans-serif`,
+
+  weight: {
+    thin: 100,
+    regular: 400,
+    semibold: 600,
+    bold: 700,
+  },
+
+  size: {
+    '12': `${rem(12)}`,
+    '16': `${rem(16)}`,
+    '18': `${rem(18)}`,
+    '20': `${rem(20)}`,
+    '22': `${rem(22)}`,
+    '24': `${rem(24)}`,
+    '28': `${rem(28)}`,
+    '30': `${rem(30)}`,
+    '32': `${rem(32)}`,
+    '36': `${rem(36)}`,
+    '42': `${rem(42)}`,
+    '44': `${rem(44)}`,
+    '48': `${rem(48)}`,
+    '52': `${rem(52)}`,
+    '60': `${rem(60)}`,
+    '62': `${rem(62)}`,
+    '72': `${rem(72)}`,
+    '84': `${rem(84)}`,
+    xSmall: `${rem(12)}`,
+    small: `${rem(16)}`,
+    medium: `${rem(24)}`,
+    large: `${rem(36)}`,
+    xLarge: `${rem(52)}`
+  }
+};
--- packages/utilities/src/js/variables/helper.ts
+++ packages/utilities/src/js/variables/helper.ts
@@ -1,6 +1,14 @@
 export const remBase = 16;
-/*
 export const rem = (pixel: number) => {
   return `${pixel / remBase}rem`
 };
+
+/*
+export const typeScale (size: number) {
+
+}
+
+export const fontSize(fontSize: ) {
+
+}
 */
