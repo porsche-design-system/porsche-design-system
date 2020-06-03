cfae71e9c6ddb55b45d2375971b9481a651eba93
Matthias Haegele <matthias.haegele@mhp.com>
2020-06-03 15:34:45 +0200

    test(storefront): refactor deadLinkChecker | bh / mh | #629

--- packages/storefront/tests/e2e/specs/deadLinkCheck.e2e.ts
+++ packages/storefront/tests/e2e/specs/deadLinkCheck.e2e.ts
@@ -30,14 +30,26 @@ fdescribe('check for dead links in storefront', () => {
   const getHeadline = async () =>
     (await page.waitForSelector('.vmark > h1')) && page.$eval('.vmark > h1', (x) => x.innerHTML);
 
+  const getPatternHeadline = async () =>
+    (await page.waitForSelector('p-headline[tag="h1"]')) && page.$eval('p-headline[tag="h1"]', (x) => x.innerHTML);
+
   // exclude URLS which should not be checked -> include all links which lead to downloads because puppeteer cant handle that
-  const urlArray: string[] = [
+  const urlWhitelist: string[] = [
+    'https://github.com/porscheui/porsche-design-system',
     'http://designsystem.porsche.com/latest/porsche-design-system-layout-template.sketch',
     'https://cdn.ui.porsche.com/porsche-design-system/font/v1/Porsche_Next_WebOTF_Lat-Gr-Cyr.zip',
-    'https://designsystem.porsche.com/latest/porsche-design-system-colors.sketchpalette'
+    'https://designsystem.porsche.com/latest/porsche-design-system-colors.sketchpalette',
+    'https://www.sitepoint.com/introduction-wai-aria/',
+    'https://adabook.com/',
+    'https://www.etsi.org/deliver/etsi_en/301500_301599/301549/02.01.02_60/en_301549v020102p.pdf',
+    '/latest/porsche-design-system-form-templates.sketch',
+    'sketch://add-library?url=https%3A%2F%2Fdesignsystem.porsche.com%2Fporsche-design-system-basic.sketch.xml',
+    'sketch://add-library?url=https%3A%2F%2Fdesignsystem.porsche.com%2Fporsche-design-system-web.sketch.xml'
   ];
 
-  const checkForRef: string[] = [];
+  const urlArray: string[] = [...urlWhitelist];
+
+  const invalidUrls: string[] = [];
 
   fit('should check all a tags for correct response', async () => {
     await page.goto(`${options.baseURL}`, { waitUntil: 'networkidle0' });
@@ -51,34 +63,39 @@ fdescribe('check for dead links in storefront', () => {
           console.log('Href which is checked', href);
           // Go to internal Url
           if (href.includes(options.baseURL)) {
-            await page.goto(href, { waitUntil: 'networkidle0' });
+            await page.goto(href, { waitUntil: 'domcontentloaded' });
             urlArray.push(href); // Push url which is checked
 
-            const headline = href.endsWith('#/') ? 'first page' : await getHeadline();
+            const headline = href.endsWith('#/')
+              ? 'first page'
+              : href.includes('patterns/forms/')
+              ? await getPatternHeadline()
+              : await getHeadline();
 
             if (headline === '404 - Page not found') {
-              checkForRef.push(href);
+              invalidUrls.push(href);
             } else {
               await linkCheckLoop();
             }
             // Go to external Url
           } else if (href.startsWith('http')) {
-            const response = await page.goto(href, { timeout: 0 });
+            const response = await page.goto(href);
             urlArray.push(href); // Push url which is checked
 
             // Check response
             if (response?.status() === 404) {
-              checkForRef.push(href);
+              invalidUrls.push(href);
             } else {
               await page.goBack({ waitUntil: 'networkidle0' });
             }
           } else {
-            checkForRef.push(href);
+            urlArray.push(href); // Push url which is checked
+            invalidUrls.push(href);
           }
         }
       }
     };
     await linkCheckLoop();
-    console.log('Check Url if valid', checkForRef);
+    console.log('Check Url if valid', invalidUrls);
   });
 });
