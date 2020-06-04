f993ea65374396226a4b3a8d7376435096fb0842
Bernd Hacker <hacker.bernd@gmail.com>
2020-06-04 17:05:45 +0200

    test(utilities): add headlines via styled-components to vrt-app, rename headlines, wrap color boxes in div | bh | #629

--- packages/utilities/src/vrt-app/src/pages/js-variables.tsx
+++ packages/utilities/src/vrt-app/src/pages/js-variables.tsx
@@ -1,48 +1,50 @@
 import React from 'react';
 import { color, headline, layout, spacing, text } from '@porsche-design-system/utilities';
+import styled from 'styled-components';
 
 const objectToArray = (object: Object): string[] =>
   Object.values(object)
     .map((x) => (typeof x === 'object' ? Object.values(x) : x))
     .flat();
 
+const styledHeadlines = Object.values(headline).map((x) => styled.div(x));
+
 export const JsVariables = (): JSX.Element => {
   const { darkTheme, ...other } = color;
   const colorArray = objectToArray(other);
   const colorDarkArray = objectToArray(darkTheme);
 
-  const renderSquares = (array: string[]) =>
-    array.map((x, index) => (
-      <div
-        key={index}
-        style={{
-          background: x,
-          height: 50,
-          width: 50,
-          display: 'inline-block',
-          textAlign: 'center',
-          lineHeight: '50px',
-          fontSize: 10
-        }}
-      >
-        {x}
-      </div>
-    ));
-
-  const headlineStyles = Object.values(headline);
+  const renderSquares = (array: string[]) => (
+    <div>
+      {array.map((x, idx) => (
+        <div
+          key={idx}
+          style={{
+            background: x,
+            height: 50,
+            width: 50,
+            display: 'inline-block',
+            textAlign: 'center',
+            lineHeight: '50px',
+            fontSize: 10
+          }}
+          children={x}
+        />
+      ))}
+    </div>
+  );
 
   return (
     <>
-      <h2>Colors</h2>
+      <h2>Default Colors</h2>
       {renderSquares(colorArray)}
-      <h2>darkTheme Colors</h2>
+
+      <h2>Dark Theme Colors</h2>
       {renderSquares(colorDarkArray)}
+
       <h2>Headlines</h2>
-      {headlineStyles.map((x, index) => (
-        <div key={index} className={`h${index + 1}`}>
-          <style>{`.h${index + 1}{'${x}'}`}</style>
-          Headline{index + 1}{' '}
-        </div>
+      {styledHeadlines.map((Comp, idx) => (
+        <Comp key={idx} children={`Headline${idx + 1}`} />
       ))}
     </>
   );
