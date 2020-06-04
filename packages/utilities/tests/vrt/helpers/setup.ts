d23d17d789bfb947d1cbf3b815d60b38f6e42ca8
Matthias Haegele <matthias.haegele@mhp.com>
2020-06-04 13:44:23 +0200

    test(utilities): implement vrt test setup | mh/bh | #629

---
+++ packages/utilities/tests/vrt/helpers/setup.ts
@@ -0,0 +1,39 @@
+import 'jasmine';
+import { VisualRegressionTester, VisualRegressionTestOptions } from '@porsche-design-system/visual-regression-tester';
+import * as puppeteer from 'puppeteer';
+import { Browser } from 'puppeteer';
+
+let browser: Browser;
+let visualRegressionTester: VisualRegressionTester;
+
+const testOptions: VisualRegressionTestOptions = {
+  viewports: [320, 480, 760, 1000, 1300, 1760],
+  fixturesDir: 'tests/vrt/fixtures',
+  resultsDir: 'tests/vrt/results',
+  tolerance: 0,
+  baseUrl: 'http://localhost:61423',
+  timeout: 90000,
+  mode: 'square-auto'
+};
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
+
+beforeAll(async () => {
+  browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--single-process']
+  });
+});
+
+afterAll(async () => {
+  if (browser) {
+    await browser.close();
+  }
+});
+
+export const getVisualRegressionTester = (): VisualRegressionTester => {
+  if (!visualRegressionTester) {
+    visualRegressionTester = new VisualRegressionTester(browser, testOptions);
+  }
+
+  return visualRegressionTester;
+};
