3e4b63c372d9f476fd5558414e7897ad68c7ad2f
Bernd Hacker <hacker.bernd@gmail.com>
2020-06-04 18:02:05 +0200

    test(utilities): prepare playgrounds and headlines for scss-variables in vrt-app | bh | #629

--- packages/utilities/src/vrt-app/src/pages/scss-variables.tsx
+++ packages/utilities/src/vrt-app/src/pages/scss-variables.tsx
@@ -1,5 +1,47 @@
 import React from 'react';
 
 export const ScssVariables = (): JSX.Element => {
-  return <div>Scss page</div>;
+  return (
+    <>
+      <div className="playground">
+        <h2>Dark Theme Colors</h2>
+        TODO
+      </div>
+
+      <div className="playground">
+        <h2>Font Weights</h2>
+        TODO
+      </div>
+
+      <div className="playground">
+        <h2>Font Sizes</h2>
+        TODO
+      </div>
+
+      <div className="playground">
+        <h2>Titles</h2>
+        TODO
+      </div>
+
+      <div className="playground">
+        <h2>Headlines</h2>
+        TODO
+      </div>
+
+      <div className="playground">
+        <h2>Text</h2>
+        TODO
+      </div>
+
+      <div className="playground">
+        <h2>Spacing</h2>
+        TODO
+      </div>
+
+      <div className="playground">
+        <h2>Layout</h2>
+        TODO
+      </div>
+    </>
+  );
 };
