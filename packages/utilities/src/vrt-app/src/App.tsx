c2111e592edc020b7832d2723cfef26b9eac666f
Matthias Haegele <matthias.haegele@mhp.com>
2020-06-04 16:36:20 +0200

    wip(utilities): implement create react app for vrt tests | mh/bh | #629

---
+++ packages/utilities/src/vrt-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { JsVariables } from './pages/js-variables';
+
+export const App = (): JSX.Element => {
+  return (
+    <>
+      <JsVariables />
+    </>
+  );
+};
