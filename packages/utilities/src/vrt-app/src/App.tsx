e33cde48227fa14c4be1cdcb8c7cad57e7a35c90
Bernd Hacker <hacker.bernd@gmail.com>
2020-06-04 16:58:41 +0200

    test(utilities): add react-router-dom to vrt-app, improve setup of vrt-app | bh | #629

--- packages/utilities/src/vrt-app/src/App.tsx
+++ packages/utilities/src/vrt-app/src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { JsVariables } from './pages/js-variables';
+import { BrowserRouter, Switch, RouteProps, Route, Redirect } from 'react-router-dom';
+import { JsVariables, ScssVariables } from './pages';
 
-export const App = (): JSX.Element => {
-  return (
-    <>
-      <JsVariables />
-    </>
-  );
-};
+const routes: RouteProps[] = [
+  {
+    path: '/js-variables',
+    component: JsVariables
+  },
+  {
+    path: '/scss-variables',
+    component: ScssVariables
+  }
+];
+
+export const App = (): JSX.Element => (
+  <BrowserRouter>
+    <Switch>
+      {routes.map((route, idx) => (
+        <Route key={idx} {...route} exact />
+      ))}
+      <Redirect path="*" to={routes[0].path as string} />
+    </Switch>
+  </BrowserRouter>
+);
