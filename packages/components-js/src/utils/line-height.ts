e944148bc48deaa10489704a6a7b6400828e78dc
Matthias Haegele <matthias.haegele@mhp.com>
2020-06-09 10:17:55 +0200

    refactore(components-js): import line-height from utilities  | mh | #629

--- packages/components-js/src/utils/line-height.ts
+++ packages/components-js/src/utils/line-height.ts
@@ -1,26 +1,5 @@
-/**
- * Scaling method of the line-height regarding its given font-size.
- * This scaling formula also exists in scss utils function p-line-height.
- * @param fontSize
- * @returns number
- */
-export const lineHeightFactor = (fontSize: number): number => {
-  const e = 2.71828;
-  const exactLineHeightFactor = 0.911 / ( 2.97 + 0.005 * Math.pow( e, 0.2 * fontSize ) ) + 1.2;
-  const exactLineHeightPx = fontSize * exactLineHeightFactor;
-  let remainingPx = exactLineHeightPx % 4;
-
-  if (remainingPx > 2) {
-    remainingPx = remainingPx - 4;
-  }
-
-  const fittedLineHeightPx = exactLineHeightPx - remainingPx;
-  const fittedLineHeightFactor = fittedLineHeightPx / fontSize;
-
-  return fittedLineHeightFactor;
-};
+import { convertLineHeight } from '@porsche-design-system/utilities/src/js/helper';
 
 export const calcLineHeightForElement = (tag: HTMLElement): number => {
-  const fontSize = parseInt(window.getComputedStyle(tag).fontSize, 10);
-  return lineHeightFactor(fontSize);
+  return convertLineHeight(window.getComputedStyle(tag).fontSize);
 };
