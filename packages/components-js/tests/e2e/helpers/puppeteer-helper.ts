84843231ba7beeee7bd0a51df0f2d148317ca7dc
Matthias Haegele <matthias.haegele@mhp.com>
2020-06-15 12:19:15 +0200

    test(components-js): wait for components to be lazy loaded | mh | #629

--- packages/components-js/tests/e2e/helpers/puppeteer-helper.ts
+++ packages/components-js/tests/e2e/helpers/puppeteer-helper.ts
@@ -1,6 +1,6 @@
 import { ElementHandle, JSHandle, NavigationOptions, Page } from 'puppeteer';
 
-export const setContentWithDesignSystem = async (page: Page, content: string, options: NavigationOptions = {waitUntil: 'networkidle0'}) =>
+export const setContentWithDesignSystem = async (page: Page, content: string, options: NavigationOptions = {waitUntil: 'networkidle0'}) => {
   await page.setContent(`
       <script nomodule src="http://localhost:3333/build/porsche-design-system.js"></script>
       <script type="module" src="http://localhost:3333/build/porsche-design-system.esm.js"></script>
@@ -8,6 +8,8 @@ export const setContentWithDesignSystem = async (page: Page, content: string, op
     `,
     options
   );
+  await page.waitForSelector('html.hydrated')
+};
 
 type GetBoxShadowOptions = { waitForTransition: boolean };
 
