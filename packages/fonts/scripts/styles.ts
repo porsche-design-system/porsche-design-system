882bf8c7166180ab75031055c945bb72a478d31f
Christoph Stickel <christoph@neochic.de>
2020-06-24 16:07:54 +0200

    minor improvement for staging font face style generation | cs | #724

--- packages/fonts/scripts/styles.ts
+++ packages/fonts/scripts/styles.ts
@@ -1,45 +1,49 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { CDN_BASE_URL, FONTS_MANIFEST } from '@porsche-design-system/fonts';
 import crypto from 'crypto';
+import { Manifest } from './build';
 
-type Options = { isStaging: boolean };
+type Options = {
+  baseUrl: string,
+  fontsManifest: Manifest,
+  addContentBasedHash: boolean
+};
 
-const prepareFontFaceVariables = (opts?: Options): void => {
+const prepareFontFaceVariables = (opts: Options): void => {
   const file = path.normalize('./src/styles/font-face.variables.scss');
 
-  const baseUrl = opts?.isStaging ? 'http://localhost:3001' : CDN_BASE_URL;
+  const { baseUrl, fontsManifest } = opts;
 
   fs.writeFileSync(
     file,
     `/* Auto Generated Below */
 
-$p-font-porsche-next-w-la-thin-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWLaThin.woff}';
-$p-font-porsche-next-w-la-thin-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWLaThin.woff2}';
-$p-font-porsche-next-w-la-regular-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWLaRegular.woff}';
-$p-font-porsche-next-w-la-regular-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWLaRegular.woff2}';
-$p-font-porsche-next-w-la-semi-bold-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWLaSemiBold.woff}';
-$p-font-porsche-next-w-la-semi-bold-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWLaSemiBold.woff2}';
-$p-font-porsche-next-w-la-bold-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWLaBold.woff}';
-$p-font-porsche-next-w-la-bold-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWLaBold.woff2}';
+$p-font-porsche-next-w-la-thin-woff: '${baseUrl}/${fontsManifest.porscheNextWLaThin.woff}';
+$p-font-porsche-next-w-la-thin-woff2: '${baseUrl}/${fontsManifest.porscheNextWLaThin.woff2}';
+$p-font-porsche-next-w-la-regular-woff: '${baseUrl}/${fontsManifest.porscheNextWLaRegular.woff}';
+$p-font-porsche-next-w-la-regular-woff2: '${baseUrl}/${fontsManifest.porscheNextWLaRegular.woff2}';
+$p-font-porsche-next-w-la-semi-bold-woff: '${baseUrl}/${fontsManifest.porscheNextWLaSemiBold.woff}';
+$p-font-porsche-next-w-la-semi-bold-woff2: '${baseUrl}/${fontsManifest.porscheNextWLaSemiBold.woff2}';
+$p-font-porsche-next-w-la-bold-woff: '${baseUrl}/${fontsManifest.porscheNextWLaBold.woff}';
+$p-font-porsche-next-w-la-bold-woff2: '${baseUrl}/${fontsManifest.porscheNextWLaBold.woff2}';
 
-$p-font-porsche-next-w-gr-thin-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWGrThin.woff}';
-$p-font-porsche-next-w-gr-thin-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWGrThin.woff2}';
-$p-font-porsche-next-w-gr-regular-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWGrRegular.woff}';
-$p-font-porsche-next-w-gr-regular-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWGrRegular.woff2}';
-$p-font-porsche-next-w-gr-semi-bold-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWGrSemiBold.woff}';
-$p-font-porsche-next-w-gr-semi-bold-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWGrSemiBold.woff2}';
-$p-font-porsche-next-w-gr-bold-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWGrBold.woff}';
-$p-font-porsche-next-w-gr-bold-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWGrBold.woff2}';
+$p-font-porsche-next-w-gr-thin-woff: '${baseUrl}/${fontsManifest.porscheNextWGrThin.woff}';
+$p-font-porsche-next-w-gr-thin-woff2: '${baseUrl}/${fontsManifest.porscheNextWGrThin.woff2}';
+$p-font-porsche-next-w-gr-regular-woff: '${baseUrl}/${fontsManifest.porscheNextWGrRegular.woff}';
+$p-font-porsche-next-w-gr-regular-woff2: '${baseUrl}/${fontsManifest.porscheNextWGrRegular.woff2}';
+$p-font-porsche-next-w-gr-semi-bold-woff: '${baseUrl}/${fontsManifest.porscheNextWGrSemiBold.woff}';
+$p-font-porsche-next-w-gr-semi-bold-woff2: '${baseUrl}/${fontsManifest.porscheNextWGrSemiBold.woff2}';
+$p-font-porsche-next-w-gr-bold-woff: '${baseUrl}/${fontsManifest.porscheNextWGrBold.woff}';
+$p-font-porsche-next-w-gr-bold-woff2: '${baseUrl}/${fontsManifest.porscheNextWGrBold.woff2}';
 
-$p-font-porsche-next-w-cy-thin-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWCyThin.woff}';
-$p-font-porsche-next-w-cy-thin-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWCyThin.woff2}';
-$p-font-porsche-next-w-cy-regular-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWCyRegular.woff}';
-$p-font-porsche-next-w-cy-regular-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWCyRegular.woff2}';
-$p-font-porsche-next-w-cy-semi-bold-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWCySemiBold.woff}';
-$p-font-porsche-next-w-cy-semi-bold-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWCySemiBold.woff2}';
-$p-font-porsche-next-w-cy-bold-woff: '${baseUrl}/${FONTS_MANIFEST.porscheNextWCyBold.woff}';
-$p-font-porsche-next-w-cy-bold-woff2: '${baseUrl}/${FONTS_MANIFEST.porscheNextWCyBold.woff2}';`
+$p-font-porsche-next-w-cy-thin-woff: '${baseUrl}/${fontsManifest.porscheNextWCyThin.woff}';
+$p-font-porsche-next-w-cy-thin-woff2: '${baseUrl}/${fontsManifest.porscheNextWCyThin.woff2}';
+$p-font-porsche-next-w-cy-regular-woff: '${baseUrl}/${fontsManifest.porscheNextWCyRegular.woff}';
+$p-font-porsche-next-w-cy-regular-woff2: '${baseUrl}/${fontsManifest.porscheNextWCyRegular.woff2}';
+$p-font-porsche-next-w-cy-semi-bold-woff: '${baseUrl}/${fontsManifest.porscheNextWCySemiBold.woff}';
+$p-font-porsche-next-w-cy-semi-bold-woff2: '${baseUrl}/${fontsManifest.porscheNextWCySemiBold.woff2}';
+$p-font-porsche-next-w-cy-bold-woff: '${baseUrl}/${fontsManifest.porscheNextWCyBold.woff}';
+$p-font-porsche-next-w-cy-bold-woff2: '${baseUrl}/${fontsManifest.porscheNextWCyBold.woff2}';`
   );
 };
 
@@ -50,7 +54,7 @@ const toHash = (str: string): string => {
     .digest('hex');
 };
 
-const compileFontFaceScss = (opts?: Options): string => {
+const compileFontFaceScss = (opts: Options): string => {
   const sass = require('node-sass');
 
   const scssPath = path.resolve(__dirname, '../src/styles/font-face.scss');
@@ -59,7 +63,7 @@ const compileFontFaceScss = (opts?: Options): string => {
     outputStyle: 'compressed'
   });
 
-  const hash = opts?.isStaging ? '' : `.${toHash(result.css)}`;
+  const hash = opts.addContentBasedHash ? `.${toHash(result.css)}` : '';
   const filename = `font-face.min${hash}.css`;
   const targetPath = path.normalize(`./dist/fonts/${filename}`);
 
@@ -68,7 +72,7 @@ const compileFontFaceScss = (opts?: Options): string => {
   return filename;
 };
 
-export function buildStyles(opts?: Options): string {
+export function buildStyles(opts: Options): string {
   prepareFontFaceVariables(opts);
   return compileFontFaceScss(opts);
 }
