9fcd08c6d6c37d6e6b79c6241db9c403f756f7b4
Christoph Stickel <christoph@neochic.de>
2020-06-25 16:38:48 +0200

    align components-pwcm staging with the cdn-local staging concept | bh / cs | #679

--- packages/components-pwcm/environment.js
+++ packages/components-pwcm/environment.js
@@ -1,18 +1,15 @@
 const path = require('path');
 const packageJson = require('./src/package.json');
 
-const isDev = !!process.env.PORSCHE_DESIGN_SYSTEM_DEV;
+const isDev = process.env.PORSCHE_DESIGN_SYSTEM_DEV === '1';
 
 module.exports = {
   deployUrl: isDev
-    ? 'http://localhost:8576/dist/dev/cdn'
+    ? 'http://localhost:8576/data'
     : 'https://cdn.ui.porsche.com/porsche-design-system/pwcm',
-  cdnDistPath: isDev
-    ? path.resolve('./dist/dev/cdn')
-    : path.resolve('./dist/p/cdn'),
-  npmDistPath: isDev
-    ? path.resolve('./dist/dev/npm')
-    : path.resolve('./dist/p/npm'),
+  cdnDistPath: path.resolve('./dist/cdn/data'),
+  cdnBasePath: path.resolve('./dist/cdn'),
+  npmDistPath: path.resolve('./dist/npm'),
   version: packageJson.version,
   snakeCaseVersion: packageJson.version.replace(/\./g, '_')
 };
