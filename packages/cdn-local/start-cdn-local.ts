e6fe6c72fd2d8dc220724dc8ea4496b441c73e09
Christoph Stickel <christoph@neochic.de>
2020-06-25 14:18:42 +0200

    improve local staging cdn to not fail if port is in use, this will prevent failing tests, if they run in parallel and multiple of them try to start local cdn | cs | #724

--- packages/cdn-local/start-cdn-local.ts
+++ packages/cdn-local/start-cdn-local.ts
@@ -9,6 +9,17 @@ const server = createServer({
   root: path.resolve(__dirname, 'cdn'),
   cors: true
 });
+
+(server as any).server.on('error', (error: { code: string }) => {
+  if (error.code === 'EADDRINUSE') {
+    console.log(`cdn-local port is already in use. maybe it's already started. doing nothing...`);
+    setInterval(() => {}, 1 << 30); // https://stackoverflow.com/a/47456805
+  }
+});
+
+(server as any).server.on('listen', () => {
+  console.log(`started cdn-local on http://localhost:${PORT}`);
+});
+
 server.listen(PORT, 'localhost');
 
-console.log(`started cdn-local on http://localhost:${PORT}`);
