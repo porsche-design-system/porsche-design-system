b43563d08e8fe8658cb3b5f2ff964e48ed2885b9
Christoph Stickel <christoph@neochic.de>
2020-06-26 09:33:02 +0200

    fix linting | cs | #679

--- packages/components-js/src/components/icon/icon/icon-helper.ts
+++ packages/components-js/src/components/icon/icon/icon-helper.ts
@@ -1,3 +1,4 @@
 export const isUrl = (str: string): boolean => str?.length > 0 && /(\/)/.test(str);
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const isStr = (val: any): val is string => typeof val === 'string';
--- packages/components-js/src/utils/get-prefixed-tag-name.ts
+++ packages/components-js/src/utils/get-prefixed-tag-name.ts
@@ -1,19 +1,14 @@
-function kebabToCamelCase(kebabCaseString: string) {
-  /**
-   * from https://stackoverflow.com/a/6661012
-   */
-  return kebabCaseString.replace(/-([a-z])/g, function (match) {
-    return match[1].toUpperCase();
-  });
-}
+/**
+ * from https://stackoverflow.com/a/6661012
+ */
+const kebabToCamelCase = (kebabCaseString: string): string =>
+  kebabCaseString.replace(/-([a-z])/g, (match) => match[1].toUpperCase());
 
-export function getPrefixedTagNames(element: HTMLElement, tagNames: string[]): { [tagName: string]: string } {
+export const getPrefixedTagNames = (element: HTMLElement, tagNames: string[]): { [tagName: string]: string } => {
   const lowerCaseTagName = element.tagName.toLowerCase();
-  const [,prefix=''] = lowerCaseTagName.match(/^(.*-)p-(.*)$/) || [];
-  return tagNames.reduce((tagNameMap,tagName) => {
-    return {
-      ...tagNameMap,
-      [kebabToCamelCase(tagName)]: `${prefix}${tagName}`
-    };
-  }, {});
-}
+  const [,prefix=''] = new RegExp(/^(.*-)p-(.*)$/).exec(lowerCaseTagName) || [];
+  return tagNames.reduce((tagNameMap, tagName) => ({
+    ...tagNameMap,
+    [kebabToCamelCase(tagName)]: `${prefix}${tagName}`
+  }), {});
+};
