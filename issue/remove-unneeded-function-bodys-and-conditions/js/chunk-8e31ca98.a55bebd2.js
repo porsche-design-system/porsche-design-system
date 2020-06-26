4f3d3e353f7721e04cae854aeabf74673439b2bd
denyo <denyo@users.noreply.github.com>
2020-06-26 09:26:33 +0000

    Automated deployment of Porsche Design System (issue/remove-unneeded-function-bodys-and-conditions): aa48ce06ff19214c1a14d5917ca001e7a62eaf38

---
+++ issue/remove-unneeded-function-bodys-and-conditions/js/chunk-8e31ca98.a55bebd2.js
@@ -0,0 +1,8 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-8e31ca98"],{c26b:function(t,n,e){"use strict";e.r(n),e.d(n,"p_content_wrapper",(function(){return p}));var r=e("55eb"),i=e("f149"),a=":host{display:block}.p-content-wrapper{margin:0 auto;padding:0;display:block}.p-content-wrapper--basic{max-width:96rem;padding-left:7vw;padding-right:7vw}@media (min-width: 1760px) and (max-width: 1919px){.p-content-wrapper--basic{padding-left:10vw;padding-right:10vw}}@media (min-width: 1920px){.p-content-wrapper--basic{padding-left:12rem;padding-right:12rem}}.p-content-wrapper--extended{max-width:120rem}",p=function(){function t(t){Object(r["q"])(this,t),this.width="basic"}return t.prototype.render=function(){var t=Object(i["a"])(Object(r["e"])("content-wrapper"),Object(r["e"])("content-wrapper--"+this.width));return Object(r["i"])("div",{class:t},Object(r["i"])("slot",null))},t}();p.style=a},f149:function(t,n,e){"use strict";e.d(n,"a",(function(){return a}));e("ef14");var r=e("53ca"),i=e("55eb"),a=Object(i["d"])((function(t){
+/*!
+    Copyright (c) 2017 Jed Watson.
+    Licensed under the MIT License (MIT), see
+    http://jedwatson.github.io/classnames
+  */
+(function(){var n={}.hasOwnProperty;function e(){for(var t=[],i=0;i<arguments.length;i++){var a=arguments[i];if(a){var p=Object(r["a"])(a);if("string"===p||"number"===p)t.push(a);else if(Array.isArray(a)&&a.length){var c=e.apply(null,a);c&&t.push(c)}else if("object"===p)for(var d in a)n.call(a,d)&&a[d]&&t.push(d)}}return t.join(" ")}t.exports?(e.default=e,t.exports=e):window.classNames=e})()}))}}]);
+//# sourceMappingURL=chunk-8e31ca98.a55bebd2.js.map
