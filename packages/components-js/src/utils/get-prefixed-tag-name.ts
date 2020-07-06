ed71b98d933c0db36486bbd026f08d0a2ced6f29
Bernd Hacker <hacker.bernd@gmail.com>
2020-07-06 15:02:10 +0200

    refactor(components-pwcm): reuse camelCase from change-case package | bh | #679

--- packages/components-js/src/utils/get-prefixed-tag-name.ts
+++ packages/components-js/src/utils/get-prefixed-tag-name.ts
@@ -1,14 +1,13 @@
-/**
- * from https://stackoverflow.com/a/6661012
- */
-const kebabToCamelCase = (kebabCaseString: string): string =>
-  kebabCaseString.replace(/-([a-z])/g, (match) => match[1].toUpperCase());
+import { camelCase } from 'change-case';
 
 export const getPrefixedTagNames = (element: HTMLElement, tagNames: string[]): { [tagName: string]: string } => {
   const lowerCaseTagName = element.tagName.toLowerCase();
-  const [,prefix=''] = new RegExp(/^(.*-)p-(.*)$/).exec(lowerCaseTagName) || [];
-  return tagNames.reduce((tagNameMap, tagName) => ({
-    ...tagNameMap,
-    [kebabToCamelCase(tagName)]: `${prefix}${tagName}`
-  }), {});
+  const [, prefix = ''] = new RegExp(/^(.*-)p-(.*)$/).exec(lowerCaseTagName) || [];
+  return tagNames.reduce(
+    (tagNameMap, tagName) => ({
+      ...tagNameMap,
+      [camelCase(tagName)]: prefix + tagName
+    }),
+    {}
+  );
 };
