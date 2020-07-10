5819d73f8fd9ddae92654b61060848ecef1e1ec8
Bernd Hacker <hacker.bernd@gmail.com>
2020-07-10 08:49:12 +0200

    test(componenst-react): clean up setup, fix typo in readme | bh

--- packages/components-react/tests/mocks/config/jest.config.js
+++ packages/components-react/tests/mocks/config/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   rootDir: '../../../',
   verbose: true,
-  setupFilesAfterEnv: ['<rootDir>/tests/mocks/config/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/tests/mocks/config/setup.ts'],
   testMatch: ['**/tests/mocks/specs/**/*.test.tsx'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
