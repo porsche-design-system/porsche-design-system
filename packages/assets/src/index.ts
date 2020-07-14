91abad93c6f62711a2942d0a6c6165bc3e46d135
Bernd Hacker <hacker.bernd@gmail.com>
2020-07-14 14:31:57 +0200

    build(assets): add workaround for resolving packages in type definitions | bh | #757

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,4 +1,44 @@
-export { CDN_BASE_URL as FONTS_CDN_BASE_URL, FONTS_MANIFEST } from '@porsche-design-system/fonts';
-export { CDN_BASE_URL as ICONS_CDN_BASE_URL, ICONS_MANIFEST } from '@porsche-design-system/icons';
-export { CDN_BASE_URL as MARQUE_CDN_BASE_URL, MARQUES_MANIFEST } from '@porsche-design-system/marque';
-export * from '@porsche-design-system/utilities';
+import { CDN_BASE_URL as FONTS_CDN_BASE_URL, FONTS_MANIFEST } from '@porsche-design-system/fonts';
+import { CDN_BASE_URL as ICONS_CDN_BASE_URL, ICONS_MANIFEST } from '@porsche-design-system/icons';
+import { CDN_BASE_URL as MARQUE_CDN_BASE_URL, MARQUES_MANIFEST } from '@porsche-design-system/marque';
+import {
+  FONT_FACE_CSS_NAME,
+  breakpoint,
+  spacing,
+  layout,
+  text,
+  color,
+  convertLineHeight,
+  font,
+  headline,
+  mediaQuery,
+  pxToRem,
+  remBase,
+  remToPx,
+  title,
+  typeScale
+} from '@porsche-design-system/utilities';
+
+export default {
+  FONTS_CDN_BASE_URL,
+  FONTS_MANIFEST,
+  ICONS_CDN_BASE_URL,
+  ICONS_MANIFEST,
+  MARQUE_CDN_BASE_URL,
+  MARQUES_MANIFEST,
+  FONT_FACE_CSS_NAME,
+  breakpoint,
+  spacing,
+  layout,
+  text,
+  color,
+  convertLineHeight,
+  font,
+  headline,
+  mediaQuery,
+  pxToRem,
+  remBase,
+  remToPx,
+  title,
+  typeScale
+};
