61b004b9c69245d0b36ceb33af45f4dec6d54f6c
Matthias Haegele <matthias.haegele@mhp.com>
2020-07-14 13:09:12 +0200

    feature(utilities): implement pdsFont | mh | #735

--- packages/utilities/projects/utilities/src/js/functions/typography.ts
+++ packages/utilities/projects/utilities/src/js/functions/typography.ts
@@ -2,6 +2,14 @@ import { font, FontWeight } from '../variables';
 import { mediaQuery } from './media-query';
 import { typeScale } from '../helper';
 
+export const pdsFont = {
+  xSmall: { fontFamily: font.family, fontWeight: font.weight.regular, ...font.size.xSmall },
+  small: { fontFamily: font.family, fontWeight: font.weight.regular, ...font.size.small },
+  medium: { fontFamily: font.family, fontWeight: font.weight.regular, ...font.size.medium },
+  large: { fontFamily: font.family, fontWeight: font.weight.regular, ...font.size.large },
+  xLarge: { fontFamily: font.family, fontWeight: font.weight.regular, ...font.size.xLarge }
+};
+
 export const title = {
   large: {
     ...font.size['32'],
