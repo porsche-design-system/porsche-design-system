b0a29f19ef6a624504bf3ef8f35e8c460bb7725f
Bernd Hacker <hacker.bernd@gmail.com>
2020-07-15 10:01:21 +0200

    fix(components-js): path to global style on cdn-local | bh | #757

--- packages/components-js/src/utils/inject-global-style.ts
+++ packages/components-js/src/utils/inject-global-style.ts
@@ -9,7 +9,7 @@ export const injectGlobalStyle = (): void => {
   link.href =
     ROLLUP_REPLACE_IS_STAGING === 'production'
       ? `${FONTS_CDN_BASE_URL}/${FONT_FACE_CSS_NAME}`
-      : 'http://localhost:3001/style/font-face.min.css';
+      : 'http://localhost:3001/style/style.min.css';
   link.type = 'text/css';
   link.rel = 'stylesheet';
   document.getElementsByTagName('head')[0].appendChild(link);
