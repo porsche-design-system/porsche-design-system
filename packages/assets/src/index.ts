f286d7d63d22c2b302cd096b322cd4fba9bc49c1
Bernd Hacker <hacker.bernd@gmail.com>
2020-07-16 16:25:53 +0200

    refactor(utilities): rename style.min.css to font-face.min.css, remove FONT_FACE_STYLE_CDN_URL from assets package | bh | #757

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -4,7 +4,6 @@ import {
   CDN_BASE_URL as P_MARQUE_CDN_BASE_URL,
   MARQUES_MANIFEST as P_MARQUES_MANIFEST
 } from '@porsche-design-system/marque';
-import { FONT_FACE_STYLE_CDN_URL as P_FONT_FACE_STYLE_CDN_URL } from '@porsche-design-system/utilities';
 
 // reexports are needed in order to build standalone typings
 export const FONTS_CDN_BASE_URL = P_FONTS_CDN_BASE_URL;
@@ -13,4 +12,3 @@ export const ICONS_CDN_BASE_URL = P_ICONS_CDN_BASE_URL;
 export const ICONS_MANIFEST = P_ICONS_MANIFEST;
 export const MARQUE_CDN_BASE_URL = P_MARQUE_CDN_BASE_URL;
 export const MARQUES_MANIFEST = P_MARQUES_MANIFEST;
-export const FONT_FACE_STYLE_CDN_URL = P_FONT_FACE_STYLE_CDN_URL;
--- packages/utilities/projects/utilities/src/js/index.ts
+++ packages/utilities/projects/utilities/src/js/index.ts
@@ -4,4 +4,4 @@ export * from './variables';
 
 /* Auto Generated Below */
 
-export const FONT_FACE_STYLE_CDN_URL = "https://cdn.ui.porsche.com/porsche-design-system/style/style.min.677d41d9905a04aadcb253f71e5f71e9.css";
+export const FONT_FACE_STYLE_CDN_URL = "https://cdn.ui.porsche.com/porsche-design-system/style/font-face.min.677d41d9905a04aadcb253f71e5f71e9.css";
