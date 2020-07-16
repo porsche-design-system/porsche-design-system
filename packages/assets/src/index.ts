dba58fb9256c7d69086cefb4a2ccadde68d60b30
Bernd Hacker <hacker.bernd@gmail.com>
2020-07-16 14:09:25 +0200

    feat(assets): reexport assets via alias | bh | #757

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,4 +1,16 @@
-export { CDN_BASE_URL as FONTS_CDN_BASE_URL, FONTS_MANIFEST } from '@porsche-design-system/fonts';
-export { CDN_BASE_URL as ICONS_CDN_BASE_URL, ICONS_MANIFEST } from '@porsche-design-system/icons';
-export { CDN_BASE_URL as MARQUE_CDN_BASE_URL, MARQUES_MANIFEST } from '@porsche-design-system/marque';
-export { STYLE_CDN_URL } from '@porsche-design-system/utilities';
+import { CDN_BASE_URL as P_FONTS_CDN_BASE_URL, FONTS_MANIFEST as P_FONTS_MANIFEST } from '@porsche-design-system/fonts';
+import { CDN_BASE_URL as P_ICONS_CDN_BASE_URL, ICONS_MANIFEST as P_ICONS_MANIFEST } from '@porsche-design-system/icons';
+import {
+  CDN_BASE_URL as P_MARQUE_CDN_BASE_URL,
+  MARQUES_MANIFEST as P_MARQUES_MANIFEST
+} from '@porsche-design-system/marque';
+import { STYLE_CDN_URL as P_STYLE_CDN_URL } from '@porsche-design-system/utilities';
+
+// reexports are needed in order to build standalone typings
+export const FONTS_CDN_BASE_URL = P_FONTS_CDN_BASE_URL;
+export const FONTS_MANIFEST = P_FONTS_MANIFEST;
+export const ICONS_CDN_BASE_URL = P_ICONS_CDN_BASE_URL;
+export const ICONS_MANIFEST = P_ICONS_MANIFEST;
+export const MARQUE_CDN_BASE_URL = P_MARQUE_CDN_BASE_URL;
+export const MARQUES_MANIFEST = P_MARQUES_MANIFEST;
+export const STYLE_CDN_URL = P_STYLE_CDN_URL;
