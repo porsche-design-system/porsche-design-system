c5f10bcf5915f1282ceef7780565f23eaf4a2ff1
Oliver Hook <oliver.hook@mhp.com>
2020-07-20 13:14:46 +0200

    change paths and structure | oh | #753

--- packages/metaicons/scripts/build.ts
+++ packages/metaicons/scripts/build.ts
@@ -16,16 +16,9 @@ const toHash = (str: string): string => {
     .digest('hex');
 };
 
-// const checkIntegrity = async (manifest: Manifest): Promise<void> => {
-//   for (const [name, size] of Object.entries(manifest)) {
-//     if (!size.small) throw new Error(`Touchicon size declaration "small" is missing in manifest for "${name}".`);
-//     if (!size.medium) throw new Error(`Touchicon size declaration "medium" is missing in manifest for "${name}".`);
-//   }
-// };
-
-const createManifestAndCopyTouchicon = async (cdn: string, files: string[]): Promise<void> => {
+const createManifestAndCopyMetaicons = async (cdn: string, files: string[]): Promise<void> => {
   fs.rmdirSync(path.normalize('./dist'), {recursive: true});
-  fs.mkdirSync(path.normalize('./dist/touchicons'), {recursive: true});
+  fs.mkdirSync(path.normalize('./dist/metaicons'), {recursive: true});
 
   const manifest: Manifest = {};
 
@@ -33,34 +26,32 @@ const createManifestAndCopyTouchicon = async (cdn: string, files: string[]): Pro
     const ext = path.extname(file);
     const sourcePath = path.normalize(file);
     const name = path.basename(sourcePath, ext);
-    const touchicons = fs.readFileSync(sourcePath, {encoding: 'binary'});
-    const hash = toHash(touchicons);
-    const filename = `${paramCase(name)}.min.${hash}.png`;
-    const targetPath = path.normalize(`./dist/touchicons/${filename}`);
+    const metaicons = fs.readFileSync(sourcePath, {encoding: 'binary'});
+    const hash = toHash(metaicons);
+    const filename = `${paramCase(name)}.${hash}.png`;
+    const targetPath = path.normalize(`./dist/metaicons/${filename}`);
 
     const nameKey = camelCase(name);
     manifest[nameKey] = filename;
 
-    fs.writeFileSync(targetPath, touchicons, {encoding: 'binary'});
+    fs.writeFileSync(targetPath, metaicons, {encoding: 'binary'});
 
-    console.log(`Touchicon "${name}" copied.`);
+    console.log(`Metaicon "${name}" copied.`);
   }
-
-  // await checkIntegrity(manifest);
 
   fs.writeFileSync(path.normalize('./index.ts'),
     `export const CDN_BASE_URL = "${cdn}";
-export const TOUCHICON_MANIFEST = ${JSON.stringify(manifest)};`
+export const METAICONS_MANIFEST = ${JSON.stringify(manifest)};`
   );
 
-  console.log('Created touchicon manifest.');
+  console.log('Created metaicons manifest.');
 };
 
 (async (): Promise<void> => {
-  const cdnTouchicons = 'https://cdn.ui.porsche.com/porsche-design-system/metaicons/touchicons';
-  const filesTouchicons = await globby('./src/touchicons/*.png');
+  const url = 'https://cdn.ui.porsche.com/porsche-design-system/metaicons/metaicons';
+  const icons = await globby('./src/**/*');
 
-  await createManifestAndCopyTouchicon(cdnTouchicons, filesTouchicons).catch(e => {
+  await createManifestAndCopyMetaicons(url, icons).catch(e => {
     console.error(e);
     process.exit(1);
   });
