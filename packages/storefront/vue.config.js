e66b6cac9a75f3ac0bee4ed0bb9c73f8226554d5
Oliver Hook <oliver.hook@mhp.com>
2020-07-23 14:07:54 +0200

    add pwa plugon to storefront | oh | #753

--- packages/storefront/vue.config.js
+++ packages/storefront/vue.config.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const metaicons = require('@porsche-design-system/assets');
+
 module.exports = {
   publicPath: './',
   chainWebpack: (config) => {
@@ -26,5 +29,34 @@ module.exports = {
   },
   devServer: {
     progress: false
+  },
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    name: 'Porsche Design System',
+    themeColor: '#FFFFFF',
+    msTileColor: '#FFFFFF',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'white',
+    iconPaths: {
+      favicon16: `${metaicons.METAICONS_CDN_BASE_URL}/${metaicons.METAICONS_MANIFEST.favicon.favicon_16x16}`,
+      favicon32: `${metaicons.METAICONS_CDN_BASE_URL}/${metaicons.METAICONS_MANIFEST.favicon.favicon_32x32}`,
+      favicon48: `${metaicons.METAICONS_CDN_BASE_URL}/${metaicons.METAICONS_MANIFEST.favicon.favicon_48x48}`,
+      appleTouchIcon: `${metaicons.METAICONS_CDN_BASE_URL}/${metaicons.METAICONS_MANIFEST.touchicon.appleTouchIcon_180x180}`,
+      maskIcon: `${metaicons.METAICONS_CDN_BASE_URL}/${metaicons.METAICONS_MANIFEST.pinnedTab.pinnedTabIcon}`,
+      msTileImage: `${metaicons.METAICONS_CDN_BASE_URL}/${metaicons.METAICONS_MANIFEST.mstile.mstile_270x270}`,
+    },
+    manifestOptions: {
+      icons: [{
+        'src': `${metaicons.METAICONS_CDN_BASE_URL}/${metaicons.METAICONS_MANIFEST.touchicon.androidChrome_192x192}`,
+        'sizes': '192x192',
+        'type': 'image/png'
+      }, {
+        'src': `${metaicons.METAICONS_CDN_BASE_URL}/${metaicons.METAICONS_MANIFEST.touchicon.androidChrome_512x512}`,
+        'sizes': '512x512',
+        'type': 'image/png'
+      }],
+      background_color: '#FFFFFF'
+    }
+
   }
 };
