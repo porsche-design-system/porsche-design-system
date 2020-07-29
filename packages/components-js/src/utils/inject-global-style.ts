75a5c84df80364faa62af63fd6bfd9ca2087f30e
Matthias Haegele <matthias.haegele@mhp.com>
2020-07-29 13:08:38 +0200

    feat: design-system-ready event to avoid fouc | mh / bh | 769

--- packages/components-js/src/utils/inject-global-style.ts
+++ packages/components-js/src/utils/inject-global-style.ts
@@ -1,16 +1,19 @@
 import { FONT_FACE_STYLE_CDN_URL } from '@porsche-design-system/utilities';
 
-export const injectGlobalStyle = (): void => {
+export const injectGlobalStyle = (): Promise<void> => {
   if (typeof document === 'undefined') {
     return;
   }
+  return new Promise((resolve) => {
+    const link = document.createElement('link');
+    link.href =
+      ROLLUP_REPLACE_IS_STAGING === 'production'
+        ? FONT_FACE_STYLE_CDN_URL
+        : 'http://localhost:3001/style/font-face.min.css';
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
+    link.onload = () => resolve();
 
-  const link = document.createElement('link');
-  link.href =
-    ROLLUP_REPLACE_IS_STAGING === 'production'
-      ? FONT_FACE_STYLE_CDN_URL
-      : 'http://localhost:3001/style/font-face.min.css';
-  link.type = 'text/css';
-  link.rel = 'stylesheet';
-  document.getElementsByTagName('head')[0].appendChild(link);
+    document.getElementsByTagName('head')[0].appendChild(link);
+  });
 };
