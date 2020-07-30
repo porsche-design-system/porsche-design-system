46c6d544c131e832f27d4b493a0b842911b54814
Matthias Haegele <matthias.haegele@mhp.com>
2020-07-30 14:59:31 +0200

    feat: style hidden div in designSystemReady function | mh | 769

--- packages/components-js/src/utils/design-system-ready.ts
+++ packages/components-js/src/utils/design-system-ready.ts
@@ -1,11 +1,37 @@
 // Export of event name to ensure easy imports
 export const PORSCHE_DESIGN_SYSTEM_READY_EVENT = 'porscheDesignSystemReady';
 
-export const designSystemReady = () => {
-  // By creating a hidden div which gets removed after fonts are loaded we force the browser to load our font immediately.
-  const hiddenFontLoader = document.createElement('div');
-  hiddenFontLoader.id = 'p-font-loader';
-  document.body.appendChild(hiddenFontLoader);
+export const designSystemReady = (): void => {
+  // By creating a hidden div which gets removed after fonts are loaded we force the browser to load our font immediately, but only if there is no preloading set.
+  let preload: boolean = true;
+  const preLoaderId = 'p-font-loader';
+  const hiddenStyleId = 'font-loader-style';
+  if (!document.querySelector(`link[rel="preload"][as="font"]`)) {
+    preload = false;
+    const hiddenStyle = document.createElement('style');
+    hiddenStyle.innerHTML = `
+    #p-font-loader {font-family: "Porsche Next";
+      font-weight: 100;
+      visibility: hidden;
+    }
+    #p-font-loader::before{
+      font-weight: 400;
+      content: '';
+    }
+    #p-font-loader::after{
+      font-weight: 600;
+      content: '';
+    }
+    #p-font-loader::first-letter{
+      font-weight: 700;
+    }`;
+    hiddenStyle.id = hiddenStyleId;
+    const hiddenFontLoader = document.createElement('div');
+    hiddenFontLoader.id = preLoaderId;
+
+    document.head.append(hiddenStyle);
+    document.body.appendChild(hiddenFontLoader);
+  }
   // Some browsers need time to load the font. While loop crashes safari so we check it recursive
   (document as any).fonts.ready.then(() => {
     let i = 0;
@@ -16,7 +42,11 @@ export const designSystemReady = () => {
       i++;
     };
     checkForFonts();
-    document.body.removeChild(hiddenFontLoader);
-    document.dispatchEvent(new Event(PORSCHE_DESIGN_SYSTEM_READY_EVENT))
+
+    if (!preload) {
+      document.body.removeChild(document.getElementById(preLoaderId));
+      document.head.removeChild(document.getElementById(hiddenStyleId))
+    }
+    document.dispatchEvent(new Event(PORSCHE_DESIGN_SYSTEM_READY_EVENT));
   });
 };
