89931a9b7ae0e8d660b3d8067955732cf9c4d19e
Matthias Haegele <matthias.haegele@mhp.com>
2020-07-30 15:00:28 +0200

    feat: check for preload before injecting style | mh | 769

--- packages/components-js/src/utils/inject-global-style.ts
+++ packages/components-js/src/utils/inject-global-style.ts
@@ -4,16 +4,22 @@ export const injectGlobalStyle = (): Promise<void> => {
   if (typeof document === 'undefined') {
     return;
   }
-  return new Promise((resolve) => {
-    const link = document.createElement('link');
-    link.href =
-      ROLLUP_REPLACE_IS_STAGING === 'production'
-        ? FONT_FACE_STYLE_CDN_URL
-        : 'http://localhost:3001/style/font-face.min.css';
-    link.type = 'text/css';
-    link.rel = 'stylesheet';
-    link.onload = () => resolve();
+  const styleUrl =
+    ROLLUP_REPLACE_IS_STAGING === 'production'
+      ? FONT_FACE_STYLE_CDN_URL
+      : 'http://localhost:3001/style/font-face.min.css';
 
-    document.getElementsByTagName('head')[0].appendChild(link);
+  return new Promise((resolve) => {
+    if (document.querySelector(`link[href="${styleUrl}"]`)) {
+      resolve();
+    } else {
+      const link = document.createElement('link');
+      link.href = styleUrl;
+      link.type = 'text/css';
+      link.rel = 'stylesheet';
+      link.onload = () => resolve();
+
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
   });
 };
