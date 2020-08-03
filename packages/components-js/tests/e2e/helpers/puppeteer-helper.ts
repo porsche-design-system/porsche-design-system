f173036a97170ccaa11cf191909117c359181785
Bernd Hacker <hacker.bernd@gmail.com>
2020-08-03 12:20:04 +0200

    test(components-js): fix e2e tests by adding base tag | bh | #679

--- packages/components-js/tests/e2e/helpers/puppeteer-helper.ts
+++ packages/components-js/tests/e2e/helpers/puppeteer-helper.ts
@@ -8,6 +8,7 @@ export const setContentWithDesignSystem = async (
   await page.setContent(
     `
       <head>
+        <base href="https://google.com"> <!-- NOTE: we need a base tag so that document.baseURI returns something else than "about: blank" -->
         <script type="text/javascript" src="http://localhost:8575/index.js"></script>
       </head>
       <body>
