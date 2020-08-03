9502fa73418f7bdc1ccad71e5e91c732492d241e
Bernd Hacker <hacker.bernd@gmail.com>
2020-08-03 16:34:24 +0200

    test: update names of cbts | bh | #679

--- packages/components-js/tests/cbt/specs/overview.cbt.ts
+++ packages/components-js/tests/cbt/specs/overview.cbt.ts
@@ -8,28 +8,28 @@ describe('Components Overview', () => {
     cbt = getCrossBrowserTester();
   });
 
-  it('should have no visual regression in IE11 for basic', async () => {
+  it('should have no visual regression in Edge 18 for basic', async () => {
     expect(await cbt.test('overview-basic', '/index.html#basic')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for action', async () => {
+  it('should have no visual regression in Edge 18 for action', async () => {
     expect(await cbt.test('overview-action', '/index.html#action')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for content', async () => {
+  it('should have no visual regression in Edge 18 for content', async () => {
     expect(await cbt.test('overview-content', '/index.html#content')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for feedback', async () => {
+  it('should have no visual regression in Edge 18 for feedback', async () => {
     expect(await cbt.test('overview-form', '/index.html#form')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for feedback', async () => {
+  it('should have no visual regression in Edge 18 for feedback', async () => {
     expect(await cbt.test('overview-feedback', '/index.html#feedback')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for icon', async () => {
+  it('should have no visual regression in Edge 18 for icon', async () => {
     expect(await cbt.test('overview-icon', '/index.html#icon')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for layout', async () => {
+  it('should have no visual regression in Edge 18 for layout', async () => {
     expect(await cbt.test('overview-layout', '/index.html#layout')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for navigation', async () => {
+  it('should have no visual regression in Edge 18 for navigation', async () => {
     expect(await cbt.test('overview-navigation', '/index.html#navigation')).toBeFalsy();
   });
 });
--- packages/components-react/tests/cbt/specs/overview.cbt.ts
+++ packages/components-react/tests/cbt/specs/overview.cbt.ts
@@ -8,25 +8,25 @@ describe('Components Overview', () => {
     cbt = getCrossBrowserTester();
   });
 
-  it('should have no visual regression in IE11 for basic', async () => {
+  it('should have no visual regression in Edge 18 for basic', async () => {
     expect(await cbt.test('overview-basic', '/basic')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for action', async () => {
+  it('should have no visual regression in Edge 18 for action', async () => {
     expect(await cbt.test('overview-action', '/action')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for form', async () => {
+  it('should have no visual regression in Edge 18 for form', async () => {
     expect(await cbt.test('overview-form', '/form')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for feedback', async () => {
+  it('should have no visual regression in Edge 18 for feedback', async () => {
     expect(await cbt.test('overview-feedback', '/feedback')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for icon', async () => {
+  it('should have no visual regression in Edge 18 for icon', async () => {
     expect(await cbt.test('overview-icon', '/icon')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for layout', async () => {
+  it('should have no visual regression in Edge 18 for layout', async () => {
     expect(await cbt.test('overview-layout', '/layout')).toBeFalsy();
   });
-  it('should have no visual regression in IE11 for navigation', async () => {
+  it('should have no visual regression in Edge 18 for navigation', async () => {
     expect(await cbt.test('overview-navigation', '/navigation')).toBeFalsy();
   });
 });
