d0cca17cf668879fdf6f4dc76f0078e2a35900e6
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-04 11:47:30 +0200

    fix(assets): change import of style file / remove from build | mh | 769

--- packages/assets/src/index.ts
+++ packages/assets/src/index.ts
@@ -1,12 +1,13 @@
-import { CDN_BASE_URL as P_FONTS_CDN_BASE_URL, FONTS_MANIFEST as P_FONTS_MANIFEST, CDN_FONT_CSS_FILE_URL as P_FONTS_CSS_FILE_URL } from '@porsche-design-system/fonts';
+import { CDN_BASE_URL as P_FONTS_CDN_BASE_URL, FONTS_MANIFEST as P_FONTS_MANIFEST } from '@porsche-design-system/fonts';
 import { CDN_BASE_URL as P_ICONS_CDN_BASE_URL, ICONS_MANIFEST as P_ICONS_MANIFEST } from '@porsche-design-system/icons';
 import {
   CDN_BASE_URL as P_MARQUE_CDN_BASE_URL,
   MARQUES_MANIFEST as P_MARQUES_MANIFEST
 } from '@porsche-design-system/marque';
+import { FONT_FACE_STYLE_CDN_URL } from '@porsche-design-system/utilities';
 
 // reexports are needed in order to build standalone typings
-export const FONTS_CDN_CSS_FILE_URL = P_FONTS_CSS_FILE_URL;
+export const FONTS_CDN_CSS_FILE_URL = FONT_FACE_STYLE_CDN_URL;
 export const FONTS_CDN_BASE_URL = P_FONTS_CDN_BASE_URL;
 export const FONTS_MANIFEST = P_FONTS_MANIFEST;
 export const ICONS_CDN_BASE_URL = P_ICONS_CDN_BASE_URL;
