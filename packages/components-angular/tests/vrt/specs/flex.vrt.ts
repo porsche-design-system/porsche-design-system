70c3a72ec742ee10cd98b8b9c23279b64920405f
Stephan Schröter <stephan.schroeter@mhp.com>
2020-08-05 17:41:25 +0200

    feat(components-angular): add vrt tests | sas | #679

---
+++ packages/components-angular/tests/vrt/specs/flex.vrt.ts
@@ -0,0 +1,16 @@
+import { getVisualRegressionTester, testOptions } from '../helpers';
+
+describe('Flex', () => {
+  it('should have no visual regression', async () => {
+    const vrt = getVisualRegressionTester();
+    expect(
+      await vrt.test(
+        'flex',
+        async () => {
+          await vrt.goTo('/flex');
+        },
+        testOptions
+      )
+    ).toBeFalsy();
+  });
+});
