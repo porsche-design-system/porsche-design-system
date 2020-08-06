52f328cfc710fbb5e1dd7038d17bc762b312d3e5
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-06 08:09:20 +0200

    feat(components-react): display warning if inject global style | mh | 769

--- packages/components-js/src/utils/inject-global-style.ts
+++ packages/components-js/src/utils/inject-global-style.ts
@@ -1,4 +1,5 @@
 import { FONT_FACE_STYLE_CDN_URL } from '@porsche-design-system/utilities';
+import { fontFaceCssElement } from '@porsche-design-system/assets';
 
 export const injectGlobalStyle = (): Promise<void> => {
   if (typeof document === 'undefined') {
@@ -20,6 +21,8 @@ export const injectGlobalStyle = (): Promise<void> => {
       link.onload = () => resolve();
 
       document.getElementsByTagName('head')[0].appendChild(link);
+      console.warn(`The Porsche Design System had to inject our font-face.css file into your head. If you already reload pls check the ${fontFaceCssElement} for changes and renew it. You can find
+      further documentation and examples on how to implement preloading at https://designsystem.porsche.com/v1/#/helpers/flash-of-unstyled-content`)
     }
   });
 };
