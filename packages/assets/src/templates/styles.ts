b8d0c2e93e2f60e73170f01e9691665b7103369d
Bernd Hacker <hacker.bernd@gmail.com>
2020-08-07 15:57:47 +0200

    feat(assets/components): add script to generate array of tags | bh | #769

--- packages/assets/src/templates/styles.ts
+++ packages/assets/src/templates/styles.ts
@@ -1,35 +1,8 @@
 import { FONT_FACE_STYLE_CDN_URL } from '@porsche-design-system/utilities';
-import { JSX } from '@porsche-design-system/components';
+import { TAG_NAMES } from '@porsche-design-system/components/src/tags';
 
 // We need to determine if we are local or production to change the url
 export const getFontFaceCSS = () => `<link rel="stylesheet" href="${FONT_FACE_STYLE_CDN_URL}">`;
 
-const componentTags: { [key in keyof JSX.IntrinsicElements]: string } = {
-  'p-button': '',
-  'p-button-pure': '',
-  'p-checkbox-wrapper': '',
-  'p-link': '',
-  'p-link-pure': '',
-  'p-link-social': '',
-  'p-select-wrapper': '',
-  'p-text-field-wrapper': '',
-  'p-pagination': '',
-  'p-radio-button-wrapper': '',
-  'p-textarea-wrapper': '',
-  'p-content-wrapper': '',
-  'p-divider': '',
-  'p-fieldset-wrapper': '',
-  'p-flex': '',
-  'p-flex-item': '',
-  'p-grid': '',
-  'p-grid-item': '',
-  'p-headline': '',
-  'p-marque': '',
-  'p-text-list': '',
-  'p-text-list-item': '',
-  'p-spinner': '',
-  'p-icon': '',
-  'p-text': ''
-};
-
-export const getPorscheDesignSystemCoreStyles = () => `<style>${Object.keys(componentTags).join(',')} { visibility: hidden }</style>`;
+export const getPorscheDesignSystemCoreStyles = () =>
+  `<style>${Object.keys(TAG_NAMES).join(',')} { visibility: hidden }</style>`;
---
+++ packages/components/scripts/tags.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const src = fs.readFileSync(path.normalize('./src/components.d.ts')).toString();
+const searchString = 'IntrinsicElements';
+const searchStringPos = src.indexOf(searchString) + searchString.length;
+const rawTags = src
+  .substr(searchStringPos, src.indexOf('}', searchStringPos) - searchStringPos + 1)
+  .replace(/[\s\n]/g, '')
+  .replace(/(\w+);/g, '"$1",');
+
+const rawJson = rawTags.slice(0, rawTags.lastIndexOf(',')) + '}';
+const tags = Object.keys(JSON.parse(rawJson));
+
+console.log(`Identified tags:\n${tags.map((x) => `  - ${x}`).join('\n')}`);
+
+const file = path.normalize('./src/tags.js');
+
+fs.writeFileSync(
+  file,
+  `
+/* Auto Generated Below */
+
+export const TAG_NAMES = [${tags.map((x) => `'${x}'`).join(', ')}]`
+);
