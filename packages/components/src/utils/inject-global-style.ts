a1f200c530aaba2eefe8d1432184f28290878f6c
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-07 13:19:48 +0200

    feat(components): remove style injection, refactor log | mh / bh | #769

--- packages/components/src/utils/inject-global-style.ts
+++ packages/components/src/utils/inject-global-style.ts
@@ -1,7 +1,7 @@
 import { FONT_FACE_STYLE_CDN_URL } from '@porsche-design-system/utilities';
-import { fontFaceCssElement } from '@porsche-design-system/assets';
+import { getFontFaceCSS } from '@porsche-design-system/assets/src';
 
-export const injectGlobalStyle = (): Promise<void> => {
+export const injectGlobalStyle = (): void => {
   if (typeof document === 'undefined') {
     return;
   }
@@ -10,19 +10,15 @@ export const injectGlobalStyle = (): Promise<void> => {
       ? FONT_FACE_STYLE_CDN_URL
       : 'http://localhost:3001/style/font-face.min.css';
 
-  return new Promise((resolve) => {
-    if (document.querySelector(`link[href="${styleUrl}"]`)) {
-      resolve();
-    } else {
-      const link = document.createElement('link');
-      link.href = styleUrl;
-      link.type = 'text/css';
-      link.rel = 'stylesheet';
-      link.onload = () => resolve();
+  if (!document.querySelector(`link[href="${styleUrl}"]`)) {
+    const link = document.createElement('link');
+    link.href = styleUrl;
+    link.type = 'text/css';
+    link.rel = 'stylesheet';
 
-      document.getElementsByTagName('head')[0].appendChild(link);
-      console.warn(`The Porsche Design System had to inject our font-face.css file into your head. If you already reload pls check the ${fontFaceCssElement} for changes and renew it. You can find
-      further documentation and examples on how to implement preloading at https://designsystem.porsche.com/v1/#/helpers/flash-of-unstyled-content`);
-    }
-  });
+    document.getElementsByTagName('head')[0].appendChild(link);
+    console.warn(`The Porsche Design System had to inject our font-face.css file into your head. Please preload it with '${getFontFaceCSS()}' in the head of your 'index.html'.
+If you are already preloading font-face.css please update your snippet to '${getFontFaceCSS()}'.
+You find further documentation and examples on how to implement preloading at https://designsystem.porsche.com/v2/#/helpers/flash-of-unstyled-content`);
+  }
 };
