25171c40fcabf5b86b90bf4f206b2170a6ad7f40
Bernd Hacker <hacker.bernd@gmail.com>
2020-08-10 13:25:46 +0200

    test(utilities): add jest dependency, use ts-jest preset and run tests in node, split clean scripts | bh | #769

--- packages/utilities/tests/unit/config/jest.config.js
+++ packages/utilities/tests/unit/config/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
   rootDir: '../../../',
-  testMatch: ['**/tests/unit/specs/**/*.+(ts|tsx|js)'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest'
-  }
+  testMatch: ['**/tests/unit/specs/**/*.+(ts|tsx|js)']
 };
