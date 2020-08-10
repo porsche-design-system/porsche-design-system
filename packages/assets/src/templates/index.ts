9ffbe7167fc815a104ad105a2ae5327d7dfc4c3f
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-10 13:54:25 +0200

    feat(partials): create new package partials, change imports/scripts accordingly | mh / bh | #769

--- packages/assets/src/templates/index.ts
+++
@@ -1,2 +0,0 @@
-/*export * from './font-loader';*/ // Not public yet.
-export * from './styles'
--- packages/components/src/utils/hide-components.ts
+++
@@ -1,4 +0,0 @@
-import { TAG_NAMES } from '../tags';
-
-export const getPorscheDesignSystemCoreStyles = () =>
-  `<style>${Object.keys(TAG_NAMES).join(',')} { visibility: hidden }</style>`;
