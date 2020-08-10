36342ebf6f0ccf3d4edafb7512f47a724526dfdc
Bernd Hacker <hacker.bernd@gmail.com>
2020-08-10 15:30:16 +0200

    refactor(assets): reorder imports | bh | #769

--- packages/assets/rollup.config.js
+++ packages/assets/rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const commonPlugins = () => [
