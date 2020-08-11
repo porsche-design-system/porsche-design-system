db36f0c4aa2a111461b6409b48dad663a849a1fe
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-11 15:56:07 +0200

    feat(utilities): implement FONT_FACE_STYLE_CDN_URL alias and mark as depricated | bh / mh | #769

--- packages/utilities/projects/utilities/src/js/index.ts
+++ packages/utilities/projects/utilities/src/js/index.ts
@@ -5,3 +5,8 @@ export * from './variables';
 /* Auto Generated Below */
 
 export const FONT_FACE_CDN_URL = "https://cdn.ui.porsche.com/porsche-design-system/style/font-face.min.677d41d9905a04aadcb253f71e5f71e9.css";
+/**
+ * @deprecated since v1.1.0.
+ * Please use FONT_FACE_CDN_URL instead.
+ */
+export const FONT_FACE_STYLE_CDN_URL = FONT_FACE_CDN_URL;
