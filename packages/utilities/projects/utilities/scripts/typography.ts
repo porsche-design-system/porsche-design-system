f7b749c4c917c4d9dd66bd9785fb0ce8877c2c63
Bernd Hacker <hacker.bernd@gmail.com>
2020-08-11 14:49:52 +0200

    refactor(utilities): generate typography.ts via script | bh | #769

---
+++ packages/utilities/projects/utilities/scripts/typography.ts
@@ -0,0 +1,96 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { font, mediaQuery } from '../src/js';
+
+const { family, size, weight } = font;
+
+const fontBase = {
+  fontFamily: family,
+  fontWeight: weight.semibold
+};
+
+const title = {
+  large: {
+    ...fontBase,
+    ...size['32'],
+    [mediaQuery('s', 'm')]: size['42'],
+    [mediaQuery('m', 'l')]: size['52'],
+    [mediaQuery('l', 'xl')]: size['62'],
+    [mediaQuery('xl')]: size['72']
+  }
+};
+
+const headline = {
+  '1': {
+    ...fontBase,
+    ...size['28'],
+    [mediaQuery('s', 'm')]: size['36'],
+    [mediaQuery('m', 'l')]: size['44'],
+    [mediaQuery('l', 'xl')]: size['52'],
+    [mediaQuery('xl')]: size['60']
+  },
+  '2': {
+    ...fontBase,
+    ...size['24'],
+    [mediaQuery('s', 'm')]: size['30'],
+    [mediaQuery('m', 'l')]: size['36'],
+    [mediaQuery('l', 'xl')]: size['42'],
+    [mediaQuery('xl')]: size['48']
+  },
+  '3': {
+    ...fontBase,
+    ...size['20'],
+    [mediaQuery('s', 'm')]: size['24'],
+    [mediaQuery('m', 'l')]: size['28'],
+    [mediaQuery('l', 'xl')]: size['32'],
+    [mediaQuery('xl')]: size['36']
+  },
+  '4': {
+    ...fontBase,
+    ...size['16'],
+    [mediaQuery('s', 'm')]: size['18'],
+    [mediaQuery('m', 'l')]: size['20'],
+    [mediaQuery('l', 'xl')]: size['22'],
+    [mediaQuery('xl')]: size['24']
+  },
+  '5': {
+    ...fontBase,
+    ...size['16']
+  }
+};
+
+const baseText = {
+  fontFamily: font.family,
+  fontWeight: font.weight.regular
+};
+
+const text = {
+  xSmall: { ...baseText, ...font.size.xSmall },
+  small: { ...baseText, ...font.size.small },
+  medium: { ...baseText, ...font.size.medium },
+  large: { ...baseText, ...font.size.large },
+  xLarge: { ...baseText, ...font.size.xLarge }
+};
+
+const file = path.normalize('./src/js/functions/typography.ts');
+
+fs.writeFileSync(
+  file,
+  `
+/* Auto Generated Below */
+
+/*
+ * Title
+ */
+export const title = ${JSON.stringify(title)};
+
+/*
+ * Headline
+ */
+export const headline = ${JSON.stringify(headline)};
+
+/*
+ * Text
+ */
+export const text = ${JSON.stringify(text)};`
+);
