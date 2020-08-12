3859a20bc9ad54e1e47102f956edd3236b3f2caf
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-12 08:37:23 +0200

    feat(partials): remove loader from typings | mh | #769

--- packages/assets/rollup.config.js
+++ packages/assets/rollup.config.js
@@ -1,6 +1,5 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 const commonPlugins = () => [
