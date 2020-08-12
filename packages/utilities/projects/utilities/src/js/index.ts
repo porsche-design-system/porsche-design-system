37d6e08e283cec5caa9a62bf12608110f6018ff5
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-12 08:27:54 +0200

    feat(fonts/utilities): remove newline / comment | mh | #769

--- packages/utilities/projects/utilities/src/js/index.ts
+++ packages/utilities/projects/utilities/src/js/index.ts
@@ -3,7 +3,6 @@ export * from './helper';
 export * from './variables';
 
 /* Auto Generated Below */
-
 export const FONT_FACE_CDN_URL = "https://cdn.ui.porsche.com/porsche-design-system/style/font-face.min.677d41d9905a04aadcb253f71e5f71e9.css";
 /**
  * @deprecated since v1.1.0.
