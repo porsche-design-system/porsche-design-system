4202758a38aadcdf6750a6bc6afd23e5e1c5520c
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-12 15:47:57 +0200

    feat(partials): add test for options | mh / bh | #769

--- packages/partials/src/styles.ts
+++ packages/partials/src/styles.ts
@@ -5,9 +5,9 @@ type Options = { withoutTags: boolean };
 
 // We need to determine if we are local or production to change the url
 export const getFontFaceCSS = (options?: Options) =>
-  options.withoutTags ? FONT_FACE_CDN_URL : `<link rel="stylesheet" href="${FONT_FACE_CDN_URL}">`;
+  options?.withoutTags ? FONT_FACE_CDN_URL : `<link rel="stylesheet" href="${FONT_FACE_CDN_URL}">`;
 
 export const getPorscheDesignSystemCoreStyles = (options?: Options) => {
   const styleInnerHtml = `${TAG_NAMES.join(',')} { visibility: hidden }`;
-  return options.withoutTags ? styleInnerHtml : `<style>${styleInnerHtml}</style>`;
+  return options?.withoutTags ? styleInnerHtml : `<style>${styleInnerHtml}</style>`;
 };
