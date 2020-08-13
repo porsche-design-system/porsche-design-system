902009005717820a76678f01a106365e7988cf1e
denyo <denyo@users.noreply.github.com>
2020-08-13 11:46:23 +0000

    Automated deployment of Porsche Design System - Storefront (issue/drop-classnames): 04fe030cf313ed64e67d5351f603cffb9097663e

--- issue/drop-classnames/service-worker.js
+++ issue/drop-classnames/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.4eb034da6d8364cdcb3a4b92a1026074.js"
+  "precache-manifest.11194d17473bc2f4bd51cfaaa3765afa.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@porsche-design-system/storefront"});
