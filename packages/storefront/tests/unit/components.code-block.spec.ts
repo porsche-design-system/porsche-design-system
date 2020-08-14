c9f2e962059edea36e4337380fe5eb13efbdfff7
Bernd Hacker <hacker.bernd@gmail.com>
2020-08-14 18:12:41 +0200

    test(storefront): make unit test for CodeBlock work with mocked store and remove redundancy | bh | #448

--- packages/storefront/tests/unit/components.code-block.spec.ts
+++ packages/storefront/tests/unit/components.code-block.spec.ts
@@ -1,19 +1,46 @@
 import { shallowMount } from '@vue/test-utils';
 import CodeBlock from '@/components/CodeBlock.vue';
+import Vuex, { Store } from 'vuex';
+import Vue from 'vue';
+import { State } from '@/store';
+import { Framework } from '@/models';
 
-async function tick(timeout = 100) {
-  await new Promise(resolve => {
+const tick = async (timeout = 100) => {
+  await new Promise((resolve) => {
     setTimeout(resolve, timeout);
   });
-}
+};
+
+type PartialState = Pick<State, 'selectedFramework'>;
 
 describe('CodeBlock.vue', () => {
+  Vue.use(Vuex);
+
+  const stubs = ['p-text'];
+  const store: Store<PartialState> = new Vuex.Store({
+    state: {
+      selectedFramework: 'vanilla-js'
+    },
+    getters: {
+      selectedFramework: (state: PartialState) => state.selectedFramework
+    },
+    mutations: {
+      setSelectedFramework: (state: PartialState, payload: Framework): void => {
+        state.selectedFramework = payload;
+      }
+    }
+  });
+
+  beforeEach(() => {
+    store.commit('setSelectedFramework', 'vanilla-js');
+  });
+
   it('should render Vanilla JS code block by default', () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag some-attribute="some value">
+        markup: `<p-some-tag some-attribute="some value">
   <span>some text</span>
 </p-some-tag>`
       }
@@ -30,23 +57,24 @@ describe('CodeBlock.vue', () => {
 
   it('should render Angular code block after selecting it', async () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag some-attribute="some value">
+        markup: `<p-some-tag some-attribute="some value">
   <span>some text</span>
 </p-some-tag>`
       }
     });
 
-    expect(wrapper.find('.tabs .tab:nth-child(2) button').text()).toBe('Angular');
-    expect(wrapper.find('.tabs .tab:nth-child(2) button').classes()).not.toContain('is-active');
+    const btn = wrapper.find('.tabs .tab:nth-child(2) button');
+    expect(btn.text()).toBe('Angular');
+    expect(btn.classes()).not.toContain('is-active');
 
-    wrapper.find('.tabs .tab:nth-child(2) button').trigger('click');
+    btn.trigger('click');
 
     await tick();
 
-    expect(wrapper.find('.tabs .tab:nth-child(2) button').classes()).toContain('is-active');
+    expect(btn.classes()).toContain('is-active');
     expect(wrapper.find('code').text()).toBe(
       `<p-some-tag [someAttribute]="'some value'">
   <span>some text</span>
@@ -56,51 +84,55 @@ describe('CodeBlock.vue', () => {
 
   it('should render React code block after selecting it', async () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag some-attribute="some value">
+        markup: `<p-some-tag some-attribute="some value">
   <span>some text</span>
 </p-some-tag>`
       }
     });
 
-    expect(wrapper.find('.tabs .tab:nth-child(3) button').text()).toBe('React');
-    expect(wrapper.find('.tabs .tab:nth-child(3) button').classes()).not.toContain('is-active');
+    const btn = wrapper.find('.tabs .tab:nth-child(3) button');
+    expect(btn.text()).toBe('React');
+    expect(btn.classes()).not.toContain('is-active');
 
-    wrapper.find('.tabs .tab:nth-child(3) button').trigger('click');
+    btn.trigger('click');
 
     await tick();
 
-    expect(wrapper.find('.tabs .tab:nth-child(3) button').classes()).toContain('is-active');
+    expect(btn.classes()).toContain('is-active');
     expect(wrapper.find('code').text()).toBe(
-      `<PSomeTag someAttribute={"some value"}>
+      `<PSomeTag someAttribute="some value">
   <span>some text</span>
-</PSomeTag>`);
+</PSomeTag>`
+    );
   });
 
   it('should theme code block', async () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text']
+      stubs,
+      store
     });
 
-    expect(wrapper.find('.code-block').classes()).not.toContain('dark');
-    expect(wrapper.find('.code-block').classes()).toContain('light');
+    const block = wrapper.find('.code-block');
+    expect(block.classes()).not.toContain('dark');
+    expect(block.classes()).toContain('light');
 
-    wrapper.setProps({theme: 'dark'});
+    wrapper.setProps({ theme: 'dark' });
 
     await tick();
 
-    expect(wrapper.find('.code-block').classes()).not.toContain('light');
-    expect(wrapper.find('.code-block').classes()).toContain('dark');
+    expect(block.classes()).not.toContain('light');
+    expect(block.classes()).toContain('dark');
   });
 
   it('should remove empty comments', () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag some-attribute="some value" class="some-class">
+        markup: `<p-some-tag some-attribute="some value" class="some-class">
   <!----><span>some text</span><!-- some comment -->
 </p-some-tag>`
       }
@@ -115,10 +147,10 @@ describe('CodeBlock.vue', () => {
 
   it('should beautify code block', () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag     some-attribute="some value"    class="some-class"   >
+        markup: `<p-some-tag     some-attribute="some value"    class="some-class"   >
                           <span       >some text</span>
                 </p-some-tag>`
       }
@@ -133,10 +165,10 @@ describe('CodeBlock.vue', () => {
 
   it('should remove Vue JS specific attributes from code block', () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag data-v-c6a10ac0="" data-v-8dbc1b2a='' data-v-7ba5bd90 some-attribute="some value" class="some-class">
+        markup: `<p-some-tag data-v-c6a10ac0="" data-v-8dbc1b2a='' data-v-7ba5bd90 some-attribute="some value" class="some-class">
   <span>some text</span>
 </p-some-tag>`
       }
@@ -151,10 +183,10 @@ describe('CodeBlock.vue', () => {
 
   it('should remove default web components attributes from code block', () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag theme="light" some-attribute="some value" class="some-class"></p-some-tag>
+        markup: `<p-some-tag theme="light" some-attribute="some value" class="some-class"></p-some-tag>
 <p-some-tag theme="light"></p-some-tag>
 <p-some-tag theme="dark"></p-some-tag>`
       }
@@ -169,10 +201,10 @@ describe('CodeBlock.vue', () => {
 
   it('should remove Stencil JS css classes from code block', async () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag some-attribute="some value" class="hydrated">
+        markup: `<p-some-tag some-attribute="some value" class="hydrated">
   <span>some text</span>
 </p-some-tag>`
       }
@@ -185,8 +217,7 @@ describe('CodeBlock.vue', () => {
     );
 
     wrapper.setProps({
-      markup:
-        `<p-some-tag some-attribute="some value" class="some-class hydrated another-class">
+      markup: `<p-some-tag some-attribute="some value" class="some-class hydrated another-class">
   <span>some text</span>
 </p-some-tag>`
     });
@@ -202,10 +233,10 @@ describe('CodeBlock.vue', () => {
 
   it('should convert code block to Angular syntax', async () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" digit-attribute="6" boolean-attribute="true">
+        markup: `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" digit-attribute="6" boolean-attribute="true">
   <span>some text</span>
   <span slot="some-slot">some slot text</span>
 </p-some-tag>`
@@ -226,10 +257,10 @@ describe('CodeBlock.vue', () => {
 
   it('should convert code block to React syntax', async () => {
     const wrapper = shallowMount(CodeBlock, {
-      stubs: ['p-text'],
+      stubs,
+      store,
       propsData: {
-        markup:
-          `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" digit-attribute="6" boolean-attribute="true">
+        markup: `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" digit-attribute="6" boolean-attribute="true">
   <span>some text</span>
 </p-some-tag>`
       }
@@ -240,7 +271,7 @@ describe('CodeBlock.vue', () => {
     await tick();
 
     expect(wrapper.find('code').text()).toBe(
-      `<PSomeTag someAttribute={"some value"} attribute={"some value"} className={"some-class"} anotherAttribute={{ bar: 'foo' }} onClick={()=> {alert('click'); return false;}} digitAttribute={6} booleanAttribute={true}>
+      `<PSomeTag someAttribute="some value" attribute="some value" className="some-class" anotherAttribute={{ bar: 'foo' }} onClick={()=> {alert('click'); return false;}} digitAttribute={6} booleanAttribute={true}>
   <span>some text</span>
 </PSomeTag>`
     );
