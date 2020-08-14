becad4946bd1144b75513a6d641e1dcf695fae60
Oliver Hook <oliver.hook@mhp.com>
2020-08-14 17:32:33 +0200

    usability improvements and some e2e tests | oh | #774

--- packages/components-js/tests/e2e/specs/select-wrapper.e2e.ts
+++ packages/components-js/tests/e2e/specs/select-wrapper.e2e.ts
@@ -766,4 +766,86 @@ describe('select-wrapper', () => {
       expect(await selectHasFocus()).toBe(false);
     });
   });
+
+  fdescribe('fake drop down with filter (combobox)', () => {
+    it('should render', async () => {
+      await setContentWithDesignSystem(
+        page,
+        `
+      <p-select-wrapper label="Some label" filter="true">
+        <select name="some-name">
+          <option value="a">Option A</option>
+          <option value="b" disabled>Option B</option>
+          <option value="c">Option C</option>
+        </select>
+      </p-select-wrapper>`
+      );
+
+      const fakeOptionList = await getSelectOptionList();
+      const fakeOptionFilter = await selectNode(
+        page,
+        'p-select-wrapper >>> .p-select-wrapper__filter-input'
+      );
+      const fakeOptionSelected = await selectNode(
+        page,
+        'p-select-wrapper >>> .p-select-wrapper__fake-option--selected'
+      );
+      const activeDescendant = await getAttribute(fakeOptionFilter, 'aria-activedescendant');
+      const selectedDescendantId = (await getProperty(fakeOptionSelected, 'id')) as string;
+
+      expect(fakeOptionList).not.toBeNull();
+      expect(fakeOptionFilter).not.toBeNull();
+      expect(activeDescendant).toEqual(selectedDescendantId);
+    });
+
+    it('should focus filter when label text is clicked', async () => {
+      await setContentWithDesignSystem(
+        page,
+        `<p-select-wrapper label="Some label" filter="true">
+          <select name="some-name">
+            <option value="a">Option A</option>
+            <option value="b">Option B</option>
+            <option value="c">Option C</option>
+          </select>
+        </p-select-wrapper>`
+      );
+
+      const labelText = await getSelectLabel();
+      const filterInput = await selectNode(page, 'p-select-wrapper >>> .p-select-wrapper__filter-input');
+      let afterFocusCalls = 0;
+      await addEventListener(filterInput, 'focus', () => afterFocusCalls++);
+
+      expect(afterFocusCalls).toBe(0);
+      await labelText.click();
+      await waitForStencilLifecycle(page);
+      expect(afterFocusCalls).toBe(1);
+    });
+
+    it('should focus filter when tab key is pressed', async () => {
+      await setContentWithDesignSystem(
+        page,
+        `<p-select-wrapper label="Some label" filter="true">
+          <select name="some-name">
+            <option value="a">Option A</option>
+            <option value="b">Option B</option>
+            <option value="c">Option C</option>
+          </select>
+        </p-select-wrapper>`
+      );
+
+      const filterInput = await selectNode(page, 'p-select-wrapper >>> .p-select-wrapper__filter-input');
+      let afterFocusCalls = 0;
+      await addEventListener(filterInput, 'focus', () => afterFocusCalls++);
+
+      expect(afterFocusCalls).toBe(0);
+      await page.keyboard.press('Tab');
+      await waitForStencilLifecycle(page);
+      expect(afterFocusCalls).toBe(1);
+    });
+
+
+
+
+
+  });
 });
