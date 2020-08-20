4f09e055f5e7f874d06e0617484387ca52fb1158
Bernd Hacker <hacker.bernd@gmail.com>
2020-08-20 14:14:42 +0200

    test(components): extend unit test for tracking pixel | bh | #900

--- packages/components/tests/unit/utils/tracking.spec.ts
+++ packages/components/tests/unit/utils/tracking.spec.ts
@@ -1,10 +1,19 @@
 import { trackEvent } from '../../../src/utils';
 
 describe('trackEvent', () => {
-  it('do a call via fetch', () => {
-    // @ts-ignore
+  it('does a call via fetch', () => {
     const spy = jest.spyOn(global, 'fetch');
+
+    let requestUri = '';
+    spy.mockImplementationOnce((val) => {
+      requestUri = val.toString();
+      return Promise.resolve(new Response());
+    });
+
     trackEvent('test', 'test');
+
+    expect(requestUri).toContain('aws.designsystem.porsche.com');
+    expect(requestUri).not.toContain('0.0.0');
     expect(spy).toHaveBeenCalledTimes(1);
     spy.mockRestore();
   });
