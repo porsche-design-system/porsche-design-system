37545fb8e1b85cf2046afaa18a6203cb913263f8
melaniekolar <melaniekolar@users.noreply.github.com>
2020-08-20 06:42:49 +0000

    Automated deployment of Porsche Design System - Storefront (v1): ca5e19f9c8efc1dae8f5446c3dc34f861dd71d21

--- v1/service-worker.js
+++ v1/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8b4f5c05f7fd1ffb743fd742ad5aa2e0.js"
+  "precache-manifest.0586f2c0afa1179c0d5d74d4dd86b821.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@porsche-design-system/storefront"});
