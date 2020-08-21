1114a09f1fcc95bfaa61a94e843a3d4ab8d7a58b
oliviapala <oliviapala@users.noreply.github.com>
2020-08-21 09:14:22 +0000

    Automated deployment of Porsche Design System - Storefront (housekeeping/sketch-libraries-release): 9e32f2a71ddbc37743094b368ef565300716fa54

--- housekeeping/sketch-libraries-release/service-worker.js
+++ housekeeping/sketch-libraries-release/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.b1e52560e5c598943bbf856f769d604d.js"
+  "precache-manifest.a27d91c023d5193ba4d8fffe6df9b26e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@porsche-design-system/storefront"});
