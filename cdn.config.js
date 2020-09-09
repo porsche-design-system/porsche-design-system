e4f51fe431d768c26195fb01e8ce2ef36f7635d2
Stephan Schröter <stephan.schroeter@mhp.com>
2020-09-09 16:54:02 +0200

    feat: dynamic cdn url config | sas | #927

--- cdn.config.js
+++ cdn.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  CDN_BASE_URL: 'https://cdn.ui.porsche.com',
-  CDN_BASE_URL_CN: 'https://cdn.ui.porsche.cn',
+  CDN_BASE_URL: PORSCHE_DESIGN_SYSTEM_CDN && PORSCHE_DESIGN_SYSTEM_CDN === 'cn' ? 'https://cdn.ui.porsche.cn' : 'https://cdn.ui.porsche.com',
   CDN_BASE_PATH_COMPONENTS: 'porsche-design-system/components',
   CDN_BASE_PATH_FONTS: 'porsche-design-system/fonts',
   CDN_BASE_PATH_ICONS: 'porsche-design-system/icons',
-  CDN_BASE_PATH_META_ICONS: 'porsche-design-system/meta-icons', // previously "metaicons"
+  CDN_BASE_PATH_META_ICONS: 'porsche-design-system/meta-icons',
   CDN_BASE_PATH_MARQUE: 'porsche-design-system/marque',
-  CDN_BASE_PATH_STYLES: 'porsche-design-system/styles' // previously "style"
+  CDN_BASE_PATH_STYLES: 'porsche-design-system/styles'
 };
