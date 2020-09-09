b59a654f3effbe64b1aeefa3e63cdd52ab8275dc
Stephan Schröter <stephan.schroeter@mhp.com>
2020-09-09 12:30:50 +0200

    feat: add cdn config file | sas | #927

---
+++ cdn.config.js
@@ -0,0 +1,9 @@
+export const CDN_BASE_URL = 'https://cdn.ui.porsche.com';
+export const CDN_BASE_URL_CN = 'https://cdn.ui.porsche.cn';
+
+export const CDN_BASE_PATH_COMPONENTS = '/porsche-design-system/components';
+export const CDN_BASE_PATH_FONTS = '/porsche-design-system/fonts';
+export const CDN_BASE_PATH_ICONS = '/porsche-design-system/icons';
+export const CDN_BASE_PATH_META_ICONS = '/porsche-design-system/meta-icons'; // previously "metaicons"
+export const CDN_BASE_PATH_MARQUE = '/porsche-design-system/marque';
+export const CDN_BASE_PATH_STYLES = '/porsche-design-system/styles'; // previously "style"
