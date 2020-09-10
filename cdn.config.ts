19b5233746914fa04da76ceaa696cfdc7116e223
Bernd Hacker <hacker.bernd@gmail.com>
2020-09-10 15:13:37 +0200

    refactor: extract basePath variable in cdn config | bh | #927

--- cdn.config.ts
+++ cdn.config.ts
@@ -11,9 +11,11 @@ declare global {
 export const CDN_BASE_URL = 'https://cdn.ui.porsche.com';
 export const CDN_BASE_URL_CN = 'https://cdn.ui.porsche.cn';
 export const CDN_BASE_URL_DYNAMIC = `typeof window !== 'undefined' && (window[CDN_KEY] || 'auto') === 'cn' ? '${CDN_BASE_URL_CN}' : '${CDN_BASE_URL}'`;
-export const CDN_BASE_PATH_COMPONENTS = 'porsche-design-system/components';
-export const CDN_BASE_PATH_FONTS = 'porsche-design-system/fonts';
-export const CDN_BASE_PATH_ICONS = 'porsche-design-system/icons';
-export const CDN_BASE_PATH_META_ICONS = 'porsche-design-system/meta-icons';
-export const CDN_BASE_PATH_MARQUE = 'porsche-design-system/marque';
-export const CDN_BASE_PATH_STYLES = 'porsche-design-system/styles';
+
+const basePath = 'porsche-design-system';
+export const CDN_BASE_PATH_COMPONENTS = `${basePath}/components`;
+export const CDN_BASE_PATH_FONTS = `${basePath}/fonts`;
+export const CDN_BASE_PATH_ICONS = `${basePath}/icons`;
+export const CDN_BASE_PATH_META_ICONS = `${basePath}/meta-icons`;
+export const CDN_BASE_PATH_MARQUE = `${basePath}/marque`;
+export const CDN_BASE_PATH_STYLES = `${basePath}/styles`;
