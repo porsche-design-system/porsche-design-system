644b7857bf5e3bdaf18388c96025a6ebffc5089a
Bernd Hacker <hacker.bernd@gmail.com>
2020-09-11 13:07:59 +0200

    fix(utilities): build font face for cdn correctly | bh / sas | #927

--- packages/partials/src/styles.ts
+++ packages/partials/src/styles.ts
@@ -6,7 +6,7 @@ type Options = {
 };
 
 export const getFontFaceCSS = (opts?: Options): string => {
-  const url = `${opts?.cdn === 'cn' ? 'https://cdn.ui.porsche.cn' : 'https://cdn.ui.porsche.com'}/porsche-design-system/styles/${opts?.cdn === 'cn' ? 'font-face.min.cn.abdac883866edfff1d135131399118d9.css' : 'font-face.min.02a9c7ba97467baa51aa8a2ac5d1a777.css'}`;
+  const url = `${opts?.cdn === 'cn' ? 'https://cdn.ui.porsche.cn' : 'https://cdn.ui.porsche.com'}/porsche-design-system/styles/${opts?.cdn === 'cn' ? 'font-face.min.cn.172e88acd22bc094e313d6d7bd56a11a.css' : 'font-face.min.b0ce2316e7754d7e76bbc1ff9fcc7932.css'}`;
   return opts?.withoutTags ? url : '<link rel=stylesheet href=$URL$>'.replace('$URL$', url);
 }
 
