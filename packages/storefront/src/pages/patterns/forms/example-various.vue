96a130eeb204f870fb25e01945f2b15f5c1b72ea
Bernd Hacker <hacker.bernd@gmail.com>
2020-09-28 11:45:17 +0200

    feat(storefront): add form validation to various form pattern | bh | #811

--- packages/storefront/src/pages/patterns/forms/example-various.vue
+++ packages/storefront/src/pages/patterns/forms/example-various.vue
@@ -4,39 +4,86 @@
     <p-grid class="form-top-spacing">
       <p-grid-item size="{ base: 12, m: 8 }">
         <p-headline variant="headline-2" tag="h1">Validation of grouped form elements</p-headline>
-        <p-text size="{ base: 'small', l: 'medium' }" class="spacing-mt-8">How to implement validation messages to
-          grouped form elements like checkbox- and radio groups.
+        <p-text size="{ base: 'small', l: 'medium' }" class="spacing-mt-8"
+          >How to implement validation messages to grouped form elements like checkbox- and radio groups.
         </p-text>
       </p-grid-item>
     </p-grid>
     <p-grid class="form-section-spacing">
       <p-grid-item size="{ base: 12, s: 10, m: 8, l: 6 }">
-        <form novalidate class="form-bottom-spacing">
+        <form novalidate class="form-bottom-spacing" @submit.prevent="onSubmit">
           <p-fieldset-wrapper label="Grouped list of checkboxes" class="form-section-spacing">
-            <p-checkbox-wrapper label="Some checkbox label 1" state="error">
-              <input type="checkbox" name="check1">
+            <p-checkbox-wrapper label="Some checkbox label 1" v-bind:state="getState('check')">
+              <input
+                type="checkbox"
+                value="check1"
+                v-bind:name="validateFieldName('check')"
+                v-model="bag.data.check.check1"
+                v-on:change="onFieldBlur"
+              />
             </p-checkbox-wrapper>
-            <p-checkbox-wrapper label="Some checkbox label 2" state="error" class="form-row-spacing">
-              <input type="checkbox" name="check2">
+            <p-checkbox-wrapper label="Some checkbox label 2" v-bind:state="getState('check')" class="form-row-spacing">
+              <input
+                type="checkbox"
+                value="check2"
+                v-bind:name="validateFieldName('check')"
+                v-model="bag.data.check.check2"
+                v-on:change="onFieldBlur"
+              />
             </p-checkbox-wrapper>
-            <p-checkbox-wrapper label="Some checkbox label 3" state="error" class="form-row-spacing">
-              <input type="checkbox" name="check3">
+            <p-checkbox-wrapper label="Some checkbox label 3" v-bind:state="getState('check')" class="form-row-spacing">
+              <input
+                type="checkbox"
+                value="check3"
+                v-bind:name="validateFieldName('check')"
+                v-model="bag.data.check.check3"
+                v-on:change="onFieldBlur"
+              />
             </p-checkbox-wrapper>
-            <p-text color="notification-error" class="form-row-spacing">Please check at least one item from the list.
+            <p-text v-if="bag.errors.check" color="notification-error" class="form-row-spacing"
+              >{{ bag.errors.check }}
             </p-text>
           </p-fieldset-wrapper>
 
           <p-fieldset-wrapper label="Grouped list of radio buttons" class="form-section-spacing">
-            <p-radio-button-wrapper label="Some radio label 1" state="error">
-              <input type="radio" name="radio1">
+            <p-radio-button-wrapper label="Some radio label 1" v-bind:state="getState('radio')">
+              <input
+                type="radio"
+                value="radio1"
+                v-bind:name="validateFieldName('radio')"
+                v-model="bag.data.radio"
+                v-on:change="onFieldBlur"
+              />
             </p-radio-button-wrapper>
-            <p-radio-button-wrapper label="Some radio label 2" state="error" class="form-row-spacing">
-              <input type="radio" name="radio1">
+            <p-radio-button-wrapper
+              label="Some radio label 2"
+              v-bind:state="getState('radio')"
+              class="form-row-spacing"
+            >
+              <input
+                type="radio"
+                value="radio2"
+                v-bind:name="validateFieldName('radio')"
+                v-model="bag.data.radio"
+                v-on:change="onFieldBlur"
+              />
             </p-radio-button-wrapper>
-            <p-radio-button-wrapper label="Some radio label 3" state="error" class="form-row-spacing">
-              <input type="radio" name="radio1">
+            <p-radio-button-wrapper
+              label="Some radio label 3"
+              v-bind:state="getState('radio')"
+              class="form-row-spacing"
+            >
+              <input
+                type="radio"
+                value="radio3"
+                v-bind:name="validateFieldName('radio')"
+                v-model="bag.data.radio"
+                v-on:change="onFieldBlur"
+              />
             </p-radio-button-wrapper>
-            <p-text color="notification-error" class="form-row-spacing">Please select one item from the list.</p-text>
+            <p-text v-if="bag.errors.radio" color="notification-error" class="form-row-spacing">{{
+              bag.errors.radio
+            }}</p-text>
           </p-fieldset-wrapper>
 
           <p-fieldset-wrapper label="Grouped short input" class="form-section-spacing">
@@ -44,25 +91,54 @@
               <p-grid-item size="{ base: 12, s: 10, m: 8, l: 6 }">
                 <p-flex class="form-grid-item-container">
                   <p-flex-item width="one-quarter" class="form-grid-item">
-                    <p-text-field-wrapper state="error" label="Day">
-                      <input type="number" placeholder="DD" name="day" aria-describedby="error-message-1">
+                    <p-text-field-wrapper v-bind:state="getState('day')" label="Day">
+                      <input
+                        type="number"
+                        placeholder="DD"
+                        v-bind:name="validateFieldName('day')"
+                        v-model="bag.data.day"
+                        v-on:blur="onFieldBlur"
+                        required
+                        aria-describedby="error-message-1"
+                      />
                     </p-text-field-wrapper>
                   </p-flex-item>
                   <p-flex-item width="one-quarter" class="form-grid-item">
-                    <p-text-field-wrapper state="error" label="Month">
-                      <input type="number" placeholder="MM" name="month" aria-describedby="error-message-2">
+                    <p-text-field-wrapper v-bind:state="getState('month')" label="Month">
+                      <input
+                        type="number"
+                        placeholder="MM"
+                        v-bind:name="validateFieldName('month')"
+                        v-model="bag.data.month"
+                        v-on:blur="onFieldBlur"
+                        required
+                        aria-describedby="error-message-2"
+                      />
                     </p-text-field-wrapper>
                   </p-flex-item>
                   <p-flex-item width="one-quarter" class="form-grid-item">
-                    <p-text-field-wrapper label="Year">
-                      <input type="number" value="1998" placeholder="YYYY" name="year">
+                    <p-text-field-wrapper v-bind:state="getState('year')" label="Year">
+                      <input
+                        type="number"
+                        placeholder="YYYY"
+                        v-bind:name="validateFieldName('year')"
+                        v-model="bag.data.year"
+                        v-on:blur="onFieldBlur"
+                        required
+                        aria-describedby="error-message-3"
+                      />
                     </p-text-field-wrapper>
                   </p-flex-item>
                 </p-flex>
-                <p-text color="notification-error" id="error-message-1" class="form-row-spacing">Please provide a valid
-                  day
+                <p-text v-if="bag.errors.day" color="notification-error" id="error-message-1" class="form-row-spacing"
+                  >{{ bag.errors.day }}
                 </p-text>
-                <p-text color="notification-error" id="error-message-2">Please provide a valid month</p-text>
+                <p-text v-if="bag.errors.month" color="notification-error" id="error-message-2">{{
+                  bag.errors.month
+                }}</p-text
+                ><p-text v-if="bag.errors.year" color="notification-error" id="error-message-3">{{
+                  bag.errors.year
+                }}</p-text>
               </p-grid-item>
             </p-grid>
           </p-fieldset-wrapper>
@@ -71,3 +147,75 @@
     </p-grid>
   </p-content-wrapper>
 </template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import { boolean, number, object, string } from 'yup';
+  import { validateName, getState, validateField, validateForm, ValidationBag, getInitialErrors } from '../../../utils';
+
+  const initialData = {
+    check: {
+      check1: false,
+      check2: false,
+      check3: false
+    },
+    radio: '',
+    day: ('' as unknown) as number,
+    month: ('' as unknown) as number,
+    year: 1998
+  };
+
+  type FormModel = typeof initialData;
+
+  @Component
+  export default class VariousForm extends Vue {
+    private validateFieldName: (field: keyof FormModel) => keyof FormModel = validateName;
+    private getState = (field: keyof FormModel) => getState(field, this.bag);
+
+    private bag: ValidationBag<FormModel> = {
+      data: { ...initialData },
+      errors: getInitialErrors(initialData),
+      schema: object<FormModel>({
+        check: object<FormModel['check']>({
+          check1: boolean(),
+          check2: boolean(),
+          check3: boolean()
+        })
+          .required()
+          .test('customTest', 'Need to select at least one option', (obj) => Object.values(obj!).some((val) => val)),
+        radio: string().required(),
+        day: number()
+          .required()
+          .min(1)
+          .max(31),
+        month: number()
+          .required()
+          .min(1)
+          .max(12),
+        year: number()
+          .required()
+          .min(1000)
+          .max(9999)
+      })
+    };
+
+    created(): void {
+      this.onSubmit();
+    }
+
+    onFieldBlur = ({ target }: FocusEvent & { target: HTMLInputElement }): void => {
+      validateField(target.name as keyof FormModel, this.bag);
+    };
+
+    onSubmit = async (): Promise<void> => {
+      const isValid = await validateForm(this.bag);
+      console.log('isValid', isValid);
+    };
+
+    onReset = (): void => {
+      this.bag.data = { ...initialData };
+      this.bag.errors = getInitialErrors(initialData);
+    };
+  }
+</script>
