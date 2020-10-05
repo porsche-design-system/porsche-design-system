cd1d242d692c8d163964c4230b7c8e9ef272a4dc
Matthias Haegele <matthias.haegele@mhp.com>
2020-10-05 11:03:27 +0200

    test(components): add hrefs to anchor tags | mh | #536

--- packages/components-angular/src/app/pages/tabs-bar.component.ts
+++ packages/components-angular/src/app/pages/tabs-bar.component.ts
@@ -5,199 +5,199 @@ import { Component } from '@angular/core';
   template: `
     <div class="playground light" title="should render tabs on light background">
       <p-tabs-bar>
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground dark" title="should render tabs on dark background">
       <p-tabs-bar [theme]="'dark'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground light" title="should render tabs with last tab selected on light background">
       <p-tabs-bar [activeTabIndex]="6">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground dark" title="should render tabs with last tab selected on dark background">
       <p-tabs-bar [activeTabIndex]="6" [theme]="'dark'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground light" title="should render tabs size medium on light background">
       <p-tabs-bar [size]="'medium'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground dark" title="should render tabs size medium on dark background">
       <p-tabs-bar [theme]="'dark'" [size]="'medium'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground light" title="should render tabs size medium with last tab selected on light background">
       <p-tabs-bar [activeTabIndex]="6" [size]="'medium'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground dark" title="should render tabs size medium with last tab selected on dark background">
       <p-tabs-bar [activeTabIndex]="6" [theme]="'dark'" [size]="'medium'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground light" title="should render tabs semibold on light background">
       <p-tabs-bar [weight]="'semibold'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground dark" title="should render tabs semibold on dark background">
       <p-tabs-bar [theme]="'dark'" [weight]="'semibold'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground light" title="should render tabs semibold and medium on light background">
       <p-tabs-bar [weight]="'semibold'" [size]="'medium'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground dark" title="should render tabs semibold and medium on dark background">
       <p-tabs-bar [theme]="'dark'" [weight]="'semibold'" [size]="'medium'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground light" title="should render tabs gradientColorScheme surface on light background">
       <p-tabs-bar [gradientColorScheme]="'surface'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground dark" title="should render tabs gradientColorScheme surface on dark background">
       <p-tabs-bar [theme]="'dark'" [gradientColorScheme]="'surface'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground light" title="should render selected tab in viewport on light background">
       <p-tabs-bar [activeTabIndex]="3">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
-        <a>Tab 8</a>
-        <a>Tab 9</a>
-        <a>Tab 10</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
+        <a href="#">Tab 8</a>
+        <a href="#">Tab 9</a>
+        <a href="#">Tab 10</a>
       </p-tabs-bar>
     </div>
 
     <div class="playground dark" title="should render selected tab in viewport in viewport on dark background">
       <p-tabs-bar [activeTabIndex]="3" [theme]="'dark'">
-        <a>Tab 1</a>
-        <a>Tab 2</a>
-        <a>Tab 3</a>
-        <a>Tab 4</a>
-        <a>Tab 5</a>
-        <a>Tab 6</a>
-        <a>Tab 7</a>
-        <a>Tab 8</a>
-        <a>Tab 9</a>
-        <a>Tab 10</a>
+        <a href="#">Tab 1</a>
+        <a href="#">Tab 2</a>
+        <a href="#">Tab 3</a>
+        <a href="#">Tab 4</a>
+        <a href="#">Tab 5</a>
+        <a href="#">Tab 6</a>
+        <a href="#">Tab 7</a>
+        <a href="#">Tab 8</a>
+        <a href="#">Tab 9</a>
+        <a href="#">Tab 10</a>
       </p-tabs-bar>
     </div>
   `
