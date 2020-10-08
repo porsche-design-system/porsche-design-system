d6bb4dc32413075620433812dcb7c43c204326c9
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-08 08:54:26 +0200

    feat(components): improve typing of getPrefixedTagNames, add getAllPrefixedTagNames | bh | #775

--- packages/components/src/tags.ts
+++ packages/components/src/tags.ts
@@ -1,2 +1,6 @@
 /* Auto Generated File */
-export const TAG_NAMES = ['p-button', 'p-button-pure', 'p-checkbox-wrapper', 'p-content-wrapper', 'p-divider', 'p-fieldset-wrapper', 'p-flex', 'p-flex-item', 'p-grid', 'p-grid-item', 'p-headline', 'p-icon', 'p-link', 'p-link-pure', 'p-link-social', 'p-marque', 'p-modal', 'p-pagination', 'p-radio-button-wrapper', 'p-select-wrapper', 'p-spinner', 'p-text', 'p-text-field-wrapper', 'p-text-list', 'p-text-list-item', 'p-textarea-wrapper'];
+export const TAG_NAMES = ['p-button', 'p-button-pure', 'p-checkbox-wrapper', 'p-content-wrapper', 'p-divider', 'p-fieldset-wrapper', 'p-flex', 'p-flex-item', 'p-grid', 'p-grid-item', 'p-headline', 'p-icon', 'p-link', 'p-link-pure', 'p-link-social', 'p-marque', 'p-modal', 'p-pagination', 'p-radio-button-wrapper', 'p-select-wrapper', 'p-spinner', 'p-text', 'p-text-field-wrapper', 'p-text-list', 'p-text-list-item', 'p-textarea-wrapper'] as const;
+export type TagName = typeof TAG_NAMES[number];
+
+// TODO: replace with generic in TS4.1: https://stackoverflow.com/questions/57807009/typescript-generic-to-turn-underscore-object-to-camel-case
+export type TagNameCamelCase = 'pButton' | 'pButtonPure' | 'pCheckboxWrapper' | 'pContentWrapper' | 'pDivider' | 'pFieldsetWrapper' | 'pFlex' | 'pFlexItem' | 'pGrid' | 'pGridItem' | 'pHeadline' | 'pIcon' | 'pLink' | 'pLinkPure' | 'pLinkSocial' | 'pMarque' | 'pModal' | 'pPagination' | 'pRadioButtonWrapper' | 'pSelectWrapper' | 'pSpinner' | 'pText' | 'pTextFieldWrapper' | 'pTextList' | 'pTextListItem' | 'pTextareaWrapper';
