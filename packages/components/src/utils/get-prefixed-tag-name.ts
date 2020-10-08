d3bd66d0f9d5d0626d22d88f602cff98c2e31dcf
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-08 08:54:26 +0200

    feat(components): improve typing of getPrefixedTagNames, add getAllPrefixedTagNames | bh | #775

--- packages/components/src/utils/get-prefixed-tag-name.ts
+++ packages/components/src/utils/get-prefixed-tag-name.ts
@@ -1,8 +1,13 @@
 import { camelCase } from 'change-case';
+import { TagName, TagNameCamelCase, TAG_NAMES } from '../tags';
 
-export const getPrefixedTagNames = (element: HTMLElement, tagNames: string[]): { [tagName: string]: string } => {
+type PrefixedTagNames = { [key in TagNameCamelCase]: string };
+
+const prefixRegex = /^(.*-)p-(.*)$/;
+
+export const getPrefixedTagNames = (element: HTMLElement, tagNames: TagName[]): Partial<PrefixedTagNames> => {
   const lowerCaseTagName = element.tagName.toLowerCase();
-  const [, prefix = ''] = new RegExp(/^(.*-)p-(.*)$/).exec(lowerCaseTagName) || [];
+  const [, prefix = ''] = new RegExp(prefixRegex).exec(lowerCaseTagName) || [];
   return tagNames.reduce(
     (tagNameMap, tagName) => ({
       ...tagNameMap,
@@ -10,4 +15,11 @@ export const getPrefixedTagNames = (element: HTMLElement, tagNames: string[]): {
     }),
     {}
   );
+};
+
+export const getAllPrefixedTagNames = (host: HTMLElement): PrefixedTagNames => {
+  const [, prefix = ''] = host.tagName.toLowerCase().match(prefixRegex) ?? [];
+  const tagNames: PrefixedTagNames = {} as PrefixedTagNames;
+  Object.values({ ...TAG_NAMES }).forEach((tag) => (tagNames[tag as TagName] = prefix + tag));
+  return tagNames;
 };
