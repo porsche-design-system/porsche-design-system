b956877cefb2f94255523a4acad22dae1a4cc3db
denyo <denyo@users.noreply.github.com>
2020-10-09 06:37:06 +0000

    Automated deployment of Porsche Design System - Storefront (issue/775): dd625a9cfb2346962f3fcd90699f6a491c907565

--- issue/775/service-worker.js
+++ issue/775/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.605fdd64d1b824df6b0a13e19bb3317f.js"
+  "precache-manifest.417fc93685ffcc955756c15c061fde02.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@porsche-design-system/storefront"});
