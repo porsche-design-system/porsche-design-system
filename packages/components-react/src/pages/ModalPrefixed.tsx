9509b3369f2c5f9d8c1708e4b65d304b5627124d
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-12 12:01:46 +0200

    test(components-react): add vrt test for prefixed modal | bh | #775

---
+++ packages/components-react/src/pages/ModalPrefixed.tsx
@@ -0,0 +1,33 @@
+import { getPrefixedComponents } from '@porsche-design-system/components-react/prefixed-components';
+import React, { useEffect } from 'react';
+
+const { PButton: PrefixedButton, PModal: PrefixedModal, PModalFooter: PrefixedModalFooter } = getPrefixedComponents({
+  prefix: 'my-prefix'
+});
+
+export const ModalPrefixedPage = (): JSX.Element => {
+  useEffect(() => {
+    document.body.style.height = '500px';
+  }, []);
+
+  const style = `
+    .playground {
+      height: 500px;
+    }
+  `;
+
+  return (
+    <>
+      <style children={style} />
+      <div className="playground light" title="should show prefixed modal on light background">
+        <PrefixedModal heading="Some Heading" open>
+          Some Content
+          <PrefixedModalFooter>
+            <PrefixedButton>Confirm</PrefixedButton>
+            <PrefixedButton variant="tertiary">Cancel</PrefixedButton>
+          </PrefixedModalFooter>
+        </PrefixedModal>
+      </div>
+    </>
+  );
+};
