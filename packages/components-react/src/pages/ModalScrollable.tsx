81aadf6a2f9c8fff1cae1f253e6004fad2cc3345
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-12 11:33:29 +0200

    test(components-react): add vrt tests for modal | bh | #775

---
+++ packages/components-react/src/pages/ModalScrollable.tsx
@@ -0,0 +1,45 @@
+import {
+  PModal as Modal,
+  PButton as Button,
+  PModalFooter as ModalFooter
+} from '@porsche-design-system/components-react';
+import React, { useEffect } from 'react';
+
+export const ModalScrollablePage = (): JSX.Element => {
+  useEffect(() => {
+    document.body.style.height = '500px';
+  }, []);
+
+  const style = `
+    .playground {
+      height: 500px;
+    }
+  `;
+
+  return (
+    <>
+      <style children={style} />
+      <div className="playground light" title="should show scrollable modal on light background">
+        <Modal heading="Some Heading with a very long title across multiple lines" open>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+          clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+          <br />
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+          clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+          <ModalFooter>
+            <Button>Confirm</Button>
+            <Button variant="tertiary">Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    </>
+  );
+};
