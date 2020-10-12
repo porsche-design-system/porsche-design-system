6381729474e9db4bd93dbd45a17bebc296c34f5d
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-12 11:55:46 +0200

    feat(components): add :host style to modal-footer | bh | #775

--- packages/components/src/components/content/modal/modal-footer/modal-footer.tsx
+++ packages/components/src/components/content/modal/modal-footer/modal-footer.tsx
@@ -1,7 +1,9 @@
 import { Component, Element, h, Host, JSX, Prop } from '@stencil/core';
 
 @Component({
-  tag: 'p-modal-footer'
+  tag: 'p-modal-footer',
+  styles: ':host { display: block; }',
+  shadow: true
 })
 export class ModalFooter {
   @Element() public host!: HTMLElement;
