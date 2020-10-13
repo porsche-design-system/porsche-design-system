bc3fb3fb369d6ca5a337e0c2251d4db1ab364221
mhaegeleMHP <mhaegeleMHP@users.noreply.github.com>
2020-10-13 10:30:39 +0000

    Automated deployment of Porsche Design System - Storefront (master): e300ff458a8e9c2d4162b16a8b680f372cfb7b01

--- latest/service-worker.js
+++ latest/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3b54d0d783d9d078a4d194b34c096352.js"
+  "precache-manifest.0812522167dc096fac33156dcef7766a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@porsche-design-system/storefront"});
