99ad78fb94b5f1e077eba51857c46790a6a6206f
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-13 16:07:13 +0200

    test(storefront): simplify vrt tests, update vrt fixtures | bh | #775

--- packages/storefront/tests/vrt/specs/story.vrt.ts
+++ packages/storefront/tests/vrt/specs/story.vrt.ts
@@ -1,14 +1,8 @@
-import { VisualRegressionTester } from '@porsche-design-system/visual-regression-tester';
 import { getVisualRegressionTester } from '../helpers/setup';
 
 describe('Story', () => {
-  let vrt: VisualRegressionTester;
-
-  beforeAll(() => {
-    vrt = getVisualRegressionTester();
-  });
-
   it('should have no visual regression', async () => {
+    const vrt = getVisualRegressionTester();
     expect(
       await vrt.test(
         'story',
@@ -16,7 +10,7 @@ describe('Story', () => {
           await vrt.goTo('/#/components/pagination#code');
           await vrt.click('.playground:nth-of-type(1) > p-tabs-bar > button:nth-of-type(2)');
         },
-        { elementSelector: '#app > .content > .main > .router-view' }
+        { elementSelector: '#app > .main > .router-view' }
       )
     ).toBeFalsy();
   });
