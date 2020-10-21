bacada15b09df4f4dcd1d358f6a0c66850b9aac3
o-hook <o-hook@users.noreply.github.com>
2020-10-21 11:29:17 +0000

    Automated deployment of Porsche Design System - Storefront (issue/862): 48db40da7ad654e9408366a6a2e66e29a647dd69

--- issue/862/service-worker.js
+++ issue/862/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.d3220bf7a751a1488f0991744b6a92ed.js"
+  "precache-manifest.39437d93cb873e567c7524685881d44e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@porsche-design-system/storefront"});
