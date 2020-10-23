f32ab3658105365eb62aa0b4aa92bc3a81f1b9fb
Matthias Haegele <matthias.haegele@mhp.com>
2020-10-23 12:52:23 +0200

    fix(components): build order | mh | #987

--- packages/components/scripts/generateTypeBundle.ts
+++ packages/components/scripts/generateTypeBundle.ts
@@ -6,18 +6,6 @@ type Framework = 'react' | 'angular';
 const BUNDLE_TYPE_FILE_NAME = 'bundle.d.ts';
 const rootDirectory = path.resolve(__dirname, '..');
 
-// dts-bundler doesnt bundle the import of HTMLStencilElement. We have to export it beforehand to ensure it gets included in the bundle.d.ts file
-const addMissingHTMLStencilElementExport = () => {
-  const appendData = "export { HTMLStencilElement } from './stencil-public-runtime';";
-  const filePath = path.resolve(rootDirectory, `dist/types/components.d.ts`);
-  const fileContent = fs.readFileSync(filePath).toString();
-
-  if (!fileContent.includes(appendData)) {
-    fs.writeFileSync(filePath, fileContent + appendData);
-    console.log('Added export ' + appendData + '\n\n')
-  }
-};
-
 const getFilePathDest = (framework: Framework): string => {
   return path.resolve(
     rootDirectory,
@@ -67,6 +55,5 @@ const updateGeneratedWrapper = (framework: Framework) => {
   }
 };
 
-addMissingHTMLStencilElementExport();
 updateGeneratedWrapper('angular');
 updateGeneratedWrapper('react');
