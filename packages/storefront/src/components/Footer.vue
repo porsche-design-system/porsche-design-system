b5e3ab7ab8730ffebe0cca4550a30380eb01b735
Stephan Schröter <stephan.schroeter@mhp.com>
2020-10-28 12:25:00 +0100

    fix(components/storefront): apply correct focus state | sas | #1001

--- packages/storefront/src/components/Footer.vue
+++ packages/storefront/src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <p-text size="x-small" align="center">
       <span>© 2020 Dr. Ing. h.c. F. Porsche AG.</span>
-      <br />
+      <br/>
       <a href="https://www.porsche.com/international/legal-notice/">Legal notice</a>
       <span> / </span>
       <a href="https://www.porsche.com/international/legal-notice/">Imprint</a>
@@ -13,9 +13,31 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import Component from 'vue-class-component';
+import Vue from 'vue';
+import Component from 'vue-class-component';
 
-  @Component
-  export default class Footer extends Vue {}
+@Component
+export default class Footer extends Vue {
+}
 </script>
+
+<style scoped lang="scss">
+@import '~@porsche-design-system/utilities/scss';
+@import '../styles/internal.variables';
+
+a {
+  outline: none;
+  text-decoration: underline;
+  color: $p-color-default;
+  transition: color $p-animation-hover-duration $p-animation-hover-bezier;
+
+  &:hover {
+    color: $p-color-state-hover;
+  }
+
+  &:focus {
+    outline-offset: 1px;
+    outline: $p-color-state-focus solid 1px;
+  }
+}
+</style>
