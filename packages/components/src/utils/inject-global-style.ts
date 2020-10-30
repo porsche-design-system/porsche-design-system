d638e5d8532b2cc57633546c8cd199fe8c84e053
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-30 09:07:02 +0100

    fix(components): replace getFontFaceCss partial with dynamic FONT_FACE_CDN_URL | bh | #1004

--- packages/components/src/utils/inject-global-style.ts
+++ packages/components/src/utils/inject-global-style.ts
@@ -1,4 +1,4 @@
-import { getFontFaceCSS } from '@porsche-design-system/partials';
+import { FONT_FACE_CDN_URL } from '@porsche-design-system/utilities';
 
 export const injectGlobalStyle = (): void => {
   if (typeof document === 'undefined') {
@@ -6,9 +6,7 @@ export const injectGlobalStyle = (): void => {
   }
 
   const styleUrl =
-    ROLLUP_REPLACE_IS_STAGING === 'production'
-      ? getFontFaceCSS({ withoutTags: true })
-      : 'http://localhost:3001/styles/font-face.min.css';
+    ROLLUP_REPLACE_IS_STAGING === 'production' ? FONT_FACE_CDN_URL : 'http://localhost:3001/styles/font-face.min.css';
 
   if (!document.querySelector(`link[href="${styleUrl}"]`)) {
     const link = document.createElement('link');
@@ -18,7 +16,7 @@ export const injectGlobalStyle = (): void => {
 
     document.getElementsByTagName('head')[0].appendChild(link);
 
-    const partial = getFontFaceCSS();
+    const partial = `<link rel=stylesheet href=${styleUrl}>`;
     console.warn(`The Porsche Design System had to inject our font-face.css file into your head. Please preload it with '${partial}' in the head of your 'index.html'.
 If you are already preloading font-face.css please update your snippet to '${partial}'.
 You find further documentation and examples on how to implement preloading at https://designsystem.porsche.com/v2/#/performance/loading-behaviour`);
