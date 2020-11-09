1a0dc45be5a7fad1f8072937a5d6b7c3d0d1077c
Stephan Schröter <stephan.schroeter@mhp.com>
2020-11-09 17:04:23 +0100

    test(components-js): add e2e test for focus | sas | #1001

--- packages/components-js/tests/e2e/specs/text-list.e2e.ts
+++ packages/components-js/tests/e2e/specs/text-list.e2e.ts
@@ -1,11 +1,11 @@
 import {
   getBrowser,
-  getElementStyle,
-  getElementStyleOnFocus,
-  selectNode,
-  setContentWithDesignSystem
+  getStyleOnFocus,
+  selectNode, setAttribute,
+  setContentWithDesignSystem, waitForStencilLifecycle
 } from '../helpers';
 import { Page } from 'puppeteer';
+import { expectedStyleOnFocus } from '../constants';
 
 describe('text', () => {
   let page: Page;
@@ -13,23 +13,31 @@ describe('text', () => {
   beforeEach(async () => (page = await getBrowser().newPage()));
   afterEach(async () => await page.close());
 
-  const getTextListLink = () => selectNode(page, 'p-text-list a');
+  const getHost = () => selectNode(page, 'p-text-list');
+  const getLink = () => selectNode(page, 'p-text-list a');
 
   describe('focus state', () => {
     it('should show outline of slotted <a> when it is focused', async () => {
       await setContentWithDesignSystem(
         page,
         `
-        <p-text-list list-type="ordered" order-type="alphabetically">
+        <p-text-list>
           <p-text-list-item>The quick brown fox jumps over the lazy dog</p-text-list-item>
           <p-text-list-item>The quick <a href="#">brown fox</a> jumps over the lazy dog</p-text-list-item>
         </p-text-list>`
       );
 
-      const link = await getTextListLink();
-      const linkOutlineInitial = await getElementStyle(link, 'outline');
+      const host = await getHost();
+      const link = await getLink();
 
-      expect(await getElementStyleOnFocus(link, 'outline')).not.toBe(linkOutlineInitial, 'link should get focus style');
+      expect(await getStyleOnFocus(link)).toBe(expectedStyleOnFocus());
+
+      await setAttribute(host, 'theme', 'dark');
+      await waitForStencilLifecycle(page);
+
+      await page.waitForTimeout(500); // we need to wait for inherited color transition
+
+      expect(await getStyleOnFocus(link)).toBe(expectedStyleOnFocus({theme: 'dark'}));
     });
   });
 });
