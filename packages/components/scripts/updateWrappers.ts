c5b17ae783f48af5967bfc3e568d253ab5b3d296
Bernd Hacker <hacker.bernd@gmail.com>
2020-11-23 16:45:26 +0100

    feat(components): rename generateTypeBundle script to updateWrappers, strip ROLLUP_REPLACE_IS_STAGING from bundle.d.ts, improve console output | bh | #1002

--- packages/components/scripts/updateWrappers.ts
+++ packages/components/scripts/updateWrappers.ts
@@ -4,28 +4,31 @@ import * as path from 'path';
 type Framework = 'react' | 'angular';
 
 const BUNDLE_TYPE_FILE_NAME = 'bundle.d.ts';
+const WRAPPER_PROJECT_PATH = 'projects/components-wrapper/src/lib';
 const rootDirectory = path.resolve(__dirname, '..');
 
 const getFilePathDest = (framework: Framework): string => {
-  return path.resolve(
-    rootDirectory,
-    `../components-${framework}/projects/components-wrapper/src/lib/${BUNDLE_TYPE_FILE_NAME}`
-  );
+  return path.resolve(rootDirectory, `../components-${framework}/${WRAPPER_PROJECT_PATH}/${BUNDLE_TYPE_FILE_NAME}`);
 };
 
 // We have to deliver the type file to the wrapper packages, because we do not provide the components package
 const copyTypesToWrapper = (framework: Framework): void => {
-  const filePathSource = path.resolve(rootDirectory, `dist/types/${BUNDLE_TYPE_FILE_NAME}`);
+  const filePath = `dist/types/${BUNDLE_TYPE_FILE_NAME}`;
+  const filePathSource = path.resolve(rootDirectory, filePath);
   const filePathDest = getFilePathDest(framework);
 
-  fs.copyFileSync(filePathSource, filePathDest);
+  const fileContent = fs.readFileSync(filePathSource, 'utf8').toString();
 
-  console.log(`File "${filePathSource}" copied to "${filePathDest}"`);
+  // remove global declaration of `const ROLLUP_REPLACE_IS_STAGING: string;`
+  const result = fileContent.replace(/declare global {\n\tconst ROLLUP_REPLACE_IS_STAGING: string;\n}\n/, '');
+  fs.writeFileSync(filePathDest, result);
+
+  console.log(`Copied '${filePath}' –> 'components-${framework}/${WRAPPER_PROJECT_PATH}/${BUNDLE_TYPE_FILE_NAME}'`);
 };
 
 // To ensure the from stencil generated wrapper use the right imports, we have to rename them.
 const updateGeneratedWrapper = (framework: Framework): void => {
-  console.log(`Update generated wrapper in "components-${framework}":`);
+  console.log(`Updating generated wrapper in "components-${framework}"`);
   copyTypesToWrapper(framework);
 
   let targetFileName = '';
@@ -37,11 +40,14 @@ const updateGeneratedWrapper = (framework: Framework): void => {
 
   const filePath = path.normalize(`../components-${framework}/projects/components-wrapper/src/lib/${targetFileName}`);
   const fileContent = fs.readFileSync(filePath, 'utf8').toString();
-  const replaceValue = `./${BUNDLE_TYPE_FILE_NAME.substr(0, BUNDLE_TYPE_FILE_NAME.indexOf('.'))}`;
-  const result = fileContent.replace(/@porsche-design-system\/components/g, replaceValue);
+
+  // replace imports from '@porsche-design-system\/components' with './bundle';
+  const replaceValue = `'./${BUNDLE_TYPE_FILE_NAME.substr(0, BUNDLE_TYPE_FILE_NAME.indexOf('.'))}'`;
+  const result = fileContent.replace(/'@porsche-design-system\/components'/g, replaceValue);
+
   fs.writeFileSync(filePath, result);
 
-  console.log(`Updated import of "${targetFileName}" from '@porsche-design-system/components' to '${replaceValue}'`);
+  console.log(`Updated import of "${targetFileName}": '@porsche-design-system/components' –> ${replaceValue}`);
 
   // React uses the alias JSX for LocalJSX, so we have to provide it
   if (framework === 'react') {
