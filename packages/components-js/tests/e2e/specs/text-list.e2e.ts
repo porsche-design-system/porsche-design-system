528eb5c587635e66059ea72599342075bb9aaadf
Matthias Haegele <matthias.haegele@mhp.com>
2020-11-25 15:45:08 +0100

    test(components-js): add focus tests | mh / sas | #975

--- packages/components-js/tests/e2e/specs/text-list.e2e.ts
+++ packages/components-js/tests/e2e/specs/text-list.e2e.ts
@@ -2,7 +2,7 @@ import {
   getBrowser,
   getStyleOnFocus,
   selectNode, setAttribute, expectedStyleOnFocus,
-  setContentWithDesignSystem, waitForInheritedCSSTransition, waitForStencilLifecycle
+  setContentWithDesignSystem, waitForInheritedCSSTransition, waitForStencilLifecycle, getOutlineStyle
 } from '../helpers';
 import { Page } from 'puppeteer';
 
@@ -12,19 +12,45 @@ describe('text', () => {
   beforeEach(async () => (page = await getBrowser().newPage()));
   afterEach(async () => await page.close());
 
+  const initTextList = (): Promise<void> => {
+   return setContentWithDesignSystem(
+      page,
+      `
+        <p-text-list>
+          <p-text-list-item>The quick brown fox jumps over the lazy dog</p-text-list-item>
+          <p-text-list-item>The quick <a onclick="return false;" href="#">brown fox</a> jumps over the lazy dog</p-text-list-item>
+        </p-text-list>`
+    );
+  };
+
   const getHost = () => selectNode(page, 'p-text-list');
   const getLink = () => selectNode(page, 'p-text-list a');
 
   describe('focus state', () => {
+    it('should be shown by keyboard navigation only for slotted <a>', async () => {
+      await initTextList();
+
+      const link = await getLink();
+      const hidden = expectedStyleOnFocus({color: 'transparent'});
+      const visible = expectedStyleOnFocus({color: 'hover'});
+
+      expect(await getOutlineStyle(link)).toBe(hidden);
+
+      await link.click();
+      await waitForInheritedCSSTransition(page);
+
+      expect(await getOutlineStyle(link)).toBe(hidden);
+
+      await page.keyboard.down('ShiftLeft');
+      await page.keyboard.press('Tab');
+      await page.keyboard.up('ShiftLeft');
+      await page.keyboard.press('Tab');
+
+      expect(await getOutlineStyle(link)).toBe(visible);
+    });
+
     it('should show outline of slotted <a> when it is focused', async () => {
-      await setContentWithDesignSystem(
-        page,
-        `
-        <p-text-list>
-          <p-text-list-item>The quick brown fox jumps over the lazy dog</p-text-list-item>
-          <p-text-list-item>The quick <a href="#">brown fox</a> jumps over the lazy dog</p-text-list-item>
-        </p-text-list>`
-      );
+      await initTextList();
 
       const host = await getHost();
       const link = await getLink();
