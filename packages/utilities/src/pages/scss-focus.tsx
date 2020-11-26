030b9c258bfa862cbbe3cae4a621e79d825d3011
Matthias Haegele <matthias.haegele@mhp.com>
2020-11-26 15:11:47 +0100

    test(utilities): refactor test pages / fixtures | mh / sas | #975

--- packages/utilities/src/pages/scss-focus.tsx
+++ packages/utilities/src/pages/scss-focus.tsx
@@ -4,18 +4,24 @@ export const ScssFocus = (): JSX.Element => (
   <>
     <h2>Focus</h2>
     <div className="playground">
-      <button id="focusable-element-regular">Some label</button>
-    </div>
-    <div className="playground">
-      <button id="focusable-element-custom">Some label</button>
-    </div>
-    <div className="playground">
-      <button id="focusable-element-pseudo" style={{position: 'relative', outline: 'transparent'}}>
+      <button id="focusable-element-regular" className="focusable-element-regular">
         Some label
       </button>
     </div>
     <div className="playground">
-      <button id="focusable-element-custom-pseudo" style={{position: 'relative', outline: 'transparent'}}>
+      <button id="focusable-element-custom" className="focusable-element-custom">
+        Some label
+      </button>
+    </div>
+    <div className="playground">
+      <div className="focusable-element-pseudo">
+        <button id="focusable-element-pseudo">
+          Some label
+        </button>
+      </div>
+    </div>
+    <div className="playground">
+      <button id="focusable-element-custom-pseudo" className="focusable-element-custom-pseudo">
         Some label
       </button>
     </div>
