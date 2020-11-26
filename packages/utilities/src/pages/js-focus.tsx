b9628ec2a26aab11747af83ed31ec597f309f575
Matthias Haegele <matthias.haegele@mhp.com>
2020-11-26 15:11:47 +0100

    test(utilities): refactor test pages / fixtures | mh / sas | #975

--- packages/utilities/src/pages/js-focus.tsx
+++ packages/utilities/src/pages/js-focus.tsx
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
-import { focus } from '@porsche-design-system/utilities';
+import { color, focus } from '@porsche-design-system/utilities';
 
 const FocusButtonRegular = styled.button`
   ${focus()}
 `;
 const FocusButtonCustom = styled.button`
-  ${focus({color: 'deeppink', offset: '2px'})}
+  ${focus({ color: 'deeppink', offset: '6px' })}
 `;
-const FocusButtonPseudo = styled.button`
-  ${focus({pseudo: '::before'})}
+const StyledDiv = styled.div`
+  position: relative;
+  padding: 1rem;
+
+  button {
+    ${focus({ pseudo: '::before' })}
+  }
 `;
 const FocusButtonCustomPseudo = styled.button`
-  ${focus({color: 'deeppink', offset: '2px', pseudo: '::after'})}
+  position: relative;
+
+  ${focus({ color: 'deeppink', offset: '6px', pseudo: '::after' })}
 `;
 
 export const JsFocus = (): JSX.Element => {
@@ -25,15 +32,12 @@ export const JsFocus = (): JSX.Element => {
         <FocusButtonCustom id="focusable-element-custom">Some label</FocusButtonCustom>
       </div>
       <div className="playground">
-        <FocusButtonPseudo id="focusable-element-pseudo" style={{position: 'relative', outline: 'transparent'}}>
-          Some label
-        </FocusButtonPseudo>
+        <StyledDiv>
+          <button id="focusable-element-pseudo">Some label</button>
+        </StyledDiv>
       </div>
       <div className="playground">
-        <FocusButtonCustomPseudo id="focusable-element-custom-pseudo"
-                                 style={{position: 'relative', outline: 'transparent'}}>
-          Some label
-        </FocusButtonCustomPseudo>
+        <FocusButtonCustomPseudo id="focusable-element-custom-pseudo">Some label</FocusButtonCustomPseudo>
       </div>
     </>
   );
