fa5bafd60f4940c2af5bffe325be0a9be0e71b2b
Bernd Hacker <hacker.bernd@gmail.com>
2020-11-26 15:36:53 +0100

    feat(components): enhance componentsReady, enable lifecycleDOMEvents again and cleanup stencil.config.ts | bh | #1035

--- packages/components/src/components-ready.ts
+++ packages/components/src/components-ready.ts
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
 
-let hasRegisteredEventListeners = false;
+import { HostElement } from '@stencil/core/internal';
+
+let isInitialized = false;
 let taskCount = 0;
 let timeout: number;
 let resolvePromise: () => void;
@@ -41,95 +43,116 @@ const decreaseCount = (): void => {
 
 export const componentsReady = (): Promise<void> => {
   console.log('componentsReady');
-  registerStencilEventListeners();
+  initialize();
   checkPromiseResolve();
   return onLoadedPromise;
 };
 
 // ------------------
 
-const waitFrame = (): Promise<number> =>
-  new Promise((resolve) => {
-    requestAnimationFrame(resolve);
-  });
+const waitFrame = async () => requestAnimationFrame;
 
-const allReady = (): Promise<any[] | void> => {
-  console.log('allReady?');
-  const promises: Promise<any>[] = [];
-  const waitForDidLoad = (elm: Element): void => {
-    if (elm != null && elm.nodeType === 1) {
-      for (let i = 0; i < elm.children.length; i++) {
-        console.log('allReady loop', i);
-        const childElm = elm.children[i];
-        if (childElm.tagName.includes('P-') && typeof (childElm as any).componentOnReady === 'function') {
-          promises.push((childElm as any).componentOnReady());
-          console.log('added promise');
-        }
-        waitForDidLoad(childElm);
+const promises: Promise<any>[] = [];
+const waitForDidLoad = (elm: Element): void => {
+  // Node.ELEMENT_NODE: An Element node like <p> or <div>
+  if (elm?.nodeType === 1) {
+    (Array.from(elm.children) as HostElement[]).forEach((childElm) => {
+      if (childElm.tagName.includes('P-') && typeof childElm.componentOnReady === 'function') {
+        promises.push(childElm.componentOnReady());
       }
-    }
-  };
-
-  // waitForDidLoad(promises, window.document.documentElement);
-  waitForDidLoad(document.getElementById('app'));
-
-  return Promise.all(promises).catch((e) => console.error(e));
-};
-
-let hasStencilLoaded: Promise<void>;
-const stencilLoaded = (): Promise<void> => {
-  console.log('stencilLoaded?');
-  hasStencilLoaded = allReady()
-    .then(waitFrame)
-    .then(allReady)
-    .then(() => {
-      console.log('stencilLoaded /// setting stencilAppLoaded = true');
-      (window as any).stencilAppLoaded = true;
+      waitForDidLoad(childElm);
     });
 
-  window.addEventListener('appload', (e) => {
-    console.log(e.type, taskCount);
-    stencilReady();
-  });
-
-  return hasStencilLoaded;
+    // for (let i = 0; i < elm.children.length; i++) {
+    //   console.log('allReady loop', i);
+    //   const childElm = elm.children[i];
+    //   if (childElm.tagName.includes('P-') && typeof (childElm as any).componentOnReady === 'function') {
+    //     promises.push((childElm as any).componentOnReady());
+    //     console.log('added promise');
+    //   }
+    //   waitForDidLoad(childElm);
+    // }
+  }
 };
 
-const stencilReady = (): Promise<void> => {
+const allReady = async (): Promise<void> => {
+  console.log('allReady?', promises.length);
+
+  // waitForDidLoad(promises, window.document.documentElement);
+  // TODO: add ability to pass any element to reduce recursive search radius
+  waitForDidLoad(document.getElementById('app'));
+
+  await Promise.all(promises).catch(console.error);
+  console.log('––> allReady', promises.length);
+  promises.length = 0; // clear array of promises for next cycle
+};
+
+let hasStencilLoaded = false;
+const stencilLoaded = async (): Promise<void> => {
+  console.log('stencilLoaded?');
+  await allReady();
+  await waitFrame();
+  await allReady();
+  console.log('stencilLoaded /// setting stencilAppLoaded = true');
+  (window as any).stencilAppLoaded = true;
+
+  registerStencilEventListeners();
+
+  hasStencilLoaded = true;
+  console.log('––> stencilLoaded');
+};
+
+const stencilReady = async (): Promise<void> => {
   console.log('stencilReady?');
-  return hasStencilLoaded.then(allReady).then(() => {
+  if (hasStencilLoaded) {
+    await allReady();
     console.log('––> stencilReady');
-  });
+  }
 };
 
 const registerStencilEventListeners = (): void => {
-  if (!hasRegisteredEventListeners) {
-    ['componentWillLoad', 'componentDidLoad', 'componentWillUpdate', 'componentDidUpdate'].forEach((event) => {
-      const handler = event.includes('Will') ? increaseCount : decreaseCount;
-      window.addEventListener(
-        `stencil_${event}`,
-        // for production
-        // handler
-        // for debugging
-        (e) => {
-          handler();
-          console.log((e.composedPath()[0] as any).tagName.toLowerCase(), e.type, taskCount);
-        }
-      );
-    });
-
-    console.log('document.readyState', document.readyState);
-    if (document.readyState === 'complete') {
-      stencilLoaded();
-    } else {
-      document.addEventListener('readystatechange', (e) => {
-        console.log('––> readystatechange', document.readyState);
-        if ((e.target as Document).readyState === 'complete') {
-          stencilLoaded();
-        }
-      });
+  // register listeners for stencil's appload event
+  window.addEventListener('appload', (e: CustomEvent) => {
+    if (isEventInStencilNamespace(e)) {
+      console.log(e.type, e.detail.namespace);
+      stencilReady();
     }
+  });
 
-    hasRegisteredEventListeners = true;
+  // register listeners for stencil's lifecycleDOMEvents
+  ['componentWillLoad', 'componentDidLoad', 'componentWillUpdate', 'componentDidUpdate'].forEach((event) => {
+    const handler = event.includes('Will') ? increaseCount : decreaseCount;
+    window.addEventListener(`stencil_${event}`, (e: CustomEvent) => {
+      if (isEventInStencilNamespace(e)) {
+        handler();
+        console.log((e.composedPath()[0] as any).tagName.toLowerCase(), e.type, taskCount);
+      }
+    });
+  });
+};
+
+const initialize = (): void => {
+  if (!isInitialized) {
+    checkDocumentReadyStateAndStencil();
+
+    const eventName = 'readystatechange';
+    const eventHandler = () => {
+      if (checkDocumentReadyStateAndStencil()) {
+        document.removeEventListener(eventName, eventHandler);
+      }
+    };
+    document.addEventListener(eventName, eventHandler);
+
+    isInitialized = true;
+  }
+};
+
+const isEventInStencilNamespace = (e: CustomEvent): boolean => e.detail.namespace === 'porsche-design-system';
+
+const checkDocumentReadyStateAndStencil = (): boolean => {
+  console.log('document.readyState', document.readyState);
+  if (document.readyState === 'complete') {
+    stencilLoaded();
+    return true;
   }
 };
