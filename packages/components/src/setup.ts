c657adf9e5d77d151cfbe4f2fe674ed77cff642e
Bernd Hacker <hacker.bernd@gmail.com>
2020-11-30 12:25:24 +0100

    feat(components): enhance componentsReady | bh / sas | #1035

--- packages/components/src/setup.ts
+++ packages/components/src/setup.ts
@@ -1,11 +1,9 @@
 import { initBlurOnFocus } from './utils/blur-on-focus';
 import { injectGlobalStyle } from './utils/inject-global-style';
 import { trackLoader } from './utils';
-// import { componentsReady } from './components-ready';
 
 export default (): void => {
   trackLoader();
   injectGlobalStyle();
   initBlurOnFocus();
-  // (window as any).componentsReady = componentsReady;
 };
