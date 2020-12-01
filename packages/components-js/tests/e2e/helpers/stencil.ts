e9545609f4afb0d546a99ad57c6a1190c4e3c958
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-01 11:02:26 +0100

    test(components-js): add timeout to waitForStencilLifecycle e2e helper | bh | #1035

--- packages/components-js/tests/e2e/helpers/stencil.ts
+++ packages/components-js/tests/e2e/helpers/stencil.ts
@@ -1,6 +1,7 @@
 import { Page } from 'puppeteer';
 
 export const waitForStencilLifecycle = async (page: Page): Promise<void> => {
+  await page.waitForTimeout(20);
   await page.evaluate(
     async (): Promise<void> => {
       await (window as any).porscheDesignSystem.componentsReady();
