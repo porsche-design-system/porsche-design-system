59eda47415e272c2ca62f0c74aaea211c6ad79f3
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-02 16:30:57 +0100

    test(components-js): improve flaky tabs and radio button wrapper e2e tests | bh / mh | #1035

--- packages/components-js/tests/e2e/specs/radio-button-wrapper.e2e.ts
+++ packages/components-js/tests/e2e/specs/radio-button-wrapper.e2e.ts
@@ -1,13 +1,20 @@
 import {
   getActiveElementId,
   getActiveElementTagName,
-  getAttribute, getStyleOnFocus,
+  getAttribute,
+  getStyleOnFocus,
   getBrowser,
   getCssClasses,
   getProperty,
-  selectNode, setAttribute,
-  setContentWithDesignSystem, expectedStyleOnFocus,
-  waitForStencilLifecycle, waitForInheritedCSSTransition, getOutlineStyle, getBoxShadowStyle
+  selectNode,
+  setAttribute,
+  setContentWithDesignSystem,
+  expectedStyleOnFocus,
+  waitForStencilLifecycle,
+  waitForInheritedCSSTransition,
+  getOutlineStyle,
+  getBoxShadowStyle,
+  enableBrowserLogging,
 } from '../helpers';
 import { Page } from 'puppeteer';
 import { FormState } from '@porsche-design-system/components/src/types';
@@ -27,14 +34,32 @@ describe('radio-button-wrapper', () => {
   const getRadioButtonLabelLink = () => selectNode(page, 'p-radio-button-wrapper [slot="label"] a');
   const getRadioButtonMessageLink = () => selectNode(page, 'p-radio-button-wrapper [slot="message"] a');
 
-  const initRadioButton = ({ useSlottedLabel, useSlottedMessage, state }: { useSlottedLabel?: boolean; useSlottedMessage?: boolean; state?: FormState; } = { useSlottedLabel: false, useSlottedMessage: false, state: 'none' }): Promise<void> => {
+  const initRadioButton = (
+    {
+      useSlottedLabel,
+      useSlottedMessage,
+      state,
+    }: { useSlottedLabel?: boolean; useSlottedMessage?: boolean; state?: FormState } = {
+      useSlottedLabel: false,
+      useSlottedMessage: false,
+      state: 'none',
+    }
+  ): Promise<void> => {
     return setContentWithDesignSystem(
       page,
       `
         <p-radio-button-wrapper state="${state}">
-          ${useSlottedLabel ? '<span slot="label">Some label with a <a href="#" onclick="return false;">link</a>.</span>' : ''}
+          ${
+            useSlottedLabel
+              ? '<span slot="label">Some label with a <a href="#" onclick="return false;">link</a>.</span>'
+              : ''
+          }
           <input type="radio" />
-          ${useSlottedMessage ? '<span slot="message">Some message with a <a href="#" onclick="return false;">link</a>.</span>' : ''}
+          ${
+            useSlottedMessage
+              ? '<span slot="message">Some message with a <a href="#" onclick="return false;">link</a>.</span>'
+              : ''
+          }
         </p-radio-button-wrapper>`
     );
   };
@@ -97,6 +122,7 @@ describe('radio-button-wrapper', () => {
   });
 
   it('should add/remove message text and update aria-label attribute with message if state changes programmatically', async () => {
+    // enableBrowserLogging(page);
     await setContentWithDesignSystem(
       page,
       `
@@ -109,28 +135,34 @@ describe('radio-button-wrapper', () => {
     const radioComponent = await getRadioButtonHost();
     const input = await selectNode(page, 'input');
 
-    expect(await getRadioButtonMessage()).toBeNull();
-    await radioComponent.evaluate((el) => el.setAttribute('state', 'error'));
-    await radioComponent.evaluate((el) => el.setAttribute('message', 'Some error message'));
+    expect(await getRadioButtonMessage()).toBeNull('initially');
+    await radioComponent.evaluate((el) => {
+      el.setAttribute('state', 'error');
+      el.setAttribute('message', 'Some error message');
+    });
     await waitForStencilLifecycle(page);
 
     expect(await getRadioButtonMessage()).toBeDefined();
     expect(await getAttribute(await getRadioButtonMessage(), 'role')).toEqual('alert');
     expect(await getProperty(input, 'ariaLabel')).toEqual('Some label. Some error message');
 
-    await radioComponent.evaluate((el) => el.setAttribute('state', 'success'));
-    await radioComponent.evaluate((el) => el.setAttribute('message', 'Some success message'));
+    await radioComponent.evaluate((el) => {
+      el.setAttribute('state', 'success');
+      el.setAttribute('message', 'Some success message');
+    });
     await waitForStencilLifecycle(page);
 
     expect(await getRadioButtonMessage()).toBeDefined();
-    expect(await getAttribute(await getRadioButtonMessage(), 'role')).toBeNull();
+    expect(await getAttribute(await getRadioButtonMessage(), 'role')).toBeNull('when state = success');
     expect(await getProperty(input, 'ariaLabel')).toEqual('Some label. Some success message');
 
-    await radioComponent.evaluate((el) => el.setAttribute('state', 'none'));
-    await radioComponent.evaluate((el) => el.setAttribute('message', ''));
+    await radioComponent.evaluate((el) => {
+      el.setAttribute('state', 'none');
+      el.setAttribute('message', '');
+    });
     await waitForStencilLifecycle(page);
 
-    expect(await getRadioButtonMessage()).toBeNull();
+    expect(await getRadioButtonMessage()).toBeNull('when state = none');
     expect(await getProperty(input, 'ariaLabel')).toEqual('Some label');
   });
 
@@ -264,8 +296,8 @@ describe('radio-button-wrapper', () => {
       await initRadioButton();
 
       const input = await getRadioButtonRealInput();
-      const hidden = expectedStyleOnFocus({color: 'transparent', css: 'boxShadow', offset: '1px'});
-      const visible = expectedStyleOnFocus({color: 'neutral', css: 'boxShadow', offset: '1px'});
+      const hidden = expectedStyleOnFocus({ color: 'transparent', css: 'boxShadow', offset: '1px' });
+      const visible = expectedStyleOnFocus({ color: 'neutral', css: 'boxShadow', offset: '1px' });
 
       expect(await getBoxShadowStyle(input)).toBe(hidden);
 
@@ -282,12 +314,12 @@ describe('radio-button-wrapper', () => {
     });
 
     it('should be shown by keyboard navigation only for slotted <a>', async () => {
-      await initRadioButton({useSlottedLabel: true, useSlottedMessage: true, state: 'error'});
+      await initRadioButton({ useSlottedLabel: true, useSlottedMessage: true, state: 'error' });
 
       const labelLink = await getRadioButtonLabelLink();
       const messageLink = await getRadioButtonMessageLink();
-      const hidden = expectedStyleOnFocus({color: 'transparent', offset: '1px'});
-      const visible = expectedStyleOnFocus({color: 'hover', offset: '1px'});
+      const hidden = expectedStyleOnFocus({ color: 'transparent', offset: '1px' });
+      const visible = expectedStyleOnFocus({ color: 'hover', offset: '1px' });
 
       expect(await getOutlineStyle(labelLink)).toBe(hidden);
       expect(await getOutlineStyle(messageLink)).toBe(hidden);
@@ -323,32 +355,38 @@ describe('radio-button-wrapper', () => {
       const host = await getRadioButtonHost();
       const input = await getRadioButtonRealInput();
 
-      expect(await getStyleOnFocus(input, 'boxShadow')).toBe(expectedStyleOnFocus({color: 'neutral', css: 'boxShadow'}));
+      expect(await getStyleOnFocus(input, 'boxShadow')).toBe(
+        expectedStyleOnFocus({ color: 'neutral', css: 'boxShadow' })
+      );
 
       await setAttribute(host, 'state', 'success');
       await waitForStencilLifecycle(page);
-      expect(await getStyleOnFocus(input, 'boxShadow')).toBe(expectedStyleOnFocus({color: 'success', css: 'boxShadow'}));
+      expect(await getStyleOnFocus(input, 'boxShadow')).toBe(
+        expectedStyleOnFocus({ color: 'success', css: 'boxShadow' })
+      );
 
       await setAttribute(host, 'state', 'error');
       await waitForStencilLifecycle(page);
-      expect(await getStyleOnFocus(input, 'boxShadow')).toBe(expectedStyleOnFocus({color: 'error', css: 'boxShadow'}));
+      expect(await getStyleOnFocus(input, 'boxShadow')).toBe(
+        expectedStyleOnFocus({ color: 'error', css: 'boxShadow' })
+      );
     });
 
     it('should show outline of slotted <a> when it is focused', async () => {
-      await initRadioButton({useSlottedLabel: true, useSlottedMessage: true, state: 'error'});
+      await initRadioButton({ useSlottedLabel: true, useSlottedMessage: true, state: 'error' });
 
       const host = await getRadioButtonHost();
       const labelLink = await getRadioButtonLabelLink();
       const messageLink = await getRadioButtonMessageLink();
 
-      expect(await getStyleOnFocus(labelLink)).toBe(expectedStyleOnFocus({offset: '1px'}));
-      expect(await getStyleOnFocus(messageLink)).toBe(expectedStyleOnFocus({color: 'error', offset: '1px'}));
+      expect(await getStyleOnFocus(labelLink)).toBe(expectedStyleOnFocus({ offset: '1px' }));
+      expect(await getStyleOnFocus(messageLink)).toBe(expectedStyleOnFocus({ color: 'error', offset: '1px' }));
 
       await setAttribute(host, 'state', 'success');
       await waitForStencilLifecycle(page);
       await waitForInheritedCSSTransition(page);
 
-      expect(await getStyleOnFocus(messageLink)).toBe(expectedStyleOnFocus({color: 'success', offset: '1px'}));
+      expect(await getStyleOnFocus(messageLink)).toBe(expectedStyleOnFocus({ color: 'success', offset: '1px' }));
     });
   });
 });
