a99fc253510107ba03846856fc293391b90fad9c
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-02 16:30:57 +0100

    test(components-js): improve flaky tabs and radio button wrapper e2e tests | bh / mh | #1035

--- packages/components-js/tests/e2e/helpers/stencil.ts
+++ packages/components-js/tests/e2e/helpers/stencil.ts
@@ -1,5 +1,9 @@
 import { Page } from 'puppeteer';
 
+export const waitForComponentsReady = async (page: Page): Promise<void> => {
+  await page.evaluate(() => (window as any).porscheDesignSystem.componentsReady());
+};
+
 export const waitForStencilLifecycle = async (page: Page): Promise<void> => {
   await page.evaluate(
     (): Promise<any> => {
