1f4f9763522b0998db8cf8972319b55ed1f8f523
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-02 15:04:18 +0100

    test(components-js): improve componenstReady e2e tests | bh | #1035

--- packages/components-js/tests/e2e/specs/components-ready.e2e.ts
+++ packages/components-js/tests/e2e/specs/components-ready.e2e.ts
@@ -1,5 +1,5 @@
 import { Page } from 'puppeteer';
-import { addEventListener, getBrowser, initAddEventListener, selectNode, setContentWithDesignSystem } from '../helpers';
+import { getBrowser, initAddEventListener, setContentWithDesignSystem } from '../helpers';
 
 describe('componentsReady', () => {
   let page: Page;
@@ -10,84 +10,43 @@ describe('componentsReady', () => {
   });
   afterEach(async () => await page.close());
 
-  const isReady = () =>
-    page.evaluate(async () => {
-      await (window as any).porscheDesignSystem.componentsReady();
-      return true;
-    });
+  const getReadyAmount = (): Promise<number> =>
+    page.evaluate(() => (window as any).porscheDesignSystem.componentsReady());
 
   it('should work for no component', async () => {
     await setContentWithDesignSystem(page, ``);
-    expect(await isReady()).toBeTrue();
+    expect(await getReadyAmount()).toBe(0);
   });
 
   it('should work for single component', async () => {
     await setContentWithDesignSystem(page, `<p-button>Button</p-button>`);
-    expect(await isReady()).toBeTrue();
+    expect(await getReadyAmount()).toBe(1);
   });
 
   it('should work for multiple components', async () => {
     await setContentWithDesignSystem(page, `<p-button>Button1</p-button><p-button>Button2</p-button>`);
-    expect(await isReady()).toBeTrue();
+    expect(await getReadyAmount()).toBe(2);
   });
 
-  it('should work for componentDidUpdate', async () => {
+  it('should work for prefixed component', async () => {
     await setContentWithDesignSystem(
       page,
       `
-      <p-tabs-bar>
-        <button>Tab 1</button>
-        <button>Tab 2</button>
-        <button>Tab 3</button>
-      </p-tabs-bar>`
+      <script type="text/javascript">porscheDesignSystem.load({ prefix: 'my-prefix' });</script>
+      <my-prefix-p-button>Button</my-prefix-p-button>`
     );
-
-    expect(await isReady()).toBeTrue();
-
-    const host = await selectNode(page, 'p-tabs-bar');
-    const [button1, button2, button3] = await host.$$('button');
-
-    let eventCounter = 0;
-    await addEventListener(host, 'tabChange', () => eventCounter++);
-
-    await button2.click();
-    expect(await isReady()).toBeTrue();
-    expect(eventCounter).toBe(1);
+    expect(await getReadyAmount()).toBe(1);
   });
 
-  xit('should ignore other stencil libraries', async () => {
-    const COUNTER_KEY = 'COUNTER_KEY';
-    await page.evaluate((COUNTER_KEY: string): void => {
-      window[COUNTER_KEY] = 0;
-      console.log('addEventListener');
-      window.addEventListener('stencil_componentWillUpdate', (e: CustomEvent) => {
-        console.log(e);
-        window[COUNTER_KEY]++;
-      });
-    }, COUNTER_KEY);
+  it('should ignore other custom web components', async () => {
+    await setContentWithDesignSystem(page, '<x-button>hi</x-button>');
+    expect(await getReadyAmount()).toBe(0);
+  });
 
-    const getCounter = (): Promise<number> =>
-      page.evaluate((COUNTER_KEY: string): number => window[COUNTER_KEY], COUNTER_KEY);
-
-    // expect(await getCounter()).toBe(0);
-    await setContentWithDesignSystem(page, '<p-button>hi</p-button>');
-    // expect(await getCounter()).toBe(0);
-    // expect(await isReady()).toBeTrue();
-
-    await page.evaluate(() => {
-      const ev = new CustomEvent('stencil_componentWillUpdate', {
-        bubbles: true,
-        composed: true,
-        detail: {
-          namespace: 'someNamespace',
-        },
-      });
-      const x = document.querySelector('p-button').dispatchEvent(ev);
-      console.log('dispatched', x);
-    });
-
-    await page.waitForTimeout(3000);
-    expect(await getCounter()).toBe(1);
-    expect(await isReady()).toBeTrue();
+  it('should work when called mulitple times', async () => {
+    await setContentWithDesignSystem(page, `<p-button>Button</p-button>`);
+    expect(await getReadyAmount()).toBe(1);
+    expect(await getReadyAmount()).toBe(1);
+    expect(await getReadyAmount()).toBe(1);
   });
 });
