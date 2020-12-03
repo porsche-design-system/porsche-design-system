f87786fbf896a75d43dee94519e2c8583ca370fd
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-03 16:21:22 +0100

    perf(components): remove unneeded round trips in select wrapper, fix e2e test | bh / sas | #1035

--- packages/components-js/tests/e2e/specs/select-wrapper.e2e.ts
+++ packages/components-js/tests/e2e/specs/select-wrapper.e2e.ts
@@ -25,14 +25,14 @@ describe('select-wrapper', () => {
   });
   afterEach(async () => await page.close());
 
-  const getSelectHost = () => selectNode(page, 'p-select-wrapper');
-  const getSelectFakeInput = () => selectNode(page, 'p-select-wrapper >>> .p-select-wrapper__fake-select');
-  const getSelectRealInput = () => selectNode(page, 'p-select-wrapper select');
-  const getSelectMessage = () => selectNode(page, 'p-select-wrapper >>> .p-select-wrapper__message');
-  const getSelectLabel = () => selectNode(page, 'p-select-wrapper >>> .p-select-wrapper__label');
-  const getSelectLabelLink = () => selectNode(page, 'p-select-wrapper [slot="label"] a');
-  const getSelectDescriptionLink = () => selectNode(page, 'p-select-wrapper [slot="description"] a');
-  const getSelectMessageLink = () => selectNode(page, 'p-select-wrapper [slot="message"] a');
+  const getHost = () => selectNode(page, 'p-select-wrapper');
+  const getFakeSelect = () => selectNode(page, 'p-select-wrapper >>> .p-select-wrapper__fake-select');
+  const getRealSelect = () => selectNode(page, 'p-select-wrapper select');
+  const getMessage = () => selectNode(page, 'p-select-wrapper >>> .p-select-wrapper__message');
+  const getLabel = () => selectNode(page, 'p-select-wrapper >>> .p-select-wrapper__label');
+  const getLabelLink = () => selectNode(page, 'p-select-wrapper [slot="label"] a');
+  const getDescriptionLink = () => selectNode(page, 'p-select-wrapper [slot="description"] a');
+  const getMessageLink = () => selectNode(page, 'p-select-wrapper [slot="message"] a');
 
   type InitOptions = {
     useSlottedLabel?: boolean;
@@ -48,6 +48,9 @@ describe('select-wrapper', () => {
       state: 'none',
     }
   ): Promise<void> => {
+    const label = !useSlottedLabel ? 'label="Some label"' : '';
+    const description = !useSlottedDescription ? 'description="Some description"' : '';
+    const message = !useSlottedMessage ? 'message="Some message"' : '';
     const slottedLabel = useSlottedLabel
       ? '<span slot="label">Some label with a <a href="#" onclick="return false;">link</a>.</span>'
       : '';
@@ -61,9 +64,7 @@ describe('select-wrapper', () => {
     return setContentWithDesignSystem(
       page,
       `
-        <p-select-wrapper state="${state}" ${!useSlottedLabel ? 'label="Some label"' : ''} ${
-        !useSlottedDescription ? 'label="Some description"' : ''
-      } ${!useSlottedMessage ? 'label="Some message"' : ''}>
+        <p-select-wrapper state="${state}" ${label} ${description} ${message}>
           ${slottedLabel}
           ${slottedDescription}
           <select>
@@ -78,14 +79,14 @@ describe('select-wrapper', () => {
 
   it('should render', async () => {
     await initSelect();
-    const el = await getSelectFakeInput();
+    const el = await getFakeSelect();
     expect(el).toBeDefined();
   });
 
   it('should add aria-label to support screen readers properly', async () => {
     await initSelect();
-    const select = await getSelectRealInput();
-    expect(await getProperty(select, 'ariaLabel')).toBe('Some label');
+    const select = await getRealSelect();
+    expect(await getProperty(select, 'ariaLabel')).toBe('Some label. Some message');
   });
 
   it('should add aria-label with description text to support screen readers properly', async () => {
@@ -101,7 +102,7 @@ describe('select-wrapper', () => {
       </p-select-wrapper>
     `
     );
-    const select = await getSelectRealInput();
+    const select = await getRealSelect();
     expect(await getProperty(select, 'ariaLabel')).toBe('Some label. Some description');
   });
 
@@ -118,7 +119,7 @@ describe('select-wrapper', () => {
       </p-select-wrapper>
     `
     );
-    const select = await getSelectRealInput();
+    const select = await getRealSelect();
     expect(await getProperty(select, 'ariaLabel')).toBe('Some label. Some message');
   });
 
@@ -135,21 +136,21 @@ describe('select-wrapper', () => {
       </p-select-wrapper>`
     );
 
-    expect(await getSelectLabel()).toBeNull();
+    expect(await getLabel()).toBeNull();
 
-    const selectComponent = await getSelectHost();
+    const selectComponent = await getHost();
     await selectComponent.evaluate((el) => el.setAttribute('label', 'Some label'));
 
-    expect(await getSelectLabel()).not.toBeNull();
+    expect(await getLabel()).not.toBeNull();
   });
 
   it('should add/remove message text and update aria-label attribute with message text if state changes programmatically', async () => {
     await initSelect();
 
-    const selectComponent = await getSelectHost();
-    const select = await getSelectRealInput();
+    const selectComponent = await getHost();
+    const select = await getRealSelect();
 
-    expect(await getSelectMessage()).toBeNull('initially');
+    expect(await getMessage()).toBeNull('initially');
 
     await selectComponent.evaluate((el) => {
       el.setAttribute('state', 'error');
@@ -157,8 +158,8 @@ describe('select-wrapper', () => {
     });
     await waitForStencilLifecycle(page);
 
-    expect(await getSelectMessage()).toBeDefined('when state = error');
-    expect(await getAttribute(await getSelectMessage(), 'role')).toEqual('alert', 'when state = error');
+    expect(await getMessage()).toBeDefined('when state = error');
+    expect(await getAttribute(await getMessage(), 'role')).toEqual('alert', 'when state = error');
     expect(await getProperty(select, 'ariaLabel')).toEqual('Some label. Some error message', 'when state = error');
 
     await selectComponent.evaluate((el) => {
@@ -167,8 +168,8 @@ describe('select-wrapper', () => {
     });
     await waitForStencilLifecycle(page);
 
-    expect(await getSelectMessage()).toBeDefined('when state = success');
-    expect(await getAttribute(await getSelectMessage(), 'role')).toBeNull('when state = success');
+    expect(await getMessage()).toBeDefined('when state = success');
+    expect(await getAttribute(await getMessage(), 'role')).toBeNull('when state = success');
     expect(await getProperty(select, 'ariaLabel')).toEqual('Some label. Some success message', 'when state = success');
 
     await selectComponent.evaluate((el) => {
@@ -177,14 +178,14 @@ describe('select-wrapper', () => {
     });
     await waitForStencilLifecycle(page);
 
-    expect(await getSelectMessage()).toBeNull('when state = none');
-    expect(await getProperty(select, 'ariaLabel')).toEqual('Some label', 'when state = none');
+    expect(await getMessage()).toBeNull('when state = none');
+    expect(await getProperty(select, 'ariaLabel')).toEqual('Some label. Some description', 'when state = none');
   });
 
   it('should focus select when label text is clicked', async () => {
     await initSelect();
 
-    const labelText = await getSelectLabel();
+    const labelText = await getLabel();
     expect(labelText).toBeDefined();
 
     async function hasSelectFocus() {
@@ -201,11 +202,11 @@ describe('select-wrapper', () => {
     expect(await hasSelectFocus()).toBe(true);
   });
 
-  it('should disable fake select when select is set disabled programmatically', async () => {
+  it('should disable fake select when select is disabled programmatically', async () => {
     await initSelect();
 
-    const fakeSelect = await getSelectFakeInput();
-    const select = await getSelectRealInput();
+    const fakeSelect = await getFakeSelect();
+    const select = await getRealSelect();
     const disabledClass = 'p-select-wrapper__fake-select--disabled';
 
     expect(await getCssClasses(fakeSelect)).not.toContain(disabledClass, 'initially');
@@ -225,7 +226,7 @@ describe('select-wrapper', () => {
     it('should be shown by keyboard navigation and on click for slotted <select>', async () => {
       await initSelect();
 
-      const textarea = await getSelectRealInput();
+      const textarea = await getRealSelect();
       const hidden = expectedStyleOnFocus({ color: 'transparent' });
       const visible = expectedStyleOnFocus({ color: 'neutral' });
 
@@ -246,9 +247,9 @@ describe('select-wrapper', () => {
     it('should be shown by keyboard navigation only for slotted <a>', async () => {
       await initSelect({ useSlottedLabel: true, useSlottedDescription: true, useSlottedMessage: true, state: 'error' });
 
-      const labelLink = await getSelectLabelLink();
-      const descriptionLink = await getSelectDescriptionLink();
-      const messageLink = await getSelectMessageLink();
+      const labelLink = await getLabelLink();
+      const descriptionLink = await getDescriptionLink();
+      const messageLink = await getMessageLink();
       const hidden = expectedStyleOnFocus({ color: 'transparent', offset: '1px' });
       const visible = expectedStyleOnFocus({ color: 'hover', offset: '1px' });
 
@@ -295,8 +296,8 @@ describe('select-wrapper', () => {
     it('should show outline of slotted <select> when it is focused', async () => {
       await initSelect();
 
-      const host = await getSelectHost();
-      const input = await getSelectRealInput();
+      const host = await getHost();
+      const input = await getRealSelect();
 
       expect(await getStyleOnFocus(input)).toBe(expectedStyleOnFocus({ color: 'neutral' }));
 
@@ -326,10 +327,10 @@ describe('select-wrapper', () => {
     it('should show outline of slotted <a> when it is focused', async () => {
       await initSelect({ useSlottedLabel: true, useSlottedDescription: true, useSlottedMessage: true, state: 'error' });
 
-      const host = await getSelectHost();
-      const labelLink = await getSelectLabelLink();
-      const descriptionLink = await getSelectDescriptionLink();
-      const messageLink = await getSelectMessageLink();
+      const host = await getHost();
+      const labelLink = await getLabelLink();
+      const descriptionLink = await getDescriptionLink();
+      const messageLink = await getMessageLink();
 
       expect(await getStyleOnFocus(labelLink)).toBe(expectedStyleOnFocus({ offset: '1px' }));
       expect(await getStyleOnFocus(descriptionLink)).toBe(expectedStyleOnFocus({ color: 'neutral', offset: '1px' }));
