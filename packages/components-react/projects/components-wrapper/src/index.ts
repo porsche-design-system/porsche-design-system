ad7264cbda66ad664208eed7923240b462e0d3aa
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-07 12:51:00 +0100

    feat(components-angular/react): reexport componentsReady in wrappers | bh / sas | #1035

--- packages/components-react/projects/components-wrapper/src/index.ts
+++ packages/components-react/projects/components-wrapper/src/index.ts
@@ -2,3 +2,4 @@ import { load } from '@porsche-design-system/components-js';
 load();
 
 export * from './lib/components-provider';
+export { componentsReady } from '@porsche-design-system/components-js';
