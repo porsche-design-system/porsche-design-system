e989c055e331988fbdb89202d973d88743af0c1a
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-07 13:42:09 +0100

    docs(components-angular): add comment to e2e test | bh | #1035

--- packages/components-angular/tests/e2e/helpers/stencil.ts
+++ packages/components-angular/tests/e2e/helpers/stencil.ts
@@ -3,7 +3,7 @@ import { Page } from 'puppeteer';
 export const waitForStencilLifecycle = async (page: Page): Promise<void> => {
   await page.evaluate(
     async (): Promise<void> => {
-      await (window as any).componentsReady();
+      await (window as any).componentsReady(); // exposed via main.ts of angular vrt app
     }
   );
 };
