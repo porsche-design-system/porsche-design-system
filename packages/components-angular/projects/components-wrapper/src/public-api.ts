da026f6ed174f7c59f375e48533428533ab07a43
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-07 12:51:00 +0100

    feat(components-angular/react): reexport componentsReady in wrappers | bh / sas | #1035

--- packages/components-angular/projects/components-wrapper/src/public-api.ts
+++ packages/components-angular/projects/components-wrapper/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/proxies';
 export * from './components-wrapper.module';
 export * from './prefix.service';
 export * from './injection.tokens';
+export { componentsReady } from '@porsche-design-system/components-js';
