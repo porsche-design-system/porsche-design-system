54290730b8a619ed4c7e739aada7ee0ce7b7621f
Matthias Haegele <matthias.haegele@mhp.com>
2020-12-08 12:38:39 +0100

    chore: run prettier | mh | #728

--- packages/utilities/projects/utilities/src/js/index.ts
+++ packages/utilities/projects/utilities/src/js/index.ts
@@ -12,7 +12,12 @@ declare global {
 }
 
 const isCdnCn = typeof window !== 'undefined' && window[CDN_KEY] === 'cn';
-export const FONT_FACE_CDN_URL = (isCdnCn ? 'https://cdn.ui.porsche.cn' : 'https://cdn.ui.porsche.com') + '/porsche-design-system/styles/' + (isCdnCn ? 'font-face.min.cn.c1b46971322e96095de49987cdc90226.css' : 'font-face.min.de7353ac41430a74da152a5bf0e7bb5b.css');
+export const FONT_FACE_CDN_URL =
+  (isCdnCn ? 'https://cdn.ui.porsche.cn' : 'https://cdn.ui.porsche.com') +
+  '/porsche-design-system/styles/' +
+  (isCdnCn
+    ? 'font-face.min.cn.c1b46971322e96095de49987cdc90226.css'
+    : 'font-face.min.de7353ac41430a74da152a5bf0e7bb5b.css');
 
 /**
  * @deprecated since v1.1.0.
