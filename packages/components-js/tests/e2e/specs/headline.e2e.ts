6822432113f22d8bfc9223373c5afa51aa08eafa
Matthias Haegele <matthias.haegele@mhp.com>
2020-12-09 14:16:23 +0100

    test(component-js): extend tests / refactor according changes to component | mh / bh | #953

--- packages/components-js/tests/e2e/specs/headline.e2e.ts
+++ packages/components-js/tests/e2e/specs/headline.e2e.ts
@@ -1,29 +1,27 @@
 import { getBrowser, setContentWithDesignSystem } from '../helpers';
 import { Page } from 'puppeteer';
-import { HeadlineVariant, TextSize } from '@porsche-design-system/components/src/types';
-import { BreakpointCustomizable } from '@porsche-design-system/components/src/utils';
+import { BreakpointCustomizable, TextSize, VariantType } from '@porsche-design-system/components/dist/types/bundle';
 
-describe('headline', () => {
+fdescribe('headline', () => {
   let page: Page;
 
   beforeEach(async () => (page = await getBrowser().newPage()));
   afterEach(async () => await page.close());
 
   const initHeadline = (opts?: {
-    variant?: HeadlineVariant;
+    variant?: VariantType;
     size?: BreakpointCustomizable<TextSize>;
     slot?: string;
     tag?: string;
   }): Promise<void> => {
-    const { variant, size, slot, tag } = opts;
+    const { variant, slot, tag } = opts;
 
-    const content =
-      (variant ? `variant="${variant}" ` : '') + (size ? ` size="${size}" ` : '') + (tag ? `tag="${tag}" ` : '');
+    const content = (variant ? `variant="${variant}" ` : '') + (tag ? `tag="${tag}"` : '');
 
     return setContentWithDesignSystem(
       page,
       `
-        <p-headline ${content}>
+        <p-headline ${content.trim()}>
           ${slot ? slot : 'Some Headline'}
         </p-headline>`
     );
@@ -33,9 +31,24 @@ describe('headline', () => {
     await page.$eval('p-headline', (el) => el.shadowRoot.querySelector('.p-headline').tagName);
 
   describe('tag', () => {
-    it('should render according to variant headline-2', async () => {
+    it('should render according to variant', async () => {
+      await initHeadline({ variant: 'large-title' });
+      expect(await getHeadlineTagName()).toBe('H1', 'for variant="large-title"');
+
+      await initHeadline({ variant: 'headline-1' });
+      expect(await getHeadlineTagName()).toBe('H1', 'for variant="headline-1"');
+
       await initHeadline({ variant: 'headline-2' });
-      expect(await getHeadlineTagName()).toBe('H2');
+      expect(await getHeadlineTagName()).toBe('H2', 'for variant="headline-2"');
+
+      await initHeadline({ variant: 'headline-3' });
+      expect(await getHeadlineTagName()).toBe('H3', 'for variant="headline-3"');
+
+      await initHeadline({ variant: 'headline-4' });
+      expect(await getHeadlineTagName()).toBe('H4', 'for variant="headline-4"');
+
+      await initHeadline({ variant: 'headline-5' });
+      expect(await getHeadlineTagName()).toBe('H5', 'for variant="headline-5"');
     });
 
     it('should render according to tag h6 when variant is set', async () => {
@@ -43,13 +56,13 @@ describe('headline', () => {
       expect(await getHeadlineTagName()).toBe('H6');
     });
 
-    it('should render as default if size is set without tag', async () => {
-      await initHeadline({ size: 'x-large' });
+    it('should render as default if variant is a size object without tag', async () => {
+      await initHeadline({ variant: 'x-large' });
       expect(await getHeadlineTagName()).toBe('H1');
     });
 
     it('should render according to tag h6 if size is set', async () => {
-      await initHeadline({ size: 'x-large', tag: 'h6' });
+      await initHeadline({ variant: 'x-large', tag: 'h6' });
       expect(await getHeadlineTagName()).toBe('H6');
     });
 
