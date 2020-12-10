519590ff3d1bf6c6f46c86a7313680c0ab2e7b24
Matthias Haegele <matthias.haegele@mhp.com>
2020-12-10 09:12:26 +0100

    chore: implement review changes | mh | #953

--- packages/components/src/components/basic/typography/headline/headlineUtils.ts
+++ packages/components/src/components/basic/typography/headline/headlineUtils.ts
@@ -11,18 +11,27 @@ export const HEADLINE_VARIANTS = [
 
 export type HeadlineVariant = typeof HEADLINE_VARIANTS[number];
 
-export type HeadlineTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+export type HeadlineTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
-export const getTagName = (host: HTMLElement, variant?: VariantType, tag?: HeadlineTags): string => {
-  const variantToTagMap: { [key in HeadlineVariant]: string } = {
-    'large-title': 'h1',
-    'headline-1': 'h1',
-    'headline-2': 'h2',
-    'headline-3': 'h3',
-    'headline-4': 'h4',
-    'headline-5': 'h5',
-  };
+export const isHeadlineVariant = (variant: VariantType): boolean => {
+  return HEADLINE_VARIANTS.includes(variant as HeadlineVariant);
+};
 
+export const hasSlottedHeadlineTag = (host: HTMLElement): boolean => {
+  const el = host.querySelector(':first-child');
+  return el?.matches('h1, h2, h3, h4, h5, h6');
+};
+
+const variantToTagMap: { [key in HeadlineVariant]: string } = {
+  'large-title': 'h1',
+  'headline-1': 'h1',
+  'headline-2': 'h2',
+  'headline-3': 'h3',
+  'headline-4': 'h4',
+  'headline-5': 'h5',
+};
+
+export const getTagName = (host: HTMLElement, variant?: VariantType, tag?: HeadlineTag): string => {
   if (hasSlottedHeadlineTag(host)) {
     return 'div';
   } else if (tag) {
@@ -32,13 +41,4 @@ export const getTagName = (host: HTMLElement, variant?: VariantType, tag?: Headl
   } else {
     return variantToTagMap[variant as HeadlineVariant];
   }
-};
-
-export const hasSlottedHeadlineTag = (host: HTMLElement): boolean => {
-  const el = host.querySelector(':first-child');
-  return el?.matches('h1, h2, h3, h4, h5, h6');
-};
-
-export const isHeadlineVariant = (variant: VariantType): boolean => {
-  return HEADLINE_VARIANTS.includes(variant as HeadlineVariant);
 };
