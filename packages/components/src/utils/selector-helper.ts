068506097fdeaded9baa8538a1cbc324b5847369
Matthias Haegele <matthias.haegele@mhp.com>
2020-12-15 16:05:45 +0100

    feat(components): create getShadowElement helper / remove unused watcher | mh / bh | #1036

--- packages/components/src/utils/selector-helper.ts
+++ packages/components/src/utils/selector-helper.ts
@@ -3,3 +3,6 @@ export const getHTMLElement = (host: HTMLElement | ShadowRoot, selector: string)
 
 export const getHTMLElements = (host: HTMLElement | ShadowRoot, selector: string): HTMLElement[] =>
   Array.from(host.querySelectorAll(selector));
+
+export const getShadowRootHTMLElement = (host: HTMLElement, selector: string): HTMLElement =>
+  host.shadowRoot.querySelector(selector);
