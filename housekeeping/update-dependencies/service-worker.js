28d17b442e098abafd7f755536d193f5e9afee89
stephanschroeter <stephanschroeter@users.noreply.github.com>
2020-12-16 10:46:24 +0000

    Automated deployment of Porsche Design System - Storefront (housekeeping/update-dependencies): 31d0feb915f85104e23c3a64e5ada882bf10d479

--- housekeeping/update-dependencies/service-worker.js
+++ housekeeping/update-dependencies/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.068a3eaa60abaa067941e7eb3a99eff2.js"
+  "precache-manifest.1db6b41edfb6d48d16ff2d099c7dd200.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "@porsche-design-system/storefront"});
