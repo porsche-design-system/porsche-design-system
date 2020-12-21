898b7f4d6c419cb2412e1c32701871b48457a981
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-21 16:25:31 +0100

    buid(assets): introduce COMPONENTS_ONLY env flag to copy only components, extend root package.json | bh

--- packages/assets/scripts/build.ts
+++ packages/assets/scripts/build.ts
@@ -1,30 +1,42 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+const isComponentsOnly = process.env.COMPONENTS_ONLY === '1';
+
 const cdnPathPackageMap = {
-  'components': '@porsche-design-system/components-js',
-  'fonts': '@porsche-design-system/fonts',
-  'icons': '@porsche-design-system/icons',
-  'marque': '@porsche-design-system/marque',
-  'meta-icons': '@porsche-design-system/meta-icons',
-  'styles': '@porsche-design-system/utilities',
+  components: '@porsche-design-system/components-js',
+  ...(!isComponentsOnly && {
+    'fonts': '@porsche-design-system/fonts',
+    'icons': '@porsche-design-system/icons',
+    'marque': '@porsche-design-system/marque',
+    'meta-icons': '@porsche-design-system/meta-icons',
+    'styles': '@porsche-design-system/utilities',
+  }),
 };
 
 const TARGET_DIRECTORY = '../cdn';
 
-(async () => {
+const copyPackageDistFiles = (): void => {
   for (const cdnPath of Object.keys(cdnPathPackageMap)) {
     const pathToPackage = require.resolve(cdnPathPackageMap[cdnPath as keyof typeof cdnPathPackageMap]);
     const relativePathToPackageFiles = `../${cdnPath}`;
     const pathToFiles = path.resolve(path.dirname(pathToPackage), relativePathToPackageFiles);
 
-    const files = await fs.promises.readdir(pathToFiles);
+    const files = fs.readdirSync(pathToFiles);
     const targetDirectory = path.resolve(__dirname, TARGET_DIRECTORY, cdnPath);
-    await fs.promises.mkdir(targetDirectory, { recursive: true });
+    fs.mkdirSync(targetDirectory, { recursive: true });
+
+    console.log(
+      `Copying contents from '${
+        cdnPathPackageMap[cdnPath as keyof typeof cdnPathPackageMap]
+      }' to '${TARGET_DIRECTORY}/${path.basename(targetDirectory)}'..`
+    );
 
     for (const file of files) {
-      await fs.promises.copyFile(path.resolve(pathToFiles, file), path.resolve(targetDirectory, file));
-      console.log(`'${file}'`, 'copied to', `'${TARGET_DIRECTORY}/${path.basename(targetDirectory)}/'`);
+      fs.copyFileSync(path.resolve(pathToFiles, file), path.resolve(targetDirectory, file));
+      console.log(`'${file}' copied to '${TARGET_DIRECTORY}/${path.basename(targetDirectory)}/'`);
     }
   }
-})();
+};
+
+copyPackageDistFiles();
