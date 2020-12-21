d1d9a540ac389cf448b768a2c708af7ed9d90d07
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-21 15:08:22 +0100

    ci: move atrifacts via move-artifacts.sh | bh

--- .github/actions/move-artifacts/move-artifacts.sh
+++ .github/actions/move-artifacts/move-artifacts.sh
@@ -4,7 +4,9 @@ ls ./packages/artifacts
 
 artifacts=( "assets/cdn" "assets/dist" "assets/bin" "components-js/dist/components-wrapper" "components-angular/projects/components-wrapper/src/lib" "components-react/projects/components-wrapper/src/lib" "partials/dist" "utilities/projects/utilities" )
 for source in ${artifacts[@]}; do
+  source=./artifacts/${source}
   dest=./packages/${source}
+  ls ${source}
   echo "Moving ${source} to ${dest}"
-#  mv ./packages/artifacts/assets/cdn ./packages/assets/cdn
+  mv ${source} ${dest}
 done
