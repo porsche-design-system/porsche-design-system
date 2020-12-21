6c392ec9f334178ef54ff55ed6571f95aa1aa043
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-21 16:05:12 +0100

    ci: improve move-artifacts.sh and add chmod for cdn binary | bh

--- .github/actions/move-artifacts/move-artifacts.sh
+++ .github/actions/move-artifacts/move-artifacts.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-ls ./packages/artifacts
-all_dirs=(./packages/artifacts/*)
-echo $all_dirs
+pushd "./packages/artifacts"
+  ls
+  all_dirs=(./*)
+  echo $all_dirs
 
-artifacts=( "assets/cdn" "assets/dist" "assets/bin" "components-js/dist/components-wrapper" "components-angular/projects/components-wrapper/src/lib" "components-react/projects/components-wrapper/src/lib" "partials/dist" "utilities/projects/utilities" )
-for source in ${all_dirs[@]}; do
-#  source=./artifacts/${source}
-  dest=./packages/${source}
-  ls ${source}
-  echo "Moving ${source} to ${dest}"
-#  mv ${source} ${dest}
-done
+  for source in ${all_dirs[@]}; do
+  #  source=./artifacts/${source}
+    dest=../packages/${source}
+    ls ${source}
+    echo "Moving ${source} to ${dest}"
+    mv ${source} ${dest}
+  done
+popd
+
+chmod +x ./packages/assets/bin/serve-cdn.js
