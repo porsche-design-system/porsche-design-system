df06bc95375cf8d2ec6b271fb7f634c3e0f7d7ea
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-21 15:50:49 +0100

    ci: debug move-artifacts.sh | bh

--- .github/actions/move-artifacts/move-artifacts.sh
+++ .github/actions/move-artifacts/move-artifacts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ls ./packages/artifacts
-all_dirs=(./packages/artifacts)
+all_dirs=(./packages/artifacts/*)
 echo $all_dirs
 
 artifacts=( "assets/cdn" "assets/dist" "assets/bin" "components-js/dist/components-wrapper" "components-angular/projects/components-wrapper/src/lib" "components-react/projects/components-wrapper/src/lib" "partials/dist" "utilities/projects/utilities" )
 for source in ${all_dirs[@]}; do
-  source=./artifacts/${source}
+#  source=./artifacts/${source}
   dest=./packages/${source}
   ls ${source}
   echo "Moving ${source} to ${dest}"
