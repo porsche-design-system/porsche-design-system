11432336e47f37a52f4f1f49cc0c1a9c42669ed7
Bernd Hacker <hacker.bernd@gmail.com>
2020-12-21 16:35:23 +0100

    build(assets): catch exception in build script | bh

--- packages/assets/scripts/build.ts
+++ packages/assets/scripts/build.ts
@@ -18,23 +18,26 @@ const TARGET_DIRECTORY = '../cdn';
 
 const copyPackageDistFiles = (): void => {
   for (const cdnPath of Object.keys(cdnPathPackageMap)) {
-    const pathToPackage = require.resolve(cdnPathPackageMap[cdnPath as keyof typeof cdnPathPackageMap]);
-    const relativePathToPackageFiles = `../${cdnPath}`;
-    const pathToFiles = path.resolve(path.dirname(pathToPackage), relativePathToPackageFiles);
+    const packageName = cdnPathPackageMap[cdnPath as keyof typeof cdnPathPackageMap];
+    try {
+      const pathToPackage = require.resolve(packageName);
+      const relativePathToPackageFiles = `../${cdnPath}`;
+      const pathToFiles = path.resolve(path.dirname(pathToPackage), relativePathToPackageFiles);
 
-    const files = fs.readdirSync(pathToFiles);
-    const targetDirectory = path.resolve(__dirname, TARGET_DIRECTORY, cdnPath);
-    fs.mkdirSync(targetDirectory, { recursive: true });
+      const files = fs.readdirSync(pathToFiles);
+      const targetDirectory = path.resolve(__dirname, TARGET_DIRECTORY, cdnPath);
+      fs.mkdirSync(targetDirectory, { recursive: true });
 
-    console.log(
-      `Copying contents from '${
-        cdnPathPackageMap[cdnPath as keyof typeof cdnPathPackageMap]
-      }' to '${TARGET_DIRECTORY}/${path.basename(targetDirectory)}'..`
-    );
+      console.log(
+        `Copying contents from '${packageName}' to '${TARGET_DIRECTORY}/${path.basename(targetDirectory)}'..`
+      );
 
-    for (const file of files) {
-      fs.copyFileSync(path.resolve(pathToFiles, file), path.resolve(targetDirectory, file));
-      console.log(`'${file}' copied to '${TARGET_DIRECTORY}/${path.basename(targetDirectory)}/'`);
+      for (const file of files) {
+        fs.copyFileSync(path.resolve(pathToFiles, file), path.resolve(targetDirectory, file));
+        console.log(`'${file}' copied to '${TARGET_DIRECTORY}/${path.basename(targetDirectory)}/'`);
+      }
+    } catch (e) {
+      console.log(`Package '${packageName}' doesn't exist. Skipping...`);
     }
   }
 };
