7cd63f9441fde7be580088adcba3ad9e298d4710
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-04 09:54:41 +0100

    test(components-js): refactor initial smoke test | bh | #758

--- packages/components-js/tests/smoke/specs/cdn.smoke.ts
+++ packages/components-js/tests/smoke/specs/cdn.smoke.ts
@@ -3,43 +3,114 @@ import { Page } from 'puppeteer';
 
 describe('cdn', () => {
   let page: Page;
-  const requests: { url: string }[] = [];
-  const responses: { url: string; status: number }[] = [];
 
-  beforeEach(async () => {
-    page = await getBrowser().newPage();
-    await page.setRequestInterception(true);
+  beforeEach(async () => (page = await getBrowser().newPage()));
+  afterEach(async () => await page.close());
 
-    requests.length = 0;
-    responses.length = 0;
+  const content = `
+<p-content-wrapper>
+  <p-marque></p-marque>
+  <p-headline variant="headline-1">Some Headline</p-headline>
+  <p-button>Some label</p-button>
+</p-content-wrapper>`;
 
-    page.removeAllListeners('request');
-    page.removeAllListeners('response');
+  describe('assets', () => {
+    type Request = { url: string };
+    type Response = { url: string; status: number };
+    const requests: Request[] = [];
+    const responses: Response[] = [];
 
-    page.on('request', (req) => {
-      const url = req.url();
+    beforeEach(async () => {
+      await page.setRequestInterception(true);
 
-      if (url.includes('cdn.ui.porsche')) {
-        requests.push({ url });
-      }
-      req.continue();
+      requests.length = 0; // clear old array
+      responses.length = 0; // clear old array
+
+      page.removeAllListeners('request');
+      page.removeAllListeners('response');
+
+      page.on('request', (req) => {
+        const url = req.url();
+
+        if (url.includes('cdn.ui.porsche')) {
+          requests.push({ url });
+        }
+        req.continue();
+      });
+
+      page.on('response', (resp) => {
+        const url = resp.url();
+        const status = resp.status();
+
+        if (url.includes('cdn.ui.porsche')) {
+          responses.push({ url, status });
+        }
+      });
     });
 
-    page.on('response', (resp) => {
-      const url = resp.url();
-      const status = resp.status();
-      console.log('response', status, url);
+    const isStatusNot200 = (item: Response): boolean => item.status !== 200;
+    const isStatus400 = (item: Response): boolean => item.status === 400;
+    const urlIncludes = (str: string) => (item: Response): boolean => item.url.includes(str);
+    const urlStartsWith = (str: string) => (item: Response): boolean => item.url.startsWith(str);
 
-      if (url.includes('cdn.ui.porsche')) {
-        responses.push({ url, status });
-      }
+    describe('.com and .cn domains', () => {
+      const assetPaths = {
+        components: 'components/porsche-design-system.v',
+        styles: 'styles/font-face.min.',
+        icons: 'icons/arrow-head-right.min.',
+        fonts: 'fonts/porsche-next-w-la-regular.min.',
+        marque: 'marque/porsche-marque-trademark.medium.min.',
+      };
+
+      it('should request from .com cdn for { cdn: "auto" } when outside of china', async () => {
+        await setContentWithDesignSystem(page, content, 'auto');
+
+        const cdnDomain = 'cdn.ui.porsche.com';
+        const baseUrl = `https://${cdnDomain}/porsche-design-system`;
+
+        expect(requests.length).toBe(responses.length);
+        expect(responses.filter(isStatusNot200).length).toBe(0);
+        expect(responses.filter(urlIncludes(cdnDomain)).length).toBe(responses.length);
+
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.components}`)).length).toBe(1);
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.styles}`)).length).toBe(1);
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.icons}`)).length).toBe(1);
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.fonts}`)).length).toBe(1);
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.marque}`)).length).toBe(1);
+
+        const responseErrors = responses.filter(isStatus400);
+        if (responseErrors.length) {
+          console.log('status 400', responseErrors);
+        }
+      });
+
+      it('should request always from .cn cdn for { cdn: "cn" }', async () => {
+        await setContentWithDesignSystem(page, content, 'cn');
+
+        const cdnDomain = 'cdn.ui.porsche.cn';
+        const baseUrl = `https://${cdnDomain}/porsche-design-system`;
+
+        expect(requests.length).toBe(responses.length);
+        expect(responses.filter(isStatusNot200).length).toBe(0);
+        expect(responses.filter(urlIncludes(cdnDomain)).length).toBe(responses.length);
+
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.components}`)).length).toBe(1);
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.styles}`)).length).toBe(1);
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.icons}`)).length).toBe(1);
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.fonts}`)).length).toBe(1);
+        expect(responses.filter(urlStartsWith(`${baseUrl}/${assetPaths.marque}`)).length).toBe(1);
+
+        const responseErrors = responses.filter(isStatus400);
+        if (responseErrors.length) {
+          console.log('status 400', responseErrors);
+        }
+      });
     });
   });
 
-  afterEach(async () => await page.close());
-
-  const COUNTER_KEY = 'lifecycleCounter';
-  const content = `
+  it('should not emit lifecycleDOMEvents', async () => {
+    const COUNTER_KEY = 'lifecycleCounter';
+    const extendedContent = `
 <script>
   window['${COUNTER_KEY}'] = 0;
   ['componentWillLoad', 'componentDidLoad', 'componentWillUpdate', 'componentDidUpdate'].forEach((event) => {
@@ -48,68 +119,12 @@ describe('cdn', () => {
     });
   })
 </script>
+${content}`;
 
-<p-content-wrapper>
-  <p-marque></p-marque>
-  <p-headline variant="headline-1">Performance test</p-headline>
-  <p-button>Some label</p-button>
-</p-content-wrapper>`;
-
-  it('should request from .com cdn for { cdn: "auto" } when outside of china', async () => {
-    await setContentWithDesignSystem(page, content, 'auto');
-
-    expect(requests.length).toBe(responses.length);
-
-    expect(responses.filter((item) => item.status !== 200).length).toBe(0);
-    expect(responses.filter((item) => item.url.includes('cdn.ui.porsche.com')).length).toBe(responses.length);
-
-    const baseUrl = 'https://cdn.ui.porsche.com/porsche-design-system';
-
-    // prettier-ignore
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/components/porsche-design-system.v`)).length).toBe(1);
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/styles/font-face.min.`)).length).toBe(1);
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/icons/arrow-head-right.min.`)).length).toBe(1);
-    // prettier-ignore
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/fonts/porsche-next-w-la-regular.min.`)).length).toBe(1);
-    // prettier-ignore
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/marque/porsche-marque-trademark.medium.min.`)).length).toBe(1);
-
-    const responseErrors = responses.filter((item) => item.status === 400);
-    if (responseErrors.length) {
-      console.log('status 400', responseErrors);
-    }
-  });
-
-  it('should request always from .cn cdn for { cdn: "cn" }', async () => {
-    await setContentWithDesignSystem(page, content, 'cn');
-
-    expect(requests.length).toBe(responses.length);
-
-    expect(responses.filter((item) => item.status !== 200).length).toBe(0);
-    expect(responses.filter((item) => item.url.includes('cdn.ui.porsche.cn')).length).toBe(responses.length);
-
-    const baseUrl = 'https://cdn.ui.porsche.cn/porsche-design-system';
-
-    // prettier-ignore
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/components/porsche-design-system.v`)).length).toBe(1);
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/styles/font-face.min.`)).length).toBe(1);
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/icons/arrow-head-right.min.`)).length).toBe(1);
-    // prettier-ignore
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/fonts/porsche-next-w-la-regular.min.`)).length).toBe(1);
-    // prettier-ignore
-    expect(responses.filter((item) => item.url.startsWith(`${baseUrl}/marque/porsche-marque-trademark.medium.min.`)).length).toBe(1);
-
-    const responseErrors = responses.filter((item) => item.status === 400);
-    if (responseErrors.length) {
-      console.log('status 400', responseErrors);
-    }
-  });
-
-  it('should not emit lifecycleDOMEvents', async () => {
     const getCountedEvents = (): Promise<number> =>
       page.evaluate((COUNTER_KEY: string) => window[COUNTER_KEY], COUNTER_KEY);
 
-    await setContentWithDesignSystem(page, content);
+    await setContentWithDesignSystem(page, extendedContent);
 
     expect(await getCountedEvents()).toBe(0);
 
