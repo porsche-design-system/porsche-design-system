b8828f13f259341a2fb68338d583833ec40ffa98
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-06 10:01:21 +0100

    test(components-js): add e2e test for deterministic component initialization | bh | #948

---
+++ packages/components-js/tests/e2e/specs/initializer.e2e.ts
@@ -0,0 +1,33 @@
+import { getBrowser, waitForComponentsReady } from '../helpers';
+import { ElementHandle, Page } from 'puppeteer';
+
+const BASE_URL = 'http://localhost:8575';
+
+describe('initializer', () => {
+  let page: Page;
+  beforeEach(async () => (page = await getBrowser().newPage()));
+  afterEach(async () => await page.close());
+
+  const goto = async (url: string) => {
+    await page.goto(`${BASE_URL}/#${url}`);
+    await waitForComponentsReady(page);
+  };
+
+  it('should initialize component deterministically', async () => {
+    await goto('initializer');
+    await page.waitForTimeout(1000);
+
+    const [component1, component2] = await page.$$('p-text-field-wrapper');
+    const getOuterHTML = (elHandle: ElementHandle) => elHandle.evaluate((el) => el.outerHTML);
+
+    const component1HTML = await getOuterHTML(component1);
+    const component2HTML = await getOuterHTML(component2);
+
+    expect(component1HTML).toBe(component2HTML);
+
+    if (component1HTML !== component2HTML) {
+      console.log('component1HTML', component1HTML);
+      console.log('component2HTML', component2HTML);
+    }
+  });
+});
