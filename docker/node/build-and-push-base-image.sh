aba16e2765745fc7a372824337544c80b4e9f396
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-06 15:28:03 +0100

    chore: merge docker tag into docker build in build-and-push-base-image.sh | bh | #948

--- docker/node/build-and-push-base-image.sh
+++ docker/node/build-and-push-base-image.sh
@@ -6,8 +6,7 @@ TAG=14.15.1-stretch-slim
 
 docker pull $REGISTRY/$IMAGE:latest
 
-docker build -t $REGISTRY/$IMAGE:$TAG .
-docker tag $REGISTRY/$IMAGE:$TAG $REGISTRY/$IMAGE:latest
+docker build -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
 
 docker push $REGISTRY/$IMAGE:$TAG
 docker push $REGISTRY/$IMAGE:latest
