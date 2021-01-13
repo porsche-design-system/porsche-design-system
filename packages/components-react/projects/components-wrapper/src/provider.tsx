5da531f0d93bbfe8b729da98fdc3deeb30e380a4
Matthias Haegele <matthias.haegele@mhp.com>
2021-01-13 16:16:28 +0100

    feat(components-react): implement useEventCalback hook | mh / bh | #1044

--- packages/components-react/projects/components-wrapper/src/provider.tsx
+++ packages/components-react/projects/components-wrapper/src/provider.tsx
@@ -14,11 +14,20 @@ export const PorscheDesignSystemProvider = ({ prefix = '', ...props }: PropsWith
   return <PorscheDesignSystemContext.Provider value={{ prefix }} {...props} />;
 };
 
-export const usePrefix = (tagName: string) => {
+export const usePrefix = (tagName: string): string => {
   const { prefix } = useContext(PorscheDesignSystemContext);
 
   if (prefix === undefined) {
     throw new Error('It appears the <PorscheDesignSystemProvider /> is missing. Make sure to wrap your App in it.');
   }
   return prefix ? `${prefix}-${tagName}` : tagName;
+};
+
+export const useEventCallback = (element: HTMLElement, eventName: string, eventHandler: (e: Event) => void): void => {
+  useEffect(() => {
+    if (eventHandler) {
+      element.addEventListener(eventName, eventHandler);
+      return () => element.removeEventListener(eventName, eventHandler);
+    }
+  }, [eventHandler]);
 };
