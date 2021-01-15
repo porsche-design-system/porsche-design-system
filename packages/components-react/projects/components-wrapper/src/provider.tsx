4022c64be4edb934b9e91c743caf28886846bd85
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-15 14:51:56 +0100

    docs(components-react): add todo to provider.tsx | bh | #1044

--- packages/components-react/projects/components-wrapper/src/provider.tsx
+++ packages/components-react/projects/components-wrapper/src/provider.tsx
@@ -9,6 +9,7 @@ type Props = { prefix?: string };
 export const PorscheDesignSystemProvider = ({ prefix = '', ...props }: PropsWithChildren<Props>): JSX.Element => {
   useEffect(() => {
     load({ prefix });
+    // TODO: maybe add prefix to deps array in case it changes
   }, []);
 
   return <PorscheDesignSystemContext.Provider value={{ prefix }} {...props} />;
