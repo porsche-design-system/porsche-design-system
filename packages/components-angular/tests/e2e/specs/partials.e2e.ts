3d03232ea301197dfa789d6942a40b0bb9f78d2a
Anton Schmidt <anton.schmidt@mhp.com>
2021-01-20 10:07:25 +0100

    test(angular|js|react): remove page from "unit" tests, adjust imports to reflect correct paths | as / sas | #1115

--- packages/components-angular/tests/e2e/specs/partials.e2e.ts
+++ packages/components-angular/tests/e2e/specs/partials.e2e.ts
@@ -1,12 +1,6 @@
-import { browser } from '../config';
-import { Page } from 'puppeteer';
-import { getFontFaceStylesheet, getInitialStyles } from '@porsche-design-system/components-js/partials';
+import { getFontFaceStylesheet, getInitialStyles } from '@porsche-design-system/components-angular/partials';
 
 describe('partials', () => {
-  let page: Page;
-  beforeEach(async () => (page = await browser.newPage()));
-  afterEach(async () => await page.close());
-
   describe('getFontFaceStylesheet()', () => {
     it('should be a function', () => {
       expect(typeof getFontFaceStylesheet).toBe('function');
--- packages/components-js/tests/e2e/specs/partials.e2e.ts
+++ packages/components-js/tests/e2e/specs/partials.e2e.ts
@@ -1,12 +1,6 @@
-import { getBrowser } from '../helpers';
-import { Page } from 'puppeteer';
 import { getFontFaceStylesheet, getInitialStyles } from '@porsche-design-system/components-js/partials';
 
 describe('partials', () => {
-  let page: Page;
-  beforeEach(async () => (page = await getBrowser().newPage()));
-  afterEach(async () => await page.close());
-
   describe('getFontFaceStylesheet()', () => {
     it('should be a function', () => {
       expect(typeof getFontFaceStylesheet).toBe('function');
