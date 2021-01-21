9300c0129902d93ad65f1127df13ab44c53af61d
Stephan Schröter <stephan.schroeter@mhp.com>
2021-01-21 15:09:53 +0100

    update(storefront): update partial docs | bh / sas | #1119

--- packages/partials/tests/unit/specs/partials.spec.ts
+++ packages/partials/tests/unit/specs/partials.spec.ts
@@ -7,14 +7,14 @@ describe('getFontFaceStylesheet', () => {
   it('should return link with FONT_FACE_STYLE_CDN_URL', () => {
     const result = getFontFaceStylesheet();
     expect(result).toContain('<link');
-    expect(result).toContain('preconnect');
+    expect(result).toContain('prefetch');
     expect(result).toContain(cdnStyleUrlWithoutHash);
   });
 
   it('should return only href', () => {
     const result = getFontFaceStylesheet({ withoutTags: true });
     expect(result).not.toContain('<link');
-    expect(result).not.toContain('preconnect');
+    expect(result).not.toContain('prefetch');
     expect(result).not.toContain('.cn.');
     expect(result).toContain(cdnStyleUrlWithoutHash);
   });
@@ -22,7 +22,7 @@ describe('getFontFaceStylesheet', () => {
   it('should return only href with cdn: "auto"', () => {
     const result = getFontFaceStylesheet({ withoutTags: true, cdn: 'auto' });
     expect(result).not.toContain('<link');
-    expect(result).not.toContain('preconnect');
+    expect(result).not.toContain('prefetch');
     expect(result).not.toContain('.cn.');
     expect(result).toContain(cdnStyleUrlWithoutHash);
   });
@@ -37,7 +37,7 @@ describe('getFontFaceStylesheet', () => {
     it('should return correct url for china cdn', () => {
       const result = getFontFaceStylesheet({ withoutTags: true, cdn: 'cn' });
       expect(result).not.toContain('<link');
-      expect(result).not.toContain('preconnect');
+      expect(result).not.toContain('prefetch');
       expect(result).toContain(cdnStyleUrlCnWithoutHash);
       expect(result).toContain('.cn.');
     });
@@ -79,15 +79,15 @@ describe('getFontPreloadLink', () => {
   describe('url with tag', () => {
     it('should return default link', () => {
       const result = getFontPreloadLink();
-      expect(result.startsWith('<link rel=preconnect href=')).toBeTruthy();
-      expect(result.endsWith('as=font type=font/woff2 crossorigin>')).toBeTruthy();
+      expect(result.startsWith('<link rel=prefetch href=')).toBeTruthy();
+      expect(result.endsWith('type=font/woff2 crossorigin>')).toBeTruthy();
       expect(result).toContain(cdnFontUrlWithoutHash + 'la-regular');
     });
 
     it('should return default China CDN link', () => {
       const result = getFontPreloadLink({ cdn: 'cn' });
-      expect(result.startsWith('<link rel=preconnect href=')).toBeTruthy();
-      expect(result.endsWith('as=font type=font/woff2 crossorigin>')).toBeTruthy();
+      expect(result.startsWith('<link rel=prefetch href=')).toBeTruthy();
+      expect(result.endsWith('type=font/woff2 crossorigin>')).toBeTruthy();
       expect(result).toContain(cdnFontUrlCnWithoutHash + 'la-regular');
     });
 
@@ -108,17 +108,17 @@ describe('getFontPreloadLink', () => {
       'should return %s subset and %s weight link',
       (subset: 'latin' | 'cyril' | 'greek', weight: 'thin' | 'regular' | 'semi-bold' | 'bold', expected) => {
         const result = getFontPreloadLink({ subset, weight: [weight] });
-        expect(result.startsWith('<link rel=preconnect href=')).toBeTruthy();
-        expect(result.endsWith('as=font type=font/woff2 crossorigin>')).toBeTruthy();
+        expect(result.startsWith('<link rel=prefetch href=')).toBeTruthy();
+        expect(result.endsWith('type=font/woff2 crossorigin>')).toBeTruthy();
         expect(result).toContain(cdnFontUrlWithoutHash + expected);
       }
     );
 
     it('should return multiple links', () => {
       const result = getFontPreloadLink({ weight: ['regular', 'semi-bold'] });
-      expect(result.startsWith('<link rel=preconnect href=')).toBeTruthy();
+      expect(result.startsWith('<link rel=prefetch href=')).toBeTruthy();
       expect(result.endsWith('as=font type=font/woff2 crossorigin>')).toBeTruthy();
-      expect(result).toContain('.woff2 as=font type=font/woff2 crossorigin><link rel=preconnect href=');
+      expect(result).toContain('.woff2 type=font/woff2 crossorigin><link rel=preload href=');
       expect(result).toContain(cdnFontUrlWithoutHash + 'la-regular');
       expect(result).toContain(cdnFontUrlWithoutHash + 'la-semi-bold');
     });
