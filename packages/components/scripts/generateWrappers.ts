d8b855b5e4c79cec2b0358bd0cb3138311fbdcb7
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-25 09:36:23 +0100

    build(components): add basic AngularWrapperGenerator | bh | #1090

--- packages/components/scripts/generateWrappers.ts
+++ packages/components/scripts/generateWrappers.ts
@@ -2,6 +2,7 @@ import { WrapperGenerator } from './wrapper-generator';
 
 const generateWrappers = (): void => {
   const generator = new WrapperGenerator();
+  generator.generate('angular');
   generator.generate('react');
   generator.generate('uxpin');
 };
