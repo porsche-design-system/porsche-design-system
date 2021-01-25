29861201dc9d1040aea7fc7ece2bac474f3fe11c
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-25 12:16:54 +0100

    build(components-angular): adjust public-api.ts, add ProxyCmp decorator | bh | #1090

---
+++ packages/components-angular/projects/components-wrapper/src/utils.ts
@@ -0,0 +1,47 @@
+import { fromEvent } from 'rxjs';
+
+export const proxyInputs = (Cmp: any, inputs: string[]) => {
+  const Prototype = Cmp.prototype;
+  inputs.forEach((item) => {
+    Object.defineProperty(Prototype, item, {
+      get() {
+        console.log('proxyInputs get', item);
+        return this.el[item];
+      },
+      set(val: any) {
+        console.log('proxyInputs set', item, val);
+        this.z.runOutsideAngular(() => (this.el[item] = val));
+      },
+    });
+  });
+};
+
+export const proxyMethods = (Cmp: any, methods: string[]) => {
+  const Prototype = Cmp.prototype;
+  methods.forEach((methodName) => {
+    Prototype[methodName] = function () {
+      const args = arguments;
+      return this.z.runOutsideAngular(() => this.el[methodName].apply(this.el, args));
+    };
+  });
+};
+
+export const proxyOutputs = (instance: any, el: any, events: string[]) => {
+  events.forEach((eventName) => (instance[eventName] = fromEvent(el, eventName)));
+};
+
+// tslint:disable-next-line: only-arrow-functions
+export function ProxyCmp(opts: { inputs?: any; methods?: any }) {
+  console.log('ProxyCmp');
+  const decorator = function (cls: any) {
+    console.log('decorator', cls, opts.inputs, opts.methods);
+    if (opts.inputs) {
+      proxyInputs(cls, opts.inputs);
+    }
+    if (opts.methods) {
+      proxyMethods(cls, opts.methods);
+    }
+    return cls;
+  };
+  return decorator;
+}
