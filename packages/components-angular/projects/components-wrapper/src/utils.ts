d64358370ffd9107a43b10b74e90247dff61f601
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-26 13:47:57 +0100

    feat(components-angular): comment out proxyMethods in ProxyCmp decorator | bh / sas | #1090

--- packages/components-angular/projects/components-wrapper/src/utils.ts
+++ packages/components-angular/projects/components-wrapper/src/utils.ts
@@ -1,46 +1,47 @@
 import { fromEvent } from 'rxjs';
 
-export const proxyInputs = (Cmp: any, inputs: string[]) => {
+const proxyInputs = (Cmp: any, inputs: string[]) => {
   const Prototype = Cmp.prototype;
   inputs.forEach((item) => {
     Object.defineProperty(Prototype, item, {
       get() {
-        // console.log('proxyInputs get', item);
+        // console.log('proxyInputs get', this.el, item, this.el[item], new Date().toISOString());
         return this.el[item];
       },
       set(val: any) {
-        // console.log('proxyInputs set', item, val);
+        // console.log('proxyInputs set', this.el, item, val, new Date().toISOString());
         this.z.runOutsideAngular(() => (this.el[item] = val));
       },
     });
   });
 };
 
-export const proxyMethods = (Cmp: any, methods: string[]) => {
-  const Prototype = Cmp.prototype;
-  methods.forEach((methodName) => {
-    Prototype[methodName] = function () {
-      const args = arguments;
-      return this.z.runOutsideAngular(() => this.el[methodName].apply(this.el, args));
-    };
-  });
-};
+// NOTE: only relevant if components use the @Method() decorator
+// currently our AngularWrapperGenerator doesn't support this
+// const proxyMethods = (Cmp: any, methods: string[]) => {
+//   const Prototype = Cmp.prototype;
+//   methods.forEach((methodName) => {
+//     Prototype[methodName] = function () {
+//       const args = arguments;
+//       return this.z.runOutsideAngular(() => this.el[methodName].apply(this.el, args));
+//     };
+//   });
+// };
 
 export const proxyOutputs = (instance: any, el: any, events: string[]) => {
   events.forEach((eventName) => (instance[eventName] = fromEvent(el, eventName)));
 };
 
 // tslint:disable-next-line: only-arrow-functions
-export function ProxyCmp({ inputs, methods }: { inputs?: any; methods?: any }) {
-  // console.log('ProxyCmp');
+export function ProxyCmp({ inputs /*, methods*/ }: { inputs?: any; methods?: any }) {
   return function (cls: any) {
-    // console.log('ProxyCmp decorator', cls, inputs, methods);
+    // console.log('ProxyCmp decorator', inputs, new Date().toISOString());
     if (inputs) {
       proxyInputs(cls, inputs);
     }
-    if (methods) {
-      proxyMethods(cls, methods);
-    }
+    // if (methods) {
+    //   proxyMethods(cls, methods);
+    // }
     return cls;
   };
 }
