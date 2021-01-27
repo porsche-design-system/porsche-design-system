fc2c84a8c17632a5adc3fb65d5e14acff063358e
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-27 13:17:07 +0100

    feat(components-angular): move module config into AppRoutingModule | bh | #1090

--- packages/components-angular/src/app/app.module.ts
+++ packages/components-angular/src/app/app.module.ts
@@ -1,19 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { PorscheDesignSystemModule } from '@porsche-design-system/components-angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import * as fromPages from './pages';
 
 @NgModule({
   declarations: [AppComponent, ...fromPages.pages],
-  imports: [
-    BrowserModule,
-    PorscheDesignSystemModule,
-    // PorscheDesignSystemModule.forRoot({ prefix: 'my-prefix' }),
-    AppRoutingModule,
-  ],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent],
 })
