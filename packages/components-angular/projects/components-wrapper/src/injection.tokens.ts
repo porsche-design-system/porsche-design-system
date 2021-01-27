d114a3ac0b64111307c83333219b9307505d65a9
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-27 14:45:13 +0100

    feat(components-angular): add static load function to PorscheDesignSystemModule, drop injection tokens | bh | #1090

--- packages/components-angular/projects/components-wrapper/src/injection.tokens.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from '@angular/core';
-
-export type PorscheDesignSystemModuleConfig = {
-  prefix?: string;
-};
-
-export const PORSCHE_DESIGN_SYSTEM_MODULE_CONFIG = new InjectionToken<PorscheDesignSystemModuleConfig>(
-  'Custom config for the Porsche Design System'
-);
