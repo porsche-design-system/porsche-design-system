25fa55468471c78d5ba7ad461b254017e978963e
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-27 08:18:15 +0100

    feat(components-angular): cleanup PorscheDesignSystemModule, add static forRoot method, remove PrefixService and unneeded injection tokens | bh | #1090

--- packages/components-angular/projects/components-wrapper/src/public-api.ts
+++ packages/components-angular/projects/components-wrapper/src/public-api.ts
@@ -4,6 +4,4 @@
 
 export * from './lib/components/barrel';
 export { PorscheDesignSystemModule } from './components-wrapper.module';
-export * from './prefix.service';
-export * from './injection.tokens';
 export { componentsReady } from '@porsche-design-system/components-js';
