b2d8000b2a26dcba1c5cce853306372b899eb71c
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-28 09:21:12 +0100

    test(components-angular): add e2e tests for prefixed component | bh | #1090

--- packages/components-angular/tests/e2e/specs/components.e2e.ts
+++ packages/components-angular/tests/e2e/specs/components.e2e.ts
@@ -1,5 +1,5 @@
 import { ConsoleMessage, ElementHandle, Page } from 'puppeteer';
-import { selectNode, waitForComponentsReady } from '../helpers';
+import { getElementAttr, getOuterHTML, selectNode, waitForComponentsReady } from '../helpers';
 import { browser } from '../config';
 
 const BASE_URL = 'http://localhost:4200';
@@ -35,52 +35,86 @@ describe('components', () => {
     expect(getErrorsAmount()).toBe(1);
   });
 
-  it('should have working events', async () => {
-    await goto('overview');
+  describe('without prefix', () => {
+    it('should have working events', async () => {
+      await goto('overview');
 
-    const tabsBar = await selectNode(page, 'p-tabs-bar');
-    const [firstBtn, secondBtn, thirdBtn] = await tabsBar.$$('button');
+      const tabsBar = await selectNode(page, 'p-tabs-bar');
+      const [firstBtn, secondBtn, thirdBtn] = await tabsBar.$$('button');
 
-    const clickElement = async (el: ElementHandle) => {
-      await el.click();
-      await page.waitForTimeout(50);
-    };
+      const clickElement = async (el: ElementHandle) => {
+        await el.click();
+        await page.waitForTimeout(50);
+      };
 
-    const counterKey = 'TAB_CHANGE_EVENT_COUNTER';
-    const getCounterValue = () => page.evaluate((counterKey: string) => window[counterKey], counterKey);
+      const counterKey = 'TAB_CHANGE_EVENT_COUNTER';
+      const getCounterValue = () => page.evaluate((counterKey: string) => window[counterKey], counterKey);
 
-    await page.evaluate((counterKey: string) => {
-      window[counterKey] = 0;
-      document.querySelector('p-tabs-bar').addEventListener('tabChange', () => {
-        window[counterKey]++;
-      });
-    }, counterKey);
+      await page.evaluate((counterKey: string) => {
+        window[counterKey] = 0;
+        document.querySelector('p-tabs-bar').addEventListener('tabChange', () => {
+          window[counterKey]++;
+        });
+      }, counterKey);
 
-    await clickElement(secondBtn);
-    expect(await getCounterValue()).toBe(1);
+      await clickElement(secondBtn);
+      expect(await getCounterValue()).toBe(1);
 
-    await clickElement(thirdBtn);
-    expect(await getCounterValue()).toBe(2);
+      await clickElement(thirdBtn);
+      expect(await getCounterValue()).toBe(2);
 
-    await clickElement(firstBtn);
-    expect(await getCounterValue()).toBe(3);
+      await clickElement(firstBtn);
+      expect(await getCounterValue()).toBe(3);
+    });
+
+    it('should initialize component deterministically', async () => {
+      await goto('core-initializer');
+      await page.waitForTimeout(1000);
+
+      const [component1, component2] = await page.$$('p-text-field-wrapper');
+
+      const component1HTML = await getOuterHTML(component1);
+      const component2HTML = await getOuterHTML(component2);
+
+      expect(component1HTML).toBe(component2HTML);
+
+      if (component1HTML !== component2HTML) {
+        console.log('component1HTML', component1HTML);
+        console.log('component2HTML', component2HTML);
+      }
+    });
   });
 
-  it('should initialize component deterministically', async () => {
-    await goto('core-initializer');
-    await page.waitForTimeout(1000);
+  fdescribe('with prefix', () => {
+    const regularSelector = 'p-text-field-wrapper';
+    const prefixedSelector = `my-prefix-${regularSelector}`;
 
-    const [component1, component2] = await page.$$('p-text-field-wrapper');
-    const getOuterHTML = (elHandle: ElementHandle) => elHandle.evaluate((el) => el.outerHTML);
+    it('should initialize angular component', async () => {
+      await goto('core-initializer-prefixed');
 
-    const component1HTML = await getOuterHTML(component1);
-    const component2HTML = await getOuterHTML(component2);
+      const prefixedComponent = await selectNode(page, prefixedSelector);
 
-    expect(component1HTML).toBe(component2HTML);
+      expect(await getElementAttr(prefixedComponent, 'ng-reflect-description')).toBe('Some Description');
+      expect(await getElementAttr(prefixedComponent, 'ng-reflect-label')).toBe('Some Label');
+    });
 
-    if (component1HTML !== component2HTML) {
-      console.log('component1HTML', component1HTML);
-      console.log('component2HTML', component2HTML);
-    }
+    it('should initialize component just like without prefix', async () => {
+      await goto('core-initializer-prefixed');
+
+      const regularComponent = await selectNode(page, regularSelector);
+      const prefixedComponent = await selectNode(page, prefixedSelector);
+
+      const component1HTML = await getOuterHTML(regularComponent);
+      const component2HTML = (await getOuterHTML(prefixedComponent))
+        .replace(new RegExp(`(</?)${prefixedSelector}`, 'g'), `$1${regularSelector}`) // normalize selector
+        .replace(` ${regularSelector}=""`, ''); // remove attribute required for prefixed component to be an angular component
+
+      expect(component1HTML).toBe(component2HTML);
+
+      if (component1HTML !== component2HTML) {
+        console.log('component1HTML', component1HTML);
+        console.log('component2HTML', component2HTML);
+      }
+    });
   });
 });
