d7fb9d2ca37db178ba1cdb354eea87e1e8daebae
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-28 15:50:32 +0100

    feat(shared): begin shared package | bh | #1118

---
+++ packages/shared/scripts/generateChunkManifest.ts
@@ -0,0 +1,12 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+type Manifest = {
+  [name: string]: string;
+};
+
+const generateChunkManifest = (): void => {
+  console.log('hi');
+};
+
+generateChunkManifest();
