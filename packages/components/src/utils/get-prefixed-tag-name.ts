9b92341e0fcad4d6c0815152eb7a624edbe544c6
Bernd Hacker <hacker.bernd@gmail.com>
2021-01-29 12:17:41 +0100

    feat(components): use type import for TagName | bh | #1118

--- packages/components/src/utils/get-prefixed-tag-name.ts
+++ packages/components/src/utils/get-prefixed-tag-name.ts
@@ -1,5 +1,6 @@
 import { camelCase } from 'change-case';
-import { TagName, TagNameCamelCase, TAG_NAMES } from '@porsche-design-system/shared';
+import type { TagName } from '@porsche-design-system/shared';
+import { TagNameCamelCase, TAG_NAMES } from '@porsche-design-system/shared';
 
 type PrefixedTagNames = { [key in TagNameCamelCase]: string };
 
