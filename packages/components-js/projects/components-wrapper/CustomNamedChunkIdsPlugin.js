925d95c4b6eb9ff22fb50b2c66acc2abbcc7fe73
Bernd Hacker <hacker.bernd@gmail.com>
2021-02-02 15:06:43 +0100

    build(components-js): add CustomNamedChunkIdsPlugin | bh | #855

---
+++ packages/components-js/projects/components-wrapper/CustomNamedChunkIdsPlugin.js
@@ -0,0 +1,64 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+
+'use strict';
+
+const { compareChunksNatural } = require('webpack/lib/util/comparators');
+const {
+  getShortChunkName,
+  getLongChunkName,
+  assignNames,
+  getUsedChunkIds,
+  assignAscendingChunkIds,
+} = require('webpack/lib/ids/IdHelpers');
+
+/** @typedef {import("../Chunk")} Chunk */
+/** @typedef {import("../Compiler")} Compiler */
+/** @typedef {import("../Module")} Module */
+
+class CustomNamedChunkIdsPlugin {
+  constructor(options) {
+    this.delimiter = (options && options.delimiter) || '$';
+    this.context = options && options.context;
+  }
+
+  /**
+   * Apply the plugin
+   * @param {Compiler} compiler the compiler instance
+   * @returns {void}
+   */
+  apply(compiler) {
+    compiler.hooks.compilation.tap('CustomNamedChunkIdsPlugin', (compilation) => {
+      compilation.hooks.chunkIds.tap('CustomNamedChunkIdsPlugin', (chunks) => {
+        const chunkGraph = compilation.chunkGraph;
+        const context = this.context ? this.context : compiler.context;
+        const delimiter = this.delimiter;
+
+        const unnamedChunks = assignNames(
+          Array.from(chunks).filter((chunk) => {
+            if (chunk.name) {
+              chunk.id = chunk.name;
+              chunk.ids = [chunk.name];
+            }
+            return chunk.id === null;
+          }),
+          (chunk) => getShortChunkName(chunk, chunkGraph, context, delimiter, compiler.root),
+          (chunk) => getLongChunkName(chunk, chunkGraph, context, delimiter, compiler.root),
+          compareChunksNatural(chunkGraph),
+          getUsedChunkIds(compilation),
+          (chunk, name) => {
+            chunk.id = name;
+            chunk.ids = [name];
+          }
+        );
+        if (unnamedChunks.length > 0) {
+          assignAscendingChunkIds(unnamedChunks, compilation);
+        }
+      });
+    });
+  }
+}
+
+module.exports = CustomNamedChunkIdsPlugin;
