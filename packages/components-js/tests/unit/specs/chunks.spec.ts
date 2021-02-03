adb7e74ba8032fd0f2392314e8d3e46ce0dead89
Bernd Hacker <hacker.bernd@gmail.com>
2021-02-03 13:55:46 +0100

    test(components-js): expect chunk size to change less than 500 bytes | bh | #855

--- packages/components-js/tests/unit/specs/chunks.spec.ts
+++ packages/components-js/tests/unit/specs/chunks.spec.ts
@@ -7,13 +7,13 @@ describe('chunks', () => {
   const resultsDir = path.resolve(baseDir, 'results');
   const statsFileName = 'stats.json';
 
-  type StatsOutput = {
+  type StatsResult = {
     chunkName: string;
     oldSize: number;
     newSize: number;
     diffSize: number;
   };
-  const statsOutputs: StatsOutput[] = [];
+  const statsResults: StatsResult[] = [];
 
   type Stats = {
     [key: string]: any;
@@ -44,16 +44,19 @@ describe('chunks', () => {
       const assetFixture = statsFixture.assets.find((x) => x.chunks[0] === chunkName);
       const { size: oldSize } = assetFixture;
 
-      const statsOutput: StatsOutput = {
+      const stat: StatsResult = {
         chunkName,
         oldSize,
         newSize,
         diffSize: newSize - oldSize,
       };
 
-      statsOutputs.push(statsOutput);
+      statsResults.push(stat);
 
-      it(`chunk size should roughly stay the same for ${statsOutput.chunkName}`, () => {});
+      it(`chunk size should roughly stay the same for ${stat.chunkName}`, () => {
+        // change should be less than 500 Bytes
+        expect(stat.diffSize).toBeLessThanOrEqual(500);
+      });
     });
 
   afterAll(() => {
@@ -61,29 +64,30 @@ describe('chunks', () => {
     const formatNumberCol = (content: string, fillString?: string): string => content.padStart(12, fillString);
     const formatKB = (input: number): string => `${(input / 1024).toFixed(2)} KB`;
     const formatDiff = (oldSize: number, diffSize: number): string =>
-      `${oldSize < 0 ? '-' : '+'} ${(diffSize / oldSize).toFixed(2)} %`;
+      `${oldSize < 0 ? '-' : '+'}${(diffSize / oldSize).toFixed(2)} %`;
 
-    const header = ['chunkName', 'oldSize', 'newSize', 'diffSize'];
+    const header = ['chunkName', 'oldSize', 'newSize', 'diffSize', 'diff %'];
 
     const tableHead = [
       header.map((x, idx) => (idx === 0 ? formatFirstCol(x) : formatNumberCol(x))),
-      Array.from(Array(4)).map((_, idx) => (idx === 0 ? formatFirstCol('', '-') : formatNumberCol('', '-'))),
+      Array.from(Array(5)).map((_, idx) => (idx === 0 ? formatFirstCol('', '-') : formatNumberCol('', '-'))),
     ]
       .map((arr) => arr.join(''))
       .join('\n');
 
-    const tableBody = statsOutputs
+    const tableBody = statsResults
       .map(({ chunkName, oldSize, newSize, diffSize }) =>
         [
           formatFirstCol(chunkName),
           formatNumberCol(`${formatKB(oldSize)}`),
           formatNumberCol(`${formatKB(newSize)}`),
+          formatNumberCol(`${formatKB(diffSize)}`),
           formatNumberCol(`${formatDiff(oldSize, diffSize)}`),
         ].join('')
       )
       .join('\n');
 
-    const output = [`Summary for ${statsOutputs.length} chunks`, '', tableHead, tableBody].join('\n');
+    const output = ['', `Summary for ${statsResults.length} chunks`, '', tableHead, tableBody].join('\n');
     console.log(output);
   });
 });
