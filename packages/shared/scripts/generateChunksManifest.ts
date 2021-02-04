bb59c43881d0061dc9ad73585d6dbc82bfc13a16
Bernd Hacker <hacker.bernd@gmail.com>
2021-02-04 15:12:54 +0100

    feat(shared): generate chunks manifest for dev build | bh / sas | #1118

--- packages/shared/scripts/generateChunksManifest.ts
+++ packages/shared/scripts/generateChunksManifest.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import type { TagName } from '../src';
 import { paramCase } from 'change-case';
+import type { TagName } from '../src';
 
 type Manifest = {
   [key in TagName | 'core']?: string;
@@ -11,11 +11,7 @@ const createManifest = (indexJsFile: string): Manifest => {
   // read web components manager to retrieve url to stencil core entrypoint
   const indexJsCode = fs.readFileSync(indexJsFile, 'utf8');
 
-  if (indexJsCode.includes('localhost:3001')) {
-    throw new Error('You need to run `yarn build:components-js-prod` in order to have a prod build.');
-  }
-
-  const [, coreFileName] = /porsche-design-system\/components\/(porsche-design-system\.v.*\.js)/.exec(indexJsCode);
+  const [, coreFileName] = /\/components\/(porsche-design-system\.v.*\.js)/.exec(indexJsCode);
 
   // read stencil core entrypoint to retrieve component chunk mapping
   const chunksDir = path.resolve(indexJsFile, '../../components');
@@ -42,15 +38,9 @@ const createManifest = (indexJsFile: string): Manifest => {
 };
 
 const generateChunksManifest = (): void => {
-  let manifest: Manifest = {}; // fallback
-
   const packageName = '@porsche-design-system/components-js';
-  try {
-    const indexJsFile = require.resolve(packageName);
-    manifest = createManifest(indexJsFile);
-  } catch (e) {
-    console.log(`Error: ${packageName} can't be resolved, so manifest will be empty`);
-  }
+  const indexJsFile = require.resolve(packageName);
+  const manifest = createManifest(indexJsFile);
 
   const chunkNames = Object.keys(manifest).filter((chunkName) => chunkName !== 'core');
   const content = `export const COMPONENT_CHUNKS_MANIFEST = ${JSON.stringify(manifest)};
