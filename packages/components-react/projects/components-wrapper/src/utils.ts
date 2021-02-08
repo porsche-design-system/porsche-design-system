fd476bf91abaa865d502bd399451cb23a1252c07
Bernd Hacker <hacker.bernd@gmail.com>
2021-02-08 14:10:09 +0100

    refactor(components/components-react): split providers into hooks and utils | bh | #1168

---
+++ packages/components-react/projects/components-wrapper/src/utils.ts
@@ -0,0 +1,37 @@
+import type { ForwardedRef, MutableRefObject } from 'react';
+
+const splitToArray = (str: string) => str.split(' ').filter((str) => str);
+
+export const getMergedClassName = (
+  domClassName: string,
+  oldClassName: string = '',
+  newClassName: string = ''
+): string => {
+  // classes previously set by component
+  const prevComponentClassNames = splitToArray(oldClassName);
+
+  // all classes not set by component -> to keep hydrated class and other classes set on host element
+  // (usually dom-manipulated class additions would be lost on rerender)
+  let domClasses = splitToArray(domClassName);
+  if (prevComponentClassNames.length > 0) {
+    domClasses = domClasses.filter((x) => !prevComponentClassNames.includes(x));
+  }
+
+  // all classes set by component
+  const componentClasses = splitToArray(newClassName);
+
+  return componentClasses.concat(domClasses).join(' ');
+};
+
+export const jsonStringify = (value: any) => (typeof value === 'object' ? JSON.stringify(value) : value);
+
+export const syncRef = (ref: ForwardedRef<HTMLElement>, elementRef: MutableRefObject<HTMLElement>) => (
+  el: HTMLElement
+): void => {
+  elementRef.current = el;
+  if (typeof ref === 'function') {
+    ref(el);
+  } else if (ref !== null) {
+    ref.current = el;
+  }
+};
