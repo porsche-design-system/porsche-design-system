62bbdb03576988485e50c2c1ba7a7fb5c66bd3f3
Stephan Schröter <stephan.schroeter@mhp.com>
2021-02-17 14:47:24 +0100

    simplify components-manager-core package | mh / sas | #1102

--- packages/components-js/projects/components-manager-core/jest.config.js
+++ packages/components-js/projects/components-manager-core/jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.js?$': require.resolve('babel-jest'),
-  },
+  verbose: true,
+  rootDir: './',
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   globals: {
     'ts-jest': {
       isolatedModules: true, // this fixes typing issues with jasmine
