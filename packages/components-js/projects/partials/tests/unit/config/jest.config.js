dbc33875deb95e2531670febeb49f38e4061d249
Matthias Haegele <matthias.haegele@mhp.com>
2021-02-22 16:06:39 +0100

    test(components-js): add isolatedModules to fix typing errors | mh | #1102

--- packages/components-js/projects/partials/tests/unit/config/jest.config.js
+++ packages/components-js/projects/partials/tests/unit/config/jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
   testEnvironment: 'node',
   verbose: true,
   rootDir: '../../../',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true, // this fixes typing issues with jasmine
+    },
+  },
   testMatch: ['**/tests/unit/specs/**/*.+(ts|tsx|js)'],
 };
