64db91606f075bcb98a47730eaf052d4c025de31
Matthias Haegele <matthias.haegele@mhp.com>
2021-02-22 12:17:21 +0100

    test(components-js): remove custom test matcher | mh | #1102

--- packages/components-js/projects/components-manager-core/jest.config.js
+++ packages/components-js/projects/components-manager-core/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   verbose: true,
   rootDir: './',
-  testMatch: ['<rootDir>/src/**/*.spec.ts'],
   globals: {
     'ts-jest': {
       isolatedModules: true, // this fixes typing issues with jasmine
