0e7b5ba0de209c5087db715fcf5103b3dafa6aeb
Matthias Haegele <matthias.haegele@mhp.com>
2021-02-25 14:12:38 +0100

    test(components): refactor applyFilterOnOptionMaps unit test | mh / bh | #1210

--- packages/components/tests/unit/components/select-wrapper.spec.ts
+++ packages/components/tests/unit/components/select-wrapper.spec.ts
@@ -1,6 +1,7 @@
 import * as domUtils from '../../../src/utils/dom';
 import { SelectWrapper } from '../../../src/components/form/select-wrapper/select-wrapper';
-import { applyFilterOnOptionMaps } from '../../../src/components/form/select-wrapper/select-wrapper-utils';
+import { applyFilterOnOptionMaps, OptionMap } from '../../../src/components/form/select-wrapper/select-wrapper-utils';
+import { getMergedClassName } from '../../../../components-react/projects/components-wrapper/src/utils';
 
 describe('select-wrapper', () => {
   it('should call getHTMLElementAndThrowIfUndefined() via connectedCallback', () => {
@@ -14,7 +15,7 @@ describe('select-wrapper', () => {
   });
 
   describe('applyFilterOnOptionMaps', () => {
-    const defaultOptionMapValues = {
+    const defaultOptionMapValues: Partial<OptionMap> = {
       disabled: false,
       hidden: false,
       initiallyHidden: false,
@@ -22,51 +23,29 @@ describe('select-wrapper', () => {
       highlighted: false,
     };
 
-    const optionMaps = [
-      {
-        key: 1,
-        value: 'First Value',
-        ...defaultOptionMapValues,
-      },
-      {
-        key: 2,
-        value: 'Second Value',
-        ...defaultOptionMapValues,
-      },
-      {
-        key: 3,
-        value: 'Third Value',
-        ...defaultOptionMapValues,
-      },
-    ];
+    const optionMaps: OptionMap[] = ['First', 'Second', 'Third'].map(
+      (value, index) =>
+        ({
+          ...defaultOptionMapValues,
+          key: index + 1,
+          value: value + ' Value',
+        } as OptionMap)
+    );
 
-    const getHiddenAmount = (maps: typeof optionMaps) => {
-      let amount = 0;
-      maps.map((item) => {
-        if (item.hidden === true) amount += 1;
-      });
-      return amount;
+    const getVisibleOptions = (optionMap: OptionMap[]): number => {
+      return optionMap.filter((item) => !item.hidden).length;
     };
 
-    it('should match only items with full match ', () => {
-      const result = applyFilterOnOptionMaps(optionMaps, 'First Value');
-      expect(getHiddenAmount(result)).toBe(2);
-    });
-    it('should match all items with upper case', () => {
-      const result = applyFilterOnOptionMaps(optionMaps, 'Value');
-      expect(getHiddenAmount(result)).toBe(0);
-    });
-    it('should match all items with lower case', () => {
-      const result = applyFilterOnOptionMaps(optionMaps, 'value');
-      expect(getHiddenAmount(result)).toBe(0);
-    });
-    it('should match items with partial search', () => {
-      const result = applyFilterOnOptionMaps(optionMaps, 'ir');
-      expect(getHiddenAmount(result)).toBe(1);
-    });
-    it('should match items with space in search value', () => {
-      const result = applyFilterOnOptionMaps(optionMaps, 'st Val');
-      expect(getHiddenAmount(result)).toBe(2);
+    it.each`
+      searchString     | expected
+      ${'First Value'} | ${1}
+      ${'Value'}       | ${3}
+      ${'value'}       | ${3}
+      ${'ir'}          | ${2}
+      ${'st Val'}      | ${1}
+    `("should be called with ('$searchString') and have '$expected' visible options", ({ searchString, expected }) => {
+      const result = applyFilterOnOptionMaps(optionMaps, searchString);
+      expect(getVisibleOptions(result)).toBe(expected);
     });
   });
 });
