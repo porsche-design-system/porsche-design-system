b8a09c7538ea01b64e47ef8c808329bb1d7db342
Matthias Haegele <matthias.haegele@mhp.com>
2021-02-25 10:02:59 +0100

    feat(nextjs): setup new nextjs project | mh | #1009

---
+++ packages/nextjs/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { PHeadline } from '@porsche-design-system/components-react';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <PHeadline>Porsche Design System Headline</PHeadline>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+}
