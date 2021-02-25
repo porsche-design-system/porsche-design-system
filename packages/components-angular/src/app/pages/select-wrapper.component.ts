4e118e979a50a045934fac9a3a41391d219fd9ba
Matthias Haegele <matthias.haegele@mhp.com>
2021-02-25 10:28:33 +0100

    test(components-js/react/angular): extend padding for vrt test / update fixtures / remove e2e test | mh / sas | #1210

--- packages/components-angular/src/app/pages/select-wrapper.component.ts
+++ packages/components-angular/src/app/pages/select-wrapper.component.ts
@@ -442,7 +442,7 @@ import { Component } from '@angular/core';
     <div
       class="playground light"
       title="should render in focus state and be open"
-      style="padding-bottom: calc(1rem + 310px)"
+      style="padding-bottom: calc(1rem + 340px)"
     >
       <p-select-wrapper [label]="'Some label'">
         <select id="open-options" name="some-name">
