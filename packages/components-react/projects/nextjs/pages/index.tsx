6ff7301124b5a79b0faa86147402ba4a2241f643
Matthias Haegele <matthias.haegele@mhp.com>
2021-02-26 08:52:36 +0100

    chore: remove nextjs project |  mh | #1009

--- packages/components-react/projects/nextjs/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import { PorscheDesignSystemProvider } from '@porsche-design-system/components-react';
-import OverviewPage from './Overview';
-
-export default function Home() {
-  return <OverviewPage />;
-}
