4a0559beb3cd1f4ad243f8af0cfc0bac16963ec1
Bernd Hacker <hacker.bernd@gmail.com>
2021-03-04 09:11:01 +0100

    feat(components): change grid-item to be base on host selectors only | bh | #961

--- packages/components/src/components/layout/grid/grid-item/grid-item.tsx
+++ packages/components/src/components/layout/grid/grid-item/grid-item.tsx
@@ -1,5 +1,4 @@
 import { JSX, Component, Host, Prop, h } from '@stencil/core';
-import { mapBreakpointPropToPrefixedClasses } from '../../../../utils';
 import type { BreakpointCustomizable } from '../../../../types';
 
 @Component({
@@ -15,13 +14,8 @@ export class GridItem {
   @Prop() public offset?: BreakpointCustomizable<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11> = 0;
 
   public render(): JSX.Element {
-    const gridItemClasses = {
-      ...mapBreakpointPropToPrefixedClasses('size', this.size),
-      ...(this.offset !== 0 && mapBreakpointPropToPrefixedClasses('offset', this.offset)),
-    };
-
     return (
-      <Host class={gridItemClasses}>
+      <Host>
         <slot />
       </Host>
     );
