5abfdb18d345dd5c342ed12e6a2fadfe77179aa4
Bernd Hacker <hacker.bernd@gmail.com>
2021-03-08 14:59:35 +0100

    feat(components): change grid-item to use jss | bh | #961

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -6,6 +6,7 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 import { BannerState, BreakpointCustomizable, ButtonType, ButtonVariant, FormState, IconName, LinkTarget, LinkVariant, NumberOfPageLinks, PageChangeEvent, TabChangeEvent, TabGradientColorTheme, TabSize, TabWeight, TextAlign, TextColor, TextSize, TextWeight, Theme } from "./types";
+import { GridOffset, GridSize } from "./components/layout/grid/grid-item/grid-item-utils";
 import { HeadlineTag, HeadlineVariant } from "./components/basic/typography/headline/headline-utils";
 export namespace Components {
     interface PBanner {
@@ -222,11 +223,11 @@ export namespace Components {
         /**
           * The offset of the column. Can be between 0 and 11. Also defines the offset of the column for specific breakpoints, like {base: 6, l: 3}. You always need to provide a base value when doing this.
          */
-        "offset"?: BreakpointCustomizable<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11>;
+        "offset"?: GridOffset;
         /**
           * The size of the column. Can be between 1 and 12. Also defines the size of the column for specific breakpoints, like {base: 6, l: 3}. You always need to provide a base value when doing this.
          */
-        "size"?: BreakpointCustomizable<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12>;
+        "size"?: GridSize;
     }
     interface PHeadline {
         /**
@@ -1137,11 +1138,11 @@ declare namespace LocalJSX {
         /**
           * The offset of the column. Can be between 0 and 11. Also defines the offset of the column for specific breakpoints, like {base: 6, l: 3}. You always need to provide a base value when doing this.
          */
-        "offset"?: BreakpointCustomizable<0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11>;
+        "offset"?: GridOffset;
         /**
           * The size of the column. Can be between 1 and 12. Also defines the size of the column for specific breakpoints, like {base: 6, l: 3}. You always need to provide a base value when doing this.
          */
-        "size"?: BreakpointCustomizable<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12>;
+        "size"?: GridSize;
     }
     interface PHeadline {
         /**
---
+++ packages/components/src/components/layout/grid/grid-item/grid-item-utils.ts
@@ -0,0 +1,99 @@
+import { breakpoint, mediaQuery, pxToRem } from '@porsche-design-system/utilities';
+import type { Styles } from '../../../../utils';
+import { attachCss, getCss } from '../../../../utils';
+import type { BreakpointCustomizable } from '../../../../types';
+
+const GRID_SIZES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+type GridSizeType = typeof GRID_SIZES[number];
+export type GridSize = BreakpointCustomizable<GridSizeType>;
+
+const GRID_OFFSETS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+type GridOffsetType = typeof GRID_OFFSETS[number];
+export type GridOffset = BreakpointCustomizable<GridOffsetType>;
+
+const paddingBase = `${parseInt(pxToRem('16px')) / 2}rem !important`;
+const paddingS = `${parseInt(pxToRem('24px')) / 2}rem !important`;
+const paddingM = `${parseInt(pxToRem('36px')) / 2}rem !important`;
+
+export const baseCss: string = getCss({
+  ':host': {
+    display: 'block',
+    boxSizing: 'border-box !important',
+    paddingLeft: paddingBase,
+    paddingRight: paddingBase,
+  },
+  [mediaQuery('s')]: {
+    ':host': {
+      paddingLeft: paddingS,
+      paddingRight: paddingS,
+    },
+  },
+  [mediaQuery('m')]: {
+    ':host': {
+      paddingLeft: paddingM,
+      paddingRight: paddingM,
+    },
+  },
+});
+
+const gridItemWidths = [
+  0,
+  8.333333,
+  16.666667,
+  25,
+  33.333333,
+  41.666667,
+  50,
+  58.333333,
+  66.666667,
+  75,
+  83.333333,
+  91.666667,
+  100,
+];
+
+const getSizeStyles = (size: GridSizeType): Styles => ({
+  width: `${gridItemWidths[size]}% !important`,
+});
+
+const getOffsetStyles = (offset: GridOffsetType): Styles => ({
+  marginLeft: `${gridItemWidths[offset]}% !important`,
+});
+
+const getJssX = <T>(rawValue: T, getStyles: (x: T) => Styles): Styles => {
+  // TODO: stop using eval
+  const value: BreakpointCustomizable<T> = eval(`(${rawValue})`);
+
+  return typeof value === 'number'
+    ? {
+        ':host': getStyles(value),
+      }
+    : Object.keys(value)
+        .filter((key) => key !== 'base')
+        .reduce(
+          (res, bp) => ({
+            ...res,
+            [mediaQuery(breakpoint[bp])]: {
+              ':host': getStyles(value[bp]),
+            },
+          }),
+          {
+            ':host': getStyles(value['base']),
+          }
+        );
+};
+
+const getJss = (size: GridSize, offset: GridOffset): Styles => {
+  const jss = getJssX(size, getSizeStyles);
+
+  for (const [key, styles] of Object.entries(getJssX(offset, getOffsetStyles))) {
+    jss[key] = Object.assign({}, jss[key], styles);
+  }
+
+  return jss;
+};
+
+export const addCss = (host: HTMLElement, size: GridSize, offset: GridOffset): void => {
+  const dynamicCss = getCss(getJss(size, offset));
+  attachCss(host, baseCss + dynamicCss);
+};
