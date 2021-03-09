160b6b06d0ab631a2b7642c80fffb081607ac162
Matthias Haegele <matthias.haegele@mhp.com>
2021-03-09 11:48:04 +0100

    test(components): add undefined unit tests | mh | #47

--- packages/components/tests/unit/components/spinner.spec.ts
+++ packages/components/tests/unit/components/spinner.spec.ts
@@ -6,9 +6,11 @@ describe('spinner', () => {
       size                               | warningAmount
       ${'small'}                         | ${0}
       ${'{ base: "small", l: "large" }'} | ${0}
+      ${undefined}                       | ${0}
+      ${{ base: undefined, l: 'large' }} | ${0}
       ${'foo'}                           | ${1}
-      ${'{ foo: "small", l: "large" }'}  | ${1}
-      ${'{ base: "foo", l: "large" }'}   | ${1}
+      ${{ foo: 'small', l: 'large' }}    | ${1}
+      ${{ base: 'foo', l: 'large' }}     | ${1}
     `("should be called with ('$size') and throw '$warningAmount' warning", ({ size, warningAmount }) => {
       const spy = jest.spyOn(global.console, 'warn').mockImplementation(() => {});
 
