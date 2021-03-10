8b1c32e96aa045dcbd514e7c5976f989b9f813f4
Matthias Haegele <matthias.haegele@mhp.com>
2021-03-10 14:33:44 +0100

    feat(components): add 'export type' / change visualization of props without <Partial | mh | #1082

--- packages/components/src/components/navigation/tabs-bar/tabs-bar-utils.ts
+++ packages/components/src/components/navigation/tabs-bar/tabs-bar-utils.ts
@@ -1,4 +1,4 @@
-import { TextWeight } from '../../../types';
+import type { TextWeight } from '../../../types';
 
 const TAB_SIZE = ['small', 'medium'] as const;
 export type TabSize = typeof TAB_SIZE[number];
