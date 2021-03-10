51a3b60587a36e2c1f98691528bd404e2924a458
Anton Schmidt <anton.schmidt@mhp.com>
2021-03-10 15:51:01 +0100

    feat(components): extract scrollPosition helper functions, add unit tests, rename toggleEnableTransistionClass, adjust tabs-bar page | as | #961

--- packages/components/src/components/navigation/tabs-bar/tabs-bar-utils.ts
+++ packages/components/src/components/navigation/tabs-bar/tabs-bar-utils.ts
@@ -1,6 +1,9 @@
 import { pxToRem } from '@porsche-design-system/utilities';
 import { prefix } from '../../../utils';
 
+export type Direction = 'prev' | 'next';
+export const FOCUS_PADDING_WIDTH = 4;
+
 export const sanitizeActiveTabIndex = (index: number, tabElementsCount: number): number => {
   if (index === undefined) {
     return undefined;
@@ -32,7 +35,7 @@ export const removeEnableTransitionClass = (statusBarElement: HTMLElement): void
   statusBarElement.classList.remove(prefix('tabs-bar__status-bar--enable-transition'));
 };
 
-export const toggleEnableTransitionClass = (
+export const determineEnableTransitionClass = (
   activeTabIndex: number,
   prevActiveTabIndex: number,
   statusBarElement: HTMLElement
@@ -44,4 +47,61 @@ export const toggleEnableTransitionClass = (
     // active to active
     addEnableTransitionClass(statusBarElement);
   }
+};
+
+export const getScrollActivePosition = (
+  direction: Direction,
+  activeTabIndex: number,
+  tabElementsCount: number,
+  scrollAreaOffsetWidth: number,
+  offsetLeft: number,
+  prevGradientWidth: number,
+  offsetWidth: number,
+  nextGradientWidth: number
+): number => {
+  let scrollPosition;
+  if (direction === 'next') {
+    if (activeTabIndex === tabElementsCount - 1) {
+      // go to last tab
+      scrollPosition = offsetLeft - FOCUS_PADDING_WIDTH;
+    } else {
+      // go to next tab
+      scrollPosition = offsetLeft - prevGradientWidth + FOCUS_PADDING_WIDTH * 2;
+    }
+  } else {
+    if (activeTabIndex === 0) {
+      // go to first tab
+      scrollPosition = 0;
+    } else {
+      // go to prev tab
+      scrollPosition = offsetLeft + offsetWidth + nextGradientWidth - scrollAreaOffsetWidth;
+    }
+  }
+  return scrollPosition;
+};
+
+export const getScrollPositionAfterPrevNextClick = (
+  direction: string,
+  currentScrollPosition: number,
+  scrollToStep: number,
+  scrollToMax: number,
+  scrollToMin: number
+): number => {
+  let scrollPosition: number;
+  if (direction === 'next') {
+    // Go to end of scroll-area when close to edge
+    if (currentScrollPosition + scrollToStep * 2 > scrollToMax) {
+      scrollPosition = scrollToMax;
+    } else {
+      scrollPosition = currentScrollPosition + scrollToStep;
+    }
+  } else {
+    // Go to start of scroll-area when close to edge
+    if (currentScrollPosition - scrollToStep * 2 < scrollToMin) {
+      scrollPosition = scrollToMin;
+    } else {
+      scrollPosition = currentScrollPosition - scrollToStep;
+    }
+  }
+  return scrollPosition;
 };
