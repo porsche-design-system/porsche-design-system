67ace167af5a641806b98eb8e091faf94a811183
Matthias Haegele <matthias.haegele@mhp.com>
2021-03-10 14:38:19 +0100

    test(storefront): extend with vrt for props-table Button | mh | #1082

--- packages/storefront/tests/vrt/specs/prop-table-tabs-bar.vrt.ts
+++ packages/storefront/tests/vrt/specs/prop-table-tabs-bar.vrt.ts
@@ -1,11 +1,11 @@
 import { getVisualRegressionTesterPropTable } from '../helpers/setup';
 
-describe('Prop Table', () => {
+describe('Prop Table Tabs-Bar', () => {
   it('should have no visual regression', async () => {
     const vrt = getVisualRegressionTesterPropTable();
     expect(
       await vrt.test(
-        'prop-table',
+        'prop-table-tabs-bar',
         async () => {
           await vrt.goTo('/#/components/tabs-bar#props');
         },
