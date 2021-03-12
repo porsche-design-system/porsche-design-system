92dec529796c4311967c408a35938dc78e621226
Bernd Hacker <hacker.bernd@gmail.com>
2021-03-12 13:06:05 +0100

    feat(components): add caching to getPrefixedTagNames | bh

--- packages/components/src/utils/get-prefixed-tag-name.ts
+++ packages/components/src/utils/get-prefixed-tag-name.ts
@@ -4,12 +4,18 @@ import { getTagName, paramCaseToCamelCase } from '.';
 
 type PrefixedTagNames = { [key in TagNameCamelCase]: string };
 
-// TODO: caching
+export const CACHE = new Map<string, PrefixedTagNames>();
+
 export const getPrefixedTagNames = (host: HTMLElement): PrefixedTagNames => {
-  const [, prefix = ''] = /^(.*-)p-(.*)$/.exec(getTagName(host)) ?? [];
-  const tagNames: PrefixedTagNames = {} as PrefixedTagNames;
-  for (const tag of TAG_NAMES) {
-    tagNames[paramCaseToCamelCase(tag)] = `${prefix}${tag}`;
+  const [, prefix = ''] = /^(.*)-p-(.*)$/.exec(getTagName(host)) || [];
+
+  if (!CACHE.has(prefix)) {
+    const tagNames: PrefixedTagNames = {} as PrefixedTagNames;
+    for (const tag of TAG_NAMES) {
+      tagNames[paramCaseToCamelCase(tag)] = prefix ? `${prefix}-${tag}` : tag;
+    }
+    CACHE.set(prefix, tagNames);
   }
-  return tagNames;
+
+  return CACHE.get(prefix);
 };
