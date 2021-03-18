8d5188f55933162b585c2ca24b0c995489f45c2f
Matthias Haegele <matthias.haegele@mhp.com>
2021-03-18 15:57:52 +0100

    feat(components): add breakpoint classes to scss | mh / as | #1112

--- packages/components/src/components/content/modal/modal-utils.ts
+++ packages/components/src/components/content/modal/modal-utils.ts
@@ -11,7 +11,7 @@ export const getFocusableElements = (host: HTMLElement, closeButton: HTMLElement
   return [closeButton].concat(getHTMLElements(host, selector));
 };
 
-export const setScrollLock = (lock: boolean, host: HTMLElement): void => {
+export const setScrollLock = (host: HTMLElement, lock: boolean): void => {
   document.body.style.overflow = lock ? 'hidden' : '';
 
   // prevent scrolling of background on iOS
@@ -19,11 +19,11 @@ export const setScrollLock = (lock: boolean, host: HTMLElement): void => {
     const addOrRemoveEventListener = lock ? 'addEventListener' : 'removeEventListener';
     document[addOrRemoveEventListener]('touchmove', (e: TouchEvent) => e.preventDefault(), false);
     // eslint-disable-next-line @typescript-eslint/unbound-method
-    host[addOrRemoveEventListener]('touchmove', (e: TouchEvent) => handleHostTouchMove(e, host));
+    host[addOrRemoveEventListener]('touchmove', (e: TouchEvent) => handleHostTouchMove(host, e));
   }
 };
 
-export const handleHostTouchMove = (e: TouchEvent, host: HTMLElement): void => {
+export const handleHostTouchMove = (host: HTMLElement, e: TouchEvent): void => {
   // Source: https://stackoverflow.com/a/43860705
   const { scrollTop, scrollHeight, offsetHeight } = host;
   const currentScroll = scrollTop + offsetHeight;
