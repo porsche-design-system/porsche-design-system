d42d8c5d868bf60b03765d0c236fa4d07c171ed1
Matthias Haegele <matthias.haegele@mhp.com>
2021-03-22 10:13:53 +0100

    test(components): remove spy type casting | mh | #1112

--- packages/components/tests/unit/components/device-detection.spec.ts
+++ packages/components/tests/unit/components/device-detection.spec.ts
@@ -7,28 +7,22 @@ describe('Device Detection', () => {
 
   describe('isTouchDevice', () => {
     it('should detect touch device ', () => {
-      windowSpy.mockImplementation(
-        () =>
-          (({
-            ontouchstart: true,
-            navigator: {
-              maxTouchPoints: 1,
-            },
-          } as unknown) as Window & typeof globalThis)
-      );
+      windowSpy.mockImplementation(() => ({
+        ontouchstart: true,
+        navigator: {
+          maxTouchPoints: 1,
+        },
+      }));
 
       expect(isTouchDevice()).toBe(true);
     });
 
     it('should detect non touch device', () => {
-      windowSpy.mockImplementation(
-        () =>
-          (({
-            navigator: {
-              maxTouchPoints: 0,
-            },
-          } as unknown) as Window & typeof globalThis)
-      );
+      windowSpy.mockImplementation(() => ({
+        navigator: {
+          maxTouchPoints: 0,
+        },
+      }));
 
       expect(isTouchDevice()).toBe(false);
     });
@@ -36,55 +30,43 @@ describe('Device Detection', () => {
 
   describe('isIos', () => {
     it('should detect mobile', () => {
-      windowSpy.mockImplementation(
-        () =>
-          ({
-            navigator: {
-              platform: 'iPhone',
-            },
-          } as Window & typeof globalThis)
-      );
+      windowSpy.mockImplementation(() => ({
+        navigator: {
+          platform: 'iPhone',
+        },
+      }));
 
       expect(isIos()).toBe(true);
     });
 
     it('should detect macIntel', () => {
-      windowSpy.mockImplementation(
-        () =>
-          ({
-            navigator: {
-              platform: 'MacIntel',
-              maxTouchPoints: 2,
-            },
-          } as Window & typeof globalThis)
-      );
+      windowSpy.mockImplementation(() => ({
+        navigator: {
+          platform: 'MacIntel',
+          maxTouchPoints: 2,
+        },
+      }));
 
       expect(isIos()).toBe(true);
     });
 
     it('should be false on windows desktop device', () => {
-      windowSpy.mockImplementation(
-        () =>
-          ({
-            navigator: {
-              platform: 'Win32',
-            },
-          } as Window & typeof globalThis)
-      );
+      windowSpy.mockImplementation(() => ({
+        navigator: {
+          platform: 'Win32',
+        },
+      }));
 
       expect(isIos()).toBe(false);
     });
 
     it('should be false on windows touchdevice', () => {
-      windowSpy.mockImplementation(
-        () =>
-          ({
-            navigator: {
-              platform: 'Win32',
-              maxTouchPoints: 2,
-            },
-          } as Window & typeof globalThis)
-      );
+      windowSpy.mockImplementation(() => ({
+        navigator: {
+          platform: 'Win32',
+          maxTouchPoints: 2,
+        },
+      }));
 
       expect(isIos()).toBe(false);
     });
