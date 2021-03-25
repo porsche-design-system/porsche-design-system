313ed1681ed5cab2d52033c8de6ab9f253ffbf68
Anton Schmidt <anton.schmidt@mhp.com>
2021-03-25 12:20:09 +0100

    test(components-angular/js/react): update vrt tests | as / sas | #1260

--- packages/components-react/src/pages/ButtonGroup.tsx
+++ packages/components-react/src/pages/ButtonGroup.tsx
@@ -1,4 +1,4 @@
-import { PButton, PButtonGroup } from '@porsche-design-system/components-react';
+import { PButton, PButtonGroup, PButtonPure } from '@porsche-design-system/components-react';
 
 export const ButtonGroupPage = (): JSX.Element => {
   return (
@@ -11,11 +11,40 @@ export const ButtonGroupPage = (): JSX.Element => {
         </PButtonGroup>
       </div>
 
+      <div className="playground light" title="should show PButtonGroup default style when direction is undefined">
+        <PButtonGroup direction={undefined}>
+          <PButton variant="primary">Some label</PButton>
+          <PButton variant="secondary">Some label</PButton>
+          <PButton variant="tertiary">Some label</PButton>
+        </PButtonGroup>
+      </div>
+
       <div className="playground light" title="should render button-group with responsive direction">
         <PButtonGroup direction={{ base: 'row', xs: 'column', s: 'row', m: 'column', l: 'row', xl: 'column' }}>
           <PButton variant="primary">Some label</PButton>
           <PButton variant="secondary">Some label</PButton>
           <PButton variant="tertiary">Some label</PButton>
+        </PButtonGroup>
+      </div>
+
+      <div
+        className="playground light"
+        title="should render button-group with PButtonPure, long text PButton and PButton with limited max-width and responsive direction"
+        style={{ background: 'deeppink' }}
+      >
+        <PButtonGroup direction={{ base: 'row', xs: 'column', s: 'row', m: 'column', l: 'row', xl: 'column' }}>
+          <PButtonPure>Some label</PButtonPure>
+          <PButton hideLabel={true}>Some label</PButton>
+          <PButton>Some label</PButton>
+          <PButton>Lorem ipsum dolor sit amet, consetetur sadipscing</PButton>
+          <PButton>Some label</PButton>
+          <PButtonPure>Some label</PButtonPure>
+          <PButton>Some label</PButton>
+          <PButton>Some label</PButton>
+          <PButton style={{ maxWidth: '150px' }}>Lorem ipsum dolor sit amet, consetetur sadipscing</PButton>
+          <PButton>Some label</PButton>
+          <PButton>Some label</PButton>
+          <PButton>Some label</PButton>
         </PButtonGroup>
       </div>
     </>
