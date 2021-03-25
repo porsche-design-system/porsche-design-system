6123023ac77194e30542ec89f7457aeac090f460
Anton Schmidt <anton.schmidt@mhp.com>
2021-03-25 12:20:09 +0100

    test(components-angular/js/react): update vrt tests | as / sas | #1260

--- packages/components-angular/src/app/pages/button-group.component.ts
+++ packages/components-angular/src/app/pages/button-group.component.ts
@@ -11,11 +11,40 @@ import { Component } from '@angular/core';
       </p-button-group>
     </div>
 
+    <div class="playground light" title="should show p-button-group default style when direction is undefined">
+      <p-button-group [direction]="undefined">
+        <p-button [variant]="'primary'">Some label</p-button>
+        <p-button [variant]="'secondary'">Some label</p-button>
+        <p-button [variant]="'tertiary'">Some label</p-button>
+      </p-button-group>
+    </div>
+
     <div class="playground light" title="should render button-group with responsive direction">
       <p-button-group [direction]="{ base: 'row', xs: 'column', s: 'row', m: 'column', l: 'row', xl: 'column' }">
         <p-button [variant]="'primary'">Some label</p-button>
         <p-button [variant]="'secondary'">Some label</p-button>
         <p-button [variant]="'tertiary'">Some label</p-button>
+      </p-button-group>
+    </div>
+
+    <div
+      class="playground light"
+      title="should render button-group with p-button-pure, long text p-button and p-button with limited max-width and responsive direction"
+      style="background: deeppink"
+    >
+      <p-button-group [direction]="{ base: 'row', xs: 'column', s: 'row', m: 'column', l: 'row', xl: 'column' }">
+        <p-button-pure>Some label</p-button-pure>
+        <p-button [hideLabel]="true">Some label</p-button>
+        <p-button>Some label</p-button>
+        <p-button>Lorem ipsum dolor sit amet, consetetur sadipscing</p-button>
+        <p-button>Some label</p-button>
+        <p-button-pure>Some label</p-button-pure>
+        <p-button>Some label</p-button>
+        <p-button>Some label</p-button>
+        <p-button style="max-width: 150px">Lorem ipsum dolor sit amet, consetetur sadipscing</p-button>
+        <p-button>Some label</p-button>
+        <p-button>Some label</p-button>
+        <p-button>Some label</p-button>
       </p-button-group>
     </div>
   `,
