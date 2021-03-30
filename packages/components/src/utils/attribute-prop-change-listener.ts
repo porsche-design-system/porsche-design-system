c6c3423578b56336eb744c4731290f64dadd936f
Bernd Hacker <hacker.bernd@gmail.com>
2021-03-30 15:00:50 +0200

    feat(components): reuse same mutation observer via initAttributePropChangeListener | bh | #1013

--- packages/components/src/utils/attribute-prop-change-listener.ts
+++ packages/components/src/utils/attribute-prop-change-listener.ts
@@ -2,7 +2,21 @@ import { forceUpdate } from '@stencil/core';
 import { addEventListener } from '.';
 /* eslint-disable no-console */
 
-const observeProperties = <T extends HTMLElement>(node: T, props: (keyof T)[], callback: () => void): void => {
+/**
+ * EVENTS
+ */
+export const observeChangeEvent = <T extends HTMLElement>(node: T, callback: () => void): void => {
+  // addEventListener(node, 'change', callback);
+  addEventListener(node, 'change', () => {
+    console.log('event listener', node);
+    callback();
+  });
+};
+
+/**
+ * PROPERTIES
+ */
+export const observeProperties = <T extends HTMLElement>(node: T, props: (keyof T)[], callback: () => void): void => {
   const proto = Object.getPrototypeOf(node);
   const forEachCallback = (prop: keyof T): void => {
     // eslint-disable-next-line @typescript-eslint/unbound-method
@@ -27,6 +41,37 @@ const observeProperties = <T extends HTMLElement>(node: T, props: (keyof T)[], c
   props.forEach(forEachCallback);
 };
 
+/**
+ * MUTATION OBSERVER
+ */
+export const mutationCallbacks: { node: HTMLElement; cb: () => void }[] = [];
+
+const mutationObserver = new MutationObserver((mutations, _observer) => {
+  console.log(mutationCallbacks.length, mutations);
+  mutationCallbacks
+    .filter(({ node }) => node === mutations[0].target)
+    .forEach((item) => {
+      console.log('mutation observer', item.node);
+      item.cb();
+    });
+});
+
+export const observeMutations = <T extends HTMLElement>(node: T, props: (keyof T)[], callback: () => void): void => {
+  console.log('observeMutations', node, props);
+  mutationCallbacks.push({ node, cb: callback });
+  mutationObserver.observe(node, { attributeFilter: props as string[] });
+};
+
+export const unobserveMutations = <T extends HTMLElement>(node: T): void => {
+  mutationCallbacks.splice(
+    mutationCallbacks.findIndex((x) => x.node === node),
+    1
+  );
+};
+
+/**
+ * PUBLIC API
+ */
 export const initAttributePropChangeListener = <T extends HTMLElement>(
   host: HTMLElement,
   node: T,
@@ -37,15 +82,12 @@ export const initAttributePropChangeListener = <T extends HTMLElement>(
     forceUpdate(host);
   };
 
-  addEventListener(node, 'click', () => {
-    console.log('event listener', host);
-    updateComponent();
-  });
-
+  observeChangeEvent(node, updateComponent);
   observeProperties(node, props, updateComponent);
+  observeMutations(node, props, updateComponent);
+};
 
-  new MutationObserver(() => {
-    console.log('mutation observer', host);
-    updateComponent();
-  }).observe(node, { attributeFilter: props });
+// TODO: make this work reliably with disconnectedCallback
+export const disconnectAttributePropChangeListener = <T extends HTMLElement>(node: T): void => {
+  unobserveMutations(node);
 };
