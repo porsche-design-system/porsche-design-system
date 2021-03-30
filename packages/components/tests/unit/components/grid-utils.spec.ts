a93d4371be848dbb307ca41b1513d2e307f9f654
Anton Schmidt <anton.schmidt@mhp.com>
2021-03-30 17:05:42 +0200

    test(components): add wrap property test to grid unit tests, update snapshots  | as / bh | #1293

--- packages/components/tests/unit/components/grid-utils.spec.ts
+++ packages/components/tests/unit/components/grid-utils.spec.ts
@@ -1,12 +1,21 @@
-import { getDynamicCss, GRID_DIRECTIONS, GridDirection } from '../../../src/components/layout/grid/grid/grid-utils';
+import type { GridDirection, GridWrap } from '../../../src/components/layout/grid/grid/grid-utils';
+import { getDynamicCss, GRID_DIRECTIONS, GRID_WRAPS } from '../../../src/components/layout/grid/grid/grid-utils';
 import { stringify } from '../helper';
 
 describe('getDynamicCss()', () => {
-  const data: GridDirection[] = [
+  const dataDirections: GridDirection[] = [
     ...GRID_DIRECTIONS,
     { base: 'column', xs: 'row', s: 'column', m: 'row', l: 'column', xl: 'row' },
   ];
-  it.each(data.map(stringify))('should return correct css for direction: %s', (direction: GridDirection) => {
-    expect(getDynamicCss(direction)).toMatchSnapshot();
+  it.each(dataDirections.map(stringify))('should return correct css for direction: %s', (direction: GridDirection) => {
+    expect(getDynamicCss(direction, 'wrap')).toMatchSnapshot();
+  });
+
+  const dataWraps: GridWrap[] = [
+    ...GRID_WRAPS,
+    { base: 'wrap', xs: 'nowrap', s: 'wrap', m: 'nowrap', l: 'wrap', xl: 'nowrap' },
+  ];
+  it.each(dataWraps.map(stringify))('should return correct css for wrap: %s', (wrap: GridWrap) => {
+    expect(getDynamicCss('row', wrap)).toMatchSnapshot();
   });
 });
