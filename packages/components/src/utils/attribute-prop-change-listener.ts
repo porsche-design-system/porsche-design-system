7792fb7c0e645abcf301acdc22cf0e2f2fc4214d
Bernd Hacker <hacker.bernd@gmail.com>
2021-03-30 09:54:56 +0200

    feat(components): make initAttributePropChangeListener work with react for input nodes | bh / mh | #1013

--- packages/components/src/utils/attribute-prop-change-listener.ts
+++ packages/components/src/utils/attribute-prop-change-listener.ts
@@ -1,29 +1,34 @@
 import { forceUpdate } from '@stencil/core';
+import { addEventListener } from '.';
 /* eslint-disable no-console */
 
 export const initAttributePropChangeListener = (host: HTMLElement, node: HTMLElement, props: string[]): void => {
-  const cb = (): void => {
+  const updateComponent = (): void => {
     console.log('cb...', host);
     forceUpdate(host);
   };
-
-  // node.addEventListener('change', () => {
-  //   console.log('event listener', host);
-  //   cb();
-  // });
+  addEventListener(node, 'click', () => {
+    console.log('event listener', host);
+    updateComponent();
+  });
 
   const proto = Object.getPrototypeOf(node);
   const forEachCallback = (prop: string): void => {
-    const descriptor = Object.getOwnPropertyDescriptor(proto, prop);
-
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const { get: superGet, set: superSet } = Object.getOwnPropertyDescriptor(proto, prop);
     Object.defineProperty(node, prop, {
+      configurable: true,
       get() {
-        return descriptor.get.apply(this);
+        return superGet.call(this);
       },
-      set(...args) {
-        console.log('set', host);
-        cb();
-        descriptor.set.apply(this, args);
+      set(val) {
+        superSet.call(this, val);
+
+        // full-circle moment for react
+        // https://github.com/facebook/react/blob/9198a5cec0936a21a5ba194a22fcbac03eba5d1d/packages/react-dom/src/client/inputValueTracking.js#L95
+        (node as any)._valueTracker?.setValue(val); // eslint-disable-line no-underscore-dangle
+
+        updateComponent();
       },
     });
   };
@@ -31,6 +36,6 @@ export const initAttributePropChangeListener = (host: HTMLElement, node: HTMLEle
 
   new MutationObserver(() => {
     console.log('mutation observer', host);
-    cb();
+    updateComponent();
   }).observe(node, { attributeFilter: props });
 };
