bf256b62ab9a688b3253303cc9b9e809f3f24217
Bernd Hacker <hacker.bernd@gmail.com>
2021-03-30 09:54:17 +0200

    feat(components): add addEventListener and removeEventListener utils | bh | #1013

--- packages/components/src/utils/dom.ts
+++ packages/components/src/utils/dom.ts
@@ -78,4 +78,16 @@ export function getHTMLElementAndThrowIfUndefined<K extends keyof HTMLElementTag
 
 export const getTagName = (el: HTMLElement): string => el.tagName.toLowerCase();
 
-// TODO: add addEventListener and removeEventListener
+export const addEventListener = (
+  el: HTMLElement,
+  type: string,
+  listener: () => void,
+  options?: boolean | AddEventListenerOptions
+): void => el.addEventListener(type, listener, options);
+
+export const removeEventListener = (
+  el: HTMLElement,
+  type: string,
+  listener: () => void,
+  options?: boolean | EventListenerOptions
+): void => el.removeEventListener(type, listener, options);
