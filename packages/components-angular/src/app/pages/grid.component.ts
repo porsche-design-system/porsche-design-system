75cf26c0a1a219b95bac2ac032ee701fce02f358
Anton Schmidt <anton.schmidt@mhp.com>
2021-03-31 16:00:07 +0200

    test(components): use gutter property the right way, update fixtures | as | #1293

--- packages/components-angular/src/app/pages/grid.component.ts
+++ packages/components-angular/src/app/pages/grid.component.ts
@@ -355,7 +355,7 @@ import { Component } from '@angular/core';
 
     <div class="playground" title="should adapt gutter, wrap and direction depending on viewport">
       <p-grid
-        [gutter]="{ base: '16', xs: '24', s: '36', m: '16', l: '24', xl: '36' }"
+        [gutter]="{ base: 16, xs: 24, s: 36, m: 16, l: 24, xl: 36 }"
         [wrap]="{ base: 'wrap', m: 'nowrap' }"
         [direction]="{ base: 'column', m: 'row' }"
       >
