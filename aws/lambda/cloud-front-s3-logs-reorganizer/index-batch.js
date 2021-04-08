26b34647f62d4f73f7d6f012f37a5311b491cd7c
Bernd Hacker <hacker.bernd@gmail.com>
2021-04-08 13:05:28 +0200

    feat(aws): extend cloud-front-s3-logs-reorganizer by ip anonymizing functionality | bh / sas | #1179

--- aws/lambda/cloud-front-s3-logs-reorganizer/index-batch.js
+++ aws/lambda/cloud-front-s3-logs-reorganizer/index-batch.js
@@ -1,13 +1,13 @@
 const AWS = require('aws-sdk');
-const { reorganizeLogFile } = require('./utils');
+const { renameAndReorganizeLogFile } = require('./utils');
 
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-async function process() {
+exports.handler = async (event) => {
   const bucketParams = {
     Bucket: 'porsche-design-system-logs',
     Prefix: 'cloudfront/',
-    Marker: 'cloudfront/EN98BRSTE4L4P.2021-04-07-15.fff1862d.anonymized.gz',
+    Marker: 'cloudfront/EN98BRSTE4L4P.2021-04-08-09.c42db7d3.anonymized.gz',
     MaxKeys: 1000,
   };
 
@@ -20,12 +20,8 @@ async function process() {
     .promise();
 
   for (const file of files.Contents) {
-    await reorganizeLogFile(file.Key);
+    await renameAndReorganizeLogFile(file.Key);
   }
 
   console.log('next marker', files.Contents[files.Contents.length - 1].Key);
-}
-
-exports.handler = async (event) => {
-  await process();
 };
--- aws/lambda/cloud-front-s3-logs-reorganizer/index.js
+++ aws/lambda/cloud-front-s3-logs-reorganizer/index.js
@@ -1,8 +1,8 @@
-const { reorganizeLogFile } = require('./utils');
+const { renameAndReorganizeLogFile } = require('./utils');
 
 exports.handler = async (event) => {
   for (const record of event.Records) {
     const { key } = record.s3.object;
-    await reorganizeLogFile(key);
+    await renameAndReorganizeLogFile(key);
   }
 };
