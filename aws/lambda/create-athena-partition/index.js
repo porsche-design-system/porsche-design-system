91ba8e92d3f5add9baaf4bd281d9170800588821
Bernd Hacker <hacker.bernd@gmail.com>
2021-04-08 09:34:59 +0200

    feat(aws): refactor create-athena-partition lambda | bh | #1179

--- aws/lambda/create-athena-partition/index.js
+++ aws/lambda/create-athena-partition/index.js
@@ -1,25 +1,27 @@
+const aws = require('aws-sdk');
+const AthenaExpress = require('athena-express');
+
+const awsCredentials = {
+  region: 'eu-central-1',
+};
+aws.config.update(awsCredentials);
+
+const athenaExpressConfig = { aws };
+const athenaExpress = new AthenaExpress(athenaExpressConfig);
+
 exports.handler = async (event) => {
   console.log('Start creating new Athena partition');
 
-  const aws = require("aws-sdk");
-  const AthenaExpress = require("athena-express");
+  const query = `ALTER TABLE s3_athena.tracking_data ADD PARTITION (year='2020', month='12', day='31', dt='2020-12-31') location 's3://porsche-design-system-athena/tracking-data/2020/12/31/'`;
 
-  const awsCredentials = {
-    region: "eu-central-1"
-  };
-  aws.config.update(awsCredentials);
-
-  const athenaExpressConfig = { aws };
-  const athenaExpress = new AthenaExpress(athenaExpressConfig);
-
-  const query = `ALTER TABLE s3_athena.tracking_data ADD PARTITION (year='2020', month='12', day='31', dt='2020-12-31') location 's3://porsche-design-system-athena/tracking-data/2020/12/31/'`
-
-  athenaExpress.query(query).then((data) => {
-    console.log('Data:', data);
-  })
-  .catch((err) => {
-    console.log('Error:', err);
-  });
+  athenaExpress
+    .query(query)
+    .then((data) => {
+      console.log('Data:', data);
+    })
+    .catch((err) => {
+      console.log('Error:', err);
+    });
 
   console.log('Finished creating new Athena partition');
 };
