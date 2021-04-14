0b121a1c5c1f8e8b9363293697582153384d8dfb
Matthias Haegele <matthias.haegele@mhp.com>
2021-04-14 16:24:11 +0200

    test(components-js): extend e2e tests | mh | #146

--- packages/components-js/tests/e2e/specs/switch.e2e.ts
+++ packages/components-js/tests/e2e/specs/switch.e2e.ts
@@ -1,21 +1,27 @@
 import { Page } from 'puppeteer';
 import {
+  addEventListener,
+  getActiveElementTagName,
   getAttribute,
   getBrowser,
   getLifecycleStatus,
+  getProperty,
+  initAddEventListener,
   selectNode,
   setAttribute,
   setContentWithDesignSystem,
   waitForStencilLifecycle,
 } from '../helpers';
-import { TabSize } from '@porsche-design-system/components/dist/types/components/navigation/tabs-bar/tabs-bar-utils';
 
 describe('switch', () => {
   let page: Page;
   beforeEach(async () => (page = await getBrowser().newPage()));
   afterEach(async () => await page.close());
 
-  const getHost = () => selectNode(page, 'p-switch');
+  const tagName = 'p-switch';
+  const getHost = () => selectNode(page, tagName);
+  const getButton = () => selectNode(page, 'p-switch >>> button');
+  const getLabel = () => selectNode(page, 'p-switch >>> p-text');
 
   const clickHandlerScript = `
     <script>
@@ -27,31 +33,100 @@ describe('switch', () => {
     </script>`;
 
   type InitOptions = {
+    disabled?: boolean;
+    tabbable?: boolean;
     otherMarkup?: string;
   };
 
   const initSwitch = (opts?: InitOptions): Promise<void> => {
-    const { otherMarkup = '' } = opts && {};
-    return setContentWithDesignSystem(page, `<p-switch label="Some label"></p-switch>${otherMarkup}`);
+    const { disabled = false, tabbable = true, otherMarkup = '' } = opts ?? {};
+    return setContentWithDesignSystem(
+      page,
+      `<p-switch label="Some label" tabbable="${tabbable}" disabled="${disabled}"></p-switch>${otherMarkup}`
+    );
   };
 
-  describe('accessibility', () => {
-    it('should set correct aria-checked value', async () => {});
-  });
-
-  describe('event', () => {
-    it('should trigger event on switch click', async () => {});
-    it('should not trigger event if switch is disabled', async () => {});
-    it('should change checked value switch click ', async () => {
+  describe('label', () => {
+    it('should check/uncheck switch on label click', async () => {
       await initSwitch({ otherMarkup: clickHandlerScript });
       const host = await getHost();
+      const label = await getLabel();
 
-      expect(await getAttribute(host, 'checked')).toBeFalse();
+      expect(await getProperty(host, 'checked')).toBeFalse();
 
-      await host.click();
+      await label.click();
       await waitForStencilLifecycle(page);
 
-      expect(await getAttribute(host, 'checked')).toBeTrue();
+      expect(await getProperty(host, 'checked')).toBeTrue();
+    });
+  });
+
+  describe('accessibility', () => {
+    it('should set correct aria-checked value', async () => {
+      await initSwitch();
+
+      const host = await getHost();
+      const button = await getButton();
+
+      expect(await getAttribute(button, 'aria-checked')).toBe('false');
+
+      await setAttribute(host, 'checked', 'true');
+      await waitForStencilLifecycle(page);
+
+      expect(await getAttribute(button, 'aria-checked')).toBe('true');
+    });
+
+    it('should set correct aria-busy value', async () => {
+      await initSwitch();
+
+      const host = await getHost();
+      const button = await getButton();
+
+      expect(await getAttribute(button, 'aria-busy')).toBeNull();
+
+      await setAttribute(host, 'loading', 'true');
+      await waitForStencilLifecycle(page);
+
+      expect(await getAttribute(button, 'aria-busy')).toBe('true');
+    });
+
+    it('should not be active element if tabbable is set to false', async () => {
+      await initSwitch({ tabbable: false });
+      await page.keyboard.press('Tab');
+
+      expect(await getActiveElementTagName(page)).not.toBe(tagName);
+    });
+  });
+
+  describe('events', () => {
+    beforeEach(async () => await initAddEventListener(page));
+
+    it('should trigger event on click', async () => {
+      await initSwitch({ otherMarkup: clickHandlerScript });
+
+      let eventCounter = 0;
+      const host = await getHost();
+      const button = await getButton();
+      await addEventListener(host, 'switchChange', () => eventCounter++);
+
+      await button.click();
+      await waitForStencilLifecycle(page);
+
+      expect(eventCounter).toBe(1);
+    });
+
+    it('should not trigger event on click if switch is disabled', async () => {
+      await initSwitch({ disabled: true, otherMarkup: clickHandlerScript });
+
+      let eventCounter = 0;
+      const host = await getHost();
+      const button = await getButton();
+      await addEventListener(host, 'switchChange', () => eventCounter++);
+
+      await button.click();
+      await waitForStencilLifecycle(page);
+
+      expect(eventCounter).toBe(0);
     });
   });
 
@@ -60,7 +135,7 @@ describe('switch', () => {
       await initSwitch();
       const status = await getLifecycleStatus(page);
 
-      expect(status.componentDidLoad['p-tabs-bar']).toBe(1, 'componentDidLoad: p-switch');
+      expect(status.componentDidLoad['p-switch']).toBe(1, 'componentDidLoad: p-switch');
       expect(status.componentDidLoad['p-text']).toBe(1, 'componentDidLoad: p-text');
 
       expect(status.componentDidLoad.all).toBe(2, 'componentDidLoad: all');
@@ -71,7 +146,7 @@ describe('switch', () => {
       await initSwitch();
       const host = await getHost();
 
-      await setAttribute(host, 'active-tab-index', '2');
+      await setAttribute(host, 'checked', 'true');
       await waitForStencilLifecycle(page);
 
       const status = await getLifecycleStatus(page);
