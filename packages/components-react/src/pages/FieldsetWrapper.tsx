ad975027f0acbfd4c3371c035ba2eb2967b4bd51
Matthias Haegele <matthias.haegele@mhp.com>
2021-04-15 14:41:44 +0200

    test(components-js/react/angular): update vrt pages + fixtures | mh | #1230

--- packages/components-react/src/pages/FieldsetWrapper.tsx
+++ packages/components-react/src/pages/FieldsetWrapper.tsx
@@ -1,8 +1,23 @@
-import { PFieldsetWrapper, PTextFieldWrapper } from '@porsche-design-system/components-react';
+import {
+  PFieldsetWrapper,
+  PTextFieldWrapper,
+  PCheckboxWrapper,
+  PRadioButtonWrapper,
+} from '@porsche-design-system/components-react';
 
 export const FieldsetWrapperPage = (): JSX.Element => {
+  const style = `
+    p-fieldset-wrapper > p-radio-button-wrapper:first-child {
+      margin-bottom: 2px;
+    }
+    p-fieldset-wrapper > p-checkbox-wrapper:not(:last-child) {
+      margin-bottom: 2px;
+    }
+  `;
   return (
     <>
+      <style children={style} />
+
       <div className="playground light" title="should render with label">
         <PFieldsetWrapper label="Some label"></PFieldsetWrapper>
       </div>
@@ -12,6 +27,110 @@ export const FieldsetWrapperPage = (): JSX.Element => {
           <PTextFieldWrapper label="Some label">
             <input type="text" name="some-name" />
           </PTextFieldWrapper>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render with required">
+        <PFieldsetWrapper label="Some label" required={true}>
+          <PCheckboxWrapper label="Some label">
+            <input type="checkbox" name="some-name-1" />
+          </PCheckboxWrapper>
+          <PCheckboxWrapper label="Some label">
+            <input type="checkbox" name="some-name-1" />
+          </PCheckboxWrapper>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render with slotted label">
+        <PFieldsetWrapper>
+          <span slot="label">Some slotted Label</span>
+          <PCheckboxWrapper label="Some label">
+            <input type="checkbox" name="some-name-1" />
+          </PCheckboxWrapper>
+          <PCheckboxWrapper label="Some label">
+            <input type="checkbox" name="some-name-1" />
+          </PCheckboxWrapper>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render with label size small">
+        <PFieldsetWrapper label="Some label" labelSize="small">
+          <PRadioButtonWrapper label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+          <PRadioButtonWrapper label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render with label size small and required">
+        <PFieldsetWrapper label="Some label" labelSize="small" required={true}>
+          <PRadioButtonWrapper label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+          <PRadioButtonWrapper label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render with error state and error message">
+        <PFieldsetWrapper label="Some label" state="error" message="Some error message">
+          <PRadioButtonWrapper state="error" label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+          <PRadioButtonWrapper state="error" label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render with error state and slotted error message">
+        <PFieldsetWrapper label="Some label" state="error">
+          <PRadioButtonWrapper state="error" label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+          <PRadioButtonWrapper state="error" label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+          <span slot="message">Some slotted error Message</span>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render with success state and success message">
+        <PFieldsetWrapper label="Some label" state="success" message="Some success message">
+          <PRadioButtonWrapper state="success" label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+          <PRadioButtonWrapper state="success" label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render with success state and slotted success message">
+        <PFieldsetWrapper label="Some label" state="success">
+          <PRadioButtonWrapper state="success" label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+          <PRadioButtonWrapper state="success" label="Some label">
+            <input type="radio" name="some-name-1" />
+          </PRadioButtonWrapper>
+          <span slot="message">Some slotted success Message</span>
+        </PFieldsetWrapper>
+      </div>
+
+      <div className="playground light" title="should render fieldset in fieldset as section">
+        <PFieldsetWrapper label="Some label">
+          <PFieldsetWrapper label="Some label" labelSize="small">
+            <PRadioButtonWrapper label="Some label">
+              <input type="radio" name="some-name-1" />
+            </PRadioButtonWrapper>
+            <PRadioButtonWrapper label="Some label">
+              <input type="radio" name="some-name-1" />
+            </PRadioButtonWrapper>
+          </PFieldsetWrapper>
         </PFieldsetWrapper>
       </div>
     </>
