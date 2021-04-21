52b08bb0d2f90ea7cd2d3bb12015db53d33f60ff
Stephan Schröter <stephan.schroeter@mhp.com>
2021-04-21 11:44:09 +0200

    feat(components): automatically breaks strings into new line when not fitting into container for text and headline | mh / sas | #146

--- packages/components-js/tests/vrt/specs/switch.vrt.ts
+++ packages/components-js/tests/vrt/specs/switch.vrt.ts
@@ -1,6 +1,6 @@
 import { getVisualRegressionTester, testOptions } from '../helpers';
 
-describe('Switch', () => {
+fdescribe('Switch', () => {
   it('should have no visual regression', async () => {
     const vrt = getVisualRegressionTester();
     expect(
--- packages/components-react/src/pages/Switch.tsx
+++ packages/components-react/src/pages/Switch.tsx
@@ -17,67 +17,67 @@ export const SwitchPage = (): JSX.Element => {
       </div>
 
       <div className="playground light" title="should render in state checked">
-        <PSwitch checked={true}>Some label</PSwitch>
+        <PSwitch checked>Some label</PSwitch>
       </div>
       <div className="playground dark" title="should render in state checked mode with dark theme">
-        <PSwitch checked={true} theme="dark">
+        <PSwitch checked theme="dark">
           Some label
         </PSwitch>
       </div>
 
       <div className="playground light" title="should render in state disabled">
-        <PSwitch disabled={true}>Some label</PSwitch>
-        <PSwitch disabled={true} checked={true}>
+        <PSwitch disabled>Some label</PSwitch>
+        <PSwitch disabled checked>
           Some label
         </PSwitch>
       </div>
 
       <div className="playground dark" title="should render in state disabled with dark theme">
-        <PSwitch disabled={true} theme="dark">
+        <PSwitch disabled theme="dark">
           Some label
         </PSwitch>
-        <PSwitch disabled={true} checked={true} theme="dark">
+        <PSwitch disabled checked theme="dark">
           Some label
         </PSwitch>
       </div>
 
       <div className="playground light" title="should render in state loading">
-        <PSwitch loading={true}>Some label</PSwitch>
-        <PSwitch loading={true} checked={true}>
+        <PSwitch loading>Some label</PSwitch>
+        <PSwitch loading checked>
           Some label
         </PSwitch>
       </div>
 
       <div className="playground dark" title="should render in state loading with dark theme">
-        <PSwitch loading={true} theme="dark">
+        <PSwitch loading theme="dark">
           Some label
         </PSwitch>
-        <PSwitch loading={true} checked={true} theme="dark">
+        <PSwitch loading checked theme="dark">
           Some label
         </PSwitch>
       </div>
 
       <div className="playground light" title="should align label to the left">
-        <PSwitch align-label="left">Some label</PSwitch>
+        <PSwitch alignLabel="left">Some label</PSwitch>
       </div>
       <div className="playground light" title="should align label to the left or right depending on viewport">
-        <PSwitch align-label="{'base': 'left', 'xs': 'right', 's': 'left', 'm': 'right', 'l': 'left', 'xl': 'right'}">
+        <PSwitch alignLabel={{'base': 'left', 'xs': 'right', 's': 'left', 'm': 'right', 'l': 'left', 'xl': 'right'}}>
           Some label
         </PSwitch>
       </div>
 
       <div className="playground light" title="should render without label">
-        <PSwitch hide-label={true}>Some label</PSwitch>
+        <PSwitch hideLabel>Some label</PSwitch>
       </div>
       <div className="playground light" title="should render with or without label depending on viewport">
-        <PSwitch hide-label="{'base': true, 'xs': false, 's': true, 'm': false, 'l': true, 'xl': false}">
+        <PSwitch hideLabel={{'base': true, 'xs': false, 's': true, 'm': false, 'l': true, 'xl': false}}>
           Some label
         </PSwitch>
       </div>
 
       <div className="playground light" title="should render with stretched label">
-        <PSwitch stretch={true}>Some label</PSwitch>
-        <PSwitch stretch={true} align-label="left">
+        <PSwitch stretch>Some label</PSwitch>
+        <PSwitch stretch alignLabel="left">
           Some label
         </PSwitch>
       </div>
@@ -86,9 +86,11 @@ export const SwitchPage = (): JSX.Element => {
       </div>
 
       <div className="playground light" title="should render with multiline label">
-        <PSwitch style={{ width: '240px' }}>Lorem ipsum dolor sit amet, consetetur sadipscing</PSwitch>
-        <PSwitch style={{ width: '240px' }} align-label="left">
-          Lorem ipsum dolor sit amet, consetetur sadipscing
+        <PSwitch style={{ width: '240px' }}>
+          This is the first time I've seen the word Pneumonoultramicroscopicsilicovolcanoconiosis. It's a long one.
+        </PSwitch>
+        <PSwitch style={{ width: '240px' }} alignLabel="left">
+          This is the first time I've seen the word Pneumonoultramicroscopicsilicovolcanoconiosis. It's a long one.
         </PSwitch>
       </div>
     </>
