9bba07b3acf15e9c703086dce73e26ac81199475
Matthias Haegele <matthias.haegele@mhp.com>
2021-04-21 10:15:31 +0200

    test(components-js/react/angular): update vrt test / fixtures | mh | #1230

--- packages/components-angular/src/app/pages/fieldset-wrapper.component.ts
+++ packages/components-angular/src/app/pages/fieldset-wrapper.component.ts
@@ -128,6 +128,30 @@ import { Component } from '@angular/core';
         </p-fieldset-wrapper>
       </p-fieldset-wrapper>
     </div>
+
+    <div class="playground light" title="should render required only on fieldset">
+      <p-fieldset-wrapper [label]="'Some label'" [labelSize]="'small'">
+        <p-text-field-wrapper [label]="'Some label'" state="error">
+          <input type="text" name="some-name" required />
+        </p-text-field-wrapper>
+        <p-textarea-wrapper [label]="'Some label'" [hideLabel]="false">
+          <textarea name="some-name" required></textarea>
+        </p-textarea-wrapper>
+        <p-radio-button-wrapper [label]="'Some label'">
+          <input type="radio" name="some-name-1" required />
+        </p-radio-button-wrapper>
+        <p-checkbox-wrapper [label]="'Some label'">
+          <input type="checkbox" name="some-name-1" required />
+        </p-checkbox-wrapper>
+        <p-select-wrapper [label]="'Some label'">
+          <select name="some-name" required>
+            <option value="a">Option A</option>
+            <option value="b">Option B</option>
+            <option value="c">Option C</option>
+          </select>
+        </p-select-wrapper>
+      </p-fieldset-wrapper>
+    </div>
   `,
 })
 export class FieldsetWrapperComponent {}
