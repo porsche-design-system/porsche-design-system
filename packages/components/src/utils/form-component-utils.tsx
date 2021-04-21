f3d00e9bc70f8a8ca7aa2de424f12172d841861f
Matthias Haegele <matthias.haegele@mhp.com>
2021-04-21 15:20:30 +0200

    feat(components): add new helper to determine parent + required / add utility for message/description/label | mh | #1230

---
+++ packages/components/src/utils/form-component-utils.tsx
@@ -0,0 +1,45 @@
+import { h, JSX } from '@stencil/core';
+import { getPrefixedTagNames } from './get-prefixed-tag-name';
+import type { FormState } from '../types';
+import { isParentFieldsetWrapperRequired, isHTMLElementRequired } from './dom';
+
+export const getMessage = (host: HTMLElement, message: string, state: FormState): JSX.Element => {
+  const PrefixedTagNames = getPrefixedTagNames(host);
+
+  return (
+    <PrefixedTagNames.pText class="message" color="inherit" role={state === 'error' ? 'alert' : null}>
+      {message || <slot name="message" />}
+    </PrefixedTagNames.pText>
+  );
+};
+
+export const getLabel = (
+  host: HTMLElement,
+  input: HTMLInputElement | HTMLTextAreaElement,
+  label: string,
+  className: string | { [p: string]: boolean },
+  labelClick: (event: MouseEvent) => void
+): JSX.Element => {
+  const PrefixedTagNames = getPrefixedTagNames(host);
+
+  return (
+    <PrefixedTagNames.pText class={className} tag="span" color="inherit" onClick={labelClick}>
+      {label || <slot name="label" />}
+      {!isParentFieldsetWrapperRequired(host) && isHTMLElementRequired(input) && <span class="required" />}
+    </PrefixedTagNames.pText>
+  );
+};
+
+export const getDescripton = (
+  host: HTMLElement,
+  description: string,
+  className: string | { [p: string]: boolean }
+): JSX.Element => {
+  const PrefixedTagNames = getPrefixedTagNames(host);
+
+  return (
+    <PrefixedTagNames.pText class={className} tag="span" color="inherit" size="x-small">
+      {description || <slot name="description" />}
+    </PrefixedTagNames.pText>
+  );
+};
