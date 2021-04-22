59a9f2dd2384f5704c4e125aacd926f8e61ea848
Bernd Hacker <hacker.bernd@gmail.com>
2021-04-22 13:36:24 +0200

    feat(storefront): change routing to history mode | bh | #759

--- packages/storefront/src/router/index.ts
+++ packages/storefront/src/router/index.ts
@@ -1,10 +1,11 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 import $store from '@/store';
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-const router = new Router({
+const router = new VueRouter({
+  mode: 'history',
   routes: [
     {
       path: '/',
