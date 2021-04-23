f88fae4cd4f79fa69756a9e391c23975cc11ef34
Anton Schmidt <anton.schmidt@mhp.com>
2021-04-23 15:48:30 +0200

    refactor(components): remove banner--close class, add dynamic close animation styles on host | as | #1306
    TODO: firefox+safari animations are broken

---
+++ packages/components/src/components/feedback/banner/banner-utils.ts
@@ -0,0 +1,47 @@
+import { attachCss, buildHostStyles, getCss, mediaQuery, pxToRem } from '../../../utils';
+
+const baseCss: string = getCss({});
+
+export const getMediaQueryStyles = (): string =>
+  getCss({
+    '@keyframes animateMobileOut': {
+      from: {
+        bottom: pxToRem(56),
+        opacity: 1,
+      },
+      to: {
+        bottom: '-100%',
+        opacity: 0,
+      },
+    },
+    '@keyframes animateDesktopOut': {
+      from: {
+        top: pxToRem(56),
+        opacity: 1,
+      },
+      to: {
+        top: '-100%',
+        opacity: 0,
+      },
+    },
+    ...buildHostStyles({
+      position: 'fixed',
+      zIndex: 99,
+      width: '100%',
+
+      [mediaQuery('xxs')]: {
+        bottom: '-100%',
+        willChange: 'opacity, bottom',
+        animation: '600ms 0s $animateMobileOut cubic-bezier(0.5, 1, 0.89, 1) forwards',
+      },
+      [mediaQuery('s')]: {
+        top: '-100%',
+        willChange: 'opacity, top',
+        animation: '600ms 0s $animateDesktopOut cubic-bezier(0.5, 1, 0.89, 1) forwards',
+      },
+    }),
+  });
+
+export const addCss = (host: HTMLElement): void => {
+  attachCss(host, baseCss + getMediaQueryStyles());
+};
