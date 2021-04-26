c1a71d5991a52f0da2e782d085266d0ee11c98b0
Bernd Hacker <hacker.bernd@gmail.com>
2021-04-26 07:09:02 +0200

    feat(aws): extend cloud-front-storeofront-rewrite-index-html lambda, add package.json | bh | #759

--- aws/lambda/cloud-front-storefront-rewrite-index-html/index.js
+++ aws/lambda/cloud-front-storefront-rewrite-index-html/index.js
@@ -1,19 +1,26 @@
 'use strict';
+
 exports.handler = (event, context, callback) => {
   // Extract the request from the CloudFront event that is sent to Lambda@Edge
-  var request = event.Records[0].cf.request;
+  const { request } = event.Records[0].cf;
 
   // Extract the URI from the request
-  var olduri = request.uri;
+  const { uri: oldUri } = request;
 
-  // Match any '/' that occurs at the end of a URI. Replace it with a default index
-  var newuri = olduri.replace(/\/$/, '/index.html');
+  // Is it not a file?
+  if (!oldUri.match(/\.[0-9a-z]{1,4}$/i)) {
+    // Match any '/' that occurs at the end of a URI. Replace it with a default index
+    const newUri = oldUri.replace(
+      /(\/(?:issue|housekeeping|release)\/(?:\d+|[\w\d-\.]+)|latest|v\d)\/?.*/,
+      '$1/index.html'
+    );
 
-  console.log('Old URI: ' + olduri);
-  console.log('New URI: ' + newuri);
+    // console.log('Old URI: ', oldUri);
+    // console.log('New URI: ', newUri);
 
-  // Replace the received URI with the URI that includes the index page
-  request.uri = newuri;
+    // Replace the received URI with the URI that includes the index page
+    request.uri = newUri;
+  }
 
   // Return to CloudFront
   return callback(null, request);
