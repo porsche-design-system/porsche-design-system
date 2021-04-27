6d9192a7013641947ba66ede649d2c661eedf7b4
Matthias Haegele <matthias.haegele@mhp.com>
2021-04-27 15:19:54 +0200

    fix(components): select-wrapper with new helper mapBreakpointPropToClasses | mh / sas | #146

--- packages/components/src/components/form/select-wrapper/select-wrapper.tsx
+++ packages/components/src/components/form/select-wrapper/select-wrapper.tsx
@@ -10,7 +10,7 @@ import {
   isDark,
   isRequired,
   isTouchDevice,
-  mapBreakpointPropToPrefixedClasses,
+  mapBreakpointPropToClasses,
   prefix,
   setAriaAttributes,
   setAttribute,
@@ -127,9 +127,7 @@ export class SelectWrapper {
     const labelClasses = {
       [prefix('select-wrapper__label')]: true,
       [prefix('select-wrapper__label--disabled')]: this.disabled,
-      ...mapBreakpointPropToPrefixedClasses('select-wrapper__label-', this.hideLabel, {
-        classSuffixes: ['hidden', 'visible'],
-      }),
+      ...mapBreakpointPropToClasses('select-wrapper__label-', this.hideLabel, ['hidden', 'visible']),
     };
     const requiredFlagClasses = {
       [prefix('select-wrapper__required')]: true,
@@ -138,9 +136,7 @@ export class SelectWrapper {
     const descriptionClasses = {
       [prefix('select-wrapper__description')]: true,
       [prefix('select-wrapper__description--disabled')]: this.disabled,
-      ...mapBreakpointPropToPrefixedClasses('select-wrapper__description-', this.hideLabel, {
-        classSuffixes: ['hidden', 'visible'],
-      }),
+      ...mapBreakpointPropToClasses('select-wrapper__description-', this.hideLabel, ['hidden', 'visible']),
     };
     const fakeSelectClasses = {
       [prefix('select-wrapper__fake-select')]: true,
