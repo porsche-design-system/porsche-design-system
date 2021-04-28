465f1fc213207aab20e537a7ae78a9df765be1b5
Matthias Haegele <matthias.haegele@mhp.com>
2021-04-28 09:04:10 +0200

    fix(components): use depricated function in select | mh | #146

--- packages/components/src/components/form/select-wrapper/select-wrapper.tsx
+++ packages/components/src/components/form/select-wrapper/select-wrapper.tsx
@@ -10,7 +10,7 @@ import {
   isDark,
   isRequired,
   isTouchDevice,
-  mapBreakpointPropToClasses,
+  mapBreakpointPropToPrefixedClasses,
   prefix,
   setAriaAttributes,
   setAttribute,
@@ -127,7 +127,7 @@ export class SelectWrapper {
     const labelClasses = {
       [prefix('select-wrapper__label')]: true,
       [prefix('select-wrapper__label--disabled')]: this.disabled,
-      ...mapBreakpointPropToClasses('p-select-wrapper__label', this.hideLabel, ['hidden', 'visible']),
+      ...mapBreakpointPropToPrefixedClasses('select-wrapper__label-', this.hideLabel, ['hidden', 'visible']),
     };
     const requiredFlagClasses = {
       [prefix('select-wrapper__required')]: true,
@@ -136,7 +136,7 @@ export class SelectWrapper {
     const descriptionClasses = {
       [prefix('select-wrapper__description')]: true,
       [prefix('select-wrapper__description--disabled')]: this.disabled,
-      ...mapBreakpointPropToClasses('p-select-wrapper__description', this.hideLabel, ['hidden', 'visible']),
+      ...mapBreakpointPropToPrefixedClasses('select-wrapper__description-', this.hideLabel, ['hidden', 'visible']),
     };
     const fakeSelectClasses = {
       [prefix('select-wrapper__fake-select')]: true,
