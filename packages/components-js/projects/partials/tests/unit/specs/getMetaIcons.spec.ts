56207f506a753631791a34613288521bcd281d85
Anton Schmidt <anton.schmidt@mhp.com>
2021-04-29 16:50:04 +0200

    text(components-js): add unit tests for url without tag | as | #1201

--- packages/components-js/projects/partials/tests/unit/specs/getMetaIcons.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getMetaIcons.spec.ts
@@ -1,4 +1,4 @@
-import { getFontLinks, getMetaIcons } from '../../../src';
+import { getMetaIcons } from '../../../src';
 
 fdescribe('getMetaIcons()', () => {
   describe('validation', () => {
@@ -49,6 +49,33 @@ fdescribe('getMetaIcons()', () => {
 
       const result = getMetaIcons({ appTitle: 'Porsche UX', cdn: 'cn' });
       expect(result).toBe(metaIconsResultCN);
+    });
+  });
+  describe('url without tag', () => {
+    it('should return default url', () => {
+      const cndMetaIconUrls = [
+        'https://cdn.ui.porsche.com/porsche-design-system/meta-icons/mstile-270x270.6003b0be8d72c03282c5e43a94cf713c.png',
+        'https://cdn.ui.porsche.com/porsche-design-system/meta-icons/favicon-16x16.f47c62d49906f99ab4408d691f03ece7.png',
+        'https://cdn.ui.porsche.com/porsche-design-system/meta-icons/favicon-32x32.10be24507223bc4ef63effe0eb750e58.png',
+        'https://cdn.ui.porsche.com/porsche-design-system/meta-icons/apple-touch-icon-180x180.bdeb42142de51637d67f7532ead2caab.png',
+        'https://cdn.ui.porsche.com/porsche-design-system/meta-icons/pinned-tab-icon.e2eea3d0fb56b5faf6b84c590f19572b.svg',
+      ];
+      const result = getMetaIcons({ appTitle: 'Porsche UX', withoutTags: true });
+
+      expect(result).toEqual(cndMetaIconUrls);
+    });
+
+    it('should return default China CDN url', () => {
+      const cndMetaIconUrlsCn = [
+        'https://cdn.ui.porsche.cn/porsche-design-system/meta-icons/mstile-270x270.6003b0be8d72c03282c5e43a94cf713c.png',
+        'https://cdn.ui.porsche.cn/porsche-design-system/meta-icons/favicon-16x16.f47c62d49906f99ab4408d691f03ece7.png',
+        'https://cdn.ui.porsche.cn/porsche-design-system/meta-icons/favicon-32x32.10be24507223bc4ef63effe0eb750e58.png',
+        'https://cdn.ui.porsche.cn/porsche-design-system/meta-icons/apple-touch-icon-180x180.bdeb42142de51637d67f7532ead2caab.png',
+        'https://cdn.ui.porsche.cn/porsche-design-sgystem/meta-icons/pinned-tab-icon.e2eea3d0fb56b5faf6b84c590f19572b.svg',
+      ];
+      const result = getMetaIcons({ appTitle: 'Porsche UX', withoutTags: true, cdn: 'cn' });
+
+      expect(result).toEqual(cndMetaIconUrlsCn);
     });
   });
 });
