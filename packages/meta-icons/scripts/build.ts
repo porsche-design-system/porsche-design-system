d75d34d1fa23e2bf0ae1f0e3e768b2ddef60cf0f
Anton Schmidt <anton.schmidt@mhp.com>
2021-05-03 15:47:37 +0200

    feat(meta-icons): generate manifest.webmanifest with different CDNs | as / bh | #1201

--- packages/meta-icons/scripts/build.ts
+++ packages/meta-icons/scripts/build.ts
@@ -3,25 +3,72 @@ import * as path from 'path';
 import * as crypto from 'crypto';
 import globby from 'globby';
 import { paramCase, camelCase } from 'change-case';
-import { CDN_BASE_URL_DYNAMIC, CDN_BASE_PATH_META_ICONS, CDN_KEY_TYPE_DEFINITION } from '../../../cdn.config';
+import {
+  CDN_BASE_URL_DYNAMIC,
+  CDN_BASE_PATH_META_ICONS,
+  CDN_KEY_TYPE_DEFINITION,
+  CDN_BASE_URL,
+  CDN_BASE_URL_CN,
+} from '../../../cdn.config';
 
-type Manifest = {
+type Cdn = 'auto' | 'cn';
+
+type MetaIconsManifest = {
   [type: string]: {
     [name: string]: string;
   };
 };
 
-const toHash = (str: string): string =>
-  crypto
-    .createHash('md5')
-    .update(str, 'utf8')
-    .digest('hex');
+const toHash = (str: string): string => crypto.createHash('md5').update(str, 'utf8').digest('hex');
 
-const createManifestAndCopyMetaIcons = async (cdn: string, files: string[]): Promise<void> => {
+const generateWebManifestAndExtendIconManifest = (metaIconsManifest: MetaIconsManifest): MetaIconsManifest => {
+  const iconPaths = Object.values(metaIconsManifest)
+    .map((item) => Object.values(item))
+    //@ts-ignore
+    .flat();
+
+  const androidIconFilenames = iconPaths.filter((iconPath: string) => iconPath.includes('android-chrome-'));
+
+  const hashedManifestFilename = writeWebManifest(androidIconFilenames, 'auto');
+  const hashedManifestFilenameCN = writeWebManifest(androidIconFilenames, 'cn');
+
+  metaIconsManifest.webManifest = {
+    auto: hashedManifestFilename,
+    cn: hashedManifestFilenameCN,
+  };
+  return metaIconsManifest;
+};
+
+const writeWebManifest = (androidIconPaths: string[], cdn: Cdn): string => {
+  const cdnURL = cdn === 'auto' ? CDN_BASE_URL : CDN_BASE_URL_CN;
+  const icons = androidIconPaths.map((androidIconPath: string) => {
+    const [, size] = androidIconPath.match(/android-chrome-([0-9]+)x\1/) ?? [];
+    if (isNaN(parseInt(size))) {
+      throw new Error('Size of android icon could not be extracted');
+    }
+    return {
+      src: `${cdnURL}/${CDN_BASE_PATH_META_ICONS}/${androidIconPath}`,
+      sizes: `${size}x${size}`,
+      type: 'image/png',
+    };
+  });
+
+  console.log('Created icon arrays', icons);
+
+  const manifest = JSON.stringify({ icons });
+  const hash = toHash(manifest);
+  const manifestFileName = `manifest${cdn === 'auto' ? '' : `.${cdn}`}.${hash}.webmanifest`;
+
+  fs.writeFileSync(path.normalize(`./dist/meta-icons/${manifestFileName}`), manifest);
+  console.log(`${manifestFileName} written.`);
+  return manifestFileName;
+};
+
+const copyMetaIconsAndBuildIconManifest = (cdn: string, files: string[]): MetaIconsManifest => {
   fs.rmdirSync(path.normalize('./dist'), { recursive: true });
   fs.mkdirSync(path.normalize('./dist/meta-icons'), { recursive: true });
 
-  const manifest: Manifest = {};
+  const manifest: MetaIconsManifest = {};
 
   for (let file of files) {
     const ext = path.extname(file);
@@ -46,6 +93,9 @@ const createManifestAndCopyMetaIcons = async (cdn: string, files: string[]): Pro
     console.log(`Meta Icon "${name}" copied.`);
   }
 
+  return manifest;
+};
+const writeMetaIconManifest = (cdn: string, manifest: MetaIconsManifest) => {
   fs.writeFileSync(
     path.normalize('./index.ts'),
     `${CDN_KEY_TYPE_DEFINITION}
@@ -57,12 +107,13 @@ export const META_ICONS_MANIFEST = ${JSON.stringify(manifest)};`
   console.log('Created meta-icons manifest.');
 };
 
-(async (): Promise<void> => {
-  const cdn = `${CDN_BASE_URL_DYNAMIC} + '/${CDN_BASE_PATH_META_ICONS}'`;
-  const icons = (await globby('./src/**/*')).sort();
+const generate = (): void => {
+  const cdnMetaIconsUrl = `${CDN_BASE_URL_DYNAMIC} + '/${CDN_BASE_PATH_META_ICONS}'`;
+  const icons = globby.sync('./src/**/*').sort();
+  const metaIconsManifest = copyMetaIconsAndBuildIconManifest(cdnMetaIconsUrl, icons);
+  const metaIconsManifestWithWebManifest = generateWebManifestAndExtendIconManifest(metaIconsManifest);
 
-  await createManifestAndCopyMetaIcons(cdn, icons).catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
-})();
+  writeMetaIconManifest(cdnMetaIconsUrl, metaIconsManifestWithWebManifest);
+};
+
+generate();
