124539a1676a29b8617984cfec5147b5664257d6
Bernd Hacker <hacker.bernd@gmail.com>
2021-05-05 09:30:12 +0200

    feat(component): add table poc | bh | #1289

---
+++ packages/components/src/components/layout/table/table.tsx
@@ -0,0 +1,41 @@
+import { Component, h, JSX, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'p-table',
+  styleUrl: 'table.scss',
+  shadow: true,
+})
+export class Table {
+  @Prop() public head?: string[] = [];
+  @Prop() public data?: object[] = [];
+
+  public componentWillRender(): void {
+    const [firstItem] = this.data;
+    if (firstItem && this.head.length !== Object.values(firstItem).length) {
+      throw new Error('Amount of supplied head elements and data structure does not match');
+    }
+  }
+
+  public render(): JSX.Element {
+    return (
+      <table>
+        <thead>
+          <tr>
+            {this.head.map((x) => (
+              <td>{x}</td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {this.data.map((x) => (
+            <tr>
+              {Object.values(x).map((y) => (
+                <td>{y}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
