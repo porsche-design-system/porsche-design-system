776ef73891d1b99a1336125ad145d44bcec4aa8e
Matthias Haegele <matthias.haegele@mhp.com>
2021-05-05 13:23:06 +0200

    doc(storefront): implement documentation about cve | mh | #1356

--- packages/storefront/storefront.config.ts
+++ packages/storefront/storefront.config.ts
@@ -607,6 +607,12 @@ export const config: StorefrontConfig = {
       () => import(`@/pages/performance/loading-behaviour.md`),
     ],
   },
+  Security: {
+    CVE: [
+      // @ts-ignore
+      () => import(`@/pages/security/cve.md`),
+    ],
+  },
   Helpers: {
     'Components Ready': [
       // @ts-ignore
