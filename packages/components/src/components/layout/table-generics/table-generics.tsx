0b202dab41844efdaa2c4f682dce84847946621c
Bernd Hacker <hacker.bernd@gmail.com>
2021-05-10 15:57:15 +0200

    feat(components): add hasAttribute and throwIfElementHasAttribute dom utility and use them in table | bh | #1289

--- packages/components/src/components/layout/table-generics/table-generics.tsx
+++ packages/components/src/components/layout/table-generics/table-generics.tsx
@@ -1,6 +1,6 @@
 import { Component, Element, Event, EventEmitter, h, JSX, Prop } from '@stencil/core';
 import type { GenericObject } from '../../../types';
-import { getClosestHTMLElement, parseJSON } from '../../../utils';
+import { getClosestHTMLElement, throwIfElementHasAttribute } from '../../../utils';
 import type { HeadItem } from './table-utils';
 import { TableHead } from '../table-wrapper-body/table-head';
 
@@ -12,30 +12,31 @@ import { TableHead } from '../table-wrapper-body/table-head';
 export class TableGenerics {
   @Element() public host!: HTMLElement;
 
-  @Prop() public head?: string | HeadItem[] = [];
-  @Prop() public data?: string | GenericObject[] = [];
+  @Prop() public head?: HeadItem[] = [];
+  @Prop() public data?: GenericObject[] = [];
   @Prop() public renderRow?: (item: GenericObject) => string = () => '';
 
   /* eslint-disable-next-line @typescript-eslint/member-ordering */
   @Event({ bubbles: false }) public headClick: EventEmitter<HeadItem>;
   /* eslint-disable-next-line @typescript-eslint/member-ordering */
   @Event({ bubbles: false }) public rowClick: EventEmitter<GenericObject>;
-  /* eslint-disable-next-line @typescript-eslint/member-ordering */
-  private dataItems: GenericObject[] = [];
+
+  public componentWillRender(): void {
+    throwIfElementHasAttribute(this.host, 'head');
+    throwIfElementHasAttribute(this.host, 'data');
+  }
 
   public render(): JSX.Element {
-    this.dataItems = parseJSON(this.data as string) as any;
-
     return (
       <table>
-        <TableHead head={(parseJSON(this.head as string) as unknown) as HeadItem[]} onHeadClick={this.headClick.emit} />
-        <tbody onClick={this.onBodyClick} innerHTML={this.dataItems.map(this.renderRow).join('')} />
+        <TableHead head={this.head} onHeadClick={this.headClick.emit} />
+        <tbody onClick={this.onBodyClick} innerHTML={this.data.map(this.renderRow).join('')} />
       </table>
     );
   }
 
   private onBodyClick = (e: MouseEvent): void => {
     const { rowIndex } = getClosestHTMLElement(e.target as HTMLElement, 'tr');
-    this.rowClick.emit(this.dataItems[rowIndex - 1]); // compensate row in thead
+    this.rowClick.emit(this.data[rowIndex - 1]); // compensate row in thead
   };
 }
