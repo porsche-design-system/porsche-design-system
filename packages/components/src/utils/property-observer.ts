03de2f5ef2bb979a216b1de0d4c0846040f61fda
Anton Schmidt <anton.schmidt@mhp.com>
2021-05-12 16:28:50 +0200

    feat(components): use Object.defineProperties | as | #1380

--- packages/components/src/utils/property-observer.ts
+++ packages/components/src/utils/property-observer.ts
@@ -1,19 +1,28 @@
 export const observeProperties = <T extends HTMLElement>(node: T, props: (keyof T)[], callback: () => void): void => {
   const proto = Object.getPrototypeOf(node);
-  const forEachCallback = (prop: keyof T): void => {
+  const createPropObject = (prop) => {
     // eslint-disable-next-line @typescript-eslint/unbound-method
     const { get, set } = Object.getOwnPropertyDescriptor(proto, prop);
-    // TODO: maybe use Object.defineProperties() instead
-    Object.defineProperty(node, prop, {
-      configurable: true,
-      get() {
-        return get.call(this);
+    return {
+      [prop]: {
+        configurable: true,
+        get() {
+          return get.call(this);
+        },
+        set(val) {
+          set.call(this, val);
+          callback();
+        },
       },
-      set(val) {
-        set.call(this, val);
-        callback();
-      },
-    });
+    };
   };
-  props.forEach(forEachCallback);
+
+  const properties = props
+    .map((prop) => createPropObject(prop))
+    .reduce((target, item) => {
+      const propName = Object.keys(item)[0];
+      return Object.assign(target, { [propName]: item[propName] });
+    });
+
+  Object.defineProperties(node, properties);
 };
