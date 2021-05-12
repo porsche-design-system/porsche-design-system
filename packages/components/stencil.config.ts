ea56b5efa5be55459b645cfdabc665b9b845f5f0
Bernd Hacker <hacker.bernd@gmail.com>
2021-05-12 13:41:27 +0200

    build(components): combine table components into single chunk | bh | #1289

--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -32,6 +32,7 @@ const isDevBuild = process.env.PDS_IS_STAGING === '1';
 const bundles: { components: TagName[] }[] = [
   { components: ['p-grid', 'p-grid-item'] },
   { components: ['p-flex', 'p-flex-item'] },
+  { components: ['p-table', 'p-table-body', 'p-table-head', 'p-table-row', 'p-table-head-cell', 'p-table-cell'] },
   { components: ['p-tabs', 'p-tabs-item'] },
   { components: ['p-text-list', 'p-text-list-item'] },
 ];
