6bc4a18a154d16e15035f4bd0e19ae07e6bce531
Bernd Hacker <hacker.bernd@gmail.com>
2021-05-12 12:17:37 +0200

    feat(components-angular): revert app.component.ts to original state | bh | #1289

--- packages/components-angular/src/app/app.component.ts
+++ packages/components-angular/src/app/app.component.ts
@@ -1,76 +1,27 @@
 import { Component } from '@angular/core';
-
-type Person = {
-  firstName: string;
-  lastName: string;
-  company: string;
-  phoneNumber: string;
-  dateOfBirth: string;
-  location: string;
-};
+import { Router } from '@angular/router';
+import { routes } from './app-routing.module';
 
 @Component({
   selector: 'app-root',
-  template: `<p-table-generics [head]="head" [data]="data" [renderRow]="renderRow"></p-table-generics>`,
+  template: `
+    <select (change)="changeRoute($event.target.value)">
+      <option disabled selected>Select a page</option>
+      <ng-container *ngFor="let route of routes">
+        <option [value]="route.path" [disabled]="route.isDisabled">{{ route.name }}</option>
+      </ng-container>
+    </select>
+    <div id="app">
+      <router-outlet></router-outlet>
+    </div>
+  `,
 })
 export class AppComponent {
-  head = ['First name', 'Last name', 'Company', 'Phone', 'Birthday', 'Location'];
-  data: Person[] = [
-    {
-      // id: '202829625',
-      firstName: 'John',
-      lastName: 'Locke',
-      company: '',
-      phoneNumber: '123123121',
-      dateOfBirth: '1981-06-12',
-      location: 'Boston',
-    },
-    {
-      // id: '202829625',
-      firstName: 'Matthias',
-      lastName: 'Kainer',
-      company: 'Company',
-      phoneNumber: '',
-      dateOfBirth: '1981-06-12',
-      location: 'Bretten',
-    },
-    {
-      // id: '202829625',
-      firstName: 'John',
-      lastName: 'Locke',
-      company: '',
-      phoneNumber: '123123121',
-      dateOfBirth: '1981-06-12',
-      location: 'Boston',
-    },
-    {
-      // id: '202829625',
-      firstName: 'Matthias',
-      lastName: 'Kainer',
-      company: 'Company',
-      phoneNumber: '',
-      dateOfBirth: '1981-06-12',
-      location: 'Bretten',
-    },
-    {
-      // id: '202829625',
-      firstName: 'John',
-      lastName: 'Locke',
-      company: '',
-      phoneNumber: '123123121',
-      dateOfBirth: '1981-06-12',
-      location: 'Boston',
-    },
-  ];
+  public routes = routes.filter((route) => !!route.name);
 
-  renderRow = ({ firstName, lastName, company, phoneNumber, dateOfBirth, location }: any): string => {
-    return `<tr>
-      <td>${firstName}</td>
-      <td>${lastName}</td>
-      <td>${company}</td>
-      <td># ${phoneNumber}</td>
-      <td>† ${dateOfBirth}</td>
-      <td>${location} (Germany)</td>
-    </tr>`;
-  };
+  constructor(private router: Router) {}
+
+  public async changeRoute(path: string): Promise<void> {
+    await this.router.navigateByUrl(`/${path}`);
+  }
 }
