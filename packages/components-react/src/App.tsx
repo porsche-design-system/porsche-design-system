b5080525fdd6cfae137a89ea2a9f27c293f9b653
Bernd Hacker <hacker.bernd@gmail.com>
2021-05-12 12:17:21 +0200

    feat(components-react): revert App.tsx to original state | bh | #1289

--- packages/components-react/src/App.tsx
+++ packages/components-react/src/App.tsx
@@ -1,87 +1,38 @@
-import { PTableGenerics } from '@porsche-design-system/components-react';
-import { useEffect, useState } from 'react';
-
-type Person = {
-  firstName: string;
-  lastName: string;
-  company: string;
-  phoneNumber: string;
-  dateOfBirth: string;
-  location: string;
-};
-
-const head = ['First name', 'Last name', 'Company', 'Phone', 'Birthday', 'Location'];
-const data: Person[] = [
-  {
-    // id: '202829625',
-    firstName: 'John',
-    lastName: 'Locke',
-    company: '',
-    phoneNumber: '123123121',
-    dateOfBirth: '1981-06-12',
-    location: 'Boston',
-  },
-  {
-    // id: '202829625',
-    firstName: 'Matthias',
-    lastName: 'Kainer',
-    company: 'Company',
-    phoneNumber: '',
-    dateOfBirth: '1981-06-12',
-    location: 'Bretten',
-  },
-  {
-    // id: '202829625',
-    firstName: 'John',
-    lastName: 'Locke',
-    company: '',
-    phoneNumber: '123123121',
-    dateOfBirth: '1981-06-12',
-    location: 'Boston',
-  },
-  {
-    // id: '202829625',
-    firstName: 'Matthias',
-    lastName: 'Kainer',
-    company: 'Company',
-    phoneNumber: '',
-    dateOfBirth: '1981-06-12',
-    location: 'Bretten',
-  },
-  {
-    // id: '202829625',
-    firstName: 'John',
-    lastName: 'Locke',
-    company: '',
-    phoneNumber: '123123121',
-    dateOfBirth: '1981-06-12',
-    location: 'Boston',
-  },
-];
+import { useState } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import { routes } from './routes';
 
 export const App = (): JSX.Element => {
-  const [shouldRender, setShouldRender] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      console.log('----------');
-      setShouldRender(true);
-    }, 1000);
-  }, []);
-
-  const renderRow = ({ firstName, lastName, company, phoneNumber, dateOfBirth, location }: any): string =>
-    `<tr>
-      <td>${firstName}</td>
-      <td>${lastName}</td>
-      <td>${company}</td>
-      <td># ${phoneNumber}</td>
-      <td>† ${dateOfBirth}</td>
-      <td>${location} (Germany)</td>
-    </tr>`;
+  const history = useHistory();
+  const [selected, setSelected] = useState(history.location.pathname);
 
   return (
     <>
-      <PTableGenerics head={head} data={data} renderRow={renderRow} />
-      {shouldRender && <PTableGenerics head={head} data={data} renderRow={renderRow} />}
+      <select
+        value={selected}
+        onChange={(e) => {
+          const { value } = e.target;
+          history.push(value);
+          setSelected(value);
+        }}
+      >
+        <option disabled value="">
+          Select a page
+        </option>
+        {routes.map((route) => (
+          <option key={route.path || route.name} disabled={route.isDisabled} value={route.path} children={route.name} />
+        ))}
+      </select>
+
+      <div id="app">
+        <Switch>
+          {routes
+            .filter((route) => !route.isDisabled)
+            .map((route) => (
+              <Route key={route.path} {...route} />
+            ))}
+        </Switch>
+      </div>
     </>
   );
 };
