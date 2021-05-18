92b42d0db046885bc1f7ba92459d81aff7c63fbf
Anton Schmidt <anton.schmidt@mhp.com>
2021-05-18 15:24:05 +0200

    feat(components-angular/js/react, storefront): update puppeteer, re-add tsignore for re.opened bug ticket of spec reporter, update fixtures | as /  mh | #1319

--- packages/storefront/tests/e2e/helpers/setup.ts
+++ packages/storefront/tests/e2e/helpers/setup.ts
@@ -6,6 +6,8 @@ let browser: Browser;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
 
 jasmine.getEnv().clearReporters();
+// TODO: remove @ts-ignore when https://github.com/bcaudan/jasmine-spec-reporter/issues/588 is fixed
+//@ts-ignore
 jasmine.getEnv().addReporter(new SpecReporter());
 
 beforeAll(async () => {
