f0fbac486413938b455a5ac010d9da3281e35923
Bernd Hacker <hacker.bernd@gmail.com>
2021-05-27 09:52:09 +0200

    docs(components): extend table docs | bh | #1289

---
+++ packages/components/src/components/layout/table/table.data.ts
@@ -0,0 +1,91 @@
+import { TableHeadItem } from './table-utils';
+
+type HeadItem = {
+  name: string;
+  key?: string;
+};
+
+type DataItem = {
+  id?: string;
+  imageUrl: string;
+  model: string;
+  date: string;
+  interest: string;
+  vin: string;
+  purchaseIntention: string;
+  status: string;
+  leadId: string;
+};
+
+export const head: HeadItem[] = [
+  { name: 'Model' },
+  { name: 'Interest', key: 'interest' },
+  { name: 'VIN', key: 'vin' },
+  { name: 'Purchase Intention', key: 'purchaseIntention' },
+  { name: 'Status', key: 'status' },
+  { name: 'Lead ID', key: 'leadId' },
+  { name: '' },
+  { name: '' },
+];
+
+export const data: DataItem[] = [
+  {
+    imageUrl: 'https://nav.porsche.com/00BC524/series-assets/1366/911.jpg',
+    model: '911 Carrera S',
+    date: '03.05.2021',
+    interest: 'New Car',
+    vin: '-',
+    purchaseIntention: '08.2020',
+    status: 'Won',
+    leadId: '0000824402',
+  },
+  {
+    imageUrl: 'https://nav.porsche.com/00BC524/series-assets/1366/taycan.jpg',
+    model: '911 Carrera S',
+    date: '03.05.2021',
+    interest: 'New Car',
+    vin: '-',
+    purchaseIntention: '11.2020',
+    status: 'Lost',
+    leadId: '0000824409',
+  },
+  {
+    imageUrl: 'https://nav.porsche.com/00BC524/series-assets/1366/911.jpg',
+    model: '911 Carrera S',
+    date: '03.05.2021',
+    interest: 'Used Car',
+    vin: '-',
+    purchaseIntention: '09.2020',
+    status: 'Won',
+    leadId: '0000824408',
+  },
+  {
+    imageUrl: 'https://nav.porsche.com/00BC524/series-assets/1366/718.jpg',
+    model: '911 Carrera S',
+    date: '03.05.2021',
+    interest: 'Used Car',
+    vin: '-',
+    purchaseIntention: '04.2020',
+    status: 'Lost',
+    leadId: '0000824407',
+  },
+  {
+    imageUrl: 'https://nav.porsche.com/00BC524/series-assets/1366/911.jpg',
+    model: '911 Carrera S',
+    date: '03.05.2021',
+    interest: 'New Car',
+    vin: '-',
+    purchaseIntention: '03.2020',
+    status: 'Won',
+    leadId: '0000824406',
+  },
+];
+
+export const headSortable = head.map<TableHeadItem>((item, i) => ({
+  ...item,
+  ...(item.name && {
+    isSortable: i > 0,
+    isSorting: i === 1,
+    direction: 'asc',
+  }),
+}));
