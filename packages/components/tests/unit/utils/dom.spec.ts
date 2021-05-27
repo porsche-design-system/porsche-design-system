12d66a24b8d54c7aa339020415ce24ff962fc0dd
Bernd Hacker <hacker.bernd@gmail.com>
2021-05-27 08:01:30 +0200

    test(components): add unit tests for hasAttribute | bh | #1289

--- packages/components/tests/unit/utils/dom.spec.ts
+++ packages/components/tests/unit/utils/dom.spec.ts
@@ -15,6 +15,7 @@ import {
   isParentFieldsetWrapperRequired,
   getRole,
   isRequiredAndParentNotRequired,
+  hasAttribute,
 } from '../../../src/utils';
 import type { HTMLElementWithRequiredProp } from '../../../src/utils';
 import type { FormState } from '../../../src/types';
@@ -212,6 +213,19 @@ describe('removeAttribute()', () => {
 
     removeAttribute(element, 'title');
     expect(element.getAttribute('title')).toBe(null);
+  });
+});
+
+describe('hasAttribute()', () => {
+  it('should return true if attribute exists', () => {
+    const element = document.createElement('div');
+    element.setAttribute('title', 'Some title');
+    expect(hasAttribute(element, 'title')).toBe(true);
+  });
+
+  it('should return false if attribute does not exist', () => {
+    const element = document.createElement('div');
+    expect(hasAttribute(element, 'title')).toBe(false);
   });
 });
 
