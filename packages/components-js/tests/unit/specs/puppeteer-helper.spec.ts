3d735d8947db67d2291060bcfb8bfc7324d4655e
Anton Schmidt <anton.schmidt@mhp.com>
2021-05-27 14:28:25 +0200

    test(components-js): add unit test for puppeteer helper | as | #1319

---
+++ packages/components-js/tests/unit/specs/puppeteer-helper.spec.ts
@@ -0,0 +1,54 @@
+import Protocol from 'devtools-protocol';
+import { findBackendNodeId } from '../../e2e/helpers';
+type Node = Pick<Protocol.DOM.Node, 'localName' | 'backendNodeId'>;
+type NodeWithChildren = Node & { children?: NodeWithChildren[] };
+type TestCase = {
+  node: NodeWithChildren;
+  selector: string;
+  expect: number;
+};
+
+describe('puppeteer-helper', () => {
+  describe('findBackendNodeId()', () => {
+    const testCases: TestCase[] = [
+      { node: { localName: 'test', backendNodeId: 1 }, selector: 'test', expect: 1 },
+      {
+        node: { localName: 'test1', backendNodeId: 1, children: [{ localName: 'test', backendNodeId: 2 }] },
+        selector: 'test',
+        expect: 2,
+      },
+      {
+        node: {
+          localName: 'test1',
+          backendNodeId: 1,
+          children: [{ localName: 'test2', backendNodeId: 2, children: [{ localName: 'test', backendNodeId: 3 }] }],
+        },
+        selector: 'test',
+        expect: 3,
+      },
+      {
+        node: {
+          localName: 'test1',
+          backendNodeId: 1,
+        },
+        selector: 'test',
+        expect: undefined,
+      },
+      {
+        node: {
+          localName: 'test1',
+          backendNodeId: 1,
+          children: [{ localName: 'test2', backendNodeId: 2, children: [{ localName: 'test3', backendNodeId: 3 }] }],
+        },
+        selector: 'test',
+        expect: undefined,
+      },
+    ];
+
+    testCases.forEach((test) => {
+      it(`should for ${test.node} and selector ${test.selector} return ${test.expect}`, () => {
+        expect(findBackendNodeId(test.node as Protocol.DOM.Node, test.selector)).toBe(test.expect);
+      });
+    });
+  });
+});
