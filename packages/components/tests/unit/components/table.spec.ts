ce64238e2577fc81209222dde40c8ef55aa56e67
Bernd Hacker <hacker.bernd@gmail.com>
2021-05-27 10:31:01 +0200

    test(components): add unit tests for table components | bh | #1289

---
+++ packages/components/tests/unit/components/table.spec.ts
@@ -0,0 +1,15 @@
+import * as slottedStylesUtils from '../../../src/utils/slotted-styles';
+import { Table } from '../../../src/components/layout/table/table/table';
+
+describe('table', () => {
+  describe('connectedCallback', () => {
+    it('should call insertSlottedStyles()', () => {
+      const spy = jest.spyOn(slottedStylesUtils, 'insertSlottedStyles');
+      const component = new Table();
+      component.host = document.createElement('p-table');
+      component.connectedCallback();
+
+      expect(spy).toBeCalledWith(component.host, expect.anything());
+    });
+  });
+});
