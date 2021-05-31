b4ce4a8f01d522304545b432283d55304cf04796
Anton Schmidt <anton.schmidt@mhp.com>
2021-05-31 11:43:24 +0200

    test(components-js): add textarea, tabs and text-field hover + focus vrtests | as / mh | #1319

--- packages/components-js/tests/vrt/specs/tabs-bar.vrt.ts
+++ packages/components-js/tests/vrt/specs/tabs-bar.vrt.ts
@@ -1,4 +1,13 @@
-import { getVisualRegressionTester, testOptions } from '../helpers';
+import { getVisualRegressionStatesTester, getVisualRegressionTester, testOptions } from '../helpers';
+import { Theme } from '@porsche-design-system/utilities';
+import {
+  CSS_ANIMATION_DURATION,
+  forceFocused,
+  forceFocusedHovered,
+  forceHovered,
+  getThemedBody,
+  setContentWithDesignSystem,
+} from '../../e2e/helpers';
 
 describe('Tabs Bar', () => {
   it('should have no visual regression', async () => {
@@ -12,6 +21,34 @@ describe('Tabs Bar', () => {
         },
         testOptions
       )
+    ).toBeFalsy();
+  });
+
+  it('should have no visual regression for :hover + :focus-visible', async () => {
+    const vrt = getVisualRegressionStatesTester();
+    expect(
+      await vrt.test('tabs-bar-states', async () => {
+        const page = await vrt.getPage();
+
+        const head = `
+          <link rel="stylesheet" href="styles.css" />`;
+
+        const getElements = (theme: Theme = 'light'): string => `
+          <p-tabs-bar theme="${theme}">
+            <button type="button">Tab One</button>
+            <button type="button">Tab Two</button>
+            <button type="button">Tab Three</button>
+          </p-tabs-bar>`;
+
+        await setContentWithDesignSystem(page, getThemedBody(getElements), { injectIntoHead: head });
+
+        await forceHovered(page, '.hovered > p-tabs-bar button');
+        await forceFocused(page, '.focused > p-tabs-bar button');
+        await forceFocusedHovered(page, '.focused-hovered > p-tabs-bar button');
+
+        //wait for all style transitions to finish
+        await page.waitForTimeout(CSS_ANIMATION_DURATION);
+      })
     ).toBeFalsy();
   });
 });
