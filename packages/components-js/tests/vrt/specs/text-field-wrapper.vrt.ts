a4a0e39a4051d53c6fac5677d4bd067baf63a470
Anton Schmidt <anton.schmidt@mhp.com>
2021-05-31 11:43:24 +0200

    test(components-js): add textarea, tabs and text-field hover + focus vrtests | as / mh | #1319

--- packages/components-js/tests/vrt/specs/text-field-wrapper.vrt.ts
+++ packages/components-js/tests/vrt/specs/text-field-wrapper.vrt.ts
@@ -1,4 +1,12 @@
-import { getVisualRegressionTester, testOptions } from '../helpers';
+import { getVisualRegressionStatesTester, getVisualRegressionTester, testOptions } from '../helpers';
+import {
+  CSS_ANIMATION_DURATION,
+  forceFocused,
+  forceFocusedHovered,
+  forceHovered,
+  getBody,
+  setContentWithDesignSystem,
+} from '../../e2e/helpers';
 
 describe('Text Field Wrapper', () => {
   it('should have no visual regression', async () => {
@@ -11,6 +19,39 @@ describe('Text Field Wrapper', () => {
         },
         testOptions
       )
+    ).toBeFalsy();
+  });
+
+  it('should have no visual regression for :hover + :focus-visible', async () => {
+    const vrt = getVisualRegressionStatesTester();
+    expect(
+      await vrt.test('text-field-wrapper-states', async () => {
+        const page = await vrt.getPage();
+
+        const head = `
+          <link rel="stylesheet" href="styles.css" />
+          <style type="text/css">p-text-field-wrapper:not(:last-child) { margin-bottom: 16px; }</style>`;
+
+        const getElements = (): string => `
+          <p-text-field-wrapper label="Some label" message="Some error validation message.">
+            <input type="text" name="some-name" />
+          </p-text-field-wrapper>
+          <p-text-field-wrapper label="Some label" state="error" message="Some error validation message.">
+            <input type="text" name="some-name" />
+          </p-text-field-wrapper>
+          <p-text-field-wrapper label="Some label" state="success" message="Some success validation message.">
+            <input type="text" name="some-name" />
+          </p-text-field-wrapper>`;
+
+        await setContentWithDesignSystem(page, getBody(getElements), { injectIntoHead: head });
+
+        await forceHovered(page, '.hovered > p-text-field-wrapper input[type="text"]');
+        await forceFocused(page, '.focused > p-text-field-wrapper input[type="text"]');
+        await forceFocusedHovered(page, '.focused-hovered > p-text-field-wrapper input[type="text"]');
+
+        //wait for all style transitions to finish
+        await page.waitForTimeout(CSS_ANIMATION_DURATION);
+      })
     ).toBeFalsy();
   });
 });
--- packages/components-js/tests/vrt/specs/textarea-wrapper.vrt.ts
+++ packages/components-js/tests/vrt/specs/textarea-wrapper.vrt.ts
@@ -1,4 +1,12 @@
-import { getVisualRegressionTester, testOptions } from '../helpers';
+import { getVisualRegressionStatesTester, getVisualRegressionTester, testOptions } from '../helpers';
+import {
+  CSS_ANIMATION_DURATION,
+  forceFocused,
+  forceFocusedHovered,
+  forceHovered,
+  getBody,
+  setContentWithDesignSystem,
+} from '../../e2e/helpers';
 
 describe('Textarea Wrapper', () => {
   it('should have no visual regression', async () => {
@@ -11,6 +19,38 @@ describe('Textarea Wrapper', () => {
         },
         testOptions
       )
+    ).toBeFalsy();
+  });
+  it('should have no visual regression for :hover + :focus-visible', async () => {
+    const vrt = getVisualRegressionStatesTester();
+    expect(
+      await vrt.test('textarea-wrapper-states', async () => {
+        const page = await vrt.getPage();
+
+        const head = `
+          <link rel="stylesheet" href="styles.css" />
+          <style type="text/css">p-textarea-wrapper:not(:last-child) { margin-bottom: 16px; }</style>`;
+
+        const getElements = (): string => `
+          <p-textarea-wrapper label="Some label" message="Some error validation message.">
+            <textarea name="some-name"></textarea>
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Some label" state="error" message="Some error validation message.">
+            <textarea name="some-name"></textarea>
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Some label" state="success" message="Some success validation message.">
+            <textarea name="some-name"></textarea>
+          </p-textarea-wrapper>`;
+
+        await setContentWithDesignSystem(page, getBody(getElements), { injectIntoHead: head });
+
+        await forceHovered(page, '.hovered > p-textarea-wrapper textarea');
+        await forceFocused(page, '.focused > p-textarea-wrapper textarea');
+        await forceFocusedHovered(page, '.focused-hovered > p-textarea-wrapper textarea');
+
+        //wait for all style transitions to finish
+        await page.waitForTimeout(CSS_ANIMATION_DURATION);
+      })
     ).toBeFalsy();
   });
 });
