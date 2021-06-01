7f45c33d2ca9adeeebeefe253484464339f5d11a
Bernd Hacker <hacker.bernd@gmail.com>
2021-06-01 13:55:26 +0200

    test(components-js/angular/react): add vrt tests for fullscreen breakpoint modal | bh / sas | #1430

---
+++ packages/components-angular/src/app/pages/modal-fullscreen-breakpoint.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-modal-fullscreen-breakpoint',
+  styles: [
+    `
+      .playground {
+        height: 500px;
+        padding: 0;
+      }
+    `,
+  ],
+  template: `
+    <div class="playground light" title="should show fullscreen modal on light background">
+      <p-modal
+        [heading]="'Some Heading with a very long title across multiple lines'"
+        [open]="true"
+        [fullscreen]="{ base: true, xs: false, s: true, m: false, l: true, xl: false }"
+      >
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+        <br />
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
+        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+        takimata sanctus est Lorem ipsum dolor sit amet.
+      </p-modal>
+    </div>
+  `,
+})
+export class ModalFullscreenBreakpointComponent {
+  constructor() {
+    document.body.style.height = '500px';
+  }
+}
