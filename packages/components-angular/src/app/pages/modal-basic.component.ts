5adabe45bd0b753c4ec4d273a8bae1644be1e0f3
Bernd Hacker <hacker.bernd@gmail.com>
2021-06-01 16:16:11 +0200

    test(components-js/angular/react): remove useless markup from modal vrt tests | bh | #1430

--- packages/components-angular/src/app/pages/modal-basic.component.ts
+++ packages/components-angular/src/app/pages/modal-basic.component.ts
@@ -16,21 +16,7 @@ import { Component } from '@angular/core';
       <p-content-wrapper>
         <div style="background: deeppink; height: 100vh"></div>
       </p-content-wrapper>
-      <p-modal [heading]="'Some Heading'" [open]="true">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-        takimata sanctus est Lorem ipsum dolor sit amet.
-        <br />
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-        consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
-        takimata sanctus est Lorem ipsum dolor sit amet.
-      </p-modal>
+      <p-modal [heading]="'Some Heading'" [open]="true">Some Content</p-modal>
     </div>
   `,
 })
