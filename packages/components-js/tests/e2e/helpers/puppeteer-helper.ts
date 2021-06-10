ceb7e84b0dc0bc403b9a2e12e4ebac56a6a4f645
Matthias Haegele <matthias.haegele@mhp.com>
2021-06-10 12:43:13 +0200

    test(components-js): implement override.css into public index | mh / as | #1360

--- packages/components-js/tests/e2e/helpers/puppeteer-helper.ts
+++ packages/components-js/tests/e2e/helpers/puppeteer-helper.ts
@@ -21,7 +21,7 @@ export const setContentWithDesignSystem = async (page: Page, content: string, op
       <head>
         <base href="http://localhost:8575"> <!-- NOTE: we need a base tag so that document.baseURI returns something else than "about:blank" -->
         <script type="text/javascript" src="http://localhost:8575/index.js"></script>
-        <link rel="stylesheet" href="styles-override.css" />
+        <link rel="stylesheet" href="overrides.css" >
         ${options.injectIntoHead}
       </head>
       <body>
