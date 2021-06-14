f8552e98046ba2cb7e3434481891608344e5f5a2
Anton Schmidt <anton.schmidt@mhp.com>
2021-06-14 10:13:24 +0200

    feat(components): clean up panel component, add base root styles | as / mh | #1333

--- packages/components/src/components/content/panel/panel.tsx
+++ packages/components/src/components/content/panel/panel.tsx
@@ -1,9 +1,8 @@
-import { Component, Element, Event, EventEmitter, Prop, Watch, h } from '@stencil/core';
+import { Component, Element, Event, EventEmitter, Prop, h } from '@stencil/core';
 import type { BreakpointCustomizable, Theme } from '../../../types';
 import { getPrefixedTagNames, isDark, mapBreakpointPropToClasses } from '../../../utils';
-import { getTitleTag, PanelSize, PanelStateChangeEvent, PanelWeight } from './panel-utils';
+import { generateGUID, getTitleTag, PanelSize, PanelStateChangeEvent, PanelWeight } from './panel-utils';
 import { HeadlineTag } from '../../basic/typography/headline/headline-utils';
-import { generateGUID } from '../../../../../components-js/tests/vrt/helpers';
 
 @Component({
   tag: 'p-panel',
@@ -34,34 +33,7 @@ export class Panel {
   /** Emitted when panel state is changed. */
   @Event({ bubbles: false }) public panelStateChange: EventEmitter<PanelStateChangeEvent>;
 
-  // private hostObserver: MutationObserver;
-  // private headlineElement: HTMLElement;
-  // private panelButtonElement: HTMLElement;
-  // private iconElement: HTMLElement;
-  // private contentContainer: HTMLElement;
   // private hasAccordionParent: boolean = getHasPAccordionParent(this.host);
-
-  @Watch('open')
-  public openChangeHandler(open: boolean): void {
-    this.open = open;
-  }
-  //
-  // public connectedCallback(): void {
-  //   this.initMutationObserver();
-  // }
-
-  // public componentDidLoad(): void {
-  // this.defineHTMLElements();
-  // this.addEventListeners();
-  // }
-  //
-  // public componentDidRender(): void {
-  //   this.setAccessibilityAttributes();
-  // }
-
-  // public disconnectedCallback(): void {
-  // this.hostObserver.disconnect();
-  // }
 
   public render(): JSX.Element {
     const TagName = getTitleTag(this.tag);
@@ -83,12 +55,12 @@ export class Panel {
             <PrefixedTagNames.pText size={this.size} weight={this.weight} color="inherit" tag="span">
               {this.panelTitle}
               {/* TODO: slotted title? */}
-              <p-icon
+              <PrefixedTagNames.pIcon
                 name={this.open ? 'close' : 'plus'}
                 aria-label={this.open ? 'Close Icon' : 'Plus icon'}
                 class="icon"
                 lazy={true}
-              ></p-icon>
+              ></PrefixedTagNames.pIcon>
             </PrefixedTagNames.pText>
           </button>
         </TagName>
@@ -102,63 +74,4 @@ export class Panel {
   private handleToggleClick = (): void => {
     this.panelStateChange.emit({ open: !this.open });
   };
-
-  // private defineHTMLElements = (): void => {
-  // const { shadowRoot } = this.host;
-  // this.headlineElement = getHTMLElement(shadowRoot, '.title');
-  // this.panelButtonElement = getHTMLElement(shadowRoot, '.button');
-  // this.iconElement = getHTMLElement(shadowRoot, '.icon');
-  // this.contentContainer = getHTMLElement(shadowRoot, '.content');
-  // };
-
-  // private addEventListeners = (): void => {
-  // this.scrollAreaElement.addEventListener('keydown', this.handleKeydown);
-  // };
-
-  // private initMutationObserver = (): void => {
-  //   this.hostObserver = new MutationObserver((): void => {
-  //     this.setAccessibilityAttributes();
-  //   });
-  //   this.hostObserver.observe(this.host, {
-  //     childList: true,
-  //     subtree: true,
-  //     characterData: true,
-  //   });
-  // };
-
-  // private handleKeydown = (e: KeyboardEvent): void => {
-  //   let upcomingPanelIndex: number;
-  //   switch (e.key) {
-  //     case 'ArrowDown':
-  //     case 'Down':
-  //       upcomingPanelIndex = this.getPrevNextTabIndex('prev');
-  //       break;
-  //
-  //     case 'ArrowUp':
-  //     case 'Up':
-  //       upcomingPanelIndex = this.getPrevNextTabIndex('next');
-  //       break;
-  //
-  //     case 'Home':
-  //       upcomingPanelIndex = 0;
-  //       break;
-  //
-  //     case 'End':
-  //       upcomingPanelIndex = this.tabElements.length - 1;
-  //       break;
-  //
-  //     case 'Enter':
-  //       this.handleTabClick(this.focusedTabIndex);
-  //       return;
-  //
-  //     default:
-  //       return;
-  //   }
-  //
-  //   if (this.hasAccordionParent) {
-  //     this.handleTabClick(upcomingPanelIndex);
-  //   }
-  //
-  //   e.preventDefault();
-  // };
 }
