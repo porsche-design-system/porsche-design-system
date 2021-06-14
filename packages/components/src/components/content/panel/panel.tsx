3341fdf1dc112700e39d27ca042c72070afe7561
Anton Schmidt <anton.schmidt@mhp.com>
2021-06-14 09:46:57 +0200

    feat(components): add panelTitle and tag to panel | as / mh | #1333

--- packages/components/src/components/content/panel/panel.tsx
+++ packages/components/src/components/content/panel/panel.tsx
@@ -1,8 +1,9 @@
 import { Component, Element, Event, EventEmitter, Prop, Watch, h } from '@stencil/core';
 import type { BreakpointCustomizable, Theme } from '../../../types';
-import { HeadlineVariant } from '../../../types';
-import { isDark, mapBreakpointPropToClasses } from '../../../utils';
-import { PanelSize, PanelStateChangeEvent } from './panel-utils';
+import { getPrefixedTagNames, isDark, mapBreakpointPropToClasses } from '../../../utils';
+import { getTitleTag, PanelSize, PanelStateChangeEvent, PanelWeight } from './panel-utils';
+import { HeadlineTag } from '../../basic/typography/headline/headline-utils';
+import { generateGUID } from '../../../../../components-js/tests/vrt/helpers';
 
 @Component({
   tag: 'p-panel',
@@ -16,24 +17,24 @@ export class Panel {
   @Prop() public size?: BreakpointCustomizable<PanelSize> = 'small';
 
   /** The text weight. */
-  // @Prop() public weight?: TabWeight = 'regular';
+  @Prop() public weight?: PanelWeight = 'regular';
 
   /** Adapts the color when used on dark background. */
   @Prop() public theme?: Theme = 'light';
 
   /** Defines the title used in panel. */
-  @Prop() public title: string;
+  @Prop() public panelTitle: string;
 
-  /** Predefined style of the headline. */
-  @Prop() public headlineVariant?: HeadlineVariant = 'headline-1';
+  /** Sets a headline tag, so it fits correctly within the outline of the page. */
+  @Prop() public tag?: HeadlineTag;
 
   /** Defines if panel is open. */
   @Prop() public open?: boolean;
 
   /** Emitted when panel state is changed. */
-  @Event({ bubbles: false }) public stateChange: EventEmitter<PanelStateChangeEvent>;
+  @Event({ bubbles: false }) public panelStateChange: EventEmitter<PanelStateChangeEvent>;
 
-  private hostObserver: MutationObserver;
+  // private hostObserver: MutationObserver;
   // private headlineElement: HTMLElement;
   // private panelButtonElement: HTMLElement;
   // private iconElement: HTMLElement;
@@ -43,62 +44,55 @@ export class Panel {
   @Watch('open')
   public openChangeHandler(open: boolean): void {
     this.open = open;
-    this.setAccessibilityAttributes();
-    if (open) {
-      // this.focusableElements = getFocusableElements(this.host, this.closeBtn);
-      // this.focusedElBeforeOpen = document.activeElement as HTMLElement;
-    } else {
-      // this.focusedElBeforeOpen?.focus();
-    }
   }
+  //
+  // public connectedCallback(): void {
+  //   this.initMutationObserver();
+  // }
 
-  public connectedCallback(): void {
-    this.initMutationObserver();
-  }
+  // public componentDidLoad(): void {
+  // this.defineHTMLElements();
+  // this.addEventListeners();
+  // }
+  //
+  // public componentDidRender(): void {
+  //   this.setAccessibilityAttributes();
+  // }
 
-  public componentDidLoad(): void {
-    this.defineHTMLElements();
-    this.addEventListeners();
-  }
-
-  public componentDidRender(): void {
-    this.setAccessibilityAttributes();
-  }
-
-  public disconnectedCallback(): void {
-    this.hostObserver.disconnect();
-  }
+  // public disconnectedCallback(): void {
+  // this.hostObserver.disconnect();
+  // }
 
   public render(): JSX.Element {
+    const TagName = getTitleTag(this.tag);
+
     const rootClasses = {
       ['root']: true,
       ['root--theme-dark']: isDark(this.theme),
-      // ['root--weight-semibold']: this.weight !== 'regular',
+      ['root--weight-semibold']: this.weight !== 'regular',
       ...mapBreakpointPropToClasses('root--size', this.size),
     };
 
+    const PrefixedTagNames = getPrefixedTagNames(this.host);
+    const id = `panel-${generateGUID()}`;
+
     return (
       <div class={rootClasses}>
-        <p-headline variant={this.headlineVariant}>
-          <button
-            aria-expanded={this.open}
-            class="button"
-            aria-controls={this.title}
-            id="accordion1id"
-            onClick={this.handleToggleClick}
-          >
-            <span class="title">
-              {this.title}
+        <TagName>
+          <button aria-expanded={this.open} aria-controls={this.panelTitle} id={id} onClick={this.handleToggleClick}>
+            <PrefixedTagNames.pText size={this.size} weight={this.weight} color="inherit" tag="span">
+              {this.panelTitle}
+              {/* TODO: slotted title? */}
               <p-icon
                 name={this.open ? 'close' : 'plus'}
                 aria-label={this.open ? 'Close Icon' : 'Plus icon'}
                 class="icon"
                 lazy={true}
               ></p-icon>
-            </span>
+            </PrefixedTagNames.pText>
           </button>
-        </p-headline>
-        <div id={this.title} role="region" aria-labelledby="accordion1id" class="content" hidden={!this.open}>
+        </TagName>
+        <div id={this.panelTitle} role="region" aria-labelledby={id} class="content" hidden={!this.open}>
           <slot />
         </div>
       </div>
@@ -106,47 +100,31 @@ export class Panel {
   }
 
   private handleToggleClick = (): void => {
-    this.stateChange.emit({ open: !this.open });
+    this.panelStateChange.emit({ open: !this.open });
   };
 
-  private setAccessibilityAttributes = (): void => {
-    // for (const [index, tab] of Object.entries(this.tabElements)) {
-    //   const tabIndex = this.activeTabIndex ?? 0;
-    //   const isFocusable = tabIndex === +index;
-    //   const isSelected = this.activeTabIndex === +index;
-    //   const attrs = {
-    //     role: 'tab',
-    //     tabindex: isFocusable ? '0' : '-1',
-    //     'aria-selected': isSelected ? 'true' : 'false',
-    //   };
-    //   for (const [key, value] of Object.entries(attrs)) {
-    //     setAttribute(tab, key, value);
-    //   }
-    // }
-  };
+  // private defineHTMLElements = (): void => {
+  // const { shadowRoot } = this.host;
+  // this.headlineElement = getHTMLElement(shadowRoot, '.title');
+  // this.panelButtonElement = getHTMLElement(shadowRoot, '.button');
+  // this.iconElement = getHTMLElement(shadowRoot, '.icon');
+  // this.contentContainer = getHTMLElement(shadowRoot, '.content');
+  // };
 
-  private defineHTMLElements = (): void => {
-    // const { shadowRoot } = this.host;
-    // this.headlineElement = getHTMLElement(shadowRoot, '.title');
-    // this.panelButtonElement = getHTMLElement(shadowRoot, '.button');
-    // this.iconElement = getHTMLElement(shadowRoot, '.icon');
-    // this.contentContainer = getHTMLElement(shadowRoot, '.content');
-  };
+  // private addEventListeners = (): void => {
+  // this.scrollAreaElement.addEventListener('keydown', this.handleKeydown);
+  // };
 
-  private addEventListeners = (): void => {
-    // this.scrollAreaElement.addEventListener('keydown', this.handleKeydown);
-  };
-
-  private initMutationObserver = (): void => {
-    this.hostObserver = new MutationObserver((): void => {
-      this.setAccessibilityAttributes();
-    });
-    this.hostObserver.observe(this.host, {
-      childList: true,
-      subtree: true,
-      characterData: true,
-    });
-  };
+  // private initMutationObserver = (): void => {
+  //   this.hostObserver = new MutationObserver((): void => {
+  //     this.setAccessibilityAttributes();
+  //   });
+  //   this.hostObserver.observe(this.host, {
+  //     childList: true,
+  //     subtree: true,
+  //     characterData: true,
+  //   });
+  // };
 
   // private handleKeydown = (e: KeyboardEvent): void => {
   //   let upcomingPanelIndex: number;
---
+++ packages/components/tests/unit/components/panel-utils.spec.ts
@@ -0,0 +1,5 @@
+describe('getTitleTag()', () => {
+  it('should work', () => {
+    expect(true).toBe(true);
+  });
+});
