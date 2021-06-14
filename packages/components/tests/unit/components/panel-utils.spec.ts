7b79d1624a6231e37f05c26848dad4fe90e75292
Anton Schmidt <anton.schmidt@mhp.com>
2021-06-14 10:37:21 +0200

    test(components): add getTitleTag() unit test | as / mh | #1333

--- packages/components/tests/unit/components/panel-utils.spec.ts
+++ packages/components/tests/unit/components/panel-utils.spec.ts
@@ -1,5 +1,16 @@
+import { getTitleTag } from '../../../src/components/content/panel/panel-utils';
+import { HeadlineTag } from '../../../src/components/basic/typography/headline/headline-utils';
+
 describe('getTitleTag()', () => {
-  it('should work', () => {
-    expect(true).toBe(true);
+  it.each<[HeadlineTag, HeadlineTag]>([
+    [undefined, 'h2'],
+    ['h1', 'h1'],
+    ['h2', 'h2'],
+    ['h3', 'h3'],
+    ['h4', 'h4'],
+    ['h5', 'h5'],
+    ['h6', 'h6'],
+  ])('for %s should return %s', (titleTag, result) => {
+    expect(getTitleTag(titleTag)).toBe(result);
   });
 });
