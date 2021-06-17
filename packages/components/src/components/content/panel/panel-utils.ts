2590b5f7f79ea103db737afffcb30e48cd619001
Matthias Haegele <matthias.haegele@mhp.com>
2021-06-17 10:34:13 +0200

    test(components): remove panel-utils test / remove unused function | mh | #1333

--- packages/components/src/components/content/panel/panel-utils.ts
+++ packages/components/src/components/content/panel/panel-utils.ts
@@ -1,17 +1,3 @@
-// import { getPrefixedTagNames, getTagName } from '../../../utils';
-
-import { HeadlineTag } from '../../basic/typography/headline/headline-utils';
-
 const PANEL_SIZE = ['small', 'medium'] as const;
 export type PanelSize = typeof PANEL_SIZE[number];
 export type PanelChangeEvent = { open: boolean };
-
-// export const getHasPAccordionParent = (hostEl: HTMLElement): boolean => {
-//   const { host } = hostEl.getRootNode() as ShadowRoot;
-//   const parentTagName = host && getTagName(host as HTMLElement);
-//   return parentTagName === getPrefixedTagNames(hostEl).pAccordion;
-// };
-
-export const getTitleTag = (tag?: HeadlineTag): string => {
-  return tag ? tag : 'h2';
-};
