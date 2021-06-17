d7adb6bb6efb69834345ba310758591e0d10e9cf
Matthias Haegele <matthias.haegele@mhp.com>
2021-06-17 16:39:48 +0200

    test(components): extend state test with hover while open | mh | #1333

--- packages/components-js/tests/vrt/specs/accordion.vrt.ts
+++ packages/components-js/tests/vrt/specs/accordion.vrt.ts
@@ -39,6 +39,12 @@ describe('Accordion', () => {
              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
              sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
             </p>
+          </p-accordion>
+          <p-accordion theme="${theme}" heading="Some heading" open="true">
+            <p ${theme === 'dark' && 'style="color: white"'}>
+             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+             sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
           </p-accordion>`;
 
         await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup));
