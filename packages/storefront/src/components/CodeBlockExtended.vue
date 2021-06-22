3364043c42a39d234000601f34d44d86591e6c43
Bernd Hacker <hacker.bernd@gmail.com>
2021-06-22 12:44:15 +0200

    feat(storefront): improve CodeBlockExtended to use CodeBlock internally, revert previous extraction of code-highlighting.scss | bh | #1289

--- packages/storefront/src/components/CodeBlockExtended.vue
+++ packages/storefront/src/components/CodeBlockExtended.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="code-block">
-    <p-tabs-bar :active-tab-index="activeTabIndex">
-      <button type="button" v-for="(framework, index) in usedFrameworks" :key="index" @click="setFramework(index)">
-        {{ framework }}
-      </button>
-    </p-tabs-bar>
-    <pre><code v-html="highlightedMarkup"></code></pre>
+  <div :class="{ example: isSlotSet }">
+    <div v-if="isSlotSet" class="demo">
+      <slot />
+    </div>
+    <CodeBlock :markup="markup" :frameworks="Object.keys(frameworks)"></CodeBlock>
   </div>
 </template>
 
@@ -14,41 +12,26 @@
   import Component from 'vue-class-component';
   import { Prop } from 'vue-property-decorator';
   import type { Framework, FrameworkMarkup } from '@/models';
-  import { getHighlightedCode } from '@/utils';
+  import CodeBlock from '@/components/CodeBlock.vue';
 
-  @Component
+  @Component({
+    components: {
+      CodeBlock,
+    },
+  })
   export default class CodeBlockExtended extends Vue {
-    @Prop({ default: [] }) public frameworks!: FrameworkMarkup;
-
-    allFrameworks: Required<FrameworkMarkup> = {
-      'vanilla-js': 'Vanilla JS',
-      angular: 'Angular',
-      react: 'React',
-      shared: 'Shared',
-    };
-
-    public get usedFrameworks(): FrameworkMarkup {
-      return Object.keys(this.frameworks).reduce((prev, key) => {
-        prev[key as Framework] = this.allFrameworks[key as Framework];
-        return prev;
-      }, {} as FrameworkMarkup);
-    }
-
-    public get activeTabIndex(): number {
-      return Object.keys(this.usedFrameworks).indexOf(this.framework);
-    }
+    @Prop({ default: {} }) public frameworks!: FrameworkMarkup;
 
     public get framework(): Framework {
       return this.$store.getters.selectedFramework;
     }
 
-    public setFramework(framework: Framework): void {
-      this.$store.commit('setSelectedFramework', framework);
+    get markup(): string {
+      return this.frameworks[this.framework]!;
     }
 
-    get highlightedMarkup(): string {
-      const markup = this.frameworks[this.framework]!;
-      return getHighlightedCode(markup, this.framework);
+    public get isSlotSet(): boolean {
+      return !!this.$scopedSlots.default;
     }
   }
 </script>
@@ -56,53 +39,19 @@
 <style scoped lang="scss">
   @import '~@porsche-design-system/utilities/scss';
   @import '../styles/internal.variables';
-  @import '../styles/code-highlighting';
 
-  .code-block {
-    margin-top: 1rem;
-
-    @include codeHighlighting('light');
-  }
-
-  code,
-  pre {
-    background: transparent;
-    font-family: Monaco, Menlo, 'Andale Mono', 'Ubuntu Mono', monospace;
-    font-size: 0.875rem;
-    line-height: 1.5;
-    tab-size: 2;
-    text-align: left;
-    white-space: pre;
-    word-spacing: normal;
-    word-break: normal;
-    word-wrap: normal;
-    hyphens: none;
-  }
-
-  pre {
-    max-height: 50rem;
-    overflow: auto;
+  .example {
     padding: $p-spacing-32;
     border: 1px solid $p-color-neutral-contrast-low;
-    margin: 0 0 $p-spacing-16;
+    background: $p-color-background-default;
+    margin-top: 1rem;
+  }
 
-    code ::v-deep {
-      .namespace {
-        opacity: 0.7;
-      }
+  ::v-deep pre {
+    max-height: 50rem;
+  }
 
-      .token.important,
-      .token.bold {
-        font-weight: bold;
-      }
-
-      .token.italic {
-        font-style: italic;
-      }
-
-      .token.entity {
-        cursor: help;
-      }
-    }
+  .demo ~ .code-block {
+    margin-top: $p-spacing-32;
   }
 </style>
