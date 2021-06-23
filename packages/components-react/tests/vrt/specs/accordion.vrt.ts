f32770f00093c395210e38cb2bdf2a3d472a92a8
Matthias Haegele <matthias.haegele@mhp.com>
2021-06-23 10:52:11 +0200

    test(components-react/angular): remove goto | mh / as | #1333

--- packages/components-react/tests/vrt/specs/accordion.vrt.ts
+++ packages/components-react/tests/vrt/specs/accordion.vrt.ts
@@ -7,7 +7,6 @@ describe('Accordion', () => {
       await vrt.test(
         'accordion',
         async () => {
-          await vrt.goTo('/text'); // to ensure fonts are already loaded before js is initialized
           await vrt.goTo('/accordion');
         },
         testOptions
