31683f1ffaed28a72ad4f4220b86bd06ba3925f3
Matthias Haegele <matthias.haegele@mhp.com>
2021-06-23 11:48:40 +0200

    test(components-js): add state tests for slotted tags | mh | #1333

--- packages/components-js/tests/vrt/specs/accordion.vrt.ts
+++ packages/components-js/tests/vrt/specs/accordion.vrt.ts
@@ -1,4 +1,5 @@
 import {
+  forceFocusedHoveredState,
   forceFocusedState,
   forceHoveredState,
   getThemedBodyMarkup,
@@ -30,19 +31,27 @@ describe('Accordion', () => {
         const page = await vrt.getPage();
         const hoverElementSelector = 'p-accordion >>> p-headline';
         const focusElementSelector = 'p-accordion >>> button';
+        const headingSlotSelector = 'p-accordion > span a';
+        const contentSlotSelector = 'p-accordion > div a';
 
         const getElementsMarkup: GetThemedMarkup = (theme) => `
           <p-accordion theme="${theme}" heading="Some heading">
-            <p>
              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
              sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
           </p-accordion>
           <p-accordion theme="${theme}" heading="Some heading" open="true">
-            <p ${theme === 'dark' && 'style="color: white"'}>
+            <div ${theme === 'dark' && 'style="color: white"'}>
              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
              sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            </p>
+            </div>
+          </p-accordion>
+          <p-accordion theme="${theme}" open="true">
+            <span slot="heading">Some <i>italic</i> and <em>emphasis</em> heading with <a href="#">Link</a></span>
+            <div ${theme === 'dark' && 'style="color: white"'}>
+             Some <i>italic</i> and <em>emphasis</em> content with <a href="#">Link</a>.
+             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+             sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </div>
           </p-accordion>`;
 
         await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup));
@@ -50,8 +59,17 @@ describe('Accordion', () => {
         await forceHoveredState(page, `.hovered > ${hoverElementSelector}`);
         await forceFocusedState(page, `.focused > ${focusElementSelector}`);
 
+        await forceHoveredState(page, `.hovered > ${headingSlotSelector}`);
+        await forceFocusedState(page, `.focused > ${headingSlotSelector}`);
+
+        await forceHoveredState(page, `.hovered > ${contentSlotSelector}`);
+        await forceFocusedState(page, `.focused > ${contentSlotSelector}`);
+
         await forceHoveredState(page, `.focused-hovered > ${hoverElementSelector}`);
         await forceFocusedState(page, `.focused-hovered > ${focusElementSelector}`);
+
+        await forceFocusedHoveredState(page, `.focused-hovered > ${headingSlotSelector}`);
+        await forceFocusedHoveredState(page, `.focused-hovered > ${contentSlotSelector}`);
       })
     ).toBeFalsy();
   });
