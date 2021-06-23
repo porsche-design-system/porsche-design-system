b46bd901712052895189e49685dd2c2bf7ea4132
Bernd Hacker <hacker.bernd@gmail.com>
2021-06-23 12:40:52 +0200

    fix(components): reexport Breakpoint type from styles utils | bh | #1289

--- packages/components/src/utils/styles.ts
+++ packages/components/src/utils/styles.ts
@@ -45,5 +45,5 @@ export const focus = (opts?: Options): JssStyle => {
   };
 };
 
-export { breakpoint } from '@porsche-design-system/utilities';
+export { Breakpoint, breakpoint } from '@porsche-design-system/utilities';
 export const mediaQuery = (minBreakpoint: Breakpoint): string => `@media (min-width: ${breakpoint[minBreakpoint]}px)`;
