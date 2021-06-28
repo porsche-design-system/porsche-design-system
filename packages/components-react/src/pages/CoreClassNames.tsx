f0e24db743d555e7a0ac862b1b736c5c420b9650
Bernd Hacker <hacker.bernd@gmail.com>
2021-06-28 11:15:53 +0200

    refactor(components-react): use string interpolation instead of concatination | bh / sas | #1289

--- packages/components-react/src/pages/CoreClassNames.tsx
+++ packages/components-react/src/pages/CoreClassNames.tsx
@@ -7,7 +7,7 @@ const SOME_CLASS_2 = 'someClass2';
 const Button = (): JSX.Element => {
   const [counter, setCounter] = useState(0);
   const props: PButtonProps = {
-    className: counter % 2 === 0 ? SOME_CLASS_1 + ' ' + SOME_CLASS_2 : SOME_CLASS_1,
+    className: counter % 2 === 0 ? `${SOME_CLASS_1} ${SOME_CLASS_2}` : SOME_CLASS_1,
     onClick: () => {
       setCounter((prevState) => prevState + 1);
     },
