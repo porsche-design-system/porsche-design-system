e93e35e59841f9a89fdad3747ddead7a6844ebab
Anton Schmidt <anton.schmidt@mhp.com>
2021-06-29 08:58:34 +0200

    fix(components-react): move import to top | as | #1333

--- packages/components-react/tests/jsdom-polyfill/config/setup.ts
+++ packages/components-react/tests/jsdom-polyfill/config/setup.ts
@@ -1,9 +1,7 @@
 import '@testing-library/jest-dom';
 import '@porsche-design-system/components-react/jsdom-polyfill';
 import { skipCheckForPorscheDesignSystemProviderDuringTests } from '@porsche-design-system/components-react';
-
-skipCheckForPorscheDesignSystemProviderDuringTests();
-
 import { ResizeObserver } from '@juggle/resize-observer';
 
+skipCheckForPorscheDesignSystemProviderDuringTests();
 global.ResizeObserver = ResizeObserver;
