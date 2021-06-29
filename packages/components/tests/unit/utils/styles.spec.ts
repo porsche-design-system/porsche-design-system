216914ccc70082bdf3c37ecc9d2301e3ce605e75
Bernd Hacker <hacker.bernd@gmail.com>
2021-06-29 14:56:52 +0200

    test(components): remove duplicate tests for pxToRem | bh | #1289

--- packages/components/tests/unit/utils/styles.spec.ts
+++ packages/components/tests/unit/utils/styles.spec.ts
@@ -11,9 +11,12 @@ import {
 describe('pxToRem()', () => {
   it.each([
     [0, 0],
+    [undefined, NaN],
+    [null, 0],
     [16, 1],
     [24, 1.5],
     [32, 2],
+    [36, 2.25],
   ])('should for parameter %s return %s', (input, result) => {
     expect(pxToRem(input)).toBe(result);
   });
