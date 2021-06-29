4c57166deeb737dcb7ea8badca0cb9d31115f98a
Bernd Hacker <hacker.bernd@gmail.com>
2021-06-29 08:23:22 +0200

    test(components-react): extend flex, grid and table polyfill tests | bh | #1289

--- packages/components-react/tests/jsdom-polyfill/specs/grid.spec.tsx
+++ packages/components-react/tests/jsdom-polyfill/specs/grid.spec.tsx
@@ -1,8 +1,13 @@
-import { componentsReady, PGrid } from '@porsche-design-system/components-react';
+import { componentsReady, PGrid, PGridItem } from '@porsche-design-system/components-react';
 import { render } from '@testing-library/react';
 
 const Sample = (): JSX.Element => {
-  return <PGrid data-testid="host" />;
+  return (
+    <PGrid data-testid="host">
+      <PGridItem size={6} data-testid="child1" />
+      <PGridItem size={6} data-testid="child2" />
+    </PGrid>
+  );
 };
 
 describe('PGrid', () => {
@@ -11,5 +16,7 @@ describe('PGrid', () => {
     await componentsReady();
 
     expect(getByTestId('host').shadowRoot).not.toBeNull();
+    expect(getByTestId('child1').shadowRoot).not.toBeNull();
+    expect(getByTestId('child2').shadowRoot).not.toBeNull();
   });
 });
