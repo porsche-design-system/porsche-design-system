76a5886dca9f0c3374eb4a276c68503301b07ba4
Anton Schmidt <anton.schmidt@mhp.com>
2021-07-01 10:05:19 +0200

    test(components): add insertSlottedStyles() unit test | as / mh  | #1333

--- packages/components/tests/unit/components/accordion.spec.ts
+++ packages/components/tests/unit/components/accordion.spec.ts
@@ -1,10 +1,23 @@
 import * as resizeObserverUtils from '../../../src/utils/resize-observer';
 import { Accordion } from '../../../src/components/content/accordion/accordion';
+import * as slottedStylesUtils from '../../../src/utils/slotted-styles';
 
 jest.mock('../../../src/utils/dom');
 jest.mock('../../../src/utils/slotted-styles');
 
 describe('accordion', () => {
+  describe('connectedCallback', () => {
+    it('should call insertSlottedStyles()', () => {
+      const spy = jest.spyOn(slottedStylesUtils, 'insertSlottedStyles');
+      const component = new Accordion();
+      component.host = document.createElement('p-accordion');
+      component.host.attachShadow({ mode: 'open' });
+      component.connectedCallback();
+
+      expect(spy).toBeCalledWith(component.host, expect.anything());
+    });
+  });
+
   describe('componentDidLoad', () => {
     it('should call observeResize()', () => {
       const spy = jest.spyOn(resizeObserverUtils, 'observeResize');
