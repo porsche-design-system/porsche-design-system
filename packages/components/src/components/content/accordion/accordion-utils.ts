a9f382ef9d31a2fdd774bf86b3f7bdac0c424d89
Anton Schmidt <anton.schmidt@mhp.com>
2021-07-02 10:33:51 +0200

    feat(components): use JssStyle reexport from utils, use addIMportantToEachRule helper | as / mh | #1333

--- packages/components/src/components/content/accordion/accordion-utils.ts
+++ packages/components/src/components/content/accordion/accordion-utils.ts
@@ -1,34 +1,33 @@
-import { buildGlobalStyles, getCss, getTagName } from '../../../utils';
+import { addImportantToEachRule, buildGlobalStyles, getCss, getTagName, JssStyle } from '../../../utils';
 import { P_ANIMATION_HOVER_DURATION } from '../../../styles';
-import type { JssStyle } from 'jss';
 import { color } from '@porsche-design-system/utilities';
 
 const ACCORDION_SIZE = ['small', 'medium'] as const;
 export type AccordionSize = typeof ACCORDION_SIZE[number];
 export type AccordionChangeEvent = { open: boolean };
 
-const slottedStyles: JssStyle = {
+const slottedStyles: JssStyle = addImportantToEachRule({
   '& a': {
-    color: 'inherit !important',
-    textDecoration: 'underline !important',
-    transition: `color ${P_ANIMATION_HOVER_DURATION} ease !important`,
-    outline: 'transparent solid 1px !important',
-    outlineOffset: '1px !important',
+    color: 'inherit',
+    textDecoration: 'underline',
+    transition: `color ${P_ANIMATION_HOVER_DURATION} ease`,
+    outline: 'transparent solid 1px',
+    outlineOffset: '1px',
     '&:hover': {
-      color: `${color.state.hover} !important`,
+      color: `${color.state.hover}`,
     },
     '&:focus': {
-      outlineColor: 'currentColor !important',
+      outlineColor: 'currentColor',
     },
     '&:focus:not(:focus-visible)': {
-      outlineColor: 'transparent !important',
+      outlineColor: 'transparent',
     },
   },
 
   '& em, & i': {
-    fontStyle: 'normal !important',
+    fontStyle: 'normal',
   },
-};
+});
 
 export const getSlottedCss = (host: HTMLElement): string => {
   return getCss(
