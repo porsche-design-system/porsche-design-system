76793cade67cf25afa69cb915fd38c5392fd1dea
Anton Schmidt <anton.schmidt@mhp.com>
2021-07-02 10:28:12 +0200

    feat(components-react): set global resize observer before running jsdom-polyfill | as / mh  | #1333

--- packages/components-react/projects/jsdom-polyfill/src/index.js
+++ packages/components-react/projects/jsdom-polyfill/src/index.js
@@ -1,6 +1,5 @@
 import { ResizeObserver } from '@juggle/resize-observer';
+global.ResizeObserver = ResizeObserver;
 
 // workaround for WebComponentManager not working in JSDOM
 require('./lib/loader.cjs').defineCustomElements();
-
-global.ResizeObserver = ResizeObserver;
