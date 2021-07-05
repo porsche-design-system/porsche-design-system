545a41f312dae2c8b0be8be24e5f77ac72454e9e
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-05 09:12:11 +0200

    feat(components): change banner jss styles from function to const | bh | #1459

--- packages/components/src/components/feedback/banner/banner-utils.ts
+++ packages/components/src/components/feedback/banner/banner-utils.ts
@@ -2,38 +2,37 @@ import { attachCss, breakpoint, getCss, mediaQuery } from '../../../utils';
 
 const easeOutQuad = 'cubic-bezier(0.5, 1, 0.89, 1)';
 
-export const getMediaQueryStyles = (): string =>
-  getCss({
-    '@keyframes animateMobileOut': {
-      from: {
-        opacity: 1,
-        transform: 'translate3d(0, 0, 0)',
-      },
-      to: {
-        opacity: 0,
-        transform: 'translate3d(0, calc(var(--p-banner-position-bottom) + 100%), 0)',
-      },
+export const closeStyles = getCss({
+  '@keyframes animateMobileOut': {
+    from: {
+      opacity: 1,
+      transform: 'translate3d(0, 0, 0)',
     },
-    '@keyframes animateDesktopOut': {
-      from: {
-        opacity: 1,
-        transform: 'translate3d(0, 0, 0)',
-      },
-      to: {
-        opacity: 0,
-        transform: 'translate3d(0, calc(-100% - var(--p-banner-position-bottom)), 0)',
-      },
+    to: {
+      opacity: 0,
+      transform: 'translate3d(0, calc(var(--p-banner-position-bottom) + 100%), 0)',
     },
-    ':host(.banner--close)': {
-      [`${mediaQuery('xxs')} and (max-width: ${breakpoint.s}px)`]: {
-        animation: `600ms $animateMobileOut ${easeOutQuad} forwards !important`,
-      },
-      [mediaQuery('s')]: {
-        animation: `600ms $animateDesktopOut ${easeOutQuad} forwards !important`,
-      },
+  },
+  '@keyframes animateDesktopOut': {
+    from: {
+      opacity: 1,
+      transform: 'translate3d(0, 0, 0)',
     },
-  });
+    to: {
+      opacity: 0,
+      transform: 'translate3d(0, calc(-100% - var(--p-banner-position-bottom)), 0)',
+    },
+  },
+  ':host(.banner--close)': {
+    [`${mediaQuery('xxs')} and (max-width: ${breakpoint.s}px)`]: {
+      animation: `600ms $animateMobileOut ${easeOutQuad} forwards !important`,
+    },
+    [mediaQuery('s')]: {
+      animation: `600ms $animateDesktopOut ${easeOutQuad} forwards !important`,
+    },
+  },
+});
 
 export const addCss = (host: HTMLElement): void => {
-  attachCss(host, getMediaQueryStyles());
+  attachCss(host, closeStyles);
 };
