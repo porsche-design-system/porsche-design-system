547549a14b5f24a462ac73b205b268b4f94dd64c
Anton Schmidt <anton.schmidt@mhp.com>
2021-07-05 10:27:28 +0200

    test(components): add getContentWrapperHeight() unit test | as | #1333

--- packages/components/tests/unit/components/accordion-utils.spec.ts
+++ packages/components/tests/unit/components/accordion-utils.spec.ts
@@ -1,4 +1,7 @@
-import { getCollapsibleElementHeight } from '../../../src/components/content/accordion/accordion-utils';
+import {
+  getCollapsibleElementHeight,
+  getContentWrapperHeight,
+} from '../../../src/components/content/accordion/accordion-utils';
 
 describe('getCollapsibleElementHeight()', () => {
   it('should return "200px" if isOpen = true', () => {
@@ -8,4 +11,26 @@ describe('getCollapsibleElementHeight()', () => {
   it('should return "0" if isOpen = false', () => {
     expect(getCollapsibleElementHeight(false, '200px')).toBe('0');
   });
+});
+
+describe('getContentWrapperHeight()', () => {
+  it.each([
+    [true, undefined, { height: 20 }, '2.25rem'],
+    [false, undefined, { height: 20 }, '3.75rem'],
+    [true, { blockSize: 300 }, { height: 20 }, '18.75rem'],
+    [true, [{ blockSize: 300 }], { height: 20 }, '18.75rem'],
+    [false, { blockSize: 300 }, { height: 20 }, '18.75rem'],
+    [false, [{ blockSize: 300 }], { height: 20 }, '18.75rem'],
+  ])(
+    'should for compact = %s border boxSize = %o and contentRect = %o return %s',
+    (compact: boolean, borderBoxSize: any, contentRect: DOMRectReadOnly, expected: string) => {
+      expect(
+        getContentWrapperHeight(
+          compact,
+          borderBoxSize as ResizeObserverSize | readonly ResizeObserverSize[],
+          contentRect
+        )
+      ).toBe(expected);
+    }
+  );
 });
