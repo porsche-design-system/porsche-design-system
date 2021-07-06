2db60d2f77dcc9f25619b396cbedda9f4171d801
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-06 07:00:25 +0200

    refactor(components): render pagination links directly via array.map, rewrite readCounterResetValue util | bh | #1482

--- packages/components/src/components/navigation/pagination/pagination.tsx
+++ packages/components/src/components/navigation/pagination/pagination.tsx
@@ -5,9 +5,14 @@ import {
   isDark,
   mapBreakpointPropToClasses,
 } from '../../../utils';
-import { createPaginationModel, getCurrentActivePage, getTotalPages, itemTypes } from './pagination-utils';
+import {
+  createPaginationModel,
+  getCounterResetValue,
+  getCurrentActivePage,
+  getTotalPages,
+  itemTypes,
+} from './pagination-utils';
 import { listenResize } from '../../../utils/window-resize-listener';
-import { readCounterResetValue } from '../../../utils/counter-reset-reader';
 import type { BreakpointCustomizable, NumberOfPageLinks, PageChangeEvent, Theme } from '../../../types';
 
 @Component({
@@ -74,8 +79,6 @@ export class Pagination {
   }
 
   public render(): JSX.Element {
-    const pageRange = this.breakpointMaxNumberOfPageLinks === 7 ? 1 : 0;
-
     const rootClasses = {
       ['root']: true,
       ['root--theme-dark']: isDark(this.theme),
@@ -83,97 +86,14 @@ export class Pagination {
     };
 
     const pageTotal = getTotalPages(this.totalItemsCount, this.itemsPerPage);
-    const activePage = getCurrentActivePage(this.activePage, pageTotal);
 
-    // generate pagination items
     const paginationModel = createPaginationModel({
-      activePage,
+      activePage: getCurrentActivePage(this.activePage, pageTotal),
       pageTotal,
-      pageRange,
+      pageRange: this.breakpointMaxNumberOfPageLinks === 7 ? 1 : 0,
     });
-    const pageItems: JSX.Element[] = [];
-    let prevItem: JSX.Element;
-    let nextItem: JSX.Element;
 
     const PrefixedTagNames = getPrefixedTagNames(this.host);
-
-    paginationModel.forEach((pageModel) => {
-      const { type, isActive, value } = pageModel;
-      if (type === itemTypes.PREVIOUS_PAGE_LINK) {
-        const paginationPrevClasses = {
-          ['prev']: true,
-          ['prev--disabled']: !isActive,
-        };
-
-        prevItem = (
-          <li {...pageModel} class="item">
-            <span
-              class={paginationPrevClasses}
-              role="button"
-              tabIndex={isActive ? 0 : null}
-              onClick={() => this.onClick(value)}
-              onKeyDown={(e) => this.onKeyDown(e, value)}
-              aria-disabled={!isActive ? 'true' : null}
-              aria-label={this.allyLabelPrev}
-            >
-              <PrefixedTagNames.pIcon name="arrow-head-left" color="inherit" />
-            </span>
-          </li>
-        );
-      } else if (type === itemTypes.ELLIPSIS) {
-        const paginationGoToClasses = {
-          ['goto']: true,
-          ['goto--ellipsis']: true,
-        };
-        pageItems.push(
-          <li {...pageModel} class="item">
-            <span class={paginationGoToClasses} />
-          </li>
-        );
-      } else if (type === itemTypes.PAGE) {
-        const paginationGoToClasses = {
-          ['goto']: true,
-          ['goto--current']: isActive,
-        };
-        pageItems.push(
-          <li {...pageModel} class="item">
-            <span
-              class={paginationGoToClasses}
-              role="button"
-              tabIndex={isActive ? null : 0}
-              aria-disabled={isActive ? 'true' : null}
-              onClick={() => this.onClick(value)}
-              onKeyDown={(e) => this.onKeyDown(e, value)}
-              aria-label={`${this.allyLabelPage} ${value}`}
-              aria-current={isActive ? 'page' : null}
-            >
-              {value}
-            </span>
-          </li>
-        );
-      } else if (type === itemTypes.NEXT_PAGE_LINK) {
-        const paginationNextClasses = {
-          ['next']: true,
-          ['next--disabled']: !isActive,
-        };
-
-        nextItem = (
-          <li {...pageModel} class="item">
-            <span
-              class={paginationNextClasses}
-              role="button"
-              tabIndex={isActive ? 0 : null}
-              onClick={() => this.onClick(value)}
-              onKeyDown={(e) => this.onKeyDown(e, value)}
-              aria-disabled={!isActive ? 'true' : null}
-              aria-label={this.allyLabelNext}
-            >
-              <PrefixedTagNames.pIcon name="arrow-head-right" color="inherit" />
-            </span>
-          </li>
-        );
-      }
-    });
 
     return (
       <nav
@@ -183,9 +103,79 @@ export class Pagination {
         ref={(el) => (this.navigationElement = el)}
       >
         <ul class="items">
-          {prevItem}
-          {pageItems}
-          {nextItem}
+          {paginationModel.map((pageModel) => {
+            const { type, isActive, value } = pageModel;
+            const spanProps = {
+              role: 'button',
+              tabIndex: isActive ? 0 : null,
+              onClick: () => this.onClick(value),
+              onKeyDown: (e) => this.onKeyDown(e, value),
+              ariaDisabled: !isActive ? 'true' : null,
+            };
+
+            switch (type) {
+              case itemTypes.PREVIOUS_PAGE_LINK:
+                const paginationPrevClasses = {
+                  ['prev']: true,
+                  ['prev--disabled']: !isActive,
+                };
+
+                return (
+                  <li class="item">
+                    <span class={paginationPrevClasses} {...spanProps} aria-label={this.allyLabelPrev}>
+                      <PrefixedTagNames.pIcon name="arrow-head-left" color="inherit" />
+                    </span>
+                  </li>
+                );
+
+              case itemTypes.ELLIPSIS:
+                const paginationEllipsisClasses = {
+                  ['goto']: true,
+                  ['goto--ellipsis']: true,
+                };
+
+                return (
+                  <li class="item">
+                    <span class={paginationEllipsisClasses} />
+                  </li>
+                );
+
+              case itemTypes.PAGE:
+                const paginationGoToClasses = {
+                  ['goto']: true,
+                  ['goto--current']: isActive,
+                };
+
+                return (
+                  <li class="item">
+                    <span
+                      class={paginationGoToClasses}
+                      {...spanProps}
+                      tabIndex={isActive ? null : 0}
+                      aria-disabled={isActive ? 'true' : null}
+                      aria-label={`${this.allyLabelPage} ${value}`}
+                      aria-current={isActive ? 'page' : null}
+                    >
+                      {value}
+                    </span>
+                  </li>
+                );
+
+              case itemTypes.NEXT_PAGE_LINK:
+                const paginationNextClasses = {
+                  ['next']: true,
+                  ['next--disabled']: !isActive,
+                };
+
+                return (
+                  <li class="item">
+                    <span class={paginationNextClasses} {...spanProps} aria-label={this.allyLabelNext}>
+                      <PrefixedTagNames.pIcon name="arrow-head-right" color="inherit" />
+                    </span>
+                  </li>
+                );
+            }
+          })}
         </ul>
       </nav>
     );
@@ -215,7 +205,6 @@ export class Pagination {
   }
 
   private updateMaxNumberOfPageLinks(): void {
-    const { size } = readCounterResetValue(this.navigationElement);
-    this.breakpointMaxNumberOfPageLinks = size;
+    this.breakpointMaxNumberOfPageLinks = getCounterResetValue(this.navigationElement);
   }
 }
