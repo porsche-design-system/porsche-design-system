84b08c180e4cd4ae2fd45ba4ce66b69f594fc2ee
Anton Schmidt <anton.schmidt@mhp.com>
2021-07-07 09:43:30 +0200

    test(components-js/angular/react): extend vrtests with compact property and navigation like link structure| as / mh | #1333

--- packages/components-react/src/pages/Accordion.tsx
+++ packages/components-react/src/pages/Accordion.tsx
@@ -1,4 +1,4 @@
-import { PAccordion } from '@porsche-design-system/components-react';
+import { PAccordion, PLinkPure } from '@porsche-design-system/components-react';
 
 export const AccordionPage = (): JSX.Element => {
   const content =
@@ -99,6 +99,7 @@ export const AccordionPage = (): JSX.Element => {
         <PAccordion heading="Some heading" open>
           {content}
         </PAccordion>
+        <PAccordion heading="Some heading">{content}</PAccordion>
       </div>
 
       <div className="playground dark" title="should render multiple accordions with one open on dark background">
@@ -107,6 +108,99 @@ export const AccordionPage = (): JSX.Element => {
         </PAccordion>
         <PAccordion heading="Some heading" theme="dark" open>
           <div style={{ color: 'white' }}>{content}</div>
+        </PAccordion>
+        <PAccordion heading="Some heading" theme="dark">
+          {content}
+        </PAccordion>
+      </div>
+
+      <div
+        className="playground light"
+        title="should render multiple compact accordions with one open on light background"
+      >
+        <PAccordion heading="Some compact Accordion heading" compact={true}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </PAccordion>
+        <PAccordion heading="Some compact Accordion heading" compact={true} open={true}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </PAccordion>
+        <PAccordion heading="Some compact Accordion heading" compact={true}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </PAccordion>
+      </div>
+
+      <div
+        className="playground dark"
+        title="should render multiple compact accordions with one open on dark background"
+      >
+        <PAccordion heading="Some compact Accordion heading" compact={true} theme="dark">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </PAccordion>
+        <PAccordion heading="Some compact Accordion heading" compact={true} open={true} theme="dark">
+          <div style={{ color: 'white' }}>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+            dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+          </div>
+        </PAccordion>
+        <PAccordion heading="Some compact Accordion heading" compact={true} theme="dark">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </PAccordion>
+      </div>
+
+      <div className="playground light" title="should render navigation like accordion on light background">
+        <PAccordion heading="Some Category" compact={true}>
+          <div>
+            <PLinkPure href="https://www.porsche.com">Some link</PLinkPure>
+          </div>
+          <div>
+            <PLinkPure href="https://www.porsche.com">Some link</PLinkPure>
+          </div>
+        </PAccordion>
+        <PAccordion heading="Some Category" compact={true} open={true}>
+          <div>
+            <PLinkPure href="https://www.porsche.com">Some link</PLinkPure>
+          </div>
+          <div>
+            <PLinkPure href="https://www.porsche.com">Some link</PLinkPure>
+          </div>
+        </PAccordion>
+      </div>
+
+      <div className="playground light" title="should render navigation like accordion on dark background">
+        <PAccordion heading="Some Category" compact={true} theme="dark">
+          <div>
+            <PLinkPure href="https://www.porsche.com" theme="dark">
+              Some link
+            </PLinkPure>
+          </div>
+          <div>
+            <PLinkPure href="https://www.porsche.com" theme="dark">
+              Some link
+            </PLinkPure>
+          </div>
+        </PAccordion>
+        <PAccordion heading="Some Category" compact={true} open={true} theme="dark">
+          <div>
+            <PLinkPure href="https://www.porsche.com" theme="dark">
+              Some link
+            </PLinkPure>
+          </div>
+          <div>
+            <PLinkPure href="https://www.porsche.com" theme="dark">
+              Some link
+            </PLinkPure>
+          </div>
+        </PAccordion>
+      </div>
+
+      <div className="playground light" title="should ignore size prop and render compact accordion">
+        <PAccordion heading="Some compact Accordion with ignored size prop" compact={true} size="medium">
+          {content}
         </PAccordion>
       </div>
     </>
