95ab213a2326f8a7a5b6f7ac28f5527d7b4830b9
Anton Schmidt <anton.schmidt@mhp.com>
2021-07-07 15:53:24 +0200

    test(comonents-angular/react): add accordion-example to test array | as / mh | #1333

--- packages/components-angular/tests/e2e/specs/examples.e2e.ts
+++ packages/components-angular/tests/e2e/specs/examples.e2e.ts
@@ -11,17 +11,12 @@ describe('examples', () => {
   });
   afterEach(async () => await page.close());
 
-  describe('accordion', () => {
-    it('should work without errors', async () => {
-      await goto(page, 'accordion-example');
-      expect(getConsoleErrorsAmount()).toBe(0);
-
-      await page.evaluate(() => console.error('test error'));
-      expect(getConsoleErrorsAmount()).toBe(1);
-    });
-  });
-
-  for (const example of ['table-example-basic', 'table-example-sorting', 'table-example-advanced']) {
+  for (const example of [
+    'accordion-example',
+    'table-example-basic',
+    'table-example-sorting',
+    'table-example-advanced',
+  ]) {
     it(`should work without error for ${example}`, async () => {
       await goto(page, example);
       expect(getConsoleErrorsAmount()).toBe(0);
