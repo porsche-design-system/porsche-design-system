cd1e56da3bbca267f5b661dc981160fcf9694947
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-08 12:01:44 +0200

    test(components-js): try to fix flaky text-field-wrapper e2e test | bh | #1466

--- packages/components-js/tests/e2e/specs/text-field-wrapper.e2e.ts
+++ packages/components-js/tests/e2e/specs/text-field-wrapper.e2e.ts
@@ -385,6 +385,7 @@ describe('text-field-wrapper', () => {
       await searchButton.click();
       await waitForEventSerialization(page);
       await waitForEventSerialization(page); // 🙈
+      await waitForEventSerialization(page); // 🙈
 
       expect(formFocusCalls).toBe(1);
     });
