c8dc9e025b9910378b7a794b922430d5d4e76809
Stephan Schröter <stephan.schroeter@mhp.com>
2021-07-12 16:50:12 +0200

    refactor(components): split jss styles from utils for modal | sas | #1496

--- packages/components/src/components/basic/marque/marque-styles.spec.ts
+++ packages/components/src/components/basic/marque/marque-styles.spec.ts
@@ -2,10 +2,7 @@ import { getComponentCss } from './marque-styles';
 import type { MarqueSize } from './marque-utils';
 
 describe('getComponentCss()', () => {
-  it.each([['responsive'], ['small'], ['medium']])(
-    'should return correct css for multiline: %s',
-    (size: MarqueSize) => {
-      expect(getComponentCss(size)).toMatchSnapshot();
-    }
-  );
+  it.each([['responsive'], ['small'], ['medium']])('should return correct css for size: %s', (size: MarqueSize) => {
+    expect(getComponentCss(size)).toMatchSnapshot();
+  });
 });
