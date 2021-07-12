be97e64f58fef16214b4a1ffe7b1695ce8e039ff
Stephan Schröter <stephan.schroeter@mhp.com>
2021-07-12 16:21:27 +0200

    refactor(components): use type import | sas | #1496

--- packages/components/src/components/layout/table/table-head-cell/table-head-cell-utils.ts
+++ packages/components/src/components/layout/table/table-head-cell/table-head-cell-utils.ts
@@ -1,4 +1,4 @@
-import { AriaAttributes } from 'react';
+import type { AriaAttributes } from 'react';
 import type { Direction, SortingChangeEvent, TableHeadCellSort } from '../table/table-utils';
 
 export const isDirectionAsc = (dir: Direction): boolean => dir === 'asc';
