af571261c031b46d91b15c35b12400ac5342c59e
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-13 11:33:51 +0200

    build(components-js): extend replaceCdnBaseUrlDynamicPlaceholder script, make cm.config.js more conditional | bh | #1331

--- packages/components-js/projects/components-wrapper/environment.ts
+++ packages/components-js/projects/components-wrapper/environment.ts
@@ -3,16 +3,15 @@ import { CDN_BASE_PATH_COMPONENTS } from '../../../../cdn.config';
 import { version } from './package.json';
 
 const isDev = process.env.PORSCHE_DESIGN_SYSTEM_DEV === '1';
-console.log('Environment:', isDev ? 'dev' : 'prod');
 
 export const deployUrl = isDev
   ? 'http://localhost:3001/components'
   : `%%%CDN_BASE_URL_DYNAMIC%%%/${CDN_BASE_PATH_COMPONENTS}`; // placeholder is replaced via replace.ts script
 
-export const cdnDistPath = path.resolve(__dirname, './dist/components');
-export const npmDistPath = path.resolve(__dirname, './dist/components-wrapper');
+export const cdnDistPath = path.resolve('./dist/components');
+export const npmDistPath = path.resolve('./dist/components-wrapper');
 export const npmDistTmpSubPath = 'dist/components-tmp';
-export const npmDistTmpPath = path.resolve(__dirname, npmDistTmpSubPath);
+export const npmDistTmpPath = path.resolve('./' + npmDistTmpSubPath);
 
 export { version } from './package.json';
 export const snakeCaseVersion = version.replace(/\.|-/g, '_');
