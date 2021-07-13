0ecb0da0a1312203e578194cf1a65c322dbacc50
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-13 09:20:42 +0200

    feat(components-js/partials): use tmp build for getLoader partial | bh | #1331

--- packages/components-js/projects/partials/scripts/generateLoaderPartial.ts
+++ packages/components-js/projects/partials/scripts/generateLoaderPartial.ts
@@ -1,12 +1,18 @@
 import * as fs from 'fs';
+import * as path from 'path';
+import { npmDistTmpPath } from '../../components-wrapper/environment';
 
 export const generateLoaderPartial = (): string => {
   const types = `type LoaderOptions = {
   withoutTags?: boolean;
 }`;
 
-  const filePath = require.resolve('@porsche-design-system/components-js');
-  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const componentsJsFilePath = require.resolve('@porsche-design-system/components-js');
+  const packageDir = path.resolve(path.dirname(componentsJsFilePath), '../..');
+  const distTmpDir = npmDistTmpPath.replace(path.resolve(__dirname, '..') + '/', ''); // extract end of path
+  const tmpFilePath = path.resolve(packageDir, distTmpDir, 'index.js');
+
+  const fileContent = fs.readFileSync(tmpFilePath, 'utf8');
 
   const func = `export const getLoader = (opts?: LoaderOptions): string => {
   const options: LoaderOptions = {
