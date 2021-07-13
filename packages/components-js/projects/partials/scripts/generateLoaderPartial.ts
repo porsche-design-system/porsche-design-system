465d295a4455402e69c870e06f390273fe330f85
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-13 09:28:56 +0200

    refactor(componentst-js/partials): simplify path handling for build without componentsReady | bh | #1331

--- packages/components-js/projects/partials/scripts/generateLoaderPartial.ts
+++ packages/components-js/projects/partials/scripts/generateLoaderPartial.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { npmDistTmpPath } from '../../components-wrapper/environment';
+import { npmDistTmpSubPath } from '../../components-wrapper/environment';
 
 export const generateLoaderPartial = (): string => {
   const types = `type LoaderOptions = {
@@ -9,9 +9,7 @@ export const generateLoaderPartial = (): string => {
 
   const componentsJsFilePath = require.resolve('@porsche-design-system/components-js');
   const packageDir = path.resolve(path.dirname(componentsJsFilePath), '../..');
-  const distTmpDir = npmDistTmpPath.replace(path.resolve(__dirname, '..') + '/', ''); // extract end of path
-  const tmpFilePath = path.resolve(packageDir, distTmpDir, 'index.js');
-
+  const tmpFilePath = path.resolve(packageDir, npmDistTmpSubPath, 'index.js');
   const fileContent = fs.readFileSync(tmpFilePath, 'utf8');
 
   const func = `export const getLoader = (opts?: LoaderOptions): string => {
