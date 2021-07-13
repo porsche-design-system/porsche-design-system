a5fbd05d1b7f34dca6bb4df4d913f85bdfb8b260
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-13 09:29:14 +0200

    test(components-js/partials): adjust and extend getLoader unit test | bh | #1331

--- packages/components-js/projects/partials/tests/unit/specs/getLoader.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getLoader.spec.ts
@@ -1,17 +1,26 @@
 import { getLoader } from '../../../src';
 import * as fs from 'fs';
+import * as path from 'path';
+import { npmDistTmpSubPath } from '../../../../components-wrapper/environment';
 
 describe('getLoader()', () => {
-  const filePath = require.resolve('@porsche-design-system/components-js');
-  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const componentsJsFilePath = require.resolve('@porsche-design-system/components-js');
+  const packageDir = path.resolve(path.dirname(componentsJsFilePath), '../..');
+  const tmpFilePath = path.resolve(packageDir, npmDistTmpSubPath, 'index.js');
+  const fileContent = fs.readFileSync(tmpFilePath, 'utf8');
 
-  it('should return content of components-js build within script tag', () => {
+  it('should return content of components-js tmp build within script tag', () => {
     const result = getLoader();
     expect(result).toMatch(`<script>${fileContent}</script>`);
   });
 
-  it('should return content of components-js build without script tag', () => {
+  it('should return content of components-js tmp build without script tag', () => {
     const result = getLoader({ withoutTags: true });
     expect(result).toMatch(fileContent);
+  });
+
+  it('should not contain componentsReady', () => {
+    const result = getLoader();
+    expect(result).not.toContain('componentsReady');
   });
 });
