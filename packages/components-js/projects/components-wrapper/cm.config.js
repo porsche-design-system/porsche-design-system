b506442ed119b49d616ef6b0a56201d23c483a93
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-13 11:33:51 +0200

    build(components-js): extend replaceCdnBaseUrlDynamicPlaceholder script, make cm.config.js more conditional | bh | #1331

--- packages/components-js/projects/components-wrapper/cm.config.js
+++ packages/components-js/projects/components-wrapper/cm.config.js
@@ -1,9 +1,13 @@
 const { cdnDistPath, deployUrl, npmDistPath, npmDistTmpPath, version } = require('./environment');
 
+const isTpmBuild = process.env.TMP_BUILD === '1';
+const targetDirectory = isTpmBuild ? npmDistTmpPath : npmDistPath;
+
+const isDev = process.env.PORSCHE_DESIGN_SYSTEM_DEV === '1';
+console.log('Environment:', isDev ? 'dev' : 'prod');
 console.log('Version:', version);
 console.log('Deploy URL:', deployUrl);
-
-const isTpmBuild = process.env.TMP_BUILD === '1';
+console.log('targetDirectory:', deployUrl);
 
 // type PorscheWebComponentManagerConfig = {
 //   version: string;
@@ -17,29 +21,29 @@ const isTpmBuild = process.env.TMP_BUILD === '1';
 // }
 
 module.exports = {
-  version: version,
-  targetDirectory: isTpmBuild ? npmDistTmpPath : npmDistPath,
+  version,
+  targetDirectory,
   deployUrl: deployUrl,
   script: `${cdnDistPath}/porsche-design-system.v*.js`,
-  copyFiles: [
-    {
-      pattern: '../components/CHANGELOG.md',
-      targetDirectory: npmDistPath,
-    },
-    {
-      pattern: './projects/components-wrapper/package.json',
-      targetDirectory: npmDistPath,
-    },
-    {
-      pattern: './projects/components-wrapper/LICENSE',
-      targetDirectory: npmDistPath,
-    },
-    {
-      pattern: './projects/components-wrapper/README.md',
-      targetDirectory: npmDistPath,
-    },
-  ],
   ...(!isTpmBuild && {
+    copyFiles: [
+      {
+        pattern: '../components/CHANGELOG.md',
+        targetDirectory,
+      },
+      {
+        pattern: './projects/components-wrapper/package.json',
+        targetDirectory,
+      },
+      {
+        pattern: './projects/components-wrapper/LICENSE',
+        targetDirectory,
+      },
+      {
+        pattern: './projects/components-wrapper/README.md',
+        targetDirectory,
+      },
+    ],
     additionalEntryFiles: [
       {
         filePath: '../components/dist/collection/components-ready.js',
