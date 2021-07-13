1397e0de81922f3a43fe7b22fdaf01e9b1a62f91
Stephan Schröter <stephan.schroeter@mhp.com>
2021-07-13 12:35:46 +0200

    refactor(components): standardize component jss for modal | sas | #1496

--- packages/components/src/components/content/modal/modal-styles.ts
+++ packages/components/src/components/content/modal/modal-styles.ts
@@ -1,46 +1,38 @@
 import { addImportantToEachRule, attachCss, buildHostStyles, getCss } from '../../../utils';
 import { color } from '@porsche-design-system/utilities';
 
-const baseCss: string = getCss(
-  buildHostStyles({
-    ...addImportantToEachRule({
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      zIndex: '99999',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      flexWrap: 'wrap',
-      background: `${color.darkTheme.background.default}e6`, // e6 = 0.9 alpha
-      transition: 'opacity 0.2s $easing ',
-      opacity: 0,
-      visibility: 'hidden',
-    }),
-    overflowY: 'auto',
-  })
-);
-
 export const getComponentCss = (open: boolean): string => {
-  return (
-    baseCss +
-    (open
-      ? getCss({
-          ...buildHostStyles(
-            addImportantToEachRule({
-              transition: 'opacity 0.6s cubic-bezier(0.16, 1, 0.3, 1), visibility 0s linear',
-              opacity: 1,
-              visibility: 'inherit',
-            })
-          ),
-          root: addImportantToEachRule({
-            transform: 'scale3d(1, 1, 1)',
-          }),
-        })
-      : '')
-  );
+  return getCss({
+    ...buildHostStyles({
+      ...addImportantToEachRule({
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: '99999',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        background: `${color.darkTheme.background.default}e6`, // e6 = 0.9 alpha
+        transition: 'opacity 0.2s $easing ',
+        opacity: 0,
+        visibility: 'hidden',
+        ...(open && {
+          transition: 'opacity 0.6s cubic-bezier(0.16, 1, 0.3, 1), visibility 0s linear',
+          opacity: 1,
+          visibility: 'inherit',
+        }),
+      }),
+      overflowY: 'auto',
+    }),
+    ...(open && {
+      root: addImportantToEachRule({
+        transform: 'scale3d(1, 1, 1)',
+      }),
+    }),
+  });
 };
 
 export const addComponentCss = (host: HTMLElement, open: boolean): void => {
