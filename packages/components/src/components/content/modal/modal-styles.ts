70457c50d6fb8c65014f47ac0af7706507fd731d
Stephan Schröter <stephan.schroeter@mhp.com>
2021-07-13 12:47:50 +0200

    fix(components): use correct transition timing function for modal | sas | #1496

--- packages/components/src/components/content/modal/modal-styles.ts
+++ packages/components/src/components/content/modal/modal-styles.ts
@@ -1,6 +1,8 @@
 import { addImportantToEachRule, attachCss, buildHostStyles, getCss } from '../../../utils';
 import { color } from '@porsche-design-system/utilities';
 
+const transitionTimingFunction = 'cubic-bezier(0.16, 1, 0.3, 1)';
+
 export const getComponentCss = (open: boolean): string => {
   return getCss({
     ...buildHostStyles({
@@ -16,11 +18,11 @@ export const getComponentCss = (open: boolean): string => {
         justifyContent: 'center',
         flexWrap: 'wrap',
         background: `${color.darkTheme.background.default}e6`, // e6 = 0.9 alpha
-        transition: 'opacity 0.2s $easing ',
+        transition: `opacity 0.2s ${transitionTimingFunction}`,
         opacity: 0,
         visibility: 'hidden',
         ...(open && {
-          transition: 'opacity 0.6s cubic-bezier(0.16, 1, 0.3, 1), visibility 0s linear',
+          transition: `opacity 0.6s ${transitionTimingFunction}, visibility 0s linear`,
           opacity: 1,
           visibility: 'inherit',
         }),
