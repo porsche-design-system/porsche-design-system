715585f39a2d8b32dd85e4d0fc84dbc010e4f215
Matthias Haegele <matthias.haegele@mhp.com>
2021-07-21 12:38:53 +0200

    test(components): add test for textFieldWrapper styles | mh | #1455

---
+++ packages/components/src/components/form/text-field-wrapper/text-field-wrapper-styles.spec.ts
@@ -0,0 +1,13 @@
+import { getSlottedTextFieldWrapperCss } from './text-field-wrapper-styles';
+
+describe('getSlottedCss()', () => {
+  it('should return correct css', () => {
+    const host = document.createElement('p-accordion');
+    expect(getSlottedTextFieldWrapperCss(host)).toMatchSnapshot();
+  });
+
+  it('should return correct css with prefix', () => {
+    const host = document.createElement('prefixed-p-accordion');
+    expect(getSlottedTextFieldWrapperCss(host)).toMatchSnapshot();
+  });
+});
