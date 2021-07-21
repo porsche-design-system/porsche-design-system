37ff17b2224e2281522ab725bcb97408336cc1c2
Matthias Haegele <matthias.haegele@mhp.com>
2021-07-21 16:27:58 +0200

    feat(components): remove obsolete reflact of theme | mh | #1455

--- packages/components/src/components/navigation/link-pure/link-pure.tsx
+++ packages/components/src/components/navigation/link-pure/link-pure.tsx
@@ -42,7 +42,7 @@ export class LinkPure {
   @Prop() public hideLabel?: BreakpointCustomizable<boolean> = false;
 
   /** Adapts the button color depending on the theme. */
-  @Prop({ reflect: true }) public theme?: Theme = 'light';
+  @Prop() public theme?: Theme = 'light';
 
   /** Target attribute where the link should be opened. */
   @Prop() public target?: LinkTarget = '_self';
