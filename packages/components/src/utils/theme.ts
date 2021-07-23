d4dfa540c16bf1b75b683d2966ff1faaa172901e
Matthias Haegele <matthias.haegele@mhp.com>
2021-07-23 12:32:20 +0200

    feat(components): create reflectThemeOnDark helper | mh | #1455

--- packages/components/src/utils/theme.ts
+++ packages/components/src/utils/theme.ts
@@ -3,3 +3,7 @@ import type { Theme } from '../types';
 export const isDark = (theme: Theme): boolean => {
   return theme === 'dark';
 };
+
+export const reflectThemeOnDark = (theme: Theme): { theme: Theme } => {
+  return isDark(theme) && { theme: 'dark' };
+};
