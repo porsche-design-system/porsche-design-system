40d0c83b3bac69925e70c67b2694072f2a511959
Matthias Haegele <matthias.haegele@mhp.com>
2021-07-23 12:32:31 +0200

    test(components): reflectThemeOnDark helper | mh | #1455

--- packages/components/src/utils/theme.spec.ts
+++ packages/components/src/utils/theme.spec.ts
@@ -1,4 +1,4 @@
-import { isDark } from '.';
+import { isDark, reflectThemeOnDark } from '.';
 
 describe('isDark()', () => {
   it('should return true for "dark"', () => {
@@ -11,5 +11,15 @@ describe('isDark()', () => {
 
   it('should return false for everything else', () => {
     expect(isDark('unknown' as any)).toBe(false);
+  });
+});
+
+describe('reflectThemeOnDark()', () => {
+  it('should return {theme: "dark"} for theme dark', () => {
+    console.log(reflectThemeOnDark('dark'));
+    expect(reflectThemeOnDark('dark')).toEqual({ theme: 'dark' });
+  });
+  it('should return {theme: "dark"} for theme dark', () => {
+    expect(reflectThemeOnDark('light')).toBe(false);
   });
 });
