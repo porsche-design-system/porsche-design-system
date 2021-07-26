4563cccba2b86c11fff2bf380f0af9b6f02fc646
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-26 17:27:54 +0200

    feat(components): extract getFocusPseudoStyles util | bh | #1455

--- packages/components/src/components/navigation/link-pure/link-pure-styles.ts
+++ packages/components/src/components/navigation/link-pure/link-pure-styles.ts
@@ -1,26 +1,8 @@
-import type { Styles } from 'jss';
-import { buildSlottedStyles, getCss, insertSlottedStyles } from '../../../utils';
+import type { Styles } from '../../../utils';
+import { buildSlottedStyles, getCss, getFocusPseudoStyles, insertSlottedStyles } from '../../../utils';
 
 export const getSlottedStyles = (): Styles => {
-  return {
-    '& a::before': {
-      content: '""',
-      position: 'absolute',
-      top: '0',
-      left: '0',
-      right: '0',
-      bottom: '0',
-      display: 'block',
-      outline: 'transparent solid 1px',
-      outlineOffset: '1px',
-    },
-    '& a:focus::before': {
-      outlineColor: 'currentColor',
-    },
-    '& a:focus:not(:focus-visible)::before': {
-      outlineColor: 'transparent',
-    },
-  };
+  return getFocusPseudoStyles({ offset: 1 });
 };
 
 export const getSlottedCss = (host: HTMLElement): string => {
--- packages/components/src/utils/styles.ts
+++ packages/components/src/utils/styles.ts
@@ -76,6 +76,45 @@ export const getFocusStyles = (opts?: GetFocusStylesOptions): JssStyle => {
   return style;
 };
 
+type GetFocusPseudoStylesOptions = {
+  color?: string;
+  offset?: number;
+};
+
+const defaultFocusPseudoStylesOptions: GetFocusPseudoStylesOptions = {
+  color: 'currentColor',
+  offset: 2,
+};
+
+/**
+ * this hack is only needed for Safari which does not support pseudo elements in slotted context (https://bugs.webkit.org/show_bug.cgi?id=178237) :-(
+ */
+export const getFocusPseudoStyles = (opts?: GetFocusPseudoStylesOptions): Styles => {
+  const options: GetFocusPseudoStylesOptions = { ...defaultFocusPseudoStylesOptions, ...opts };
+
+  const style: Styles = {
+    '& a::before': {
+      content: '""',
+      display: 'block',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      outline: '1px solid transparent',
+      outlineOffset: `${options.offset}px`,
+    },
+    '& a:focus::before': {
+      outlineColor: options.color,
+    },
+    '& a:focus:not(:focus-visible)::before': {
+      outlineColor: 'transparent',
+    },
+  };
+
+  return style;
+};
+
 export { Breakpoint, breakpoint } from '@porsche-design-system/utilities';
 export const mediaQuery = (minBreakpoint: Breakpoint): string => `@media (min-width: ${breakpoint[minBreakpoint]}px)`;
 
