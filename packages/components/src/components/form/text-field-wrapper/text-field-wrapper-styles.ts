e31834d6a277eb1708d9851316e4b17166860f8a
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-26 17:32:06 +0200

    fix(components): bad slotted styles for text-field-wrapper | bh | #1455

--- packages/components/src/components/form/text-field-wrapper/text-field-wrapper-styles.ts
+++ packages/components/src/components/form/text-field-wrapper/text-field-wrapper-styles.ts
@@ -3,15 +3,16 @@ import { buildSlottedStyles, getBaseSlottedStyles, getCss, insertSlottedStyles, 
 export const getSlottedStyles = (): Styles => {
   return {
     ...getBaseSlottedStyles(),
-    '& input&::-webkit-outer-spin-button, & input::-webkit-inner-spin-button, & input[type="search"]::-webkit-search-decoration':
+    '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button, & input[type="search"]::-webkit-search-decoration':
       {
         WebkitAppearance: 'none',
         appearance: 'none',
       },
-    '& input[type="text"]::-webkit-contacts-auto-fill-button, & input[type="text"]::-webkit-credentials-auto-fill-button':
-      {
+    '& input[type="text"]': {
+      '&::-webkit-contacts-auto-fill-button, &::-webkit-credentials-auto-fill-button': {
         marginRight: '2.4375rem',
       },
+    },
   };
 };
 
