7cc5d286c3df068400a4d5853343475b0ae6fc54
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-27 09:43:00 +0200

    refactor(components): remove useless getSlottedStyles util from text-field-wrapper-styles | bh | #1455

--- packages/components/src/components/form/text-field-wrapper/text-field-wrapper-styles.ts
+++ packages/components/src/components/form/text-field-wrapper/text-field-wrapper-styles.ts
@@ -1,23 +1,21 @@
 import { buildSlottedStyles, getBaseSlottedStyles, getCss, insertSlottedStyles, Styles } from '../../../utils';
 
-export const getSlottedStyles = (): Styles => {
-  return {
-    ...getBaseSlottedStyles(),
-    '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button, & input[type="search"]::-webkit-search-decoration':
-      {
-        WebkitAppearance: 'none',
-        appearance: 'none',
-      },
-    '& input[type="text"]': {
-      '&::-webkit-contacts-auto-fill-button, &::-webkit-credentials-auto-fill-button': {
-        marginRight: '2.4375rem',
-      },
-    },
-  };
-};
-
 export const getSlottedCss = (host: HTMLElement): string => {
-  return getCss(buildSlottedStyles(host, getSlottedStyles()));
+  return getCss(
+    buildSlottedStyles(host, {
+      ...getBaseSlottedStyles(),
+      '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button, & input[type="search"]::-webkit-search-decoration':
+        {
+          WebkitAppearance: 'none',
+          appearance: 'none',
+        },
+      '& input[type="text"]': {
+        '&::-webkit-contacts-auto-fill-button, &::-webkit-credentials-auto-fill-button': {
+          marginRight: '2.4375rem',
+        },
+      },
+    })
+  );
 };
 
 export const addSlottedCss = (host: HTMLElement): void => {
