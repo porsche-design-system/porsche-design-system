4fc2a6a460e51aa4102f59360a6fa6f9876dde97
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-27 14:45:40 +0200

    test(components): add unit tests for buildResponsiveStyles | bh | #1455

--- packages/components/src/utils/jss.spec.ts
+++ packages/components/src/utils/jss.spec.ts
@@ -3,6 +3,7 @@ import {
   buildGlobalStyles,
   buildHostStyles,
   buildResponsiveHostStyles,
+  buildResponsiveStyles,
   getCss,
   isObject,
   mergeDeep,
@@ -136,6 +137,25 @@ describe('buildResponsiveHostStyles()', () => {
       '@media (min-width: 1000px)': { ':host': { width: 500 } },
       '@media (min-width: 1300px)': { ':host': { width: 600 } },
       '@media (min-width: 1760px)': { ':host': { width: 700 } },
+    });
+  });
+});
+
+describe('buildResponsiveStyles()', () => {
+  const getStyles = (val: number): JssStyle => ({ root: { width: 100 * val } });
+
+  it('should return flat jss for simple type', () => {
+    expect(buildResponsiveStyles(6, getStyles)).toStrictEqual({ root: { width: 600 } });
+  });
+
+  it('should return nested jss for responsive type', () => {
+    expect(buildResponsiveStyles({ base: 6, xs: 3, s: 4, m: 5, l: 6, xl: 7 }, getStyles)).toStrictEqual({
+      root: { width: 600 },
+      '@media (min-width: 480px)': { root: { width: 300 } },
+      '@media (min-width: 760px)': { root: { width: 400 } },
+      '@media (min-width: 1000px)': { root: { width: 500 } },
+      '@media (min-width: 1300px)': { root: { width: 600 } },
+      '@media (min-width: 1760px)': { root: { width: 700 } },
     });
   });
 });
