40349648d70bf23c6bd03135f062d0a21d51f460
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-28 08:55:37 +0200

    test(components-js): use correct selector for link states vrt | bh | #1455

--- packages/components-js/tests/vrt/specs/link.vrt.ts
+++ packages/components-js/tests/vrt/specs/link.vrt.ts
@@ -49,11 +49,11 @@ describe('Link', () => {
         await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup), { injectIntoHead: head });
 
         await forceHoveredState(page, '.hovered > p-link >>> a');
-        await forceHoveredState(page, '.hovered > p-link > span');
+        await forceHoveredState(page, '.hovered > p-link > a');
         await forceFocusedState(page, '.focused > p-link >>> a');
-        await forceFocusedState(page, '.focused > p-link > span');
+        await forceFocusedState(page, '.focused > p-link > a');
         await forceFocusedHoveredState(page, '.focused-hovered > p-link >>> a');
-        await forceFocusedHoveredState(page, '.focused-hovered > p-link > span');
+        await forceFocusedHoveredState(page, '.focused-hovered > p-link > a');
       })
     ).toBeFalsy();
   });
