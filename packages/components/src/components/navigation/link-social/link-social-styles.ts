8bb298ced6f52306ab2c5c52bd0bbbd637f8fda3
Bernd Hacker <hacker.bernd@gmail.com>
2021-07-29 09:47:47 +0200

    feat(components): migrate link-social to jss | bh | #1455

--- packages/components/src/components/navigation/link-social/link-social-styles.ts
+++ packages/components/src/components/navigation/link-social/link-social-styles.ts
@@ -1,12 +1,119 @@
 import {
+  addImportantToEachRule,
+  addImportantToRule,
+  attachCss,
+  BreakpointCustomizable,
+  buildHostStyles,
+  buildResponsiveStyles,
   buildSlottedStyles,
   buildSlottedStylesForDarkTheme,
   getCss,
   getFocusPseudoStyles,
+  getFocusStyles,
+  GetStylesFunction,
   insertSlottedStyles,
+  isDark,
+  JssStyle,
   mergeDeep,
+  pxToRemWithUnit,
+  Styles,
+  transitionDuration,
+  transitionTimingFunction,
 } from '../../../utils';
 import { color } from '@porsche-design-system/utilities';
+import type { Theme } from '../../../types';
+import type { SocialIconName } from './link-social-utils';
+import { getHideLabelStyles as getLinkHideLabelStyles } from '../link/link-styles';
+
+const getHideLabelStyles: GetStylesFunction = (hideLabel: boolean): JssStyle => {
+  const baseHideLabelStyles = getLinkHideLabelStyles(hideLabel);
+  return mergeDeep(
+    baseHideLabelStyles,
+    hideLabel
+      ? {
+          root: {
+            padding: 0,
+          },
+        }
+      : {
+          root: {
+            padding: `${pxToRemWithUnit(12)} ${pxToRemWithUnit(16)} ${pxToRemWithUnit(12)} ${pxToRemWithUnit(40)}`,
+          },
+          icon: {
+            left: pxToRemWithUnit(11),
+            top: pxToRemWithUnit(11),
+          },
+        }
+  );
+};
+
+const getColorStyles = (icon: SocialIconName, isDarkTheme: boolean): Styles => {
+  return {
+    '&:hover, &:active': icon
+      ? {
+          color: color.darkTheme.default,
+          backgroundColor: color.external[icon.split('-')[1]],
+        }
+      : {
+          ...(isDarkTheme && { color: color.default }),
+          backgroundColor: isDarkTheme ? '#e0e0e0' : '#151718',
+        },
+  };
+};
+
+export const getComponentCss = (
+  icon: SocialIconName,
+  hideLabel: BreakpointCustomizable<boolean>,
+  theme: Theme
+): string => {
+  const isDarkTheme = isDark(theme);
+
+  return getCss(
+    mergeDeep<Styles>(
+      {
+        ...buildHostStyles({
+          display: 'inline-flex',
+          verticalAlign: 'top',
+          position: addImportantToRule('relative'),
+          cursor: 'pointer',
+        }),
+        root: {
+          display: 'flex',
+          width: '100%',
+          minWidth: pxToRemWithUnit(48),
+          minHeight: pxToRemWithUnit(48),
+          position: 'relative',
+          margin: 0,
+          padding: 0,
+          boxSizing: 'border-box',
+          appearance: 'none',
+          textDecoration: 'none',
+          backgroundColor: color.neutralContrast.high,
+          color: color.darkTheme.default,
+          ...getColorStyles(icon, isDarkTheme),
+          transition:
+            `background-color ${transitionDuration} ${transitionTimingFunction},` +
+            `color ${transitionDuration} ${transitionTimingFunction}`,
+          ...getFocusStyles({ theme }),
+        },
+        icon: {
+          position: 'absolute',
+          width: pxToRemWithUnit(24),
+          height: pxToRemWithUnit(24),
+        },
+        '::slotted(a)': addImportantToEachRule({
+          display: 'block',
+          position: 'static',
+          textDecoration: 'none',
+          color: 'inherit',
+          lineHeight: 'inherit',
+          outline: 'transparent none',
+        }),
+      },
+      buildResponsiveStyles(hideLabel, getHideLabelStyles)
+    )
+  );
+};
 
 export const getSlottedCss = (host: HTMLElement): string => {
   return getCss(
@@ -19,6 +126,15 @@ export const getSlottedCss = (host: HTMLElement): string => {
       })
     )
   );
+};
+
+export const addComponentCss = (
+  host: HTMLElement,
+  icon: SocialIconName,
+  hideLabel: BreakpointCustomizable<boolean>,
+  theme: Theme
+): void => {
+  attachCss(host, getComponentCss(icon, hideLabel, theme));
 };
 
 export const addSlottedCss = (host: HTMLElement): void => {
