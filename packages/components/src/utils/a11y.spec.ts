3577b5a0748663587bb7f1db371040e9fc26f4da
Bernd Hacker <hacker.bernd@gmail.com>
2021-08-04 11:32:28 +0200

    test(components): add unit tests for a11y utils | bh | #1079

---
+++ packages/components/src/utils/a11y.spec.ts
@@ -0,0 +1,47 @@
+import { booleanToString, setAriaAttributes, SetAriaAttributesOptions } from '.';
+import * as domUtils from './dom';
+
+describe('setAriaAttributes()', () => {
+  const node = document.createElement('div');
+  const label = 'Some label';
+  const message = 'Some message';
+
+  it.each<SetAriaAttributesOptions>([
+    { label },
+    { message },
+    { label, message },
+    { state: 'error' },
+    { state: 'success' },
+    { state: 'none' },
+  ])('should call setAttribute and removeAttribute with correct params for options: %o', (options) => {
+    const setAttributeSpy = jest.spyOn(domUtils, 'setAttribute');
+    const removeAttributeSpy = jest.spyOn(domUtils, 'removeAttribute');
+
+    setAriaAttributes(node, options);
+
+    if (options.label && !options.message) {
+      expect(setAttributeSpy).toHaveBeenCalledWith(node, 'aria-label', options.label);
+    } else if (!options.label && options.message) {
+      expect(setAttributeSpy).toHaveBeenCalledTimes(0);
+    } else if (options.label && options.message) {
+      expect(setAttributeSpy).toHaveBeenCalledWith(node, 'aria-label', options.label + '. ' + options.message);
+    }
+
+    if (options.state === 'error') {
+      expect(setAttributeSpy).toHaveBeenCalledWith(node, 'aria-invalid', 'true');
+    } else if (options.state) {
+      expect(removeAttributeSpy).toHaveBeenCalledWith(node, 'aria-invalid');
+    }
+  });
+});
+
+describe('booleanToString()', () => {
+  it.each<[boolean, string]>([
+    [true, 'true'],
+    [false, 'false'],
+    [null, 'false'],
+    [undefined, 'false'],
+  ])('should for %o return %o', (value, result) => {
+    expect(booleanToString(value)).toBe(result);
+  });
+});
