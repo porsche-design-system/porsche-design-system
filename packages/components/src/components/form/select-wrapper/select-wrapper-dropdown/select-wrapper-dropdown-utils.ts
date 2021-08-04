b02f5354f4299b8d390e53dfcd334c27b161e7a4
Bernd Hacker <hacker.bernd@gmail.com>
2021-08-04 11:37:25 +0200

    refactor(components): remove const aliasing in select-wrapper-dropdown-utils | bh | #1079

--- packages/components/src/components/form/select-wrapper/select-wrapper-dropdown/select-wrapper-dropdown-utils.ts
+++ packages/components/src/components/form/select-wrapper/select-wrapper-dropdown/select-wrapper-dropdown-utils.ts
@@ -34,11 +34,11 @@ export const getOptionAriaAttributes = ({
 
 export const determineDirection = (host: HTMLElement): DropdownDirectionInternal => {
   const { length } = getHTMLElements(host.shadowRoot, '.option:not([aria-hidden="true"])');
-  const { top: spaceTop } = host.getBoundingClientRect();
+  const { top } = host.getBoundingClientRect();
 
   const listHeight = length >= MAX_CHILDREN ? OPTION_HEIGHT * MAX_CHILDREN : OPTION_HEIGHT * length;
-  const spaceBottom = window.innerHeight - spaceTop - SELECT_HEIGHT;
-  return spaceBottom <= listHeight && spaceTop >= listHeight ? 'up' : 'down';
+  const spaceBottom = window.innerHeight - top - SELECT_HEIGHT;
+  return spaceBottom <= listHeight && top >= listHeight ? 'up' : 'down';
 };
 
 export const handleScroll = (host: HTMLElement, highlightedIndex: number): void => {
