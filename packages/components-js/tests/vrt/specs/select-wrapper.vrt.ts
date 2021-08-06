f5cb246b10e9b68fd9da7a66de946dbd7eef1438
Bernd Hacker <hacker.bernd@gmail.com>
2021-08-06 08:51:15 +0200

    test(components-js): extend select-wrapper vrt states test by filter and disabled states | bh | #1079

--- packages/components-js/tests/vrt/specs/select-wrapper.vrt.ts
+++ packages/components-js/tests/vrt/specs/select-wrapper.vrt.ts
@@ -31,39 +31,104 @@ describe('Select Wrapper', () => {
       await vrt.test('select-wrapper-states', async () => {
         const page = await vrt.getPage();
 
-        const head = `<style type="text/css">p-select-wrapper:not(:last-child) { margin-bottom: 1rem; }</style>`;
+        const head = `<style type="text/css">
+          .playground div {
+            display: flex;
+          }
+          p-select-wrapper {
+            width: 20rem;
+          }
+          p-select-wrapper:not(:last-child) {
+            margin-right: 1rem;
+            margin-bottom: 1rem;
+          }
+        </style>`;
 
-        const slottedMarkup = `<span slot="label">Some slotted label with a <a href="#">link</a>.</span>
-<span slot="description">Some slotted description with a <a href="#">link</a>.</span>
-<span slot="message">Some slotted validation message with a <a href="#">link</a>.</span>`;
-
-        const selectMarkup = `<select>
+        const getSelectMarkup = (opts?: { disabled?: boolean }): string => `
+<select${opts?.disabled ? ' disabled' : ''}>
   <option value="a">Option A</option>
 </select>`;
 
-        const getElementsMarkup: GetThemedMarkup = (theme) => `
-          <p-select-wrapper theme="${theme}" label="Some label">
-            ${selectMarkup}
-          </p-select-wrapper>
-          <p-select-wrapper theme="${theme}" label="Some label" state="error" message="Some error validation message.">
-            ${selectMarkup}
-          </p-select-wrapper>
-          <p-select-wrapper theme="${theme}" label="Some label" state="success" message="Some success validation message.">
-            ${selectMarkup}
-          </p-select-wrapper>
+        const getSlottedMarkup = (opts?: { disabled?: boolean }): string => `
+<span slot="label">Some${opts?.disabled ? ' disabled' : ''} slotted label with a <a href="#">link</a>.</span>
+<span slot="description">Some slotted description with a <a href="#">link</a>.</span>
+<span slot="message">Some slotted message with a <a href="#">link</a>.</span>`;
 
-          <p-select-wrapper theme="${theme}">
-            ${slottedMarkup}
-            ${selectMarkup}
-          </p-select-wrapper>
-          <p-select-wrapper theme="${theme}" state="error">
-            ${slottedMarkup}
-            ${selectMarkup}
-          </p-select-wrapper>
-          <p-select-wrapper theme="${theme}" state="success">
-            ${slottedMarkup}
-            ${selectMarkup}
-          </p-select-wrapper>`;
+        const getElementsMarkup: GetThemedMarkup = (theme) => `
+          <div>
+            <p-select-wrapper theme="${theme}" label="Some label">
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" label="Some label" state="error" message="Some error message.">
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" label="Some label" state="success" message="Some success message.">
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+          </div>
+          <div>
+            <p-select-wrapper theme="${theme}" filter label="Some filter label">
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" filter label="Some filter label" state="error" message="Some error message.">
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" filter label="Some filter label" state="success" message="Some success message.">
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+          </div>
+
+          <div>
+            <p-select-wrapper theme="${theme}" label="Some disabled label">
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" label="Some disabled label" state="error" message="Some error message.">
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" label="Some disabled label" state="success" message="Some success message.">
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+          </div>
+          <div>
+            <p-select-wrapper theme="${theme}" filter label="Some disabled filter label">
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" filter label="Some disabled filter label" state="error" message="Some error message.">
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" filter label="Some disabled filter label" state="success" message="Some success message.">
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+          </div>
+
+          <div>
+            <p-select-wrapper theme="${theme}">
+              ${getSlottedMarkup()}
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" state="error">
+              ${getSlottedMarkup()}
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" state="success">
+              ${getSlottedMarkup()}
+              ${getSelectMarkup()}
+            </p-select-wrapper>
+          </div>
+          <div>
+            <p-select-wrapper theme="${theme}">
+              ${getSlottedMarkup({ disabled: true })}
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" state="error">
+              ${getSlottedMarkup({ disabled: true })}
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+            <p-select-wrapper theme="${theme}" state="success">
+              ${getSlottedMarkup({ disabled: true })}
+              ${getSelectMarkup({ disabled: true })}
+            </p-select-wrapper>
+          </div>`;
         // TODO add hover test on fake option after select refactoring
 
         await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup), { injectIntoHead: head });
