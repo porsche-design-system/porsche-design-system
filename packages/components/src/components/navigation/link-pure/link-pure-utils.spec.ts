c73bf04851e2c0956d965e345986de3f292c7e32
Matthias Haegele <matthias.haegele@mhp.com>
2021-08-18 09:15:23 +0200

    test(components): add unit test for hasSubline helper | mh | #1484

--- packages/components/src/components/navigation/link-pure/link-pure-utils.spec.ts
+++ packages/components/src/components/navigation/link-pure/link-pure-utils.spec.ts
@@ -1,4 +1,4 @@
-import { hasIcon } from './link-pure-utils';
+import { hasIcon, hasSubline } from './link-pure-utils';
 
 describe('hasIcon', () => {
   it('should return true if called with valid iconName', () => {
@@ -6,5 +6,20 @@ describe('hasIcon', () => {
   });
   it('should return false if iconName = none', () => {
     expect(hasIcon('none')).toBe(false);
+  });
+});
+
+describe('hasSubline', () => {
+  it('should return true with slotted subline', () => {
+    const host = document.createElement('p-link-pure');
+    const paragraph = document.createElement('p');
+    paragraph.slot = 'subline';
+    host.appendChild(paragraph);
+
+    expect(hasSubline(host)).toBe(true);
+  });
+  it('should return false without subline', () => {
+    const host = document.createElement('p-link-pure');
+    expect(hasSubline(host)).toBe(false);
   });
 });
