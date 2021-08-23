e7727a482c9a4ad5b5be433837f59a4c3930bccc
Matthias Haegele <matthias.haegele@mhp.com>
2021-08-23 14:16:46 +0200

    test(components): update unit test to warning instead of error | mh | #1484

--- packages/components/src/components/action/button-pure/button-pure-utils.spec.ts
+++ packages/components/src/components/action/button-pure/button-pure-utils.spec.ts
@@ -1,37 +1,17 @@
-import { throwIfIconIsNoneAndIsLoading } from './button-pure-utils';
+import { warnIfIconIsNoneAndIsLoading } from './button-pure-utils';
 
-describe('throwIfIconIsNoneAndIsLoading()', () => {
-  it('should throw error if property icon = none and loading = true', () => {
-    const host = document.createElement('p-button-pure');
-
-    let error = undefined;
-    try {
-      throwIfIconIsNoneAndIsLoading(host, 'none', true);
-    } catch (e) {
-      error = e.message;
-    }
-    expect(error).not.toBe(undefined);
+describe('warnIfIconIsNoneAndIsLoading()', () => {
+  beforeEach(() => {
+    jest.spyOn(global.console, 'warn').mockImplementation(() => {});
   });
 
-  it('should not throw error if icon !== none and loading = true', () => {
+  it('should print warning if property icon = none and loading = true', () => {
     const host = document.createElement('p-button-pure');
-    let error = undefined;
-    try {
-      throwIfIconIsNoneAndIsLoading(host, 'highway', true);
-    } catch (e) {
-      error = e.message;
-    }
-    expect(error).toBe(undefined);
-  });
 
-  it('should not throw error if icon = none and loading = false', () => {
-    const host = document.createElement('p-button-pure');
-    let error = undefined;
-    try {
-      throwIfIconIsNoneAndIsLoading(host, 'none', false);
-    } catch (e) {
-      error = e.message;
-    }
-    expect(error).toBe(undefined);
+    warnIfIconIsNoneAndIsLoading(host, 'none', true);
+    warnIfIconIsNoneAndIsLoading(host, 'highway', true);
+    warnIfIconIsNoneAndIsLoading(host, 'none', false);
+
+    expect(console.warn).toBeCalledTimes(1);
   });
 });
