877834aece108c811ded376bd7f1afd862373b94
Matthias Haegele <matthias.haegele@mhp.com>
2021-08-24 10:25:46 +0200

    test(components): add typing | mh | #1484

--- packages/components/src/components/action/button-pure/button-pure-styles.spec.ts
+++ packages/components/src/components/action/button-pure/button-pure-styles.spec.ts
@@ -2,10 +2,11 @@ import { getComponentCss } from './button-pure-styles';
 import { BreakpointCustomizable } from '../../../utils';
 
 describe('getComponentCss()', () => {
-  it.each([false, true, { base: true, xs: false, s: true, m: false, l: true, xl: false }])(
-    'should return correct css for stretch: %o',
-    (stretch: BreakpointCustomizable<boolean>) => {
-      expect(getComponentCss(stretch)).toMatchSnapshot();
-    }
-  );
+  it.each<BreakpointCustomizable<boolean>>([
+    false,
+    true,
+    { base: true, xs: false, s: true, m: false, l: true, xl: false },
+  ])('should return correct css for stretch: %o', (stretch) => {
+    expect(getComponentCss(stretch)).toMatchSnapshot();
+  });
 });
