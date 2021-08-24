33c5420f77c1a49d7fd89d56a06634792a8d0d92
Matthias Haegele <matthias.haegele@mhp.com>
2021-08-24 13:52:08 +0200

    test(components-angular/react): align vrt pages to new label wording | mh | #1484

--- packages/components-react/src/pages/ButtonPure.tsx
+++ packages/components-react/src/pages/ButtonPure.tsx
@@ -5,6 +5,9 @@ export const ButtonPurePage = (): JSX.Element => {
     p-button-pure:not(:last-child) {
       margin-right: 8px;
     }
+    .stretched-buttons p-button-pure {
+      margin-right: 0;
+    }
   `;
 
   return (
@@ -12,17 +15,17 @@ export const ButtonPurePage = (): JSX.Element => {
       <style children={style} />
 
       <div className="playground light" title="should render button with label">
-        <PButtonPure>Some label</PButtonPure>
-        <PButtonPure disabled>Some label</PButtonPure>
-        <PButtonPure loading>Some label</PButtonPure>
+        <PButtonPure>Label default</PButtonPure>
+        <PButtonPure disabled>Label disabled</PButtonPure>
+        <PButtonPure loading>Label loading</PButtonPure>
       </div>
       <div className="playground dark" title="should render button with label on dark theme">
-        <PButtonPure theme="dark">Some label</PButtonPure>
+        <PButtonPure theme="dark">Label default</PButtonPure>
         <PButtonPure disabled theme="dark">
-          Some label
+          Label disabled
         </PButtonPure>
         <PButtonPure loading theme="dark">
-          Some label
+          Label loading
         </PButtonPure>
       </div>
 
@@ -49,27 +52,27 @@ export const ButtonPurePage = (): JSX.Element => {
 
       <div className="playground light" title="should render button with responsive label">
         <PButtonPure hideLabel={{ base: true, xs: false, s: true, m: false, l: true, xl: false }}>
-          Some label
+          Label Responsive
         </PButtonPure>
         <PButtonPure hideLabel={{ base: true, xs: false, s: true, m: false, l: true, xl: false }}>
-          Some label
+          Label Responsive
           <p slot="subline">Some subline</p>
         </PButtonPure>
       </div>
 
       <div className="playground light" title="should render button with different size">
-        <PButtonPure size="x-small">Some label</PButtonPure>
+        <PButtonPure size="x-small">Label size x-small</PButtonPure>
         <br />
-        <PButtonPure size="small">Some label</PButtonPure>
+        <PButtonPure size="small">Label size small</PButtonPure>
         <br />
-        <PButtonPure size="medium">Some label</PButtonPure>
+        <PButtonPure size="medium">Label size medium</PButtonPure>
         <br />
-        <PButtonPure size="large">Some label</PButtonPure>
+        <PButtonPure size="large">Label size large</PButtonPure>
         <br />
-        <PButtonPure size="x-large">Some label</PButtonPure>
+        <PButtonPure size="x-large">Label size x-large</PButtonPure>
         <br />
         <PButtonPure size="inherit" style={{ fontSize: 48 }}>
-          Some label
+          Label size inherit
         </PButtonPure>
       </div>
 
@@ -78,97 +81,104 @@ export const ButtonPurePage = (): JSX.Element => {
           size={{ base: 'x-small', xs: 'small', s: 'medium', m: 'large', l: 'x-large', xl: 'inherit' }}
           style={{ fontSize: 48 }}
         >
-          Some label
+          Label responsive size
         </PButtonPure>
       </div>
 
       <div className="playground light" title="should render button with different weight">
-        <PButtonPure weight="thin">Some label</PButtonPure>
-        <PButtonPure weight="regular">Some label</PButtonPure>
-        <PButtonPure weight="semibold">Some label</PButtonPure>
-        <PButtonPure weight="bold">Some label</PButtonPure>
+        <PButtonPure weight="thin">Label weight thin</PButtonPure>
+        <PButtonPure weight="regular">Label weight regular</PButtonPure>
+        <PButtonPure weight="semibold">Label weight semibold</PButtonPure>
+        <PButtonPure weight="bold">Label weight bold</PButtonPure>
       </div>
 
       <div className="playground light" title="should render button with specific icon">
-        <PButtonPure icon="delete">Some label</PButtonPure>
-        <PButtonPure iconSource="./assets/icon-custom-kaixin.svg">Some label</PButtonPure>
+        <PButtonPure icon="delete">Label with specific icon</PButtonPure>
+        <PButtonPure icon-source="./assets/icon-custom-kaixin.svg">Label with local icon-source</PButtonPure>
       </div>
 
       <div className="playground light" title="should render button with multiline label">
-        <PButtonPure style={{ width: 240 }}>Lorem ipsum dolor sit amet, consetetur sadipscing</PButtonPure>
+        <PButtonPure style={{ width: 240 }}>
+          Label multiline lorem ipsum dolor sit amet, consetetur sadipscing
+        </PButtonPure>
       </div>
 
       <div className="playground light" title="should render button-pure with custom clickable area">
-        <PButtonPure style={{ padding: '1rem' }}>Some label</PButtonPure>
+        <PButtonPure style={{ padding: '1rem' }}>Label with custom click-area</PButtonPure>
         <PButtonPure hideLabel style={{ padding: '1rem' }}>
-          Some label
+          Label with custom click-area
         </PButtonPure>
       </div>
 
       <div className="playground light" title="should render with subline">
         <PButtonPure size="small">
-          Some label<p slot="subline">Some subline</p>
+          Label size small
+          <p slot="subline">Some subline</p>
         </PButtonPure>
         <PButtonPure size="medium">
-          Some label<p slot="subline">Some subline</p>
+          Label size medium
+          <p slot="subline">Some subline</p>
         </PButtonPure>
         <PButtonPure size="large">
-          Some label<p slot="subline">Some subline</p>
+          Label size large
+          <p slot="subline">Some subline</p>
         </PButtonPure>
         <PButtonPure size="x-large">
-          Some label<p slot="subline">Some subline</p>
+          Label size x-large
+          <p slot="subline">Some subline</p>
         </PButtonPure>
         <PButtonPure size="medium" disabled>
-          Some label<p slot="subline">Some subline</p>
+          Label disabled
+          <p slot="subline">Some subline</p>
         </PButtonPure>
       </div>
 
       <div className="playground light" title="should render with no icon">
-        <PButtonPure icon="none">Without icon</PButtonPure>
+        <PButtonPure icon="none">Label without icon</PButtonPure>
         <PButtonPure size="small" icon="none">
-          Without icon
+          Label without icon
           <p slot="subline">Some subline</p>
         </PButtonPure>
       </div>
 
       <div className="playground light" title="should render icon if hide-label and icon none is set">
-        <PButtonPure hide-label={true} icon="none">
-          With hideLabel and no icon
+        <PButtonPure hideLabel icon="none">
+          Label with hideLabel and no icon
         </PButtonPure>
-        <PButtonPure hide-label={true} size="small" icon="none">
-          With hideLabel and no icon
+        <PButtonPure hideLabel size="small" icon="none">
+          Label with hideLabel and no icon
           <p slot="subline">Some subline</p>
         </PButtonPure>
       </div>
 
       <div className="playground light" title="should align label to the left">
-        <PButtonPure alignLabel="left">Align-label left</PButtonPure>
+        <PButtonPure alignLabel="left">Label aligned left</PButtonPure>
       </div>
       <div className="playground light" title="should align label to the left or right depending on viewport">
         <PButtonPure alignLabel={{ base: 'left', xs: 'right', s: 'left', m: 'right', l: 'left', xl: 'right' }}>
-          With breakpoint customizable align-label
+          Label responsive aligned left
         </PButtonPure>
       </div>
 
-      <div className="playground light" title="should render with stretched label">
-        <PButtonPure stretch={true}>Stretched icon left</PButtonPure>
-        <PButtonPure stretch={true} alignLabel="left">
-          Stretched icon right
+      <div className="playground light stretched-buttons" title="should render with stretched label">
+        <PButtonPure stretch>Label stretched</PButtonPure>
+        <PButtonPure stretch alignLabel="left">
+          Label stretched aligned left
         </PButtonPure>
       </div>
       <div className="playground light" title="should render with stretched label depending on viewport">
         <PButtonPure stretch={{ base: true, xs: false, s: true, m: false, l: true, xl: false }}>
-          Stretched depending on viewport
+          Label responsive stretched
         </PButtonPure>
       </div>
 
       <div className="playground light" title="should not align label left or stretch if it has a subline">
         <PButtonPure alignLabel="left">
-          With align-label and subline
+          Label aligned left
           <p slot="subline">Some subline</p>
         </PButtonPure>
-        <PButtonPure stretch={true}>
-          With stretch and subline
+        <PButtonPure stretch>
+          Label streched
           <p slot="subline">Some subline</p>
         </PButtonPure>
       </div>
