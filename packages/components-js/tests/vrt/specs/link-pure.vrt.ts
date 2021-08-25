af62015828eb749847f1a7819ac1a2e7d42c7f34
Matthias Haegele <matthias.haegele@mhp.com>
2021-08-25 11:44:24 +0200

    test(components-js): extend states test with custom icon / change br to div | mh  | #1484

--- packages/components-js/tests/vrt/specs/link-pure.vrt.ts
+++ packages/components-js/tests/vrt/specs/link-pure.vrt.ts
@@ -30,30 +30,33 @@ describe('Link Pure', () => {
       await vrt.test('link-pure-states', async () => {
         const page = await vrt.getPage();
 
-        const head = `<style type="text/css">p-link-pure:not(:last-child) { margin-right: 8px; }</style>`;
+        const head = `<style type="text/css">p-link-pure:not(:last-child) { margin-right: 8px; } .spacing-top { margin-top: 0.5rem }</style>`;
 
         const getElementsMarkup: GetThemedMarkup = (theme) => `
           <p-link-pure theme="${theme}" href="#">Label default</p-link-pure>
           <p-link-pure theme="${theme}" href="#">Label default <p slot="subline">Some Subline</p></p-link-pure>
           <p-link-pure theme="${theme}"><a href="#">Label slotted</a></p-link-pure>
-           <p-link-pure theme="${theme}" align-label="left">Label aligned left</p-link-pure>
-          <br/>
-          <br/>
-          <p-link-pure theme="${theme}" stretch="true">Label stretched</p-link-pure>
-          <br/>
-          <p-link-pure theme="${theme}" align-label="left" stretch="true">Label aligned left stretched</p-link-pure>
-          <br/>
-          <p-link-pure theme="${theme}" icon="none">Label without Icon</p-link-pure>
-          <p-link-pure theme="${theme}" icon="none">Label without Icon <p slot="subline">Some slightly longer subline</p></p-link-pure>`;
+          <p-link-pure theme="${theme}" align-label="left">Label aligned left</p-link-pure>
+          <p-link-pure theme="${theme}" align-label="left" icon="logoDelicious">Label aligned left</p-link-pure>
+          <div class="spacing-top">
+            <p-link-pure theme="${theme}" icon="none">Label without Icon</p-link-pure>
+            <p-link-pure theme="${theme}" icon="none">Label without Icon <p slot="subline">Some slightly longer subline</p></p-link-pure>
+          </div>
+          <div class="spacing-top">
+            <p-link-pure theme="${theme}" stretch="true">Label stretched</p-link-pure>
+          </div>
+          <div class="spacing-top">
+            <p-link-pure theme="${theme}" align-label="left" stretch="true">Label aligned left stretched</p-link-pure>
+          </div>`;
 
         await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup), { injectIntoHead: head });
 
-        await forceHoveredState(page, '.hovered > p-link-pure >>> a');
-        await forceHoveredState(page, '.hovered > p-link-pure >>> span');
-        await forceFocusedState(page, '.focused > p-link-pure >>> a');
-        await forceFocusedState(page, '.focused > p-link-pure >>> span');
-        await forceFocusedHoveredState(page, '.focused-hovered > p-link-pure >>> a');
-        await forceFocusedHoveredState(page, '.focused-hovered > p-link-pure >>> span');
+        await forceHoveredState(page, '.hovered p-link-pure >>> a');
+        await forceHoveredState(page, '.hovered p-link-pure >>> span');
+        await forceFocusedState(page, '.focused p-link-pure >>> a');
+        await forceFocusedState(page, '.focused p-link-pure >>> span');
+        await forceFocusedHoveredState(page, '.focused-hovered p-link-pure >>> a');
+        await forceFocusedHoveredState(page, '.focused-hovered p-link-pure >>> span');
       })
     ).toBeFalsy();
   });
