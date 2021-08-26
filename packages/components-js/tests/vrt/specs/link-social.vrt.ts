ae60406274eecc7ee1e4604cf982c541e02101c5
Oliver Hook <oliver.hook@mhp.com>
2021-08-26 18:04:14 +0200

    test(components-js): enhance vrt test of focus styles | oh | #1556

--- packages/components-js/tests/vrt/specs/link-social.vrt.ts
+++ packages/components-js/tests/vrt/specs/link-social.vrt.ts
@@ -34,17 +34,29 @@ describe('Link Social', () => {
 
         const getElementsMarkup: GetThemedMarkup = (theme) => `
           <p-link-social theme="${theme}" href="https://www.porsche.com/">Fallback</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.porsche.com/" hide-label="true">Fallback</p-link-social>
           <p-link-social theme="${theme}" href="https://www.facebook.com/" icon="logo-facebook">Facebook</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.facebook.com/" icon="logo-facebook" hide-label="true">Facebook</p-link-social>
           <p-link-social theme="${theme}" href="https://www.google.com/" icon="logo-google">Google</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.google.com/" icon="logo-google" hide-label="true">Google</p-link-social>
           <p-link-social theme="${theme}" href="https://www.instagram.com/" icon="logo-instagram">Instagram</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.instagram.com/" icon="logo-instagram" hide-label="true">Instagram</p-link-social>
           <p-link-social theme="${theme}" href="https://www.linkedin.com/" icon="logo-linkedin">LinkedIn</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.linkedin.com/" icon="logo-linkedin" hide-label="true">LinkedIn</p-link-social>
           <p-link-social theme="${theme}" href="https://www.pinterest.com/" icon="logo-pinterest">Pinterest</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.pinterest.com/" icon="logo-pinterest" hide-label="true">Pinterest</p-link-social>
           <p-link-social theme="${theme}" href="https://www.twitter.com/" icon="logo-twitter">Twitter</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.twitter.com/" icon="logo-twitter" hide-label="true">Twitter</p-link-social>
           <p-link-social theme="${theme}" href="https://www.wechat.com/" icon="logo-wechat">Wechat</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.wechat.com/" icon="logo-wechat" hide-label="true">Wechat</p-link-social>
           <p-link-social theme="${theme}" href="https://wa.me/491525557912" icon="logo-whatsapp">Whatsapp</p-link-social>
+          <p-link-social theme="${theme}" href="https://wa.me/491525557912" icon="logo-whatsapp" hide-label="true">Whatsapp</p-link-social>
           <p-link-social theme="${theme}" href="https://www.xing.com" icon="logo-xing">XING</p-link-social>
+          <p-link-social theme="${theme}" href="https://www.xing.com" icon="logo-xing" hide-label="true">XING</p-link-social>
           <p-link-social theme="${theme}" href="https://www.youtube.com" icon="logo-youtube">Youtube</p-link-social>
-          <p-link-social theme="${theme}" icon="logo-youtube"><a href="https://www.youtube.com">Slotted Youtube Anchor</a></p-link-social>`;
+          <p-link-social theme="${theme}" href="https://www.youtube.com" icon="logo-youtube" hide-label="true">Youtube</p-link-social>
+          <p-link-social theme="${theme}" icon="logo-youtube"><a href="https://www.youtube.com">Slotted Youtube Anchor</a></p-link-social>
+          <p-link-social theme="${theme}" icon="logo-youtube" hide-label="true"><a href="https://www.youtube.com">Slotted Youtube Anchor</a></p-link-social>`;
 
         await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup), { injectIntoHead: head });
 
