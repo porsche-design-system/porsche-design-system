b13710f6491899c5c24a930f96b492364fffed1b
Matthias Haegele <matthias.haegele@mhp.com>
2021-08-30 16:38:18 +0200

    feat(email): remove unneeded steps in build / make build sync | mh | #1492

--- packages/email/scripts/build.ts
+++ packages/email/scripts/build.ts
@@ -6,76 +6,34 @@ import { paramCase, camelCase } from 'change-case';
 import { CDN_BASE_URL_DYNAMIC, CDN_KEY_TYPE_DEFINITION, CDN_BASE_PATH_EMAIL } from '../../../cdn.config';
 
 type Manifest = {
-  [name: string]: {
-    [size: string]: {
-      [resolution: string]: string;
-    };
-  };
+  [name: string]: string;
 };
 
 const toHash = (str: string): string => crypto.createHash('md5').update(str, 'utf8').digest('hex');
 
-const checkIntegrity = async (manifest: Manifest): Promise<void> => {
-  for (const [name, size] of Object.entries(manifest)) {
-    if (name.includes('Email')) {
-      return;
-    } else {
-      if (!size.small) {
-        throw new Error(`Marque size declaration "small" is missing in manifest for "${name}".`);
-      }
-      if (!size.medium) {
-        throw new Error(`Marque size declaration "medium" is missing in manifest for "${name}".`);
-      }
-      for (const resolution of Object.values(size)) {
-        if (!resolution['1x']) {
-          throw new Error(`Marque resolution declaration "1x" is missing in manifest for "${name}".`);
-        }
-        if (!resolution['2x']) {
-          throw new Error(`Marque resolution declaration "2x" is missing in manifest for "${name}".`);
-        }
-        if (!resolution['3x']) {
-          throw new Error(`Marque resolution declaration "3x" is missing in manifest for "${name}".`);
-        }
-      }
-    }
-  }
-};
-
-const createManifestAndCopyMarque = async (cdn: string, files: string[]): Promise<void> => {
+const createManifestAndCopyMarque = (cdn: string, files: string[]): void => {
   fs.rmdirSync(path.normalize('./dist'), { recursive: true });
   fs.mkdirSync(path.normalize('./dist/email'), { recursive: true });
 
   const manifest: Manifest = {};
 
-  for (let file of files) {
+  for (const file of files) {
     const ext = path.extname(file);
     const sourcePath = path.normalize(file);
-    const info = path.basename(sourcePath, ext).split(/[.@]/g);
-    const name = info[0];
-    const size = info[1];
-    const resolution = info[2];
+    const [name] = path.basename(sourcePath, ext).split(/[.]/g);
     const marque = fs.readFileSync(sourcePath, { encoding: 'binary' });
     const hash = toHash(marque);
-    const filename = `${paramCase(name)}.${paramCase(size)}.min.${hash}@${paramCase(resolution)}.png`;
+    const filename = `${paramCase(name)}.min.${hash}}.png`;
     const targetPath = path.normalize(`./dist/email/${filename}`);
 
     const nameKey = camelCase(name);
-    const sizeKey = camelCase(size);
-    const resolutionKey = camelCase(resolution);
-    manifest[nameKey] = {
-      ...manifest[nameKey],
-      [sizeKey]: {
-        ...manifest[nameKey]?.[sizeKey],
-        [resolutionKey]: filename,
-      },
-    };
+
+    manifest[nameKey] = filename;
 
     fs.writeFileSync(targetPath, marque, { encoding: 'binary' });
 
-    console.log(`Marque "${name}" copied as "${size}" variant in ${resolution} resolution.`);
+    console.log(`Marque "${name}" copied`);
   }
-
-  await checkIntegrity(manifest);
 
   fs.writeFileSync(
     path.normalize('./index.ts'),
@@ -88,12 +46,7 @@ export const EMAIL_MANIFEST = ${JSON.stringify(manifest)};`
   console.log('Created email manifest.');
 };
 
-(async (): Promise<void> => {
-  const cdn = `${CDN_BASE_URL_DYNAMIC} + '/${CDN_BASE_PATH_EMAIL}'`;
-  const files = (await globby('./src/**/*.png')).sort();
+const cdn = `${CDN_BASE_URL_DYNAMIC} + '/${CDN_BASE_PATH_EMAIL}'`;
+const files = globby.sync('./src/**/*.png').sort();
 
-  await createManifestAndCopyMarque(cdn, files).catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
-})();
+createManifestAndCopyMarque(cdn, files);
