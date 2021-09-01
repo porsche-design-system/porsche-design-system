871e63ae40f66ab347d75ec0dc7c75391b658efd
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-01 09:58:43 +0200

    refactor(components): move dropdown utils into select-wrapper-dropdown-utils | bh | #1079

--- packages/components/src/components/form/select-wrapper/select-wrapper/select-wrapper-utils.ts
+++ packages/components/src/components/form/select-wrapper/select-wrapper/select-wrapper-utils.ts
@@ -1,9 +1,7 @@
-import { getTagName, hasAttribute, isTouchDevice } from '../../../../utils';
+import { isTouchDevice } from '../../../../utils';
 
 export type DropdownDirectionInternal = 'down' | 'up';
 export type DropdownDirection = DropdownDirectionInternal | 'auto';
-
-export type KeyboardDirectionInternal = DropdownDirectionInternal;
 
 export const isCustomDropdown = (filter: boolean, native: boolean): boolean => {
   // TODO: one liner?
@@ -14,122 +12,5 @@ export const isCustomDropdown = (filter: boolean, native: boolean): boolean => {
     return false;
   } else {
     return !isTouchDevice();
-  }
-};
-
-export type OptionMap = {
-  value: string;
-  disabled: boolean;
-  hidden: boolean; // used for filtering
-  initiallyHidden: boolean;
-  selected: boolean;
-  highlighted: boolean;
-  title?: string; // for optgroup
-};
-
-export const getOptionsElements = (select: HTMLSelectElement): HTMLOptionElement[] => Array.from(select.options);
-
-export const getOptionMaps = (options: HTMLOptionElement[]): OptionMap[] =>
-  options.map((item) => {
-    const { selected, parentElement, previousElementSibling } = item;
-    const option: OptionMap = {
-      value: item.text,
-      disabled: hasAttribute(item, 'disabled'),
-      hidden: false,
-      initiallyHidden: hasAttribute(item, 'hidden'),
-      selected,
-      highlighted: selected,
-      ...(getTagName(parentElement) === 'optgroup' &&
-        previousElementSibling === null && { title: (parentElement as HTMLOptGroupElement).label }),
-    };
-    return option;
-  });
-
-export const updateSelectedOptionMaps = (options: OptionMap[], newIndex: number): OptionMap[] =>
-  options.map((item, idx) => ({
-    ...item,
-    selected: idx === newIndex,
-    highlighted: idx === newIndex,
-    hidden: false,
-  }));
-
-export const updateHighlightedOptionMaps = (options: OptionMap[], newIndex: number): OptionMap[] =>
-  options.map((item, idx) => ({
-    ...item,
-    highlighted: idx === newIndex,
-  }));
-
-export const resetHighlightedOptionMaps = (options: OptionMap[]): OptionMap[] =>
-  options.map((item) => ({ ...item, highlighted: false }));
-
-export const updateFirstHighlightedOptionMaps = (options: OptionMap[]): OptionMap[] =>
-  updateHighlightedOptionMaps(options, 0);
-
-export const updateLastHighlightedOptionMaps = (options: OptionMap[]): OptionMap[] =>
-  updateHighlightedOptionMaps(options, options.length - 1);
-
-export const getHighlightedOptionMapIndex = (arr: OptionMap[]): number => arr.findIndex((item) => item.highlighted);
-export const getSelectedOptionMapIndex = (arr: OptionMap[]): number => arr.findIndex((item) => item.selected);
-
-export const getSelectedOptionMap = (arr: OptionMap[]): OptionMap => arr.find((item) => item.selected);
-
-export const getHighlightedOptionMap = (arr: OptionMap[]): OptionMap => arr.find((item) => item.highlighted);
-
-export const getValidOptions = (options: OptionMap[]): OptionMap[] =>
-  options.filter((item) => !item.hidden && !item.initiallyHidden && !item.disabled);
-
-export const getMatchingOptionMaps = (options: OptionMap[], searchString: string): OptionMap[] => {
-  const lowerCaseSearchString = searchString.toLowerCase();
-  return lowerCaseSearchString && options.filter((item) => item.value.toLowerCase() === lowerCaseSearchString);
-};
-
-export const updateFilteredOptionMaps = (options: OptionMap[], searchString: string): OptionMap[] => {
-  const lowerCaseSearchString = searchString.toLowerCase();
-  return options.map((item) => ({
-    ...item,
-    hidden: !item.initiallyHidden && !item.value.toLowerCase().includes(lowerCaseSearchString),
-  }));
-};
-
-export const resetFilteredOptionMaps = (options: OptionMap[]): OptionMap[] =>
-  options.map((item) => ({
-    ...item,
-    hidden: false,
-  }));
-
-export const hasFilterResults = (options: OptionMap[]): boolean =>
-  options.some((item) => !item.hidden && !item.initiallyHidden);
-
-export const getNewOptionMapIndex = (options: OptionMap[], direction: KeyboardDirectionInternal): number => {
-  const validItems = getValidOptions(options);
-  const validMax = validItems.length - 1;
-  // prob. needs to be <= 0
-  if (validMax < 0) {
-    return;
-  }
-
-  let i = getHighlightedOptionMapIndex(validItems);
-  if (direction === 'down') {
-    i = i < validMax ? i + 1 : 0;
-  } else if (direction === 'up') {
-    i = i > 0 ? i - 1 : validMax;
-  }
-
-  return options.indexOf(validItems[i]);
-};
-
-export type DropdownInteractionType = 'show' | 'hide' | 'toggle';
-export const getDropdownVisibility = (
-  isOpen: boolean,
-  type: DropdownInteractionType,
-  resetFilter: () => void
-): boolean => {
-  if (isOpen && (type === 'hide' || type === 'toggle')) {
-    resetFilter();
-    return false;
-  } else if (!isOpen && (type === 'show' || type === 'toggle')) {
-    return true;
-  } else {
-    return isOpen;
   }
 };
