8dc191d48e424a8b2870a710fa75c75dbb2bfd6f
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-07 14:14:47 +0200

    test(components): adjust unit tests for select-wrapper and select-wrapper-dropdown | bh | #1079

--- packages/components/src/components/form/select-wrapper/select-wrapper-dropdown/select-wrapper-dropdown-styles.spec.ts
+++ packages/components/src/components/form/select-wrapper/select-wrapper-dropdown/select-wrapper-dropdown-styles.spec.ts
@@ -1,24 +1,28 @@
 import { getButtonStyles, getComponentCss, getFilterStyles, getListStyles } from './select-wrapper-dropdown-styles';
 import type { DropdownDirectionInternal } from '../select-wrapper/select-wrapper-utils';
 import type { FormState, Theme } from '../../../../types';
+import { getCss } from '../../../../utils';
 
 describe('getButtonStyles()', () => {
-  it.each<[boolean, FormState, Theme]>([
-    [false, 'none', 'light'],
-    [false, 'none', 'dark'],
-    [false, 'success', 'light'],
-    [false, 'success', 'dark'],
-    [false, 'error', 'light'],
-    [false, 'error', 'dark'],
-    [true, 'none', 'light'],
-    [true, 'none', 'dark'],
-    [true, 'success', 'light'],
-    [true, 'success', 'dark'],
-    [true, 'error', 'light'],
-    [true, 'error', 'dark'],
-  ])('should return correct css for disabled: %o, state: %o and theme: %o', (disabled, state, theme) => {
-    expect(getButtonStyles(disabled, state, theme)).toMatchSnapshot();
-  });
+  it.each<[boolean, FormState, Theme, boolean]>([
+    [false, 'none', 'light', true],
+    [false, 'none', 'dark', false],
+    [false, 'success', 'light', true],
+    [false, 'success', 'dark', false],
+    [false, 'error', 'light', true],
+    [false, 'error', 'dark', false],
+    [true, 'none', 'light', true],
+    [true, 'none', 'dark', false],
+    [true, 'success', 'light', true],
+    [true, 'success', 'dark', false],
+    [true, 'error', 'light', true],
+    [true, 'error', 'dark', false],
+  ])(
+    'should return correct css for disabled: %o, state: %o, theme: %o and isOpen: %o',
+    (disabled, state, theme, isOpen) => {
+      expect(getCss(getButtonStyles(disabled, state, theme, isOpen))).toMatchSnapshot();
+    }
+  );
 });
 
 describe('getFilterStyles()', () => {
@@ -36,7 +40,7 @@ describe('getFilterStyles()', () => {
     [true, 'error', 'light'],
     [true, 'error', 'dark'],
   ])('should return correct css for disabled: %o, state: %o and theme: %o', (disabled, state, theme) => {
-    expect(getFilterStyles(disabled, state, theme)).toMatchSnapshot();
+    expect(getCss(getFilterStyles(disabled, state, theme))).toMatchSnapshot();
   });
 });
 
@@ -51,7 +55,7 @@ describe('getListStyles()', () => {
     ['up', true, 'dark'],
     ['up', false, 'dark'],
   ])('should return correct css for direction: %o, isOpen: %o and theme: %o', (direction, isOpen, theme) => {
-    expect(getListStyles(direction, isOpen, theme)).toMatchSnapshot();
+    expect(getCss(getListStyles(direction, isOpen, theme))).toMatchSnapshot();
   });
 });
 
