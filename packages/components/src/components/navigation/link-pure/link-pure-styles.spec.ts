d4e9b7711abaa6f78f5cbcf4484763168110ce29
Stephan Schröter <stephan.schroeter@mhp.com>
2021-09-08 13:50:16 +0200

    test: extend style unit test for link-pure | mh / sas | #1562

--- packages/components/src/components/navigation/link-pure/link-pure-styles.spec.ts
+++ packages/components/src/components/navigation/link-pure/link-pure-styles.spec.ts
@@ -15,6 +15,23 @@ describe('getSlottedCss()', () => {
 });
 
 describe('getComponentCss()', () => {
+  const breakpointCustomizableBoolean = { base: true, xs: false, s: true, m: false, l: true, xl: false };
+  const breakpointCustomizableSize: BreakpointCustomizable<TextSize> = {
+    base: 'x-small',
+    xs: 'small',
+    s: 'medium',
+    m: 'large',
+    l: 'x-large',
+    xl: 'inherit',
+  };
+  const breakpointCustomizableAlignLabel: AlignLabel = {
+    base: 'left',
+    xs: 'right',
+    s: 'left',
+    m: 'right',
+    l: 'left',
+    xl: 'right',
+  };
   it.each<
     [
       LinkButtonPureIconName,
@@ -27,12 +44,30 @@ describe('getComponentCss()', () => {
       boolean,
       Theme
     ]
-  >([['arrow-head-right', false, false, 'small', false, 'right', false, false, 'light']])(
-    'should return correct css',
-    (icon, active, stretch, size, hideLabel, alignLabel, hasSubline, hasHref, theme) => {
-      expect(
-        getComponentCss(icon, active, stretch, size, hideLabel, alignLabel, hasSubline, hasHref, theme)
-      ).toMatchSnapshot();
-    }
-  );
+  >([
+    ['arrow-head-right', false, false, 'small', false, 'right', false, true, 'light'],
+    ['arrow-head-right', false, false, 'small', false, 'right', false, true, 'dark'],
+    ['arrow-head-right', true, true, 'x-large', false, 'left', false, true, 'light'],
+    ['arrow-head-right', true, true, 'x-large', false, 'left', false, true, 'dark'],
+    ['arrow-head-right', false, false, 'small', true, 'right', false, true, 'light'],
+    ['arrow-head-right', false, false, 'small', true, 'right', true, true, 'light'],
+    ['arrow-head-right', false, false, 'small', breakpointCustomizableBoolean, 'right', false, true, 'light'],
+    ['none', false, false, 'small', false, 'right', false, true, 'light'],
+    ['arrow-head-right', false, false, 'small', false, 'right', false, false, 'light'],
+    [
+      'arrow-head-right',
+      false,
+      breakpointCustomizableBoolean,
+      breakpointCustomizableSize,
+      false,
+      breakpointCustomizableAlignLabel,
+      false,
+      true,
+      'light',
+    ],
+  ])('should return correct css', (icon, active, stretch, size, hideLabel, alignLabel, hasSubline, hasHref, theme) => {
+    expect(
+      getComponentCss(icon, active, stretch, size, hideLabel, alignLabel, hasSubline, hasHref, theme)
+    ).toMatchSnapshot();
+  });
 });
