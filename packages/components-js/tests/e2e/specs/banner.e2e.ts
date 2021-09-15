9313981daa535d10bd5f311763519fde285f980e
Oliver Hook <oliver.hook@mhp.com>
2021-09-15 15:10:25 +0200

    test(components-js): add accessibility tree test for banner | oh | #1564

--- packages/components-js/tests/e2e/specs/banner.e2e.ts
+++ packages/components-js/tests/e2e/specs/banner.e2e.ts
@@ -237,4 +237,28 @@ describe('banner', () => {
       expect(status.componentDidUpdate.all).withContext('componentDidUpdate: all').toBe(2);
     });
   });
+
+  describe('accessibility', () => {
+    it('should expose correct initial accessibility tree', async () => {
+      await initBanner('neutral');
+      const getCloseButton = () => selectNode(page, 'p-banner >>> p-button-pure >>> button');
+      const getWrapper = () => selectNode(page, 'p-banner >>> p-content-wrapper');
+
+      const snapshotWrapper = await page.accessibility.snapshot({
+        root: await getWrapper(),
+        interestingOnly: false,
+      });
+
+      const snapshotButton = await page.accessibility.snapshot({
+        root: await getCloseButton(),
+      });
+
+      expect(snapshotWrapper.role).toBe('alertdialog');
+      expect(snapshotWrapper.name).toBe('Some notification title with an anchor.');
+      expect(snapshotWrapper.description).toBe('Some notification description with an anchor.');
+      expect(snapshotButton.role).toBe('button');
+      expect(snapshotButton.name).toBe('Close notification');
+      expect(snapshotButton.focused).toBe(true); // do we test this somewhere else?
+    });
+  });
 });
