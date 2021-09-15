cea3ca2c0361bffbcec0acec455011fbee046620
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-15 16:55:16 +0200

    test(components-js/angular/react): fix select-wrapper vrt | bh

--- packages/components-angular/tests/vrt/specs/select-wrapper.vrt.ts
+++ packages/components-angular/tests/vrt/specs/select-wrapper.vrt.ts
@@ -3,10 +3,12 @@ import { CSS_TRANSITION_DURATION, getVisualRegressionTester, vrtTest } from '@po
 describe('Select Wrapper', () => {
   it('should have no visual regression', async () => {
     expect(
-      await vrtTest(getVisualRegressionTester(), 'select-wrapper', '/select-wrapper', async (page) => {
-        await page.click('#open-options');
-        await page.evaluate(() => (window as any).componentsReady());
-        await page.waitForTimeout(CSS_TRANSITION_DURATION);
+      await vrtTest(getVisualRegressionTester(), 'select-wrapper', '/select-wrapper', {
+        scenario: async (page) => {
+          await page.click('#open-options');
+          await page.evaluate(() => (window as any).componentsReady());
+          await page.waitForTimeout(CSS_TRANSITION_DURATION);
+        },
       })
     ).toBeFalsy();
   });
