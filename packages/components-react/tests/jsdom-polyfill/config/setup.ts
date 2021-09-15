99ceeb75c7a9f1b6d79ea8edcc8915e03ed328b9
Matthias Haegele <matthias.haegele@mhp.com>
2021-09-15 15:48:46 +0200

    feat(components): catch if fetch is undefined | mh / as | #1565

--- packages/components-react/tests/jsdom-polyfill/config/setup.ts
+++ packages/components-react/tests/jsdom-polyfill/config/setup.ts
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom';
-
 import { skipCheckForPorscheDesignSystemProviderDuringTests } from '@porsche-design-system/components-react';
 import '@porsche-design-system/components-react/jsdom-polyfill';
 skipCheckForPorscheDesignSystemProviderDuringTests();
