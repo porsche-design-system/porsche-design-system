442030ce1e832d6f6805df5c6cc020a821808cf8
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-16 12:06:57 +0200

    Revert "test(components-js/angular/react): fix flaky link-pure and button-pure vrts | bh"
    This reverts commit ffab6a22af4d1f0290b8b93e00b5e6f8267012bd.

--- packages/components-react/tests/vrt/specs/button-pure.vrt.ts
+++ packages/components-react/tests/vrt/specs/button-pure.vrt.ts
@@ -1,11 +1,7 @@
-import { CSS_TRANSITION_DURATION, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
+import { getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 
 describe('Button Pure', () => {
   it('should have no visual regression', async () => {
-    expect(
-      await vrtTest(getVisualRegressionTester(), 'button-pure', '/button-pure', {
-        scenario: (page) => page.waitForTimeout(CSS_TRANSITION_DURATION),
-      })
-    ).toBeFalsy();
+    expect(await vrtTest(getVisualRegressionTester(), 'button-pure', '/button-pure')).toBeFalsy();
   });
 });
