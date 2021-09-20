f99f4d1403995ce9bb95b571e2b7cfa8bb683adb
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-20 11:32:46 +0200

    refactor(components): rename variables in line-height utils from caps | bh

--- packages/components/src/utils/line-height.ts
+++ packages/components/src/utils/line-height.ts
@@ -1,18 +1,17 @@
 import { font } from '@porsche-design-system/utilities';
 
 // map is prefilled with static values from utilities package
-const FONT_SIZE_MAP = new Map<number, number>(
+const lineHeightMap = new Map<number, number>(
   Object.entries(font.size)
     .map<[number, number]>(([key, val]) => [parseInt(key, 10), val.lineHeight as number])
     .filter(([key]) => !isNaN(key as any))
 );
-const FONT_SIZE_REGEX = /^(\d+\.?\d*)(rem|px)$/;
 
 export const calculateLineHeight = (input: string): number => {
-  const [, fontSizeValue, fontSizeUnit] = FONT_SIZE_REGEX.exec(input);
+  const [, fontSizeValue, fontSizeUnit] = /^(\d+\.?\d*)(rem|px)$/.exec(input);
   const fontSize = fontSizeUnit === 'rem' ? parseFloat(fontSizeValue) * 16 : parseFloat(fontSizeValue);
 
-  if (!FONT_SIZE_MAP.has(fontSize)) {
+  if (!lineHeightMap.has(fontSize)) {
     const exactLineHeightPx = fontSize * (0.911 / (2.97 + 0.005 * Math.pow(2.71828, 0.2 * fontSize)) + 1.2);
     let remainingPx = exactLineHeightPx % 4;
 
@@ -23,10 +22,10 @@ export const calculateLineHeight = (input: string): number => {
     const roundingFactor = 10000000000;
     const fittedLineHeightFactor = (exactLineHeightPx - remainingPx) / fontSize;
 
-    FONT_SIZE_MAP.set(fontSize, Math.round(fittedLineHeightFactor * roundingFactor) / roundingFactor);
+    lineHeightMap.set(fontSize, Math.round(fittedLineHeightFactor * roundingFactor) / roundingFactor);
   }
 
-  return FONT_SIZE_MAP.get(fontSize);
+  return lineHeightMap.get(fontSize);
 };
 
 export const calcLineHeightForElement = (tag: HTMLElement): number => {
