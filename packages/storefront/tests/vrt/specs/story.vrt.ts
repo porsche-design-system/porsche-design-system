cda939723228e558b0a8c2cfc187a9916dde2fe3
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-20 13:31:08 +0200

    refactor(shared/angular/components-js/storefront): remove unused CSS_TRANSITION_DURATION const | bh

--- packages/storefront/tests/vrt/specs/story.vrt.ts
+++ packages/storefront/tests/vrt/specs/story.vrt.ts
@@ -1,4 +1,4 @@
-import { CSS_TRANSITION_DURATION, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
+import { getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 import { routerViewSelector } from '../helpers/setup';
 
 describe('Story', () => {
@@ -8,8 +8,7 @@ describe('Story', () => {
         elementSelector: routerViewSelector,
         scenario: async (page) => {
           await page.click('.playground:nth-of-type(1) > p-tabs-bar > button:nth-of-type(2)');
-          await page.waitForTimeout(CSS_TRANSITION_DURATION);
-          await page.waitForTimeout(CSS_TRANSITION_DURATION); // it's actual js scrolling and no transition
+          await page.waitForTimeout(500); // it's actual js scrolling and no transition
         },
       })
     ).toBeFalsy();
