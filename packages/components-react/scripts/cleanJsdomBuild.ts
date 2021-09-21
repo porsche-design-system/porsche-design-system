aaba85e3eb824efa5ece13dd24378149a066fd07
Matthias Haegele <matthias.haegele@mhp.com>
2021-09-21 12:08:05 +0200

    refactor(components-react): improve and shorten cleanJsdomBuild script | mh / bh | #1565

--- packages/components-react/scripts/cleanJsdomBuild.ts
+++ packages/components-react/scripts/cleanJsdomBuild.ts
@@ -1,67 +1,45 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-const getFileContent = (): { filePath: string; fileContent: string } => {
-  const rootDirectory = path.resolve(__dirname, '..');
-  const directory = path.resolve(rootDirectory, 'dist/components-wrapper/jsdom-polyfill');
-  const filePath = path.resolve(directory, 'index.js');
-
-  return { filePath, fileContent: fs.readFileSync(filePath, 'utf8') };
-};
-
-const cleanConsoleWarnInInjectToGlobalStyles = (): void => {
-  const { filePath, fileContent } = getFileContent();
-  const result = fileContent.replace(
+const cleanConsoleWarnInInjectToGlobalStyles = (fileContent: string): string => {
+  return fileContent.replace(
     /console\.warn\(`The Porsche Design System had to inject our font-face.css file into your head.(.|\s)*?\);/,
     ''
   );
-
-  fs.writeFileSync(filePath, result);
-
-  console.log('Cleaned inject global styles warning ');
 };
 
-const conditionalFetch = `if(!window.PDS_SKIP_FETCH) {
-      $1
-    }`;
-
-const addConditionToGlobalScripts = () => {
-  const { filePath, fileContent } = getFileContent();
-  const result = fileContent.replace(/(appGlobals\.globalScripts\(\);)/, conditionalFetch);
-
-  fs.writeFileSync(filePath, result);
-
-  console.log('Added fetch condition to globalScripts');
+const addConditionToGlobalScripts = (fileContent: string): string => {
+  return fileContent.replace(/(appGlobals\.globalScripts\(\);)/, 'if(!window.PDS_SKIP_FETCH) { $1 }');
 };
 
-const addConditionToPdsFetch = () => {
-  const { filePath, fileContent } = getFileContent();
-  const replaceValue = `const pdsFetch = \(input, init\) => !window.PDS_SKIP_FETCH ? fetch\(input, init\) : 'undefined'`;
-
-  const result = fileContent.replace(/const pdsFetch = \(input, init\) => fetch\(input, init\);/s, replaceValue);
-
-  fs.writeFileSync(filePath, result);
-
-  console.log(`Added fetch condition to icon`);
+const addConditionToPdsFetch = (fileContent: string): string => {
+  return fileContent.replace(
+    /(const pdsFetch = \(input, init\) =>) (fetch\(input, init\);)/,
+    '$1 window.PDS_SKIP_FETCH ? undefined : $2'
+  );
 };
 
-const addPictureConditionToMarque = () => {
-  const { filePath, fileContent } = getFileContent();
-
-  const conditionalPicture = `$1 !window.PDS_SKIP_FETCH ?$2 : undefined;`;
-
-  const result = fileContent.replace(/(const picture =)( \(resizeObserver.*? }\)\)\));/s, conditionalPicture);
-
-  fs.writeFileSync(filePath, result);
-
-  console.log(`Added fetch condition to marque`);
+const addPictureConditionToMarque = (fileContent: string): string => {
+  return fileContent.replace(
+    /(const picture =)( \(resizeObserver(?:.|\s)*?;)/,
+    '$1 window.PDS_SKIP_FETCH ? undefined : $2'
+  );
 };
 
 const cleanJsdomBuild = (): void => {
-  cleanConsoleWarnInInjectToGlobalStyles();
-  addConditionToGlobalScripts();
-  addConditionToPdsFetch();
-  addPictureConditionToMarque();
+  const directory = require.resolve('@porsche-design-system/components-react');
+  const filePath = path.resolve(directory, '../jsdom-polyfill/index.js');
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+
+  const content = [
+    cleanConsoleWarnInInjectToGlobalStyles,
+    addConditionToGlobalScripts,
+    addConditionToPdsFetch,
+    addPictureConditionToMarque,
+  ].reduce((previousResult, fn) => fn(previousResult), fileContent);
+
+  fs.writeFileSync(filePath, content);
+  console.log('jsdom build successful cleaned');
 };
 
 cleanJsdomBuild();
