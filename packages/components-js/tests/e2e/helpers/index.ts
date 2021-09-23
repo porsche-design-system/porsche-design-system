8a21b3b7587f93609998879c98992702a8e159ee
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-23 08:12:16 +0200

    test(components-js): add declare global browser typing for e2e tests | bh | #1564

--- packages/components-js/tests/e2e/helpers/index.ts
+++ packages/components-js/tests/e2e/helpers/index.ts
@@ -1,4 +1,10 @@
+import type { Browser } from 'puppeteer';
+
 export * from './accessibility';
 export * from './events';
 export * from './puppeteer-helper';
 export * from './stencil';
+
+declare global {
+  const browser: Browser;
+}
