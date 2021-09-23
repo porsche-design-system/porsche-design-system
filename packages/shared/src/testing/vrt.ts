62851420a1ce41c7af4916e449cf6d2efb98e07f
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-23 08:15:44 +0200

    refactor(shared): remove commented jasmine code | bh | #1564

--- packages/shared/src/testing/vrt.ts
+++ packages/shared/src/testing/vrt.ts
@@ -1,8 +1,6 @@
 import { VisualRegressionTester } from '@porsche-design-system/visual-regression-tester';
 import type { TestOptions, VisualRegressionTestOptions } from '@porsche-design-system/visual-regression-tester';
-import { launch } from 'puppeteer';
 import type { Browser, Page } from 'puppeteer';
-import { SpecReporter } from 'jasmine-spec-reporter';
 
 export type {
   VisualRegressionTester,
@@ -12,31 +10,6 @@ export type {
 declare global {
   const browser: Browser;
 }
-
-// let browser: Browser;
-
-// jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
-//
-// jasmine.getEnv().clearReporters();
-// jasmine.getEnv().addReporter(new SpecReporter());
-//
-// beforeAll(async () => {
-//   browser = await launch({
-//     args: [
-//       '--no-sandbox',
-//       '--disable-setuid-sandbox',
-//       '--disable-dev-shm-usage',
-//       '--single-process',
-//       '--disable-web-security',
-//     ],
-//   });
-// });
-//
-// afterAll(async () => {
-//   if (browser) {
-//     await browser.close();
-//   }
-// });
 
 const defaultOptions: VisualRegressionTestOptions = {
   viewports: [320, 480, 760, 1000, 1300, 1760],
