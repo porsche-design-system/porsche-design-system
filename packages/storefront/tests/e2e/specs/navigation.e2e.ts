108520a3812024954aecbf8081c36fea9f734148
Bernd Hacker <hacker.bernd@gmail.com>
2021-09-23 15:17:53 +0200

    test(storefront): migrate e2e tests to jest, add workaround for console logs | bh | #1564

--- packages/storefront/tests/e2e/specs/navigation.e2e.ts
+++ packages/storefront/tests/e2e/specs/navigation.e2e.ts
@@ -5,104 +5,94 @@ import { paramCase } from 'change-case';
 import * as path from 'path';
 import * as fs from 'fs';
 
-describe('navigation', () => {
-  let browserPage: Page;
-  // const logPages: string[] = [];
-  // const logTabs: string[] = [];
+// describe('navigation', () => {
+let browserPage: Page;
+// const logPages: string[] = [];
+// const logTabs: string[] = [];
 
-  beforeEach(async () => (browserPage = await browser.newPage()));
-  afterEach(async () => await browserPage.close());
+beforeEach(async () => (browserPage = await browser.newPage()));
+afterEach(async () => await browserPage.close());
 
-  const isLinkActive = async (element: ElementHandle | null): Promise<boolean> =>
-    element ? ((await (await element.getProperty('active')).jsonValue()) as boolean) : false;
-  const getClassNames = async (element: ElementHandle | null): Promise<string> =>
-    element ? ((await (await element.getProperty('className')).jsonValue()) as string) : '';
-  const getMainTitle = async (page: Page): Promise<string> => page.$eval('.vmark > h1', (x) => x.innerHTML);
-  const hasPageObjectObject = async (page: Page): Promise<boolean> =>
-    page.evaluate(() => document.body.innerText.includes('[object Object]'));
+const isLinkActive = async (element: ElementHandle | null): Promise<boolean> =>
+  element ? ((await (await element.getProperty('active')).jsonValue()) as boolean) : false;
+const getClassNames = async (element: ElementHandle | null): Promise<string> =>
+  element ? ((await (await element.getProperty('className')).jsonValue()) as string) : '';
+const getMainTitle = async (page: Page): Promise<string> => page.$eval('.vmark > h1', (x) => x.innerHTML);
+const hasPageObjectObject = async (page: Page): Promise<boolean> =>
+  page.evaluate(() => document.body.innerText.includes('[object Object]'));
 
-  const injectCSSOverrides = async () => {
-    const pathToShared = require.resolve('@porsche-design-system/shared');
-    const pathToOverrides = path.resolve(pathToShared, '../css/styles.css');
-    const overrides = fs.readFileSync(pathToOverrides, 'utf8');
+const injectCSSOverrides = async () => {
+  const pathToShared = require.resolve('@porsche-design-system/shared');
+  const pathToOverrides = path.resolve(pathToShared, '../css/styles.css');
+  const overrides = fs.readFileSync(pathToOverrides, 'utf8');
 
-    await browserPage.evaluate((overrides) => {
-      const styleTag = document.createElement('style');
-      styleTag.innerText = overrides;
-      document.getElementsByTagName('head')[0].appendChild(styleTag);
-    }, overrides);
-  };
+  await browserPage.evaluate((overrides) => {
+    const styleTag = document.createElement('style');
+    styleTag.innerText = overrides;
+    document.getElementsByTagName('head')[0].appendChild(styleTag);
+  }, overrides);
+};
 
-  for (const [category, pages] of Object.entries(STOREFRONT_CONFIG)) {
-    for (const [page, tabs] of Object.entries(pages).sort(([a], [b]) => a.localeCompare(b))) {
-      ((category: string, page: string) => {
-        it(`should navigate to "${category} > ${page}"`, async () => {
-          await browserPage.goto(baseURL, { waitUntil: 'networkidle0' });
-          await injectCSSOverrides();
-          await browserPage.waitForSelector('html.hydrated');
+for (const [category, pages] of Object.entries(STOREFRONT_CONFIG)) {
+  for (const [page, tabs] of Object.entries(pages).sort(([a], [b]) => a.localeCompare(b))) {
+    ((category: string, page: string) => {
+      it(`should navigate to "${category} > ${page}"`, async () => {
+        await browserPage.goto(baseURL, { waitUntil: 'networkidle0' });
+        await injectCSSOverrides();
+        await browserPage.waitForSelector('html.hydrated');
 
-          const [accordionButton] = await browserPage.$x(
-            `//div[@class='sidebar']/nav/p-accordion[@heading='${category}']`
-          );
-          const href = `\/${paramCase(category)}\/${paramCase(page)}`;
-          const [linkElement] = await browserPage.$x(
-            `//div[@class='sidebar']/nav//p-link-pure[contains(., '${page}')][@href='${href}']`
-          );
+        const [accordionButton] = await browserPage.$x(
+          `//div[@class='sidebar']/nav/p-accordion[@heading='${category}']`
+        );
+        const href = `\/${paramCase(category)}\/${paramCase(page)}`;
+        const [linkElement] = await browserPage.$x(
+          `//div[@class='sidebar']/nav//p-link-pure[contains(., '${page}')][@href='${href}']`
+        );
 
-          await accordionButton.click();
+        await accordionButton.click();
 
-          expect(await isLinkActive(linkElement))
-            .withContext('link should be inactive initially')
-            .toBe(false);
+        expect(await isLinkActive(linkElement), 'link should be inactive initially').toBe(false);
 
-          await linkElement.click();
-          await browserPage.waitForSelector('.vmark');
+        await linkElement.click();
+        await browserPage.waitForSelector('.vmark');
 
-          expect(await isLinkActive(linkElement))
-            .withContext('link should be active after click')
-            .toBe(true);
-          expect(await getMainTitle(browserPage))
-            .withContext('should show correct main title for page view')
-            .toBe(page);
+        expect(await isLinkActive(linkElement), 'link should be active after click').toBe(true);
+        expect(await getMainTitle(browserPage), 'should show correct main title for page view').toBe(page);
 
-          if (!Array.isArray(tabs)) {
-            for (const [index, tab] of Object.entries(Object.keys(tabs))) {
-              const [tabElement] = await browserPage.$x(
-                `//p-tabs-bar//a[contains(., '${tab}')][@href='\/${paramCase(category)}\/${paramCase(
-                  page
-                )}\/${paramCase(tab)}']`
+        if (!Array.isArray(tabs)) {
+          for (const [index, tab] of Object.entries(Object.keys(tabs))) {
+            const [tabElement] = await browserPage.$x(
+              `//p-tabs-bar//a[contains(., '${tab}')][@href='\/${paramCase(category)}\/${paramCase(page)}\/${paramCase(
+                tab
+              )}']`
+            );
+
+            if (parseInt(index) === 0) {
+              expect(await getClassNames(tabElement), 'should have first tab active initially').toContain(
+                'router-link-active'
               );
-
-              if (parseInt(index) === 0) {
-                expect(await getClassNames(tabElement))
-                  .withContext('should have first tab active initially')
-                  .toContain('router-link-active');
-              } else {
-                expect(await getClassNames(tabElement))
-                  .withContext('should have tab not active initially')
-                  .not.toContain('router-link-active');
-              }
-
-              await tabElement.click();
-              await browserPage.waitForSelector('.vmark');
-
-              expect(await getClassNames(tabElement))
-                .withContext('should have tab active after click')
-                .toContain('router-link-active');
-              expect(await getMainTitle(browserPage))
-                .withContext('should show correct main title for tab view')
-                .toBe(page);
-              expect(await hasPageObjectObject(browserPage))
-                .withContext('should not contain [object Object]')
-                .toBe(false);
-
-              // logTabs.push(`${category} > ${page} > ${tab}`);
+            } else {
+              expect(await getClassNames(tabElement), 'should have tab not active initially').not.toContain(
+                'router-link-active'
+              );
             }
-          }
 
-          // logPages.push(`${category} > ${page}`);
-        });
-      })(category, page);
-    }
+            await tabElement.click();
+            await browserPage.waitForSelector('.vmark');
+
+            expect(await getClassNames(tabElement), 'should have tab active after click').toContain(
+              'router-link-active'
+            );
+            expect(await getMainTitle(browserPage), 'should show correct main title for tab view').toBe(page);
+            expect(await hasPageObjectObject(browserPage), 'should not contain [object Object]').toBe(false);
+
+            // logTabs.push(`${category} > ${page} > ${tab}`);
+          }
+        }
+
+        // logPages.push(`${category} > ${page}`);
+      });
+    })(category, page);
   }
-});
+}
+// });
