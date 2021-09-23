d7f68fced1a485e9476bdb820bd02b9d24559daf
Matthias Haegele <matthias.haegele@mhp.com>
2021-09-23 11:08:50 +0200

    feat(components-react): handle jsdom build replacements with rollup | mh

--- packages/components-react/projects/jsdom-polyfill/rollup.config.js
+++ packages/components-react/projects/jsdom-polyfill/rollup.config.js
@@ -3,6 +3,7 @@ import polyfill from 'rollup-plugin-polyfill';
 import pkg from '@porsche-design-system/js/package.json';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import modify from 'rollup-plugin-modify';
 
 const polyfills = [
   '@juggle/resize-observer',
@@ -25,6 +26,22 @@ export default {
     commonjs({ dynamicRequireTargets: ['projects/jsdom-polyfill/src/**/*.js'] }),
     polyfill(polyfills),
     resolve(),
+    modify({
+      find: /console.warn(`The Porsche Design System had to inject our font-face.css file into your head.(.|s)*?);/,
+      replace: '',
+    }),
+    modify({
+      find: /(appGlobals\.globalScripts\(\);)/,
+      replace: (match) => `if(!window.PDS_SKIP_FETCH) { ${match} }`,
+    }),
+    modify({
+      find: /(const pdsFetch = \(input, init\) =>) (fetch\(input, init\);)/,
+      replace: (match, capGrp1, capGrp2) => `${capGrp1} window.PDS_SKIP_FETCH ? undefined : ${capGrp2}`,
+    }),
+    modify({
+      find: /(const picture =)( \(resizeObserver(?:.|\s)*?;)/,
+      replace: (match, capGrp1, capGrp2) => `${capGrp1} window.PDS_SKIP_FETCH ? undefined : ${capGrp2}`,
+    }),
     terser({
       output: {
         comments: false,
