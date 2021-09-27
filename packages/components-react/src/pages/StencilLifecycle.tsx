f0f2491f09aa7fd39fcb0918318d05e13bff7361
Matthias Haegele <matthias.haegele@mhp.com>
2021-09-27 15:58:35 +0200

    refactor(components-react): improve state naming and rendering on test page | mh / bh / as | #1588

--- packages/components-react/src/pages/StencilLifecycle.tsx
+++ packages/components-react/src/pages/StencilLifecycle.tsx
@@ -2,24 +2,20 @@ import { PLinkPure } from '@porsche-design-system/components-react';
 import { useState } from 'react';
 
 export const StencilLifecyclesPage = (): JSX.Element => {
-  const [key, setKey] = useState({
-    linkPure: 0,
-    headline: false,
-  });
+  const [key, setKey] = useState(0);
 
-  console.log(key.linkPure);
   return (
     <>
       <button
         onClick={() => {
-          setKey(({ linkPure }) => ({ linkPure: linkPure + 1, headline: true }));
+          setKey((prev) => prev + 1);
         }}
       >
         Render Link and Change key on click
       </button>
 
-      {key.headline && (
-        <PLinkPure key={key.linkPure} size="large" href="https://porsche.com">
+      {key > 0 && (
+        <PLinkPure key={key} size="large" href="https://porsche.com">
           Some Link with non default props
         </PLinkPure>
       )}
