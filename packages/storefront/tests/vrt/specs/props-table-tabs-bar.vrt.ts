6c4f4c7c0ef714e8275683717456375dec272a56
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-05 08:11:24 +0200

    test(storefront): change vrts to it.each blocks | bh | #1564

--- packages/storefront/tests/vrt/specs/props-table-tabs-bar.vrt.ts
+++ packages/storefront/tests/vrt/specs/props-table-tabs-bar.vrt.ts
@@ -1,12 +1,10 @@
 import { getVisualRegressionPropTableTester, vrtTest } from '@porsche-design-system/shared/testing';
 import { routerViewSelector } from '../helpers';
 
-describe('Props Table Tabs-Bar', () => {
-  it('should have no visual regression', async () => {
-    expect(
-      await vrtTest(getVisualRegressionPropTableTester(), 'props-table-tabs-bar', '/components/tabs-bar/props', {
-        elementSelector: routerViewSelector,
-      })
-    ).toBeFalsy();
-  });
+it('should have no visual regression', async () => {
+  expect(
+    await vrtTest(getVisualRegressionPropTableTester(), 'props-table-tabs-bar', '/components/tabs-bar/props', {
+      elementSelector: routerViewSelector,
+    })
+  ).toBeFalsy();
 });
