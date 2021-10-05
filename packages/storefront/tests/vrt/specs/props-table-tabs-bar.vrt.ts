ae92067ae6e53ff12940db20ca4b3081aa9cff3b
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-05 07:55:49 +0200

    test(shared/components-js): change vrts to it.each blocks, stop sharing vrt instances | bh | #1564

--- packages/storefront/tests/vrt/specs/props-table-tabs-bar.vrt.ts
+++ packages/storefront/tests/vrt/specs/props-table-tabs-bar.vrt.ts
@@ -1,10 +1,10 @@
-import { getVisualRegressionTesterPropTable, vrtTest } from '@porsche-design-system/shared/testing';
+import { getVisualRegressionPropTableTester, vrtTest } from '@porsche-design-system/shared/testing';
 import { routerViewSelector } from '../helpers';
 
 describe('Props Table Tabs-Bar', () => {
   it('should have no visual regression', async () => {
     expect(
-      await vrtTest(getVisualRegressionTesterPropTable(), 'props-table-tabs-bar', '/components/tabs-bar/props', {
+      await vrtTest(getVisualRegressionPropTableTester(), 'props-table-tabs-bar', '/components/tabs-bar/props', {
         elementSelector: routerViewSelector,
       })
     ).toBeFalsy();
