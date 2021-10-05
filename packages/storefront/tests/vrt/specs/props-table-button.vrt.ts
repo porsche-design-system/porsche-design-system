f0527fe9fd691eb3cc9ff5cc8ba7257feb0c2001
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-05 08:11:24 +0200

    test(storefront): change vrts to it.each blocks | bh | #1564

--- packages/storefront/tests/vrt/specs/props-table-button.vrt.ts
+++ packages/storefront/tests/vrt/specs/props-table-button.vrt.ts
@@ -1,12 +1,10 @@
 import { getVisualRegressionPropTableTester, vrtTest } from '@porsche-design-system/shared/testing';
 import { routerViewSelector } from '../helpers';
 
-describe('Props Table Button', () => {
-  it('should have no visual regression', async () => {
-    expect(
-      await vrtTest(getVisualRegressionPropTableTester(), 'props-table-button', '/components/button/props', {
-        elementSelector: routerViewSelector,
-      })
-    ).toBeFalsy();
-  });
+it('should have no visual regression', async () => {
+  expect(
+    await vrtTest(getVisualRegressionPropTableTester(), 'props-table-button', '/components/button/props', {
+      elementSelector: routerViewSelector,
+    })
+  ).toBeFalsy();
 });
--- packages/storefront/tests/vrt/specs/story.vrt.ts
+++ packages/storefront/tests/vrt/specs/story.vrt.ts
@@ -1,16 +1,14 @@
-import { getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
+import { defaultViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 import { routerViewSelector } from '../helpers';
 
-describe('Story', () => {
-  it('should have no visual regression', async () => {
-    expect(
-      await vrtTest(getVisualRegressionTester(), 'story', '/components/pagination/examples', {
-        elementSelector: routerViewSelector,
-        scenario: async (page) => {
-          await page.click('.playground:nth-of-type(1) > p-tabs-bar > button:nth-of-type(2)');
-          await page.waitForTimeout(500); // it's actual js scrolling and no transition
-        },
-      })
-    ).toBeFalsy();
-  });
+it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
+  expect(
+    await vrtTest(getVisualRegressionTester(viewport), 'story', '/components/pagination/examples', {
+      elementSelector: routerViewSelector,
+      scenario: async (page) => {
+        await page.click('.playground:nth-of-type(1) > p-tabs-bar > button:nth-of-type(2)');
+        await page.waitForTimeout(500); // it's actual js scrolling and no transition
+      },
+    })
+  ).toBeFalsy();
 });
