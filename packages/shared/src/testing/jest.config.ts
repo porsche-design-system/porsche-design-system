e2e9db07e72e86c54b1187fb089bf8bc5c6b69c3
Matthias Haegele <matthias.haegele@mhp.com>
2021-10-06 14:02:44 +0200

    test(shared): remove max worker setting | mh / bh | #1564

--- packages/shared/src/testing/jest.config.ts
+++ packages/shared/src/testing/jest.config.ts
@@ -11,7 +11,6 @@ export const config: Config.InitialOptions = {
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  maxWorkers: 4,
   globals: {
     'ts-jest': {
       isolatedModules: true, // disable type-checking and compile each file as an isolated module
