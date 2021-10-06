e605e7eadef8877eb5bf9c9050e3c884840e29e6
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-06 16:41:43 +0200

    test(components-js): remove describe block from lifecycle-after-disconnect e2e test | bh | #1610

--- packages/components-js/tests/e2e/specs/lifecycle-after-disconnect.e2e.ts
+++ packages/components-js/tests/e2e/specs/lifecycle-after-disconnect.e2e.ts
@@ -1,20 +1,18 @@
 import { getConsoleErrorsAmount, goto, initConsoleObserver } from '../helpers';
 import { Page } from 'puppeteer';
 
-describe('lifecycle-after-disconnect', () => {
-  let page: Page;
-  beforeEach(async () => (page = await browser.newPage()));
-  afterEach(async () => await page.close());
+let page: Page;
+beforeEach(async () => (page = await browser.newPage()));
+afterEach(async () => await page.close());
 
-  it('should not crash after disconnectedCallback', async () => {
-    initConsoleObserver(page);
+fit('should not crash after disconnectedCallback', async () => {
+  initConsoleObserver(page);
 
-    await goto('lifecycle-after-disconnect');
-    await page.waitForTimeout(1500);
+  await goto('lifecycle-after-disconnect');
+  await page.waitForTimeout(1500);
 
-    expect(getConsoleErrorsAmount()).toBe(0);
+  expect(getConsoleErrorsAmount()).toBe(0);
 
-    await page.evaluate(() => console.error('test error'));
-    expect(getConsoleErrorsAmount()).toBe(1);
-  });
+  await page.evaluate(() => console.error('test error'));
+  expect(getConsoleErrorsAmount()).toBe(1);
 });
