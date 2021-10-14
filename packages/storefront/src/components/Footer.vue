6bdc053280c3fc8f84c8fda604d90f3f532f1f9f
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-14 14:14:08 +0200

    feat(storefront): change links for imprint and privacy policy to ux platform | bh | #1599

--- packages/storefront/src/components/Footer.vue
+++ packages/storefront/src/components/Footer.vue
@@ -3,9 +3,9 @@
     <p-text size="x-small" align="center">
       <span>© 2021 Dr. Ing. h.c. F. Porsche AG.</span>
       <br />
-      <a href="https://www.porsche.com/international/legal-notice/">Legal notice</a>
+      <a href="https://ux.porsche.com/privacy-policy">Privacy Policy</a>
       <span> / </span>
-      <a href="https://www.porsche.com/international/legal-notice/">Imprint</a>
+      <a href="https://ux.porsche.com/imprint">Imprint</a>
       <span> / </span>
       <router-link :to="`/license`">License</router-link>
     </p-text>
