6a50dd2c9354f9d2b5d7150876ac7629e99375e6
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-20 11:21:02 +0200

    test(components): add banner-inline-styles unit test and adjust banner-styles unit test | bh | #1571

---
+++ packages/components/src/components/feedback/banner-inline/banner-inline-styles.spec.ts
@@ -0,0 +1,29 @@
+import { getComponentCss, getSlottedCss } from './banner-inline-styles';
+import type { BannerState, Theme } from '../../../types';
+
+describe('getComponentCss()', () => {
+  it.each<[BannerState, Theme]>([
+    ['neutral', 'light'],
+    ['warning', 'light'],
+    ['success', 'light'],
+    ['error', 'light'],
+    ['neutral', 'dark'],
+    ['warning', 'dark'],
+    ['success', 'dark'],
+    ['error', 'dark'],
+  ])('should return correct css for state: %s and theme: %s', (state, theme) => {
+    expect(getComponentCss(state, theme)).toMatchSnapshot();
+  });
+});
+
+describe('getSlottedCss()', () => {
+  it('should return correct css', () => {
+    const host = document.createElement('p-banner-inline');
+    expect(getSlottedCss(host)).toMatchSnapshot();
+  });
+
+  it('should return correct css with prefix', () => {
+    const host = document.createElement('prefixed-p-banner-inline');
+    expect(getSlottedCss(host)).toMatchSnapshot();
+  });
+});
