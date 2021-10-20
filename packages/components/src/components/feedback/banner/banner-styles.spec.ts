4a72aa03fffe984384001b18032ef315c92289f1
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-20 11:21:02 +0200

    test(components): add banner-inline-styles unit test and adjust banner-styles unit test | bh | #1571

--- packages/components/src/components/feedback/banner/banner-styles.spec.ts
+++ packages/components/src/components/feedback/banner/banner-styles.spec.ts
@@ -1,29 +1,7 @@
-import { getComponentCss, getSlottedCss } from './banner-styles';
-import type { BannerState, Theme } from '../../../types';
+import { getComponentCss } from './banner-styles';
 
 describe('getComponentCss()', () => {
-  it.each<[BannerState, Theme]>([
-    ['neutral', 'light'],
-    ['warning', 'light'],
-    ['success', 'light'],
-    ['error', 'light'],
-    ['neutral', 'dark'],
-    ['warning', 'dark'],
-    ['success', 'dark'],
-    ['error', 'dark'],
-  ])('should return correct css for state: %s and theme: %s', (state, theme) => {
-    expect(getComponentCss(state, theme)).toMatchSnapshot();
-  });
-});
-
-describe('getSlottedCss()', () => {
   it('should return correct css', () => {
-    const host = document.createElement('p-banner');
-    expect(getSlottedCss(host)).toMatchSnapshot();
-  });
-
-  it('should return correct css with prefix', () => {
-    const host = document.createElement('prefixed-p-banner');
-    expect(getSlottedCss(host)).toMatchSnapshot();
+    expect(getComponentCss()).toMatchSnapshot();
   });
 });
