614e9878d90f46d9e0c0655be365d73c7515a651
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-20 09:06:31 +0200

    feat(components): add raw banner-inline component | bh | #1571

---
+++ packages/components/src/components/feedback/banner-inline/banner-inline-styles.ts
@@ -0,0 +1,47 @@
+import {
+  addImportantToRule,
+  breakpoint,
+  buildSlottedStyles,
+  getBaseSlottedStyles,
+  getCss,
+  mediaQuery,
+} from '../../../utils';
+
+const easeOutQuad = 'cubic-bezier(0.5, 1, 0.89, 1)';
+
+export const getComponentCss = (): string => {
+  return getCss({
+    '@keyframes animateMobileOut': {
+      from: {
+        opacity: 1,
+        transform: 'translate3d(0,0,0)',
+      },
+      to: {
+        opacity: 0,
+        transform: 'translate3d(0, calc(var(--p-banner-position-bottom) + 100%), 0)',
+      },
+    },
+    '@keyframes animateDesktopOut': {
+      from: {
+        opacity: 1,
+        transform: 'translate3d(0,0,0)',
+      },
+      to: {
+        opacity: 0,
+        transform: 'translate3d(0, calc(-100% - var(--p-banner-position-bottom)), 0)',
+      },
+    },
+    ':host(.banner--close)': {
+      [`${mediaQuery('xxs')} and (max-width: ${breakpoint.s}px)`]: {
+        animation: addImportantToRule(`600ms $animateMobileOut ${easeOutQuad} forwards`),
+      },
+      [mediaQuery('s')]: {
+        animation: addImportantToRule(`600ms $animateDesktopOut ${easeOutQuad} forwards`),
+      },
+    },
+  });
+};
+
+export const getSlottedCss = (host: HTMLElement): string => {
+  return getCss(buildSlottedStyles(host, getBaseSlottedStyles()));
+};
---
+++ packages/components/src/components/feedback/banner-inline/banner-inline.tsx
@@ -0,0 +1,126 @@
+import { JSX, Component, Prop, h, Element, Event, EventEmitter, Host } from '@stencil/core';
+import {
+  getPrefixedTagNames,
+  isDark,
+  getThemeDarkAttribute,
+  attachComponentCss,
+  attachSlottedCss,
+  hasHeading,
+} from '../../../utils';
+import type { BannerState, Theme } from '../../../types';
+import { getComponentCss, getSlottedCss } from './banner-inline-styles';
+
+@Component({
+  tag: 'p-banner-inline',
+  shadow: true,
+})
+export class BannerInline {
+  @Element() public host!: HTMLElement;
+
+  /** Heading of the banner. */
+  @Prop() public heading?: string = '';
+
+  /** Description of the banner. */
+  @Prop() public description?: string = '';
+
+  /** State of the banner. */
+  @Prop() public state?: BannerState = 'neutral';
+
+  /** Defines if the banner can be closed/removed by the user. */
+  @Prop() public persistent?: boolean = false;
+
+  /** Defines the width of the banner corresponding to the `content-wrapper` dimensions */
+  @Prop() public width?: 'basic' | 'extended' | 'fluid' = 'basic';
+
+  /** Adapts the banner color depending on the theme. */
+  @Prop() public theme?: Theme = 'light';
+
+  /** Emitted when the close button is clicked. */
+  @Event({ bubbles: false }) public dismiss?: EventEmitter<void>;
+
+  private closeButton: HTMLButtonElement;
+
+  public connectedCallback(): void {
+    attachComponentCss(this.host, getComponentCss);
+    attachSlottedCss(this.host, getSlottedCss);
+    if (!this.persistent) {
+      document.addEventListener('keydown', this.onKeyboardEvent);
+    }
+  }
+
+  public componentDidLoad(): void {
+    if (!this.persistent) {
+      this.closeButton.focus();
+    }
+  }
+
+  public disconnectedCallback(): void {
+    if (!this.persistent) {
+      document.removeEventListener('keydown', this.onKeyboardEvent);
+    }
+  }
+
+  public render(): JSX.Element {
+    const rootClasses = {
+      ['root']: true,
+      [`root--${this.state}`]: this.state !== 'neutral',
+      ['root--theme-dark']: isDark(this.theme),
+    };
+
+    const bannerLabelId = 'banner-label';
+    const bannerDescriptionId = 'banner-description';
+    const PrefixedTagNames = getPrefixedTagNames(this.host);
+
+    return (
+      <Host {...getThemeDarkAttribute(this.theme)}>
+        <PrefixedTagNames.pContentWrapper
+          width={this.width}
+          role="alertdialog"
+          aria-labelledby={bannerLabelId}
+          aria-describedby={bannerDescriptionId}
+        >
+          <div class={rootClasses}>
+            {this.state !== 'neutral' && (
+              <PrefixedTagNames.pIcon name={this.state === 'error' ? 'exclamation' : 'warning'} class="icon" />
+            )}
+            <div class="content">
+              {hasHeading(this.host, this.heading) && (
+                <PrefixedTagNames.pHeadline variant="headline-5" id={bannerLabelId}>
+                  {this.heading || <slot name="heading" />}
+                </PrefixedTagNames.pHeadline>
+              )}
+              {/*{hasNamedSlot(this.host, 'description') && (*/}
+              <PrefixedTagNames.pText id={bannerDescriptionId}>
+                <slot />
+              </PrefixedTagNames.pText>
+              {/*)}*/}
+              {!this.persistent && (
+                <div class="close">
+                  <PrefixedTagNames.pButtonPure
+                    type="button"
+                    icon="close"
+                    hideLabel={true}
+                    onClick={this.removeBanner}
+                    ref={(el) => (this.closeButton = el)}
+                  >
+                    Close notification
+                  </PrefixedTagNames.pButtonPure>
+                </div>
+              )}
+            </div>
+          </div>
+        </PrefixedTagNames.pContentWrapper>
+      </Host>
+    );
+  }
+
+  private onKeyboardEvent = ({ key }: KeyboardEvent): void => {
+    if (key === 'Esc' || key === 'Escape') {
+      this.dismiss.emit();
+    }
+  };
+
+  private removeBanner = (): void => {
+    this.dismiss.emit();
+  };
+}
