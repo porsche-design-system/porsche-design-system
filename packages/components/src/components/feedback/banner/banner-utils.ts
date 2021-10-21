6a9f8ac8f67079ebfdb20b8134b1bedeef361bc2
Bernd Hacker <hacker.bernd@gmail.com>
2021-10-21 15:16:58 +0200

    refactor(components): import getSlottedCss in banner from its utils, bring back unit test | bh | #1571

--- packages/components/src/components/feedback/banner/banner-utils.ts
+++ packages/components/src/components/feedback/banner/banner-utils.ts
@@ -1,8 +1,2 @@
-import { buildSlottedStyles, getBaseSlottedStyles, getCss } from '../../../utils';
-
 export const BANNER_STATES = ['error', 'warning', 'neutral'] as const;
 export type BannerState = typeof BANNER_STATES[number];
-
-export const getSlottedCss = (host: HTMLElement): string => {
-  return getCss(buildSlottedStyles(host, getBaseSlottedStyles()));
-};
