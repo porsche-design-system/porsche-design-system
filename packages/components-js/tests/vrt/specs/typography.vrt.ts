5a26bbcf1e67985958a6935ba1e1253d5465df25
Stephan Schröter <stephan.schroeter@mhp.com>
2021-10-22 17:00:27 +0200

    test: split typography vrt tests for components-angular | sas

--- packages/components-js/tests/vrt/specs/typography.vrt.ts
+++ packages/components-js/tests/vrt/specs/typography.vrt.ts
@@ -1,5 +1,37 @@
 import { getVisualRegressionOverviewTester, vrtTest } from '@porsche-design-system/shared/testing';
 
 it('should have no visual regression', async () => {
-  expect(await vrtTest(getVisualRegressionOverviewTester(), 'typography', '/#typography')).toBeFalsy();
+  expect(
+    await vrtTest(getVisualRegressionOverviewTester(), 'typography-fallback-strategy', '/#typography-fallback-strategy')
+  ).toBeFalsy();
+});
+
+it('should have no visual regression', async () => {
+  expect(
+    await vrtTest(
+      getVisualRegressionOverviewTester(),
+      'typography-porsche-next-latin',
+      '/#typography-porsche-next-latin'
+    )
+  ).toBeFalsy();
+});
+
+it('should have no visual regression', async () => {
+  expect(
+    await vrtTest(
+      getVisualRegressionOverviewTester(),
+      'typography-porsche-next-greek-and-coptic',
+      '/#typography-porsche-next-greek-and-coptic'
+    )
+  ).toBeFalsy();
+});
+
+it('should have no visual regression', async () => {
+  expect(
+    await vrtTest(
+      getVisualRegressionOverviewTester(),
+      'typography-porsche-next-cyril',
+      '/#typography-porsche-next-cyril'
+    )
+  ).toBeFalsy();
 });
