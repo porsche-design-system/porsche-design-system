7b621629f02dec521e79b94503c88874ff70f6c6
Matthias Haegele <matthias.haegele@mhp.com>
2021-10-26 14:11:02 +0200

    test(components-js/angular/react): update fixtures, align frameworks | mh / as | #1540

--- packages/components-angular/src/app/pages/text-field-wrapper.component.ts
+++ packages/components-angular/src/app/pages/text-field-wrapper.component.ts
@@ -4,19 +4,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'page-text-field-wrapper',
   template: `
     <div class="playground light" title="should render with label">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label default'">
         <input type="text" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with label and placeholder">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label with placeholder'">
         <input type="text" name="some-name" placeholder="Some placeholder" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with label, description and placeholder">
-      <p-text-field-wrapper [label]="'Some label'" [description]="'Some description'">
+      <p-text-field-wrapper [label]="'Label with description'" [description]="'Some description'">
         <input type="text" name="some-name" placeholder="Some placeholder" />
       </p-text-field-wrapper>
     </div>
@@ -29,7 +29,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
     <div class="playground light" title="should render with responsive label and description">
       <p-text-field-wrapper
-        [label]="'Some label'"
+        [label]="'Label responsive'"
         [description]="'Some description'"
         [hideLabel]="{ base: true, xs: false, s: true, m: false, l: true, xl: false }"
       >
@@ -38,94 +38,94 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     </div>
 
     <div class="playground light" title="should render in required state">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label required'">
         <input type="text" name="some-name" required />
       </p-text-field-wrapper>
-      <p-text-field-wrapper [label]="'This is a very insanely super long label across multiple lines'">
+      <p-text-field-wrapper [label]="'This is a very insanely super long required label across multiple lines'">
         <input type="text" name="some-name" required />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render in disabled state">
-      <p-text-field-wrapper [label]="'Some label'" [description]="'Some description'">
+      <p-text-field-wrapper [label]="'Label disabled'" [description]="'Some description'">
         <input type="text" name="some-name" disabled />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with placeholder in disabled state">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label disabled with placeholder'">
         <input type="text" name="some-name" placeholder="Some placeholder" disabled />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with readonly state">
-      <p-text-field-wrapper [label]="'Some label'" [description]="'Some description'">
+      <p-text-field-wrapper [label]="'Label readonly with description'" [description]="'Some description'">
         <input type="text" name="some-name" value="Some value" readonly />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type number">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type number'">
         <input type="number" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type email">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type email'">
         <input type="email" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type tel">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type tel'">
         <input type="tel" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type url">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type url'">
         <input type="url" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type date">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type date'">
         <input type="date" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type time">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type time'">
         <input type="time" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type month">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type month'">
         <input type="month" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type week">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type week'">
         <input type="week" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with type password in different states">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type password'">
         <input type="password" name="some-name" value="some password" />
       </p-text-field-wrapper>
       <br />
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type password disabled'">
         <input type="password" name="some-name" value="some password" disabled />
       </p-text-field-wrapper>
       <br />
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type password readonly'">
         <input type="password" name="some-name" value="some password" readonly />
       </p-text-field-wrapper>
       <br />
-      <p-text-field-wrapper [label]="'Some label'" style="width: 240px;">
+      <p-text-field-wrapper [label]="'Label type password'" style="width: 240px;">
         <input
           type="password"
           name="some-name"
@@ -135,45 +135,53 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     </div>
 
     <div class="playground light" title="should render with type search in different states">
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type search'">
         <input type="search" name="some-name" />
       </p-text-field-wrapper>
       <br />
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type search disabled'">
         <input type="search" name="some-name" disabled />
       </p-text-field-wrapper>
       <br />
-      <p-text-field-wrapper [label]="'Some label'">
+      <p-text-field-wrapper [label]="'Label type search readonly'">
         <input type="search" name="some-name" readonly />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with error state and error message">
-      <p-text-field-wrapper [label]="'Some label'" [state]="'error'" [message]="'error message'">
+      <p-text-field-wrapper [label]="'Label state error with message'" [state]="'error'" [message]="'error message'">
         <input type="text" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with error state and no error message">
-      <p-text-field-wrapper [label]="'Some label'" [state]="'error'">
+      <p-text-field-wrapper [label]="'Label state error without message'" [state]="'error'">
         <input type="text" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with success state and success message">
-      <p-text-field-wrapper [label]="'Some label'" [state]="'success'" [message]="'success message'">
+      <p-text-field-wrapper
+        [label]="'Label state success with message'"
+        [state]="'success'"
+        [message]="'success message'"
+      >
         <input type="text" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with success state and no success message">
-      <p-text-field-wrapper [label]="'Some label'" [state]="'success'">
+      <p-text-field-wrapper [label]="'Label state success without message'" [state]="'success'">
         <input type="text" name="some-name" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with default state and no message">
-      <p-text-field-wrapper [label]="'Some label'" [state]="'none'" [message]="'this message should be hidden'">
+      <p-text-field-wrapper
+        [label]="'Label default state no message'"
+        [state]="'none'"
+        [message]="'this message should be hidden'"
+      >
         <input type="text" name="some-name" />
       </p-text-field-wrapper>
     </div>
@@ -183,7 +191,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       title="should render label, description and message by slotted content with error state"
     >
       <p-text-field-wrapper [state]="'error'">
-        <span slot="label">Some label with a <a href="https://designsystem.porsche.com">link</a>.</span>
+        <span slot="label">Label slotted with a <a href="https://designsystem.porsche.com">link</a>.</span>
         <span slot="description">Some description with a <a href="https://designsystem.porsche.com">link</a>.</span>
         <input type="text" name="some-name" placeholder="Some placeholder" />
         <span slot="message">Some error message with a <a href="https://designsystem.porsche.com">link</a>.</span>
@@ -195,7 +203,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       title="should render label, description and message by slotted content with success state"
     >
       <p-text-field-wrapper [state]="'success'">
-        <span slot="label">Some label with a <a href="https://designsystem.porsche.com">link</a>.</span>
+        <span slot="label">Label slotted with a <a href="https://designsystem.porsche.com">link</a>.</span>
         <span slot="description">Some description with a <a href="https://designsystem.porsche.com">link</a>.</span>
         <input type="text" name="some-name" placeholder="Some placeholder" />
         <span slot="message">Some success message with a <a href="https://designsystem.porsche.com">link</a>.</span>
@@ -218,19 +226,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     </div>
 
     <div class="playground light" title="should render with unit">
-      <p-text-field-wrapper [label]="'With unit'" [unit]="'km/h'">
+      <p-text-field-wrapper [label]="'Label with unit'" [unit]="'km/h'">
         <input type="number" name="some-name" value="300" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render with unit position suffix">
-      <p-text-field-wrapper [label]="'With unit position suffix'" [unit]="'kWh'" [unitPosition]="'suffix'">
+      <p-text-field-wrapper [label]="'Label with unit position suffix'" [unit]="'kWh'" [unitPosition]="'suffix'">
         <input type="number" name="some-name" value="400" />
       </p-text-field-wrapper>
     </div>
 
     <div class="playground light" title="should render a maximum of five characters per unit">
-      <p-text-field-wrapper [label]="'With long unit cut to 5 chars'" [unit]="'kg/m³55555'">
+      <p-text-field-wrapper [label]="'Label with long unit'" [unit]="'kg/m³55555'">
         <input type="number" name="some-name" value="7777" />
       </p-text-field-wrapper>
     </div>
