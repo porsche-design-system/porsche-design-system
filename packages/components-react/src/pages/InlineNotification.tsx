70b6a2f98eb93532c49134fc08d9411dfaacb42d
Stephan Schröter <stephan.schroeter@mhp.com>
2021-10-27 13:48:15 +0200

    test(components-react): fix vrt | sas | #1571

--- packages/components-react/src/pages/InlineNotification.tsx
+++ packages/components-react/src/pages/InlineNotification.tsx
@@ -4,69 +4,45 @@ export const InlineNotificationPage = (): JSX.Element => {
   return (
     <>
       <div className="playground light" title="should show inline-notification neutral on light background">
-        <PInlineNotification
-          heading="Some inline-notification heading"
-          description="Some inline-notification description."
-        />
+        <PInlineNotification heading="Some neutral heading" description="Some description" />
       </div>
 
       <div className="playground dark" title="should show inline-notification neutral on dark background">
-        <PInlineNotification
-          heading="Some inline-notification heading"
-          description="Some inline-notification description."
-          theme="dark"
-        />
+        <PInlineNotification heading="Some neutral heading" description="Some description" theme="dark" />
       </div>
 
       <div className="playground light" title="should show inline-notification warning on light background">
-        <PInlineNotification
-          heading="Some warning inline-notification heading"
-          description="Some inline-notification description."
-          state="warning"
-        />
+        <PInlineNotification heading="Some warning heading" description="Some description" state="warning" />
       </div>
 
       <div className="playground dark" title="should show inline-notification warning on dark background">
         <PInlineNotification
-          heading="Some warning inline-notification heading"
-          description="Some inline-notification description."
+          heading="Some warning heading"
+          description="Some description"
           state="warning"
           theme="dark"
         />
       </div>
 
       <div className="playground light" title="should show inline-notification success on light background">
-        <PInlineNotification
-          heading="Some success inline-notification heading"
-          description="Some inline-notification description."
-          state="success"
-        />
+        <PInlineNotification heading="Some success heading" description="Some description" state="success" />
       </div>
 
       <div className="playground dark" title="should show inline-notification success on dark background">
         <PInlineNotification
-          heading="Some success inline-notification heading"
-          description="Some inline-notification description."
+          heading="Some success heading"
+          description="Some description"
           state="success"
           theme="dark"
         />
       </div>
 
       <div className="playground light" title="should show inline-notification error on light background">
-        <PInlineNotification
-          heading="Some error inline-notification heading"
-          description="Some inline-notification description."
-          state="error"
-        />
+        <PInlineNotification heading="Some error heading" description="Some description" state="error" />
       </div>
 
       <div className="playground dark" title="should show inline-notification error on dark background">
-        <PInlineNotification
-          heading="Some error inline-notification heading"
-          description="Some inline-notification description."
-          state="error"
-          theme="dark"
-        />
+        <PInlineNotification heading="Some error heading" description="Some description" state="error" theme="dark" />
       </div>
 
       <div
@@ -74,50 +50,55 @@ export const InlineNotificationPage = (): JSX.Element => {
         title="should show inline-notification with slotted content on light background"
       >
         <PInlineNotification>
-          <span slot="heading">Some slotted inline-notification heading</span>
-          Some slotted inline-notification description. And some <a href="https://www.porsche.com/">LINK</a> element.
+          <span slot="heading">Some slotted heading</span>
+          Some slotted description with a <a href="https://www.porsche.com/">LINK</a> element.
         </PInlineNotification>
       </div>
 
       <div className="playground dark" title="should show inline-notification with slotted content on dark background">
         <PInlineNotification theme="dark">
-          <span slot="heading">Some slotted inline-notification heading</span>
-          Some slotted inline-notification description. And some <a href="https://www.porsche.com/">LINK</a> element.
+          <span slot="heading">Some slotted heading</span>
+          Some slotted description with a <a href="https://www.porsche.com/">LINK</a> element.
         </PInlineNotification>
       </div>
 
       <div className="playground light" title="should show inline-notification with action button">
         <PInlineNotification
-          heading="Some action button inline-notification heading"
-          description="Some inline-notification description."
-          actionLabel="Some action"
-          actionIcon="arrow-double-right"
+          heading="Some heading with action button"
+          description="Some description"
+          actionLabel="Some action label"
         />
       </div>
 
       <div className="playground light" title="should show inline-notification with loading action button">
         <PInlineNotification
-          heading="Some action button inline-notification heading"
-          description="Some inline-notification description."
-          actionLabel="Some loading action"
+          heading="Some heading with action button"
+          description="Some description"
+          actionLabel="Some loading action label"
           actionLoading
         />
       </div>
 
       <div className="playground light" title="should show inline-notification in persistent mode">
+        <PInlineNotification heading="Some persistent heading" description="Some description" persistent />
+      </div>
+
+      <div className="playground light" title="should show inline-notification in persistent mode with action button">
         <PInlineNotification
-          heading="Some persistent inline-notification heading"
-          description="Some inline-notification description."
+          heading="Some persistent heading with action button"
+          description="Some description"
           persistent
+          actionLabel="Some action label with custom icon"
+          actionIcon="refresh"
         />
       </div>
 
       <div className="playground light" title="should show inline-notification in persistent mode with action button">
         <PInlineNotification
-          heading="Some persistent inline-notification heading"
-          description="Some inline-notification description."
-          persistent
-          actionLabel="Some action"
+          style={{ width: '240px' }}
+          heading="Some heading with a very long text across multiple lines"
+          description="Some description with a very long text across multiple lines"
+          actionLabel="Some action label with custom icon"
         />
       </div>
     </>
