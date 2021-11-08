18a43232f2b57f05653842561133e97f21cf1f05
Bernd Hacker <hacker.bernd@gmail.com>
2021-11-08 16:41:15 +0100

    feat(components-react): add useToastManager hook, cleanup examples | bh / as | #1500

--- packages/components-react/src/examples/ToastExample.tsx
+++ packages/components-react/src/examples/ToastExample.tsx
@@ -1,33 +1,26 @@
-import { PToastC, PToastItemC } from '@porsche-design-system/components-react';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef } from 'react';
+import { useToastManager, PToast } from '@porsche-design-system/components-react';
 
 export const ToastExamplePage = (): JSX.Element => {
-  const [toasts, setToasts] = useState([
-    { toastId: '1', message: 'asd1' },
-    { toastId: '2', message: 'asd2' },
-  ]);
+  const { addMessage } = useToastManager();
 
   useEffect(() => {
+    // @ts-ignore
+    addMessage({ message: 'asd', state: 'success' });
+    // @ts-ignore
+    addMessage({ message: 'asd2', state: 'success' });
     setTimeout(() => {
-      setToasts(() => [{ toastId: '3', message: 'asd3' }]);
+      // @ts-ignore
+      addMessage({ message: 'asd3', state: 'success' });
     }, 10000);
   }, []);
 
   return (
     <>
-      <PToastC>
-        {toasts.map((toast) => (
-          <PToastItemC
-            key={toast.toastId}
-            // @ts-ignore
-            onClose={(e) => {
-              // @ts-ignore
-              setToasts((toasts) => toasts.filter((toast) => toast.toastId !== e.target.toastId));
-            }}
-            {...toast}
-          />
-        ))}
-      </PToastC>
+      <button onClick={() => addMessage({ message: 'some message ' + new Date().toISOString(), state: 'success' })}>
+        Add Toast
+      </button>
+      <PToast />
     </>
   );
 };
