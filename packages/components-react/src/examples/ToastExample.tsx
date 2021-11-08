f4f990a6fbb105ddf9d1b89cf0b8b62a918b7a9c
Bernd Hacker <hacker.bernd@gmail.com>
2021-11-08 17:00:44 +0100

    feat(components-react): cleanup ToastExample | bh / as | #1500

--- packages/components-react/src/examples/ToastExample.tsx
+++ packages/components-react/src/examples/ToastExample.tsx
@@ -1,16 +1,13 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useToastManager, PToast } from '@porsche-design-system/components-react';
 
 export const ToastExamplePage = (): JSX.Element => {
   const { addMessage } = useToastManager();
 
   useEffect(() => {
-    // @ts-ignore
     addMessage({ message: 'asd', state: 'success' });
-    // @ts-ignore
     addMessage({ message: 'asd2', state: 'success' });
     setTimeout(() => {
-      // @ts-ignore
       addMessage({ message: 'asd3', state: 'success' });
     }, 10000);
   }, []);
