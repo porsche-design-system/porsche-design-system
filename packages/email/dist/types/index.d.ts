d6e0102abb8c25ad5d1f6ff68c991ccfa29de39f
Anton Schmidt <anton.schmidt@mhp.com>
2021-11-09 15:36:42 +0100

    feat(email): remove email dist folder | as / bh  | #1500

--- packages/email/dist/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare global {
-    interface Window {
-        PORSCHE_DESIGN_SYSTEM_CDN: 'auto' | 'cn';
-    }
-}
-export declare const CDN_BASE_URL: string;
-export declare const EMAIL_MANIFEST: {
-    porscheMarque: string;
-};
