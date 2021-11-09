8d2f2ce62acea0f1426807b8598767ad8622fe04
Bernd Hacker <hacker.bernd@gmail.com>
2021-11-09 10:28:07 +0100

    feat(components-angular): add injectable ToastManager with example page | bh | #1500

--- packages/components-angular/projects/components-wrapper/src/public-api.ts
+++ packages/components-angular/projects/components-wrapper/src/public-api.ts
@@ -2,3 +2,4 @@ export { componentsReady } from '@porsche-design-system/components-js';
 export * from './lib/components/barrel';
 export * from './lib/types';
 export { PorscheDesignSystemModule, PorscheDesignSystemModuleConfig } from './components-wrapper.module';
+export { ToastManager } from './toast-manager';
