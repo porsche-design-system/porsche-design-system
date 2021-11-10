79d8701d8256654177de3caba8f80e0c2a125c34
Matthias Haegele <matthias.haegele@mhp.com>
2021-11-10 13:11:10 +0100

    feat(components): implement flexible popover position | mh / sas | #786

---
+++ packages/components/src/components/feedback/popover/popover-styles.ts
@@ -0,0 +1,72 @@
+import { addImportantToEachRule, buildHostStyles, getCss } from '../../../utils';
+
+const getFlyoutPosition = (x: string, y: string) => {
+  const direction = x + y;
+
+  switch (direction) {
+    case 'rightbottom':
+      return {
+        top: 0,
+        left: '2rem',
+      };
+    case 'rightcenter':
+      return {
+        top: '50%',
+        left: '2rem',
+        transform: 'translateY(-50%)',
+      };
+    case 'righttop':
+      return {
+        bottom: 0,
+        left: '2rem',
+      };
+    case 'centerbottom':
+    case 'centercenter':
+      return {
+        top: '2rem',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      };
+    case 'centertop':
+      return {
+        bottom: '2rem',
+        left: '50%',
+        transform: 'translateX(-50%)',
+      };
+    case 'leftbottom':
+      return {
+        top: 0,
+        right: '2rem',
+      };
+    case 'leftcenter':
+      return {
+        top: '50%',
+        right: '2rem',
+        transform: 'translateY(-50%)',
+      };
+    case 'lefttop':
+      return {
+        bottom: 0,
+        right: '2rem',
+      };
+  }
+};
+
+export const getComponentCss = (directionX, directionY): string => {
+  console.log(directionX, directionY);
+  return getCss({
+    ...buildHostStyles(
+      addImportantToEachRule({
+        position: 'relative',
+        display: 'inline-block',
+        background: 'deepskyblue',
+      })
+    ),
+    flyout: {
+      position: 'absolute',
+      padding: '.5rem 1rem',
+      boxShadow: '0 0 10px 0 rgba(0,0,0,1)',
+      ...getFlyoutPosition(directionX, directionY),
+    },
+  });
+};
