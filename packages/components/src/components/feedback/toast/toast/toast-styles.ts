532d68f328ea56b303e9967a5764b0bd1ed28aee
Anton Schmidt <anton.schmidt@mhp.com>
2021-11-10 14:11:12 +0100

    feat(components): add debugging progress styles | as | #1500

--- packages/components/src/components/feedback/toast/toast/toast-styles.ts
+++ packages/components/src/components/feedback/toast/toast/toast-styles.ts
@@ -3,20 +3,35 @@ import {
   attachComponentCss,
   buildHostStyles,
   getCss,
+  getThemedColors,
   pxToRemWithUnit,
 } from '../../../../utils';
 import type { ToastOffset } from './toast-utils';
+import { TOAST_DEFAULT_TIMEOUT } from './toast-manager';
 
 export const getComponentCss = (offset: ToastOffset): string => {
-  return getCss(
-    buildHostStyles(
+  return getCss({
+    ...buildHostStyles(
       addImportantToEachRule({
         position: 'fixed',
         bottom: pxToRemWithUnit(offset.bottom),
         left: pxToRemWithUnit(8),
       })
-    )
-  );
+    ),
+    progress: {
+      animationName: '$progress',
+      animationDuration: `${TOAST_DEFAULT_TIMEOUT}ms`,
+      animationTimingFunction: 'linear',
+      animationFillMode: 'forwards',
+      display: 'block',
+      background: 'hotpink',
+      height: '.5rem',
+    },
+    '@keyframes progress': {
+      from: { width: '100%' },
+      to: { width: 0 },
+    },
+  });
 };
 
 export const addComponentCss = (host: HTMLElement, offset: ToastOffset): void => {
