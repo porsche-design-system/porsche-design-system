e68f3a3970d7f579fb281bf5b70b983fcd7518ac
Anton Schmidt <anton.schmidt@mhp.com>
2021-11-10 10:32:06 +0100

    feat(components/angular/react): update ToastManagerType, use ToastState import instead of redefining it | as | #1500

--- packages/components-angular/projects/components-wrapper/src/toast-manager.ts
+++ packages/components-angular/projects/components-wrapper/src/toast-manager.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { componentsReady, ToastManagerInstance, ToastMessage } from './public-api';
+import { componentsReady, ToastManager as ToastManagerType, ToastMessage } from './public-api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ToastManager {
   public addToast(msg: ToastMessage): void {
-    const toast: HTMLElement & { getManager(): Promise<ToastManagerInstance> } = document.querySelector('p-toast');
+    const toast: HTMLElement & { getManager(): Promise<ToastManagerType> } = document.querySelector('p-toast');
     componentsReady(toast.parentElement).then(() => {
       toast.getManager().then((manager) => manager.addToast(msg));
     });
