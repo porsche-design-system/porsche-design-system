e0a72975ce403bcdb65ac360ab24bbae6d0dca07
Stephan Schröter <stephan.schroeter@mhp.com>
2021-11-11 17:29:37 +0100

    feat: auto positioning of popover | mh / sas | #786

--- packages/components/src/components/feedback/popover/popover-styles.ts
+++ packages/components/src/components/feedback/popover/popover-styles.ts
@@ -1,72 +1,76 @@
 import { addImportantToEachRule, buildHostStyles, getCss } from '../../../utils';
-import { HorizontalDirection, VerticalDirection } from './popover-utils';
 
-const getFlyoutPosition = (x: HorizontalDirection, y: VerticalDirection) => {
-  const direction = x + y;
+// const getPopoverPosition = (x: HorizontalDirection, y: VerticalDirection) => {
+//   const direction = x + y;
+//
+//   switch (direction) {
+//     case 'rightbottom':
+//       return {
+//         top: 0,
+//         left: '2rem',
+//       };
+//     case 'rightcenter':
+//       return {
+//         top: '50%',
+//         left: '2rem',
+//         transform: 'translateY(-50%)',
+//       };
+//     case 'righttop':
+//       return {
+//         bottom: 0,
+//         left: '2rem',
+//       };
+//     case 'centerbottom':
+//     case 'centercenter':
+//       return {
+//         top: '2rem',
+//         left: '50%',
+//         transform: 'translateX(-50%)',
+//       };
+//     case 'centertop':
+//       return {
+//         bottom: '2rem',
+//         left: '50%',
+//         transform: 'translateX(-50%)',
+//       };
+//     case 'leftbottom':
+//       return {
+//         top: 0,
+//         right: '2rem',
+//       };
+//     case 'leftcenter':
+//       return {
+//         top: '50%',
+//         right: '2rem',
+//         transform: 'translateY(-50%)',
+//       };
+//     case 'lefttop':
+//       return {
+//         bottom: 0,
+//         right: '2rem',
+//       };
+//   }
+// };
 
-  switch (direction) {
-    case 'rightbottom':
-      return {
-        top: 0,
-        left: '2rem',
-      };
-    case 'rightcenter':
-      return {
-        top: '50%',
-        left: '2rem',
-        transform: 'translateY(-50%)',
-      };
-    case 'righttop':
-      return {
-        bottom: 0,
-        left: '2rem',
-      };
-    case 'centerbottom':
-    case 'centercenter':
-      return {
-        top: '2rem',
-        left: '50%',
-        transform: 'translateX(-50%)',
-      };
-    case 'centertop':
-      return {
-        bottom: '2rem',
-        left: '50%',
-        transform: 'translateX(-50%)',
-      };
-    case 'leftbottom':
-      return {
-        top: 0,
-        right: '2rem',
-      };
-    case 'leftcenter':
-      return {
-        top: '50%',
-        right: '2rem',
-        transform: 'translateY(-50%)',
-      };
-    case 'lefttop':
-      return {
-        bottom: 0,
-        right: '2rem',
-      };
-  }
-};
-
-export const getComponentCss = (directionX, directionY): string => {
+export const getComponentCss = (): string => {
   return getCss({
-    ...buildHostStyles(
-      addImportantToEachRule({
+    ...buildHostStyles({
+      verticalAlign: 'top',
+      ...addImportantToEachRule({
         position: 'relative',
         display: 'inline-block',
-        background: 'deepskyblue',
-      })
-    ),
+      }),
+    }),
     popover: {
       position: 'absolute',
+      zIndex: 99999,
+      background: 'white',
       padding: '.5rem 1rem',
       boxShadow: '0 0 10px 0 rgba(0,0,0,1)',
-      ...getFlyoutPosition(directionX, directionY),
+      maxWidth: '90vw',
+      top: '2rem',
+      left: '50%',
+      transform: 'translateX(-50%)',
     },
   });
 };
