ef3295e7ea3f48bb523fa79e734df20f837a7362
Anton Schmidt <anton.schmidt@mhp.com>
2021-11-17 11:42:43 +0100

    test(components): add unit tests for toast-manager, remove console logs | as / bh | #1500

--- packages/components/src/components/feedback/toast/toast/toast-manager.spec.ts
+++ packages/components/src/components/feedback/toast/toast/toast-manager.spec.ts
@@ -1,29 +1,153 @@
-describe('register()', () => {
-  it('should throw', () => {});
-  it('should return toast manager instance', () => {});
+import { ToastManager, ToastManagerClass } from './toast-manager';
+import { ToastState } from './toast-utils';
+import * as stencilCore from '@stencil/core';
+let toastManager: ToastManager;
+
+beforeEach(() => {
+  toastManager = new ToastManagerClass();
 });
 
-describe('unregister()', () => {
-  it('should remove toastEl reference, remove messages and clear timeout', () => {});
+describe('register()', () => {
+  const toastElement = document.createElement('p-toast');
+
+  it('should throw if toastEl is already defined', () => {
+    expect(() => {
+      toastManager.register(toastElement);
+    }).not.toThrow();
+
+    expect(() => {
+      toastManager.register(toastElement);
+    }).toThrowErrorMatchingInlineSnapshot('"<p-toast> was rendered multiple times."');
+  });
+
+  it('should return toast manager instance', () => {
+    expect(toastManager.register(toastElement)).toEqual(toastManager);
+    expect(toastManager['toastEl']).toBeDefined();
+  });
 });
 
 describe('addMessage()', () => {
-  it('should throw if no toastEl reference is set', () => {});
-  it('should set message state to neutral if none was provided', () => {});
-  it('should throw if an invalid message state was provided', () => {});
-  it('should add messages to messages array', () => {});
-  it('should force update if first element was added to messages array', () => {});
+  const toastElement = document.createElement('p-toast');
+  beforeEach(() => {
+    toastManager.register(toastElement);
+  });
+
+  it('should throw if no toastEl reference is set', () => {
+    toastManager.unregister();
+    expect(() => {
+      toastManager.addMessage({ message: 'Some Message' });
+    }).toThrowErrorMatchingInlineSnapshot('"Missing <p-toast> element."');
+  });
+
+  it('should set message state to neutral if none was provided', () => {
+    toastManager.addMessage({ message: 'Some Message' });
+    expect(toastManager['messages'][0]).toEqual({ message: 'Some Message', state: 'neutral' });
+  });
+
+  it('should throw if an invalid message state was provided', () => {
+    expect(() => {
+      toastManager.addMessage({ message: 'Some Message', state: 'wonky' as ToastState });
+    }).toThrowErrorMatchingSnapshot();
+  });
+
+  it('should add messages to messages array', () => {
+    toastManager.addMessage({ message: 'Some Message One' });
+    toastManager.addMessage({ message: 'Some Message Two', state: 'success' });
+    expect(toastManager['messages']).toEqual([
+      { message: 'Some Message One', state: 'neutral' },
+      { message: 'Some Message Two', state: 'success' },
+    ]);
+  });
+
+  it('should force update if first element was added to messages array', () => {
+    const spy = jest.spyOn(stencilCore, 'forceUpdate');
+    toastManager.addMessage({ message: 'Some Message One' });
+    expect(spy).toHaveBeenCalledWith(toastElement);
+
+    toastManager.addMessage({ message: 'Some Message One' });
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).not.toHaveBeenCalledTimes(2);
+  });
 });
 
 describe('dismissToastItem()', () => {
-  it('should clear timeout, remove first element in array by shifting and trigger force update', () => {});
+  const toastElement = document.createElement('p-toast');
+  beforeEach(() => {
+    toastManager.register(toastElement);
+  });
+
+  it('should remove timeout', () => {
+    toastManager.addMessage({ message: 'Some Message One' });
+    toastManager.dismissToastItem();
+    expect(toastManager['timeout']).toBeNull();
+  });
+
+  it('should remove first element in array', () => {
+    toastManager.addMessage({ message: 'Some Message One' });
+    toastManager.addMessage({ message: 'Some Message Two' });
+    toastManager.dismissToastItem();
+
+    expect(toastManager['messages']).toEqual([{ message: 'Some Message Two', state: 'neutral' }]);
+  });
+
+  it('should trigger force update', () => {
+    toastManager.addMessage({ message: 'Some Message' });
+    const spy = jest.spyOn(stencilCore, 'forceUpdate');
+    toastManager.dismissToastItem();
+
+    expect(spy).toHaveBeenCalledWith(toastElement);
+  });
+
+  it('should not have error when called without message', () => {
+    expect(() => toastManager.dismissToastItem()).not.toThrowError();
+  });
 });
 
 describe('getToast()', () => {
-  it('should get first element in array', () => {});
-  it('should get undefined if array is empty', () => {});
+  beforeEach(() => {
+    const toastElement = document.createElement('p-toast');
+    toastManager.register(toastElement);
+  });
+
+  it('should return first element in array', () => {
+    toastManager.addMessage({ message: 'Some Message One' });
+    toastManager.addMessage({ message: 'Some Message Two' });
+
+    expect(toastManager.getToast()).toEqual({ message: 'Some Message One', state: 'neutral' });
+  });
+
+  it('should return undefined if array is empty', () => {
+    expect(toastManager.getToast()).toBeUndefined();
+  });
 });
 
 describe('startTimeout()', () => {
-  it('should set a timeout', () => {});
+  const toastElement = document.createElement('p-toast');
+  beforeEach(() => {
+    toastManager.register(toastElement);
+  });
+
+  it('should set a timeout if message is available', () => {
+    toastManager.addMessage({ message: 'Some Message One' });
+    toastManager.startTimeout();
+    expect(toastManager['timeout']).toBeDefined();
+  });
+
+  it('should not set a timeout if no messages are available', () => {
+    toastManager.startTimeout();
+    expect(toastManager['timeout']).toBeUndefined();
+  });
+});
+
+describe('unregister()', () => {
+  it('should remove toastEl reference, remove messages and clear timeout', () => {
+    const toastElement = document.createElement('p-toast');
+    toastManager.register(toastElement);
+    toastManager.addMessage({ message: 'Some Message' });
+    toastManager.unregister();
+
+    expect(toastManager['toastEl']).toBeNull();
+    expect(toastManager['messages']).toEqual([]);
+    expect(toastManager['timeout']).toBeNull();
+  });
 });
