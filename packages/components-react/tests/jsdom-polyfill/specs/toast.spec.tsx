1cce7c1263b885649dd3e6ae811fd1de2210556a
Bernd Hacker <hacker.bernd@gmail.com>
2021-11-17 08:06:44 +0100

    test(componenst-react): extend toast jsdom-polyfill test | bh | #1500

--- packages/components-react/tests/jsdom-polyfill/specs/toast.spec.tsx
+++ packages/components-react/tests/jsdom-polyfill/specs/toast.spec.tsx
@@ -1,10 +1,25 @@
-import { componentsReady, PToast } from '@porsche-design-system/components-react';
+import { componentsReady, PToast, useToastManager } from '@porsche-design-system/components-react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
 
 const Sample = (): JSX.Element => {
+  const { addMessage } = useToastManager();
+  const [eventCounter, setEventCounter] = useState(0);
+
   return (
     <>
       <PToast data-testid="host" />
+      <button
+        onClick={() => {
+          console.log('click');
+          addMessage({ message: 'Some message' });
+          setEventCounter((prev) => prev + 1);
+        }}
+      >
+        Add Toast
+      </button>
+      <div data-testid="debug">{`Event Counter: ${eventCounter};`}</div>
     </>
   );
 };
@@ -15,5 +30,21 @@ describe('PToast', () => {
     await componentsReady();
 
     expect(getByTestId('host').shadowRoot).not.toBeNull();
+  });
+
+  it('should have working useToastManager hook', async () => {
+    const { getByTestId, getByRole } = render(<Sample />);
+    await componentsReady();
+
+    const debug = getByTestId('debug');
+    expect(debug.innerHTML).toBe('Event Counter: 0;');
+
+    const button = getByRole('button');
+    userEvent.click(button);
+    expect(debug.innerHTML).toBe('Event Counter: 1;');
+    await new Promise((resolve) => setTimeout(resolve)); // 😿
+    await componentsReady();
+
+    expect(getByTestId('host').shadowRoot.querySelector('p-toast-item').shadowRoot).not.toBeNull();
   });
 });
