433950e4516747794f6f99755b55dc645e5cc6d6
Bernd Hacker <hacker.bernd@gmail.com>
2021-11-17 15:00:23 +0100

    test(components): update toast-styles unit tests | bh / as | #1500

--- packages/components/src/components/feedback/toast/toast/toast-styles.spec.ts
+++ packages/components/src/components/feedback/toast/toast/toast-styles.spec.ts
@@ -2,7 +2,7 @@ import { getComponentCss } from './toast-styles';
 import type { ToastOffset } from './toast-utils';
 
 describe('getComponentCss()', () => {
-  it.each<ToastOffset>([{ bottom: 10 }, { bottom: 100 }, undefined])(
+  it.each<ToastOffset>([{ base: 10 }, { base: 10, xs: 20, s: 10, m: 20, l: 10, xl: 20 }, undefined])(
     'should return correct css for offset: %o',
     (offset) => {
       expect(getComponentCss(offset)).toMatchSnapshot();
