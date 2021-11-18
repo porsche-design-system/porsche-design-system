a9f7881884f12225e39a5272db52c0ecd94746ef
Bernd Hacker <hacker.bernd@gmail.com>
2021-11-18 11:30:24 +0100

    test(components-js/angular/react): improve toast vrts | bh / as | #1500

--- packages/components-angular/src/app/pages/toast-offset.component.ts
+++ packages/components-angular/src/app/pages/toast-offset.component.ts
@@ -9,13 +9,10 @@ import { ToastManager } from '@porsche-design-system/components-angular';
         transform: translateX(0);
         height: 56px;
       }
-      p-toast {
-        --p-toast-position: fixed;
-      }
     `,
   ],
   template: `
-    <div class="playground light" title="should render toast neutral on light background with offset {bottom: 0}">
+    <div class="playground light" title="should render toast neutral on light background with offsetBottom">
       <div class="inner">
         <p-toast [offsetBottom]="{ base: 0, xs: 10, s: 0, m: 10, l: 0, xl: 0 }"></p-toast>
       </div>
