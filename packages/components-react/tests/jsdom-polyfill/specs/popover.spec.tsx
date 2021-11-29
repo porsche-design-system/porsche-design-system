92590f2ac6928cd3bdaf04357d51d859c5251765
Matthias Haegele <matthias.haegele@mhp.com>
2021-11-29 10:19:39 +0100

    test(components-react): implement jsdom-polyfill test | mh | #786

--- packages/components-react/tests/jsdom-polyfill/specs/popover.spec.tsx
+++ packages/components-react/tests/jsdom-polyfill/specs/popover.spec.tsx
@@ -1,10 +1,15 @@
-import { PPopover } from '../../../projects/components-wrapper/src';
+import { PPopover, componentsReady } from '../../../projects/components-wrapper/src';
+import { render } from '@testing-library/react';
 
 const Sample = (): JSX.Element => {
-  return <PPopover />;
+  return <PPopover data-testid="host">Some Popover Content</PPopover>;
 };
-describe('Popover', () => {
-  it('should', async () => {
-    expect(true).toBe(true);
+
+describe('PPopover', () => {
+  it('should have initialized shadow dom', async () => {
+    const { getByTestId } = render(<Sample />);
+    await componentsReady();
+
+    expect(getByTestId('host').shadowRoot).not.toBeNull();
   });
 });
