1a90eee29057a5b71c94151902c8804adbb34fb6
Matthias Haegele <matthias.haegele@mhp.com>
2021-11-30 10:46:40 +0100

    test(components-js): add openPopoverScript to overview page / add popover to page / update fixture | mh / sas | #786

--- packages/components-js/tests/vrt/specs/overview.vrt.ts
+++ packages/components-js/tests/vrt/specs/overview.vrt.ts
@@ -1,5 +1,21 @@
 import { getVisualRegressionOverviewTester, vrtTest } from '@porsche-design-system/shared/testing';
+import { openPopoverAndSetBackground } from '../helpers';
+
+const openPopovers = async (page): Promise<void> => {
+  await openPopoverAndSetBackground(page);
+  return page.evaluate(() => {
+    document.querySelectorAll('my-prefix-p-popover').forEach((x) => {
+      const button = x.shadowRoot.querySelector('my-prefix-p-button-pure').shadowRoot.querySelector('button');
+      button.click();
+    });
+  });
+};
 
 it('should have no visual regression', async () => {
-  expect(await vrtTest(getVisualRegressionOverviewTester(), 'overview', '/#overview')).toBeFalsy();
+  expect(
+    await vrtTest(getVisualRegressionOverviewTester(), 'overview', '/#overview', {
+      scenario: openPopovers,
+      initialViewportHeight: 1000,
+    })
+  ).toBeFalsy();
 });
