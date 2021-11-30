9b8d16a7bb33cac2424506c377c53720261fa664
Matthias Haegele <matthias.haegele@mhp.com>
2021-11-30 15:13:32 +0100

    feat(components-angular): add binding to direction | mh | #786

--- packages/components-angular/src/app/pages/popover-overview.component.ts
+++ packages/components-angular/src/app/pages/popover-overview.component.ts
@@ -12,21 +12,21 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
       <span style="position: fixed; top: 0; right: 0; bottom: 0; width: 1rem; background: rgba(0, 0, 255, 0.1)"> </span>
       <!--   Top Left to right   -->
       <span style="position: absolute; top: 1.5rem; left: 1rem">
-        <p-popover direction="right">
+        <p-popover [direction]="'right'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div>
         </p-popover>
       </span>
       <span style="position: absolute; top: 2rem; left: 13rem">
-        <p-popover direction="right">
+        <p-popover [direction]="'right'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div>
         </p-popover>
       </span>
       <span style="position: absolute; top: 3rem; left: 25rem">
-        <p-popover direction="right">
+        <p-popover [direction]="'right'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div>
@@ -34,21 +34,21 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
       </span>
       <!--   Bottom Left to right   -->
       <span style="position: absolute; bottom: 1.5rem; left: 1rem">
-        <p-popover direction="right">
+        <p-popover [direction]="'right'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div>
         </p-popover>
       </span>
       <span style="position: absolute; bottom: 2rem; left: 13rem">
-        <p-popover direction="right">
+        <p-popover [direction]="'right'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div>
         </p-popover>
       </span>
       <span style="position: absolute; bottom: 3rem; left: 25rem">
-        <p-popover direction="right">
+        <p-popover [direction]="'right'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div>
@@ -56,21 +56,21 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
       </span>
       <!--   Top  Right to left   -->
       <span style="position: absolute; top: 1.5rem; right: 1rem">
-        <p-popover direction="left">
+        <p-popover [direction]="'left'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div></p-popover
         >
       </span>
       <span style="position: absolute; top: 2rem; right: 13rem">
-        <p-popover direction="left">
+        <p-popover [direction]="'left'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div></p-popover
         >
       </span>
       <span style="position: absolute; top: 3rem; right: 25rem">
-        <p-popover direction="left">
+        <p-popover [direction]="'left'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div></p-popover
@@ -78,21 +78,21 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
       </span>
       <!--   Bottom  Right to left   -->
       <span style="position: absolute; bottom: 1.5rem; right: 1rem">
-        <p-popover direction="left">
+        <p-popover [direction]="'left'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div></p-popover
         >
       </span>
       <span style="position: absolute; bottom: 2rem; right: 13rem">
-        <p-popover direction="left">
+        <p-popover [direction]="'left'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div></p-popover
         >
       </span>
       <span style="position: absolute; bottom: 3rem; right: 25rem">
-        <p-popover direction="left">
+        <p-popover [direction]="'left'">
           <div>Some popover</div>
           <div>Some popover</div>
           <div>Some popover</div></p-popover
@@ -100,31 +100,31 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
       </span>
       <!--    Top Center-->
       <span style="position: absolute; top: 4.5rem; right: 50rem">
-        <p-popover direction="top"><div>Direction Top</div></p-popover>
+        <p-popover [direction]="'top'"><div>Direction Top</div></p-popover>
       </span>
       <span style="position: absolute; top: 4rem; right: 65rem">
-        <p-popover direction="top"><div>Direction Top</div></p-popover>
+        <p-popover [direction]="'top'"><div>Direction Top</div></p-popover>
       </span>
       <!--    Bottom Center-->
       <span style="position: absolute; bottom: 4.5rem; right: 50rem">
-        <p-popover direction="bottom"><div>Direction Bottom</div></p-popover>
+        <p-popover [direction]="'bottom'"><div>Direction Bottom</div></p-popover>
       </span>
       <span style="position: absolute; bottom: 4rem; right: 65rem">
-        <p-popover direction="bottom"><div>Direction Bottom</div></p-popover>
+        <p-popover [direction]="'bottom'"><div>Direction Bottom</div></p-popover>
       </span>
       <!--    Left Center-->
       <span style="position: absolute; top: 10rem; left: 10rem">
-        <p-popover direction="left"><div>Direction Left</div></p-popover>
+        <p-popover [direction]="'left'"><div>Direction Left</div></p-popover>
       </span>
       <span style="position: absolute; top: 15rem; left: 9rem">
-        <p-popover direction="left"><div>Direction Left</div></p-popover>
+        <p-popover [direction]="'left'"><div>Direction Left</div></p-popover>
       </span>
       <!--    Right Center-->
       <span style="position: absolute; top: 10rem; right: 10.5rem">
-        <p-popover direction="right"><div>Direction Right</div></p-popover>
+        <p-popover [direction]="'right'"><div>Direction Right</div></p-popover>
       </span>
       <span style="position: absolute; top: 15rem; right: 9rem">
-        <p-popover direction="right"><div>Direction Right</div></p-popover>
+        <p-popover [direction]="'right'"><div>Direction Right</div></p-popover>
       </span>
     </div>
   `,
