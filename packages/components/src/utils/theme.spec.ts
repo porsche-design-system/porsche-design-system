c55089ce6c86280da5fbf47db72a63f36f67947e
Stephan Schröter <stephan.schroeter@mhp.com>
2021-12-01 13:54:59 +0100

    test: add unit test for isLightElectric() | sas | #1679

--- packages/components/src/utils/theme.spec.ts
+++ packages/components/src/utils/theme.spec.ts
@@ -1,4 +1,4 @@
-import { isDark, getThemeDarkAttribute } from './theme';
+import { isDark, getThemeDarkAttribute, isLightElectric } from './theme';
 
 describe('isDark()', () => {
   it('should return true for "dark"', () => {
@@ -11,6 +11,20 @@ describe('isDark()', () => {
 
   it('should return false for everything else', () => {
     expect(isDark('unknown' as any)).toBe(false);
+  });
+});
+
+describe('isLightElectric()', () => {
+  it('should return true for "light-electric"', () => {
+    expect(isLightElectric('dark')).toBe(true);
+  });
+
+  it('should return false for "light"', () => {
+    expect(isLightElectric('light')).toBe(false);
+  });
+
+  it('should return false for everything else', () => {
+    expect(isLightElectric('unknown' as any)).toBe(false);
   });
 });
 
