de380d983ab5a3f39f210100f7c45abbaf3aef72
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-01 10:22:14 +0100

    test(components-js/react/angular): position popover more center / remove wrapping span | mh / bh | #786

--- packages/components-angular/src/app/pages/popover.component.ts
+++ packages/components-angular/src/app/pages/popover.component.ts
@@ -3,15 +3,13 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 @Component({
   selector: 'page-popover',
   template: `
-    <div class="playground light" title="should render multiple popovers" style="height: 500px; width: 100%">
-      <span style="position: absolute; top: 50vh; left: 50vw">
-        <p-popover>
-          <span
-            >Some slotted and deeply nested <a href="#">linked</a>, <b>bold</b>, <strong>strong</strong>,
-            <em>emphasized</em> and <i>italic</i> text</span
-          >
-        </p-popover>
-      </span>
+    <div class="playground light" title="should render multiple popovers" style="height: 500px;">
+      <p-popover style="top: 50%; left: 50%; margin: -12px 0 0 -12px">
+        <span
+          >Some slotted and deeply nested <a href="#">linked</a>, <b>bold</b>, <strong>strong</strong>,
+          <em>emphasized</em> and <i>italic</i> text</span
+        >
+      </p-popover>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
--- packages/components-react/src/pages/Popover.tsx
+++ packages/components-react/src/pages/Popover.tsx
@@ -3,19 +3,13 @@ import { PPopover } from '@porsche-design-system/components-react';
 export const PopoverPage = (): JSX.Element => {
   return (
     <>
-      <div
-        className="playground light"
-        title="should render multiple popovers"
-        style={{ height: '500px', width: '100%' }}
-      >
-        <span style={{ position: 'absolute', top: '50vh', left: '50vw' }}>
-          <PPopover>
-            <span>
-              Some slotted and deeply nested <a href="#">linked</a>, <b>bold</b>, <strong>strong</strong>,
-              <em> emphasized</em> and <i>italic</i> text
-            </span>
-          </PPopover>
-        </span>
+      <div className="playground light" title="should render multiple popovers" style={{ height: '500px' }}>
+        <PPopover style={{ top: '50%', left: '50%', margin: '-12px 0 0 -12px' }}>
+          <span>
+            Some slotted and deeply nested <a href="#">linked</a>, <b>bold</b>, <strong>strong</strong>,
+            <em> emphasized</em> and <i>italic</i> text
+          </span>
+        </PPopover>
       </div>
     </>
   );
