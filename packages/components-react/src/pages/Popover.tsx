30c4ea228d9c2e3346d151f4499ae2b027e8ff61
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-01 09:01:59 +0100

    test(components-js/react/angular): test only one popover on multiple viewports | mh | #786

--- packages/components-react/src/pages/Popover.tsx
+++ packages/components-react/src/pages/Popover.tsx
@@ -6,28 +6,14 @@ export const PopoverPage = (): JSX.Element => {
       <div
         className="playground light"
         title="should render multiple popovers"
-        style={{ height: '800px', width: '100%' }}
+        style={{ height: '500px', width: '100%' }}
       >
-        <span style={{ position: 'absolute', top: '20vh', left: '20vw' }}>
-          <PPopover>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna
-          </PPopover>
-        </span>
-
         <span style={{ position: 'absolute', top: '50vh', left: '50vw' }}>
           <PPopover>
             <span>
               Some slotted and deeply nested <a href="#">linked</a>, <b>bold</b>, <strong>strong</strong>,
               <em> emphasized</em> and <i>italic</i> text
             </span>
-          </PPopover>
-        </span>
-
-        <span style={{ position: 'absolute', bottom: '20vh', right: '20vw' }}>
-          <PPopover>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-            dolore magna
           </PPopover>
         </span>
       </div>
