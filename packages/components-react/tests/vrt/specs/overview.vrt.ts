8d47cba1777ced05b7b4982f48aa9f23c069b8c8
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-01 08:23:52 +0100

    test(components-js/react/angular): add explanation why initial height is needed on overview vrt | mh | #786

--- packages/components-react/tests/vrt/specs/overview.vrt.ts
+++ packages/components-react/tests/vrt/specs/overview.vrt.ts
@@ -5,6 +5,7 @@ it('should have no visual regression', async () => {
   expect(
     await vrtTest(getVisualRegressionOverviewTester(), 'overview', '/overview', {
       scenario: (page) => openPopoversAndSetBackground(page, false, true),
+      // We need a initial height to ensure the popover can open to the preferred direction
       initialViewportHeight: 1000,
     })
   ).toBeFalsy();
