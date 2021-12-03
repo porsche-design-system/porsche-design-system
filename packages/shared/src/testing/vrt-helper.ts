726792f09443f999772f30cefe325bea85b7b2a5
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-03 14:27:21 +0100

    fix(shared): vrt helper | mh | #786

--- packages/shared/src/testing/vrt-helper.ts
+++ packages/shared/src/testing/vrt-helper.ts
@@ -14,26 +14,32 @@ export const openPopoversAndHighlightSpacer = async (
 
   await page.setViewport(bodyHeightWidth);
 
-  await page.evaluate(async (withBackground) => {
-    // Enable multiple open popovers
-    document.addEventListener('mousedown', (e) => e.stopPropagation(), true);
+  await page.evaluate(
+    async (withBackground, isVanillaJS) => {
+      // Enable multiple open popovers
+      document.addEventListener('mousedown', (e) => e.stopPropagation(), true);
 
-    const popoverEls = document.querySelectorAll('p-popover, my-prefix-p-popover');
+      const popoverEls = document.querySelectorAll('p-popover, my-prefix-p-popover');
 
-    popoverEls.forEach((popover) => {
-      const button = popover.shadowRoot.querySelector('p-button-pure, my-prefix-p-button-pure');
-      (button as HTMLElement).click();
-    });
-    // Workaround to have a delay due to waitForStencilLifecycle is not available
-    const newPopover = document.createElement('p-popover');
-    newPopover.style.margin = '-10rem';
-    document.body.appendChild(newPopover);
-    isVanillaJS ? await (window as any).porscheDesignSystem.componentsReady() : await (window as any).componentsReady();
-
-    if (withBackground) {
       popoverEls.forEach((popover) => {
-        (popover.shadowRoot.querySelector('.spacer') as HTMLElement).style.background = 'rgba(255, 0, 0, 0.4)';
+        const button = popover.shadowRoot.querySelector('p-button-pure, my-prefix-p-button-pure');
+        (button as HTMLElement).click();
       });
-    }
-  }, withBackground);
+      // Workaround to have a delay due to waitForStencilLifecycle is not available
+      const newPopover = document.createElement('p-popover');
+      newPopover.style.margin = '-10rem';
+      document.body.appendChild(newPopover);
+      isVanillaJS
+        ? await (window as any).porscheDesignSystem.componentsReady()
+        : await (window as any).componentsReady();
+
+      if (withBackground) {
+        popoverEls.forEach((popover) => {
+          (popover.shadowRoot.querySelector('.spacer') as HTMLElement).style.background = 'rgba(255, 0, 0, 0.4)';
+        });
+      }
+    },
+    withBackground,
+    isVanillaJS
+  );
 };
