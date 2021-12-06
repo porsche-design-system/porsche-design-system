6d42f1495122956c1fc8bc6a1a8e95cd90e575ed
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-06 11:44:36 +0100

    test(components-react): add jsdom-polyfill test to check open/close behavior | mh / bh | #786

--- packages/components-react/tests/jsdom-polyfill/specs/popover.spec.tsx
+++ packages/components-react/tests/jsdom-polyfill/specs/popover.spec.tsx
@@ -1,5 +1,6 @@
 import { PPopover, componentsReady } from '@porsche-design-system/components-react';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 const Sample = (): JSX.Element => {
   return <PPopover data-testid="host">Some Popover Content</PPopover>;
@@ -11,5 +12,18 @@ describe('PPopover', () => {
     await componentsReady();
 
     expect(getByTestId('host').shadowRoot).not.toBeNull();
+  });
+
+  it('should be opened on click and closed on second click', async () => {
+    const { getByTestId } = render(<Sample />);
+    await componentsReady();
+
+    const button = getByTestId('host').shadowRoot.querySelector('p-button-pure');
+
+    userEvent.click(button);
+    await waitFor(() => expect(getByTestId('host').shadowRoot.querySelector('.popover')).not.toBeNull());
+
+    userEvent.click(button);
+    await waitFor(() => expect(getByTestId('host').shadowRoot.querySelector('.popover')).toBeNull());
   });
 });
