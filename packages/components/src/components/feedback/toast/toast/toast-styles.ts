050af35cf05a27cd12e2ae8f0a73c0c7a6b8d534
Stephan Schröter <stephan.schroeter@mhp.com>
2021-12-10 11:11:07 +0100

    refactor: reuse style definitions of content-wrapper within toast | mz / sas | #1683

--- packages/components/src/components/feedback/toast/toast/toast-styles.ts
+++ packages/components/src/components/feedback/toast/toast/toast-styles.ts
@@ -2,6 +2,7 @@ import { addImportantToEachRule, buildHostStyles, getCss, mediaQuery, pxToRemWit
 import { TOAST_Z_INDEX } from '../../../../constants';
 import { getAnimationIn, getAnimationOut, getKeyframesMobile } from '../../banner/banner-styles';
 import { TOAST_ANIMATION_DURATION_VAR } from './toast-manager';
+import { contentWrapperMargin } from '../../../layout/content-wrapper/content-wrapper-styles';
 
 const toastPositionBottomVar = '--p-toast-position-bottom';
 const toastPositionBottomVarInternal = '--p-toast-position-bottom-internal';
@@ -12,8 +13,8 @@ export const getComponentCss = (): string => {
     ...buildHostStyles(
       addImportantToEachRule({
         position: 'fixed',
-        left: '7vw', // aligned with banner's content-wrapper
-        right: '7vw', // aligned with banner's content-wrapper
+        left: contentWrapperMargin,
+        right: contentWrapperMargin,
         // Needs a not overwritable internal css variable to cover default position depending on viewport size and to handle animation properly.
         // In addition, a public css variable can be passed to overwrite the default position.
         [toastPositionBottomVarInternal]: `var(${toastPositionBottomVar}, ${pxToRemWithUnit(56)})`,
@@ -21,7 +22,7 @@ export const getComponentCss = (): string => {
         maxWidth: '42rem',
         zIndex: TOAST_Z_INDEX,
         [mediaQuery('s')]: {
-          left: pxToRemWithUnit(64), // aligned with banner's content-wrapper
+          left: pxToRemWithUnit(64),
           right: 'auto',
           [toastPositionBottomVarInternal]: `var(${toastPositionBottomVar}, ${pxToRemWithUnit(64)})`,
           bottom: `var(${toastPositionBottomVarInternal})`,
