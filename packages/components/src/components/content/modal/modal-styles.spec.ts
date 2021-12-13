699b194788ce9b640eb1f6b8fc43b9f7227237eb
Stephan Schröter <stephan.schroeter@mhp.com>
2021-12-13 11:26:46 +0100

    test: add styles unit test for modal | mz / sas | #1683

--- packages/components/src/components/content/modal/modal-styles.spec.ts
+++ packages/components/src/components/content/modal/modal-styles.spec.ts
@@ -1,7 +1,13 @@
 import { getComponentCss } from './modal-styles';
+import type { BreakpointCustomizable } from '../../../types';
 
 describe('getComponentCss()', () => {
-  it.each<boolean>([false, true])('should return correct css for open: %s', (open) => {
-    expect(getComponentCss(open)).toMatchSnapshot();
+  it.each<[boolean, BreakpointCustomizable<boolean>]>([
+    [false, false],
+    [true, false],
+    [true, true],
+    [true, { base: true, xs: false, s: true, m: false, l: true, xl: false }],
+  ])('should return correct css for open: %s, fullscreen: %s', (open, fullscreen) => {
+    expect(getComponentCss(open, fullscreen)).toMatchSnapshot();
   });
 });
