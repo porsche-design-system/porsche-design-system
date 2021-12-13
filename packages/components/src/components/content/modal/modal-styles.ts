3d6ca09468a7763c21af08acb755931045eb2e13
Marcus Zierke <marcus.zierke@constafff.com>
2021-12-13 09:37:30 +0100

    refactor: draft for responsiveStyles with fullscreen prop  | mz / sas | #1683

--- packages/components/src/components/content/modal/modal-styles.ts
+++ packages/components/src/components/content/modal/modal-styles.ts
@@ -1,3 +1,4 @@
+import type { BreakpointCustomizable, GetStylesFunction, JssStyle } from '../../../utils';
 import {
   addImportantToEachRule,
   buildHostStyles,
@@ -8,16 +9,25 @@ import {
   mergeDeep,
   pxToRemWithUnit,
 } from '../../../utils';
-import type { BreakpointCustomizable, JssStyle, GetStylesFunction, Styles } from '../../../utils';
 import { color } from '@porsche-design-system/utilities';
 import { MODAL_Z_INDEX } from '../../../constants';
-import { contentWrapperMaxWidth, contentWrapperMargin } from '../../layout/content-wrapper/content-wrapper-styles';
+import {
+  contentWrapperMargin,
+  contentWrapperMarginXl,
+  contentWrapperMarginXxl,
+  contentWrapperMaxWidth,
+} from '../../layout/content-wrapper/content-wrapper-styles';
 
 const transitionTimingFunction = 'cubic-bezier(.16,1,.3,1)';
 
 const modalMinWidth = pxToRemWithUnit(272);
 const modalMaxWidth = contentWrapperMaxWidth;
 const modalMargin = `7vh ${contentWrapperMargin}`;
+const modalMarginXl = `10vh ${contentWrapperMarginXl}`;
+const modalMarginXxl = `10vh ${contentWrapperMarginXxl}`;
+const modalHeaderPadding = `0 0 ${pxToRemWithUnit(16)}`;
+const modalHeaderPaddingM = `0 0 ${pxToRemWithUnit(24)}`;
+const modalHeaderPaddingXxl = `0 0 ${pxToRemWithUnit(32)}`;
 
 export const getFullscreenStyles: GetStylesFunction = (fullscreen: boolean): JssStyle => {
   return fullscreen
@@ -37,7 +47,7 @@ export const getFullscreenStyles: GetStylesFunction = (fullscreen: boolean): Jss
 
 export const getComponentCss = (open: boolean, fullscreen: BreakpointCustomizable<boolean>): string => {
   return getCss(
-    mergeDeep<Styles>({
+    mergeDeep({
       ...buildHostStyles({
         ...addImportantToEachRule({
           position: 'fixed',
@@ -70,10 +80,44 @@ export const getComponentCss = (open: boolean, fullscreen: BreakpointCustomizabl
         transition: `transform .6s ${transitionTimingFunction}`,
         transform: open ? 'scale3d(1,1,1)' : 'scale3d(.9,.9,1)',
         padding: pxToRemWithUnit(32),
+        backgroundColor: color.background.default,
+        [mediaQuery('xxl')]: {
+          padding: pxToRemWithUnit(64),
+          margin: modalMarginXxl,
+          border: '5px solid pink',
+        },
         [mediaQuery('xl')]: {
+          margin: modalMarginXl,
+          border: '5px solid blue',
+        },
+        [mediaQuery('l')]: {
+          border: '5px solid yellow',
+        },
+        [mediaQuery('m')]: {
           padding: pxToRemWithUnit(40), // TODO: check how to apply padding
+          border: '5px solid purple',
+        },
+        [mediaQuery('s')]: {
+          border: '5px solid orange',
+        },
+        [mediaQuery('xs')]: {
+          border: '5px solid mint',
         },
         ...buildResponsiveStyles(fullscreen, getFullscreenStyles),
+      },
+      header: {
+        display: 'flex',
+        padding: modalHeaderPadding,
+        [mediaQuery('xxl')]: {
+          padding: modalHeaderPaddingXxl,
+        },
+        [mediaQuery('m')]: {
+          padding: modalHeaderPaddingM,
+        },
+      },
+      close: {
+        marginLeft: 'auto',
+        padding: `0 0 0 ${pxToRemWithUnit(16)}`,
       },
     })
   );
