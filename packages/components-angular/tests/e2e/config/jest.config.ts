b52be500760a3ef69119a86a8c265e93f378f1f8
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-15 16:42:29 +0100

    Revert "Revert "Merge branch 'master' into issue/1694""
    This reverts commit 1bb81ba7fbcfa1c4ad5114d38f05a66cf5ff35d5.

--- packages/components-angular/tests/e2e/config/jest.config.ts
+++ packages/components-angular/tests/e2e/config/jest.config.ts
@@ -1,2 +1,12 @@
 import { config } from '@porsche-design-system/shared/testing/jest.config';
-export default config;
+
+export default {
+  ...config,
+  moduleFileExtensions: ['ts', 'js', 'mjs'],
+  resolver: 'jest-preset-angular/build/resolvers/ng-jest-resolver.js',
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transform: {
+    '^.+\\.mjs?$': 'jest-preset-angular',
+    '^.+\\.ts?$': 'ts-jest',
+  },
+};
