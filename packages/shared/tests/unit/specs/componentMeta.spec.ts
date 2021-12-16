a848ea67ccf2b18c5bcbff09edff2f1a51afab31
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-16 15:43:17 +0100

    test(shared): test componentMeta directly instead of generating dict | mh | #1694

--- packages/shared/tests/unit/specs/componentMeta.spec.ts
+++ packages/shared/tests/unit/specs/componentMeta.spec.ts
@@ -1,22 +1,13 @@
-import { ComponentMeta, FOCUSABLE_TAG_NAMES_CAMEL_CASE, getComponentMeta, TAG_NAMES, TagName } from '../../../src';
+import { componentMeta, FOCUSABLE_TAG_NAMES_CAMEL_CASE } from '../../../src';
 
-describe('getComponentMeta()', () => {
-  it('should match snapshot for getComponentMeta() dictionary', () => {
-    let dict: { [key in TagName]: ComponentMeta };
-    TAG_NAMES.forEach(
-      (tagName) =>
-        (dict = {
-          ...dict,
-          [tagName]: getComponentMeta(tagName),
-        })
-    );
-
-    expect(dict).toMatchSnapshot();
+describe('componentMeta', () => {
+  it('should match snapshot', () => {
+    expect(componentMeta).toMatchSnapshot();
   });
 });
 
 describe('FOCUSABLE_TAG_NAMES_CAMEL_CASE', () => {
-  it('should match snapshot for FOCUSABLE_TAG_NAMES_CAMEL_CASE', () => {
+  it('should match snapshot', () => {
     expect(FOCUSABLE_TAG_NAMES_CAMEL_CASE).toMatchSnapshot();
   });
 });
