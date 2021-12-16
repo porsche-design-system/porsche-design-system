ab2bdaa04496e03e226a3fe11d3533241466344e
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-16 15:37:28 +0100

    test(shared): change snapshot to whole dict instead of single calls | mh | #1694

--- packages/shared/tests/unit/specs/componentMeta.spec.ts
+++ packages/shared/tests/unit/specs/componentMeta.spec.ts
@@ -1,13 +1,22 @@
-import { FOCUSABLE_TAG_NAMES_CAMEL_CASE, getComponentMeta, TAG_NAMES } from '../../../src';
+import { ComponentMeta, FOCUSABLE_TAG_NAMES_CAMEL_CASE, getComponentMeta, TAG_NAMES, TagName } from '../../../src';
 
 describe('getComponentMeta()', () => {
-  it.each(TAG_NAMES)('should match snapshot for %s', (tagName) => {
-    expect(getComponentMeta(tagName)).toMatchSnapshot();
+  it('should match snapshot for getComponentMeta() dictionary', () => {
+    let dict: { [key in TagName]: ComponentMeta };
+    TAG_NAMES.forEach(
+      (tagName) =>
+        (dict = {
+          ...dict,
+          [tagName]: getComponentMeta(tagName),
+        })
+    );
+
+    expect(dict).toMatchSnapshot();
   });
 });
 
 describe('FOCUSABLE_TAG_NAMES_CAMEL_CASE', () => {
-  it('should match snapshot for %s', () => {
+  it('should match snapshot for FOCUSABLE_TAG_NAMES_CAMEL_CASE', () => {
     expect(FOCUSABLE_TAG_NAMES_CAMEL_CASE).toMatchSnapshot();
   });
 });
