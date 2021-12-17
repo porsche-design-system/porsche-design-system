8dc3a0a7ff35cfc9754a3414062fb8a630b15d52
Bernd Hacker <hacker.bernd@gmail.com>
2021-12-17 09:18:34 +0100

    test(components): cleanup and reorder unit tests for text-field-wrapper-utils | bh | #1684

--- packages/components/src/components/form/text-field-wrapper/text-field-wrapper-utils.spec.ts
+++ packages/components/src/components/form/text-field-wrapper/text-field-wrapper-utils.spec.ts
@@ -10,6 +10,63 @@ import {
 } from './text-field-wrapper-utils';
 import { FormState } from '../../../types';
 
+const getInputElement = (): HTMLInputElement => document.createElement('input');
+
+describe('hasCounter()', () => {
+  it('should return true if maxLength is defined', async () => {
+    const inputElement = getInputElement();
+    inputElement.maxLength = 20;
+    expect(hasCounter(inputElement)).toBe(true);
+  });
+
+  it('should return false if maxLength is undefined', async () => {
+    const inputElement = document.createElement('input');
+    Object.defineProperty(inputElement, 'maxLength', { value: -1 }); // jsdom defaults to 524288 which is 512 KB
+    expect(hasCounter(inputElement)).toBe(false);
+  });
+});
+
+describe('hasCounterAndIsTypeText()', () => {
+  it('should return true if maxLength is defined and input is of type "text"', async () => {
+    const inputElement = getInputElement();
+    inputElement.maxLength = 20;
+    inputElement.type = 'text';
+    expect(hasCounterAndIsTypeText(inputElement)).toBe(true);
+  });
+
+  it('should return false if maxLength is defined and input is of type "number"', async () => {
+    const inputElement = getInputElement();
+    inputElement.maxLength = 20;
+    inputElement.type = 'number';
+    expect(hasCounterAndIsTypeText(inputElement)).toBe(false);
+  });
+});
+
+describe('hasUnitAndIsTypeNumber()', () => {
+  it('should return true if unit is "EUR" and input is of type "number"', () => {
+    const inputElement = getInputElement();
+    inputElement.type = 'number';
+    expect(hasUnitAndIsTypeNumber(inputElement, 'EUR')).toBe(true);
+  });
+
+  it('should return false if unit is "" and input is of type "number"', () => {
+    const inputElement = getInputElement();
+    inputElement.type = 'number';
+    expect(hasUnitAndIsTypeNumber(inputElement, '')).toBe(false);
+  });
+});
+
+describe('setCounterInnerHtml()', () => {
+  it('should return character count as innerText for element ', async () => {
+    const inputElement = getInputElement();
+    inputElement.maxLength = 20;
+    inputElement.value = 'some';
+    const counterElement = document.createElement('span');
+    setCounterInnerHtml(inputElement, counterElement);
+    expect(counterElement.innerText).toBe('4/20');
+  });
+});
+
 describe('getInputPadding()', () => {
   it.each<[TextFieldWrapperUnitPosition, FormState, string]>([
     ['prefix', 'none', '0.6875rem 0.6875rem 0.6875rem 3.75rem'],
@@ -35,57 +92,5 @@ describe('setInputStyles()', () => {
 describe('throwIfUnitLengthExceeded()', () => {
   it('should throw error if unit length > 5', () => {
     expect(() => throwIfUnitLengthExceeded('123456')).toThrow();
-  });
-});
-
-describe('hasCounter()', () => {
-  it('return true if maxLength is defined', async () => {
-    const inputElement = document.createElement('input');
-    Object.defineProperty(inputElement, 'maxLength', { writable: true, configurable: true, value: 20 });
-    expect(hasCounter(inputElement)).toBe(true);
-  });
-  it('return false if maxLength is undefined', async () => {
-    const inputElement = document.createElement('input');
-    Object.defineProperty(inputElement, 'maxLength', {});
-    expect(hasCounter(inputElement)).toBe(false);
-  });
-});
-
-describe('hasCounterAndIsTypeText()', () => {
-  it('return true if maxLength is defined and input is of type "text"', async () => {
-    const inputElement = document.createElement('input');
-    Object.defineProperty(inputElement, 'maxLength', { writable: true, configurable: true, value: 20 });
-    Object.defineProperty(inputElement, 'type', { writable: true, configurable: true, value: 'text' });
-    expect(hasCounterAndIsTypeText(inputElement)).toBe(true);
-  });
-  it('return false if maxLength is defined and input is of type "number"', async () => {
-    const inputElement = document.createElement('input');
-    Object.defineProperty(inputElement, 'maxLength', { writable: true, configurable: true, value: 20 });
-    Object.defineProperty(inputElement, 'type', { writable: true, configurable: true, value: 'number' });
-    expect(hasCounterAndIsTypeText(inputElement)).toBe(false);
-  });
-});
-
-describe('hasUnitAndIsTypeNumber()', () => {
-  it('return true if unit is "EUR" and input is of type "number"', () => {
-    const inputElement = document.createElement('input');
-    Object.defineProperty(inputElement, 'type', { writable: true, configurable: true, value: 'number' });
-    expect(hasUnitAndIsTypeNumber(inputElement, 'EUR')).toBe(true);
-  });
-  it('return false if unit is "" and input is of type "number"', () => {
-    const inputElement = document.createElement('input');
-    Object.defineProperty(inputElement, 'type', { writable: true, configurable: true, value: 'number' });
-    expect(hasUnitAndIsTypeNumber(inputElement, '')).toBe(false);
-  });
-});
-
-describe('setCounterInnerHtml()', () => {
-  it('return character count as innerText for element ', async () => {
-    const element = document.createElement('input');
-    const counterElement = document.createElement('span');
-    Object.defineProperty(element, 'maxLength', { writable: true, configurable: true, value: 20 });
-    Object.defineProperty(element, 'value', { writable: true, configurable: true, value: 'vier' });
-    setCounterInnerHtml(element, counterElement);
-    expect(counterElement.innerText).toBe('4/20');
   });
 });
