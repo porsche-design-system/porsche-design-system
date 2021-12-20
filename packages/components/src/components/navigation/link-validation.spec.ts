84ba2c1ff89d944565ce0ff29b5f7d51060152e4
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-20 13:36:01 +0100

    test(components): test new validation | mh | #1655

--- packages/components/src/components/navigation/link-validation.spec.ts
+++ packages/components/src/components/navigation/link-validation.spec.ts
@@ -1,16 +1,33 @@
 import { throwIfInvalidLinkUsage } from './link-validation';
 
+let span: HTMLElement;
+let anchor: HTMLAnchorElement;
+
+beforeEach(() => {
+  span = document.createElement('span');
+  anchor = document.createElement('a');
+});
+
 it('should throw error when used without href and slottedAnchor', () => {
-  expect(() => throwIfInvalidLinkUsage(document.body, undefined)).toThrowErrorMatchingInlineSnapshot(
-    `"Usage of body is not valid. Please provide a href property or a slotted anchor."`
+  expect(() => throwIfInvalidLinkUsage(document.createElement('p-link'), undefined)).toThrowErrorMatchingInlineSnapshot(
+    `"Usage of p-link is not valid. Please provide a href property or a slotted anchor."`
+  );
+});
+
+it('should throw error when used with href on host and anchor', () => {
+  span.appendChild(anchor);
+  anchor.href = '#';
+
+  expect(() => throwIfInvalidLinkUsage(span, '#')).toThrowErrorMatchingInlineSnapshot(
+    `"Usage of span is not valid. Please use with href property OR slotted anchor."`
   );
 });
 
 it('should not throw error if used with slottedAnchor', () => {
-  document.body.appendChild(document.createElement('a'));
-  expect(() => throwIfInvalidLinkUsage(document.body, undefined)).not.toThrow();
+  span.appendChild(anchor);
+  expect(() => throwIfInvalidLinkUsage(span, undefined)).not.toThrow();
 });
 
 it('should not throw error when href is defined', () => {
-  expect(() => throwIfInvalidLinkUsage(document.body, '#')).not.toThrow();
+  expect(() => throwIfInvalidLinkUsage(span, '#')).not.toThrow();
 });
