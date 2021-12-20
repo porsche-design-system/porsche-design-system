d4568e839f3dd337e6c7e470fb087a9f55b637b8
Bernd Hacker <hacker.bernd@gmail.com>
2021-12-20 14:39:53 +0100

    test(components-js): extend textarea-wrapper vrt | bh | #1684

--- packages/components-js/tests/vrt/specs/textarea-wrapper.vrt.ts
+++ packages/components-js/tests/vrt/specs/textarea-wrapper.vrt.ts
@@ -13,7 +13,7 @@ import {
   vrtTest,
 } from '@porsche-design-system/shared/testing';
 
-it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
+fit.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
   expect(await vrtTest(getVisualRegressionTester(viewport), 'textarea-wrapper', '/#textarea-wrapper')).toBeFalsy();
 });
 
