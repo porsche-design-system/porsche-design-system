3271b6f0f410298d9ead0dd74bd5ae8de830d7c2
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-20 13:21:01 +0100

    test(components): fix unit tests after lifecycle change | mh | #1655

--- packages/components/src/components/navigation/link/link.spec.ts
+++ packages/components/src/components/navigation/link/link.spec.ts
@@ -1,43 +1,30 @@
 import { Link } from './link';
 import * as linkValidationUtils from '../link-validation';
-import * as jssUtils from '../../../utils/jss';
 import * as focusHandling from '../../../utils/focus-handling';
 
-describe('connectedCallback()', () => {
-  it('should call improveFocusHandlingForCustomElement() ', () => {
-    const spy = jest.spyOn(focusHandling, 'improveFocusHandlingForCustomElement');
-    const component = new Link();
-    component.host = document.createElement('p-link');
-    component.connectedCallback();
-
-    expect(spy).toBeCalledWith(component.host);
-  });
-});
-
-describe('componentWillRender()', () => {
+describe('componentWillLoad()', () => {
   let linkValidationUtilsSpy;
-  let jssUtilsSpy;
 
   beforeEach(() => {
     linkValidationUtilsSpy = jest
       .spyOn(linkValidationUtils, 'throwIfInvalidLinkUsage')
       .mockImplementationOnce(() => {});
-    jssUtilsSpy = jest.spyOn(jssUtils, 'attachComponentCss').mockImplementationOnce(() => {});
   });
 
-  it('should call attachComponentsCss() ', () => {
+  it('should call throwIfInvalidLinkUsage() ', () => {
     const component = new Link();
     component.host = document.createElement('p-link');
-    component.componentWillRender();
-
-    expect(jssUtilsSpy).toBeCalledTimes(1);
-  });
-
-  it('should call validateLinkUsage() ', () => {
-    const component = new Link();
-    component.host = document.createElement('p-link');
-    component.componentWillRender();
+    component.componentWillLoad();
 
     expect(linkValidationUtilsSpy).toBeCalledWith(component.host, component.href);
+  });
+
+  it('should call improveFocusHandlingForCustomElement() ', () => {
+    const spy = jest.spyOn(focusHandling, 'improveFocusHandlingForCustomElement');
+    const component = new Link();
+    component.host = document.createElement('p-link');
+    component.componentWillLoad();
+
+    expect(spy).toBeCalledWith(component.host);
   });
 });
