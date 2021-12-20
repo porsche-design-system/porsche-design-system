22e2a94a45195f125e233b8943fcc5bb2b75e5e5
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-20 11:08:14 +0100

    test(components): align inline snapshot with message | mh | #1655

--- packages/components/src/utils/link-validation.spec.ts
+++ packages/components/src/utils/link-validation.spec.ts
@@ -2,7 +2,7 @@ import { validateLinkUsage } from './link-validation';
 
 it('should throw error when used without href and slottedAnchor', () => {
   expect(() => validateLinkUsage(document.body, undefined)).toThrowErrorMatchingInlineSnapshot(
-    `"Usage of ${document.body.tagName} is not valid. Please provide a href property. For further information see https://designsystem.porsche.com/v2/"`
+    `"Usage of ${document.body.tagName} is not valid. Please provide a href property. For further information see https://designsystem.porsche.com"`
   );
 });
 
