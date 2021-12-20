f9d961fd910b53ad5a685056959e3a7d65cf6a1f
Bernd Hacker <hacker.bernd@gmail.com>
2021-12-20 13:30:56 +0100

    test(components-js): extend textarea-wrapper states vrt | bh | #1684

--- packages/components-js/tests/vrt/specs/textarea-wrapper.vrt.ts
+++ packages/components-js/tests/vrt/specs/textarea-wrapper.vrt.ts
@@ -24,47 +24,78 @@ it('should have no visual regression for :hover + :focus-visible', async () => {
       const page = vrt.getPage();
 
       const head = `
-        <style type="text/css">
-          p-textarea-wrapper:not(:last-child) { margin-bottom: 1rem; }
+        <style>
+          .playground div { display: flex; }
+          .playground div > * { width: 40%; }
+          p-textarea-wrapper:not(:last-child) {
+            margin-right: 1rem;
+            margin-bottom: 1rem;
+          }
           textarea { min-height: initial; }
         </style>`;
 
-      // TODO: disabled state
+      const child = '<textarea>Value</textarea>';
+      const childReadonly = child.replace(/((?: \/)?>)/, ' readonly$1');
+      const childDisabled = child.replace(/((?: \/)?>)/, ' disabled$1');
+
       const getElementsMarkup: GetMarkup = () => `
-        <p-textarea-wrapper label="Some label">
-          <textarea name="some-name"></textarea>
-        </p-textarea-wrapper>
-        <p-textarea-wrapper label="Readonly">
-          <textarea name="some-name" readonly>Readonly</textarea>
-        </p-textarea-wrapper>
-        <p-textarea-wrapper label="Some label" state="error" message="Some error validation message.">
-          <textarea name="some-name"></textarea>
-        </p-textarea-wrapper>
-        <p-textarea-wrapper label="Some label" state="success" message="Some success validation message.">
-          <textarea name="some-name"></textarea>
-        </p-textarea-wrapper>
-        <p-textarea-wrapper>
-          <span slot="label">Some label with a <a href="#">link</a>.</span>
-          <span slot="description">Some description with a <a href="#">link</a>.</span>
-          <textarea name="some-name"></textarea>
-        </p-textarea-wrapper>
-        <p-textarea-wrapper label="Some label" description="Some description" state="error">
-          <textarea name="some-name"></textarea>
-          <span slot="message">Some error message with a <a href="#">link</a>.</span>
-        </p-textarea-wrapper>
-        <p-textarea-wrapper label="Some label" description="Some description" state="success">
-          <textarea name="some-name"></textarea>
-          <span slot="message">Some success message with a <a href="#">link</a>.</span>
-        </p-textarea-wrapper>`;
+        <div>
+          <p-textarea-wrapper label="Default">
+            ${child}
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Error" state="error" message="Error">
+            ${child}
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Success" state="success" message="Success">
+            ${child}
+          </p-textarea-wrapper>
+        </div>
+        <div>
+          <p-textarea-wrapper label="Readonly">
+            ${childReadonly}
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Readonly Error" state="error" message="Error">
+            ${childReadonly}
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Readonly Success" state="success" message="Success">
+            ${childReadonly}
+          </p-textarea-wrapper>
+        </div>
+        <div>
+          <p-textarea-wrapper label="Disabled">
+            ${childDisabled}
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Disabled Error" state="error" message="Error">
+            ${childDisabled}
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Disabled Success" state="success" message="Success">
+            ${childDisabled}
+          </p-textarea-wrapper>
+        </div>
+        <div>
+          <p-textarea-wrapper>
+            <span slot="label">Slotted <a href="#">label</a></span>
+            <span slot="description">Slotted <a href="#">description</a></span>
+            ${child}
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Error" description="Some description" state="error">
+            ${child}
+            <span slot="message">Slotted <a href="#">error</a></span>
+          </p-textarea-wrapper>
+          <p-textarea-wrapper label="Success" description="Some description" state="success">
+            ${child}
+            <span slot="message">Slotted <a href="#">success</a></span>
+          </p-textarea-wrapper>
+        </div>`;
 
       await setContentWithDesignSystem(page, getBodyMarkup(getElementsMarkup), { injectIntoHead: head });
 
-      await forceHoverState(page, '.hover > p-textarea-wrapper textarea');
-      await forceHoverState(page, '.hover > p-textarea-wrapper a');
-      await forceFocusState(page, '.focus > p-textarea-wrapper textarea');
-      await forceFocusState(page, '.focus > p-textarea-wrapper a');
-      await forceFocusHoverState(page, '.focus-hover > p-textarea-wrapper textarea');
-      await forceFocusHoverState(page, '.focus-hover > p-textarea-wrapper a');
+      await forceHoverState(page, '.hover p-textarea-wrapper textarea');
+      await forceHoverState(page, '.hover p-textarea-wrapper a');
+      await forceFocusState(page, '.focus p-textarea-wrapper textarea');
+      await forceFocusState(page, '.focus p-textarea-wrapper a');
+      await forceFocusHoverState(page, '.focus-hover p-textarea-wrapper textarea');
+      await forceFocusHoverState(page, '.focus-hover p-textarea-wrapper a');
     })
   ).toBeFalsy();
 });
