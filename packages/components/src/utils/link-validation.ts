773154bc53d6ba0887a700576e3c62098e3df9cc
Matthias Hägele <60395476+mhaegeleMHP@users.noreply.github.com>
2021-12-20 10:56:06 +0100

    Update packages/components/src/utils/link-validation.ts

--- packages/components/src/utils/link-validation.ts
+++ packages/components/src/utils/link-validation.ts
@@ -3,7 +3,7 @@ import { getHTMLElement } from './dom';
 export const validateLinkUsage = (host: HTMLElement, href: string | undefined) => {
   if (!href && !getHTMLElement(host, 'a')) {
     throw new Error(
-      `Usage of ${host.tagName} is not valid. Please provide a href property. For further information see https://designsystem.porsche.com/v2/`
+      `Usage of ${host.tagName} is not valid. Please provide a href property. For further information see https://designsystem.porsche.com`
     );
   }
 };
