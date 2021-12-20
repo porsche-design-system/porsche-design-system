debb3690cbf29e1af29f26345928488fb2dfd844
Anton Schmidt <anton.schmidt@mhp.com>
2021-12-20 13:28:47 +0100

    refactor(storefront): extract search component | as | #1555

--- packages/storefront/src/components/Sidebar.vue
+++ packages/storefront/src/components/Sidebar.vue
@@ -1,37 +1,6 @@
 <template>
   <nav>
-    <ais-instant-search :index-name="getAlgoliaIndexName()" :search-client="searchClient">
-      <ais-search-box>
-        <debounced-search-box :on-focus="shouldDisplayHits" v-on:query-change="shouldDisplayHits" />
-      </ais-search-box>
-
-      <ais-state-results>
-        <template v-slot="{ results: { hits } }">
-          {{ shouldHideNavigation(hits) }}
-          <ais-hits
-            v-show="displayHits"
-            :transform-items="transformItems"
-            :class-names="{
-              'ais-Hits': 'hits',
-              'ais-Hits-item': 'hits-item',
-            }"
-          >
-            <template v-slot:item="{ item }">
-              <section>
-                <p-text :weight="'semibold'" :tag="'div'" :size="'small'" class="category">{{ item.category }}</p-text>
-                <ul>
-                  <li v-for="(hit, index) in item.hits" :key="index">
-                    <p-link-pure class="link" icon="none" @click="() => (displayHits = false)">
-                      <router-link :to="hit.url"> {{ hit.page }} {{ hit.tab ? ' - ' + hit.tab : '' }} </router-link>
-                    </p-link-pure>
-                  </li>
-                </ul>
-              </section>
-            </template>
-          </ais-hits>
-        </template>
-      </ais-state-results>
-    </ais-instant-search>
+    <Search :hideNavigation="this.hideNavigation" v-on:hide-navigation-change="shouldHideNavigation" />
     <template v-if="!this.hideNavigation">
       <p-accordion
         v-for="(pages, category, index) in config"
@@ -56,47 +25,17 @@
 <script lang="ts">
   import Vue from 'vue';
   import Component from 'vue-class-component';
-  import algoliasearch from 'algoliasearch/lite';
-  import { createInMemoryCache } from '@algolia/cache-in-memory';
   import { Watch } from 'vue-property-decorator';
   import { StorefrontConfig } from '@/models';
-  import { ALGOLIA_APP_ID, ALGOLIA_READONLY_KEY, config as storefrontConfig } from '@/../storefront.config';
   import { capitalCase, paramCase } from 'change-case';
   import { Route } from 'vue-router';
+  import { config as storefrontConfig } from '@/../storefront.config';
 
-  import DebouncedSearchBox from '@/components/DebouncedSearchBox.vue';
-
-  type AlgoliaRecord = {
-    objectID: string;
-    name: string;
-    content: string;
-    category: string;
-    page: string;
-    tab?: string;
-    url: string;
-  };
-
-  type AlgoliaResult = {
-    category: string;
-    hits: AlgoliaRecord[];
-  };
-
-  type AlgoliaRequest = {
-    indexName: string;
-    params: AlgoliaRequestParams;
-  };
-
-  type AlgoliaRequestParams = {
-    facets: string[];
-    query: string;
-    tagFilters: string;
-    highlightPreTag?: string;
-    highlightPostTag?: string;
-  };
+  import Search from '@/components/Search.vue';
 
   @Component({
     components: {
-      DebouncedSearchBox,
+      Search,
     },
   })
   export default class Sidebar extends Vue {
@@ -104,32 +43,6 @@
     public paramCase = paramCase;
     public accordion: { [id: string]: boolean } = {};
     public hideNavigation = false;
-    public displayHits = false;
-
-    public algoliaClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_READONLY_KEY, {
-      responsesCache: createInMemoryCache(),
-      requestsCache: createInMemoryCache({ serializable: false }),
-    });
-
-    public searchClient = {
-      ...this.algoliaClient,
-      algoliaClient: this.algoliaClient,
-      search(requests: AlgoliaRequest[]) {
-        // remove initial search
-        if (requests.every(({ params }: AlgoliaRequest) => !params?.query.trim())) {
-          return Promise.resolve({
-            results: requests.map(() => ({
-              hits: [],
-              nbHits: 0,
-              nbPages: 0,
-              page: 0,
-              processingTimeMS: 0,
-            })),
-          });
-        }
-        return this.algoliaClient.search(requests);
-      },
-    };
 
     public getRoute(category: string, page: string): string {
       return `/${paramCase(category)}/${paramCase(page)}`;
@@ -139,6 +52,7 @@
       const route = this.getRoute(category, page);
       return this.$route.path.startsWith(route);
     }
+
     private created(): void {
       Object.keys(this.config).map((category) => {
         this.accordion[category] = false;
@@ -164,36 +78,13 @@
       this.accordion = { ...this.accordion, [category]: !this.accordion[category] };
     }
 
-    shouldHideNavigation(hits: AlgoliaResult[]): void {
-      this.hideNavigation = this.displayHits && hits.length > 0;
-    }
-
-    shouldDisplayHits(query: string): void {
-      this.displayHits = !!query;
+    shouldHideNavigation(hideNavigation: boolean): void {
+      this.hideNavigation = hideNavigation;
     }
 
     private static category(route: Route): string {
       const { category } = route.params;
       return category ? capitalCase(category) : '';
-    }
-
-    getAlgoliaIndexName(): string {
-      const baseHref = (document.querySelector('base') as HTMLBaseElement).getAttribute('href') as string;
-      // on localhost baseHref is '/'
-      return baseHref.length > 1 ? baseHref.slice(1, -1).replace('/', '_') : 'localhost';
-    }
-
-    transformItems(items: AlgoliaRecord[]): AlgoliaResult[] {
-      return items.reduce((results, current) => {
-        const categoryIndex = results.findIndex((result) => result.category === current.category);
-        if (categoryIndex >= 0) {
-          // reduce amount of displayed hits per category to 5 when using distinct on PAGE instead of CATEGORY
-          results[categoryIndex].hits.length < 5 && results[categoryIndex].hits.push(current);
-        } else {
-          results.push({ category: current.category, hits: [current] });
-        }
-        return results;
-      }, [] as AlgoliaResult[]);
     }
   }
 </script>
@@ -207,33 +98,9 @@
   }
 
   .link {
+    width: 100%;
     margin: $p-spacing-4 0;
     display: inline-block;
     text-decoration: none;
-  }
-  .hits {
-    position: absolute;
-    width: p-px-to-rem(280px);
-    background: $p-color-background-default;
-    z-index: 1;
-    left: 0;
-  }
-  .ais-SearchBox {
-    margin-bottom: p-px-to-rem(24px);
-  }
-
-  .category {
-    padding-bottom: $p-spacing-8;
-  }
-
-  p-link-pure {
-    width: 100%;
-  }
-</style>
-
-<style lang="scss">
-  @import '~@porsche-design-system/utilities/scss';
-  .hits-item {
-    padding: $p-spacing-8 $p-spacing-32 $p-spacing-8;
   }
 </style>
