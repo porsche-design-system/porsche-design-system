c79fe96c38ac1df7896eaf05fe01856ddc121482
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-21 11:32:55 +0100

    test(components): test lifecycles without mock but correct usage of component | mh | #1655

--- packages/components/src/components/navigation/link/link.spec.ts
+++ packages/components/src/components/navigation/link/link.spec.ts
@@ -3,28 +3,25 @@ import * as linkValidationUtils from '../link-validation';
 import * as focusHandling from '../../../utils/focus-handling';
 
 describe('componentWillLoad()', () => {
-  let linkValidationUtilsSpy;
-
-  beforeEach(() => {
-    linkValidationUtilsSpy = jest
-      .spyOn(linkValidationUtils, 'throwIfInvalidLinkUsage')
-      .mockImplementationOnce(() => {});
-  });
-
   it('should call throwIfInvalidLinkUsage() ', () => {
+    const spy = jest.spyOn(linkValidationUtils, 'throwIfInvalidLinkUsage');
+
     const component = new Link();
     component.host = document.createElement('p-link');
-    component.componentWillLoad();
+    component.href = '#';
 
-    expect(linkValidationUtilsSpy).toBeCalledWith(component.host, component.href);
+    component.componentWillLoad();
+    expect(spy).toBeCalledWith(component.host, component.href);
   });
 
   it('should call improveFocusHandlingForCustomElement() ', () => {
     const spy = jest.spyOn(focusHandling, 'improveFocusHandlingForCustomElement');
+
     const component = new Link();
     component.host = document.createElement('p-link');
-    component.componentWillLoad();
+    component.href = '#';
 
+    component.componentWillLoad();
     expect(spy).toBeCalledWith(component.host);
   });
 });
