cb08eda58321faaaf3b10d03782309ebebec2d74
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-21 11:32:55 +0100

    test(components): test lifecycles without mock but correct usage of component | mh | #1655

--- packages/components/src/components/navigation/link-pure/link-pure.spec.ts
+++ packages/components/src/components/navigation/link-pure/link-pure.spec.ts
@@ -2,42 +2,27 @@ import { LinkPure } from './link-pure';
 import * as transitionListenerUtils from '../../../utils/transition-listener';
 import * as linkValidationUtils from '../link-validation';
 import * as focusHandling from '../../../utils/focus-handling';
-import * as slottedStylesUtils from '../../../utils/slotted-styles';
 
 jest.mock('../../../utils/focus-handling');
 
-describe('connectedCallback()', () => {
-  it('should call attachSlottedCss() ', () => {
-    const spy = jest.spyOn(slottedStylesUtils, 'attachSlottedCss');
-    const component = new LinkPure();
-    component.host = document.createElement('p-link-pure');
-    component.connectedCallback();
-
-    expect(spy).toBeCalledWith(component.host, expect.anything());
-  });
-});
-
 describe('componentWillLoad()', () => {
-  let linkValidationUtilsSpy;
-
-  beforeEach(() => {
-    linkValidationUtilsSpy = jest
-      .spyOn(linkValidationUtils, 'throwIfInvalidLinkUsage')
-      .mockImplementationOnce(() => {});
-  });
-
   it('should call throwIfInvalidLinkUsage() ', () => {
+    const spy = jest.spyOn(linkValidationUtils, 'throwIfInvalidLinkUsage');
+
     const component = new LinkPure();
     component.host = document.createElement('p-link-pure');
+    component.href = '#';
     component.componentWillLoad();
 
-    expect(linkValidationUtilsSpy).toBeCalledWith(component.host, component.href);
+    expect(spy).toBeCalledWith(component.host, component.href);
   });
 
   it('should call improveFocusHandlingForCustomElement() ', () => {
     const focusHandlingSpy = jest.spyOn(focusHandling, 'improveFocusHandlingForCustomElement');
+
     const component = new LinkPure();
     component.host = document.createElement('p-link-pure');
+    component.href = '#';
     component.componentWillLoad();
 
     expect(focusHandlingSpy).toBeCalledWith(component.host);
