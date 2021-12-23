f75dc50f4a1bf83b2b86644aa188532f5e3c9c0b
Matthias Haegele <matthias.haegele@mhp.com>
2021-12-23 12:17:00 +0100

    feat(shared): change focusable detection | mh | #1686

--- packages/shared/scripts/generateComponentMeta.ts
+++ packages/shared/scripts/generateComponentMeta.ts
@@ -49,10 +49,8 @@ const generateComponentMeta = (): void => {
   }, {} as { [key in TagName]: string });
 
   // simple (mostly atomic) focusable components are identified here
-  const atomicFocusableTagNames: TagName[] = TAG_NAMES.filter(
-    (tagName) =>
-      componentSourceCode[tagName].includes('improveFocusHandlingForCustomElement(') ||
-      componentSourceCode[tagName].includes('<button')
+  const atomicFocusableTagNames: TagName[] = TAG_NAMES.filter((tagName) =>
+    componentSourceCode[tagName].includes('delegatesFocus: true')
   );
 
   const meta: ComponentsMeta = TAG_NAMES.reduce((result, tagName) => {
