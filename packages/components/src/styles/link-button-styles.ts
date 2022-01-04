34f664bd1e8c25a8dab77c009610707e76d471f0
Stephan Schröter <stephan.schroeter@mhp.com>
2022-01-04 14:42:02 +0100

    refactor(components): simplify link + button styles | sas

--- packages/components/src/styles/link-button-styles.ts
+++ packages/components/src/styles/link-button-styles.ts
@@ -9,7 +9,6 @@ import {
   getThemedColorsDarken,
   getTransition,
   isDark,
-  mergeDeep,
   pxToRemWithUnit,
 } from '../utils';
 import { color } from '@porsche-design-system/utilities';
@@ -85,73 +84,64 @@ const getVariantColors = (
   return colors[theme][variant];
 };
 
-// TODO: can be optimized by reducing getVisibilityStyle + getSlottedLinkStyles depending on hasHref prop
-export const getVisibilityStyle = (visible: boolean): JssStyle => {
-  return visible
+const linkButtonPadding = `${pxToRemWithUnit(11)} ${pxToRemWithUnit(15)} ${pxToRemWithUnit(11)} ${pxToRemWithUnit(39)}`;
+
+export const getRootStyles: GetStylesFunction = (hideLabel: boolean): JssStyle => {
+  return {
+    padding: hideLabel ? 0 : linkButtonPadding,
+  };
+};
+
+export const getIconStyles: GetStylesFunction = (hideLabel: boolean): JssStyle => {
+  return hideLabel
     ? {
-        width: '100%',
-        height: 'auto',
-        margin: 0,
-        overflow: 'visible',
-        textIndent: 0,
+        left: '50%',
+        top: '50%',
+        transform: 'translate3d(-50%, -50%, 0)',
       }
     : {
+        left: pxToRemWithUnit(11),
+        top: pxToRemWithUnit(11),
+        transform: 'translate3d(0,0,0)',
+      };
+};
+
+export const getLabelStyles: GetStylesFunction = (hideLabel: boolean): JssStyle => {
+  return hideLabel
+    ? {
         width: 1,
         height: 1,
         margin: '0 0 0 -1px',
         overflow: 'hidden',
         textIndent: -1,
-      };
-};
-
-const linkPadding = `${pxToRemWithUnit(11)} ${pxToRemWithUnit(15)} ${pxToRemWithUnit(11)} ${pxToRemWithUnit(39)}`;
-
-export const getRootStyles: GetStylesFunction = (hideLabel: boolean): JssStyle => ({
-  root: {
-    padding: hideLabel ? 0 : linkPadding,
-  },
-});
-
-export const getIconLabelStyles: GetStylesFunction = (hideLabel: boolean): JssStyle => {
-  return hideLabel
-    ? {
-        icon: {
-          left: '50%',
-          top: '50%',
-          transform: 'translate3d(-50%, -50%, 0)',
-        },
-        label: getVisibilityStyle(!hideLabel),
       }
     : {
-        icon: {
-          left: pxToRemWithUnit(11),
-          top: pxToRemWithUnit(11),
-          transform: 'translate3d(0,0,0)',
-        },
-        label: getVisibilityStyle(!hideLabel),
+        width: '100%',
+        height: 'auto',
+        margin: 0,
+        overflow: 'visible',
+        textIndent: 0,
       };
 };
 
 export const getSlottedLinkStyles: GetStylesFunction = (hideLabel: boolean): JssStyle => {
-  return addImportantToEachRule({
-    '::slotted(a)': hideLabel
-      ? {
-          position: 'absolute',
-          ...getInset(),
-          padding: 0,
-          overflow: 'hidden',
-          whiteSpace: 'nowrap',
-          textIndent: '99999px',
-        }
-      : {
-          position: 'static',
-          ...getInset('auto'),
-          padding: linkPadding,
-          overflow: 'visible',
-          whiteSpace: 'normal',
-          textIndent: 0,
-        },
-  });
+  return hideLabel
+    ? {
+        position: 'absolute',
+        ...getInset(),
+        padding: 0,
+        overflow: 'hidden',
+        whiteSpace: 'nowrap',
+        textIndent: '99999px',
+      }
+    : {
+        position: 'static',
+        ...getInset('auto'),
+        padding: linkButtonPadding,
+        overflow: 'visible',
+        whiteSpace: 'normal',
+        textIndent: 0,
+      };
 };
 
 export const getLinkButtonStyles = (
@@ -167,83 +157,82 @@ export const getLinkButtonStyles = (
   const { disabledColor } = getThemedColors(theme);
   const iconLabelColor = isDisabledOrLoading ? (isTertiary ? disabledColor : 'rgba(255, 255, 255, 0.55)') : baseColor;
 
-  return mergeDeep<Styles>(
-    {
-      ':host': {
-        display: 'inline-flex',
-        verticalAlign: 'top',
-      },
-      root: {
-        display: 'flex',
-        width: '100%',
-        minWidth: pxToRemWithUnit(48),
-        minHeight: pxToRemWithUnit(48),
-        position: 'relative',
-        margin: 0,
-        padding: 0,
-        boxSizing: 'border-box',
-        appearance: 'none',
-        textDecoration: 'none',
-        textAlign: 'left',
-        border: '1px solid currentColor',
-        backgroundColor: isTertiary ? 'transparent' : 'currentColor',
-        cursor: isDisabledOrLoading ? 'not-allowed' : 'pointer',
-        color: isDisabledOrLoading ? disabledColor : primaryColor,
-        transition:
-          getTransition('background-color') + ',' + getTransition('border-color') + ',' + getTransition('color'),
-        ...(!hasSlottedAnchor && getFocusStyles()),
-        ...(!isDisabledOrLoading && {
-          '&:hover, &:active': {
-            color: primaryColorHover,
-            ...(isTertiary && {
-              backgroundColor: 'currentColor',
-              '& $label, & $icon': {
-                color: isDarkTheme ? color.default : darkTheme.default,
-              },
-            }),
-          },
-        }),
-      },
-      icon: {
-        position: 'absolute',
-        width: pxToRemWithUnit(24),
-        height: pxToRemWithUnit(24),
-        color: iconLabelColor,
-        pointerEvents: 'none',
-      },
-      label: {
-        display: 'block',
-        boxSizing: 'border-box',
-        color: iconLabelColor,
-      },
-      ...(hasSlottedAnchor &&
-        addImportantToEachRule({
-          '::slotted(a)': {
-            display: 'block',
-            textDecoration: 'none',
-            color: 'inherit',
-            lineHeight: 'inherit',
-            outline: 'transparent solid 1px',
-            outlineOffset: '3px',
-          },
-          '::slotted(a::-moz-focus-inner)': {
-            border: 0,
-          },
-          '::slotted(a:focus)': {
-            outlineColor: primaryColor,
-          },
-          '::slotted(a:hover:focus)': {
-            outlineColor: primaryColorHover,
-          },
-          '::slotted(a:focus:not(:focus-visible))': {
-            outlineColor: 'transparent',
-          },
-        })),
+  return {
+    ':host': {
+      display: 'inline-flex',
+      verticalAlign: 'top',
     },
-    // TODO: would be better to handle one responsive style prop by one style function
-    buildResponsiveStyles(hideLabel, getIconLabelStyles),
-    !hasSlottedAnchor
-      ? buildResponsiveStyles(hideLabel, getRootStyles)
-      : buildResponsiveStyles(hideLabel, getSlottedLinkStyles)
-  );
+    root: {
+      display: 'flex',
+      width: '100%',
+      minWidth: pxToRemWithUnit(48),
+      minHeight: pxToRemWithUnit(48),
+      position: 'relative',
+      margin: 0,
+      padding: 0,
+      boxSizing: 'border-box',
+      appearance: 'none',
+      textDecoration: 'none',
+      textAlign: 'left',
+      border: '1px solid currentColor',
+      backgroundColor: isTertiary ? 'transparent' : 'currentColor',
+      cursor: isDisabledOrLoading ? 'not-allowed' : 'pointer',
+      color: isDisabledOrLoading ? disabledColor : primaryColor,
+      transition:
+        getTransition('background-color') + ',' + getTransition('border-color') + ',' + getTransition('color'),
+      ...(!hasSlottedAnchor && {
+        ...buildResponsiveStyles(hideLabel, getRootStyles),
+        ...getFocusStyles(),
+      }),
+      ...(!isDisabledOrLoading && {
+        '&:hover, &:active': {
+          color: primaryColorHover,
+          ...(isTertiary && {
+            backgroundColor: 'currentColor',
+            '& $label, & $icon': {
+              color: isDarkTheme ? color.default : darkTheme.default,
+            },
+          }),
+        },
+      }),
+    },
+    icon: {
+      position: 'absolute',
+      width: pxToRemWithUnit(24),
+      height: pxToRemWithUnit(24),
+      color: iconLabelColor,
+      pointerEvents: 'none',
+      ...buildResponsiveStyles(hideLabel, getIconStyles),
+    },
+    label: {
+      display: 'block',
+      boxSizing: 'border-box',
+      color: iconLabelColor,
+      ...buildResponsiveStyles(hideLabel, getLabelStyles),
+    },
+    ...(hasSlottedAnchor &&
+      addImportantToEachRule({
+        '::slotted(a)': {
+          display: 'block',
+          textDecoration: 'none',
+          color: 'inherit',
+          lineHeight: 'inherit',
+          outline: 'transparent solid 1px',
+          outlineOffset: '3px',
+          ...buildResponsiveStyles(hideLabel, getSlottedLinkStyles),
+        },
+        '::slotted(a::-moz-focus-inner)': {
+          border: 0,
+        },
+        '::slotted(a:focus)': {
+          outlineColor: primaryColor,
+        },
+        '::slotted(a:hover:focus)': {
+          outlineColor: primaryColorHover,
+        },
+        '::slotted(a:focus:not(:focus-visible))': {
+          outlineColor: 'transparent',
+        },
+      })),
+  };
 };
