b7235064e8ede9a0ef5f772b57771785b5ce5608
Stephan Schröter <stephan.schroeter@mhp.com>
2022-01-04 13:28:42 +0100

    test(components): update style snapshot for link | sas

--- packages/components/src/components/navigation/link/link-styles.spec.ts
+++ packages/components/src/components/navigation/link/link-styles.spec.ts
@@ -3,18 +3,18 @@ import { BreakpointCustomizable, LinkVariant, Theme } from '../../../types';
 
 describe('getComponentCss()', () => {
   it.each<[LinkVariant, BreakpointCustomizable<boolean>, boolean, Theme]>([
-    ['primary', false, false, 'light'],
-    ['primary', false, false, 'dark'],
-    ['secondary', false, false, 'light'],
-    ['secondary', false, false, 'dark'],
-    ['tertiary', false, false, 'light'],
-    ['tertiary', false, false, 'dark'],
     ['primary', false, true, 'light'],
-    ['primary', { base: true, xs: false, s: true, m: false, l: true, xl: false }, true, 'dark'],
+    ['primary', false, true, 'dark'],
+    ['secondary', false, true, 'light'],
+    ['secondary', false, true, 'dark'],
+    ['tertiary', false, true, 'light'],
+    ['tertiary', false, true, 'dark'],
+    ['primary', false, false, 'light'],
+    ['primary', { base: true, xs: false, s: true, m: false, l: true, xl: false }, false, 'dark'],
   ])(
-    'should return correct css for variant: %s, hideLabel: %s, hasRef: %s and theme: %s',
-    (variant, hideLabel, hasHref, theme) => {
-      expect(getComponentCss(variant, hideLabel, hasHref, theme)).toMatchSnapshot();
+    'should return correct css for variant: %s, hideLabel: %s, hasSlottedAnchor: %s and theme: %s',
+    (variant, hideLabel, hasSlottedAnchor, theme) => {
+      expect(getComponentCss(variant, hideLabel, hasSlottedAnchor, theme)).toMatchSnapshot();
     }
   );
 });
