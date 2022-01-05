5d959799fc5c0a7e96a74461b1b29faa733fa67a
Marcus Zierke <marcus.zierke@constafff.com>
2022-01-05 12:48:57 +0100

    feat: bug fixes for headline-styles | mz

--- packages/components/src/components/basic/typography/headline/headline-styles.ts
+++ packages/components/src/components/basic/typography/headline/headline-styles.ts
@@ -6,67 +6,20 @@ import {
   getThemedColors,
   mergeDeep,
   JssStyle,
-  mediaQuery as query,
   Styles,
-  pxToRemWithUnit,
-  calculateLineHeight,
 } from '../../../../utils';
 import { HeadlineVariant, TextAlign, TextColor, Theme, VariantType } from '../../../../types';
-import { mediaQuery, font } from '@porsche-design-system/utilities';
-
-export type MinToMaxBreakpoint = 'base' | 'sm' | 'ml' | 'lxl' | 'xl';
-
-const getMediaQueryMultiple: { [key in MinToMaxBreakpoint]: number } = {
-  base: 0,
-  sm: 1,
-  ml: 2,
-  lxl: 3,
-  xl: 4,
-};
-
-const variantFontSizeMapper: { [key in VariantType]: { base: number; step: number } } = {
-  'large-title': { base: 32, step: 10 },
-  'headline-1': { base: 28, step: 8 },
-  'headline-2': { base: 24, step: 6 },
-  'headline-3': { base: 20, step: 4 },
-  'headline-4': { base: 16, step: 2 },
-  'headline-5': { base: 16, step: 0 },
-};
-
-const getFontSize = (variant: HeadlineVariant, area: MinToMaxBreakpoint): number => {
-  return (
-    variantFontSizeMapper[variant as VariantType].base +
-    getMediaQueryMultiple[area] * variantFontSizeMapper[variant as VariantType].step
-  );
-};
+import { title, headline } from '@porsche-design-system/utilities';
 
 const getVariantStyle = (variant: HeadlineVariant): JssStyle => {
   if (variant === 'inherit') {
     return { fontSize: 'inherit' };
   }
-
-  return {
-    fontSize: pxToRemWithUnit(getFontSize(variant, 'base')),
-    lineHeight: calculateLineHeight(getFontSize(variant, 'base')),
-    fontWeight: font.weight.semibold, // to root since it never changes?
-    fontFamily: font.family, // to root since it never changes?
-    [mediaQuery('s', 'm')]: {
-      fontSize: pxToRemWithUnit(getFontSize(variant, 'sm')),
-      lineHeight: calculateLineHeight(getFontSize(variant, 'sm')),
-    },
-    [mediaQuery('m', 'l')]: {
-      fontSize: pxToRemWithUnit(getFontSize(variant, 'ml')),
-      lineHeight: calculateLineHeight(getFontSize(variant, 'ml')),
-    },
-    [mediaQuery('l', 'xl')]: {
-      fontSize: pxToRemWithUnit(getFontSize(variant, 'lxl')),
-      lineHeight: calculateLineHeight(getFontSize(variant, 'lxl')),
-    },
-    [query('xl')]: {
-      fontSize: pxToRemWithUnit(getFontSize(variant, 'xl')),
-      lineHeight: calculateLineHeight(getFontSize(variant, 'xl')),
-    },
-  };
+  if (variant === 'large-title') {
+    return title.large;
+  }
+  const headlineVariant = variant as VariantType;
+  return headline[headlineVariant.slice(-1)];
 };
 
 export const getComponentCss = (
@@ -103,17 +56,15 @@ export const getComponentCss = (
       root: {
         padding: 0,
         margin: 0,
-        textAlign: 'left',
+        webkitTextSizeAdjust: 'none', // stop iOS safari from adjusting font size when screen rotation is changing
+        align,
         color: baseColor,
         overflowWrap: 'break-word',
         wordWrap: 'break-word',
         hyphens: 'auto',
         whiteSpace: 'inherit',
-        // use typ.ts definitions for title & headline
         ...getVariantStyle(variant),
-        // condition for color & align
-        ...(align && { textAlign: align }),
-        ...(color && { color: 'inherit' }),
+        ...(color !== 'default' && { color: 'inherit' }),
         ...(ellipsis && {
           maxWidth: '100%',
           overflow: 'hidden',
