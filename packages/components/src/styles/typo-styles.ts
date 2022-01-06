9ee1c4ee46a3f39ab464964b04f49e326cf38d09
Marcus Zierke <marcus.zierke@constafff.com>
2022-01-06 13:11:26 +0100

    feat(component): bug fix for text component to pass vrt tests | mz

--- packages/components/src/styles/typo-styles.ts
+++ packages/components/src/styles/typo-styles.ts
@@ -1,21 +1,28 @@
-export const getDefaultSlottedTypoStyles = {
-  margin: 'inherit',
-  padding: 'inherit',
-  fontFamily: 'inherit',
-  fontWeight: 'inherit',
-  fontSize: 'inherit',
-  lineHeight: 'inherit',
-  color: 'inherit',
-  textAlign: 'inherit',
-  overflowWrap: 'inherit',
-  wordWrap: 'inherit',
-  hyphens: 'inherit',
-  whiteSpace: 'inherit',
+import { JssStyle } from 'jss';
+
+export const getDefaultSlottedTypoStyles = (): JssStyle => {
+  return {
+    margin: 'inherit',
+    padding: 'inherit',
+    fontFamily: 'inherit',
+    fontWeight: 'inherit',
+    fontSize: 'inherit',
+    lineHeight: 'inherit',
+    fontStyle: 'normal',
+    color: 'inherit',
+    textAlign: 'inherit',
+    overflowWrap: 'inherit',
+    wordWrap: 'inherit',
+    hyphens: 'inherit',
+    whiteSpace: 'inherit',
+  };
 };
 
-export const getDefaultEllipsisStyles = {
-  maxWidth: '100%',
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-  whiteSpace: 'nowrap',
+export const getDefaultEllipsisStyles = (): JssStyle => {
+  return {
+    maxWidth: '100%',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+  };
 };
