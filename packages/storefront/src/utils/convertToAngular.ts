b03b525ce3bd42bf339f512a2aa37f987e59642e
Matthias Haegele <matthias.haegele@mhp.com>
2022-01-10 16:09:27 +0100

    feat(storefront): split angular codeblock formatting into seperate file / functions | mh | #1686

---
+++ packages/storefront/src/utils/convertToAngular.ts
@@ -0,0 +1,53 @@
+import { camelCase } from 'change-case';
+
+export const transformEvents = (markup: string): string =>
+  markup.replace(/\son(.+?)="(.*?)"/g, (m, $key, $value) => {
+    return ` (${$key})="${$value}"`;
+  });
+
+export const transformAttributesWithObjectValues = (markup: string): string =>
+  markup.replace(/\s([a-z-]+)="{(.*?)}"/g, (m, $key, $value) => {
+    return ` [${camelCase($key)}]="{${$value}}"`;
+  });
+
+export const transformAttributesWithNotDigitValue = (markup: string): string =>
+  markup.replace(/\s([a-z-]+)="([^-\d].*?)"/g, (m, $key, $value) => {
+    if ($key.startsWith('aria-')) {
+      // handle aria attributes
+      return ` ${$key}="${$value}"`;
+    } else {
+      return ` [${camelCase($key)}]="'${$value}'"`;
+    }
+  });
+
+export const transformAttributesWithDigitValue = (markup: string): string => {
+  return markup.replace(/\s([a-z-]+)="(-?\d*)"/g, (m, $key, $value) => {
+    if ($key === 'name') {
+      // surround numeric "name" attribute values with single quotes
+      return ` [${$key}]="'${$value}'"`;
+    } else {
+      return ` [${camelCase($key)}]="${$value}"`;
+    }
+  });
+};
+
+export const cleanBooleanValues = (markup: string): string => {
+  // remove single quotes from boolean values
+  return markup.replace(/\s(\[[A-Za-z]+\])="'(true|false)'"/g, ' $1="$2"');
+};
+
+export const cleanClassAndSlotAttributes = (markup: string): string => {
+  // remove brackets from "class" and "slot("|slot) attributes
+  return markup.replace(/\s\[(class|slot)]="'(.*?)'"/g, ' $1="$2"');
+};
+
+export const convertToAngular = (markup: string): string => {
+  return [
+    transformEvents,
+    transformAttributesWithObjectValues,
+    transformAttributesWithNotDigitValue,
+    transformAttributesWithDigitValue,
+    cleanBooleanValues,
+    cleanClassAndSlotAttributes,
+  ].reduce((previousResult, fn) => fn(previousResult), markup);
+};
--- packages/storefront/src/utils/index.ts
+++ packages/storefront/src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './code-highlighting';
+export * from './convertToAngular';
 export * from './formatting';
 export * from './validation';
