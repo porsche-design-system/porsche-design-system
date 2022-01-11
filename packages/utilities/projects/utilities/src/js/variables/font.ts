67858b250ebb8a49713ffd601541c9634a90a729
Bernd Hacker <hacker.bernd@gmail.com>
2022-01-11 09:43:07 +0100

    perf(utilities): reduce redundancy of fontSize object | bh

--- packages/utilities/projects/utilities/src/js/variables/font.ts
+++ packages/utilities/projects/utilities/src/js/variables/font.ts
@@ -42,40 +42,47 @@ export const fontWeight: { [key in FontWeight]: number } = {
   semibold: 600,
   bold: 700,
 };
+
+// To boost performance, size is defined static.
+const xSmall: FontSizeLineHeight = { fontSize: '0.75rem', lineHeight: 1.6666666667 };
+const small: FontSizeLineHeight = { fontSize: '1rem', lineHeight: 1.5 };
+const medium: FontSizeLineHeight = { fontSize: '1.5rem', lineHeight: 1.5 };
+const large: FontSizeLineHeight = { fontSize: '2.25rem', lineHeight: 1.3333333333 };
+const xLarge: FontSizeLineHeight = { fontSize: '3.25rem', lineHeight: 1.2307692308 };
+
 export const fontSize: { [key in FontSize]: FontSizeLineHeight } = {
-  '12': { fontSize: '0.75rem', lineHeight: 1.6666666667 },
-  '16': { fontSize: '1rem', lineHeight: 1.5 },
+  '12': xSmall,
+  '16': small,
   '18': { fontSize: '1.125rem', lineHeight: 1.5555555556 },
   '20': { fontSize: '1.25rem', lineHeight: 1.4 },
   '22': { fontSize: '1.375rem', lineHeight: 1.4545454545 },
-  '24': { fontSize: '1.5rem', lineHeight: 1.5 },
+  '24': medium,
   '28': { fontSize: '1.75rem', lineHeight: 1.4285714286 },
   '30': { fontSize: '1.875rem', lineHeight: 1.3333333333 },
   '32': { fontSize: '2rem', lineHeight: 1.375 },
-  '36': { fontSize: '2.25rem', lineHeight: 1.3333333333 },
+  '36': large,
   '42': { fontSize: '2.625rem', lineHeight: 1.2380952381 },
   '44': { fontSize: '2.75rem', lineHeight: 1.1818181818 },
   '48': { fontSize: '3rem', lineHeight: 1.25 },
-  '52': { fontSize: '3.25rem', lineHeight: 1.2307692308 },
+  '52': xLarge,
   '60': { fontSize: '3.75rem', lineHeight: 1.2 },
   '62': { fontSize: '3.875rem', lineHeight: 1.2258064516 },
   '72': { fontSize: '4.5rem', lineHeight: 1.2222222222 },
   '84': { fontSize: '5.25rem', lineHeight: 1.1904761905 },
-  xSmall: { fontSize: '0.75rem', lineHeight: 1.6666666667 },
-  small: { fontSize: '1rem', lineHeight: 1.5 },
-  medium: { fontSize: '1.5rem', lineHeight: 1.5 },
-  large: { fontSize: '2.25rem', lineHeight: 1.3333333333 },
-  xLarge: { fontSize: '3.25rem', lineHeight: 1.2307692308 },
+  xSmall,
+  small,
+  medium,
+  large,
+  xLarge,
 };
 
 export const font: FontType = {
   family: fontFamily,
   weight: fontWeight,
-  // To boost performance, size is defined static.
   size: fontSize,
 };
 
 export const defaultFontFamilyAndWeight: Pick<Properties, 'fontFamily' | 'fontWeight'> = {
   fontFamily,
-  fontWeight: fontWeight.regular,
+  fontWeight: 400,
 };
