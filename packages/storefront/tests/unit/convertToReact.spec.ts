29a9dc78184041a39dc4d0f11e356ea4dd005012
Matthias Haegele <matthias.haegele@mhp.com>
2022-01-11 10:39:47 +0100

    test(storefront): add tests for transformation functions | mh | #1686

--- packages/storefront/tests/unit/convertToReact.spec.ts
+++ packages/storefront/tests/unit/convertToReact.spec.ts
@@ -1,40 +1,159 @@
-import { convertToReact } from '../../src/utils';
+import {
+  convertToReact,
+  transformBooleanAndDigitValues,
+  transformClassAttribute,
+  transformCustomElementTagName,
+  transformEventsToReactSyntax,
+  transformInputs,
+  transformObjectValues,
+  transformStandardAttributes,
+  transformStyleAttribute,
+  transformToSelfClosingTags,
+} from '../../src/utils';
+import * as reactUtils from '../../src/utils/convertToReact';
 
-describe('convertToReact()', () => {
-  it('should convert markup to React syntax', () => {
-    const markup = `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" digit-attribute="6" boolean-attribute="true">
-  <span>some text</span>
+const markup = `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" onchange="alert('change'); return false;" digit-attribute="6" boolean-attribute="true">
+  <span>Some text</span>
+  <input type="checkbox">
+  <button type="button"></button>
 </p-some-tag>`;
 
-    expect(convertToReact(markup)).toBe(
-      `<PSomeTag someAttribute="some value" attribute="some value" className="some-class" anotherAttribute={{ bar: 'foo' }} onClick={() => { alert('click'); return false; }} digitAttribute={6} booleanAttribute={true}>
-  <span>some text</span>
+describe('transformObjectValues()', () => {
+  it('should remove quotes and add double brackets to value, transform attribute to camelCase', () => {
+    expect(transformObjectValues(markup)).toBe(
+      `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" anotherAttribute={{ bar: 'foo' }} onclick="alert('click'); return false;" onchange="alert('change'); return false;" digit-attribute="6" boolean-attribute="true">
+  <span>Some text</span>
+  <input type="checkbox">
+  <button type="button"></button>
+</p-some-tag>`
+    );
+  });
+});
+
+describe('transformStandardAttributes()', () => {
+  it('should transform attributes to camelCase', () => {
+    expect(transformStandardAttributes(markup)).toBe(
+      `<p-some-tag someAttribute="some value" attribute="some value" class="some-class" anotherAttribute="{ bar: 'foo' }" onclick="alert('click'); return false;" onchange="alert('change'); return false;" digitAttribute="6" booleanAttribute="true">
+  <span>Some text</span>
+  <input type="checkbox">
+  <button type="button"></button>
+</p-some-tag>`
+    );
+  });
+});
+
+describe('transformClassAttribute()', () => {
+  it('should transform class to className', () => {
+    expect(transformClassAttribute(markup)).toBe(
+      `<p-some-tag some-attribute="some value" attribute="some value" className="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" onchange="alert('change'); return false;" digit-attribute="6" boolean-attribute="true">
+  <span>Some text</span>
+  <input type="checkbox">
+  <button type="button"></button>
+</p-some-tag>`
+    );
+  });
+});
+
+describe('transformEventsToReactSyntax()', () => {
+  it('should transform events to react event binding syntax', () => {
+    expect(transformEventsToReactSyntax(markup)).toBe(
+      `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onClick={() => { alert('click'); return false; }} onChange={() => { alert('change'); return false; }} digit-attribute="6" boolean-attribute="true">
+  <span>Some text</span>
+  <input type="checkbox">
+  <button type="button"></button>
+</p-some-tag>`
+    );
+  });
+});
+
+describe('transformBooleanAndDigitValues()', () => {
+  it('should remove quotes and add brackets to boolean and digit values', () => {
+    expect(transformBooleanAndDigitValues(markup)).toBe(
+      `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" onchange="alert('change'); return false;" digit-attribute={6} boolean-attribute={true}>
+  <span>Some text</span>
+  <input type="checkbox">
+  <button type="button"></button>
+</p-some-tag>`
+    );
+  });
+});
+
+describe('transformCustomElementTagName()', () => {
+  it('should transform tag-name to PascalCase', () => {
+    expect(transformCustomElementTagName(markup)).toBe(
+      `<PSomeTag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" onchange="alert('change'); return false;" digit-attribute="6" boolean-attribute="true">
+  <span>Some text</span>
+  <input type="checkbox">
+  <button type="button"></button>
 </PSomeTag>`
     );
   });
+});
 
-  it('should add closing slash on input tags', () => {
-    const markup = '<input type="checkbox">';
-    expect(convertToReact(markup)).toBe('<input type="checkbox" />');
+describe('transformInputs()', () => {
+  it('should add closing dash to input', () => {
+    expect(transformInputs(markup)).toBe(
+      `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" onchange="alert('change'); return false;" digit-attribute="6" boolean-attribute="true">
+  <span>Some text</span>
+  <input type="checkbox" />
+  <button type="button"></button>
+</p-some-tag>`
+    );
+  });
+});
+
+describe('transformToSelfClosingTags()', () => {
+  it('should transform tags without children to self-closing', () => {
+    expect(transformToSelfClosingTags(markup)).toBe(
+      `<p-some-tag some-attribute="some value" attribute="some value" class="some-class" another-attribute="{ bar: 'foo' }" onclick="alert('click'); return false;" onchange="alert('change'); return false;" digit-attribute="6" boolean-attribute="true">
+  <span>Some text</span>
+  <input type="checkbox">
+  <button type="button" />
+</p-some-tag>`
+    );
+  });
+});
+
+describe('transformStyleAttribute', () => {
+  it('should quote values', () => {
+    const markup = '<div style="display: block"></div>';
+    expect(transformStyleAttribute(markup)).toBe("<div style={{ display: 'block' }}></div>");
+  });
+  it('should transform properties to camelCase', () => {
+    const markup = '<div style="text-align: center"></div>';
+    expect(transformStyleAttribute(markup)).toBe("<div style={{ textAlign: 'center' }}></div>");
+  });
+  it('should handle multiple styles', () => {
+    const markup = '<div style="text-align: center; font-size: 16px"></div>';
+    expect(transformStyleAttribute(markup)).toBe("<div style={{ textAlign: 'center', fontSize: '16px' }}></div>");
+  });
+});
+
+describe('convertToReact()', () => {
+  it.each([
+    'transformObjectValues',
+    'transformStandardAttributes',
+    'transformClassAttribute',
+    'transformEventsToReactSyntax',
+    'transformBooleanAndDigitValues',
+    'transformCustomElementTagName',
+    'transformInputs',
+    'transformToSelfClosingTags',
+    'transformStyleAttribute',
+  ])('should call %s', (fn) => {
+    const spy = jest.spyOn(reactUtils, fn as any);
+    convertToReact(markup);
+
+    expect(spy).toBeCalledTimes(1);
   });
 
-  it('should transform to self closing tags', () => {
-    expect(convertToReact('<button type="button"></button>')).toBe('<button type="button" />');
-    expect(convertToReact('<PButton type="button"></PButton>')).toBe('<PButton type="button" />');
-  });
-
-  describe('inline styles', () => {
-    it('should quote values', () => {
-      const markup = '<div style="display: block"></div>';
-      expect(convertToReact(markup)).toBe("<div style={{ display: 'block' }} />");
-    });
-    it('should transform properties to camelCase', () => {
-      const markup = '<div style="text-align: center"></div>';
-      expect(convertToReact(markup)).toBe("<div style={{ textAlign: 'center' }} />");
-    });
-    it('should handle multiple styles', () => {
-      const markup = '<div style="text-align: center; font-size: 16px"></div>';
-      expect(convertToReact(markup)).toBe("<div style={{ textAlign: 'center', fontSize: '16px' }} />");
-    });
+  it('should convert markup to React syntax', () => {
+    expect(convertToReact(markup)).toBe(
+      `<PSomeTag someAttribute="some value" attribute="some value" className="some-class" anotherAttribute={{ bar: 'foo' }} onClick={() => { alert('click'); return false; }} onChange={() => { alert('change'); return false; }} digitAttribute={6} booleanAttribute={true}>
+  <span>Some text</span>
+  <input type="checkbox" />
+  <button type="button" />
+</PSomeTag>`
+    );
   });
 });
