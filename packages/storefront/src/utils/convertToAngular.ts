9965ba41b112da3171afba3091bb3a8b77c32441
Matthias Haegele <matthias.haegele@mhp.com>
2022-01-11 15:40:23 +0100

    feat(storefront): remove unneeded function bodys | mh | #1686

--- packages/storefront/src/utils/convertToAngular.ts
+++ packages/storefront/src/utils/convertToAngular.ts
@@ -1,34 +1,22 @@
 import { camelCase } from 'change-case';
 
 export const transformEventsToAngularSyntax = (markup: string): string =>
-  markup.replace(/\son(.+?)="(.*?)"/g, (m, $key, $value) => {
-    return ` (${$key})="${$value}"`;
-  });
+  markup.replace(/\son(.+?)="(.*?)"/g, (m, $key, $value) => ` (${$key})="${$value}"`);
 
 export const transformAttributesWithObjectValues = (markup: string): string =>
-  markup.replace(/\s([a-z-]+)="{(.*?)}"/g, (m, $key, $value) => {
-    return ` [${camelCase($key)}]="{${$value}}"`;
-  });
+  markup.replace(/\s([a-z-]+)="{(.*?)}"/g, (m, $key, $value) => ` [${camelCase($key)}]="{${$value}}"`);
 
 export const transformAttributesWithNotDigitValue = (markup: string): string =>
-  markup.replace(/\s([a-z-]+)="([^-\d].*?)"/g, (m, $key, $value) => {
-    if ($key.startsWith('aria-')) {
-      // handle aria attributes
-      return ` ${$key}="${$value}"`;
-    } else {
-      return ` [${camelCase($key)}]="'${$value}'"`;
-    }
-  });
+  markup.replace(/\s([a-z-]+)="([^-\d].*?)"/g, (m, $key, $value) =>
+    // handle aria attributes
+    $key.startsWith('aria-') ? ` ${$key}="${$value}"` : ` [${camelCase($key)}]="'${$value}'"`
+  );
 
 export const transformAttributesWithDigitValue = (markup: string): string =>
-  markup.replace(/\s([a-z-]+)="(-?\d*)"/g, (m, $key, $value) => {
-    if ($key === 'name') {
-      // surround numeric "name" attribute values with single quotes
-      return ` [${$key}]="'${$value}'"`;
-    } else {
-      return ` [${camelCase($key)}]="${$value}"`;
-    }
-  });
+  markup.replace(/\s([a-z-]+)="(-?\d*)"/g, (m, $key, $value) =>
+    // surround numeric "name" attribute values with single quotes
+    $key === 'name' ? ` [${$key}]="'${$value}'"` : ` [${camelCase($key)}]="${$value}"`
+  );
 
 export const cleanBooleanValues = (markup: string): string =>
   // remove single quotes from boolean values
@@ -38,8 +26,8 @@ export const cleanClassAndSlotAttributes = (markup: string): string =>
   // remove brackets from "class" and "slot("|slot) attributes
   markup.replace(/\s\[(class|slot)]="'(.*?)'"/g, ' $1="$2"');
 
-export const convertToAngular = (markup: string): string => {
-  return [
+export const convertToAngular = (markup: string): string =>
+  [
     transformEventsToAngularSyntax,
     transformAttributesWithObjectValues,
     transformAttributesWithNotDigitValue,
@@ -47,4 +35,3 @@ export const convertToAngular = (markup: string): string => {
     cleanBooleanValues,
     cleanClassAndSlotAttributes,
   ].reduce((previousResult, fn) => fn(previousResult), markup);
-};
