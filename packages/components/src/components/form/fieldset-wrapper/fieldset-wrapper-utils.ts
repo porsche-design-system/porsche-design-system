42e2018401e35cea554a7fe82bbdf080664ed7bd
Bernd Hacker <hacker.bernd@gmail.com>
2022-01-13 11:46:55 +0100

    Apply suggestions from code review

--- packages/components/src/components/form/fieldset-wrapper/fieldset-wrapper-utils.ts
+++ packages/components/src/components/form/fieldset-wrapper/fieldset-wrapper-utils.ts
@@ -1,3 +1,3 @@
-import { TextSize } from '../../basic/typography/text/text-utils';
+import type { TextSize } from '../../basic/typography/text/text-utils';
 
 export type FieldsetWrapperLabelSize = Extract<TextSize, 'small' | 'medium'>;
