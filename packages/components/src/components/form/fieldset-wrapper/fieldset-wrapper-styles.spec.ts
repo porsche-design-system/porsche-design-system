dbc0d75dc22e8ab7371b3ed27fd9eb90fd2e216e
Stephan Schröter <stephan.schroeter@mhp.com>
2022-01-13 10:09:21 +0100

    refactor(components): align fieldset-wrapper with other form elements regarding style of functional component usage | bh / sas

--- packages/components/src/components/form/fieldset-wrapper/fieldset-wrapper-styles.spec.ts
+++ packages/components/src/components/form/fieldset-wrapper/fieldset-wrapper-styles.spec.ts
@@ -2,18 +2,13 @@ import { getComponentCss } from './fieldset-wrapper-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['none', true, 'medium', true, true],
-    ['none', true, 'medium', true, false],
-    ['none', true, 'medium', false, false],
-    ['none', true, 'medium', true, true],
-    ['none', true, 'small', true, true],
-    ['none', true, 'small', true, true],
-    ['success', true, 'medium', true, true],
-    ['error', true, 'medium', true, true],
-  ])(
-    'should return correct css for state: %s, required: %s, labelSize: %s, hasLabel: %s and hasMessage: %s',
-    (...args) => {
-      expect(getComponentCss(...args)).toMatchSnapshot();
-    }
-  );
+    ['none', 'medium', true],
+    ['none', 'medium', false],
+    ['none', 'small', true],
+    ['none', 'small', false],
+    ['success', 'medium', false],
+    ['error', 'medium', false],
+  ])('should return correct css for state: %s, labelSize: %s and hasLabel: %s', (...args) => {
+    expect(getComponentCss(...args)).toMatchSnapshot();
+  });
 });
