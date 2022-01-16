f9123782d17bdc30e35a90ccb8cd6b2e9a333de4
Marcus Zierke <marcus.zierke@constafff.com>
2022-01-16 18:49:53 +0100

    test(component-js): unit test for tabs-style updated | mz

--- packages/components/src/components/content/tabs/tabs/tabs-styles.spec.ts
+++ packages/components/src/components/content/tabs/tabs/tabs-styles.spec.ts
@@ -1,7 +1,10 @@
 import { getComponentCss } from './tabs-styles';
 
 describe('getComponentCss()', () => {
-  it('should return correct css ', () => {
-    expect(getComponentCss()).toMatchSnapshot();
-  });
+  it.each<Parameters<typeof getComponentCss>>([['light'], ['dark'], ['light-electric']])(
+    'should return correct css for theme: %s ',
+    (...args) => {
+      expect(getComponentCss(...args)).toMatchSnapshot();
+    }
+  );
 });
