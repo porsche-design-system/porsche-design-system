f865db4e6a8b9be42ea7e90d09b27fbcf25101cf
Bernd Hacker <hacker.bernd@gmail.com>
2022-01-17 13:10:06 +0100

    test(components-react): add unit tests for getByRole() | bh | #1605

---
+++ packages/components-react/tests/jsdom-polyfill/specs/shadow.spec.tsx
@@ -0,0 +1,66 @@
+import {
+  componentsReady,
+  PButton,
+  PCheckboxWrapper,
+  PRadioButtonWrapper,
+  PSelectWrapper,
+  PTextareaWrapper,
+  PTextFieldWrapper,
+} from '@porsche-design-system/components-react';
+import { render, fireEvent, getByRole } from '@testing-library/react';
+
+describe('getByRole()', () => {
+  it('should be supported for most form wrappers', async () => {
+    const { getByRole } = render(
+      <>
+        <PCheckboxWrapper label="Checkbox">
+          <input type="checkbox" />
+        </PCheckboxWrapper>
+        <PRadioButtonWrapper label="Radio Button">
+          <input type="radio" />
+        </PRadioButtonWrapper>
+        <PTextFieldWrapper label="Text Field">
+          <input type="text" />
+        </PTextFieldWrapper>
+        <PTextareaWrapper label="Textarea">
+          <textarea />
+        </PTextareaWrapper>
+      </>
+    );
+
+    await componentsReady();
+
+    expect(getByRole('checkbox', { name: 'Checkbox' })).toBeInTheDocument();
+    expect(getByRole('radio', { name: 'Radio Button' })).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'Text Field' })).toBeInTheDocument();
+    expect(getByRole('textbox', { name: 'Textarea' })).toBeInTheDocument();
+  });
+
+  it('should be supported for PButton with role button', async () => {
+    const { getByRole } = render(<PButton role="button">Button</PButton>);
+
+    await componentsReady();
+
+    expect(getByRole('button', { name: 'Button' })).toBeInTheDocument();
+  });
+
+  it('should be supported for PSelectWrapper', async () => {
+    const callback = jest.fn();
+
+    const { getByTestId } = render(
+      <PSelectWrapper>
+        <select data-testid="Select" value="1" onChange={(e) => callback(e.target.value)}>
+          <option value="1">Option 1</option>
+          <option value="2">Option 2</option>
+          <option value="3">Option 3</option>
+        </select>
+      </PSelectWrapper>
+    );
+
+    await componentsReady();
+
+    fireEvent.change(getByTestId('Select'), { target: { value: '2' } });
+
+    expect(callback).toHaveBeenCalledWith('2');
+  });
+});
