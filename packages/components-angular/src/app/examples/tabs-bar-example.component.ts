ccd4ecdd4dde0143548b73f0c3369b08125173fe
Marcus Zierke <marcus.zierke@constafff.com>
2022-01-19 22:37:26 +0100

    refactor(components): example packages refactored | mz

--- packages/components-angular/src/app/examples/tabs-bar-example.component.ts
+++ packages/components-angular/src/app/examples/tabs-bar-example.component.ts
@@ -3,43 +3,6 @@ import type { TabChangeEvent } from '@porsche-design-system/components-angular';
 
 @Component({
   selector: 'page-tabs-bar-example',
-  template: `
-    <p-tabs-bar [activeTabIndex]="tabIndex" (tabChange)="onTabChange($event)">
-      <button type="button" id="tab-item1" aria-controls="tab-panel-1">Tab One</button>
-      <button type="button" id="tab-item2" aria-controls="tab-panel-2">Tab Two</button>
-      <button type="button" id="tab-item3" aria-controls="tab-panel-3">Tab Three</button>
-    </p-tabs-bar>
-
-    /* single quotes leftover */
-    <div
-      id="'tab-panel1"
-      [hidden]="tabIndex === 0 ? false : true"
-      [tabindex]="tabIndex === 0 ? 0 : -1"
-      role="tabpanel"
-      aria-labelledby="tab-item-1"
-    >
-      <p-text>Your content of Tab 1</p-text>
-    </div>
-    /* check attribute syntax for react and anguarl component */
-    <div
-      id="'tab-panel2"
-      [hidden]="tabIndex === 1 ? false : true"
-      [tabindex]="tabIndex === 1 ? 0 : -1"
-      role="tabpanel"
-      aria-labelledby="tab-item-2"
-    >
-      <p-text>Your content of Tab 2</p-text>
-    </div>
-    <div
-      id="'tab-panel3"
-      [hidden]="tabIndex === 2 ? false : true"
-      [tabindex]="tabIndex === 2 ? 0 : -1"
-      role="tabpanel"
-      aria-labelledby="tab-item-3"
-    >
-      <p-text>Your content of Tab 3</p-text>
-    </div>
-  `,
   styles: [
     `
       div[role='tabpanel'] {
@@ -48,9 +11,38 @@ import type { TabChangeEvent } from '@porsche-design-system/components-angular';
         margin-top: 8px;
       }
     `,
-    'div[role=tabpanel]:focus { outline-color: black; }',
-    'div[role=tabpanel]:focus:not(:focus-visible) { outline-color: transparent; }',
+    `
+      div[role='tabpanel']:focus {
+        outline-color: black;
+      }
+    `,
+    `
+      div[role='tabpanel']:focus:not(:focus-visible) {
+        outline-color: transparent;
+      }
+    `,
   ],
+  template: `
+    <p-tabs-bar [activeTabIndex]="tabIndex" (tabChange)="onTabChange($event)">
+      <ng-container *ngFor="let tabPanel of ['One', 'Two', 'Three']; let i = index">
+        <button type="button" [attr.id]="'tab-item-' + (i + 1)" [attr.aria-controls]="'tab-panel-' + (i + 1)">
+          Tab {{ tabPanel }}
+        </button>
+      </ng-container>
+    </p-tabs-bar>
+
+    <ng-container *ngFor="let idx of [0, 1, 2]">
+      <div
+        [attr.id]="'tab-panel-' + (idx + 1)"
+        [hidden]="tabIndex !== idx"
+        [tabindex]="tabIndex === idx ? 0 : -1"
+        role="tabpanel"
+        [attr.aria-labelledby]="'tab-item-' + (idx + 1)"
+      >
+        <p-text>Your content of Tab {{ idx + 1 }}</p-text>
+      </div>
+    </ng-container>
+  `,
 })
 export class TabsBarExampleComponent {
   tabIndex: number = 0;
