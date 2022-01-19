e36ccdd014ce4dfa6baf9e25074aa8eeb8a06954
Bernd Hacker <hacker.bernd@gmail.com>
2022-01-19 12:31:49 +0100

    feat(storefront): extend PartialDocs | bh | #1569

--- packages/storefront/src/components/PartialDocs.vue
+++ packages/storefront/src/components/PartialDocs.vue
@@ -9,10 +9,16 @@
   import { constantCase } from 'change-case';
   import { Framework, FrameworkMarkup } from '@/models';
 
+  type Param = {
+    value: string;
+    comment?: string;
+    usage?: string;
+  };
+
   @Component
   export default class PartialDocs extends Vue {
     @Prop({ default: '' }) public name!: string;
-    @Prop({ default: '' }) public params!: string;
+    @Prop({ default: [{ value: '' }] }) public params!: Param[];
     @Prop({ default: 'body' }) public location!: string;
 
     public get activeFramework(): Framework {
@@ -25,39 +31,47 @@
         this.activeFramework
       );
       const partialImportPathJs = partialImportPath.replace('vanilla-js', 'js').replace(/'/g, '\\"');
-      const paramsJs = this.params.replace(/'/g, '\\"');
+      const paramsJs = this.params.map(({ value }) => value.replace(/'/g, '\\"'))[0];
       const placeholder = `PLACEHOLDER_PORSCHE_DESIGN_SYSTEM_${constantCase(this.name.replace('get', ''))}`;
       const scriptTag = 'script';
 
+      const examplesReact =
+        `<${this.location}>\n  ` +
+        this.params
+          .map(({ value, comment, usage }, index, array) => {
+            const needsTag = value.includes('withoutTags');
+            const openingScriptTag = needsTag ? `<${scriptTag}>` : '';
+            const closingScriptTag = needsTag ? `</${scriptTag}>` : '';
+            const isFirstWithoutTags = index <= array.findIndex(({ value }) => value.includes('withoutTags'));
+
+            return [
+              isFirstWithoutTags && '<!-- Without tags -->',
+              comment && `<!-- ${comment} -->`,
+              usage?.replace('$$$PARTIAL$$$', `${partialImportPath}(${value})`) ||
+                `${openingScriptTag}<%= ${partialImportPath}(${value}) %>${closingScriptTag}`,
+            ]
+              .filter((x) => x)
+              .join('\n  ');
+          })
+          .join('\n\n  ') +
+        `\n</${this.location}>`;
+
+      const examplesJs = `// index.html
+<${this.location}>
+  <!--${placeholder}-->
+</${this.location}>
+
+// package.json (tested on macOS, the script may need to be adjusted depending on the operating system used)
+// make sure to adjust the path to the index.html file
+"scripts": {
+  "prestart": "yarn replace",
+  "replace": "placeholder='<!--${placeholder}-->' && partial=$placeholder$(node -e 'console.log(${partialImportPathJs}(${paramsJs}))') && regex=$placeholder'.*' && sed -i '' -E -e \\"s@$regex@$partial@\\" index.html"
+}`;
+
       return {
-        'vanilla-js': `// index.html
-  <${this.location}>
-    <!--${placeholder}-->
-  </${this.location}>
-
-  // package.json (tested on macOS, the script may need to be adjusted depending on the operating system used)
-  // make sure to adjust the path to the index.html file
-  "scripts": {
-    "prestart": "yarn replace",
-    "replace": "placeholder='<!--${placeholder}-->' && partial=$placeholder$(node -e 'console.log(${partialImportPathJs}(${paramsJs}))') && regex=$placeholder'.*' && sed -i '' -E -e \\"s@$regex@$partial@\\" index.html"
-  }`,
-        react: `// index.html
-
-  <${this.location}>
-    <%= ${partialImportPath}(${this.params}) %>
-
-    // With custom prefix
-    <%= ${partialImportPath}({ prefix: 'my-prefix' }) %>
-
-    // With multiple custom prefixes
-    <%= ${partialImportPath}({ prefix: ['my-prefix', 'another-prefix'] }) %>
-  </${this.location}>
-
-  <${this.location}>
-    // without script tag
-    <${scriptTag}><%= ${partialImportPath}({ withoutTags: true }) %></${scriptTag}>
-  </${this.location}>`,
-        angular: `ok`,
+        'vanilla-js': examplesJs,
+        react: examplesReact,
+        angular: `coming soon`,
       };
     }
   }
