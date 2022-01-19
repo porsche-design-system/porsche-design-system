e28be7559fb2cbf3dfe854afa04066daee5c7cb4
Marcus Zierke <marcus.zierke@constafff.com>
2022-01-19 19:00:25 +0100

    fix(component-js): tabs-item-styles with refactored | mz

--- packages/components/src/components/content/tabs/tabs-item/tabs-item-styles.ts
+++ packages/components/src/components/content/tabs/tabs-item/tabs-item-styles.ts
@@ -1,22 +1,25 @@
-import { addImportantToEachRule, getCss, getThemedColors } from '../../../../utils';
+import { addImportantToEachRule, getCss, getFocusStyles, getThemedColors } from '../../../../utils';
 import type { ThemeExtendedElectric } from '../../../../types';
+import type { JssStyle } from '../../../../utils';
 
 export const getComponentCss = (theme: ThemeExtendedElectric): string => {
   const { baseColor } = getThemedColors(theme);
 
+  const hostFocusStyle: JssStyle = getFocusStyles({ color: baseColor });
+  Object.keys(hostFocusStyle).forEach((item) => {
+    if (item.charAt(0) === '&') {
+      hostFocusStyle[`&(${item.slice(1)})`] = hostFocusStyle[item];
+      delete hostFocusStyle[item];
+    }
+  });
+
   return getCss({
     ':host': addImportantToEachRule({
       display: 'block',
-      outline: 'transparent solid 1px',
       '&([hidden])': {
         display: 'none',
       },
-      '&(:focus)': {
-        outlineColor: baseColor,
-      },
-      '&(:focus:not(:focus-visible))': {
-        outlineColor: 'transparent',
-      },
+      ...hostFocusStyle,
     }),
   });
 };
