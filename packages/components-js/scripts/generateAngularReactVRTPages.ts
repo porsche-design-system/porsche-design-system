61443dbb5547c3833cc9f5775df605f2cc541e24
Fariba Qarib <fariba.qarib@mhp.com>
2022-01-24 12:59:51 +0100

    feat(components-js): implement auto generate angular and react pages | fq / bh | #1153

--- packages/components-js/scripts/generateAngularReactVRTPages.ts
+++ packages/components-js/scripts/generateAngularReactVRTPages.ts
@@ -1,34 +1,50 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { paramCase } from 'change-case';
+import { paramCase, pascalCase } from 'change-case';
 import * as globby from 'globby';
+const rootDirectory = path.resolve(__dirname, '..');
 
 const generateAngularReactVRTPages = (): void => {
-  const rootDirectory = path.resolve(__dirname, '..');
   const pagesDirectory = path.resolve(rootDirectory, './src/pages');
   const htmlFiles = globby.sync(`${pagesDirectory}/**/*.html`);
 
   const htmlFileContentMap: { [key: string]: string } = htmlFiles
     .map((filePath) => [path.basename(filePath).split('.')[0], fs.readFileSync(filePath, 'utf8')])
     .reduce((result, [key, content]) => ({ ...result, [key]: content }), {});
-
+  generateVRTPages(htmlFileContentMap, 'angular');
+  generateVRTPages(htmlFileContentMap, 'react');
+};
+const generateVRTPages = (htmlFileContentMap: { [key: string]: string }, frameWorkType: 'angular' | 'react'): void => {
   Object.entries(htmlFileContentMap)
     .filter((_, i) => i === 0)
     .forEach(([fileName, fileContent]) => {
-      console.log(fileName);
-      console.log(fileContent);
+      if (frameWorkType === 'angular') {
+        fileName = `${fileName}.component.ts`;
+        fileName = path.resolve(rootDirectory, '../components-angular/src/app/pages', fileName);
+
+        fileContent = `<div class="playground light" title="should render accordion on light background">
+                <p-accordion [heading]="'Some heading'">
+                  {{ content }}
+                </p-accordion>
+            </div>`;
+      } else if (frameWorkType === 'react') {
+        fileName = `${pascalCase(fileName)}.tsx`;
+
+        fileName = path.resolve(rootDirectory, '../components-react/src/pages', fileName);
+        fileContent = `
+           <div className="playground light" title="should render accordion on light background">
+              <PAccordion heading="Some heading">{content}</PAccordion>
+            </div>
+
+            <div className="playground dark" title="should render accordion on dark background">
+              <PAccordion heading="Some heading" theme="dark">
+                {content}
+              </PAccordion>
+            </div>`;
+      }
+      fs.writeFileSync(fileName, fileContent);
+      console.log(`Generated ${fileName}`);
     });
-
-  const content = ``;
-
-  // const targetDirectory = path.normalize('./projects/components-wrapper/lib');
-  // fs.mkdirSync(path.resolve(targetDirectory), { recursive: true });
-
-  // const targetFileName = 'chunksManifest.ts';
-  // const targetFile = path.resolve(targetDirectory, targetFileName);
-  // fs.writeFileSync(targetFile, content);
-  //
-  // console.log(`Generated ${targetFileName} for ${Object.keys(manifest).length} chunks`);
 };
 
 generateAngularReactVRTPages();
