d0e01e92bd26ddba99ddb9cb6da6d1ea450ae8ef
PhilippTreupel <code@philipp.studio>
2022-01-28 12:16:52 +0100

    feat(components-js): support jsx for iconLinks partial | pt / mh | #1410

--- packages/components-js/projects/partials/scripts/generateIconLinksPartial.ts
+++ packages/components-js/projects/partials/scripts/generateIconLinksPartial.ts
@@ -10,26 +10,36 @@ type IconLinksOptions = {
   icons?: IconNameCamelCase[];
   cdn?: Cdn;
   withoutTags?: boolean;
+  format?: PartialFormat
 };
 type IconLinksOptionsWithTags = IconLinksOptions & {
   withoutTags?: false;
+  format?: 'html';
 };
 type IconLinksOptionsWithoutTags = IconLinksOptions & {
   withoutTags?: true;
-};`;
+  format?: 'html';
+};
+type IconLinksOptionsJsx = IconLinksOptions & {
+  withoutTags?: false;
+  format?: 'jsx';
+}`;
 
   const link = minifyHTML('<link rel="prefetch" href="${url}" as="image" type="image/svg+xml" crossorigin>');
 
   const func = `export function getIconLinks(opts?: IconLinksOptionsWithTags): string;
 export function getIconLinks(opts?: IconLinksOptionsWithoutTags): string[];
-export function getIconLinks(opts?: IconLinksOptions): string | string[] {
-  const options: IconLinksOptions = {
+export function getIconLinks(opts?: IconLinksOptionsJsx): JSX.Element[];
+export function getIconLinks(opts?: IconLinksOptions): string | string[] | JSX.Element[] {
+  const { icons, cdn, withoutTags, format }: IconLinksOptions = {
     icons: ['arrowHeadRight'],
     cdn: 'auto',
     withoutTags: false,
+    format: 'html',
     ...opts
   };
-  const { icons, cdn, withoutTags } = options;
+
+  deprecationWarningWithoutTags('getIconLinks', withoutTags);
 
   const supportedIconNames: IconNameCamelCase[] = ${JSON.stringify(ICON_NAMES)};
   const invalidIconNames = icons.filter((x) => !supportedIconNames.includes(x));
@@ -45,11 +55,13 @@ Please use only valid icon names:
   const cdnBaseUrl = getCdnBaseUrl(cdn);
   const manifest = ${JSON.stringify(ICONS_MANIFEST)};
   const urls = icons.map((icon) => \`\${cdnBaseUrl}/${CDN_BASE_PATH_ICONS}/\${manifest[icon]}\`);
-  const links = urls
+  const linksHtml = urls
     .map((url) => \`${link}\`)
     .join('');
+  const linksJsx = urls.map((url) => <link rel="prefetch" href={url} as="image" type="image/svg+xml" crossOrigin="true" />);
 
-  return withoutTags ? urls : links;
+  const markup = format === 'html' ? linksHtml : linksJsx
+  return withoutTags ? urls : markup;
 };`;
 
   return [types, func].join('\n\n');
