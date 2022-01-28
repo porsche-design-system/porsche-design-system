68da43a46b384a67487178caeafa587f98522c95
PhilippTreupel <code@philipp.studio>
2022-01-28 12:17:15 +0100

    feat(components-js): support jsx for loaderScript partial | pt / mh | #1410

--- packages/components-js/projects/partials/scripts/generateLoaderScriptPartial.ts
+++ packages/components-js/projects/partials/scripts/generateLoaderScriptPartial.ts
@@ -6,20 +6,32 @@ export const generateLoaderScriptPartial = (): string => {
   const types = `type LoaderScriptOptions = {
   prefix?: string | string[];
   withoutTags?: boolean;
-}`;
+  format?: PartialFormat
+}
+type LoaderScriptOptionsHtml = LoaderScriptOptions & {
+  format?: 'html';
+};
+type LoaderScriptOptionsJsx = LoaderScriptOptions & {
+   withoutTags?: 'false';
+   format?: 'jsx';
+};`;
 
   const componentsJsFilePath = require.resolve('@porsche-design-system/components-js');
   const packageDir = path.resolve(path.dirname(componentsJsFilePath), '../..');
   const tmpFilePath = path.resolve(packageDir, npmDistTmpSubPath, 'index.js');
   const fileContent = fs.readFileSync(tmpFilePath, 'utf8');
 
-  const func = `export const getLoaderScript = (opts?: LoaderScriptOptions): string => {
-  const options: LoaderScriptOptions = {
+  const func = `export function getLoaderScript(opts?: LoaderScriptOptionsHtml): string;
+export function getLoaderScript(opts?: LoaderScriptOptionsJsx): JSX.Element;
+export function getLoaderScript(opts?: LoaderScriptOptions): string | JSX.Element {
+  const { prefix, withoutTags, format }: LoaderScriptOptions = {
     prefix: undefined,
     withoutTags: false,
+    format: 'html',
     ...opts
   };
-  const { prefix, withoutTags } = options;
+
+  deprecationWarningWithoutTags('getLoaderScript', withoutTags);
 
   const loadCalls = prefix
     ? Array.isArray(prefix)
@@ -28,7 +40,11 @@ export const generateLoaderScriptPartial = (): string => {
     : 'porscheDesignSystem.load()';
   const scriptContent = ${JSON.stringify(fileContent)} + loadCalls;
 
-  return withoutTags ? scriptContent : \`<script>\${scriptContent}</script>\`;
+  // there is no other solution than using dangerouslySetInnerHTML since JSX elements are rendered by the createElement() function
+  // https://stackoverflow.com/a/64815699
+  const markup = format === 'html' ? \`<script>\${scriptContent}</script>\` : <script dangerouslySetInnerHTML={{__html: scriptContent}} />;
+
+  return withoutTags ? scriptContent : markup;
 };`;
 
   return [types, func].join('\n\n');
