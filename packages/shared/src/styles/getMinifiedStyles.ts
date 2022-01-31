fcfb7c6d6d452ac7f1e1cba3a07434e41a9c3015
Anton Schmidt <anton.schmidt@mhp.com>
2022-01-31 14:00:33 +0100

    feat(shared): implement same functionality in getMinifiedStyles as in getCss, adjust tests | as / mz | #1339

--- packages/shared/src/styles/getMinifiedStyles.ts
+++ packages/shared/src/styles/getMinifiedStyles.ts
@@ -1,21 +1,26 @@
-import type { Styles } from 'jss';
+import type { Rule, Styles } from 'jss';
 import jss from 'jss';
 import preset from 'jss-preset-default';
+import jssPluginSortMediaQueries from 'jss-plugin-sort-css-media-queries';
+
 // @ts-ignore
-jss.setup({ ...preset(), createGenerateId: () => (rule: Rule) => rule.key });
+jss.setup({
+  ...preset(),
+  createGenerateId: () => (rule: Rule) => rule.key,
+  plugins: [...preset().plugins, jssPluginSortMediaQueries({ combineMediaQueries: true })],
+});
 
 export const getMinifiedStyles = (style: Styles): string => {
   return jss
     .createStyleSheet(style)
     .toString()
-    .replace(/\\+/g, '') // remove backslashes
-    .replace(/(media)\s/g, '$1') // remove space after media
-    .replace(/;(?=\s+})/g, '') // remove semicolon before closing curly brace
+    .replace(/\.\\(?=:)/g, '') // remove default '.'
+    .replace(/[\n\\]+/g, '') // remove backslashes
     .replace(/\s(?={)/g, '') // remove space before opening curly brace
+    .replace(/;(?=\s*})/g, '') // remove semicolon before closing curly brace
+    .replace(/(media)\s/g, '$1') // remove space after media
     .replace(/,\s/g, ',') // remove unneeded white space after comma separation
     .replace(/\s\s+/g, '') // remove white space
     .replace(/:\s(?=.*)/g, ':') // remove white space after colon
     .replace(/\n+/g, ''); // remove new line
 };
-// TODO: remove dot before colon
-// TODO: sort props
