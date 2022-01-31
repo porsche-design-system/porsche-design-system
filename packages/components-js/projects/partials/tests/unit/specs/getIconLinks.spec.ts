266a6175a7c33910784338ba7931aaad3f5f88a6
Matthias Haegele <matthias.haegele@mhp.com>
2022-01-31 16:10:55 +0100

    test(components-js): jsx output / deprecation warning of partials | mh | #1410

--- packages/components-js/projects/partials/tests/unit/specs/getIconLinks.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getIconLinks.spec.ts
@@ -58,21 +58,68 @@ describe('getIconLinks()', () => {
     });
   });
 
+  describe('format jsx', () => {
+    it('should return "arrowHeadRight" link by default', () => {
+      const result = getIconLinks();
+      expect(result).toMatch(getIconLinkRegEx('arrowHeadRight'));
+    });
+
+    it('should return multiple links', () => {
+      const result = getIconLinks({ format: 'jsx', icons: ['truck', 'volumeUp', 'mobile'] });
+      result.forEach((element) => {
+        // href is variable due to hash and cant be overwritten so we clone the element
+        const link = { ...element, props: { ...element.props, href: getUrl('truck', 'auto') } };
+        expect(link).toMatchSnapshot();
+      });
+    });
+
+    ICON_NAMES.forEach((iconName: IconNameCamelCase) => {
+      it(`should return icon link for ['${iconName}']`, () => {
+        const result = getIconLinks({ format: 'jsx', icons: [iconName] });
+        result.forEach((element) => {
+          // href is variable due to hash and cant be overwritten so we clone the element
+          const link = { ...element, props: { ...element.props, href: getUrl(iconName, 'auto') } };
+          expect(link).toMatchSnapshot();
+        });
+      });
+    });
+  });
+
   describe('url without tag', () => {
+    let consoleWarnSpy;
+
+    beforeEach(() => (consoleWarnSpy = jest.spyOn(global.console, 'warn').mockImplementation(() => {})));
+    afterEach(() => jest.clearAllMocks());
+
     it('should return "arrowHeadRight" url by default', () => {
       const result = getIconLinks({ withoutTags: true });
+
+      expect(consoleWarnSpy).toBeCalledWith(
+        'The option "{ withoutTags: true }" of partial getIconLinks() is deprecated and will be removed in v3'
+      );
+
       expect(result.length).toBe(1);
       expect(result[0]).toMatch(getIconUrlRegEx('arrowHeadRight'));
     });
 
     it('should return default "arrowHeadRight" China CDN url', () => {
       const result = getIconLinks({ withoutTags: true, cdn: 'cn' });
+
+      expect(consoleWarnSpy).toBeCalledWith(
+        'The option "{ withoutTags: true }" of partial getIconLinks() is deprecated and will be removed in v3'
+      );
+
       expect(result.length).toBe(1);
       expect(result[0]).toMatch(getIconUrlRegEx('arrowHeadRight', 'cn'));
     });
 
     it('should return multiple urls', () => {
       const result = getIconLinks({ withoutTags: true, icons: ['truck', 'volumeUp', 'mobile', 'arrowDoubleUp'] });
+
+      expect(consoleWarnSpy).toBeCalledWith(
+        'The option "{ withoutTags: true }" of partial getIconLinks() is deprecated and will be removed in v3'
+      );
+
       expect(result.length).toBe(4);
       expect(result[0]).toMatch(getIconUrlRegEx('truck'));
       expect(result[1]).toMatch(getIconUrlRegEx('volumeUp'));
@@ -83,6 +130,11 @@ describe('getIconLinks()', () => {
     ICON_NAMES.forEach((iconName: IconNameCamelCase) => {
       it(`should return icon url for ['${iconName}']`, () => {
         const result = getIconLinks({ withoutTags: true, icons: [iconName] });
+
+        expect(consoleWarnSpy).toBeCalledWith(
+          'The option "{ withoutTags: true }" of partial getIconLinks() is deprecated and will be removed in v3'
+        );
+
         expect(result.length).toBe(1);
         expect(result[0]).toMatch(getIconUrlRegEx(iconName));
       });
