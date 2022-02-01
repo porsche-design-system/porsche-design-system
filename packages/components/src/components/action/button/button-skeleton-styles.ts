b173a17f3156f7c1f06f272a097812670b8942c0
marcuszierke <marcuszierke@gmail.com>
2022-02-01 16:58:07 +0100

    feat(components): baseSkeletonStyle added | as / mz #1339

--- packages/components/src/components/action/button/button-skeleton-styles.ts
+++ packages/components/src/components/action/button/button-skeleton-styles.ts
@@ -1,16 +1,39 @@
 import { pxToRemWithUnit } from '../../../styles';
 import { getMinifiedStyles } from '../../../../../shared/src/styles/getMinifiedStyles';
+import { Styles } from 'jss';
 
 export const getButtonSkeletonStyles = (): string => {
   return getMinifiedStyles({
     '@global': {
       'p-button:not(.hydrated)': {
-        display: 'block',
         width: pxToRemWithUnit(300),
         height: pxToRemWithUnit(150),
-        background: 'PDS_REPLACE_WITH_THEME_COLOR',
-        visibility: 'visible',
+        ...getBaseSkeletonStyles(),
+        '&::before': {
+          position: 'absolute',
+          content: '""',
+          height: '100%',
+          width: '100%',
+          backgroundImage: `linear-gradient(to right, ${SKELETON_COLOR_THEME_PLACEHOLDER} 0%, rgba(0,0,0,0.05) 20%, ${SKELETON_COLOR_THEME_PLACEHOLDER} 40%, ${SKELETON_COLOR_THEME_PLACEHOLDER} 100%)`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: '450px 400px',
+          animation: 'shimmer 1s linear infinite',
+          left: '0',
+          top: '0',
+        },
       },
     },
   });
+};
+
+export const SKELETON_COLOR_THEME_PLACEHOLDER = 'PDS_REPLACE_WITH_THEME_COLOR';
+
+export const getBaseSkeletonStyles = (): Styles => {
+  return {
+    // hide select / slotted
+    display: 'block',
+    background: `${SKELETON_COLOR_THEME_PLACEHOLDER}`,
+    visibility: 'visible',
+    color: 'transparent',
+  };
 };
--- packages/components/src/components/form/select-wrapper/select-wrapper/select-wrapper-skeleton-styles.ts
+++ packages/components/src/components/form/select-wrapper/select-wrapper/select-wrapper-skeleton-styles.ts
@@ -1,14 +1,13 @@
 import { getMinifiedStyles } from '../../../../../../shared/src/styles/getMinifiedStyles';
 import { pxToRemWithUnit } from '../../../../styles';
+import { getBaseSkeletonStyles } from '../../../action/button/button-skeleton-styles';
 
 export const getSelectWrapperSkeletonStyles = (): string => {
   return getMinifiedStyles({
     '@global': {
       'p-select-wrapper:not(.hydrated)': {
-        display: 'block',
         height: pxToRemWithUnit(150),
-        background: 'PDS_REPLACE_WITH_THEME_COLOR',
-        visibility: 'visible',
+        ...getBaseSkeletonStyles(),
       },
     },
   });
