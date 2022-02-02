271772fa87d2fe61b76dcd8745da888c32911fd3
Bernd Hacker <hacker.bernd@gmail.com>
2022-02-02 08:45:25 +0100

    feat(components-js): improve generation of routes for react via generateAngularReactVRTPages | bh | #1153

--- packages/components-react/src/pages/index.ts
+++ packages/components-react/src/pages/index.ts
@@ -1,55 +1,296 @@
+import type { RouteType } from '../routes';
+
 export * from './CoreClassNames';
 export * from './Events';
 export * from './StencilLifecycles';
 export * from './Table';
 
 /* Auto Generated Below */
-export * from './Accordion';
-export * from './Banner';
-export * from './Button';
-export * from './ButtonGroup';
-export * from './ButtonPure';
-export * from './CheckboxWrapper';
-export * from './ContentWrapper';
-export * from './CoreInitializer';
-export * from './Divider';
-export * from './FieldsetWrapper';
-export * from './Flex';
-export * from './Grid';
-export * from './Headline';
-export * from './Icon';
-export * from './InlineNotification';
-export * from './Link';
-export * from './LinkPure';
-export * from './LinkSocial';
-export * from './Marque';
-export * from './ModalBasic';
-export * from './ModalFullscreen';
-export * from './ModalFullscreenBreakpoint';
-export * from './ModalFullWidthSlot';
-export * from './ModalNoHeading';
-export * from './ModalPrefixed';
-export * from './ModalScrollable';
-export * from './ModalSlottedHeading';
+/* eslint-disable import/first */
 export * from './Overview';
-export * from './Pagination';
-export * from './Popover';
-export * from './RadioButtonWrapper';
-export * from './SelectWrapper';
-export * from './Spinner';
-export * from './Switch';
-export * from './Tabs';
-export * from './TabsBar';
-export * from './Text';
-export * from './TextareaWrapper';
-export * from './TextFieldWrapper';
-export * from './TextList';
-export * from './ToastBasic';
-export * from './ToastBasicDark';
-export * from './ToastBasicLongText';
-export * from './ToastOffset';
-export * from './ToastPrefixed';
-export * from './TypographyCyril';
-export * from './TypographyFallbackStrategy';
-export * from './TypographyGreekAndCoptic';
-export * from './TypographyLatin';
+export * from './CoreInitializer';
+import { AccordionPage } from './Accordion';
+import { BannerPage } from './Banner';
+import { ButtonPage } from './Button';
+import { ButtonGroupPage } from './ButtonGroup';
+import { ButtonPurePage } from './ButtonPure';
+import { CheckboxWrapperPage } from './CheckboxWrapper';
+import { ContentWrapperPage } from './ContentWrapper';
+import { DividerPage } from './Divider';
+import { FieldsetWrapperPage } from './FieldsetWrapper';
+import { FlexPage } from './Flex';
+import { GridPage } from './Grid';
+import { HeadlinePage } from './Headline';
+import { IconPage } from './Icon';
+import { InlineNotificationPage } from './InlineNotification';
+import { LinkPage } from './Link';
+import { LinkPurePage } from './LinkPure';
+import { LinkSocialPage } from './LinkSocial';
+import { MarquePage } from './Marque';
+import { ModalBasicPage } from './ModalBasic';
+import { ModalFullscreenPage } from './ModalFullscreen';
+import { ModalFullscreenBreakpointPage } from './ModalFullscreenBreakpoint';
+import { ModalFullWidthSlotPage } from './ModalFullWidthSlot';
+import { ModalNoHeadingPage } from './ModalNoHeading';
+import { ModalPrefixedPage } from './ModalPrefixed';
+import { ModalScrollablePage } from './ModalScrollable';
+import { ModalSlottedHeadingPage } from './ModalSlottedHeading';
+import { PaginationPage } from './Pagination';
+import { PopoverPage } from './Popover';
+import { RadioButtonWrapperPage } from './RadioButtonWrapper';
+import { SelectWrapperPage } from './SelectWrapper';
+import { SpinnerPage } from './Spinner';
+import { SwitchPage } from './Switch';
+import { TabsPage } from './Tabs';
+import { TabsBarPage } from './TabsBar';
+import { TextPage } from './Text';
+import { TextareaWrapperPage } from './TextareaWrapper';
+import { TextFieldWrapperPage } from './TextFieldWrapper';
+import { TextListPage } from './TextList';
+import { ToastBasicPage } from './ToastBasic';
+import { ToastBasicDarkPage } from './ToastBasicDark';
+import { ToastBasicLongTextPage } from './ToastBasicLongText';
+import { ToastOffsetPage } from './ToastOffset';
+import { ToastPrefixedPage } from './ToastPrefixed';
+import { TypographyCyrilPage } from './TypographyCyril';
+import { TypographyFallbackStrategyPage } from './TypographyFallbackStrategy';
+import { TypographyGreekAndCopticPage } from './TypographyGreekAndCoptic';
+import { TypographyLatinPage } from './TypographyLatin';
+
+export const generatedRoutes: RouteType[] = [
+  {
+    name: 'Accordion',
+    component: AccordionPage,
+    path: '/accordion',
+  },
+  {
+    name: 'Banner',
+    component: BannerPage,
+    path: '/banner',
+  },
+  {
+    name: 'Button',
+    component: ButtonPage,
+    path: '/button',
+  },
+  {
+    name: 'Button Group',
+    component: ButtonGroupPage,
+    path: '/button-group',
+  },
+  {
+    name: 'Button Pure',
+    component: ButtonPurePage,
+    path: '/button-pure',
+  },
+  {
+    name: 'Checkbox Wrapper',
+    component: CheckboxWrapperPage,
+    path: '/checkbox-wrapper',
+  },
+  {
+    name: 'Content Wrapper',
+    component: ContentWrapperPage,
+    path: '/content-wrapper',
+  },
+  {
+    name: 'Divider',
+    component: DividerPage,
+    path: '/divider',
+  },
+  {
+    name: 'Fieldset Wrapper',
+    component: FieldsetWrapperPage,
+    path: '/fieldset-wrapper',
+  },
+  {
+    name: 'Flex',
+    component: FlexPage,
+    path: '/flex',
+  },
+  {
+    name: 'Grid',
+    component: GridPage,
+    path: '/grid',
+  },
+  {
+    name: 'Headline',
+    component: HeadlinePage,
+    path: '/headline',
+  },
+  {
+    name: 'Icon',
+    component: IconPage,
+    path: '/icon',
+  },
+  {
+    name: 'Inline Notification',
+    component: InlineNotificationPage,
+    path: '/inline-notification',
+  },
+  {
+    name: 'Link',
+    component: LinkPage,
+    path: '/link',
+  },
+  {
+    name: 'Link Pure',
+    component: LinkPurePage,
+    path: '/link-pure',
+  },
+  {
+    name: 'Link Social',
+    component: LinkSocialPage,
+    path: '/link-social',
+  },
+  {
+    name: 'Marque',
+    component: MarquePage,
+    path: '/marque',
+  },
+  {
+    name: 'Modal Basic',
+    component: ModalBasicPage,
+    path: '/modal-basic',
+  },
+  {
+    name: 'Modal Fullscreen',
+    component: ModalFullscreenPage,
+    path: '/modal-fullscreen',
+  },
+  {
+    name: 'Modal Fullscreen Breakpoint',
+    component: ModalFullscreenBreakpointPage,
+    path: '/modal-fullscreen-breakpoint',
+  },
+  {
+    name: 'Modal Full Width Slot',
+    component: ModalFullWidthSlotPage,
+    path: '/modal-full-width-slot',
+  },
+  {
+    name: 'Modal No Heading',
+    component: ModalNoHeadingPage,
+    path: '/modal-no-heading',
+  },
+  {
+    name: 'Modal Prefixed',
+    component: ModalPrefixedPage,
+    path: '/modal-prefixed',
+  },
+  {
+    name: 'Modal Scrollable',
+    component: ModalScrollablePage,
+    path: '/modal-scrollable',
+  },
+  {
+    name: 'Modal Slotted Heading',
+    component: ModalSlottedHeadingPage,
+    path: '/modal-slotted-heading',
+  },
+  {
+    name: 'Pagination',
+    component: PaginationPage,
+    path: '/pagination',
+  },
+  {
+    name: 'Popover',
+    component: PopoverPage,
+    path: '/popover',
+  },
+  {
+    name: 'Radio Button Wrapper',
+    component: RadioButtonWrapperPage,
+    path: '/radio-button-wrapper',
+  },
+  {
+    name: 'Select Wrapper',
+    component: SelectWrapperPage,
+    path: '/select-wrapper',
+  },
+  {
+    name: 'Spinner',
+    component: SpinnerPage,
+    path: '/spinner',
+  },
+  {
+    name: 'Switch',
+    component: SwitchPage,
+    path: '/switch',
+  },
+  {
+    name: 'Tabs',
+    component: TabsPage,
+    path: '/tabs',
+  },
+  {
+    name: 'Tabs Bar',
+    component: TabsBarPage,
+    path: '/tabs-bar',
+  },
+  {
+    name: 'Text',
+    component: TextPage,
+    path: '/text',
+  },
+  {
+    name: 'Textarea Wrapper',
+    component: TextareaWrapperPage,
+    path: '/textarea-wrapper',
+  },
+  {
+    name: 'Text Field Wrapper',
+    component: TextFieldWrapperPage,
+    path: '/text-field-wrapper',
+  },
+  {
+    name: 'Text List',
+    component: TextListPage,
+    path: '/text-list',
+  },
+  {
+    name: 'Toast Basic',
+    component: ToastBasicPage,
+    path: '/toast-basic',
+  },
+  {
+    name: 'Toast Basic Dark',
+    component: ToastBasicDarkPage,
+    path: '/toast-basic-dark',
+  },
+  {
+    name: 'Toast Basic Long Text',
+    component: ToastBasicLongTextPage,
+    path: '/toast-basic-long-text',
+  },
+  {
+    name: 'Toast Offset',
+    component: ToastOffsetPage,
+    path: '/toast-offset',
+  },
+  {
+    name: 'Toast Prefixed',
+    component: ToastPrefixedPage,
+    path: '/toast-prefixed',
+  },
+  {
+    name: 'Typography Cyril',
+    component: TypographyCyrilPage,
+    path: '/typography-cyril',
+  },
+  {
+    name: 'Typography Fallback Strategy',
+    component: TypographyFallbackStrategyPage,
+    path: '/typography-fallback-strategy',
+  },
+  {
+    name: 'Typography Greek And Coptic',
+    component: TypographyGreekAndCopticPage,
+    path: '/typography-greek-and-coptic',
+  },
+  {
+    name: 'Typography Latin',
+    component: TypographyLatinPage,
+    path: '/typography-latin',
+  },
+];
