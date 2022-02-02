229167468315197034a6925cb3e5d38c03a0ffd5
marcuszierke <marcuszierke@gmail.com>
2022-02-03 00:26:09 +0100

    feat(components): first draft for Mutation Observer added | as / mz #1339

--- packages/components-js/projects/partials/scripts/generateInitialStylesPartial.ts
+++ packages/components-js/projects/partials/scripts/generateInitialStylesPartial.ts
@@ -29,48 +29,99 @@ export const generateInitialStylesPartial = (): string => {
   const tagNamesWithSkeleton = joinArrayElementsToString(TAG_NAMES_WITH_SKELETON);
 
   const func = `export const getInitialStyles = (opts?: InitialStylesOptions): string => {
-  const options: InitialStylesOptions = {
-    prefix: '',
-    withoutTags: false,
-    theme: 'light',
-    ...opts
-  };
-  const { prefix, withoutTags, theme } = options;
+    const options: InitialStylesOptions = {
+      prefix: '',
+      withoutTags: false,
+      theme: 'light',
+      ...opts
+    };
+    const { prefix, withoutTags, theme } = options;
 
 
-  const tagNames = [${tagNames}];
-  let styleInnerHtml = tagNames.map((x) => prefix
-    ? \`\${prefix}-\${x}\`
-    : x
-  ).join(',') + '{visibility:hidden}';
+    const tagNames = [${tagNames}];
+    let styleInnerHtml = tagNames.map((x) => prefix
+      ? \`\${prefix}-\${x}\`
+      : x
+    ).join(',') + '{visibility:hidden}';
 
-  const tagNamesWithSkeleton = [${tagNamesWithSkeleton}];
+    const tagNamesWithSkeleton = [${tagNamesWithSkeleton}];
 
-  const skeletonStyles =  ${JSON.stringify(skeletonStyles)};
+    const skeletonStyles = ${JSON.stringify(skeletonStyles)};
 
-  let skeletonStyleStringWithPrefix = tagNamesWithSkeleton.map((tagName)=> {
-    if(prefix){
-      const tagRegExp = new RegExp(tagName, 'g');
-      return \`\${skeletonStyles[tagName].replace(tagRegExp, \`\${prefix}-\${tagName}\`)}\`;
-    } else {
-      return skeletonStyles[tagName];
+    // const addAriaAttributesToSkeletonTagNames = (tagName) => {
+    //   const el = document.querySelector(tagName);
+    //   el.ariaBusy = true;
+    //   el.ariaHidden = true;
+    // }
+
+    const skeletonStyleWithPrefix = [];
+
+    tagNamesWithSkeleton.forEach((tagName)=> {
+      if(prefix){
+      const prefixedTagName = \`\${prefix}-\${tagName}\`;
+        const tagRegExp = new RegExp(tagName, 'g');
+        skeletonStyleWithPrefix.push(\`\${skeletonStyles[tagName].replace(tagRegExp, prefixedTagName)}\`);
+        // addAriaAttributesToSkeletonTagNames(prefixedTagName);
+      } else {
+        skeletonStyleWithPrefix.push(skeletonStyles[tagName]);
+        // addAriaAttributesToSkeletonTagNames(tagName);
+      }
+    });
+
+    let skeletonStyleStringWithPrefix = skeletonStyleWithPrefix.join('');
+
+    skeletonStyleStringWithPrefix = skeletonStyleStringWithPrefix.replace(/${SKELETON_COLOR_THEME_PLACEHOLDER}/g,\`\${theme === 'light' ? '#E3E4E5': '#626669'}\`);
+    skeletonStyleStringWithPrefix = skeletonStyleStringWithPrefix.replace(/${SKELETON_LINEAR_GRADIENT_COLOR_1}/g,\`\${theme === 'light' ? '#E3E4E5': '#656871'}\`);
+    skeletonStyleStringWithPrefix = skeletonStyleStringWithPrefix.replace(/${SKELETON_LINEAR_GRADIENT_COLOR_2}/g,\`\${theme === 'light' ? '#0000000d': '#888b94'}\`);
+
+    styleInnerHtml += skeletonStyleStringWithPrefix;
+
+    styleInnerHtml += '\\\\${skeletonKeyframes}';
+    // START FOR SCRIPT TO SET ARIA ATTRIBUTES
+    const toggleAriaAttributes = (target, state) => {
+      target.ariaBusy = state;
+      target.ariaHidden = state;
     }
-  }).join('');
 
+    const setSkeletonAriaProperties = (target) => {
+      if (target.className.includes("hydrated")) {
+        toggleAriaAttributes(target, false)
+        unobserveAttributes(target);
+      } else {
+        toggleAriaAttributes(target, true);
+      }
+    }
 
-  skeletonStyleStringWithPrefix = skeletonStyleStringWithPrefix.replace(/${SKELETON_COLOR_THEME_PLACEHOLDER}/g,\`\${theme === 'light' ? '#E3E4E5': '#626669'}\`);
-  skeletonStyleStringWithPrefix = skeletonStyleStringWithPrefix.replace(/${SKELETON_LINEAR_GRADIENT_COLOR_1}/g,\`\${theme === 'light' ? '#E3E4E5': '#656871'}\`);
-  skeletonStyleStringWithPrefix = skeletonStyleStringWithPrefix.replace(/${SKELETON_LINEAR_GRADIENT_COLOR_2}/g,\`\${theme === 'light' ? '#0000000d': '#888b94'}\`);
+    const attributeMutationMap = new Map();
 
-  styleInnerHtml += skeletonStyleStringWithPrefix;
+    const attributeObserver = new MutationObserver((mutations) => {
+      mutations
+        // reduce array to only entries that have really a changed value
+        .filter((mutation) => mutation.oldValue !== mutation.target.getAttribute(mutation.attributeName))
+        // remove duplicates so we execute callback only once per node
+        .filter((mutation, idx, arr) => arr.findIndex((m) => m.target === mutation.target) === idx)
+        .forEach((mutation) => attributeMutationMap.get(mutation.target)?.(mutation.target));
+    });
 
-  styleInnerHtml += '\\\\${skeletonKeyframes}';
+    const observeAttributes = (node, attributes, callback) => {
+      // node might not be defined in connectedCallback
+      if (node) {
+        attributeMutationMap.set(node, callback);
+        attributeObserver.observe(node, { attributeFilter: attributes, attributeOldValue: true });
+      }
+    };
 
-  const result = withoutTags
-    ? styleInnerHtml
-    : \`<style>\${styleInnerHtml}</style>\`;
-   return result;
-};`;
+    const unobserveAttributes = (node) => {
+      attributeMutationMap.delete(node);
+    };
 
+    ['p-button'].forEach(tagName => {
+    observeAttributes(document.querySelector(tagName), ['class'], setSkeletonAriaProperties)
+    })
+    // END FOR SCRIPT TO SET ARIA ATTRIBUTES
+
+    const result = withoutTags ? styleInnerHtml : \`<style>\${styleInnerHtml}</style>\`;
+    return result;
+  };`;
   return [types, func].join('\n\n');
 };
