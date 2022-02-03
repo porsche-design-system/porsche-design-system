4a44471d8751609b454858dfd3398b6a7880d074
Anton Schmidt <anton.schmidt@mhp.com>
2022-02-03 16:52:56 +0100

    refactor(components/-js): extract base skeleton styles to own file | as / mz | #13339

--- packages/components/src/components/action/button/button-skeleton-styles.ts
+++ packages/components/src/components/action/button/button-skeleton-styles.ts
@@ -1,6 +1,6 @@
 import { pxToRemWithUnit } from '../../../styles';
 import { getMinifiedStyles } from '../../../../../shared/src/styles/getMinifiedStyles';
-import { Styles } from 'jss';
+import { getBaseSkeletonStyles } from '../../../../src/styles/skeleton-base-styles';
 
 export const getButtonSkeletonStyles = (): string => {
   return getMinifiedStyles({
@@ -12,35 +12,4 @@ export const getButtonSkeletonStyles = (): string => {
       },
     },
   });
-};
-
-export const SKELETON_COLOR_THEME_PLACEHOLDER = 'PDS_REPLACE_WITH_THEME_COLOR';
-
-export const SKELETON_LINEAR_GRADIENT_COLOR_1 = 'PDS_REPLACE_WITH_LINEAR_GRADIENT_1';
-export const SKELETON_LINEAR_GRADIENT_COLOR_2 = 'PDS_REPLACE_WITH_LINEAR_GRADIENT_2';
-
-export const getBaseSkeletonStyles = (): Styles => {
-  return {
-    // TODO: hide select / slotted
-    display: 'block',
-    background: `${SKELETON_COLOR_THEME_PLACEHOLDER}`,
-    visibility: 'visible',
-    position: 'relative',
-    color: 'transparent',
-    '& > *': {
-      visibility: 'hidden',
-    },
-    '&::before': {
-      position: 'absolute',
-      content: '""',
-      height: '100%',
-      width: '100%',
-      backgroundImage: `linear-gradient(to right, ${SKELETON_LINEAR_GRADIENT_COLOR_1} 0%, ${SKELETON_LINEAR_GRADIENT_COLOR_2} 20%, ${SKELETON_LINEAR_GRADIENT_COLOR_1} 40%, ${SKELETON_LINEAR_GRADIENT_COLOR_1} 100%)`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: '450px 400px',
-      animation: 'shimmer 1s linear infinite',
-      left: '0',
-      top: '0',
-    },
-  };
 };
