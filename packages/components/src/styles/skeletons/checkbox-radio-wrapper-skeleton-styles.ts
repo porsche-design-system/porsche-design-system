9c946a556e9aa88cf0d40fc4077bbe72f9705820
Anton Schmidt <anton.schmidt@mhp.com>
2022-02-04 14:12:26 +0100

    refactor(components/components-js): combine skeleton style selectors for smaller footprint in head and adjust partial script  | as | #1339

---
+++ packages/components/src/styles/skeletons/checkbox-radio-wrapper-skeleton-styles.ts
@@ -0,0 +1,16 @@
+import { getMinifiedStyles } from '@porsche-design-system/shared-src/src/styles/getMinifiedStyles';
+import { pxToRemWithUnit } from '../common-styles';
+import { ELEMENT_SKELETON_HEIGHT } from './skeleton-base-styles';
+
+export const getCheckboxRadioWrapperSkeletonStyles = (): string => {
+  return getMinifiedStyles({
+    '@global': {
+      'p-checkbox-wrapper, p-radio-button-wrapper': {
+        '&:not(.hydrated)': {
+          width: pxToRemWithUnit(81.5),
+          height: pxToRemWithUnit(ELEMENT_SKELETON_HEIGHT / 2),
+        },
+      },
+    },
+  });
+};
--- packages/components/src/styles/skeletons/skeleton-base-styles.ts
+++ packages/components/src/styles/skeletons/skeleton-base-styles.ts
@@ -1,10 +1,10 @@
 import { Styles } from 'jss';
-import { pxToRemWithUnit } from '../common-styles';
 
 export const SKELETON_COLOR_THEME_PLACEHOLDER = 'PDS_REPLACE_WITH_THEME_COLOR';
 export const SKELETON_LINEAR_GRADIENT_COLOR_1 = 'PDS_REPLACE_WITH_LINEAR_GRADIENT_1';
 export const SKELETON_LINEAR_GRADIENT_COLOR_2 = 'PDS_REPLACE_WITH_LINEAR_GRADIENT_2';
 export const BUTTON_LINK_SKELETON_WIDTH = 100;
+export const ELEMENT_SKELETON_HEIGHT = 48;
 
 export const getBaseSkeletonStyles = (): Styles => {
   return {
@@ -28,36 +28,5 @@ export const getBaseSkeletonStyles = (): Styles => {
       left: '0',
       top: '0',
     },
-  };
-};
-
-export const getButtonLinkSkeletonStyles = (): Styles => {
-  return {
-    display: 'flex',
-    width: pxToRemWithUnit(BUTTON_LINK_SKELETON_WIDTH),
-    height: pxToRemWithUnit(48),
-  };
-};
-
-export const getButtonLinkPureSkeletonStyles = (): Styles => {
-  return {
-    display: 'flex',
-    width: pxToRemWithUnit(BUTTON_LINK_SKELETON_WIDTH),
-    height: pxToRemWithUnit(24),
-  };
-};
-
-export const getSelectTextFieldSkeletonStyles = (): Styles => {
-  return {
-    maxWidth: pxToRemWithUnit(450),
-    height: pxToRemWithUnit(48),
-    margin: `${pxToRemWithUnit(28)} 0 0 0`,
-  };
-};
-
-export const getCheckboxRadioButtonSkeletonStyles = (): Styles => {
-  return {
-    width: pxToRemWithUnit(81.5),
-    height: pxToRemWithUnit(24),
   };
 };
