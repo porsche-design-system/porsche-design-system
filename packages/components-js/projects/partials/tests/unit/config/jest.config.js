d8d35b437b9b15ffe5a0519729186551c4d0ead8
Matthias Haegele <matthias.haegele@mhp.com>
2022-02-07 16:13:24 +0100

    test(components-js): enable typechecking in partial unit tests | mh / bh | #1410

--- packages/components-js/projects/partials/tests/unit/config/jest.config.js
+++ packages/components-js/projects/partials/tests/unit/config/jest.config.js
@@ -3,10 +3,5 @@ module.exports = {
   testEnvironment: 'jsdom',
   verbose: true,
   rootDir: '../../../',
-  globals: {
-    'ts-jest': {
-      isolatedModules: true, // this fixes typing issues with jasmine
-    },
-  },
   testMatch: ['**/tests/unit/specs/**/*.+(ts|tsx|js)'],
 };
