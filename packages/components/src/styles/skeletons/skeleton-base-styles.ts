3a1a1e87dfa060ade3bd338ee1c7c40f6718a19f
Anton Schmidt <anton.schmidt@mhp.com>
2022-02-09 09:51:24 +0100

    feat(components/-js): move visibility to pseudo elements, keep actual components hidden, remove fieldset-wrapper styles, use actual fieldset-wrapper | as / mz | #1339

--- packages/components/src/styles/skeletons/skeleton-base-styles.ts
+++ packages/components/src/styles/skeletons/skeleton-base-styles.ts
@@ -8,27 +8,33 @@ export const SKELETON_LINEAR_GRADIENT_COLOR_2 = 'PDS_REPLACE_WITH_LINEAR_GRADIEN
 export const BUTTON_LINK_SKELETON_WIDTH = 192;
 export const ELEMENT_SKELETON_HEIGHT = 48;
 
+const getPseudoElementStyles = (): Styles => {
+  return {
+    position: 'absolute',
+    content: '""',
+    height: '100%',
+    width: '100%',
+    visibility: 'visible',
+    left: '0',
+    top: '0',
+  };
+};
+
 export const getBaseSkeletonStyles = (): Styles => {
   return {
     display: 'block',
-    background: `${SKELETON_COLOR_THEME_PLACEHOLDER}`,
-    visibility: 'visible',
     position: 'relative',
     color: 'transparent',
-    '& > *': {
-      visibility: 'hidden',
-    },
     '&::before': {
-      position: 'absolute',
-      content: '""',
-      height: '100%',
-      width: '100%',
+      ...getPseudoElementStyles(),
+      background: `${SKELETON_COLOR_THEME_PLACEHOLDER}`,
+    },
+    '&::after': {
+      ...getPseudoElementStyles(),
       backgroundImage: `linear-gradient(to right, ${SKELETON_LINEAR_GRADIENT_COLOR_1} 0%, ${SKELETON_LINEAR_GRADIENT_COLOR_2} 20%, ${SKELETON_LINEAR_GRADIENT_COLOR_1} 40%, ${SKELETON_LINEAR_GRADIENT_COLOR_1} 100%)`,
       backgroundRepeat: 'no-repeat',
       backgroundSize: '450px 400px',
       animation: 'shimmer 1s linear infinite',
-      left: '0',
-      top: '0',
     },
   };
 };
