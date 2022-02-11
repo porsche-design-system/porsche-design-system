21b343a40bfc9a87051beb107747255b22bcac9c
Matthias Haegele <matthias.haegele@mhp.com>
2022-02-11 08:51:44 +0100

    chore: upgrade sass / type/node / http server  | mh

--- packages/storefront/.eslintrc.js
+++ packages/storefront/.eslintrc.js
@@ -11,6 +11,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    'vue/multi-word-component-names': ['error', { ignores: ['Playground'] }],
   },
 };
