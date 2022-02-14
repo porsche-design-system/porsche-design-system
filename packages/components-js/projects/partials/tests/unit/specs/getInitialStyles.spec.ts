04ea27f3e68ce3e81feb44ab6f13d91f308254df
Anton Schmidt <anton.schmidt@mhp.com>
2022-02-14 16:19:58 +0100

    test(components-js): add tests for skeletons in initial styles, make skeletonCOmponents array exclusive upon getInitialStyles call | as / mz | #1339

--- packages/components-js/projects/partials/tests/unit/specs/getInitialStyles.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getInitialStyles.spec.ts
@@ -1,6 +1,7 @@
 import { getInitialStyles } from '../../../src';
+import type { SkeletonComponentChunkName } from '../../../src';
 import { render } from '@testing-library/react';
-import { INTERNAL_TAG_NAMES, TAG_NAMES } from '@porsche-design-system/shared';
+import { INTERNAL_TAG_NAMES, TAG_NAMES, TAG_NAMES_WITH_SKELETON } from '@porsche-design-system/shared';
 
 const filteredTagNames = TAG_NAMES.filter((x) => !INTERNAL_TAG_NAMES.includes(x));
 const tagNames = filteredTagNames.join(',');
@@ -13,10 +14,23 @@ describe('format: html', () => {
     expect(result).toMatch(regex);
   });
 
+  it('should return core and skeleton styles', () => {
+    const result = getInitialStyles({ skeletonComponents: TAG_NAMES_WITH_SKELETON as SkeletonComponentChunkName[] });
+    expect(result).toMatchSnapshot();
+  });
+
   it('should add custom prefixes to component names', () => {
     const result = getInitialStyles({ prefix: 'custom-prefix' });
     const regex = new RegExp(`<style>${prefixedTagNames}{visibility:hidden}</style>`);
     expect(result).toMatch(regex);
+  });
+
+  it('should add custom prefixes to skeleton component names', () => {
+    const result = getInitialStyles({
+      prefix: 'custom-prefix',
+      skeletonComponents: TAG_NAMES_WITH_SKELETON as SkeletonComponentChunkName[],
+    });
+    expect(result).toMatchSnapshot();
   });
 });
 
@@ -25,6 +39,14 @@ describe('format: jsx', () => {
     const { container } = render(getInitialStyles({ format: 'jsx' }));
     const regex = new RegExp(`<style>${tagNames}{visibility:hidden}</style>`);
     expect(container.innerHTML).toMatch(regex);
+  });
+
+  it('should return core and skeleton styles', () => {
+    const result = getInitialStyles({
+      format: 'jsx',
+      skeletonComponents: TAG_NAMES_WITH_SKELETON as SkeletonComponentChunkName[],
+    });
+    expect(result).toMatchSnapshot();
   });
 
   it('should add custom prefix to component names', () => {
@@ -39,5 +61,33 @@ describe('withoutTags: true', () => {
     const result = getInitialStyles({ withoutTags: true });
     const regex = new RegExp(`${tagNames}{visibility:hidden}`);
     expect(result).toMatch(regex);
+  });
+});
+
+describe('skeletonComponents subset', () => {
+  const skeletonTagNamesWithoutButton = TAG_NAMES_WITH_SKELETON.filter((x) => x !== 'p-button');
+
+  it('should return core and p-button skeleton styles', () => {
+    const result = getInitialStyles({ skeletonComponents: ['p-button'] });
+
+    expect(result.includes('p-button:not(.hydrated)')).toBe(true);
+
+    skeletonTagNamesWithoutButton.forEach((tagName) =>
+      expect(result.includes(`${tagName}:not(.hydrated)`)).toBe(false)
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should return prefixed core and p-button skeleton styles', () => {
+    const prefixedTagNamesWithSkeleton = skeletonTagNamesWithoutButton.map((x) => `custom-prefix-${x}`);
+
+    const result = getInitialStyles({ prefix: 'custom-prefix', skeletonComponents: ['p-button'] });
+
+    expect(result.includes('custom-prefix-p-button:not(.hydrated)')).toBe(true);
+
+    prefixedTagNamesWithSkeleton.forEach((tagName) => expect(result.includes(`${tagName}:not(.hydrated)`)).toBe(false));
+
+    expect(result).toMatchSnapshot();
   });
 });
