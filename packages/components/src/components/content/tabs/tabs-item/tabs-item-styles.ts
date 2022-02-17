8908502e79d89fe3ac7819d6b0347a0830be4130
Bernd Hacker <hacker.bernd@gmail.com>
2022-02-17 10:00:44 +0100

    feat(components): replace safari detection with focus-visible feature detection and use it in modal and tabs-item styles | bh | #1753

--- packages/components/src/components/content/tabs/tabs-item/tabs-item-styles.ts
+++ packages/components/src/components/content/tabs/tabs-item/tabs-item-styles.ts
@@ -1,24 +1,22 @@
 import type { ThemeExtendedElectric } from '../../../../types';
-import { getCss, isSafari } from '../../../../utils';
+import { getCss } from '../../../../utils';
 import { addImportantToEachRule, getFocusStyles, getThemedColors } from '../../../../styles';
+import { getFocusVisibleFallback } from '../../../../styles/focus-visible-fallback';
 import type { JssStyle } from 'jss';
 
 export const getComponentCss = (theme: ThemeExtendedElectric): string => {
-  // TODO: remove safari fallback once :focus-visible is supported
-  const hostFocusStyle = isSafari
-    ? { outline: 0 }
-    : Object.entries(getFocusStyles({ color: getThemedColors(theme).baseColor })).reduce((result, [key, val]) => {
-        result[key.startsWith('&') ? `&(${key.slice(1)})` : key] = val;
-        return result;
-      }, {} as JssStyle);
-
   return getCss({
     ':host': addImportantToEachRule({
       display: 'block',
       '&([hidden])': {
         display: 'none',
       },
-      ...hostFocusStyle,
+      ...getFocusVisibleFallback(
+        Object.entries(getFocusStyles({ color: getThemedColors(theme).baseColor })).reduce((result, [key, val]) => {
+          result[key.startsWith('&') ? `&(${key.slice(1)})` : key] = val;
+          return result;
+        }, {} as JssStyle)
+      ),
     }),
   });
 };
