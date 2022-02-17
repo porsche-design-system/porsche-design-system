ed4f1832f8d7f32babd45ddcdd23e2a3419ec819
Matthias Haegele <matthias.haegele@mhp.com>
2022-02-17 10:05:17 +0100

    fix(components-js): resolve react/jsx-runtime into bundle | mh

--- packages/components-js/projects/partials/rollup.config.js
+++ packages/components-js/projects/partials/rollup.config.js
@@ -1,20 +1,18 @@
 import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
 export default [
   {
     input: 'src/index.ts',
-    external: ['react/jsx-runtime'],
     output: {
       esModule: false,
       dir: 'dist',
       format: 'umd',
       name: pkg.name,
       exports: 'named',
-      globals: {
-        'react/jsx-runtime': 'jsxRuntime',
-      },
     },
-    plugins: [typescript({ declaration: true, declarationDir: 'dist', rootDir: 'src' })],
+    plugins: [typescript({ declaration: true, declarationDir: 'dist', rootDir: 'src' }), nodeResolve(), commonjs()],
   },
 ];
