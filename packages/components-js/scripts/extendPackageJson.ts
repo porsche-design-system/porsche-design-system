c0da5832f937f0748f8a3456aa398936fed2be03
Matthias Haegele <matthias.haegele@mhp.com>
2022-02-17 14:58:19 +0100

    feat(components-js): implement script to extend utilities package.json | mh | #1716

---
+++ packages/components-js/scripts/extendPackageJson.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const rootDirectory = path.resolve(__dirname, '..');
+const targetDirectory = path.resolve(rootDirectory, 'dist/components-wrapper');
+
+const pkgJson = require('../dist/components-wrapper/package.json');
+
+// add dependency
+Object.assign(pkgJson, {
+  dependencies: { '@porsche-design-system/components-js/utilities-jss': 'file:utilities/jss' },
+});
+
+fs.writeFileSync(path.resolve(targetDirectory, 'package.json'), JSON.stringify(pkgJson, null, 2));
