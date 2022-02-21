e73d8ed86575cfb9e35880a359f1f2f176ac6093
Bernd Hacker <hacker.bernd@gmail.com>
2022-02-21 17:32:20 +0100

    fix(components): bundling | bh

--- packages/components/src/setup.ts
+++ packages/components/src/setup.ts
@@ -1,5 +1,7 @@
 import { injectGlobalStyle } from './utils/inject-global-style';
+import { isObject } from './utils';
 
 export default (): void => {
   injectGlobalStyle();
+  isObject({}); // to trick bundling and avoid separate jss chunk 🤷
 };
