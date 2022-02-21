9e1437333326067335f5ccb32aaf58ba0107d650
Stephan Schröter <stephan.schroeter@mhp.com>
2022-02-21 15:32:11 +0100

    feat(components): remove aws file reference | bh / sas

--- packages/components/src/setup.ts
+++ packages/components/src/setup.ts
@@ -1,7 +1,5 @@
 import { injectGlobalStyle } from './utils/inject-global-style';
-import { trackLoader } from './utils';
 
 export default (): void => {
-  trackLoader();
   injectGlobalStyle();
 };
