57b2c6c8f142b46b1db4352f268b027af987ceb0
Matthias Haegele <matthias.haegele@mhp.com>
2022-02-22 08:20:33 +0100

    feat(components-js): resolve react/jsx-runtime into build | mh

--- packages/components-js/projects/partials/rollup.config.js
+++ packages/components-js/projects/partials/rollup.config.js
@@ -1,28 +1,29 @@
 import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 import pkg from './package.json';
 
 const input = 'src/index.ts';
-const external = ['react/jsx-runtime'];
 
 export default [
   {
     input,
-    external,
+    // Packages that use the partials via node are not able to read esm syntax, therefore we need a cjs build
     output: {
       esModule: false,
       dir: 'dist',
-      format: 'umd',
+      format: 'cjs',
       name: pkg.name,
       exports: 'named',
-      globals: {
-        'react/jsx-runtime': 'jsxRuntime',
-      },
     },
-    plugins: [typescript({ declaration: true, declarationDir: 'dist', rootDir: 'src' })],
+    // Our partials return jsx from react/jsx-runtime. We need to resolve the dependency into the build to make it readable in VanillaJS and Angular.
+    plugins: [typescript({ declaration: true, declarationDir: 'dist', rootDir: 'src' }), nodeResolve(), commonjs()],
   },
   {
     input,
-    external,
+    external: ['react/jsx-runtime'],
+    // To be treeshakable we need a esm build. See https://webpack.js.org/guides/tree-shaking/#conclusion
     output: { dir: 'dist/esm', format: 'esm' },
     plugins: [typescript()],
   },
