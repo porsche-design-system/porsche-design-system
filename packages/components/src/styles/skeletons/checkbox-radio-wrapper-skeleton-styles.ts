f42aa92e82eb939365208a11c2236276e250b6e4
Anton Schmidt <anton.schmidt@mhp.com>
2022-02-24 13:29:55 +0100

    feat(components): add hide label skeleton property style to checkbox and radio | as | #1339

--- packages/components/src/styles/skeletons/checkbox-radio-wrapper-skeleton-styles.ts
+++ packages/components/src/styles/skeletons/checkbox-radio-wrapper-skeleton-styles.ts
@@ -6,18 +6,23 @@ import {
   extendPseudoWithTheme,
   getBaseSkeletonStyle,
   getThemedPseudoStyle,
+  PDS_SKELETON_CLASS_PREFIX,
 } from './base-skeleton-styles';
 
 export const getCheckboxRadioWrapperSkeletonCss = (): string => {
+  const checkboxRadioElementHeight = ELEMENT_SKELETON_DIMENSION / 2;
   return getMinifiedCss({
     '@global': {
       'p-checkbox-wrapper, p-radio-button-wrapper': {
         '&:not(.hydrated)': {
           ...extendPseudoWithTheme({
-            styleFunction: () => getBaseSkeletonStyle(false, ELEMENT_SKELETON_DIMENSION / 2),
+            styleFunction: () => getBaseSkeletonStyle(false, checkboxRadioElementHeight),
           }),
           display: 'block',
           minWidth: pxToRemWithUnit(BUTTON_LINK_SKELETON_WIDTH),
+          [`&[hide-label=true], &.${PDS_SKELETON_CLASS_PREFIX}hide-label`]: {
+            minWidth: pxToRemWithUnit(checkboxRadioElementHeight),
+          },
           ...getThemedPseudoStyle(),
         },
       },
