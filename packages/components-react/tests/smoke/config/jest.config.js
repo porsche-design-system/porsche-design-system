b72eab010d904574299529641bd1bb5ce685edcd
Matthias Haegele <matthias.haegele@mhp.com>
2022-02-24 14:13:29 +0100

    test(components-react): improve test setup / improve spec | mh

--- packages/components-react/tests/smoke/config/jest.config.js
+++ packages/components-react/tests/smoke/config/jest.config.js
@@ -2,8 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   rootDir: '../../../',
   verbose: true,
-  testMatch: ['**/tests/smoke/specs/**/*.smoke.ts'],
   modulePathIgnorePatterns: ['<rootDir>/dist'],
-  clearMocks: true,
-  restoreMocks: true,
+  testMatch: ['**/tests/smoke/specs/**/*.smoke.ts'],
 };
