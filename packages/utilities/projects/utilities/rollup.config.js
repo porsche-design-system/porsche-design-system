f39c6181842fb70173db9744dd690e3190156b4b
Matthias Haegele <matthias.haegele@mhp.com>
2022-02-28 11:26:46 +0100

    feat(utilities): add sideEffects false to package.template.json / exclude spec files from bundling | mh | #1716

--- packages/utilities/projects/utilities/rollup.config.js
+++ packages/utilities/projects/utilities/rollup.config.js
@@ -7,7 +7,9 @@ export default [
       dir: 'dist/jss',
       format: 'cjs',
     },
-    plugins: [typescript({ declaration: true, declarationDir: 'dist/jss/types', rootDir: 'src/jss' })],
+    plugins: [
+      typescript({ declaration: true, declarationDir: 'dist/jss/types', exclude: '**.spec.ts', rootDir: 'src/jss' }),
+    ],
   },
   {
     input: 'src/jss/index.ts',
