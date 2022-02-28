c72673b791534c608c9adce872e94162ab083093
Anton Schmidt <anton.schmidt@mhp.com>
2022-02-28 12:05:25 +0100

    test(components): add tests for getTypographyElementHeight, round the values, update snapshots | as / mz | #1339

--- packages/components/src/styles/skeletons/headline-skeleton-styles.spec.ts
+++ packages/components/src/styles/skeletons/headline-skeleton-styles.spec.ts
@@ -1,5 +1,30 @@
-import { getHeadlineSkeletonCss } from './headline-skeleton-styles';
+import { getHeadlineSkeletonCss, getTypographyElementHeight } from './headline-skeleton-styles';
+import {
+  headline1,
+  headline2,
+  headline3,
+  headline4,
+  headline5,
+  titleLarge,
+  textSmall,
+} from '@porsche-design-system/utilities-v2';
 
-it('should return correct css', () => {
-  expect(getHeadlineSkeletonCss()).toMatchSnapshot();
+describe('getHeadlineSkeletonCss()', () => {
+  it('should return correct css', () => {
+    expect(getHeadlineSkeletonCss()).toMatchSnapshot();
+  });
+});
+
+describe('getTypographyElementHeight()', () => {
+  it.each([
+    [headline1, 40],
+    [headline2, 36],
+    [headline3, 28],
+    [headline4, 24],
+    [headline5, 24],
+    [titleLarge, 44],
+    [textSmall, 24],
+  ])('should for typography: %j return %s', (typography: typeof headline1, result: number) => {
+    expect(getTypographyElementHeight(typography)).toBe(result);
+  });
 });
