3321cfb902a8d6093b7ff08521164b08c7a28af8
marcuszierke <marcuszierke@gmail.com>
2022-02-28 12:28:17 +0100

    test(components): tests for text-skeleton-styles updated | mz | #1339

--- packages/components/src/styles/skeletons/text-skeleton-styles.spec.ts
+++ packages/components/src/styles/skeletons/text-skeleton-styles.spec.ts
@@ -1,5 +1,18 @@
-import { getTextSkeletonCss } from './text-skeleton-styles';
+import { getTextHeadlineSkeletonStyle, getTextSkeletonCss } from './text-skeleton-styles';
+import { headline2 } from '@porsche-design-system/utilities-v2';
+import { getTypographyElementHeight } from './headline-skeleton-styles';
 
-it('should return correct css', () => {
-  expect(getTextSkeletonCss()).toMatchSnapshot();
+describe('getTextSkeletonCss()', () => {
+  it('should return correct css', () => {
+    expect(getTextSkeletonCss()).toMatchSnapshot();
+  });
+});
+
+describe('getTextHeadlineSkeletonStyle()', () => {
+  it.each([[32], [getTypographyElementHeight(headline2)]])(
+    'should for elementHeight: %j return %s',
+    (elementHeight) => {
+      expect(getTextHeadlineSkeletonStyle(elementHeight)).toMatchSnapshot();
+    }
+  );
 });
