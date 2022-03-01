0be6ae77a651cfbb714e3f948f32892055e39e4e
marcuszierke <marcuszierke@gmail.com>
2022-03-02 00:13:16 +0100

    test(components): smoke test adjusted | mz | #1339

--- packages/components-react/tests/smoke/specs/bundling.smoke.ts
+++ packages/components-react/tests/smoke/specs/bundling.smoke.ts
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 import * as globby from 'globby';
-import { npmDistTmpSubPath } from '../../../../components-js/projects/components-wrapper/environment';
-import { INTERNAL_TAG_NAMES, TAG_NAMES } from '@porsche-design-system/shared';
+import { INTERNAL_TAG_NAMES, TAG_NAMES, TAG_NAMES_WITH_SKELETON } from '@porsche-design-system/shared';
 
 const buildDirectory = path.resolve('./build/static/js');
 const [mainChunkFilePath] = globby.sync(`${buildDirectory}/main.*.chunk.js`);
@@ -13,14 +12,19 @@ it('should only contain initial-styles partial', () => {
   const tagNames = TAG_NAMES.filter((x) => !INTERNAL_TAG_NAMES.includes(x))
     .map((x) => `"${x}"`)
     .join(',');
-  // {50,70} & {10,20} quantifiers to increase accuracy of regex instead using .*
-  const regex = new RegExp(`\\[${tagNames}\\].{50,70}\\.join\\(","\\)\\+"{visibility:hidden}".{10,20}"<style>"`);
+  const tagNamesWithSkeleton = TAG_NAMES_WITH_SKELETON.map((x) => `"${x}"`).join(',');
+  // quantifiers ({start, end}) to increase accuracy of regex instead using .*
+  const regex = new RegExp(
+    `\\[${tagNames}\\].{0,10}\\[${tagNamesWithSkeleton}\\].{140,160}\\.join\\(", "\\)\\,.{50,70}\\.join\\(","\\)\\)\\).{110,130}\\.join\\(","\\)\\+"{visibility:hidden}".{110,130}"<style>"`
+  );
 
   expect(mainChunkFileContent).toMatch(regex);
 
   const withoutTagsMatches = mainChunkFileContent.match(/withoutTags/g);
   const formatHtmlMatches = mainChunkFileContent.match(/format:["']html["']/g);
+  const skeletonTagNamesMatches = mainChunkFileContent.match(/skeletonTagNames/g);
 
   expect(withoutTagsMatches.length).toBe(2);
   expect(formatHtmlMatches.length).toBe(1);
+  expect(skeletonTagNamesMatches.length).toBe(2);
 });
