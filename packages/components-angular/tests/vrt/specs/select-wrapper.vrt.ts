11eaf236a6054770da0f1a02d4e4800b21df8c28
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-07 10:16:20 +0100

    test(components-/angular/js/react): remove unneeded scenario in skeleton vr tests | as | #1339

--- packages/components-angular/tests/vrt/specs/select-wrapper.vrt.ts
+++ packages/components-angular/tests/vrt/specs/select-wrapper.vrt.ts
@@ -13,11 +13,6 @@ it.each(defaultViewports)('should have no visual regression for viewport %s', as
 
 it.each(defaultViewports)('should have no skeleton visual regression for viewport %s', async (viewport) => {
   expect(
-    await vrtTest(getVisualRegressionTester(viewport), 'select-wrapper-skeleton', '/select-wrapper-skeleton', {
-      scenario: async (page) => {
-        await page.click('#open-options');
-        await page.evaluate(() => (window as any).componentsReady());
-      },
-    })
+    await vrtTest(getVisualRegressionTester(viewport), 'select-wrapper-skeleton', '/select-wrapper-skeleton')
   ).toBeFalsy();
 });
