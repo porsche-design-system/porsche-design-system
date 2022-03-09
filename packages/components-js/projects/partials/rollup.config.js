a805c53140c2393080655dd4de28a023b53280f0
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-09 09:41:43 +0100

    refactor(components-js): use rollup-plugin-generate-package-json instead of copying package.json template | bh | #1793

--- packages/components-js/projects/partials/rollup.config.js
+++ packages/components-js/projects/partials/rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
 
 const input = 'src/index.ts';
 
@@ -10,6 +11,16 @@ export default [
     output: {
       dir: 'dist',
       format: 'cjs',
+      plugins: [
+        generatePackageJson({
+          baseContents: {
+            main: 'index.js',
+            module: 'esm/index.js',
+            types: 'index.d.ts',
+            sideEffects: false,
+          },
+        }),
+      ],
     },
     // Our partials contain jsx. We resolve react/jsx-runtime into the build to make it readable in VanillaJS and Angular.
     plugins: [typescript({ declaration: true, declarationDir: 'dist', rootDir: 'src' }), nodeResolve(), commonjs()],
