08d369c0749947d1a778963eabbaf6b9e71f4259
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-09 14:49:20 +0100

    feat(components): add patchStencilMarker, add errors when something goes wrong | as / mz | #1339

--- packages/components/scripts/patchStencil.ts
+++ packages/components/scripts/patchStencil.ts
@@ -1,7 +1,15 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { componentMeta } from '@porsche-design-system/shared';
+const PDS_PATCH_START = '// PDS PATCH START';
+const PDS_PATCH_EMD = '// PDS PATCH END';
 
+/**
+ * This script patches stencil.js behaviour when loading components:
+ * When the shadowDom is attached by stencil, any child becomes "invisible".
+ * To enable us to see skeleton components slotted inside e.g. p-fieldset-wrapper while it is being loaded, we patch a slot into it.
+ * Finally, once the component is loaded, we remove that added slot again.
+ */
 const patchStencil = (): void => {
   const tagNamesToAddSlotTo = Object.entries(componentMeta).reduce((prev, [tagName, value]) => {
     return value.shouldPatchSlot ? [...prev, tagName] : prev;
@@ -10,31 +18,62 @@ const patchStencil = (): void => {
 
   const stencilIndexFilePath = path.resolve(require.resolve('@stencil/core'), '../../client', 'index.js');
   const stencilIndexFile = fs.readFileSync(stencilIndexFilePath, 'utf-8');
+  const pdsPatchStartRegEx = new RegExp(`(${PDS_PATCH_START})`, 'g');
+  const getScriptPatchMarkerCount = (script: string) => (script.match(pdsPatchStartRegEx) || []).length;
 
-  const removeSkeletonFirstChild = 'elm.shadowRoot.removeChild(elm.shadowRoot.firstChild);';
-
-  if (!stencilIndexFile.includes(removeSkeletonFirstChild)) {
-    const addSkeletonSlotScript = `                        if (${tagNamesToAddSlotToAsString}.includes(cmpMeta.$tagName$)) {
-                          const skeletonSlot = document.createElement('slot');
-                          self.shadowRoot.appendChild(skeletonSlot)
-                        }
+  if (getScriptPatchMarkerCount(stencilIndexFile) === 0) {
+    // no markers found, patch the stencil script
+    const addSkeletonSlotScript = `                            ${PDS_PATCH_START}
+                            if (${tagNamesToAddSlotToAsString}.includes(cmpMeta.$tagName$)) {
+                              self.shadowRoot.appendChild(document.createElement('slot'))
+                            }
+                            ${PDS_PATCH_EMD}
 `;
-    const removeSkeletonSlotScript = `    const hasPatchedSkeletonSlot = ${tagNamesToAddSlotToAsString}.some(tagName => {
+    const removeSkeletonSlotScript = `    ${PDS_PATCH_START}
+    const hasPatchedSkeletonSlot = ${tagNamesToAddSlotToAsString}.some(tagName => {
         const tagNameRegExp = new RegExp(\`\${tagName.toUpperCase()}(?!-)\`);
         return elm.tagName.match(tagNameRegExp);
     });
     if (hasPatchedSkeletonSlot) {
         console.log(elm.shadowRoot);
-        ${removeSkeletonFirstChild}
+        elm.shadowRoot.removeChild(elm.shadowRoot.firstChild);
     }
+    ${PDS_PATCH_EMD}
 `;
+    // add skeleton slot script
+    let patchedStencilIndexFile = stencilIndexFile.replace(
+      /(self\.attachShadow\(\{ mode: 'open' \}\);\n.*?\}\n)/g,
+      `$1${addSkeletonSlotScript}`
+    );
 
-    const patchedStencilIndexFile = stencilIndexFile
-      .replace(/(self\.attachShadow\(\{ mode: 'open' \}\);\n.*?\}\n)/g, `$1${addSkeletonSlotScript}`) // add skeleton slot script
-      .replace(/(.*?if \(BUILD\.style && isInitialLoad\) \{)/g, `${removeSkeletonSlotScript}$1`); // remove skeleton slot script
+    if (getScriptPatchMarkerCount(patchedStencilIndexFile) !== 1) {
+      process.stderr.write(
+        `Failed patching skeleton slot for ${tagNamesToAddSlotToAsString} into stencil. Position for addSkeletonSlotScript not found.\n`
+      );
+      return;
+    }
+    // remove skeleton slot script
+    patchedStencilIndexFile = patchedStencilIndexFile.replace(
+      /(.*?if \(BUILD\.style && isInitialLoad\) \{)/g,
+      `${removeSkeletonSlotScript}$1`
+    );
 
-    fs.writeFileSync(stencilIndexFilePath, patchedStencilIndexFile);
-    process.stdout.write(`Successfully patched skeleton slot for ${tagNamesToAddSlotToAsString} into stencil.\n`);
+    if (getScriptPatchMarkerCount(patchedStencilIndexFile) === 2) {
+      // patched successfully
+      fs.writeFileSync(stencilIndexFilePath, patchedStencilIndexFile);
+      process.stdout.write(`Successfully patched skeleton slot for ${tagNamesToAddSlotToAsString} into stencil.\n`);
+    } else if (getScriptPatchMarkerCount(patchedStencilIndexFile) === 1) {
+      process.stderr.write(
+        `Failed patching skeleton slot for ${tagNamesToAddSlotToAsString} into stencil. Position for removeSkeletonSlotScript not found.\n`
+      );
+    } else {
+      // something went wrong, not writing to file
+      process.stderr.write(
+        `Failed patching skeleton slot for ${tagNamesToAddSlotToAsString} into stencil. Not all markers were found.\n`
+      );
+    }
+  } else {
+    process.stderr.write(`Stencil already patched. Doing nothing.\n`);
   }
 };
 
