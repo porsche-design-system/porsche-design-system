f3ecde91c64d15349e22f9ec53e925064ed393f2
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-09 16:09:27 +0100

    feat(shared): set jest's maxWorkers to 1 in CI | bh

--- packages/shared/src/testing/jest.config.ts
+++ packages/shared/src/testing/jest.config.ts
@@ -11,7 +11,7 @@ export const config: Config.InitialOptions = {
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  maxWorkers: process.env.CI === 'true' ? 2 : 4,
+  maxWorkers: process.env.CI === 'true' ? 1 : 4,
   globals: {
     'ts-jest': {
       isolatedModules: true, // disable type-checking and compile each file as an isolated module
