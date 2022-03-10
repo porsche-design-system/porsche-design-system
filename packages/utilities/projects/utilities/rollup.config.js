d27098503c99416de9876c5ba26912782a559a10
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-10 11:13:21 +0100

    refactor(utilities): create package.json via rollup | bh | #1716

--- packages/utilities/projects/utilities/rollup.config.js
+++ packages/utilities/projects/utilities/rollup.config.js
@@ -1,18 +1,33 @@
 import typescript from '@rollup/plugin-typescript';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+
+const input = 'src/jss/index.ts';
 
 export default [
   {
-    input: 'src/jss/index.ts',
+    input,
     output: {
       dir: 'dist/jss',
       format: 'cjs',
+      plugins: [
+        generatePackageJson({
+          baseContents: {
+            name: '@porsche-design-system/utilities-jss',
+            version: '0.0.0',
+            main: 'index.js',
+            module: 'esm/index.js',
+            types: 'types/index.d.ts',
+            sideEffects: false,
+          },
+        }),
+      ],
     },
     plugins: [
       typescript({ declaration: true, declarationDir: 'dist/jss/types', exclude: '**.spec.ts', rootDir: 'src/jss' }),
     ],
   },
   {
-    input: 'src/jss/index.ts',
+    input,
     output: {
       dir: 'dist/jss/esm',
       format: 'esm',
