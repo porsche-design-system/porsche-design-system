b905f9d7c9934b93f1fab913ecb77b1324a009c1
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-10 08:48:08 +0100

    test: execute vrt tests with half as many workers as e2e tests | bh

--- packages/shared/src/testing/jest.config.ts
+++ packages/shared/src/testing/jest.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from '@jest/types';
 
+const defaultWorkers = process.env.CI === 'true' ? 1 : 2;
+const maxWorkers = process.env.TYPE === 'VRT' ? defaultWorkers : defaultWorkers * 2;
+
+console.log('Running Jest with maxWorkers:', maxWorkers);
+
 export const config: Config.InitialOptions = {
   preset: 'jest-puppeteer',
   rootDir: '..',
@@ -11,7 +16,7 @@ export const config: Config.InitialOptions = {
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
-  maxWorkers: process.env.CI === 'true' ? 1 : 2,
+  maxWorkers: maxWorkers,
   testSequencer: '@signed/jest-alphabetical-sequencer',
   globals: {
     'ts-jest': {
