8d6ff047d0954959e19cbc84d3a6184265513223
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-14 11:12:10 +0100

    feat(shared): resolve shared-src package | bh | #1716

--- packages/shared/rollup.config.js
+++ packages/shared/rollup.config.js
@@ -1,6 +1,5 @@
 import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
-import generatePackageJson from 'rollup-plugin-generate-package-json';
 import pkg from './package.json';
 
 const input = 'src/index.ts';
@@ -12,20 +11,6 @@ export default [
       dir: 'dist',
       format: 'cjs',
       preserveModules: true,
-      plugins: [
-        generatePackageJson({
-          baseContents: (packageJson) => ({
-            ...packageJson,
-            name: '@porsche-design-system/shared',
-            main: 'index.js',
-            module: 'esm/index.js',
-            types: 'types/index.d.ts',
-            sideEffects: false,
-            scripts: {},
-            devDependencies: {},
-          }),
-        }),
-      ],
     },
     plugins: [
       copy({
