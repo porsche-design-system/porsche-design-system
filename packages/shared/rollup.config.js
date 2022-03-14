96deca3880d56d044bd3f0751374f55cae177d94
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-14 16:04:25 +0100

    Revert "feat(shared): resolve shared-src package | bh | #1716"
    This reverts commit 8d6ff047d0954959e19cbc84d3a6184265513223.

--- packages/shared/rollup.config.js
+++ packages/shared/rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import copy from 'rollup-plugin-copy';
+import generatePackageJson from 'rollup-plugin-generate-package-json';
 import pkg from './package.json';
 
 const input = 'src/index.ts';
@@ -11,6 +12,20 @@ export default [
       dir: 'dist',
       format: 'cjs',
       preserveModules: true,
+      plugins: [
+        generatePackageJson({
+          baseContents: (packageJson) => ({
+            ...packageJson,
+            name: '@porsche-design-system/shared',
+            main: 'index.js',
+            module: 'esm/index.js',
+            types: 'types/index.d.ts',
+            sideEffects: false,
+            scripts: {},
+            devDependencies: {},
+          }),
+        }),
+      ],
     },
     plugins: [
       copy({
