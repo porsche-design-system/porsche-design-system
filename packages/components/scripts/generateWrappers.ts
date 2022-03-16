98d09ae90ee0eaf6d5d56b020b40d46ddd48968d
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-16 16:49:25 +0100

    feat(components): add skeleton feature toggle for component wrappers | as / bh | #1339

--- packages/components/scripts/generateWrappers.ts
+++ packages/components/scripts/generateWrappers.ts
@@ -1,7 +1,7 @@
 import { WrapperGenerator } from './wrapper-generator';
-
+export const generateSkeletons = true;
 const generateWrappers = (): void => {
-  const generator = new WrapperGenerator();
+  const generator = new WrapperGenerator({ hasSkeletonSupport: true }); // to remove skeleton property classes in the generated wrappers, set to false
   generator.generate('angular');
   generator.generate('react');
   generator.generate('uxpin');
