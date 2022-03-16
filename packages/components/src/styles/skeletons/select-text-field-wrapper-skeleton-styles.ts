37cf6668b979d11fe07543290128592c200ada55
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-16 11:36:43 +0100

    feat(components): reuse selectors for textarea, select and text field wrappers skeletons | as | #1339

--- packages/components/src/styles/skeletons/select-text-field-wrapper-skeleton-styles.ts
+++ packages/components/src/styles/skeletons/select-text-field-wrapper-skeleton-styles.ts
@@ -4,22 +4,21 @@ import {
   extendPseudoWithThemeJssStyle,
   getAfterMinHeight,
   getBaseSkeletonJssStyle,
+  getDescriptionAndLabelSelector,
+  getDescriptionOnlySelector,
   getElementBackgroundGradient,
   getHiddenLabelJssStyle,
+  getHideLabelSelector,
   getSkeletonElementHeight,
-  getSkeletonPropertyNames,
   getThemedPseudoJssStyle,
   LABEL_HEIGHT,
   LABEL_HEIGHT_SPACING,
   LABEL_HEIGHT_WITH_DESCRIPTION,
 } from './';
-import { PDS_SKELETON_CLASS_PREFIX } from '@porsche-design-system/shared';
 import { pxToRemWithUnit } from '../common-styles';
 import type { Styles } from 'jss';
 
 export const getSelectTextFieldWrapperSkeletonStyles = (): Styles<'@global'> => {
-  const skeletonPropertyNames = getSkeletonPropertyNames('p-select-wrapper');
-
   return {
     '@global': {
       'p-select-wrapper, p-text-field-wrapper': {
@@ -33,35 +32,29 @@ export const getSelectTextFieldWrapperSkeletonStyles = (): Styles<'@global'> => 
           height: getSkeletonElementHeight(ELEMENT_SKELETON_DIMENSION),
 
           // TODO: use constants for getComponentMeta for "property" class and values
-          // hideLabel or no description and no label
-          [`&[${skeletonPropertyNames.hideLabel}=true], &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.hideLabel}, &:not([${skeletonPropertyNames.description}]):not([${skeletonPropertyNames.label}]):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label})`]:
-            getHiddenLabelJssStyle(),
-          // description no label and no hideLabel
-          [`&[${skeletonPropertyNames.description}]:not([${skeletonPropertyNames.label}]):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label}), &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}:not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label})`]:
-            {
-              height: getSkeletonElementHeight(ELEMENT_SKELETON_DIMENSION, false, true),
-              '&::before': {
-                height: pxToRemWithUnit(LABEL_HEIGHT),
-                background: getElementBackgroundGradient(72, 6, true),
-              },
-              '&::after': {
-                top: pxToRemWithUnit(LABEL_HEIGHT),
-                minHeight: getAfterMinHeight(LABEL_HEIGHT),
-              },
+          [getHideLabelSelector('p-select-wrapper')]: getHiddenLabelJssStyle(),
+          [getDescriptionOnlySelector('p-select-wrapper')]: {
+            height: getSkeletonElementHeight(ELEMENT_SKELETON_DIMENSION, false, true),
+            '&::before': {
+              height: pxToRemWithUnit(LABEL_HEIGHT),
+              background: getElementBackgroundGradient(72, 6, true),
             },
-          // description, label and not hideLabel
-          [`&[${skeletonPropertyNames.description}][${skeletonPropertyNames.label}]:not([${skeletonPropertyNames.hideLabel}=true]), &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label}:not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.hideLabel})`]:
-            {
-              height: getSkeletonElementHeight(ELEMENT_SKELETON_DIMENSION, true, true),
-              '&::before': {
-                height: pxToRemWithUnit(LABEL_HEIGHT_WITH_DESCRIPTION),
-                background: getElementBackgroundGradient(LABEL_HEIGHT_WITH_DESCRIPTION, LABEL_HEIGHT_SPACING, true),
-              },
-              '&::after': {
-                top: pxToRemWithUnit(LABEL_HEIGHT_WITH_DESCRIPTION),
-                minHeight: getAfterMinHeight(LABEL_HEIGHT_WITH_DESCRIPTION),
-              },
+            '&::after': {
+              top: pxToRemWithUnit(LABEL_HEIGHT),
+              minHeight: getAfterMinHeight(LABEL_HEIGHT),
             },
+          },
+          [getDescriptionAndLabelSelector('p-select-wrapper')]: {
+            height: getSkeletonElementHeight(ELEMENT_SKELETON_DIMENSION, true, true),
+            '&::before': {
+              height: pxToRemWithUnit(LABEL_HEIGHT_WITH_DESCRIPTION),
+              background: getElementBackgroundGradient(LABEL_HEIGHT_WITH_DESCRIPTION, LABEL_HEIGHT_SPACING, true),
+            },
+            '&::after': {
+              top: pxToRemWithUnit(LABEL_HEIGHT_WITH_DESCRIPTION),
+              minHeight: getAfterMinHeight(LABEL_HEIGHT_WITH_DESCRIPTION),
+            },
+          },
           ...getThemedPseudoJssStyle(true),
         },
       },
--- packages/components/src/styles/skeletons/textarea-wrapper-skeleton-styles.ts
+++ packages/components/src/styles/skeletons/textarea-wrapper-skeleton-styles.ts
@@ -12,13 +12,31 @@ import {
   LABEL_HEIGHT_WITH_DESCRIPTION,
   TEXTAREA_SKELETON_HEIGHT,
 } from './';
-import { PDS_SKELETON_CLASS_PREFIX } from '@porsche-design-system/shared';
+import { PDS_SKELETON_CLASS_PREFIX, TagName } from '@porsche-design-system/shared';
 import { pxToRemWithUnit } from '../common-styles';
 import { Styles } from 'jss';
 
-export const getTextareaWrapperSkeletonStyles = (): Styles<'@global'> => {
-  const skeletonPropertyNames = getSkeletonPropertyNames('p-textarea-wrapper');
+type TextAreaSelectWrapperTagName = Extract<TagName, 'p-textarea-wrapper' | 'p-select-wrapper'>;
 
+export const getHideLabelSelector = (tagName: TextAreaSelectWrapperTagName): string => {
+  // hideLabel or no description and no label
+  const skeletonPropertyNames = getSkeletonPropertyNames(tagName);
+  return `&[${skeletonPropertyNames.hideLabel}=true], &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.hideLabel}, &:not([${skeletonPropertyNames.description}]):not([${skeletonPropertyNames.label}]):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label})`;
+};
+
+export const getDescriptionOnlySelector = (tagName: TextAreaSelectWrapperTagName): string => {
+  // description no label and no hideLabel
+  const skeletonPropertyNames = getSkeletonPropertyNames(tagName);
+  return `&[${skeletonPropertyNames.description}]:not([${skeletonPropertyNames.label}]):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label}), &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}:not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label})`;
+};
+
+export const getDescriptionAndLabelSelector = (tagName: TextAreaSelectWrapperTagName): string => {
+  // description, label and not hideLabel
+  const skeletonPropertyNames = getSkeletonPropertyNames(tagName);
+  return `&[${skeletonPropertyNames.description}][${skeletonPropertyNames.label}]:not([${skeletonPropertyNames.hideLabel}=true]), &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label}:not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.hideLabel})`;
+};
+
+export const getTextareaWrapperSkeletonStyles = (): Styles<'@global'> => {
   return {
     '@global': {
       'p-textarea-wrapper': {
@@ -30,35 +48,30 @@ export const getTextareaWrapperSkeletonStyles = (): Styles<'@global'> => {
           minHeight: getSkeletonElementHeight(TEXTAREA_SKELETON_HEIGHT),
 
           // TODO: use constants for getComponentMeta for "property" class and values
-          // hideLabel or no description and no label
-          [`&[${skeletonPropertyNames.hideLabel}=true], &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.hideLabel}, &:not([${skeletonPropertyNames.description}]):not([${skeletonPropertyNames.label}]):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label})`]:
-            getHiddenLabelJssStyle(),
-          // description no label and no hideLabel
-          [`&[${skeletonPropertyNames.description}]:not([${skeletonPropertyNames.label}]):not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label}), &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}:not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label})`]:
-            {
-              minHeight: getSkeletonElementHeight(TEXTAREA_SKELETON_HEIGHT, false, true),
-              '&::before': {
-                height: pxToRemWithUnit(LABEL_HEIGHT),
-                background: getElementBackgroundGradient(72, 6, true),
-              },
-              '&::after': {
-                top: pxToRemWithUnit(LABEL_HEIGHT),
-                minHeight: getAfterMinHeight(LABEL_HEIGHT),
-              },
+
+          [getHideLabelSelector('p-textarea-wrapper')]: getHiddenLabelJssStyle(),
+          [getDescriptionOnlySelector('p-textarea-wrapper')]: {
+            minHeight: getSkeletonElementHeight(TEXTAREA_SKELETON_HEIGHT, false, true),
+            '&::before': {
+              height: pxToRemWithUnit(LABEL_HEIGHT),
+              background: getElementBackgroundGradient(72, 6, true),
             },
-          // description, label and not hideLabel
-          [`&[${skeletonPropertyNames.description}][${skeletonPropertyNames.label}]:not([${skeletonPropertyNames.hideLabel}=true]), &.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.description}.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.label}:not(.${PDS_SKELETON_CLASS_PREFIX}${skeletonPropertyNames.hideLabel})`]:
-            {
-              minHeight: getSkeletonElementHeight(TEXTAREA_SKELETON_HEIGHT, true, true),
-              '&::before': {
-                height: pxToRemWithUnit(LABEL_HEIGHT_WITH_DESCRIPTION),
-                background: getElementBackgroundGradient(LABEL_HEIGHT_WITH_DESCRIPTION, LABEL_HEIGHT_SPACING, true),
-              },
-              '&::after': {
-                top: pxToRemWithUnit(LABEL_HEIGHT_WITH_DESCRIPTION),
-                minHeight: getAfterMinHeight(LABEL_HEIGHT_WITH_DESCRIPTION),
-              },
+            '&::after': {
+              top: pxToRemWithUnit(LABEL_HEIGHT),
+              minHeight: getAfterMinHeight(LABEL_HEIGHT),
             },
+          },
+          [getDescriptionAndLabelSelector('p-textarea-wrapper')]: {
+            minHeight: getSkeletonElementHeight(TEXTAREA_SKELETON_HEIGHT, true, true),
+            '&::before': {
+              height: pxToRemWithUnit(LABEL_HEIGHT_WITH_DESCRIPTION),
+              background: getElementBackgroundGradient(LABEL_HEIGHT_WITH_DESCRIPTION, LABEL_HEIGHT_SPACING, true),
+            },
+            '&::after': {
+              top: pxToRemWithUnit(LABEL_HEIGHT_WITH_DESCRIPTION),
+              minHeight: getAfterMinHeight(LABEL_HEIGHT_WITH_DESCRIPTION),
+            },
+          },
 
           ...getThemedPseudoJssStyle(true),
         },
