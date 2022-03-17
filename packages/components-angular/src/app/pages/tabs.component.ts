0bb395d67470501ff56585ffa3ada30b0354cf63
Stephan Schröter <stephan.schroeter@mhp.com>
2022-03-17 18:24:51 +0100

    test(components-js): improve vrt fixture for tabs | sas | #1791

--- packages/components-angular/src/app/pages/tabs.component.ts
+++ packages/components-angular/src/app/pages/tabs.component.ts
@@ -18,7 +18,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
     <div class="playground dark" title="should render tabs on dark background">
       <p-tabs [theme]="'dark'">
-        <p-tabs-item [label]="'Tab 1'"><div style="color: white">Tab Content 1</div></p-tabs-item>
+        <p-tabs-item [label]="'Tab 1'">Tab Content 1</p-tabs-item>
         <p-tabs-item [label]="'Tab 2'">Tab Content 2</p-tabs-item>
         <p-tabs-item [label]="'Tab 3'">Tab Content 3</p-tabs-item>
         <p-tabs-item [label]="'Tab 4'">Tab Content 4</p-tabs-item>
@@ -48,7 +48,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         <p-tabs-item [label]="'Tab 4'">Tab Content 4</p-tabs-item>
         <p-tabs-item [label]="'Tab 5'">Tab Content 5</p-tabs-item>
         <p-tabs-item [label]="'Tab 6'">Tab Content 6</p-tabs-item>
-        <p-tabs-item [label]="'Tab 7'"><div style="color: white">Tab Content 7</div></p-tabs-item>
+        <p-tabs-item [label]="'Tab 7'">Tab Content 7</p-tabs-item>
       </p-tabs>
     </div>
 
@@ -66,7 +66,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
     <div class="playground dark" title="should render tabs size medium on dark background">
       <p-tabs [theme]="'dark'" [size]="'medium'">
-        <p-tabs-item [label]="'Tab 1'"><div style="color: white">Tab Content 1</div></p-tabs-item>
+        <p-tabs-item [label]="'Tab 1'">Tab Content 1</p-tabs-item>
         <p-tabs-item [label]="'Tab 2'">Tab Content 2</p-tabs-item>
         <p-tabs-item [label]="'Tab 3'">Tab Content 3</p-tabs-item>
         <p-tabs-item [label]="'Tab 4'">Tab Content 4</p-tabs-item>
@@ -96,7 +96,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         <p-tabs-item [label]="'Tab 4'">Tab Content 4</p-tabs-item>
         <p-tabs-item [label]="'Tab 5'">Tab Content 5</p-tabs-item>
         <p-tabs-item [label]="'Tab 6'">Tab Content 6</p-tabs-item>
-        <p-tabs-item [label]="'Tab 7'"><div style="color: white">Tab Content 7</div></p-tabs-item>
+        <p-tabs-item [label]="'Tab 7'">Tab Content 7</p-tabs-item>
       </p-tabs>
     </div>
 
@@ -114,7 +114,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
     <div class="playground dark" title="should render tabs semibold on dark background">
       <p-tabs [theme]="'dark'" [weight]="'semibold'">
-        <p-tabs-item [label]="'Tab 1'"><div style="color: white">Tab Content 1</div></p-tabs-item>
+        <p-tabs-item [label]="'Tab 1'">Tab Content 1</p-tabs-item>
         <p-tabs-item [label]="'Tab 2'">Tab Content 2</p-tabs-item>
         <p-tabs-item [label]="'Tab 3'">Tab Content 3</p-tabs-item>
         <p-tabs-item [label]="'Tab 4'">Tab Content 4</p-tabs-item>
@@ -138,7 +138,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
     <div class="playground dark" title="should render tabs semibold and medium on dark background">
       <p-tabs [theme]="'dark'" [weight]="'semibold'" [size]="'medium'">
-        <p-tabs-item [label]="'Tab 1'"><div style="color: white">Tab Content 1</div></p-tabs-item>
+        <p-tabs-item [label]="'Tab 1'">Tab Content 1</p-tabs-item>
         <p-tabs-item [label]="'Tab 2'">Tab Content 2</p-tabs-item>
         <p-tabs-item [label]="'Tab 3'">Tab Content 3</p-tabs-item>
         <p-tabs-item [label]="'Tab 4'">Tab Content 4</p-tabs-item>
@@ -162,7 +162,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
     <div class="playground dark" title="should render tabs gradientColorScheme surface on dark background">
       <p-tabs [theme]="'dark'" [gradientColorScheme]="'surface'">
-        <p-tabs-item [label]="'Tab 1'"><div style="color: white">Tab Content 1</div></p-tabs-item>
+        <p-tabs-item [label]="'Tab 1'">Tab Content 1</p-tabs-item>
         <p-tabs-item [label]="'Tab 2'">Tab Content 2</p-tabs-item>
         <p-tabs-item [label]="'Tab 3'">Tab Content 3</p-tabs-item>
         <p-tabs-item [label]="'Tab 4'">Tab Content 4</p-tabs-item>
@@ -192,7 +192,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         <p-tabs-item [label]="'Tab 1'">Tab Content 1</p-tabs-item>
         <p-tabs-item [label]="'Tab 2'">Tab Content 2</p-tabs-item>
         <p-tabs-item [label]="'Tab 3'">Tab Content 3</p-tabs-item>
-        <p-tabs-item [label]="'Tab 4'"><div style="color: white">Tab Content 4</div></p-tabs-item>
+        <p-tabs-item [label]="'Tab 4'">Tab Content 4</p-tabs-item>
         <p-tabs-item [label]="'Tab 5'">Tab Content 5</p-tabs-item>
         <p-tabs-item [label]="'Tab 6'">Tab Content 6</p-tabs-item>
         <p-tabs-item [label]="'Tab 7'">Tab Content 7</p-tabs-item>
