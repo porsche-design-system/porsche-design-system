5c8f34a383ebc2f79fbe727b14c26164cb6aa78b
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-21 15:05:34 +0100

    feat(components): remove unused variable | as | #1339

--- packages/components/scripts/generateWrappers.ts
+++ packages/components/scripts/generateWrappers.ts
@@ -1,5 +1,4 @@
 import { WrapperGenerator } from './wrapper-generator';
-export const generateSkeletons = true;
 const generateWrappers = (): void => {
   const generator = new WrapperGenerator({ hasSkeletonSupport: true }); // to remove skeleton property classes in the generated wrappers, set to false
   generator.generate('angular');
