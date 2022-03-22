b9794ab9130089e722cba152322b0c5269b4e9e2
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-22 10:50:21 +0100

    feat(components): remove headline, text, text list and text list item skeleton styles and tests | as | #1339

--- packages/components/src/styles/skeletons/text-list-skeleton-styles.spec.ts
+++
@@ -1,5 +0,0 @@
-// import { getTextListSkeletonStyles } from './text-list-skeleton-styles';
-//
-// xit('should return correct css', () => {
-//   expect(getTextListSkeletonStyles()).toMatchSnapshot();
-// });
