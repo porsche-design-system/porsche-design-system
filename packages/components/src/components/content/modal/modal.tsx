bf70d88db3320d8aab6cd549e55460d431d08105
Stephan Schröter <stephan.schroeter@mhp.com>
2022-03-22 09:59:41 +0100

    feat(components): make scroll top of modal more robust | sas / bh | #1808

--- packages/components/src/components/content/modal/modal.tsx
+++ packages/components/src/components/content/modal/modal.tsx
@@ -87,7 +87,10 @@ export class Modal {
 
   public componentDidRender(): void {
     if (this.open) {
-      setTimeout(() => (this.host.scrollTop = 0), 25); // reset scroll top to zero in case content is longer than viewport height, - some timeout is needed although it shouldn't
+      // reset scroll top to zero in case content is longer than viewport height, - some timeout is needed although it shouldn't
+      for (let i = 0; i < 4; i++) {
+        setTimeout(() => (this.host.scrollTop = 0), i * 5);
+      }
       this.dialog.focus(); // needs to happen after render
     }
   }
