95a04918172dcd1d3baa8216116707fe59e55170
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-22 09:02:24 +0100

    feat(components): comment out all unneeded skeleton styles and tests | as | #1339

--- packages/components/src/styles/skeletons/text-list-skeleton-styles.ts
+++ packages/components/src/styles/skeletons/text-list-skeleton-styles.ts
@@ -1,16 +1,16 @@
-import { pxToRemWithUnit } from '../common-styles';
-import { getWrapperSkeletonJssStyle } from './fieldset-wrapper-skeleton-styles';
-import { Styles } from 'jss';
-
-export const getTextListSkeletonStyles = (): Styles<'@global'> => {
-  return {
-    '@global': {
-      'p-text-list': {
-        '&:not(.hydrated)': {
-          ...getWrapperSkeletonJssStyle(),
-          marginTop: pxToRemWithUnit(6),
-        },
-      },
-    },
-  };
-};
+// import { pxToRemWithUnit } from '../common-styles';
+// import { getWrapperSkeletonJssStyle } from './fieldset-wrapper-skeleton-styles';
+// import { Styles } from 'jss';
+//
+// export const getTextListSkeletonStyles = (): Styles<'@global'> => {
+//   return {
+//     '@global': {
+//       'p-text-list': {
+//         '&:not(.hydrated)': {
+//           ...getWrapperSkeletonJssStyle(),
+//           marginTop: pxToRemWithUnit(6),
+//         },
+//       },
+//     },
+//   };
+// };
