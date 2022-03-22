805b5486874ec734a59b8db0d20b6a0491d8eac2
Stephan Schröter <stephan.schroeter@mhp.com>
2022-03-22 17:45:56 +0100

    feat(utilities/utilities): use fontSize object in headline + title styles | sas

--- packages/utilities/projects/utilities/src/jss/typography/headline1.ts
+++ packages/utilities/projects/utilities/src/jss/typography/headline1.ts
@@ -1,21 +1,17 @@
-import { fontBehavior, fontFamily, fontStyle, fontVariant, fontWeight } from '../font/font';
+import { fontBehavior, fontFamily, fontSize, fontStyle, fontVariant, fontWeight } from '../font/font';
 import { mediaQueryMin, mediaQueryMinMax } from '../media-query';
+
+const { large, xLarge } = fontSize;
 
 export const headline1 = {
   font: `${fontStyle} ${fontVariant} ${fontWeight.semibold} 1.75rem/1.4285714286 ${fontFamily}`,
   ...fontBehavior,
-  [mediaQueryMinMax('s', 'm')]: {
-    fontSize: '2.25rem',
-    lineHeight: 1.3333333333,
-  },
+  [mediaQueryMinMax('s', 'm')]: large,
   [mediaQueryMinMax('m', 'l')]: {
     fontSize: '2.75rem',
     lineHeight: 1.1818181818,
   },
-  [mediaQueryMinMax('l', 'xl')]: {
-    fontSize: '3.25rem',
-    lineHeight: 1.2307692308,
-  },
+  [mediaQueryMinMax('l', 'xl')]: xLarge,
   [mediaQueryMin('xl')]: {
     fontSize: '3.75rem',
     lineHeight: 1.2,
--- packages/utilities/projects/utilities/src/jss/typography/titleLarge.ts
+++ packages/utilities/projects/utilities/src/jss/typography/titleLarge.ts
@@ -1,4 +1,4 @@
-import { fontBehavior, fontFamily, fontStyle, fontVariant, fontWeight } from '../font/font';
+import { fontBehavior, fontFamily, fontSize, fontStyle, fontVariant, fontWeight } from '../font/font';
 import { mediaQueryMin, mediaQueryMinMax } from '../media-query';
 
 export const titleLarge = {
@@ -8,10 +8,7 @@ export const titleLarge = {
     fontSize: '2.625rem',
     lineHeight: 1.2380952381,
   },
-  [mediaQueryMinMax('m', 'l')]: {
-    fontSize: '3.25rem',
-    lineHeight: 1.2307692308,
-  },
+  [mediaQueryMinMax('m', 'l')]: fontSize.xLarge,
   [mediaQueryMinMax('l', 'xl')]: {
     fontSize: '3.875rem',
     lineHeight: 1.2258064516,
