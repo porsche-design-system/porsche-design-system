dfb098360b762461c44921e5c8ed608db4752d11
Stephan Schröter <stephan.schroeter@mhp.com>
2022-03-22 14:06:51 +0100

    feat(utilities): fix typing | sas / bh | #1808

--- packages/utilities/projects/utilities/src/jss/font/font-shared.ts
+++ packages/utilities/projects/utilities/src/jss/font/font-shared.ts
@@ -2,8 +2,8 @@ export type FontSize = 'xSmall' | 'small' | 'medium' | 'large' | 'xLarge';
 export type FontWeight = 'thin' | 'regular' | 'semibold' | 'bold';
 export type FontSizeLineHeight = { fontSize: string; lineHeight: number };
 export type FontBehavior = {
-  overflowWrap: string;
-  hyphens: string;
-  WebkitTextSizeAdjust: string;
-  textSizeAdjust: string;
+  overflowWrap: 'break-word';
+  hyphens: 'auto';
+  WebkitTextSizeAdjust: 'none';
+  textSizeAdjust: 'none';
 };
