775545030cd1b91ba191d2254dcec3bd6247c958
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-23 12:30:18 +0100

    feat(shared): enable skeletons for counter test | as / mz | #1339

--- packages/shared/src/constants/skeleton.ts
+++ packages/shared/src/constants/skeleton.ts
@@ -3,7 +3,7 @@
  * and not generate skeleton styles in the initialStylesPartial
  * set this to false
  */
-export const SKELETONS_ACTIVE = false;
+export const SKELETONS_ACTIVE = true;
 export const PDS_SKELETON_CLASS_PREFIX = 'pds-skeleton--';
 export const describeif = SKELETONS_ACTIVE ? describe : xdescribe;
 export const itif = SKELETONS_ACTIVE ? it : xit;
