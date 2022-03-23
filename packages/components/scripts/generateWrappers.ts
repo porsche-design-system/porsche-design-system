838c9491b99ca95b2b7dd6a06a19fb4f5f45fbf9
Anton Schmidt <anton.schmidt@mhp.com>
2022-03-23 10:47:00 +0100

    feat(components): turn off skeleton property class name generation in angular + react (skeleton feature toggle) | as / mz | #1339

--- packages/components/scripts/generateWrappers.ts
+++ packages/components/scripts/generateWrappers.ts
@@ -1,6 +1,6 @@
 import { WrapperGenerator } from './wrapper-generator';
 const generateWrappers = (): void => {
-  const generator = new WrapperGenerator({ hasSkeletonSupport: true }); // to remove skeleton property classes in the generated wrappers, set to false
+  const generator = new WrapperGenerator({ hasSkeletonSupport: false }); // to remove skeleton property classes in the generated wrappers, set to false
   generator.generate('angular');
   generator.generate('react');
   generator.generate('uxpin');
