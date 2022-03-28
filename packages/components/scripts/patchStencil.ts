752152fe85bdab4e8062e0b24e4c14907947fe71
Bernd Hacker <hacker.bernd@gmail.com>
2022-03-28 15:09:14 +0200

    fix(components): make patchStencil regex support prefixing | bh / as | #1339

--- packages/components/scripts/patchStencil.ts
+++ packages/components/scripts/patchStencil.ts
@@ -46,8 +46,7 @@ const patchStencil = (): void => {
       const removeSkeletonSlotScript = `    ${PDS_PATCH_START}
     // NOTE: this following is executed on every component update
     const hasPatchedSkeletonSlot = ${tagNamesToAddSlotToAsString}.some(tagName => {
-        const tagNameRegExp = new RegExp(\`^\${tagName}$\`, 'i');
-        return elm.tagName.match(tagNameRegExp);
+        return elm.tagName.match(new RegExp(\`^(?:[\\w-]+-)?\${tagName}$\`, 'i'));
     });
     if (hasPatchedSkeletonSlot) {
         elm.shadowRoot.removeChild(elm.shadowRoot.firstChild);
