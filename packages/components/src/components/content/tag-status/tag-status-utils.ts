5fa24f934bad87a24674812ac7c6c40445f8bf0b
Matthias Haegele <matthias.haegele@mhp.com>
2022-03-29 16:23:20 +0200

    feat(components): first draft of tag component | mh / bh | #1752

--- packages/components/src/components/content/tag-status/tag-status-utils.ts
+++ packages/components/src/components/content/tag-status/tag-status-utils.ts
@@ -1,18 +1,1 @@
-import { Theme } from '../../../types';
-import { isThemeDark } from '../../../utils';
-
-export type TagColors =
-  | 'background-default'
-  | 'background-surface'
-  | 'contrast-high'
-  | 'notification-neutral-soft'
-  | 'notification-success-soft'
-  | 'notification-error-soft'
-  | 'notification-warning-soft';
-
-export const getTheme = (theme: Theme, color: TagColors) => {
-  const isDarkTheme = isThemeDark(theme);
-  const isContrastHigh = color === 'contrast-high';
-
-  return !isContrastHigh ? theme : isDarkTheme ? 'light' : 'dark';
-};
+export type TagColor = 'default' | 'surface' | 'contrast-high' | 'neutral' | 'success' | 'error' | 'warning';
