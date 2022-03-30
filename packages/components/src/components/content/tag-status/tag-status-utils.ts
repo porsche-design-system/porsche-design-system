f69c1982d2f1cc5367261d3d90a8d8b20075f6da
Matthias Haegele <matthias.haegele@mhp.com>
2022-03-30 14:56:19 +0200

    feat(components): align color names with text component | mh | #1752

--- packages/components/src/components/content/tag-status/tag-status-utils.ts
+++ packages/components/src/components/content/tag-status/tag-status-utils.ts
@@ -1,1 +1,13 @@
-export type TagColor = 'default' | 'surface' | 'contrast-high' | 'neutral' | 'success' | 'error' | 'warning';
+import type { TextColor } from '../../../types';
+
+export type TagColor =
+  | Extract<
+      TextColor,
+      | 'default'
+      | 'neutral-contrast-high'
+      | 'notification-success'
+      | 'notification-warning'
+      | 'notification-error'
+      | 'notification-neutral'
+    >
+  | 'background-surface';
