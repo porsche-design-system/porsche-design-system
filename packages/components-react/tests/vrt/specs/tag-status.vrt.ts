99e363757f19fb01e769e4a33619df93be875360
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-01 14:42:08 +0200

    test(components-react/angular): add vrt specs | mh | #1752

---
+++ packages/components-react/tests/vrt/specs/tag-status.vrt.ts
@@ -0,0 +1,5 @@
+import { defaultViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
+
+it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
+  expect(await vrtTest(getVisualRegressionTester(viewport), 'tag-status', '/tag-status')).toBeFalsy();
+});
