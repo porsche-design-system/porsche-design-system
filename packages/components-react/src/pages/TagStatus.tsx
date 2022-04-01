9a3bfc55efab2992fd2bdd1c6d1c72007668017e
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-01 14:38:05 +0200

    test(components-js): add tag-status page | mh | #1752

---
+++ packages/components-react/src/pages/TagStatus.tsx
@@ -0,0 +1,115 @@
+/* Auto Generated File */
+import { PTagStatus } from '@porsche-design-system/components-react';
+
+export const TagStatusPage = (): JSX.Element => {
+  const style = `
+    p-tag-status:not(:last-child) {
+      margin-right: 0.5rem;
+    }
+  `;
+
+  return (
+    <>
+      <style children={style} />
+
+      <div className="playground light" title="should show default tag on light background">
+        <PTagStatus>Tag with default props</PTagStatus>
+      </div>
+
+      <div className="playground light" title="should show different background colors on light background">
+        <PTagStatus color="default">Tag default</PTagStatus>
+        <PTagStatus color="background-surface">Tag background-surface</PTagStatus>
+        <PTagStatus color="neutral-contrast-high">Tag neutral-contrast-high</PTagStatus>
+        <PTagStatus color="notification-neutral">Tag notification-neutral</PTagStatus>
+        <PTagStatus color="notification-success">Tag notification-success</PTagStatus>
+        <PTagStatus color="notification-error">Tag notification-error</PTagStatus>
+        <PTagStatus color="notification-warning">Tag notification-warning</PTagStatus>
+      </div>
+
+      <div className="playground light" title="should show different background colors and icons on light background">
+        <PTagStatus icon="car" color="default">Tag default</PTagStatus>
+        <PTagStatus icon="highway" color="background-surface">Tag background-surface</PTagStatus>
+        <PTagStatus iconSource="./assets/icon-custom-kaixin.svg">Tag custom icon</PTagStatus>
+      </div>
+
+      <div className="playground light" title="should show different background colors with link on light background">
+        <PTagStatus color="default"><a href="#">Tag default with link</a></PTagStatus>
+        <PTagStatus color="background-surface"><a href="#">Tag background-surface with link</a></PTagStatus>
+      </div>
+
+      <div className="playground light" title="should show different background colors with link and icon on light background">
+        <PTagStatus icon="car" color="default"><a href="#">Tag default with link</a></PTagStatus>
+        <PTagStatus icon="highway" color="background-surface"
+          ><a href="#">Tag background-surface with link</a></PTagStatus
+        >
+      </div>
+
+      <div className="playground light" title="should show different background colors with button on light background">
+        <PTagStatus color="default"><button>Tag default with link</button>></PTagStatus>
+        <PTagStatus color="background-surface"><button>Tag background-surface with link</button>></PTagStatus>
+      </div>
+
+      <div className="playground light" title="should show different background colors with button and icon on light background">
+        <PTagStatus icon="car" color="default"><button>Tag default with link</button>></PTagStatus>
+        <PTagStatus icon="highway" color="background-surface"
+          ><button>Tag background-surface with link</button>></PTagStatus
+        >
+      </div>
+
+      <div className="playground light" title="should show different multiline tags on light background">
+        <div style={{ width: '100px' }}>
+          <PTagStatus color="neutral-contrast-high">Some multiline tag</PTagStatus>
+          <PTagStatus color="neutral-contrast-high" icon="car">Some multiline tag</PTagStatus>
+          <PTagStatus color="notification-success"><a href="#">Some multiline Link</a></PTagStatus>
+          <PTagStatus color="notification-success" icon="car"><button>Some multiline Button</button></PTagStatus>
+        </div>
+      </div>
+
+      <div className="playground dark" title="should show default tag on dark background">
+        <PTagStatus theme="dark">Tag with default props</PTagStatus>
+      </div>
+
+      <div className="playground dark" title="should show different background colors on dark background">
+        <PTagStatus theme="dark" color="default">Tag default</PTagStatus>
+        <PTagStatus theme="dark" color="background-surface">Tag background-surface</PTagStatus>
+        <PTagStatus theme="dark" color="neutral-contrast-high">Tag neutral-contrast-high</PTagStatus>
+        <PTagStatus theme="dark" color="notification-neutral">Tag notification-neutral</PTagStatus>
+        <PTagStatus theme="dark" color="notification-success">Tag notification-success</PTagStatus>
+        <PTagStatus theme="dark" color="notification-error">Tag notification-error</PTagStatus>
+        <PTagStatus theme="dark" color="notification-warning">Tag notification-warning</PTagStatus>
+      </div>
+
+      <div className="playground dark" title="should show different background colors and icons on light dark">
+        <PTagStatus theme="dark" icon="car" color="default">Tag default</PTagStatus>
+        <PTagStatus theme="dark" icon="highway" color="background-surface">Tag background-surface</PTagStatus>
+        <PTagStatus theme="dark" iconSource="./assets/icon-custom-kaixin.svg">Tag custom icon</PTagStatus>
+      </div>
+
+      <div className="playground dark" title="should show different background colors with link on dark background">
+        <PTagStatus theme="dark" color="default"><a href="#">Tag default with link</a></PTagStatus>
+        <PTagStatus theme="dark" color="background-surface"><a href="#">Tag background-surface with link</a></PTagStatus>
+      </div>
+
+      <div className="playground dark" title="should show different background colors with link and icon on dark background">
+        <PTagStatus theme="dark" icon="car" color="default"><a href="#">Tag default with link</a></PTagStatus>
+        <PTagStatus theme="dark" icon="highway" color="background-surface"
+          ><a href="#">Tag background-surface with link</a></PTagStatus
+        >
+      </div>
+
+      <div className="playground dark" title="should show different background colors with button on dark background">
+        <PTagStatus theme="dark" color="default"><button>Tag default with link</button>></PTagStatus>
+        <PTagStatus theme="dark" color="background-surface"
+          ><button>Tag background-surface with link</button>></PTagStatus
+        >
+      </div>
+
+      <div className="playground dark" title="should show different background colors with button and icon on dark background">
+        <PTagStatus theme="dark" icon="car" color="default"><button>Tag default with link</button>></PTagStatus>
+        <PTagStatus theme="dark" icon="highway" color="background-surface"
+          ><button>Tag background-surface with link</button>></PTagStatus
+        >
+      </div>
+    </>
+  );
+};
