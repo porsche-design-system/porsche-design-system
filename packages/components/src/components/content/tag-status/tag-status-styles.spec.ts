0344b9d9ec29546c113d2f4d3fe7f93c93b71750
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-04 13:59:15 +0200

    test(components): extend styles snapshot | mh / bh | #1752

--- packages/components/src/components/content/tag-status/tag-status-styles.spec.ts
+++ packages/components/src/components/content/tag-status/tag-status-styles.spec.ts
@@ -2,11 +2,23 @@ import { getComponentCss } from './tag-status-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['light', 'default', 'car', true],
-    ['light', 'neutral-contrast-high', 'highway', true],
-    ['light', 'background-surface', undefined, true],
-    ['dark', 'notification-error', 'car', false],
-  ])('should return correct css for theme: %s, color: %s, icon: %s and isFocusable: %s', (...args) => {
+    ['light', 'default', true],
+    ['light', 'default', false],
+    ['light', 'neutral-contrast-high', true],
+    ['light', 'notification-success', true],
+    ['light', 'notification-warning', true],
+    ['light', 'notification-error', true],
+    ['light', 'notification-neutral', true],
+    ['light', 'background-surface', true],
+    ['dark', 'default', true],
+    ['dark', 'default', false],
+    ['dark', 'neutral-contrast-high', true],
+    ['dark', 'notification-success', true],
+    ['dark', 'notification-warning', true],
+    ['dark', 'notification-error', true],
+    ['dark', 'notification-neutral', true],
+    ['dark', 'background-surface', true],
+  ])('should return correct css for theme: %s, color: %s and isFocusable: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
 });
