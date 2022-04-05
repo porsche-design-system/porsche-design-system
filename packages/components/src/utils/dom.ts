3e8542fce38d7febd636ae6816e4d77e19ec56d5
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-05 13:07:25 +0200

    feat(components): rename type / host to element | mh / bh | #1752

--- packages/components/src/utils/dom.ts
+++ packages/components/src/utils/dom.ts
@@ -3,27 +3,33 @@ import type { FormState } from '../types';
 import type { TagNameCamelCase } from '@porsche-design-system/shared';
 import { getPrefixedTagNames, getTagName } from './tag-name';
 
-type Host = HTMLElement | ShadowRoot;
+type HTMLElementOrShadowRoot = HTMLElement | ShadowRoot;
 
 // prettier-ignore
-export function getHTMLElement<K extends keyof HTMLElementTagNameMap>(host: Host, selector: K): HTMLElementTagNameMap[K] | null;
-export function getHTMLElement<E extends Element = Element>(host: Host, selector: string): E | null;
+export function getHTMLElement<K extends keyof HTMLElementTagNameMap>(element: HTMLElementOrShadowRoot, selector: K): HTMLElementTagNameMap[K] | null;
+export function getHTMLElement<E extends Element = Element>(
+  element: HTMLElementOrShadowRoot,
+  selector: string
+): E | null;
 // prettier-ignore
-export function getHTMLElement<K extends keyof HTMLElementTagNameMap>(host: Host, selector: K): HTMLElementTagNameMap[K] | null {
-  return host.querySelector(selector);
+export function getHTMLElement<K extends keyof HTMLElementTagNameMap>(element: HTMLElementOrShadowRoot, selector: K): HTMLElementTagNameMap[K] | null {
+  return element.querySelector(selector);
 }
 
 // prettier-ignore
-export function getHTMLElements<K extends keyof HTMLElementTagNameMap>(host: Host, selector: K): HTMLElementTagNameMap[K][];
-export function getHTMLElements<E extends Element = Element>(host: Host, selector: string): E[];
+export function getHTMLElements<K extends keyof HTMLElementTagNameMap>(element: HTMLElementOrShadowRoot, selector: K): HTMLElementTagNameMap[K][];
+export function getHTMLElements<E extends Element = Element>(element: HTMLElementOrShadowRoot, selector: string): E[];
 // prettier-ignore
-export function getHTMLElements<K extends keyof HTMLElementTagNameMap>(host: Host, selector: K): HTMLElementTagNameMap[K][] {
-  return Array.from(host.querySelectorAll(selector));
+export function getHTMLElements<K extends keyof HTMLElementTagNameMap>(element: HTMLElementOrShadowRoot, selector: K): HTMLElementTagNameMap[K][] {
+  return Array.from(element.querySelectorAll(selector));
 }
 
 // prettier-ignore
 export function getShadowRootHTMLElement<K extends keyof HTMLElementTagNameMap>(host: HTMLElement, selector: K): HTMLElementTagNameMap[K] | null;
-export function getShadowRootHTMLElement<E extends Element = Element>(host: Host, selector: string): E | null;
+export function getShadowRootHTMLElement<E extends Element = Element>(
+  element: HTMLElementOrShadowRoot,
+  selector: string
+): E | null;
 // prettier-ignore
 export function getShadowRootHTMLElement<K extends keyof HTMLElementTagNameMap>(host: HTMLElement, selector: string): HTMLElementTagNameMap[K] | null {
   return getHTMLElement(host.shadowRoot, selector);
@@ -57,33 +63,37 @@ export type HTMLElementWithRequiredProp = HTMLElement & { required: boolean };
 
 export const isRequired = (el: HTMLElementWithRequiredProp): boolean => !!el.required;
 
-export const hasNamedSlot = (el: Host, slotName: string): boolean => !!getHTMLElement(el, `[slot="${slotName}"]`);
+export const hasNamedSlot = (el: HTMLElementOrShadowRoot, slotName: string): boolean =>
+  !!getHTMLElement(el, `[slot="${slotName}"]`);
 
-export const getSlotTextContent = (el: Host, slotName: string): string =>
+export const getSlotTextContent = (el: HTMLElementOrShadowRoot, slotName: string): string =>
   getHTMLElement(el, `[slot="${slotName}"]`)?.textContent;
 
-export const hasLabel = (host: Host, label: string): boolean => {
-  return !!label || hasNamedSlot(host, 'label');
+export const hasLabel = (element: HTMLElementOrShadowRoot, label: string): boolean => {
+  return !!label || hasNamedSlot(element, 'label');
 };
 
-export const hasMessage = (host: Host, message: string, state: FormState): boolean => {
-  return !!(message || hasNamedSlot(host, 'message')) && ['success', 'error'].includes(state);
+export const hasMessage = (element: HTMLElementOrShadowRoot, message: string, state: FormState): boolean => {
+  return !!(message || hasNamedSlot(element, 'message')) && ['success', 'error'].includes(state);
 };
 
-export const hasDescription = (host: Host, description: string): boolean => {
-  return !!description || hasNamedSlot(host, 'description');
+export const hasDescription = (element: HTMLElementOrShadowRoot, description: string): boolean => {
+  return !!description || hasNamedSlot(element, 'description');
 };
 
-export const hasHeading = (host: Host, heading: string): boolean => {
-  return !!heading || hasNamedSlot(host, 'heading');
+export const hasHeading = (element: HTMLElementOrShadowRoot, heading: string): boolean => {
+  return !!heading || hasNamedSlot(element, 'heading');
 };
 
 // prettier-ignore
-export function getHTMLElementAndThrowIfUndefined<K extends keyof HTMLElementTagNameMap>(host: Host, selector: K): HTMLElementTagNameMap[K] | null;
-export function getHTMLElementAndThrowIfUndefined<E extends Element = Element>(host: Host, selector: string): E | null;
+export function getHTMLElementAndThrowIfUndefined<K extends keyof HTMLElementTagNameMap>(element: HTMLElementOrShadowRoot, selector: K): HTMLElementTagNameMap[K] | null;
+export function getHTMLElementAndThrowIfUndefined<E extends Element = Element>(
+  element: HTMLElementOrShadowRoot,
+  selector: string
+): E | null;
 // prettier-ignore
-export function getHTMLElementAndThrowIfUndefined<K extends keyof HTMLElementTagNameMap>(host: Host, selector: K): HTMLElementTagNameMap[K] | null {
-  const el = getHTMLElement(host, selector);
+export function getHTMLElementAndThrowIfUndefined<K extends keyof HTMLElementTagNameMap>(element: HTMLElementOrShadowRoot, selector: K): HTMLElementTagNameMap[K] | null {
+  const el = getHTMLElement(element, selector);
   if (!el) {
     throw new Error(`Child HTMLElement ${selector} is missing.`);
   }
