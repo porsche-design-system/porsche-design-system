fe06663ff8597934d6ad796c5797c4d7e47821cb
Bernd Hacker <hacker.bernd@gmail.com>
2022-04-05 15:16:07 +0200

    test(components-js): update e2e snapshots | bh | #1752

--- packages/components-js/tests/e2e/specs/tag-status.e2e.ts
+++ packages/components-js/tests/e2e/specs/tag-status.e2e.ts
@@ -1,4 +1,4 @@
-import { Page } from 'puppeteer';
+import type { Page } from 'puppeteer';
 import {
   expectA11yToMatchSnapshot,
   getLifecycleStatus,
@@ -8,63 +8,63 @@ import {
   waitForStencilLifecycle,
 } from '../helpers';
 
-describe('tag-status', () => {
-  let page: Page;
-  beforeEach(async () => (page = await browser.newPage()));
-  afterEach(async () => await page.close());
+let page: Page;
+beforeEach(async () => (page = await browser.newPage()));
+afterEach(async () => await page.close());
 
-  type InitOpts = {
-    withIcon?: boolean;
-  };
+type InitOpts = {
+  withIcon?: boolean;
+};
 
-  const initTagStatus = async (props?: InitOpts) => {
-    const { withIcon } = props ?? {};
-    const content = `<p-tag-status${withIcon ? ' icon="car"' : ''}>Some Tag</p-tag-status>`;
+const initTagStatus = async (props?: InitOpts) => {
+  const { withIcon } = props || {};
+  const attributes = withIcon ? ' icon="car"' : '';
 
-    await setContentWithDesignSystem(page, content);
-  };
+  const content = `<p-tag-status ${attributes}>Some Tag</p-tag-status>`;
 
-  const getHost = () => selectNode(page, 'p-tag-status');
-  const getSpan = () => selectNode(page, 'p-tag-status >>> span');
+  await setContentWithDesignSystem(page, content);
+};
 
-  describe('lifecycle', () => {
-    it('should work without unnecessary round trips on init', async () => {
-      await initTagStatus();
-      const status = await getLifecycleStatus(page);
+const getHost = () => selectNode(page, 'p-tag-status');
+const getSpan = () => selectNode(page, 'p-tag-status >>> span');
 
-      expect(status.componentDidLoad['p-tag-status'], 'componentDidLoad: p-tag-status').toBe(1);
+describe('lifecycle', () => {
+  it('should work without unnecessary round trips on init', async () => {
+    await initTagStatus();
+    const status = await getLifecycleStatus(page);
 
-      expect(status.componentDidLoad.all, 'componentDidLoad: all').toBe(1);
-      expect(status.componentDidUpdate.all, 'componentDidUpdate: all').toBe(0);
-    });
+    expect(status.componentDidLoad['p-tag-status'], 'componentDidLoad: p-tag-status').toBe(1);
 
-    it('should work without unnecessary round trips on prop change', async () => {
-      await initTagStatus({ withIcon: true });
-      const host = await getHost();
-
-      await setProperty(host, 'icon', 'highway');
-      await waitForStencilLifecycle(page);
-
-      const status = await getLifecycleStatus(page);
-
-      expect(status.componentDidUpdate['p-tag-status'], 'componentDidUpdate: p-tag-status').toBe(1);
-      expect(status.componentDidUpdate['p-icon'], 'componentDidUpdate: p-icon').toBe(1);
-
-      expect(status.componentDidLoad.all, 'componentDidLoad: all').toBe(2);
-      expect(status.componentDidUpdate.all, 'componentDidUpdate: all').toBe(2);
-    });
+    expect(status.componentDidLoad.all, 'componentDidLoad: all').toBe(1);
+    expect(status.componentDidUpdate.all, 'componentDidUpdate: all').toBe(0);
   });
 
-  describe('accessibility', () => {
-    it('should expose correct initial accessibility tree', async () => {
-      await initTagStatus();
-      await expectA11yToMatchSnapshot(page, await getSpan(), { interestingOnly: false });
-    });
+  it('should work without unnecessary round trips on prop change', async () => {
+    await initTagStatus({ withIcon: true });
+    const host = await getHost();
 
-    it('should expose correct accessibility tree with icon', async () => {
-      await initTagStatus({ withIcon: true });
+    await setProperty(host, 'icon', 'highway');
+    await waitForStencilLifecycle(page);
 
-      await expectA11yToMatchSnapshot(page, await getSpan(), { interestingOnly: false });
-    });
+    const status = await getLifecycleStatus(page);
+
+    expect(status.componentDidUpdate['p-tag-status'], 'componentDidUpdate: p-tag-status').toBe(1);
+    expect(status.componentDidUpdate['p-icon'], 'componentDidUpdate: p-icon').toBe(1);
+
+    expect(status.componentDidLoad.all, 'componentDidLoad: all').toBe(2);
+    expect(status.componentDidUpdate.all, 'componentDidUpdate: all').toBe(2);
+  });
+});
+
+describe('accessibility', () => {
+  it('should expose correct initial accessibility tree', async () => {
+    await initTagStatus();
+    await expectA11yToMatchSnapshot(page, await getSpan(), { interestingOnly: false });
+  });
+
+  it('should expose correct accessibility tree with icon', async () => {
+    await initTagStatus({ withIcon: true });
+
+    await expectA11yToMatchSnapshot(page, await getSpan(), { interestingOnly: false });
   });
 });
