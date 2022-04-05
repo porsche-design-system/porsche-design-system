7f01feddb5723f6b7ea76fd8cf1d2547740ca095
Bernd Hacker <hacker.bernd@gmail.com>
2022-04-05 09:25:54 +0200

    feat(components): remove unneded overflowWrap and hyphens style from tag-status, update snapshots | bh / mh | #1752

--- packages/components/src/components/content/tag-status/tag-status-styles.spec.ts
+++ packages/components/src/components/content/tag-status/tag-status-styles.spec.ts
@@ -2,23 +2,23 @@ import { getComponentCss } from './tag-status-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['light', 'default', true],
-    ['light', 'default', false],
-    ['light', 'neutral-contrast-high', true],
-    ['light', 'notification-success', true],
-    ['light', 'notification-warning', true],
-    ['light', 'notification-error', true],
-    ['light', 'notification-neutral', true],
-    ['light', 'background-surface', true],
-    ['dark', 'default', true],
-    ['dark', 'default', false],
-    ['dark', 'neutral-contrast-high', true],
-    ['dark', 'notification-success', true],
-    ['dark', 'notification-warning', true],
-    ['dark', 'notification-error', true],
-    ['dark', 'notification-neutral', true],
-    ['dark', 'background-surface', true],
-  ])('should return correct css for theme: %s, color: %s and isFocusable: %s', (...args) => {
+    ['default', true, 'light'],
+    ['default', false, 'light'],
+    ['neutral-contrast-high', true, 'light'],
+    ['notification-success', true, 'light'],
+    ['notification-warning', true, 'light'],
+    ['notification-error', true, 'light'],
+    ['notification-neutral', true, 'light'],
+    ['background-surface', true, 'light'],
+    ['default', true, 'dark'],
+    ['default', false, 'dark'],
+    ['neutral-contrast-high', true, 'dark'],
+    ['notification-success', true, 'dark'],
+    ['notification-warning', true, 'dark'],
+    ['notification-error', true, 'dark'],
+    ['notification-neutral', true, 'dark'],
+    ['background-surface', true, 'dark'],
+  ])('should return correct css for color: %s, isFocusable: %s and theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
 });
