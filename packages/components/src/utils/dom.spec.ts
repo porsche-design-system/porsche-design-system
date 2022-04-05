5894a2d86f91e440560a91611ee16383956fcb4a
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-05 13:06:37 +0200

    test(components): add getHTMLElement tests | mh / bh | #1752

--- packages/components/src/utils/dom.spec.ts
+++ packages/components/src/utils/dom.spec.ts
@@ -2,6 +2,7 @@ import type { HTMLElementWithRequiredProp } from './dom';
 import {
   addEventListener,
   getAttribute,
+  getHTMLElement,
   getHTMLElementAndThrowIfUndefined,
   getRole,
   getSlotTextContent,
@@ -114,6 +115,26 @@ describe('getHTMLElementAndThrowIfUndefined()', () => {
   });
 });
 
+describe('getHTMLElement()', () => {
+  it('should call querySelector on element with selector', () => {
+    const element = document.createElement('div');
+
+    const spy = jest.spyOn(element, 'querySelector');
+    getHTMLElement(element, 'span');
+    expect(spy).toBeCalledWith('span');
+
+    getHTMLElement(element, 'div');
+    expect(spy).toBeCalledWith('div');
+  });
+
+  it('should return result of querySelector', () => {
+    const element = document.createElement('div');
+    const childElement = document.createElement('span');
+
+    jest.spyOn(element, 'querySelector').mockReturnValue(childElement);
+    expect(getHTMLElement(element, 'span')).toBe(childElement);
+  });
+});
 describe('throwIfRootNodeIsNotOfKind()', () => {
   it('should throw error if root node tag does not match', () => {
     const child = document.createElement('p-select-wrapper-dropdown');
