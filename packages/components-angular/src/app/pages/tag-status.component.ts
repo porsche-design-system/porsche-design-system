a35e2eaf8b18851f97483f586aaf73db311dfe04
Bernd Hacker <hacker.bernd@gmail.com>
2022-04-05 08:40:20 +0200

    test(components-js): extend tag-status vrt test | bh | #1752

--- packages/components-angular/src/app/pages/tag-status.component.ts
+++ packages/components-angular/src/app/pages/tag-status.component.ts
@@ -5,6 +5,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'page-tag-status',
   styles: [
     `
+      .playground {
+        margin-bottom: -0.5rem;
+      }
+    
+      p-tag-status {
+        margin-bottom: 0.5rem;
+      }
+    
       p-tag-status:not(:last-child) {
         margin-right: 0.5rem;
       }
@@ -12,95 +20,121 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   ],
   template: `
     <div class="playground light" title="should show different background colors on light background">
-      <p-tag-status>Tag with default props</p-tag-status>
-      <p-tag-status [color]="'default'">Tag default</p-tag-status>
-      <p-tag-status [color]="'background-surface'">Tag background-surface</p-tag-status>
-      <p-tag-status [color]="'neutral-contrast-high'">Tag neutral-contrast-high</p-tag-status>
-      <p-tag-status [color]="'notification-neutral'">Tag notification-neutral</p-tag-status>
-      <p-tag-status [color]="'notification-success'">Tag notification-success</p-tag-status>
-      <p-tag-status [color]="'notification-error'">Tag notification-error</p-tag-status>
-      <p-tag-status [color]="'notification-warning'">Tag notification-warning</p-tag-status>
+      <p-tag-status>Default</p-tag-status>
+      <p-tag-status [color]="'default'">Color default</p-tag-status>
+      <p-tag-status [color]="'background-surface'">Color background-surface</p-tag-status>
+      <p-tag-status [color]="'neutral-contrast-high'">Color neutral-contrast-high</p-tag-status>
+      <p-tag-status [color]="'notification-neutral'">Color notification-neutral</p-tag-status>
+      <p-tag-status [color]="'notification-success'">Color notification-success</p-tag-status>
+      <p-tag-status [color]="'notification-error'">Color notification-error</p-tag-status>
+      <p-tag-status [color]="'notification-warning'">Color notification-warning</p-tag-status>
+    </div>
+
+    <div class="playground light surface" title="should show different background colors on light surface background">
+      <p-tag-status>Default</p-tag-status>
+      <p-tag-status [color]="'default'">Color default</p-tag-status>
+      <p-tag-status [color]="'background-surface'">Color background-surface</p-tag-status>
+      <p-tag-status [color]="'neutral-contrast-high'">Color neutral-contrast-high</p-tag-status>
+      <p-tag-status [color]="'notification-neutral'">Color notification-neutral</p-tag-status>
+      <p-tag-status [color]="'notification-success'">Color notification-success</p-tag-status>
+      <p-tag-status [color]="'notification-error'">Color notification-error</p-tag-status>
+      <p-tag-status [color]="'notification-warning'">Color notification-warning</p-tag-status>
     </div>
 
     <div class="playground dark" title="should show different background colors on dark background">
-      <p-tag-status [theme]="'dark'">Tag with default props</p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'default'">Tag default</p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'background-surface'">Tag background-surface</p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'neutral-contrast-high'">Tag neutral-contrast-high</p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'notification-neutral'">Tag notification-neutral</p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'notification-success'">Tag notification-success</p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'notification-error'">Tag notification-error</p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'notification-warning'">Tag notification-warning</p-tag-status>
+      <p-tag-status [theme]="'dark'">Default</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'default'">Color default</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'background-surface'">Color background-surface</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'neutral-contrast-high'">Color neutral-contrast-high</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'notification-neutral'">Color notification-neutral</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'notification-success'">Color notification-success</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'notification-error'">Color notification-error</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'notification-warning'">Color notification-warning</p-tag-status>
+    </div>
+
+    <div class="playground dark surface" title="should show different background colors on dark surface background">
+      <p-tag-status [theme]="'dark'">Default</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'default'">Color default</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'background-surface'">Color background-surface</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'neutral-contrast-high'">Color neutral-contrast-high</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'notification-neutral'">Color notification-neutral</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'notification-success'">Color notification-success</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'notification-error'">Color notification-error</p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'notification-warning'">Color notification-warning</p-tag-status>
     </div>
 
     <div class="playground light" title="should show different background colors and icons on light background">
-      <p-tag-status [icon]="'car'" [color]="'default'">Tag default</p-tag-status>
-      <p-tag-status [icon]="'highway'" [color]="'background-surface'">Tag background-surface</p-tag-status>
-      <p-tag-status [iconSource]="'./assets/icon-custom-kaixin.svg'">Tag custom icon</p-tag-status>
+      <p-tag-status [icon]="'car'" [color]="'default'">Color default</p-tag-status>
+      <p-tag-status [icon]="'highway'" [color]="'background-surface'">Color background-surface</p-tag-status>
+      <p-tag-status [iconSource]="'./assets/icon-custom-kaixin.svg'" [color]="'background-surface'">
+        Color background-surface
+      </p-tag-status>
     </div>
 
     <div class="playground dark" title="should show different background colors and icons on dark background">
-      <p-tag-status [theme]="'dark'" [icon]="'car'" [color]="'default'">Tag default</p-tag-status>
-      <p-tag-status [theme]="'dark'" [icon]="'highway'" [color]="'background-surface'">Tag background-surface</p-tag-status>
-      <p-tag-status [theme]="'dark'" [iconSource]="'./assets/icon-custom-kaixin.svg'">Tag custom icon</p-tag-status>
+      <p-tag-status [theme]="'dark'" [icon]="'car'" [color]="'default'">Color default</p-tag-status>
+      <p-tag-status [theme]="'dark'" [icon]="'highway'" [color]="'background-surface'">Color background-surface</p-tag-status>
+      <p-tag-status [theme]="'dark'" [iconSource]="'./assets/icon-custom-kaixin.svg'" [color]="'background-surface'">
+        Color background-surface
+      </p-tag-status>
     </div>
 
     <div class="playground light" title="should show different background colors with link on light background">
-      <p-tag-status [color]="'default'"><a [href]="'#'">Tag default with link</a></p-tag-status>
-      <p-tag-status [color]="'background-surface'"><a [href]="'#'">Tag background-surface with link</a></p-tag-status>
+      <p-tag-status [color]="'default'"><a [href]="'#'">Color default link</a></p-tag-status>
+      <p-tag-status [color]="'background-surface'"><a [href]="'#'">Color background-surface link</a></p-tag-status>
+      <p-tag-status [icon]="'car'" [color]="'default'"><a [href]="'#'">Color default link</a></p-tag-status>
+      <p-tag-status [icon]="'highway'" [color]="'background-surface'"><a [href]="'#'">Color background-surface link</a></p-tag-status>
+      <p-tag-status [iconSource]="'./assets/icon-custom-kaixin.svg'" [color]="'default'">
+        <a [href]="'#'">Color default link</a>
+      </p-tag-status>
     </div>
 
     <div class="playground dark" title="should show different background colors with link on dark background">
-      <p-tag-status [theme]="'dark'" [color]="'default'"><a [href]="'#'">Tag default with link</a></p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'background-surface'"><a [href]="'#'">Tag background-surface with link</a></p-tag-status>
-    </div>
-
-    <div class="playground light" title="should show different background colors with link and icon on light background">
-      <p-tag-status [icon]="'car'" [color]="'default'"><a [href]="'#'">Tag default with link</a></p-tag-status>
-      <p-tag-status [icon]="'highway'" [color]="'background-surface'"
-        ><a [href]="'#'">Tag background-surface with link</a></p-tag-status
-      >
-    </div>
-
-    <div class="playground dark" title="should show different background colors with link and icon on dark background">
-      <p-tag-status [theme]="'dark'" [icon]="'car'" [color]="'default'"><a [href]="'#'">Tag default with link</a></p-tag-status>
-      <p-tag-status [theme]="'dark'" [icon]="'highway'" [color]="'background-surface'"
-        ><a [href]="'#'">Tag background-surface with link</a></p-tag-status
-      >
+      <p-tag-status [theme]="'dark'" [color]="'default'"><a [href]="'#'">Color default link</a></p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'background-surface'"><a [href]="'#'">Color background-surface link</a></p-tag-status>
+      <p-tag-status [theme]="'dark'" [icon]="'car'" [color]="'default'"><a [href]="'#'">Color default link</a></p-tag-status>
+      <p-tag-status [theme]="'dark'" [icon]="'highway'" [color]="'background-surface'">
+        <a [href]="'#'">Color background-surface link</a>
+      </p-tag-status>
+      <p-tag-status [theme]="'dark'" [iconSource]="'./assets/icon-custom-kaixin.svg'" [color]="'default'">
+        <a [href]="'#'">Color default link</a>
+      </p-tag-status>
     </div>
 
     <div class="playground light" title="should show different background colors with button on light background">
-      <p-tag-status [color]="'default'"><button>Tag default with button</button></p-tag-status>
-      <p-tag-status [color]="'background-surface'"><button>Tag background-surface with button</button></p-tag-status>
+      <p-tag-status [color]="'default'"><button>Color default button</button></p-tag-status>
+      <p-tag-status [color]="'background-surface'"><button>Color background-surface button</button></p-tag-status>
+      <p-tag-status [icon]="'car'" [color]="'default'"><button>Color default button</button></p-tag-status>
+      <p-tag-status [icon]="'highway'" [color]="'background-surface'">
+        <button>Color background-surface button</button>
+      </p-tag-status>
+      <p-tag-status [iconSource]="'./assets/icon-custom-kaixin.svg'" [color]="'default'">
+        <button>Color default button</button>
+      </p-tag-status>
     </div>
 
     <div class="playground dark" title="should show different background colors with button on dark background">
-      <p-tag-status [theme]="'dark'" [color]="'default'"><button>Tag default with button</button></p-tag-status>
-      <p-tag-status [theme]="'dark'" [color]="'background-surface'"
-        ><button>Tag background-surface with button</button></p-tag-status
-      >
-    </div>
-
-    <div class="playground light" title="should show different background colors with button and icon on light background">
-      <p-tag-status [icon]="'car'" [color]="'default'"><button>Tag default with button</button></p-tag-status>
-      <p-tag-status [icon]="'highway'" [color]="'background-surface'"
-        ><button>Tag background-surface with button</button></p-tag-status
-      >
-    </div>
-
-    <div class="playground dark" title="should show different background colors with button and icon on dark background">
-      <p-tag-status [theme]="'dark'" [icon]="'car'" [color]="'default'"><button>Tag default with button</button></p-tag-status>
-      <p-tag-status [theme]="'dark'" [icon]="'highway'" [color]="'background-surface'"
-        ><button>Tag background-surface with button</button></p-tag-status
-      >
+      <p-tag-status [theme]="'dark'" [color]="'default'"><button>Color default button</button></p-tag-status>
+      <p-tag-status [theme]="'dark'" [color]="'background-surface'"><button>Color background-surface button</button></p-tag-status>
+      <p-tag-status [theme]="'dark'" [icon]="'car'" [color]="'default'"><button>Color default button</button></p-tag-status>
+      <p-tag-status [theme]="'dark'" [icon]="'highway'" [color]="'background-surface'">
+        <button>Color background-surface button</button>
+      </p-tag-status>
+      <p-tag-status [theme]="'dark'" [iconSource]="'./assets/icon-custom-kaixin.svg'" [color]="'default'">
+        <button>Color default button</button>
+      </p-tag-status>
     </div>
 
     <div class="playground light" title="should show different multiline tags on light background">
-      <div style="width: 100px">
-        <p-tag-status [color]="'neutral-contrast-high'">Some multiline tag</p-tag-status>
-        <p-tag-status [color]="'neutral-contrast-high'" [icon]="'car'">Some multiline tag</p-tag-status>
-        <p-tag-status [color]="'notification-success'"><a [href]="'#'">Some multiline Link</a></p-tag-status>
-        <p-tag-status [color]="'notification-success'" [icon]="'car'"><button>Some multiline button</button></p-tag-status>
+      <div style="width: 100px; overflow: hidden">
+        <p-tag-status [color]="'neutral-contrast-high'">Text that is very long</p-tag-status>
+        <p-tag-status [color]="'neutral-contrast-high'" [icon]="'car'">Text with icon that is very long</p-tag-status>
+        <p-tag-status [color]="'notification-success'" [icon]="'car'">
+          <a [href]="'#'">Link with icon that is very long</a>
+        </p-tag-status>
+        <p-tag-status [color]="'notification-success'" [icon]="'car'">
+          <button>Button with icon that is very long</button>
+        </p-tag-status>
       </div>
     </div>
   `,
--- packages/components-js/tests/vrt/specs/tag-status.vrt.ts
+++ packages/components-js/tests/vrt/specs/tag-status.vrt.ts
@@ -1,5 +1,4 @@
 import {
-  forceFocusHoverState,
   forceFocusState,
   forceHoverState,
   getThemedBodyMarkup,
