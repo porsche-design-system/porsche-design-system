b5a6757065123f653e4180b145e89fdf5d1788bf
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-06 16:13:16 +0200

    feat(components): introduce TAG_STATUS_COLORS array | mh / bh | #1752

--- packages/components/src/components/content/tag-status/tag-status-utils.ts
+++ packages/components/src/components/content/tag-status/tag-status-utils.ts
@@ -1,13 +1,11 @@
-import type { TextColor } from '../../../types';
+export const TAG_STATUS_COLORS = [
+  'default',
+  'neutral-contrast-high',
+  'notification-success',
+  'notification-warning',
+  'notification-error',
+  'notification-neutral',
+  'background-surface',
+] as const;
 
-export type TagStatusColor =
-  | Extract<
-      TextColor,
-      | 'default'
-      | 'neutral-contrast-high'
-      | 'notification-success'
-      | 'notification-warning'
-      | 'notification-error'
-      | 'notification-neutral'
-    >
-  | 'background-surface';
+export type TagStatusColor = typeof TAG_STATUS_COLORS[number];
