a34dce3cb48f55cb28c707d47491812f92cdae4e
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-06 12:40:43 +0200

    test(components): add tag-status lifecycle unit test  | mh / bh | #1752

---
+++ packages/components/src/components/content/tag-status/tag-status.spec.ts
@@ -0,0 +1,16 @@
+import * as getDirectChildHTMLElementUtils from '../../../utils/dom/getDirectChildHTMLElement';
+import { TagStatus } from './tag-status';
+
+describe('componentWillRender', () => {
+  it('should call getDirectChildHTMLElement() with correct parameters', () => {
+    const spy = jest.spyOn(getDirectChildHTMLElementUtils, 'getDirectChildHTMLElement');
+    const component = new TagStatus();
+    component.host = document.createElement('p-tag-status');
+    // pseudo-class selector ':scope>*' is missing in jsdom
+    try {
+      component.componentWillRender();
+    } catch {}
+
+    expect(spy).toBeCalledWith(component.host, 'a,button');
+  });
+});
