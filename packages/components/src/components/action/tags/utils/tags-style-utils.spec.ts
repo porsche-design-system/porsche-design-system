390cd2039cded0ece24f900cce4b962f337afb20
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-07 13:45:04 +0200

    test(components): improve tags-style-utils spec | mh | #1752

--- packages/components/src/components/action/tags/utils/tags-style-utils.spec.ts
+++ packages/components/src/components/action/tags/utils/tags-style-utils.spec.ts
@@ -1,26 +1,26 @@
 import { getThemedBackgroundColor } from './tags-style-utils';
 import { getThemedColors } from '../../../../styles';
+import { TagStatusColor } from '../tag-status/tag-status-utils';
+import { Theme } from '../../../../types';
 
 describe('getThemedBackgroundColor()', () => {
-  const themedColorsLight = getThemedColors('light');
-  const themedColorsDark = getThemedColors('dark');
-
-  it.each<Parameters<typeof getThemedBackgroundColor>>([
-    ['default', themedColorsLight],
-    ['neutral-contrast-high', themedColorsLight],
-    ['notification-success', themedColorsLight],
-    ['notification-warning', themedColorsLight],
-    ['notification-error', themedColorsLight],
-    ['notification-neutral', themedColorsLight],
-    ['background-surface', themedColorsLight],
-    ['default', themedColorsDark],
-    ['neutral-contrast-high', themedColorsDark],
-    ['notification-success', themedColorsDark],
-    ['notification-warning', themedColorsDark],
-    ['notification-error', themedColorsDark],
-    ['notification-neutral', themedColorsDark],
-    ['background-surface', themedColorsDark],
-  ])('should return correct css for color: %s, isFocusable: %s and theme: %s', (...args) => {
-    expect(getThemedBackgroundColor(...args)).toMatchSnapshot();
+  it.each<[TagStatusColor, Theme]>([
+    ['default', 'light'],
+    ['neutral-contrast-high', 'light'],
+    ['notification-success', 'light'],
+    ['notification-warning', 'light'],
+    ['notification-error', 'light'],
+    ['notification-neutral', 'light'],
+    ['background-surface', 'light'],
+    ['default', 'dark'],
+    ['neutral-contrast-high', 'dark'],
+    ['notification-success', 'dark'],
+    ['notification-warning', 'dark'],
+    ['notification-error', 'dark'],
+    ['notification-neutral', 'dark'],
+    ['background-surface', 'dark'],
+  ])('should return correct backgroundColor for color: %s, theme: %s', (color, theme) => {
+    const themedColors = getThemedColors(theme);
+    expect(getThemedBackgroundColor(color, themedColors)).toMatchSnapshot();
   });
 });
