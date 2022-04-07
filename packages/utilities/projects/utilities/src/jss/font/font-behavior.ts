3cc8f7fc15937e67023a7056c52bb2316f920dac
Anton Schmidt <anton.schmidt@mhp.com>
2022-04-07 14:05:40 +0200

    feat(utilities): move overflowWrap back to font behavior to preserve headline behavior, update snapshots | as | #1838

--- packages/utilities/projects/utilities/src/jss/font/font-behavior.ts
+++ packages/utilities/projects/utilities/src/jss/font/font-behavior.ts
@@ -1,6 +1,7 @@
 import type { FontBehavior } from './font-shared';
 
 export const fontBehavior: FontBehavior = {
+  overflowWrap: 'break-word',
   WebkitTextSizeAdjust: 'none', // stop iOS safari from adjusting font size when screen rotation is changing
   textSizeAdjust: 'none',
 };
--- packages/utilities/projects/utilities/src/jss/font/font-hyphenation.ts
+++ packages/utilities/projects/utilities/src/jss/font/font-hyphenation.ts
@@ -1,6 +1,5 @@
 import { FontHyphenation } from './font-shared';
 
 export const fontHyphenation: FontHyphenation = {
-  overflowWrap: 'break-word',
   hyphens: 'auto',
 };
