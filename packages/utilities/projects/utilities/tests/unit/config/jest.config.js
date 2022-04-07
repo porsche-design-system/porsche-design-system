d63c45f210d817ae2e50c179e37921b5467a894b
Stephan Schröter <stephan.schroeter@mhp.com>
2022-04-07 11:07:11 +0200

    test(utilities/utilities): covering scss implementation | sas | #1742

--- packages/utilities/projects/utilities/tests/unit/config/jest.config.js
+++ packages/utilities/projects/utilities/tests/unit/config/jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   testEnvironment: 'node',
   verbose: true,
   rootDir: '../../../',
-  testMatch: ['**/src/jss/**/*.spec.ts', '**/tests/unit/specs/**/*.spec.ts'],
+  testMatch: ['**/src/jss/**/*.spec.ts', '**/src/scss/**/*.spec.ts', '**/tests/unit/specs/**/*.spec.ts'],
 };
