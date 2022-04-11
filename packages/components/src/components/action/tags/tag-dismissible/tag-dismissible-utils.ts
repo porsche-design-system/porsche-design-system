c53332393648d2e1a2817d10e426a92e0817b676
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-11 13:26:11 +0200

    feat(components): add aria property to tag-dismissible | mh / bh | #1752

--- packages/components/src/components/action/tags/tag-dismissible/tag-dismissible-utils.ts
+++ packages/components/src/components/action/tags/tag-dismissible/tag-dismissible-utils.ts
@@ -1,2 +1,5 @@
 export const TAG_DISMISSIBLE_COLORS = ['default', 'background-surface'] as const;
 export type TagDismissibleColor = typeof TAG_DISMISSIBLE_COLORS[number];
+
+export const TAG_DISMISSIBLE_ARIA_ATTRIBUTES = ['aria-label'] as const;
+export type TagDismissibleAriaAttribute = typeof TAG_DISMISSIBLE_ARIA_ATTRIBUTES[number];
