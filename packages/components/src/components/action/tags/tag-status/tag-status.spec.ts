8ef502db16a302f5ed585a43758023b63f4aa10e
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-11 11:08:21 +0200

    feat(components): add validation of color to status tag | mh / bh | #1752

--- packages/components/src/components/action/tags/tag-status/tag-status.spec.ts
+++ packages/components/src/components/action/tags/tag-status/tag-status.spec.ts
@@ -1,5 +1,7 @@
 import * as getDirectChildHTMLElementUtils from '../../../../utils/dom/getDirectChildHTMLElement';
+import * as validationUtils from '../../../../utils/validation';
 import { TagStatus } from './tag-status';
+import { TAG_STATUS_COLORS } from './tag-status-utils';
 
 describe('componentWillRender', () => {
   it('should call getDirectChildHTMLElement() with correct parameters', () => {
@@ -12,5 +14,19 @@ describe('componentWillRender', () => {
     } catch {}
 
     expect(spy).toBeCalledWith(component.host, 'a,button');
+  });
+
+  it('should call throwIfValueIsInvalid() with correct parameters', () => {
+    const spy = jest.spyOn(validationUtils, 'throwIfValueIsInvalid');
+    const component = new TagStatus();
+    component.host = document.createElement('p-tag-status');
+    component.host.attachShadow({ mode: 'open' });
+    component.color = 'default';
+    // pseudo-class selector ':scope>*' is missing in jsdom
+    try {
+      component.componentWillRender();
+    } catch {}
+
+    expect(spy).toBeCalledWith('default', TAG_STATUS_COLORS, 'color');
   });
 });
