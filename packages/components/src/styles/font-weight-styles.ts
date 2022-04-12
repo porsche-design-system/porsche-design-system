15a854cf210867156f91d369d5bbdacd3b677f37
Stephan Schröter <stephan.schroeter@mhp.com>
2022-04-12 15:30:32 +0200

    feat: minor improvements | sas | #1742

---
+++ packages/components/src/styles/font-weight-styles.ts
@@ -0,0 +1,6 @@
+import { TextWeight } from '../types';
+import { fontWeight } from '@porsche-design-system/utilities-v2';
+
+export const getFontWeight = (weight: TextWeight): number => {
+  return fontWeight[weight === 'semibold' ? 'semiBold' : weight];
+};
