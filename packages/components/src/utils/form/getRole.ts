0b040787d6c72923608d66f28f0582c34dea7d0e
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-13 12:29:09 +0200

    feat(components): split form utils | mh / bh | #1752

--- packages/components/src/utils/form/getRole.ts
+++ packages/components/src/utils/form/getRole.ts
@@ -1,7 +1,5 @@
 import type { FormState } from '../../types';
 
-export const isVisibleFormState = (state: FormState): boolean => state === 'success' || state === 'error';
-
 export const getRole = (state: FormState): string => {
   return state === 'error' ? 'alert' : state === 'success' ? 'status' : null;
 };
