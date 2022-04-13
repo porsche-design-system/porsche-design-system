5863c8bf643681dc47b9ab7d7a2f0b36b249dd7b
Stephan Schröter <stephan.schroeter@mhp.com>
2022-04-13 12:35:41 +0200

    Update packages/components/src/styles/font-weight-styles.ts
    Co-authored-by: Bernd Hacker <hacker.bernd@gmail.com>

--- packages/components/src/styles/font-weight-styles.ts
+++ packages/components/src/styles/font-weight-styles.ts
@@ -1,4 +1,4 @@
-import { TextWeight } from '../types';
+import type { TextWeight } from '../types';
 import { fontWeight } from '@porsche-design-system/utilities-v2';
 
 export const getFontWeight = (weight: TextWeight): number => {
