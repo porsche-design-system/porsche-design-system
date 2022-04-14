465cb3614c84982c2f444865d75810e353257479
marcuszierke <marcuszierke@gmail.com>
2022-04-14 12:10:00 +0200

    fix(utilities): change requests implemented | mz | #1743

--- packages/utilities/projects/utilities/tests/unit/specs/style-comparison.spec.tsx
+++ packages/utilities/projects/utilities/tests/unit/specs/style-comparison.spec.tsx
@@ -7,14 +7,20 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
 import { getFocus, headingMedium } from '../../../src/jss';
-import { createUseStyles, JssProvider } from 'react-jss';
+import { createUseStyles } from 'react-jss';
 
 const focusMixin: string = fs.readFileSync(path.resolve('./src/scss/_focus.scss'), 'utf8');
 const headingMixin: string = fs.readFileSync(path.resolve('./src/scss/lib/_heading.scss'), 'utf8');
 
-const cssStyles = sass.compileString(
-  `${focusMixin} ${headingMixin} .focus { @include pds-focus($color: currentColor, $offset: 2px);};.heading {@include pds-heading-medium;}`
-);
+const cssStyles = sass.compileString(`
+  ${focusMixin} ${headingMixin}
+  .focus {
+    @include pds-focus($color: currentColor, $offset: 2px);
+  };
+  .heading {
+    @include pds-heading-medium;
+  }
+`);
 
 const SampleStyles: string = styled.div({ focus: getFocus() as any, heading: headingMedium });
 const useStyles = createUseStyles({ focus: getFocus(), heading: headingMedium });
@@ -37,32 +43,23 @@ const StyledComponentsSample = (): JSX.Element => (
 );
 
 const JssSample = (): JSX.Element => {
-  const jssStyles = useStyles();
-  return (
-    <JssProvider>
-      <div className={jssStyles.focus} role="jss-focus">
-        jss_focus
-      </div>
-      <div className={jssStyles.heading} role="jss-heading">
-        jss_heading_medium
-      </div>
-    </JssProvider>
-  );
+  useStyles();
+  return <div />;
 };
 
-beforeEach(() => {
-  render(<JssSample />);
-  jssStyles = document.querySelector('style[data-jss]').innerHTML;
-});
-
 describe('JSS, styled-components and SCSS', () => {
-  it('should compile styled-components correctly to jss styles', () => {
+  beforeEach(() => {
+    render(<JssSample />);
+    jssStyles = document.querySelector('style[data-jss]').innerHTML;
+  });
+
+  it('should have equal styles for styled-components and jss', () => {
     render(<StyledComponentsSample />);
     const styledComponentsStyles = document.querySelector('style[data-styled]').innerHTML;
     expect(prettified(jssStyles)).toBe(prettified(styledComponentsStyles));
   });
 
-  it('should compile scss styles correctly to jss styles', () => {
+  it('should have equal styles for scss and jss', () => {
     expect(prettified(jssStyles)).toBe(prettified(cssStyles.css));
   });
 });
