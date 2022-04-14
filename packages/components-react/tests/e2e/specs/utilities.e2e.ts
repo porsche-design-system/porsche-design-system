1458ef4f1956cf4c153304c81e6b5f36d5043e28
marcuszierke <marcuszierke@gmail.com>
2022-04-14 17:57:31 +0200

    fix(utilities): missing files for renaming added | mz | #1743

--- packages/components-react/tests/e2e/specs/utilities.e2e.ts
+++ packages/components-react/tests/e2e/specs/utilities.e2e.ts
@@ -1,5 +1,5 @@
-import * as jsUtilities from '@porsche-design-system/components-js/utilities/jss';
-import * as reactUtilities from '@porsche-design-system/components-react/utilities/jss';
+import * as jsUtilities from '@porsche-design-system/components-js/utilities/js';
+import * as reactUtilities from '@porsche-design-system/components-react/utilities/js';
 
 it('should reexport all utilities from components-js', () => {
   expect(reactUtilities).toEqual(jsUtilities);
