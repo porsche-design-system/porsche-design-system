0d5f1133df1d8e46a323106b1bc185acae979194
marcuszierke <marcuszierke@gmail.com>
2022-04-15 19:29:58 +0200

    feat(utilities): jest.config set up for jsdom and node  | mz | #1743

--- packages/utilities/projects/utilities/tests/unit/config/jest.config.js
+++ packages/utilities/projects/utilities/tests/unit/config/jest.config.js
@@ -1,12 +1,25 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  verbose: true,
-  rootDir: '../../../',
-  testMatch: ['**/src/**/*.spec.ts', '**/tests/unit/specs/**/*.spec.(ts|tsx)'],
-  globals: {
-    'ts-jest': {
-      isolatedModules: true, // this fixes typing issues with jasmine
+  projects: [
+    {
+      displayName: 'node',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      verbose: true,
+      rootDir: './',
+      testMatch: ['**/src/**/*.spec.ts', '**/tests/unit/specs/**/*.spec.ts'],
     },
-  },
+    {
+      displayName: 'dom',
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      verbose: true,
+      rootDir: './',
+      testMatch: ['**/tests/unit/specs/**/*.spec.tsx'],
+      globals: {
+        'ts-jest': {
+          isolatedModules: true, // this fixes typing issues with jasmine
+        },
+      },
+    },
+  ],
 };
