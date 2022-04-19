b9283a8dd8d78879619e75dce47f249cd1f02fdb
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-19 16:04:04 +0200

    feat(components-react): create object with reduce instead of static | mh | #1857

--- packages/components-react/projects/uxpin-wrapper/src/spacing.ts
+++ packages/components-react/projects/uxpin-wrapper/src/spacing.ts
@@ -1,22 +1,18 @@
-export const SPACING = [4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80] as const;
+import { Properties } from 'csstype';
+
+const SPACING = [4, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80] as const;
 export type Spacing = typeof SPACING[number];
 
-type MapSpacingToPaddingOpts = {
-  spacingTop?: Spacing;
-  spacingLeft?: Spacing;
-  spacingRight?: Spacing;
-  spacingBottom?: Spacing;
+type GetPaddingStylesOpts = {
+  spacingTop: Spacing;
+  spacingLeft: Spacing;
+  spacingRight: Spacing;
+  spacingBottom: Spacing;
 };
 
-export const mapSpacingToPadding = (
-  props: MapSpacingToPaddingOpts
-): { paddingTop: Spacing; paddingLeft: Spacing; paddingRight: Spacing; paddingBottom: Spacing } => {
-  const { spacingTop, spacingLeft, spacingRight, spacingBottom } = props;
-
-  return {
-    paddingTop: spacingTop,
-    paddingLeft: spacingLeft,
-    paddingRight: spacingRight,
-    paddingBottom: spacingBottom,
-  };
+export const getPaddingStyles = (spacings: GetPaddingStylesOpts): Properties => {
+  return Object.entries(spacings).reduce((result, [key, value]) => {
+    result[key.replace('spacing', 'padding')] = value && `${value}px`;
+    return result;
+  }, {} as Properties);
 };
