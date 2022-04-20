987a77fedd45641561caf728d77054afe3cd9d58
Matthias Hägele <60395476+mhaegeleMHP@users.noreply.github.com>
2022-04-20 11:39:39 +0200

    Update packages/components-react/tests/unit/specs/spacing.spec.ts

--- packages/components-react/tests/unit/specs/spacing.spec.ts
+++ packages/components-react/tests/unit/specs/spacing.spec.ts
@@ -3,7 +3,6 @@ import { getPaddingStyles } from '../../../projects/uxpin-wrapper/src/spacing';
 describe('getPaddingStyles', () => {
   it('should correctly map spacings to padding', () => {
     expect(getPaddingStyles({ spacingTop: undefined, spacingLeft: 4, spacingRight: 8, spacingBottom: 24 })).toEqual({
-      paddingTop: undefined,
       paddingLeft: '4px',
       paddingRight: '8px',
       paddingBottom: '24px',
