6130e9d707d12086fb17474a02220a185931b433
Matthias Hägele <60395476+mhaegeleMHP@users.noreply.github.com>
2022-04-20 11:36:57 +0200

    Update packages/components-react/tests/unit/specs/spacing.spec.ts

--- packages/components-react/tests/unit/specs/spacing.spec.ts
+++ packages/components-react/tests/unit/specs/spacing.spec.ts
@@ -1,6 +1,6 @@
 import { getPaddingStyles } from '../../../projects/uxpin-wrapper/src/spacing';
 
-describe('mapSpacingToPadding', () => {
+describe('getPaddingStyles', () => {
   it('should correctly map spacings to padding', () => {
     expect(getPaddingStyles({ spacingTop: undefined, spacingLeft: 4, spacingRight: 8, spacingBottom: 24 })).toEqual({
       paddingTop: undefined,
