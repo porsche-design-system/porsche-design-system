fe3394d494ccb6d15a86feed006afd5e1485be1b
marcuszierke <marcuszierke@gmail.com>
2022-04-20 11:02:23 +0200

    fix(utilities/-deprecated): chagne requests implemented | mz | #1743

--- packages/utilities/projects/utilities/tests/unit/config/jest.config.js
+++ packages/utilities/projects/utilities/tests/unit/config/jest.config.js
@@ -1,25 +1,7 @@
 module.exports = {
-  projects: [
-    {
-      displayName: 'node',
-      preset: 'ts-jest',
-      testEnvironment: 'node',
-      verbose: true,
-      rootDir: './',
-      testMatch: ['**/src/**/*.spec.ts', '**/tests/unit/specs/**/*.spec.ts'],
-    },
-    {
-      displayName: 'dom',
-      preset: 'ts-jest',
-      testEnvironment: 'jsdom',
-      verbose: true,
-      rootDir: './',
-      testMatch: ['**/tests/unit/specs/**/*.spec.tsx'],
-      globals: {
-        'ts-jest': {
-          isolatedModules: true, // this fixes typing issues with jasmine
-        },
-      },
-    },
-  ],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  rootDir: '../../../',
+  testMatch: ['**/src/**/*.spec.ts', '**/tests/unit/specs/**/*.spec.(ts|tsx)'],
 };
--- packages/utilities/projects/utilities/tests/unit/specs/compare-helper-styles.spec.tsx
+++ packages/utilities/projects/utilities/tests/unit/specs/compare-helper-styles.spec.tsx
@@ -1,8 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
 import * as fs from 'fs';
 import * as path from 'path';
 import * as sass from 'sass';
 import * as prettier from 'prettier';
-import styled, { StyleSheetManager } from 'styled-components';
+import styled, { CSSObject, CSSProperties, StyleSheetManager } from 'styled-components';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
@@ -22,12 +25,12 @@ const cssStyles = sass.compileString(`
   }
 `);
 
-const SampleStyles: string = styled.div({ focus: getFocus() as any, heading: headingMedium });
+const SampleStyles = styled.div({ focus: getFocus() as any, heading: headingMedium });
 const useStyles = createUseStyles({ focus: getFocus(), heading: headingMedium });
 let jssStyles: string;
 const STYLED_COMPONENTS_AUTO_GENERATED_CLASS_NAME: string = 'cgVBvh';
 
-const prettified = (style: string): string => {
+const formatAndNeutralizeStyle = (style: string): string => {
   const classRegExp = new RegExp(`(.${STYLED_COMPONENTS_AUTO_GENERATED_CLASS_NAME} )`, 'g');
   style = prettier.format(style, { parser: 'scss' });
   return style
@@ -47,19 +50,17 @@ const JssSample = (): JSX.Element => {
   return <div />;
 };
 
-describe('JSS, styled-components and SCSS', () => {
-  beforeEach(() => {
-    render(<JssSample />);
-    jssStyles = document.querySelector('style[data-jss]').innerHTML;
-  });
+beforeEach(() => {
+  render(<JssSample />);
+  jssStyles = document.querySelector('style[data-jss]')!.innerHTML;
+});
 
-  it('should have equal styles for styled-components and jss', () => {
-    render(<StyledComponentsSample />);
-    const styledComponentsStyles = document.querySelector('style[data-styled]').innerHTML;
-    expect(prettified(jssStyles)).toBe(prettified(styledComponentsStyles));
-  });
+it('should have equal styles for styled-components and jss', () => {
+  render(<StyledComponentsSample />);
+  const styledComponentsStyles = document.querySelector('style[data-styled]')!.innerHTML;
+  expect(formatAndNeutralizeStyle(jssStyles)).toBe(formatAndNeutralizeStyle(styledComponentsStyles));
+});
 
-  it('should have equal styles for scss and jss', () => {
-    expect(prettified(jssStyles)).toBe(prettified(cssStyles.css));
-  });
+it('should have equal styles for scss and jss', () => {
+  expect(formatAndNeutralizeStyle(jssStyles)).toBe(formatAndNeutralizeStyle(cssStyles.css));
 });
