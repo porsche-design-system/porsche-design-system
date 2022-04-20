9c9441d4dbe7f56a8b9f24ee4836eaf31d6f4ba8
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-20 15:58:59 +0200

    feat(components): move construct-style-sheets-polyfill import to d.ts file to improve bundle | mh / bh  | #1857

---
+++ packages/components/src/CSSStyleSheet.d.ts
@@ -0,0 +1,3 @@
+// TODO: Types are needed for jss.ts. Remove as soon as they are available in lib.dom.d.ts
+// ShadowRoot.adoptedStyleSheets & CSSStyleSheet.replaceSync is missing in lib.dom.d.ts  https://github.com/microsoft/TypeScript/issues/30022
+import 'construct-style-sheets-polyfill';
