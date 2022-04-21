171b3455971ff6936f1c4af7a75a5e68d61dbf0d
Bernd Hacker <hacker.bernd@gmail.com>
2022-04-21 09:32:30 +0200

    test(storefront): extract a11yAnalyze() and a11yFinalize() e2e utils | bh | #461

---
+++ packages/storefront/tests/e2e/helpers/axe-helper.ts
@@ -0,0 +1,31 @@
+import { AxeDevToolsPuppeteer } from '@axe-devtools/puppeteer';
+import Reporter from '@axe-devtools/reporter';
+import * as fs from 'fs';
+import type { Page } from 'puppeteer';
+import { baseURL } from './';
+
+const AXE_RESULTS_DIR = './tests/e2e/results';
+fs.rmSync(AXE_RESULTS_DIR, { force: true, recursive: true });
+
+const reporter = new Reporter('a11y', AXE_RESULTS_DIR);
+
+// to avoid duplicate scans and errors in writing reports
+const analyzedUrls: string[] = [];
+
+export const a11yAnalyze = async (page: Page) => {
+  const pageUrl = page.url();
+
+  if (!analyzedUrls.includes(pageUrl)) {
+    const result = await new AxeDevToolsPuppeteer(page).analyze();
+
+    const testId = pageUrl.replace(baseURL + '/', '').replace(/\//g, '-') || 'root';
+    reporter.logTestResult(testId, result);
+
+    analyzedUrls.push(pageUrl);
+  }
+};
+
+export const a11yFinalize = () => {
+  reporter.buildHTML(AXE_RESULTS_DIR);
+  analyzedUrls.length = 0; // reset
+};
