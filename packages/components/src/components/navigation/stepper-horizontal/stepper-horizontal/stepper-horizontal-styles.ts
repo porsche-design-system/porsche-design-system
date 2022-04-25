5098abace3dc93b8f9993cc8dea7f9900085cd3d
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-25 15:18:16 +0200

    feat(components): use horizontal scroll wrapper in stepper horizontal | mh | #1861

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-styles.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-styles.ts
@@ -1,7 +1,12 @@
 import { getCss } from '../../../../utils';
-import { addImportantToEachRule } from '../../../../styles';
+import { addImportantToEachRule, getThemedColors } from '../../../../styles';
+import { getFunctionalComponentHorizontalScrollWrapperStyles } from '../../../common/horizontal-scrolling/horizontal-scroll-wrapper-styles';
+import type { Theme } from '../../../../types';
 
-export const getComponentCss = (): string => {
+export const getComponentCss = (theme: Theme): string => {
+  const { backgroundColor } = getThemedColors(theme);
+  const gradientColorTransparent = backgroundColor + (backgroundColor.length === 4 ? '0' : '00');
+
   return getCss({
     '@global': {
       ':host': addImportantToEachRule({
@@ -11,5 +16,6 @@ export const getComponentCss = (): string => {
         marginRight: '1em',
       },
     },
+    ...getFunctionalComponentHorizontalScrollWrapperStyles(backgroundColor, gradientColorTransparent),
   });
 };
