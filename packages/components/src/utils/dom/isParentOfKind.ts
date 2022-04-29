26e16d371b8dd941e16125076bfb4342c602ae0b
Matthias Haegele <matthias.haegele@mhp.com>
2022-04-29 11:21:50 +0200

    feat(components): extend isParentOfKind to check outside shadowRoot | mh | #1875

--- packages/components/src/utils/dom/isParentOfKind.ts
+++ packages/components/src/utils/dom/isParentOfKind.ts
@@ -1,7 +1,8 @@
 import type { TagNameCamelCase } from '@porsche-design-system/shared';
 import { getPrefixedTagNames, getTagName } from '../tag-name';
 
-export const isParentOfKind = (element: HTMLElement, tagName: TagNameCamelCase): boolean => {
-  const { parentElement } = element;
-  return parentElement && getTagName(parentElement) === getPrefixedTagNames(element)[tagName];
+export const isParentOfKind = (element: HTMLElement, tagName: TagNameCamelCase, inShadowRoot?: boolean): boolean => {
+  const parentElement = inShadowRoot ? (element.getRootNode() as ShadowRoot).host : element.parentElement;
+
+  return parentElement && getTagName(parentElement as HTMLElement) === getPrefixedTagNames(element)[tagName];
 };
