6fa073398dbb676e250ae1ec9bbc21622499b98c
Anton Schmidt <anton.schmidt@mhp.com>
2022-05-03 17:34:30 +0200

    test(components): adjust unit tests | as | #1875

--- packages/components/src/components/navigation/tabs-bar/tabs-bar-styles.spec.ts
+++ packages/components/src/components/navigation/tabs-bar/tabs-bar-styles.spec.ts
@@ -2,15 +2,15 @@ import { getComponentCss } from './tabs-bar-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['small', 'regular', 'default', 'light'],
-    ['small', 'regular', 'default', 'dark'],
-    ['small', 'regular', 'default', 'light-electric'],
-    ['medium', 'regular', 'default', 'light'],
-    ['medium', 'regular', 'default', 'dark'],
-    ['medium', 'regular', 'default', 'light-electric'],
-    ['small', 'regular', 'surface', 'light'],
-    ['small', 'semibold', 'default', 'light'],
-    [{ base: 'small', xs: 'medium', s: 'small', m: 'medium', l: 'small', xl: 'medium' }, 'regular', 'default', 'light'],
+    ['small', 'regular', 'light'],
+    ['small', 'regular', 'dark'],
+    ['small', 'regular', 'light-electric'],
+    ['medium', 'regular', 'light'],
+    ['medium', 'regular', 'dark'],
+    ['medium', 'regular', 'light-electric'],
+    ['small', 'regular', 'light'],
+    ['small', 'semibold', 'light'],
+    [{ base: 'small', xs: 'medium', s: 'small', m: 'medium', l: 'small', xl: 'medium' }, 'regular', 'light'],
   ])('should return correct css for size: %j, weight: %s, gradientColorScheme: %s and theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
