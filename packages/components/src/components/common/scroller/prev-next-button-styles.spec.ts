aba92493aff720a34687d754863ca305a5cc4ea8
Anton Schmidt <anton.schmidt@mhp.com>
2022-05-03 17:34:30 +0200

    test(components): adjust unit tests | as | #1875

--- packages/components/src/components/common/scroller/prev-next-button-styles.spec.ts
+++ packages/components/src/components/common/scroller/prev-next-button-styles.spec.ts
@@ -1,7 +1,11 @@
 import { getFunctionalComponentPrevNextButtonStyles } from './prev-next-button-styles';
 
 describe('getFunctionalComponentStateMessageStyles()', () => {
-  it('should return correct JssStyle', () => {
-    expect(getFunctionalComponentPrevNextButtonStyles('#f2f2f2', '#f2f2f20000')).toMatchSnapshot();
+  it('should return correct JssStyle for hasTabsBarParent: false', () => {
+    expect(getFunctionalComponentPrevNextButtonStyles('#f2f2f2', false)).toMatchSnapshot();
+  });
+
+  it('should return correct JssStyle for hasTabsBarParent: true', () => {
+    expect(getFunctionalComponentPrevNextButtonStyles('#f2f2f2', true)).toMatchSnapshot();
   });
 });
