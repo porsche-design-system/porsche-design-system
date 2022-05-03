dcdf797de54bfbe3fe67c9965be895386f718dc1
Bernd Hacker <hacker.bernd@gmail.com>
2022-05-03 10:31:02 +0200

    test(components-react/nextjs): fix failing overview vrt | bh | #1149

--- packages/components-react/projects/nextjs/tests/vrt/specs/overview.vrt.ts
+++ packages/components-react/projects/nextjs/tests/vrt/specs/overview.vrt.ts
@@ -7,7 +7,6 @@ import {
 it('should have no visual regression', async () => {
   expect(
     await vrtTest(getVisualRegressionOverviewTester(), 'overview', '/overview', {
-      elementSelector: '#__next',
       scenario: (page) => openPopoversAndHighlightSpacer(page),
     })
   ).toBeFalsy();
