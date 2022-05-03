285bcee64ff403d167f5475b87c8570168c15d58
Bernd Hacker <hacker.bernd@gmail.com>
2022-05-03 17:38:51 +0200

    fix(components-react/nextjs): usage of next's Head components | bh | #1833

--- packages/components-react/projects/nextjs/pages/_app.tsx
+++ packages/components-react/projects/nextjs/pages/_app.tsx
@@ -4,33 +4,40 @@ import { componentsReady, PorscheDesignSystemProvider } from '@porsche-design-sy
 import { routes } from '../routes';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   const router = useRouter();
   const [selected, setSelected] = useState(router.route);
 
   return (
-    <PorscheDesignSystemProvider>
-      <select
-        value={selected}
-        onChange={(e) => {
-          const { value } = e.target;
-          setSelected(value);
-          router.push(value);
-        }}
-      >
-        <option disabled value="">
-          Select a page
-        </option>
-        {routes.map((route) => (
-          <option key={route.path} value={route.path} children={route.name} />
-        ))}
-      </select>
+    <>
+      <Head>
+        <title>Porsche Design System - NextJS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <PorscheDesignSystemProvider>
+        <select
+          value={selected}
+          onChange={(e) => {
+            const { value } = e.target;
+            setSelected(value);
+            router.push(value);
+          }}
+        >
+          <option disabled value="">
+            Select a page
+          </option>
+          {routes.map((route) => (
+            <option key={route.path} value={route.path} children={route.name} />
+          ))}
+        </select>
 
-      <div id="app">
-        <Component {...pageProps} />
-      </div>
-    </PorscheDesignSystemProvider>
+        <div id="app">
+          <Component {...pageProps} />
+        </div>
+      </PorscheDesignSystemProvider>
+    </>
   );
 };
 
--- packages/components-react/projects/nextjs/pages/_document.tsx
+++ packages/components-react/projects/nextjs/pages/_document.tsx
@@ -5,9 +5,6 @@ const Document = (): JSX.Element => {
   return (
     <Html lang="en">
       <Head>
-        <meta charSet="utf-8" />
-        <title>Porsche Design System - NextJS</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="http://localhost:3001/meta-icons/favicon-32x32.10be24507223bc4ef63effe0eb750e58.png" />
 
         <link rel="stylesheet" href="http://localhost:3001/styles/font-face.min.css" />
