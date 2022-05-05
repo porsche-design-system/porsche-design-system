36738d32e7e01a056b9bdff0c74d67e4717b6820
Matthias Haegele <matthias.haegele@mhp.com>
2022-05-05 08:22:03 +0200

    feat(components): move getScrollActivePosition() to tabs bar utils | mh | #1875

--- packages/components/src/components/common/scroller/scroller-utils.spec.ts
+++ packages/components/src/components/common/scroller/scroller-utils.spec.ts
@@ -1,49 +1,5 @@
 import type { Direction } from './scroller-utils';
-import { getScrollActivePosition, getScrollPositionAfterPrevNextClick } from './scroller-utils';
-
-describe('getScrollActivePosition()', () => {
-  it('should return scrollActivePosition = 16 if scrolling to last tab', () => {
-    expect(
-      getScrollActivePosition([{ offsetLeft: 20, offsetWidth: 0 }] as HTMLElement[], 'next', 0, undefined, undefined)
-    ).toBe(16);
-  });
-
-  it('should return scrollActivePosition = 8 if direction is "next", next tab is set as active', () => {
-    expect(
-      getScrollActivePosition(
-        [
-          { offsetLeft: 20, offsetWidth: 0 },
-          { offsetLeft: 0, offsetWidth: 0 },
-        ] as HTMLElement[],
-        'next',
-        0,
-        undefined,
-        20
-      )
-    ).toBe(8);
-  });
-
-  it('should return scrollActivePosition = 0 if direction is "prev" and first tab is set as active', () => {
-    expect(
-      getScrollActivePosition([{ offsetLeft: 0, offsetWidth: 0 }] as HTMLElement[], 'prev', 0, undefined, undefined)
-    ).toBe(0);
-  });
-
-  it('should return scrollActivePosition = 41 if scrolling to previous tab', () => {
-    expect(
-      getScrollActivePosition(
-        [
-          { offsetLeft: 0, offsetWidth: 0 },
-          { offsetLeft: 20, offsetWidth: 5 },
-        ] as HTMLElement[],
-        'prev',
-        1,
-        4,
-        20
-      )
-    ).toBe(41);
-  });
-});
+import { getScrollPositionAfterPrevNextClick } from './scroller-utils';
 
 describe('getScrollPositionAfterPrevNextClick()', () => {
   it.each<[number, number, Direction, number]>([
