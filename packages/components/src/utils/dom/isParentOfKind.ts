2aa740c31eef1fff5d4aa708db3d655a4c9dd935
Anton Schmidt <anton.schmidt@mhp.com>
2022-05-12 11:53:10 +0200

    feat(components): extract util function | as | #1875

--- packages/components/src/utils/dom/isParentOfKind.ts
+++ packages/components/src/utils/dom/isParentOfKind.ts
@@ -1,8 +1,8 @@
 import type { TagNameCamelCase } from '@porsche-design-system/shared';
 import { getPrefixedTagNames, getTagName } from '../tag-name';
 
-export const isParentOfKind = (element: HTMLElement, tagName: TagNameCamelCase, inShadowRoot?: boolean): boolean => {
-  const parentElement = inShadowRoot ? (element.getRootNode() as ShadowRoot).host : element.parentElement;
+export const isParentOfKind = (element: HTMLElement, tagName: TagNameCamelCase): boolean => {
+  const parentElement = element.parentElement;
 
   return parentElement && getTagName(parentElement as HTMLElement) === getPrefixedTagNames(element)[tagName];
 };
