cd55f9a1a52de1070eeb492b15b91c5f9fa78f64
Anton Schmidt <anton.schmidt@mhp.com>
2022-05-12 16:26:10 +0200

    test(components-js): add vr tests with visible prev button | as | #1875

--- packages/components-angular/src/app/pages/scroller.component.ts
+++ packages/components-angular/src/app/pages/scroller.component.ts
@@ -1,5 +1,6 @@
 /* Auto Generated File */
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { componentsReady } from '@porsche-design-system/components-angular';
 
 @Component({
   selector: 'page-scroller',
@@ -18,6 +19,30 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
     <div class="playground dark" title="should render scroller on dark background">
       <p-scroller [theme]="'dark'">
+        <a [href]="'#'">Anchor 1</a>
+        <a [href]="'#'">Anchor 2</a>
+        <a [href]="'#'">Anchor 3</a>
+        <a [href]="'#'">Anchor 4</a>
+        <a [href]="'#'">Anchor 5</a>
+        <a [href]="'#'">Anchor 6</a>
+        <a [href]="'#'">Anchor 7</a>
+      </p-scroller>
+    </div>
+
+    <div class="playground light" title="should render scroller with prev button on light background">
+      <p-scroller [dataId]="'prev-light'">
+        <a [href]="'#'">Anchor 1</a>
+        <a [href]="'#'">Anchor 2</a>
+        <a [href]="'#'">Anchor 3</a>
+        <a [href]="'#'">Anchor 4</a>
+        <a [href]="'#'">Anchor 5</a>
+        <a [href]="'#'">Anchor 6</a>
+        <a [href]="'#'">Anchor 7</a>
+      </p-scroller>
+    </div>
+
+    <div class="playground dark" title="should render scroller with prev button on dark background">
+      <p-scroller [dataId]="'prev-dark'" [theme]="'dark'">
         <a [href]="'#'">Anchor 1</a>
         <a [href]="'#'">Anchor 2</a>
         <a [href]="'#'">Anchor 3</a>
@@ -51,7 +76,47 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         <a [href]="'#'">Anchor 7</a>
       </p-scroller>
     </div>
+
+    <div
+      class="playground light surface"
+      title="should render scroller with prev button and gradientColorScheme surface on light background"
+    >
+      <p-scroller [dataId]="'prev-surface-light'" [gradientColorScheme]="'surface'">
+        <a [href]="'#'">Anchor 1</a>
+        <a [href]="'#'">Anchor 2</a>
+        <a [href]="'#'">Anchor 3</a>
+        <a [href]="'#'">Anchor 4</a>
+        <a [href]="'#'">Anchor 5</a>
+        <a [href]="'#'">Anchor 6</a>
+        <a [href]="'#'">Anchor 7</a>
+      </p-scroller>
+    </div>
+
+    <div
+      class="playground dark surface"
+      title="should render scroller with prev button and gradientColorScheme surface on dark background"
+    >
+      <p-scroller [dataId]="'prev-surface-dark'" [theme]="'dark'" [gradientColorScheme]="'surface'">
+        <a [href]="'#'">Anchor 1</a>
+        <a [href]="'#'">Anchor 2</a>
+        <a [href]="'#'">Anchor 3</a>
+        <a [href]="'#'">Anchor 4</a>
+        <a [href]="'#'">Anchor 5</a>
+        <a [href]="'#'">Anchor 6</a>
+        <a [href]="'#'">Anchor 7</a>
+      </p-scroller>
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScrollerComponent {}
+export class ScrollerComponent implements OnInit {
+  ngOnInit() {
+    // Use alias for function to bypass setAllReady in generateAngularReactVRTPages.ts
+    const PDSReady = componentsReady;
+    PDSReady().then(() => {
+      document.querySelectorAll(`p-scroller[data-id^='prev']`).forEach((scroller) => {
+        scroller.shadowRoot.querySelector('.action--next p-button-pure').click();
+      });
+    });
+  }
+}
--- packages/components-react/src/pages/Scroller.tsx
+++ packages/components-react/src/pages/Scroller.tsx
@@ -1,7 +1,19 @@
 /* Auto Generated File */
 import { PScroller } from '@porsche-design-system/components-react';
+import { useEffect } from 'react';
+import { pollComponentsReady } from '../pollComponentsReady';
 
 export const ScrollerPage = (): JSX.Element => {
+  useEffect(() => {
+    // Use alias for function to bypass setAllReady in generateAngularReactVRTPages.ts
+    const PDSReady = pollComponentsReady;
+    PDSReady().then(() => {
+      document.querySelectorAll(`p-scroller[data-id^='prev']`).forEach((scroller) => {
+        scroller.shadowRoot.querySelector('.action--next p-button-pure').click();
+      });
+    });
+  }, []);
+
   return (
     <>
       <div className="playground light" title="should render scroller on light background">
@@ -28,6 +40,30 @@ export const ScrollerPage = (): JSX.Element => {
         </PScroller>
       </div>
 
+      <div className="playground light" title="should render scroller with prev button on light background">
+        <PScroller dataId="prev-light">
+          <a href="#">Anchor 1</a>
+          <a href="#">Anchor 2</a>
+          <a href="#">Anchor 3</a>
+          <a href="#">Anchor 4</a>
+          <a href="#">Anchor 5</a>
+          <a href="#">Anchor 6</a>
+          <a href="#">Anchor 7</a>
+        </PScroller>
+      </div>
+
+      <div className="playground dark" title="should render scroller with prev button on dark background">
+        <PScroller dataId="prev-dark" theme="dark">
+          <a href="#">Anchor 1</a>
+          <a href="#">Anchor 2</a>
+          <a href="#">Anchor 3</a>
+          <a href="#">Anchor 4</a>
+          <a href="#">Anchor 5</a>
+          <a href="#">Anchor 6</a>
+          <a href="#">Anchor 7</a>
+        </PScroller>
+      </div>
+
       <div className="playground light surface" title="should render scroller gradientColorScheme surface on light background">
         <PScroller gradientColorScheme="surface">
           <a href="#">Anchor 1</a>
@@ -42,6 +78,36 @@ export const ScrollerPage = (): JSX.Element => {
 
       <div className="playground dark surface" title="should render scroller gradientColorScheme surface on dark background">
         <PScroller theme="dark" gradientColorScheme="surface">
+          <a href="#">Anchor 1</a>
+          <a href="#">Anchor 2</a>
+          <a href="#">Anchor 3</a>
+          <a href="#">Anchor 4</a>
+          <a href="#">Anchor 5</a>
+          <a href="#">Anchor 6</a>
+          <a href="#">Anchor 7</a>
+        </PScroller>
+      </div>
+
+      <div
+        className="playground light surface"
+        title="should render scroller with prev button and gradientColorScheme surface on light background"
+      >
+        <PScroller dataId="prev-surface-light" gradientColorScheme="surface">
+          <a href="#">Anchor 1</a>
+          <a href="#">Anchor 2</a>
+          <a href="#">Anchor 3</a>
+          <a href="#">Anchor 4</a>
+          <a href="#">Anchor 5</a>
+          <a href="#">Anchor 6</a>
+          <a href="#">Anchor 7</a>
+        </PScroller>
+      </div>
+
+      <div
+        className="playground dark surface"
+        title="should render scroller with prev button and gradientColorScheme surface on dark background"
+      >
+        <PScroller dataId="prev-surface-dark" theme="dark" gradientColorScheme="surface">
           <a href="#">Anchor 1</a>
           <a href="#">Anchor 2</a>
           <a href="#">Anchor 3</a>
