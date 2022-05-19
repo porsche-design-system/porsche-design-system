d1af676db72dd47a787d0f5fc760b37a5e2f23cc
Matthias Haegele <matthias.haegele@mhp.com>
2022-05-19 09:00:37 +0200

    feat(components): validate that only stepper-horizontal-items are used in stepper-horizontal | mh | #1861

---
+++ packages/components/src/utils/dom/areChildrenOfKind.ts
@@ -0,0 +1,10 @@
+import { TagNameCamelCase } from '@porsche-design-system/shared';
+import { getPrefixedTagNames, getTagName } from '../tag-name';
+
+export const areChildrenOfKind = (element: HTMLElement, tagName: TagNameCamelCase): boolean => {
+  const children = Array.from(element.children);
+
+  return (
+    children && children.every((child: HTMLElement) => getTagName(child) === getPrefixedTagNames(element)[tagName])
+  );
+};
