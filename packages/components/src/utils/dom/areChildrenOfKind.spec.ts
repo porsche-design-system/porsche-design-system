a0baa181d37456e82377fcf9d899238a87ecaac9
Matthias Haegele <matthias.haegele@mhp.com>
2022-05-19 09:00:52 +0200

    test(components): test validation | mh | #1861

---
+++ packages/components/src/utils/dom/areChildrenOfKind.spec.ts
@@ -0,0 +1,21 @@
+import { areChildrenOfKind } from './areChildrenOfKind';
+
+it('should return true if every children tag matches', () => {
+  const parent = document.createElement('p-grid');
+  const child1 = document.createElement('p-grid-item');
+  const child2 = document.createElement('p-grid-item');
+  parent.appendChild(child1);
+  parent.appendChild(child2);
+
+  expect(areChildrenOfKind(parent, 'pGridItem')).toBe(true);
+});
+
+it('should return false if a children tag does not match', () => {
+  const parent = document.createElement('p-grid');
+  const child1 = document.createElement('p-grid-item');
+  const child2 = document.createElement('div');
+  parent.appendChild(child1);
+  parent.appendChild(child2);
+
+  expect(areChildrenOfKind(parent, 'pGridItem')).toBe(false);
+});
