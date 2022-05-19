c26ad6e41ee61448392e20965ca62451a25da64d
Stephan Schröter <stephan.schroeter@mhp.com>
2022-05-19 10:45:28 +0200

    feat(assets): prepare package to be released as public package | sas | #1872

--- packages/assets/rollup.config.js
+++ packages/assets/rollup.config.js
@@ -1,5 +1,10 @@
 import resolve from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
 import typescript from '@rollup/plugin-typescript';
+
+const rootDir = '../..';
+const outputDir = 'dist';
+const input = 'src/index.ts';
 
 const commonPlugins = [
   resolve({
@@ -9,16 +14,30 @@ const commonPlugins = [
 
 export default [
   {
-    input: 'src/index.ts',
+    input,
     output: {
-      dir: 'dist',
+      dir: outputDir,
       format: 'cjs',
     },
-    plugins: [...commonPlugins, typescript({ declaration: true, declarationDir: 'dist/types', rootDir: 'src' })],
+    plugins: [
+      ...commonPlugins,
+      typescript({ declaration: true, declarationDir: `${outputDir}/types`, rootDir: 'src' }),
+    ],
   },
   {
-    input: 'src/index.ts',
-    output: { dir: 'dist/esm', format: 'esm' },
-    plugins: [...commonPlugins, typescript()],
+    input,
+    output: { dir: `${outputDir}/esm`, format: 'esm' },
+    plugins: [
+      ...commonPlugins,
+      typescript(),
+      copy({
+        targets: [
+          { src: `${rootDir}/LICENSE`, dest: outputDir },
+          { src: `${rootDir}/NOTICE`, dest: outputDir },
+          { src: 'README.md', dest: outputDir },
+          { src: 'CHANGELOG.md', dest: outputDir },
+        ],
+      }),
+    ],
   },
 ];
