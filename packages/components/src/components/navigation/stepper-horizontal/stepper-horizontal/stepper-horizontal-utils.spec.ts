c52165172716fee1ef6f52cd5b2eeaba99af386f
Matthias Haegele <matthias.haegele@mhp.com>
2022-05-20 12:36:49 +0200

    test(components): add unit tests for validation | mh | #1861

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-utils.spec.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-utils.spec.ts
@@ -1,4 +1,4 @@
-import { getIndexOfStepWithStateCurrent } from './stepper-horizontal-utils';
+import { getIndexOfStepWithStateCurrent, throwIfMultipleCurrentStates } from './stepper-horizontal-utils';
 
 describe('getIndexOfStepWithStateCurrent()', () => {
   it('should return correct index', () => {
@@ -9,5 +9,34 @@ describe('getIndexOfStepWithStateCurrent()', () => {
   it('should return -1 when no current state is found', () => {
     const items = [{}, {}, {}];
     expect(getIndexOfStepWithStateCurrent(items as HTMLPStepperHorizontalItemElement[])).toBe(-1);
+  });
+});
+
+describe('throwIfMultipleCurrentStates()', () => {
+  it('should throw error if multiple children have state="current"', () => {
+    const stepperHorizontal = document.createElement('p-stepper-horizontal');
+    const item1: HTMLPStepperHorizontalItemElement = document.createElement('p-stepper-horizontal-item');
+    const item2: HTMLPStepperHorizontalItemElement = document.createElement('p-stepper-horizontal-item');
+
+    item1.state = 'current';
+    item2.state = 'current';
+
+    stepperHorizontal.appendChild(item1);
+    stepperHorizontal.appendChild(item2);
+
+    expect(() => throwIfMultipleCurrentStates(stepperHorizontal, [item1, item2])).toThrow();
+  });
+
+  it('should not throw error if only one child has state="current"', () => {
+    const stepperHorizontal = document.createElement('p-stepper-horizontal');
+    const item1: HTMLPStepperHorizontalItemElement = document.createElement('p-stepper-horizontal-item');
+    const item2: HTMLPStepperHorizontalItemElement = document.createElement('p-stepper-horizontal-item');
+
+    item1.state = 'current';
+
+    stepperHorizontal.appendChild(item1);
+    stepperHorizontal.appendChild(item2);
+
+    expect(() => throwIfMultipleCurrentStates(stepperHorizontal, [item1, item2])).not.toThrow();
   });
 });
