c80e42a2b99f4bf60822bfdc6cac80c3325094f7
Matthias Haegele <matthias.haegele@mhp.com>
2022-05-20 13:06:20 +0200

    feat(components): add accessibility hints to item | mh | #1861

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal-item/stepper-horizontal-item-utils.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal-item/stepper-horizontal-item-utils.ts
@@ -1,4 +1,4 @@
-import { IconName } from '@porsche-design-system/icons';
+import type { IconName } from '@porsche-design-system/icons';
 import { getTagName } from '../../../../utils';
 
 export type StepperState = 'current' | 'complete' | 'warning';
@@ -7,9 +7,7 @@ export const isStateCompleteOrWarning = (state: StepperState): boolean => {
   return state === 'complete' || state === 'warning';
 };
 
-export const getIconName = (
-  state: Extract<StepperState, 'complete' | 'warning'>
-): Extract<IconName, 'success' | 'warning'> => {
+export const getIconName = (state: StepperState): Extract<IconName, 'success' | 'warning'> => {
   return state === 'complete' ? 'success' : 'warning';
 };
 
