b558fdbd4cae7700f5624d711259194d06fdf7c3
Bernd Hacker <hacker.bernd@gmail.com>
2022-05-25 15:04:55 +0200

    feat(components): add basic styling to segmented-control-item | bh | #1839

--- packages/components/src/components/segmented-control-item-styles.ts
+++ packages/components/src/components/segmented-control-item-styles.ts
@@ -1,7 +1,10 @@
 import { getCss } from '../utils';
-import { addImportantToRule } from '../styles';
+import { addImportantToRule, getFocusJssStyle, getHoverJssStyle, getThemedColors, pxToRemWithUnit } from '../styles';
+import { textSmall } from '@porsche-design-system/utilities-v2';
 
 export const getComponentCss = (isDisabled: boolean): string => {
+  const { backgroundSurfaceColor, disabledColor, baseColor } = getThemedColors('light');
+
   return getCss({
     '@global': {
       ':host': {
@@ -9,11 +12,20 @@ export const getComponentCss = (isDisabled: boolean): string => {
       },
       button: {
         display: 'block',
-        // minHeight: '40px',
+        minHeight: pxToRemWithUnit(48),
         height: '100%',
         width: '100%',
-        padding: 0,
-        ...(isDisabled && {}),
+        padding: `0 ${pxToRemWithUnit(40)}`,
+        margin: 0,
+        border: 0,
+        background: backgroundSurfaceColor,
+        ...textSmall,
+        color: isDisabled ? disabledColor : baseColor,
+        cursor: isDisabled ? 'not-allowed' : 'pointer',
+        ...(!isDisabled && {
+          ...getHoverJssStyle(),
+          ...getFocusJssStyle(),
+        }),
       },
     },
     icon: {},
