6a13a44cf167ebd3ba83aa1e4e9de0b7d33cce41
Bernd Hacker <hacker.bernd@gmail.com>
2022-05-30 10:59:20 +0200

    refactor(components): extract getItemMaxWidth and syncItemsProps utils of segmented-control | bh | #1839

---
+++ packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-utils.ts
@@ -0,0 +1,7 @@
+import type { SegmentedControlBackgroundColor } from '../segmented-control/segmented-control-utils';
+import type { Theme } from '../../../types';
+
+export type SegmentedControlItemInternalHTMLProps = {
+  backgroundColor: SegmentedControlBackgroundColor;
+  theme: Theme;
+};
--- packages/components/src/components/segmented-control/segmented-control/segmented-control-utils.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-utils.ts
@@ -1,2 +1,34 @@
+import type { Theme } from '../../../types';
+import type { SegmentedControlItemInternalHTMLProps } from '../segmented-control-item/segmented-control-item-utils';
+
 export const SEGMENTED_CONTROL_BACKGROUND_COLORS = ['background-surface', 'background-default'] as const;
 export type SegmentedControlBackgroundColor = typeof SEGMENTED_CONTROL_BACKGROUND_COLORS[number];
+
+export const getItemMaxWidth = async (host: HTMLElement): Promise<number> => {
+  const widths = await Promise.all(
+    Array.from(host.children).map(async (item) => {
+      const clone = item.cloneNode(true) as HTMLElement;
+      clone.style.position = 'absolute';
+
+      host.parentElement.append(clone);
+      await (clone as any).componentOnReady();
+      const { width } = getComputedStyle(clone);
+      clone.remove();
+
+      return width;
+    })
+  );
+
+  return Math.max(...widths.map(parseFloat)) * 1.03;
+};
+
+export const syncItemsProps = (
+  host: HTMLElement,
+  theme: Theme,
+  backgroundColor: SegmentedControlBackgroundColor
+): void => {
+  Array.from(host.children).forEach((item: HTMLElement & SegmentedControlItemInternalHTMLProps) => {
+    item.theme = theme;
+    item.backgroundColor = backgroundColor;
+  });
+};
