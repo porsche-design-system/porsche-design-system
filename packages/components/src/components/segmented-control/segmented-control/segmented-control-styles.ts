cfd27cd2dc5686e6e7749618998f068d0575c2b4
Bernd Hacker <hacker.bernd@gmail.com>
2022-05-30 14:24:57 +0200

    feat(components): extend getItemMaxWidth to respect icon and font, share variables with segmented-control-item styles | bh | #1839

--- packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
@@ -1,7 +1,7 @@
 import { getCss } from '../../../utils';
 import { addImportantToEachRule } from '../../../styles';
 
-const MAX_ITEM_WIDTH = 200;
+const MAX_ITEM_WIDTH = 220;
 
 export const getComponentCss = (maxWidth: number): string => {
   const minWidth = maxWidth > MAX_ITEM_WIDTH ? MAX_ITEM_WIDTH : maxWidth;
--- packages/components/src/components/segmented-control/segmented-control/segmented-control-utils.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-utils.ts
@@ -1,21 +1,38 @@
 import type { Theme } from '../../../types';
 import type { SegmentedControlItemInternalHTMLProps } from '../segmented-control-item/segmented-control-item-utils';
+import type { SegmentedControlItem } from '../segmented-control-item/segmented-control-item';
+import {
+  ITEM_PADDING,
+  ICON_SIZE,
+  ICON_MARGIN,
+  ITEM_FONT,
+} from '../segmented-control-item/segmented-control-item-styles';
+import { fontFamily } from '@porsche-design-system/utilities-v2';
 
 export const SEGMENTED_CONTROL_BACKGROUND_COLORS = ['background-surface', 'background-default'] as const;
 export type SegmentedControlBackgroundColor = typeof SEGMENTED_CONTROL_BACKGROUND_COLORS[number];
 
 // temporary dom node to measure max-width of children content
-const tempDiv = document.createElement('div'); // TODO: font-family?
+const tempDiv = document.createElement('div');
 tempDiv.style.position = 'absolute';
 tempDiv.style.visibility = 'hidden';
-tempDiv.style.padding = '0 2.5rem';
+tempDiv.style.padding = `0 ${ITEM_PADDING}`;
 tempDiv.style.boxSizing = 'border-box';
+tempDiv.style.font = ITEM_FONT.replace(fontFamily, '"Porsche Next",sans-serif');
+
+const tempIcon = document.createElement('div');
+tempIcon.style.display = 'inline-block';
+tempIcon.style.width = ICON_SIZE;
+tempIcon.style.marginRight = ICON_MARGIN;
 
 export const getItemMaxWidth = (host: HTMLElement): number => {
   host.shadowRoot.append(tempDiv);
 
-  const widths = Array.from(host.children).map((item) => {
+  const widths = Array.from(host.children).map((item: HTMLElement & SegmentedControlItem) => {
     tempDiv.innerHTML = item.innerHTML;
+    if (item.icon) {
+      tempDiv.prepend(tempIcon);
+    }
     return parseFloat(getComputedStyle(tempDiv).width);
   });
 
