aaa5ffa7d35e0739ee14aad9252c25c270644ad9
Bernd Hacker <hacker.bernd@gmail.com>
2022-05-30 10:45:07 +0200

    test(components): remove unneeded styles from segmented-control | bh | #1839

--- packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
@@ -1,18 +1,18 @@
 import { getCss } from '../../../utils';
 import { addImportantToEachRule } from '../../../styles';
 
+const MAX_ITEM_WIDTH = 200;
+
 export const getComponentCss = (maxWidth: number): string => {
-  const minWidth = maxWidth > 200 ? 200 : maxWidth;
+  const minWidth = maxWidth > MAX_ITEM_WIDTH ? MAX_ITEM_WIDTH : maxWidth;
 
   return getCss({
     '@global': {
       ':host': addImportantToEachRule({
         display: 'grid',
-        width: '100%',
-        gridAutoColumns: '1fr',
-        gridAutoRows: '1fr',
-        gridGap: '4px',
+        gridAutoRows: '1fr', // for equal height
         gridTemplateColumns: `repeat(auto-fit, minmax(${minWidth}px, min-content))`,
+        gridGap: '4px',
       }),
     },
   });
