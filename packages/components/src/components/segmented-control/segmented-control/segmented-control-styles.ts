422e9d83179e41f16bb44a55ac7c392a772fad2f
Bernd Hacker <hacker.bernd@gmail.com>
2022-05-31 11:40:18 +0200

    feat(components): extend getItemMaxWidth to respect segmented-control-item's label, use sans-serif for all calculations  | bh | #1839

--- packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
@@ -4,14 +4,14 @@ import { addImportantToEachRule } from '../../../styles';
 const MAX_ITEM_WIDTH = 220;
 
 export const getComponentCss = (maxWidth: number): string => {
-  const minWidth = maxWidth > MAX_ITEM_WIDTH ? MAX_ITEM_WIDTH : maxWidth;
+  const width = maxWidth > MAX_ITEM_WIDTH ? MAX_ITEM_WIDTH : maxWidth;
 
   return getCss({
     '@global': {
       ':host': addImportantToEachRule({
         display: 'grid',
         gridAutoRows: '1fr', // for equal height
-        gridTemplateColumns: `repeat(auto-fit, ${minWidth}px)`,
+        gridTemplateColumns: `repeat(auto-fit, ${width}px)`,
         gridGap: '4px',
       }),
     },
