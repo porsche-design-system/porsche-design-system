7d961cda18948d76ac0bcca91884ffdae2862d1d
Anton Schmidt <anton.schmidt@mhp.com>
2022-06-01 10:26:48 +0200

    feat(components): remove prevState and prevStepIndex from StepChangeEvent | as / mh | #1861

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-utils.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-utils.ts
@@ -1,7 +1,6 @@
-import type { StepperState } from '../stepper-horizontal-item/stepper-horizontal-item-utils';
 import { getTagName } from '../../../../utils';
 
-export type StepChangeEvent = { activeStepIndex: number; prevState: StepperState; prevStepIndex: number };
+export type StepChangeEvent = { activeStepIndex: number };
 
 export const getIndexOfStepWithStateCurrent = (stepperHorizontalItems: HTMLPStepperHorizontalItemElement[]): number => {
   return stepperHorizontalItems.findIndex((item) => item.state === 'current');
