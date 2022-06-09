3d51dc687d7bd477777c993fec0b27ecb0ab3a80
Bernd Hacker <hacker.bernd@gmail.com>
2022-06-09 10:29:06 +0200

    fix(components): typings and environment conditions of hoverMediaQuery | bh | #1851

--- packages/components/src/styles/hover-media-query.ts
+++ packages/components/src/styles/hover-media-query.ts
@@ -1,8 +1,14 @@
-import type { Styles } from 'jss';
-const hoverMediaQueryExp = '@media(hover:hover)';
+import type { JssStyle } from 'jss';
 
-// this helper should be wrapped around any hover style to disable hover behaviour on mobile´
-export const hoverMediaQuery = (style: Styles): Styles =>
-  ROLLUP_REPLACE_IS_STAGING === 'production' || process.env.NODE_ENV === 'test'
-    ? { [hoverMediaQueryExp]: style }
-    : style;
+/**
+ * utility to wrap jss styles parameter in `@media (hover: hover)`
+ * which is used to not have hover styles on touch devices
+ */
+export const hoverMediaQuery = (style: JssStyle): JssStyle =>
+  // puppeteer/chromium does not support `@media (hover: hover)` in headless mode
+  // see https://github.com/puppeteer/puppeteer/issues/5096 and https://github.com/puppeteer/puppeteer/issues/4820
+  ROLLUP_REPLACE_IS_STAGING === 'production' || // prod build
+  process.env.NODE_ENV === 'test' || // unit tests
+  (ROLLUP_REPLACE_IS_STAGING === 'staging' && process.env.NODE_ENV === 'development') // dev via yarn start
+    ? { '@media(hover:hover)': style } // used for prod build, yarn start and unit tests
+    : style; // used for staging build in e2e and vrt tests
