99f9e2182d5e54c4e331ae0cb13758f3999f2007
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-09 13:38:17 +0200

    test(components): add prefix test to areChildrenOfKind | mh / as | #1861

--- packages/components/src/utils/dom/areChildrenOfKind.spec.ts
+++ packages/components/src/utils/dom/areChildrenOfKind.spec.ts
@@ -10,6 +10,16 @@ it('should return true if every children tag matches', () => {
   expect(areChildrenOfKind(parent, 'pGridItem')).toBe(true);
 });
 
+it('should return true if every prefixed children tag matches', () => {
+  const parent = document.createElement('my-prefix-p-grid');
+  const child1 = document.createElement('my-prefix-p-grid-item');
+  const child2 = document.createElement('my-prefix-p-grid-item');
+  parent.appendChild(child1);
+  parent.appendChild(child2);
+
+  expect(areChildrenOfKind(parent, 'pGridItem')).toBe(true);
+});
+
 it('should return false if a children tag does not match', () => {
   const parent = document.createElement('p-grid');
   const child1 = document.createElement('p-grid-item');
