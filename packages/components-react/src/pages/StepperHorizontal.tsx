d65e4f83713f34f8f4386d79b2f27d8217fc73f5
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-10 08:59:27 +0200

    feat(components-js/storefront): remove theme from items | mh | #1861

--- packages/components-react/src/pages/StepperHorizontal.tsx
+++ packages/components-react/src/pages/StepperHorizontal.tsx
@@ -20,15 +20,15 @@ export const StepperHorizontalPage = (): JSX.Element => {
 
       <div className="playground dark" title="should render all numbers from 1 to 9 on dark background">
         <PStepperHorizontal theme="dark">
-          <PStepperHorizontalItem theme="dark">Step 1</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 2</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 3</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 4</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 5</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 6</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 7</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 8</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 9</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 1</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 2</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 3</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 4</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 5</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 6</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 7</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 8</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 9</PStepperHorizontalItem>
         </PStepperHorizontal>
       </div>
 
@@ -45,16 +45,12 @@ export const StepperHorizontalPage = (): JSX.Element => {
 
       <div className="playground dark" title="should render stepper on dark background">
         <PStepperHorizontal theme="dark">
-          <PStepperHorizontalItem theme="dark" state="current">Step Current</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="warning">Step Warning</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step Complete</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="warning" disabled={true}
-            >Step Warning Disabled
-          </PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete" disabled={true}
-            >Step Complete Disabled
-          </PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="current">Step Current</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="warning">Step Warning</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step Complete</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="warning" disabled={true}>Step Warning Disabled </PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete" disabled={true}>Step Complete Disabled </PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step</PStepperHorizontalItem>
         </PStepperHorizontal>
       </div>
 
@@ -74,15 +70,15 @@ export const StepperHorizontalPage = (): JSX.Element => {
 
       <div className="playground dark" title="should render arrow left on smaller viewports on dark background">
         <PStepperHorizontal theme="dark">
-          <PStepperHorizontalItem theme="dark" state="complete">Step 1</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 2</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 3</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 4</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 5</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 6</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 7</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 8</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="current">Step Current</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 1</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 2</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 3</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 4</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 5</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 6</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 7</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 8</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="current">Step Current</PStepperHorizontalItem>
         </PStepperHorizontal>
       </div>
 
@@ -102,15 +98,15 @@ export const StepperHorizontalPage = (): JSX.Element => {
 
       <div className="playground dark" title="should render arrow left and right on smaller viewports on dark background">
         <PStepperHorizontal theme="dark">
-          <PStepperHorizontalItem theme="dark" state="complete">Step 1</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 2</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 3</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="complete">Step 4</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark" state="current">Step Current</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 6</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 7</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 8</PStepperHorizontalItem>
-          <PStepperHorizontalItem theme="dark">Step 9</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 1</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 2</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 3</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="complete">Step 4</PStepperHorizontalItem>
+          <PStepperHorizontalItem state="current">Step Current</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 6</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 7</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 8</PStepperHorizontalItem>
+          <PStepperHorizontalItem>Step 9</PStepperHorizontalItem>
         </PStepperHorizontal>
       </div>
     </>
