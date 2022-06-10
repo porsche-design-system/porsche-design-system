a5a88b702f48440d843243719ffb5bce0be12f37
Stephan Schröter <stephan.schroeter@mhp.com>
2022-06-10 11:15:20 +0200

    refactor: abstract object composition for aria attributes into a pure fuction | sas | #1789

--- packages/components/src/components/action/button/button-utils.ts
+++ packages/components/src/components/action/button/button-utils.ts
@@ -1,2 +1,18 @@
+import type { SelectedAriaAttributes } from '../../../types';
+import type { AriaAttributes } from '../../../aria-types';
+import { isDisabledOrLoading, parseAndGetAriaAttributes } from '../../../utils';
+
 export const BUTTON_ARIA_ATTRIBUTES = ['aria-label', 'aria-expanded', 'aria-pressed', 'aria-haspopup'] as const;
 export type ButtonAriaAttributes = typeof BUTTON_ARIA_ATTRIBUTES[number];
+
+export const getButtonAriaAttributes = (
+  isDisabled: boolean,
+  isLoading: boolean,
+  aria: SelectedAriaAttributes<ButtonAriaAttributes>
+): Pick<AriaAttributes, 'aria-disabled' | 'aria-busy'> => {
+  return {
+    ...parseAndGetAriaAttributes(aria, BUTTON_ARIA_ATTRIBUTES),
+    'aria-disabled': isDisabledOrLoading(isDisabled, isLoading) ? 'true' : null,
+    'aria-busy': isLoading ? 'true' : null,
+  };
+};
