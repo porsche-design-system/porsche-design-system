230c1d10d807e8a547274b447086319ce1dbc10d
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-10 11:23:57 +0200

    feat(components): change scroller display to fix safari bug | mh | #1861

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-styles.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-styles.ts
@@ -1,9 +1,13 @@
 import { getCss } from '../../../../utils';
-import { addImportantToRule } from '../../../../styles';
+import { addImportantToRule, pxToRemWithUnit } from '../../../../styles';
 
 export const getComponentCss = (): string => {
   return getCss({
     '@global': {
+      ':host': {
+        display: 'block',
+        height: addImportantToRule(pxToRemWithUnit(32)),
+      },
       '::slotted(*:not(:last-child))': {
         marginRight: addImportantToRule('1em'),
       },
