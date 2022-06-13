e2d95e87cdf0808982a10cfc2c0080d1b251eb55
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-13 11:07:55 +0200

    fix(components): styles of stepper horizontal | mh / as | #1861

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-styles.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-styles.ts
@@ -1,12 +1,15 @@
 import { getCss } from '../../../../utils';
-import { addImportantToRule, pxToRemWithUnit } from '../../../../styles';
+import { addImportantToEachRule, addImportantToRule, pxToRemWithUnit } from '../../../../styles';
 
 export const getComponentCss = (): string => {
   return getCss({
     '@global': {
       ':host': {
         display: 'block',
-        height: addImportantToRule(pxToRemWithUnit(32)),
+        ...addImportantToEachRule({
+          position: 'relative',
+          height: pxToRemWithUnit(32),
+        }),
       },
       '::slotted(*:not(:last-child))': {
         marginRight: addImportantToRule('1em'),
