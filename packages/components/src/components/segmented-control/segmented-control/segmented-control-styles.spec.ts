4baed89673089830d34dd8b2b01e1f783919d6a8
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-14 15:40:02 +0200

    test(components): add styles test for max width| mh | #1839

--- packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.spec.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.spec.ts
@@ -1,7 +1,10 @@
 import { getComponentCss } from './segmented-control-styles';
 
 describe('getComponentCss()', () => {
-  it.each<Parameters<typeof getComponentCss>>([[80], [82]])('should return correct css for maxWidth: %s', (...args) => {
-    expect(getComponentCss(...args)).toMatchSnapshot();
-  });
+  it.each<Parameters<typeof getComponentCss>>([[80], [82], [230]])(
+    'should return correct css for maxWidth: %s',
+    (...args) => {
+      expect(getComponentCss(...args)).toMatchSnapshot();
+    }
+  );
 });
