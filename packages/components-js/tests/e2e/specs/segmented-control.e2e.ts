dfb1204d84dea1487b8d4cd029232952755dd6dc
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-14 13:31:39 +0200

    test(components-js): add event e2e tests | mh | #1839

--- packages/components-js/tests/e2e/specs/segmented-control.e2e.ts
+++ packages/components-js/tests/e2e/specs/segmented-control.e2e.ts
@@ -1,12 +1,13 @@
 import {
   expectA11yToMatchSnapshot,
-  getAttribute,
   getLifecycleStatus,
   initAddEventListener,
   selectNode,
   setContentWithDesignSystem,
   setProperty,
   waitForStencilLifecycle,
+  addEventListener,
+  reattachElement,
 } from '../helpers';
 import type { Page } from 'puppeteer';
 
@@ -21,6 +22,13 @@ afterEach(async () => await page.close());
 const getHost = () => selectNode(page, 'p-segmented-control');
 const getItemHost = () => selectNode(page, 'p-segmented-control-item');
 const getItemButton = () => selectNode(page, 'p-segmented-control-item >>> button');
+const getAllItems = () => page.$$('p-segmented-control-item');
+const getAllButtons = async () =>
+  Promise.all(
+    (await getAllItems()).map(async (x) =>
+      (await x.evaluateHandle((x) => x.shadowRoot.querySelector('button'))).asElement()
+    )
+  );
 
 const initSegmentedControl = (opts?: { amount: number }): Promise<void> => {
   const { amount = 1 } = opts || {};
@@ -41,7 +49,64 @@ xit('should correctly recalculate width on item add/remove', async () => {});
 xit('should correctly recalculate width on item content change', async () => {});
 xit('should correctly recalculate width on item prop change', async () => {});
 
-xdescribe('events', () => {});
+describe('events', () => {
+  it('should trigger event on item click which is not selected', async () => {
+    await initSegmentedControl({ amount: 2 });
+    const host = await getHost();
+    const [button1, button2] = await getAllButtons();
+
+    let eventCounter = 0;
+    await addEventListener(host, 'segmentedControlChange', () => eventCounter++);
+
+    // Remove and re-attach component to check if events are duplicated / fire at all
+    await reattachElement(page, 'p-segmented-control');
+
+    await button2.click();
+    await waitForStencilLifecycle(page);
+
+    expect(eventCounter).toBe(1);
+
+    await button1.click();
+    await waitForStencilLifecycle(page);
+
+    expect(eventCounter).toBe(2);
+  });
+
+  it('should not trigger event if item is disabled', async () => {
+    await initSegmentedControl({ amount: 2 });
+    const host = await getHost();
+    const [, item2] = await getAllItems();
+    const [, button2] = await getAllButtons();
+
+    let eventCounter = 0;
+    await addEventListener(host, 'segmentedControlChange', (e) => {
+      eventCounter++;
+      console.log(e);
+    });
+
+    await setProperty(item2, 'disabled', true);
+    await waitForStencilLifecycle(page);
+
+    await button2.click();
+    await waitForStencilLifecycle(page);
+
+    expect(eventCounter).toBe(0);
+  });
+
+  it('should not trigger event if item is selected', async () => {
+    await initSegmentedControl();
+    const host = await getHost();
+    const button = await getItemButton();
+
+    let eventCounter = 0;
+    await addEventListener(host, 'segmentedControlChange', () => eventCounter++);
+
+    await button.click();
+    await waitForStencilLifecycle(page);
+
+    expect(eventCounter).toBe(0);
+  });
+});
 
 xdescribe('keyboard', () => {});
 
