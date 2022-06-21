4fff70261b5a983a2afb42fc00f838867766db8b
Anton Schmidt <anton.schmidt@mhp.com>
2022-06-21 13:27:35 +0200

    test(components): adjust unit test to use spy and mock implementation | as | #1861

--- packages/components/src/utils/dom/throwIfChildrenAreNotOfKind.spec.ts
+++ packages/components/src/utils/dom/throwIfChildrenAreNotOfKind.spec.ts
@@ -1,6 +1,8 @@
 import { throwIfChildrenAreNotOfKind } from './throwIfChildrenAreNotOfKind';
-
+import * as areChildrenOfKindUtil from './areChildrenOfKind';
 it('should throw error if children tag does not match', () => {
+  const spy = jest.spyOn(areChildrenOfKindUtil, 'areChildrenOfKind').mockImplementationOnce(() => false);
+
   const parent = document.createElement('p-grid');
   const child1 = document.createElement('p-grid-item');
   const child2 = document.createElement('div');
@@ -8,9 +10,11 @@ it('should throw error if children tag does not match', () => {
   parent.appendChild(child2);
 
   expect(() => throwIfChildrenAreNotOfKind(parent, 'pGridItem')).toThrow();
+  expect(spy).toHaveBeenCalledWith(parent, 'pGridItem');
 });
 
 it('should not throw error if parent tag matches', () => {
+  const spy = jest.spyOn(areChildrenOfKindUtil, 'areChildrenOfKind').mockImplementationOnce(() => true);
   const parent = document.createElement('p-grid');
   const child1 = document.createElement('p-grid-item');
   const child2 = document.createElement('p-grid-item');
@@ -18,9 +22,11 @@ it('should not throw error if parent tag matches', () => {
   parent.appendChild(child2);
 
   expect(() => throwIfChildrenAreNotOfKind(parent, 'pGridItem')).not.toThrow();
+  expect(spy).toHaveBeenCalledWith(parent, 'pGridItem');
 });
 
 it('should not throw error if prefixed parent tag matches', () => {
+  const spy = jest.spyOn(areChildrenOfKindUtil, 'areChildrenOfKind').mockImplementationOnce(() => true);
   const parent = document.createElement('my-prefix-p-grid');
   const child1 = document.createElement('my-prefix-p-grid-item');
   const child2 = document.createElement('my-prefix-p-grid-item');
@@ -28,4 +34,5 @@ it('should not throw error if prefixed parent tag matches', () => {
   parent.appendChild(child2);
 
   expect(() => throwIfChildrenAreNotOfKind(parent, 'pGridItem')).not.toThrow();
+  expect(spy).toHaveBeenCalledWith(parent, 'pGridItem');
 });
