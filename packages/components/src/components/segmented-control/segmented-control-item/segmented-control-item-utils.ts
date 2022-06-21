caa39232b694a9b2fa05a91afe7d8b9ccea65684
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-21 13:12:40 +0200

    feat(components): remove unused functions + tests | mh / bh | #1839

--- packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-utils.ts
+++ packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-utils.ts
@@ -13,11 +13,3 @@ export const getButtonAttributes = (isSelected: boolean, isDisabled: boolean): A
   ...getButtonBaseAriaAttributes(isDisabled, false),
   ...parseAndGetAriaAttributes({ 'aria-pressed': isSelected }),
 });
-
-export const isFirstElementChild = (host: HTMLElement): boolean => host.parentElement.firstElementChild === host;
-
-export const parentHasValue = (host: HTMLElement): boolean =>
-  !!(host.parentElement as HTMLPSegmentedControlElement).value;
-
-export const isSegmentedControlItemFocusable = (host: HTMLElement, isSelected: boolean): boolean =>
-  isSelected || (!parentHasValue(host) && isFirstElementChild(host));
