907a39cfc7f4e3f3735ffba3c07a1a46f6104b49
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-21 13:02:49 +0200

    test(components): improve unit test of throwIfChildrenAreNotOfKind | mh / bh | #1839

--- packages/components/src/utils/dom/areChildrenOfKind.ts
+++ packages/components/src/utils/dom/areChildrenOfKind.ts
@@ -3,6 +3,6 @@ import { getPrefixedTagNames, getTagName } from '../tag-name';
 
 export const areChildrenOfKind = (element: HTMLElement, tagName: TagNameCamelCase): boolean => {
   const children = Array.from(element.children);
-  const prefixedElementTagNames = getPrefixedTagNames(element);
-  return children && children.every((child: HTMLElement) => getTagName(child) === prefixedElementTagNames[tagName]);
+  const prefixedElementTagName = getPrefixedTagNames(element)[tagName];
+  return children.every((child: HTMLElement) => getTagName(child) === prefixedElementTagName);
 };
