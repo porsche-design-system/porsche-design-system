31e87f34ca7f7d7a7dd7dca875564a6524c802f0
Bernd Hacker <hacker.bernd@gmail.com>
2022-06-21 15:24:36 +0200

    refactor(components): change double ternary to logical or in segmented-control-styles | bh | #1839

--- packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
@@ -1,18 +1,18 @@
 import { getCss } from '../../../utils';
 import { addImportantToEachRule } from '../../../styles';
 
-const MAX_ITEM_WIDTH = 220;
 const MIN_ITEM_WIDTH = 46;
+const MAX_ITEM_WIDTH = 220;
 
 export const getComponentCss = (maxWidth: number): string => {
-  const width = maxWidth > MAX_ITEM_WIDTH ? MAX_ITEM_WIDTH : maxWidth < MIN_ITEM_WIDTH ? MIN_ITEM_WIDTH : maxWidth;
+  maxWidth = (maxWidth > MAX_ITEM_WIDTH && MAX_ITEM_WIDTH) || (maxWidth < MIN_ITEM_WIDTH && MIN_ITEM_WIDTH) || maxWidth;
 
   return getCss({
     '@global': {
       ':host': addImportantToEachRule({
         display: 'grid',
         gridAutoRows: '1fr', // for equal height
-        gridTemplateColumns: `repeat(auto-fit, ${width}px)`,
+        gridTemplateColumns: `repeat(auto-fit, ${maxWidth}px)`,
         gridGap: '4px',
       }),
     },
