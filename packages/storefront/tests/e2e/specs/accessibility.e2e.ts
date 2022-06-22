7ab5502858117f6af6d12edc87eba5525615ba49
Bernd Hacker <hacker.bernd@gmail.com>
2022-06-22 11:05:14 +0200

    test(storefront): cover link focus e2e test via markdown vrt, rename axe example e2e test to accessibility | bh | #1892

--- packages/storefront/tests/e2e/specs/accessibility.e2e.ts
+++ packages/storefront/tests/e2e/specs/accessibility.e2e.ts
@@ -1,23 +1,18 @@
+import type { Page } from 'puppeteer';
 import { baseURL } from '../helpers';
-import type { ElementHandle, Page } from 'puppeteer';
-
-const getStyleOnFocus = async (element: ElementHandle): Promise<string> => {
-  await element.focus();
-  return await element.evaluate((el: Element): string => {
-    return `${getComputedStyle(el).outline} ${getComputedStyle(el).outlineOffset}`;
-  });
-};
+import { a11yAnalyze, a11yFinalize } from '../helpers/axe-helper';
 
 let page: Page;
 beforeEach(async () => (page = await browser.newPage()));
 afterEach(async () => await page.close());
 
-describe('focus', () => {
-  it(`should style markdown links on focus`, async () => {
-    await page.goto(`${baseURL}/markdown`, { waitUntil: 'networkidle0' });
+it('works', async () => {
+  await page.goto(baseURL, { waitUntil: 'networkidle0' });
 
-    const linkElement = await page.$(`.vmark a`);
+  await a11yAnalyze(page);
 
-    expect(await getStyleOnFocus(linkElement)).toBe('rgb(0, 0, 0) solid 1px 1px');
-  });
+  await page.goto(baseURL + '/about/introduction', { waitUntil: 'networkidle0' });
+  await a11yAnalyze(page);
+
+  await a11yFinalize();
 });
--- packages/storefront/tests/vrt/specs/markdown.vrt.ts
+++ packages/storefront/tests/vrt/specs/markdown.vrt.ts
@@ -1,10 +1,13 @@
 import { defaultViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 import { routerViewSelector } from '../helpers';
+import { forceFocusState } from '../../../../components-js/tests/vrt/helpers';
 
 it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
   expect(
     await vrtTest(getVisualRegressionTester(viewport), 'markdown', '/markdown', {
       elementSelector: routerViewSelector,
+      // focus first of two links
+      scenario: (page) => forceFocusState(page, 'a[href="https://designsystem.porsche.com/"]:not([title])'),
     })
   ).toBeFalsy();
 });
