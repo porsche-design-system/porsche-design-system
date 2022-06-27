cb105f5c1be168acbdb1ec94373e53d997293024
Stephan Schröter <stephan.schroeter@mhp.com>
2022-06-27 13:52:28 +0200

    feat(browser-notification): split browser-support and cookie-support | sas | #1888

--- packages/browser-notification/src/banners/init-banner.ts
+++
@@ -1,9 +0,0 @@
-import { version } from '../../package.json';
-import { isIEorEdgeHTML } from './banner-helpers';
-import { CDN_PATH } from '../utils';
-
-if (isIEorEdgeHTML()) {
-  const script = document.createElement('script');
-  script.src = `${CDN_PATH}/banner.min.${version}.js`;
-  document.body.appendChild(script);
-}
--- packages/browser-notification/src/cookie-support/cookie-support.ts
+++ packages/browser-notification/src/cookie-support/cookie-support.ts
@@ -1,7 +1,7 @@
-import type { Lang, Locales } from '../utils';
+import type { Lang, Locales } from '../types';
 
 ((): void => {
-  const ID = 'pds-cookie-notification-overlay';
+  const ID = 'porsche-design-system-cookie-support';
 
   // prettier-ignore
   const locales: Locales = {
@@ -103,20 +103,20 @@ import type { Lang, Locales } from '../utils';
   // prettier-ignore
   const lang = document.getElementsByTagName('html')[0].getAttribute('lang')?.slice(0, 2) as Lang;
   const locale = lang in locales ? lang : 'en';
-  
+
   const { title, content } = locales[locale];
   const htmlMarkup = `<strong>${title}</strong><p>${content}</p>`;
-  
+
   // prettier-ignore
   const css = `#ID{position:fixed;display:flex;justify-content:center;margin:0;padding:1rem 7vw;top:0;bottom:0;left:0;right:0;box-sizing:border-box;z-index:999999;background:#fff}#ID>div{position:relative;width:100%;max-width:96rem;margin:0;padding:0;display:flex;flex-direction:column;align-items:center;justify-content:center;text-align:center;font-size:1rem;font-family:'Porsche Next','Arial Narrow',Arial,'Heiti SC',SimHei,sans-serif;font-weight:400;line-height:1.5;color:#000}#ID>div svg{width:5rem;height:5rem;margin:0 0 .5rem;padding:0;fill:#ff9b00}#ID>div strong{display:block;margin:0 0 1rem;font-size:1.5rem}#ID>div>p{margin:.5rem 0 0;max-width:59.25rem;width:100%}#ID .show--at-768,#ID .show--at-768-ilb{display:none}@media only screen and (min-width:768px){#ID .show--at-768{display:block}#ID .show--at-768-ilb{display:inline-block}#ID>div>svg{width:8.5rem;height:8.5rem;margin:0 0 1rem}#ID>div>strong{font-size:3rem}#ID>div>div{margin:2rem 0 0}}`.replace(/#ID/g, `#${ID}`);
   // prettier-ignore
   const html = `<div><style>${css}</style><svg xmlns=http://www.w3.org/2000/svg viewBox="0 0 24 24" width=100% height=100% focusable=false><path d="M12 3L3 21h18zm0 2.24L19.38 20H4.62z"/><path d="M12.5 15l.5-5h-2l.49 5h1.01zM11 16h2v2h-2z"/></svg>${htmlMarkup}</div>`;
-   
+
   const bodyMarkup = document.createElement('div');
   bodyMarkup.id = ID;
   bodyMarkup.innerHTML = html;
   document.body.appendChild(bodyMarkup);
-      
+
   document.body.style.overflow = 'hidden';
 
   /* Auto Generated End */
