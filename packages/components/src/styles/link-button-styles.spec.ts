ff76633882df509ffa7581d8dfda7eebdb689e3e
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-27 13:45:17 +0200

    test(components): Split the regex cases to ensure that all scenarios are covered | mh | #1925

--- packages/components/src/styles/link-button-styles.spec.ts
+++ packages/components/src/styles/link-button-styles.spec.ts
@@ -1,9 +1,12 @@
 import { getCss } from '../utils';
 import { getLinkButtonStyles } from './link-button-styles';
 
-it('should have ">" combinator for .label & .icon on root:hover & root:active', () => {
-  // Direct child selector fixes https://github.com/porscheui/porsche-design-system/pull/1941 in safari < v15.5 for variant tertiary
-  expect(getCss(getLinkButtonStyles('tertiary', false, false, false, 'light'))).toMatch(
-    /\.root:(?:hover|active) > \.(?:label|icon)/
-  );
-});
+it.each([/\.root:hover > \.label/, /\.root:hover > \.icon/, /\.root:active > \.label/, /\.root:active > \.icon/])(
+  'should have ">" combinator for %s',
+  (regex) => {
+    const css = getCss(getLinkButtonStyles('tertiary', false, false, false, 'light'));
+
+    // Direct child selector fixes https://github.com/porscheui/porsche-design-system/pull/1941 in safari < v15.5 for variant tertiary
+    expect(css).toMatch(regex);
+  }
+);
