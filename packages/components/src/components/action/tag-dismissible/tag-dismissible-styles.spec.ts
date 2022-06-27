f8423fd79f8e78e3c01621d91412ee478b4319ec
Matthias Haegele <matthias.haegele@mhp.com>
2022-06-27 16:24:42 +0200

    test(components): improve wording / remove it each | mh / bh | #1925

--- packages/components/src/components/action/tag-dismissible/tag-dismissible-styles.spec.ts
+++ packages/components/src/components/action/tag-dismissible/tag-dismissible-styles.spec.ts
@@ -10,7 +10,7 @@ describe('getComponentCss()', () => {
   });
 
   // Direct child selector fixes https://github.com/porscheui/porsche-design-system/pull/1941 in safari < v15.5
-  it('should have ">" combinator for .icon on button:hover', () => {
-    expect(getComponentCss('background-default', true)).toMatch(/button:hover > \.icon/);
+  it('should have ">" combinator on .button:hover for .icon', () => {
+    expect(getComponentCss('background-default', true)).toMatch('button:hover > .icon');
   });
 });
--- packages/components/src/styles/link-button-styles.spec.ts
+++ packages/components/src/styles/link-button-styles.spec.ts
@@ -1,12 +1,12 @@
 import { getCss } from '../utils';
 import { getLinkButtonStyles } from './link-button-styles';
 
-it.each([/\.root:hover > \.label/, /\.root:hover > \.icon/, /\.root:active > \.label/, /\.root:active > \.icon/])(
-  'should have ">" combinator for %s',
-  (regex) => {
-    const css = getCss(getLinkButtonStyles('tertiary', false, false, false, 'light'));
+it('should have ">" combinator on .root:hover/.root:active for .icon/.label', () => {
+  const css = getCss(getLinkButtonStyles('tertiary', false, false, false, 'light'));
 
-    // Direct child selector fixes https://github.com/porscheui/porsche-design-system/pull/1941 in safari < v15.5 for variant tertiary
-    expect(css).toMatch(regex);
-  }
-);
+  // Direct child selector fixes https://github.com/porscheui/porsche-design-system/pull/1941 in safari < v15.5 for variant tertiary
+  expect(css).toMatch('.root:hover > .label');
+  expect(css).toMatch('.root:hover > .icon');
+  expect(css).toMatch('.root:active > .label');
+  expect(css).toMatch('.root:active > .icon');
+});
