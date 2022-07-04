741256eb324b150982a27078c71cacd9f77cfb3e
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-04 14:01:56 +0200

    test(components): extend lifecycleValidation.spec to verify observeAttributes() and and unobserveAttributes() calls | bh | #1842

--- packages/components/src/components/lifecycleValidation.spec.ts
+++ packages/components/src/components/lifecycleValidation.spec.ts
@@ -4,12 +4,14 @@ import * as getHTMLElementAndThrowIfUndefinedUtils from '../utils/dom/getHTMLEle
 import * as jssUtils from '../utils/jss';
 import * as slottedStylesUtils from '../utils/slotted-styles';
 import * as getDirectChildHTMLElementUtils from '../utils/dom/getDirectChildHTMLElement';
+import * as attributeObserverUtils from '../utils/attribute-observer';
 import { TAG_NAMES_CONSTRUCTOR_MAP } from '../test-utils/tag-names-constructor-map';
 import { addParentAndSetRequiredProps } from '../test-utils/addParentAndSetRequiredProps';
 
 const tagNamesWithRequiredChild = TAG_NAMES.filter((tagName) => getComponentMeta(tagName).requiredChild);
 const tagNamesWithJss = TAG_NAMES.filter((tagName) => getComponentMeta(tagName).styling === 'jss');
 const tagNamesWithSlottedCss = TAG_NAMES.filter((tagName) => getComponentMeta(tagName).hasSlottedCss);
+const tagNamesWithObserveAttributes = TAG_NAMES.filter((tagName) => getComponentMeta(tagName).hasObserveAttributes);
 
 it('should have same amount of elements in TAG_NAMES_CONSTRUCTOR_MAP as in TAG_NAMES', () => {
   expect(Object.keys(TAG_NAMES_CONSTRUCTOR_MAP).length).toBe(TAG_NAMES.length);
@@ -72,7 +74,7 @@ it.each<TagName>(tagNamesWithJss)('should call attachComponentCss() in correct l
   expect(spyCalls).toBe(1); // via connectedCallback or componentWillRender
 });
 
-it.each<TagName>(tagNamesWithSlottedCss)('should call attachSlottedCss() in correct lifecycle for %s', (tagName) => {
+it.each<TagName>(tagNamesWithSlottedCss)('should call attachSlottedCss() via connectedCallback for %s', (tagName) => {
   const spy = jest.spyOn(slottedStylesUtils, 'attachSlottedCss');
 
   const component = new TAG_NAMES_CONSTRUCTOR_MAP[tagName]();
@@ -84,4 +86,38 @@ it.each<TagName>(tagNamesWithSlottedCss)('should call attachSlottedCss() in corr
   } catch (e) {}
 
   expect(spy).toBeCalledWith(component.host, expect.any(Function)); // 2 parameters within connectedCallback
+});
+
+describe.each<TagName>(tagNamesWithObserveAttributes)('%s', (tagName) => {
+  const component = new TAG_NAMES_CONSTRUCTOR_MAP[tagName]();
+  component.host = document.createElement(tagName);
+  component.host.attachShadow({ mode: 'open' });
+  const el = document.createElement('div');
+
+  it('should call observeAttributes() via connectedCallback', () => {
+    const spy = jest.spyOn(attributeObserverUtils, 'observeAttributes');
+    component.connectedCallback();
+
+    expect(spy).toBeCalledWith(undefined, expect.any(Array), expect.any(Function));
+  });
+
+  it('should call observeAttributes() via componentWillLoad', () => {
+    jest.spyOn(getHTMLElementAndThrowIfUndefinedUtils, 'getHTMLElementAndThrowIfUndefined').mockReturnValue(el);
+    const spy = jest.spyOn(attributeObserverUtils, 'observeAttributes');
+
+    if (tagName === 'p-select-wrapper') {
+      (component as any).native = true;
+    }
+
+    component.componentWillLoad();
+
+    expect(spy).toBeCalledWith(el, expect.any(Array), expect.any(Function));
+  });
+
+  it('should call unobserveAttributes() via disconnectedCallback', () => {
+    const spy = jest.spyOn(attributeObserverUtils, 'unobserveAttributes');
+    component.disconnectedCallback();
+
+    expect(spy).toBeCalledWith(el);
+  });
 });
