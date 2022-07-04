0fcc5c96bfe3fe53e57b70e8042a118b18eef8b7
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-04 15:02:08 +0200

    feat(shared): extend generateComponentMeta to extract requiredProps | bh | #1842

--- packages/components/src/components/navigation/link-validation.ts
+++ packages/components/src/components/navigation/link-validation.ts
@@ -1,6 +1,7 @@
 import { getHTMLElement, getTagName } from '../../utils';
 
 export const throwIfInvalidLinkUsage = (host: HTMLElement, href: string): void => {
+  // TODO: needs to be direct and only child
   const hasAnchor = getHTMLElement(host, 'a');
   if ((!href && !hasAnchor) || (href && hasAnchor)) {
     throw new Error(`Usage of ${getTagName(host)} is not valid. Please provide a href property or a slotted anchor.`);
