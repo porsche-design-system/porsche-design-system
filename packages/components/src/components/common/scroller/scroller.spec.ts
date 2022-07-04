4b9cbba698281ba439e5a33ed015601d296623a0
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-04 12:14:09 +0200

    test(components): adjust scroller.spec | bh | #1842

--- packages/components/src/components/common/scroller/scroller.spec.ts
+++ packages/components/src/components/common/scroller/scroller.spec.ts
@@ -1,56 +1,55 @@
 import { Scroller } from './scroller';
-import * as throwIfParentIsNotOneOfKindUtils from '../../../utils/validation/throwIfParentIsNotOneOfKind';
+import * as throwIfRootNodeIsNotOneOfKindUtils from '../../../utils/validation/throwIfRootNodeIsNotOneOfKind';
+
+describe('connectedCallback', () => {
+  it('should call throwIfRootNodeIsNotOneOfKind() with correct parameters', () => {
+    const spy = jest.spyOn(throwIfRootNodeIsNotOneOfKindUtils, 'throwIfRootNodeIsNotOneOfKind');
+    const component = new Scroller();
+
+    try {
+      component.connectedCallback();
+    } catch (e) {}
+
+    expect(spy).toBeCalledWith(undefined, ['pTabsBar', 'pStepperHorizontal']);
+  });
+});
 
 describe('componentDidLoad', () => {
-  describe('connectedCallback', () => {
-    it('should call throwIfParentIsNotOfKind() with correct parameters', () => {
-      const spy = jest.spyOn(throwIfParentIsNotOneOfKindUtils, 'throwIfParentIsNotOneOfKind');
-      const component = new Scroller();
+  it('should call initIntersectionObserver()', () => {
+    const component = new Scroller();
+    const spy = jest.spyOn(component, 'initIntersectionObserver' as any);
 
-      try {
-        component.connectedCallback();
-      } catch (e) {}
+    try {
+      component.componentDidLoad();
+    } catch (e) {}
 
-      expect(spy).toBeCalledWith(undefined, ['pTabsBar', 'pStepperHorizontal']);
-    });
+    expect(spy).toBeCalledTimes(1);
   });
-  describe('componentDidLoad', () => {
-    it('should call initIntersectionObserver()', () => {
-      const component = new Scroller();
-      const spy = jest.spyOn(component, 'initIntersectionObserver' as any);
+});
 
-      try {
-        component.componentDidLoad();
-      } catch (e) {}
+describe('componentShouldUpdate', () => {
+  it('should return true if prop name does not match scrollToPosition', () => {
+    const component = new Scroller();
 
-      expect(spy).toBeCalledTimes(1);
-    });
+    expect(component.componentShouldUpdate(0, 0, 'theme')).toBe(true);
   });
 
-  describe('componentShouldUpdate', () => {
-    it('should return true if prop name does not match scrollToPosition', () => {
-      const component = new Scroller();
+  it('should return true if prop name matches "scrollToPosition" and isPrevHidden and isNextHidden are false', () => {
+    const component = new Scroller();
+    component.isPrevHidden = false;
+    component.isNextHidden = false;
+    expect(component.componentShouldUpdate(0, 0, 'scrollToPosition')).toBe(true);
+  });
 
-      expect(component.componentShouldUpdate(0, 0, 'theme')).toBe(true);
-    });
+  it('should return false if prop name matches "scrollToPosition" and isPrevHidden is true', () => {
+    const component = new Scroller();
+    component.isPrevHidden = true;
+    expect(component.componentShouldUpdate(0, 0, 'scrollToPosition')).toBe(false);
+  });
 
-    it('should return true if prop name matches "scrollToPosition" and isPrevHidden and isNextHidden are false', () => {
-      const component = new Scroller();
-      component.isPrevHidden = false;
-      component.isNextHidden = false;
-      expect(component.componentShouldUpdate(0, 0, 'scrollToPosition')).toBe(true);
-    });
-
-    it('should return false if prop name matches "scrollToPosition" and isPrevHidden is true', () => {
-      const component = new Scroller();
-      component.isPrevHidden = true;
-      expect(component.componentShouldUpdate(0, 0, 'scrollToPosition')).toBe(false);
-    });
-
-    it('should return false if prop name matches "scrollToPosition" and isNextHidden is true', () => {
-      const component = new Scroller();
-      component.isNextHidden = true;
-      expect(component.componentShouldUpdate(0, 0, 'scrollToPosition')).toBe(false);
-    });
+  it('should return false if prop name matches "scrollToPosition" and isNextHidden is true', () => {
+    const component = new Scroller();
+    component.isNextHidden = true;
+    expect(component.componentShouldUpdate(0, 0, 'scrollToPosition')).toBe(false);
   });
 });
