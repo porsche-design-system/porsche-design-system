d3c3ff1c125cde2345da1e72a6bd3e1b3d25f262
Stephan Schröter <stephan.schroeter@mhp.com>
2022-07-04 13:46:10 +0200

    feat: several file path updates because of fallbacks package | sas | #1888

--- packages/components-js/projects/partials/tests/unit/specs/getBrowserSupportFallbackScript.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getBrowserSupportFallbackScript.spec.ts
@@ -1,4 +1,4 @@
-import { getBrowserSupportScript } from '../../../src';
+import { getBrowserSupportFallbackScript } from '../../../src';
 import { render } from '@testing-library/react';
 
 const scriptCom = 'https://cdn.ui.porsche.com/feature-detection/browser-support.[a-z0-9]{32}.js';
@@ -8,14 +8,14 @@ jest.mock('../../../src/shared');
 
 describe('format: html', () => {
   it('should return script', () => {
-    const result = getBrowserSupportScript();
+    const result = getBrowserSupportFallbackScript();
     const regex = new RegExp(scriptCom);
 
     expect(result).toMatch(regex);
   });
 
   it('should return script for china cdn', () => {
-    const result = getBrowserSupportScript({ cdn: 'cn', format: 'html' });
+    const result = getBrowserSupportFallbackScript({ cdn: 'cn', format: 'html' });
     const regex = new RegExp(scriptCn);
 
     expect(result).toMatch(regex);
@@ -24,14 +24,14 @@ describe('format: html', () => {
 
 describe('format: jsx', () => {
   it('should return script', () => {
-    const { container } = render(getBrowserSupportScript({ format: 'jsx' }));
+    const { container } = render(getBrowserSupportFallbackScript({ format: 'jsx' }));
     const regex = new RegExp(scriptCom);
 
     expect(container.innerHTML).toMatch(regex);
   });
 
   it('should return script for china cdn', () => {
-    const { container } = render(getBrowserSupportScript({ cdn: 'cn', format: 'jsx' }));
+    const { container } = render(getBrowserSupportFallbackScript({ cdn: 'cn', format: 'jsx' }));
     const regex = new RegExp(scriptCn);
 
     expect(container.innerHTML).toMatch(regex);
--- packages/components-js/projects/partials/tests/unit/specs/getCookiesFallbackScript.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getCookiesFallbackScript.spec.ts
@@ -1,4 +1,4 @@
-import { getCookieSupportScript } from '../../../src';
+import { getCookiesFallbackScript } from '../../../src';
 import { render } from '@testing-library/react';
 
 const scriptCom = 'https://cdn.ui.porsche.com/feature-detection/cookie-support.[a-z0-9]{32}.js';
@@ -8,14 +8,14 @@ jest.mock('../../../src/shared');
 
 describe('format: html', () => {
   it('should return script', () => {
-    const result = getCookieSupportScript();
+    const result = getCookiesFallbackScript();
     const regex = new RegExp(scriptCom);
 
     expect(result).toMatch(regex);
   });
 
   it('should return script for china cdn', () => {
-    const result = getCookieSupportScript({ cdn: 'cn', format: 'html' });
+    const result = getCookiesFallbackScript({ cdn: 'cn', format: 'html' });
     const regex = new RegExp(scriptCn);
 
     expect(result).toMatch(regex);
@@ -24,14 +24,14 @@ describe('format: html', () => {
 
 describe('format: jsx', () => {
   it('should return script', () => {
-    const { container } = render(getCookieSupportScript({ format: 'jsx' }));
+    const { container } = render(getCookiesFallbackScript({ format: 'jsx' }));
     const regex = new RegExp(scriptCom);
 
     expect(container.innerHTML).toMatch(regex);
   });
 
   it('should return script for china cdn', () => {
-    const { container } = render(getCookieSupportScript({ cdn: 'cn', format: 'jsx' }));
+    const { container } = render(getCookiesFallbackScript({ cdn: 'cn', format: 'jsx' }));
     const regex = new RegExp(scriptCn);
 
     expect(container.innerHTML).toMatch(regex);
