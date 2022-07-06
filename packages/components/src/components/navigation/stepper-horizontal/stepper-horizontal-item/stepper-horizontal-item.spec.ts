cc1a3c7ad8fc6db78c85e2e4025992414e66578a
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-06 09:36:08 +0200

    test(components): delete duplicate manual lifecycle tests for throwIfParentIsNotOfKind | bh | #1842

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal-item/stepper-horizontal-item.spec.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal-item/stepper-horizontal-item.spec.ts
@@ -1,18 +1,6 @@
-import * as throwIfParentIsNotOfKindUtils from '../../../../utils/validation/throwIfParentIsNotOfKind';
 import * as jssUtils from '../../../../utils/jss';
 import * as stepperHorizontalItemUtils from './stepper-horizontal-item-utils';
 import { StepperHorizontalItem } from './stepper-horizontal-item';
-
-describe('connectedCallback', () => {
-  it('should call throwIfParentIsNotOfKind() with correct parameter', () => {
-    const spy = jest.spyOn(throwIfParentIsNotOfKindUtils, 'throwIfParentIsNotOfKind').mockImplementationOnce(() => {});
-    const component = new StepperHorizontalItem();
-
-    component.connectedCallback();
-
-    expect(spy).toBeCalledWith(component.host, 'pStepperHorizontal');
-  });
-});
 
 describe('componentWillRender', () => {
   it('should call throwIfCurrentAndDisabled() with correct parameter', () => {
