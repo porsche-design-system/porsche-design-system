d03282b59672059d2ee08d3dd21bd48f22627e85
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-06 09:36:08 +0200

    test(components): delete duplicate manual lifecycle tests for throwIfParentIsNotOfKind | bh | #1842

--- packages/components/src/components/content/table/table-head-cell/table-head-cell.spec.ts
+++ packages/components/src/components/content/table/table-head-cell/table-head-cell.spec.ts
@@ -1,36 +1,22 @@
-import * as throwIfParentIsNotOfKindUtils from '../../../../utils/validation/throwIfParentIsNotOfKind';
 import * as throwIfElementHasAttributeUtils from '../../../../utils/validation/throwIfElementHasAttribute';
 import { TableHeadCell } from './table-head-cell';
 
-describe('table-head-cell', () => {
-  describe('connectedCallback', () => {
-    it('should call throwIfParentIsNotOfKind() with correct parameters', () => {
-      const spy = jest.spyOn(throwIfParentIsNotOfKindUtils, 'throwIfParentIsNotOfKind');
-      const component = new TableHeadCell();
+describe('connectedCallback', () => {
+  it('should call throwIfElementHasAttribute() with correct parameters', () => {
+    const spy = jest.spyOn(throwIfElementHasAttributeUtils, 'throwIfElementHasAttribute');
+    const component = new TableHeadCell();
+    component.host = document.createElement('p-table-head-cell');
+    component.host.attachShadow({ mode: 'open' });
 
-      try {
-        component.connectedCallback();
-      } catch (e) {}
+    const parent = document.createElement('p-table-head-row');
+    parent.appendChild(component.host);
 
-      expect(spy).toBeCalledWith(undefined, 'pTableHeadRow');
-    });
+    try {
+      component.connectedCallback();
+    } catch (e) {
+      console.log(e);
+    }
 
-    it('should call throwIfElementHasAttribute() with correct parameters', () => {
-      const spy = jest.spyOn(throwIfElementHasAttributeUtils, 'throwIfElementHasAttribute');
-      const component = new TableHeadCell();
-      component.host = document.createElement('p-table-head-cell');
-      component.host.attachShadow({ mode: 'open' });
-
-      const parent = document.createElement('p-table-head-row');
-      parent.appendChild(component.host);
-
-      try {
-        component.connectedCallback();
-      } catch (e) {
-        console.log(e);
-      }
-
-      expect(spy).toBeCalledWith(component.host, 'sort');
-    });
+    expect(spy).toBeCalledWith(component.host, 'sort');
   });
 });
