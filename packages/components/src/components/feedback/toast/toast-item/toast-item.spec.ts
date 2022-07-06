b2de150269208a8dad715f1627f4de213bda7eb1
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-06 10:20:14 +0200

    test(components): delete duplicate and obsolete manual lifecycle tests for throwIfRootNodeIsNotOfKind and throwIfRootNodeIsNotOneOfKind | bh | #1842

--- packages/components/src/components/feedback/toast/toast-item/toast-item.spec.ts
+++ packages/components/src/components/feedback/toast/toast-item/toast-item.spec.ts
@@ -1,20 +1,6 @@
 import { ToastItem } from './toast-item';
 import * as throwIfValueIsInvalidUtils from '../../../../utils/validation/throwIfValueIsInvalid';
-import * as throwIfRootNodeIsNotOfKindUtils from '../../../../utils/validation/throwIfRootNodeIsNotOfKind';
 import { TOAST_STATES } from '../toast/toast-utils';
-
-describe('connectedCallback', () => {
-  it('should call throwIfRootNodeIsNotOfKind() with correct parameters', () => {
-    const spy = jest.spyOn(throwIfRootNodeIsNotOfKindUtils, 'throwIfRootNodeIsNotOfKind');
-    const component = new ToastItem();
-
-    try {
-      component.connectedCallback();
-    } catch (e) {}
-
-    expect(spy).toBeCalledWith(component.host, 'pToast');
-  });
-});
 
 describe('componentWillRender', () => {
   it('should call throwIfValueIsInvalid() with correct parameters', () => {
