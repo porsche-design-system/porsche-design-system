34ab0bf9df2a7717e8acb252ff69bfba5c078370
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-06 11:19:21 +0200

    test(components): extend unit tests of getDirectChildHTMLElement | bh | #1842

--- packages/components/src/utils/dom/getDirectChildHTMLElement.spec.ts
+++ packages/components/src/utils/dom/getDirectChildHTMLElement.spec.ts
@@ -1,22 +1,42 @@
 import * as getHTMLElementUtils from './getHTMLElement';
 import { getDirectChildHTMLElement } from './getDirectChildHTMLElement';
 
-it('should call getHTMLElement() with element and selector parameter', () => {
-  const element = document.createElement('div');
-  const spy = jest.spyOn(getHTMLElementUtils, 'getHTMLElement');
+it('should call getHTMLElement() with element and simple selector parameter', () => {
+  const parent = document.createElement('div');
+  const spy = jest.spyOn(getHTMLElementUtils, 'getHTMLElement').mockImplementation();
 
-  getDirectChildHTMLElement(element, 'span');
-  expect(spy).toBeCalledWith(element, ':scope>span');
+  getDirectChildHTMLElement(parent, 'span');
+  expect(spy).toBeCalledWith(parent, ':scope>span');
 });
 
-it('should return split comma separated selectors', () => {
-  const element = document.createElement('div');
-  const spy = jest.spyOn(getHTMLElementUtils, 'getHTMLElement');
+it('should call getHTMLElement() with element and comma separated selector', () => {
+  const parent = document.createElement('div');
+  const spy = jest.spyOn(getHTMLElementUtils, 'getHTMLElement').mockImplementation();
 
-  // pseudo-class selector ':scope>*' is missing in jsdom
-  try {
-    getDirectChildHTMLElement(element, 'span,button');
-  } catch {}
+  getDirectChildHTMLElement(parent, 'span,button');
 
-  expect(spy).toBeCalledWith(element, ':scope>span,:scope>button');
+  expect(spy).toBeCalledWith(parent, ':scope>span,:scope>button');
+});
+
+it('should return direct child element', () => {
+  const parent = document.createElement('div');
+  const child = document.createElement('button');
+  parent.append(child);
+
+  expect(getDirectChildHTMLElement(parent, 'button')).toBe(child);
+});
+
+it('should return null if there is no child element', () => {
+  const parent = document.createElement('div');
+  expect(getDirectChildHTMLElement(parent, 'button')).toBe(null);
+});
+
+it('should return null for nested child element', () => {
+  const parent = document.createElement('div');
+  const child = document.createElement('span');
+  const nestedChild = document.createElement('button');
+  child.append(nestedChild);
+  parent.append(child);
+
+  expect(getDirectChildHTMLElement(parent, 'button')).toBe(null);
 });
