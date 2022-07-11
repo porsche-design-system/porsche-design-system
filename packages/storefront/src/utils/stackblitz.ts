499897005caa0a8ca96cac18dd2b57e0ed40b5bb
Matthias Haegele <matthias.haegele@mhp.com>
2022-07-11 15:49:10 +0200

    feat(storefront): enable react sandbox | mh | #1951

--- packages/storefront/src/utils/stackblitz.ts
+++ packages/storefront/src/utils/stackblitz.ts
@@ -1,31 +1,84 @@
 import sdk from '@stackblitz/sdk';
 import { convertMarkup } from '@/utils/formatting';
 import { Framework } from '@/models';
-import { version as vanillaJsVersion } from '../../../components-js/projects/components-wrapper/package.json';
+import { version } from '../../../components-js/projects/components-wrapper/package.json';
 
 type OpenOptions = {
   markup: string;
-  framework: Framework;
+  componentName: string;
   additionalJavaScriptLogic?: string;
 };
 
 export const openVanillaJS = (props: OpenOptions) => {
-  const { markup, framework, additionalJavaScriptLogic } = props;
+  const { markup, componentName, additionalJavaScriptLogic } = props;
 
-  sdk.openProject({
-    files: {
-      'index.html': `${convertMarkup(markup, framework)}`,
-      'index.js': `import * as porscheDesignSystem from '@porsche-design-system/components-js'
+  sdk.openProject(
+    {
+      files: {
+        'index.html': `${markup}`,
+        'index.js': `import * as porscheDesignSystem from '@porsche-design-system/components-js'
 porscheDesignSystem.load();
 
 ${additionalJavaScriptLogic}
 `,
+      },
+      template: 'javascript',
+      title: 'Vanilla JS Example',
+      description: `Porsche Design System ${componentName} example`,
+      dependencies: {
+        '@porsche-design-system/components-js': `${version}`,
+      },
     },
-    template: 'javascript',
-    title: 'Vanilla JS Example',
-    description: 'Porsche Design System components example',
-    dependencies: {
-      '@porsche-design-system/components-js': `${vanillaJsVersion}`,
+    {
+      openFile: 'index.html',
+    }
+  );
+};
+
+export const openReact = (props: OpenOptions) => {
+  const { markup, componentName } = props;
+
+  sdk.openProject(
+    {
+      files: {
+        'App.tsx': `import * as React from 'react';
+import { ${componentName} } from '@porsche-design-system/components-react'
+export default function App() {
+  return (
+    <div>
+      ${markup}
+    </div>
+  );
+}`,
+        'index.html': `<div id="root"></div>`,
+        'index.tsx': `import * as React from 'react';
+          import { StrictMode } from "react";
+          import * as ReactDOMClient from "react-dom/client";
+          import App from "./App";
+          import { PorscheDesignSystemProvider } from "@porsche-design-system/components-react";
+          import './style.css';
+
+          const rootElement = document.getElementById("root");
+          const root = ReactDOMClient.createRoot(rootElement);
+
+          root.render(
+            <StrictMode>
+              <PorscheDesignSystemProvider>
+                <App />
+              </PorscheDesignSystemProvider>
+            </StrictMode>
+          );`,
+        'style.css': `*:not(:last-child) { margin-right: 0.5rem; margin-bottom: 0.5rem; }`,
+      },
+      template: 'create-react-app',
+      title: 'React Example',
+      description: `Porsche Design System ${componentName} example`,
+      dependencies: {
+        '@porsche-design-system/components-react': `${version}`,
+      },
     },
-  });
+    {
+      openFile: 'App.tsx',
+    }
+  );
 };
