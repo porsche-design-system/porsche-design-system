3b82ffb56f0f783198fc100cec406b883463a56b
Stephan Schröter <stephan.schroeter@mhp.com>
2022-07-11 12:45:56 +0200

    test(components-js/partials): improve test description and integration | sas | #1888

--- packages/components-js/projects/partials/tests/unit/specs/getBrowserSupportFallbackScript.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getBrowserSupportFallbackScript.spec.ts
@@ -1,20 +1,22 @@
 import { getBrowserSupportFallbackScript } from '../../../src';
 import { render } from '@testing-library/react';
 
-const scriptCom = 'https://cdn.ui.porsche.com/porsche-design-system/fallbacks/browser-support.[a-z0-9]{32}.js';
-const scriptCn = 'https://cdn.ui.porsche.cn/porsche-design-system/fallbacks/browser-support.[a-z0-9]{32}.js';
+const scriptCom =
+  '<script>[\\s\\S]*https://cdn.ui.porsche.com/porsche-design-system/fallbacks/browser-support.[a-z0-9]{32}.js[\\s\\S]*</script>';
+const scriptCn =
+  '<script>[\\s\\S]*https://cdn.ui.porsche.cn/porsche-design-system/fallbacks/browser-support.[a-z0-9]{32}.js[\\s\\S]*</script>';
 
 jest.mock('../../../src/shared');
 
 describe('format: html', () => {
-  it('should return script', () => {
+  it('should return script with cdn url', () => {
     const result = getBrowserSupportFallbackScript();
     const regex = new RegExp(scriptCom);
 
     expect(result).toMatch(regex);
   });
 
-  it('should return script for china cdn', () => {
+  it('should return script with china cdn url', () => {
     const result = getBrowserSupportFallbackScript({ cdn: 'cn', format: 'html' });
     const regex = new RegExp(scriptCn);
 
@@ -23,14 +25,14 @@ describe('format: html', () => {
 });
 
 describe('format: jsx', () => {
-  it('should return script', () => {
+  it('should return script with cdn url', () => {
     const { container } = render(getBrowserSupportFallbackScript({ format: 'jsx' }));
     const regex = new RegExp(scriptCom);
 
     expect(container.innerHTML).toMatch(regex);
   });
 
-  it('should return script for china cdn', () => {
+  it('should return script with china cdn url', () => {
     const { container } = render(getBrowserSupportFallbackScript({ cdn: 'cn', format: 'jsx' }));
     const regex = new RegExp(scriptCn);
 
