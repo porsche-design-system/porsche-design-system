4413bcaedadd3819e32e1ba762819f26eeb64a76
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-13 13:47:37 +0200

    fix(components): flip key comparison in shape validation | bh | #1842

--- packages/components/src/utils/validation/validateProps.ts
+++ packages/components/src/utils/validation/validateProps.ts
@@ -178,10 +178,10 @@ export const AllowedTypes: {
   shape: <T>(shapeStructure: { [key in keyof T]: ValidatorFunction }): ValidatorFunction => {
     return (propName, propValue, componentName) => {
       if (propValue) {
-        const shapeKeys = Object.keys(shapeStructure);
+        const propValueKeys = Object.keys(propValue);
         if (
-          // check structure
-          Object.keys(propValue as object).some((key) => !shapeKeys.includes(key)) ||
+          // check structure, but propValue could contain additional keys
+          Object.keys(shapeStructure).some((key) => !propValueKeys.includes(key)) ||
           // check values
           Object.entries(shapeStructure).some(([structureKey, validatorFunc]: [string, ValidatorFunction]) =>
             validatorFunc(structureKey, propValue[structureKey], componentName)
