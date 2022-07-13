9ed8d3e9e5bcc77cfbd6630decb2729d110ce202
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-13 10:51:01 +0200

    feat(components): add validation to segmented-control and stepper-horizontal | bh | #1842

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal.tsx
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal.tsx
@@ -1,5 +1,7 @@
-import { Component, Element, Event, EventEmitter, Host, JSX, Prop, h } from '@stencil/core';
+import { Component, Element, Event, EventEmitter, h, Host, JSX, Prop } from '@stencil/core';
+import type { PropTypes } from '../../../../utils';
 import {
+  AllowedTypes,
   attachComponentCss,
   getPrefixedTagNames,
   getScrollActivePosition,
@@ -7,6 +9,7 @@ import {
   throwIfChildCountIsExceeded,
   throwIfChildrenAreNotOfKind,
   unobserveChildren,
+  validateProps,
 } from '../../../../utils';
 import { getComponentCss } from './stepper-horizontal-styles';
 import type { StepChangeEvent } from './stepper-horizontal-utils';
@@ -16,8 +19,13 @@ import {
   throwIfMultipleCurrentStates,
 } from './stepper-horizontal-utils';
 import type { Theme } from '../../../../types';
+import { THEMES } from '../../../../types';
 import { getClickedItem } from '../../../../utils/dom/getClickedItem';
 import { getScrollerElements } from '../../../common/scroller/scroller-utils';
+
+const propTypes: PropTypes<typeof StepperHorizontal> = {
+  theme: AllowedTypes.oneOf<Theme>(THEMES),
+};
 
 @Component({
   tag: 'p-stepper-horizontal',
@@ -57,6 +65,7 @@ export class StepperHorizontal {
   }
 
   public componentWillRender(): void {
+    validateProps(this, propTypes, 'p-stepper-horizontal');
     syncItemsProps(this.host, this.theme);
   }
 
