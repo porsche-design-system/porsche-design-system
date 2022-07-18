1cc99d8bc898d99a655a32308fc1f1896ba8cf71
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-18 13:09:57 +0200

    test(components): adjust link-pure unit test | bh | #1842

--- packages/components/src/components/navigation/link-pure/link-pure.spec.ts
+++ packages/components/src/components/navigation/link-pure/link-pure.spec.ts
@@ -1,11 +1,11 @@
 import { LinkPure } from './link-pure';
 import * as buttonLinkPureUtils from '../../../utils/button-link-pure-utils';
 import * as transitionListenerUtils from '../../../utils/transition-listener';
-import * as throwIfInvalidLinkUsageUtils from '../../../utils/validation/throwIfInvalidLinkUsage';
+import * as throwIfInvalidLinkPureUsageUtils from '../../../utils/validation/throwIfInvalidLinkPureUsage';
 
 describe('componentWillLoad', () => {
-  it('should call throwIfInvalidLinkUsage() with correct parameters', () => {
-    const spy = jest.spyOn(throwIfInvalidLinkUsageUtils, 'throwIfInvalidLinkUsage');
+  it('should call throwIfInvalidLinkPureUsage() with correct parameters', () => {
+    const spy = jest.spyOn(throwIfInvalidLinkPureUsageUtils, 'throwIfInvalidLinkPureUsage');
 
     const component = new LinkPure();
     component.host = document.createElement('p-link-pure');
