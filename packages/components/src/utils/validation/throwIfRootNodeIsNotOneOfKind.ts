eeea46ecf6883e58546209f49b066907f02a6dec
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-19 13:13:02 +0200

    Update packages/components/src/utils/validation/throwIfRootNodeIsNotOneOfKind.ts

--- packages/components/src/utils/validation/throwIfRootNodeIsNotOneOfKind.ts
+++ packages/components/src/utils/validation/throwIfRootNodeIsNotOneOfKind.ts
@@ -5,7 +5,7 @@ export const throwIfRootNodeIsNotOneOfKind = (element: HTMLElement, tagNames: Ta
   const shadowHost = (element.getRootNode() as ShadowRoot)?.host as HTMLElement;
   const actualTagName = shadowHost && getTagName(shadowHost);
   const prefixedTagNames = getPrefixedTagNames(element);
-  const allowedTagNames = tagNames.map((tagName) => prefixedTagNames[tagName]).join(', ');
+  const allowedTagNames = tagNames.map((tagName) => prefixedTagNames[tagName]);
 
   if (!allowedTagNames.includes(actualTagName)) {
     throw new Error(`${getTagName(element)} can't be used like this`);
