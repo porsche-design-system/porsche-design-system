505b88450fc491c5d715714d34882e22aa21638b
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-19 07:41:24 +0200

    test(components): remove unneeded mocks in stepper-horizontal-item and hover-media-query unit tests | bh | #1842

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal-item/stepper-horizontal-item.spec.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal-item/stepper-horizontal-item.spec.ts
@@ -1,14 +1,12 @@
 import * as stepperHorizontalItemUtils from './stepper-horizontal-item-utils';
 import { StepperHorizontalItem } from './stepper-horizontal-item';
 
-jest.mock('../../../../utils/jss');
-jest.mock('../../../../utils/validation/validateProps');
-
 describe('componentWillRender', () => {
   it('should call throwIfCurrentAndDisabled() with correct parameter', () => {
     const spy = jest.spyOn(stepperHorizontalItemUtils, 'throwIfCurrentAndDisabled').mockImplementationOnce(() => {});
     const component = new StepperHorizontalItem();
     component.host = document.createElement('p-stepper-horizontal-item') as any;
+    component.host.attachShadow({ mode: 'open' });
 
     component.componentWillRender();
 
