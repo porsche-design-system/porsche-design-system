12693598a0a55604a49b46bfbdbd944ae2055bc4
Stephan Schröter <stephan.schroeter@mhp.com>
2022-07-21 10:58:49 +0200

    test(utilities/utilities): add snapshot tests | sas | #1805

---
+++ packages/utilities/projects/utilities/src/js/grid/grid.spec.ts
@@ -0,0 +1,12 @@
+import * as fromGrid from './';
+
+it('should provide all grid exports', () => {
+  expect(Object.keys(fromGrid).length).toBe(4);
+});
+
+it.each<keyof typeof fromGrid>(Object.keys(fromGrid) as (keyof typeof fromGrid)[])(
+  'should contain correct values for %s',
+  (item) => {
+    expect(fromGrid[item]).toMatchSnapshot();
+  }
+);
