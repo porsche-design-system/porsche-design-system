681d9f6812ea73e15285f38bb829a2c57c5b0984
Stephan Schröter <stephan.schroeter@mhp.com>
2022-07-21 10:52:50 +0200

    feat(utilities/utilities): simplify grid gutter + safe zone | sas

--- packages/utilities/projects/utilities/src/js/grid/grid.ts
+++ packages/utilities/projects/utilities/src/js/grid/grid.ts
@@ -1,42 +1,13 @@
-import { mediaQueryMin } from '../mediaQuery';
+import { gridGutter } from './grid-gutter';
+import { gridMaxWidth } from './grid-max-width';
+import { gridSafeZone } from './grid-safe-zone';
 
-// variables are exported for internal usage by deep import only, no public api
-export const gridMaxWidth = '120rem';
-export const gridGutter: { base: string; s: string; m: string } = {
-  base: '1rem',
-  s: '1.5rem',
-  m: '2.25rem',
-};
-export const gridSafeZone: { base: string; xl: string; xxl: string } = {
-  base: '7vw',
-  xl: '10vw',
-  xxl: '12rem',
-};
-
-const getGridTemplateColumns = (safeZone: string, gutter: string): string => {
-  const calc = `calc(${safeZone} - ${gutter})`;
-  return `[grid-start] ${calc} [content-start] repeat(12, minmax(0, 1fr)) [content-end] ${calc} [grid-end]`;
-};
-
+const calc = `calc(${gridSafeZone} - ${gridGutter})`;
 export const grid = {
   display: 'grid',
-  gridColumnGap: gridGutter.base,
-  gridTemplateColumns: getGridTemplateColumns(gridSafeZone.base, gridGutter.base),
+  gridColumnGap: gridGutter,
+  gridTemplateColumns: `[grid-start] ${calc} [content-start] repeat(12, minmax(0, 1fr)) [content-end] ${calc} [grid-end]`,
   maxWidth: gridMaxWidth,
   margin: '0 auto',
   padding: 0,
-  [mediaQueryMin('s')]: {
-    gridColumnGap: gridGutter.s,
-    gridTemplateColumns: getGridTemplateColumns(gridSafeZone.base, gridGutter.s),
-  },
-  [mediaQueryMin('m')]: {
-    gridColumnGap: gridGutter.m,
-    gridTemplateColumns: getGridTemplateColumns(gridSafeZone.base, gridGutter.m),
-  },
-  [mediaQueryMin('xl')]: {
-    gridTemplateColumns: getGridTemplateColumns(gridSafeZone.xl, gridGutter.m),
-  },
-  [mediaQueryMin('xxl')]: {
-    gridTemplateColumns: getGridTemplateColumns(gridSafeZone.xxl, gridGutter.m),
-  },
 };
