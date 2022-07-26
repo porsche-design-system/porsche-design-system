cf94d1fd45f15b0e4978658935cff79a0eed8c12
Bernd Hacker <hacker.bernd@gmail.com>
2022-07-26 14:03:24 +0200

    test(components-js): extend text-field-wrapper vrt | bh | #1967

--- packages/components-react/src/pages/TextFieldWrapper.tsx
+++ packages/components-react/src/pages/TextFieldWrapper.tsx
@@ -153,12 +153,20 @@ export const TextFieldWrapperPage = (): JSX.Element => {
         <PTextFieldWrapper label="Type search readonly">
           <input type="search" readOnly />
         </PTextFieldWrapper>
-        <PTextFieldWrapper label="Type search without native clear button">
+        <PTextFieldWrapper label="Type search with long value">
           <input
             type="search"
             defaultValue="some really long password with many words and amazing special characters, letters big and small, numbers"
           />
         </PTextFieldWrapper>
+        <form>
+          <PTextFieldWrapper label="Type search with long value within form">
+            <input
+              type="search"
+              defaultValue="some really long password with many words and amazing special characters, letters big and small, numbers"
+            />
+          </PTextFieldWrapper>
+        </form>
       </div>
 
       <div className="playground light" title="should render with error state and error message">
