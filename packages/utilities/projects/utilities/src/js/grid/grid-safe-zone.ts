fc9f95aa249b8f35ebffe5e0ed6999ee12581ec5
Stephan Schröter <stephan.schroeter@mhp.com>
2022-07-26 10:22:50 +0200

    feat(utilities/utilities): use fluid interpolation for grid safe zone | sas | #1805

--- packages/utilities/projects/utilities/src/js/grid/grid-safe-zone.ts
+++ packages/utilities/projects/utilities/src/js/grid/grid-safe-zone.ts
@@ -1,20 +1,5 @@
-export const gridSafeZone = 'max(1rem, min(7vw, 8.4rem))';
-
-export const gridSafeZone7vwLinear = 'max(1rem, min(7vw, 8.4rem))';
-
-export const gridSafeZoneFluidInterpolation = {
+// TODO: use clamp() as soon as browser support is good enough for Safari
+export const gridSafeZone = {
   base: 'max(1rem, min(7vw, 7.7rem))',
   xl: 'max(7.7rem, min(calc(43vw - 39.6rem), 12rem))',
 };
-
-export const gridSafeZoneImprovedLinear = 'max(1.4rem, min(calc(12vw - 2.4rem), 12rem))';
-
-// // export const gridSafeZone = {
-// //   base: 'max(16px, min(7vw, 123.2px))',
-// //   xl: 'max(123.2px, min(calc(43vw - 633.6px), 192px))',
-// // };
-//
-// // export const gridSafeZone = {
-// //   base: 'max(16px, 7vw)',
-// //   xl: 'min(calc(43vw - 633.6px), 192px)',
-// // };
