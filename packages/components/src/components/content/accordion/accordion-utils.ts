15ada976188c8cb1af1a937e0b897c2a00a0b610
Anton Schmidt <anton.schmidt@mhp.com>
2022-07-26 13:31:56 +0200

    refactor(components): extract resize observer | as | #1979

--- packages/components/src/components/content/accordion/accordion-utils.ts
+++ packages/components/src/components/content/accordion/accordion-utils.ts
@@ -32,41 +32,6 @@ export const warnIfCompactAndSizeIsSet = (
   }
 };
 
-export const resizeMap: Map<Node, (entry: ResizeObserverEntry) => void> = new Map();
-
-export const isResizeObserverDefined = (): boolean => 'ResizeObserver' in window;
-
-export let useMutationObserverFallback = !isResizeObserverDefined();
-
-export const useMutationObserverFallbackOverride = (overrideValue: boolean): boolean =>
-  (useMutationObserverFallback = overrideValue);
-
-const resizeObserver =
-  isResizeObserverDefined() &&
-  new ResizeObserver((entries) => {
-    entries.forEach((resizeEntry) => resizeMap.get(resizeEntry.target)?.(resizeEntry));
-  });
-
-export const observeResize = <T extends HTMLElement>(
-  node: T,
-  callback: (entry: ResizeObserverEntry) => void,
-  options?: ResizeObserverOptions
-): void => {
-  // node might not be defined in connectedCallback
-  if (node) {
-    resizeMap.set(node, callback);
-    resizeObserver.observe(node, options);
-  }
-};
-
-export const unobserveResize = <T extends HTMLElement>(node: T): void => {
-  // node might not be defined in disconnectedCallback
-  if (node) {
-    resizeMap.delete(node);
-    resizeObserver.unobserve(node);
-  }
-};
-
 export const registeredAccordions: Accordion[] = [];
 
 export const onWindowResize = (): void => {
