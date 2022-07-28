3a4b4f483b38c442ec78fc2716ca64e35433f0ae
Matthias Haegele <matthias.haegele@mhp.com>
2022-07-28 13:57:02 +0200

    feat(storefront): cleanup code / rename consts | mh | #1951

--- packages/storefront/src/utils/stackblitz/openInStackBlitz.ts
+++ packages/storefront/src/utils/stackblitz/openInStackBlitz.ts
@@ -1,10 +1,10 @@
 import { convertMarkup } from '@/utils/formatting';
 import { themeDark } from '@porsche-design-system/utilities-v2';
-import type { Framework, Theme } from '@/models';
 import { openVanillaJS } from '@/utils/stackblitz/openVanillaJs';
 import { openReact } from '@/utils/stackblitz/openReact';
 import { openAngular } from '@/utils/stackblitz/openAngular';
 import { pascalCase } from 'change-case';
+import type { Framework, Theme } from '@/models';
 
 type OpenInStackBlitzOpts = {
   markup: string;
@@ -19,7 +19,7 @@ export type StackBlitzFrameworkOpts = Omit<OpenInStackBlitzOpts, 'framework' | '
   description: string;
   isThemeDark: boolean;
   bodyStyles: string;
-  componentNames?: string;
+  reactComponentsToImport?: string;
 };
 
 export const themeDarkBodyStyles = `body { background: ${themeDark.background.base}; }`;
@@ -31,7 +31,7 @@ export const openInStackBlitz = (props: OpenInStackBlitzOpts): void => {
   const pdsComponents = Array.from(markup.matchAll(/<((?:\w|-)+)(?:.|\n)*?>/g) ?? [])
     .map(([, x]) => x)
     .filter((tagName, idx, arr) => arr.findIndex((t) => t.startsWith('p-') && t === tagName) === idx);
-  const componentNames = pdsComponents.map((x) => pascalCase(x)).join(', ');
+  const reactComponentsToImport = pdsComponents.map((x) => pascalCase(x)).join(', ');
   const isThemeDark = theme === 'dark';
 
   const openProps: StackBlitzFrameworkOpts = {
@@ -50,7 +50,7 @@ export const openInStackBlitz = (props: OpenInStackBlitzOpts): void => {
     case 'react':
       return openReact({
         ...openProps,
-        componentNames,
+        reactComponentsToImport,
       });
     default:
       return openVanillaJS(openProps);
