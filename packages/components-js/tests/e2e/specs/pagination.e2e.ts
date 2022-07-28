1b1692cfd8f294ed4d44ac4e210604a88fb09d90
Anton Schmidt <anton.schmidt@mhp.com>
2022-07-28 12:21:06 +0200

    test(components-js): remove unneeded describe blocks | as | #1979

--- packages/components-js/tests/e2e/specs/pagination.e2e.ts
+++ packages/components-js/tests/e2e/specs/pagination.e2e.ts
@@ -11,99 +11,97 @@ import {
   waitForStencilLifecycle,
 } from '../helpers';
 
-describe('pagination', () => {
-  let page: Page;
+let page: Page;
 
-  beforeEach(async () => (page = await browser.newPage()));
-  afterEach(async () => await page.close());
+beforeEach(async () => (page = await browser.newPage()));
+afterEach(async () => await page.close());
 
-  const getHost = () => selectNode(page, 'p-pagination');
-  const getNextButton = () => selectNode(page, 'p-pagination >>> li:last-child span');
-  const getNav = () => selectNode(page, 'p-pagination >>> nav');
-  const getPaginationItems = async () => (await (await getNav()).$$('span')).slice(1, -1); // without prev and next
+const getHost = () => selectNode(page, 'p-pagination');
+const getNextButton = () => selectNode(page, 'p-pagination >>> li:last-child span');
+const getNav = () => selectNode(page, 'p-pagination >>> nav');
+const getPaginationItems = async () => (await (await getNav()).$$('span')).slice(1, -1); // without prev and next
 
-  const initPagination = (opts?: { activePage?: number }) => {
-    const { activePage = 1 } = opts ?? {};
+const initPagination = (opts?: { activePage?: number }) => {
+  const { activePage = 1 } = opts ?? {};
 
-    return setContentWithDesignSystem(
-      page,
-      `<p-pagination total-items-count="500" items-per-page="25" active-page="${activePage}"></p-pagination>`
-    );
-  };
+  return setContentWithDesignSystem(
+    page,
+    `<p-pagination total-items-count="500" items-per-page="25" active-page="${activePage}"></p-pagination>`
+  );
+};
 
-  it('should have no errors if disconnected before fully loaded', async () => {
-    initConsoleObserver(page);
+it('should have no errors if disconnected before fully loaded', async () => {
+  initConsoleObserver(page);
 
-    await setContentWithDesignSystem(page, ``);
-    await page.evaluate(() => {
-      const el = document.createElement('p-pagination');
-      document.body.append(el);
+  await setContentWithDesignSystem(page, ``);
+  await page.evaluate(() => {
+    const el = document.createElement('p-pagination');
+    document.body.append(el);
 
-      setTimeout(() => el.remove(), 10);
-    });
-
-    await page.waitForTimeout(10);
-
-    expect(getConsoleErrorsAmount()).toBe(0);
-
-    await page.evaluate(() => console.error('test error'));
-    expect(getConsoleErrorsAmount()).toBe(1);
+    setTimeout(() => el.remove(), 10);
   });
 
-  // TODO: Component has to be refactored. Test fails atm. because it updates on initial render.
-  xdescribe('lifecycle', () => {
-    it('should work without unnecessary round trips on init', async () => {
-      await initPagination();
-      const status = await getLifecycleStatus(page);
+  await page.waitForTimeout(10);
 
-      console.log(status);
+  expect(getConsoleErrorsAmount()).toBe(0);
 
-      expect(status.componentDidLoad['p-pagination'], 'componentDidLoad: p-pagination').toBe(1);
-      expect(status.componentDidLoad['p-icon'], 'componentDidLoad: p-icon').toBe(2);
+  await page.evaluate(() => console.error('test error'));
+  expect(getConsoleErrorsAmount()).toBe(1);
+});
 
-      expect(status.componentDidLoad.all, 'componentDidLoad: all').toBe(3);
-      expect(status.componentDidUpdate.all, 'componentDidUpdate: all').toBe(0);
-    });
+// TODO: Component has to be refactored. Test fails atm. because it updates on initial render.
+xdescribe('lifecycle', () => {
+  it('should work without unnecessary round trips on init', async () => {
+    await initPagination();
+    const status = await getLifecycleStatus(page);
+
+    console.log(status);
+
+    expect(status.componentDidLoad['p-pagination'], 'componentDidLoad: p-pagination').toBe(1);
+    expect(status.componentDidLoad['p-icon'], 'componentDidLoad: p-icon').toBe(2);
+
+    expect(status.componentDidLoad.all, 'componentDidLoad: all').toBe(3);
+    expect(status.componentDidUpdate.all, 'componentDidUpdate: all').toBe(0);
+  });
+});
+
+describe('accessibility', () => {
+  it('should expose correct initial accessibility tree', async () => {
+    await initPagination();
+    const navigation = await getNav();
+
+    await expectA11yToMatchSnapshot(page, navigation, { interestingOnly: false });
   });
 
-  describe('accessibility', () => {
-    it('should expose correct initial accessibility tree', async () => {
-      await initPagination();
-      const navigation = await getNav();
+  it('should expose correct accessibility tree if disabled attribute on button next is toggled', async () => {
+    await initPagination({ activePage: 20 });
 
-      await expectA11yToMatchSnapshot(page, navigation, { interestingOnly: false });
-    });
+    const host = await getHost();
+    await expectA11yToMatchSnapshot(page, await getNextButton(), { message: 'If disabled' });
 
-    it('should expose correct accessibility tree if disabled attribute on button next is toggled', async () => {
-      await initPagination({ activePage: 20 });
+    await setProperty(host, 'activePage', 15);
+    await waitForStencilLifecycle(page);
 
-      const host = await getHost();
-      await expectA11yToMatchSnapshot(page, await getNextButton(), { message: 'If disabled' });
+    await expectA11yToMatchSnapshot(page, await getNextButton(), { message: 'If not disabled' });
+  });
 
-      await setProperty(host, 'activePage', 15);
-      await waitForStencilLifecycle(page);
+  it('should have aria-current = page if selected', async () => {
+    await initPagination();
 
-      await expectA11yToMatchSnapshot(page, await getNextButton(), { message: 'If not disabled' });
-    });
+    const host = await getHost();
+    const paginationItems = await getPaginationItems();
+    const firstPageItem = paginationItems[0];
 
-    it('should have aria-current = page if selected', async () => {
-      await initPagination();
+    expect(await getAttribute(firstPageItem, 'aria-current')).toBe('page');
 
-      const host = await getHost();
-      const paginationItems = await getPaginationItems();
-      const firstPageItem = paginationItems[0];
+    await setProperty(host, 'activePage', 2);
+    await waitForStencilLifecycle(page);
 
-      expect(await getAttribute(firstPageItem, 'aria-current')).toBe('page');
+    expect(await getAttribute(firstPageItem, 'aria-current')).toBeNull();
 
-      await setProperty(host, 'activePage', 2);
-      await waitForStencilLifecycle(page);
+    await setProperty(host, 'activePage', 1);
+    await waitForStencilLifecycle(page);
 
-      expect(await getAttribute(firstPageItem, 'aria-current')).toBeNull();
-
-      await setProperty(host, 'activePage', 1);
-      await waitForStencilLifecycle(page);
-
-      expect(await getAttribute(firstPageItem, 'aria-current')).toBe('page');
-    });
+    expect(await getAttribute(firstPageItem, 'aria-current')).toBe('page');
   });
 });
