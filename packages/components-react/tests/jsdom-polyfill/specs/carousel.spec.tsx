b1805f14043ac04c955a445ac2a5988bebd10e22
Bernd Hacker <hacker.bernd@gmail.com>
2022-08-08 16:29:34 +0200

    test(components-react/uxpin): add basic carousel test | bh | #1903

---
+++ packages/components-react/tests/jsdom-polyfill/specs/carousel.spec.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { componentsReady, PCarousel } from '@porsche-design-system/components-react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getByRoleShadowed } from '@porsche-design-system/components-react/testing';
+
+const Sample = (): JSX.Element => {
+  const [eventCounter, setEventCounter] = useState(0);
+
+  return (
+    <>
+      <PCarousel data-testid="host">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </PCarousel>
+      <div data-testid="debug">Event Counter: {eventCounter}</div>
+    </>
+  );
+};
+
+it('should have initialized shadow dom', async () => {
+  const { getByTestId } = render(<Sample />);
+  await componentsReady();
+
+  expect(getByTestId('host').shadowRoot).not.toBeNull();
+});
+
+xit('should have working events', async () => {
+  const { getByTestId } = render(<Sample />);
+  await componentsReady();
+
+  const debug = getByTestId('debug');
+  const button = getByRoleShadowed('button');
+
+  expect(debug.innerHTML).toBe('Event Counter: 0;');
+
+  await userEvent.click(button);
+  expect(debug.innerHTML).toBe('Event Counter: 1;');
+});
