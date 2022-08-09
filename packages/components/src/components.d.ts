210fcf1335e2f9462f6ce2da8cbd7e4dda3fd46e
Bernd Hacker <hacker.bernd@gmail.com>
2022-08-09 07:23:39 +0200

    feat(components): add alternative draft of carousel | bh | #1903

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -202,6 +202,13 @@ export namespace Components {
          */
         "theme"?: ThemeExtendedElectric;
     }
+    interface PCarouselAlt {
+        "disablePagination"?: BreakpointCustomizable<boolean>;
+        /**
+          * Adapts the color when used on dark background.
+         */
+        "theme"?: ThemeExtendedElectric;
+    }
     interface PCheckboxWrapper {
         /**
           * Show or hide label. For better accessibility it's recommended to show the label.
@@ -1116,6 +1123,10 @@ export interface PCarouselCustomEvent<T> extends CustomEvent<T> {
     detail: T;
     target: HTMLPCarouselElement;
 }
+export interface PCarouselAltCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLPCarouselAltElement;
+}
 export interface PInlineNotificationCustomEvent<T> extends CustomEvent<T> {
     detail: T;
     target: HTMLPInlineNotificationElement;
@@ -1196,6 +1207,12 @@ declare global {
     var HTMLPCarouselElement: {
         prototype: HTMLPCarouselElement;
         new (): HTMLPCarouselElement;
+    };
+    interface HTMLPCarouselAltElement extends Components.PCarouselAlt, HTMLStencilElement {
+    }
+    var HTMLPCarouselAltElement: {
+        prototype: HTMLPCarouselAltElement;
+        new (): HTMLPCarouselAltElement;
     };
     interface HTMLPCheckboxWrapperElement extends Components.PCheckboxWrapper, HTMLStencilElement {
     }
@@ -1486,6 +1503,7 @@ declare global {
         "p-button-group": HTMLPButtonGroupElement;
         "p-button-pure": HTMLPButtonPureElement;
         "p-carousel": HTMLPCarouselElement;
+        "p-carousel-alt": HTMLPCarouselAltElement;
         "p-checkbox-wrapper": HTMLPCheckboxWrapperElement;
         "p-content-wrapper": HTMLPContentWrapperElement;
         "p-divider": HTMLPDividerElement;
@@ -1702,6 +1720,17 @@ declare namespace LocalJSX {
           * Emitted when carousel's position changes.
          */
         "onCarouselChange"?: (event: PCarouselCustomEvent<void>) => void;
+        /**
+          * Adapts the color when used on dark background.
+         */
+        "theme"?: ThemeExtendedElectric;
+    }
+    interface PCarouselAlt {
+        "disablePagination"?: BreakpointCustomizable<boolean>;
+        /**
+          * Emitted when carousel's position changes.
+         */
+        "onCarouselChange"?: (event: PCarouselAltCustomEvent<void>) => void;
         /**
           * Adapts the color when used on dark background.
          */
@@ -2662,6 +2691,7 @@ declare namespace LocalJSX {
         "p-button-group": PButtonGroup;
         "p-button-pure": PButtonPure;
         "p-carousel": PCarousel;
+        "p-carousel-alt": PCarouselAlt;
         "p-checkbox-wrapper": PCheckboxWrapper;
         "p-content-wrapper": PContentWrapper;
         "p-divider": PDivider;
@@ -2721,6 +2751,7 @@ declare module "@stencil/core" {
             "p-button-group": LocalJSX.PButtonGroup & JSXBase.HTMLAttributes<HTMLPButtonGroupElement>;
             "p-button-pure": LocalJSX.PButtonPure & JSXBase.HTMLAttributes<HTMLPButtonPureElement>;
             "p-carousel": LocalJSX.PCarousel & JSXBase.HTMLAttributes<HTMLPCarouselElement>;
+            "p-carousel-alt": LocalJSX.PCarouselAlt & JSXBase.HTMLAttributes<HTMLPCarouselAltElement>;
             "p-checkbox-wrapper": LocalJSX.PCheckboxWrapper & JSXBase.HTMLAttributes<HTMLPCheckboxWrapperElement>;
             "p-content-wrapper": LocalJSX.PContentWrapper & JSXBase.HTMLAttributes<HTMLPContentWrapperElement>;
             "p-divider": LocalJSX.PDivider & JSXBase.HTMLAttributes<HTMLPDividerElement>;
---
+++ packages/components/src/components/content/carousel/carousel-alt.tsx
@@ -0,0 +1,90 @@
+import { Component, Element, Event, EventEmitter, h, Host, Prop } from '@stencil/core';
+import { AllowedTypes, attachComponentCss, THEMES_EXTENDED_ELECTRIC, validateProps } from '../../../utils';
+import type { BreakpointCustomizable, PropTypes, ThemeExtendedElectric } from '../../../types';
+import { getComponentCss } from './carousel-styles';
+import { A11y, Navigation, Pagination, Swiper } from 'swiper';
+
+const propTypes: PropTypes<typeof CarouselAlt> = {
+  disablePagination: AllowedTypes.breakpoint('boolean'),
+  theme: AllowedTypes.oneOf<ThemeExtendedElectric>(THEMES_EXTENDED_ELECTRIC),
+};
+
+@Component({
+  tag: 'p-carousel-alt',
+  shadow: true,
+})
+export class CarouselAlt {
+  @Element() public host!: HTMLElement;
+
+  @Prop() public disablePagination?: BreakpointCustomizable<boolean> = false;
+
+  /** Adapts the color when used on dark background. */
+  @Prop() public theme?: ThemeExtendedElectric = 'light';
+
+  /** Emitted when carousel's position changes. */
+  @Event({ bubbles: false }) public carouselChange: EventEmitter<void>;
+
+  private swiperContainer: HTMLElement;
+  private swiperPagination: HTMLElement;
+  private swiperNextBtn: HTMLElement;
+  private swiperPrevBtn: HTMLElement;
+  private slides: HTMLElement[];
+
+  public componentWillLoad(): void {
+    this.slides = Array.from(this.host.children) as HTMLElement[];
+    this.slides.forEach((el, i) => el.setAttribute('slot', 'slide-' + i));
+  }
+
+  public componentDidLoad(): void {
+    new Swiper(this.swiperContainer, {
+      slidesPerView: 2,
+      // slidesPerGroup: 2,
+      spaceBetween: 10,
+      // cssMode: true,
+      modules: [A11y, Navigation, Pagination],
+      a11y: {
+        id: 'swiper', // for stable dom snapshots
+      },
+      navigation: {
+        nextEl: this.swiperNextBtn,
+        prevEl: this.swiperPrevBtn,
+      },
+      pagination: {
+        el: this.swiperPagination,
+        clickable: true,
+      },
+    });
+  }
+
+  public componentWillRender(): void {
+    validateProps(this, propTypes);
+    attachComponentCss(this.host, getComponentCss, this.disablePagination, this.theme);
+  }
+
+  public render(): JSX.Element {
+    // const PrefixedTagNames = getPrefixedTagNames(this.host);
+
+    return (
+      <Host>
+        <div class="swiper" ref={(ref) => (this.swiperContainer = ref)}>
+          <div class="swiper-wrapper">
+            {this.slides.map((_, i) => (
+              <div class="swiper-slide">
+                <slot name={'slide-' + i} />
+              </div>
+            ))}
+          </div>
+
+          <div class="swiper-button-prev" ref={(ref) => (this.swiperPrevBtn = ref)}></div>
+          <div class="swiper-button-next" ref={(ref) => (this.swiperNextBtn = ref)}></div>
+
+          <div class="swiper-pagination" ref={(ref) => (this.swiperPagination = ref)}></div>
+        </div>
+      </Host>
+    );
+  }
+
+  // private onButtonClick = (): void => {
+  //   this.carouselChange.emit();
+  // };
+}
