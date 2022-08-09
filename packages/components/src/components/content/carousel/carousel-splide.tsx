fcbfea8e32645406a102896ae882df2bdba673d8
Bernd Hacker <hacker.bernd@gmail.com>
2022-08-09 11:55:17 +0200

    feat(components): draft carousel with splide | bh | #1903

---
+++ packages/components/src/components/content/carousel/carousel-splide.tsx
@@ -0,0 +1,125 @@
+import { Component, Element, Event, EventEmitter, h, Host, Prop } from '@stencil/core';
+import { AllowedTypes, attachComponentCss, getPrefixedTagNames, THEMES, validateProps } from '../../../utils';
+import type { BreakpointCustomizable, PropTypes, Theme } from '../../../types';
+import { getComponentCss } from './carousel-styles';
+import { Splide } from '@splidejs/splide';
+import { ButtonPure } from '../../action/button-pure/button-pure';
+
+const propTypes: PropTypes<typeof CarouselSplide> = {
+  disablePagination: AllowedTypes.breakpoint('boolean'),
+  theme: AllowedTypes.oneOf<Theme>(THEMES),
+};
+
+@Component({
+  tag: 'p-carousel-splide',
+  shadow: true,
+})
+export class CarouselSplide {
+  @Element() public host!: HTMLElement;
+
+  @Prop() public disablePagination?: BreakpointCustomizable<boolean> = false;
+
+  /** Adapts the color when used on dark background. */
+  @Prop() public theme?: Theme = 'light';
+
+  /** Emitted when carousel's position changes. */
+  @Event({ bubbles: false }) public carouselChange: EventEmitter<void>;
+
+  private splide: Splide;
+  private swiperContainer: HTMLElement;
+  private btnPrev: ButtonPure;
+  private btnNext: ButtonPure;
+  private pagination: HTMLElement;
+  private slides: HTMLElement[];
+  private slidesPerView = 1; // 1
+  private slidesPerGroup = 1; // 1
+
+  public componentWillLoad(): void {
+    this.slides = Array.from(this.host.children) as HTMLElement[];
+    this.slides.forEach((el, i) => el.setAttribute('slot', `slide-${i}`));
+  }
+
+  public componentDidLoad(): void {
+    this.splide = new Splide(this.swiperContainer, {
+      arrows: false,
+      pagination: false,
+      drag: false,
+      perPage: this.slidesPerView,
+      perMove: this.slidesPerGroup,
+      gap: 16,
+    });
+
+    this.splide.on('mounted', () => {
+      const { start = 0 } = this.splide.options;
+      this.btnPrev.disabled = start === 0;
+      this.btnNext.disabled = start === this.slides.length - 1;
+
+      this.pagination.children[start].classList.add('bullet--active');
+    });
+
+    this.splide.on('move', (newIndex, prevIndex): void => {
+      this.btnPrev.disabled = newIndex === 0;
+      this.btnNext.disabled = newIndex === this.slides.length - 1;
+
+      const { children } = this.pagination;
+      children[prevIndex].classList.remove('bullet--active');
+      children[newIndex].classList.add('bullet--active');
+    });
+
+    this.splide.mount();
+  }
+
+  public componentWillRender(): void {
+    validateProps(this, propTypes);
+    attachComponentCss(this.host, getComponentCss, this.disablePagination, this.theme);
+  }
+
+  public render(): JSX.Element {
+    const PrefixedTagNames = getPrefixedTagNames(this.host);
+
+    return (
+      <Host>
+        <div class="splide" ref={(ref) => (this.swiperContainer = ref)}>
+          <div class="splide__track">
+            <div class="splide__list">
+              {this.slides.map((_, i) => (
+                <div class="splide__slide">
+                  <slot name={`slide-${i}`} />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <PrefixedTagNames.pButtonPure
+            class="btn btn--prev"
+            icon="arrow-head-left"
+            hide-label="true"
+            ref={(ref) => (this.btnPrev = ref)}
+            onClick={() => this.splide.go('<')}
+          >
+            Previous slide
+          </PrefixedTagNames.pButtonPure>
+          <PrefixedTagNames.pButtonPure
+            class="btn btn--next"
+            icon="arrow-head-right"
+            hide-label="true"
+            ref={(ref) => (this.btnNext = ref)}
+            onClick={() => this.splide.go('>')}
+          >
+            Next slide
+          </PrefixedTagNames.pButtonPure>
+
+          <div class="pagination" ref={(ref) => (this.pagination = ref)}>
+            {this.slides.map(() => (
+              <span class="bullet"></span>
+            ))}
+          </div>
+        </div>
+      </Host>
+    );
+  }
+
+  // private onButtonClick = (): void => {
+  //   this.carouselChange.emit();
+  // };
+}
--- packages/components/src/components/content/carousel/carousel-styles.ts
+++ packages/components/src/components/content/carousel/carousel-styles.ts
@@ -18,91 +18,226 @@ import { addImportantToEachRule, getThemedColors } from '../../../styles';
 //   scroll-snap-type: x mandatory;
 // }`;
 
+const splideCss = `@keyframes splide-loading {
+  0% {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(1turn);
+  }
+}
+.splide__track--draggable {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+.splide__track--fade > .splide__list {
+  display: block;
+}
+.splide__track--fade > .splide__list > .splide__slide {
+  left: 0;
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  z-index: 0;
+}
+.splide__track--fade > .splide__list > .splide__slide.is-active {
+  opacity: 1;
+  position: relative;
+  z-index: 1;
+}
+.splide--rtl {
+  direction: rtl;
+}
+.splide__track--ttb > .splide__list {
+  display: block;
+}
+.splide__container {
+  box-sizing: border-box;
+  position: relative;
+}
+.splide__list {
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  display: -ms-flexbox;
+  display: flex;
+  height: 100%;
+  margin: 0 !important;
+  padding: 0 !important;
+}
+.splide.is-initialized:not(.is-active) .splide__list {
+  display: block;
+}
+.splide__pagination {
+  -ms-flex-align: center;
+  align-items: center;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  -ms-flex-pack: center;
+  justify-content: center;
+  margin: 0;
+  pointer-events: none;
+}
+.splide__pagination li {
+  display: inline-block;
+  line-height: 1;
+  list-style-type: none;
+  margin: 0;
+  pointer-events: auto;
+}
+.splide__progress__bar {
+  width: 0;
+}
+.splide {
+  position: relative;
+  visibility: hidden;
+}
+.splide.is-initialized,
+.splide.is-rendered {
+  visibility: visible;
+}
+.splide__slide {
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  box-sizing: border-box;
+  -ms-flex-negative: 0;
+  flex-shrink: 0;
+  list-style-type: none !important;
+  margin: 0;
+  position: relative;
+}
+.splide__slide img {
+  vertical-align: bottom;
+}
+.splide__spinner {
+  animation: splide-loading 1s linear infinite;
+  border: 2px solid #999;
+  border-left-color: transparent;
+  border-radius: 50%;
+  bottom: 0;
+  contain: strict;
+  display: inline-block;
+  height: 20px;
+  left: 0;
+  margin: auto;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 20px;
+}
+.splide__sr {
+  clip: rect(0 0 0 0);
+  border: 0;
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+}
+.splide__toggle.is-active .splide__toggle__play,
+.splide__toggle__pause {
+  display: none;
+}
+.splide__toggle.is-active .splide__toggle__pause {
+  display: inline;
+}
+.splide__track {
+  overflow: hidden;
+  position: relative;
+  z-index: 0;
+}`;
+
 export const getComponentCss = (
   _disablePagination: BreakpointCustomizable<boolean>,
   theme: ThemeExtendedElectric
 ): string => {
   const { brandColor, contrastLowColor } = getThemedColors(theme);
 
-  return getCss({
-    '@global': {
-      ':host': {
-        display: 'block',
+  return (
+    getCss({
+      '@global': {
+        ':host': {
+          display: 'block',
+        },
+        '::slotted(*)': addImportantToEachRule({
+          // position: 'relative',
+          // width: '100%',
+          // height: '100%',
+          // flexShrink: 0,
+          // transitionProperty: 'transform',
+          // boxSizing: 'border-box',
+        }),
       },
-      '::slotted(*)': addImportantToEachRule({
-        // position: 'relative',
-        // width: '100%',
-        // height: '100%',
-        // flexShrink: 0,
-        // transitionProperty: 'transform',
-        // boxSizing: 'border-box',
-      }),
-    },
-    swiper: {
-      height: 'inherit',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      position: 'relative',
-      overflow: 'hidden',
-      listStyle: 'none',
-      padding: 'px',
-      // zIndex: 1,
-    },
-    'swiper-pointer-events': {
-      touchAction: 'pan-y',
-    },
-    // TODO: make this work
-    'swiper-backface-hidden .swiper-slide': {
-      transform: 'translateZ(0)',
-      WebkitBackfaceVisibility: 'hidden',
-      backfaceVisibility: 'hidden',
-    },
-    'swiper-wrapper': {
-      position: 'relative',
-      width: '100%',
-      height: '100%',
-      // zIndex: 1,
-      display: 'flex',
-      transform: 'translate3d(0,0,0)',
-      transitionProperty: 'transform',
-      boxSizing: 'content-box',
-    },
-    'swiper-slide': {
-      position: 'relative',
-      width: '100%',
-      height: '100%',
-      flexShrink: 0,
-      transform: 'translate3d(0px, 0, 0)',
-      transitionProperty: 'transform',
-    },
-    btn: {
-      position: 'absolute',
-      top: '50%',
-      transform: 'translate3d(0,-50%,0)',
-      '&--prev': {
-        left: 0,
+      swiper: {
+        height: 'inherit',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        position: 'relative',
+        overflow: 'hidden',
+        listStyle: 'none',
+        padding: 'px',
+        // zIndex: 1,
       },
-      '&--next': {
-        right: 0,
+      'swiper-pointer-events': {
+        touchAction: 'pan-y',
       },
-    },
-    pagination: {
-      display: 'grid',
-      gridAutoColumns: '7px',
-      gridAutoFlow: 'column',
-      position: 'absolute',
-      gap: '3px',
-      height: '7px',
-      bottom: 0,
-      left: '50%',
-      transform: 'translate3d(-50%,0,0)',
-    },
-    bullet: {
-      borderRadius: '50%',
-      background: contrastLowColor,
-      '&--active': {
-        background: brandColor,
+      // TODO: make this work
+      'swiper-backface-hidden .swiper-slide': {
+        transform: 'translateZ(0)',
+        WebkitBackfaceVisibility: 'hidden',
+        backfaceVisibility: 'hidden',
       },
-    },
-  });
+      'swiper-wrapper': {
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        // zIndex: 1,
+        display: 'flex',
+        transform: 'translate3d(0,0,0)',
+        transitionProperty: 'transform',
+        boxSizing: 'content-box',
+      },
+      'swiper-slide': {
+        position: 'relative',
+        width: '100%',
+        height: '100%',
+        flexShrink: 0,
+        transform: 'translate3d(0px, 0, 0)',
+        transitionProperty: 'transform',
+      },
+      btn: {
+        position: 'absolute',
+        top: '50%',
+        transform: 'translate3d(0,-50%,0)',
+        '&--prev': {
+          left: 0,
+        },
+        '&--next': {
+          right: 0,
+        },
+      },
+      pagination: {
+        display: 'grid',
+        gridAutoColumns: '7px',
+        gridAutoFlow: 'column',
+        position: 'absolute',
+        gap: '3px',
+        height: '7px',
+        bottom: 0,
+        left: '50%',
+        transform: 'translate3d(-50%,0,0)',
+      },
+      bullet: {
+        borderRadius: '50%',
+        background: contrastLowColor,
+        '&--active': {
+          background: brandColor,
+        },
+      },
+    }) + splideCss
+  );
 };
