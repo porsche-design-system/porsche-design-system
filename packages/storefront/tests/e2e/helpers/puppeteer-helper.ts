7ace1ebae6859b8389dd6793c35e1e5d30bacbae
Matthias Haegele <matthias.haegele@mhp.com>
2022-08-12 14:00:24 +0200

    test(storefront): use xpath to get buttons of framework tabs bar / chain selector | mh | #1951

--- packages/storefront/tests/e2e/helpers/puppeteer-helper.ts
+++ packages/storefront/tests/e2e/helpers/puppeteer-helper.ts
@@ -16,3 +16,15 @@ export const getConsoleErrorsAmount = () => consoleMessages.length;
 
 export const getElementInnerText = (element: ElementHandle): Promise<string> =>
   element.evaluate((el) => (el as HTMLElement).innerText);
+
+export const getFrameworkButtons = async (page: Page): Promise<ElementHandle<Node>[]> =>
+  (
+    await Promise.all([
+      page.$x("//button[text() = 'Vanilla JS']"),
+      page.$x("//button[text() = 'Angular']"),
+      page.$x("//button[text() = 'React']"),
+    ])
+  )
+    .map(([handle]) => handle)
+    .flat()
+    .filter((x) => x); // get rid of null values
