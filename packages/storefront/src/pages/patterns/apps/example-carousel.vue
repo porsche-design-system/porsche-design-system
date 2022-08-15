398cfe5c534eeef33b9ed123c884cffd9ac3dee1
Bernd Hacker <hacker.bernd@gmail.com>
2022-08-15 15:51:27 +0200

    feat(storefront): extend example-carousel pattern | bh | #1903

--- packages/storefront/src/pages/patterns/apps/example-carousel.vue
+++ packages/storefront/src/pages/patterns/apps/example-carousel.vue
@@ -3,69 +3,13 @@
     <section>
       <h2>Within content-wrapper</h2>
 
-      <p-carousel heading="Some Heading">
-        <div>slide 1</div>
-        <div>slide 2</div>
-        <div>slide 3</div>
-        <div>slide 4</div>
-        <div>slide 5</div>
-        <div>slide 6</div>
-      </p-carousel>
-
-      <p-carousel
-        heading="Multiline Heading could be quite long especially on smaller screens but how would the carousel be accessible with out one?"
-        slides-per-page="2"
-      >
-        <div>slide 1</div>
-        <div>slide 2</div>
-        <div>slide 3</div>
-        <div>slide 4</div>
-        <div>slide 5</div>
-        <div>slide 6</div>
-      </p-carousel>
-
-      <p-carousel slides-per-page="{base: 3, s: 1, m: 3}">
-        <div>slide 1</div>
-        <div>slide 2</div>
-        <div>slide 3</div>
-        <div>slide 4</div>
-        <div>slide 5</div>
-        <div>slide 6</div>
-      </p-carousel>
+      <div v-html="content"></div>
     </section>
 
     <section>
       <h2>Without content-wrapper</h2>
 
-      <p-carousel heading="Some Heading">
-        <div>slide 1</div>
-        <div>slide 2</div>
-        <div>slide 3</div>
-        <div>slide 4</div>
-        <div>slide 5</div>
-        <div>slide 6</div>
-      </p-carousel>
-
-      <p-carousel
-        heading="Multiline Heading could be quite long especially on smaller screens but how would the carousel be accessible with out one?"
-        slides-per-page="2"
-      >
-        <div>slide 1</div>
-        <div>slide 2</div>
-        <div>slide 3</div>
-        <div>slide 4</div>
-        <div>slide 5</div>
-        <div>slide 6</div>
-      </p-carousel>
-
-      <p-carousel slides-per-page="{base: 3, s: 1, m: 3}">
-        <div>slide 1</div>
-        <div>slide 2</div>
-        <div>slide 3</div>
-        <div>slide 4</div>
-        <div>slide 5</div>
-        <div>slide 6</div>
-      </p-carousel>
+      <div v-html="content"></div>
     </section>
   </main>
 </template>
@@ -75,7 +19,75 @@
   import Component from 'vue-class-component';
 
   @Component()
-  export default class ExampleCarousel extends Vue {}
+  export default class ExampleCarousel extends Vue {
+    content = `
+<p-carousel heading="Some Heading">
+  <div>Slide 1</div>
+  <div>Slide 2</div>
+  <div>Slide 3</div>
+  <div>Slide 4</div>
+  <div>Slide 5</div>
+  <div>Slide 6</div>
+</p-carousel>
+
+<p-carousel>
+  <h2 slot="heading">Slotted Heading with default styles</h2>
+  <div>Slide 1</div>
+  <div>Slide 2</div>
+  <div>Slide 3</div>
+  <div>Slide 4</div>
+  <div>Slide 5</div>
+  <div>Slide 6</div>
+</p-carousel>
+
+<p-carousel heading="Some wrapped Heading" wrap-heading="true">
+  <div>Slide 1</div>
+  <div>Slide 2</div>
+  <div>Slide 3</div>
+  <div>Slide 4</div>
+  <div>Slide 5</div>
+  <div>Slide 6</div>
+</p-carousel>
+
+<p-carousel
+  heading="Multiline Heading could be quite long especially on smaller screens but how would the carousel be accessible with out one?"
+  slides-per-page="2"
+>
+  <div>Slide 1</div>
+  <div>Slide 2</div>
+  <div>Slide 3</div>
+  <div>Slide 4</div>
+  <div>Slide 5</div>
+  <div>Slide 6</div>
+</p-carousel>
+
+<p-carousel heading="Responsive slides per page" slides-per-page="{base: 3, s: 1, m: 3}">
+  <div>Slide 1</div>
+  <div>Slide 2</div>
+  <div>Slide 3</div>
+  <div>Slide 4</div>
+  <div>Slide 5</div>
+  <div>Slide 6</div>
+</p-carousel>
+
+<p-carousel heading="Some Heading" slides-per-page="1.2">
+  <div>Slide 1</div>
+  <div>Slide 2</div>
+  <div>Slide 3</div>
+  <div>Slide 4</div>
+  <div>Slide 5</div>
+  <div>Slide 6</div>
+</p-carousel>
+
+<p-carousel heading="Some Heading" disable-pagination="true">
+  <div>Slide 1</div>
+  <div>Slide 2</div>
+  <div>Slide 3</div>
+  <div>Slide 4</div>
+  <div>Slide 5</div>
+  <div>Slide 6</div>
+</p-carousel>`;
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -96,7 +108,7 @@
     }
   }
 
-  p-carousel {
+  :deep(p-carousel) {
     background: lightsalmon;
 
     &:not(:last-of-type) {
