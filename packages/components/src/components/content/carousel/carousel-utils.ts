39ccadd7d54c5186652d586ecf167fed8387c612
Bernd Hacker <hacker.bernd@gmail.com>
2022-08-15 12:18:30 +0200

    refactor(components): extract some carousel utils | bh | #1903

--- packages/components/src/components/content/carousel/carousel-utils.ts
+++ packages/components/src/components/content/carousel/carousel-utils.ts
@@ -3,6 +3,9 @@ import type { ResponsiveOptions } from '@splidejs/splide';
 import type { BreakpointCustomizable, BreakpointKey } from '../../../types';
 import { mergeDeep, parseJSON } from '../../../utils';
 import { breakpoint } from '@porsche-design-system/utilities-v2';
+import type { Splide } from '@splidejs/splide';
+import { ButtonPure } from '../../action/button-pure/button-pure';
+import { bulletActiveClass } from './carousel-styles';
 
 export type CarouselI18n = Partial<Pick<Options['i18n'], 'slideLabel' | 'prev' | 'next' | 'first' | 'last'>>;
 export type CarouselChangeEvent = { activeIndex: number; previousIndex: number };
@@ -41,4 +44,33 @@ export const toSplideBreakpoints = (
           [propName]: value,
         },
       };
+};
+
+export const getAmountOfSlides = (splide: Splide): number => splide.Components.Slides.getLength();
+
+export const isFirstSlide = (splide: Splide): boolean => splide.index === 0;
+export const isLastSlide = (splide: Splide): boolean => splide.index === getAmountOfSlides(splide) - 1;
+
+export const slidePrev = (splide: Splide): void => {
+  splide.go(isFirstSlide(splide) ? getAmountOfSlides(splide) - 1 : '<');
+};
+
+export const slideNext = (splide: Splide): void => {
+  splide.go(isLastSlide(splide) ? 0 : '>');
+};
+
+export const updatePrevNextButtonAria = (btnPrev: ButtonPure, btnNext: ButtonPure, splide: Splide): void => {
+  const { i18n } = splide.options;
+  btnPrev.aria = { 'aria-label': i18n[isFirstSlide(splide) ? 'last' : 'prev'] };
+  btnNext.aria = { 'aria-label': i18n[isLastSlide(splide) ? 'first' : 'next'] };
+};
+
+export const updatePagination = (paginationEl: HTMLElement, newIndex: number, prevIndex?: number): void => {
+  // TODO: calculation of amount of bullets
+  const { children } = paginationEl;
+  children[newIndex].classList.add(bulletActiveClass);
+
+  if (prevIndex >= 0) {
+    children[prevIndex].classList.remove(bulletActiveClass);
+  }
 };
