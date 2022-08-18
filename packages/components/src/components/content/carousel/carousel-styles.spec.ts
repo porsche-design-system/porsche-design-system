b36c9c8d332ec50b340b50b0c6f38de82d6e5436
Bernd Hacker <hacker.bernd@gmail.com>
2022-08-18 11:45:43 +0200

    test(components): add unit tests for carousel-styles | bh | #1903

---
+++ packages/components/src/components/content/carousel/carousel-styles.spec.ts
@@ -0,0 +1,13 @@
+import { getComponentCss } from './carousel-styles';
+
+describe('getComponentCss()', () => {
+  it.each<Parameters<typeof getComponentCss>>([
+    [false, false, 'light'],
+    [false, true, 'light'],
+    [true, false, 'light'],
+    [true, true, 'light'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'light'],
+  ])('should return correct css for wrapHeading: %s, disablePagination: %j and theme: %s', (...args) => {
+    expect(getComponentCss(...args)).toMatchSnapshot();
+  });
+});
