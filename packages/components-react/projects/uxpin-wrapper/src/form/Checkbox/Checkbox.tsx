09f47f7e621a1bf1c6b244f2b3e8132b82e35fb6
michaelrambeau <mikeair@gmail.com>
2022-08-23 18:37:16 +0900

    Simplify `Checkbox` re-using the generated `CheckboxWrapper` component

--- packages/components-react/projects/uxpin-wrapper/src/form/Checkbox/Checkbox.tsx
+++ packages/components-react/projects/uxpin-wrapper/src/form/Checkbox/Checkbox.tsx
@@ -1,50 +1,15 @@
-import { ForwardedRef, forwardRef, PropsWithChildren, useRef } from 'react';
-import { useBrowserLayoutEffect, usePrefix } from '../../hooks';
-import { syncRef } from '../../utils';
-import { getPaddingStyles } from '../../spacing';
-import { partitionProps } from '../../form-utils';
-import { PCheckboxWrapperProps } from '../../lib/components/CheckboxWrapper/CheckboxWrapper';
+import { CheckboxWrapper, PCheckboxWrapperProps } from '../../lib/components/CheckboxWrapper/CheckboxWrapper';
 import { DummyCheckbox, DummyCheckboxProps, dummyCheckboxPropsKeys } from '../../dummy/DummyCheckbox';
+import { partitionProps } from '../../form-utils';
 
 export type PCheckboxProps = PCheckboxWrapperProps & DummyCheckboxProps;
 
-export const Checkbox = forwardRef(
-  (props: PropsWithChildren<PCheckboxProps>, ref: ForwardedRef<HTMLElement>): JSX.Element => {
-    const elementRef = useRef<HTMLElement>();
-    const Tag = usePrefix('p-checkbox-wrapper');
+export const Checkbox = (props: PCheckboxProps): JSX.Element => {
+  const [dummyCheckboxProps, wrapperProps] = partitionProps(props, dummyCheckboxPropsKeys);
 
-    const [dummyCheckboxProps, otherProps] = partitionProps(props, dummyCheckboxPropsKeys);
-
-    const {
-      hideLabel = false,
-      label = '',
-      message = '',
-      state = 'none',
-      spacingTop,
-      spacingLeft,
-      spacingRight,
-      spacingBottom,
-      ...rest
-    } = otherProps;
-
-    const propsToSync = [hideLabel, label, message, state];
-    useBrowserLayoutEffect(() => {
-      const { current } = elementRef;
-      ['hideLabel', 'label', 'message', 'state'].forEach(
-        (propName, i) => ((current as any)[propName] = propsToSync[i])
-      );
-    }, propsToSync);
-
-    const wrapperProps = {
-      ...rest,
-      style: { ...getPaddingStyles({ spacingTop, spacingLeft, spacingRight, spacingBottom }) },
-      ref: syncRef(elementRef, ref),
-    };
-
-    return (
-      <Tag {...wrapperProps}>
-        <DummyCheckbox type="checkbox" {...dummyCheckboxProps} />
-      </Tag>
-    );
-  }
-);
+  return (
+    <CheckboxWrapper {...wrapperProps}>
+      <DummyCheckbox type="checkbox" {...dummyCheckboxProps} />
+    </CheckboxWrapper>
+  );
+};
