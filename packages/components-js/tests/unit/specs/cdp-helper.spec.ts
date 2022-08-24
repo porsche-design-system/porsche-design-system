69bb7bb4271e492e44b82739f2560f3691ab171b
Matthias Haegele <matthias.haegele@mhp.com>
2022-08-24 09:06:28 +0200

    test(components-js): improve cdp helper to support classes in shadow | mh / tg | #1955

--- packages/components-js/tests/unit/specs/cdp-helper.spec.ts
+++ packages/components-js/tests/unit/specs/cdp-helper.spec.ts
@@ -123,7 +123,7 @@ describe('cdp-helper', () => {
       });
     });
 
-    it('should throw error if shadowRootNodeName is not an "Element.localName"', () => {
+    xit('should throw error if shadowRootNodeName is not an "Element.localName"', () => {
       expect(() => resolveSelector('.hover > p-checkbox-wrapper >>> .tabs-bar')).toThrowErrorMatchingInlineSnapshot(
         '"\\">>> .tabs-bar\\" selector has to be an \\"Element.localName\\" in shadow-root"'
       );
