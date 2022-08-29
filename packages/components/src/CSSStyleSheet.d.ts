ba415685c7783832e6241523896126bec1df5d6d
Bernd Hacker <hacker.bernd@gmail.com>
2022-08-29 08:19:15 +0200

    docs(componennts): add todo in CSSStyleSheet.d.ts | bh | #1903

--- packages/components/src/CSSStyleSheet.d.ts
+++ packages/components/src/CSSStyleSheet.d.ts
@@ -1,3 +1,4 @@
+// TODO: remove with TypeScript 4.8
 // TODO: Types are needed for jss.ts. Remove as soon as they are available in lib.dom.d.ts
 // ShadowRoot.adoptedStyleSheets & CSSStyleSheet.replaceSync is missing in lib.dom.d.ts  https://github.com/microsoft/TypeScript/issues/30022
 import 'construct-style-sheets-polyfill';
