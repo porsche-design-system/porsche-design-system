bef46508d38b321f0b5830dd298466dbfa841a07
TatjanaGartner <tatjana.gartner@mhp.com>
2022-08-29 13:36:25 +0200

    feat(components-react): update storefront scroll-to-position example | tg | #1955

--- packages/components-react/src/examples/ScrollerExample.tsx
+++ packages/components-react/src/examples/ScrollerExample.tsx
@@ -1,27 +1,18 @@
-import { PButton, PScroller } from '@porsche-design-system/components-react';
+import { PScroller, PTagDismissible } from '@porsche-design-system/components-react';
 import { useState } from 'react';
 
 export const ScrollerExamplePage = (): JSX.Element => {
   const style = `
-    .scroller > span {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 3rem;
-      width: 300px;
-      border: 1px solid deeppink;
-    }
+      .scroller > *:not(:last-child) {
+        margin-right: 1rem;
+      }
 
-    .scroller > span:not(:last-child) {
-      margin-right: 1rem;
-    }
-
-    p-button {
-      margin: 0 1rem 1rem 0;
-    }
+      #app > button {
+        margin: 0 1rem 1rem 0;
+      }
   `;
 
-  const [scrollPosition, setScrollPosition] = useState<number>(290);
+  const [scrollPosition, setScrollPosition] = useState<number>(220);
   const [isSmooth, setIsSmooth] = useState<boolean>(false);
 
   const clickHandler = (scrollPosition: number, isSmooth: boolean) => {
@@ -34,14 +25,14 @@ export const ScrollerExamplePage = (): JSX.Element => {
       <style dangerouslySetInnerHTML={{ __html: style }} />
 
       <button onClick={() => clickHandler(0, true)}>Scroll to start</button>
-      <button onClick={() => clickHandler(290, true)}>Scroll to middle</button>
-      <button onClick={() => clickHandler(900, true)}>Scroll to end</button>
+      <button onClick={() => clickHandler(220, true)}>Scroll to middle</button>
+      <button onClick={() => clickHandler(720, true)}>Scroll to end</button>
 
-      <div style={{ maxWidth: '600px' }}>
+      <div style={{ maxWidth: '400px', whiteSpace: 'nowrap' }}>
         <PScroller className="scroller" scrollToPosition={{ scrollPosition, isSmooth }}>
-          <span>Start</span>
-          <span>Middle</span>
-          <span>End</span>
+          <PTagDismissible>START - some tag content</PTagDismissible>
+          <PTagDismissible>MIDDLE - some tag content</PTagDismissible>
+          <PTagDismissible>END - some tag content</PTagDismissible>
         </PScroller>
       </div>
     </>
