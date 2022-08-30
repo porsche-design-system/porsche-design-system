df89f3de944569f7f7d16b4ccf4cce866910c3c1
TatjanaGartner <tatjana.gartner@mhp.com>
2022-08-30 15:48:42 +0200

    test(components): update getComponentCss parameter order | tg | #1955

--- packages/components/src/components/common/scroller/scroller-styles.spec.ts
+++ packages/components/src/components/common/scroller/scroller-styles.spec.ts
@@ -2,18 +2,18 @@ import { getComponentCss } from './scroller-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['default', 'light', false, false, 'center'],
-    ['surface', 'dark', false, false, 'center'],
-    ['default', 'light-electric', false, false, 'center'],
-    ['surface', 'light', false, false, 'center'],
-    ['default', 'dark', false, false, 'center'],
-    ['surface', 'light-electric', false, false, 'center'],
-    ['default', 'light', false, false, 'top'],
-    ['default', 'light', true, false, undefined],
-    ['default', 'dark', false, true, undefined],
-    ['default', 'light', true, true, undefined],
+    ['default', false, false, 'center', 'light'],
+    ['surface', false, false, 'center', 'dark'],
+    ['default', false, false, 'center', 'light-electric'],
+    ['surface', false, false, 'center', 'light'],
+    ['default', false, false, 'center', 'dark'],
+    ['surface', false, false, 'center', 'light-electric'],
+    ['default', false, false, 'top', 'light'],
+    ['default', true, false, undefined, 'light'],
+    ['default', false, true, undefined, 'dark'],
+    ['default', true, true, undefined, 'light'],
   ])(
-    'should return correct css for gradientColorScheme: %s, theme: %s, isPrevHidden: %s, isNextHidden: %s and scrollIndicatorPosition: %s ',
+    'should return correct css for gradientColorScheme: %s, isNextHidden: %s, isPrevHidden: %s and scrollIndicatorPosition: %s, theme: %s ',
     (...args) => {
       expect(getComponentCss(...args)).toMatchSnapshot();
     }
