79915ff9db506c3ef23305692a423a4bcb3c1860
Anton Schmidt <anton.schmidt@mhp.com>
2022-09-01 14:00:20 +0200

    test(components-js): add test descriptions, work around ios safari empty image on viewport < 358, add viewport height for modal scroll tests | as | #1898

--- packages/components-js/tests/vrt/helpers/cb-vrt-helper.ts
+++ packages/components-js/tests/vrt/helpers/cb-vrt-helper.ts
@@ -11,19 +11,31 @@ const defaultOptions = {
 type CBVRTestOptions = {
   namePostfix?: string;
   scenario?: (page: Page) => Promise<void>;
+  viewportHeight?: number;
 };
 
 export const cbVRT = async (route: string, viewport: Viewport = 1000, options?: CBVRTestOptions): Promise<void> => {
-  const { scenario, namePostfix = '' } = options || {};
+  const { scenario, namePostfix = '', viewportHeight } = options || {};
   const { baseUrl } = defaultOptions;
   const testName = `${route}${namePostfix}-${viewport}`;
-  return test(testName, async ({ page }) => {
+  return test(testName, async ({ page }, workerInfo) => {
+    const isMobileSafari = workerInfo.project.name === 'Mobile Safari';
+
+    // Skip this resolution for Mobile Safari because of bug where scrollbars are always visible https://github.com/microsoft/playwright/issues/844
+    if (viewport === 2560 && isMobileSafari) {
+      return;
+    }
+
     await page.setViewportSize({
-      width: viewport,
-      height: viewport,
+      width: viewport === 320 && isMobileSafari ? 358 : viewport, // Workaround for Mobile Safari if viewport < 358 an empty screenshot is created, since the Target is closed after navigation
+      height: viewportHeight ? viewportHeight : viewport,
     });
     await page.goto(`${baseUrl}/#${route}`, { waitUntil: 'networkidle' });
     await page.evaluate(() => (window as any).componentsReady());
+
+    if (isMobileSafari) {
+      await page.evaluate(() => window.scrollTo(0, 0));
+    }
 
     if (scenario) {
       await scenario(page);
--- packages/components-js/tests/vrt/specs/modal.cb-vrt.ts
+++ packages/components-js/tests/vrt/specs/modal.cb-vrt.ts
@@ -1,44 +1,61 @@
 import { extendedViewports, cbVRT } from '../helpers/cb-vrt-helper';
-
-extendedViewports.forEach(async (viewport) => {
-  await cbVRT('modal-basic', viewport);
-});
-
-extendedViewports.forEach(async (viewport) => {
-  await cbVRT('modal-scrollable', viewport, {
-    scenario: (page) =>
-      page.evaluate(() => {
-        document.querySelector('p-modal').scrollTo(0, 10000);
-      }),
+import { test } from '@playwright/test';
+test.describe('should have no visual regression for basic modal', () => {
+  extendedViewports.forEach(async (viewport) => {
+    await cbVRT('modal-basic', viewport);
   });
 });
 
-// single resolution
-cbVRT('modal-prefixed');
-
-extendedViewports.forEach(async (viewport) => {
-  await cbVRT('modal-fullscreen', viewport);
-});
-
-extendedViewports.forEach(async (viewport) => {
-  await cbVRT('modal-fullscreen-breakpoint', viewport);
-  await cbVRT('modal-fullscreen-breakpoint', viewport, {
-    namePostfix: '-m',
-    scenario: (page) =>
-      page.evaluate(() => {
-        (document.querySelector('p-modal') as any).fullscreen = { base: false, m: true };
-      }),
+test.describe('should have no visual regression for scrollable modal', () => {
+  extendedViewports.forEach(async (viewport) => {
+    await cbVRT('modal-scrollable', viewport, {
+      viewportHeight: 320,
+      scenario: (page) =>
+        page.evaluate(() => {
+          document.querySelector('p-modal').scrollTo(0, 10000);
+        }),
+    });
   });
 });
 
-extendedViewports.forEach(async (viewport) => {
-  await cbVRT('modal-full-width-slot', viewport);
+test.describe('should have no visual regression for prefixed modal', async () => {
+  // single resolution
+  await cbVRT('modal-prefixed');
 });
 
-extendedViewports.forEach(async (viewport) => {
-  await cbVRT('modal-no-heading', viewport);
+test.describe('should have no visual regression for fullscreen modal', () => {
+  extendedViewports.forEach(async (viewport) => {
+    await cbVRT('modal-fullscreen', viewport);
+  });
 });
 
-extendedViewports.forEach(async (viewport) => {
-  await cbVRT('modal-slotted-heading', viewport);
+test.describe('should have no visual regression for fullscreen breakpoint modal', () => {
+  extendedViewports.forEach(async (viewport) => {
+    await cbVRT('modal-fullscreen-breakpoint', viewport);
+    await cbVRT('modal-fullscreen-breakpoint', viewport, {
+      namePostfix: '-m',
+      scenario: (page) =>
+        page.evaluate(() => {
+          (document.querySelector('p-modal') as any).fullscreen = { base: false, m: true };
+        }),
+    });
+  });
+});
+
+test.describe('should have no visual regression for full-width-slot', () => {
+  extendedViewports.forEach(async (viewport) => {
+    await cbVRT('modal-full-width-slot', viewport);
+  });
+});
+
+test.describe('should have no visual regression for modal without heading', () => {
+  extendedViewports.forEach(async (viewport) => {
+    await cbVRT('modal-no-heading', viewport);
+  });
+});
+
+test.describe('should have no visual regression for modal with slotted heading', () => {
+  extendedViewports.forEach(async (viewport) => {
+    await cbVRT('modal-slotted-heading', viewport);
+  });
 });
