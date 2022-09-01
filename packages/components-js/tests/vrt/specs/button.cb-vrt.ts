a9cc17773a246cbda04eb7945bd101ae950bd009
Anton Schmidt <anton.schmidt@mhp.com>
2022-09-01 16:49:41 +0200

    test(components-js): add button, button-pure and button-group tests | as | #1898

---
+++ packages/components-js/tests/vrt/specs/button.cb-vrt.ts
@@ -0,0 +1,12 @@
+import { test } from '@playwright/test';
+import { cbVRT, defaultViewports, testIfSkeletonsActive } from '../helpers/cb-vrt-helper';
+
+test.describe('should have no visual regression', () => {
+  defaultViewports.forEach(async (viewport) => {
+    await cbVRT('button', viewport);
+  });
+});
+
+testIfSkeletonsActive('should have no visual regression for skeleton', async () => {
+  await cbVRT('button-skeleton');
+});
