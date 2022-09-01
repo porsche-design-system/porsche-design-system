a6efbfd59f7f61a6a3447ecde8ef05d5ef96439e
TatjanaGartner <tatjana.gartner@mhp.com>
2022-09-01 19:51:06 +0200

    feat(components-js): remove unnecessary ; from inputparser | tg | #1955

--- packages/components/scripts/wrapper-generator/InputParser.ts
+++ packages/components/scripts/wrapper-generator/InputParser.ts
@@ -50,7 +50,7 @@ export class InputParser {
       // fix consumer typing for accessibility props with string type
       .replace(/(export declare type SelectedAriaAttributes<T extends keyof AriaAttributes> = .*?) \| string;/, '$1;')
       // fix consumer typing for CarouselInternationalization prop with string type
-      .replace(/(export declare type CarouselInternationalization = .*?) \| string;/, '$1;');
+      .replace(/(export declare type CarouselInternationalization = .*?) \| string;/, '$1;')
       .replace(/(export declare type SelectedAriaAttributes<T extends keyof AriaAttributes> = .*?) \| string;/, '$1;')
       // fix consumer typing for ScrollToPosition prop with string type
       .replace(/(export declare type ScrollToPosition = .*?) \| string;/, '$1;');
