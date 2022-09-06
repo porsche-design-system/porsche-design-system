073e526747004f138b111da012d24c9017b3ed21
Anton Schmidt <anton.schmidt@mhp.com>
2022-09-06 17:00:25 +0200

    test(components-js): add tests for all components | as | #1898
    Content wrapper, gird, headline, popover and select-wrapper seem to not work yet

---
+++ packages/components-js/tests/vrt/specs/popover.cb-vrt.ts
@@ -0,0 +1,6 @@
+import { cbVRT, openPopoversAndHighlightSpacerPlaywright } from '../../vrt/helpers/cb-vrt-helper';
+import { test } from '@playwright/test';
+
+test.describe('should have no visual regression', async () => {
+  await cbVRT('popover', { scenario: (page) => openPopoversAndHighlightSpacerPlaywright(page) });
+});
