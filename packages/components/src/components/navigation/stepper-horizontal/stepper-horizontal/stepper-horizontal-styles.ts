8f4d9a84be782c59948a7a5f6a31601cad7b567e
Bernd Hacker <hacker.bernd@gmail.com>
2022-09-06 14:20:21 +0200

    fix(components): margin of stepper-horizontal-item and icon width in safari | bh | #2001

--- packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-styles.ts
+++ packages/components/src/components/navigation/stepper-horizontal/stepper-horizontal/stepper-horizontal-styles.ts
@@ -1,5 +1,4 @@
 import { buildResponsiveStyles, getCss } from '../../../../utils';
-import { addImportantToRule } from '../../../../styles';
 import type { StepperHorizontalSize } from './stepper-horizontal-utils';
 import type { BreakpointCustomizable } from '../../../../types';
 import { fontSize, textSmall } from '@porsche-design-system/utilities-v2';
@@ -9,9 +8,6 @@ export const getComponentCss = (size: BreakpointCustomizable<StepperHorizontalSi
     '@global': {
       ':host': {
         display: 'block',
-      },
-      '::slotted(*:not(:last-child))': {
-        marginRight: addImportantToRule('1em'),
       },
     },
     scroller: {
