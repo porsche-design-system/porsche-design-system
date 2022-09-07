c57d86d62b990e800b30ca3c8f221534b79d28c5
Anton Schmidt <anton.schmidt@mhp.com>
2022-09-07 11:51:09 +0200

    test(components-js): add select wrapper cb vrt fixture, it uses native dropdown since touch is enabled | as | #1898

--- packages/components-js/tests/vrt/specs/select-wrapper.cb-vrt.ts
+++ packages/components-js/tests/vrt/specs/select-wrapper.cb-vrt.ts
@@ -4,6 +4,8 @@ import { test } from '@playwright/test';
 test.describe('should have no visual regression', async () => {
   await cbVRT('select-wrapper', {
     scenario: async (page) => {
+      // expanded select will be rendered by the system, not the browser,
+      // since touchDevice will be detected and thus will not appear on screenshot
       await page.locator('#open-options').click();
       await page.evaluate(() => (window as any).componentsReady());
     },
