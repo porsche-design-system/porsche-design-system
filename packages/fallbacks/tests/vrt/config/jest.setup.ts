e167a38b87206d20921bee6169ec217f906a6230
Stephan Schröter <stephan.schroeter@mhp.com>
2022-09-12 13:05:49 +0200

    test(fallbacks): update vrt fixtures + results dir | sas | #1898

--- packages/fallbacks/tests/vrt/config/jest.setup.ts
+++ packages/fallbacks/tests/vrt/config/jest.setup.ts
@@ -2,4 +2,6 @@ import { setCustomOptions } from '@porsche-design-system/shared/testing';
 
 setCustomOptions({
   baseUrl: 'http://localhost:4201',
+  fixturesDir: 'tests/vrt/fixtures',
+  resultsDir: 'tests/vrt/results',
 });
