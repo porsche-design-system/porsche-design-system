564c21ef11229d12abe3c33db1d1ea6cffb500cd
Stephan Schröter <stephan.schroeter@mhp.com>
2022-09-12 13:17:20 +0200

    test: update vrt fixtures + results dir | sas | #1898

--- packages/components-js/tests/vrt/puppeteer/config/jest.setup.ts
+++ packages/components-js/tests/vrt/puppeteer/config/jest.setup.ts
@@ -2,6 +2,4 @@ import { setCustomOptions } from '@porsche-design-system/shared/testing';
 
 setCustomOptions({
   baseUrl: 'http://localhost:8575',
-  fixturesDir: 'tests/vrt/puppeteer/fixtures',
-  resultsDir: 'tests/vrt/puppeteer/results',
 });
