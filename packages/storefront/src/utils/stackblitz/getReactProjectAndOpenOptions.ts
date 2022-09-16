e0e6765e87b1bf9c2a4976b2edb438b82aa366e6
Stephan Schröter <stephan.schroeter@mhp.com>
2022-09-16 16:08:20 +0200

    test(storefront): add unit tests for Angular StackBlitz integration supporting local development + issue branches | sas | #2025

--- packages/storefront/src/utils/stackblitz/getReactProjectAndOpenOptions.ts
+++ packages/storefront/src/utils/stackblitz/getReactProjectAndOpenOptions.ts
@@ -1,7 +1,7 @@
 import { devDependencies, dependencies } from '../../../../components-react/package.json';
 import { default as tsconfig } from '../../../../components-react/tsconfig.json';
 import { getSharedImportConstants, getExternalDependencies, removeSharedImport } from './helper';
-import { convertMarkup } from '@/utils';
+import { convertMarkup } from '../../utils/formatting';
 import type {
   DependencyMap,
   GetStackblitzProjectAndOpenOptions,
