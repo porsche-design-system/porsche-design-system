904b051b1f80e1199fa7677ccbf85b4f67709327
Stephan Schröter <stephan.schroeter@mhp.com>
2022-09-19 15:17:06 +0200

    refactor(storefront): use globby instead of glob | sas | #2025

--- packages/storefront/scripts/generateComponentsBundleForStackBlitz.ts
+++ packages/storefront/scripts/generateComponentsBundleForStackBlitz.ts
@@ -1,33 +1,23 @@
-import { glob } from 'glob';
 import fs from 'fs';
+import * as globby from 'globby';
 
 const generateComponentsBundleForStackBlitz = (framework: 'js' | 'angular' | 'react'): void => {
   const targetPath = 'src/lib/porsche-design-system';
   const targetFile = `${targetPath}/components-${framework}.json`;
+  const bundle: { [path: string]: string } = {};
+  const files = globby.sync(`../components-${framework}/dist/components-wrapper/**/*.{js,mjs,ts,json}`);
 
-  glob(
-    `../components-${framework}/dist/components-wrapper/**/*.{js,mjs,ts,json}`,
-    {
-      ignore: [
-        '../**/*/jsdom-polyfill/**/*',
-      ]
-    },
-    (err, files) => {
-      const bundle: { [path: string]: string } = {};
+  for (const file of files) {
+    // Define filename/path as it should be structured in StackBlitz
+    const path = file.replace(new RegExp(`\\.\\.\\/components-${framework}\\/dist\\/components-wrapper`), `@porsche-design-system/components-${framework}`);
+    // Update components-js package imports to local and relative ones since no components-js package will be installed from npm in StackBlitz
+    bundle[path] = `${fs.readFileSync(file, 'utf-8').replace(/(?<!"name": ")@porsche-design-system\/components-js/g, `./${'../'.repeat((file.match(/\//g) || []).length - 3)}components-js`)}`;
+  }
 
-      for (const file of files) {
-        // Define filename/path as it should be structured in StackBlitz
-        const path = file.replace(new RegExp(`\\.\\.\\/components-${framework}\\/dist\\/components-wrapper`), `@porsche-design-system/components-${framework}`);
-        // Update components-js package imports to local and relative ones since no components-js package will be installed from npm in StackBlitz
-        bundle[path] = `${fs.readFileSync(file, 'utf-8').replace(/(?<!"name": ")@porsche-design-system\/components-js/g, `./${'../'.repeat((file.match(/\//g) || []).length - 3)}components-js`)}`;
-      }
+  fs.mkdirSync(targetPath, {recursive: true});
+  fs.writeFileSync(targetFile, JSON.stringify(bundle));
 
-      fs.mkdirSync(targetPath, {recursive: true});
-      fs.writeFileSync(targetFile, JSON.stringify(bundle));
-
-      console.log(`Write @porsche-design-system/components-${framework} bundle for StackBlitz into "${targetFile}"`);
-    }
-  );
+  console.log(`Write @porsche-design-system/components-${framework} bundle for StackBlitz into "${targetFile}"`);
 };
 
 generateComponentsBundleForStackBlitz('js');
