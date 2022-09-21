afc276de132e6a2df56501713fde3935c9071b51
TatjanaGartner <tatjana.gartner@mhp.com>
2022-09-21 09:34:09 +0200

    feat(components): set lineheight on size inherit | tg | #2018

--- packages/components/src/components/action/button-pure/button-pure.tsx
+++ packages/components/src/components/action/button-pure/button-pure.tsx
@@ -10,6 +10,7 @@ import {
   improveButtonHandlingForCustomElement,
   isDisabledOrLoading,
   isSizeInherit,
+  setLineHeightOnSizeInherit,
   TEXT_SIZES,
   TEXT_WEIGHTS,
   THEMES_EXTENDED_ELECTRIC_DARK,
@@ -103,6 +104,8 @@ export class ButtonPure {
 
   private buttonTag: HTMLElement;
   private iconTag: HTMLElement;
+  private labelTag: HTMLElement;
+  private sublineTag: HTMLElement;
 
   private get isDisabledOrLoading(): boolean {
     return isDisabledOrLoading(this.disabled, this.loading);
@@ -150,6 +153,9 @@ export class ButtonPure {
         this.iconTag.style.height = size;
       });
     }
+
+    setLineHeightOnSizeInherit(this.size, this.labelTag);
+    setLineHeightOnSizeInherit(this.size, this.sublineTag);
   }
 
   public render(): JSX.Element {
@@ -186,12 +192,12 @@ export class ButtonPure {
                 aria-hidden="true"
               />
             ))}
-          <span class="label">
+          <span class="label" ref={(el) => (this.labelTag = el)}>
             <slot />
           </span>
         </button>
         {hasSubline && (
-          <div id="subline" class="subline">
+          <div id="subline" class="subline" ref={(el) => (this.sublineTag = el)}>
             <slot name="subline" />
           </div>
         )}
