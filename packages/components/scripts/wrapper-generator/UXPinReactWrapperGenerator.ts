394a4184f210fac70fc508b50a87612c31072bb2
michaelrambeau <mikeair@gmail.com>
2022-09-28 09:53:05 +0900

    Simplify and fix missing spacing props

--- packages/components/scripts/wrapper-generator/UXPinReactWrapperGenerator.ts
+++ packages/components/scripts/wrapper-generator/UXPinReactWrapperGenerator.ts
@@ -145,7 +145,8 @@ export class UXPinReactWrapperGenerator extends ReactWrapperGenerator {
 
     // add spacing props to every component
     const spacings = this.spacingProps.map((x) => `${x}?: Spacing;`).join('\n  ');
-    props = props.replace(/(HTMLAttributes<{}> & {\n)/, `$1  ${spacings}\n`);
+    const HTMLAttributesType = this.generateHTMLAttributesType();
+    props = props.replace(new RegExp(`(${HTMLAttributesType} & {\n)`), `$1  ${spacings}\n`);
 
     return props;
   }
