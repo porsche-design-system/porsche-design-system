640960c3846c6752a95eeecf6174c5457ce3623e
Bernd Hacker <hacker.bernd@gmail.com>
2022-09-29 14:32:04 +0200

    feat(components): simplify modal styles | bh | #2018

--- packages/components/src/components/content/modal/modal-styles.ts
+++ packages/components/src/components/content/modal/modal-styles.ts
@@ -1,16 +1,7 @@
 import type { JssStyle } from 'jss';
 import type { GetJssStyleFunction } from '../../../utils';
 import type { BreakpointCustomizable, BreakpointKey } from '../../../types';
-import {
-  gridSafeZone,
-  mediaQueryMin,
-  fontBehavior,
-  fontFamily,
-  fontStyle,
-  fontVariant,
-  fontWeight,
-  mediaQueryMinMax,
-} from '@porsche-design-system/utilities-v2';
+import { gridSafeZone, mediaQueryMin, fontWeight, textMedium, textLarge } from '@porsche-design-system/utilities-v2';
 import { BREAKPOINTS, buildResponsiveStyles, getCss, mergeDeep, parseJSON, buildSlottedStyles } from '../../../utils';
 import {
   addImportantToEachRule,
@@ -126,21 +117,16 @@ export const getComponentCss = (
         [mediaQueryM]: getSlottedJssStyle(40, hasHeader),
         [mediaQueryXxl]: getSlottedJssStyle(64, hasHeader),
       }),
-      'h1,::slotted([slot=heading])': addImportantToEachRule({
-        font: `${fontStyle} ${fontVariant} ${fontWeight.semiBold} 1.5rem/1.5 ${fontFamily}`,
-        ...fontBehavior,
-        [mediaQueryMinMax('s', 'm')]: {
-          fontSize: '1.5rem',
-          lineHeight: 1.5,
-        },
-
-        [mediaQueryMin('m')]: {
-          fontSize: '2.25rem',
-          lineHeight: 1.3333333333,
-        },
+      h1: {
+        ...textMedium,
+        fontWeight: fontWeight.semiBold,
         margin: 0,
         color: baseColor,
-      }),
+        [mediaQueryMin('m')]: {
+          ...textLarge,
+          fontWeight: fontWeight.semiBold,
+        },
+      },
     },
     root: mergeDeep(
       {
