5ceacad6eac6dfafba95968fdcfd34c11071da5e
Bernd Hacker <hacker.bernd@gmail.com>
2022-09-29 08:47:22 +0200

    test(components): adjust unit test for button-pure and linkn-pure calls of setLineHeightOnSizeInherit() | bh | #2018

--- packages/components/src/components/action/button-pure/button-pure.spec.ts
+++ packages/components/src/components/action/button-pure/button-pure.spec.ts
@@ -39,12 +39,10 @@ describe('componentDidLoad', () => {
     expect(spy).toBeCalledWith(undefined, 'font-size', expect.anything());
   });
 
-  it('should call setLineHeightOnSizeInherit() with correct parameters when size="inherit"', () => {
+  it('should call setLineHeightOnSizeInherit() with correct parameters', () => {
     const spySetLineHeight = jest.spyOn(lineHeightUtils, 'setLineHeightOnSizeInherit');
     const component = new ButtonPure();
     component.host = document.createElement('p-button-pure');
-    component.host.style.fontSize = '48px';
-    component.size = 'inherit';
     component['labelTag'] = document.createElement('span');
     component.componentDidLoad();
 
