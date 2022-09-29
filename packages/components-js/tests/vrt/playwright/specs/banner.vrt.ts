507bf97cdc3b2aac6aa4afaf4bcf756312268f75
Stephan Schröter <stephan.schroeter@mhp.com>
2022-09-29 11:59:54 +0200

    test: fix banner vrt | sas / bh | #2018

--- packages/components-js/tests/vrt/playwright/specs/banner.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/banner.vrt.ts
@@ -2,10 +2,5 @@ import { executeVisualRegressionTest } from '../helpers/playwright-helper';
 import { test } from '@playwright/test';
 
 test.describe('should have no visual regression', async () => {
-  await executeVisualRegressionTest('banner', {
-    scenario: async (page) => {
-      await page.waitForFunction(() => document.querySelector('template') === null);
-      await page.evaluate(() => (window as any).componentsReady());
-    },
-  });
+  await executeVisualRegressionTest('banner');
 });
