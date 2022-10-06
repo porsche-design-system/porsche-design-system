3cac1b73923e81d5453f08e79707eca40ecae876
Bernd Hacker <hacker.bernd@gmail.com>
2022-10-06 14:31:43 +0200

    fix: wrong dockerfile reference in build-and-push-base-image-playwright.sh | bh | #1927

--- docker/node/build-and-push-base-image-playwright.sh
+++ docker/node/build-and-push-base-image-playwright.sh
@@ -6,7 +6,7 @@ TAG=v1.26.0-focal
 
 docker pull $REGISTRY/$IMAGE:latest
 
-docker build -f Dockerfile-node -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
+docker build -f Dockerfile-playwright -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
 
 docker push $REGISTRY/$IMAGE:$TAG
 docker push $REGISTRY/$IMAGE:latest
