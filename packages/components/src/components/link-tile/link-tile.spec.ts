9edbeb43217f835e42d8a2e24564d579778611f8
Matthias Haegele <matthias.haegele@mhp.com>
2022-10-18 09:01:49 +0200

    test(components): extend lifecycle unit test | mh | #1897

--- packages/components/src/components/link-tile/link-tile.spec.ts
+++ packages/components/src/components/link-tile/link-tile.spec.ts
@@ -1,5 +1,6 @@
 import { LinkTile } from './link-tile';
 import * as throwIfInvalidLinkPureUsageUtils from '../../utils/validation/throwIfInvalidLinkPureUsage';
+import * as throwIfAlignTopWithoutCompactUtils from './link-tile-utils';
 
 describe('componentWillLoad', () => {
   it('should call throwIfInvalidLinkPureUsage() with correct parameters', () => {
@@ -11,5 +12,16 @@ describe('componentWillLoad', () => {
 
     component.componentWillLoad();
     expect(spy).toBeCalledWith(component.host, component.href);
+  });
+
+  it('should call throwIfAlignTopWithoutCompact() with correct parameters', () => {
+    const spy = jest.spyOn(throwIfAlignTopWithoutCompactUtils, 'throwIfAlignTopWithoutCompact');
+
+    const component = new LinkTile();
+    component.host = document.createElement('p-link-tile');
+    component.href = '#';
+
+    component.componentWillLoad();
+    expect(spy).toBeCalledWith(component.host, component.align, component.compact);
   });
 });
