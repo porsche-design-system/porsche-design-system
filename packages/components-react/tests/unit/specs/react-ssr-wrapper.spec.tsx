b4d21f6db87a5329284a4bf321e29717574fce91
Bernd Hacker <hacker.bernd@gmail.com>
2022-10-20 15:50:45 +0200

    test(components-react): add default children to react-ssr-wrapper test | bh | #1221

--- packages/components-react/tests/unit/specs/react-ssr-wrapper.spec.tsx
+++ packages/components-react/tests/unit/specs/react-ssr-wrapper.spec.tsx
@@ -2,17 +2,40 @@ import { render } from '@testing-library/react';
 import * as fromComponents from '../../../projects/react-ssr-wrapper/src/lib/components';
 import { PorscheDesignSystemProvider } from '../../../projects/react-ssr-wrapper/src/provider';
 import * as minifyCssUtils from '../../../projects/react-ssr-wrapper/src/minifyCss';
+import { getComponentMeta } from '@porsche-design-system/shared';
+import type { TagName } from '@porsche-design-system/shared';
+import { paramCase } from 'change-case';
 
 it.each(Object.keys(fromComponents))('should render dsr component for %s', async (componentName) => {
   // @ts-ignore
   const Component = fromComponents[componentName];
 
-  // skip minification for snapshots
+  // skip minifyCss for snapshots
   jest.spyOn(minifyCssUtils, 'minifyCss').mockImplementation((css) => css);
+
+  // default children
+  const { requiredChild } = getComponentMeta(paramCase(componentName) as TagName);
+  const [RequiredChildTag, requiredChildAttrs] = requiredChild?.split(' ') || [];
+  const requiredChildProps = requiredChildAttrs
+    ? requiredChildAttrs
+        .split(' ')
+        .map((pair) => pair.split('='))
+        .reduce((res, [key, val]) => ({ ...res, [key]: val }), {})
+    : null;
+  const props = {
+    children: requiredChild ? <RequiredChildTag {...requiredChildProps} /> : 'Some child',
+    // dangerouslySetInnerHTML: {
+    //   __html: requiredChild
+    //     ? requiredChild.startsWith('input')
+    //       ? `<${requiredChild} />`
+    //       : `<${requiredChild}></${requiredChild}>`
+    //     : 'Some child',
+    // },
+  };
 
   const { container } = render(
     <PorscheDesignSystemProvider>
-      <Component />
+      <Component {...props} />
     </PorscheDesignSystemProvider>
   );
 
