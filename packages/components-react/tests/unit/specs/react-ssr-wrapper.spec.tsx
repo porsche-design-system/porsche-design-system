56f086f6221989fbbeedba21273e865433e8f036
Bernd Hacker <hacker.bernd@gmail.com>
2022-10-20 11:38:27 +0200

    test(components-react): mock minifyCss util for generic react-ssr-wrapper snapshots | bh | #1221

--- packages/components-react/tests/unit/specs/react-ssr-wrapper.spec.tsx
+++ packages/components-react/tests/unit/specs/react-ssr-wrapper.spec.tsx
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/react';
 import * as fromComponents from '../../../projects/react-ssr-wrapper/src/lib/components';
 import { PorscheDesignSystemProvider } from '../../../projects/react-ssr-wrapper/src/provider';
+import * as minifyCssUtils from '../../../projects/react-ssr-wrapper/src/minifyCss';
 
 it.each(Object.keys(fromComponents))('should render dsr component for %s', async (componentName) => {
   // @ts-ignore
   const Component = fromComponents[componentName];
+
+  // skip minification for snapshots
+  jest.spyOn(minifyCssUtils, 'minifyCss').mockImplementation((css) => css);
 
   const { container } = render(
     <PorscheDesignSystemProvider>
