c6a6ef026ef13e56f749d9f7cb547580352a0c4d
TatjanaGartner <tatjana.gartner@mhp.com>
2022-10-20 15:45:56 +0200

    feat(components):  add missing describe block | tg / mh | #1897

--- packages/components/src/components/link-tile/link-tile-utils.spec.ts
+++ packages/components/src/components/link-tile/link-tile-utils.spec.ts
@@ -1,23 +1,25 @@
 import { throwIfAlignTopWithoutCompact } from './link-tile-utils';
 
-it('should throw error when used with align top and compact false', () => {
-  const divElement = document.createElement('div');
-  expect(() => throwIfAlignTopWithoutCompact(divElement, 'top', false)).toThrowErrorMatchingInlineSnapshot(
-    '"Usage of div is not valid. Top alignment is only possible when compact is true."'
-  );
-});
+describe('throwIfAlignTopWithoutCompact()', () => {
+  it('should throw error when used with align top and compact false', () => {
+    const divElement = document.createElement('div');
+    expect(() => throwIfAlignTopWithoutCompact(divElement, 'top', false)).toThrowErrorMatchingInlineSnapshot(
+      '"Usage of div is not valid. Top alignment is only possible when compact is true."'
+    );
+  });
 
-it('should not throw error when used with align top and compact true', () => {
-  const divElement = document.createElement('div');
-  expect(() => throwIfAlignTopWithoutCompact(divElement, 'top', true)).not.toThrow();
-});
+  it('should not throw error when used with align top and compact true', () => {
+    const divElement = document.createElement('div');
+    expect(() => throwIfAlignTopWithoutCompact(divElement, 'top', true)).not.toThrow();
+  });
 
-it('should not throw error when used with align bottom and compact true', () => {
-  const divElement = document.createElement('div');
-  expect(() => throwIfAlignTopWithoutCompact(divElement, 'bottom', true)).not.toThrow();
-});
+  it('should not throw error when used with align bottom and compact true', () => {
+    const divElement = document.createElement('div');
+    expect(() => throwIfAlignTopWithoutCompact(divElement, 'bottom', true)).not.toThrow();
+  });
 
-it('should not throw error when used with align bottom and compact false', () => {
-  const divElement = document.createElement('div');
-  expect(() => throwIfAlignTopWithoutCompact(divElement, 'bottom', false)).not.toThrow();
+  it('should not throw error when used with align bottom and compact false', () => {
+    const divElement = document.createElement('div');
+    expect(() => throwIfAlignTopWithoutCompact(divElement, 'bottom', false)).not.toThrow();
+  });
 });
