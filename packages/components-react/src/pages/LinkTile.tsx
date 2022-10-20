4600c1ea9489b6c14e5f2430c1a115b7735472d0
TatjanaGartner <tatjana.gartner@mhp.com>
2022-10-20 11:36:47 +0200

    feat(components-js/-angular/-react): add alt tag to <img /> tag and add images to components-react and components-angular | tg | #1897

--- packages/components-react/src/pages/LinkTile.tsx
+++ packages/components-react/src/pages/LinkTile.tsx
@@ -22,13 +22,13 @@ export const LinkTilePage = (): JSX.Element => {
 
       <div className="playground light container" title="should render default link-tile on light background">
         <PLinkTile href="#" label="Some Label" description="Default">
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
       </div>
 
       <div className="playground light grid" style={{ fontSize: '24px' }} title="should render different sizes on light background">
         <PLinkTile href="#" label="Some label" description="Size inherit" size="inherit">
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
         <PLinkTile
           href="#"
@@ -36,13 +36,13 @@ export const LinkTilePage = (): JSX.Element => {
           description="Size breakpoint"
           size={{ base: 'default', xs: 'inherit', s: 'default', m: 'inherit', l: 'default', xl: 'inherit' }}
         >
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
       </div>
 
       <div className="playground light grid" title="should render different weights on light background">
         <PLinkTile href="#" label="Some label" description="Weight regular" weight="regular">
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
         <PLinkTile
           href="#"
@@ -50,22 +50,22 @@ export const LinkTilePage = (): JSX.Element => {
           description="Weight breakpoint"
           weight={{ base: 'regular', xs: 'semibold', s: 'regular', m: 'semibold', l: 'regular', xl: 'semibold' }}
         >
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
       </div>
 
       <div className="playground light grid" title="should render different aspect ratios on light background">
         <PLinkTile href="#" label="Some label" description="Aspect ratio 16:9" aspectRatio="16:9">
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
         <PLinkTile label="Some label" description="Aspect ratio 1:1" href="#" aspectRatio="1:1">
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
         <PLinkTile href="#" label="Some label" description="Aspect ratio 3:4" aspectRatio="3:4">
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
         <PLinkTile href="#" label="Some label" description="Aspect ratio 9:16" aspectRatio="9:16">
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
         <PLinkTile
           href="#"
@@ -73,25 +73,25 @@ export const LinkTilePage = (): JSX.Element => {
           description="Aspect ratio breakpoint"
           aspectRatio={{ base: '16:9', xs: '1:1', s: '3:4', m: '9:16', l: '4:3', xl: '16:9'}}
         >
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
       </div>
 
       <div className="playground light container" title="should render compact on light background">
         <PLinkTile href="#" label="Some label" description="Compact" compact={true}>
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
       </div>
 
       <div className="playground light container" title="should render align top on light background">
         <PLinkTile href="#" label="Some label" description="Align top" align="top" compact={true}>
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
       </div>
 
       <div className="playground light container" title="should render without gradient">
         <PLinkTile href="#" label="Some label" description="Gradient false" gradient={false}>
-          <img src="./assets/porsche_beach.jpg" />
+          <img src="../assets/porsche_beach.jpg" alt="Beach" />
         </PLinkTile>
       </div>
 
@@ -101,7 +101,7 @@ export const LinkTilePage = (): JSX.Element => {
           label="Some label"
           description="Some long text on white background. Some long text on white background. Some long text on white background. Some long text on white background."
         >
-          <img />
+          <img alt={} />
         </PLinkTile>
         <PLinkTile
           href="#"
@@ -109,14 +109,14 @@ export const LinkTilePage = (): JSX.Element => {
           description="Some long text on white background. Some long text on white background. Some long text on white background. Some long text on white background."
           compact={true}
         >
-          <img />
+          <img alt={} />
         </PLinkTile>
         <PLinkTile
           href="#"
           label="Some label"
           description="Some long text on office background. Some long text on office background. Some long text on office background. Some long text on office background."
         >
-          <img src="./assets/porsche_office.jpg" />
+          <img src="../assets/porsche_office.jpg" alt="Office" />
         </PLinkTile>
         <PLinkTile
           href="#"
@@ -124,7 +124,7 @@ export const LinkTilePage = (): JSX.Element => {
           description="Some long text on office background. Some long text on office background. Some long text on office background. Some long text on office background."
           compact={true}
         >
-          <img src="./assets/porsche_office.jpg" />
+          <img src="../assets/porsche_office.jpg" alt="Office" />
         </PLinkTile>
       </div>
 
@@ -136,7 +136,7 @@ export const LinkTilePage = (): JSX.Element => {
           <picture>
             <source media="(min-width: 760px)" srcSet={"./assets/porsche_beach.jpg"} />
             <source media="(min-width: 1000px)" srcSet={"./assets/porsche_office.jpg"} />
-            <img src="./assets/porsche_office.jpg" alt="Office" />
+            <img src="../assets/porsche_office.jpg" alt="Office" />
           </picture>
         </PLinkTile>
       </div>
