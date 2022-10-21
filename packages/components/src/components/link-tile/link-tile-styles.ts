3f17ae68f8a65cfe15ec9792bbdebc306bfeaa96
Matthias Haegele <matthias.haegele@mhp.com>
2022-10-21 09:05:44 +0200

    feat(components): implement review comments | mh | #1897

--- packages/components/src/components/link-tile/link-tile-styles.ts
+++ packages/components/src/components/link-tile/link-tile-styles.ts
@@ -5,34 +5,26 @@ import { hoverMediaQuery } from '../../styles/hover-media-query';
 import type { BreakpointCustomizable } from '../../types';
 import type { LinkTileAspectRatio, LinkTileAlign, LinkTileWeight, LinkTileSize } from './link-tile-utils';
 import { buildResponsiveStyles, buildSlottedStyles, getCss, mergeDeep } from '../../utils';
-import {
-  fontBehavior,
-  fontFamily,
-  fontHyphenation,
-  fontStyle,
-  fontVariant,
-  fontWeight,
-  mediaQueryMin,
-} from '@porsche-design-system/utilities-v2';
+import { mediaQueryMin, textSmall } from '@porsche-design-system/utilities-v2';
 import type { JssStyle } from 'jss';
 
-const aspectRatioPaddingTop: { [key in LinkTileAspectRatio]: { paddingTop: string } } = {
-  '1:1': { paddingTop: '100%' },
-  '4:3': { paddingTop: '75%' },
-  '3:4': { paddingTop: '133.33%' },
-  '16:9': { paddingTop: '56.25%' },
-  '9:16': { paddingTop: '177.75%' },
+const aspectRatioPaddingTop: { [key in LinkTileAspectRatio]: string } = {
+  '1:1': '100%',
+  '4:3': '75%',
+  '3:4': '133.33%',
+  '16:9': '56.25%',
+  '9:16': '177.75%',
 };
 
 const getGradientBackground = (isCompact: boolean, isTopAligned: boolean): string => {
   const gradient =
-    'rgba(31, 31, 31, 0.9) 0%, ' +
-    'rgba(31, 31, 31, 0.9) 20%, ' +
-    'rgba(31, 31, 31, 0.852589) 26.67%, ' +
-    'rgba(32, 32, 32, 0.768225) 33.33%, ' +
-    'rgba(33, 33, 33, 0.668116) 40%, ' +
-    'rgba(34, 34, 34, 0.557309) 46.67%, ' +
-    'rgba(35, 35, 35, 0.442691) 53.33% ,' +
+    'rgba(31, 31, 31, 0.9) 0%,' +
+    'rgba(31, 31, 31, 0.9) 20%,' +
+    'rgba(31, 31, 31, 0.852589) 26.67%,' +
+    'rgba(32, 32, 32, 0.768225) 33.33%,' +
+    'rgba(33, 33, 33, 0.668116) 40%,' +
+    'rgba(34, 34, 34, 0.557309) 46.67%,' +
+    'rgba(35, 35, 35, 0.442691) 53.33%,' +
     'rgba(36, 36, 36, 0.331884) 60%,' +
     'rgba(37, 37, 37, 0.231775) 66.67%,' +
     'rgba(38, 38, 38, 0.147411) 73.33%,' +
@@ -46,7 +38,10 @@ const getGradientBackground = (isCompact: boolean, isTopAligned: boolean): strin
     : `linear-gradient(to top, ${gradient} 100%);`;
 };
 
-const sizeMap = {
+const sizeMap: {
+  inherit: { lineHeight: string; fontSize: string };
+  default: { lineHeight: number; fontSize: string };
+} = {
   inherit: {
     lineHeight: 'inherit',
     fontSize: 'inherit',
@@ -81,9 +76,7 @@ export const getComponentCss = (
       },
       p: {
         color: getThemedTextColor('dark', 'default'),
-        font: `${fontStyle} ${fontVariant} ${fontWeight.semiBold} 1.25rem/1.5555555556 ${fontFamily}`,
-        ...fontBehavior,
-        ...fontHyphenation,
+        ...textSmall,
         ...mergeDeep(
           buildResponsiveStyles(size, (s: LinkTileSize) => sizeMap[s]),
           buildResponsiveStyles(weight, (w: LinkTileWeight) => ({ fontWeight: getFontWeight(w) }))
@@ -94,7 +87,9 @@ export const getComponentCss = (
     },
     root: {
       height: 0,
-      ...buildResponsiveStyles(aspectRatio, (ratio: LinkTileAspectRatio) => aspectRatioPaddingTop[ratio]),
+      ...buildResponsiveStyles(aspectRatio, (ratio: LinkTileAspectRatio) => ({
+        paddingTop: aspectRatioPaddingTop[ratio],
+      })),
       position: 'relative',
       transform: 'translate3d(0,0,0)', // Change stacking context for position fixed
       ...hoverMediaQuery({
