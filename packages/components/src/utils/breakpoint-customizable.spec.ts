8ceaee194448891af24015f4412f149ddfcc6a36
TatjanaGartner <tatjana.gartner@mhp.com>
2022-10-21 15:16:11 +0200

    feat(components):  remove "\" from character set and add unit test for  parseJSON() | tg | #1897

---
+++ packages/components/src/utils/breakpoint-customizable.spec.ts
@@ -0,0 +1,26 @@
+import { parseJSON } from './breakpoint-customizable';
+
+describe('parseJSON()', () => {
+  it.each<string>([
+    '{ base: true, s: false }',
+    '{ "m": "right", s: "left" }',
+    " { base: 'column', s: 'row' }",
+    '{ base: "small", l: "medium", "xl": "large" } ',
+    " { l: 'inherit', s: 'default' }",
+    " {xl: 'initial', l: 'equal'} ",
+    '{ base: 36, xs: 6, s: 4, m: 16, l:2, "xl":16 }',
+    '{xs: "inherit", m: "default"}',
+    '{ xs: "1:1", s: "3:4", m: "4:3", l: "9:16", xl: "16:9" }',
+  ])('should return parsed object for %s', (input) => {
+    expect(parseJSON(input)).toBeTruthy();
+  });
+  it('should return not formatted string for boolean type', () => {
+    expect(parseJSON(false)).toEqual(false);
+  });
+  it('should return not formatted string for number type', () => {
+    expect(parseJSON(1)).toEqual(1);
+  });
+  it('should return not formatted string for object type', () => {
+    expect(parseJSON({ base: 'small' })).toEqual({ base: 'small' });
+  });
+});
