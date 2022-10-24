ec3ba5bf69ba2012343eeb0351249355f0ca901d
Bernd Hacker <hacker.bernd@gmail.com>
2022-10-24 11:09:50 +0200

    test(components-react): fix core-class-names e2e test | bh

--- packages/components-react/tests/e2e/specs/core-class-names.e2e.ts
+++ packages/components-react/tests/e2e/specs/core-class-names.e2e.ts
@@ -21,7 +21,7 @@ it('should map className to class initially', async () => {
   await goto(page, 'core-class-names');
 
   const button = await getButton1();
-  expect(await getClassName(button)).toBe(`${SOME_CLASS_1} ${SOME_CLASS_2}${HYDRATED_CLASS}`);
+  expect(await getClassName(button)).toBe(`${SOME_CLASS_1} ${SOME_CLASS_2} ${HYDRATED_CLASS}`);
 });
 
 it('should keep hydrated class on rerender with className change', async () => {
@@ -45,11 +45,11 @@ it('should keep added class on rerender with className change', async () => {
     el.classList.add(addedClass);
   }, addedClass);
 
-  expect(await getClassName(button)).toBe(`${SOME_CLASS_1} ${SOME_CLASS_2}${HYDRATED_CLASS} ${addedClass}`);
+  expect(await getClassName(button)).toBe(`${SOME_CLASS_1} ${SOME_CLASS_2} ${HYDRATED_CLASS} ${addedClass}`);
 
   await button.click();
 
-  expect(await getClassName(button)).toBe(`${SOME_CLASS_1}${HYDRATED_CLASS} ${addedClass}`);
+  expect(await getClassName(button)).toBe(`${SOME_CLASS_1} ${HYDRATED_CLASS} ${addedClass}`);
 });
 
 it('should keep other classes if one is removed', async () => {
@@ -57,11 +57,11 @@ it('should keep other classes if one is removed', async () => {
 
   const button = await getButton1();
 
-  expect(await getClassName(button)).toBe(`${SOME_CLASS_1} ${SOME_CLASS_2}${HYDRATED_CLASS}`);
+  expect(await getClassName(button)).toBe(`${SOME_CLASS_1} ${SOME_CLASS_2} ${HYDRATED_CLASS}`);
 
   await button.click();
 
-  expect(await getClassName(button)).toBe(`${SOME_CLASS_1}${HYDRATED_CLASS}`);
+  expect(await getClassName(button)).toBe(`${SOME_CLASS_1} ${HYDRATED_CLASS}`);
 });
 
 it('should not interfere with classNames of another PButton', async () => {
@@ -72,11 +72,11 @@ it('should not interfere with classNames of another PButton', async () => {
 
   await button1.click();
 
-  expect(await getClassName(button1), 'button 1 after button 1 click').toBe(`${SOME_CLASS_1}${HYDRATED_CLASS}`);
+  expect(await getClassName(button1), 'button 1 after button 1 click').toBe(`${SOME_CLASS_1} ${HYDRATED_CLASS}`);
 
   await button2.click();
 
-  expect(await getClassName(button1), 'button 1 after button 2 click').toBe(`${SOME_CLASS_1}${HYDRATED_CLASS}`);
+  expect(await getClassName(button1), 'button 1 after button 2 click').toBe(`${SOME_CLASS_1} ${HYDRATED_CLASS}`);
 
-  expect(await getClassName(button2), 'button 2 after button 2 click').toBe(`${SOME_CLASS_1}${HYDRATED_CLASS}`);
+  expect(await getClassName(button2), 'button 2 after button 2 click').toBe(`${SOME_CLASS_1} ${HYDRATED_CLASS}`);
 });
