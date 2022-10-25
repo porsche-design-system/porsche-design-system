8ba29fa8ec51f0d4cbfb62aa7210c613bb4bd75b
Matthias Haegele <matthias.haegele@mhp.com>
2022-10-25 14:15:22 +0200

    feat(components-react): test default values for dummy | mh | #1897

--- packages/components-react/projects/uxpin-wrapper/src/dummy/DummyImg.tsx
+++ packages/components-react/projects/uxpin-wrapper/src/dummy/DummyImg.tsx
@@ -4,5 +4,10 @@ export type DummyImgProps = {
 };
 
 export const DummyImg = (props: DummyImgProps): JSX.Element => {
-  return <img {...props} />;
+  const {
+    src = 'https://files.porsche.com/filestore/image/multimedia/none/992-gt3-modelimage-sideshot/model/765dfc51-51bc-11eb-80d1-005056bbdc38/porsche-model.png',
+    alt = '',
+  } = props;
+
+  return <img src={src} alt={alt} />;
 };
