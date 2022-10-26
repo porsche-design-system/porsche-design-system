971f5ba458619929580cc84e2dfe4e9f7a3880f2
Matthias Haegele <matthias.haegele@mhp.com>
2022-10-26 08:46:41 +0200

    feat(uxpin): extend dummy img with inline style prop | mh | #1897

--- packages/components-react/projects/uxpin-wrapper/src/dummy/DummyImg.tsx
+++ packages/components-react/projects/uxpin-wrapper/src/dummy/DummyImg.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useRef } from 'react';
+
 export type DummyImgProps = {
   src?: string;
   alt?: string;
+  /**
+   * @uxpincontroltype textfield(4)
+   */
+  inlineStyle?: string;
 };
 
 export const DummyImg = (props: DummyImgProps): JSX.Element => {
+  const elementRef = useRef<HTMLImageElement>();
+
   const {
     src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyAQMAAAAk8RryAAAABlBMVEUAAAD2vP9xXLiUAAAAAXRSTlMAQObYZgAAABxJREFUGNNjYOBgYGBhYKAZ/R8MDsD4Q5amkz8ASp4PtTYYQZIAAAAASUVORK5CYII=',
     alt = 'Some image text',
+    inlineStyle,
   } = props;
+
+  useEffect(() => {
+    // fighting the framework
+    elementRef.current.setAttribute('style', inlineStyle);
+  }, [inlineStyle]);
 
   return <img src={src} alt={alt} />;
 };
