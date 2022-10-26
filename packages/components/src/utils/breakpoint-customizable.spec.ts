7d66b09a15fe4b9daa096a52e60ec46de99ed91a
Matthias Haegele <matthias.haegele@mhp.com>
2022-10-26 10:57:32 +0200

    test(components): update breakpoint-customizable.spec | mh / tg | #1897

--- packages/components/src/utils/breakpoint-customizable.spec.ts
+++ packages/components/src/utils/breakpoint-customizable.spec.ts
@@ -2,20 +2,14 @@ import { parseJSON } from './breakpoint-customizable';
 
 describe('parseJSON()', () => {
   it.each<string>([
-    '{ base: true, s: false }',
-    '{ "m": "right", s: "left" }',
-    '{ base: "small", l: "medium", "xl": "large" } ',
-    '{ base: 36, xs: 6, s: 4, m: 16, l:2, "xl":16 }',
-    '{xs: "inherit", m: "default"}',
-    ' { xs: "1:1", s: "3:4", m: "4:3", l: "9:16", xl: "16:9" }',
-    "{ base: 'column', s: 'row' }",
-    "{ l: 'inherit', s: 'default' }",
-    " {xl: 'initial', l: 'equal'} ",
+    '{base: true, s: false}',
+    "{base: 'initial', l: 'equal'}",
+    '{base: "initial", l: "equal"}',
+    '{"base": "right", "s": "left"}',
+    '{base: 36, xs: 6, s: 4, m: 16, l: 2}',
+    '{xs: "1:1", s: "3:4", m: "4:3", l: "9:16", xl: "16:9"}',
   ])('should return parsed object for %s', (input) => {
     expect(parseJSON(input)).toBeTruthy();
-  });
-  it('should convert single quotes to double quotes', () => {
-    expect(parseJSON(" { base: 'column', s: 'row' }")).toEqual({ base: 'column', s: 'row' });
   });
   it('should return not formatted string for boolean type', () => {
     expect(parseJSON(false)).toEqual(false);
