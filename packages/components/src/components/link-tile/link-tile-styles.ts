1d2b715c328814092a548cd9f2dd7100c41b0238
TatjanaGartner <tatjana.gartner@mhp.com>
2022-10-26 11:51:54 +0200

    feat(components): use record type for aspect ratio padding, remove whitespace within rgba | tg | #1897

--- packages/components/src/components/link-tile/link-tile-styles.ts
+++ packages/components/src/components/link-tile/link-tile-styles.ts
@@ -14,7 +14,7 @@ import { buildResponsiveStyles, buildSlottedStyles, getCss, mergeDeep } from '..
 import { mediaQueryMin, textSmall } from '@porsche-design-system/utilities-v2';
 import type { JssStyle } from 'jss';
 
-const aspectRatioPaddingTop: { [key in LinkTileAspectRatio]: string } = {
+const aspectRatioPaddingTop: Record<LinkTileAspectRatio, string> = {
   '1:1': '100%',
   '4:3': '75%',
   '3:4': '133.33%',
@@ -24,20 +24,20 @@ const aspectRatioPaddingTop: { [key in LinkTileAspectRatio]: string } = {
 
 const getGradientBackground = (isCompact: boolean, isTopAligned: boolean): string => {
   const gradient =
-    'rgba(31, 31, 31, 0.9) 0%,' +
-    'rgba(31, 31, 31, 0.9) 20%,' +
-    'rgba(31, 31, 31, 0.852589) 26.67%,' +
-    'rgba(32, 32, 32, 0.768225) 33.33%,' +
-    'rgba(33, 33, 33, 0.668116) 40%,' +
-    'rgba(34, 34, 34, 0.557309) 46.67%,' +
-    'rgba(35, 35, 35, 0.442691) 53.33%,' +
-    'rgba(36, 36, 36, 0.331884) 60%,' +
-    'rgba(37, 37, 37, 0.231775) 66.67%,' +
-    'rgba(38, 38, 38, 0.147411) 73.33%,' +
-    'rgba(39, 39, 39, 0.0816599) 80%,' +
-    'rgba(39, 39, 39, 0.03551) 86.67%,' +
-    'rgba(39, 39, 39, 0.0086472) 93.33%,' +
-    'rgba(39, 39, 39, 0)';
+    'rgba(31,31,31,0.9) 0%,' +
+    'rgba(31,31,31,0.9) 20%,' +
+    'rgba(31,31,31,0.852589) 26.67%,' +
+    'rgba(32,32,32,0.768225) 33.33%,' +
+    'rgba(33,33,33,0.668116) 40%,' +
+    'rgba(34,34,34,0.557309) 46.67%,' +
+    'rgba(35,35,35,0.442691) 53.33%,' +
+    'rgba(36,36,36,0.331884) 60%,' +
+    'rgba(37,37,37,0.231775) 66.67%,' +
+    'rgba(38,38,38,0.147411) 73.33%,' +
+    'rgba(39,39,39,0.0816599) 80%,' +
+    'rgba(39,39,39,0.03551) 86.67%,' +
+    'rgba(39,39,39,0.0086472) 93.33%,' +
+    'rgba(39,39,39,0)';
 
   return isCompact && isTopAligned
     ? `linear-gradient(${gradient} 100%);`
@@ -52,6 +52,7 @@ const sizeMap: {
     lineHeight: 'inherit',
     fontSize: 'inherit',
   },
+  // these values are calculated and no util exists
   default: { fontSize: '1.25rem', lineHeight: 1.5555555556 },
 };
 
