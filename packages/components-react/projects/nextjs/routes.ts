5a556ae1ef28cd86887bee1fc2720f2d31acad41
Bernd Hacker <hacker.bernd@gmail.com>
2022-10-26 10:02:02 +0200

    feat(components-react/nextjs): add table vrt page | bh | #1221

--- packages/components-react/projects/nextjs/routes.ts
+++ packages/components-react/projects/nextjs/routes.ts
@@ -2,7 +2,8 @@ import { generatedRoutes } from './pages';
 
 export const sitemap = {
   home: { path: '/', name: 'Home' },
+  table: { path: '/table', name: 'Table' },
   ...generatedRoutes,
 };
 
-export const routes = Object.values(sitemap);
+export const routes = Object.values(sitemap).sort((a, b) => a.name.localeCompare(b.name));
