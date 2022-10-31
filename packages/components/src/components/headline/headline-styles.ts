d4073ed6bed74a617abdffcdc1581d1f22b99a6c
Stephan Schröter <stephan.schroeter@mhp.com>
2022-10-31 11:38:10 +0100

    feat(components): improve ssr support for headline when using variant="inherit" | sas | #1221

--- packages/components/src/components/headline/headline-styles.ts
+++ packages/components/src/components/headline/headline-styles.ts
@@ -32,7 +32,7 @@ const getSizeJssStyle = (textSize: TextSize): JssStyle => {
   const { semiBold: fontWeightSemiBold } = fontWeight;
   return textSize === 'inherit'
     ? {
-        lineHeight: textSize,
+        lineHeight: 1.3333333333, // use some default line-height to improve SSR support, which will be overwritten after hydration
         fontSize: textSize,
         fontWeight: fontWeightSemiBold,
       }
