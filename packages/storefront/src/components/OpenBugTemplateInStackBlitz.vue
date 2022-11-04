cc3cd2181a45f0a109827e2b261dcc84a21c78eb
TatjanaGartner <tatjana.gartner@mhp.com>
2022-11-04 10:07:26 +0100

    feat(storefront): wrap p-text and p-segmented-control within grid | tg / mh | #1952

--- packages/storefront/src/components/OpenBugTemplateInStackBlitz.vue
+++ packages/storefront/src/components/OpenBugTemplateInStackBlitz.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
-    <p-text>Choose your Framework:</p-text>
-    <p-segmented-control :value="selectedFramework" v-model="selectedFramework" aria-label="Choose your Framework:">
-      <p-segmented-control-item v-for="(framework, index) in frameworks" :key="index" :value="framework">{{
-        frameworkNameMap[framework]
-      }}</p-segmented-control-item>
-    </p-segmented-control>
+    <div>
+      <p-text>Choose your Framework:</p-text>
+      <p-segmented-control :value="selectedFramework" v-model="selectedFramework" aria-label="Choose your Framework:">
+        <p-segmented-control-item v-for="(framework, index) in frameworks" :key="index" :value="framework">{{
+          frameworkNameMap[framework]
+        }}</p-segmented-control-item>
+      </p-segmented-control>
+    </div>
     <p-select-wrapper label="Choose your Porsche Design System version:">
       <select v-model="selectedPdsVersion" name="some-name">
         <option v-for="(pdsVersion, index) in pdsVersions" :key="index" :value="pdsVersion">{{ pdsVersion }}</option>
@@ -58,8 +60,8 @@
     gap: 1rem;
     margin-top: 1rem;
   }
-  p-segmented-control {
-    margin-top: -0.75rem;
+  p-text {
+    margin-bottom: 0.25rem;
   }
   p-select-wrapper {
     justify-self: start;
