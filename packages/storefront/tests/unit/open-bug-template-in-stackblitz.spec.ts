7894b88ceb8905b19ca7c6e55968414fc90e7de7
TatjanaGartner <tatjana.gartner@mhp.com>
2022-11-07 16:25:58 +0100

    feat(storefront): add describe block including the component name | tg | #1952

--- packages/storefront/tests/unit/open-bug-template-in-stackblitz.spec.ts
+++ packages/storefront/tests/unit/open-bug-template-in-stackblitz.spec.ts
@@ -1,28 +1,30 @@
 import OpenBugTemplateInStackBlitz from '@/components/OpenBugTemplateInStackBlitz.vue';
 import { json } from 'stream/consumers';
 
-describe('this.fetchVersions()', () => {
-  it('should call global fetch() with correct parameters', async () => {
-    const url = 'https://registry.npmjs.org/@porsche-design-system/components-js';
-    const fetchSpy = jest.spyOn(global, 'fetch');
-    const component: any = new OpenBugTemplateInStackBlitz();
+describe('OpenBugTemplateInStackBlitz.vue', () => {
+  describe('this.fetchVersions()', () => {
+    it('should call global fetch() with correct parameters', async () => {
+      const url = 'https://registry.npmjs.org/@porsche-design-system/components-js';
+      const fetchSpy = jest.spyOn(global, 'fetch');
+      const component: any = new OpenBugTemplateInStackBlitz();
 
-    const result = await component['fetchVersions']();
+      const result = await component['fetchVersions']();
 
-    expect(fetchSpy).toBeCalledWith(url, { headers: { accept: 'application/vnd.npm.install-v1+json' } });
+      expect(fetchSpy).toBeCalledWith(url, { headers: { accept: 'application/vnd.npm.install-v1+json' } });
 
-    expect(result.length).toBeGreaterThan(0);
-    expect(result).toContain('2.17.0');
+      expect(result.length).toBeGreaterThan(0);
+      expect(result).toContain('2.17.0');
+    });
   });
-});
 
-describe('this.getFilteredVersions()', () => {
-  it('should return filtered versions', async () => {
-    const component: any = new OpenBugTemplateInStackBlitz();
-    expect(component['getFilteredVersions'](['1.2.3-rc.1', '1.2.3', '1.2.4', '1.2.5', '1.2.3-beta.0'])).toEqual([
-      '1.2.3',
-      '1.2.4',
-      '1.2.5',
-    ]);
+  describe('this.getFilteredVersions()', () => {
+    it('should return filtered versions', async () => {
+      const component: any = new OpenBugTemplateInStackBlitz();
+      expect(component['getFilteredVersions'](['1.2.3-rc.1', '1.2.3', '1.2.4', '1.2.5', '1.2.3-beta.0'])).toEqual([
+        '1.2.3',
+        '1.2.4',
+        '1.2.5',
+      ]);
+    });
   });
 });
