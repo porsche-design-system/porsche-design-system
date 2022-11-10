3e909698f25de878c3b66301661bb4f6c2089b42
Matthias Haegele <matthias.haegele@mhp.com>
2022-11-10 09:48:48 +0100

    feat(components): add validation for core chunk | mh / tg | #1976

--- packages/components/src/utils/validation/validatePartialUsage.ts
+++ packages/components/src/utils/validation/validatePartialUsage.ts
@@ -1,13 +1,27 @@
 export const validatePartialUsage = (): void => {
   const { head } = document;
 
+  // getFontLinks la-regular
   if (
-    !Array.from(head.querySelectorAll('link')).find((el) =>
+    !Array.from(head.querySelectorAll('link[rel="preload"][as="font"]')).find((el) =>
       el.getAttribute('href').includes('porsche-next-w-la-regular')
     )
   ) {
     console.warn(
       `Please make sure to apply the 'getInitialFontLinks()' partial as described at https://designsystem.porsche.com/v2/partials/font-links`
+    );
+  }
+
+  // getChunkLinks coreChunk check initial
+  if (
+    !Array.from(head.querySelectorAll('link[rel="preload"][as="script"][crossorigin]')).find((el) =>
+      /cdn\.ui\.porsche\.c(om|n)\/porsche-design-system\/components\/porsche-design-system\.v(?:\d+\.){3}[\da-z]+\.js/.test(
+        el.getAttribute('href')
+      )
+    )
+  ) {
+    console.warn(
+      `Please make sure to apply the 'getChunkLinks()' partial as described at https://designsystem.porsche.com/v2/partials/font-links`
     );
   }
 };
