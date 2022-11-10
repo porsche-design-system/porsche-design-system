1edd88760c8a7e897469544328d89a5c05286dba
Matthias Haegele <matthias.haegele@mhp.com>
2022-11-10 10:26:44 +0100

    feat(components): split into validation functions / split console.warn | mh / tg | #1976

--- packages/components/src/utils/validation/validatePartialUsage.ts
+++ packages/components/src/utils/validation/validatePartialUsage.ts
@@ -1,33 +1,55 @@
 export const validatePartialUsage = (): void => {
-  const { head } = document;
+  const { head, body } = document;
 
-  // getFontLinks la-regular
+  validateGetFontLinksUsage(head);
+  validateGetComponentChunkLinksUsage(head);
+  validateGetLoaderScriptUsage(body);
+};
+
+const validateGetFontLinksUsage = (documentHead: HTMLElement): void => {
   if (
-    !Array.from(head.querySelectorAll('link[rel="preload"][as="font"]')).find((el) =>
+    !Array.from(documentHead.querySelectorAll('link[rel="preload"][as="font"]')).find((el) =>
       el.getAttribute('href').includes('porsche-next-w-la-regular')
     )
   ) {
-    console.warn(
-      `Please make sure to apply the 'getInitialFontLinks()' partial as described at https://designsystem.porsche.com/v2/partials/font-links`
-    );
+    partialValidationWarning('getFontLink');
   }
+};
 
-  // getChunkLinks coreChunk check initial
+const validateGetComponentChunkLinksUsage = (documentHead: HTMLElement): void => {
   if (
-    !Array.from(head.querySelectorAll('link[rel="preload"][as="script"][crossorigin]')).find((el) =>
+    !Array.from(documentHead.querySelectorAll('link[rel="preload"][as="script"][crossorigin]')).find((el) =>
       /cdn\.ui\.porsche\.c(om|n)\/porsche-design-system\/components\/porsche-design-system\.v(?:\d+\.){3}[\da-z]+\.js/.test(
         el.getAttribute('href')
       )
     )
   ) {
-    console.warn(
-      `Please make sure to apply the 'getChunkLinks()' partial as described at https://designsystem.porsche.com/v2/partials/font-links`
-    );
+    partialValidationWarning('getComponentChunkLinks');
   }
+};
 
-  if (!Array.from(head.querySelectorAll('link')).find((el) => el.getAttribute('href').includes('font-face.min'))) {
-    console.warn(
-      `Please make sure to apply the 'getFontFaceStylesheet()' partial as described at https://designsystem.porsche.com/v2/partials/font-face-stylesheet`
-    );
+const validateGetLoaderScriptUsage = (documentBody: HTMLElement): void => {
+  if (
+    !Array.from(documentBody.querySelectorAll('script')).find((el) => {
+      const innerHTML = el.innerHTML;
+
+      return innerHTML.startsWith('var porscheDesignSystem;') && innerHTML.endsWith('porscheDesignSystem.load()');
+    })
+  ) {
+    partialValidationWarning('getLoaderScript');
   }
+};
+
+type PartialNames = 'getFontLink' | 'getComponentChunkLinks' | 'getLoaderScript';
+
+const partialValidationWarning = (partialName: PartialNames): void => {
+  const partialNameToLinkMap: Record<PartialNames, string> = {
+    getFontLink: 'font-links',
+    getComponentChunkLinks: 'component-chunk-links',
+    getLoaderScript: 'loader-script',
+  };
+
+  console.warn(
+    `You are not using '${partialName}()'. The Porsche Design System recommends the usage of the '${partialName}()' partial as described at https://designsystem.porsche.com/v2/partials/${partialNameToLinkMap[partialName]} to enhance performance and loading behavior`
+  );
 };
