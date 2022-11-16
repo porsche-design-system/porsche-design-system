796807664333adab836191db98cb3197c454f9f5
Matthias Haegele <matthias.haegele@mhp.com>
2022-11-16 11:14:43 +0100

    feat(shared): rename variables | mh / tg | #1976

--- packages/shared/scripts/generateChunkNames.ts
+++ packages/shared/scripts/generateChunkNames.ts
@@ -1,16 +1,17 @@
 import { TAG_NAMES } from '../src/lib/tagNames';
+import type { TagName } from '../src/lib/tagNames';
 import { bundles } from '../../components/stencil.config';
 import path from 'path';
 import fs from 'fs';
 
 const generateChunkNames = (): void => {
-  const bundlesWithoutParent = bundles.map(({ components }) => components.slice(1)).flat();
-  const tagNamesWithoutChildren = TAG_NAMES.filter((chunk) => !bundlesWithoutParent.includes(chunk));
+  const tagNamesWithoutChunk = bundles.map(({ components }) => components.slice(1)).flat();
+  const tagNamesWithChunk: TagName[] = TAG_NAMES.filter((chunk) => !tagNamesWithoutChunk.includes(chunk));
 
-  const content = `export const COMPONENT_CHUNK_NAMES = [${tagNamesWithoutChildren
+  const content = `export const COMPONENT_TAG_NAMES_WITH_CHUNK = [${tagNamesWithChunk
     .map((x) => `'${x}'`)
     .join(', ')}] as const;
-export type ComponentChunkName = typeof COMPONENT_CHUNK_NAMES[number];`;
+export type ComponentChunkName = typeof COMPONENT_TAG_NAMES_WITH_CHUNK[number];`;
 
   const targetDirectory = path.normalize('./src/lib');
   fs.mkdirSync(path.resolve(targetDirectory), { recursive: true });
@@ -19,7 +20,7 @@ export type ComponentChunkName = typeof COMPONENT_CHUNK_NAMES[number];`;
   const targetFile = path.resolve(targetDirectory, targetFileName);
   fs.writeFileSync(targetFile, content);
 
-  console.log(`Generated ${targetFileName} for ${tagNamesWithoutChildren.length} tags`);
+  console.log(`Generated ${targetFileName} for ${tagNamesWithChunk.length} tags`);
 };
 
 generateChunkNames();
