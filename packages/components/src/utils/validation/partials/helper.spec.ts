ebdb2aedc7fefded909ab6efb8d9c70402dfc75c
Matthias Haegele <matthias.haegele@mhp.com>
2022-11-16 16:47:11 +0100

    test(components): improve unit test of partialValidation helper | mh / tg | #1976

--- packages/components/src/utils/validation/partials/helper.spec.ts
+++ packages/components/src/utils/validation/partials/helper.spec.ts
@@ -24,14 +24,17 @@ describe('getPreloadedTagNamesForCoreChunk()', () => {
     coreChunkLinkElement = document.createElement('link');
     document.head.appendChild(coreChunkLinkElement);
   });
+
   it('should return empty [] if nextSibling is undefined', () => {
     expect(getPreloadedTagNamesForCoreChunk(coreChunkLinkElement)).toEqual([]);
   });
+
   it('should return empty [] if nextSibling.href is undefined', () => {
     const nextSibling = document.createElement('link');
     document.head.appendChild(nextSibling);
     expect(getPreloadedTagNamesForCoreChunk(coreChunkLinkElement)).toEqual([]);
   });
+
   it('should return empty [] if nextSibling.href does not include "porsche-design-system."', () => {
     const nextSibling = document.createElement('link');
     nextSibling.href = 'some-href';
@@ -39,55 +42,74 @@ describe('getPreloadedTagNamesForCoreChunk()', () => {
 
     expect(getPreloadedTagNamesForCoreChunk(coreChunkLinkElement)).toEqual([]);
   });
-  it('should return preloaded tag names if nextSibling.href includes "porsche-design-system."', () => {
-    const nextSibling1 = document.createElement('link');
-    nextSibling1.href =
-      'href="https://cdn.ui.porsche.com/porsche-design-system/components/porsche-design-system.text.6c494c2050a87f393ea4.js';
-    const nextSibling2 = document.createElement('link');
-    nextSibling2.href =
-      'https://cdn.ui.porsche.com/porsche-design-system/components/porsche-design-system.accordion.672e1be1ea3640e24667.js';
-    const nextSibling3 = document.createElement('link');
-    nextSibling3.href =
-      'https://cdn.ui.porsche.com/porsche-design-system/components/porsche-design-system.grid.f471d5944c4c0bc0bcb5.js';
-    document.head.appendChild(nextSibling1);
-    document.head.appendChild(nextSibling2);
-    document.head.appendChild(nextSibling3);
 
-    expect(getPreloadedTagNamesForCoreChunk(coreChunkLinkElement)).toEqual(['p-text', 'p-accordion', 'p-grid']);
+  it('should return preloaded tag names for core chunk link if nextSibling.href includes "porsche-design-system."', () => {
+    [
+      'https://cdn.ui.porsche.com/porsche-design-system/components/porsche-design-system.text.6c494c2050a87f393ea4.js',
+      'https://cdn.ui.porsche.com/porsche-design-system/components/porsche-design-system.accordion.672e1be1ea3640e24667.js',
+      'https://cdn.ui.porsche.com/porsche-design-system/components/porsche-design-system.v2.17.0.9a579f8ff7708b1885a9.js', // Next core chunk
+      'https://cdn.ui.porsche.com/porsche-design-system/components/porsche-design-system.grid.f471d5944c4c0bc0bcb5.js',
+    ].forEach((href) => {
+      const link = document.createElement('link');
+      link.href = href;
+      document.head.appendChild(link);
+    });
+
+    expect(getPreloadedTagNamesForCoreChunk(coreChunkLinkElement)).toEqual(['p-text', 'p-accordion']);
   });
 });
 
 describe('getPreloadedTagNamesForVersion()', () => {
   const version = '1.2.3';
-  it('should return empty [] for version when version is not found in dom', () => {
+
+  it('should return empty [] for version if no core chunk link is found', () => {
     expect(getPreloadedTagNamesForVersion(version)).toEqual([]);
   });
-  it('should call getPreloadedTagNamesForCoreChunk() with correct parameter when version is found in dom', () => {
-    const coreChunkLinkElement = document.createElement('link');
-    coreChunkLinkElement.href = `porsche-design-system.v${version}`;
-    document.head.appendChild(coreChunkLinkElement);
+
+  it('should call document.querySelector() with correct parameter', () => {
+    const documentQuerySelectorSpy = jest.spyOn(document, 'querySelector');
+
+    getPreloadedTagNamesForVersion(version);
+    expect(documentQuerySelectorSpy).toBeCalledWith('[href*=porsche-design-system\\.v1\\.2\\.3]');
+
+    getPreloadedTagNamesForVersion('1.2.4');
+    expect(documentQuerySelectorSpy).toBeCalledWith('[href*=porsche-design-system\\.v1\\.2\\.4]');
+  });
+
+  it('should call getPreloadedTagNamesForCoreChunk() with correct parameter when core chunk link is found', () => {
+    const querySelectorMockReturn = document.createElement('link');
+    jest.spyOn(document, 'querySelector').mockReturnValue(querySelectorMockReturn);
     const spy = jest.spyOn(helperUtils, 'getPreloadedTagNamesForCoreChunk');
 
     getPreloadedTagNamesForVersion(version);
 
-    expect(spy).toBeCalledWith(coreChunkLinkElement);
+    expect(spy).toBeCalledWith(querySelectorMockReturn);
   });
 });
 
 describe('getPreloadedTagNamesForVersions()', () => {
   const versions = ['1.2.3', '4.5.6', '7.8.9'];
+
   it('should return preloaded tag names for versions', () => {
-    expect(getPreloadedTagNamesForVersions(versions)).toEqual({ '1.2.3': [], '4.5.6': [], '7.8.9': [] });
+    const mockReturnValue: TagName[] = ['p-text'];
+    jest.spyOn(helperUtils, 'getPreloadedTagNamesForVersion').mockReturnValue(mockReturnValue);
+
+    expect(getPreloadedTagNamesForVersions(versions)).toEqual({
+      '1.2.3': mockReturnValue,
+      '4.5.6': mockReturnValue,
+      '7.8.9': mockReturnValue,
+    });
   });
+
   it('should call getPreloadedTagNamesForVersion() with correct parameter', () => {
     const spy = jest.spyOn(helperUtils, 'getPreloadedTagNamesForVersion');
 
     getPreloadedTagNamesForVersions(versions);
 
+    expect(spy).toHaveBeenNthCalledWith(1, '1.2.3');
+    expect(spy).toHaveBeenNthCalledWith(2, '4.5.6');
+    expect(spy).toHaveBeenNthCalledWith(3, '7.8.9');
     expect(spy).toBeCalledTimes(3);
-    expect(spy).toBeCalledWith('1.2.3');
-    expect(spy).toBeCalledWith('4.5.6');
-    expect(spy).toBeCalledWith('7.8.9');
   });
 });
 
