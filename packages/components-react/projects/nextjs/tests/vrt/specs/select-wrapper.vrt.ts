db912a6f1d412b28320ea67f97e50825bee3f10d
Bernd Hacker <hacker.bernd@gmail.com>
2022-11-16 13:38:43 +0100

    test(components-react/nextjs): fix select-wrapper vrt | bh / sas | #1221

--- packages/components-react/projects/nextjs/tests/vrt/specs/select-wrapper.vrt.ts
+++ packages/components-react/projects/nextjs/tests/vrt/specs/select-wrapper.vrt.ts
@@ -4,10 +4,6 @@ it.each(defaultViewports)('should have no visual regression for viewport %s', as
   expect(
     await vrtTest(getVisualRegressionTester(viewport), 'select-wrapper', '/select-wrapper', {
       javaScriptEnabled: false,
-      scenario: async (page) => {
-        await page.click('#open-options');
-        await page.evaluate(() => (window as any).componentsReady());
-      },
     })
   ).toBeFalsy();
 });
