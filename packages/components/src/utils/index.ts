6ba9be8d8890ae36e4d29f636c6f13450383dd32
Stephan Schröter <stephan.schroeter@mhp.com>
2022-11-23 15:34:48 +0100

    feat(components): remove buildStaticLineHeightMap(), calculateLineHeight() and calcLineHeightForElement() | sas | #2111

--- packages/components/src/utils/index.ts
+++ packages/components/src/utils/index.ts
@@ -12,7 +12,6 @@ export * from './form';
 export * from './inject-global-style'; // to trick bundling and avoid separate jss chunk 🤷
 export * from './jss';
 export { parseJSONAttribute } from './json';
-export * from './line-height';
 export { paramCaseToCamelCase } from './paramCaseToCamelCase';
 export * from './pds-fetch';
 export * from './property-observer';
