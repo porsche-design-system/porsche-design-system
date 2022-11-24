a9aa389f4e23614983dccfe1f3a80a4a93d35088
Bernd Hacker <hacker.bernd@gmail.com>
2022-11-24 16:14:27 +0100

    feat(shared): extend clearGitHubActionLogs script by deleteWorkflowRun calls | bh | #2120

--- packages/shared/scripts/clearGitHubActionLogs.ts
+++ packages/shared/scripts/clearGitHubActionLogs.ts
@@ -2,37 +2,46 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { Octokit } from '@octokit/core';
 
-// https://docs.github.com/en/rest/actions/workflow-runs#delete-a-workflow-run
+const config = dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
+const { GITHUB_PERSONAL_ACCESS_TOKEN } = config.parsed;
 
-const PAGE_SIZE = 30;
+if (!GITHUB_PERSONAL_ACCESS_TOKEN) {
+  throw new Error('GITHUB_PERSONAL_ACCESS_TOKEN is missing. Please add it to your `.env` file.');
+}
 
-const fetchWorkflowRunIds = async (
-  octokit: Octokit,
-  page: number
-): Promise<{ totalCount: number; workflowRunIds: number[] }> => {
+const octokit = new Octokit({
+  auth: GITHUB_PERSONAL_ACCESS_TOKEN,
+});
+
+const PAGE_SIZE = 50; // default is 30, max is 100
+const OWNER_AND_REPO: { owner: string; repo: string } = {
+  owner: 'porsche-design-system',
+  repo: 'porsche-design-system',
+};
+
+const fetchWorkflowRunIds = async (page: number): Promise<{ totalCount: number; workflowRunIds: number[] }> => {
   // https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
   const { data } = await octokit.request(
     'GET /repos/{owner}/{repo}/actions/runs{?actor,branch,event,status,per_page,page,created,exclude_pull_requests,check_suite_id,head_sha}',
     {
-      owner: 'porsche-design-system',
-      repo: 'porsche-design-system',
+      ...OWNER_AND_REPO,
       per_page: PAGE_SIZE,
       page: page,
-      created: '<2021-01-01', // before this date
+      created: '<2021-07-01', // before this date
     }
   );
 
   return { totalCount: data.total_count, workflowRunIds: data.workflow_runs.map((x) => x.id) };
 };
 
-const fetchAllWorkflowRunIds = async (octokit: Octokit): Promise<number[]> => {
+const fetchAllWorkflowRunIds = async (): Promise<number[]> => {
   let results = [];
 
   let currentPage = 1;
   let totalPages = 5;
   while (currentPage <= totalPages) {
     console.log(`fetchWorkflowRunIds (${currentPage}/${totalPages})`);
-    const result = await fetchWorkflowRunIds(octokit, currentPage);
+    const result = await fetchWorkflowRunIds(currentPage);
     results = [...results, ...result.workflowRunIds];
     console.log(`results: ${results.length}/${result.totalCount}`);
 
@@ -43,20 +52,26 @@ const fetchAllWorkflowRunIds = async (octokit: Octokit): Promise<number[]> => {
   return results;
 };
 
-const clearGitHubActionLogs = async () => {
-  const config = dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
-  const { GITHUB_PERSONAL_ACCESS_TOKEN } = config.parsed;
-
-  if (!GITHUB_PERSONAL_ACCESS_TOKEN) {
-    throw new Error('GITHUB_PERSONAL_ACCESS_TOKEN is missing. Please add it to your `.env` file.');
+const deleteWorkflowRun = async (runId: number): Promise<void> => {
+  // https://docs.github.com/en/rest/actions/workflow-runs#delete-a-workflow-run
+  try {
+    await octokit.request('DELETE /repos/{owner}/{repo}/actions/runs/{run_id}', {
+      ...OWNER_AND_REPO,
+      run_id: runId,
+    });
+  } catch (e) {
+    console.log(`${e.status}: ${e.request.method} ${e.request.url}`);
   }
+};
 
-  const octokit = new Octokit({
-    auth: GITHUB_PERSONAL_ACCESS_TOKEN,
-  });
+const clearGitHubActionLogs = async () => {
+  const workflowRunIds = await fetchAllWorkflowRunIds();
+  const { length: total } = workflowRunIds;
 
-  const result = await fetchAllWorkflowRunIds(octokit);
-  console.log('total:', result);
+  for (let i = 0; i < total; i++) {
+    console.log(`deleteWorkflowRun: ${workflowRunIds[i]} (${i + 1}/${total})`);
+    await deleteWorkflowRun(workflowRunIds[i]);
+  }
 };
 
 (async () => {
