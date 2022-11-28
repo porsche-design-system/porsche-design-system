9fd1797898e075c77fc5c1f2b9128a8eca89e9d4
TatjanaGartner <tatjana.gartner@mhp.com>
2022-11-28 11:32:33 +0100

    refactor(shared): improve constant names  | tg | #1976

--- packages/shared/scripts/generatePartialNames.ts
+++ packages/shared/scripts/generatePartialNames.ts
@@ -6,9 +6,9 @@ const generateTagNamesWithChunk = (): void => {
   // can't resolve @porsche-design-system/components-js without building it first, therefore we use relative path
   const componentsJsSourceDirectory = path.resolve('../components-js/projects/partials/scripts');
 
-  const partialsFile = globby.sync(`${componentsJsSourceDirectory}/*.ts`);
+  const partialFiles = globby.sync(`${componentsJsSourceDirectory}/*.ts`);
 
-  const partials = partialsFile
+  const partialNames = partialFiles
     .filter((file) => !file.includes('utils' || 'buildPartials')) // skip utils files and buildPartial file
     .map((file) => {
       const fileContent = fs.readFileSync(file, 'utf8');
@@ -18,7 +18,7 @@ const generateTagNamesWithChunk = (): void => {
     .filter((x) => x) // filter out undefined values;
     .sort();
 
-  const content = `export const PARTIAL_NAMES = [${partials.map((x) => `'${x}'`).join(', ')}] as const;
+  const content = `export const PARTIAL_NAMES = [${partialNames.map((x) => `'${x}'`).join(', ')}] as const;
 export type PartialName = typeof PARTIAL_NAMES[number];`;
 
   const targetDirectory = path.normalize('./src/lib');
@@ -28,7 +28,7 @@ export type PartialName = typeof PARTIAL_NAMES[number];`;
   const targetFile = path.resolve(targetDirectory, targetFileName);
   fs.writeFileSync(targetFile, content);
 
-  console.log(`Generated ${targetFileName} for ${partials.length} tags`);
+  console.log(`Generated ${targetFileName} for ${partialNames.length} tags`);
 };
 
 generateTagNamesWithChunk();
