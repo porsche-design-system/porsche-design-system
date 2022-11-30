622914ff09bb35a79dece13493c7fc2386c19cef
Bernd Hacker <hacker.bernd@gmail.com>
2022-11-30 17:08:08 +0100

    Update packages/components/src/utils/inject-global-style.ts

--- packages/components/src/utils/inject-global-style.ts
+++ packages/components/src/utils/inject-global-style.ts
@@ -7,8 +7,8 @@ export const injectGlobalStyle = (): void => {
 
   const styleUrl =
     ROLLUP_REPLACE_IS_STAGING === 'production' ? FONT_FACE_CDN_URL : 'http://localhost:3001/styles/font-face.min.css';
-  if (!document.head.querySelector(`link[href="${styleUrl}"]`)) {
-    const { head } = document;
+  const { head } = document;
+    if (!head.querySelector(`link[href="${styleUrl}"]`)) {
     const link = document.createElement('link');
     link.href = styleUrl;
     link.type = 'text/css';
