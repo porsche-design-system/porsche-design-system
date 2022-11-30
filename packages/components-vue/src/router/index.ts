dc721982839c87cc4060b91c8a4064f1261e2b5e
Matthias Haegele <matthias.haegele@mhp.com>
2022-11-30 09:29:26 +0100

    feat(components-vue): clean up app / run prettier over files | mh | #2088

--- packages/components-vue/src/router/index.ts
+++ packages/components-vue/src/router/index.ts
@@ -1,21 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/customButton",
-      name: "customButton",
+      path: '/',
+      name: 'customButton',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/CustomButtonView.vue"),
+      component: () => import('../views/CustomButtonView.vue'),
     },
   ],
 });
