ea8f92d2494518dee3073f2fd142693929540233
Bernd Hacker <hacker.bernd@gmail.com>
2022-11-30 07:44:40 +0100

    test(components-js): split dsr-ponyfill e2e test into 4 separate testss | bh

--- packages/components-js/tests/e2e/playwright/specs/dsr-ponyfill.e2e.ts
+++ packages/components-js/tests/e2e/playwright/specs/dsr-ponyfill.e2e.ts
@@ -1,4 +1,4 @@
-import type { Page } from '@playwright/test';
+import type { Page, ElementHandle } from '@playwright/test';
 import { expect, test } from '@playwright/test';
 import { getDSRPonyfill } from '@porsche-design-system/components-js/partials';
 import { setProperty, supportsDeclarativeShadowDOM, waitForComponentsReady, waitForStencilLifecycle } from '../utils';
@@ -31,6 +31,7 @@ export const setPageWithContent = async (page: Page): Promise<void> => {
       </template>
       Some label
     </p-button>
+
     <p-button id="without-dsr-template">Some label</p-button>
   </body>
 </html>`,
@@ -38,82 +39,102 @@ export const setPageWithContent = async (page: Page): Promise<void> => {
   );
 };
 
-const getButtonSelector = (hasDsrTemplate: boolean) =>
-  `p-button#${hasDsrTemplate ? 'with-dsr-template' : 'without-dsr-template'}`;
-
-const hasButtonTemplateTag = async (page: Page, hasDsrTemplate: boolean): Promise<boolean> => {
-  return await page.evaluate((selector) => {
-    return !!document.querySelector(`${selector} template`);
-  }, getButtonSelector(hasDsrTemplate));
+const getButtons = (page: Page): Promise<[ElementHandle<HTMLElement>, ElementHandle<HTMLElement>]> => {
+  return page.$$('p-button') as Promise<[ElementHandle<HTMLElement>, ElementHandle<HTMLElement>]>;
 };
 
-const hasButtonShadowRoot = async (page: Page, hasDsrTemplate: boolean): Promise<boolean> => {
-  return await page.evaluate((selector) => {
-    return !!document.querySelector(selector).shadowRoot;
-  }, getButtonSelector(hasDsrTemplate));
+const hasTemplateTag = async (handle: ElementHandle<HTMLElement>): Promise<boolean> => {
+  return handle.evaluate((el) => !!el.querySelector('template'));
 };
 
-const getButtonComputedColor = async (page: Page, hasDsrTemplate: boolean): Promise<string> => {
-  return await page.evaluate((selector) => {
-    const el = document.querySelector(selector)?.shadowRoot?.querySelector('button');
-    return el ? getComputedStyle(el).color : '';
-  }, getButtonSelector(hasDsrTemplate));
+const hasShadowRoot = async (handle: ElementHandle<HTMLElement>): Promise<boolean> => {
+  return handle.evaluate((el) => !!el.shadowRoot);
 };
 
-const changeButtonPropAffectingComputedColor = async (page: Page): Promise<void> => {
-  await setProperty(await page.$(getButtonSelector(true)), 'variant', 'primary');
-  await setProperty(await page.$(getButtonSelector(false)), 'variant', 'primary');
+const getComputedColor = async (handle: ElementHandle<HTMLElement>): Promise<string> => {
+  return handle.evaluate((el) => {
+    const buttonEl = el.shadowRoot?.querySelector('button');
+    return buttonEl ? getComputedStyle(buttonEl).color : '';
+  });
+};
+
+const applyPonyfill = async (page: Page): Promise<void> => {
+  await page.addScriptTag({ content: getDSRPonyfill().replace(/<script>([\s\S]*)<\/script>/, '$1') });
+};
+
+// initialize hydration phase for Porsche Design System
+const initPorscheDesignSystem = async (page: Page): Promise<void> => {
+  await page.addScriptTag({ content: 'porscheDesignSystem.load();' });
+  await waitForComponentsReady(page);
   await waitForStencilLifecycle(page);
 };
 
 test.describe('DSR (Declarative Shadow DOM) Ponyfill', () => {
-  test('should handle initial, dsr ponyfill and hydration phase', async ({ page }) => {
+  test('should handle initial phase correctly', async ({ page }) => {
     await setPageWithContent(page);
+    const [buttonWithTemplate, button] = await getButtons(page);
 
     // evaluate what the native behavior of the browser looks like
-    expect(await hasButtonTemplateTag(page, false)).toBe(false);
-    expect(await hasButtonShadowRoot(page, false)).toBe(false);
-    expect(await getButtonComputedColor(page, false)).toBe('');
+    const hasDSRSupport = await supportsDeclarativeShadowDOM(page);
 
-    expect(await hasButtonTemplateTag(page, true)).toBe(!(await supportsDeclarativeShadowDOM(page)));
-    expect(await hasButtonShadowRoot(page, true)).toBe(await supportsDeclarativeShadowDOM(page));
-    expect(await getButtonComputedColor(page, true)).toBe(
-      (await supportsDeclarativeShadowDOM(page)) ? 'rgb(9, 1, 1)' : ''
-    );
+    expect(await hasTemplateTag(buttonWithTemplate)).toBe(!hasDSRSupport);
+    expect(await hasShadowRoot(buttonWithTemplate)).toBe(hasDSRSupport);
+    expect(await getComputedColor(buttonWithTemplate)).toBe(hasDSRSupport ? 'rgb(9, 1, 1)' : '');
 
-    // apply ponyfill for browsers that don't support DSR
-    await page.addScriptTag({ content: getDSRPonyfill().replace(/<script>([\s\S]*)<\/script>/, '$1') });
+    expect(await hasTemplateTag(button)).toBe(false);
+    expect(await hasShadowRoot(button)).toBe(false);
+    expect(await getComputedColor(button)).toBe('');
+  });
 
-    expect(await hasButtonTemplateTag(page, false)).toBe(false);
-    expect(await hasButtonShadowRoot(page, false)).toBe(false);
-    expect(await getButtonComputedColor(page, false)).toBe('');
+  test('should handle dsr ponyfill phase correctly', async ({ page }) => {
+    await setPageWithContent(page);
+    const [buttonWithTemplate, button] = await getButtons(page);
 
-    expect(await hasButtonTemplateTag(page, true)).toBe(false);
-    expect(await hasButtonShadowRoot(page, true)).toBe(true);
-    expect(await getButtonComputedColor(page, true)).toBe('rgb(9, 1, 1)');
+    await applyPonyfill(page);
 
-    // initialize hydration phase for Porsche Design System
-    await page.addScriptTag({ content: 'porscheDesignSystem.load();' });
-    await waitForComponentsReady(page);
-    await waitForStencilLifecycle(page);
+    expect(await hasTemplateTag(buttonWithTemplate)).toBe(false);
+    expect(await hasShadowRoot(buttonWithTemplate)).toBe(true);
+    expect(await getComputedColor(buttonWithTemplate)).toBe('rgb(9, 1, 1)');
 
-    expect(await hasButtonTemplateTag(page, false)).toBe(false);
-    expect(await hasButtonShadowRoot(page, false)).toBe(true);
-    expect(await getButtonComputedColor(page, false)).toBe('rgb(50, 54, 57)');
+    expect(await hasTemplateTag(button)).toBe(false);
+    expect(await hasShadowRoot(button)).toBe(false);
+    expect(await getComputedColor(button)).toBe('');
+  });
 
-    expect(await hasButtonTemplateTag(page, true)).toBe(false);
-    expect(await hasButtonShadowRoot(page, true)).toBe(true);
-    expect(await getButtonComputedColor(page, true)).toBe('rgb(50, 54, 57)');
+  test('should handle hydration phase after ponyfill correctly', async ({ page }) => {
+    await setPageWithContent(page);
+    const [buttonWithTemplate, button] = await getButtons(page);
+
+    await applyPonyfill(page);
+    await initPorscheDesignSystem(page);
+
+    expect(await hasTemplateTag(buttonWithTemplate)).toBe(false);
+    expect(await hasShadowRoot(buttonWithTemplate)).toBe(true);
+    expect(await getComputedColor(buttonWithTemplate)).toBe('rgb(50, 54, 57)');
+
+    expect(await hasTemplateTag(button)).toBe(false);
+    expect(await hasShadowRoot(button)).toBe(true);
+    expect(await getComputedColor(button)).toBe('rgb(50, 54, 57)');
+  });
+
+  test('should handle prop change after ponyfill and hydration correctly', async ({ page }) => {
+    await setPageWithContent(page);
+    const [buttonWithTemplate, button] = await getButtons(page);
+
+    await applyPonyfill(page);
+    await initPorscheDesignSystem(page);
 
     // apply a prop change to the button component that results in a color change to confirm hydration
-    await changeButtonPropAffectingComputedColor(page);
+    await setProperty(buttonWithTemplate, 'variant', 'primary');
+    await setProperty(button, 'variant', 'primary');
+    await waitForStencilLifecycle(page);
 
-    expect(await hasButtonTemplateTag(page, false)).toBe(false);
-    expect(await hasButtonShadowRoot(page, false)).toBe(true);
-    expect(await getButtonComputedColor(page, false)).toBe('rgb(213, 0, 28)');
+    expect(await hasTemplateTag(buttonWithTemplate)).toBe(false);
+    expect(await hasShadowRoot(buttonWithTemplate)).toBe(true);
+    expect(await getComputedColor(buttonWithTemplate)).toBe('rgb(213, 0, 28)');
 
-    expect(await hasButtonTemplateTag(page, true)).toBe(false);
-    expect(await hasButtonShadowRoot(page, true)).toBe(true);
-    expect(await getButtonComputedColor(page, true)).toBe('rgb(213, 0, 28)');
+    expect(await hasTemplateTag(button)).toBe(false);
+    expect(await hasShadowRoot(button)).toBe(true);
+    expect(await getComputedColor(button)).toBe('rgb(213, 0, 28)');
   });
 });
