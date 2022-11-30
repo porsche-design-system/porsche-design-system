e78a0e4a8db5fa51d528fd383f1346986c5e808e
TatjanaGartner <tatjana.gartner@mhp.com>
2022-11-30 12:23:53 +0100

    refactor(components): revert inject globalStyle changes  | tg | #1976

--- packages/components/src/utils/inject-global-style.ts
+++ packages/components/src/utils/inject-global-style.ts
@@ -1,6 +1,10 @@
 import { FONT_FACE_CDN_URL } from '@porsche-design-system/styles';
 
 export const injectGlobalStyle = (): void => {
+  if (typeof document === 'undefined') {
+    return;
+  }
+
   const styleUrl =
     ROLLUP_REPLACE_IS_STAGING === 'production' ? FONT_FACE_CDN_URL : 'http://localhost:3001/styles/font-face.min.css';
   if (!document.head.querySelector(`link[href="${styleUrl}"]`)) {
