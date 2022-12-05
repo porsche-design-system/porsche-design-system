e3400729f13ea07041fca5dc6b8c0a84d6bda178
Bernd Hacker <hacker.bernd@gmail.com>
2022-12-05 11:38:03 +0100

    build(components-vue): start cdn with yarn start, add build-app and start-app scripts | bh | #2088

--- packages/components-vue/vite.config.ts
+++ packages/components-vue/vite.config.ts
@@ -5,10 +5,16 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // optimizeDeps: {
+  //   include: ['@porsche-design-system/components-js'],
+  // },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: './dist/demo-app',
   },
 });
