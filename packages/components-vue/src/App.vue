827a46c1ff231fd484f4e1814777219191d938e0
Matthias Haegele <matthias.haegele@mhp.com>
2022-12-07 11:03:19 +0100

    feat(components-vue): remove :disabled in option / cast target instead of typing | mh / bh | #2088

--- packages/components-vue/src/App.vue
+++ packages/components-vue/src/App.vue
@@ -10,8 +10,8 @@
   const options = routes.map((item) => ({ path: item.path, name: item.name }));
   const selected = ref(options[0].path);
 
-  const onChange = (e: Event & { target: HTMLSelectElement }) => {
-    const { value } = e.target;
+  const onChange = (e: Event) => {
+    const { value } = e.target as HTMLSelectElement;
     selected.value = value;
     router.push(value);
   };
@@ -20,7 +20,7 @@
 <template>
   <select :value="selected" @change="onChange($event)">
     <option disabled value="">Select a page</option>
-    <option v-for="(item, index) in options" v-bind:key="index" :disabled="item.isDisabled" :value="item.path">
+    <option v-for="(item, index) in options" v-bind:key="index" :value="item.path">
       {{ item.name }}
     </option>
   </select>
