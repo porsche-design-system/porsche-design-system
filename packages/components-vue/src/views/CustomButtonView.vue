62fc50d45a904074cccb61d867d6ce5b2158d11f
Matthias Haegele <matthias.haegele@mhp.com>
2022-12-07 10:08:50 +0100

    feat(components-vue): clean CustomButtonView | mh | #2088

--- packages/components-vue/src/views/CustomButtonView.vue
+++ packages/components-vue/src/views/CustomButtonView.vue
@@ -3,20 +3,9 @@
   import { ref } from 'vue';
 
   const tmp = ref(false);
-  const switchStatus = ref(false);
 
   function log() {
     tmp.value = true;
-  }
-
-  function onSwitchClick() {
-    console.log('switch change');
-    tmp.value = !tmp.value;
-  }
-
-  function onSwitchClick2() {
-    console.log('switch change 2');
-    tmp.value = !tmp.value;
   }
 </script>
 
