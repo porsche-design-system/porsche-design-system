cedb8375526dc5ff20e2312f55c973d6ee501914
Matthias Haegele <matthias.haegele@mhp.com>
2022-12-07 10:08:25 +0100

    feat(components-vue): make OverviewView multiword | mh | #2088

--- packages/components-vue/src/router/index.ts
+++ packages/components-vue/src/router/index.ts
@@ -11,17 +11,9 @@ export const routes: RouteRecordRaw[] = [
     component: () => import('../views/CustomButtonView.vue'),
   },
   {
-    path: '/customSwitch',
-    name: 'customSwitch',
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/CustomSwitchView.vue'),
-  },
-  {
     path: '/overview',
     name: 'Overview',
-    component: () => import('../views/Overview.vue'),
+    component: () => import('../views/OverviewView.vue'),
   },
 ];
 
