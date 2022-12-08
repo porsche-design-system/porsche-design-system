12148b6a33d17d7b6781de413367fb633ea5407f
Bernd Hacker <hacker.bernd@gmail.com>
2022-12-08 09:27:23 +0100

    test(components-vue): temporary disable non existing partials and utilities imports in e2e tests | bh | #2088

--- packages/components-vue/tests/e2e/specs/partials.e2e.ts
+++ packages/components-vue/tests/e2e/specs/partials.e2e.ts
@@ -1,6 +1,6 @@
 import * as jsPartials from '@porsche-design-system/components-js/partials';
-import * as vuePartials from '@porsche-design-system/components-vue/partials';
+// import * as vuePartials from '@porsche-design-system/components-vue/partials';
 
 xit('should reexport all partials from components-js', () => {
-  expect(vuePartials).toEqual(jsPartials);
+  // expect(vuePartials).toEqual(jsPartials);
 });
--- packages/components-vue/tests/e2e/specs/utilities.e2e.ts
+++ packages/components-vue/tests/e2e/specs/utilities.e2e.ts
@@ -1,6 +1,6 @@
 import * as jsUtilities from '@porsche-design-system/components-js/utilities/js';
-import * as vueUtilities from '@porsche-design-system/components-vue/utilities/js';
+// import * as vueUtilities from '@porsche-design-system/components-vue/utilities/js';
 
 xit('should reexport all utilities from components-js', () => {
-  expect(vueUtilities).toEqual(jsUtilities);
+  // expect(vueUtilities).toEqual(jsUtilities);
 });
