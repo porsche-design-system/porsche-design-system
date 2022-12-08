0eada6cd5c8d478328d802bf27fb9f9778eac3e5
Stephan Schröter <stephan.schroeter@mhp.com>
2022-12-08 18:52:15 +0100

    test(components-js): update vrt fixtures for modal scrollable | sas | #2111

--- packages/components-angular/src/app/pages/modal-scrollable.component.ts
+++ packages/components-angular/src/app/pages/modal-scrollable.component.ts
@@ -52,7 +52,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
         gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
         elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
-        eos et accusam et justo duo dolores et ea rebum<span id="scroll-into-view">.</span>
+        eos et accusam<span id="scroll-into-view">.</span>
       </p-modal>
     </div>
   `,
