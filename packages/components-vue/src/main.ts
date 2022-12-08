9c96cf068084f5964f62106eb229f9fd0990e35d
Bernd Hacker <hacker.bernd@gmail.com>
2022-12-08 17:09:37 +0100

    fix(components-vue): use correct import in demo-app | bh | #2088

--- packages/components-vue/src/main.ts
+++ packages/components-vue/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { componentsReady } from '../projects/vue-wrapper/src/public-api';
+import { componentsReady } from '@porsche-design-system/components-vue';
 
 const app = createApp(App);
 
