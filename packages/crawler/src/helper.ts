81e8035f240b14512033e685dc2cc9ee57e8d4c6
TatjanaGartner <tatjana.gartner@mhp.com>
2022-12-09 14:34:50 +0100

    feat(crawler): move getTagNamesWithProperties() to helper.ts | tg | #2114

--- packages/crawler/src/helper.ts
+++ packages/crawler/src/helper.ts
@@ -1,5 +1,17 @@
 import fs from 'fs';
 import { crawlerConfig as config } from '../constants';
+import { componentMeta } from '@porsche-design-system/shared';
+
+export type TagNamesWithProperties = Record<string, string[]>;
+
+export const getTagNamesWithProperties = (): TagNamesWithProperties =>
+  Object.entries(componentMeta).reduce(
+    (result, [key, value]) => ({
+      ...result,
+      [key]: value.props ? Object.keys(value.props) : {},
+    }),
+    {}
+  );
 
 export const removeOutdatedReports = (): void => {
   fs.readdirSync(config.reportFolderName)
