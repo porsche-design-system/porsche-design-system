7781f87a0c38677fe220ed7dc8a933e96024e33f
Matthias Haegele <matthias.haegele@mhp.com>
2022-12-13 14:41:21 +0100

    feat(components-vue): introduce useToastManager to vue utils | mh | #2088

--- packages/components-vue/projects/vue-wrapper/src/utils.ts
+++ packages/components-vue/projects/vue-wrapper/src/utils.ts
@@ -1,5 +1,6 @@
 import { inject } from 'vue';
 import type { InjectionKey } from 'vue';
+import type { ToastMessage } from './lib/types';
 
 export const prefixInjectionKey = Symbol('pdsPrefix') as InjectionKey<'pdsPrefix'>;
 
@@ -25,4 +26,15 @@ export const addEventListenerToElementRef = <T extends HTMLElement, E extends st
   elementRef.addEventListener(eventName, (e) => {
     emit(eventName, (e as CustomEvent).detail);
   });
+};
+// TODO: Check naming with Alvaro
+export const useToastManager = (): { addMessage: (message: ToastMessage) => void } => {
+  const tagName = getPrefixedTagName('p-toast');
+
+  return {
+    addMessage: (message: ToastMessage): void => {
+      const toast = document.body.querySelector(tagName) as HTMLElement & { addMessage: (message: ToastMessage) => void };
+      customElements.whenDefined(tagName).then(() => toast.addMessage(message));
+    },
+  };
 };
