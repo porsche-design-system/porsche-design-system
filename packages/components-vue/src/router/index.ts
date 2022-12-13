121d7f6155bfaa549ebfd6ef96ada0eab9f4321d
Bernd Hacker <hacker.bernd@gmail.com>
2022-12-13 13:37:48 +0100

    feat(components-vue): map pages to routes | bh | #2088

--- packages/components-vue/src/router/index.ts
+++ packages/components-vue/src/router/index.ts
@@ -1,19 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import type { RouteRecordRaw } from 'vue-router';
+import type { RouteComponent, RouteRecordRaw } from 'vue-router';
 import * as fromPages from '../pages';
 
-export const routes: RouteRecordRaw[] = [
-  {
-    path: '/events',
-    name: 'Events',
-    component: fromPages.EventsPage,
-  },
-  {
-    path: '/overview',
-    name: 'Overview',
-    component: fromPages.OverviewPage,
-  },
-];
+export const routes: RouteRecordRaw[] = Object.keys(fromPages).map<RouteRecordRaw>((page) => {
+  const name = page.replace(/Page$/, '');
+  return {
+    path: '/' + name.toLowerCase(),
+    name: name,
+    component: (fromPages as Record<string, RouteComponent>)[page],
+  };
+});
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
