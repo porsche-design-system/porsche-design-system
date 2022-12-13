0583a5db5c12ba49b4d03bef943d53371c89dae6
Matthias Haegele <matthias.haegele@mhp.com>
2022-12-13 14:41:52 +0100

    feat(components-vue): implement Toast component which uses useToastManager cb | mh | #2088

---
+++ packages/components-vue/src/components/ToastComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { PToast, useToastManager } from '@porsche-design-system/components-vue';
+  import { onMounted } from 'vue';
+
+  const { addMessage } = useToastManager();
+
+  onMounted(() => {
+    addMessage({ text: 'some message' });
+  });
+</script>
+
+<template>
+  <PToast />
+</template>
