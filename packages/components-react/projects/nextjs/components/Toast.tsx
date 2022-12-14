582f6210a39dbbde72f529765361e89c48f8a2d9
Bernd Hacker <hacker.bernd@gmail.com>
2022-12-14 13:28:37 +0100

    feat(components-react): add missing theme prop to extracted Toast component | bh | #2088

--- packages/components-react/projects/nextjs/components/Toast.tsx
+++ packages/components-react/projects/nextjs/components/Toast.tsx
@@ -1,5 +1,5 @@
-import { PToast, useToastManager } from '@porsche-design-system/components-react';
-import type { Theme } from '@porsche-design-system/components-react';
+import { PToast, useToastManager } from '@porsche-design-system/components-react/ssr';
+import type { Theme } from '@porsche-design-system/components-react/ssr';
 import { useEffect } from 'react';
 
 export const Toast = ({ text, theme }: { text: string; theme: Theme }): JSX.Element => {
