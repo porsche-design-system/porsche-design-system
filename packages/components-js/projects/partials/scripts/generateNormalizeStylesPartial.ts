ed5977b20e31a8ddcb4f34e1dbc6c1e67a8fbd28
TatjanaGartner <tatjana.gartner@mhp.com>
2022-12-21 16:51:46 +0100

    feat(components-js): add custom pds styles to normalize.css styles  | tg | #2112

--- packages/components-js/projects/partials/scripts/generateNormalizeStylesPartial.ts
+++ packages/components-js/projects/partials/scripts/generateNormalizeStylesPartial.ts
@@ -4,16 +4,15 @@ import jssPluginGlobal from 'jss-plugin-global';
 import jssPluginNested from 'jss-plugin-nested';
 import jssPluginCamelCase from 'jss-plugin-camel-case';
 import jssPluginSortMediaQueries from 'jss-plugin-sort-css-media-queries';
-
-global.ROLLUP_REPLACE_IS_STAGING = 'production';
+import { color, font, fontFamily } from '@porsche-design-system/utilities';
 
 export const generateNormalizeStylesPartial = (): string => {
   const types = `type GetNormalizeStylesOptions = {
   cdn?: Cdn;
   format?: Format;
 };
-type GetNormalizeStylesOptionsWithoutTags = GetNormalizeStylesOptionsWithoutTags & { format: 'html' };
-type GetNormalizeStylesOptionsWithoutTags = GetNormalizeStylesOptionsWithoutTags & { format: 'jsx' }`;
+type GetNormalizeStylesOptionsFormatHtml = GetNormalizeStylesOptions & { format: 'html' };
+type GetNormalizeStylesOptionsFormatJsx = GetNormalizeStylesOptions & { format: 'jsx' }`;
   `;`;
 
   const minifyCss = (css: string): string =>
@@ -35,39 +34,59 @@ type GetNormalizeStylesOptionsWithoutTags = GetNormalizeStylesOptionsWithoutTags
       })
       .toString();
 
-  const fontWeight = {
-    thin: 100,
-    regular: 400,
-    semiBold: 600,
-    bold: 700,
-  };
-
   const normalizeCss = normalize[Object.keys(normalize)[0]];
 
   normalizeCss['html']['textSizeAdjust'] = 'none';
   normalizeCss['html']['WebkitTextSizeAdjust'] = 'none'; // stop iOS safari from adjusting font size when screen rotation is changing
+  normalizeCss['html']['fontFamily'] = fontFamily;
+  // TODO: line-height, some default text styles should be provided and margin + padding is reset
 
   normalizeCss['a']['color'] = 'inherit';
   normalizeCss['a']['textDecoration'] = 'underline';
-  normalizeCss['a']['outline'] = '1px solid transparent';
-  normalizeCss['a']['outlineOffset'] = '1px';
-  normalizeCss['a::-moz-focus-inner'] = {};
-  normalizeCss['a::-moz-focus-inner']['border'] = 0;
-  normalizeCss['a:focus'] = {};
-  normalizeCss['a:focus']['outlineColor'] = 'currentcolor';
-  normalizeCss['a:focus:not(:focus-visible)'] = {};
-  normalizeCss['a:focus:not(:focus-visible)']['outlineColor'] = 'transparent';
 
-  normalizeCss['b, strong']['fontWeight'] = fontWeight.bold;
+  normalizeCss['@media(hover:hover)'] = {};
+  normalizeCss['@media(hover:hover)']['a'] = {};
+  normalizeCss['@media(hover:hover)']['a']['transition'] = 'color var(--p-transition-duration, .24s) ease';
+  normalizeCss['@media(hover:hover)']['a:hover'] = {};
+  normalizeCss['@media(hover:hover)']['a:hover']['color'] = color.state.hover;
+
+  normalizeCss['@media(hover:hover)']['[data-theme="dark"] a'] = {};
+  normalizeCss['@media(hover:hover)']['[data-theme="dark"] a']['transition'] =
+    'color var(--p-transition-duration, .24s) ease';
+  normalizeCss['@media(hover:hover)']['[data-theme="dark"] a:hover'] = {};
+  normalizeCss['@media(hover:hover)']['[data-theme="dark"] a:hover']['color'] = color.darkTheme.state.hover;
+
+  normalizeCss['b, strong']['fontWeight'] = font.weight.bold;
 
   normalizeCss['em, i'] = {};
   normalizeCss['em, i']['fontStyle'] = 'normal';
 
-  const normalizePDSCss = minifyCss(
-    getCss({
-      '@global': normalizeCss,
-    })
-  );
+  const addFocusStyles = (): void => {
+    const focusableElements = ['a', 'button', 'input', 'select', 'textarea'];
+    const focusableElementsString = focusableElements.join(',');
+    normalizeCss[focusableElementsString] = {};
+    normalizeCss[focusableElementsString]['outline'] = '1px solid transparent';
+    normalizeCss[focusableElementsString]['outlineOffset'] = '1px';
+
+    // TODO: use focus() from utilities?
+    for (const el of focusableElements) {
+      normalizeCss[`${el}::-moz-focus-inner`] = {};
+      normalizeCss[`${el}::-moz-focus-inner`]['border'] = 0;
+      normalizeCss[`${el}:focus`] = {};
+      normalizeCss[`${el}:focus`]['outlineColor'] = 'currentColor';
+      normalizeCss[`${el}:focus:not(:focus-visible)`] = {};
+      normalizeCss[`${el}:focus:not(:focus-visible)`]['outlineColor'] = 'transparent';
+    }
+  };
+
+  const getNormalizeStyles = (): string => {
+    addFocusStyles();
+    return minifyCss(
+      getCss({
+        '@global': { ...normalizeCss },
+      })
+    );
+  };
 
   const normalizeStylesFunction = `export function getNormalizeStyles(opts?: GetNormalizeStylesOptionsFormatHtml): string;
 export function getNormalizeStyles(opts?: GetNormalizeStylesOptionsFormatJsx): JSX.Element;
@@ -82,7 +101,7 @@ export function getNormalizeStyles(opts?: GetNormalizeStylesOptions): string | J
   const styleProps = { ['data-pds-normalize-styles']: '' };
   const styleAttributes = convertPropsToAttributeString(styleProps);
 
-  const normalizeStyles = \`${normalizePDSCss}\`;
+  const normalizeStyles = \`${getNormalizeStyles()}\`;
 
   return format === 'html' ? \`<style \$\{styleAttributes\}>\${normalizeStyles}</style>\` : <style {...styleProps} dangerouslySetInnerHTML={{ __html: normalizeStyles }}/>;
 }`;
