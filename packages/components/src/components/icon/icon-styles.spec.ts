e8c7113537bbcce9c5dfde708e0b686f1c99d209
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-01-04 16:32:42 +0100

    feat(components): disable unit test | sas / bh | #2182

--- packages/components/src/components/icon/icon-styles.spec.ts
+++ packages/components/src/components/icon/icon-styles.spec.ts
@@ -2,12 +2,12 @@ import { getComponentCss } from './icon-styles';
 
 xdescribe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['default', 'small', 'light'],
-    ['default', 'small', 'dark'],
-    ['brand', 'small', 'light'],
+    // ['default', 'small', 'light'],
+    // ['default', 'small', 'dark'],
+    // ['brand', 'small', 'light'],
     ['inherit', 'small', 'light'],
-    ['default', 'large', 'light'],
-    ['default', 'inherit', 'light'],
+    // ['default', 'large', 'light'],
+    // ['default', 'inherit', 'light'],
   ])('should return correct css for color: %s, size: %s and theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
