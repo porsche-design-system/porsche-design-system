87710932f0b630a633f17fb13d9112d987d9ed71
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-01-05 12:50:43 +0100

    text(components): re-enable and update style snapshot tests | sas | #2182

--- packages/components/src/styles/text-icon-styles.spec.ts
+++ packages/components/src/styles/text-icon-styles.spec.ts
@@ -2,25 +2,25 @@ import { getThemedTextColor } from './text-icon-styles';
 
 xdescribe('getThemedTextColor()', () => {
   it.each<Parameters<typeof getThemedTextColor>>([
-    // ['light', 'brand'],
-    // ['light', 'default'],
+    ['light', 'brand'], // TODO: shall be deprecated with v3-alpha
+    ['light', 'default'], // TODO: shall be deprecated with v3-alpha
     ['light', 'neutral-contrast-high'],
     ['light', 'neutral-contrast-medium'],
     ['light', 'neutral-contrast-low'],
     ['light', 'notification-success'],
     ['light', 'notification-warning'],
     ['light', 'notification-error'],
-    // ['light', 'notification-neutral'],
+    ['light', 'notification-neutral'], // TODO: shall be deprecated with v3-alpha
     ['light', 'inherit'],
-    // ['dark', 'brand'],
-    // ['dark', 'default'],
+    ['dark', 'brand'], // TODO: shall be deprecated with v3-alpha
+    ['dark', 'default'], // TODO: shall be deprecated with v3-alpha
     ['dark', 'neutral-contrast-high'],
     ['dark', 'neutral-contrast-medium'],
     ['dark', 'neutral-contrast-low'],
     ['dark', 'notification-success'],
     ['dark', 'notification-warning'],
     ['dark', 'notification-error'],
-    // ['dark', 'notification-neutral'],
+    ['dark', 'notification-neutral'], // TODO: shall be deprecated with v3-alpha
     ['dark', 'inherit'],
   ])('should return correct css for theme: %s and textColor: %s', (...args) => {
     expect(getThemedTextColor(...args)).toMatchSnapshot();
