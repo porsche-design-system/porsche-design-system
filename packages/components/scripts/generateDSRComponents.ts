a36f1684ae96453913d483e73ebd46b18d780bf4
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-01-05 13:56:25 +0100

    fix(components): fix import of generated dsr components | sas | #2182

--- packages/components/scripts/generateDSRComponents.ts
+++ packages/components/scripts/generateDSRComponents.ts
@@ -230,7 +230,10 @@ import { get${componentName}Css } from '${stylesBundleImportPath}';
       } else if (tagName === 'p-icon') {
         newFileContent = newFileContent
           .replace(/^/, "import { ICONS_MAP } from '@porsche-design-system/icons';\n") // add missing import
-          .replace(/( } from '@porsche-design-system\/components\/dist\/utils';)/, ', paramCaseToCamelCase, isUrl$1') // add missing import
+          .replace(
+            /(import {.*)(} from '@porsche-design-system\/components\/dist\/utils';)/,
+            '$1, paramCaseToCamelCase, isUrl$2'
+          ) // add missing import
           .replace(
             /(<i key={this\.key\+\+} className="root") \/>/,
             `$1 dangerouslySetInnerHTML={{ __html: isUrl(this.props.source) ? '<img src="\'+ this.props.source +\'" alt="" />' : ICONS_MAP[paramCaseToCamelCase(this.props.name) as keyof typeof ICONS_MAP] || '' }} />`
