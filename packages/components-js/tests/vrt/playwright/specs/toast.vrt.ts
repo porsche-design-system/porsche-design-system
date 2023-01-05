fed55aa76e2dd9b6fd271a108ee77ed3ec2f4459
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-01-05 10:43:50 +0100

    test: disable playwright vrt tests | sas / bh | #2182

--- packages/components-js/tests/vrt/playwright/specs/toast.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/toast.vrt.ts
@@ -5,18 +5,18 @@ test.skip('should have no visual regression', async () => {
   await executeVisualRegressionTest('toast-basic');
 });
 
-test.describe('should have no visual regression when dark', async () => {
+test.skip('should have no visual regression when dark', async () => {
   await executeVisualRegressionTest('toast-basic-dark');
 });
 
-test.describe('should have no visual regression with long-text', async () => {
+test.skip('should have no visual regression with long-text', async () => {
   await executeVisualRegressionTest('toast-basic-long-text');
 });
 
-test.describe('should have no visual regression with offset', async () => {
+test.skip('should have no visual regression with offset', async () => {
   await executeVisualRegressionTest('toast-offset');
 });
 
-test.describe('should have no visual regression with prefix', async () => {
+test.skip('should have no visual regression with prefix', async () => {
   await executeVisualRegressionTest('toast-prefixed');
 });
