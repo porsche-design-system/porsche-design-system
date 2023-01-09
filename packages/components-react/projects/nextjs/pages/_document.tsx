785119760331e70b2e620ee1428e3d8496e447bf
TatjanaGartner <tatjana.gartner@mhp.com>
2023-01-09 17:37:32 +0100

    feat(components-react/next-js): apply getNormalizeStyles() partial | tg | #2112

--- packages/components-react/projects/nextjs/pages/_document.tsx
+++ packages/components-react/projects/nextjs/pages/_document.tsx
@@ -5,6 +5,7 @@ import {
   getDSRPonyfill,
   getFontLinks,
   getInitialStyles,
+  getNormalizeStyles,
 } from '@porsche-design-system/components-react/partials';
 import { getSharedStyles } from '../styles/getSharedStyles';
 
@@ -16,6 +17,7 @@ const Document = (): JSX.Element => {
 
         <link rel="stylesheet" href="http://localhost:3001/styles/font-face.min.css" />
         {getInitialStyles({ format: 'jsx' })}
+        {getNormalizeStyles({ format: 'jsx' })}
         {getFontLinks({ weights: ['thin', 'regular', 'semi-bold', 'bold'], withoutTags: true })
           .map((x) => x.replace('https://cdn.ui.porsche.com/porsche-design-system', 'http://localhost:3001'))
           .map((url) => (
