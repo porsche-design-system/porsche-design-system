35b14337aba3a37da3d3456cff80dd6977600190
Bernd Hacker <hacker.bernd@gmail.com>
2023-01-10 09:30:40 +0100

    test(components-react/jsdom-polyfill): add unit tests for componentsReady | bh | #2203

---
+++ packages/components-react/projects/jsdom-polyfill/tests/unit/specs/componentsReady.spec.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { componentsReady, PButton } from '@porsche-design-system/components-react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getByRoleShadowed } from '@porsche-design-system/components-react/testing';
+
+const Sample = (): JSX.Element => {
+  const [active, setActive] = useState(false);
+
+  return (
+    <>
+      <PButton onClick={() => setActive(true)}>Button 1</PButton>
+      {active && <PButton>Button 2</PButton>}
+    </>
+  );
+};
+
+it('should return 0 when nothing is rendered', async () => {
+  expect(await componentsReady()).toBe(0);
+});
+
+it('should return 1 after component is rendered initially', async () => {
+  render(<Sample />);
+  expect(await componentsReady()).toBe(1);
+});
+
+it('should return 2 after button is clicked', async () => {
+  render(<Sample />);
+  expect(await componentsReady()).toBe(1);
+
+  const button = getByRoleShadowed('button');
+  await userEvent.click(button);
+
+  expect(await componentsReady()).toBe(2);
+});
