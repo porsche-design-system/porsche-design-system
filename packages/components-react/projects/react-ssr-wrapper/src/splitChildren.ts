8cdd5c50a13b4aaab648e9a301883a835a5acc33
Matthias Haegele <matthias.haegele@mhp.com>
2023-01-12 15:52:26 +0100

    feat(components-react/ssr): move splitting of children into helper function | mh / bh | #2236

---
+++ packages/components-react/projects/react-ssr-wrapper/src/splitChildren.ts
@@ -0,0 +1,18 @@
+import type { ReactElement } from 'react';
+
+export const splitChildren = (
+  children: ReactElement
+): {
+  children: ReactElement[];
+  namedSlotChildren: ReactElement[];
+  otherChildren: ReactElement[];
+} => {
+  const childrenArray: ReactElement[] = (Array.isArray(children) ? children : children ? [children] : []).filter(
+    (x) => x !== undefined && x !== null // children are filtered due to cases where conditionally rendered children can be undefined.
+  );
+
+  const otherChildren = childrenArray.filter((child) => !child.props?.slot);
+  const namedSlotChildren = childrenArray.filter((child) => child.props?.slot);
+
+  return { children: childrenArray, namedSlotChildren, otherChildren };
+};
