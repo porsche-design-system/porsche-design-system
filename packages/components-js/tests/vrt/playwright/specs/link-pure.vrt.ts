78472bceed3ccbe59eefbd4912a10afe35698aa8
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-01-12 15:14:34 +0100

    test(components-js): enable playwright vrt test for button-pure + link-pure | sas | #2182

--- packages/components-js/tests/vrt/playwright/specs/link-pure.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/link-pure.vrt.ts
@@ -1,6 +1,6 @@
 import { executeVisualRegressionTest } from '../helpers/playwright-helper';
 import { test } from '@playwright/test';
 
-test('should have no visual regression', async () => {
+test.describe('should have no visual regression', async () => {
   await executeVisualRegressionTest('link-pure');
 });
