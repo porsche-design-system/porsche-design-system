6816f3a8dc66ef962577b17569e1ca3ff55cd9a1
Matthias Haegele <matthias.haegele@mhp.com>
2023-01-17 11:02:53 +0100

    feat(components-react/ssr): improve typing of splitChildren | mh | #2236

--- packages/components-react/projects/react-ssr-wrapper/src/splitChildren.ts
+++ packages/components-react/projects/react-ssr-wrapper/src/splitChildren.ts
@@ -1,13 +1,24 @@
-import type { ReactElement } from 'react';
+import type { ReactElement, ReactNode } from 'react';
 
+/**
+ * @param children derived from PropsWithChildren
+ *
+ * type PropsWithChildren<P = unknown> = P & { children?: ReactNode | undefined };
+ * type ReactNode = ReactElement | string | number | ReactFragment | ReactPortal | boolean | null | undefined;
+ * interface ReactElement<P = any, T extends string | JSXElementConstructor<any> = string | JSXElementConstructor<any>> {
+ *     type: T;
+ *     props: P;
+ *     key: Key | null;
+ * }
+ */
 export const splitChildren = (
-  children: ReactElement | ReactElement[]
+  children: ReactNode | undefined
 ): {
-  children: ReactElement[];
+  children: Exclude<ReactNode, null | undefined>[];
   namedSlotChildren: ReactElement[];
-  otherChildren: ReactElement[];
+  otherChildren: Exclude<ReactNode, null | undefined>[];
 } => {
-  const childrenArray: ReactElement[] = (Array.isArray(children) ? children : children ? [children] : []).filter(
+  const childrenArray = (Array.isArray(children) ? children : children ? [children] : []).filter(
     (x) => x !== undefined && x !== null // children are filtered due to cases where conditionally rendered children can be undefined.
   );
 
