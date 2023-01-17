9dfb8392ded3aa2399683262aea57523909430c3
Oliver Hook <oliver.hook@porsche.digital>
2023-01-17 18:14:17 +0100

    test(components): fixed spec files | oh | #2121

--- packages/components/src/components/button/button-styles.spec.ts
+++ packages/components/src/components/button/button-styles.spec.ts
@@ -2,15 +2,17 @@ import { getComponentCss } from './button-styles';
 
 xdescribe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['arrow-right', '', 'primary', false, false, 'light'],
-    ['arrow-right', '', 'primary', false, false, 'dark'],
-    ['arrow-right', '', 'secondary', false, false, 'light'],
-    ['arrow-right', '', 'secondary', false, false, 'dark'],
-    ['arrow-right', '', 'tertiary', false, false, 'light'],
-    ['arrow-right', '', 'tertiary', false, false, 'dark'],
-    ['arrow-right', '', 'primary', false, true, 'light'],
-    ['arrow-right', '', 'primary', { base: true, xs: false, s: true, m: false, l: true, xl: false }, true, 'dark'],
-  ])('should return correct css for icon: %s, iconSource: %s, variant: %s, hideLabel: %s, isDisabledOrLoading: %s and theme: %s', (...args) => {
+    ['arrow-right', '', 'primary', false, false, false, false, 'light'],
+    ['arrow-right', '', 'primary', false, false, false, false, 'dark'],
+    ['arrow-right', '', 'secondary', false, false, false, false, 'light'],
+    ['arrow-right', '', 'secondary', false, false, false, false, 'dark'],
+    ['arrow-right', '', 'tertiary', false, false, false, false, 'light'],
+    ['arrow-right', '', 'tertiary', false, false, false, false, 'dark'],
+    ['arrow-right', '', 'primary', false, true, false, false, 'light'],
+    ['arrow-right', '', 'primary', false, false, true, false, 'light'],
+    ['arrow-right', '', 'primary', false, false, false, true, 'light'],
+    ['arrow-right', '', 'primary', { base: true, xs: false, s: true, m: false, l: true, xl: false }, true,false, false, 'dark'],
+  ])('should return correct css for icon: %s, iconSource: %s, variant: %s, hideLabel: %s, disabled: %s, isDisabledOrLoading: %s, loading: %s and theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
 });
