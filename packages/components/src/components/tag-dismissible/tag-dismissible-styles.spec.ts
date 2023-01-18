97ba882c89e1a23e555a913707e28a8aabee24b7
Matthias Haegele <matthias.haegele@mhp.com>
2023-01-18 11:51:19 +0100

    test(components): extend style spec of tag-dismissible | mh | #2150

--- packages/components/src/components/tag-dismissible/tag-dismissible-styles.spec.ts
+++ packages/components/src/components/tag-dismissible/tag-dismissible-styles.spec.ts
@@ -1,16 +1,19 @@
 import { getComponentCss } from './tag-dismissible-styles';
 
-xdescribe('getComponentCss()', () => {
+describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['background-default', true],
-    ['background-default', false],
-    ['background-surface', true],
+    ['background-default', 'light', true],
+    ['background-default', 'light', false],
+    ['background-surface', 'light', true],
+    ['background-default', 'dark', true],
+    ['background-default', 'dark', false],
+    ['background-surface', 'dark', true],
   ])('should return correct css for color: %s and hasLabel: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
 
   // Direct child selector fixes https://github.com/porsche-design-system/porsche-design-system/pull/1941 in safari < v15.5
   it('should have ">" combinator on .button:hover for .icon', () => {
-    expect(getComponentCss('background-default', true)).toMatch('button:hover > .icon');
+    expect(getComponentCss('background-default', 'light', true)).toMatch('button:hover > .icon');
   });
 });
