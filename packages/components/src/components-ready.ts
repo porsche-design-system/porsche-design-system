dace0aa0c0d6bb62ef76aa7b82a7e78b35b363a2
Bernd Hacker <hacker.bernd@gmail.com>
2023-01-18 08:31:07 +0100

    perf(components): change componentsReady recursive children iteration with regex to querySelectorAll based on TAG_NAMES | bh | #2203

--- packages/components/src/components-ready.ts
+++ packages/components/src/components-ready.ts
@@ -1,4 +1,5 @@
 import type { HostElement } from '@stencil/core/internal';
+import { TAG_NAMES } from '@porsche-design-system/shared';
 
 type PromiseResolve = (amount: number) => void;
 
@@ -66,22 +67,11 @@ const allComponentsLoaded = (el: HTMLElement, resolve: PromiseResolve): void => 
 };
 
 const collectAllComponentOnReadyPromises = (el: HTMLElement): Promise<HostElement>[] => {
-  let readyPromises: Promise<HostElement>[] = [];
+  const selector = document.porscheDesignSystem[ROLLUP_REPLACE_VERSION].prefixes
+    .map((p) => (p ? TAG_NAMES.map((t) => `${p}-${t}`) : TAG_NAMES))
+    .join(',');
 
-  // Node.ELEMENT_NODE: An Element node like <p> or <div>
-  if (el?.nodeType === 1) {
-    (Array.from(el.children) as HostElement[]).forEach((childEl) => {
-      if (isDesignSystemElement(childEl)) {
-        readyPromises.push(childEl.componentOnReady());
-      }
-      readyPromises = readyPromises.concat(collectAllComponentOnReadyPromises(childEl));
-    });
-  }
-
-  return readyPromises;
-};
-
-const regex = /^(.*-)?P-(.*)$/;
-const isDesignSystemElement = (el: HostElement): boolean => {
-  return regex.exec(el.tagName) && typeof el.componentOnReady === 'function';
+  return Array.from<HostElement>(el.querySelectorAll(selector))
+    .filter((item) => typeof item.componentOnReady === 'function')
+    .map((item) => item.componentOnReady());
 };
