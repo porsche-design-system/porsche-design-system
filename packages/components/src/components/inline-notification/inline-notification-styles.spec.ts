85a474f5f0d63b12567af643a74c4f0168698e18
TatjanaGartner <tatjana.gartner@mhp.com>
2023-01-18 13:03:08 +0100

    test(components): reenable and update tests | tg | #2133

--- packages/components/src/components/inline-notification/inline-notification-styles.spec.ts
+++ packages/components/src/components/inline-notification/inline-notification-styles.spec.ts
@@ -8,28 +8,28 @@ import {
 } from './inline-notification-styles';
 import type { InlineNotificationState } from './inline-notification-utils';
 
-xdescribe('getComponentCss()', () => {
+describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['neutral', false, false, 'light'],
+    ['info', false, false, 'light'],
     ['warning', false, false, 'light'],
     ['success', false, false, 'light'],
     ['error', false, false, 'light'],
-    ['neutral', true, false, 'light'],
-    ['neutral', false, true, 'light'],
-    ['neutral', true, true, 'light'],
-    ['neutral', false, false, 'dark'],
+    ['info', true, false, 'light'],
+    ['info', false, true, 'light'],
+    ['info', true, true, 'light'],
+    ['info', false, false, 'dark'],
     ['warning', false, false, 'dark'],
     ['success', false, false, 'dark'],
     ['error', false, false, 'dark'],
-    ['neutral', true, false, 'dark'],
-    ['neutral', false, true, 'dark'],
-    ['neutral', true, true, 'dark'],
+    ['info', true, false, 'dark'],
+    ['info', false, true, 'dark'],
+    ['info', true, true, 'dark'],
   ])('should return correct css for state: %s, hasAction: %s, hasClose: %s and theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
 });
 
-xdescribe('getSlottedCss()', () => {
+describe('getSlottedCss()', () => {
   it('should return correct css', () => {
     const host = document.createElement('p-inline-notification');
     expect(getSlottedCss(host)).toMatchSnapshot();
@@ -41,13 +41,13 @@ xdescribe('getSlottedCss()', () => {
   });
 });
 
-xdescribe('getNotificationRootJssStyles()', () => {
+describe('getNotificationRootJssStyles()', () => {
   it.each<Parameters<typeof getNotificationRootJssStyle>>([
-    ['neutral', 'light'],
+    ['info', 'light'],
     ['success', 'light'],
     ['error', 'light'],
     ['warning', 'light'],
-    ['neutral', 'dark'],
+    ['info', 'dark'],
     ['success', 'dark'],
     ['error', 'dark'],
     ['warning', 'dark'],
@@ -56,8 +56,8 @@ xdescribe('getNotificationRootJssStyles()', () => {
   });
 });
 
-xdescribe('getNotificationIconJssStyle()', () => {
-  it.each<InlineNotificationState>(['neutral', 'success', 'error', 'warning'])(
+describe('getNotificationIconJssStyle()', () => {
+  it.each<InlineNotificationState>(['info', 'success', 'error', 'warning'])(
     'should return correct JssStyle for state: %s ',
     (state) => {
       expect(getNotificationIconJssStyle(state)).toMatchSnapshot();
@@ -65,13 +65,13 @@ xdescribe('getNotificationIconJssStyle()', () => {
   );
 });
 
-xdescribe('getNotificationContentJssStyle()', () => {
+describe('getNotificationContentJssStyle()', () => {
   it('should return correct JssStyle', () => {
     expect(getNotificationContentJssStyle()).toMatchSnapshot();
   });
 });
 
-xdescribe('getCloseIconJssStyle()', () => {
+describe('getCloseIconJssStyle()', () => {
   it('should return correct JssStyle', () => {
     expect(getCloseIconJssStyle()).toMatchSnapshot();
   });
