0a65297a6565b3d4acb664f77886e2d3e9f54af9
TatjanaGartner <tatjana.gartner@mhp.com>
2023-01-18 18:18:55 +0100

    fix(components): use correct filter for icon colors | tg | #2133

--- packages/components/src/styles/icon-filters.ts
+++ packages/components/src/styles/icon-filters.ts
@@ -5,15 +5,15 @@ type ThemedIconFilter = 'info' | 'success' | 'warning' | 'error';
 export type ThemedIconFilters = { [key in ThemedIconFilter]: string };
 
 const themeLight = {
-  info: 'invert(31%) sepia(32%) saturate(5493%) hue-rotate(216deg) brightness(90%) contrast(107%)',
-  success: 'invert(62%) sepia(61%) saturate(551%) hue-rotate(86deg) brightness(86%) contrast(80%)',
+  info: 'invert(62%) sepia(61%) saturate(551%) hue-rotate(86deg) brightness(86%) contrast(80%)',
+  success: 'invert(31%) sepia(32%) saturate(5493%) hue-rotate(216deg) brightness(90%) contrast(107%)',
   warning: 'invert(74%) sepia(91%) saturate(343%) hue-rotate(348deg) brightness(92%) contrast(86%)',
   error: 'invert(25%) sepia(62%) saturate(2003%) hue-rotate(335deg) brightness(100%) contrast(97%)',
 };
 
 const themeDark = {
-  info: 'invert(31%) sepia(32%) saturate(5493%) hue-rotate(216deg) brightness(90%) contrast(107%)',
-  success: 'invert(59%) sepia(22%) saturate(1342%) hue-rotate(86deg) brightness(96%) contrast(88%)',
+  info: 'invert(59%) sepia(22%) saturate(1342%) hue-rotate(86deg) brightness(96%) contrast(88%)',
+  success: 'invert(31%) sepia(32%) saturate(5493%) hue-rotate(216deg) brightness(90%) contrast(107%)',
   warning: 'invert(72%) sepia(94%) saturate(303%) hue-rotate(354deg) brightness(89%) contrast(94%)',
   error: 'invert(28%) sepia(34%) saturate(3133%) hue-rotate(333deg) brightness(95%) contrast(100%)',
 };
