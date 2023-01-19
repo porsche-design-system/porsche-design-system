c7e3f1eb5d1623868fa7b923ba6b509dadb9d85e
Bernd Hacker <hacker.bernd@gmail.com>
2023-01-19 07:51:05 +0100

    fix(components): exception in undefined document.porscheDesignSystem on yarn start | bh | #2151

--- packages/components/src/setup.ts
+++ packages/components/src/setup.ts
@@ -2,7 +2,11 @@ import { validatePartialUsage } from './utils/validation/partials/validatePartia
 import { injectGlobalStyle } from './utils';
 
 export default (): void => {
-  document.porscheDesignSystem?.[ROLLUP_REPLACE_VERSION].readyResolve(); // provided via load() of components-js package
+  if (process.env.NODE_ENV !== 'development') {
+    // not available during `yarn start` of components package
+    document.porscheDesignSystem[ROLLUP_REPLACE_VERSION].readyResolve(); // provided via load() of components-js package
+  }
+
   injectGlobalStyle();
   validatePartialUsage();
 };
