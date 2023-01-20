91f302b473f2d29c156318db25de137908f1c1b8
TatjanaGartner <tatjana.gartner@mhp.com>
2023-01-20 16:18:05 +0100

    test(components/shared): remove getSlottedCss test, update snapshot | tg | #2133

--- packages/components/src/components/banner/banner-styles.spec.ts
+++ packages/components/src/components/banner/banner-styles.spec.ts
@@ -4,25 +4,12 @@ import {
   getBoxShadow,
   getComponentCss,
   getKeyframesMobile,
-  getSlottedCss,
   KeyframesDirection,
 } from './banner-styles';
 
 describe('getComponentCss()', () => {
   it('should return correct css', () => {
     expect(getComponentCss()).toMatchSnapshot();
-  });
-});
-
-describe('getSlottedCss()', () => {
-  it('should return correct css', () => {
-    const host = document.createElement('p-banner');
-    expect(getSlottedCss(host)).toMatchSnapshot();
-  });
-
-  it('should return correct css with prefix', () => {
-    const host = document.createElement('prefixed-p-banner');
-    expect(getSlottedCss(host)).toMatchSnapshot();
   });
 });
 
