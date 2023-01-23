9c321c80fa0db8042ade426044e78aeeb1436f8d
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-01-23 14:02:03 +0100

    test(components): update snapshots | sas / oh | #2121

--- packages/components/src/styles/link-button-styles.spec.ts
+++ packages/components/src/styles/link-button-styles.spec.ts
@@ -2,7 +2,7 @@ import { getCss } from '../utils';
 import { getLinkButtonStyles } from './link-button-styles';
 
 it('should have ">" combinator on .root:hover for .icon/.label', () => {
-  const css = getCss(getLinkButtonStyles('none', '','tertiary', false, false, false, 'light'));
+  const css = getCss(getLinkButtonStyles('arrow-right', '','tertiary', false, false, false, 'light'));
 
   // Direct child selector fixes https://github.com/porsche-design-system/porsche-design-system/pull/1941 in safari < v15.5 for variant tertiary
   expect(css).toMatch('.root:hover > .label');
