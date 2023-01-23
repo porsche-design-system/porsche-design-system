caec9da58e34657d895e90044c168e11e2fcb01f
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-01-23 14:53:21 +0100

    test(components): update style snapshots for button | sas / oh | #2121

--- packages/components/src/components/button/button-styles.spec.ts
+++ packages/components/src/components/button/button-styles.spec.ts
@@ -2,18 +2,29 @@ import { getComponentCss } from './button-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['arrow-right', '', 'primary', false, false, false, false, 'light'],
-    ['arrow-right', '', 'primary', false, false, false, false, 'dark'],
-    ['arrow-right', '', 'secondary', false, false, false, false, 'light'],
-    ['arrow-right', '', 'secondary', false, false, false, false, 'dark'],
-    ['arrow-right', '', 'tertiary', false, false, false, false, 'light'],
-    ['arrow-right', '', 'tertiary', false, false, false, false, 'dark'],
-    ['arrow-right', '', 'primary', false, true, false, false, 'light'],
-    ['arrow-right', '', 'primary', false, false, true, false, 'light'],
-    ['arrow-right', '', 'primary', false, false, false, true, 'light'],
-    ['arrow-right', '', 'primary', { base: true, xs: false, s: true, m: false, l: true, xl: false }, true,false, false, 'dark'],
-    ['none', '', 'primary', false, true, false, true, 'light'],
-  ])('should return correct css for icon: %s, iconSource: %s, variant: %s, hideLabel: %s, disabled: %s, isDisabledOrLoading: %s, loading: %s and theme: %s', (...args) => {
-    expect(getComponentCss(...args)).toMatchSnapshot();
-  });
+    ['arrow-right', '', 'primary', false, false, false, 'light'],
+    ['arrow-right', '', 'primary', false, false, false, 'dark'],
+    ['arrow-right', '', 'secondary', false, false, false, 'light'],
+    ['arrow-right', '', 'secondary', false, false, false, 'dark'],
+    ['arrow-right', '', 'tertiary', false, false, false, 'light'],
+    ['arrow-right', '', 'tertiary', false, false, false, 'dark'],
+    ['arrow-right', '', 'primary', false, true, false, 'light'],
+    ['arrow-right', '', 'primary', false, false, false, 'light'],
+    ['arrow-right', '', 'primary', false, false, true, 'light'],
+    [
+      'arrow-right',
+      '',
+      'primary',
+      { base: true, xs: false, s: true, m: false, l: true, xl: false },
+      true,
+      false,
+      'dark',
+    ],
+    ['none', '', 'primary', false, true, true, 'light'],
+  ])(
+    'should return correct css for icon: %s, iconSource: %s, variant: %s, hideLabel: %s, disabled: %s, loading: %s and theme: %s',
+    (...args) => {
+      expect(getComponentCss(...args)).toMatchSnapshot();
+    }
+  );
 });
