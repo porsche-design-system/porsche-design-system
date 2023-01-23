1f7d5f56c5b05fa83eaf24cbcd263b58b9d72c14
TatjanaGartner <tatjana.gartner@mhp.com>
2023-01-23 16:20:32 +0100

    test(components-js): add banner to component scaling vrt | tg | #2123

--- packages/components-js/tests/vrt/puppeteer/specs/component-scaling.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/component-scaling.vrt.ts
@@ -1,7 +1,7 @@
 import { vrtTest } from '@porsche-design-system/shared/testing';
 import { getVisualRegressionStatesTester } from 'shared/src/testing/vrt';
 
-const components = ['checkbox-wrapper', 'radio-button-wrapper'];
+const components = ['banner', 'checkbox-wrapper', 'inline-notification', 'radio-button-wrapper'];
 
 it.each(components)('should have no visual regression for scaled component %s', async (component) => {
   expect(
