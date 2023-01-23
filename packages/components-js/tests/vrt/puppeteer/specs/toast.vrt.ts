fad0f16487f586bb52b4e770ca5e7ea8c0a2c6ed
TatjanaGartner <tatjana.gartner@mhp.com>
2023-01-23 13:24:04 +0100

    feat(components-js/-react/-angular/-nextjs): reenable toast vrt tests | tg | #2154

--- packages/components-js/tests/vrt/puppeteer/specs/toast.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/toast.vrt.ts
@@ -6,19 +6,19 @@ import {
 } from '@porsche-design-system/shared/testing';
 
 describe('toast-basic', () => {
-  xit.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
+  it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
     expect(await vrtTest(getVisualRegressionTester(viewport), 'toast-basic', '/#toast-basic')).toBeFalsy();
   });
 });
 
 describe('toast-basic-dark', () => {
-  xit('should have no visual regression for viewport 1000', async () => {
+  it('should have no visual regression for viewport 1000', async () => {
     expect(await vrtTest(getVisualRegressionStatesTester(), 'toast-basic-dark', '/#toast-basic-dark')).toBeFalsy();
   });
 });
 
 describe('toast-basic-long-text', () => {
-  xit.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
+  it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
     expect(
       await vrtTest(getVisualRegressionTester(viewport), 'toast-basic-long-text', '/#toast-basic-long-text')
     ).toBeFalsy();
@@ -26,13 +26,13 @@ describe('toast-basic-long-text', () => {
 });
 
 describe('toast-offset', () => {
-  xit('should have no visual regression for viewport 1000', async () => {
+  it('should have no visual regression for viewport 1000', async () => {
     expect(await vrtTest(getVisualRegressionStatesTester(), 'toast-offset', '/#toast-offset')).toBeFalsy();
   });
 });
 
 describe('toast-prefixed', () => {
-  xit('should have no visual regression for viewport 1000', async () => {
+  it('should have no visual regression for viewport 1000', async () => {
     expect(await vrtTest(getVisualRegressionStatesTester(), 'toast-prefixed', '/#toast-prefixed')).toBeFalsy();
   });
 });
