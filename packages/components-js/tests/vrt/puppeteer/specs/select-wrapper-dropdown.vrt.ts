8d370f3b59766a71f99a66566b2168aad76e6329
Bernd Hacker <hacker.bernd@gmail.com>
2023-01-24 11:27:32 +0100

    test(shared): update componentMeta snapshot | bh | #2143

--- packages/components-js/tests/vrt/puppeteer/specs/select-wrapper-dropdown.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/select-wrapper-dropdown.vrt.ts
@@ -1,7 +1,7 @@
 import { forceHoverState, setContentWithDesignSystem } from '../helpers';
 import { waitForComponentsReady } from '../../../e2e/puppeteer/helpers';
 import { getVisualRegressionStatesTester } from '@porsche-design-system/shared/testing';
-import { Theme } from '@porsche-design-system/components/dist/types/types';
+import type { Theme } from '@porsche-design-system/components/dist/types/types';
 
 it('should have no visual regression for :hover', async () => {
   const vrt = getVisualRegressionStatesTester();
