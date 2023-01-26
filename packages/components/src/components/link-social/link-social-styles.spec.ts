1211cc18813d25caaf3ee7e76737cc3522f487d5
Oliver Hook <oliver.hook@porsche.digital>
2023-01-26 13:51:33 +0100

    test(components): delete unused css spec files | oh | #2134

--- packages/components/src/components/link-social/link-social-styles.spec.ts
+++
@@ -1,7 +0,0 @@
-import { getComponentCss } from './link-social-styles';
-
-describe('getComponentCss()', () => {
-  it('should return correct css: %s', () => {
-    expect(getComponentCss()).toMatchSnapshot();
-  });
-});
