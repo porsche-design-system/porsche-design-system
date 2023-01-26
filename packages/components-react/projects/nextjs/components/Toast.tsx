15b884b87c4c6ba513369dd9797cda74f8135ec5
TatjanaGartner <tatjana.gartner@mhp.com>
2023-01-26 15:00:58 +0100

    feat(components-nextjs): extend state property | tg | #2133

--- packages/components-react/projects/nextjs/components/Toast.tsx
+++ packages/components-react/projects/nextjs/components/Toast.tsx
@@ -2,11 +2,11 @@ import { PToast, useToastManager } from '@porsche-design-system/components-react
 import type { Theme } from '@porsche-design-system/components-react/ssr';
 import { useEffect } from 'react';
 
-export const Toast = ({ text, theme }: { text: string; theme?: Theme }): JSX.Element => {
+export const Toast = ({ text, theme, state }: { text: string; theme?: Theme; state: string }): JSX.Element => {
   const { addMessage } = useToastManager();
   useEffect(() => {
     addMessage({ text });
-  }, [addMessage, text]);
+  }, [addMessage, text, state]);
 
   return <PToast theme={theme} />;
 };
