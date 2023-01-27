790bd6e92890b519ea5a1fbc10257ce9c51f6068
Anastasiia Antonova <antonova64@gmail.com>
2023-01-27 15:01:17 +0100

    feat(components): tag - unit test for warn color check | aa | #2149

--- packages/components/src/components/tag/tag.spec.ts
+++ packages/components/src/components/tag/tag.spec.ts
@@ -1,5 +1,6 @@
 import * as getDirectChildHTMLElementUtils from '../../utils/dom/getDirectChildHTMLElement';
 import { Tag } from './tag';
+import * as TagUtils from './tag-utils';
 
 describe('render', () => {
   it('should call getDirectChildHTMLElement() with correct parameters', () => {
@@ -11,5 +12,16 @@ describe('render', () => {
     component.render();
 
     expect(spy).toBeCalledWith(component.host, 'a,button');
+  });
+  it('should call warnIfColorNotificationNeutralIsUsed() with correct parameters', () => {
+    const spy = jest.spyOn(TagUtils, 'warnIfColorNotificationNeutralIsUsed');
+    const component = new Tag();
+    component.host = document.createElement('p-tag');
+    component.color = 'notification-neutral';
+    component.host.attachShadow({ mode: 'open' });
+
+    component.render();
+
+    expect(spy).toBeCalledWith(component.host, 'notification-neutral');
   });
 });
