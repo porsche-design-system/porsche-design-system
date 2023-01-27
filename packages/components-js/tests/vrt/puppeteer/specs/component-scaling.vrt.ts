5996829972ad5b1cb86b16f556c78f6e2802747a
Anastasiia Antonova <antonova64@gmail.com>
2023-01-27 11:29:27 +0100

    feat(components-js): add tag to component scaling vrt test | aa | #2149

--- packages/components-js/tests/vrt/puppeteer/specs/component-scaling.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/component-scaling.vrt.ts
@@ -1,7 +1,7 @@
 import { vrtTest } from '@porsche-design-system/shared/testing';
 import { getVisualRegressionStatesTester } from 'shared/src/testing/vrt';
 
-const components = ['checkbox-wrapper', 'radio-button-wrapper', 'link', 'button', 'tag-dismissible', 'spinner'];
+const components = ['checkbox-wrapper', 'radio-button-wrapper', 'link', 'button', 'tag-dismissible', 'spinner', 'tag'];
 
 it.each(components)('should have no visual regression for scaled component %s', async (component) => {
   expect(
