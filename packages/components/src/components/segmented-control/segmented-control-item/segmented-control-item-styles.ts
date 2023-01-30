a76b2cf4ca1cee79251976d48e7d0d085606f6ce
Anastasiia Antonova <antonova64@gmail.com>
2023-01-30 18:29:48 +0100

    feat(components): segmented control item - apply filter to icon | aa | #2142

--- packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-styles.ts
+++ packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-styles.ts
@@ -59,6 +59,11 @@ export const getColors = (
       };
 };
 
+export const getIconFilter = (theme: Theme, isDisabled: boolean): string => {
+  const isDark = theme === 'dark';
+  return isDisabled ? (isDark ? 'contrast(17%)' : 'invert(100%) contrast(17%)') : 'invert(100%)';
+};
+
 export const getComponentCss = (
   isDisabled: boolean,
   isSelected: boolean,
@@ -70,6 +75,7 @@ export const getComponentCss = (
   const { focusColor } = getThemedColors(theme);
   const { buttonColor, labelColor, borderColor, hoverBorderColor } = getColors(isDisabled, isSelected, bgColor, theme);
   const hasIcon = icon || iconSource;
+  const iconHasFilter = isDisabled || theme === 'dark';
 
   return getCss({
     '@global': {
@@ -132,6 +138,9 @@ export const getComponentCss = (
       height: ICON_SIZE,
       width: ICON_SIZE,
       marginRight: ICON_MARGIN,
+      ...(iconHasFilter && {
+        filter: getIconFilter(theme, isDisabled),
+      }),
     },
   });
 };
