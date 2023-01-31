5b32ea971e99f3b563efe8cd687bc6b7e4f2d6f5
Oliver Hook <oliver.hook@porsche.digital>
2023-01-31 10:07:12 +0100

    test(components): add unit test | oh | #2134

---
+++ packages/components/src/utils/validation/throwIfComponentIsDeprecated.spec.ts
@@ -0,0 +1,13 @@
+import { throwIfComponentIsDeprecated } from './throwIfComponentIsDeprecated';
+
+const warningMessage =
+  'Component div is deprecated and will be removed with next major release. Use "link" component with corresponding social icon instead.';
+
+describe('with host element', () => {
+  it('should throw warning', () => {
+    const host = document.createElement('div');
+    jest.spyOn(console, 'warn').mockImplementation();
+    throwIfComponentIsDeprecated(host);
+    expect(console.warn).toHaveBeenCalledWith(expect.stringContaining(warningMessage));
+  });
+});
