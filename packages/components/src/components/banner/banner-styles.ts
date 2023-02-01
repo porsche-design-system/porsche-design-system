1e62dd11c8dd01547eaf0225cdabcd42c02f1731
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-01 19:29:48 +0100

    feat(components): improve chunking of banner + toast styles | sas | #2133

--- packages/components/src/components/banner/banner-styles.ts
+++ packages/components/src/components/banner/banner-styles.ts
@@ -1,10 +1,17 @@
 import type { JssStyle } from 'jss';
 import type { BannerWidth } from './banner-utils';
+import type { KeyframesDirection } from './banner-shared-styles';
 import { getMediaQueryMin, getMediaQueryMinMax } from '@porsche-design-system/utilities-v2';
 import { getCss } from '../../utils';
-import { addImportantToRule } from '../../styles';
 import { BANNER_Z_INDEX } from '../../constants';
 import { getContentWrapperStyle } from '../content-wrapper/content-wrapper-shared-styles';
+import {
+  getAnimationIn,
+  getAnimationOut,
+  getBoxShadow,
+  getKeyframes,
+  getKeyframesMobile,
+} from './banner-shared-styles';
 
 const bannerPositionTypeVar = '--p-banner-position-type';
 const bannerPositionTopVar = '--p-banner-position-top';
@@ -12,55 +19,16 @@ const bannerPositionBottomVar = '--p-banner-position-bottom';
 const bannerZIndexVar = '--p-internal-banner-z-index';
 const bannerAnimationDurationVar = '--p-animation-duration';
 
-const easeInQuad = 'cubic-bezier(0.45,0,0.55,1)';
-const easeOutQuad = 'cubic-bezier(0.5,1,0.89,1)';
-export const ANIMATION_DURATION = 600;
+const bannerOffset = '56px';
 
-const mediaQueryS = getMediaQueryMin('s');
 const mediaQueryBase = getMediaQueryMinMax('base', 's');
-
-export const getBoxShadow = (): JssStyle => ({
-  boxShadow: '0 2px 4px 0 rgba(0,0,0,0.05),0 15px 20px 0 rgba(0,0,0,0.2)',
-});
-
-export const getAnimationIn = (keyframesName: string, durationVar?: string): JssStyle => {
-  const duration = durationVar ? `var(${durationVar},${ANIMATION_DURATION}ms)` : `${ANIMATION_DURATION}ms`;
-  return {
-    animation: `${duration} $${keyframesName} ${easeInQuad} forwards`,
-  };
-};
-
-export const getAnimationOut = (keyframesName: string): JssStyle => ({
-  animation: addImportantToRule(`${ANIMATION_DURATION}ms $${keyframesName} ${easeOutQuad} forwards`),
-});
-
-export type KeyframesDirection = 'in' | 'out';
-const getKeyframes = (direction: KeyframesDirection, outsideStyle: JssStyle): JssStyle => {
-  const insideStyle: JssStyle = { opacity: 1, transform: 'translate3d(0,0,0)' };
-  return direction === 'in'
-    ? {
-        from: outsideStyle,
-        to: insideStyle,
-      }
-    : {
-        from: insideStyle,
-        to: outsideStyle,
-      };
-};
-
-export const getKeyframesMobile = (direction: KeyframesDirection, bottomVar: string): JssStyle =>
-  getKeyframes(direction, {
-    opacity: 0,
-    transform: `translate3d(0,calc(var(${bottomVar}) + 100%),0)`, // space before and after "+" is crucial
-  });
+const mediaQueryS = getMediaQueryMin('s');
 
 const getKeyframesDesktop = (direction: KeyframesDirection, topVar: string): JssStyle =>
   getKeyframes(direction, {
     opacity: 0,
     transform: `translate3d(0,calc(-100% - var(${topVar})),0)`, // space before and after "-" is crucial
   });
-
-const bannerOffset = '56px';
 
 export const getComponentCss = (width: BannerWidth): string => {
   return getCss({
