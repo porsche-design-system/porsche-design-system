c033ad064e4ed4bc151cccfafae463b05f7b0869
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-01 10:57:02 +0100

    test: use optimized vrt test scenarios for tag | sas | #2149

--- packages/components-react/projects/nextjs/pages/tag.tsx
+++ packages/components-react/projects/nextjs/pages/tag.tsx
@@ -24,10 +24,12 @@ const TagPage: NextPage = (): JSX.Element => {
       <div className="playground light" title="should show different background colors on light background">
         <PTag>Default</PTag>
         <PTag color="background-default">Color background-default</PTag>
+        <PTag color="background-base">Color background-base</PTag>
         <PTag color="background-surface">Color background-surface</PTag>
         <PTag color="neutral-contrast-high">Color neutral-contrast-high</PTag>
+        <PTag color="primary">Color primary</PTag>
         <PTag color="notification-neutral">Color notification-neutral</PTag>
-        <PTag color="notification-information">Color notification-information</PTag>
+        <PTag color="notification-info">Color notification-info</PTag>
         <PTag color="notification-success">Color notification-success</PTag>
         <PTag color="notification-error">Color notification-error</PTag>
         <PTag color="notification-warning">Color notification-warning</PTag>
@@ -36,10 +38,12 @@ const TagPage: NextPage = (): JSX.Element => {
       <div className="playground light surface" title="should show different background colors on light surface background">
         <PTag>Default</PTag>
         <PTag color="background-default">Color background-default</PTag>
+        <PTag color="background-base">Color background-base</PTag>
         <PTag color="background-surface">Color background-surface</PTag>
         <PTag color="neutral-contrast-high">Color neutral-contrast-high</PTag>
+        <PTag color="primary">Color primary</PTag>
         <PTag color="notification-neutral">Color notification-neutral</PTag>
-        <PTag color="notification-information">Color notification-information</PTag>
+        <PTag color="notification-info">Color notification-info</PTag>
         <PTag color="notification-success">Color notification-success</PTag>
         <PTag color="notification-error">Color notification-error</PTag>
         <PTag color="notification-warning">Color notification-warning</PTag>
@@ -48,10 +52,12 @@ const TagPage: NextPage = (): JSX.Element => {
       <div className="playground dark" title="should show different background colors on dark background">
         <PTag theme="dark">Default</PTag>
         <PTag theme="dark" color="background-default">Color background-default</PTag>
+        <PTag theme="dark" color="background-base">Color background-base</PTag>
         <PTag theme="dark" color="background-surface">Color background-surface</PTag>
         <PTag theme="dark" color="neutral-contrast-high">Color neutral-contrast-high</PTag>
+        <PTag theme="dark" color="primary">Color primary</PTag>
         <PTag theme="dark" color="notification-neutral">Color notification-neutral</PTag>
-        <PTag theme="dark" color="notification-information">Color notification-information</PTag>
+        <PTag theme="dark" color="notification-info">Color notification-info</PTag>
         <PTag theme="dark" color="notification-success">Color notification-success</PTag>
         <PTag theme="dark" color="notification-error">Color notification-error</PTag>
         <PTag theme="dark" color="notification-warning">Color notification-warning</PTag>
@@ -60,23 +66,25 @@ const TagPage: NextPage = (): JSX.Element => {
       <div className="playground dark surface" title="should show different background colors on dark surface background">
         <PTag theme="dark">Default</PTag>
         <PTag theme="dark" color="background-default">Color background-default</PTag>
+        <PTag theme="dark" color="background-base">Color background-base</PTag>
         <PTag theme="dark" color="background-surface">Color background-surface</PTag>
         <PTag theme="dark" color="neutral-contrast-high">Color neutral-contrast-high</PTag>
+        <PTag theme="dark" color="primary">Color primary</PTag>
         <PTag theme="dark" color="notification-neutral">Color notification-neutral</PTag>
-        <PTag theme="dark" color="notification-information">Color notification-information</PTag>
+        <PTag theme="dark" color="notification-info">Color notification-info</PTag>
         <PTag theme="dark" color="notification-success">Color notification-success</PTag>
         <PTag theme="dark" color="notification-error">Color notification-error</PTag>
         <PTag theme="dark" color="notification-warning">Color notification-warning</PTag>
       </div>
 
       <div className="playground light" title="should show different background colors and icons on light background">
-        <PTag icon="car" color="background-default">Color background-default</PTag>
+        <PTag icon="car" color="background-base">Color background-base</PTag>
         <PTag icon="highway" color="background-surface">Color background-surface</PTag>
-        <PTag iconSource="./assets/icon-custom-kaixin.svg" color="background-surface"> Color background-surface </PTag>
+        <PTag iconSource="./assets/icon-custom-kaixin.svg" color="background-surface"> Color background-surface</PTag>
       </div>
 
       <div className="playground dark" title="should show different background colors and icons on dark background">
-        <PTag theme="dark" icon="car" color="background-default">Color background-default</PTag>
+        <PTag theme="dark" icon="car" color="background-base">Color background-base</PTag>
         <PTag theme="dark" icon="highway" color="background-surface">Color background-surface</PTag>
         <PTag theme="dark" iconSource="./assets/icon-custom-kaixin.svg" color="background-surface">
           Color background-surface
@@ -84,48 +92,48 @@ const TagPage: NextPage = (): JSX.Element => {
       </div>
 
       <div className="playground light" title="should show different background colors with link on light background">
-        <PTag color="background-default"><a href="#">Color background-default link</a></PTag>
+        <PTag color="background-base"><a href="#">Color background-base link</a></PTag>
         <PTag color="background-surface"><a href="#">Color background-surface link</a></PTag>
-        <PTag icon="car" color="background-default"><a href="#">Color background-default link</a></PTag>
+        <PTag icon="car" color="background-base"><a href="#">Color background-base link</a></PTag>
         <PTag icon="highway" color="background-surface"><a href="#">Color background-surface link</a></PTag>
-        <PTag iconSource="./assets/icon-custom-kaixin.svg" color="background-default">
-          <a href="#">Color background-default link</a>
+        <PTag iconSource="./assets/icon-custom-kaixin.svg" color="background-base">
+          <a href="#">Color background-base link</a>
         </PTag>
       </div>
 
       <div className="playground dark" title="should show different background colors with link on dark background">
-        <PTag theme="dark" color="background-default"><a href="#">Color background-default link</a></PTag>
+        <PTag theme="dark" color="background-base"><a href="#">Color background-base link</a></PTag>
         <PTag theme="dark" color="background-surface"><a href="#">Color background-surface link</a></PTag>
-        <PTag theme="dark" icon="car" color="background-default"><a href="#">Color background-default link</a></PTag>
+        <PTag theme="dark" icon="car" color="background-base"><a href="#">Color background-base link</a></PTag>
         <PTag theme="dark" icon="highway" color="background-surface">
           <a href="#">Color background-surface link</a>
         </PTag>
-        <PTag theme="dark" iconSource="./assets/icon-custom-kaixin.svg" color="background-default">
-          <a href="#">Color background-default link</a>
+        <PTag theme="dark" iconSource="./assets/icon-custom-kaixin.svg" color="background-base">
+          <a href="#">Color background-base link</a>
         </PTag>
       </div>
 
       <div className="playground light" title="should show different background colors with button on light background">
-        <PTag color="background-default"><button>Color background-default button</button></PTag>
+        <PTag color="background-base"><button>Color background-base button</button></PTag>
         <PTag color="background-surface"><button>Color background-surface button</button></PTag>
-        <PTag icon="car" color="background-default"><button>Color background-default button</button></PTag>
+        <PTag icon="car" color="background-base"><button>Color background-base button</button></PTag>
         <PTag icon="highway" color="background-surface">
           <button>Color background-surface button</button>
         </PTag>
-        <PTag iconSource="./assets/icon-custom-kaixin.svg" color="background-default">
-          <button>Color background-default button</button>
+        <PTag iconSource="./assets/icon-custom-kaixin.svg" color="background-base">
+          <button>Color background-base button</button>
         </PTag>
       </div>
 
       <div className="playground dark" title="should show different background colors with button on dark background">
-        <PTag theme="dark" color="background-default"><button>Color background-default button</button></PTag>
+        <PTag theme="dark" color="background-base"><button>Color background-base button</button></PTag>
         <PTag theme="dark" color="background-surface"><button>Color background-surface button</button></PTag>
-        <PTag theme="dark" icon="car" color="background-default"><button>Color background-default button</button></PTag>
+        <PTag theme="dark" icon="car" color="background-base"><button>Color background-base button</button></PTag>
         <PTag theme="dark" icon="highway" color="background-surface">
           <button>Color background-surface button</button>
         </PTag>
-        <PTag theme="dark" iconSource="./assets/icon-custom-kaixin.svg" color="background-default">
-          <button>Color background-default button</button>
+        <PTag theme="dark" iconSource="./assets/icon-custom-kaixin.svg" color="background-base">
+          <button>Color background-base button</button>
         </PTag>
       </div>
 
@@ -139,8 +147,8 @@ const TagPage: NextPage = (): JSX.Element => {
 
       <div className="playground light" title="should show different multiline tags on light background">
         <div style={{ width: '100px', overflow: 'auto', border: '1px solid deeppink' }}>
-          <PTag color="neutral-contrast-high">Text that is very long</PTag>
-          <PTag color="neutral-contrast-high" icon="car">Text with icon that is very long</PTag>
+          <PTag color="primary">Text that is very long</PTag>
+          <PTag color="primary" icon="car">Text with icon that is very long</PTag>
           <PTag color="notification-success" icon="car">
             <a href="#">Link with icon that is very long</a>
           </PTag>
