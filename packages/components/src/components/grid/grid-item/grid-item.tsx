7f1fd147aa0775a31dd787b61da5d01c6eefcbb1
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-02 11:51:15 +0100

    feat(components): remove GridItemInternalHTMLProps type for grid | sas | #2155

--- packages/components/src/components/grid/grid-item/grid-item.tsx
+++ packages/components/src/components/grid/grid-item/grid-item.tsx
@@ -1,5 +1,5 @@
 import { Component, Element, h, JSX, Prop } from '@stencil/core';
-import type { GridItemInternalHTMLProps, GridItemOffset, GridItemSize } from './grid-item-utils';
+import type { GridItemOffset, GridItemSize } from './grid-item-utils';
 import { GRID_ITEM_OFFSETS, GRID_ITEM_SIZES } from './grid-item-utils';
 import { getComponentCss } from './grid-item-styles';
 import type { BreakpointCustomizable, PropTypes } from '../../../types';
@@ -21,7 +21,7 @@ const propTypes: PropTypes<typeof GridItem> = {
   shadow: true,
 })
 export class GridItem {
-  @Element() public host!: HTMLElement & GridItemInternalHTMLProps;
+  @Element() public host!: HTMLElement;
 
   /** The size of the column. Can be between 1 and 12. Also defines the size of the column for specific breakpoints, like {base: 6, l: 3}. You always need to provide a base value when doing this. */
   @Prop() public size?: BreakpointCustomizable<GridItemSize> = 1;
