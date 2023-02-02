ca029cf1f8a911337a72ad53411f6fccab188b8a
Matthias Haegele <matthias.haegele@mhp.com>
2023-02-02 12:08:48 +0100

    test(components-js): update fixtures with custom button | mh | #2141

--- packages/components-js/tests/vrt/puppeteer/specs/scroller.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/scroller.vrt.ts
@@ -46,9 +46,9 @@ it('should have no visual regression for :hover + :focus-visible', async () => {
           .forEach((scroller) => ((scroller as any).scrollToPosition = { scrollPosition: 100 }))
       );
 
-      await forceHoverState(page, '.hover p-scroller >>> p-button-pure >>> button'); // Scroll indicator hover
+      await forceHoverState(page, '.hover p-scroller >>> button'); // Scroll indicator hover
       await forceFocusState(page, '.focus p-scroller >>> .scroll-wrapper');
-      await forceHoverState(page, '.focus-hover p-scroller >>> p-button-pure >>> button');
+      await forceHoverState(page, '.focus-hover p-scroller >>> button');
       await forceFocusState(page, '.focus-hover p-scroller >>> .scroll-wrapper');
     })
   ).toBeFalsy();
