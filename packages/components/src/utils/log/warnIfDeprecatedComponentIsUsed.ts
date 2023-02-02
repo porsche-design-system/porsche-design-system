a5126400decdcc5bfb10ace82a69ef61f81482dd
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-02 13:30:35 +0100

    feat(components): use same deprecation warning function for link-social and grid | sas | #2155

--- packages/components/src/utils/log/warnIfDeprecatedComponentIsUsed.ts
+++ packages/components/src/utils/log/warnIfDeprecatedComponentIsUsed.ts
@@ -1,6 +1,5 @@
 import { getTagNameWithoutPrefix } from '../tag-name';
 
-// TODO: add missing unit test
 export const warnIfDeprecatedComponentIsUsed = (host: HTMLElement, message?: string): void => {
   console.warn(
     `[Porsche Design System] Component "${getTagNameWithoutPrefix(
--- packages/components/src/utils/validation/warnIfComponentIsDeprecated.ts
+++
@@ -1,5 +0,0 @@
-import { getTagName } from '../../utils';
-
-export const warnIfComponentIsDeprecated = (host: HTMLElement, message: string): void => {
-  console.warn(`${getTagName(host)}: ${message}`);
-};
