248d1132f97247ee8ccdb663cab4c96ced647d87
Anastasiia Antonova <antonova64@gmail.com>
2023-02-03 07:26:13 +0100

    feat(components): carousel - unit test update | aa | #2127

--- packages/components/src/components/carousel/carousel-styles.spec.ts
+++ packages/components/src/components/carousel/carousel-styles.spec.ts
@@ -2,12 +2,12 @@ import { getComponentCss } from './carousel-styles';
 
 xdescribe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, 'dark'],
-    [false, false, 'light'],
-    [false, true, 'light'],
-    [true, false, 'light'],
-    [true, true, 'light'],
-    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'light'],
+    [false, false, 'dark', 'left'],
+    [false, false, 'light', 'left'],
+    [false, true, 'light', 'left'],
+    [true, false, 'light', 'center'],
+    [true, true, 'light', 'center'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'light', 'center'],
   ])('should return correct css for wrapContent: %s, disablePagination: %j and theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
