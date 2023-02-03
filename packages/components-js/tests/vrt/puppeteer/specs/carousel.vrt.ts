859e97e6688e37cafd943e7e87e308434780d617
Anastasiia Antonova <antonova64@gmail.com>
2023-02-03 16:21:49 +0100

    test(components-js): carousel - enable vrt tests | aa | #2127

--- packages/components-js/tests/vrt/puppeteer/specs/carousel.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/carousel.vrt.ts
@@ -14,11 +14,11 @@ import {
 } from '@porsche-design-system/shared/testing';
 import type { Theme } from '@porsche-design-system/utilities-v2';
 
-xit.each(extendedViewports)('should have no visual regression for viewport %s', async (viewport) => {
+it.each(extendedViewports)('should have no visual regression for viewport %s', async (viewport) => {
   expect(await vrtTest(getVisualRegressionTester(viewport), 'carousel', '/#carousel')).toBeFalsy();
 });
 
-xit('should have no visual regression for :hover + :focus-visible', async () => {
+it('should have no visual regression for :hover + :focus-visible', async () => {
   const vrt = getVisualRegressionStatesTester();
   expect(
     await vrt.test('carousel-states', async () => {
