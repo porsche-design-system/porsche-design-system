e3c136e3352a7034dd735d9c864f302b774bd9f2
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-05 17:20:52 +0100

    test(components-angular): move component vrt specs to dedicated folder and setup design-tokens vrt specs | #2182

--- packages/components-angular/tests/vrt/specs/utilities-grid.vrt.ts
+++
@@ -1,8 +0,0 @@
-import { extendedViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
-
-// TODO: due to rounding differences between SCSS and JS, screenshot can't be in in sync with React integration
-xit.each(extendedViewports)('should have no visual regression for viewport %s', async (viewport) => {
-  expect(
-    await vrtTest(getVisualRegressionTester(viewport), 'utilities-grid-example', '/utilities-grid-example')
-  ).toBeFalsy();
-});
