55eeacc1a46d8701fa4155ed399587b90689ae3d
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-06 10:40:22 +0100

    test(components): update style snapshot for text | sas | #2131

--- packages/components/src/components/text/text-styles.spec.ts
+++ packages/components/src/components/text/text-styles.spec.ts
@@ -1,10 +1,12 @@
-import { getComponentCss, getSlottedCss } from './text-styles';
+import { getComponentCss } from './text-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
+    ['small', 'regular', 'left', 'primary', false, 'light'],
     ['small', 'regular', 'left', 'default', false, 'light'],
     ['x-large', 'thin', 'right', 'brand', true, 'dark'],
     ['x-large', 'regular', 'center', 'brand', false, 'dark'],
+    ['large', 'semi-bold', 'left', 'notification-info', true, 'light'],
     ['large', 'semibold', 'left', 'notification-error', true, 'light'],
     ['medium', 'bold', 'right', 'neutral-contrast-high', true, 'dark'],
     ['x-small', 'semibold', 'left', 'inherit', false, 'light'],
