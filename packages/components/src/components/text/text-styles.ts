8390a876e764a12d3dacf811f07644ba62b2169f
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-06 09:31:09 +0100

    feat(components): refactor text matching new design language | sas | #2131

--- packages/components/src/components/text/text-styles.ts
+++ packages/components/src/components/text/text-styles.ts
@@ -1,11 +1,11 @@
-import type { JssStyle } from 'jss';
 import type { BreakpointCustomizable, TextAlign, TextColor, TextSize, TextWeight, Theme } from '../../types';
-import { buildResponsiveStyles, getCss, textMap } from '../../utils';
+import { buildResponsiveStyles, getCss, textSizeMap } from '../../utils';
 import { addImportantToEachRule } from '../../styles';
 import { textSmallStyle } from '@porsche-design-system/utilities-v2';
 import { getEllipsisJssStyle, getSlottedTypographyJssStyle } from '../../styles/typography-styles';
 import { getThemedTextColor } from '../../styles/text-icon-styles';
 import { getFontWeight } from '../../styles/font-weight-styles';
+import { hostHiddenStyles } from '../../styles/host-hidden-styles';
 
 export const getComponentCss = (
   size: BreakpointCustomizable<TextSize>,
@@ -15,21 +15,10 @@ export const getComponentCss = (
   ellipsis: boolean,
   theme: Theme
 ): string => {
-  // function is local to reuse `weight` parameter
-  // TODO: font shorthand isn't really the best choice but we don't have any better alternative atm
-  const getSizeJssStyle = (textSize: TextSize): JssStyle => {
-    const fontWeightValue = getFontWeight(weight);
-    return textSize === 'inherit'
-      ? {
-          fontSize: textSize,
-          fontWeight: fontWeightValue,
-        }
-      : { font: textMap[textSize].font.replace('400', fontWeightValue) };
-  };
-
   return getCss({
     '@global': {
       ':host': {
+        ...hostHiddenStyles,
         display: 'block',
       },
       '::slotted': {
@@ -40,15 +29,19 @@ export const getComponentCss = (
     },
     root: {
       display: 'inherit',
-      padding: 0,
       margin: 0,
+      padding: 0,
       textAlign: align,
       ...textSmallStyle,
+      letterSpacing: 'normal',
       color: getThemedTextColor(theme, color),
       listStyleType: 'none',
       whiteSpace: 'inherit',
       ...(ellipsis && getEllipsisJssStyle()),
-      ...buildResponsiveStyles(size, getSizeJssStyle),
+      ...buildResponsiveStyles(size, (sizeValue: TextSize) => ({
+        fontSize: sizeValue === 'inherit' ? sizeValue : textSizeMap[sizeValue],
+        fontWeight: getFontWeight(weight),
+      })),
     },
   });
 };
