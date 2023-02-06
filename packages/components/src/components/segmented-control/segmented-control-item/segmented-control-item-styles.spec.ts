0f6a076f26cc02075cd34437d798f093e548ac3f
Anastasiia Antonova <antonova64@gmail.com>
2023-02-06 14:16:14 +0100

    test(components): segmented control - theme as last parameter - unit tests update | aa | #2142

--- packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-styles.spec.ts
+++ packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-styles.spec.ts
@@ -2,14 +2,17 @@ import { getComponentCss } from './segmented-control-item-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, 'background-default', 'light'],
-    [true, false, 'background-default', 'light'],
-    [true, true, 'background-default', 'light'],
-    [false, true, 'background-default', 'light'],
-    [false, false, 'background-surface', 'light'],
-    [false, false, 'background-default', 'dark'],
-    [false, false, 'background-surface', 'dark'],
-  ])('should return correct css for isDisabled: %s, isSelected: %s, bgColor: %s and theme: %s', (...args) => {
-    expect(getComponentCss(...args)).toMatchSnapshot();
-  });
+    [false, false, 'background-default', 'increase', undefined, 'light'],
+    [true, false, 'background-default', 'increase', undefined, 'light'],
+    [true, true, 'background-default', undefined, 'iconSource', 'light'],
+    [false, true, 'background-default', undefined, 'iconSource', 'light'],
+    [false, false, 'background-surface', 'question', undefined, 'light'],
+    [false, false, 'background-default', 'lock', undefined, 'dark'],
+    [false, false, 'background-surface', undefined, undefined, 'dark'],
+  ])(
+    'should return correct css for isDisabled: %s, isSelected: %s, bgColor: %s, icon: %s, iconSource: %s and theme: %s',
+    (...args) => {
+      expect(getComponentCss(...args)).toMatchSnapshot();
+    }
+  );
 });
