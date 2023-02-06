861ffdc1bb65f661e465a6bb14256caacdb29cb1
Anastasiia Antonova <antonova64@gmail.com>
2023-02-06 14:26:16 +0100

    test(components): segmented control - get css refactoring - unit tests update | aa | #2142

--- packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-styles.spec.ts
+++ packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-styles.spec.ts
@@ -2,13 +2,13 @@ import { getComponentCss } from './segmented-control-item-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, 'background-default', 'increase', undefined, 'light'],
-    [true, false, 'background-default', 'increase', undefined, 'light'],
-    [true, true, 'background-default', undefined, 'iconSource', 'light'],
-    [false, true, 'background-default', undefined, 'iconSource', 'light'],
-    [false, false, 'background-surface', 'question', undefined, 'light'],
-    [false, false, 'background-default', 'lock', undefined, 'dark'],
-    [false, false, 'background-surface', undefined, undefined, 'dark'],
+    [false, false, 'background-default', true, 'light'],
+    [true, false, 'background-default', true, 'light'],
+    [true, true, 'background-default', true, 'light'],
+    [false, true, 'background-default', true, 'light'],
+    [false, false, 'background-surface', false, 'light'],
+    [false, false, 'background-default', false, 'dark'],
+    [false, false, 'background-surface', false, 'dark'],
   ])(
     'should return correct css for isDisabled: %s, isSelected: %s, bgColor: %s, icon: %s, iconSource: %s and theme: %s',
     (...args) => {
