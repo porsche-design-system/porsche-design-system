1d67117e8193f5e2e30394c64ed323eb9a4a5cf1
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-07 10:56:06 +0100

    feat(components): content-wrapper is in sync with grid styles | sas / oh | #2310

--- packages/components/src/components/content-wrapper/content-wrapper-styles-shared.ts
+++ packages/components/src/components/content-wrapper/content-wrapper-styles-shared.ts
@@ -1,28 +1,53 @@
 import type { JssStyle } from 'jss';
 import type { ContentWrapperWidth } from './content-wrapper-utils';
-import { getMediaQueryMin, gridSafeZoneBase, gridSafeZoneXXL, gridWidthMax } from '@porsche-design-system/utilities-v2';
+import {
+  getMediaQueryMin,
+  gridGap,
+  gridSafeZoneBase,
+  gridSafeZoneXXL,
+  gridWidthMax,
+  gridWidthMin,
+} from '@porsche-design-system/utilities-v2';
 
-const widthMap: { [key in ContentWrapperWidth]?: JssStyle } = {
-  basic: {
-    margin: '0 auto',
+const oneColumnWidthS = `calc((100% - ${gridSafeZoneBase} * 2 - ${gridGap} * 13) / 14)`;
+const oneColumnWidthXXL = `calc((min(100%, ${gridWidthMax}) - ${gridSafeZoneXXL} * 2 - ${gridGap} * 13) / 14)`;
+
+const widthMap: { [key in Exclude<ContentWrapperWidth, 'full' | 'fluid'>]: JssStyle } = {
+  narrow: {
     padding: `0 ${gridSafeZoneBase}`,
-    maxWidth: gridWidthMax,
+    boxSizing: 'border-box',
+    [getMediaQueryMin('s')]: {
+      padding: `0 calc(${gridSafeZoneBase} + ${gridGap} * 3 + ${oneColumnWidthS} * 3)`,
+    },
+    [getMediaQueryMin('xxl')]: {
+      padding: `0 calc(${gridSafeZoneXXL} + ${gridGap} * 3 + ${oneColumnWidthXXL} * 3)`,
+    },
+  },
+  basic: {
+    padding: `0 ${gridSafeZoneBase}`,
+    boxSizing: 'border-box',
+    [getMediaQueryMin('s')]: {
+      padding: `0 calc(${gridSafeZoneBase} + ${gridGap} + ${oneColumnWidthS})`,
+    },
+    [getMediaQueryMin('xxl')]: {
+      padding: `0 calc(${gridSafeZoneXXL} + ${gridGap} + ${oneColumnWidthXXL})`,
+    },
+  },
+  extended: {
+    padding: `0 ${gridSafeZoneBase}`,
     boxSizing: 'border-box',
     [getMediaQueryMin('xxl')]: {
       padding: `0 ${gridSafeZoneXXL}`,
     },
   },
-  extended: {
-    margin: '0 auto',
-    maxWidth: gridWidthMax,
-  },
 };
 
-// TODO: maybe this should be part of utilities sub package as "getGridWrapperStyle"?
 export const getContentWrapperStyle = (width: ContentWrapperWidth): JssStyle => {
   return {
     width: '100%',
-    minWidth: 0, // needed for possible flex context within content-wrapper
+    margin: '0 auto',
+    minWidth: gridWidthMin,
+    maxWidth: gridWidthMax,
     ...widthMap[width],
   };
 };
--- packages/components/src/components/content-wrapper/content-wrapper-utils.ts
+++ packages/components/src/components/content-wrapper/content-wrapper-utils.ts
@@ -1,5 +1,6 @@
 export const CONTENT_WRAPPER_BACKGROUND_COLORS = ['transparent', 'default'] as const;
 export type ContentWrapperBackgroundColor = typeof CONTENT_WRAPPER_BACKGROUND_COLORS[number];
 
-export const CONTENT_WRAPPER_WIDTHS = ['basic', 'extended', 'fluid'] as const;
+// fluid is deprecated and will be mapped to full
+export const CONTENT_WRAPPER_WIDTHS = ['narrow', 'basic', 'extended', 'fluid', 'full'] as const;
 export type ContentWrapperWidth = typeof CONTENT_WRAPPER_WIDTHS[number];
