4aebd4a3f9cc93bbdf650bd94a779770bf91f132
Bernd Hacker <hacker.bernd@gmail.com>
2023-02-08 10:16:41 +0100

    feat(components): deprecate content-wrapper | bh

--- packages/components/src/components/content-wrapper/content-wrapper.tsx
+++ packages/components/src/components/content-wrapper/content-wrapper.tsx
@@ -15,6 +15,7 @@ const propTypes: PropTypes<typeof ContentWrapper> = {
   theme: AllowedTypes.oneOf<Theme>(THEMES),
 };
 
+/** @deprecated since v3.0.0, will be removed with next major release. Use native CSS Grid instead. */
 @Component({
   tag: 'p-content-wrapper',
   shadow: true,
