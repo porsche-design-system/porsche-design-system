80b53bdb4693e14b5a543195d718f728d52f053f
Anastasiia Antonova <antonova64@gmail.com>
2023-02-09 16:24:03 +0100

    feat(components): carousel styles - unit tests update | aa | #2127

--- packages/components/src/components/carousel/carousel-styles.spec.ts
+++ packages/components/src/components/carousel/carousel-styles.spec.ts
@@ -2,13 +2,16 @@ import { getComponentCss } from './carousel-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, 'dark', 'left'],
-    [false, false, 'light', 'left'],
-    [false, true, 'light', 'left'],
-    [true, false, 'light', 'center'],
-    [true, true, 'light', 'center'],
-    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'light', 'center'],
-  ])('should return correct css for wrapContent: %s, disablePagination: %j and theme: %s', (...args) => {
-    expect(getComponentCss(...args)).toMatchSnapshot();
-  });
+    [false, false, 400, 'left', 'dark'],
+    [false, false, 400, 'left', 'light'],
+    [false, true, 400, 'left', 'light'],
+    [true, false, 400, 'center', 'light'],
+    [true, true, 400, 'center', 'light'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 400, 'center', 'light'],
+  ])(
+    'should return correct css for wrapContent: %s, disablePagination: %j, splideSpeed: %s, alignHeader: %s and theme: %s',
+    (...args) => {
+      expect(getComponentCss(...args)).toMatchSnapshot();
+    }
+  );
 });
