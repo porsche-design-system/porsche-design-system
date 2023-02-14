7c6a0271dcdc28c6a396f9a20ff4188829795c91
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-14 15:14:05 +0100

    test(utilities-deprecated/utilities): update FONT_FACE_CDN_URL | sas | #2306

--- packages/utilities-deprecated/projects/utilities/src/js/index.ts
+++ packages/utilities-deprecated/projects/utilities/src/js/index.ts
@@ -10,7 +10,7 @@ declare global {
 }
 
 const isCdnCn = typeof window !== 'undefined' && window.PORSCHE_DESIGN_SYSTEM_CDN === 'cn';
-export const FONT_FACE_CDN_URL = (isCdnCn ? 'https://cdn.ui.porsche.cn' : 'https://cdn.ui.porsche.com') + '/porsche-design-system/styles/' + (isCdnCn ? 'font-face.min.cn.ec02d2159cb7c560e10be175ce8af166.css' : 'font-face.min.13dbdc94bb389e67a5c1bd140e500366.css');
+export const FONT_FACE_CDN_URL = (isCdnCn ? 'https://cdn.ui.porsche.cn' : 'https://cdn.ui.porsche.com') + '/porsche-design-system/styles/' + (isCdnCn ? 'font-face.min.cn.dbb9b1f73aba20c5fad38212405e1a68.css' : 'font-face.min.955063f599f3c828248b49f258753ba8.css');
 
 /**
  * @deprecated since v1.1.0.
