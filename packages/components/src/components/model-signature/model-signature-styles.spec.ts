0627743b72b00402d385b17c5dad944a12327bc8
Bernd Hacker <hacker.bernd@gmail.com>
2023-02-15 11:38:27 +0100

    test(components): prepare model-signature-styles unit test | bh | #2136

--- packages/components/src/components/model-signature/model-signature-styles.spec.ts
+++ packages/components/src/components/model-signature/model-signature-styles.spec.ts
@@ -1,10 +1,24 @@
 import { getComponentCss } from './model-signature-styles';
 
 xdescribe('getComponentCss()', () => {
-  it.each<Parameters<typeof getComponentCss>>([['718', 'small', 'primary', 'light']])(
-    'should return correct css for size: %s and theme: %s',
-    (...args) => {
-      expect(getComponentCss(...args)).toMatchSnapshot();
-    }
-  );
+  it.each<Parameters<typeof getComponentCss>>([
+    ['718', 'small', 'primary', 'light'],
+    ['718', 'small', 'contrast-low', 'light'],
+    ['718', 'small', 'contrast-medium', 'light'],
+    ['718', 'small', 'contrast-high', 'light'],
+    ['718', 'small', 'inherit', 'light'],
+    ['718', 'medium', 'primary', 'light'],
+    ['718', 'inherit', 'primary', 'light'],
+    ['718', 'inherit', 'inherit', 'light'],
+    ['718', 'small', 'primary', 'dark'],
+    ['718', 'small', 'contrast-low', 'dark'],
+    ['718', 'small', 'contrast-medium', 'dark'],
+    ['718', 'small', 'contrast-high', 'dark'],
+    ['718', 'small', 'inherit', 'dark'],
+    ['718', 'medium', 'primary', 'dark'],
+    ['718', 'inherit', 'primary', 'dark'],
+    ['718', 'inherit', 'inherit', 'dark'],
+  ])('should return correct css for model: %s, size: %s, color: %s and theme: %s', (...args) => {
+    expect(getComponentCss(...args)).toMatchSnapshot();
+  });
 });
