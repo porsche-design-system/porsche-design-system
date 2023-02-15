dd12a88b36ea6f5bf718e8988baef06641330e1e
Bernd Hacker <hacker.bernd@gmail.com>
2023-02-15 14:01:35 +0100

    feat(storefront): add label to SelectOptions | bh | #2136

--- packages/storefront/src/components/SelectOptions.vue
+++ packages/storefront/src/components/SelectOptions.vue
@@ -1,14 +1,18 @@
 <template>
-  <select :value="value" aria-label="label" @input="$emit('input', $event.target.value)">
-    <option disabled>{{ label }}</option>
-    <option v-for="value in values" v-bind:key="value" :value="value">{{ value }}</option>
-  </select>
+  <label>
+    <p-text :theme="theme">{{ name }}:</p-text>
+    <select :value="value" aria-label="label" @input="$emit('input', $event.target.value)">
+      <option disabled>{{ label }}</option>
+      <option v-for="value in values" v-bind:key="value" :value="value">{{ value }}</option>
+    </select>
+  </label>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import Component from 'vue-class-component';
   import { Prop } from 'vue-property-decorator';
+  import type { Theme } from '@/models';
 
   @Component
   export default class SelectOptions extends Vue {
@@ -18,6 +22,10 @@
 
     get label(): string {
       return `Select ${this.name}`;
+    }
+
+    get theme(): Theme {
+      return this.$store.getters.theme;
     }
   }
 </script>
