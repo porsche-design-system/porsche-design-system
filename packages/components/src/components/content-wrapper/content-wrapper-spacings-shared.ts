a92955ef15791d08275a09ca8248f5b52d1cb8f9
Anastasiia Antonova <antonova64@gmail.com>
2023-02-20 12:07:28 +0100

    feat(components): content wrapper shared spacings - improve type | aa | #2127

--- packages/components/src/components/content-wrapper/content-wrapper-spacings-shared.ts
+++ packages/components/src/components/content-wrapper/content-wrapper-spacings-shared.ts
@@ -1,13 +1,15 @@
 import type { ContentWrapperWidth } from './content-wrapper-utils';
 import { gridGap, gridSafeZoneBase, gridSafeZoneXXL, gridWidthMax } from '@porsche-design-system/utilities-v2';
 
+type PartiallyOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>; // possibility to define some keys to be optional
 export const oneColumnWidthBase = `calc((100% - ${gridSafeZoneBase} * 2 - ${gridGap} * 5) / 6)`;
 export const oneColumnWidthS = `calc((100% - ${gridSafeZoneBase} * 2 - ${gridGap} * 13) / 14)`;
 export const oneColumnWidthXXL = `calc((min(100%, ${gridWidthMax}) - ${gridSafeZoneXXL} * 2 - ${gridGap} * 13) / 14)`;
 export const offsetHorizontalXXL = `max(0px, (100% - ${gridWidthMax}) / 2)`;
 
 export type GridSpacingWidth = Exclude<ContentWrapperWidth, 'full' | 'fluid'>;
-export type GridSpacingDataForWidth = [string, Partial<{ [key in 's' | 'xxl']: string }>];
+// I think it's better (safer) to explicitly define, which keys are optional and which not. If the format changes in the future - we can adjust the type
+export type GridSpacingDataForWidth = [string, PartiallyOptional<{ [key in 's' | 'xxl']: string }, 's'>]; // 's' is an optional key
 export type GridSpacingMap = { [key in GridSpacingWidth]: GridSpacingDataForWidth };
 
 export const gridSpacingMapConfig: GridSpacingMap = {
