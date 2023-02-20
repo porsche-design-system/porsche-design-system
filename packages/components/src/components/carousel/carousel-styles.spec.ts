af24c41da1d3618333a2eb0047841a7b45f5ce3d
Anastasiia Antonova <antonova64@gmail.com>
2023-02-20 13:57:25 +0100

    feat(components): carousel styles unit test spec update | aa | #2127

--- packages/components/src/components/carousel/carousel-styles.spec.ts
+++ packages/components/src/components/carousel/carousel-styles.spec.ts
@@ -5,9 +5,15 @@ describe('getComponentCss()', () => {
     ['basic', false, 400, 'left', 'dark'],
     ['basic', false, 400, 'left', 'light'],
     ['basic', true, 400, 'left', 'light'],
+    ['basic', false, 400, 'center', 'dark'],
+    ['basic', false, 400, 'center', 'light'],
+    ['basic', true, 400, 'center', 'light'],
     ['extended', false, 400, 'center', 'light'],
     ['extended', true, 400, 'center', 'light'],
     ['extended', { base: true, xs: false, s: true, m: false, l: true, xl: false }, 400, 'center', 'light'],
+    ['extended', false, 400, 'left', 'light'],
+    ['extended', true, 400, 'left', 'light'],
+    ['extended', { base: true, xs: false, s: true, m: false, l: true, xl: false }, 400, 'left', 'light'],
   ])(
     'should return correct css for wrapContent: %s, disablePagination: %j, splideSpeed: %s, alignHeader: %s and theme: %s',
     (...args) => {
