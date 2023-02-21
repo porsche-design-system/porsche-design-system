36d99b311df05323cf8bac9f7379927e95bdbc60
Anastasiia Antonova <antonova64@gmail.com>
2023-02-21 17:13:33 +0100

    test(components): carousel styles unit test - change test parameters | aa | #2127

--- packages/components/src/components/carousel/carousel-styles.spec.ts
+++ packages/components/src/components/carousel/carousel-styles.spec.ts
@@ -15,7 +15,7 @@ describe('getComponentCss()', () => {
     ['extended', true, 400, 'left', 'light'],
     ['extended', { base: true, xs: false, s: true, m: false, l: true, xl: false }, 400, 'left', 'light'],
   ])(
-    'should return correct css for wrapContent: %s, disablePagination: %j, splideSpeed: %s, alignHeader: %s and theme: %s',
+    'should return correct css for width: %s, disablePagination: %j, splideSpeed: %s, alignHeader: %s and theme: %s',
     (...args) => {
       expect(getComponentCss(...args)).toMatchSnapshot();
     }
