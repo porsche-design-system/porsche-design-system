4fca0fe5cab13657cbca5650a4b39094d434c653
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-21 13:14:18 +0100

    test: update vrt scenario for text-list | sas | #2153

--- packages/components-angular/src/app/pages/text-list.component.ts
+++ packages/components-angular/src/app/pages/text-list.component.ts
@@ -6,97 +6,153 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   template: `
     <div class="playground light" title="should show unordered text list on light background">
       <p-text-list>
-        <p-text-list-item>Unordered quick brown fox jumps over the lazy dog</p-text-list-item>
+        <p-text-list-item>Unordered root list item</p-text-list-item>
         <p-text-list-item>
-          Unordered quick brown fox jumps over the lazy dog
+          Unordered root list item
           <p-text-list>
-            <p-text-list-item>Unordered quick brown fox jumps over the lazy dog</p-text-list-item>
-            <p-text-list-item>
-              Unordered quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-            </p-text-list-item>
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
           </p-text-list>
         </p-text-list-item>
-        <p-text-list-item>Unordered quick brown fox jumps over the lazy dog</p-text-list-item>
+        <p-text-list-item>Unordered root list item</p-text-list-item>
       </p-text-list>
     </div>
 
     <div class="playground dark" title="should show unordered text list on dark background">
       <p-text-list [theme]="'dark'">
-        <p-text-list-item>Unordered quick brown fox jumps over the lazy dog</p-text-list-item>
+        <p-text-list-item>Unordered root list item</p-text-list-item>
         <p-text-list-item>
-          Unordered quick brown fox jumps over the lazy dog
+          Unordered root list item
           <p-text-list [theme]="'dark'">
-            <p-text-list-item>Unordered quick brown fox jumps over the lazy dog</p-text-list-item>
-            <p-text-list-item>
-              Unordered quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-            </p-text-list-item>
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
           </p-text-list>
         </p-text-list-item>
-        <p-text-list-item>Unordered quick brown fox jumps over the lazy dog</p-text-list-item>
+        <p-text-list-item>Unordered root list item</p-text-list-item>
       </p-text-list>
     </div>
 
-    <div class="playground light" title="should show ordered text list numbered on light background">
+    <div class="playground light" title="should show ordered text list Numbered, on light background">
       <p-text-list [listType]="'ordered'">
-        <p-text-list-item>Ordered quick brown fox jumps over the lazy dog</p-text-list-item>
         <p-text-list-item>
-          Ordered quick brown fox jumps over the lazy dog
-          <p-text-list [listType]="'ordered'">
-            <p-text-list-item>Ordered quick brown fox jumps over the lazy dog</p-text-list-item>
-            <p-text-list-item>
-              Ordered quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-            </p-text-list-item>
+          Numbered, ordered root list item
+          <p-text-list [listType]="'unordered'">
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
           </p-text-list>
         </p-text-list-item>
-        <p-text-list-item>Ordered quick brown fox jumps over the lazy dog</p-text-list-item>
+        <p-text-list-item>
+          Numbered, ordered root list item
+          <p-text-list [listType]="'ordered'">
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+          </p-text-list>
+        </p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
       </p-text-list>
     </div>
 
-    <div class="playground dark" title="should show ordered text list numbered on dark background">
+    <div class="playground dark" title="should show ordered text list Numbered, on dark background">
       <p-text-list [listType]="'ordered'" [theme]="'dark'">
-        <p-text-list-item>Ordered quick brown fox jumps over the lazy dog</p-text-list-item>
         <p-text-list-item>
-          Ordered quick brown fox jumps over the lazy dog
-          <p-text-list [listType]="'ordered'" [theme]="'dark'">
-            <p-text-list-item>Ordered quick brown fox jumps over the lazy dog</p-text-list-item>
-            <p-text-list-item>
-              Ordered quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-            </p-text-list-item>
+          Numbered, ordered root list item
+          <p-text-list [listType]="'unordered'" [theme]="'dark'">
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
           </p-text-list>
         </p-text-list-item>
-        <p-text-list-item>Ordered quick brown fox jumps over the lazy dog</p-text-list-item>
+        <p-text-list-item>
+          Numbered, ordered root list item
+          <p-text-list [listType]="'ordered'" [theme]="'dark'">
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+          </p-text-list>
+        </p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
       </p-text-list>
     </div>
 
     <div class="playground light" title="should show ordered text list alphabetically on light background">
       <p-text-list [listType]="'ordered'" [orderType]="'alphabetically'">
-        <p-text-list-item>Ordered alphabetically quick brown fox jumps over the lazy dog</p-text-list-item>
         <p-text-list-item>
-          Ordered alphabetically quick brown fox jumps over the lazy dog
-          <p-text-list [listType]="'ordered'" [orderType]="'alphabetically'">
-            <p-text-list-item>Ordered alphabetically quick brown fox jumps over the lazy dog</p-text-list-item>
-            <p-text-list-item>
-              Ordered alphabetically quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-            </p-text-list-item>
+          Alphabetically, ordered root list item
+          <p-text-list [listType]="'unordered'">
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
           </p-text-list>
         </p-text-list-item>
-        <p-text-list-item>Ordered alphabetically quick brown fox jumps over the lazy dog</p-text-list-item>
+        <p-text-list-item>
+          Alphabetically, ordered root list item
+          <p-text-list [listType]="'ordered'" [orderType]="'alphabetically'">
+            <p-text-list-item>Alphabetically, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Alphabetically, ordered nested list item</p-text-list-item>
+          </p-text-list>
+        </p-text-list-item>
+        <p-text-list-item>Alphabetically, ordered root list item</p-text-list-item>
       </p-text-list>
     </div>
 
     <div class="playground dark" title="should show ordered text list alphabetically on dark background">
       <p-text-list [listType]="'ordered'" [orderType]="'alphabetically'" [theme]="'dark'">
-        <p-text-list-item>Ordered alphabetically quick brown fox jumps over the lazy dog</p-text-list-item>
         <p-text-list-item>
-          Ordered alphabetically quick brown fox jumps over the lazy dog
+          Alphabetically, ordered root list item
+          <p-text-list [listType]="'unordered'" [theme]="'dark'">
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
+            <p-text-list-item>Unordered nested list item</p-text-list-item>
+          </p-text-list>
+        </p-text-list-item>
+        <p-text-list-item>
+          Alphabetically, ordered root list item
           <p-text-list [listType]="'ordered'" [orderType]="'alphabetically'" [theme]="'dark'">
-            <p-text-list-item>Ordered alphabetically quick brown fox jumps over the lazy dog</p-text-list-item>
+            <p-text-list-item>Alphabetically, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Alphabetically, ordered nested list item</p-text-list-item>
+          </p-text-list>
+        </p-text-list-item>
+        <p-text-list-item>Alphabetically, ordered root list item</p-text-list-item>
+      </p-text-list>
+    </div>
+
+    <div class="playground light" title="should handle edge cases">
+      <p-text-list [listType]="'ordered'">
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
+        <p-text-list-item>
+          Numbered, ordered root list item
+          <p-text-list [listType]="'ordered'">
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+            <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
             <p-text-list-item>
-              Ordered alphabetically quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
+              Numbered, ordered nested list item
+              <p-text-list [listType]="'ordered'">
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+                <p-text-list-item>Numbered, ordered nested list item</p-text-list-item>
+              </p-text-list>
             </p-text-list-item>
           </p-text-list>
         </p-text-list-item>
-        <p-text-list-item>Ordered alphabetically quick brown fox jumps over the lazy dog</p-text-list-item>
+        <p-text-list-item>Numbered, ordered root list item</p-text-list-item>
       </p-text-list>
     </div>
 
--- packages/components-react/src/pages/TextList.tsx
+++ packages/components-react/src/pages/TextList.tsx
@@ -6,97 +6,153 @@ export const TextListPage = (): JSX.Element => {
     <>
       <div className="playground light" title="should show unordered text list on light background">
         <PTextList>
-          <PTextListItem>Unordered quick brown fox jumps over the lazy dog</PTextListItem>
+          <PTextListItem>Unordered root list item</PTextListItem>
           <PTextListItem>
-            Unordered quick brown fox jumps over the lazy dog
+            Unordered root list item
             <PTextList>
-              <PTextListItem>Unordered quick brown fox jumps over the lazy dog</PTextListItem>
-              <PTextListItem>
-                Unordered quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-              </PTextListItem>
+              <PTextListItem>Unordered nested list item</PTextListItem>
+              <PTextListItem>Unordered nested list item</PTextListItem>
             </PTextList>
           </PTextListItem>
-          <PTextListItem>Unordered quick brown fox jumps over the lazy dog</PTextListItem>
+          <PTextListItem>Unordered root list item</PTextListItem>
         </PTextList>
       </div>
 
       <div className="playground dark" title="should show unordered text list on dark background">
         <PTextList theme="dark">
-          <PTextListItem>Unordered quick brown fox jumps over the lazy dog</PTextListItem>
+          <PTextListItem>Unordered root list item</PTextListItem>
           <PTextListItem>
-            Unordered quick brown fox jumps over the lazy dog
+            Unordered root list item
             <PTextList theme="dark">
-              <PTextListItem>Unordered quick brown fox jumps over the lazy dog</PTextListItem>
-              <PTextListItem>
-                Unordered quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-              </PTextListItem>
+              <PTextListItem>Unordered nested list item</PTextListItem>
+              <PTextListItem>Unordered nested list item</PTextListItem>
             </PTextList>
           </PTextListItem>
-          <PTextListItem>Unordered quick brown fox jumps over the lazy dog</PTextListItem>
+          <PTextListItem>Unordered root list item</PTextListItem>
         </PTextList>
       </div>
 
-      <div className="playground light" title="should show ordered text list numbered on light background">
+      <div className="playground light" title="should show ordered text list Numbered, on light background">
         <PTextList listType="ordered">
-          <PTextListItem>Ordered quick brown fox jumps over the lazy dog</PTextListItem>
           <PTextListItem>
-            Ordered quick brown fox jumps over the lazy dog
-            <PTextList listType="ordered">
-              <PTextListItem>Ordered quick brown fox jumps over the lazy dog</PTextListItem>
-              <PTextListItem>
-                Ordered quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-              </PTextListItem>
+            Numbered, ordered root list item
+            <PTextList listType="unordered">
+              <PTextListItem>Unordered nested list item</PTextListItem>
+              <PTextListItem>Unordered nested list item</PTextListItem>
             </PTextList>
           </PTextListItem>
-          <PTextListItem>Ordered quick brown fox jumps over the lazy dog</PTextListItem>
+          <PTextListItem>
+            Numbered, ordered root list item
+            <PTextList listType="ordered">
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+            </PTextList>
+          </PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
         </PTextList>
       </div>
 
-      <div className="playground dark" title="should show ordered text list numbered on dark background">
+      <div className="playground dark" title="should show ordered text list Numbered, on dark background">
         <PTextList listType="ordered" theme="dark">
-          <PTextListItem>Ordered quick brown fox jumps over the lazy dog</PTextListItem>
           <PTextListItem>
-            Ordered quick brown fox jumps over the lazy dog
-            <PTextList listType="ordered" theme="dark">
-              <PTextListItem>Ordered quick brown fox jumps over the lazy dog</PTextListItem>
-              <PTextListItem>
-                Ordered quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-              </PTextListItem>
+            Numbered, ordered root list item
+            <PTextList listType="unordered" theme="dark">
+              <PTextListItem>Unordered nested list item</PTextListItem>
+              <PTextListItem>Unordered nested list item</PTextListItem>
             </PTextList>
           </PTextListItem>
-          <PTextListItem>Ordered quick brown fox jumps over the lazy dog</PTextListItem>
+          <PTextListItem>
+            Numbered, ordered root list item
+            <PTextList listType="ordered" theme="dark">
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+            </PTextList>
+          </PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
         </PTextList>
       </div>
 
       <div className="playground light" title="should show ordered text list alphabetically on light background">
         <PTextList listType="ordered" orderType="alphabetically">
-          <PTextListItem>Ordered alphabetically quick brown fox jumps over the lazy dog</PTextListItem>
           <PTextListItem>
-            Ordered alphabetically quick brown fox jumps over the lazy dog
-            <PTextList listType="ordered" orderType="alphabetically">
-              <PTextListItem>Ordered alphabetically quick brown fox jumps over the lazy dog</PTextListItem>
-              <PTextListItem>
-                Ordered alphabetically quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
-              </PTextListItem>
+            Alphabetically, ordered root list item
+            <PTextList listType="unordered">
+              <PTextListItem>Unordered nested list item</PTextListItem>
+              <PTextListItem>Unordered nested list item</PTextListItem>
             </PTextList>
           </PTextListItem>
-          <PTextListItem>Ordered alphabetically quick brown fox jumps over the lazy dog</PTextListItem>
+          <PTextListItem>
+            Alphabetically, ordered root list item
+            <PTextList listType="ordered" orderType="alphabetically">
+              <PTextListItem>Alphabetically, ordered nested list item</PTextListItem>
+              <PTextListItem>Alphabetically, ordered nested list item</PTextListItem>
+            </PTextList>
+          </PTextListItem>
+          <PTextListItem>Alphabetically, ordered root list item</PTextListItem>
         </PTextList>
       </div>
 
       <div className="playground dark" title="should show ordered text list alphabetically on dark background">
         <PTextList listType="ordered" orderType="alphabetically" theme="dark">
-          <PTextListItem>Ordered alphabetically quick brown fox jumps over the lazy dog</PTextListItem>
           <PTextListItem>
-            Ordered alphabetically quick brown fox jumps over the lazy dog
+            Alphabetically, ordered root list item
+            <PTextList listType="unordered" theme="dark">
+              <PTextListItem>Unordered nested list item</PTextListItem>
+              <PTextListItem>Unordered nested list item</PTextListItem>
+            </PTextList>
+          </PTextListItem>
+          <PTextListItem>
+            Alphabetically, ordered root list item
             <PTextList listType="ordered" orderType="alphabetically" theme="dark">
-              <PTextListItem>Ordered alphabetically quick brown fox jumps over the lazy dog</PTextListItem>
+              <PTextListItem>Alphabetically, ordered nested list item</PTextListItem>
+              <PTextListItem>Alphabetically, ordered nested list item</PTextListItem>
+            </PTextList>
+          </PTextListItem>
+          <PTextListItem>Alphabetically, ordered root list item</PTextListItem>
+        </PTextList>
+      </div>
+
+      <div className="playground light" title="should handle edge cases">
+        <PTextList listType="ordered">
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
+          <PTextListItem>
+            Numbered, ordered root list item
+            <PTextList listType="ordered">
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+              <PTextListItem>Numbered, ordered nested list item</PTextListItem>
               <PTextListItem>
-                Ordered alphabetically quick brown fox jumps over the lazy dog, the lazy dog jumps over the quick brown fox
+                Numbered, ordered nested list item
+                <PTextList listType="ordered">
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                  <PTextListItem>Numbered, ordered nested list item</PTextListItem>
+                </PTextList>
               </PTextListItem>
             </PTextList>
           </PTextListItem>
-          <PTextListItem>Ordered alphabetically quick brown fox jumps over the lazy dog</PTextListItem>
+          <PTextListItem>Numbered, ordered root list item</PTextListItem>
         </PTextList>
       </div>
 
