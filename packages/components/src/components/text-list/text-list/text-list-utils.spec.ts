4b6ee0f03bf66197b9e001a2660f18e6506f608a
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-02-21 16:45:12 +0100

    test(components): update utils unit test for text-list | sas | #2153

--- packages/components/src/components/text-list/text-list/text-list-utils.spec.ts
+++ packages/components/src/components/text-list/text-list/text-list-utils.spec.ts
@@ -1,19 +1,20 @@
+import type { ListType, OrderType } from './text-list-utils';
 import { isListTypeOrdered, isOrderTypeNumbered } from './text-list-utils';
 
 describe('isListTypeOrdered()', () => {
-  it.each<Parameters<typeof isListTypeOrdered>>([['ordered'], ['unordered']])(
-    'should return correct boolean for list type: %s',
-    (...args) => {
-      expect(isListTypeOrdered(...args)).toMatchSnapshot();
-    }
-  );
+  it.each<[ListType, boolean]>([
+    ['ordered', true],
+    ['unordered', false],
+  ])('should for %s return %s', (listType, expected) => {
+    expect(isListTypeOrdered(listType)).toBe(expected);
+  });
 });
 
 describe('isOrderTypeNumbered()', () => {
-  it.each<Parameters<typeof isOrderTypeNumbered>>([['numbered'], ['alphabetically']])(
-    'should return correct boolean for order type: %s',
-    (...args) => {
-      expect(isOrderTypeNumbered(...args)).toMatchSnapshot();
-    }
-  );
+  it.each<[OrderType, boolean]>([
+    ['numbered', true],
+    ['alphabetically', false],
+  ])('should for %s return %s', (orderType, expected) => {
+    expect(isOrderTypeNumbered(orderType)).toBe(expected);
+  });
 });
