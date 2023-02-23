bbf1a8205ad7dec56d7791b1712690784c7222c7
Anastasiia Antonova <antonova64@gmail.com>
2023-02-23 19:49:13 +0100

    feat(components): table - remove scroller styles | aa | #2147

--- packages/components/src/components/table/table/table-styles.ts
+++ packages/components/src/components/table/table/table-styles.ts
@@ -34,10 +34,6 @@ export const getComponentCss = (): string => {
     root: {
       position: 'relative',
     },
-    'scroll-area': {
-      overflow: 'auto visible',
-      ...getFocusJssStyle({ offset: -1 }),
-    },
     table: {
       position: 'relative',
       width: '100%',
@@ -46,38 +42,6 @@ export const getComponentCss = (): string => {
       textAlign: 'left',
       color: primaryColor,
       whiteSpace: 'nowrap',
-    },
-    'scroll-trigger': {
-      position: 'absolute',
-      top: 0,
-      right: '1px',
-      width: '1px',
-      height: '1px',
-      visibility: 'hidden',
-    },
-    'scroll-indicator': {
-      position: 'absolute',
-      top: 0,
-      right: 0,
-      bottom: 0,
-      paddingLeft: spacingStaticLarge,
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      background: 'linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 50%)',
-      '&::before': {
-        content: '""',
-        position: 'absolute',
-        top: 0,
-        right: 0,
-        bottom: 0,
-        width: pxToRemWithUnit(48),
-        pointerEvents: 'auto',
-      },
-    },
-    'scroll-button': {
-      padding: pxToRemWithUnit(12),
-      pointerEvents: 'auto',
     },
   });
 };
