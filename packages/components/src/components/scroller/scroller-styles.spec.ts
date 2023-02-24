4fe77de72cdada2e2758f25ae0ba05e1ad669f44
Anastasiia Antonova <antonova64@gmail.com>
2023-02-24 15:14:42 +0100

    feat(components): scroller unit test update - new scrollbar param | aa | #2147

--- packages/components/src/components/scroller/scroller-styles.spec.ts
+++ packages/components/src/components/scroller/scroller-styles.spec.ts
@@ -2,16 +2,16 @@ import { getComponentCss } from './scroller-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['default', false, false, 'center', 'light'],
-    ['surface', false, false, 'center', 'dark'],
-    ['surface', false, false, 'center', 'light'],
-    ['default', false, false, 'center', 'dark'],
-    ['default', false, false, 'top', 'light'],
-    ['default', true, false, undefined, 'light'],
-    ['default', false, true, undefined, 'dark'],
-    ['default', true, true, undefined, 'light'],
+    ['default', false, false, 'center', false, 'light'],
+    ['surface', false, false, 'center', false, 'dark'],
+    ['surface', false, false, 'center', false, 'light'],
+    ['default', false, false, 'center', false, 'dark'],
+    ['default', false, false, 'top', true, 'light'],
+    ['default', true, false, undefined, true, 'light'],
+    ['default', false, true, undefined, true, 'dark'],
+    ['default', true, true, undefined, true, 'light'],
   ])(
-    'should return correct css for gradientColorScheme: %s, isNextHidden: %s, isPrevHidden: %s and scrollIndicatorPosition: %s, theme: %s ',
+    'should return correct css for gradientColorScheme: %s, isNextHidden: %s, isPrevHidden: %s, scrollIndicatorPosition: %s, scrollbar: %s and theme: %s ',
     (...args) => {
       expect(getComponentCss(...args)).toMatchSnapshot();
     }
