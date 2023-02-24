d9ac5a529f0262684f78ab1ca107f0c3b131ed15
Anastasiia Antonova <antonova64@gmail.com>
2023-02-24 11:01:05 +0100

    feat(components): table head row comment | aa | #2147

--- packages/components/src/components/table/table-head-row/table-head-row-styles.ts
+++ packages/components/src/components/table/table-head-row/table-head-row-styles.ts
@@ -8,6 +8,7 @@ export const getComponentCss = (): string => {
         display: 'table-row',
         ...hostHiddenStyles,
         '&::before': {
+          // since "before" element counts as table cell, and we have "before" in table rows, we have to add it also to table-head-row to make it to have the same amount of cells
           content: '""',
         },
       }),
