293d52e15618eacd7068b3cf43a4de549ad210a3
Anastasiia Antonova <antonova64@gmail.com>
2023-02-28 16:19:51 +0100

    feat(components-angular): table - vrt tests demo adjustment | aa | #2147

--- packages/components-angular/src/app/pages/table.component.ts
+++ packages/components-angular/src/app/pages/table.component.ts
@@ -1,11 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { dataVrt, headVrt } from '@porsche-design-system/shared';
+import { Theme } from '@porsche-design-system/components-angular';
 
 @Component({
   selector: 'page-table',
   template: `
-    <div class="playground light" title="should render table">
-      <p-table caption="Some caption">
+    <div
+      [class]="'playground ' + theme"
+      [title]="'should render table with ' + theme + ' mode'"
+      *ngFor="let theme of themes"
+    >
+      <p-table caption="Some caption" [theme]="theme">
         <p-table-head>
           <p-table-head-row>
             <p-table-head-cell
@@ -15,12 +20,13 @@ import { dataVrt, headVrt } from '@porsche-design-system/shared';
               [multiline]="item.multiline"
               [style]="item.style?.minWidth ? 'min-width: 250px;' : null"
               [innerHTML]="item.name"
+              [theme]="theme"
             ></p-table-head-cell>
           </p-table-head-row>
         </p-table-head>
         <p-table-body>
-          <p-table-row *ngFor="let item of data">
-            <p-table-cell>
+          <p-table-row *ngFor="let item of data" [theme]="theme">
+            <p-table-cell [theme]="theme">
               <img
                 [src]="item.imageUrl"
                 [width]="item.imageWidth"
@@ -30,26 +36,30 @@ import { dataVrt, headVrt } from '@porsche-design-system/shared';
               />
               <span [innerHTML]="item.html"></span>
             </p-table-cell>
-            <p-table-cell [multiline]="true">{{ item.longText }}</p-table-cell>
-            <p-table-cell>{{ item.shortText }}</p-table-cell>
-            <p-table-cell>{{ item.shortText }}</p-table-cell>
-            <p-table-cell>{{ item.shortText }}</p-table-cell>
-            <p-table-cell>
-              <p-button-pure icon="edit" style="padding: .5rem;">Edit</p-button-pure>
-              <p-button-pure icon="delete" style="padding: .5rem;">Delete</p-button-pure>
+            <p-table-cell [multiline]="true" [theme]="theme">{{ item.longText }}</p-table-cell>
+            <p-table-cell [theme]="theme">{{ item.shortText }}</p-table-cell>
+            <p-table-cell [theme]="theme">{{ item.shortText }}</p-table-cell>
+            <p-table-cell [theme]="theme">{{ item.shortText }}</p-table-cell>
+            <p-table-cell [theme]="theme">
+              <p-button-pure [theme]="theme" icon="edit" style="padding: .5rem;">Edit</p-button-pure>
+              <p-button-pure [theme]="theme" icon="delete" style="padding: .5rem;">Delete</p-button-pure>
             </p-table-cell>
           </p-table-row>
         </p-table-body>
       </p-table>
     </div>
 
-    <div class="playground light" title="should render table with unstyled slotted caption">
-      <p-table>
+    <div
+      [class]="'playground ' + theme"
+      [title]="'should render table with unstyled slotted caption with ' + theme + ' mode'"
+      *ngFor="let theme of themes"
+    >
+      <p-table [theme]="theme">
         <span slot="caption">Some unstyled caption <a href="#">with a link</a></span>
         <p-table-head>
           <p-table-head-row>
-            <p-table-head-cell>Column 1</p-table-head-cell>
-            <p-table-head-cell>Column 2</p-table-head-cell>
+            <p-table-head-cell [theme]="theme">Column 1</p-table-head-cell>
+            <p-table-head-cell [theme]="theme">Column 2</p-table-head-cell>
           </p-table-head-row>
         </p-table-head>
       </p-table>
@@ -60,4 +70,5 @@ import { dataVrt, headVrt } from '@porsche-design-system/shared';
 export class TableComponent {
   public head = headVrt;
   public data = dataVrt;
+  public themes: Theme[] = ['light', 'dark'];
 }
