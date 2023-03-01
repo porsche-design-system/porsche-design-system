f70dced928c950c486ede16fe4a1a8b32fd9bbed
Anastasiia Antonova <antonova64@gmail.com>
2023-03-01 14:03:44 +0100

    feat(shared): puppeteer config - revert changes | aa | #2147

--- packages/shared/src/testing/jest-puppeteer.config.ts
+++ packages/shared/src/testing/jest-puppeteer.config.ts
@@ -2,12 +2,17 @@ import type { PuppeteerNodeLaunchOptions } from 'puppeteer';
 
 const launch: PuppeteerNodeLaunchOptions = {
   headless: true,
-  ignoreDefaultArgs: ['--hide-scrollbars'],
   defaultViewport: {
     width: 1920,
     height: 800,
   },
-  args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-web-security'],
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--single-process',
+    '--disable-web-security',
+  ],
 };
 
 const hasServer = !!process.env.PORT;
