08bffe0a03a7c5f24f0f37015a1b9c232c321f33
TatjanaGartner <tatjana.gartner@mhp.com>
2023-03-01 11:24:43 +0100

    feat(components): add condition for ROLLUP_REPLACE_IS_STAGING | tg | #2331

--- packages/components/rollup-utils.config.js
+++ packages/components/rollup-utils.config.js
@@ -30,9 +30,11 @@ ${utilsExports}
 
 generateUtilsEntryFile();
 
+const isDevBuild = process.env.PDS_IS_STAGING === '1';
+
 const sharedPlugins = [
   replace({
-    ROLLUP_REPLACE_IS_STAGING: '"production"',
+    ROLLUP_REPLACE_IS_STAGING: isDevBuild ? '"staging"' : '"production"',
     preventAssignment: true,
   }),
   commonjs(),
