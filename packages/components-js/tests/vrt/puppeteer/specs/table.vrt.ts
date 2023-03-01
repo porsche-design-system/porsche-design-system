c2d6083cb702a068cf6368805a45b81c3fb728bf
Anastasiia Antonova <antonova64@gmail.com>
2023-03-01 15:29:40 +0100

    feat(components-js): table - add dark theme for puppeteer vrt state test | aa | #2147

--- packages/components-js/tests/vrt/puppeteer/specs/table.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/table.vrt.ts
@@ -2,10 +2,10 @@ import {
   forceFocusHoverState,
   forceFocusState,
   forceHoverState,
-  getBodyMarkup,
-  GetMarkup,
+  getThemedBodyMarkup,
   setContentWithDesignSystem,
 } from '../helpers';
+import type { GetThemedMarkup } from '../helpers';
 import {
   defaultViewports,
   getVisualRegressionStatesTester,
@@ -23,27 +23,27 @@ it('should have no visual regression for :hover + :focus-visible', async () => {
     await vrt.test('table-states', async () => {
       const page = vrt.getPage();
 
-      const getElementsMarkup: GetMarkup = () => `
-<p-table>
+      const getElementsMarkup: GetThemedMarkup = (theme) => `
+<p-table theme="${theme}">
   <span slot="caption">Some caption <a href="#">with a link</a></span>
   <p-table-head>
     <p-table-head-row>
-      <p-table-head-cell style="min-width: 2000px;">Some head cell</p-table-head-cell>
+      <p-table-head-cell theme="${theme}" style="min-width: 2000px;">Some head cell</p-table-head-cell>
     </p-table-head-row>
   </p-table-head>
   <p-table-body>
-    <p-table-row>
-      <p-table-cell>Some <a href="#">link</a></p-table-cell>
+    <p-table-row theme="${theme}">
+      <p-table-cell theme="${theme}">Some <a href="#">link</a></p-table-cell>
     </p-table-row>
-    <p-table-row>
-      <p-table-cell>Some cell</p-table-cell>
+    <p-table-row theme="${theme}">
+      <p-table-cell theme="${theme}">Some cell</p-table-cell>
     </p-table-row>
-    <p-table-row>
-      <p-table-cell>Some cell</p-table-cell>
+    <p-table-row theme="${theme}">
+      <p-table-cell theme="${theme}">Some cell</p-table-cell>
     </p-table-row>
   </p-table-body>
 </p-table>`;
-      await setContentWithDesignSystem(page, getBodyMarkup(getElementsMarkup));
+      await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup));
 
       await page.evaluate(() => {
         document.querySelectorAll('p-table-head-cell').forEach((el) => {
