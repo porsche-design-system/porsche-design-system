9feb9b883486227f1bee47790895ee1c9534064b
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-01 13:39:06 +0100

    feat(components-js): improve normalize styles for getInitialStyles partial | sas / bh | #2321

--- packages/components-js/projects/partials/scripts/generateInitialStylesPartial.ts
+++ packages/components-js/projects/partials/scripts/generateInitialStylesPartial.ts
@@ -1,30 +1,9 @@
+import type { Styles } from 'jss';
 import { joinArrayElementsToString } from './utils';
-import { INTERNAL_TAG_NAMES, TAG_NAMES, getMinifiedCss } from '@porsche-design-system/shared';
-import { JssStyle, Styles } from 'jss';
-import {
-  themeDark,
-  fontWeight,
-  themeLight,
-  fontFamily,
-  fontHyphenationStyle,
-  fontLineHeight,
-  borderRadiusSmall,
-  frostedGlassStyle,
-  borderWidthBase,
-  borderRadiusMedium,
-} from '@porsche-design-system/utilities-v2';
+import { getMinifiedCss, INTERNAL_TAG_NAMES, TAG_NAMES } from '@porsche-design-system/shared';
+import { fontFamily, fontHyphenationStyle, fontLineHeight, fontWeight } from '@porsche-design-system/utilities-v2';
 
 const tagNames = joinArrayElementsToString(TAG_NAMES.filter((x) => !INTERNAL_TAG_NAMES.includes(x)));
-
-export const getInsetJssStyle = (value: 'auto' | number = 0): JssStyle => {
-  value = value === 0 || value === 'auto' ? value : (`${value}px` as any);
-  return {
-    top: value,
-    left: value,
-    right: value,
-    bottom: value,
-  };
-};
 
 export const generateInitialStylesPartial = (): string => {
   const types = `type GetInitialStylesOptions = {
@@ -34,11 +13,16 @@ export const generateInitialStylesPartial = (): string => {
 
   const normalizeStyles: Styles = {
     '@global': {
-      '*': {
-        fontFamily: fontFamily,
+      'html, body': {
+        fontFamily,
         lineHeight: fontLineHeight,
+        letterSpacing: 'normal',
         textSizeAdjust: 'none',
         WebkitTextSizeAdjust: 'none', // stop iOS safari from adjusting font size when screen rotation is changing
+      },
+
+      'h1, h2, h3, h4, h5, h6': {
+        fontWeight: fontWeight.semiBold,
       },
 
       p: {
@@ -46,112 +30,16 @@ export const generateInitialStylesPartial = (): string => {
         ...fontHyphenationStyle,
       },
 
-      'h1, h2, h3, h4, h5, h6': {
-        fontWeight: fontWeight.regular,
-      },
-
-      a: {
-        color: 'inherit',
-        textDecoration: 'underline',
-        borderRadius: borderRadiusSmall,
-        borderColor: 'transparent', // default value is needed for smooth transition
-        '@media(hover:hover)': {
-          transition: 'background-color var(--p-transition-duration, .24s) ease',
-          '&:hover': {
-            ...frostedGlassStyle,
-            backgroundColor: themeLight.state.hover,
-          },
-          '[data-theme=dark] &:hover': {
-            backgroundColor: themeDark.state.hover,
-          },
-        },
-      },
-
       'b, strong': {
         fontWeight: fontWeight.bold,
       },
 
-      'em, i': {
-        fontStyle: 'normal',
-      },
-
-      'a, button': {
-        position: 'relative',
-        outline: 0, // hide default focus outline
-        '&:focus::before': {
-          content: '""',
-          position: 'absolute',
-          borderRadius: borderRadiusMedium,
-          border: `${borderWidthBase} solid ${themeLight.state.focus}`,
-          ...getInsetJssStyle(-4),
-        },
-        '&:focus:not(:focus-visible):not(:disabled)::before': {
-          border: 0,
-        },
-      },
-
-      'button:focus::before': getInsetJssStyle(-6),
-
-      '[data-theme=dark] a, [data-theme=dark] button': {
-        '&:focus::before': {
-          border: `${borderWidthBase} solid ${themeDark.state.focus}`,
-        },
-        '&:disabled': {
-          '&:focus::before': {
-            border: 0,
-          },
-        },
-      },
-
-      // Pseudo-elements are not supported on these elements
-      'input, select, textarea': {
-        outline: `${borderWidthBase} solid transparent`,
-        outlineOffset: '2px',
-        '&:focus': {
-          borderRadius: borderRadiusSmall,
-          outlineColor: `${themeLight.state.focus}`,
-        },
-        '&:focus:not(:focus-visible)': {
-          outlineColor: 'transparent',
-        },
-      },
-
-      '[data-theme=dark] input, [data-theme=dark] select, [data-theme=dark] textarea': {
-        '&:focus': {
-          outlineColor: `${themeDark.state.focus}`,
-        },
-      },
-
       // the following selectors don't work within ::slotted() pseudo selector, therefore we have to apply them via light DOM
-      'input::-webkit-outer-spin-button, input::-webkit-inner-spin-button, input[type=search]::-webkit-search-decoration':
-        {
-          WebkitAppearance: 'none',
-          appearance: 'none',
-        },
-
-      'input[type=search]::-webkit-search-cancel-button': {
-        display: 'none',
-      },
-
-      'input::-webkit-calendar-picker-indicator': {
-        filter: 'invert(3%) sepia(7%) saturate(2930%) hue-rotate(188deg) brightness(91%) contrast(103%)', // property color can not be used here, this is the filter for themeLight.primary
-      },
-      '[data-theme=dark] input::-webkit-calendar-picker-indicator': {
-        filter: 'invert(97%) sepia(55%) saturate(2840%) hue-rotate(180deg) brightness(114%) contrast(103%)', // property color can not be used here, this is the filter for themeDark.primary
-      },
-      'input::-webkit-calendar-picker-indicator:hover': {
-        cursor: 'pointer',
-      },
-
-      // Safari input type date and time has incorrect sizing
-      'input::-webkit-datetime-edit': {
-        display: 'block',
-        padding: 0,
-      },
-
-      // Safari input type date and time has incorrect placeholder color for dark theme
-      '[data-theme=dark] input::-webkit-datetime-edit': {
-        WebkitTextFillColor: themeDark.primary,
+      input: {
+        '&::-webkit-outer-spin-button, &::-webkit-inner-spin-button, &::-webkit-search-decoration, &::-webkit-search-cancel-button, &::-webkit-calendar-picker-indicator':
+          {
+            display: 'none',
+          },
       },
     },
   };
