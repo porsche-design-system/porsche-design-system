57001819f9d759c35bb086b072fe0732bc72814d
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-02 13:15:07 +0100

    test(utilities/utilities): update getFocusStyle style snapshot | sas | #2321

--- packages/utilities/projects/utilities/src/js/focus/getFocusStyle.spec.ts
+++ packages/utilities/projects/utilities/src/js/focus/getFocusStyle.spec.ts
@@ -6,10 +6,15 @@ it('should provide all exports', () => {
 });
 
 describe('getFocusStyle()', () => {
-  it.each<Parameters<typeof getFocusStyle>>([[], [{ offset: '5rem' }], [{ theme: 'dark' }]])(
-    'should return correct css for opts: %s',
-    (...args) => {
-      expect(getFocusStyle(...args)).toMatchSnapshot();
-    }
-  );
+  it.each<Parameters<typeof getFocusStyle>>([
+    [{ inset: undefined, borderRadius: undefined, theme: undefined }],
+    [{ inset: 'small', borderRadius: 'small', theme: 'light' }],
+    [{ inset: 'medium', borderRadius: 'medium', theme: 'light' }],
+    [{ inset: '-4px -2px', borderRadius: '6px', theme: 'light' }],
+    [{ inset: 'small', borderRadius: 'small', theme: 'dark' }],
+    [{ inset: 'medium', borderRadius: 'medium', theme: 'dark' }],
+    [{ inset: '-4px -2px', borderRadius: '6px', theme: 'dark' }],
+  ])('should return correct css for opts: %s', (...args) => {
+    expect(getFocusStyle(...args)).toMatchSnapshot();
+  });
 });
