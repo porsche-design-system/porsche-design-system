e6734e40aa170ef8f50ccaeaedb98d29c1562d52
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-02 13:08:43 +0100

    feat(utilities/utilities): add getHoverStyle and improve getFocusStyle | sas | #2321

--- packages/utilities/projects/utilities/src/js/focus/getFocusStyle.ts
+++ packages/utilities/projects/utilities/src/js/focus/getFocusStyle.ts
@@ -3,33 +3,37 @@ import { themeDarkStateFocus, themeLightStateFocus } from '../theme';
 import { borderRadiusMedium, borderRadiusSmall, borderWidthBase } from '../border';
 
 type Size = 'small' | 'medium';
-type GetFocusOptions = {
-  offset?: Size | string;
-  borderRadius?: Size;
+type Inset = Size;
+type BorderRadius = Size;
+type Options = {
+  inset?: Inset | string;
+  borderRadius?: BorderRadius | string;
   theme?: Theme;
 };
 
-export const getFocusStyle = (opts?: GetFocusOptions) => {
-  const offset = opts?.offset === 'small' ? '-4px' : opts?.offset === 'medium' ? '-6px' : opts?.offset || '-4px';
+export const getFocusStyle = (opts?: Options) => {
+  const inset = opts?.inset === 'small' ? '-4px' : opts?.inset === 'medium' ? '-6px' : opts?.inset || '-4px';
+  const borderRadius =
+    opts?.borderRadius === 'small'
+      ? borderRadiusSmall
+      : opts?.borderRadius === 'medium'
+      ? borderRadiusMedium
+      : opts?.borderRadius || borderRadiusSmall;
 
   return {
     position: 'relative',
     // we can't use outline, since border-radius has no effect on outline style in Safari
     outline: 0,
-    '&:focus::before': {
+    '&:focus::after': {
       content: '""',
       position: 'absolute',
-      // we can use inset as soon as browser support is sufficient
-      top: offset,
-      left: offset,
-      right: offset,
-      bottom: offset,
+      inset,
+      pointerEvents: 'none', // necessary to be able to select elements behind pseudo-element
       border: `${borderWidthBase} solid ${opts?.theme === 'dark' ? themeDarkStateFocus : themeLightStateFocus}`,
-      borderRadius: opts?.borderRadius === 'medium' ? borderRadiusMedium : borderRadiusSmall,
-      pointerEvents: 'none', // necessary to be able to select elements behind ::before pseudo-element
+      borderRadius,
     },
     // why? have a look at this article https://developer.paciellogroup.com/blog/2018/03/focus-visible-and-backwards-compatibility/
-    '&:focus:not(:focus-visible)::before': {
+    '&:focus:not(:focus-visible)::after': {
       borderColor: 'transparent',
     },
   } as const;
