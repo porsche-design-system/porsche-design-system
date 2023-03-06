fad2bbe68e6c4e6f73416abce5bfc1f477a615c1
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-06 13:06:47 +0100

    test(components-js): update vrt states scenario for modal | sas | #2321

--- packages/components-js/tests/vrt/puppeteer/specs/modal.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/modal.vrt.ts
@@ -1,4 +1,17 @@
-import { extendedViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
+import {
+  extendedViewports,
+  getVisualRegressionStatesTester,
+  getVisualRegressionTester,
+  vrtTest,
+} from '@porsche-design-system/shared/testing';
+import {
+  forceFocusHoverState,
+  forceFocusState,
+  forceHoverState,
+  getBodyMarkup,
+  GetMarkup,
+  setContentWithDesignSystem,
+} from '../helpers';
 
 it.each(extendedViewports)('should have no visual regression for modal for viewport %s', async (viewport) => {
   expect(
@@ -6,6 +19,50 @@ it.each(extendedViewports)('should have no visual regression for modal for viewp
       scenario: async (page) => {
         await page.mouse.click(0, 0); // click top left corner of the page to remove focus on modal
       },
+    })
+  ).toBeFalsy();
+});
+
+it('should have no visual regression for :hover + :focus-visible', async () => {
+  const vrt = getVisualRegressionStatesTester();
+  expect(
+    await vrt.test('modal-states', async () => {
+      const page = vrt.getPage();
+
+      const head = `
+        <style>
+          .playground {
+            height: 300px;
+            transform: translate3d(0, 0, 0);
+          }
+        </style>`;
+
+      const getElementsMarkup: GetMarkup = () => `
+        <p-modal open="true">
+          <div slot="heading">
+            Some slotted heading
+            <span>
+              and some slotted, deeply nested <a href="#">anchor</a>.
+            </span>
+          </div>
+          Some content
+          <span>
+            and some slotted, deeply nested <a href="#">anchor</a>.
+          </span>
+        </p-modal>`;
+
+      await setContentWithDesignSystem(page, getBodyMarkup(getElementsMarkup), {
+        injectIntoHead: head,
+      });
+
+      await forceHoverState(page, '.hover p-modal a');
+      await forceHoverState(page, '.hover p-modal >>> p-button-pure >>> button');
+      await forceFocusState(page, '.focus p-modal a');
+      await forceFocusState(page, '.focus p-modal >>> div');
+      await forceFocusState(page, '.focus p-modal >>> p-button-pure >>> button');
+      await forceFocusHoverState(page, '.focus-hover p-modal a');
+      await forceFocusHoverState(page, '.focus-hover p-modal >>> div');
+      await forceFocusHoverState(page, '.focus-hover p-modal >>> p-button-pure >>> button');
     })
   ).toBeFalsy();
 });
