c9b24bfaa8cfcedac606bfffeae857efb9e0818d
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-06 14:02:20 +0100

    test(components-js): update vrt states scenario for inline-notification | sas | #2321

--- packages/components-js/tests/vrt/puppeteer/specs/inline-notification.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/inline-notification.vrt.ts
@@ -25,13 +25,26 @@ it('should have no visual regression for :hover + :focus-visible', async () => {
     await vrt.test('inline-notification-states', async () => {
       const page = vrt.getPage();
 
+      const head = `
+        <style>
+          body { display: grid; grid-template-columns: repeat(2, 50%); }
+        </style>`;
+
       const getElementsMarkup: GetThemedMarkup = (theme) => `
         <p-inline-notification theme="${theme}" action-label="Retry">
-          <span slot="heading">Some heading with a <a href="#">link</a>.</span>
-          Some description with a <a href="#">link</a>.
+          <span slot="heading">
+            Slotted heading
+            <span>
+              and some slotted, deeply nested <a href="#">anchor</a>.
+            </span>
+          </span>
+          Slotted description
+          <span>
+            and some slotted, deeply nested <a href="#">anchor</a>.
+          </span>
         </p-inline-notification>`;
 
-      await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup));
+      await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup), { injectIntoHead: head });
 
       await forceHoverState(page, '.hover p-inline-notification a');
       await forceHoverState(page, '.hover p-inline-notification >>> p-button-pure >>> button');
