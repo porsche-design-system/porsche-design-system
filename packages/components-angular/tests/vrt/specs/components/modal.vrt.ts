2329849c89e714c18c9b811c4d3385bfc393bcce
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-06 10:18:18 +0100

    test(components-js): disable auto focus of modal | sas | #2321

--- packages/components-angular/tests/vrt/specs/components/modal.vrt.ts
+++ packages/components-angular/tests/vrt/specs/components/modal.vrt.ts
@@ -2,5 +2,11 @@ import { extendedViewports, getVisualRegressionTester, vrtTest } from '@porsche-
 
 // TODO: renders slightly different compared to JS and React version (regression looks still good, maybe use vrt tolerance or ensure dom noes + styles are exactly the same in all framework implementations?)
 xit.each(extendedViewports)('should have no visual regression for basic modal for viewport %s', async (viewport) => {
-  expect(await vrtTest(getVisualRegressionTester(viewport), 'modal', '/modal')).toBeFalsy();
+  expect(
+    await vrtTest(getVisualRegressionTester(viewport), 'modal', '/modal', {
+      scenario: async (page) => {
+        await page.mouse.click(0, 0); // click top left corner of the page to remove focus on modal
+      },
+    })
+  ).toBeFalsy();
 });
--- packages/components-js/tests/vrt/playwright/specs/modal.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/modal.vrt.ts
@@ -2,5 +2,9 @@ import { executeVisualRegressionTest } from '../helpers/playwright-helper';
 import { test } from '@playwright/test';
 
 test.describe('should have no visual regression for modal', async () => {
-  await executeVisualRegressionTest('modal');
+  await executeVisualRegressionTest('modal', {
+    scenario: async (page) => {
+      await page.mouse.click(0, 0); // click top left corner of the page to remove focus on modal
+    },
+  });
 });
