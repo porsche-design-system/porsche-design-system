931d9a98b4407d75eb41118d4fde3b4c97f54fa5
Anastasiia Antonova <antonova64@gmail.com>
2023-03-06 11:55:09 +0100

    feat(components-angular): vrt test demo - use native ul and li | aa | #2147

--- packages/components-angular/src/app/pages/scroller.component.ts
+++ packages/components-angular/src/app/pages/scroller.component.ts
@@ -203,28 +203,28 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
     <div class="playground light" title="should render scroller with scrollbar true">
       <p-scroller [scrollbar]="true">
-        <p-text-list>
-          <p-text-list-item>
+        <ul>
+          <li>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
             dolore magna aliquyam erat, sed diam voluptua. <br />At vero eos et accusam et justo duo dolores et ea rebum.
-          </p-text-list-item>
-          <p-text-list-item>
+          </li>
+          <li>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
             dolore magna aliquyam erat, sed diam voluptua. <br />At vero eos et accusam et justo duo dolores et ea rebum.
-            <p-text-list>
-              <p-text-list-item>
+            <ul>
+              <li>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
                 dolore magna aliquyam erat, sed diam voluptua. <br />At vero eos et accusam et justo duo dolores et ea
                 rebum.
-              </p-text-list-item>
-              <p-text-list-item>
+              </li>
+              <li>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
                 dolore magna aliquyam erat, sed diam voluptua. <br />At vero eos et accusam et justo duo dolores et ea
                 rebum.
-              </p-text-list-item>
-            </p-text-list>
-          </p-text-list-item>
-        </p-text-list>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </p-scroller>
     </div>
   `,
