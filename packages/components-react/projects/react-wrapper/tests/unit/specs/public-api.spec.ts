a9c203de931554453b95ae8a32d63d1f7fb3ae84
Bernd Hacker <hacker.bernd@gmail.com>
2023-03-06 12:08:12 +0100

    test(components-angular/react): adjust public-api unit tests | bh | #2374

--- packages/components-react/projects/react-wrapper/tests/unit/specs/public-api.spec.ts
+++ packages/components-react/projects/react-wrapper/tests/unit/specs/public-api.spec.ts
@@ -4,13 +4,15 @@ import {
   skipCheckForPorscheDesignSystemProviderDuringTests,
   skipPorscheDesignSystemCDNRequestsDuringTests,
   useToastManager,
+  IconName,
+  TabsChangeEvent,
 } from '../../../src/public-api';
 
 it('should expose componentsReady()', () => {
   expect(typeof componentsReady).toBe('function');
 });
 
-it('should expose PorscheDesignSystemProvider()', () => {
+it('should expose PorscheDesignSystemProvider', () => {
   expect(typeof PorscheDesignSystemProvider).toBe('function');
 });
 
@@ -24,4 +26,11 @@ it('should expose useToastManager()', () => {
 
 it('should expose skipPorscheDesignSystemCDNRequestsDuringTests()', () => {
   expect(typeof skipPorscheDesignSystemCDNRequestsDuringTests).toBe('function');
+});
+
+it('should expose types from root', () => {
+  const icon: IconName = '360';
+  const event: TabsChangeEvent = { activeTabIndex: 1 };
+  expect(icon).toBe('360');
+  expect(event).toEqual({ activeTabIndex: 1 });
 });
