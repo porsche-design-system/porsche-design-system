af2a926d4bfa43a26817a8a3f138e0e2ec1162b8
TatjanaGartner <tatjana.gartner@mhp.com>
2023-03-06 17:23:05 +0100

    test(components): add unit test for p-button-tile | tg | #2361

---
+++ packages/components/src/components/button-tile/button-tile.spec.ts
@@ -0,0 +1,14 @@
+import { ButtonTile } from './button-tile';
+import * as throwIfAlignTopWithoutCompactUtils from '../../utils/link-button-tile/link-button-tile-align';
+
+describe('componentWillLoad', () => {
+  it('should call throwIfAlignTopAndNotCompact() with correct parameters', () => {
+    const spy = jest.spyOn(throwIfAlignTopWithoutCompactUtils, 'throwIfAlignTopAndNotCompact');
+
+    const component = new ButtonTile();
+    component.host = document.createElement('p-button-tile');
+
+    component.componentWillLoad();
+    expect(spy).toBeCalledWith(component.host, component.align, component.compact);
+  });
+});
