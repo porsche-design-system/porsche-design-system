cf9a50504820d94b136d76ecbef2e4886e2b616d
Anastasiia Antonova <antonova64@gmail.com>
2023-03-07 12:51:42 +0100

    feat(components): table row styles spec update | aa | #2147

--- packages/components/src/components/table/table-row/table-row-styles.spec.ts
+++ packages/components/src/components/table/table-row/table-row-styles.spec.ts
@@ -1,10 +1,7 @@
 import { getComponentCss } from './table-row-styles';
 
 describe('getComponentCss()', () => {
-  it.each<Parameters<typeof getComponentCss>>([['dark'], ['light']])(
-    'should return correct css for theme: %s',
-    (...args) => {
-      expect(getComponentCss(...args)).toMatchSnapshot();
-    }
-  );
+  it('should return correct css', () => {
+    expect(getComponentCss()).toMatchSnapshot();
+  });
 });
