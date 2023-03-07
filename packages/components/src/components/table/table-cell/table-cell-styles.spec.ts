9bea88b8c8e42346cd8a81f9440d4a135274413f
Anastasiia Antonova <antonova64@gmail.com>
2023-03-07 13:04:01 +0100

    feat(components): table cell styles - unit test update | aa | #2147

--- packages/components/src/components/table/table-cell/table-cell-styles.spec.ts
+++ packages/components/src/components/table/table-cell/table-cell-styles.spec.ts
@@ -1,10 +1,10 @@
 import { getComponentCss } from './table-cell-styles';
 
 describe('getComponentCss()', () => {
-  it.each<Parameters<typeof getComponentCss>>([
-    [true, 'dark'],
-    [false, 'light'],
-  ])('should return correct css for multiline: %s and theme: %s', (...args) => {
-    expect(getComponentCss(...args)).toMatchSnapshot();
-  });
+  it.each<Parameters<typeof getComponentCss>>([[true], [false]])(
+    'should return correct css for multiline: %s',
+    (...args) => {
+      expect(getComponentCss(...args)).toMatchSnapshot();
+    }
+  );
 });
