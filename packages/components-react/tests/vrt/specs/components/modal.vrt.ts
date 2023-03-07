bf008cd7747408e143b8b0e232d3326725b82210
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-07 17:02:35 +0100

    test: update vrt text expectation for modal | sas | #2321

--- packages/components-react/tests/vrt/specs/components/modal.vrt.ts
+++ packages/components-react/tests/vrt/specs/components/modal.vrt.ts
@@ -1,6 +1,6 @@
 import { extendedViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 
-it.each(extendedViewports)('should have no visual regression for basic modal for viewport %s', async (viewport) => {
+it.each(extendedViewports)('should have no visual regression for modal for viewport %s', async (viewport) => {
   expect(
     await vrtTest(getVisualRegressionTester(viewport), 'modal', '/modal', {
       scenario: async (page) => {
