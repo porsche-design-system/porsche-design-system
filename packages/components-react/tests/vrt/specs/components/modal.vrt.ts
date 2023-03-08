8160226c4846b7bf35ffe9b018cee80e6485f8a7
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-08 11:45:32 +0100

    test(components-react): disable vrt test for modal | sas | #2321

--- packages/components-react/tests/vrt/specs/components/modal.vrt.ts
+++ packages/components-react/tests/vrt/specs/components/modal.vrt.ts
@@ -1,6 +1,6 @@
 import { extendedViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 
-it.each(extendedViewports)('should have no visual regression for modal for viewport %s', async (viewport) => {
+xit.each(extendedViewports)('should have no visual regression for modal for viewport %s', async (viewport) => {
   expect(
     await vrtTest(getVisualRegressionTester(viewport), 'modal', '/modal', {
       scenario: async (page) => {
