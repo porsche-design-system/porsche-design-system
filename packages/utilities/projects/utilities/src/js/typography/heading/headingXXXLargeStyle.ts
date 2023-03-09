8de8053293c2e01f6920d9e52a94f2b2a280cd21
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-09 09:38:14 +0100

    feat(utilities/utilities): remove headingXXXLargeStyle and add displaySmallStyle | sas | #2321

--- packages/utilities/projects/utilities/src/js/typography/heading/headingXXXLargeStyle.ts
+++
@@ -1,6 +0,0 @@
-import { _headingFontPartA, _headingFontPartB } from './headingShared';
-import { fontSizeDisplaySmall } from '../../font';
-
-export const headingXXXLargeStyle = {
-  font: `${_headingFontPartA}${fontSizeDisplaySmall}${_headingFontPartB}`,
-} as const;
