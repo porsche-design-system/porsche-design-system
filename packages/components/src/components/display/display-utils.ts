1d1de87563be9cb74aa235ce20d46a68fa551687
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-09 10:52:05 +0100

    feat(components): add size small to display | sas / mb | #2372

--- packages/components/src/components/display/display-utils.ts
+++ packages/components/src/components/display/display-utils.ts
@@ -5,7 +5,7 @@ import { hasSpecificSlottedTag } from '../../utils';
 export const DISPLAY_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const;
 export type DisplayTag = typeof DISPLAY_TAGS[number];
 
-export const DISPLAY_SIZES = ['medium', 'large', 'inherit'] as const;
+export const DISPLAY_SIZES = ['small', 'medium', 'large', 'inherit'] as const;
 export type DisplaySize = typeof DISPLAY_SIZES[number];
 
 export const DISPLAY_COLORS = ['primary', 'inherit'] as const;
@@ -14,6 +14,7 @@ export type DisplayColor = typeof DISPLAY_COLORS[number];
 export type DisplayAlign = TextAlign;
 
 export const displaySizeToTagMap: { [key in DisplaySize]: string } = {
+  small: 'h3',
   medium: 'h2',
   large: 'h1',
   inherit: 'h1',
