053f7e2e2a66c7912e5b5c70c005f9e334f175c6
TatjanaGartner <tatjana.gartner@mhp.com>
2023-03-10 15:15:48 +0100

    feat(component): improve typing for sharedTilePropTypes | tg | #2361

--- packages/components/src/components/link-tile/link-tile-utils.ts
+++ packages/components/src/components/link-tile/link-tile-utils.ts
@@ -6,18 +6,19 @@ import {
   LINK_BUTTON_TILE_ALIGNS,
   LINK_BUTTON_TILE_ASPECT_RATIOS,
   LINK_BUTTON_TILE_SIZES,
-  LINK_BUTTON_TILE_WEIGHTS,
   LinkTileAlign,
   LinkTileAspectRatio,
   LinkTileSize,
-  LinkTileWeight,
 } from '../../utils';
 
-export const sharedTilePropTypes:
-  | Omit<PropTypes<typeof ButtonTile>, 'type' | 'disabled' | 'loading' | 'icon' | 'iconSource' | 'aria'>
-  | Omit<PropTypes<typeof LinkTile>, 'href' | 'target' | 'download' | 'rel' | 'aria'> = {
+type CommonButtonAndLinkTileProps = {
+  [K in keyof PropTypes<typeof ButtonTile> & keyof PropTypes<typeof LinkTile>]:
+    | PropTypes<typeof ButtonTile>[K]
+    | PropTypes<typeof LinkTile>[K];
+};
+
+export const sharedTilePropTypes: Omit<CommonButtonAndLinkTileProps, 'aria' | 'weight'> = {
   size: AllowedTypes.breakpoint<LinkTileSize>(LINK_BUTTON_TILE_SIZES),
-  weight: AllowedTypes.breakpoint<LinkTileWeight>(LINK_BUTTON_TILE_WEIGHTS),
   aspectRatio: AllowedTypes.breakpoint<LinkTileAspectRatio>(LINK_BUTTON_TILE_ASPECT_RATIOS),
   label: AllowedTypes.string,
   description: AllowedTypes.string,
