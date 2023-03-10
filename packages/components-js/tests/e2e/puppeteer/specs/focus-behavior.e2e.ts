1fe66c120ff7b7bcc20057dbadeb8c4a8e431917
Matthias Haegele <matthias.haegele@mhp.com>
2023-03-10 08:51:04 +0100

    test(components-js): remove adjustment in focus behavior test / update default dom snap | mh | #2352

--- packages/components-js/tests/e2e/puppeteer/specs/focus-behavior.e2e.ts
+++ packages/components-js/tests/e2e/puppeteer/specs/focus-behavior.e2e.ts
@@ -16,11 +16,6 @@ TAG_NAMES.filter((tagName) => getComponentMeta(tagName).isDelegatingFocus).forEa
   const href = tagName.includes('link') || tagName.includes('marque') ? ' href="#"' : '';
   const value = tagName.includes('segmented-control-item') ? ' value="some value"' : '';
   const state = tagName.includes('stepper-horizontal-item') ? ' state="complete"' : '';
-  const description = tagName.includes('link-tile') ? ' description="Some description"' : '';
-  const requiredLinkTileModelSignatureProps =
-    tagName === 'p-link-tile-model-signature'
-      ? ` primary-link-props="{ label: 'Some label', href: '#'}" secondary-link-props="{ label: 'Some label', href: '#'}"`
-      : '';
 
   // TODO: we could refactor buildDefaultComponentMarkup() to use it here
   const wrapInRequiredParentIfNeeded = (child: string): string => {
@@ -29,9 +24,7 @@ TAG_NAMES.filter((tagName) => getComponentMeta(tagName).isDelegatingFocus).forEa
   };
 
   it(`should be removed from tab order for ${tagName}`, async () => {
-    const component = wrapInRequiredParentIfNeeded(
-      `<${tagName}${href}${state}${description}${requiredLinkTileModelSignatureProps} tabindex="-1">Some label</${tagName}>`
-    );
+    const component = wrapInRequiredParentIfNeeded(`<${tagName}${href}${state} tabindex="-1">Some label</${tagName}>`);
     await setContentWithDesignSystem(
       page,
       `<a href="#" id="before">before</a>
@@ -48,9 +41,7 @@ ${component}
   it(`should delegate focus into shadow dom for ${tagName}`, async () => {
     await setContentWithDesignSystem(
       page,
-      wrapInRequiredParentIfNeeded(
-        `<${tagName}${href}${state}${value}${description}${requiredLinkTileModelSignatureProps}>Some label</${tagName}>`
-      )
+      wrapInRequiredParentIfNeeded(`<${tagName}${href}${state}${value}>Some label</${tagName}>`)
     );
 
     const host = await selectNode(page, tagName);
