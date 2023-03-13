6a09f17b19a36577054582e776bc3e4e3f063455
Bernd Hacker <hacker.bernd@gmail.com>
2023-03-13 10:38:59 +0100

    Update packages/storefront/src/components/Sidebar.vue
    Co-authored-by: Anastasiia Antonova <antonova64@gmail.com>

--- packages/storefront/src/components/Sidebar.vue
+++ packages/storefront/src/components/Sidebar.vue
@@ -31,7 +31,7 @@
   import { Route } from 'vue-router';
   import { config as storefrontConfig } from '@/../storefront.config';
   import Search from '@/components/Search.vue';
-  import { TagName } from '@porsche-design-system/shared';
+  import type { TagName } from '@porsche-design-system/shared';
   import { getComponentMeta } from '@porsche-design-system/component-meta';
 
   @Component({
