9d9252db393121979e768c2639aa055fd20775ea
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-14 15:52:55 +0100

    test(components-js): add style snapshot for initial styles partial, processed by prettier | sas

--- packages/components-js/projects/partials/tests/unit/specs/getInitialStyles.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getInitialStyles.spec.ts
@@ -1,18 +1,23 @@
 import { getInitialStyles } from '../../../src';
 import { render } from '@testing-library/react';
+import * as prettier from 'prettier';
 
 // to skip validation
 jest.mock('../../../src/shared');
 
+const getFormattedCSSWithoutTag = (style: string): string => {
+  return prettier.format(style.replace(/<style.*>([\s\S]*)<\/style>/g, '$1'), { parser: 'css' });
+};
+
 describe('format: html', () => {
   it('should return core styles', () => {
     const result: string = getInitialStyles();
-    expect(result).toMatchSnapshot();
+    expect(getFormattedCSSWithoutTag(result)).toMatchSnapshot();
   });
 
   it('should add custom prefixes to component names', () => {
     const result: string = getInitialStyles({ prefix: 'custom-prefix' });
-    expect(result).toMatchSnapshot();
+    expect(getFormattedCSSWithoutTag(result)).toMatchSnapshot();
   });
 });
 
@@ -20,12 +25,12 @@ describe('format: jsx', () => {
   it('should return core styles', () => {
     const result: JSX.Element = getInitialStyles({ format: 'jsx' });
     const { container } = render(result);
-    expect(container.innerHTML).toMatchSnapshot();
+    expect(getFormattedCSSWithoutTag(container.innerHTML)).toMatchSnapshot();
   });
 
   it('should add custom prefix to component names', () => {
     const result: JSX.Element = getInitialStyles({ format: 'jsx', prefix: 'custom-prefix' });
     const { container } = render(result);
-    expect(container.innerHTML).toMatchSnapshot();
+    expect(getFormattedCSSWithoutTag(container.innerHTML)).toMatchSnapshot();
   });
 });
