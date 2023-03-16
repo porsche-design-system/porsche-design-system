2708563eb66df0e7bc72c1d16f41dd2eaa9aa8a7
Bernd Hacker <hacker.bernd@gmail.com>
2023-03-16 09:25:54 +0100

    feat(components-angular): add RouterLinkDirectiveDelegate | bh | #2297

--- packages/components-angular/src/app/examples/link-example.component.ts
+++ packages/components-angular/src/app/examples/link-example.component.ts
@@ -1,5 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Directive,
+  ElementRef,
+  HostListener,
+  OnChanges,
+  OnInit,
+  Optional,
+} from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { LocationStrategy } from '@angular/common';
 
 // only angular router support needed
 // extend existing wrappers or offer new one or custom directive?
@@ -10,13 +20,15 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'page-link-example',
   template: `
-    <p-link href="/">Link 1 default</p-link>
-    <p-link href="/" (click)="onClick($event)">Link 2 click listener</p-link>
+    <p-link [href]="'/'">Link 1 default</p-link>
+    <p-link [href]="'/'" (click)="onClick($event)">Link 2 click listener</p-link>
     <p-link [routerLink]="'/'">Link 3 routerLink</p-link>
-    <p-link href="/" [routerLink]="'/'">Link 3 routerLink with href</p-link>
-    <p-link href="/" [routerLink]="'/'" (click)="$event.preventDefault()"
+    <p-link [href]="'/'" [routerLink]="'/'">Link 3 routerLink with href</p-link>
+    <p-link [href]="'/'" [routerLink]="'/'" (click)="$event.preventDefault()"
       >Link 3 routerLink with href and prevent</p-link
     >
+    <p-link [routerLink]="'/'">Link 4 routerLink</p-link>
+    <my-prefix-p-link p-link [routerLink]="'/'">Link 4 routerLink prefixed</my-prefix-p-link>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -27,4 +39,46 @@ export class LinkExampleComponent {
     e.preventDefault();
     this.router.navigateByUrl((e.target as any).href);
   };
+}
+
+@Directive({
+  selector: 'p-link[routerLink],[p-link][routerLink]',
+})
+export class RouterLinkDirectiveDelegate implements OnInit, OnChanges {
+  constructor(
+    private locationStrategy: LocationStrategy,
+    private elementRef: ElementRef,
+    private router: Router,
+    @Optional() private routerLink?: RouterLink
+  ) {}
+
+  ngOnInit(): void {
+    this.updateHref();
+  }
+
+  ngOnChanges(): void {
+    this.updateHref();
+  }
+
+  private updateHref() {
+    if (this.routerLink?.urlTree) {
+      this.elementRef.nativeElement.href = this.locationStrategy.prepareExternalUrl(
+        this.router.serializeUrl(this.routerLink.urlTree)
+      );
+    }
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(ev: UIEvent): void {
+    /**
+     * This prevents the browser from
+     * performing a page reload when pressing
+     * an Ionic component with routerLink.
+     * The page reload interferes with routing
+     * and causes ion-back-button to disappear
+     * since the local history is wiped on reload.
+     */
+    ev.preventDefault();
+    this.router.navigateByUrl(this.elementRef.nativeElement.href);
+  }
 }
