5c99d353c1cfe91bc2d8a55a7cb6b679a617134a
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-16 10:10:50 +0100

    test(components-react): add vrt test page for focus | sas

---
+++ packages/components-react/src/styles/StylesHoverExample.tsx
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+import {
+  getFocusStyle,
+  getHoverStyle,
+  gridGap,
+  headingMediumStyle,
+  spacingFluidMedium,
+  textSmallStyle,
+  themeDarkBackgroundBase,
+  themeDarkPrimary,
+  themeLightBackgroundBase,
+  themeLightPrimary,
+} from '@porsche-design-system/components-react/styles';
+
+// Wrapper
+const getWrapperStyle = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  gap: gridGap,
+  padding: spacingFluidMedium,
+} as const;
+
+const WrapperLight = styled.div({
+  ...getWrapperStyle,
+  background: themeLightBackgroundBase,
+  color: themeLightPrimary,
+});
+
+const WrapperDark = styled.div({
+  ...getWrapperStyle,
+  background: themeDarkBackgroundBase,
+  color: themeDarkPrimary,
+});
+
+// Typography
+const Heading = styled.h3({
+  ...headingMediumStyle,
+  textAlign: 'center',
+  width: '100%',
+  margin: 0,
+});
+
+const Paragraph = styled.p({
+  ...textSmallStyle,
+  margin: 0,
+  maxWidth: '15rem',
+});
+
+// Anchor
+const NativeAnchor = styled.a({
+  ...textSmallStyle,
+  ...getHoverStyle(),
+  ...getFocusStyle(),
+  color: 'inherit',
+});
+
+export const StylesHoverExample = (): JSX.Element => {
+  return (
+    <>
+      <WrapperLight>
+        <Heading>Focus Light (only visible by keyboard navigation)</Heading>
+        <NativeAnchor href="#">Some Anchor</NativeAnchor>
+        <Paragraph>
+          Lorem Ipsum <NativeAnchor href="#">is simply dummy text of the printing</NativeAnchor> and typesetting
+          industry.
+        </Paragraph>
+      </WrapperLight>
+      <WrapperDark>
+        <Heading>Focus Dark (only visible by keyboard navigation)</Heading>
+        <NativeAnchor href="#">Some Anchor</NativeAnchor>
+        <Paragraph>
+          Lorem Ipsum <NativeAnchor href="#">is simply dummy text of the printing</NativeAnchor> and typesetting
+          industry.
+        </Paragraph>
+      </WrapperDark>
+    </>
+  );
+};
