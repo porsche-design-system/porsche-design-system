ccb4012514c5974bc3477e988bbed484142ab9fe
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-16 11:14:24 +0100

    test(components-react/angular): force hover in hover style scenario | sas

--- packages/components-react/tests/vrt/specs/styles/hover.vrt.ts
+++ packages/components-react/tests/vrt/specs/styles/hover.vrt.ts
@@ -1,7 +1,14 @@
 import { extendedViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
+// TODO: we should move CDP to either shared package or maybe even better to visual-regression-tester itself
+import { forceHoverState } from '../../../../../components-js/tests/vrt/puppeteer/helpers';
 
 const id = 'styles-hover';
-// TODO: we need to force hover state by CDP
 it.each(extendedViewports)('should have no visual regression for viewport %s', async (viewport) => {
-  expect(await vrtTest(getVisualRegressionTester(viewport), id, `/${id}`)).toBeFalsy();
+  expect(
+    await vrtTest(getVisualRegressionTester(viewport), id, `/${id}`, {
+      scenario: async (page) => {
+        await forceHoverState(page, 'a');
+      },
+    })
+  ).toBeFalsy();
 });
