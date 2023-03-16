81fd59a55e3d5b47fb9864f946a1ee3683c94656
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-16 10:19:53 +0100

    test(components-angular/react): add vrt test for hover style | sas

--- packages/components-angular/tests/vrt/specs/styles/hover.vrt.ts
+++ packages/components-angular/tests/vrt/specs/styles/hover.vrt.ts
@@ -1,6 +1,6 @@
 import { extendedViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 
-const id = 'styles-focus';
+const id = 'styles-hover';
 // TODO: we need to force focus state by CDP
 it.each(extendedViewports)('should have no visual regression for viewport %s', async (viewport) => {
   expect(await vrtTest(getVisualRegressionTester(viewport), id, `/${id}`)).toBeFalsy();
--- packages/components-react/tests/vrt/specs/styles/hover.vrt.ts
+++ packages/components-react/tests/vrt/specs/styles/hover.vrt.ts
@@ -1,7 +1,7 @@
 import { extendedViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 
-const id = 'styles-focus';
-// TODO: we need to force focus state by CDP
+const id = 'styles-hover';
+// TODO: we need to force hover state by CDP
 it.each(extendedViewports)('should have no visual regression for viewport %s', async (viewport) => {
   expect(await vrtTest(getVisualRegressionTester(viewport), id, `/${id}`)).toBeFalsy();
 });
