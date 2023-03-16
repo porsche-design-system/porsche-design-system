f32a442c6d4ce55ca45fb2c47c305cb1cec15dfe
TatjanaGartner <tatjana.gartner@mhp.com>
2023-03-16 15:00:36 +0100

    feat(components): align typing for deprecated weight semibold | tg | #2361

--- packages/components/src/utils/link-button-tile/link-button-tile-utils.ts
+++ packages/components/src/utils/link-button-tile/link-button-tile-utils.ts
@@ -2,26 +2,26 @@ import type { BreakpointCustomizable } from '../breakpoint-customizable';
 import { getTagName } from '../tag-name';
 
 export const LINK_BUTTON_TILE_ASPECT_RATIOS = ['1:1', '4:3', '3:4', '16:9', '9:16'] as const;
-export type LinkTileAspectRatio = typeof LINK_BUTTON_TILE_ASPECT_RATIOS[number];
+export type LinkTileAspectRatio = (typeof LINK_BUTTON_TILE_ASPECT_RATIOS)[number];
 export type ButtonTileAspectRatio = LinkTileAspectRatio;
 
 export const LINK_BUTTON_TILE_SIZES = ['default', 'inherit'] as const;
-export type LinkTileSize = typeof LINK_BUTTON_TILE_SIZES[number];
+export type LinkTileSize = (typeof LINK_BUTTON_TILE_SIZES)[number];
 export type ButtonTileSize = LinkTileSize;
 
-// 'semibold' is deprecated and will be mapped to 'semi-bold'
 /** @deprecated */
-export const LINK_TILE_WEIGHTS_DEPRECATED = ['semibold'] as const;
-/** @deprecated */
-export type LinkTileWeightDeprecated = typeof LINK_TILE_WEIGHTS_DEPRECATED[number];
-export const LINK_TILE_WEIGHTS = ['regular', 'semi-bold', ...LINK_TILE_WEIGHTS_DEPRECATED] as const;
-export type LinkTileWeight = typeof LINK_TILE_WEIGHTS[number];
+export type LinkTileWeightDeprecated = 'semibold';
 
-export const BUTTON_TILE_WEIGHTS = ['regular', 'semi-bold'] as const;
-export type ButtonTileWeight = typeof BUTTON_TILE_WEIGHTS[number];
+export const TILE_WEIGHTS = ['regular', 'semi-bold'] as const;
+export type TileWeight = (typeof TILE_WEIGHTS)[number];
+
+export const LINK_TILE_WEIGHTS = [...TILE_WEIGHTS, 'semibold'] as const;
+export type LinkTileWeight = (typeof LINK_TILE_WEIGHTS)[number];
+
+export type ButtonTileWeight = TileWeight;
 
 export const LINK_BUTTON_TILE_ALIGNS = ['top', 'bottom'] as const;
-export type LinkTileAlign = typeof LINK_BUTTON_TILE_ALIGNS[number];
+export type LinkTileAlign = (typeof LINK_BUTTON_TILE_ALIGNS)[number];
 export type ButtonTileAlign = LinkTileAlign;
 
 // does not take care of breakpoint customizable
