c365a70b38f931243b37992509b0ce2d0c4ebc8e
Oliver Hook <oliver.hook@porsche.digital>
2023-03-20 18:42:10 +0100

    test(components): added unit test and update snapshots | oh | #2261

--- packages/components/src/styles/colors.spec.ts
+++ packages/components/src/styles/colors.spec.ts
@@ -1,9 +1,17 @@
 import { getThemedColors } from './colors';
+import * as a11yUtils from '../utils/a11y/a11y';
 
 describe('getThemedColors()', () => {
   it.each<Parameters<typeof getThemedColors>>([['light'], ['dark']])(
     'should return correct colors for theme: %s',
     (theme) => {
+      expect(getThemedColors(theme)).toMatchSnapshot();
+    }
+  );
+  it.each<Parameters<typeof getThemedColors>>([['light'], ['dark']])(
+    'should return correct partial high contrast mode scheme colors for theme: %s',
+    (theme) => {
+      jest.spyOn(a11yUtils, 'highContrastMode').mockReturnValue(true);
       expect(getThemedColors(theme)).toMatchSnapshot();
     }
   );
