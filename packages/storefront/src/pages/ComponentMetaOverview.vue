3679ee96bc8123a0cab5a3dcb58dd8c1cf354b9a
Bernd Hacker <hacker.bernd@gmail.com>
2023-03-20 11:06:46 +0100

    feat(component-meta/storefront): add deprecatedPropValues | bh

--- packages/storefront/src/pages/ComponentMetaOverview.vue
+++ packages/storefront/src/pages/ComponentMetaOverview.vue
@@ -19,16 +19,18 @@
 <script lang="ts">
   import Vue from 'vue';
   import Component from 'vue-class-component';
-  import { TAG_NAMES } from '@porsche-design-system/shared';
+  import { INTERNAL_TAG_NAMES, TAG_NAMES } from '@porsche-design-system/shared';
   import type { TagName } from '@porsche-design-system/shared';
   import { getComponentMeta } from '@porsche-design-system/component-meta';
   import type { ComponentMeta } from '@porsche-design-system/component-meta';
-  import { SwitchChangeEvent } from '../../../components-angular/dist/angular-wrapper';
+  import { SwitchChangeEvent } from '../../../components/dist/types/bundle';
+
+  const tagNames = TAG_NAMES.filter((x) => !INTERNAL_TAG_NAMES.includes(x));
 
   @Component
   export default class ComponentMetaOverview extends Vue {
     get headRow(): string {
-      return ['', ...TAG_NAMES]
+      return ['', ...tagNames]
         .map((tagName) => {
           const { isDeprecated } = getComponentMeta(tagName as TagName) || {};
           return `<p-table-head-cell>${tagName}${isDeprecated ? ' 🚫' : ''}</p-table-head-cell>`;
@@ -49,53 +51,60 @@
 
       const content = rowKeys
         .map((key) => {
-          const cells = TAG_NAMES.map((tagName) => {
-            const meta = getComponentMeta(tagName);
-            let value = meta[key];
+          const cells = tagNames
+            .map((tagName) => {
+              const meta = getComponentMeta(tagName);
+              let value = meta[key];
 
-            if (value && (key === 'props' || key === 'eventNames')) {
-              const {
-                deprecatedProps = [],
-                deprecatedEventNames = [],
-                breakpointCustomizableProps = [],
-                allowedPropValues = {},
-              } = meta;
+              if (value && (key === 'props' || key === 'eventNames')) {
+                const {
+                  deprecatedProps = [],
+                  deprecatedEventNames = [],
+                  breakpointCustomizableProps = [],
+                  allowedPropValues = {},
+                  deprecatedPropValues = {},
+                } = meta;
 
-              if (key === 'props') {
-                value = Object.keys(value);
-                value = value.map((val) => {
-                  let allowedValues = allowedPropValues[val] as string;
-                  if (Array.isArray(allowedValues)) {
-                    allowedValues = allowedValues.map((x) => (x === null ? 'undefined' : x)).join('<br>– ');
-                  }
+                if (key === 'props') {
+                  value = Object.keys(value);
+                  value = value.map((val) => {
+                    let allowedValues = allowedPropValues[val] as string;
+                    if (Array.isArray(allowedValues)) {
+                      const deprecatedValues = deprecatedPropValues[val];
+                      allowedValues = allowedValues
+                        .map((x) => (x === null ? 'undefined' : x))
+                        .map((x) => (deprecatedValues?.includes(x) ? x + ' 🚫' : x))
+                        .join('<br>– ');
+                    }
 
-                  return (
-                    '<span class="prop">' +
-                    val +
-                    [deprecatedProps.includes(val) && ' 🚫', breakpointCustomizableProps.includes(val) && ' 🛠️']
-                      .filter((x) => x)
-                      .join('') +
-                    '</span>' +
-                    (allowedValues ? '<div style="display: none">– ' + allowedValues + '</div>' : '')
-                  );
-                });
-              } else if (key === 'eventNames') {
-                value = (value as string[]).map((val) => val + (deprecatedEventNames.includes(val) ? ' 🚫' : ''));
+                    return (
+                      '<span class="prop">' +
+                      val +
+                      [deprecatedProps.includes(val) && ' 🚫', breakpointCustomizableProps.includes(val) && ' 🛠️']
+                        .filter((x) => x)
+                        .join('') +
+                      '</span>' +
+                      (allowedValues ? '<div style="display: none">– ' + allowedValues + '</div>' : '')
+                    );
+                  });
+                } else if (key === 'eventNames') {
+                  value = (value as string[]).map((val) => val + (deprecatedEventNames.includes(val) ? ' 🚫' : ''));
+                }
               }
-            }
 
-            let cellContent = value
-              ? Array.isArray(value)
-                ? value
-                    .sort()
-                    .map((val) => (key === 'nestedComponents' ? val : `<code>${val}</code>`))
-                    .join('<br>')
-                : value
-              : '';
-            cellContent = cellContent === true ? '✅' : cellContent;
+              let cellContent = value
+                ? Array.isArray(value)
+                  ? value
+                      .sort()
+                      .map((val) => (key === 'nestedComponents' ? val : `<code>${val}</code>`))
+                      .join('<br>')
+                  : value
+                : '';
+              cellContent = cellContent === true ? '✅' : cellContent;
 
-            return `<p-table-cell>${cellContent}</p-table-cell>`;
-          }).join('');
+              return `<p-table-cell>${cellContent}</p-table-cell>`;
+            })
+            .join('');
 
           return `<p-table-row>
     <p-table-cell>${key}</p-table-cell>
