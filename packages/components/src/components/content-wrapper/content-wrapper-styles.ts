a1a64993d2ecfa48557ded19e56664ee87b5b291
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-21 14:25:04 +0100

    feat(components): use gridColumnWidth for content-wrapper styles | sas | #2350

--- packages/components/src/components/content-wrapper/content-wrapper-styles.ts
+++ packages/components/src/components/content-wrapper/content-wrapper-styles.ts
@@ -1,7 +1,30 @@
 import type { ContentWrapperWidth } from './content-wrapper-utils';
 import { getCss } from '../../utils';
-import { getContentWrapperStyle } from './content-wrapper-styles-shared';
 import { addImportantToEachRule, hostHiddenStyles } from '../../styles';
+import {
+  getMediaQueryMin,
+  gridColumnWidthS,
+  gridColumnWidthXXL,
+  gridGap,
+  gridSafeZoneBase,
+  gridSafeZoneS,
+  gridSafeZoneXXL,
+  gridWidthMax,
+} from '@porsche-design-system/utilities-v2';
+
+const columnAmount: { [key in Exclude<ContentWrapperWidth, 'full' | 'fluid'>]: number } = {
+  narrow: 4,
+  basic: 2,
+  extended: 1,
+};
+
+const offsetHorizontalXXL = `max(0px, (100% - ${gridWidthMax}) / 2)`;
+
+const paddingHorizontalBase = gridSafeZoneBase;
+const paddingHorizontalS = (width: ContentWrapperWidth): string =>
+  `calc(${gridSafeZoneS} + (${gridGap} + ${gridColumnWidthS}) * ${columnAmount[width]})`;
+const paddingHorizontalXXL = (width: ContentWrapperWidth): string =>
+  `calc(${offsetHorizontalXXL} + ${gridSafeZoneXXL} + (${gridGap} + ${gridColumnWidthXXL}) * ${columnAmount[width]})`;
 
 export const getComponentCss = (width: ContentWrapperWidth): string => {
   return getCss({
@@ -11,6 +34,25 @@ export const getComponentCss = (width: ContentWrapperWidth): string => {
         ...addImportantToEachRule(hostHiddenStyles),
       },
     },
-    root: getContentWrapperStyle(width),
+    root: {
+      display: 'block',
+      margin: 0,
+      width: 'auto', // ensure value is set to default width, although style is used in light dom
+      minWidth: 0, // needed for some flex context
+      maxWidth: gridWidthMax,
+      ...(['full', 'fluid'].includes(width)
+        ? {
+            padding: `0 ${offsetHorizontalXXL}`,
+          }
+        : {
+            padding: `0 ${paddingHorizontalBase}`,
+            [getMediaQueryMin('s')]: {
+              padding: `0 ${paddingHorizontalS(width)}`,
+            },
+            [getMediaQueryMin('xxl')]: {
+              padding: `0 ${paddingHorizontalXXL(width)}`,
+            },
+          }),
+    },
   });
 };
