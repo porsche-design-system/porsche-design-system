f69bc172b3b437f2df1d88744e2301f21682bc51
Bernd Hacker <hacker.bernd@gmail.com>
2023-03-22 14:49:51 +0100

    test(components-js): fix carousel vrt page for slidesPerPage="auto" | bh | #2382

--- packages/components-angular/src/app/pages/carousel.component.ts
+++ packages/components-angular/src/app/pages/carousel.component.ts
@@ -350,7 +350,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     </div>
 
     <div class="playground light" title="should render carousel with slidesPerPage=auto">
-      <p-carousel [heading]="'slidesPerPage=auto'" slidesPerPage="auto">
+      <p-carousel [heading]="'slidesPerPage=auto'" [slidesPerPage]="'auto'">
         <div style="width: 10vw">Slide 1 10vw</div>
         <div style="width: 200px">Slide 2 200px</div>
         <div style="width: 100px">Slide 3 100px</div>
