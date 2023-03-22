d787dfc6263d6e9d781c88cac741eacd95dcbcb5
Anastasiia Antonova <antonova64@gmail.com>
2023-03-22 16:03:59 +0000

    feat(components-js): table puppeteer - adjust vrt test | aa | #2147

--- packages/components-js/tests/vrt/puppeteer/specs/table.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/table.vrt.ts
@@ -12,6 +12,7 @@ import {
   getVisualRegressionTester,
   vrtTest,
 } from '@porsche-design-system/shared/testing';
+import type { Theme } from '@porsche-design-system/utilities-v2';
 
 const getElementsMarkup: GetThemedMarkup = (theme) => `
 <p-table theme="${theme}">
@@ -40,65 +41,37 @@ it.each(defaultViewports)('should have no visual regression for viewport %s', as
   expect(await vrtTest(getVisualRegressionTester(viewport), 'table', '/#table')).toBeFalsy();
 });
 
-it('should have no visual regression for :hover + :focus-visible and light theme', async () => {
-  const vrt = getVisualRegressionStatesTester();
-  expect(
-    await vrt.test('table-states-light', async () => {
-      const page = vrt.getPage();
+describe.each<Theme>(['light', 'dark'])('%s', (theme) => {
+  it('should have no visual regression for :hover + :focus-visible and %s theme', async () => {
+    const vrt = getVisualRegressionStatesTester();
+    expect(
+      await vrt.test(`table-states-${theme}`, async () => {
+        const page = vrt.getPage();
 
-      await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup, { themes: ['light'] }));
+        await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup, { themes: [theme] }));
 
-      await page.evaluate(() => {
-        document.querySelectorAll('p-table-head-cell').forEach((el) => {
-          (el as any).sort = { id: 'some-id', active: true, direction: 'asc' };
+        await page.evaluate(() => {
+          document.querySelectorAll('p-table-head-cell').forEach((el) => {
+            (el as any).sort = { id: 'some-id', active: true, direction: 'asc' };
+          });
         });
-      });
 
-      // TODO: scroll trigger :hover + :focus-visible test is missing due piercing selector only works for nested child
-      // TODO: `await forceFocusedState(page, '.focus p-table >>> .scroll-area');`, no class is selectable after piercing selector
+        // TODO: scroll trigger :hover + :focus-visible test is missing due piercing selector only works for nested child
+        // TODO: `await forceFocusedState(page, '.focus p-table >>> .scroll-area');`, no class is selectable after piercing selector
 
-      await forceFocusState(page, '.focus p-table-cell a');
-      await forceFocusState(page, '.focus [slot="caption"] a');
-      await forceFocusState(page, '.focus p-table-head-cell >>> button');
+        await forceFocusState(page, '.focus p-table-cell a');
+        await forceFocusState(page, '.focus [slot="caption"] a');
+        await forceFocusState(page, '.focus p-table-head-cell >>> button');
 
-      await forceFocusHoverState(page, '.focus-hover p-table-cell a');
-      await forceFocusHoverState(page, '.focus-hover [slot="caption"] a');
-      await forceFocusHoverState(page, '.focus-hover p-table-head-cell >>> button');
+        await forceFocusHoverState(page, '.focus-hover p-table-cell a');
+        await forceFocusHoverState(page, '.focus-hover [slot="caption"] a');
+        await forceFocusHoverState(page, '.focus-hover p-table-head-cell >>> button');
 
-      await forceHoverState(page, '.hover p-table-cell a');
-      await forceHoverState(page, '.hover [slot="caption"] a');
-      await forceHoverState(page, '.hover p-table-row:nth-child(3)');
-      await forceHoverState(page, '.hover p-table-head-cell >>> button');
-    })
-  ).toBeFalsy();
-});
-
-it('should have no visual regression for :hover + :focus-visible and dark theme', async () => {
-  const vrt = getVisualRegressionStatesTester();
-  expect(
-    await vrt.test('table-states-dark', async () => {
-      const page = vrt.getPage();
-
-      await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup, { themes: ['dark'] }));
-
-      await page.evaluate(() => {
-        document.querySelectorAll('p-table-head-cell').forEach((el) => {
-          (el as any).sort = { id: 'some-id', active: true, direction: 'asc' };
-        });
-      });
-
-      await forceFocusState(page, '.focus p-table-cell a');
-      await forceFocusState(page, '.focus [slot="caption"] a');
-      await forceFocusState(page, '.focus p-table-head-cell >>> button');
-
-      await forceFocusHoverState(page, '.focus-hover p-table-cell a');
-      await forceFocusHoverState(page, '.focus-hover [slot="caption"] a');
-      await forceFocusHoverState(page, '.focus-hover p-table-head-cell >>> button');
-
-      await forceHoverState(page, '.hover p-table-cell a');
-      await forceHoverState(page, '.hover [slot="caption"] a');
-      await forceHoverState(page, '.hover p-table-row:nth-child(3)');
-      await forceHoverState(page, '.hover p-table-head-cell >>> button');
-    })
-  ).toBeFalsy();
+        await forceHoverState(page, '.hover p-table-cell a');
+        await forceHoverState(page, '.hover [slot="caption"] a');
+        await forceHoverState(page, '.hover p-table-row:nth-child(3)');
+        await forceHoverState(page, '.hover p-table-head-cell >>> button');
+      })
+    ).toBeFalsy();
+  });
 });
