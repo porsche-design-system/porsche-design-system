9abf4a78878ea6a31cf227a652eb2b052b02b5ed
Bernd Hacker <hacker.bernd@gmail.com>
2023-03-22 08:41:06 +0100

    test(components-react): add carousel e2e test verifying lifecycles | bh | #2382

---
+++ packages/components-react/tests/e2e/specs/carousel.e2e.ts
@@ -0,0 +1,23 @@
+import type { Page } from 'puppeteer';
+import { getLifecycleStatus, goto, selectNode, trackLifecycleStatus, waitForComponentsReady } from '../helpers';
+
+let page: Page;
+beforeEach(async () => (page = await browser.newPage()));
+afterEach(async () => await page.close());
+
+it('should not cause new lifecycle when nothing on the component changes', async () => {
+  await goto(page, 'carousel-example-events');
+  await trackLifecycleStatus(page);
+  expect(await waitForComponentsReady(page)).toBe(2); // p-carousel and p-text
+
+  const initialStatus = await getLifecycleStatus(page);
+  expect(initialStatus.componentDidUpdate.all, 'initial componentDidUpdate: all').toBe(0); // tracking was started after page was loaded
+  expect(initialStatus.componentDidLoad.all, 'initial componentDidLoad: all').toBe(0); // tracking was started after page was loaded
+
+  const nextButton = await selectNode(page, 'p-carousel >>> p-button-pure:last-of-type >>> button');
+  await nextButton.click();
+
+  const finalStatus = await getLifecycleStatus(page);
+  expect(finalStatus.componentDidUpdate.all, 'final componentDidUpdate: all').toBe(0);
+  expect(finalStatus.componentDidLoad.all, 'final componentDidLoad: all').toBe(0);
+});
