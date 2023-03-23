a184d54e2df77b91588828e5b5b2558d925e13bc
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-23 15:41:11 +0100

    test: add dummy overview vrt fixture | sas / bh | #2350

--- packages/components-js/tests/vrt/puppeteer/specs/overview.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/overview.vrt.ts
@@ -14,3 +14,14 @@ it('should have no visual regression', async () => {
     })
   ).toBeFalsy();
 });
+
+it('should have no visual regression', async () => {
+  expect(
+    await vrtTest(getVisualRegressionOverviewTester(), 'overview-flaky', '/#overview-flaky', {
+      scenario: async (page) => {
+        await openPopoversAndHighlightSpacer(page);
+        await page.mouse.click(0, 0); // Click top left corner of the page to remove focus on banner
+      },
+    })
+  ).toBeFalsy();
+});
