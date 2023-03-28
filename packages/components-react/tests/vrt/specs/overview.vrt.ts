8c36c69e3a7f25ce7fc941492d1005987c945a24
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-28 12:36:15 +0200

    test: add retry times to flaky overview vrt test | sas | #2424

--- packages/components-react/tests/vrt/specs/overview.vrt.ts
+++ packages/components-react/tests/vrt/specs/overview.vrt.ts
@@ -4,6 +4,9 @@ import {
   vrtTest,
 } from '@porsche-design-system/shared/testing';
 
+// TODO: (overview test is flaky) we shouldn't rely on retries since computed result has to be deterministic
+jest.retryTimes(3);
+
 it('should have no visual regression', async () => {
   expect(
     await vrtTest(getVisualRegressionOverviewTester(), 'overview', '/overview', {
