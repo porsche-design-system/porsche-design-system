4eaaf4271a2aebba4f68bdb45f1c810174afa864
TatjanaGartner <tatjana.gartner@mhp.com>
2023-03-29 17:27:47 +0200

    feat(components-vue): add partials with vite-plugin-html | tg | #2201

--- packages/components-vue/vite.config.ts
+++ packages/components-vue/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import * as path from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 const rootDir = '../..';
 const projectDir = 'projects/vue-wrapper';
@@ -16,6 +17,14 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          getFontLinks: require('@porsche-design-system/components-js/partials').getFontLinks(),
+          getInitialStyles: require('@porsche-design-system/components-js/partials').getInitialStyles(),
+        },
+      },
+    }),
     viteStaticCopy({
       targets: [
         `${rootDir}/LICENSE`,
