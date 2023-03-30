0d6babe1dd996aa6fb3fb552c13c1a8bc04e244f
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-03-30 14:38:05 +0200

    test(components-vue): add PSwitch import to overview flaky page | sas | #2419

--- packages/components-vue/src/pages/OverviewFlakyPage.vue
+++ packages/components-vue/src/pages/OverviewFlakyPage.vue
@@ -6,6 +6,7 @@
     PLinkTileModelSignature,
     PStepperHorizontal,
     PStepperHorizontalItem,
+    PSwitch,
     PorscheDesignSystemProvider,
   } from '@porsche-design-system/components-vue';
 </script>
