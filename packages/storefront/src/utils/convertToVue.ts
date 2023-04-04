3b98602332f3bc5077b177552a2d84185ce92220
Anastasiia Antonova <antonova64@gmail.com>
2023-04-04 21:55:46 +0200

    feat(storefront): convert to vue - property transformation | aa | #2198

--- packages/storefront/src/utils/convertToVue.ts
+++ packages/storefront/src/utils/convertToVue.ts
@@ -1,46 +1,39 @@
-import { camelCase, pascalCase } from 'change-case';
+import { camelCase } from 'change-case';
+import { transformCustomElementTagName, transformToSelfClosingTags } from '@/utils/convertToReact';
 
-export const transformObjectValues = (markup: string): string =>
-  // remove quotes from object values but add double brackets and camelCase
-  markup.replace(/\s(\S+)="({.*?})"/g, (m, $key, $value) => ` ${camelCase($key)}={${$value}}`);
+export const transformEventsToViewSyntax = (markup: string): string =>
+  markup.replace(/\son([a-z]+?)="(.*?)"/g, ' @$1="$2"');
 
-export const transformEvents = (markup: string): string =>
-  // transform to camelCase event binding syntax
-  markup.replace(/\son([a-z]+?)="(.*?)"/g, (m, $key, $value) => ` @on${pascalCase($key)}={() => { ${$value} }}`);
+export const transformAttributesWithObjectValues = (markup: string): string =>
+  markup.replace(/\s([a-z-]+)="{(.*?)}"/g, (m, $key, $value) => ` :${camelCase($key)}="{${$value}}"`);
 
-export const transformBooleanDigitAndUndefinedValues = (markup: string): string =>
-  markup.replace(/\s(\S+)="(true|false|-?\d*|undefined)"/g, ' $1={$2}').replace(/{(911|718)}/g, '"$1"'); // TODO replace temporary 911|718 work around with more generic approach
+export const transformAttributesWithNotDigitValue = (markup: string): string =>
+  markup.replace(/\s([a-z-]+)="([^-\d].*?)"/g, (m, $key, $value) =>
+    // handle aria attributes
+    $key.startsWith('aria-') ? ` ${$key}="${$value}"` : ` :${camelCase($key)}="'${$value}'"`
+  );
 
-export const transformCustomElementTagName = (markup: string): string =>
-  markup.replace(/<(\/?)(p-[\w-]+)/g, (m, $slash, $tag) => `<${$slash}${pascalCase($tag)}`);
+export const transformAttributesWithDigitValue = (markup: string): string =>
+  markup.replace(/\s([a-z-]+)="(-?\d*)"/g, (m, $key, $value) =>
+    $key === 'maxlength'
+      ? ` [maxLength]="${$value}"`
+      : // surround numeric "name" and "model" prop values with single quotes
+      $key === 'name' || $key === 'model' // TODO replace temporary 911|718 work around with more generic approach
+      ? ` :${$key}="'${$value}'"`
+      : ` :${camelCase($key)}="${$value}"`
+  );
 
-export const transformInputs = (markup: string): string => markup.replace(/(<input(?:.[^/]*?))>/g, '$1 />');
-
-export const transformToSelfClosingTags = (markup: string): string =>
-  markup.replace(/(<([A-Za-z-]+)[^>]*?)>\s*<\/\2>/g, '$1 />');
-
-export const transformStyleAttribute = (markup: string): string =>
-  markup.replace(/\sstyle="([\s\S]*?)"/g, (m, $style: string) => {
-    $style = $style
-      .replace(/;/g, ',') // transform semi colons to comma
-      .replace(/,\s*$/g, ''); // remove last comma
-
-    const pairs = $style.split(',').map((p) => {
-      const [prop, val] = p.split(':').map((x) => x.trim());
-      const value = val.match(/^\d+(?:\.\d+)?$/) ? val : `'${val}'`; // numbers don't need quotes
-      return `${camelCase(prop)}: ${value}`;
-    });
-
-    return ` style={{ ${pairs.join(', ')} }}`;
-  });
+export const cleanBooleanAndUndefinedValues = (markup: string): string =>
+  // remove single quotes from boolean values
+  markup.replace(/\s(\[[A-Za-z]+\])="'(true|false|undefined)'"/g, ' $1="$2"');
 
 export const convertToVue = (markup: string): string =>
   [
-    transformObjectValues,
-    transformEvents,
-    transformBooleanDigitAndUndefinedValues,
+    transformEventsToViewSyntax,
+    transformAttributesWithObjectValues,
+    transformAttributesWithNotDigitValue,
+    transformAttributesWithDigitValue,
+    cleanBooleanAndUndefinedValues,
     transformCustomElementTagName,
-    transformInputs,
     transformToSelfClosingTags,
-    transformStyleAttribute,
   ].reduce((previousResult, fn) => fn(previousResult), markup);
