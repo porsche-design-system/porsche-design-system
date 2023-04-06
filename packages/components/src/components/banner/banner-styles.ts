4ae5094c6ed6e77936d7c6fcaf126fd75c92d797
Henri Joss <henri.joss@mhp.com>
2023-04-06 16:17:11 +0200

    fix(components): adds maxWidth 100% | hj / bh | #2159

--- packages/components/src/components/banner/banner-styles.ts
+++ packages/components/src/components/banner/banner-styles.ts
@@ -24,50 +24,49 @@ const easeOutQuad = 'cubic-bezier(0.5,1,0.89,1)';
 export const getComponentCss = (isOpen: boolean): string => {
   return getCss({
     '@global': {
-      ':host': {
-        ...addImportantToEachRule({
-          [cssVariableTop]: '56px',
-          [cssVariableBottom]: '56px',
-          position: 'fixed',
-          top: 'auto',
-          bottom: `var(${cssVariableBottom})`,
-          left: gridExtendedOffsetBase,
-          right: gridExtendedOffsetBase,
-          margin: 0,
-          padding: 0,
-          width: 'auto',
-          zIndex: `var(${cssVariableZIndex},${BANNER_Z_INDEX})`,
-          ...dropShadowHighStyle,
-          ...(isOpen
-            ? {
-                opacity: 1,
-                visibility: 'inherit',
-                transform: 'translate3d(0,0,0)',
-                transition: `opacity ${duration} ${easeInQuad}, transform ${duration} ${easeInQuad}`,
-              }
-            : {
-                opacity: 0,
-                visibility: 'hidden',
-                transform: `translate3d(0,calc(var(${cssVariableBottom}) + 100%),0)`,
-                '&(.hydrated),&(.ssr)': {
-                  transition: `visibility 0s linear ${duration}, opacity ${duration} ${easeOutQuad}, transform ${duration} ${easeOutQuad}`,
-                },
-              }),
-          [getMediaQueryMin('s')]: {
-            top: `var(${cssVariableTop})`,
-            bottom: 'auto',
-            left: gridExtendedOffsetS,
-            right: gridExtendedOffsetS,
-            // space before and after "-" is crucial)
-            ...(!isOpen && { transform: `translate3d(0,calc(-100% - var(${cssVariableTop})),0)` }),
-          },
-          [getMediaQueryMin('xxl')]: {
-            left: gridExtendedOffsetXXL,
-            right: gridExtendedOffsetXXL,
-          },
-          ...hostHiddenStyles,
-        }),
-      },
+      ':host': addImportantToEachRule({
+        [cssVariableTop]: '56px',
+        [cssVariableBottom]: '56px',
+        position: 'fixed',
+        top: 'auto',
+        bottom: `var(${cssVariableBottom})`,
+        left: gridExtendedOffsetBase,
+        right: gridExtendedOffsetBase,
+        margin: 0,
+        padding: 0,
+        width: 'auto',
+        maxWidth: '100%', // If component is wrapped in container with maxWidth
+        zIndex: `var(${cssVariableZIndex},${BANNER_Z_INDEX})`,
+        ...dropShadowHighStyle,
+        ...(isOpen
+          ? {
+              opacity: 1,
+              visibility: 'inherit',
+              transform: 'translate3d(0,0,0)',
+              transition: `opacity ${duration} ${easeInQuad}, transform ${duration} ${easeInQuad}`,
+            }
+          : {
+              opacity: 0,
+              visibility: 'hidden',
+              transform: `translate3d(0,calc(var(${cssVariableBottom}) + 100%),0)`,
+              '&(.hydrated),&(.ssr)': {
+                transition: `visibility 0s linear ${duration}, opacity ${duration} ${easeOutQuad}, transform ${duration} ${easeOutQuad}`,
+              },
+            }),
+        [getMediaQueryMin('s')]: {
+          top: `var(${cssVariableTop})`,
+          bottom: 'auto',
+          left: gridExtendedOffsetS,
+          right: gridExtendedOffsetS,
+          // space before and after "-" is crucial)
+          ...(!isOpen && { transform: `translate3d(0,calc(-100% - var(${cssVariableTop})),0)` }),
+        },
+        [getMediaQueryMin('xxl')]: {
+          left: gridExtendedOffsetXXL,
+          right: gridExtendedOffsetXXL,
+        },
+        ...hostHiddenStyles,
+      }),
     },
   });
 };
