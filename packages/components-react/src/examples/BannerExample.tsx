328e2666bb67a370caf401ea32f8ccf4821bfb85
Henri Joss <henri.joss@mhp.com>
2023-04-06 09:07:07 +0200

    refactor(components-react): changes to useCallback & removes react import  | hj / bh | #2159

--- packages/components-react/src/examples/BannerExample.tsx
+++ packages/components-react/src/examples/BannerExample.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { PButton, PBanner } from '@porsche-design-system/components-react';
 
 export const BannerExamplePage = (): JSX.Element => {
   const [isBannerOpen, setIsBannerOpen] = useState(false);
 
-  const handleDismiss = () => {
-    setIsBannerOpen(false);
-  };
-
-  const handleButtonClick = () => {
+  const onOpen = useCallback(() => {
     setIsBannerOpen(true);
-  };
+  }, []);
+
+  const onDismiss = useCallback(() => {
+    setIsBannerOpen(false);
+  }, []);
 
   return (
     <>
-      <PButton type="button" onClick={handleButtonClick}>
+      <PButton type="button" onClick={onOpen}>
         Open Banner
       </PButton>
-      <PBanner open={isBannerOpen} heading="Some Heading" description="Some Description" onDismiss={handleDismiss} />
+      <PBanner open={isBannerOpen} heading="Some Heading" description="Some Description" onDismiss={onDismiss} />
     </>
   );
 };
