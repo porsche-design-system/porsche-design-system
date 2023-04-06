4acc22ff4d81c1ecfedf4912c42de7ece5a1559c
Henri Joss <henri.joss@mhp.com>
2023-04-06 08:21:24 +0200

    feat(components-react): adds banner example | hj | #2159

---
+++ packages/components-react/src/examples/BannerExample.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import { PButton, PBanner } from '@porsche-design-system/components-react';
+
+export const BannerExamplePage = () => {
+  const [isBannerOpen, setIsBannerOpen] = useState(false);
+
+  const handleDismiss = () => {
+    setIsBannerOpen(false);
+  };
+
+  const handleButtonClick = () => {
+    setIsBannerOpen(true);
+  };
+
+  return (
+    <>
+      <PButton type="button" onClick={handleButtonClick}>
+        Open Banner
+      </PButton>
+      <PBanner open={isBannerOpen} heading="Some Heading" description="Some Description" onDismiss={handleDismiss} />
+    </>
+  );
+};
