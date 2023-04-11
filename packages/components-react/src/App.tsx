941c299c32814e7e2e76aacc769d102200fb2e1b
Bernd Hacker <hacker.bernd@gmail.com>
2023-04-11 09:18:41 +0200

    feat(components-react): add global click handler | bh | #2297

--- packages/components-react/src/App.tsx
+++ packages/components-react/src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import type { MouseEvent } from 'react';
+import { useCallback, useState } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { routes } from './routes';
 
 export const App = (): JSX.Element => {
   const navigate = useNavigate();
   const [selected, setSelected] = useState('');
+
+  // global click handler for custom elements with href property
+  const onContentClick = useCallback((e: MouseEvent<HTMLDivElement>): void => {
+    const { href } = e.target as any;
+    if (href?.startsWith('/')) {
+      e.preventDefault();
+      navigate(href);
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
@@ -24,7 +34,7 @@ export const App = (): JSX.Element => {
         ))}
       </select>
 
-      <div id="app">
+      <div id="app" onClick={onContentClick}>
         <Routes>
           {routes
             .filter((route) => !route.isDisabled)
