880c6fdbce5897c291f873768ea00e24ca5ad95a
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-04-13 12:04:37 +0200

    test: update vrt scenario for crest | sas / oh | #2478

--- packages/components-angular/src/app/pages/crest.component.ts
+++ packages/components-angular/src/app/pages/crest.component.ts
@@ -7,6 +7,30 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     <div class="playground" title="should show crest">
       <p-crest></p-crest>
     </div>
+
+    <div class="playground" title="should render with custom click area">
+      <p-crest [href]="'#'" style="padding: 1.5rem"></p-crest>
+    </div>
+
+    <div class="playground" title="should not exceed parents width">
+      <div style="width: 20px; background: lightsalmon">
+        <p-crest></p-crest>
+      </div>
+      <br />
+      <div style="width: 20px; background: lightsalmon">
+        <p-crest [href]="'#'"></p-crest>
+      </div>
+    </div>
+
+    <div class="playground" title="should not exceed parents height">
+      <div style="height: 20px; background: lightsalmon">
+        <p-crest></p-crest>
+      </div>
+      <br />
+      <div style="height: 20px; background: lightsalmon">
+        <p-crest [href]="'#'"></p-crest>
+      </div>
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- packages/components-react/projects/nextjs/pages/crest.tsx
+++ packages/components-react/projects/nextjs/pages/crest.tsx
@@ -8,6 +8,30 @@ const CrestPage: NextPage = (): JSX.Element => {
       <div className="playground" title="should show crest">
         <PCrest />
       </div>
+
+      <div className="playground" title="should render with custom click area">
+        <PCrest href="#" style={{ padding: '1.5rem' }} />
+      </div>
+
+      <div className="playground" title="should not exceed parents width">
+        <div style={{ width: '20px', background: 'lightsalmon' }}>
+          <PCrest />
+        </div>
+        <br />
+        <div style={{ width: '20px', background: 'lightsalmon' }}>
+          <PCrest href="#" />
+        </div>
+      </div>
+
+      <div className="playground" title="should not exceed parents height">
+        <div style={{ height: '20px', background: 'lightsalmon' }}>
+          <PCrest />
+        </div>
+        <br />
+        <div style={{ height: '20px', background: 'lightsalmon' }}>
+          <PCrest href="#" />
+        </div>
+      </div>
     </>
   );
 };
