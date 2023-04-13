83011112ad9a0c8acbe0d9fe936ea29c061c1105
TatjanaGartner <tatjana.gartner@mhp.com>
2023-04-13 18:35:15 +0200

    test(components-react): move package test | tg | #2444

--- packages/components-js/tests/smoke/specs/package.smoke.ts
+++ packages/components-js/tests/smoke/specs/package.smoke.ts
@@ -5,30 +5,14 @@ import * as fs from 'fs';
 describe('package content', () => {
   const componentsJsFilePath = require.resolve('@porsche-design-system/components-js');
   const componentsJsPackageDir = path.resolve(componentsJsFilePath, '..');
-  const componentsJsFilePaths = globby.sync(`${componentsJsPackageDir}/**/*.js`);
+  const filePaths = globby.sync(`${componentsJsPackageDir}/**/*.js`);
 
-  const componentsReactFilePath = require.resolve('@porsche-design-system/components-react');
-  const componentsReactPackageDir = path.resolve(componentsReactFilePath, '..');
-  const componentsReactFilePaths = globby.sync(`${componentsReactPackageDir}/**/*.js`);
-
-  it.each(
-    componentsJsFilePaths.map((filePath) => [
-      filePath.replace(componentsJsPackageDir, 'dist/components-wrapper'),
-      filePath,
-    ])
-  )('should not contain CDN_BASE_URL_DYNAMIC placeholder or localhost in bundled js file: %s', (_, filePath) => {
-    const fileContent = fs.readFileSync(path.resolve(componentsJsPackageDir, filePath), 'utf8');
-    expect(fileContent).not.toContain('CDN_BASE_URL_DYNAMIC');
-    expect(fileContent).not.toContain('localhost');
-  });
-
-  it.each(
-    componentsReactFilePaths.map((filePath) => [
-      filePath.replace(componentsReactPackageDir, 'dist/react-wrapper'),
-      filePath,
-    ])
-  )('should not contain localhost in bundled react-wrapper file: %s', (_, filePath) => {
-    const fileContent = fs.readFileSync(path.resolve(componentsReactPackageDir, filePath), 'utf8');
-    expect(fileContent).not.toContain('localhost');
-  });
+  it.each(filePaths.map((filePath) => [filePath.replace(componentsJsPackageDir, 'dist/components-wrapper'), filePath]))(
+    'should not contain CDN_BASE_URL_DYNAMIC placeholder or localhost in bundled js file: %s',
+    (_, filePath) => {
+      const fileContent = fs.readFileSync(path.resolve(componentsJsPackageDir, filePath), 'utf8');
+      expect(fileContent).not.toContain('CDN_BASE_URL_DYNAMIC');
+      expect(fileContent).not.toContain('localhost');
+    }
+  );
 });
---
+++ packages/components-react/projects/react-ssr-wrapper/tests/unit/specs/package.spec.ts
@@ -0,0 +1,19 @@
+import * as path from 'path';
+import * as globby from 'globby';
+import * as fs from 'fs';
+
+describe('package content', () => {
+  const componentsReactFilePath = require.resolve('@porsche-design-system/components-react');
+  const componentsReactPackageDir = path.resolve(componentsReactFilePath, '..');
+  const componentsReactFilePaths = globby.sync(`${componentsReactPackageDir}/**/*.js`);
+
+  it.each(
+    componentsReactFilePaths.map((filePath) => [
+      filePath.replace(componentsReactPackageDir, 'dist/react-wrapper'),
+      filePath,
+    ])
+  )('should not contain localhost in bundled react-wrapper file: %s', (_, filePath) => {
+    const fileContent = fs.readFileSync(path.resolve(componentsReactPackageDir, filePath), 'utf8');
+    expect(fileContent).not.toContain('localhost');
+  });
+});
