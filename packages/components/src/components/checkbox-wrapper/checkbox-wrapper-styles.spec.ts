3030fc264433183d47deb9cd2302c19fd96fea4d
Anastasiia Antonova <antonova64@gmail.com>
2023-04-13 19:40:17 +0200

    feat(components): checkbox wrapper styles - unit test update | aa | #2439

--- packages/components/src/components/checkbox-wrapper/checkbox-wrapper-styles.spec.ts
+++ packages/components/src/components/checkbox-wrapper/checkbox-wrapper-styles.spec.ts
@@ -2,32 +2,39 @@ import { getComponentCss } from './checkbox-wrapper-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, 'none', true, 'light'],
-    [false, 'none', false, 'light'],
-    [true, 'none', true, 'light'],
-    [true, 'none', false, 'light'],
-    [false, 'success', true, 'light'],
-    [false, 'success', false, 'light'],
-    [true, 'success', true, 'light'],
-    [true, 'success', false, 'light'],
-    [false, 'error', true, 'light'],
-    [false, 'error', false, 'light'],
-    [true, 'error', true, 'light'],
-    [true, 'error', false, 'light'],
-    [false, 'none', true, 'dark'],
-    [false, 'none', false, 'dark'],
-    [true, 'none', true, 'dark'],
-    [true, 'none', false, 'dark'],
-    [false, 'success', true, 'dark'],
-    [false, 'success', false, 'dark'],
-    [true, 'success', true, 'dark'],
-    [true, 'success', false, 'dark'],
-    [false, 'error', true, 'dark'],
-    [false, 'error', false, 'dark'],
-    [true, 'error', true, 'dark'],
-    [true, 'error', false, 'dark'],
-    [{ base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'light'],
-  ])('should return correct css for hideLabel: %o, state: %s, isDisabled: %s and theme %s', (...args) => {
-    expect(getComponentCss(...args)).toMatchSnapshot();
-  });
+    [false, 'none', true, false, false, 'light'],
+    [false, 'none', false, false, false, 'light'],
+    [true, 'none', true, false, false, 'light'],
+    [true, 'none', false, false, false, 'light'],
+    [true, 'none', true, true, false, 'light'],
+    [true, 'none', false, true, true, 'light'],
+    [false, 'success', true, false, false, 'light'],
+    [false, 'success', false, false, false, 'light'],
+    [true, 'success', true, false, false, 'light'],
+    [true, 'success', false, false, false, 'light'],
+    [false, 'error', true, false, false, 'light'],
+    [false, 'error', false, false, false, 'light'],
+    [true, 'error', true, false, false, 'light'],
+    [true, 'error', false, false, false, 'light'],
+    [false, 'none', true, false, false, 'dark'],
+    [false, 'none', false, false, false, 'dark'],
+    [true, 'none', true, false, false, 'dark'],
+    [true, 'none', false, false, false, 'dark'],
+    [true, 'none', true, true, false, 'dark'],
+    [true, 'none', false, true, true, 'dark'],
+    [false, 'success', true, false, false, 'dark'],
+    [false, 'success', false, false, false, 'dark'],
+    [true, 'success', true, false, false, 'dark'],
+    [true, 'success', false, false, false, 'dark'],
+    [false, 'error', true, false, false, 'dark'],
+    [false, 'error', false, false, false, 'dark'],
+    [true, 'error', true, false, false, 'dark'],
+    [true, 'error', false, false, false, 'dark'],
+    [{ base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, false, false, 'light'],
+  ])(
+    'should return correct css for hideLabel: %o, state: %s, isDisabled: %s, isLoading: %s, isChecked: %s and theme %s',
+    (...args) => {
+      expect(getComponentCss(...args)).toMatchSnapshot();
+    }
+  );
 });
