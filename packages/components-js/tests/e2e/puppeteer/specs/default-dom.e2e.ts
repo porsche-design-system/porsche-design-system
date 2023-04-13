c7dfd4258b021e74995f62d13f9abc70d68e4292
Henri Joss <henri.joss@mhp.com>
2023-04-13 13:26:15 +0200

    fix(components-js): flaky default-dom | hj | #fix-flaky-tests

--- packages/components-js/tests/e2e/puppeteer/specs/default-dom.e2e.ts
+++ packages/components-js/tests/e2e/puppeteer/specs/default-dom.e2e.ts
@@ -3,10 +3,13 @@ import {
   expectShadowDomToMatchSnapshot,
   goto,
   selectNode,
+  setContentWithDesignSystem,
   waitForComponentsReady,
+  waitForStencilLifecycle,
 } from '../helpers';
 import type { Page } from 'puppeteer';
 import { INTERNAL_TAG_NAMES, TAG_NAMES } from '@porsche-design-system/shared';
+import * as beautify from 'js-beautify';
 
 let page: Page;
 beforeEach(async () => (page = await browser.newPage()));
@@ -15,12 +18,21 @@ afterEach(async () => await page.close());
 it.each(TAG_NAMES.filter((x) => !INTERNAL_TAG_NAMES.includes(x)))(
   'should have no basic DOM regression for %s',
   async (tagName) => {
-    await goto(page, ''); // start page
+    await page.setContent(
+      `<!DOCTYPE html>
+    <html>
+      <head>
+        <base href="http://localhost:8575"> <!-- NOTE: we need a base tag so that document.baseURI returns something else than "about:blank" -->
+        <script type="text/javascript" src="http://localhost:8575/index.js"></script>
+      </head>
+      <body>
+        <script type="text/javascript">porscheDesignSystem.load();</script>
+        ${buildDefaultComponentMarkup(tagName)}
+      </body>
+    </html>`,
+      { waitUntil: 'networkidle0' }
+    );
 
-    const markup = buildDefaultComponentMarkup(tagName);
-    await page.evaluate((markup: string) => {
-      document.getElementById('app').innerHTML = markup;
-    }, markup);
     await waitForComponentsReady(page);
     if (tagName === 'p-icon') {
       // some buffer for the svg to load
