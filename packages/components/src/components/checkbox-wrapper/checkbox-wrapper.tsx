2370d8f022ebd566a33177a46e24cd3c92e50aa4
TatjanaGartner <tatjana.gartner@mhp.com>
2023-04-19 11:08:50 +0200

    fix(components): apply radio-button-wrapper workaround for safari also on checkbox-wrapper | tg | #2487

--- packages/components/src/components/checkbox-wrapper/checkbox-wrapper.tsx
+++ packages/components/src/components/checkbox-wrapper/checkbox-wrapper.tsx
@@ -19,6 +19,7 @@ import { getComponentCss } from './checkbox-wrapper-styles';
 import type { CheckboxWrapperState } from './checkbox-wrapper-utils';
 import { StateMessage } from '../common/state-message/state-message';
 import { Required } from '../common/required/required';
+import { addChangeListener } from '../../utils/checkbox-radio-button-wrapper/checkbox-radio-button-wrapper-utils';
 
 const propTypes: PropTypes<typeof CheckboxWrapper> = {
   label: AllowedTypes.string,
@@ -58,6 +59,7 @@ export class CheckboxWrapper {
 
   public componentWillLoad(): void {
     this.input = getOnlyChildOfKindHTMLElementOrThrow(this.host, 'input[type=checkbox]');
+    addChangeListener(this.input);
     this.observeAttributes(); // once initially
   }
 
