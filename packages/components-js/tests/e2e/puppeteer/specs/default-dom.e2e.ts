6ac5879c8fb3f5a56c0ef14bf5ed8851099d2059
Henri Joss <henri.joss@mhp.com>
2023-04-19 09:43:26 +0200

    chore(components-js): removes unused imports | hj | #fix-flaky-tests

--- packages/components-js/tests/e2e/puppeteer/specs/default-dom.e2e.ts
+++ packages/components-js/tests/e2e/puppeteer/specs/default-dom.e2e.ts
@@ -3,13 +3,10 @@ import {
   expectShadowDomToMatchSnapshot,
   goto,
   selectNode,
-  setContentWithDesignSystem,
   waitForComponentsReady,
-  waitForStencilLifecycle,
 } from '../helpers';
 import type { Page } from 'puppeteer';
 import { INTERNAL_TAG_NAMES, TAG_NAMES } from '@porsche-design-system/shared';
-import * as beautify from 'js-beautify';
 
 let page: Page;
 beforeEach(async () => (page = await browser.newPage()));
