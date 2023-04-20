064e2331f24ce280855ff6a65a877f9fc913eb17
Bernd Hacker <hacker.bernd@gmail.com>
2023-04-20 11:36:12 +0200

    refactor(components): import FONT_FACE_CDN_URL from assets package in injectGlobalStyle | bh

--- packages/components/src/utils/inject-global-style.ts
+++ packages/components/src/utils/inject-global-style.ts
@@ -1,4 +1,4 @@
-import { FONT_FACE_CDN_URL } from '@porsche-design-system/styles';
+import { FONT_FACE_CDN_URL } from '@porsche-design-system/assets';
 
 export const injectGlobalStyle = (): void => {
   if (typeof document === 'undefined') {
@@ -8,7 +8,7 @@ export const injectGlobalStyle = (): void => {
   const styleUrl =
     ROLLUP_REPLACE_IS_STAGING === 'production' ? FONT_FACE_CDN_URL : 'http://localhost:3001/styles/font-face.min.css';
   const { head } = document;
-    if (!head.querySelector(`link[href="${styleUrl}"]`)) {
+  if (!head.querySelector(`link[href="${styleUrl}"]`)) {
     const link = document.createElement('link');
     link.href = styleUrl;
     link.type = 'text/css';
