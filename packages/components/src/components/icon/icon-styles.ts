b799a6dc2399f9c2f11d7f3ce5cc5aea0cdce776
TatjanaGartner <tatjana.gartner@mhp.com>
2023-04-21 10:57:03 +0200

    fix(components): notifications icon color on programmatic state change | tg | #2487

--- packages/components/src/components/icon/icon-styles.ts
+++ packages/components/src/components/icon/icon-styles.ts
@@ -99,6 +99,7 @@ export const getComponentCss = (
         margin: 0,
         padding: 0,
         ...(!isColorInherit && {
+          transform: 'translateZ(0)', // Safari: Programmatic icon color change (relevant for notifications and pagination)
           filter: filter[theme][color],
           ...(isHighContrastMode &&
             getSchemedHighContrastMediaQuery(
