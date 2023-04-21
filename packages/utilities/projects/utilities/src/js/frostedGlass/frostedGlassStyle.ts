e7c0d0864b4cd39386261accbff7af507d80c3b8
Anastasiia Antonova <antonova64@gmail.com>
2023-04-21 12:58:48 +0200

    fix(utilities): frosted glass style - test fix translate z | aa

--- packages/utilities/projects/utilities/src/js/frostedGlass/frostedGlassStyle.ts
+++ packages/utilities/projects/utilities/src/js/frostedGlass/frostedGlassStyle.ts
@@ -2,5 +2,5 @@ const backdropFilter = 'blur(32px)';
 export const frostedGlassStyle = {
   WebkitBackdropFilter: backdropFilter,
   backdropFilter,
-  transform: 'translate3d(0,0,0)',
+  transform: 'translateZ(0)',
 } as const;
