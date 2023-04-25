65676519016e56d2695014e56a8d95b9b0bdd37a
Bernd Hacker <hacker.bernd@gmail.com>
2023-04-25 13:16:34 +0200

    feat: upgrade playwright | bh | #2277

--- docker/node/build-and-push-base-image-playwright.sh
+++ docker/node/build-and-push-base-image-playwright.sh
@@ -2,7 +2,7 @@
 
 REGISTRY=ghcr.io/porsche-design-system/porsche-design-system
 IMAGE=playwright
-TAG=v1.27.0-focal-v3-universal
+TAG=v1.32.3-focal-v3-universal
 
 docker pull $REGISTRY/$IMAGE:latest
 
