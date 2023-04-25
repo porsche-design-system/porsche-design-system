c3f72cf73541e73ecf22507c8868f3d83d2e0129
Bernd Hacker <hacker.bernd@gmail.com>
2023-04-25 14:55:21 +0200

    ci: build universal docker image next to old image | bh | #2277

--- docker/node/build-and-push-base-image-playwright.sh
+++ docker/node/build-and-push-base-image-playwright.sh
@@ -2,10 +2,15 @@
 
 REGISTRY=ghcr.io/porsche-design-system/porsche-design-system
 IMAGE=playwright
-TAG=v1.32.3-focal-v3-universal
+TAG=v1.32.3-focal-v3
 
 docker pull $REGISTRY/$IMAGE:latest
 
+# multiplatform universal image
 # https://dev.to/docker/choosing-the-right-docker-image-for-your-apple-m1-pro-440l
-docker buildx build -f Dockerfile-playwright --platform linux/amd64,linux/arm64 --push -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
+docker buildx build -f Dockerfile-playwright --platform linux/amd64,linux/arm64 --push -t $REGISTRY/$IMAGE:$TAG-universal .
 
+# default image
+docker build -f Dockerfile-playwright -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
+docker push $REGISTRY/$IMAGE:$TAG
+docker push $REGISTRY/$IMAGE:latest
