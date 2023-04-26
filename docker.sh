dd841a08f4ae1535b47528b5640e6dca00885832
Bernd Hacker <hacker.bernd@gmail.com>
2023-04-26 07:43:57 +0200

    ci: disable docker universal build | bh | #2277

--- docker.sh
+++ docker.sh
@@ -9,5 +9,7 @@ export RUN_UID="$(id -u)"
 export RUN_GID="$(id -g)"
 export COMPOSE_PROJECT_NAME="porsche-design-system-v1"
 
+docker pull ghcr.io/porsche-design-system/porsche-design-system/playwright:v1.32.3-focal-v3
+
 docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" run --rm change-volume-owner
 docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" run --service-ports --rm design-system "${@}"
--- docker/node/build-and-push-base-image-playwright.sh
+++ docker/node/build-and-push-base-image-playwright.sh
@@ -4,13 +4,12 @@ REGISTRY=ghcr.io/porsche-design-system/porsche-design-system
 IMAGE=playwright
 TAG=v1.32.3-focal-v3
 
-docker pull $REGISTRY/$IMAGE:latest
-
 # multiplatform universal image
 # https://dev.to/docker/choosing-the-right-docker-image-for-your-apple-m1-pro-440l
-docker buildx build -f Dockerfile-playwright --platform linux/amd64,linux/arm64 --push -t $REGISTRY/$IMAGE:$TAG-universal .
+# docker buildx build -f Dockerfile-playwright --platform linux/amd64,linux/arm64 --push -t $REGISTRY/$IMAGE:$TAG-universal .
 
 # default image
+docker pull $REGISTRY/$IMAGE:latest
 docker build -f Dockerfile-playwright -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
 docker push $REGISTRY/$IMAGE:$TAG
 docker push $REGISTRY/$IMAGE:latest
