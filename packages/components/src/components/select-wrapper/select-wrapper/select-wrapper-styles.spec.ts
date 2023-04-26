4280bfb6d375d67a38c532eafb59c2fa203fa945
Henri Joss <henri.joss@mhp.com>
2023-04-26 10:58:17 +0200

    test(components): updates test | hj | #2487-select-border

--- packages/components/src/components/select-wrapper/select-wrapper/select-wrapper-styles.spec.ts
+++ packages/components/src/components/select-wrapper/select-wrapper/select-wrapper-styles.spec.ts
@@ -2,21 +2,21 @@ import { getComponentCss } from './select-wrapper-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, 'none', 'light'],
-    [false, false, 'success', 'light'],
-    [false, false, 'error', 'light'],
-    [false, true, 'none', 'light'],
-    [false, true, 'success', 'light'],
-    [false, true, 'error', 'light'],
-    [false, false, 'none', 'dark'],
-    [false, false, 'success', 'dark'],
-    [false, false, 'error', 'dark'],
-    [false, true, 'none', 'dark'],
-    [false, true, 'success', 'dark'],
-    [false, true, 'error', 'dark'],
-    [true, true, 'error', 'dark'],
-    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', 'light'],
-  ])('should return correct css for hideLabel: %o, state: %s and theme: %s', (...args) => {
+    [false, false, false, 'none', 'light'],
+    [false, true, false, 'success', 'light'],
+    [false, false, false, 'error', 'light'],
+    [false, true, true, 'none', 'light'],
+    [false, false, true, 'success', 'light'],
+    [false, true, true, 'error', 'light'],
+    [false, false, false, 'none', 'dark'],
+    [false, true, false, 'success', 'dark'],
+    [false, false, false, 'error', 'dark'],
+    [false, true, true, 'none', 'dark'],
+    [false, false, true, 'success', 'dark'],
+    [false, true, true, 'error', 'dark'],
+    [true, false, true, 'error', 'dark'],
+    [false, true, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', 'light'],
+  ])('should return correct css for disabled: %s, native: %s, hideLabel: %o, state: %s and theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
 });
