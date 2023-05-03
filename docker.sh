6ecd8f2f728bb36d41e4a0d176e0048fa8aa16ce
Bernd Hacker <hacker.bernd@gmail.com>
2023-05-03 16:32:27 +0200

    ci: set DOCKER_DEFAULT_PLATFORM env variable via docker.sh | bh

--- docker.sh
+++ docker.sh
@@ -8,8 +8,7 @@ SCRIPT_DIR="$(cd `dirname ${0}` && pwd)"
 export RUN_UID="$(id -u)"
 export RUN_GID="$(id -g)"
 export COMPOSE_PROJECT_NAME="porsche-design-system-v1"
-
-docker pull ghcr.io/porsche-design-system/porsche-design-system/playwright:v1.33.0-focal --platform linux/amd64
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
 docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" run --rm change-volume-owner
 docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" run --service-ports --rm design-system "${@}"
