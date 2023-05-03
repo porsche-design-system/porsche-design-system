03f4cfb4580ccef30b40e3daaa8d63638737cc72
Henri Joss <henri.joss@mhp.com>
2023-05-03 13:30:32 +0200

    test(components-js): adds select-wrapper scenario & refactor test | hj | #2487-select-border

--- packages/components-js/tests/vrt/puppeteer/specs/component-high-contrast-mode.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/component-high-contrast-mode.vrt.ts
@@ -5,6 +5,7 @@ import {
   vrtTest,
 } from '@porsche-design-system/shared/testing';
 import type { Component } from '../helpers';
+import { Theme } from '@porsche-design-system/utilities';
 
 const components: Component[] = [
   'accordion',
@@ -65,34 +66,29 @@ const scenarioModalOrBanner = async (page: Page) => {
   await page.mouse.click(0, 0); // click top left corner of the page to remove focus on modal
 };
 
+const scenarioSelectWrapper = async (page: Page) => {
+  await page.click('#open-options');
+};
+
+const highContrastTest = async (component: Component, theme: Theme) => {
+  await vrtTest(
+    getVisualRegressionStatesTester(),
+    `${component === 'fieldset-wrapper' ? 'fieldset' : `${component}`}-high-contrast-${theme}`,
+    `/#${component}`,
+    {
+      ...(component === 'popover' && { scenario: scenarioPopover }),
+      ...(component === 'select-wrapper' && { scenario: scenarioSelectWrapper }),
+      ...(['modal', 'banner'].includes(component) && { scenario: scenarioModalOrBanner }),
+      forcedColorsEnabled: true,
+      prefersColorScheme: theme,
+    }
+  );
+};
+
 it.each(components)('should have no visual regression for component in high-contrast light: %s', async (component) => {
-  expect(
-    await vrtTest(
-      getVisualRegressionStatesTester(),
-      `${component === 'fieldset-wrapper' ? 'fieldset' : `${component}`}-high-contrast-light`,
-      `/#${component}`,
-      {
-        ...(component === 'popover' && { scenario: scenarioPopover }),
-        ...(['modal', 'banner'].includes(component) && { scenario: scenarioModalOrBanner }),
-        forcedColorsEnabled: true,
-        prefersColorScheme: 'light',
-      }
-    )
-  ).toBeFalsy();
+  expect(await highContrastTest(component, 'light')).toBeFalsy();
 });
 
 it.each(components)('should have no visual regression for component in high-contrast dark: %s', async (component) => {
-  expect(
-    await vrtTest(
-      getVisualRegressionStatesTester(),
-      `${component === 'fieldset-wrapper' ? 'fieldset' : `${component}`}-high-contrast-dark`,
-      `/#${component}`,
-      {
-        ...(component === 'popover' && { scenario: scenarioPopover }),
-        ...(['modal', 'banner'].includes(component) && { scenario: scenarioModalOrBanner }),
-        forcedColorsEnabled: true,
-        prefersColorScheme: 'dark',
-      }
-    )
-  ).toBeFalsy();
+  expect(await highContrastTest(component, 'dark')).toBeFalsy();
 });
