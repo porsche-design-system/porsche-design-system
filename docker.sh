85ddb7fd3eb55c35b8d0da7eb2e219a7721c5c9a
Bernd Hacker <hacker.bernd@gmail.com>
2023-05-03 16:21:22 +0200

    ci: add docker pull to docker.sh | bh

--- docker.sh
+++ docker.sh
@@ -9,5 +9,7 @@ export RUN_UID="$(id -u)"
 export RUN_GID="$(id -g)"
 export COMPOSE_PROJECT_NAME="porsche-design-system-v1"
 
+docker pull ghcr.io/porsche-design-system/porsche-design-system/playwright:v1.33.0-focal --platform linux/amd64
+
 docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" run --rm change-volume-owner
 docker-compose -f "${SCRIPT_DIR}/docker-compose.yml" run --service-ports --rm design-system "${@}"
