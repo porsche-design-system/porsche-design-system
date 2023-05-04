137debd7f1fc8e6990979b82397697cba4a69f9d
Bernd Hacker <hacker.bernd@gmail.com>
2023-05-04 10:47:25 +0200

    test(components-angular/react): remove unused done callback in styles drop-shadow vrts | bh

--- packages/components-react/tests/vrt/specs/styles/drop-shadow.vrt.ts
+++ packages/components-react/tests/vrt/specs/styles/drop-shadow.vrt.ts
@@ -1,6 +1,6 @@
 import { getVisualRegressionStatesTester, vrtTest } from '@porsche-design-system/shared/testing';
 
 const id = 'styles-drop-shadow';
-it('should have no visual regression', async (viewport) => {
+it('should have no visual regression', async () => {
   expect(await vrtTest(getVisualRegressionStatesTester(), id, `/${id}`)).toBeFalsy();
 });
