13961b84d8eae1801354cc88cdf3f26bdbd45a6e
Oliver Hook <oliver.hook@porsche.digital>
2023-05-09 08:25:49 +0200

    fix(components): update wordmark svg and fix safari rendering | oh | #2500

--- packages/components/src/components/wordmark/wordmark-styles.ts
+++ packages/components/src/components/wordmark/wordmark-styles.ts
@@ -21,7 +21,13 @@ export const getComponentCss = (size: WordmarkSize, theme: Theme): string => {
           maxWidth: '100%',
           maxHeight: '100%',
           boxSizing: 'content-box', // needed for correct height calculation when padding is set on host (e.g. custom click area)
-          ...(!(size === 'inherit') && { height: 'clamp(0.63rem, 0.42vw + 0.5rem, 1rem)' }),
+          ...(!(size === 'inherit') && {
+            height: 'clamp(0.63rem, 0.42vw + 0.5rem, 1rem)',
+            // workaround for Safari to optimize image rendering
+            '@supports (height: round(down, 1px, 1px))': {
+              height: 'round(down, clamp(0.63rem, 0.42vw + 0.5rem, 1rem), 1px)',
+            },
+          }),
           ...hostHiddenStyles,
         }),
       },
