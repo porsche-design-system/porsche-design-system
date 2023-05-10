ab0042e3b943ec13756e76bd9a0d301971f6d3d7
Bernd Hacker <hacker.bernd@gmail.com>
2023-05-10 10:42:02 +0200

    docs(utilities): add comments to dropShadow styles | bh | #2519

--- packages/utilities/projects/utilities/src/js/dropShadow/dropShadowLowStyle.ts
+++ packages/utilities/projects/utilities/src/js/dropShadow/dropShadowLowStyle.ts
@@ -1,5 +1,5 @@
 import { _dropShadowBackgroundColor } from './dropShadowShared';
 
 export const dropShadowLowStyle = {
-  boxShadow: `0px 3px 8px ${_dropShadowBackgroundColor}`,
+  boxShadow: `0px 3px 8px ${_dropShadowBackgroundColor}`, // filter: drop-shadow() causes visual glitches in Firefox in combination with frostedGlassStyle
 } as const;
