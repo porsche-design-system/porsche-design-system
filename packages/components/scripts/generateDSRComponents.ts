62c745e88ed14db1af9fe0daf20ddc190c874394
Henri Joss <henri.joss@mhp.com>
2023-05-15 11:26:27 +0200

    fix(components): replacement for hasSecondaryContent in dsr generator | hj | #2431

--- packages/components/scripts/generateDSRComponents.ts
+++ packages/components/scripts/generateDSRComponents.ts
@@ -176,7 +176,7 @@ import { get${componentName}Css } from '${stylesBundleImportPath}';
             `namedSlotChildren.filter(({ props: { slot } }) => slot === 'subline').length > 0`
           )
           .replace(
-            /hasNamedSlot\(this\.props\.host, '(caption|title|description|heading|header|footer)'\)/g,
+            /hasNamedSlot\(this\.props\.host, '(caption|title|description|heading|header|footer|secondary-content)'\)/g,
             `namedSlotChildren.filter(({ props: { slot } }) => slot === '$1').length > 0`
           );
       } else if (newFileContent.includes('FunctionalComponent')) {
@@ -244,8 +244,8 @@ import { get${componentName}Css } from '${stylesBundleImportPath}';
           );
       } else if (tagName === 'p-flyout') {
         newFileContent = newFileContent
-          .replace(/this\.props\.(hasHeader|hasFooter)/g, '$1')
-          .replace(/(?:hasHeader|hasFooter) =/g, 'const $&');
+          .replace(/this\.props\.(hasHeader|hasFooter|hasSecondaryContent)/g, '$1')
+          .replace(/(?:hasHeader|hasFooter|hasSecondaryContent) =/g, 'const $&');
       } else if (tagName === 'p-tabs') {
         newFileContent = newFileContent
           .replace(/this\.tabsItemElements(\.map)/, `otherChildren$1`)
