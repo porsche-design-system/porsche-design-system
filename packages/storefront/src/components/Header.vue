37cb192f5787d982bc85a175964fa065b20b9c88
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-05-25 15:08:36 +0200

    feat(storefront): update header component to match new design language on desktop view | sas | #2531

--- packages/storefront/src/components/Header.vue
+++ packages/storefront/src/components/Header.vue
@@ -1,78 +1,95 @@
 <template>
-  <nav>
-    <div class="wordmark">
-      <router-link to="/" v-slot="{ href, navigate }">
-        <p-wordmark :href="href" @click="navigate"></p-wordmark>
-      </router-link>
+  <header>
+    <div>
+      <p-heading size="small" class="heading">Porsche Design System</p-heading>
+      <VersionSelect class="select" />
     </div>
-    <VersionSelect class="versionSelect" />
-    <Search class="search" />
-  </nav>
+    <router-link to="/" v-slot="{ href, navigate }">
+      <p-wordmark :href="href" @click="navigate"></p-wordmark>
+    </router-link>
+    <div>
+      <Search class="search" />
+      <p-link-pure
+        class="anchor"
+        :icon-source="require('@/assets/github.svg')"
+        href="https://github.com/porsche-design-system/porsche-design-system"
+        target="_blank"
+        hide-label="true"
+        style="padding: 13px"
+      >
+        Navigate to GitHub repository of Porsche Design System
+      </p-link-pure>
+      <Menu class="menu"></Menu>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import Component from 'vue-class-component';
-  import { version } from '@porsche-design-system/components-js/package.json';
-  import Search from '@/components/Search.vue';
-  import Brand from '@/components/Brand.vue';
   import VersionSelect from '@/components/VersionSelect.vue';
+  import Search from '@/components/Search.vue';
+  import Menu from '@/components/Menu.vue';
 
   @Component({
     components: {
       VersionSelect,
-      Brand,
       Search,
+      Menu,
     },
   })
-  export default class Header extends Vue {
-    public version: string = version;
-
-    public versionOptions: string[] = ['v1', 'v2', 'v3', 'latest'];
-    public onVersionChange = (event: Event): void => {
-      window.location.href = `https://designsystem.porsche.com/${(event.target as HTMLInputElement).value}/`;
-    };
-  }
+  export default class Header extends Vue {}
 </script>
 
 <style scoped lang="scss">
   @use '@porsche-design-system/components-js/styles' as *;
 
-  nav {
-    display: flex;
-    flex-grow: 1;
-    gap: $pds-spacing-static-medium;
+  header {
+    @include pds-frosted-glass;
+    position: fixed;
+    inset: 0 0 auto;
+    z-index: 1;
+    display: grid;
+    grid-template-columns: minmax(0, 1fr) auto minmax(0, 1fr);
+    gap: $pds-grid-gap;
     align-items: center;
-    padding: 0 $pds-spacing-static-medium;
-    // background: #fff;
-  }
+    padding: $pds-spacing-static-medium $pds-grid-wide-offset-base;
+    border-bottom: 1px solid $pds-theme-light-background-surface;
+    background-color: rgb(255 255 255 / 79%);
+    -webkit-backdrop-filter: blur(8px); // TODO: remove, as soon as "pds-frosted-glass" mixin was adjusted to 8px blur
+    backdrop-filter: blur(8px); // TODO: remove, as soon as "pds-frosted-glass" mixin was adjusted to 8px blur
 
-  .wordmark {
-    flex: 1 0 auto;
-    text-align: center;
-    @include pds-media-query-min-max('base', 's') {
-      flex: 1 0 100%;
+    @include pds-media-query-min('s') {
+      padding: $pds-spacing-static-medium $pds-grid-wide-offset-s;
+    }
+
+    @include pds-media-query-min('xxl') {
+      padding: $pds-spacing-static-medium $pds-grid-wide-offset-xxl;
+    }
+
+    & > :first-child,
+    & > :last-child {
+      display: flex;
+      align-items: center;
+      gap: $pds-spacing-static-small;
+    }
+
+    & > :last-child {
+      justify-content: flex-end;
     }
   }
 
-  p-wordmark {
-    vertical-align: middle;
-    @include pds-media-query-min-max('base', 's') {
-      vertical-align: top;
-    }
-  }
-
-  .versionSelect {
-    @include pds-media-query-min-max('base', 's') {
+  .heading,
+  .select,
+  .search,
+  .anchor {
+    @include pds-media-query-max('s') {
       display: none;
     }
   }
 
-  .search {
-    position: relative;
-    flex: 0 1 auto;
-    max-width: 17.5rem;
-    @include pds-media-query-min-max('base', 's') {
+  .menu {
+    @include pds-media-query-min('s') {
       display: none;
     }
   }
