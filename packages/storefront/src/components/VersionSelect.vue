aff12d6b9813ba3a3ad4e730841c749ef59d378f
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-05-25 15:23:29 +0200

    feat(storefront): clean up version select component | sas | #2531

--- packages/storefront/src/components/VersionSelect.vue
+++ packages/storefront/src/components/VersionSelect.vue
@@ -1,33 +1,25 @@
 <template>
-  <!--<p-select-wrapper label="Some label" hide-label="true">
-    <select name="some-name">
-      <option v-for="option in versionOptions" :key="option" v-bind:value="option">{{ option }}</option>
-    </select>
-  </p-select-wrapper>-->
-  <select v-on:change="onVersionChange" aria-label="Switch Porsche Design System version">
-      <option v-for="option in versionOptions" :key="option" v-bind:value="option">{{ option }}</option>
+  <select v-model="version" v-on:change="onVersionChange" aria-label="Switch Porsche Design System version">
+    <option v-for="option in versionOptions" :key="option" :value="option">{{ option }}</option>
   </select>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
   import Component from 'vue-class-component';
-  import { version } from '@porsche-design-system/components-js/package.json';
-  import Search from '@/components/Search.vue';
-  import Brand from '@/components/Brand.vue';
+  // import { version } from '@porsche-design-system/components-js/package.json';
 
   @Component({
-    components: {
-      Brand,
-      Search,
-    },
+    components: {},
   })
   export default class VersionSelect extends Vue {
-    public version: string = version;
+    // public version: string = version;
 
-    public versionOptions: string[] = ['v1', 'v2', 'v3'];
+    public version = 'v3';
+    public versionOptions: string[] = ['v3', 'v2', 'v1'];
+
     public onVersionChange = (event: Event): void => {
-      window.location.href = `https://designsystem.porsche.com/${(event.target as HTMLInputElement).value}/`;
+      window.location.href = `https://designsystem.porsche.com/${(event.target as HTMLInputElement).value}`;
     };
   }
 </script>
@@ -38,18 +30,15 @@
   select {
     @include pds-focus;
     @include pds-text-x-small;
-    min-width: 2rem;
-    border: 0;
-    padding: 2px $pds-spacing-static-small;
-    box-sizing: content-box;
-    border-radius: $pds-border-radius-small;
     -webkit-appearance: none;
     appearance: none;
-    background: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0Ij48cGF0aCBkPSJtMjAgOC43NS04IDYtOC02djEuMjZMMTIgMTZsOC01Ljk5VjguNzV6Ii8+PC9zdmc+") $pds-theme-light-background-surface no-repeat center right 7px;
-    background-size: 14px;
-  }
-
-  option {
-    font: inherit;
+    border: 0;
+    min-width: 2rem;
+    padding: $pds-spacing-static-x-small $pds-spacing-static-small;
+    cursor: pointer;
+    box-sizing: content-box;
+    border-radius: $pds-border-radius-small;
+    background: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0Ij48cGF0aCBkPSJtMjAgOC43NS04IDYtOC02djEuMjZMMTIgMTZsOC01Ljk5VjguNzV6Ii8+PC9zdmc+')
+      $pds-theme-light-background-surface no-repeat center right 7px / 14px;
   }
 </style>
