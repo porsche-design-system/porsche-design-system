d8f83732e843261265c208b188d3babbe742ef2a
Henri Joss <henri.joss@mhp.com>
2023-06-05 15:36:27 +0200

    test(components-js): adds console logs | hj | #2431

--- packages/components-js/tests/vrt/playwright/specs/overview.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/overview.vrt.ts
@@ -1,9 +1,16 @@
 import { test } from '@playwright/test';
 import { executeVisualRegressionTest, openPopovers } from '../helpers/playwright-helper';
 
-test.describe('should have no visual regression', async () => {
+test.describe.only('should have no visual regression', async () => {
   await executeVisualRegressionTest('overview', {
     scenario: async (page) => {
+      // Enable console logging
+      context.on('console', (msg) => {
+        const type = msg.type();
+        const location = msg.location();
+        console.log(`[${type}] ${location.url}:${location.lineNumber}:${location.columnNumber} ${msg.text()}`);
+      });
+
       await openPopovers(page);
       await page.mouse.click(0, 0); // Click top left corner of the page to remove focus on banner
     },
