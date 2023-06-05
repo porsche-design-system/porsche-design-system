db6e005d83ccf2a48ceed6193c2052fbddb4a5ab
Henri Joss <henri.joss@mhp.com>
2023-06-05 10:54:38 +0200

    test(components-js): log components ready | hj | #2431

--- packages/components-js/tests/vrt/playwright/specs/flyout.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/flyout.vrt.ts
@@ -6,8 +6,10 @@ test.describe('should have no visual regression for flyout', async () => {
     scenario: async (page) => {
       await page.mouse.click(0, 0); // click top left corner of the page to remove focus on flyout
       // Scroll down flyouts
-      await page.$$eval('.scroll-here', (scrollElements) => {
-        scrollElements.forEach((el) => el.scrollIntoView());
+      console.log(await page.evaluate(() => (window as any).componentsReady()));
+      await page.$$eval('.scroll-here', async (scrollElements) => {
+        scrollElements.forEach((el) => el.scrollIntoView({ behavior: 'instant' as any }));
+        await new Promise((resolve) => setTimeout(resolve, 500));
       });
     },
   });
