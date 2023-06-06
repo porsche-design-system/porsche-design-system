d0e978c6b463c47b37785ad9bacdbf5c8f257d9c
Henri Joss <henri.joss@mhp.com>
2023-06-06 09:29:25 +0200

    test(components-js): reverts changes | hj | #2431

--- packages/components-js/tests/vrt/playwright/specs/overview.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/overview.vrt.ts
@@ -4,13 +4,6 @@ import { executeVisualRegressionTest, openPopovers } from '../helpers/playwright
 test.describe.only('should have no visual regression', async () => {
   await executeVisualRegressionTest('overview', {
     scenario: async (page) => {
-      // Enable console logging
-      page.on('console', (msg) => {
-        const type = msg.type();
-        const location = msg.location();
-        console.log(`[${type}] ${location.url}:${location.lineNumber}:${location.columnNumber} ${msg.text()}`);
-      });
-
       await openPopovers(page);
       await page.mouse.click(0, 0); // Click top left corner of the page to remove focus on banner
     },
