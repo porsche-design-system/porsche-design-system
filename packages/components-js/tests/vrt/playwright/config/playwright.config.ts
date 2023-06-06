6debc9506b62b422a046daf0d39c86c2952403d5
Henri Joss <henri.joss@mhp.com>
2023-06-06 11:57:04 +0200

    test(components-js): typo | hj | #2431

--- packages/components-js/tests/vrt/playwright/config/playwright.config.ts
+++ packages/components-js/tests/vrt/playwright/config/playwright.config.ts
@@ -33,7 +33,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 0, // Retrys are causing the test to be stuck in the pipeline
+  retries: 0, // Retries are causing the test to be stuck in the pipeline
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
