0cc25aa18cb402bbb0e0cf470bd7578a9f70fcad
Henri Joss <henri.joss@mhp.com>
2023-06-07 12:06:34 +0200

    refactor(components): changes test description | hj | #2431

--- packages/components-js/tests/vrt/puppeteer/specs/flyout.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/flyout.vrt.ts
@@ -13,7 +13,7 @@ import {
   setContentWithDesignSystem,
 } from '../helpers';
 
-it.each(furtherExtendedViewports)('should have no visual regression for flyout for viewport %s', async (viewport) => {
+it.each(furtherExtendedViewports)('should have no visual regression for viewport %s', async (viewport) => {
   expect(
     await vrtTest(getVisualRegressionTester(viewport), 'flyout', '/#flyout', {
       scenario: async (page) => {
