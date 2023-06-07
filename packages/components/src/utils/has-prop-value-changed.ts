e77e74bdeac678608ca0cfebc75d30ad5bfbd20c
TatjanaGartner <tatjana.gartner@mhp.com>
2023-06-07 08:22:56 +0200

    refactor(components): improve hasPropValueChanged utility | tg | #2569

--- packages/components/src/utils/has-prop-value-changed.ts
+++ packages/components/src/utils/has-prop-value-changed.ts
@@ -2,31 +2,28 @@ import { isObject } from './jss';
 
 export const hasPropValueChanged = (newVal: unknown, oldVal: unknown): boolean => {
   if (typeof newVal !== 'object' || typeof oldVal !== 'object') {
-    // string, boolean, number
+    // primitive types
     return newVal === oldVal;
-  } else if (Array.isArray(newVal) || Array.isArray(oldVal)) {
-    return (
-      Array.isArray(newVal) &&
-      Array.isArray(oldVal) &&
-      newVal.length === oldVal.length &&
-      newVal.every((item) => oldVal.includes(item)) &&
-      oldVal.every((item) => newVal.includes(item))
-    );
-  }
+  } else if (Array.isArray(newVal) && Array.isArray(oldVal)) {
+    // type array
+    return newVal.length === oldVal.length && newVal.every((item) => oldVal.includes(item));
+  } else {
+    // type object
+    const keys1 = Object.keys(newVal);
+    const keys2 = Object.keys(oldVal);
 
-  const keys1 = Object.keys(newVal);
-  const keys2 = Object.keys(oldVal);
-
-  if (keys1.length === keys2.length) {
-    for (const key of keys1) {
-      const val1 = newVal[key];
-      const val2 = oldVal[key];
-      const areObjects = isObject(val1 as Record<string, any>) && isObject(val2 as Record<string, any>);
-      if ((areObjects && !hasPropValueChanged(val1, val2)) || (!areObjects && val1 !== val2)) {
-        return false;
+    if (keys1.length === keys2.length) {
+      for (const key of keys1) {
+        const val1 = newVal[key];
+        const val2 = oldVal[key];
+        const areObjects = isObject(val1 as Record<string, any>) && isObject(val2 as Record<string, any>);
+        if ((areObjects && !hasPropValueChanged(val1, val2)) || (!areObjects && val1 !== val2)) {
+          return false;
+        }
       }
+      return true;
+    } else {
+      return false;
     }
-    return true;
   }
-  return false;
 };
