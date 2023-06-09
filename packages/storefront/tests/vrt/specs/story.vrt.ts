8a247d3439ce20a00a5e8182af1937c2a435530f
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-06-09 21:25:08 +0200

    test(storefront): make dark theme story test more robust | sas | #2489

--- packages/storefront/tests/vrt/specs/story.vrt.ts
+++ packages/storefront/tests/vrt/specs/story.vrt.ts
@@ -15,15 +15,13 @@ it.each(furtherExtendedViewports)('should have no visual regression for viewport
   ).toBeFalsy();
 });
 
-// TODO: we shouldn't rely on retries since computed result has to be deterministic
-jest.retryTimes(3);
-
 it('should have no visual regression with dark theme', async () => {
   expect(
     await vrtTest(getVisualRegressionStatesTester(), 'story-dark', '/components/pagination/examples', {
       elementSelector: mainViewSelector,
       scenario: async (page) => {
         await page.click('.playground > p-tabs-bar > button:nth-of-type(2)');
+        await page.mouse.move(0, 0); // avoid potential frosted glass hover issues on tabs-bar
         await new Promise((resolve) => setTimeout(resolve, 500)); // wait for animation/transition of tabs bar
       },
     })
