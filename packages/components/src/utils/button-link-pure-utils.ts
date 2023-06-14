d29bcce115c5a5d75b9307a78637b81da4ce4b42
Bernd Hacker <hacker.bernd@gmail.com>
2023-06-14 13:16:29 +0200

    feat(components): align formatting of warning message | bh | #2385

--- packages/components/src/utils/button-link-pure-utils.ts
+++ packages/components/src/utils/button-link-pure-utils.ts
@@ -11,8 +11,6 @@ export const warnIfParentIsPTextAndIconIsNone = (
   iconSource: string
 ): void => {
   if (!hasVisibleIcon(iconName, iconSource) && isParentOfKind(host, 'p-text')) {
-    consoleWarn(
-      `${getTagNameWithoutPrefix(host)} should not be used inside 'p-text'. Please use a <button> or <a> tag.`
-    );
+    consoleWarn(`${getTagNameWithoutPrefix(host)} should not be used inside p-text. Please use a <button> or <a> tag.`);
   }
 };
