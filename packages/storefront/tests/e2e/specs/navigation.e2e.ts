216d9d1387bffdc0ececfdd7dcd45c697c93385e
TatjanaGartner <tatjana.gartner@mhp.com>
2023-06-19 18:20:20 +0200

    test(storefront): improve navigation.e2e | tg / bh

--- packages/storefront/tests/e2e/specs/navigation.e2e.ts
+++ packages/storefront/tests/e2e/specs/navigation.e2e.ts
@@ -39,12 +39,9 @@ const injectCSSOverrides = async () => {
   }, styles);
 };
 
-// TODO: we shouldn't rely on retries since computed result has to be deterministic
-jest.retryTimes(1);
-
 // TODO: we should prepare a test array and loop by it.each instead to have better console output while test case is executed
 for (const [category, pages] of Object.entries(STOREFRONT_CONFIG)) {
-  for (const [page, tabs] of Object.entries(pages).sort(([a], [b]) => a.localeCompare(b))) {
+  for (const [page, tabs] of Object.entries(pages)) {
     ((category: string, page: string) => {
       it(`should navigate to "${category} > ${page}"`, async () => {
         const [accordionButton] = await browserPage.$x(`//aside//nav/p-accordion[@heading='${category}']`);
@@ -52,14 +49,23 @@ for (const [category, pages] of Object.entries(STOREFRONT_CONFIG)) {
         const [linkElement] = await browserPage.$x(
           `//aside//nav//p-link-pure/a[contains(., '${page}')][@href='${href}']/parent::p-link-pure`
         );
-        await browserPage.setRequestInterception(true);
 
         await accordionButton.click();
 
         expect(await isLinkActive(linkElement), 'sidebar link should not be active initially').toBe(false);
 
+        const mainTag = await browserPage.$('main');
+        await mainTag.evaluate((element: Element & { prevInnerHtml: string }) => {
+          element.prevInnerHtml = element.innerHTML;
+        });
+
         await linkElement.click();
-        await browserPage.waitForNetworkIdle();
+
+        await browserPage.waitForFunction(
+          (element: Element & { prevInnerHtml: string }) => element.innerHTML !== element.prevInnerHtml,
+          {},
+          mainTag
+        );
         await browserPage.evaluate(() => (window as any).componentsReady());
 
         expect(await isLinkActive(linkElement), 'sidebar link should be active after click').toBe(true);
@@ -72,27 +78,27 @@ for (const [category, pages] of Object.entries(STOREFRONT_CONFIG)) {
             // console.log(`${category} > ${page} > ${tab}`);
             const tabHref = `\/${paramCase(category)}\/${paramCase(page)}\/${paramCase(tab)}`;
             const [tabElement] = await browserPage.$x(`//p-tabs-bar//a[contains(., '${tab}')][@href='${tabHref}']`);
+            const isFirstTab = parseInt(index) === 0;
 
             const isTabElementActiveInitially = await isTabActive(tabElement);
-            if (parseInt(index) === 0) {
+            if (isFirstTab) {
               expect(isTabElementActiveInitially, 'should have first tab active initially').toBe(true);
             } else {
               expect(isTabElementActiveInitially, 'should not have tab active initially').toBe(false);
+              await mainTag.evaluate((element: Element & { prevInnerHtml: string }) => {
+                element.prevInnerHtml = element.innerHTML;
+              });
+              await tabElement.click();
+              await browserPage.waitForFunction(
+                (element: Element & { prevInnerHtml: string }) => element.innerHTML !== element.prevInnerHtml,
+                {},
+                mainTag
+              );
+              expect(await isTabActive(tabElement), 'should have tab active after click').toBe(true);
             }
 
-            await tabElement.click();
-            await browserPage.on('requestfinished', (request) => {
-              if (request.url().includes('chunk')) {
-                request.continue();
-              }
-            });
-            await browserPage.on('request', (request) => {
-              if (request.isInterceptResolutionHandled()) return;
-              request.continue();
-            });
             await browserPage.evaluate(() => (window as any).componentsReady());
 
-            expect(await isTabActive(tabElement), 'should have tab active after click').toBe(true);
             expect(await getMainTitle(browserPage), 'should show correct main title for tab page').toBe(page);
             expect(await hasPageObjectObject(browserPage), 'should not contain [object Object] on tab page').toBe(
               false
