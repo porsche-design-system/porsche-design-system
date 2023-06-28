9847875d324288aab84924ec25440e5032570854
Bernd Hacker <hacker.bernd@gmail.com>
2023-06-28 15:23:01 +0200

    chore: delete utilities-deprecated package and all references to it | bh | #2540

--- packages/utilities-deprecated/tests/unit/config/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
-  rootDir: '../../../',
-  testMatch: ['**/tests/unit/specs/**/*.+(ts|tsx|js)'],
-};
--- packages/utilities-deprecated/tests/vrt/config/jest.config.ts
+++
@@ -1,2 +0,0 @@
-import { config } from '@porsche-design-system/shared/testing/jest.config';
-export default { ...config, setupFilesAfterEnv: [...config.setupFilesAfterEnv!, '<rootDir>/config/jest.setup.ts'] };
