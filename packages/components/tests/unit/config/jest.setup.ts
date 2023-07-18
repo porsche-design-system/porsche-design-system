b3cac41c4e183b2d80fa40748e0696b590e2b7ba
Bernd Hacker <hacker.bernd@gmail.com>
2023-07-18 16:33:36 +0200

    test(components): set window.PORSCHE_DESIGN_SYSTEM_CDN_URL via jest.setup.ts | bh | #2655

--- packages/components/tests/unit/config/jest.setup.ts
+++ packages/components/tests/unit/config/jest.setup.ts
@@ -2,3 +2,11 @@
 import 'whatwg-fetch';
 import 'construct-style-sheets-polyfill';
 import '../mocks/match-media.mock';
+
+declare global {
+  interface Window {
+    PORSCHE_DESIGN_SYSTEM_CDN_URL: string;
+  }
+}
+
+window.PORSCHE_DESIGN_SYSTEM_CDN_URL = 'https://cdn.ui.porsche.com';
