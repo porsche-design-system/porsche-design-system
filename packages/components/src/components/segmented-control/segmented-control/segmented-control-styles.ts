778614d9214ffdb00ac0653dda5b9c991672a2ee
TatjanaGartner <tatjana.gartner@mhp.com>
2023-07-19 14:45:36 +0200

    refactor(components): improve typing for segmented control property columns | tg | #2637

--- packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.ts
@@ -1,26 +1,20 @@
 import { buildResponsiveStyles, getCss } from '../../../utils';
 import { addImportantToEachRule, hostHiddenStyles } from '../../../styles';
 import type { BreakpointCustomizable } from '../../../types';
-import { SegmentedControlColumn } from './segmented-control-utils';
+import type { SegmentedControlColumns } from './segmented-control-utils';
 
 const MIN_ITEM_WIDTH = 46;
 const MAX_ITEM_WIDTH = 220;
 
-export const getComponentCss = (
-  maxItemWidth: number,
-  columns: BreakpointCustomizable<SegmentedControlColumn>
-): string => {
-  const maxWidth =
-    (maxItemWidth > MAX_ITEM_WIDTH && MAX_ITEM_WIDTH) ||
-    (maxItemWidth < MIN_ITEM_WIDTH && MIN_ITEM_WIDTH) ||
-    maxItemWidth;
+export const getComponentCss = (maxWidth: number, columns: BreakpointCustomizable<SegmentedControlColumns>): string => {
+  maxWidth = (maxWidth > MAX_ITEM_WIDTH && MAX_ITEM_WIDTH) || (maxWidth < MIN_ITEM_WIDTH && MIN_ITEM_WIDTH) || maxWidth;
 
   return getCss({
     '@global': {
       ':host': addImportantToEachRule({
         display: 'grid',
         gridAutoRows: '1fr', // for equal height
-        ...buildResponsiveStyles(columns, (col: number | 'auto') => ({
+        ...buildResponsiveStyles(columns, (col: SegmentedControlColumns) => ({
           gridTemplateColumns: col === 'auto' ? `repeat(auto-fit, ${maxWidth}px)` : `repeat(${col}, minmax(0, 1fr))`,
         })),
         gap: '6px',
--- packages/components/src/components/segmented-control/segmented-control/segmented-control-utils.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-utils.ts
@@ -17,8 +17,8 @@ export type SegmentedControlBackgroundColor = (typeof SEGMENTED_CONTROL_BACKGROU
 
 export type SegmentedControlUpdateEvent = { value: string | number };
 
-export const SEGMENTED_CONTROL_COLUMNS = [...Array.from({ length: 25 }, (_, i) => i + 1), 'auto'];
-export type SegmentedControlColumn = (typeof SEGMENTED_CONTROL_COLUMNS)[number];
+export const SEGMENTED_CONTROL_COLUMNS = ['auto', ...Array.from({ length: 25 }, (_, i) => i + 1)];
+export type SegmentedControlColumns = (typeof SEGMENTED_CONTROL_COLUMNS)[number];
 
 // Expect Porsche Next to be available and use sans-serif (wide font for safety buffer) as fallback
 const tempFont = 'Porsche Next, sans-serif';
