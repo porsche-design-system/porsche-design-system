a2f1f8d1878f30a7a837d1441e94c1efeca7d3f6
Bernd Hacker <hacker.bernd@gmail.com>
2023-07-20 08:55:46 +0200

    test(components-js/jsdom-polyfill): adjust PDS_SKIP_FETCH unit test | bh | #2655

--- packages/components-js/projects/jsdom-polyfill/tests/unit/specs/PDS_SKIP_FETCH.spec.ts
+++ packages/components-js/projects/jsdom-polyfill/tests/unit/specs/PDS_SKIP_FETCH.spec.ts
@@ -58,7 +58,7 @@ describe('window.PDS_SKIP_FETCH = false', () => {
     (window as any).PDS_SKIP_FETCH = false;
   });
 
-  // TODO: cant' get this since this is skipped in the jsdom-polyfill entry
+  // TODO: can't get this since this is skipped in the jsdom-polyfill entry
   xit('should fetch font-face css', () => {
     const link = document.querySelector('head').querySelector('link[rel="stylesheet"]');
     console.log(link);
@@ -90,6 +90,6 @@ describe('window.PDS_SKIP_FETCH = false', () => {
 
     const img = document.querySelector('p-icon').shadowRoot.querySelector('img');
 
-    expect(img.src).toBe(`http://localhost:3001/icons/${ICONS_MANIFEST['arrow-right']}`);
+    expect(img.src).toBe(`https://cdn.ui.porsche.com/porsche-design-system/icons/${ICONS_MANIFEST['arrow-right']}`);
   });
 });
