8343d0e27516f5bfb3158a7efda3c600dfbbf63f
Henri Joss <henri.joss@mhp.com>
2023-07-31 11:26:44 +0200

    test(components-js): expect correct amount of componentsReady | #2067 | hj

--- packages/components-js/projects/jsdom-polyfill/tests/unit/specs/multi-select.spec.ts
+++ packages/components-js/projects/jsdom-polyfill/tests/unit/specs/multi-select.spec.ts
@@ -3,7 +3,7 @@ import { getMarkup } from '../helper';
 
 it('should have initialized shadow dom', async () => {
   document.body.innerHTML = getMarkup('p-multi-select');
-  expect(await componentsReady()).toBe(1);
+  expect(await componentsReady()).toBe(4); // multi-select itself + 3 multi-select-options
 
   const el = document.body.firstElementChild;
   expect(el.shadowRoot).not.toBeNull();
