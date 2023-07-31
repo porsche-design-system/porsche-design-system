6350279e02c8a0c76b47316f016072b1e6958788
Henri Joss <henri.joss@mhp.com>
2023-07-31 09:37:54 +0200

    test(components-js): adds multi-select jsdom-polyfill test | #2067 | hj

---
+++ packages/components-js/projects/jsdom-polyfill/tests/unit/specs/multi-select.spec.ts
@@ -0,0 +1,11 @@
+import { componentsReady } from '@porsche-design-system/components-js';
+import { getMarkup } from '../helper';
+
+it('should have initialized shadow dom', async () => {
+  document.body.innerHTML = getMarkup('p-multi-select');
+  expect(await componentsReady()).toBe(1);
+
+  const el = document.body.firstElementChild;
+  expect(el.shadowRoot).not.toBeNull();
+  expect(el.className).toBe('hydrated');
+});
