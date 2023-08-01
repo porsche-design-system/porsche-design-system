520fc5465fe336890cc13f6860db83df922ad197
Bernd Hacker <hacker.bernd@gmail.com>
2023-08-01 12:45:25 +0200

    fix: add optional chaining in CDN_BASE_URL_DYNAMIC of document.porscheDesignSystem?.cdn | bh | #2655

--- cdn.config.ts
+++ cdn.config.ts
@@ -19,7 +19,7 @@ export const CDN_BASE_URL_CN = 'https://cdn.ui.porsche.cn';
 
 // the document variable is defined during component-js load() call
 // the typeof document check is needed for various node scripts importing from files that contain document.porscheDesignSystem.cdn
-export const CDN_BASE_URL_DYNAMIC = `(typeof document !== 'undefined' ? document.porscheDesignSystem.cdn : '${CDN_BASE_URL}')`;
+export const CDN_BASE_URL_DYNAMIC = `(typeof document !== 'undefined' ? document.porscheDesignSystem?.cdn : '${CDN_BASE_URL}')`;
 
 const basePath = 'porsche-design-system';
 export const CDN_BASE_PATH_COMPONENTS = `${basePath}/components`;
