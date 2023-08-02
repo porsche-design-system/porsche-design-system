9babc5fde8693cd914bece33d439c9d7a4756157
TatjanaGartner <tatjana.gartner@mhp.com>
2023-08-02 16:43:01 +0200

    feat(components-nextjs): use new headerPartials and FooterPartials components within app directory and set cdn | tg | #2671

--- packages/components-react/projects/nextjs/app/layout.tsx
+++ packages/components-react/projects/nextjs/app/layout.tsx
@@ -1,16 +1,11 @@
 import { PorscheDesignSystemProvider } from '@porsche-design-system/components-react/ssr';
-import { getSharedStyles } from '../styles/getSharedStyles';
-import {
-  getBrowserSupportFallbackScript,
-  getCookiesFallbackScript,
-  getDSRPonyfill,
-  getFontLinks,
-  getInitialStyles,
-} from '@porsche-design-system/components-react/partials';
 import NextScript from 'next/script';
 import { Select } from '../components/Select';
+import { FooterPartials, HeaderPartials } from '../components';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const cdn = 'local';
+
   return (
     <html lang="en">
       <head>
@@ -18,24 +13,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="http://localhost:3001/meta-icons/favicon-32x32.5638ae59e85b68cac7febfffe555dbe7.png" />
 
-        {getInitialStyles({ format: 'jsx' })}
-        <link rel="stylesheet" href="http://localhost:3001/styles/font-face.min.css" />
-        {Array.from(getFontLinks({ weights: ['regular', 'semi-bold', 'bold'] }).matchAll(/https:\/\/[a-z0-9./-]+/g))
-          .map(([url]) => url.replace('https://cdn.ui.porsche.com/porsche-design-system', 'http://localhost:3001'))
-          .map((url) => (
-            <link key={url} rel="preload" href={url} as="font" type="font/woff2" crossOrigin="" />
-          ))}
-        {getSharedStyles()}
+        <HeaderPartials cdn={cdn} />
       </head>
       <body>
-        <PorscheDesignSystemProvider>
+        <PorscheDesignSystemProvider cdn="auto">
           <Select />
           <div id="app">{children}</div>
         </PorscheDesignSystemProvider>
         <NextScript />
-        {getDSRPonyfill({ format: 'jsx' })}
-        {getBrowserSupportFallbackScript({ format: 'jsx' })}
-        {getCookiesFallbackScript({ format: 'jsx' })}
+        <FooterPartials cdn={cdn} />
       </body>
     </html>
   );
