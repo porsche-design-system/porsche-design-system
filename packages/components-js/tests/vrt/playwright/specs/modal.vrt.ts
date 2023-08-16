ba6983173573a69c90cccc1ecfdbe2411e58c9da
Bernd Hacker <hacker.bernd@gmail.com>
2023-08-16 11:24:43 +0200

    test(components-js): update modal vrt fixtures | bh | #2493

--- packages/components-js/tests/vrt/playwright/specs/modal.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/modal.vrt.ts
@@ -5,6 +5,15 @@ test.describe('should have no visual regression', async () => {
   await executeVisualRegressionTest('modal', {
     scenario: async (page) => {
       await page.mouse.click(0, 0); // click top left corner of the page to remove focus on modal
+
+      await page.evaluate(() => {
+        // page is initially 1px high and then resized which affects scroll based behavior
+        window.addEventListener('resize', () => {
+          const modal = Array.from(document.querySelectorAll('p-modal')).pop();
+          modal.scrollBy({ top: 5 });
+          modal.scrollBy({ top: -5 });
+        });
+      });
     },
   });
 });
