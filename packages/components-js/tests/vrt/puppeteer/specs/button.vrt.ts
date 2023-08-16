c46ce1a6e1143d22fa9261ca54347536054d86e3
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-08-16 13:26:45 +0200

    test(components-js): add new vrt test setup for button as POC | sas | #2665

--- packages/components-js/tests/vrt/puppeteer/specs/button.vrt.ts
+++ packages/components-js/tests/vrt/puppeteer/specs/button.vrt.ts
@@ -13,23 +13,78 @@ import {
   vrtTest,
 } from '@porsche-design-system/shared/testing';
 import { type Page } from 'puppeteer';
+import { describe } from 'node:test';
 
-// TODO: (button states test is flaky) we shouldn't rely on retries since computed result has to be deterministic
-jest.retryTimes(3);
+// jest.retryTimes(3);
 
-it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
-  expect(await vrtTest(getVisualRegressionTester(viewport), 'button', '/#button')).toBeFalsy();
-});
+const component = 'button';
 
-// TODO: abstract into helper function, like vrtTest(…)
-const statesScenario = async (page: Page, prefersColorScheme: 'light' | 'dark'): Promise<void> => {
-  const head = `
+describe(component, () => {
+  it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
+    expect(await vrtTest(getVisualRegressionTester(viewport), component, `/${component}`)).toBeFalsy();
+  });
+
+  it('should have no visual regression for dark theme', async () => {
+    expect(
+      await vrtTest(getVisualRegressionTester(1000), `${component}.theme-dark`, `/${component}`, {
+        forceComponentTheme: 'dark',
+      })
+    ).toBeFalsy();
+  });
+
+  it('should have no visual regression for auto theme with prefers-color-scheme light', async () => {
+    expect(
+      await vrtTest(getVisualRegressionTester(1000), component, `/${component}`, {
+        forceComponentTheme: 'auto',
+        prefersColorScheme: 'light',
+      })
+    ).toBeFalsy();
+  });
+
+  it('should have no visual regression for auto theme with prefers-color-scheme dark', async () => {
+    expect(
+      await vrtTest(getVisualRegressionTester(1000), `${component}.theme-dark`, `/${component}`, {
+        forceComponentTheme: 'auto',
+        prefersColorScheme: 'dark',
+      })
+    ).toBeFalsy();
+  });
+
+  it('should have no visual regression for high contrast mode light', async () => {
+    expect(
+      await vrtTest(getVisualRegressionTester(1000), `${component}.high-contrast-light`, `/${component}`, {
+        forcedColorsEnabled: true,
+        prefersColorScheme: 'light',
+      })
+    ).toBeFalsy();
+  });
+
+  it('should have no visual regression for high contrast mode dark', async () => {
+    expect(
+      await vrtTest(getVisualRegressionTester(1000), `${component}.high-contrast-dark`, `/${component}`, {
+        forcedColorsEnabled: true,
+        prefersColorScheme: 'dark',
+      })
+    ).toBeFalsy();
+  });
+
+  it('should have no visual regression in scale mode', async () => {
+    expect(
+      await vrtTest(getVisualRegressionTester(1000), `${component}.scaling`, `/${component}`, {
+        scalePageFontSize: true,
+      })
+    ).toBeFalsy();
+  });
+
+  // TODO: abstract into helper function, like vrtTest(…)
+  /*const statesScenario = async (page: Page, prefersColorScheme: 'light' | 'dark'): Promise<void> => {
+    const head = `
         <style>
           body { display: grid; grid-template-columns: repeat(3, 33.33333%); }
           p-button:not(:last-child) { margin-right: 1rem; margin-bottom: 1rem; }
         </style>`;
 
-  const getElementsMarkup: GetThemedMarkup = (theme) => `
+    const getElementsMarkup: GetThemedMarkup = (theme) => `
         <p-button theme="${theme}" variant="primary">Primary</p-button>
         <p-button theme="${theme}" variant="secondary">Secondary</p-button>
         <p-button theme="${theme}" variant="tertiary">Tertiary</p-button>
@@ -45,31 +100,32 @@ const statesScenario = async (page: Page, prefersColorScheme: 'light' | 'dark'):
         <p-button theme="${theme}" variant="secondary" loading>Loading Secondary</p-button>
         <p-button theme="${theme}" variant="tertiary" loading>Loading Tertiary</p-button>`;
 
-  await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup), {
-    injectIntoHead: head,
-  });
+    await setContentWithDesignSystem(page, getThemedBodyMarkup(getElementsMarkup), {
+      injectIntoHead: head,
+    });
 
-  const cdpSession = await page.target().createCDPSession();
-  await cdpSession.send('Emulation.setEmulatedMedia', {
-    features: [{ name: 'prefers-color-scheme', value: prefersColorScheme || 'light' }],
-  });
+    const cdpSession = await page.target().createCDPSession();
+    await cdpSession.send('Emulation.setEmulatedMedia', {
+      features: [{ name: 'prefers-color-scheme', value: prefersColorScheme || 'light' }],
+    });
 
-  await forceHoverState(page, '.hover p-button >>> button');
-  await forceFocusState(page, '.focus p-button'); // native outline should not be visible
-  await forceFocusState(page, '.focus p-button >>> button');
-  await forceFocusHoverState(page, '.focus-hover p-button >>> button');
-};
+    await forceHoverState(page, '.hover p-button >>> button');
+    await forceFocusState(page, '.focus p-button'); // native outline should not be visible
+    await forceFocusState(page, '.focus p-button >>> button');
+    await forceFocusHoverState(page, '.focus-hover p-button >>> button');
+  };
 
-it.each(['light', 'dark'] as const)(
-  'should have no visual regression for :hover + :focus-visible with prefers-color-scheme: %s',
-  async (prefersColorScheme) => {
-    const vrt = getVisualRegressionStatesTester();
+  it.each(['light', 'dark'] as const)(
+    'should have no visual regression for :hover + :focus-visible with prefers-color-scheme: %s',
+    async (prefersColorScheme) => {
+      const vrt = getVisualRegressionStatesTester();
 
-    expect(
-      await vrt.test(`button-states-${prefersColorScheme}`, async () => {
-        const page = vrt.getPage();
-        await statesScenario(page, prefersColorScheme);
-      })
-    ).toBeFalsy();
-  }
-);
+      expect(
+        await vrt.test(`button-states-${prefersColorScheme}`, async () => {
+          const page = vrt.getPage();
+          await statesScenario(page, prefersColorScheme);
+        })
+      ).toBeFalsy();
+    }
+  );*/
+});
--- packages/components-react/src/pages/Button.tsx
+++ packages/components-react/src/pages/Button.tsx
@@ -2,49 +2,22 @@
 import { PButton } from '@porsche-design-system/components-react';
 
 export const ButtonPage = (): JSX.Element => {
-  const style = `
-    @media only screen and (min-width: 760px) {
-      #app,
-      :host {
-        display: grid;
-        grid-template-columns: repeat(2, 50%);
-      }
-    }
-  `;
-
   return (
     <>
-      <style dangerouslySetInnerHTML={{ __html: style }} />
-
       <div className="playground light auto-layout" title="should render button primary with label only">
         <PButton variant="primary">Some label</PButton>
         <PButton variant="primary" loading={true}>Some label</PButton>
         <PButton variant="primary" disabled={true}>Some label</PButton>
-      </div>
-      <div className="playground dark auto-layout" title="should render button primary with label only on dark theme">
-        <PButton variant="primary" theme="dark">Some label</PButton>
-        <PButton variant="primary" theme="dark" loading={true}>Some label</PButton>
-        <PButton variant="primary" theme="dark" disabled={true}>Some label</PButton>
       </div>
       <div className="playground light auto-layout" title="should render button primary with label and icon">
         <PButton variant="primary" icon="arrow-right">Some label</PButton>
         <PButton variant="primary" icon="arrow-right" loading={true}>Some label</PButton>
         <PButton variant="primary" icon="arrow-right" disabled={true}>Some label</PButton>
       </div>
-      <div className="playground dark auto-layout" title="should render button primary with label and icon on dark theme">
-        <PButton variant="primary" theme="dark" icon="arrow-right">Some label</PButton>
-        <PButton variant="primary" theme="dark" icon="arrow-right" loading={true}>Some label</PButton>
-        <PButton variant="primary" theme="dark" icon="arrow-right" disabled={true}>Some label</PButton>
-      </div>
       <div className="playground light auto-layout" title="should render button primary without label">
         <PButton variant="primary" hideLabel={true} icon="arrow-right">Some label</PButton>
         <PButton variant="primary" hideLabel={true} icon="arrow-right" loading={true}>Some label</PButton>
         <PButton variant="primary" hideLabel={true} icon="arrow-right" disabled={true}>Some label</PButton>
-      </div>
-      <div className="playground dark auto-layout" title="should render button primary without label on dark theme">
-        <PButton variant="primary" hideLabel={true} icon="arrow-right" theme="dark">Some label</PButton>
-        <PButton variant="primary" hideLabel={true} icon="arrow-right" theme="dark" loading={true}>Some label</PButton>
-        <PButton variant="primary" hideLabel={true} icon="arrow-right" theme="dark" disabled={true}>Some label</PButton>
       </div>
 
       <div className="playground light auto-layout" title="should render button primary as default with label only">
@@ -52,33 +25,15 @@ export const ButtonPage = (): JSX.Element => {
         <PButton loading={true}>Some label</PButton>
         <PButton disabled={true}>Some label</PButton>
       </div>
-      <div className="playground dark auto-layout" title="should render button primary as default with label only">
-        <PButton theme="dark">Some label</PButton>
-        <PButton theme="dark" loading={true}>Some label</PButton>
-        <PButton theme="dark" disabled={true}>Some label</PButton>
-      </div>
       <div className="playground light auto-layout" title="should render button primary as default with label and icon">
         <PButton icon="arrow-right">Some label</PButton>
         <PButton icon="arrow-right" loading={true}>Some label</PButton>
         <PButton icon="arrow-right" disabled={true}>Some label</PButton>
       </div>
-      <div
-        className="playground dark auto-layout"
-        title="should render button primary as default with label and icon on dark theme"
-      >
-        <PButton theme="dark" icon="arrow-right">Some label</PButton>
-        <PButton theme="dark" icon="arrow-right" loading={true}>Some label</PButton>
-        <PButton theme="dark" icon="arrow-right" disabled={true}>Some label</PButton>
-      </div>
       <div className="playground light auto-layout" title="should render button primary as default without label">
         <PButton hideLabel={true} icon="arrow-right">Some label</PButton>
         <PButton hideLabel={true} icon="arrow-right" loading={true}>Some label</PButton>
         <PButton hideLabel={true} icon="arrow-right" disabled={true}>Some label</PButton>
-      </div>
-      <div className="playground dark auto-layout" title="should render button primary as default without label on dark theme">
-        <PButton hideLabel={true} icon="arrow-right" theme="dark">Some label</PButton>
-        <PButton hideLabel={true} icon="arrow-right" theme="dark" loading={true}>Some label</PButton>
-        <PButton hideLabel={true} icon="arrow-right" theme="dark" disabled={true}>Some label</PButton>
       </div>
 
       <div className="playground light auto-layout" title="should render button secondary with label only">
@@ -86,30 +41,15 @@ export const ButtonPage = (): JSX.Element => {
         <PButton variant="secondary" loading={true}>Some label</PButton>
         <PButton variant="secondary" disabled={true}>Some label</PButton>
       </div>
-      <div className="playground dark auto-layout" title="should render button secondary with label only on dark theme">
-        <PButton variant="secondary" theme="dark">Some label</PButton>
-        <PButton variant="secondary" theme="dark" loading={true}>Some label</PButton>
-        <PButton variant="secondary" theme="dark" disabled={true}>Some label</PButton>
-      </div>
       <div className="playground light auto-layout" title="should render button secondary with label and icon">
         <PButton variant="secondary" icon="arrow-right">Some label</PButton>
         <PButton variant="secondary" icon="arrow-right" loading={true}>Some label</PButton>
         <PButton variant="secondary" icon="arrow-right" disabled={true}>Some label</PButton>
       </div>
-      <div className="playground dark auto-layout" title="should render button secondary with label and icon on dark theme">
-        <PButton variant="secondary" theme="dark" icon="arrow-right">Some label</PButton>
-        <PButton variant="secondary" theme="dark" icon="arrow-right" loading={true}>Some label</PButton>
-        <PButton variant="secondary" theme="dark" icon="arrow-right" disabled={true}>Some label</PButton>
-      </div>
       <div className="playground light auto-layout" title="should render button secondary without label">
         <PButton variant="secondary" hideLabel={true} icon="arrow-right">Some label</PButton>
         <PButton variant="secondary" hideLabel={true} icon="arrow-right" loading={true}>Some label</PButton>
         <PButton variant="secondary" hideLabel={true} icon="arrow-right" disabled={true}>Some label</PButton>
-      </div>
-      <div className="playground dark auto-layout" title="should render button secondary without label on dark theme">
-        <PButton variant="secondary" hideLabel={true} icon="arrow-right" theme="dark">Some label</PButton>
-        <PButton variant="secondary" hideLabel={true} icon="arrow-right" theme="dark" loading={true}>Some label</PButton>
-        <PButton variant="secondary" hideLabel={true} icon="arrow-right" theme="dark" disabled={true}>Some label</PButton>
       </div>
 
       <div
@@ -121,28 +61,12 @@ export const ButtonPage = (): JSX.Element => {
         <PButton variant="tertiary" disabled={true}>Some label</PButton>
       </div>
       <div
-        className="playground dark auto-layout"
-        title="should render button secondary if tertiary prop is set (deprecated) with label on dark theme"
-      >
-        <PButton variant="tertiary" theme="dark">Some label</PButton>
-        <PButton variant="tertiary" theme="dark" loading={true}>Some label</PButton>
-        <PButton variant="tertiary" theme="dark" disabled={true}>Some label</PButton>
-      </div>
-      <div
         className="playground light auto-layout"
         title="should render button secondary if tertiary prop is set (deprecated) with label and icon"
       >
         <PButton variant="tertiary" icon="arrow-right">Some label</PButton>
         <PButton variant="tertiary" icon="arrow-right" loading={true}>Some label</PButton>
         <PButton variant="tertiary" icon="arrow-right" disabled={true}>Some label</PButton>
-      </div>
-      <div
-        className="playground dark auto-layout"
-        title="should render button secondary if tertiary prop is set (deprecated) with label and icon on dark theme"
-      >
-        <PButton variant="tertiary" theme="dark" icon="arrow-right">Some label</PButton>
-        <PButton variant="tertiary" theme="dark" icon="arrow-right" loading={true}>Some label</PButton>
-        <PButton variant="tertiary" theme="dark" icon="arrow-right" disabled={true}>Some label</PButton>
       </div>
       <div
         className="playground light auto-layout"
@@ -152,26 +76,9 @@ export const ButtonPage = (): JSX.Element => {
         <PButton variant="tertiary" hideLabel={true} icon="arrow-right" loading={true}>Some label</PButton>
         <PButton variant="tertiary" hideLabel={true} icon="arrow-right" disabled={true}>Some label</PButton>
       </div>
-      <div
-        className="playground dark auto-layout"
-        title="should render button secondary if tertiary prop is set (deprecated) without label on dark theme"
-      >
-        <PButton variant="tertiary" hideLabel={true} icon="arrow-right" theme="dark">Some label</PButton>
-        <PButton variant="tertiary" hideLabel={true} icon="arrow-right" theme="dark" loading={true}>Some label</PButton>
-        <PButton variant="tertiary" hideLabel={true} icon="arrow-right" theme="dark" disabled={true}>Some label</PButton>
-      </div>
 
       <div className="playground light auto-layout" title="should render button primary with responsive label">
         <PButton hideLabel={{ base: true, xs: false, s: true, m: false, l: true, xl: false }} icon="arrow-right"
-          >Some label</PButton
-        >
-      </div>
-
-      <div className="playground dark auto-layout" title="should render button primary with responsive label on dark theme">
-        <PButton
-          theme="dark"
-          hideLabel={{ base: true, xs: false, s: true, m: false, l: true, xl: false }}
-          icon="arrow-right"
           >Some label</PButton
         >
       </div>
@@ -185,29 +92,10 @@ export const ButtonPage = (): JSX.Element => {
         <PButton iconSource="./assets/icon-custom-kaixin.svg" variant="tertiary">Some label</PButton>
       </div>
 
-      <div className="playground dark auto-layout" title="should render button with specific icon on dark theme">
-        <PButton theme="dark" icon="delete">Some label</PButton>
-        <PButton theme="dark" iconSource="./assets/icon-custom-kaixin.svg">Some label</PButton>
-        <PButton theme="dark" icon="delete" variant="primary">Some label</PButton>
-        <PButton theme="dark" iconSource="./assets/icon-custom-kaixin.svg" variant="primary">Some label</PButton>
-        <PButton theme="dark" icon="delete" variant="tertiary">Some label</PButton>
-        <PButton theme="dark" iconSource="./assets/icon-custom-kaixin.svg" variant="tertiary">Some label</PButton>
-      </div>
-
       <div className="playground light auto-layout" title="should render button with multiline label">
         <PButton style={{ width: '15rem' }}>Lorem ipsum dolor sit amet, consetetur sadipscing</PButton>
         <PButton style={{ width: '15rem' }} icon="arrow-right">Lorem ipsum dolor sit amet, consetetur sadipscing</PButton>
         <PButton style={{ width: '15rem' }} icon="arrow-right" loading={true}
-          >Lorem ipsum dolor sit amet, consetetur sadipscing</PButton
-        >
-      </div>
-
-      <div className="playground dark auto-layout" title="should render button with multiline label on dark theme">
-        <PButton theme="dark" style={{ width: '15rem' }}>Lorem ipsum dolor sit amet, consetetur sadipscing</PButton>
-        <PButton theme="dark" style={{ width: '15rem' }} icon="arrow-right"
-          >Lorem ipsum dolor sit amet, consetetur sadipscing</PButton
-        >
-        <PButton theme="dark" style={{ width: '15rem' }} icon="arrow-right" loading={true}
           >Lorem ipsum dolor sit amet, consetetur sadipscing</PButton
         >
       </div>
@@ -218,19 +106,6 @@ export const ButtonPage = (): JSX.Element => {
         <PButton variant="primary" hideLabel={true} icon="arrow-right" style={{ width: '100%' }}>Some label</PButton>
         <PButton variant="primary" loading={true} style={{ width: '100%' }}>Some label</PButton>
         <PButton variant="primary" disabled={true} style={{ width: '100%' }}>Some label</PButton>
-      </div>
-
-      <div
-        className="playground dark auto-layout"
-        title="should render button with centered text/icon if set to 100% width on dark theme"
-      >
-        <PButton theme="dark" variant="primary" style={{ width: '100%' }}>Some label</PButton>
-        <PButton theme="dark" variant="primary" icon="arrow-right" style={{ width: '100%' }}>Some label</PButton>
-        <PButton theme="dark" variant="primary" hideLabel={true} icon="arrow-right" style={{ width: '100%' }}
-          >Some label</PButton
-        >
-        <PButton theme="dark" variant="primary" loading={true} style={{ width: '100%' }}>Some label</PButton>
-        <PButton theme="dark" variant="primary" disabled={true} style={{ width: '100%' }}>Some label</PButton>
       </div>
     </>
   );
