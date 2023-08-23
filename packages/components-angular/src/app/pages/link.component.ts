22db4ac2b0a2ceb3e5174d95d9fc8452462995d7
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-08-23 16:01:07 +0200

    test(components-js): update test page and vrt fixtures for link | #2665

--- packages/components-angular/src/app/pages/link.component.ts
+++ packages/components-angular/src/app/pages/link.component.ts
@@ -3,45 +3,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'page-link',
-  styles: [
-    `
-      @media only screen and (min-width: 760px) {
-        #app,
-        :host {
-          display: grid;
-          grid-template-columns: repeat(2, 50%);
-        }
-      }
-    `,
-  ],
   template: `
     <div class="playground light auto-layout" title="should render primary with label only">
       <p-link [variant]="'primary'" [href]="'https://www.porsche.com'">Some label</p-link>
       <p-link [variant]="'primary'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
-    <div class="playground dark auto-layout" title="should render primary with label only on dark theme">
-      <p-link [variant]="'primary'" [href]="'https://www.porsche.com'" [theme]="'dark'">Some label</p-link>
-      <p-link [variant]="'primary'" [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
+
     <div class="playground light auto-layout" title="should render primary with label and icon">
       <p-link [variant]="'primary'" [href]="'https://www.porsche.com'" [icon]="'arrow-right'">Some label</p-link>
       <p-link [variant]="'primary'" [icon]="'arrow-right'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
-    <div class="playground dark auto-layout" title="should render primary with label and icon on dark theme">
-      <p-link [variant]="'primary'" [href]="'https://www.porsche.com'" [icon]="'arrow-right'" [theme]="'dark'">Some label</p-link>
-      <p-link [variant]="'primary'" [icon]="'arrow-right'" [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
+
     <div class="playground light auto-layout" title="should render primary without label">
       <p-link [variant]="'primary'" [href]="'https://www.porsche.com'" [hideLabel]="true" [icon]="'arrow-right'">Some label</p-link>
       <p-link [variant]="'primary'" [hideLabel]="true" [icon]="'arrow-right'">
-        <a [href]="'https://www.porsche.com'">Some label</a>
-      </p-link>
-    </div>
-    <div class="playground dark auto-layout" title="should render primary without label on dark theme">
-      <p-link [variant]="'primary'" [href]="'https://www.porsche.com'" [hideLabel]="true" [icon]="'arrow-right'" [theme]="'dark'"
-        >Some label
-      </p-link>
-      <p-link [variant]="'primary'" [hideLabel]="true" [icon]="'arrow-right'" [theme]="'dark'">
         <a [href]="'https://www.porsche.com'">Some label</a>
       </p-link>
     </div>
@@ -50,54 +25,30 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       <p-link [href]="'https://www.porsche.com'">Some label</p-link>
       <p-link><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
-    <div class="playground dark auto-layout" title="should render primary as default with label only on dark theme">
-      <p-link [href]="'https://www.porsche.com'" [theme]="'dark'">Some label</p-link>
-      <p-link [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
+
     <div class="playground light auto-layout" title="should render primary as default with label and icon">
       <p-link [href]="'https://www.porsche.com'" [icon]="'arrow-right'">Some label</p-link>
       <p-link [icon]="'arrow-right'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
-    <div class="playground dark auto-layout" title="should render primary as default with label and icon on dark theme">
-      <p-link [href]="'https://www.porsche.com'" [icon]="'arrow-right'" [theme]="'dark'">Some label</p-link>
-      <p-link [icon]="'arrow-right'" [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
+
     <div class="playground light auto-layout" title="should render primary as default without label">
       <p-link [href]="'https://www.porsche.com'" [hideLabel]="true" [icon]="'arrow-right'">Some label</p-link>
       <p-link [hideLabel]="true" [icon]="'arrow-right'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
-    <div class="playground dark auto-layout" title="should render primary as default without label on dark theme">
-      <p-link [theme]="'dark'" [href]="'https://www.porsche.com'" [hideLabel]="true" [icon]="'arrow-right'">Some label</p-link>
-      <p-link [hideLabel]="true" [icon]="'arrow-right'" [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
 
     <div class="playground light auto-layout" title="should render secondary with label only">
       <p-link [variant]="'secondary'" [href]="'https://www.porsche.com'">Some label</p-link>
       <p-link [variant]="'secondary'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
-    <div class="playground dark auto-layout" title="should render secondary with label only on dark theme">
-      <p-link [variant]="'secondary'" [href]="'https://www.porsche.com'" [theme]="'dark'">Some label</p-link>
-      <p-link [variant]="'secondary'" [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
+
     <div class="playground light auto-layout" title="should render secondary with label and icon">
       <p-link [variant]="'secondary'" [href]="'https://www.porsche.com'" [icon]="'arrow-right'">Some label</p-link>
       <p-link [variant]="'secondary'" [icon]="'arrow-right'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
-    <div class="playground dark auto-layout" title="should render secondary with label and icon on dark theme">
-      <p-link [variant]="'secondary'" [href]="'https://www.porsche.com'" [icon]="'arrow-right'" [theme]="'dark'">Some label</p-link>
-      <p-link [variant]="'secondary'" [icon]="'arrow-right'" [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
+
     <div class="playground light auto-layout" title="should render secondary without label">
       <p-link [variant]="'secondary'" [href]="'https://www.porsche.com'" [hideLabel]="true" [icon]="'arrow-right'">Some label</p-link>
       <p-link [variant]="'secondary'" [hideLabel]="true" [icon]="'arrow-right'"
-        ><a [href]="'https://www.porsche.com'">Some label</a></p-link
-      >
-    </div>
-    <div class="playground dark auto-layout" title="should render secondary without label on dark theme">
-      <p-link [variant]="'secondary'" [href]="'https://www.porsche.com'" [hideLabel]="true" [icon]="'arrow-right'" [theme]="'dark'"
-        >Some label
-      </p-link>
-      <p-link [variant]="'secondary'" [hideLabel]="true" [icon]="'arrow-right'" [theme]="'dark'"
         ><a [href]="'https://www.porsche.com'">Some label</a></p-link
       >
     </div>
@@ -109,13 +60,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       <p-link [variant]="'tertiary'" [href]="'https://www.porsche.com'">Some label</p-link>
       <p-link [variant]="'tertiary'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
-    <div
-      class="playground dark auto-layout"
-      title="should render secondary if tertiary prop is set (deprecated) with label only on dark theme"
-    >
-      <p-link [variant]="'tertiary'" [href]="'https://www.porsche.com'" [theme]="'dark'">Some label</p-link>
-      <p-link [variant]="'tertiary'" [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
+
     <div
       class="playground light auto-layout"
       title="should render secondary if tertiary prop is set (deprecated) with label and icon"
@@ -123,13 +68,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       <p-link [variant]="'tertiary'" [href]="'https://www.porsche.com'" [icon]="'arrow-right'">Some label</p-link>
       <p-link [variant]="'tertiary'" [icon]="'arrow-right'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
     </div>
-    <div
-      class="playground dark auto-layout"
-      title="should render secondary if tertiary prop is set (deprecated) with label and icon on dark theme"
-    >
-      <p-link [variant]="'tertiary'" [href]="'https://www.porsche.com'" [icon]="'arrow-right'" [theme]="'dark'">Some label</p-link>
-      <p-link [variant]="'tertiary'" [icon]="'arrow-right'" [theme]="'dark'"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-    </div>
+
     <div
       class="playground light auto-layout"
       title="should render secondary if tertiary prop is set (deprecated) without label"
@@ -139,32 +78,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         ><a [href]="'https://www.porsche.com'">Some label</a></p-link
       >
     </div>
-    <div
-      class="playground dark auto-layout"
-      title="should render secondary if tertiary prop is set (deprecated) without label on dark theme"
-    >
-      <p-link [variant]="'tertiary'" [href]="'https://www.porsche.com'" [hideLabel]="true" [icon]="'arrow-right'" [theme]="'dark'"
-        >Some label
-      </p-link>
-      <p-link [variant]="'tertiary'" [hideLabel]="true" [icon]="'arrow-right'" [theme]="'dark'"
-        ><a [href]="'https://www.porsche.com'">Some label</a></p-link
-      >
-    </div>
 
     <div class="playground light auto-layout" title="should render primary with responsive label">
       <p-link
-        [variant]="'primary'"
-        [hideLabel]="{ base: true, xs: false, s: true, m: false, l: true, xl: false }"
-        [icon]="'arrow-right'"
-        [href]="'https://www.porsche.com'"
-      >
-        Some label
-      </p-link>
-    </div>
-
-    <div class="playground dark auto-layout" title="should render primary with responsive label on dark theme">
-      <p-link
-        [theme]="'dark'"
         [variant]="'primary'"
         [hideLabel]="{ base: true, xs: false, s: true, m: false, l: true, xl: false }"
         [icon]="'arrow-right'"
@@ -185,17 +101,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       </p-link>
     </div>
 
-    <div class="playground dark auto-layout" title="should render link with specific icon on dark theme">
-      <p-link [theme]="'dark'" [icon]="'phone'" [href]="'https://www.porsche.com'">Some label</p-link>
-      <p-link [theme]="'dark'" [iconSource]="'./assets/icon-custom-kaixin.svg'" [href]="'https://www.porsche.com'">Some label</p-link>
-      <p-link [theme]="'dark'" [icon]="'phone'" [href]="'https://www.porsche.com'">Some label</p-link>
-      <p-link [theme]="'dark'" [iconSource]="'./assets/icon-custom-kaixin.svg'" [href]="'https://www.porsche.com'">Some label</p-link>
-      <p-link [theme]="'dark'" [icon]="'phone'" [variant]="'secondary'" [href]="'https://www.porsche.com'">Some label</p-link>
-      <p-link [theme]="'dark'" [iconSource]="'./assets/icon-custom-kaixin.svg'" [variant]="'secondary'" [href]="'https://www.porsche.com'"
-        >Some label
-      </p-link>
-    </div>
-
     <div class="playground light auto-layout" title="should render with multiline label with icon">
       <p-link style="width: 15rem" [icon]="'arrow-right'" [href]="'https://www.porsche.com'"
         >Lorem ipsum dolor sit amet, consetetur sadipscing
@@ -205,27 +110,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       </p-link>
     </div>
 
-    <div class="playground dark auto-layout" title="should render with multiline label with icon on dark theme">
-      <p-link [theme]="'dark'" style="width: 15rem" [icon]="'arrow-right'" [href]="'https://www.porsche.com'"
-        >Lorem ipsum dolor sit amet, consetetur sadipscing
-      </p-link>
-      <p-link [theme]="'dark'" style="width: 15rem" [icon]="'arrow-right'">
-        <a [href]="'https://www.porsche.com'">Lorem ipsum dolor sit amet, consetetur sadipscing</a>
-      </p-link>
-    </div>
-
     <div class="playground light auto-layout" title="should render with multiline label without icon">
       <p-link style="width: 15rem" [href]="'https://www.porsche.com'">Lorem ipsum dolor sit amet, consetetur sadipscing</p-link>
       <p-link style="width: 15rem">
-        <a [href]="'https://www.porsche.com'">Lorem ipsum dolor sit amet, consetetur sadipscing</a>
-      </p-link>
-    </div>
-
-    <div class="playground dark auto-layout" title="should render with multiline label without icon on dark theme">
-      <p-link [theme]="'dark'" style="width: 15rem" [href]="'https://www.porsche.com'"
-        >Lorem ipsum dolor sit amet, consetetur sadipscing</p-link
-      >
-      <p-link [theme]="'dark'" style="width: 15rem">
         <a [href]="'https://www.porsche.com'">Lorem ipsum dolor sit amet, consetetur sadipscing</a>
       </p-link>
     </div>
@@ -241,32 +128,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
         >Some label
       </p-link>
       <p-link [variant]="'primary'" [hideLabel]="true" [icon]="'arrow-right'" style="width: 100%"
-        ><a [href]="'https://www.porsche.com'">Some label</a></p-link
-      >
-    </div>
-
-    <div
-      class="playground dark auto-layout"
-      title="should render with centered text/icon if set to 100% width on dark theme"
-    >
-      <p-link [theme]="'dark'" [variant]="'primary'" [href]="'https://www.porsche.com'" style="width: 100%">Some label</p-link>
-      <p-link [theme]="'dark'" [variant]="'primary'" style="width: 100%"><a [href]="'https://www.porsche.com'">Some label</a></p-link>
-      <p-link [theme]="'dark'" [variant]="'primary'" [icon]="'arrow-right'" [href]="'https://www.porsche.com'" style="width: 100%"
-        >Some label</p-link
-      >
-      <p-link [theme]="'dark'" [variant]="'primary'" [icon]="'arrow-right'" style="width: 100%"
-        ><a [href]="'https://www.porsche.com'">Some label</a></p-link
-      >
-      <p-link
-        [theme]="'dark'"
-        [variant]="'primary'"
-        [hideLabel]="true"
-        [icon]="'arrow-right'"
-        [href]="'https://www.porsche.com'"
-        style="width: 100%"
-        >Some label
-      </p-link>
-      <p-link [theme]="'dark'" [variant]="'primary'" [hideLabel]="true" [icon]="'arrow-right'" style="width: 100%"
         ><a [href]="'https://www.porsche.com'">Some label</a></p-link
       >
     </div>
