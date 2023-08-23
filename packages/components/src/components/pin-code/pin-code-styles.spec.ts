05a86726b0f1983bdbb09804debf11dcca0f2746
TatjanaGartner <tatjana.gartner@mhp.com>
2023-08-23 16:19:16 +0200

    test(components): add loading prop to styles unit test | tg | #773

--- packages/components/src/components/pin-code/pin-code-styles.spec.ts
+++ packages/components/src/components/pin-code/pin-code-styles.spec.ts
@@ -2,9 +2,11 @@ import { getComponentCss } from './pin-code-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, 'none', false, 'light'],
-    [true, 'error', true, 'dark'],
-    [{ base: true, xs: false, s: true, m: false, l: true, xl: false }, 'success', false, 'light'],
+    [false, 'none', false, false, 'light'],
+    [false, 'none', false, true, 'light'],
+    [false, 'none', true, false, 'light'],
+    [true, 'error', false, false, 'dark'],
+    [{ base: true, xs: false, s: true, m: false, l: true, xl: false }, 'success', false, false, 'light'],
   ])('should return correct css for hideLabel: %o, state: %s, isDisabled: %s, theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
