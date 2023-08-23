3341658a9f08d5558bb4b9d117f03c1e23168ef2
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-08-23 14:55:03 +0200

    test(components-js): update test page and vrt fixtures for fieldset-wrapper | #2665

--- packages/components-react/projects/nextjs/pages/fieldset-wrapper.tsx
+++ packages/components-react/projects/nextjs/pages/fieldset-wrapper.tsx
@@ -4,13 +4,6 @@ import { PCheckboxWrapper, PFieldsetWrapper, PRadioButtonWrapper, PSelectWrapper
 
 const FieldsetWrapperPage: NextPage = (): JSX.Element => {
   const style = `
-    @media only screen and (min-width: 760px) {
-      #app,
-      :host {
-        display: grid;
-        grid-template-columns: repeat(2, 50%);
-      }
-    }
     p-fieldset-wrapper > *:not(:last-child) {
       margin-bottom: 2px;
     }
@@ -20,15 +13,11 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
     <>
       <style dangerouslySetInnerHTML={{ __html: style }} />
 
-      <div className="playground light" title="should render with label on light background">
+      <div className="playground light" title="should render with label">
         <PFieldsetWrapper label="Some label" />
       </div>
 
-      <div className="playground dark" title="should render with label on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" />
-      </div>
-
-      <div className="playground light" title="should render with label and text-field-wrapper with defined spacing on light background">
+      <div className="playground light" title="should render with label and text-field-wrapper with defined spacing">
         <PFieldsetWrapper label="Some label">
           <PTextFieldWrapper label="Some label">
             <input type="text" name="some-name" />
@@ -36,15 +25,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with label and text-field-wrapper with defined spacing on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark">
-          <PTextFieldWrapper label="Some label" theme="dark">
-            <input type="text" name="some-name" />
-          </PTextFieldWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with required on light background">
+      <div className="playground light" title="should render with required">
         <PFieldsetWrapper label="Some label" required={true}>
           <PCheckboxWrapper label="Some label">
             <input type="checkbox" name="some-name-1" />
@@ -55,18 +36,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with required on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" required={true}>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" />
-          </PCheckboxWrapper>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" />
-          </PCheckboxWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with slotted label on light background">
+      <div className="playground light" title="should render with slotted label">
         <PFieldsetWrapper>
           <span slot="label">Some slotted label</span>
           <PCheckboxWrapper label="Some label">
@@ -78,19 +48,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with slotted label on dark background">
-        <PFieldsetWrapper theme="dark">
-          <span slot="label">Some slotted label</span>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" />
-          </PCheckboxWrapper>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" />
-          </PCheckboxWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with label size small on light background">
+      <div className="playground light" title="should render with label size small">
         <PFieldsetWrapper label="Some label" labelSize="small">
           <PRadioButtonWrapper label="Some label">
             <input type="radio" name="some-name-1" />
@@ -101,18 +59,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with label size small on dark background">
-        <PFieldsetWrapper label="Some label" labelSize="small" theme="dark">
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with label size small and required on light background">
+      <div className="playground light" title="should render with label size small and required">
         <PFieldsetWrapper label="Some label" labelSize="small" required={true}>
           <PRadioButtonWrapper label="Some label">
             <input type="radio" name="some-name-1" />
@@ -123,18 +70,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with label size small and required on dark background">
-        <PFieldsetWrapper label="Some label" labelSize="small" theme="dark" required={true}>
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with error state and error message on light background">
+      <div className="playground light" title="should render with error state and error message">
         <PFieldsetWrapper label="Some label" state="error" message="Some error message">
           <PRadioButtonWrapper state="error" label="Some label">
             <input type="radio" name="some-name-1" />
@@ -145,18 +81,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with error state and error message on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" state="error" message="Some error message">
-          <PRadioButtonWrapper state="error" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper state="error" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with error state and slotted error message on light background">
+      <div className="playground light" title="should render with error state and slotted error message">
         <PFieldsetWrapper label="Some label" state="error">
           <PRadioButtonWrapper state="error" label="Some label">
             <input type="radio" name="some-name-1" />
@@ -168,19 +93,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with error state and slotted error message on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" state="error">
-          <PRadioButtonWrapper state="error" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper state="error" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <span slot="message">Some slotted error message</span>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with success state and success message on light background">
+      <div className="playground light" title="should render with success state and success message">
         <PFieldsetWrapper label="Some label" state="success" message="Some success message">
           <PRadioButtonWrapper state="success" label="Some label">
             <input type="radio" name="some-name-1" />
@@ -191,18 +104,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with success state and success message on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" state="success" message="Some success message">
-          <PRadioButtonWrapper state="success" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper state="success" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with success state and slotted success message on light background">
+      <div className="playground light" title="should render with success state and slotted success message">
         <PFieldsetWrapper label="Some label" state="success">
           <PRadioButtonWrapper state="success" label="Some label">
             <input type="radio" name="some-name-1" />
@@ -214,19 +116,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with success state and slotted success message on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" state="success">
-          <PRadioButtonWrapper state="success" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper state="success" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <span slot="message">Some slotted success message</span>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render fieldset in fieldset as section on light background">
+      <div className="playground light" title="should render fieldset in fieldset as section">
         <PFieldsetWrapper label="Some label">
           <PFieldsetWrapper label="Some label" labelSize="small">
             <PRadioButtonWrapper label="Some label">
@@ -239,20 +129,7 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render fieldset in fieldset as section on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark">
-          <PFieldsetWrapper label="Some label" theme="dark" labelSize="small">
-            <PRadioButtonWrapper label="Some label" theme="dark">
-              <input type="radio" name="some-name-1" />
-            </PRadioButtonWrapper>
-            <PRadioButtonWrapper label="Some label" theme="dark">
-              <input type="radio" name="some-name-1" />
-            </PRadioButtonWrapper>
-          </PFieldsetWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render required only on fieldset on light background">
+      <div className="playground light" title="should render required only on fieldset">
         <PFieldsetWrapper label="Some label" labelSize="small" required={true}>
           <PTextFieldWrapper label="Some label" state="error">
             <input type="text" name="some-name" required />
@@ -267,30 +144,6 @@ const FieldsetWrapperPage: NextPage = (): JSX.Element => {
             <input type="checkbox" name="some-name-1" required />
           </PCheckboxWrapper>
           <PSelectWrapper label="Some label">
-            <select name="some-name" required>
-              <option value="a">Option A</option>
-              <option value="b">Option B</option>
-              <option value="c">Option C</option>
-            </select>
-          </PSelectWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground dark" title="should render required only on fieldset on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" labelSize="small" required={true}>
-          <PTextFieldWrapper label="Some label" theme="dark" state="error">
-            <input type="text" name="some-name" required />
-          </PTextFieldWrapper>
-          <PTextareaWrapper label="Some label" theme="dark" hideLabel={false}>
-            <textarea name="some-name" required />
-          </PTextareaWrapper>
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" required />
-          </PRadioButtonWrapper>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" required />
-          </PCheckboxWrapper>
-          <PSelectWrapper label="Some label" theme="dark">
             <select name="some-name" required>
               <option value="a">Option A</option>
               <option value="b">Option B</option>
--- packages/components-react/src/pages/FieldsetWrapper.tsx
+++ packages/components-react/src/pages/FieldsetWrapper.tsx
@@ -3,13 +3,6 @@ import { PCheckboxWrapper, PFieldsetWrapper, PRadioButtonWrapper, PSelectWrapper
 
 export const FieldsetWrapperPage = (): JSX.Element => {
   const style = `
-    @media only screen and (min-width: 760px) {
-      #app,
-      :host {
-        display: grid;
-        grid-template-columns: repeat(2, 50%);
-      }
-    }
     p-fieldset-wrapper > *:not(:last-child) {
       margin-bottom: 2px;
     }
@@ -19,15 +12,11 @@ export const FieldsetWrapperPage = (): JSX.Element => {
     <>
       <style dangerouslySetInnerHTML={{ __html: style }} />
 
-      <div className="playground light" title="should render with label on light background">
+      <div className="playground light" title="should render with label">
         <PFieldsetWrapper label="Some label" />
       </div>
 
-      <div className="playground dark" title="should render with label on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" />
-      </div>
-
-      <div className="playground light" title="should render with label and text-field-wrapper with defined spacing on light background">
+      <div className="playground light" title="should render with label and text-field-wrapper with defined spacing">
         <PFieldsetWrapper label="Some label">
           <PTextFieldWrapper label="Some label">
             <input type="text" name="some-name" />
@@ -35,15 +24,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with label and text-field-wrapper with defined spacing on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark">
-          <PTextFieldWrapper label="Some label" theme="dark">
-            <input type="text" name="some-name" />
-          </PTextFieldWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with required on light background">
+      <div className="playground light" title="should render with required">
         <PFieldsetWrapper label="Some label" required={true}>
           <PCheckboxWrapper label="Some label">
             <input type="checkbox" name="some-name-1" />
@@ -54,18 +35,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with required on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" required={true}>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" />
-          </PCheckboxWrapper>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" />
-          </PCheckboxWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with slotted label on light background">
+      <div className="playground light" title="should render with slotted label">
         <PFieldsetWrapper>
           <span slot="label">Some slotted label</span>
           <PCheckboxWrapper label="Some label">
@@ -77,19 +47,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with slotted label on dark background">
-        <PFieldsetWrapper theme="dark">
-          <span slot="label">Some slotted label</span>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" />
-          </PCheckboxWrapper>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" />
-          </PCheckboxWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with label size small on light background">
+      <div className="playground light" title="should render with label size small">
         <PFieldsetWrapper label="Some label" labelSize="small">
           <PRadioButtonWrapper label="Some label">
             <input type="radio" name="some-name-1" />
@@ -100,18 +58,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with label size small on dark background">
-        <PFieldsetWrapper label="Some label" labelSize="small" theme="dark">
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with label size small and required on light background">
+      <div className="playground light" title="should render with label size small and required">
         <PFieldsetWrapper label="Some label" labelSize="small" required={true}>
           <PRadioButtonWrapper label="Some label">
             <input type="radio" name="some-name-1" />
@@ -122,18 +69,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with label size small and required on dark background">
-        <PFieldsetWrapper label="Some label" labelSize="small" theme="dark" required={true}>
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with error state and error message on light background">
+      <div className="playground light" title="should render with error state and error message">
         <PFieldsetWrapper label="Some label" state="error" message="Some error message">
           <PRadioButtonWrapper state="error" label="Some label">
             <input type="radio" name="some-name-1" />
@@ -144,18 +80,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with error state and error message on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" state="error" message="Some error message">
-          <PRadioButtonWrapper state="error" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper state="error" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with error state and slotted error message on light background">
+      <div className="playground light" title="should render with error state and slotted error message">
         <PFieldsetWrapper label="Some label" state="error">
           <PRadioButtonWrapper state="error" label="Some label">
             <input type="radio" name="some-name-1" />
@@ -167,19 +92,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with error state and slotted error message on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" state="error">
-          <PRadioButtonWrapper state="error" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper state="error" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <span slot="message">Some slotted error message</span>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with success state and success message on light background">
+      <div className="playground light" title="should render with success state and success message">
         <PFieldsetWrapper label="Some label" state="success" message="Some success message">
           <PRadioButtonWrapper state="success" label="Some label">
             <input type="radio" name="some-name-1" />
@@ -190,18 +103,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with success state and success message on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" state="success" message="Some success message">
-          <PRadioButtonWrapper state="success" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper state="success" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render with success state and slotted success message on light background">
+      <div className="playground light" title="should render with success state and slotted success message">
         <PFieldsetWrapper label="Some label" state="success">
           <PRadioButtonWrapper state="success" label="Some label">
             <input type="radio" name="some-name-1" />
@@ -213,19 +115,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render with success state and slotted success message on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" state="success">
-          <PRadioButtonWrapper state="success" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <PRadioButtonWrapper state="success" label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" />
-          </PRadioButtonWrapper>
-          <span slot="message">Some slotted success message</span>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render fieldset in fieldset as section on light background">
+      <div className="playground light" title="should render fieldset in fieldset as section">
         <PFieldsetWrapper label="Some label">
           <PFieldsetWrapper label="Some label" labelSize="small">
             <PRadioButtonWrapper label="Some label">
@@ -238,20 +128,7 @@ export const FieldsetWrapperPage = (): JSX.Element => {
         </PFieldsetWrapper>
       </div>
 
-      <div className="playground dark" title="should render fieldset in fieldset as section on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark">
-          <PFieldsetWrapper label="Some label" theme="dark" labelSize="small">
-            <PRadioButtonWrapper label="Some label" theme="dark">
-              <input type="radio" name="some-name-1" />
-            </PRadioButtonWrapper>
-            <PRadioButtonWrapper label="Some label" theme="dark">
-              <input type="radio" name="some-name-1" />
-            </PRadioButtonWrapper>
-          </PFieldsetWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground light" title="should render required only on fieldset on light background">
+      <div className="playground light" title="should render required only on fieldset">
         <PFieldsetWrapper label="Some label" labelSize="small" required={true}>
           <PTextFieldWrapper label="Some label" state="error">
             <input type="text" name="some-name" required />
@@ -266,30 +143,6 @@ export const FieldsetWrapperPage = (): JSX.Element => {
             <input type="checkbox" name="some-name-1" required />
           </PCheckboxWrapper>
           <PSelectWrapper label="Some label">
-            <select name="some-name" required>
-              <option value="a">Option A</option>
-              <option value="b">Option B</option>
-              <option value="c">Option C</option>
-            </select>
-          </PSelectWrapper>
-        </PFieldsetWrapper>
-      </div>
-
-      <div className="playground dark" title="should render required only on fieldset on dark background">
-        <PFieldsetWrapper label="Some label" theme="dark" labelSize="small" required={true}>
-          <PTextFieldWrapper label="Some label" theme="dark" state="error">
-            <input type="text" name="some-name" required />
-          </PTextFieldWrapper>
-          <PTextareaWrapper label="Some label" theme="dark" hideLabel={false}>
-            <textarea name="some-name" required />
-          </PTextareaWrapper>
-          <PRadioButtonWrapper label="Some label" theme="dark">
-            <input type="radio" name="some-name-1" required />
-          </PRadioButtonWrapper>
-          <PCheckboxWrapper label="Some label" theme="dark">
-            <input type="checkbox" name="some-name-1" required />
-          </PCheckboxWrapper>
-          <PSelectWrapper label="Some label" theme="dark">
             <select name="some-name" required>
               <option value="a">Option A</option>
               <option value="b">Option B</option>
