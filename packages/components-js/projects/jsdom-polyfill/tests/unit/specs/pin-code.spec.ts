8f293ca4b21e27d369c2f490a4183d8820ef9886
TatjanaGartner <tatjana.gartner@mhp.com>
2023-08-23 11:03:33 +0200

    test(components-js): add jsdom-polyfill test for custom event | tg | #773

--- packages/components-js/projects/jsdom-polyfill/tests/unit/specs/pin-code.spec.ts
+++ packages/components-js/projects/jsdom-polyfill/tests/unit/specs/pin-code.spec.ts
@@ -1,5 +1,7 @@
 import { componentsReady } from '@porsche-design-system/components-js';
 import { getMarkup } from '../helper';
+import userEvent from '@testing-library/user-event';
+import { getByLabelTextShadowed } from '../../../src/testing';
 
 it('should have initialized shadow dom', async () => {
   document.body.innerHTML = getMarkup('p-pin-code');
@@ -8,4 +10,22 @@ it('should have initialized shadow dom', async () => {
   const el = document.body.firstElementChild;
   expect(el.shadowRoot).not.toBeNull();
   expect(el.className).toBe('hydrated');
+});
+
+it('should have working events', async () => {
+  document.body.innerHTML = getMarkup('p-pin-code') + `<div id="debug">Event Counter: <span>0</span></div>`;
+  await componentsReady();
+
+  const el = document.body.firstElementChild;
+  el.addEventListener('update', () => {
+    debugEl.querySelector('span').innerHTML = '1';
+  });
+
+  const debugEl = document.querySelector('#debug');
+  expect(debugEl.innerHTML).toBe('Event Counter: <span>0</span>');
+
+  const input = getByLabelTextShadowed('Some label. 1 of 4');
+  await userEvent.click(input);
+  await userEvent.keyboard('1');
+  expect(debugEl.innerHTML).toBe('Event Counter: <span>1</span>');
 });
