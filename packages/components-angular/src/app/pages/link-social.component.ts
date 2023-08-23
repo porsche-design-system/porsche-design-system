6c3929fbc58a5eca5a0faf9604858c8c88b2949d
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-08-23 16:10:07 +0200

    test(components-js): update test page and vrt fixtures for link-social | #2665

--- packages/components-angular/src/app/pages/link-social.component.ts
+++ packages/components-angular/src/app/pages/link-social.component.ts
@@ -3,39 +3,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'page-link-social',
-  styles: [
-    `
-      p-link-social:not(:last-child) {
-        margin-right: 0.5rem;
-      }
-    `,
-  ],
   template: `
-    <div class="playground light" title="should render with label">
+    <div class="playground light auto-layout" title="should render with label">
       <p-link-social [href]="'https://www.facebook.com'" [icon]="'logo-facebook'">Some label</p-link-social>
       <p-link-social [icon]="'logo-facebook'"><a [href]="'https://www.facebook.com'">Some label</a></p-link-social>
     </div>
-    <div class="playground dark" title="should render on dark theme">
-      <p-link-social [href]="'https://www.facebook.com'" [icon]="'logo-facebook'" [theme]="'dark'">Some label</p-link-social>
-      <p-link-social [icon]="'logo-facebook'" [theme]="'dark'"><a [href]="'https://www.facebook.com'">Some label</a></p-link-social>
-    </div>
 
-    <div class="playground light" title="should render without label">
+    <div class="playground light auto-layout" title="should render without label">
       <p-link-social [href]="'https://www.facebook.com'" [icon]="'logo-facebook'" [hideLabel]="true">Some label</p-link-social>
       <p-link-social [icon]="'logo-facebook'" [hideLabel]="true">
         <a [href]="'https://www.facebook.com'">Some label</a>
       </p-link-social>
     </div>
-    <div class="playground dark" title="should render without label on dark theme">
-      <p-link-social [href]="'https://www.facebook.com'" [icon]="'logo-facebook'" [hideLabel]="true" [theme]="'dark'">
-        Some label
-      </p-link-social>
-      <p-link-social [icon]="'logo-facebook'" [hideLabel]="true" [theme]="'dark'">
-        <a [href]="'https://www.facebook.com'">Some label</a>
-      </p-link-social>
-    </div>
 
-    <div class="playground light" title="should render with responsive label">
+    <div class="playground light auto-layout" title="should render with responsive label">
       <p-link-social
         [hideLabel]="{ base: true, xs: false, s: true, m: false, l: true, xl: false }"
         [href]="'https://www.facebook.com'"
@@ -45,19 +26,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
       </p-link-social>
     </div>
 
-    <div class="playground light" title="should render with specific icon">
+    <div class="playground light auto-layout" title="should render with specific icon">
       <p-link-social [icon]="'logo-delicious'" [href]="'https://www.delicious.com'">Some label</p-link-social>
       <p-link-social [iconSource]="'./assets/icon-custom-kaixin.svg'" [href]="'https://www.kaixin.com'">Some label</p-link-social>
     </div>
 
-    <div class="playground dark" title="should render with specific icon on dark theme">
-      <p-link-social [theme]="'dark'" [icon]="'logo-delicious'" [href]="'https://www.delicious.com'">Some label</p-link-social>
-      <p-link-social [theme]="'dark'" [iconSource]="'./assets/icon-custom-kaixin.svg'" [href]="'https://www.kaixin.com'"
-        >Some label</p-link-social
-      >
-    </div>
-
-    <div class="playground light" title="should render with multiline label">
+    <div class="playground light auto-layout" title="should render with multiline label">
       <p-link-social style="width: 15rem" [icon]="'logo-facebook'" [href]="'https://www.facebook.com'">
         Lorem ipsum dolor sit amet, consetetur sadipscing
       </p-link-social>
--- packages/components-react/projects/nextjs/pages/link-social.tsx
+++ packages/components-react/projects/nextjs/pages/link-social.tsx
@@ -3,41 +3,21 @@ import type { NextPage } from 'next';
 import { PLinkSocial } from '@porsche-design-system/components-react/ssr';
 
 const LinkSocialPage: NextPage = (): JSX.Element => {
-  const style = `
-    p-link-social:not(:last-child) {
-      margin-right: 0.5rem;
-    }
-  `;
-
   return (
     <>
-      <style dangerouslySetInnerHTML={{ __html: style }} />
-
-      <div className="playground light" title="should render with label">
+      <div className="playground light auto-layout" title="should render with label">
         <PLinkSocial href="https://www.facebook.com" icon="logo-facebook">Some label</PLinkSocial>
         <PLinkSocial icon="logo-facebook"><a href="https://www.facebook.com">Some label</a></PLinkSocial>
       </div>
-      <div className="playground dark" title="should render on dark theme">
-        <PLinkSocial href="https://www.facebook.com" icon="logo-facebook" theme="dark">Some label</PLinkSocial>
-        <PLinkSocial icon="logo-facebook" theme="dark"><a href="https://www.facebook.com">Some label</a></PLinkSocial>
-      </div>
 
-      <div className="playground light" title="should render without label">
+      <div className="playground light auto-layout" title="should render without label">
         <PLinkSocial href="https://www.facebook.com" icon="logo-facebook" hideLabel={true}>Some label</PLinkSocial>
         <PLinkSocial icon="logo-facebook" hideLabel={true}>
           <a href="https://www.facebook.com">Some label</a>
         </PLinkSocial>
       </div>
-      <div className="playground dark" title="should render without label on dark theme">
-        <PLinkSocial href="https://www.facebook.com" icon="logo-facebook" hideLabel={true} theme="dark">
-          Some label
-        </PLinkSocial>
-        <PLinkSocial icon="logo-facebook" hideLabel={true} theme="dark">
-          <a href="https://www.facebook.com">Some label</a>
-        </PLinkSocial>
-      </div>
 
-      <div className="playground light" title="should render with responsive label">
+      <div className="playground light auto-layout" title="should render with responsive label">
         <PLinkSocial
           hideLabel={{ base: true, xs: false, s: true, m: false, l: true, xl: false }}
           href="https://www.facebook.com"
@@ -47,19 +27,12 @@ const LinkSocialPage: NextPage = (): JSX.Element => {
         </PLinkSocial>
       </div>
 
-      <div className="playground light" title="should render with specific icon">
+      <div className="playground light auto-layout" title="should render with specific icon">
         <PLinkSocial icon="logo-delicious" href="https://www.delicious.com">Some label</PLinkSocial>
         <PLinkSocial iconSource="./assets/icon-custom-kaixin.svg" href="https://www.kaixin.com">Some label</PLinkSocial>
       </div>
 
-      <div className="playground dark" title="should render with specific icon on dark theme">
-        <PLinkSocial theme="dark" icon="logo-delicious" href="https://www.delicious.com">Some label</PLinkSocial>
-        <PLinkSocial theme="dark" iconSource="./assets/icon-custom-kaixin.svg" href="https://www.kaixin.com"
-          >Some label</PLinkSocial
-        >
-      </div>
-
-      <div className="playground light" title="should render with multiline label">
+      <div className="playground light auto-layout" title="should render with multiline label">
         <PLinkSocial style={{ width: '15rem' }} icon="logo-facebook" href="https://www.facebook.com">
           Lorem ipsum dolor sit amet, consetetur sadipscing
         </PLinkSocial>
