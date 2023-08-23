f6e17bc92478081c4099ddd74bbe6cbc1f3fd0ef
Henri Joss <henri.joss@mhp.com>
2023-08-23 16:30:28 +0200

    test(components-js): adds jsdom polyfill multi-select events tests | #2067 | hj

--- packages/components-js/projects/jsdom-polyfill/tests/unit/specs/multi-select.spec.ts
+++ packages/components-js/projects/jsdom-polyfill/tests/unit/specs/multi-select.spec.ts
@@ -1,5 +1,6 @@
 import { componentsReady } from '@porsche-design-system/components-js';
 import { getMarkup } from '../helper';
+import userEvent from '@testing-library/user-event';
 
 it('should have initialized shadow dom', async () => {
   document.body.innerHTML = getMarkup('p-multi-select');
@@ -8,4 +9,21 @@ it('should have initialized shadow dom', async () => {
   const el = document.body.firstElementChild;
   expect(el.shadowRoot).not.toBeNull();
   expect(el.className).toBe('hydrated');
+});
+
+it('should have working events', async () => {
+  document.body.innerHTML = getMarkup('p-multi-select') + `<div id="debug">Event Counter: <span>0</span></div>`;
+  await componentsReady();
+
+  const multiSelect = document.querySelector('p-multi-select');
+  multiSelect.addEventListener('update', () => {
+    debugEl.querySelector('span').innerHTML = '1';
+  });
+
+  const debugEl = document.querySelector('#debug');
+  expect(debugEl.innerHTML).toBe('Event Counter: <span>0</span>');
+
+  const button = document.querySelector('p-multi-select-option:last-child').shadowRoot.querySelector('div.option');
+  await userEvent.click(button);
+  expect(debugEl.innerHTML).toBe('Event Counter: <span>1</span>');
 });
