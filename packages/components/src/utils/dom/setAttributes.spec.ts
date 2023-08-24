ae9c7984c07489cc13fad951b7014c7cbb6cd81e
Henri Joss <henri.joss@mhp.com>
2023-08-24 11:52:36 +0200

    test(components): adds setAttributes test | #2067 | hj

---
+++ packages/components/src/utils/dom/setAttributes.spec.ts
@@ -0,0 +1,18 @@
+import { setAttributes } from './setAttributes';
+
+it('should set attributes', () => {
+  const element = document.createElement('select');
+  const attributes = {
+    multiple: 'true',
+    'aria-hidden': 'true',
+    tabindex: '-1',
+    slot: 'select',
+  };
+
+  setAttributes(element, attributes);
+
+  expect(element.getAttribute('multiple')).toBe(attributes.multiple);
+  expect(element.getAttribute('aria-hidden')).toBe(attributes['aria-hidden']);
+  expect(element.getAttribute('tabindex')).toBe(attributes.tabindex);
+  expect(element.getAttribute('slot')).toBe(attributes.slot);
+});
