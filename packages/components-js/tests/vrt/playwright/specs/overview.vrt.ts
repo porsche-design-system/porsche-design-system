3d1afd2122be2985bd4b0c1244fb4dfe22ece57b
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-08-24 21:04:37 +0200

    test(components-js): update vrt fixtures | sas | #2665

--- packages/components-js/tests/vrt/playwright/specs/overview.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/overview.vrt.ts
@@ -8,6 +8,7 @@ test.describe('overview', async () => {
   test(`should have no visual regression`, async ({ page }) => {
     const viewportWidth = 1920;
     await setupScenario(page, `/overview`, viewportWidth);
+    await page.mouse.click(0, 0);
     await expect(page.locator('#app')).toHaveScreenshot(`overview-${viewportWidth}.png`);
   });
 });
@@ -19,6 +20,7 @@ test.describe('overview notifications', async () => {
   test(`should have no visual regression`, async ({ page }) => {
     const viewportWidth = 1000;
     await setupScenario(page, `/overview-notifications`, viewportWidth);
+    await page.mouse.click(0, 0);
     await expect(page.locator('#app')).toHaveScreenshot(`overview-notifications-${viewportWidth}.png`);
   });
 });
