d70e3e80ada53cdd1b6632c550be6ddfcb24f0af
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-08-24 13:52:39 +0200

    test(components-js): update test page and vrt fixtures for wordmark | #2665

--- packages/components-react/projects/nextjs/pages/wordmark.tsx
+++ packages/components-react/projects/nextjs/pages/wordmark.tsx
@@ -3,42 +3,18 @@ import type { NextPage } from 'next';
 import { PWordmark } from '@porsche-design-system/components-react/ssr';
 
 const WordmarkPage: NextPage = (): JSX.Element => {
-  const style = `
-    @media only screen and (min-width: 760px) {
-      #app,
-      :host {
-        display: grid;
-        grid-template-columns: repeat(2, 50%);
-      }
-    }
-  `;
-
   return (
     <>
-      <style dangerouslySetInnerHTML={{ __html: style }} />
-
-      <div className="playground light" title="should render default on light background">
+      <div className="playground light" title="should render default">
         <PWordmark />
       </div>
 
-      <div className="playground dark" title="should render default on dark background">
-        <PWordmark theme="dark" />
-      </div>
-
-      <div className="playground light" title="should render size inherit on light background">
+      <div className="playground light" title="should render size inherit">
         <PWordmark size="inherit" style={{ height: '20px' }} />
       </div>
 
-      <div className="playground dark" title="should render size inherit on dark background">
-        <PWordmark theme="dark" size="inherit" style={{ height: '20px' }} />
-      </div>
-
-      <div className="playground light" title="should render default with custom click area on light background">
+      <div className="playground light" title="should render default with custom click area">
         <PWordmark href="#" style={{ padding: '1.5rem' }} />
-      </div>
-
-      <div className="playground dark" title="should render default with custom clickarea on dark background">
-        <PWordmark href="#" theme="dark" style={{ padding: '1.5rem' }} />
       </div>
 
       <div className="playground light" title="should not exceed parents width">
