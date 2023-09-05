48079d5655673f76a4edcb4a0a06a15f337292cc
Oliver Hook <oliver.hook@porsche.digital>
2023-09-05 11:17:26 +0200

    fix(components-react): vrt test of flyout with grid | oh | #2733

--- packages/components-react/src/styles/StylesFlyoutGridExample.tsx
+++ packages/components-react/src/styles/StylesFlyoutGridExample.tsx
@@ -1,9 +1,25 @@
 import { PFlyout } from '@porsche-design-system/components-react';
+import styled from 'styled-components';
+import { spacingFluidLarge, spacingStaticMedium } from '@porsche-design-system/components-react/styles';
 import { GridLayout } from '../components';
+
+// Wrapper
+const Wrapper = styled.div({
+  opacity: 0,
+  maxWidth: '1180px',
+  boxSizing: 'border-box',
+  paddingTop: spacingFluidLarge,
+  paddingRight: spacingStaticMedium,
+  paddingBottom: spacingFluidLarge,
+  paddingLeft: spacingStaticMedium,
+});
 
 export const StylesFlyoutGridExample = (): JSX.Element => {
   return (
     <>
+      <Wrapper>
+        <GridLayout gridVisualizer={false} />
+      </Wrapper>
       <PFlyout open={true}>
         <GridLayout gridVisualizer={false} />
       </PFlyout>
