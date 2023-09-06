8c3028ede1c18dd3792e6fc94e41018df7d7a869
Oliver Hook <oliver.hook@porsche.digital>
2023-09-06 15:40:24 +0200

    test(components-react): optimize vrt and add dummy fixtures | oh | #2733

--- packages/components-react/src/styles/StylesFlyoutGridExample.tsx
+++ packages/components-react/src/styles/StylesFlyoutGridExample.tsx
@@ -1,27 +1,41 @@
-import { PFlyout } from '@porsche-design-system/components-react';
+import { PFlyout, PHeading, PButton, PText } from '@porsche-design-system/components-react';
 import styled from 'styled-components';
-import { spacingFluidLarge, spacingStaticMedium } from '@porsche-design-system/components-react/styles';
+import { spacingFluidLarge, gridGap } from '@porsche-design-system/components-react/styles';
 import { GridLayout } from '../components';
 
 // Wrapper
 const Wrapper = styled.div({
   opacity: 0,
+  overflowX: 'hidden',
   maxWidth: '1180px',
   boxSizing: 'border-box',
-  paddingTop: spacingFluidLarge,
-  paddingRight: spacingStaticMedium,
-  paddingBottom: spacingFluidLarge,
-  paddingLeft: spacingStaticMedium,
+  paddingTop: '100px',
+  paddingRight: spacingFluidLarge,
+  paddingBottom: '150px',
+  paddingLeft: spacingFluidLarge,
+  '--pds-internal-grid-outer-column': `calc(${spacingFluidLarge} - ${gridGap})`,
+  '--pds-internal-grid-margin': `calc(${spacingFluidLarge} * -1)`,
 });
 
 export const StylesFlyoutGridExample = (): JSX.Element => {
   return (
     <>
+      {/* Wrapper and nested GridLayout is needed to calculate the whole height of rendered frid example to support full height screenshot of flyout with grid */}
       <Wrapper>
         <GridLayout gridVisualizer={false} />
       </Wrapper>
       <PFlyout open={true}>
+        <div slot="header">
+          <PHeading tag="h5" size="large">
+            Sticky Heading
+          </PHeading>
+          <PText size="small">Sticky header text</PText>
+        </div>
         <GridLayout gridVisualizer={false} />
+        <div slot="footer">
+          <PButton type="button">Footer Button</PButton>
+        </div>
+        <div slot="sub-footer">Some Sub Footer Content</div>
       </PFlyout>
     </>
   );
