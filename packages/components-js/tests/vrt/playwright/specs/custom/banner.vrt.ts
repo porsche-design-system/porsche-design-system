8a2b985b0afa5626291d150d9d4f65ceb6ebde7b
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-09-06 15:49:54 +0200

    test(components-js): add banner vrt tests for screens > 1760px | sas | #2665

---
+++ packages/components-js/tests/vrt/playwright/specs/custom/banner.vrt.ts
@@ -0,0 +1,16 @@
+import { expect, test } from '@playwright/test';
+import { setupScenario } from '../../helpers';
+
+const component = 'banner';
+
+// executed in Chrome only
+test.describe(component, async () => {
+  test.skip(({ browserName }) => browserName !== 'chromium', 'CDP is available in Chromium only.');
+
+  [1920, 2560, 3000].forEach((viewportWidth) => {
+    test(`should have no visual regression for viewport ${viewportWidth}`, async ({ page }) => {
+      await setupScenario(page, `/${component}`, viewportWidth);
+      await expect(page.locator('#app')).toHaveScreenshot(`${component}-${viewportWidth}.png`);
+    });
+  });
+});
