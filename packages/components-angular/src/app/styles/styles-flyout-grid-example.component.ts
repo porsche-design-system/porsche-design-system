d24bc45d2e5d625ff225622022875f6f7404f95f
Oliver Hook <oliver.hook@porsche.digital>
2023-09-06 16:38:59 +0200

    test(components-angular): fix test pages | oh | #2733

--- packages/components-angular/src/app/styles/styles-flyout-grid-example.component.ts
+++ packages/components-angular/src/app/styles/styles-flyout-grid-example.component.ts
@@ -1,10 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'page-styles-flyout-grid-example',
+  styles: [
+    `
+      @use '@porsche-design-system/components-angular/styles' as *;
+
+      div.wrapper {
+        opacity: 0;
+        overflow-x: hidden;
+        max-width: 1180px;
+        box-sizing: border-box;
+        padding-top: 100px;
+        padding-right: $pds-spacing-fluid-large;
+        padding-bottom: 150px;
+        padding-left: $pds-spacing-fluid-large;
+        --pds-internal-grid-outer-column: calc(#{$pds-spacing-fluid-large} - #{$pds-grid-gap});
+        --pds-internal-grid-margin: calc(#{$pds-spacing-fluid-large} * -1);
+      }
+    `,
+  ],
   template: `
-    <p-flyout [open]="true">
+    <div class="wrapper">
       <grid-layout [visualizer]="visualizer" />
+    </div>
+    <p-flyout [open]="true">
+      <div slot="header">
+        <p-heading tag="h5" size="large"> Sticky Heading </p-heading>
+        <p-text size="small">Sticky header text</p-text>
+      </div>
+      <grid-layout [visualizer]="visualizer" />
+      <div slot="footer">
+        <p-button type="button">Footer Button</p-button>
+      </div>
+      <div slot="sub-footer">Some Sub Footer Content</div>
     </p-flyout>
   `,
 })
