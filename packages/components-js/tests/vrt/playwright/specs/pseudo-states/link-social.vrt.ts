1633a1d0ddf65ec6f6313cc4f1e272227d02b5d9
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-09-06 09:32:06 +0200

    test(components-js): add popover overview vrt test | sas | #2665

---
+++ packages/components-js/tests/vrt/playwright/specs/pseudo-states/link-social.vrt.ts
@@ -0,0 +1,90 @@
+import { expect, type Page, test } from '@playwright/test';
+import {
+  baseSchemes,
+  baseThemes,
+  baseViewportWidth,
+  forceFocusHoverState,
+  forceFocusState,
+  forceHoverState,
+  getPlaygroundPseudoStatesMarkup,
+  type PrefersColorScheme,
+  setContentWithDesignSystem,
+} from '../../helpers';
+import { type Theme } from '@porsche-design-system/utilities-v2';
+
+const component = 'link-social';
+
+const scenario = async (page: Page, theme: Theme, scheme?: PrefersColorScheme): Promise<void> => {
+  const markup = () => `
+    <p-link-social href="https://www.porsche.com/">Fallback</p-link-social>
+    <p-link-social href="https://www.porsche.com/" hide-label="true">Fallback</p-link-social>
+    <p-link-social href="https://www.facebook.com/" icon="logo-facebook">Facebook</p-link-social>
+    <p-link-social href="https://www.facebook.com/" icon="logo-facebook" hide-label="true">Facebook</p-link-social>
+    <p-link-social href="https://www.google.com/" icon="logo-google">Google</p-link-social>
+    <p-link-social href="https://www.google.com/" icon="logo-google" hide-label="true">Google</p-link-social>
+    <p-link-social href="https://www.instagram.com/" icon="logo-instagram">Instagram</p-link-social>
+    <p-link-social href="https://www.instagram.com/" icon="logo-instagram" hide-label="true">Instagram</p-link-social>
+    <p-link-social href="https://www.kakaocorp.com/" icon="logo-kakaotalk">KakaoTalk</p-link-social>
+    <p-link-social href="https://www.kakaocorp.com/" icon="logo-kakaotalk" hide-label="true">KakaoTalk</p-link-social>
+    <p-link-social href="https://www.linkedin.com/" icon="logo-linkedin">LinkedIn</p-link-social>
+    <p-link-social href="https://www.linkedin.com/" icon="logo-linkedin" hide-label="true">LinkedIn</p-link-social>
+    <p-link-social href="https://www.naver.com/" icon="logo-naver">Naver</p-link-social>
+    <p-link-social href="https://www.naver.com/" icon="logo-naver" hide-label="true">Naver</p-link-social>
+    <p-link-social href="https://www.pinterest.com/" icon="logo-pinterest">Pinterest</p-link-social>
+    <p-link-social href="https://www.pinterest.com/" icon="logo-pinterest" hide-label="true">Pinterest</p-link-social>
+    <p-link-social href="https://www.reddit.com/" icon="logo-reddit">Reddit</p-link-social>
+    <p-link-social href="https://www.reddit.com/" icon="logo-reddit" hide-label="true">Reddit</p-link-social>
+    <p-link-social href="https://www.tiktok.com/" icon="logo-tiktok">TikTok</p-link-social>
+    <p-link-social href="https://www.tiktok.com/" icon="logo-tiktok" hide-label="true">TikTok</p-link-social>
+    <p-link-social href="https://www.twitter.com/" icon="logo-twitter">Twitter</p-link-social>
+    <p-link-social href="https://www.twitter.com/" icon="logo-twitter" hide-label="true">Twitter</p-link-social>
+    <p-link-social href="https://www.wechat.com/" icon="logo-wechat">Wechat</p-link-social>
+    <p-link-social href="https://www.wechat.com/" icon="logo-wechat" hide-label="true">Wechat</p-link-social>
+    <p-link-social href="https://wa.me/491525557912" icon="logo-whatsapp">Whatsapp</p-link-social>
+    <p-link-social href="https://wa.me/491525557912" icon="logo-whatsapp" hide-label="true">Whatsapp</p-link-social>
+    <p-link-social href="https://www.xing.com" icon="logo-xing">XING</p-link-social>
+    <p-link-social href="https://www.xing.com" icon="logo-xing" hide-label="true">XING</p-link-social>
+    <p-link-social href="https://www.youtube.com" icon="logo-youtube">Youtube</p-link-social>
+    <p-link-social href="https://www.youtube.com" icon="logo-youtube" hide-label="true">Youtube</p-link-social>
+    <p-link-social icon="logo-youtube"><a href="https://www.youtube.com">Slotted Youtube Anchor</a></p-link-social>
+    <p-link-social icon="logo-youtube" hide-label="true"><a href="https://www.youtube.com">Slotted Youtube Anchor</a></p-link-social>`;
+
+  await setContentWithDesignSystem(page, getPlaygroundPseudoStatesMarkup(markup, { autoLayout: 'inline' }), {
+    forceComponentTheme: theme,
+    prefersColorScheme: scheme,
+  });
+
+  await forceHoverState(page, '.hover p-link-social >>> a');
+  await forceHoverState(page, '.hover p-link-social >>> span');
+  await forceFocusState(page, '.focus p-link-social'); // native outline should not be visible
+  await forceFocusState(page, '.focus p-link-social >>> a');
+  await forceFocusState(page, '.focus p-link-social a');
+  await forceFocusHoverState(page, '.focus-hover p-link-social >>> a');
+  await forceHoverState(page, '.focus-hover p-link-social >>> span'); // with slotted <a>, the shadowed <span> is used for hover styling
+  await forceFocusHoverState(page, '.focus-hover p-link-social a');
+};
+
+// executed in Chrome only
+test.describe(component, async () => {
+  test.skip(({ browserName }) => browserName !== 'chromium');
+
+  baseThemes.forEach((theme) => {
+    test(`should have no visual regression for :hover + :focus-visible with theme ${theme}`, async ({ page }) => {
+      await scenario(page, theme);
+      await expect(page.locator('#app')).toHaveScreenshot(
+        `${component}-${baseViewportWidth}-states-theme-${theme}.png`
+      );
+    });
+  });
+
+  baseSchemes.forEach((scheme) => {
+    test(`should have no visual regression for :hover + :focus-visible with theme auto and prefers-color-scheme ${scheme}`, async ({
+      page,
+    }) => {
+      await scenario(page, 'auto', scheme);
+      await expect(page.locator('#app')).toHaveScreenshot(
+        `${component}-${baseViewportWidth}-states-theme-${scheme}.png`
+      ); // fixture is aliased since result has to be equal
+    });
+  });
+});
---
+++ packages/components-js/tests/vrt/playwright/specs/pseudo-states/radio-button-wrapper.vrt.ts
@@ -0,0 +1,110 @@
+import { expect, type Page, test } from '@playwright/test';
+import {
+  baseSchemes,
+  baseThemes,
+  baseViewportWidth,
+  forceFocusHoverState,
+  forceFocusState,
+  forceHoverState,
+  generateGUID,
+  getPlaygroundPseudoStatesMarkup,
+  type PrefersColorScheme,
+  setContentWithDesignSystem,
+} from '../../helpers';
+import { type Theme } from '@porsche-design-system/utilities-v2';
+
+const component = 'radio-button-wrapper';
+
+const scenario = async (page: Page, theme: Theme, scheme?: PrefersColorScheme): Promise<void> => {
+  const markup = () => `
+    <p-radio-button-wrapper label="When input gets hovered or focused">
+      <input type="radio" name="some-name" />
+    </p-radio-button-wrapper>
+    <div class="force-label">
+      <p-radio-button-wrapper label="When label gets hovered or focused">
+        <input type="radio" name="some-name" />
+      </p-radio-button-wrapper>
+    </div>
+    <p-radio-button-wrapper label="Some label" state="error" message="Some error validation message.">
+      <input type="radio" name="some-name" />
+    </p-radio-button-wrapper>
+    <p-radio-button-wrapper label="Some label" state="success" message="Some success validation message.">
+      <input type="radio" name="some-name" />
+    </p-radio-button-wrapper>
+    <p-radio-button-wrapper label="Some label">
+      <input type="radio" name="${generateGUID()}" checked/>
+    </p-radio-button-wrapper>
+    <p-radio-button-wrapper label="Some label" state="error" message="Some error validation message.">
+      <input type="radio" name="${generateGUID()}" checked/>
+    </p-radio-button-wrapper>
+    <p-radio-button-wrapper label="Some label" state="success" message="Some success validation message.">
+      <input type="radio" name="${generateGUID()}" checked />
+    </p-radio-button-wrapper>
+    <p-radio-button-wrapper>
+      <span slot="label">
+        Slotted label
+        <span>
+          and some slotted, deeply nested <a href="#">anchor</a>.
+        </span>
+      </span>
+      <input type="radio" name="some-name" />
+    </p-radio-button-wrapper>
+    <p-radio-button-wrapper label="Some label" state="error">
+      <input type="radio" name="some-name" />
+      <span slot="message">
+        Slotted error message
+        <span>
+          and some slotted, deeply nested <a href="#">anchor</a>.
+        </span>
+      </span>
+    </p-radio-button-wrapper>
+    <p-radio-button-wrapper label="Some label" state="success">
+      <input type="radio" name="some-name" />
+      <span slot="message">
+        Slotted success message
+        <span>
+          and some slotted, deeply nested <a href="#">anchor</a>.
+        </span>
+      </span>
+    </p-radio-button-wrapper>`;
+
+  await setContentWithDesignSystem(page, getPlaygroundPseudoStatesMarkup(markup, { autoLayout: 'block' }), {
+    forceComponentTheme: theme,
+    prefersColorScheme: scheme,
+  });
+
+  await forceHoverState(page, '.hover p-radio-button-wrapper input[type="radio"]');
+  await forceHoverState(page, '.hover .force-label > p-radio-button-wrapper >>> span');
+  await forceHoverState(page, '.hover p-radio-button-wrapper span a');
+  await forceFocusState(page, '.focus p-radio-button-wrapper input[type="radio"]');
+  await forceFocusState(page, '.focus p-radio-button-wrapper span a');
+  await forceFocusHoverState(page, '.focus-hover p-radio-button-wrapper input[type="radio"]');
+  await forceFocusState(page, '.focus-hover .force-label > p-radio-button-wrapper input[type="radio"]');
+  await forceHoverState(page, '.focus-hover .force-label > p-radio-button-wrapper >>> span');
+  await forceFocusHoverState(page, '.focus-hover p-radio-button-wrapper span a');
+};
+
+// executed in Chrome only
+test.describe(component, async () => {
+  test.skip(({ browserName }) => browserName !== 'chromium');
+
+  baseThemes.forEach((theme) => {
+    test(`should have no visual regression for :hover + :focus-visible with theme ${theme}`, async ({ page }) => {
+      await scenario(page, theme);
+      await expect(page.locator('#app')).toHaveScreenshot(
+        `${component}-${baseViewportWidth}-states-theme-${theme}.png`
+      );
+    });
+  });
+
+  baseSchemes.forEach((scheme) => {
+    test(`should have no visual regression for :hover + :focus-visible with theme auto and prefers-color-scheme ${scheme}`, async ({
+      page,
+    }) => {
+      await scenario(page, 'auto', scheme);
+      await expect(page.locator('#app')).toHaveScreenshot(
+        `${component}-${baseViewportWidth}-states-theme-${scheme}.png`
+      ); // fixture is aliased since result has to be equal
+    });
+  });
+});
