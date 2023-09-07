ea16af9751e71dae9e0aeaa58e22c139a0c420c2
TatjanaGartner <tatjana.gartner@mhp.com>
2023-09-07 13:57:34 +0200

    feat(components-react): improve framework example | tg | #773

--- packages/components-react/src/examples/PinCodeExampleControlled.tsx
+++ packages/components-react/src/examples/PinCodeExampleControlled.tsx
@@ -5,11 +5,10 @@ import { PPinCode, PText } from '@porsche-design-system/components-react';
 export const PinCodeExampleControlledPage = (): JSX.Element => {
   const length = 4;
   const [currentValue, setCurrentValue] = useState(['', '', '', '']);
-  const [isComplete, setIsComplete] = useState(currentValue.join('').length === length);
+  const isComplete = currentValue.join('').length === length;
 
   const onUpdate = useCallback((e: CustomEvent<PinCodeUpdateEvent>) => {
     setCurrentValue(e.detail.value);
-    setIsComplete(e.detail.value.join('').length === length);
   }, []);
 
   return (
