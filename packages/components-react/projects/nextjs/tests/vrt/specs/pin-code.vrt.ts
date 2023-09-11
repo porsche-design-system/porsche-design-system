55793109a32ace1a1ee0e421890160bd3a93cb88
TatjanaGartner <tatjana.gartner@mhp.com>
2023-09-11 14:20:46 +0200

    feat(components-nextjs): update pin code vrt | tg | #773

--- packages/components-react/projects/nextjs/tests/vrt/specs/pin-code.vrt.ts
+++ packages/components-react/projects/nextjs/tests/vrt/specs/pin-code.vrt.ts
@@ -1,15 +1,9 @@
-import {
-  defaultViewports,
-  getVisualRegressionTester,
-  pinCodeScenario,
-  vrtTest,
-} from '@porsche-design-system/shared/testing';
+import { defaultViewports, getVisualRegressionTester, vrtTest } from '@porsche-design-system/shared/testing';
 
 it.each(defaultViewports)('should have no visual regression for viewport %s', async (viewport) => {
   expect(
     await vrtTest(getVisualRegressionTester(viewport), 'pin-code', '/pin-code', {
       javaScriptEnabled: false,
-      scenario: (page) => pinCodeScenario(page),
     })
   ).toBeFalsy();
 });
