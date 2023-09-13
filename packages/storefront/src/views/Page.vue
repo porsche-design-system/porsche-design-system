388a29447ea75a9b641601fa6b6854461c342d04
Stephan Schröter <stephan.schroeter@porsche.digital>
2023-09-13 09:43:06 +0200

    Update packages/storefront/src/views/Page.vue
    Co-authored-by: Bernd Hacker <hacker.bernd@gmail.com>

--- packages/storefront/src/views/Page.vue
+++ packages/storefront/src/views/Page.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p-tabs-bar :theme="$store.getters.platformTheme" v-if="hasTabs" :active-tab-index="activeTabIndex" size="medium">
+    <p-tabs-bar v-if="hasTabs" :theme="$store.getters.platformTheme" :active-tab-index="activeTabIndex" size="medium">
       <router-link v-for="(tab, index) in tabs" :key="index" :to="createTabLink(tab)">{{ tab }}</router-link>
     </p-tabs-bar>
     <Markdown>
