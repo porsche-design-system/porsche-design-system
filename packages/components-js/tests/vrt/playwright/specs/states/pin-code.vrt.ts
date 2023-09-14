01a1d9b81c1bbff8150e70e4b8edeb4376c070a9
TatjanaGartner <tatjana.gartner@mhp.com>
2023-09-14 14:42:31 +0200

    fix(components-js): indentation | tg | #773

--- packages/components-js/tests/vrt/playwright/specs/states/pin-code.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/states/pin-code.vrt.ts
@@ -15,42 +15,43 @@ import { type Theme } from '@porsche-design-system/utilities-v2';
 const component = 'pin-code';
 
 const scenario = async (page: Page, theme: Theme, scheme?: PrefersColorScheme): Promise<void> => {
-  const markup = () => `<p-pin-code theme="${theme}" label="input gets hovered or focused"></p-pin-code>
-        <p-pin-code theme="${theme}" label="Disabled" disabled="true"></p-pin-code>
-        <p-pin-code theme="${theme}" label="Error state" state="error" message="Some error validation message."></p-pin-code>
-        <p-pin-code theme="${theme}" label="Success state" state="success" message="Some success validation message."></p-pin-code>
-        <p-pin-code theme="${theme}">
-          <span slot="label">
-            Slotted label
-            <span>
-              and some slotted, deeply nested <a href="#">anchor</a>.
-            </span>
-          </span>
-        </p-pin-code>
-        <p-pin-code theme="${theme}" label="Some label" state="error">
-          <span slot="description">
-            Slotted description
-            <span>
-              and some slotted, deeply nested <a href="#">anchor</a>.
-            </span>
-          </span>
-        </p-pin-code>
-        <p-pin-code theme="${theme}" label="Some label" state="error">
-          <span slot="message">
-            Slotted error message
-            <span>
-              and some slotted, deeply nested <a href="#">anchor</a>.
-            </span>
-          </span>
-        </p-pin-code>
-        <p-pin-code theme="${theme}" label="Some label" state="success">
-          <span slot="message">
-            Slotted success message
-            <span>
-              and some slotted, deeply nested <a href="#">anchor</a>.
-            </span>
-          </span>
-        </p-pin-code>`;
+  const markup = () => `
+    <p-pin-code theme="${theme}" label="input gets hovered or focused"></p-pin-code>
+    <p-pin-code theme="${theme}" label="Disabled" disabled="true"></p-pin-code>
+    <p-pin-code theme="${theme}" label="Error state" state="error" message="Some error validation message."></p-pin-code>
+    <p-pin-code theme="${theme}" label="Success state" state="success" message="Some success validation message."></p-pin-code>
+    <p-pin-code theme="${theme}">
+      <span slot="label">
+        Slotted label
+        <span>
+          and some slotted, deeply nested <a href="#">anchor</a>.
+        </span>
+      </span>
+    </p-pin-code>
+    <p-pin-code theme="${theme}" label="Some label" state="error">
+      <span slot="description">
+        Slotted description
+        <span>
+          and some slotted, deeply nested <a href="#">anchor</a>.
+        </span>
+      </span>
+    </p-pin-code>
+    <p-pin-code theme="${theme}" label="Some label" state="error">
+      <span slot="message">
+        Slotted error message
+        <span>
+          and some slotted, deeply nested <a href="#">anchor</a>.
+        </span>
+      </span>
+    </p-pin-code>
+    <p-pin-code theme="${theme}" label="Some label" state="success">
+      <span slot="message">
+        Slotted success message
+        <span>
+          and some slotted, deeply nested <a href="#">anchor</a>.
+        </span>
+      </span>
+    </p-pin-code>`;
 
   await setContentWithDesignSystem(page, getPlaygroundPseudoStatesMarkup(markup), {
     forceComponentTheme: theme,
