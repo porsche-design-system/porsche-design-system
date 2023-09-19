9f59b2cd23b45d241950c291a9f5f5c6d275d227
TatjanaGartner <tatjana.gartner@mhp.com>
2023-09-19 10:22:04 +0200

    refactor(components): isFormSubmittable util | tg | #773

--- packages/components/src/components/pin-code/pin-code-utils.ts
+++ packages/components/src/components/pin-code/pin-code-utils.ts
@@ -1,6 +1,9 @@
 import type { FormState } from '../../utils/form/form-state';
 import type { Styles } from 'jss';
 import { consoleWarn, getPrefixedTagNames, getTagNameWithoutPrefix, setAttributes } from '../../utils';
+import { Components } from '../../components';
+import PButton = Components.PButton;
+import PButtonPure = Components.PButtonPure;
 
 export const PIN_CODE_TYPES = ['number', 'password'] as const;
 export type PinCodeType = (typeof PIN_CODE_TYPES)[number];
@@ -101,10 +104,11 @@ export const isFormSubmittable = (host: HTMLElement, form: HTMLFormElement): boo
   const PrefixedTagNames = getPrefixedTagNames(host);
 
   return !!(
-    form.querySelectorAll('input:not([type=submit]):not([type=hidden])').length === 1 || // other sibling form elements e.g. select, textarea do not prevent a submit
-    form.querySelector(`${PrefixedTagNames.pButton}[type=submit]`) ||
-    form.querySelector(`${PrefixedTagNames.pButtonPure}[type=submit]`) ||
-    form.querySelector('button[type=submit]') ||
-    form.querySelector('input[type=submit]')
+    form.querySelectorAll('input:not([type=submit]):not([type=hidden])').length === 1 || // other sibling form elements e.g. select, textarea do not prevent submission
+    (form.querySelector(PrefixedTagNames.pButton) as PButton)?.type === 'submit' ||
+    (form.querySelector(PrefixedTagNames.pButtonPure) as PButtonPure)?.type === 'submit' ||
+    form.querySelector(
+      `${PrefixedTagNames.pButton}[type=submit], ${PrefixedTagNames.pButtonPure}[type=submit], button[type=submit], input[type=submit]`
+    )
   );
 };
