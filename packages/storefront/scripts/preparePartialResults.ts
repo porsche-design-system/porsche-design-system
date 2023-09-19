c221ee1fd02b6803bdf6748c3612f9b952fa7c5b
Bernd Hacker <hacker.bernd@gmail.com>
2023-09-19 09:28:38 +0200

    fix(storefront): remove getInitialStyles hash and use unsafe-inline for csp | bh | #2742

--- packages/storefront/scripts/preparePartialResults.ts
+++ packages/storefront/scripts/preparePartialResults.ts
@@ -29,20 +29,12 @@ export const initialStyles = ${JSON.stringify(initialStyles)};
   const indexHtmlFilePath = path.resolve(__dirname, '../public/index.html');
   const indexHtmlContent = fs.readFileSync(indexHtmlFilePath, 'utf8');
 
-  const styleHashes = [
-    getInitialStyles({ format: 'sha256' }),
-    'unsafe-hashes',
-    'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=', // echo -n 'display: none;' | openssl sha256 -binary | openssl base64
-    'sha256-qnVkQSG7pWu17hBhIw0kCpfEB3XGvt0mNRa6+uM6OUU=', // echo -n 'display: block;' | openssl sha256 -binary | openssl base64
-    'sha256-biLFinpqYMtWHmXfkA1BPeCY0/fNt46SAZ+BBk5YUog=', // echo -n '' | openssl sha256 -binary | openssl base64
-  ]
-    .map((hash) => `'${hash}'`)
-    .join(' ');
   const scriptHashes = [getLoaderScript({ format: 'sha256' }), getBrowserSupportFallbackScript({ format: 'sha256' })]
     .map((hash) => `'${hash}'`)
     .join(' ');
 
-  const cspMeta = `<meta http-equiv="Content-Security-Policy" content="default-src 'self' https://cdn.ui.porsche.com; style-src 'self' https://cdn.ui.porsche.com ${styleHashes}; script-src 'self' https://cdn.ui.porsche.com ${scriptHashes}; connect-src 'self' https://*.algolia.net https://*.algolianet.com" />`;
+  // NOTE: getInitialStyles hash would be great to use but we need 'unsafe-inline' for all our inline styles and this can't be combined with a whitelisted hash
+  const cspMeta = `<meta http-equiv="Content-Security-Policy" content="default-src 'self' https://cdn.ui.porsche.com; style-src 'self' 'unsafe-inline' https://cdn.ui.porsche.com; script-src 'self' https://cdn.ui.porsche.com ${scriptHashes}; connect-src 'self' https://*.algolia.net https://*.algolianet.com" />`;
   console.log(cspMeta);
 
   const placeholder = '<!-- CSP_PLACEHOLDER -->';
