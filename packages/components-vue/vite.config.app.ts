067df209e2d1245890187df42ec0fd7d409d2778
Bernd Hacker <hacker.bernd@gmail.com>
2023-09-27 11:00:35 +0200

    fix(components-vue): integrate missing partials and use local cdn | bh

--- packages/components-vue/vite.config.app.ts
+++ packages/components-vue/vite.config.app.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import * as partials from '@porsche-design-system/components-vue/partials';
 
 // Config to build static demo-app
 export default defineConfig({
@@ -12,8 +13,16 @@ export default defineConfig({
     createHtmlPlugin({
       inject: {
         data: {
-          fontLinks: require('@porsche-design-system/components-vue/partials').getFontLinks(),
-          initialStyles: require('@porsche-design-system/components-vue/partials').getInitialStyles(),
+          headPartials: [
+            partials.getInitialStyles(),
+            partials.getFontFaceStylesheet(),
+            partials.getFontLinks({ weights: ['regular', 'semi-bold', 'bold'] }),
+          ]
+            .join('\n')
+            .replace(/https:\/\/cdn\.ui\.porsche\.com\/porsche-design-system/g, 'http://localhost:3001'),
+          bodyPartials: [partials.getBrowserSupportFallbackScript(), partials.getCookiesFallbackScript()]
+            .join('\n')
+            .replace(/https:\/\/cdn\.ui\.porsche\.com\/porsche-design-system/g, 'http://localhost:3001'),
         },
       },
     }),
