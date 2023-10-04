9b06cf03a3c21b27c48c33d4a0a7c608bb32b33c
Henri Joss <henri.joss@mhp.com>
2023-10-04 11:00:17 +0200

    test(components): add modal dark theme to style tests | hj | #2491

--- packages/components/src/components/modal/modal-styles.spec.ts
+++ packages/components/src/components/modal/modal-styles.spec.ts
@@ -3,25 +3,25 @@ import type { BreakpointCustomizable } from '../../types';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, true, false, false],
-    [false, true, true, false, false],
-    [false, true, false, false, false],
-    [false, true, false, true, false],
-    [false, false, false, true, false],
-    [false, false, true, true, false],
-    [false, true, true, true, false],
-    [true, false, true, false, false],
-    [true, true, true, false, false],
-    [true, true, false, false, false],
-    [true, true, false, true, false],
-    [true, false, false, true, false],
-    [true, false, true, true, false],
-    [true, true, true, true, false],
-    [true, { base: true, xs: false, s: true, m: false, l: true, xl: false }, true, false, false],
-    [true, false, false, true, true],
-    [true, false, false, false, true],
+    [false, false, true, false, false, 'light'],
+    [false, true, true, false, false, 'dark'],
+    [false, true, false, false, false, 'light'],
+    [false, true, false, true, false, 'dark'],
+    [false, false, false, true, false, 'light'],
+    [false, false, true, true, false, 'dark'],
+    [false, true, true, true, false, 'light'],
+    [true, false, true, false, false, 'dark'],
+    [true, true, true, false, false, 'light'],
+    [true, true, false, false, false, 'dark'],
+    [true, true, false, true, false, 'light'],
+    [true, false, false, true, false, 'dark'],
+    [true, false, true, true, false, 'light'],
+    [true, true, true, true, false, 'dark'],
+    [true, { base: true, xs: false, s: true, m: false, l: true, xl: false }, true, false, false, 'light'],
+    [true, false, false, true, true, 'dark'],
+    [true, false, false, false, true, 'light'],
   ])(
-    'should return correct css for open: %s, fullscreen: %o, dismissButton: %s, hasHeader: %s and hasFooter: %s',
+    'should return correct css for open: %s, fullscreen: %o, dismissButton: %s, hasHeader: %s, hasFooter: %s and theme: %s',
     (...args) => {
       expect(getComponentCss(...args)).toMatchSnapshot();
     }
