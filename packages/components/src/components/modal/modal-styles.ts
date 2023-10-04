0b076f6dc4fd3260cee5e9cbc2e14dd40e187339
Henri Joss <henri.joss@mhp.com>
2023-10-04 10:34:46 +0200

    feat(components): add dark theme to modal | hj | #2491

--- packages/components/src/components/modal/modal-styles.ts
+++ packages/components/src/components/modal/modal-styles.ts
@@ -178,7 +178,7 @@ export const getComponentCss = (
             borderColor: fullscreenValue ? primaryColor : darkThemePrimaryColor,
             ...getInsetJssStyle(fullscreenValue ? 0 : -4),
             ...prefersColorSchemeDarkMediaQuery(theme, {
-              borderColor: fullscreenValue ? darkThemePrimaryColor : primaryColor,
+              borderColor: darkThemePrimaryColor,
             }),
           })),
         },
