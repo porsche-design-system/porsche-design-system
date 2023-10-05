f78bd59757ca3d6d556deb7de6e94c450bca1699
Bernd Hacker <hacker.bernd@gmail.com>
2023-10-05 11:16:22 +0200

    test(components-angular/react): add styles-skeleton vrts | bh | #2318

---
+++ packages/components-angular/tests/vrt/specs/styles/skeleton.vrt.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@playwright/test';
+
+const style = 'styles-skeleton';
+const viewportWidth = 1000;
+
+test.describe(style, async () => {
+  test(`should have no visual regression for viewport ${viewportWidth}`, async ({ page }) => {
+    await page.goto(`/${style}`);
+    await page.setViewportSize({ width: viewportWidth, height: 600 });
+    await expect(page.locator('#app')).toHaveScreenshot(`${style}-${viewportWidth}.png`);
+  });
+});
