345de6d59e9a94be6115379a349a3f2d8ab6b38d
Bernd Hacker <hacker.bernd@gmail.com>
2023-10-05 15:39:41 +0200

    test(utilities): extend dependency-tree unit test for esm build | bh | #2318

--- packages/utilities/projects/utilities/tests/unit/specs/dependency-tree.spec.ts
+++ packages/utilities/projects/utilities/tests/unit/specs/dependency-tree.spec.ts
@@ -1,9 +1,21 @@
 // @ts-ignore
 import madge from 'madge';
+import { module } from '@porsche-design-system/utilities-v2/package.json';
 
-it('should have no regression for dependency tree', async () => {
-  const entrypoint = require.resolve('@porsche-design-system/utilities-v2');
-  const tree = await madge(entrypoint);
+const entrypointCjs = require.resolve('@porsche-design-system/utilities-v2');
+
+it('should have no regression for cjs dependency tree', async () => {
+  const tree = await madge(entrypointCjs);
+
+  // visualize result
+  // await tree.image('result.svg')
+
+  expect(tree.obj()).toMatchSnapshot();
+});
+
+it('should have no regression for esm dependency tree', async () => {
+  const entrypointEsm = entrypointCjs.replace(/dist\/cjs\/.*/, module);
+  const tree = await madge(entrypointEsm);
 
   // visualize result
   // await tree.image('result.svg')
