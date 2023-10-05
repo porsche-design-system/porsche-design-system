5c88551ca9769e0484e2434973bd7c54a1e35b57
Bernd Hacker <hacker.bernd@gmail.com>
2023-10-05 11:27:52 +0200

    feat(storefront): add docs for skeleton styles | bh | #2318

---
+++ packages/storefront/src/pages/patterns/styles/example-skeleton.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <h3 class="heading">Skeletons Square</h3>
+      <div class="skeleton-square skeleton-square--small"></div>
+      <div class="skeleton-square skeleton-square--medium"></div>
+    </div>
+
+    <div class="wrapper">
+      <h3 class="heading">Skeletons Heading/Text</h3>
+      <div class="skeleton-text skeleton-text--small"></div>
+      <div class="skeleton-text skeleton-text--medium"></div>
+      <div class="skeleton-text skeleton-text--large"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component
+  export default class ExampleStylesSkeleton extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  // TODO: we should import from vue, but we need to prepare CI first
+  @use '@porsche-design-system/components-js/styles' as *;
+
+  // Wrapper
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    justify-content: center;
+    gap: $pds-grid-gap;
+    padding: $pds-spacing-fluid-medium;
+  }
+
+  // Typography
+  .heading {
+    @include pds-heading-medium;
+    color: $pds-theme-light-primary;
+    text-align: center;
+    width: 100%;
+    margin: 0;
+  }
+
+  // Skeletons
+  .skeleton-square {
+    height: 100px;
+    width: 100px;
+
+    &--small {
+      @include pds-skeleton('small');
+    }
+    &--medium {
+      @include pds-skeleton('medium');
+    }
+  }
+
+  .skeleton-text {
+    @include pds-skeleton('small');
+    height: $pds-font-line-height;
+    width: 100px;
+
+    &--small {
+      font-size: $pds-font-size-text-small;
+    }
+    &--medium {
+      font-size: $pds-font-size-text-medium;
+    }
+    &--large {
+      font-size: $pds-font-size-text-large;
+    }
+  }
+</style>
---
+++ packages/storefront/src/pages/styles/skeleton.md
@@ -0,0 +1,53 @@
+# Skeleton
+
+<TableOfContents></TableOfContents>
+
+## Example
+
+<Playground :frameworkMarkup="codeExample" :externalStackBlitzDependencies="['styled-components']">
+  <ExampleStylesSkeleton />
+</Playground>
+
+## Usage
+
+The skeleton styles can be used to illustrate the loading state of more complex elements, for example a list of cards.
+
+## Styles
+
+The styles are available as `JavaScript` and `SCSS` version. Look at the example above to see how the styles work.
+
+#### JS
+
+JavaScript styles can be imported by
+`import { … } from '@porsche-design-system/components-{js|angular|react|vue}/styles';`.
+
+- `getSkeletonStyle({ borderRadius: 'small'|'medium'|string })`
+
+---
+
+#### SCSS
+
+SCSS styles can be imported by `@use '@porsche-design-system/components-{js|angular|react|vue}/styles' as *;`
+
+- `@mixin pds-skeleton($border-radius: 'small' | 'medium' | any)`
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { getStylesSkeletonCodeSamples } from '@porsche-design-system/shared';
+import { adjustSelectedFramework } from '@/utils';
+import ExampleStylesSkeleton from '@/pages/patterns/styles/example-skeleton.vue';
+
+@Component({
+  components: {
+    ExampleStylesSkeleton
+  },
+})
+export default class Code extends Vue {
+  codeExample = getStylesSkeletonCodeSamples();
+
+  public mounted(): void {
+    adjustSelectedFramework(this.codeExample);
+  }
+}
+</script>
