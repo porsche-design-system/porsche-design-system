8613c484277478cec367c432424107995741ab5f
Bernd Hacker <hacker.bernd@gmail.com>
2023-10-12 16:35:05 +0200

    docs(components): add comments in setScrollLock util | bh | #2798

--- packages/components/src/utils/setScrollLock.ts
+++ packages/components/src/utils/setScrollLock.ts
@@ -1,4 +1,11 @@
 export const setScrollLock = (isOpen: boolean): void => {
+  // just setting `overflow: hidden` is not enough for iOS Safari with collapsed address bar where body is still scrollable
+  // there are libraries like `body-scroll-lock` and `body-scroll-lock-upgrade` which advertise to handle it, but they
+  // rely on event manipulation and also don't work reliable
+  // https://github.com/willmcpo/body-scroll-lock/blob/master/src/bodyScrollLock.js
+  // https://github.com/rick-liruixin/body-scroll-lock-upgrade/blob/develop/src/body-scroll-lock.ts
+  // therefore the easiest solution is the following one, which causes the address bar to uncollapse/expand again
+
   const {
     body: { style, parentElement: htmlEl },
   } = document;
