2227e3ae5253da58b63635f6f4a6d2abc70fdf6b
Henri Joss <henri.joss@mhp.com>
2023-10-16 16:15:41 +0200

    test(components-js): add popover table vrt test | hj | #2572

--- packages/components-js/tests/vrt/playwright/helpers/popover-helper.ts
+++ packages/components-js/tests/vrt/playwright/helpers/popover-helper.ts
@@ -4,12 +4,19 @@ export const openAllPopover = async (page: Page): Promise<void> => {
   await page.evaluate(() => {
     document.addEventListener('mousedown', (e) => e.stopPropagation(), true);
     document.querySelectorAll('p-popover, my-prefix-p-popover').forEach((popover) => {
+      popover.shadowRoot.querySelector('button').click();
+    });
+  });
+};
+
+export const setAllPopoverManual = async (page: Page): Promise<void> => {
+  await page.evaluate(() => {
+    document.querySelectorAll('p-popover, my-prefix-p-popover').forEach((popover) => {
       // Spacer is only available in native case since it's not conditionally rendered
       const spacer = popover.shadowRoot.querySelector('.spacer');
       if (spacer) {
-        (spacer as HTMLElement).popover = 'manual'; // Set to manual to allow multiple popovers to be open at the same time
+        (spacer as any).popover = 'manual'; // Set to manual to allow multiple popovers to be open at the same time
       }
-      popover.shadowRoot.querySelector('button').click();
     });
   });
 };
