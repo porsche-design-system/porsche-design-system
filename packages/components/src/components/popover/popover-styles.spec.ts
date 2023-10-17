48ef58e2f998e5be7fe4ab60d538052570417f31
Henri Joss <henri.joss@mhp.com>
2023-10-17 11:29:05 +0200

    test(components): update popover styles test | hj | #2572

--- packages/components/src/components/popover/popover-styles.spec.ts
+++ packages/components/src/components/popover/popover-styles.spec.ts
@@ -2,14 +2,15 @@ import { getComponentCss } from './popover-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['top', 'light'],
-    ['right', 'light'],
-    ['bottom', 'light'],
-    ['left', 'light'],
-    ['top', 'dark'],
-    ['right', 'dark'],
-    ['bottom', 'dark'],
-    ['left', 'dark'],
+    ['top', false, 'light'],
+    ['right', false, 'light'],
+    ['bottom', false, 'light'],
+    ['left', false, 'light'],
+    ['top', false, 'dark'],
+    ['right', false, 'dark'],
+    ['bottom', false, 'dark'],
+    ['left', false, 'dark'],
+    ['top', true, 'light'],
   ])('should return correct css for direction: %s and theme: %s', (...args) => {
     expect(getComponentCss(...args)).toMatchSnapshot();
   });
