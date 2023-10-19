7799402681dd3f290ac9e194d8d277a9f3cfe813
Henri Joss <henri.joss@mhp.com>
2023-10-19 08:55:56 +0200

    refactor(components): check env with hasWindow instead try/catch | hj | #2572

--- packages/components/src/utils/supportsNativePopover.ts
+++ packages/components/src/utils/supportsNativePopover.ts
@@ -1,14 +1,15 @@
+import { hasWindow } from './has-window';
+
 /**
  * Checks if the current environment supports the native Popover API.
  *
  * @returns {boolean} `true` if the native Popover API is supported, `false` otherwise.
  */
 export const supportsNativePopover = (): boolean => {
-  try {
-    return HTMLElement.prototype.hasOwnProperty('popover');
-  } catch {
+  if (!hasWindow) {
     return false;
   }
+  return HTMLElement.prototype.hasOwnProperty('popover');
 };
 
 // determine it once
