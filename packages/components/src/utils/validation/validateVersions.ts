67b312d389a79c97b6de1fc743090ad437a56927
Henri Joss <henri.joss@mhp.com>
2023-11-02 17:09:09 +0100

    feat(components): add version validation | hj | #2642

--- packages/components/src/utils/validation/validateVersions.ts
+++ packages/components/src/utils/validation/validateVersions.ts
@@ -1,29 +1,32 @@
-import { getPorscheDesignSystemPrefixesForVersions } from './partials/helper';
 import { consoleWarn } from '../log';
 
 // TODO: Reuse everywhere?
-export type Version = `${number}.${number}.${number}`;
+export type Version = `${number}.${number}.${number}${`-rc.${number}` | ''}`;
 
 /**
  * Validates the versions of the Porsche Design System and logs a warning if multiple different versions are detected.
  * If multiple versions are found, it recommends upgrading to the latest used version.
  */
 export const validateVersions = () => {
-  const prefixesForVersions = getPorscheDesignSystemPrefixesForVersions();
-  if (Object.keys(prefixesForVersions).length > 1) {
-    const sortedEntries = Object.entries(prefixesForVersions).sort((a: [Version, string[]], b: [Version, string[]]) =>
+  const { cdn, ...versions } = document.porscheDesignSystem;
+  if (Object.keys(versions).length > 1) {
+    const sanitizedVersions = Object.keys(versions).reduce((result, key) => {
+      result[key] = versions[key].prefixes;
+      return result;
+    }, {});
+    const sortedVersions = Object.entries(sanitizedVersions).sort((a: [Version, string[]], b: [Version, string[]]) =>
       sortVersions(a[0], b[0])
     );
     consoleWarn(
       `Multiple different versions are used with following prefixes:\n`,
-      Object.fromEntries(sortedEntries),
-      `\nPlease upgrade all instances to the latest used version: ${sortedEntries.at(-1)[0]}`
+      Object.fromEntries(sortedVersions),
+      `\nPlease upgrade all instances to the latest used version: ${sortedVersions.at(-1)[0]}`
     );
   }
 };
 
 /**
- * Compares two version numbers in the pattern x.x.x.
+ * Compares two version numbers with the pattern x.x.x or x.x.x-rc.x
  *
  * @param {Version} versionA - The first version number to compare.
  * @param {Version} versionB - The second version number to compare.
@@ -33,8 +36,8 @@ export const validateVersions = () => {
  *   - 0 if versionA is equal to versionB.
  */
 export const sortVersions = (versionA: Version, versionB: Version): number => {
-  const a = versionA.split('.').map(Number);
-  const b = versionB.split('.').map(Number);
+  const a = versionA.replace('-rc', '').split('.').map(Number);
+  const b = versionB.replace('-rc', '').split('.').map(Number);
 
   for (let i = 0; i < a.length; i++) {
     if (a[i] < b[i]) return -1;
