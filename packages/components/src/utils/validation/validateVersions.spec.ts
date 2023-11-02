3681ac6e45aa5f26d4a7f937a528443b20d26b21
Henri Joss <henri.joss@mhp.com>
2023-11-02 15:58:48 +0100

    feat(components): add version validation | hj | #2642

--- packages/components/src/utils/validation/validateVersions.spec.ts
+++ packages/components/src/utils/validation/validateVersions.spec.ts
@@ -1,4 +1,5 @@
-import { validateVersions } from './validateVersions';
+import { sortVersions, validateVersions, Version } from './validateVersions';
+import * as validateVersionsUtils from './validateVersions';
 import * as loggerUtils from '../log/logger';
 import * as helperUtils from './partials/helper';
 
@@ -27,6 +28,7 @@ describe('validateVersions()', () => {
     };
 
     const consoleWarnSpy = jest.spyOn(loggerUtils, 'consoleWarn');
+    const sortVersionsSpy = jest.spyOn(validateVersionsUtils, 'sortVersions');
     const getPorscheDesignSystemPrefixesForVersionsSpy = jest
       .spyOn(helperUtils, 'getPorscheDesignSystemPrefixesForVersions')
       .mockReturnValue(versions);
@@ -34,12 +36,40 @@ describe('validateVersions()', () => {
     validateVersions();
 
     expect(getPorscheDesignSystemPrefixesForVersionsSpy).toHaveBeenCalled();
+    expect(sortVersionsSpy).toHaveBeenCalled();
     expect(consoleWarnSpy).toHaveBeenCalledWith(
       `Multiple different versions are used with following prefixes:\n`,
       versions,
       '\nPlease upgrade all instances to the latest used version: 1.2.5'
     );
   });
+
+  it('should warn about multiple used versions in correct oder and suggest correct version to upgrade', () => {
+    const versions = {
+      '3.9.0': ['prefix'],
+      '1.2.5': ['prefix, another-prefix'],
+      '1.2.3': [''],
+      '1.2.4': ['prefix, another-prefix'],
+      '2.5.1': ['prefix'],
+    };
+
+    const consoleWarnSpy = jest.spyOn(loggerUtils, 'consoleWarn');
+    const sortVersionsSpy = jest.spyOn(validateVersionsUtils, 'sortVersions');
+    const getPorscheDesignSystemPrefixesForVersionsSpy = jest
+      .spyOn(helperUtils, 'getPorscheDesignSystemPrefixesForVersions')
+      .mockReturnValue(versions);
+
+    validateVersions();
+
+    expect(getPorscheDesignSystemPrefixesForVersionsSpy).toHaveBeenCalled();
+    expect(sortVersionsSpy).toHaveBeenCalled();
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      `Multiple different versions are used with following prefixes:\n`,
+      versions,
+      '\nPlease upgrade all instances to the latest used version: 3.9.0'
+    );
+  });
+
   it('should not warn about multiple used versions when only one version is used', () => {
     const versions = {
       '1.2.3': [''],
@@ -54,5 +84,34 @@ describe('validateVersions()', () => {
 
     expect(getPorscheDesignSystemPrefixesForVersionsSpy).toHaveBeenCalled();
     expect(consoleWarnSpy).not.toHaveBeenCalled();
+  });
+});
+
+describe('sortVersions()', () => {
+  it.each([
+    ['2.0.0', '1.0.0', 1],
+    ['1.1.0', '1.0.0', 1],
+    ['1.0.1', '1.0.0', 1],
+    ['2.1.2', '2.1.1', 1],
+  ])('(%s) > (%s) = %d', (versionA: Version, versionB: Version, expected: number) => {
+    expect(sortVersions(versionA, versionB)).toBe(expected);
+  });
+
+  it.each([
+    ['1.0.0', '2.0.0', -1],
+    ['1.0.0', '1.1.0', -1],
+    ['1.0.0', '1.0.1', -1],
+    ['2.1.1', '2.1.2', -1],
+  ])('(%s) < (%s) = %d', (versionA: Version, versionB: Version, expected: number) => {
+    expect(sortVersions(versionA, versionB)).toBe(expected);
+  });
+
+  it.each([
+    ['1.0.0', '1.0.0', 0],
+    ['2.3.4', '2.3.4', 0],
+    ['3.5.6', '3.5.6', 0],
+    ['2.1.1', '2.1.1', 0],
+  ])('(%s) == (%s) = %d', (versionA: Version, versionB: Version, expected: number) => {
+    expect(sortVersions(versionA, versionB)).toBe(expected);
   });
 });
--- packages/components/src/utils/validation/validateVersions.ts
+++ packages/components/src/utils/validation/validateVersions.ts
@@ -1,15 +1,45 @@
 import { getPorscheDesignSystemPrefixesForVersions } from './partials/helper';
 import { consoleWarn } from '../log';
 
+// TODO: Reuse everywhere?
+export type Version = `${number}.${number}.${number}`;
+
+/**
+ * Validates the versions of the Porsche Design System and logs a warning if multiple different versions are detected.
+ * If multiple versions are found, it recommends upgrading to the latest used version.
+ */
 export const validateVersions = () => {
   const prefixesForVersions = getPorscheDesignSystemPrefixesForVersions();
   if (Object.keys(prefixesForVersions).length > 1) {
+    const sortedEntries = Object.entries(prefixesForVersions).sort((a: [Version, string[]], b: [Version, string[]]) =>
+      sortVersions(a[0], b[0])
+    );
     consoleWarn(
       `Multiple different versions are used with following prefixes:\n`,
-      prefixesForVersions,
-      `\nPlease upgrade all instances to the latest used version: ${
-        Object.keys(prefixesForVersions)[Object.keys(prefixesForVersions).length - 1]
-      }`
+      Object.fromEntries(sortedEntries),
+      `\nPlease upgrade all instances to the latest used version: ${sortedEntries.at(-1)[0]}`
     );
   }
+};
+
+/**
+ * Compares two version numbers in the pattern x.x.x.
+ *
+ * @param {Version} versionA - The first version number to compare.
+ * @param {Version} versionB - The second version number to compare.
+ * @returns {number} Returns:
+ *   - 1 if versionA is greater than versionB.
+ *   - -1 if versionA is less than versionB.
+ *   - 0 if versionA is equal to versionB.
+ */
+export const sortVersions = (versionA: Version, versionB: Version): number => {
+  const a = versionA.split('.').map(Number);
+  const b = versionB.split('.').map(Number);
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] < b[i]) return -1;
+    if (a[i] > b[i]) return 1;
+  }
+
+  return 0;
 };
