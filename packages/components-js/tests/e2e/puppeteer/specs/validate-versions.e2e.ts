660a99217706a7207d37503aaa95d645af558b07
Henri Joss <henri.joss@mhp.com>
2023-11-07 10:54:19 +0100

    test(components-js): add validate versions e2e test | hj | #2642

---
+++ packages/components-js/tests/e2e/puppeteer/specs/validate-versions.e2e.ts
@@ -0,0 +1,62 @@
+import { Page } from 'puppeteer';
+import {
+  enableBrowserLogging,
+  getConsoleWarningMessagesRaw,
+  initConsoleObserver,
+  setContentWithDesignSystem,
+} from '../helpers';
+import { version } from '../../../../dist/components-wrapper/package.json';
+import { getLoaderScript } from '../../../../projects/partials';
+import { expect } from '@playwright/test';
+import { getExternalLoaderScriptForPrefixes } from 'crawler/tests/e2e/helpers';
+
+let page: Page;
+beforeEach(async () => {
+  page = await browser.newPage();
+  initConsoleObserver(page);
+});
+afterEach(async () => await page.close());
+
+it('should show warning about multiple different versions correctly', async () => {
+  enableBrowserLogging(page);
+
+  const prefixes = ['test', 'my-prefix'];
+
+  await setContentWithDesignSystem(
+    page,
+    `
+    ${getLoaderScript({ prefix: [''] })}
+    ${getExternalLoaderScriptForPrefixes(prefixes)}`,
+    { withoutWaitForComponentsReady: true }
+  );
+
+  const rawWarnings = getConsoleWarningMessagesRaw();
+  const versionWarning = rawWarnings.find((warning) => warning.text().includes('Multiple different versions'));
+
+  expect(versionWarning).toBeDefined();
+
+  const warningArgs = await Promise.all(versionWarning.args().map(async (arg) => await arg.evaluate((arg) => arg)));
+
+  expect(warningArgs).toEqual([
+    `[Porsche Design System v${version}]`,
+    'Multiple different versions are used with following prefixes:\n',
+    { '2.18.0': prefixes, [`${version}`]: [''] },
+    `\nPlease upgrade all instances to the latest used version: ${version}`,
+  ]);
+});
+
+it('should not show warning about multiple different versions if only one version is used', async () => {
+  enableBrowserLogging(page);
+
+  await setContentWithDesignSystem(
+    page,
+    `
+    ${getLoaderScript({ prefix: [''] })}`,
+    { withoutWaitForComponentsReady: true }
+  );
+
+  const rawWarnings = getConsoleWarningMessagesRaw();
+  const versionWarning = rawWarnings.find((warning) => warning.text().includes('Multiple different versions'));
+
+  expect(versionWarning).not.toBeDefined();
+});
