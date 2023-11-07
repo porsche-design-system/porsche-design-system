bbddcadd9c4fbe74a8bae78977265be48ae96b5b
Henri Joss <henri.joss@mhp.com>
2023-11-07 13:20:03 +0100

    test(components-js): add fn to get old loader script | hj | #2642

--- packages/components-js/tests/e2e/puppeteer/helpers/puppeteer-helper.ts
+++ packages/components-js/tests/e2e/puppeteer/helpers/puppeteer-helper.ts
@@ -284,7 +284,7 @@ export const initConsoleObserver = (page: Page): void => {
 };
 
 const getConsoleErrors = () => consoleMessages.filter((x) => x.type() === 'error');
-const getConsoleWarnings = () => consoleMessages.filter((x) => x.type() === 'warning');
+export const getConsoleWarnings = () => consoleMessages.filter((x) => x.type() === 'warning');
 export const getConsoleErrorsAmount = () => getConsoleErrors().length;
 export const getConsoleErrorMessages = () =>
   getConsoleErrors()
@@ -295,8 +295,6 @@ export const getConsoleWarningMessages = () =>
   getConsoleWarnings()
     .map((msg) => '- ' + msg.text())
     .join('\n');
-
-export const getConsoleWarningMessagesRaw = (): ConsoleMessage[] => getConsoleWarnings();
 
 const thrownErrors: string[] = [];
 
@@ -415,4 +413,17 @@ export const getHTMLAttributes = <T extends object>(props: T): string => {
       return `${attributeName}="${attributeValue}"`;
     })
     .join(' ');
+};
+
+export const getV2LoaderScriptForPrefixes = (prefixes: string[]): string => {
+  const prefixLoaders = prefixes
+    .map((prefix) => `porscheDesignSystem.load(\{ prefix: '${prefix}' \});`)
+    .join('\n      ');
+
+  // the script below has been copied from https://designsystem.porsche.com/v2/
+  return `
+    <script data-pds-loader-script="">
+      var porscheDesignSystem;(()=>{"use strict";var e={d:(t,s)=>{for(var o in s)e.o(s,o)&&!e.o(t,o)&&Object.defineProperty(t,o,{enumerable:!0,get:s[o]})},o:(e,t)=>Object.prototype.hasOwnProperty.call(e,t),r:e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})}},t={};function s(e){if("noModule"in HTMLScriptElement.prototype){const t=("script",document.createElement("script"));t.src=e,t.setAttribute("crossorigin",""),document.body.appendChild(t)}}e.r(t),e.d(t,{load:()=>n});const o="porscheDesignSystem";const n=(e={prefix:""})=>{!function({script:e,version:t,prefix:n}){const r=function(e){const t=(document[o]||(document[o]={}),document[o]),{[e]:s=null}=t;if(null===s){const s={isLoaded:!1,prefixes:[],registerCustomElements:null};t[e]=s}return t[e]}(t)||{},{isLoaded:i,prefixes:c,registerCustomElements:d}=r;i||(s(e),r.isLoaded=!0),c.includes(n)||(c.push(n),d&&d(n))}(Object.assign(Object.assign({},{version:"2.18.0",script:(typeof window!=='undefined'&&window.PORSCHE_DESIGN_SYSTEM_CDN==='cn'?'https://cdn.ui.porsche.cn':'https://cdn.ui.porsche.com')+"/porsche-design-system/components/porsche-design-system.v2.18.0.ec5f4e3768659fe2c13b.js"}),e))};porscheDesignSystem=t})();
+      ${prefixLoaders}
+    </script>`;
 };
