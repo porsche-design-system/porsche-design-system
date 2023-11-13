8791cd2f68d173bfee539cb70f7a0c026d06c109
Henri Joss <henri.joss@mhp.com>
2023-11-13 09:27:23 +0100

    refactor(components): change to only apply fix to chromium based browsers | hj | #2820

--- packages/components/src/styles/checkbox-radio-styles.ts
+++ packages/components/src/styles/checkbox-radio-styles.ts
@@ -1,6 +1,6 @@
 import type { BreakpointCustomizable, Theme } from '../types';
 import type { Styles } from 'jss';
-import { buildResponsiveStyles, isDisabledOrLoading, isHighContrastMode } from '../utils';
+import { buildResponsiveStyles, isDisabledOrLoading, isHighContrastMode, supportsChromiumMediaQuery } from '../utils';
 import {
   addImportantToEachRule,
   colorSchemeStyles,
@@ -117,14 +117,18 @@ export const getCheckboxRadioJssStyle = (
           ...(!isHighContrastMode &&
             hoverMediaQuery({
               '&(input:hover), .text:hover ~ &(input)': {
-                transition: 'unset', // Fixes chrome bug where border-color is stuck on hover color
+                ...supportsChromiumMediaQuery({
+                  transition: 'unset', // Fixes chrome bug where border-color is stuck on hover color
+                }),
                 borderColor: uncheckedHoverColor,
                 ...prefersColorSchemeDarkMediaQuery(theme, {
                   borderColor: uncheckedHoverColorDark,
                 }),
               },
               '&(input:checked:hover), .text:hover ~ &(input:checked)': {
-                transition: 'unset', // Fixes chrome bug where border-color is stuck on hover color
+                ...supportsChromiumMediaQuery({
+                  transition: 'unset', // Fixes chrome bug where border-color is stuck on hover color
+                }),
                 borderColor: checkedHoverColor,
                 backgroundColor: checkedHoverColor,
                 ...prefersColorSchemeDarkMediaQuery(theme, {
