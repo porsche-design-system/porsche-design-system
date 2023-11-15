db923c02aa4637cd2aed5a5a95e45478451c7724
Henri Joss <henri.joss@mhp.com>
2023-11-15 12:19:46 +0100

    test(components-js): add button threshold config | hj | #fix-flaky-vrt

--- packages/components-js/tests/vrt/playwright/specs/states/button.vrt.ts
+++ packages/components-js/tests/vrt/playwright/specs/states/button.vrt.ts
@@ -9,6 +9,7 @@ import {
   getPlaygroundPseudoStatesMarkup,
   type PrefersColorScheme,
   setContentWithDesignSystem,
+  thresholdConfig,
 } from '../../helpers';
 import { type Theme } from '@porsche-design-system/utilities-v2';
 
@@ -50,7 +51,8 @@ test.describe(component, async () => {
     test(`should have no visual regression for :hover + :focus-visible with theme ${theme}`, async ({ page }) => {
       await scenario(page, theme);
       await expect(page.locator('#app')).toHaveScreenshot(
-        `${component}-${baseViewportWidth}-states-theme-${theme}.png`
+        `${component}-${baseViewportWidth}-states-theme-${theme}.png`,
+        thresholdConfig
       );
     });
   });
@@ -61,7 +63,8 @@ test.describe(component, async () => {
     }) => {
       await scenario(page, 'auto', scheme);
       await expect(page.locator('#app')).toHaveScreenshot(
-        `${component}-${baseViewportWidth}-states-theme-${scheme}.png`
+        `${component}-${baseViewportWidth}-states-theme-${scheme}.png`,
+        thresholdConfig
       ); // fixture is aliased since result has to be equal
     });
   });
