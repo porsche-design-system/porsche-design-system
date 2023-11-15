b825b8736f1b90d207cfbf2f3c91dc40984b2cb0
Oliver Hook <oliver.hook@porsche.digital>
2023-11-15 15:07:17 +0100

    test(components-js): fix selector | oh | #2725

--- packages/components-js/tests/vrt/playwright/helpers/select-wrapper-helper.ts
+++ packages/components-js/tests/vrt/playwright/helpers/select-wrapper-helper.ts
@@ -4,7 +4,10 @@ export const openAllSelectWrapper = async (page: Page): Promise<void> => {
   await page.evaluate(() => {
     document.addEventListener('mousedown', (e) => e.stopPropagation(), true);
     document.querySelectorAll('p-select-wrapper, my-prefix-p-select-wrapper').forEach((select) => {
-      select.shadowRoot.querySelector('button').click();
+      select.shadowRoot
+        .querySelector('p-select-wrapper-dropdown' || 'my-prefix-p-select-wrapper-dropdown')
+        .shadowRoot.querySelector('button' || 'input')
+        .click();
     });
   });
 };
