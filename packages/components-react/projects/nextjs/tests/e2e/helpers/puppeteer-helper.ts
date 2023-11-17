eb012497b6e01ad5deeac0e969efc8bff8123bce
TatjanaGartner <tatjana.gartner@mhp.com>
2023-11-17 14:01:24 +0100

    test(components-react): add e2e test | tg | #2886

--- packages/components-react/projects/nextjs/tests/e2e/helpers/puppeteer-helper.ts
+++ packages/components-react/projects/nextjs/tests/e2e/helpers/puppeteer-helper.ts
@@ -1,4 +1,5 @@
 import type { ConsoleMessage, Page } from 'puppeteer';
+import { ElementHandle } from 'puppeteer';
 
 export const waitForComponentsReady = (page: Page): Promise<number> => {
   // componentsReady is exposed via index.tsx of react vrt app
@@ -29,4 +30,21 @@ export const initConsoleObserver = (page: Page): void => {
   });
 };
 export const getConsoleErrorsAmount = () => consoleMessages.filter((x) => x.type() === 'error').length;
-export const getConsoleWarningsAmount = () => consoleMessages.filter((x) => x.type() === 'warning').length;
+
+export const selectNode = async (page: Page, selector: string): Promise<ElementHandle> => {
+  const selectorParts = selector.split('>>>');
+  const shadowRootSelectors =
+    selectorParts.length > 1
+      ? selectorParts
+          .slice(1)
+          .map((x) => `.shadowRoot.querySelector('${x.trim()}')`)
+          .join('')
+      : '';
+  return (
+    await page.evaluateHandle(`document.querySelector('${selectorParts[0].trim()}')${shadowRootSelectors}`)
+  ).asElement() as ElementHandle;
+};
+
+export const getAttribute = (element: ElementHandle, attribute: string): Promise<string> => {
+  return element.evaluate((el: HTMLElement, attr: string) => el.getAttribute(attr), attribute);
+};
