1e81670c9b5d833d5fb363866519c387d177627d
Bernd Hacker <hacker.bernd@gmail.com>
2023-11-20 15:22:25 +0100

    test(components-angular/react/vue): adjust provider e2e tests to user theme-injection page | bh | #1185

--- packages/components-vue/tests/e2e/specs/provider.e2e.ts
+++ packages/components-vue/tests/e2e/specs/provider.e2e.ts
@@ -6,13 +6,40 @@ let page: Page;
 beforeEach(async () => (page = await browser.newPage()));
 afterEach(async () => await page.close());
 
-it('should change p-accordion theme to dark', async () => {
-  await goto(page, 'accordion-example');
+it('should provide global theme correctly to 4th p-button', async () => {
+  await goto(page, 'theme-injection');
 
-  const accordion = await page.$('p-accordion');
+  const [button1, button2, button3, button4Initial] = await page.$$('p-button');
   const getTheme = (handle: ElementHandle<Element>): Promise<Theme> => handle.evaluate((el) => (el as any).theme);
-  expect(await getTheme(accordion)).toBe('light');
+  expect(await getTheme(button1)).toBe('light');
+  expect(await getTheme(button2)).toBe('dark');
+  expect(await getTheme(button3)).toBe('light');
+  expect(button4Initial).toBeUndefined();
 
+  // conditionally render button4
+  await button3.click();
+  const [, , , button4Render1] = await page.$$('p-button');
+  expect(button4Render1).toBeDefined();
+  expect(await getTheme(button4Render1)).toBe('light');
+
+  // change global theme
   await page.select('select[name="theme"]', 'dark');
-  expect(await getTheme(accordion)).toBe('dark');
+  expect(await getTheme(button3)).toBe('dark');
+  expect(await getTheme(button4Render1)).toBe('dark');
+
+  // remove button4
+  await button3.click();
+  const [, , , button4] = await page.$$('p-button');
+  expect(button4).toBeUndefined();
+
+  // render button4 again
+  await button3.click();
+  const [, , , button4Render2] = await page.$$('p-button');
+  expect(button4Render2).toBeDefined();
+  expect(await getTheme(button4Render2)).toBe('dark');
+
+  // change global theme
+  await page.select('select[name="theme"]', 'light');
+  expect(await getTheme(button3)).toBe('light');
+  expect(await getTheme(button4Render2)).toBe('light');
 });
