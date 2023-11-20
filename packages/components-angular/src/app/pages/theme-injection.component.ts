26e12b8138764b92e510c05a0df2289d9e5c7848
Bernd Hacker <hacker.bernd@gmail.com>
2023-11-20 15:19:50 +0100

    feat(components-angular): align theme injection page with other frameworks | bh | #1185

--- packages/components-angular/src/app/pages/theme-injection.component.ts
+++ packages/components-angular/src/app/pages/theme-injection.component.ts
@@ -1,12 +1,11 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { THEME_TOKEN } from '@porsche-design-system/components-angular';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'page-theme-injection',
   template: `
     <div class="playground light">
-      <p-button [theme]="'light'" (click)="theme$.next('light')">Light Button</p-button>
-      <p-button [theme]="'dark'" (click)="theme$.next('dark')">Dark Button</p-button>
+      <p-button [theme]="'light'">Light Button</p-button>
+      <p-button [theme]="'dark'">Dark Button</p-button>
       <br />
       <p-button (click)="isVisible = !isVisible">Show/Hide Button</p-button>
       <p-button *ngIf="isVisible">Global Theme</p-button>
@@ -15,6 +14,5 @@ import { THEME_TOKEN } from '@porsche-design-system/components-angular';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ThemeInjectionComponent {
-  theme$ = inject(THEME_TOKEN);
   isVisible = false;
 }
