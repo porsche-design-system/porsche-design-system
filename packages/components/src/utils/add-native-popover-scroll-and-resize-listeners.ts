98ec5cbb32a0002acb6d4d6fbfafb43c689c1309
Oliver Hook <oliver.hook@porsche.digital>
2023-11-21 16:52:03 +0100

    feat(components): remove window scroll listeners | oh | #2725

--- packages/components/src/utils/add-native-popover-scroll-and-resize-listeners.ts
+++ packages/components/src/utils/add-native-popover-scroll-and-resize-listeners.ts
@@ -14,12 +14,10 @@ export const addNativePopoverScrollAndResizeListeners = (
   const hidePopover = (): void => {
     nativePopover.hidePopover();
     callback?.();
-    window.removeEventListener('scroll', hidePopover);
     window.removeEventListener('resize', hidePopover);
     tableScrollArea.removeEventListener('scroll', hidePopover);
   };
 
-  window.addEventListener('scroll', hidePopover, { once: true });
   window.addEventListener('resize', hidePopover, { once: true });
   tableScrollArea.addEventListener('scroll', hidePopover, { once: true });
 };
