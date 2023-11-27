8dbb42f846975355bf52d56146d55ef9c1b46aa0
Bernd Hacker <hacker.bernd@gmail.com>
2023-11-27 15:58:22 +0100

    fix(components-react/react-ssr-wrapper): minifyCss removing escaping backslash in selectors | bh | #2813

--- packages/components-react/projects/react-ssr-wrapper/src/minifyCss.ts
+++ packages/components-react/projects/react-ssr-wrapper/src/minifyCss.ts
@@ -1,2 +1,7 @@
-export const minifyCss = (css: string): string =>
-  css.replace(/\s\s+|\.\\(?=:)|[\n\\]+| (?={)|;(?=\s+})|(:|media)\s(?=.*;?)/g, '$1');
+export const minifyCss = (css: string): string => {
+  // TODO: could remove whitespace in selectors following
+  // - commas: e.g. `, `
+  // - before/after + combinator, e.g. ` + `
+  // - and potentially other combinators
+  return css.replace(/\s\s+|\.\\(?=:)|[\n\\]+(?!\+)| (?={)|;(?=\s+})|(:|media)\s(?=.*;?)/g, '$1');
+};
