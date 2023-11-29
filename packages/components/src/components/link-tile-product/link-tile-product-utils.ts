c05bffb9685f04a3c9008d5cb6924265864f56a4
Henri Joss <henri.joss@mhp.com>
2023-11-29 13:09:12 +0100

    chore(components): remove unused const | hj | #2877

--- packages/components/src/components/link-tile-product/link-tile-product-utils.ts
+++ packages/components/src/components/link-tile-product/link-tile-product-utils.ts
@@ -1,10 +1,6 @@
 import { LinkTarget } from '../../utils/link-button/link-target';
-import { LinkAriaAttribute } from '../../utils';
 
 export type LinkTileProductTarget = LinkTarget;
-export type LinkTileProductAriaAttribute = LinkAriaAttribute;
 export type LinkTileProductUpdateEvent = {
   liked: boolean;
 };
-
-export const tagsSlotName = 'tags';
