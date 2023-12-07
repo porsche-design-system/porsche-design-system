673acedc6fbf667d990f0fc7df826e1f5beec4ef
Henri Joss <henri.joss@mhp.com>
2023-12-07 11:27:10 +0100

    refactor(components): change link-tile-product structure | hj | #2877

--- packages/components/src/components/link-tile-product/link-tile-product-utils.ts
+++ packages/components/src/components/link-tile-product/link-tile-product-utils.ts
@@ -1,6 +1,10 @@
 import { LinkTarget } from '../../utils/link-button/link-target';
+import { TileAspectRatio } from '../../utils';
 
 export type LinkTileProductTarget = LinkTarget;
-export type LinkTileProductUpdateEvent = {
+export type LinkTileProductLikeEvent = {
   liked: boolean;
 };
+
+export const TILE_PRODUCT_ASPECT_RATIOS = ['3:4', '9:16'] as const;
+export type LinkTileProductAspectRatio = Extract<TileAspectRatio, (typeof TILE_PRODUCT_ASPECT_RATIOS)[number]>;
