f99f33fa4cfad70b6789397a18f7b582e0691531
Henri Joss <henri.joss@mhp.com>
2023-12-11 08:17:00 +0100

    test(components-js): add jsdom polyfill event test | hj | #2877

--- packages/components-js/projects/jsdom-polyfill/tests/unit/specs/link-tile-product.spec.ts
+++ packages/components-js/projects/jsdom-polyfill/tests/unit/specs/link-tile-product.spec.ts
@@ -1,5 +1,7 @@
 import { componentsReady } from '@porsche-design-system/components-js';
 import { getMarkup } from '../helper';
+import userEvent from '@testing-library/user-event';
+import { getByRoleShadowed } from '@porsche-design-system/components-js/testing';
 
 it('should have initialized shadow dom', async () => {
   document.body.innerHTML = getMarkup('p-link-tile-product');
@@ -8,4 +10,21 @@ it('should have initialized shadow dom', async () => {
   const el = document.body.firstElementChild;
   expect(el.shadowRoot).not.toBeNull();
   expect(el.className).toBe('hydrated');
+});
+
+it('should have working events', async () => {
+  document.body.innerHTML = getMarkup('p-link-tile-product') + `<div id="debug">Event Counter: <span>0</span></div>`;
+  await componentsReady();
+
+  const el = document.body.firstElementChild;
+  el.addEventListener('like', () => {
+    debugEl.querySelector('span').innerHTML = '1';
+  });
+
+  const debugEl = document.querySelector('#debug');
+  expect(debugEl.innerHTML).toBe('Event Counter: <span>0</span>');
+
+  const button = getByRoleShadowed('button');
+  await userEvent.click(button);
+  expect(debugEl.innerHTML).toBe('Event Counter: <span>1</span>');
 });
