2ae13c18fb780ab6fd7838ff396556e2706c8447
TatjanaGartner <tatjana.gartner@mhp.com>
2023-12-15 16:51:33 +0100

    refactor(components): improve return type of isShadowRootParentOfKind util and add missing unit tests | tg | #2922

--- packages/components/src/utils/dom/isShadowRootParentOfKind.spec.ts
+++ packages/components/src/utils/dom/isShadowRootParentOfKind.spec.ts
@@ -1,10 +1,23 @@
 import { isShadowRootParentOfKind } from './isShadowRootParentOfKind';
 
-// TODO: tests are missing
 it('should return true if child is inside shadow dom and parent tag matches', () => {
   const parent = document.createElement('p-grid');
   const child = document.createElement('p-grid-item');
   parent.attachShadow({ mode: 'open' }).appendChild(child);
 
   expect(isShadowRootParentOfKind(child, 'p-grid')).toBe(true);
+});
+
+it('should return false if no parent element is found', () => {
+  const child = document.createElement('p-grid-item');
+
+  expect(isShadowRootParentOfKind(child, 'p-grid')).toBe(false);
+});
+
+it('should return false if child is inside shadow dom and parent tag does not match', () => {
+  const parent = document.createElement('p-grid');
+  const child = document.createElement('p-grid-item');
+  parent.attachShadow({ mode: 'open' }).appendChild(child);
+
+  expect(isShadowRootParentOfKind(child, 'p-fieldset')).toBe(false);
 });
