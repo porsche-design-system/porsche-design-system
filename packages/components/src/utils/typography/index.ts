60b6189d56278b1b33255f4085f51716801bfd8f
TatjanaGartner <tatjana.gartner@mhp.com>
2023-12-20 10:59:23 +0100

    feat(components): minor improvements | tg | #2922

--- packages/components/src/utils/typography/index.ts
+++ packages/components/src/utils/typography/index.ts
@@ -1,5 +1,5 @@
 export * from './isSizeInherit';
+export * from './text-size';
 export * from './typography-align';
 export * from './typography-text-color';
-export * from './text-size';
 export * from './typography-text-weight';
