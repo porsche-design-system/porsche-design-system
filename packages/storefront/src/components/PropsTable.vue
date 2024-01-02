bbd943c36b90a23a5230ee0c834497daf1afec16
Bernd Hacker <hacker.bernd@gmail.com>
2024-01-02 14:36:22 +0100

    feat(storefront): extend PropsTable to handle allowedValues and isBreakpointCustomizable | bh | #2821

--- packages/storefront/src/components/PropsTable.vue
+++ packages/storefront/src/components/PropsTable.vue
@@ -19,10 +19,8 @@
           <code>{{ paramCase(name) }}</code>
           <span v-if="value.isDeprecated" title="deprecated"> 🚫</span>
         </td>
-        <td v-html="formatDescription(value.description)"></td>
-        <td>
-          <code>{{ value.type }}</code>
-        </td>
+        <td v-html="formatDescription(value)"></td>
+        <td v-html="formatType(value)"></td>
         <td>
           <code>{{ value.defaultValue ?? 'undefined' }}</code>
         </td>
@@ -36,8 +34,10 @@
   import Component from 'vue-class-component';
   import { Prop } from 'vue-property-decorator';
   import { paramCase } from 'change-case';
-  import { getComponentMeta, type ComponentMeta } from '@porsche-design-system/component-meta';
+  import { getComponentMeta, type ComponentMeta, type PropMeta } from '@porsche-design-system/component-meta';
   import type { TagName } from '@porsche-design-system/shared';
+
+  const wrapInCode = (input: string): string => `<code>${input.replace(/</g, '&lt;').replace(/>/g, '&gt;')}</code>`;
 
   @Component
   export default class PropsTable extends Vue {
@@ -49,12 +49,26 @@
       return this.component ? getComponentMeta(this.component).propsMeta : {};
     }
 
-    public formatDescription(input?: string): string {
-      return input
-        ? input
+    public formatDescription(meta: PropMeta): string {
+      return meta.description
+        ? meta.description
             .replace(/@(deprecated)/, '<strong class="deprecated">$1</strong>') // deprecated annotation
             .replace(/`(.+?)`/g, '<code>$1</code>') // prop references in backticks
         : '';
+    }
+
+    public formatType(meta: PropMeta): string {
+      return Array.isArray(meta.allowedValues) || meta.isBreakpointCustomizable
+        ? [
+            meta.type !== 'string' && meta.type !== 'number' && meta.type !== 'boolean'
+              ? `type ${meta.type} =` // literal types, etc.
+              : meta.type, // simple type
+            ...(Array.isArray(meta.allowedValues) ? meta.allowedValues.map((val) => `'${val}'`) : []),
+            ...(meta.isBreakpointCustomizable ? [`BreakpointCustomizable<${meta.type}>`] : []),
+          ]
+            .map(wrapInCode)
+            .join('<br>\n')
+        : wrapInCode(meta.type);
     }
   }
 </script>
