69c5c4e46de8d40e300a3a56a4bdb638ae1ea352
Bernd Hacker <hacker.bernd@gmail.com>
2024-01-03 10:57:43 +0100

    feat(storefront): improve output formatting in PropsTable | bh | #2821

--- packages/storefront/src/components/PropsTable.vue
+++ packages/storefront/src/components/PropsTable.vue
@@ -22,7 +22,9 @@
         <td v-html="formatDescription(value)"></td>
         <td v-html="formatType(value)"></td>
         <td>
-          <code>{{ value.defaultValue ?? 'undefined' }}</code>
+          <code>{{
+            (typeof value.defaultValue === 'string' ? `'${value.defaultValue}'` : value.defaultValue) ?? 'undefined'
+          }}</code>
         </td>
       </tr>
     </tbody>
@@ -37,53 +39,67 @@
   import { getComponentMeta, type ComponentMeta, type PropMeta } from '@porsche-design-system/component-meta';
   import type { TagName } from '@porsche-design-system/shared';
 
-  const wrapInCodeCode = (input: string): string =>
-    `<code>${input.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br>')}</code>`;
+  const formatHtml = (input: string): string =>
+    input.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br>');
+
+  const wrapInCodeCode = (input: string): string => `<code>${formatHtml(input)}</code>`;
 
   @Component
   export default class PropsTable extends Vue {
-    @Prop({ default: undefined }) public component?: TagName;
+    @Prop() public component!: TagName;
 
     paramCase = paramCase;
 
     public get propsMeta(): ComponentMeta['propsMeta'] {
-      return this.component ? getComponentMeta(this.component).propsMeta : {};
+      const unorderedPropsMeta = getComponentMeta(this.component)?.propsMeta;
+      return unorderedPropsMeta
+        ? Object.keys(unorderedPropsMeta)
+            .sort()
+            .reduce((result, key) => ({ ...result, [key]: unorderedPropsMeta[key] }), {})
+        : {};
     }
 
     public formatDescription(meta: PropMeta): string {
       return meta.description
         ? meta.description
             .replace(/@(deprecated)/, '<strong class="deprecated">$1</strong>') // deprecated annotation
-            .replace(/`(.+?)`/g, '<code>$1</code>') // prop references in backticks
+            .replace(/`(.+?)`/g, (_, g1) => `<code>${formatHtml(g1)}</code>`) // prop references in backticks
         : '';
     }
 
     public formatType(meta: PropMeta): string {
       return Array.isArray(meta.allowedValues) || meta.isBreakpointCustomizable
         ? [
-            meta.type !== 'string' && meta.type !== 'number' && meta.type !== 'boolean'
-              ? `type ${meta.type} =` // literal types, etc.
-              : meta.type, // simple type
+            ...(meta.type.includes('|')
+              ? [] // inline union type is taken care of via allowedValues
+              : [
+                  meta.type !== 'string' && meta.type !== 'number' && meta.type !== 'boolean'
+                    ? `type ${meta.type} =` // literal types, etc.
+                    : meta.type, // simple type
+                ]),
             ...(Array.isArray(meta.allowedValues)
               ? meta.allowedValues.map(
                   (val) =>
-                    wrapInCodeCode(`'${val}'`) +
+                    wrapInCodeCode(val === 'string' ? val : `'${val}'`) +
                     (meta.deprecatedValues?.includes(val) ? '<span title="deprecated"> 🚫</span>' : '')
                 )
               : []),
             ...(meta.isBreakpointCustomizable ? [`BreakpointCustomizable<${meta.type}>`] : []),
           ]
+            // allowedValues are already wrapped with code tag because of trailing deprecated icon, but others are not
             .map((item) => (item.match(/<code>.+?<\/code>/) ? item : wrapInCodeCode(item)))
             .join('<br>\n')
         : meta.isAria && typeof meta.allowedValues === 'object'
-          ? wrapInCodeCode(
-              `type ${meta.type} = {\n` +
-                Object.entries(meta.allowedValues)
-                  .map(([key, val]) => `&nbsp;&nbsp;'${key}'?: ${val};`)
-                  .join('\n') +
-                '\n}'
+          ? // aria props
+            wrapInCodeCode(
+              `type ${meta.type} = {
+${Object.entries(meta.allowedValues)
+  // possible values are output as string, even though actual types are more precise
+  .map(([key, val]) => `&nbsp;&nbsp;'${key}'?: ${val};`)
+  .join('\n')}
+}`
             )
-          : wrapInCodeCode(meta.type);
+          : wrapInCodeCode(meta.type); // all other cases
     }
   }
 </script>
