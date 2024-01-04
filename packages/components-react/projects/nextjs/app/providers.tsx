731be74c2ff1a6222956be096665932cde15f1e7
Henri Joss <henri.joss@mhp.com>
2024-01-04 13:56:36 +0100

    feat(components-react/next-js): revert changes list | hj

--- packages/components-react/projects/nextjs/app/providers.tsx
+++ packages/components-react/projects/nextjs/app/providers.tsx
@@ -3,17 +3,13 @@
 import { type JSX, type PropsWithChildren, useState } from 'react';
 import { componentsReady, PorscheDesignSystemProvider, type Theme } from '@porsche-design-system/components-react/ssr';
 import { Selects } from '../components';
-import { useSearchParams } from 'next/navigation';
 
 export const Providers = ({ children }: PropsWithChildren<{}>): JSX.Element => {
   const [theme, setTheme] = useState<Theme>('light');
 
-  const searchParams = useSearchParams();
-  const isPageLoadedInIFrame = !!searchParams?.get('iframe');
-
   return (
     <>
-      {!isPageLoadedInIFrame && <Selects theme={theme} setTheme={setTheme} />}
+      <Selects theme={theme} setTheme={setTheme} />
       <PorscheDesignSystemProvider cdn="auto" theme={theme} children={children} />
     </>
   );
