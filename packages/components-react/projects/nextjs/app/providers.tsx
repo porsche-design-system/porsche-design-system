d8120287905120452d910ef9dc9860ecb3caf30b
Henri Joss <henri.joss@mhp.com>
2024-01-04 11:42:25 +0100

    feat(components-react/next-js): render selects conditionally | hj

--- packages/components-react/projects/nextjs/app/providers.tsx
+++ packages/components-react/projects/nextjs/app/providers.tsx
@@ -3,13 +3,17 @@
 import { type JSX, type PropsWithChildren, useState } from 'react';
 import { componentsReady, PorscheDesignSystemProvider, type Theme } from '@porsche-design-system/components-react/ssr';
 import { Selects } from '../components';
+import { useSearchParams } from 'next/navigation';
 
 export const Providers = ({ children }: PropsWithChildren<{}>): JSX.Element => {
   const [theme, setTheme] = useState<Theme>('light');
 
+  const searchParams = useSearchParams();
+  const isPageLoadedInIFrame = !!searchParams?.get('iframe');
+
   return (
     <>
-      <Selects theme={theme} setTheme={setTheme} />
+      {!isPageLoadedInIFrame && <Selects theme={theme} setTheme={setTheme} />}
       <PorscheDesignSystemProvider cdn="auto" theme={theme} children={children} />
     </>
   );
