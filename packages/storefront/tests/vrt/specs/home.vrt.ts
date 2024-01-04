cbd6f5ae30131fcd157bd947487379f044ba3124
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-01-04 12:39:02 +0100

    test(storefront): update vrt screenshots | sas

--- packages/storefront/tests/vrt/specs/home.vrt.ts
+++ packages/storefront/tests/vrt/specs/home.vrt.ts
@@ -1,11 +1,8 @@
 import { expect, test } from '@playwright/test';
 
-const schemes = ['light', 'dark'] as const;
-const viewportWidth = 1000;
-const viewportWidths = [320, 480, 760, 1300, 1760, 1920, 2560, 3000] as const;
-
 test.describe('home', async () => {
-  schemes.forEach((scheme) => {
+  (['light', 'dark'] as const).forEach((scheme) => {
+    const viewportWidth = 1000;
     test(`should have no visual regression for viewport ${viewportWidth} and theme auto with prefers-color-scheme ${scheme}`, async ({
       page,
     }) => {
@@ -18,13 +15,13 @@ test.describe('home', async () => {
       );
       await page.setViewportSize({
         width: viewportWidth,
-        height: 600,
+        height: await page.evaluate(() => document.body.clientHeight),
       });
       await expect(page.locator('#app')).toHaveScreenshot(`home-${viewportWidth}-scheme-${scheme}.png`);
     });
   });
 
-  viewportWidths.forEach((viewportWidth) => {
+  ([320, 480, 760, 1300, 1760, 1920, 2560, 3000] as const).forEach((viewportWidth) => {
     test(`should have no visual regression for viewport ${viewportWidth}`, async ({ page }) => {
       await page.goto('/');
       await page.evaluate(() =>
