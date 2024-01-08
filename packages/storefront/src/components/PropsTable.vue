e6972d16b2166d112ee9f7bea04a455c88dc0760
Bernd Hacker <hacker.bernd@gmail.com>
2024-01-08 10:56:55 +0100

    refactor(storefront): extract PropsTable formatting utils | bh | #2821

--- packages/storefront/src/components/PropsTable.vue
+++ packages/storefront/src/components/PropsTable.vue
@@ -21,13 +21,9 @@
             <code>{{ paramCase(name) }}</code>
             <span v-if="value.isDeprecated" title="deprecated"> 🚫</span>
           </td>
-          <td v-html="formatDescription(value)"></td>
-          <td v-html="formatType(value)"></td>
-          <td>
-            <code>{{
-              (typeof value.defaultValue === 'string' ? `'${value.defaultValue}'` : value.defaultValue) ?? 'undefined'
-            }}</code>
-          </td>
+          <td v-html="formatPropDescription(value)"></td>
+          <td v-html="formatPropType(value)"></td>
+          <td v-html="formatPropDefaultValue(value)"></td>
         </tr>
       </tbody>
     </table>
@@ -41,11 +37,7 @@
   import { paramCase } from 'change-case';
   import { getComponentMeta, type ComponentMeta, type PropMeta } from '@porsche-design-system/component-meta';
   import type { TagName } from '@porsche-design-system/shared';
-
-  const formatHtml = (input: string): string =>
-    input.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br>');
-
-  const wrapInCodeCode = (input: string): string => `<code>${formatHtml(input)}</code>`;
+  import { formatPropDefaultValue, formatPropDescription, formatPropType } from '@/utils';
 
   @Component
   export default class PropsTable extends Vue {
@@ -77,54 +69,16 @@
         : false;
     }
 
-    public formatDescription(meta: PropMeta): string {
-      return meta.description
-        ? meta.description
-            .replace(/@(deprecated)/, '<strong class="deprecated">$1</strong>') // deprecated annotation
-            .replace(/`(.+?)`/g, (_, g1) => `<code>${formatHtml(g1)}</code>`) // prop references in backticks
-        : '';
+    public formatPropDescription(meta: PropMeta): string {
+      return formatPropDescription(meta);
     }
 
-    public formatType(meta: PropMeta): string {
-      return Array.isArray(meta.allowedValues) || meta.isBreakpointCustomizable
-        ? [
-            ...(meta.type.includes('|')
-              ? [] // inline union type is taken care of via allowedValues
-              : [
-                  meta.type !== 'string' && meta.type !== 'number' && meta.type !== 'boolean'
-                    ? `type ${meta.type} =` // literal types, etc.
-                    : meta.type, // simple type
-                ]),
-            ...(Array.isArray(meta.allowedValues)
-              ? meta.allowedValues.map(
-                  (val) =>
-                    wrapInCodeCode(
-                      val === 'string'
-                        ? val
-                        : val === null
-                          ? 'undefined'
-                          : typeof val === 'number'
-                            ? `${val}`
-                            : `'${val}'`
-                    ) + (meta.deprecatedValues?.includes(val) ? '<span title="deprecated"> 🚫</span>' : '')
-                )
-              : []),
-            ...(meta.isBreakpointCustomizable ? [`BreakpointCustomizable<${meta.type}>`] : []),
-          ]
-            // allowedValues are already wrapped with code tag because of trailing deprecated icon, but others are not
-            .map((item) => (item.match(/<code>.+?<\/code>/) ? item : wrapInCodeCode(item)))
-            .join('<br>\n')
-        : meta.isAria && typeof meta.allowedValues === 'object'
-          ? // aria props
-            wrapInCodeCode(
-              `type ${meta.type} = {
-${Object.entries(meta.allowedValues)
-  // possible values are output as string, even though actual types are more precise
-  .map(([key, val]) => `&nbsp;&nbsp;'${key}'?: ${val};`)
-  .join('\n')}
-}`
-            )
-          : wrapInCodeCode(meta.type); // all other cases
+    public formatPropType(meta: PropMeta): string {
+      return formatPropType(meta);
+    }
+
+    public formatPropDefaultValue(meta: PropMeta): string {
+      return formatPropDefaultValue(meta);
     }
   }
 </script>
--- packages/storefront/src/utils/formatting.ts
+++ packages/storefront/src/utils/formatting.ts
@@ -1,6 +1,6 @@
 import { paramCase } from 'change-case';
 import type { TagName } from '@porsche-design-system/shared';
-import { getComponentMeta } from '@porsche-design-system/component-meta';
+import { getComponentMeta, PropMeta } from '@porsche-design-system/component-meta';
 import { convertToAngular } from './convertToAngular';
 import { convertToReact } from './convertToReact';
 import { convertToVue } from './convertToVue';
@@ -47,4 +47,70 @@ export const convertMarkup = (markup: string, framework: Framework): string => {
     default:
       return cleanedMarkup;
   }
+};
+
+export const formatHtml = (input: string): string => {
+  return input.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\n/g, '<br>');
+};
+
+export const wrapInCodeTag = (input: string): string => `<code>${formatHtml(input)}</code>`;
+
+export const formatPropDescription = (meta: PropMeta): string => {
+  return meta.description
+    ? meta.description
+        .replace(/@(deprecated)/, '<strong class="deprecated">$1</strong>') // deprecated annotation
+        .replace(/`(.+?)`/g, (_, g1) => `<code>${formatHtml(g1)}</code>`) // prop references in backticks
+    : '';
+};
+
+export const formatPropType = (meta: PropMeta): string => {
+  return Array.isArray(meta.allowedValues) || meta.isBreakpointCustomizable
+    ? [
+        ...(meta.type.includes('|')
+          ? [] // inline union type is taken care of via allowedValues
+          : [
+              meta.type !== 'string' && meta.type !== 'number' && meta.type !== 'boolean'
+                ? `type ${meta.type} =` // literal types, etc.
+                : meta.type, // simple type
+            ]),
+        ...(Array.isArray(meta.allowedValues)
+          ? meta.allowedValues.map(
+              (val) =>
+                wrapInCodeTag(
+                  val === 'string'
+                    ? val // `string` type is fine
+                    : val === null
+                      ? 'undefined' // `null` isn't allowed as part of validateProps, but `undefined` is
+                      : typeof val === 'number'
+                        ? `${val}` // numbers don't need string quotes, e.g. `1`, `911`
+                        : `'${val}'` // everything else is a string value, e.g. `'start'` or `'16:9'`
+                ) + (meta.deprecatedValues?.includes(val) ? '<span title="deprecated"> 🚫</span>' : '')
+            )
+          : []),
+        ...(meta.isBreakpointCustomizable ? [`BreakpointCustomizable<${meta.type}>`] : []),
+      ]
+        // allowedValues are already wrapped with code tag because of trailing deprecated icon, but others are not
+        .map((item) => (item.match(/<code>.+?<\/code>/) ? item : wrapInCodeTag(item)))
+        .join('<br>\n')
+    : meta.isAria && typeof meta.allowedValues === 'object'
+      ? // aria props
+        wrapInCodeTag(
+          `type ${meta.type} = {
+${Object.entries(meta.allowedValues)
+  // possible values are output as string, even though actual types are more precise
+  .map(([key, val]) => `&nbsp;&nbsp;'${key}'?: ${val};`)
+  .join('\n')}
+}`
+        )
+      : wrapInCodeTag(meta.type); // all other cases
+};
+
+export const formatPropDefaultValue = (meta: PropMeta): string => {
+  return wrapInCodeTag(
+    (typeof meta.defaultValue === 'string'
+      ? `'${meta.defaultValue}'`
+      : typeof meta.defaultValue === 'object' || typeof meta.defaultValue === 'boolean'
+        ? JSON.stringify(meta.defaultValue).replace(/[{:,]/g, '$& ').replace(/}/g, ' $&')
+        : `${meta.defaultValue}`) ?? 'undefined'
+  );
 };
