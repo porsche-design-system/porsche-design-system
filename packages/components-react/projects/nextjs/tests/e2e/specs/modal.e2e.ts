ce003b7ff09a83ccd56c3a818abb44fbf584dfd5
Henri Joss <henri.joss@mhp.com>
2024-01-08 14:56:40 +0100

    test(components-js): fix flaky e2e | hj

--- packages/components-react/projects/nextjs/tests/e2e/specs/modal.e2e.ts
+++ packages/components-react/projects/nextjs/tests/e2e/specs/modal.e2e.ts
@@ -29,7 +29,7 @@ it('should keep same scroll position when modal is opened and closed with route 
   expect(await (await getRootElement()).evaluate((el) => el.scrollTop)).toBe(336);
 
   const dismissButton = await getDismissButton();
-  await Promise.all([
+  const [response] = await Promise.all([
     page.waitForNavigation({
       waitUntil: 'networkidle0',
     }),
@@ -38,6 +38,7 @@ it('should keep same scroll position when modal is opened and closed with route 
   await page.waitForFunction(() => !document.querySelector('p-modal'));
   await page.waitForSelector('#modal-standalone-page'); // Fixes flakiness in pipeline
 
+  expect(await getBodyStyle()).toBe('');
   expect(await (await getRootElement()).evaluate((el) => el.scrollTop)).toBe(336);
 });
 
