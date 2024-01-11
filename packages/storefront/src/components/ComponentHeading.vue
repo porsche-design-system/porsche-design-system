b744e4cb3efbf5f1932e591ffad8e9d37f6b1dd0
Bernd Hacker <hacker.bernd@gmail.com>
2024-01-11 11:41:12 +0100

    feat(storefront): remove console.log in formatPropType util | bh

--- packages/storefront/src/components/ComponentHeading.vue
+++ packages/storefront/src/components/ComponentHeading.vue
@@ -1,7 +1,16 @@
 <template>
   <!-- using h1 instead of p-heading for more stable navigation e2e test -->
+  <!-- to prevent additional space characters within tags -->
   <!-- prettier-ignore -->
-  <h1>{{ name }}<span v-if="isDeprecated" title="This component is deprecated and will be removed with the next major release."> 🚫</span></h1>
+  <h1>{{ name }}<span
+      v-if="getFlag('isDeprecated')"
+      title="This component is deprecated and will be removed with the next major release."
+    > 🚫 </span
+    ><span
+      v-else-if="getFlag('isExperimental')"
+      title="This component is experimental and might change in the future."
+    > 🧪</span>
+  </h1>
 </template>
 
 <script lang="ts">
@@ -9,15 +18,15 @@
   import Component from 'vue-class-component';
   import { Prop } from 'vue-property-decorator';
   import { paramCase } from 'change-case';
-  import { getComponentMeta } from '@porsche-design-system/component-meta';
+  import { ComponentMeta, getComponentMeta } from '@porsche-design-system/component-meta';
   import type { TagName } from '@porsche-design-system/shared';
 
   @Component
   export default class ComponentHeading extends Vue {
     @Prop({ default: '' }) public name!: string;
 
-    get isDeprecated(): boolean {
-      return getComponentMeta(`p-${paramCase(this.name)}` as TagName).isDeprecated;
+    public getFlag(flag: Extract<keyof ComponentMeta, 'isDeprecated' | 'isExperimental'>): boolean | undefined {
+      return getComponentMeta(`p-${paramCase(this.name)}` as TagName)[flag];
     }
   }
 </script>
