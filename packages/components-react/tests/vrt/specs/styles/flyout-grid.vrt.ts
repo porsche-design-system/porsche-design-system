e53fd3540aa989b393b66dbd350e30b68400300a
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-01-23 16:22:34 +0100

    test(components-react/angular): adjust flyout-grid height | sas

--- packages/components-react/tests/vrt/specs/styles/flyout-grid.vrt.ts
+++ packages/components-react/tests/vrt/specs/styles/flyout-grid.vrt.ts
@@ -12,7 +12,10 @@ test.describe(style, async () => {
   ([...viewportWidths, viewportWidthXXL, viewportWidth3XL, viewportWidth4XL] as const).forEach((viewportWidth) => {
     test(`should have no visual regression for viewport ${viewportWidth}`, async ({ page }) => {
       await page.goto(`/${style}`);
-      await page.setViewportSize({ width: viewportWidth, height: 600 });
+      await page.setViewportSize({
+        width: viewportWidth,
+        height: await page.evaluate(() => document.body.clientHeight),
+      });
       await expect(page.locator('#app')).toHaveScreenshot(`${style}-${viewportWidth}.png`);
     });
   });
