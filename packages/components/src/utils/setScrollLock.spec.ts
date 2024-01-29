758ae577324361d61782f03557c9790c855cea13
Henri Joss <henri.joss@mhp.com>
2024-01-29 14:48:47 +0100

    fix(components): revert to old scrolllock implementation | hj | #fix-scrolllock

--- packages/components/src/utils/setScrollLock.spec.ts
+++ packages/components/src/utils/setScrollLock.spec.ts
@@ -1,98 +1,57 @@
 import { setScrollLock } from './setScrollLock';
 
-describe('iOS Safari', () => {
-  it('should add correct body styles for parameter isOpen: true and !body.style.top: true', () => {
-    jest.spyOn(window, 'navigator', 'get').mockImplementation(
-      () =>
-        ({
-          userAgent:
-            'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
-        } as any)
-    );
-    document.body.style.top = '';
+it('should add correct body styles on Desktop Safari for parameter isOpen: true', () => {
+  jest.spyOn(window, 'navigator', 'get').mockImplementation(
+    () =>
+      ({
+        userAgent:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
+      }) as any
+  );
+  setScrollLock(true);
 
-    expect(document.body.getAttribute('style')).toBe(null);
-    setScrollLock(true);
-
-    expect(document.body.style.top).toBe('0px');
-    expect(document.body.style.overflowY).toBe('scroll');
-    expect(document.body.style.position).toBe('fixed');
-  });
-
-  it('should only remove previously added body styles on iOS Safari for parameter isOpen: false', () => {
-    jest.spyOn(window, 'navigator', 'get').mockImplementation(
-      () =>
-        ({
-          userAgent:
-            'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
-        } as any)
-    );
-
-    document.body.style.display = 'flex';
-    setScrollLock(true);
-    setScrollLock(false);
-
-    expect(document.body.style.top).toBe('');
-    expect(document.body.style.overflowY).toBe('');
-    expect(document.body.style.position).toBe('');
-    expect(document.body.getAttribute('style')).toBe('display: flex;');
-  });
+  expect(document.body.style.overflow).toBe('hidden');
 });
 
-describe('Desktop Browser', () => {
-  it('should add correct body styles on Desktop Safari for parameter isOpen: true', () => {
-    jest.spyOn(window, 'navigator', 'get').mockImplementation(
-      () =>
-        ({
-          userAgent:
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
-        } as any)
-    );
-    setScrollLock(true);
+it('should add correct body styles on Desktop Chrome for parameter isOpen: true', () => {
+  jest.spyOn(window, 'navigator', 'get').mockImplementation(
+    () =>
+      ({
+        userAgent:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+      }) as any
+  );
+  setScrollLock(true);
 
-    expect(document.body.style.overflow).toBe('hidden');
-  });
+  expect(document.body.style.overflow).toBe('hidden');
+});
 
-  it('should add correct body styles on Desktop Chrome for parameter isOpen: true', () => {
-    jest.spyOn(window, 'navigator', 'get').mockImplementation(
-      () =>
-        ({
-          userAgent:
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-        } as any)
-    );
-    setScrollLock(true);
+it('should add correct body styles on Desktop Firefox for parameter isOpen: true', () => {
+  jest.spyOn(window, 'navigator', 'get').mockImplementation(
+    () =>
+      ({
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0',
+      }) as any
+  );
+  setScrollLock(true);
 
-    expect(document.body.style.overflow).toBe('hidden');
-  });
+  expect(document.body.style.overflow).toBe('hidden');
+});
 
-  it('should add correct body styles on Desktop Firefox for parameter isOpen: true', () => {
-    jest.spyOn(window, 'navigator', 'get').mockImplementation(
-      () =>
-        ({
-          userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0',
-        } as any)
-    );
-    setScrollLock(true);
+it('should only remove previously added body styles on Desktop Safari for parameter isOpen: false', () => {
+  jest.spyOn(window, 'navigator', 'get').mockImplementation(
+    () =>
+      ({
+        userAgent:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
+      }) as any
+  );
 
-    expect(document.body.style.overflow).toBe('hidden');
-  });
+  document.body.style.display = 'flex';
+  document.body.style.top = '0px';
+  setScrollLock(true);
+  setScrollLock(false);
 
-  it('should only remove previously added body styles on Desktop Safari for parameter isOpen: false', () => {
-    jest.spyOn(window, 'navigator', 'get').mockImplementation(
-      () =>
-        ({
-          userAgent:
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
-        } as any)
-    );
-
-    document.body.style.display = 'flex';
-    document.body.style.top = '0px';
-    setScrollLock(true);
-    setScrollLock(false);
-
-    expect(document.body.style.overflow).toBe('');
-    expect(document.body.getAttribute('style')).toBe('display: flex; top: 0px;');
-  });
+  expect(document.body.style.overflow).toBe('');
+  expect(document.body.getAttribute('style')).toBe('display: flex; top: 0px;');
 });
--- packages/components/src/utils/setScrollLock.ts
+++ packages/components/src/utils/setScrollLock.ts
@@ -1,3 +1,9 @@
 export const setScrollLock = (isOpen: boolean): void => {
+  // This doesn't work reliably in iOS Safari with collapsed address bar where body is still scrollable
+  // there are libraries like `body-scroll-lock` and `body-scroll-lock-upgrade` which advertise to handle it, but they
+  // rely on event manipulation and also don't work reliable
+  // https://github.com/willmcpo/body-scroll-lock/blob/master/src/bodyScrollLock.js
+  // https://github.com/rick-liruixin/body-scroll-lock-upgrade/blob/develop/src/body-scroll-lock.ts
+  // Since solutions where the html and body nodes are manipulated can cause all kinds of other problems, we keep this simple solution for now
   document.body.style.overflow = isOpen ? 'hidden' : '';
 };
