72733c9470f924e059592de74400438defb36bfb
Henri Joss <henri.joss@mhp.com>
2024-02-05 11:38:48 +0100

    feat(storefront): add playground checkbox | hj | #2921

---
+++ packages/storefront/src/components/PlaygroundCheckbox.vue
@@ -0,0 +1,37 @@
+<template>
+  <label>
+    <input type="checkbox" :checked="checked" @change="$emit('change', $event)" />
+    <p-text :theme="theme">{{ name }}</p-text>
+  </label>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import { Prop } from 'vue-property-decorator';
+  import type { PlaygroundTheme } from '../models';
+
+  @Component
+  export default class PlaygroundCheckbox extends Vue {
+    @Prop({ default: 'prop' }) public name!: string;
+    @Prop({ default: false }) public checked!: boolean;
+
+    get theme(): PlaygroundTheme {
+      return this.$store.getters.playgroundTheme;
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @use '@porsche-design-system/components-js/styles' as *;
+
+  p-text {
+    display: inline-block;
+  }
+
+  input {
+    font: inherit;
+    padding: 2px 4px;
+    margin-inline-end: $pds-spacing-fluid-x-small;
+  }
+</style>
