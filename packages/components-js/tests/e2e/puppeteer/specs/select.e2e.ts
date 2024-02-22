c84b5b5aad2289a30dd29ed4af094aa09a3364ce
Henri Joss <henri.joss@mhp.com>
2024-02-22 10:43:22 +0100

    test(components-js): adjust lifecycle test | hj | #2921

--- packages/components-js/tests/e2e/puppeteer/specs/select.e2e.ts
+++ packages/components-js/tests/e2e/puppeteer/specs/select.e2e.ts
@@ -1402,8 +1402,8 @@ describe('lifecycle', () => {
 
     const status2 = await getLifecycleStatus(page);
     expect(status2.componentDidUpdate['p-select-option'], 'componentDidUpdate: p-select-option').toBe(2);
-    expect(status2.componentDidUpdate['p-select'], 'componentDidUpdate: p-select').toBe(2);
-    expect(status2.componentDidUpdate.all, 'componentDidUpdate: all').toBe(4);
+    expect(status2.componentDidUpdate['p-select'], 'componentDidUpdate: p-select').toBe(3); // Keyboard actions cause update in order to update sr highlighted option text
+    expect(status2.componentDidUpdate.all, 'componentDidUpdate: all').toBe(5);
   });
 });
 
