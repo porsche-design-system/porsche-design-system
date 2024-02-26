2f0697fc2e9ce433ecbf28278e8c0eefac8b95fb
Bernd Hacker <hacker.bernd@gmail.com>
2024-02-26 13:49:28 +0100

    feat(components-js/partials): extend getInitialStyles partial for mobile safari's input type="date" and type="time" | bh | #3043

--- packages/components-js/projects/partials/tests/unit/specs/getInitialStyles.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getInitialStyles.spec.ts
@@ -52,16 +52,16 @@ describe('format: jsx', () => {
 describe('format: sha256', () => {
   it('should return hash for core styles', async () => {
     const result = getInitialStyles({ format: 'sha256' });
-    expect(result).toMatchInlineSnapshot(`"'sha256-XBDzTbtSMmUmNgWQq4IZiPE9vZ3tUfihakhHws/xW7g='"`);
+    expect(result).toMatchInlineSnapshot(`"'sha256-y2YMQgeKfL+3E8tFMbeGP+pUbPbUzW0apNY6SOFMFI4='"`);
   });
 
   it('should return hash for core styles with custom prefix', async () => {
     const result = getInitialStyles({ format: 'sha256', prefix: 'custom-prefix' });
-    expect(result).toMatchInlineSnapshot(`"'sha256-o6AyOFxaQ3E++499cFOp5eEW6HX2AK9ZhiZyTLEKO6w='"`);
+    expect(result).toMatchInlineSnapshot(`"'sha256-Ub4Oay9kcTQFd5xWjnkuOmDm8/09hT+3HE7mns2Qsm0='"`);
   });
 
   it('should return hash for core styles with multiple custom prefixes', async () => {
     const result = getInitialStyles({ format: 'sha256', prefix: ['', 'some-prefix', 'another-prefix'] });
-    expect(result).toMatchInlineSnapshot(`"'sha256-X8fDsvbBAXDPNcdOUTAJXO+fDFZetx9v/Aeus5GzFlI='"`);
+    expect(result).toMatchInlineSnapshot(`"'sha256-mP7H9zo3B1DnyiHb+CxYWq41aXq2TAkU+0TGH6rIj2g='"`);
   });
 });
