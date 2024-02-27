1d56096d805372f770079ba65153b4d0ab25b563
Bernd Hacker <hacker.bernd@gmail.com>
2024-02-27 14:44:42 +0100

    feat(components): extend getSlottedTextFieldTextareaSelectStyles by mobile safari media query to have native placeholder coloring for input type="date" and type="time" | bh | #3043

--- packages/components/src/styles/form-styles.ts
+++ packages/components/src/styles/form-styles.ts
@@ -50,10 +50,13 @@ export const getSlottedTextFieldTextareaSelectStyles = (
       boxSizing: 'border-box',
       border: `${borderWidthBase} solid ${formStateColor || contrastMediumColor}`,
       borderRadius: borderRadiusSmall,
-      background: 'transparent',
       font: textSmallStyle.font.replace('ex', 'ex + 6px'), // a minimum line-height is needed for input, otherwise value is scrollable in Chrome, +6px is aligned with how Safari visualize date/time input highlighting
       textIndent: 0,
-      color: primaryColor,
+      // conditional hopefully for mobile safari, only
+      '@supports (-webkit-touch-callout: none)': {
+        background: 'transparent',
+        color: primaryColor,
+      },
       transition: `${getTransition('background-color')}, ${getTransition('border-color')}, ${getTransition('color')}`, // for smooth transitions between e.g. disabled states
       ...prefersColorSchemeDarkMediaQuery(theme, {
         borderColor: formStateColorDark || contrastMediumColorDark,
