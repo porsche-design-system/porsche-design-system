c288df5ebe306b66378d94ba50c2a540750b9353
Henri Joss <henri.joss@mhp.com>
2024-02-29 15:04:02 +0100

    refactor(components-js): use const fn & check browserName with includes | hj | #e2e-playwright-migration

--- packages/components-js/tests/e2e/playwright/helpers/skipInBrowser.ts
+++ packages/components-js/tests/e2e/playwright/helpers/skipInBrowser.ts
@@ -1,19 +1,13 @@
 import { PlaywrightWorkerOptions } from 'playwright/types/test';
 import { test } from '@playwright/test';
 
-function insertSkipper(browserNames: PlaywrightWorkerOptions['browserName'][]) {
-  for (const ignoredBrowser of browserNames) {
-    test.skip(({ browserName }) => ignoredBrowser === browserName);
-  }
-}
+const insertSkipper = (browserNames: PlaywrightWorkerOptions['browserName'][]) =>
+  test.skip(({ browserName }) => browserNames.includes(browserName));
 
-export function skipInBrowser(browserNames: PlaywrightWorkerOptions['browserName'][], testFunction?: () => void) {
-  if (!testFunction) {
-    insertSkipper(browserNames);
-  } else {
-    test.describe(`skip in ${browserNames.join(', ')}`, () => {
-      insertSkipper(browserNames);
-      testFunction();
-    });
-  }
-}
+export const skipInBrowser = (browserNames: PlaywrightWorkerOptions['browserName'][], testFunction?: () => void) =>
+  !testFunction
+    ? insertSkipper(browserNames)
+    : test.describe(`skip in ${browserNames.join(', ')}`, () => {
+        insertSkipper(browserNames);
+        testFunction();
+      });
