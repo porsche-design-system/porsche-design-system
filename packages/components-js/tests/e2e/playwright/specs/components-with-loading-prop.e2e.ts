55fc4ea34b961d94ed1b17ebd48339a94af72bb4
Michael Worm <michael.worm@mhp.com>
2024-02-29 11:19:09 +0100

    test(components-js): Migrate components-with-loading-prop test to playwright | mw | #3071

---
+++ packages/components-js/tests/e2e/playwright/specs/components-with-loading-prop.e2e.ts
@@ -0,0 +1,87 @@
+import type { ElementHandle, Page } from 'playwright';
+import { expect, test } from '@playwright/test';
+import { TAG_NAMES, type TagName } from '@porsche-design-system/shared';
+import { getComponentMeta } from '@porsche-design-system/component-meta';
+import {
+  buildDefaultComponentMarkup,
+  selectNode,
+  setContentWithDesignSystem,
+  setProperty,
+  waitForStencilLifecycle,
+} from '../helpers';
+
+const tagNamesWithLoadingProp: TagName[] = TAG_NAMES.filter((tagName) => getComponentMeta(tagName).propsMeta?.loading);
+
+for (const tagName of tagNamesWithLoadingProp) {
+  test.describe(tagName, () => {
+    const getLoadingStatus = async (page: Page): Promise<ElementHandle> => {
+      const [nestedComponentWithLoadingProp] = tagNamesWithLoadingProp.filter((tagNameWithLoadingProp) =>
+        getComponentMeta(tagName).nestedComponents?.includes(tagNameWithLoadingProp)
+      );
+
+      return await selectNode(
+        page,
+        nestedComponentWithLoadingProp
+          ? `${tagName} >>> ${nestedComponentWithLoadingProp} >>> .loading` // e.g. for p-button-tile
+          : `${tagName} >>> .loading`
+      );
+    };
+
+    const getLoadingMessage = async (page: Page): Promise<string> => {
+      return (await getLoadingStatus(page)).evaluate((el) => el.textContent);
+    };
+
+    test.describe('for loading="true"', () => {
+      const markup = buildDefaultComponentMarkup(tagName).replace(/>/, ' loading="true">'); // add loading attribute
+
+      test('should render loading message initially', async ({ page }) => {
+        await setContentWithDesignSystem(page, markup);
+
+        expect(await getLoadingMessage(page)).toBe('Loading');
+      });
+
+      test('should render loading finished message when loading is set to false', async ({ page }) => {
+        await setContentWithDesignSystem(page, markup);
+
+        const host = await selectNode(page, tagName);
+        await setProperty(host, 'loading', false);
+        await waitForStencilLifecycle(page);
+
+        expect(await getLoadingMessage(page)).toBe('Loading finished');
+      });
+    });
+
+    test.describe('for loading="false"', () => {
+      const markup = buildDefaultComponentMarkup(tagName);
+
+      test('should render no loading message initially', async ({ page }) => {
+        await setContentWithDesignSystem(page, markup);
+
+        expect(await getLoadingMessage(page)).toBe('');
+      });
+
+      test('should render loading message when loading is set to true', async ({ page }) => {
+        await setContentWithDesignSystem(page, markup);
+
+        const host = await selectNode(page, tagName);
+        await setProperty(host, 'loading', true);
+        await waitForStencilLifecycle(page);
+
+        expect(await getLoadingMessage(page)).toBe('Loading');
+      });
+
+      test('should render loading finished message when loading is set to true, then to false', async ({ page }) => {
+        await setContentWithDesignSystem(page, markup);
+
+        const host = await selectNode(page, tagName);
+        await setProperty(host, 'loading', true);
+        await waitForStencilLifecycle(page);
+
+        await setProperty(host, 'loading', false);
+        await waitForStencilLifecycle(page);
+
+        expect(await getLoadingMessage(page)).toBe('Loading finished');
+      });
+    });
+  });
+}
