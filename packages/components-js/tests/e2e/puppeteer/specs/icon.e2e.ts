f3900500676b804a2a109c7063b038c1d0915001
Michael Worm <michael.worm@mhp.com>
2024-03-01 13:43:31 +0100

    test(components-js): Migrate icon test to playwright | mw | #3071

--- packages/components-js/tests/e2e/puppeteer/specs/icon.e2e.ts
+++ packages/components-js/tests/e2e/puppeteer/specs/icon.e2e.ts
@@ -1,11 +1,4 @@
-import {
-  expectA11yToMatchSnapshot,
-  getLifecycleStatus,
-  selectNode,
-  setContentWithDesignSystem,
-  setProperty,
-  waitForStencilLifecycle,
-} from '../helpers';
+import { expectA11yToMatchSnapshot, selectNode, setContentWithDesignSystem } from '../helpers';
 import type { Page } from 'puppeteer';
 import type { IconName } from '@porsche-design-system/components/dist/types/bundle';
 
@@ -16,8 +9,6 @@ afterEach(async () => await page.close());
 type InitOptions = {
   name?: IconName;
 };
-
-const initOptions: InitOptions[] = [{}];
 
 const initIcon = async (opts?: InitOptions): Promise<void> => {
   const { name } = opts || {};
@@ -30,36 +21,7 @@ const initIcon = async (opts?: InitOptions): Promise<void> => {
   await setContentWithDesignSystem(page, content);
 };
 
-const getHost = async () => selectNode(page, 'p-icon');
 const getIconImg = async () => selectNode(page, 'p-icon >>> img');
-
-describe('lifecycle', () => {
-  initOptions.forEach((opts) => {
-    it('should work without unnecessary round trips on init', async () => {
-      await initIcon(opts);
-      const status = await getLifecycleStatus(page);
-
-      expect(status.componentDidLoad['p-icon'], 'componentDidLoad: p-icon').toBe(1);
-
-      expect(status.componentDidLoad.all, 'componentDidLoad: all').toBe(1);
-      expect(status.componentDidUpdate.all, 'componentDidUpdate: all').toBe(0);
-    });
-
-    it('should work without unnecessary round trips after state change', async () => {
-      await initIcon({ ...opts, name: 'highway' });
-      const host = await getHost();
-
-      await setProperty(host, 'name', 'car');
-      await waitForStencilLifecycle(page);
-      const status = await getLifecycleStatus(page);
-
-      expect(status.componentDidUpdate['p-icon'], 'componentDidUpdate: p-icon').toBe(1);
-
-      expect(status.componentDidLoad.all, 'componentDidLoad: all').toBe(1);
-      expect(status.componentDidUpdate.all, 'componentDidUpdate: all').toBe(1);
-    });
-  });
-});
 
 describe('accessibility', () => {
   it('should expose correct initial accessibility tree properties', async () => {
