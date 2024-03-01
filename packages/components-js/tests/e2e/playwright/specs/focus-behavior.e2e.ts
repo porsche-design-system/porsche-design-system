0886ffb0f6765b4dcb8de0aa3721f6b5c2f463f3
Michael Worm <michael.worm@mhp.com>
2024-03-01 13:32:51 +0100

    test(components-js): Migrate focus behaviour test to playwright | mw | #3071

--- packages/components-js/tests/e2e/playwright/specs/focus-behavior.e2e.ts
+++ packages/components-js/tests/e2e/playwright/specs/focus-behavior.e2e.ts
@@ -1,16 +1,14 @@
+import { expect, test } from '@playwright/test';
 import { TAG_NAMES } from '@porsche-design-system/shared';
 import { getComponentMeta } from '@porsche-design-system/component-meta';
 import {
   expectToSkipFocusOnComponent,
   getActiveElementTagName,
-  selectNode,
   setContentWithDesignSystem,
+  skipInBrowser,
 } from '../helpers';
-import type { Page } from 'puppeteer';
 
-let page: Page;
-beforeEach(async () => (page = await browser.newPage()));
-afterEach(async () => await page.close());
+skipInBrowser(['webkit', 'firefox']);
 
 TAG_NAMES.filter((tagName) => getComponentMeta(tagName).isDelegatingFocus).forEach((tagName) => {
   const href =
@@ -25,7 +23,7 @@ TAG_NAMES.filter((tagName) => getComponentMeta(tagName).isDelegatingFocus).forEa
     return requiredParent ? `<${requiredParent}>${child}</${requiredParent}>` : child;
   };
 
-  it(`should be removed from tab order for ${tagName}`, async () => {
+  test(`should be removed from tab order for ${tagName}`, async ({ page }) => {
     const component = wrapInRequiredParentIfNeeded(`<${tagName}${href}${state} tabindex="-1">Some label</${tagName}>`);
     await setContentWithDesignSystem(
       page,
@@ -34,19 +32,19 @@ ${component}
 <a href="#" id="after">after</a>`
     );
 
-    const host = await selectNode(page, tagName);
-    const before = await selectNode(page, '#before');
+    const host = await page.$(tagName);
+    const before = await page.$('#before');
 
     await expectToSkipFocusOnComponent(page, host, before);
   });
 
-  it(`should delegate focus into shadow dom for ${tagName}`, async () => {
+  test(`should delegate focus into shadow dom for ${tagName}`, async ({ page }) => {
     await setContentWithDesignSystem(
       page,
       wrapInRequiredParentIfNeeded(`<${tagName}${href}${state}${value}>Some label</${tagName}>`)
     );
 
-    const host = await selectNode(page, tagName);
+    const host = await page.$(tagName);
     const elTagName = await host.evaluate((el) => el.tagName);
 
     await page.keyboard.press('Tab');
