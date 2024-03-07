545b5f3d586f87466904b491959a14c1684c4677
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-03-07 18:44:02 +0100

    test(storefront): disable sitemap e2e test/build | sas

--- packages/storefront/tests/e2e/specs/sitemap.e2e.ts
+++ packages/storefront/tests/e2e/specs/sitemap.e2e.ts
@@ -1,7 +1,8 @@
 import { test } from '@playwright/test';
 import { buildSitemap, getSitemap } from '../helpers';
 
-test('should have complete sitemap.json', async ({ page }) => {
+// TODO: for unknown reasons the sitemap generation test got super slow
+test.fixme('should have complete sitemap.json', async ({ page }) => {
   test.setTimeout(2147483647);
 
   const oldUrls = getSitemap(); // reads fixture/sitemap.json
