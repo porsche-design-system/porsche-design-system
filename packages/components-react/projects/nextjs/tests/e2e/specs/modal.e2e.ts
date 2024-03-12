02fe3b7f28b7571d7b4c2c61a226856ad8ae8648
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-03-12 12:25:04 +0100

    test(components-react/nextjs): improve modal e2e test | sas

--- packages/components-react/projects/nextjs/tests/e2e/specs/modal.e2e.ts
+++ packages/components-react/projects/nextjs/tests/e2e/specs/modal.e2e.ts
@@ -1,5 +1,6 @@
 import { type Page, test, expect } from '@playwright/test';
 import { goto, getAttribute } from '../helpers';
+import { viewportWidthM } from '@porsche-design-system/shared/testing/playwright.vrt.config';
 
 const pageUrl = '/modal-standalone';
 const getOpenModalBtn = (page: Page) => page.getByRole('button', { name: 'Open Modal (without route change)' }); // selectNode(page, 'p-button >>> button');
@@ -7,6 +8,11 @@ const getLinkToModal = (page: Page) => page.getByRole('link', { name: 'Link to M
 const getDismissButton = (page: Page) => page.getByRole('button', { name: 'Dismiss modal' }); // selectNode(page, 'p-modal >>> p-button-pure >>> button');
 const getRootElement = (page: Page) => page.$('html');
 const getBodyStyle = async (page: Page) => getAttribute(await page.$('body'), 'style');
+
+test.use({
+  // ensures expected scroll positions are the same
+  viewport: { width: viewportWidthM, height: 600 },
+});
 
 test('should keep same scroll position when modal is opened and closed with route change', async ({ page }) => {
   await goto(page, pageUrl);
@@ -17,20 +23,20 @@ test('should keep same scroll position when modal is opened and closed with rout
   expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(0);
 
   await linkToModal.scrollIntoViewIfNeeded();
-  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(412);
+  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(532);
 
   await linkToModal.click();
-  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(412);
+  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(532);
 
   await page.waitForLoadState();
   const dismissButton = await getDismissButton(page);
 
-  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(412);
+  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(532);
 
   await dismissButton.click();
 
   await page.waitForFunction(() => !document.querySelector('p-modal'));
-  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(412);
+  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(532);
 });
 
 test('should keep same scroll position when modal is opened and closed', async ({ page }) => {
@@ -42,15 +48,15 @@ test('should keep same scroll position when modal is opened and closed', async (
   expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(0);
 
   await openModalBtn.scrollIntoViewIfNeeded();
-  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(412);
+  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(532);
 
   await openModalBtn.click();
-  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(412);
+  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(532);
 
   await page.waitForLoadState();
   const dismissButton = await getDismissButton(page);
   await dismissButton.click();
 
   expect(await getBodyStyle(page)).toBe('');
-  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(412);
+  expect(await rootElement.evaluate((el) => el.scrollTop)).toBe(532);
 });
