2c43d26fce1ca27cec005645260fdd3cc749c926
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-03-13 18:06:55 +0100

    test(components-js): refactor page.$ | sas

--- packages/components-js/tests/vrt/helpers/get-value-of-for-attribute.ts
+++ packages/components-js/tests/vrt/helpers/get-value-of-for-attribute.ts
@@ -1,6 +1,5 @@
-import type { Page } from '@playwright/test';
-import { selectNode } from '../../a11y/helpers';
+import { type Page } from '@playwright/test';
 
 export const getValueOfForAttribute = async (page: Page, selector: string) => {
-  return await (await selectNode(page, selector)).evaluate((label) => label.getAttribute('for'));
+  return await (await page.$(selector)).evaluate((label) => label.getAttribute('for'));
 };
