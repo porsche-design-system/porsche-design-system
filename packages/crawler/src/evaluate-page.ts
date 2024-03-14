1ce2738389e90630d729f6c2fd60b18904b6bb9f
Michael Worm <michael.worm@mhp.com>
2024-03-14 16:59:55 +0100

    feat(crawler): Fixed some bugs in crawler | mw

--- packages/crawler/src/evaluate-page.ts
+++ packages/crawler/src/evaluate-page.ts
@@ -22,51 +22,35 @@ declare global {
 
 const pdsTagNamesWithPropertyNames = getPdsTagNamesWithPropertyNames();
 
-const getPdsComponentNameByPrefix = (el: Element, prefix: string): TagName | null => {
-  const tagName = el.tagName.toLowerCase();
+const getPdsComponentNameByPrefix = async (locator: Locator, prefix: string): Promise<TagName | null> => {
+  const tagName = await locator.evaluate((el) => el.tagName.toLowerCase());
   const tagNames = Object.keys(pdsTagNamesWithPropertyNames);
 
   return (tagNames.find((compName) => (prefix ? `${prefix}-${compName}` : compName) === tagName) as TagName) || null;
 };
 
-const parseIfJSON = (jsonStr: string | number | symbol): object | string | number | symbol => {
-  if (typeof jsonStr === 'string') {
-    try {
-      // jsonStr is potentially JSON parsable string, e.g. "{ base: 'block', l: 'inline' }" or "true" or "123"
-      return JSON.parse(
-        jsonStr
-          .replace(/'/g, '"') // convert single quotes to double quotes
-          .replace(/[\s"]?([a-z\-]+)[\s"]?:([^//])/g, '"$1":$2') // wrap keys in double quotes if they don't have them but ignore potential urls
-      );
-    } catch {
-      // jsonStr is string, e.g. "block" or "inline"
-      return jsonStr;
-    }
-  } else {
-    // jsonStr is object, e.g. { base: 'block', l: 'inline' } or number, e.g. 123 or boolean, e.g. true
-    return jsonStr;
-  }
-};
+const getHighestLevelChildrenOrTextContent = (locator: Locator): Promise<string | null> =>
+  locator.evaluate(
+    (el) =>
+      Array.from(el.children).reduce((result, child) => {
+        if (child.children.length) {
+          const copy = child.cloneNode(true) as Element;
 
-const getHighestLevelChildrenOrTextContent = (el: Element): string | null =>
-  Array.from(el.children).reduce((result, child) => {
-    if (child.children.length) {
-      const copy = child.cloneNode(true) as Element;
+          // we save only the highest dom level in the reports, in order not to make them too big
+          copy.innerHTML = '(the content was stripped)';
 
-      // we save only the highest dom level in the reports, in order not to make them too big
-      copy.innerHTML = '(the content was stripped)';
+          return result + copy.outerHTML;
+        }
 
-      return result + copy.outerHTML;
-    }
+        return result + child.outerHTML;
+      }, '' as string) || el.textContent
+  );
 
-    return result + child.outerHTML;
-  }, '' as string) || el.textContent;
-
-const getHostPdsComponent = (el: Element, prefix: string): TagName | null => {
-  const rootNode = (el.getRootNode() as ShadowRoot).host;
+const getHostPdsComponent = async (locator: Locator, prefix: string): Promise<TagName | null> => {
+  const rootNode = await locator.evaluate((el) => (el.getRootNode() as ShadowRoot).host);
 
   if (rootNode) {
-    return getPdsComponentNameByPrefix(rootNode, prefix);
+    return getPdsComponentNameByPrefix(locator, prefix);
   } else {
     return null;
   }
@@ -78,65 +62,90 @@ const getAllConsumedProperties = <
   PComponentPropertyName extends keyof PComponentElement,
   PComponentPropertyValue extends keyof PComponentElement[PComponentPropertyName],
 >(
-  el: Element,
+  locator: Locator,
   allPdsPropertiesForComponentName: string[]
-): Properties => {
+): Promise<Properties> => {
   // we consider this element as PDS Component, therefore we need this alias here
-  const pEl = el as PComponentElement;
+  return locator.evaluate((el, allPdsPropertiesForComponentName) => {
+    const pEl = el as PComponentElement;
 
-  const stringifyIfObject = (val: PComponentPropertyValue): PComponentPropertyValue => {
-    const parsedVal = parseIfJSON(val);
-    // check if it's an object and stringify
-    return typeof parsedVal === 'object'
-      ? (JSON.stringify(parsedVal) as PComponentPropertyValue)
-      : (parsedVal as PComponentPropertyValue);
-  };
+    return allPdsPropertiesForComponentName.reduce((result, propName) => {
+      const propValue = pEl[propName as PComponentPropertyName] as PComponentPropertyValue;
 
-  return allPdsPropertiesForComponentName.reduce((result, propName) => {
-    const propValue = pEl[propName as PComponentPropertyName] as PComponentPropertyValue;
-    return {
-      ...result,
-      [propName]: stringifyIfObject(propValue),
-    };
-  }, {});
+      let parsedVal = propValue;
+
+      if (typeof propValue === 'string') {
+        try {
+          // jsonStr is potentially JSON parsable string, e.g. "{ base: 'block', l: 'inline' }" or "true" or "123"
+          parsedVal = JSON.parse(
+            propValue
+              .replace(/'/g, '"') // convert single quotes to double quotes
+              .replace(/[\s"]?([a-z\-]+)[\s"]?:([^//])/g, '"$1":$2') // wrap keys in double quotes if they don't have them but ignore potential urls
+          );
+        } catch {
+          // jsonStr is string, e.g. "block" or "inline"
+        }
+      } else {
+        // jsonStr is object, e.g. { base: 'block', l: 'inline' } or number, e.g. 123 or boolean, e.g. true
+      }
+
+      return {
+        ...result,
+        // check if it's an object and stringify
+        [propName]:
+          typeof parsedVal === 'object'
+            ? (JSON.stringify(parsedVal) as PComponentPropertyValue)
+            : (parsedVal as PComponentPropertyValue),
+      };
+    }, {});
+  }, allPdsPropertiesForComponentName);
 };
 
 const getConsumedTagNamesForPrefix = async (prefix: string, pdsElements: Locator[]): Promise<TagNameData[]> => {
-  return await Promise.all(
-    pdsElements.map(async (locator) => {
-      const el = await locator.evaluate((el) => el);
+  const tagNameDatas: TagNameData[] = [];
 
-      const componentName = getPdsComponentNameByPrefix(el, prefix);
+  for (const pdsElement of pdsElements) {
+    const componentName = await getPdsComponentNameByPrefix(pdsElement, prefix);
 
-      if (!componentName) {
-        throw new Error('Could not find component name');
-      }
+    if (!componentName) {
+      throw new Error('Could not find component name');
+    }
 
-      const slotInfo = getHighestLevelChildrenOrTextContent(el);
-      const hostPdsComponent = getHostPdsComponent(el, prefix);
+    const slotInfo = await getHighestLevelChildrenOrTextContent(pdsElement);
+    const hostPdsComponent = await getHostPdsComponent(pdsElement, prefix);
 
-      return {
-        [componentName]: {
-          properties: getAllConsumedProperties(el, pdsTagNamesWithPropertyNames[componentName]),
-          children: slotInfo ? slotInfo : null,
-          hostPdsComponent: hostPdsComponent ? hostPdsComponent : null,
-        },
-      } as TagNameData;
-    })
-  );
+    // TODO: Check if that is correct because it was not needed before
+    if (!hostPdsComponent) {
+      continue;
+    }
+
+    tagNameDatas.push({
+      [componentName]: {
+        properties: await getAllConsumedProperties(pdsElement, pdsTagNamesWithPropertyNames[componentName]),
+        children: slotInfo ? slotInfo : null,
+        hostPdsComponent: hostPdsComponent ? hostPdsComponent : null,
+      },
+    } as TagNameData);
+  }
+
+  return tagNameDatas;
 };
 
 // get all dom elements from body
 const getAllPdsElementsForPrefix = async (allDOMElements: Locator[], prefix: string): Promise<Locator[]> => {
   const tagNames = Object.keys(pdsTagNamesWithPropertyNames);
+  const selector = (prefix ? tagNames.map((tagName) => `${prefix}-${tagName}`) : tagNames).join();
+  const matches = [];
 
-  return await Promise.all(
-    allDOMElements.filter(async (locator: Locator) => {
-      const selector = (prefix ? tagNames.map((tagName) => `${prefix}-${tagName}`) : tagNames).join();
+  for (const domElement of allDOMElements) {
+    const result = await domElement.evaluate((el, selector) => el.matches(selector), selector);
 
-      return await locator.evaluate((el, selector) => el.matches(selector), selector);
-    })
-  );
+    if (result) {
+      matches.push(domElement);
+    }
+  }
+
+  return matches;
 };
 
 export const evaluatePage = async (page: Page): Promise<ConsumedTagNamesForVersionsAndPrefixes> => {
