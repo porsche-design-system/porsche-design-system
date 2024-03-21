d263d34ca5365a7b83401484e5c78388dd3797ca
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-03-21 14:44:10 +0100

    test: re-use shared e2e config | sas

--- packages/shared/src/testing/playwright.e2e.ts
+++ packages/shared/src/testing/playwright.e2e.ts
@@ -16,6 +16,15 @@ export const config: Config = {
     timeout: 5000,
   },
   snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}{ext}',
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'off', // 'on-first-retry' causes CI job to get stuck
+    viewport: null,
+  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
