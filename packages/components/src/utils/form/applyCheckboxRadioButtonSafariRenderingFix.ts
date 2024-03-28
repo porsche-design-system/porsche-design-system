e563427f39116d41dabcf387db0242b8ab51b4f2
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-03-28 13:31:35 +0100

    test(components): fix ts errors | sas | #3126

--- packages/components/src/utils/form/applyCheckboxRadioButtonSafariRenderingFix.ts
+++ packages/components/src/utils/form/applyCheckboxRadioButtonSafariRenderingFix.ts
@@ -35,6 +35,8 @@ export const applyCheckboxRadioButtonSafariRenderingFix = (element: HTMLElement)
       const sheet = new CSSStyleSheet();
       sheet.replaceSync(`${element.tagName.toLowerCase()}>input:checked{transform:rotateZ(0)}`);
       // TODO: for some reason unit test in Docker environment throws TS2339: Property 'push' does not exist on type 'readonly CSSStyleSheet[]'
+      /* eslint-disable @typescript-eslint/prefer-ts-expect-error, @typescript-eslint/ban-ts-comment */
+      // @ts-ignore
       documentOrShadowRoot.adoptedStyleSheets?.push(sheet);
     }
   }
