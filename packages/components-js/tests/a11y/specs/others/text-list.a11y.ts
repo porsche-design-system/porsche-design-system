ffa134ac10aace60406a72722d57cf32e000724b
Oliver Hook <oliver.hook@porsche.digital>
2024-04-16 12:26:51 +0200

    test(components-js): restructured other a11y tests | oh | #1919

---
+++ packages/components-js/tests/a11y/specs/others/text-list.a11y.ts
@@ -0,0 +1,28 @@
+import { setContentWithDesignSystem } from '../../helpers';
+import { type Page, test, expect } from '@playwright/test';
+
+const initTextList = (page: Page): Promise<void> => {
+  return setContentWithDesignSystem(
+    page,
+    `
+    <p-text-list>
+      <p-text-list-item>The quick brown fox jumps over the lazy dog</p-text-list-item>
+      <p-text-list-item>The quick <a onclick="return false;" href="#">brown fox</a> jumps over the lazy dog</p-text-list-item>
+      <p-text-list-item>
+        The quick brown fox jumps over the lazy dog
+        <p-text-list>
+          <p-text-list-item>
+            The quick brown fox jumps over the lazy dog
+          </p-text-list-item>
+        </p-text-list>
+      </p-text-list-item>
+    </p-text-list>`
+  );
+};
+
+test.fixme('should expose correct initial accessibility tree', async ({ page }) => {
+  await initTextList(page);
+  const getList = (page: Page) => page.$('p-text-list [role="list"]');
+
+  // await expectA11yToMatchSnapshot(page, await getList(), { interestingOnly: false });
+});
