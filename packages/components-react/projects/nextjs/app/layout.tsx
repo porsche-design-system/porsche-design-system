57f2d706463d21b299f215922de7f28e60b5af0b
Henri Joss <henri.joss@mhp.com>
2024-04-17 10:30:54 +0200

    test(components-react/next-js): revert since useServerInsertedHTML is not compatible with our tests | hj | #3137

--- packages/components-react/projects/nextjs/app/layout.tsx
+++ packages/components-react/projects/nextjs/app/layout.tsx
@@ -1,50 +1,24 @@
 import NextScript from 'next/script';
 import type { PropsWithChildren } from 'react';
-import { preload } from 'react-dom';
 import { FooterPartials, HeaderPartials } from '../components';
 import { Providers } from './providers';
-import type { Metadata, Viewport } from 'next';
-import {
-  getComponentChunkLinks,
-  getFontLinks,
-  getMetaTagsAndIconLinks,
-} from '@porsche-design-system/components-react/partials';
-
-const { themeColor, appleWebApp, icons } = getMetaTagsAndIconLinks({
-  appTitle: 'Porsche Design System - NextJS',
-  format: 'js',
-});
-
-export const viewport: Viewport = {
-  themeColor,
-};
-
-export const metadata: Metadata = {
-  title: 'Porsche Design System - NextJS',
-  appleWebApp,
-  icons,
-};
 
 export default function RootLayout({ children }: PropsWithChildren<{}>): JSX.Element {
   const cdn = 'local';
-  const getCdnURL = (href: string) =>
-    cdn !== 'local' ? href : href.replace('https://cdn.ui.porsche.com/porsche-design-system', 'http://localhost:3001');
-
-  getComponentChunkLinks({ format: 'js', components: ['heading'] }).forEach(({ href, options }) =>
-    preload(getCdnURL(href), options)
-  );
-
-  getFontLinks({ format: 'js', weights: ['regular', 'semi-bold', 'bold'] }).forEach(({ href, options }) =>
-    preload(getCdnURL(href), options)
-  );
 
   return (
     <html lang="en">
+      <head>
+        <title>Porsche Design System - NextJS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="http://localhost:3001/meta-icons/favicon-32x32.d42ac2876697ec701e95e4a4d608fb0e.png" />
+
+        <HeaderPartials cdn={cdn} />
+      </head>
       <body>
         <Providers>
           <div id="app">{children}</div>
         </Providers>
-        <HeaderPartials cdn={cdn} />
         <NextScript />
         <FooterPartials cdn={cdn} />
       </body>
--- packages/components-react/projects/nextjs/components/HeaderPartials.tsx
+++ packages/components-react/projects/nextjs/components/HeaderPartials.tsx
@@ -1,33 +1,32 @@
-'use client';
-
-import { getFontFaceStylesheet, getInitialStyles } from '@porsche-design-system/components-react/partials';
+import {
+  getFontFaceStylesheet,
+  getFontLinks,
+  getInitialStyles,
+} from '@porsche-design-system/components-react/partials';
 import { getSharedStyles } from '../styles/getSharedStyles';
-import { useServerInsertedHTML } from 'next/navigation';
-import { useRef } from 'react';
 
 type Props = {
   cdn?: 'local' | 'auto' | 'cn';
 };
 
-export const HeaderPartials = ({ cdn = 'local' }: Props) => {
-  const isServerInserted = useRef(false);
-
-  useServerInsertedHTML(() => {
-    if (!isServerInserted.current) {
-      isServerInserted.current = true;
-      return (
-        <>
-          {getInitialStyles({ format: 'jsx' })}
-          {getSharedStyles()}
-          {cdn !== 'local' ? (
-            getFontFaceStylesheet({ format: 'jsx' })
-          ) : (
-            <link rel="stylesheet" href="http://localhost:3001/styles/font-face.min.css" />
-          )}
-        </>
-      );
-    }
-  });
-
-  return null;
+export const HeaderPartials = ({ cdn = 'local' }: Props): JSX.Element => {
+  return cdn !== 'local' ? (
+    <>
+      {getInitialStyles({ format: 'jsx' })}
+      {getFontFaceStylesheet({ format: 'jsx', cdn })}
+      {getFontLinks({ format: 'jsx', cdn, weights: ['regular', 'semi-bold', 'bold'] })}
+      {getSharedStyles()}
+    </>
+  ) : (
+    <>
+      {getInitialStyles({ format: 'jsx' })}
+      <link rel="stylesheet" href="http://localhost:3001/styles/font-face.min.css" />
+      {Array.from(getFontLinks({ weights: ['regular', 'semi-bold', 'bold'] }).matchAll(/https:\/\/[a-z0-9./-]+/g))
+        .map(([url]) => url.replace('https://cdn.ui.porsche.com/porsche-design-system', 'http://localhost:3001'))
+        .map((url) => (
+          <link key={url} rel="preload" href={url} as="font" type="font/woff2" crossOrigin="" />
+        ))}
+      {getSharedStyles()}
+    </>
+  );
 };
