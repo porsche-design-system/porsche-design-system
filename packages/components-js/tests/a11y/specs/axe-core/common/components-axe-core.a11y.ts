7569901f96b62152df701cb600e1928965bf7d35
Oliver Hook <oliver.hook@porsche.digital>
2024-04-17 15:34:52 +0200

    test(components-js): add axe core theme support and adapt vrt pages | oh | #1919

--- packages/components-js/tests/a11y/specs/axe-core/common/components-axe-core.a11y.ts
+++ packages/components-js/tests/a11y/specs/axe-core/common/components-axe-core.a11y.ts
@@ -2,7 +2,7 @@ import { type Page } from '@playwright/test';
 import { test, expect } from '../../../helpers';
 import { setupScenario } from '../../../../vrt/helpers';
 import { TAG_NAMES, type TagName } from '@porsche-design-system/shared';
-import { viewportWidthXXS, viewportWidthM } from '@porsche-design-system/shared/testing/playwright.vrt';
+import { viewportWidthXXS, viewportWidthM, themes } from '@porsche-design-system/shared/testing/playwright.vrt';
 import { getComponentMeta } from '@porsche-design-system/component-meta';
 
 const components = (TAG_NAMES as unknown as TagName[])
@@ -31,29 +31,53 @@ test(`should have certain amount of components`, () => {
 });
 
 components.forEach((component) => {
+  const isComponentThemeable = (component: string): boolean =>
+    getComponentMeta(`p-${component}` as TagName).isThemeable;
+
   // executed in Chrome only
   test.describe(component, async () => {
     test.skip(({ browserName }) => browserName !== 'chromium');
 
     // Accessibility: AXE-Core tests basic
-    // regular tests on 2 different viewports
+    // regular themed tests on 2 different viewports
     [viewportWidthXXS, viewportWidthM].forEach((viewportWidth) => {
-      test(`should have no accessibility regression for viewport ${viewportWidth}`, async ({
-        page,
-        makeAxeBuilder,
-      }, testInfo) => {
-        await setupScenario(page, `/${component}`, viewportWidth);
-        await revertAutoFocus(page, component);
+      if (isComponentThemeable(component)) {
+        themes.forEach((theme) => {
+          test(`should have no accessibility regression for viewport ${viewportWidth} with theme ${theme}`, async ({
+            page,
+            makeAxeBuilder,
+          }, testInfo) => {
+            await setupScenario(page, `/${component}`, viewportWidth, { forceComponentTheme: theme });
+            await revertAutoFocus(page, component);
 
-        const accessibilityScanResults = await makeAxeBuilder().analyze();
+            const accessibilityScanResults = await makeAxeBuilder().analyze();
 
-        await testInfo.attach(`a11y-scan-results-${component}-${viewportWidth}`, {
-          body: JSON.stringify(accessibilityScanResults.violations, null, 2),
-          contentType: 'application/json',
+            await testInfo.attach(`a11y-scan-results-${component}-${viewportWidth}-theme-${theme}`, {
+              body: JSON.stringify(accessibilityScanResults.violations, null, 2),
+              contentType: 'application/json',
+            });
+
+            expect(accessibilityScanResults.violations.length).toBe(0);
+          });
         });
+      } else {
+        test(`should have no accessibility regression for viewport ${viewportWidth}`, async ({
+          page,
+          makeAxeBuilder,
+        }, testInfo) => {
+          await setupScenario(page, `/${component}`, viewportWidth);
+          await revertAutoFocus(page, component);
 
-        expect(accessibilityScanResults.violations.length).toBe(0);
-      });
+          const accessibilityScanResults = await makeAxeBuilder().analyze();
+
+          await testInfo.attach(`a11y-scan-results-${component}-${viewportWidth}`, {
+            body: JSON.stringify(accessibilityScanResults.violations, null, 2),
+            contentType: 'application/json',
+          });
+
+          expect(accessibilityScanResults.violations.length).toBe(0);
+        });
+      }
     });
   });
 });
