e24a62a543790850db25e4a174bc20698c7fee94
Oliver Hook <oliver.hook@porsche.digital>
2024-04-22 08:55:16 +0200

    test(components-js): optimize vrt pages for axe-core tests | oh | #1919

--- packages/components-js/scripts/convertToReactVRTPage.ts
+++ packages/components-js/scripts/convertToReactVRTPage.ts
@@ -118,8 +118,7 @@ $2`
   fileContent = fileContent
     .replace(/(<textarea.*)>\s*(.+?)\s*(<\/textarea>)/g, '$1 defaultValue="$2">$3')
     .replace(/(<input[^>]*?) v(alue=)/g, '$1 defaultV$2') // for input
-    .replace(/(<input[^>]*?) c(hecked)/g, '$1 defaultC$2') // for checkbox + radio
-    .replace(/tabindex="/g, 'tabIndex="'); // for tabindex attribute
+    .replace(/(<input[^>]*?) c(hecked)/g, '$1 defaultC$2'); // for checkbox + radio
 
   fileContent = fileContent.replace(/(\n +)(<(?:strong|em|b|i)>)/g, "$1{' '}$2"); // for forced whitespace
 
