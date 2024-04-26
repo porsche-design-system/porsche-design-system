f0171d46bf213a732ca7cc93316adca0e2436a07
Michael Worm <michael.worm@mhp.com>
2024-04-26 16:05:36 +0200

    feat(components): Popover banner animation | mw | #3129

--- packages/components/src/components/banner/banner-styles.ts
+++ packages/components/src/components/banner/banner-styles.ts
@@ -5,8 +5,10 @@ import {
   gridExtendedOffsetBase,
   gridExtendedOffsetS,
   gridExtendedOffsetXXL,
+  motionDurationLong,
   motionDurationModerate,
   motionEasingIn,
+  motionEasingOut,
 } from '@porsche-design-system/utilities-v2';
 import { getCss } from '../../utils';
 import { colorSchemeStyles, cssVariableAnimationDuration, hostHiddenStyles } from '../../styles';
@@ -32,12 +34,28 @@ export const getComponentCss = (isOpen: boolean): string => {
         maxWidth: '100%', // If component is wrapped in container with maxWidth
         ...dropShadowHighStyle,
         borderRadius: borderRadiusSmall, // needed for rounded box-shadow
-        animation: `$slideIn var(${cssVariableAnimationDuration}, ${motionDurationModerate}) ${motionEasingIn} forwards`,
+        ...(isOpen
+          ? {
+              opacity: 1,
+              visibility: 'inherit',
+              transform: 'translate3d(0,0,0)',
+              transition: `transform var(${cssVariableAnimationDuration}, ${motionDurationModerate}) ${motionEasingIn}, opacity var(${cssVariableAnimationDuration}, ${motionDurationModerate}) ${motionEasingIn}`,
+            }
+          : {
+              opacity: 0,
+              visibility: 'hidden',
+              transform: `translate3d(0,calc(var(${cssVariableBottom},${topBottomFallback}) + 100%),0)`,
+              '&(.hydrated),&(.ssr)': {
+                transition: `visibility 0s linear var(${cssVariableAnimationDuration}, ${motionDurationLong}), transform var(${cssVariableAnimationDuration}, ${motionDurationModerate}) ${motionEasingOut}, opacity var(${cssVariableAnimationDuration}, ${motionDurationModerate}) ${motionEasingOut}`,
+              },
+            }),
         [getMediaQueryMin('s')]: {
           top: `var(${cssVariableTop},${topBottomFallback})`,
           bottom: 'auto',
           left: gridExtendedOffsetS,
           right: gridExtendedOffsetS,
+          // space before and after "-" is crucial)
+          ...(!isOpen && { transform: `translate3d(0,calc(-100% - var(${cssVariableTop},${topBottomFallback})),0)` }),
         },
         [getMediaQueryMin('xxl')]: {
           left: gridExtendedOffsetXXL,
@@ -45,16 +63,6 @@ export const getComponentCss = (isOpen: boolean): string => {
         },
         ...colorSchemeStyles,
         ...hostHiddenStyles,
-      },
-      '@keyframes slideIn': {
-        from: {
-          opacity: 0,
-          transform: `translate3d(0, calc(-100% - var(${cssVariableTop},${topBottomFallback})), 0)`,
-        },
-        to: {
-          opacity: 1,
-          transform: 'translate3d(0,0,0)',
-        },
       },
     },
   });
