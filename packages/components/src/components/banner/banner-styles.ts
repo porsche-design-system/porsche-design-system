921eec05d79951b4a4f468bd7575fbfbbe5dd61a
Michael Worm <michael.worm@mhp.com>
2024-04-30 14:17:28 +0200

    feat(components): Remove important for display at banner | mw | #3129

--- packages/components/src/components/banner/banner-styles.ts
+++ packages/components/src/components/banner/banner-styles.ts
@@ -26,56 +26,58 @@ const topBottomFallback = '56px';
 export const getComponentCss = (isOpen: boolean): string => {
   return getCss({
     '@global': {
-      ':host': addImportantToEachRule({
-        position: 'fixed',
+      ':host': {
         display: 'block',
-        border: '0',
-        outline: '0',
-        bottom: `var(${cssVariableBottom},${topBottomFallback})`,
-        left: gridExtendedOffsetBase,
-        right: gridExtendedOffsetBase,
-        margin: 0,
-        padding: 0,
-        width: 'auto',
-        maxWidth: '100%', // If component is wrapped in container with maxWidth
-        ...dropShadowHighStyle,
-        borderRadius: borderRadiusSmall, // needed for rounded box-shadow
-        ...(isOpen
-          ? {
-              opacity: 1,
-              visibility: 'inherit',
-              pointerEvents: 'inherit',
-              transform: 'translate3d(0,0,0)',
-              transition: `${getTransition('transform', 'moderate', 'in')}, ${getTransition('opacity', 'moderate', 'in')}`,
-            }
-          : {
-              opacity: 0,
-              visibility: 'hidden',
-              pointerEvents: 'none',
-              transform: `translate3d(0,calc(var(${cssVariableBottom},${topBottomFallback}) + 100%),0)`,
-              '&(.hydrated),&(.ssr)': {
-                transition: `visibility 0s linear var(${cssVariableAnimationDuration}, ${motionDurationLong}), ${getTransition('transform', 'moderate', 'out')}, ${getTransition('opacity', 'moderate', 'out')}`,
-                // during transition the element will be removed from top-layer immediately, resulting in other elements laying over (as of Mai 2024 only Chrome is fixed by this)
-                '@supports (transition-behavior: allow-discrete)': {
-                  transition: `visibility 0s linear var(${cssVariableAnimationDuration}, ${motionDurationLong}), ${getTransition('transform', 'moderate', 'out')}, ${getTransition('opacity', 'moderate', 'out')}, overlay var(${cssVariableAnimationDuration}, ${motionDurationModerate}) ${motionEasingOut} allow-discrete`,
+        ...addImportantToEachRule({
+          position: 'fixed',
+          border: '0',
+          outline: '0',
+          bottom: `var(${cssVariableBottom},${topBottomFallback})`,
+          left: gridExtendedOffsetBase,
+          right: gridExtendedOffsetBase,
+          margin: 0,
+          padding: 0,
+          width: 'auto',
+          maxWidth: '100%', // If component is wrapped in container with maxWidth
+          ...dropShadowHighStyle,
+          borderRadius: borderRadiusSmall, // needed for rounded box-shadow
+          ...(isOpen
+            ? {
+                opacity: 1,
+                visibility: 'inherit',
+                pointerEvents: 'inherit',
+                transform: 'translate3d(0,0,0)',
+                transition: `${getTransition('transform', 'moderate', 'in')}, ${getTransition('opacity', 'moderate', 'in')}`,
+              }
+            : {
+                opacity: 0,
+                visibility: 'hidden',
+                pointerEvents: 'none',
+                transform: `translate3d(0,calc(var(${cssVariableBottom},${topBottomFallback}) + 100%),0)`,
+                '&(.hydrated),&(.ssr)': {
+                  transition: `visibility 0s linear var(${cssVariableAnimationDuration}, ${motionDurationLong}), ${getTransition('transform', 'moderate', 'out')}, ${getTransition('opacity', 'moderate', 'out')}`,
+                  // during transition the element will be removed from top-layer immediately, resulting in other elements laying over (as of Mai 2024 only Chrome is fixed by this)
+                  '@supports (transition-behavior: allow-discrete)': {
+                    transition: `visibility 0s linear var(${cssVariableAnimationDuration}, ${motionDurationLong}), ${getTransition('transform', 'moderate', 'out')}, ${getTransition('opacity', 'moderate', 'out')}, overlay var(${cssVariableAnimationDuration}, ${motionDurationModerate}) ${motionEasingOut} allow-discrete`,
+                  },
                 },
-              },
-            }),
-        [getMediaQueryMin('s')]: {
-          top: `var(${cssVariableTop},${topBottomFallback})`,
-          bottom: 'auto',
-          left: gridExtendedOffsetS,
-          right: gridExtendedOffsetS,
-          // space before and after "-" is crucial)
-          ...(!isOpen && { transform: `translate3d(0,calc(-100% - var(${cssVariableTop},${topBottomFallback})),0)` }),
-        },
-        [getMediaQueryMin('xxl')]: {
-          left: gridExtendedOffsetXXL,
-          right: gridExtendedOffsetXXL,
-        },
-        ...colorSchemeStyles,
-        ...hostHiddenStyles,
-      }),
+              }),
+          [getMediaQueryMin('s')]: {
+            top: `var(${cssVariableTop},${topBottomFallback})`,
+            bottom: 'auto',
+            left: gridExtendedOffsetS,
+            right: gridExtendedOffsetS,
+            // space before and after "-" is crucial)
+            ...(!isOpen && { transform: `translate3d(0,calc(-100% - var(${cssVariableTop},${topBottomFallback})),0)` }),
+          },
+          [getMediaQueryMin('xxl')]: {
+            left: gridExtendedOffsetXXL,
+            right: gridExtendedOffsetXXL,
+          },
+          ...colorSchemeStyles,
+          ...hostHiddenStyles,
+        }),
+      },
     },
   });
 };
