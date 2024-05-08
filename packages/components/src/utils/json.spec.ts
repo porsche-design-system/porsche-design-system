380293159aa1e8cf5352f562f9980517bde35322
Oliver Hook <oliver.hook@porsche.digital>
2024-05-08 17:00:45 +0200

    test(components): enhance tests with single quotes as unicode | oh | #3171

--- packages/components/src/utils/json.spec.ts
+++ packages/components/src/utils/json.spec.ts
@@ -5,6 +5,7 @@ describe('parseJSONAttribute()', () => {
     {
       'aria-label': 'Some label',
     },
+    "{aria-label: 'Some label'}",
     "{'aria-label': 'Some label'}",
     "{'aria-label':'Some label'}",
     '{"aria-label": "Some label"}',
@@ -12,6 +13,21 @@ describe('parseJSONAttribute()', () => {
   ])('should return parsed object for %o', (input) => {
     expect(parseJSONAttribute(input)).toEqual({
       'aria-label': 'Some label',
+    });
+  });
+
+  it.each<string | object>([
+    {
+      'aria-label': "Some label's",
+    },
+    "{aria-label: 'Some label\\u0027s'}",
+    "{'aria-label': 'Some label\\u0027s'}",
+    "{'aria-label':'Some label\\u0027s'}",
+    "{aria-label: 'Some label\\u0027s'}",
+    '{"aria-label":"Some label\\u0027s"}',
+  ])('should return parsed object with single quoted value for %o', (input) => {
+    expect(parseJSONAttribute(input)).toEqual({
+      'aria-label': "Some label's",
     });
   });
 
