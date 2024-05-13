b7ea2351d2b67d1ee1024f8a50f21f97f2cc4ca2
Michael Worm <michael.worm@mhp.com>
2024-05-13 13:59:41 +0200

    feat(components): Add describe to lifecycle e2e | mw | #3129

--- packages/components-js/tests/e2e/specs/lifecycle-after-disconnect.e2e.ts
+++ packages/components-js/tests/e2e/specs/lifecycle-after-disconnect.e2e.ts
@@ -10,28 +10,30 @@ import { INTERNAL_TAG_NAMES, TAG_NAMES, TagName } from '@porsche-design-system/s
 const tagNames: TagName[] = TAG_NAMES.filter((tagName) => !INTERNAL_TAG_NAMES.includes(tagName));
 
 for (const tagName of tagNames) {
-  test(`should not throw error after disconnectedCallback for ${tagName}`, async ({ page }) => {
-    // TODO: Banner is now using popover attribute which needs an update of Playwright so Firefox supports it (#3129)
-    test.skip(({ browserName }) => tagName === 'p-banner' && browserName === 'firefox');
+  test.describe(tagName, () => {
+    test(`should not throw error after disconnectedCallback`, async ({ page }) => {
+      // TODO: Banner is now using popover attribute which needs an update of Playwright so Firefox supports it (#3129)
+      test.skip(({ browserName }) => tagName === 'p-banner' && browserName === 'firefox');
 
-    initConsoleObserver(page);
-    await goto(page, ''); // start page
+      initConsoleObserver(page);
+      await goto(page, ''); // start page
 
-    const markup = buildDefaultComponentMarkup(tagName);
+      const markup = buildDefaultComponentMarkup(tagName);
 
-    await page.evaluate(
-      ({ tagName, markup }) => {
-        document.getElementById('app').innerHTML = markup;
-        document.getElementById('app').querySelector(tagName).remove(); // remove component immediately
-      },
-      { tagName, markup }
-    );
+      await page.evaluate(
+        ({ tagName, markup }) => {
+          document.getElementById('app').innerHTML = markup;
+          document.getElementById('app').querySelector(tagName).remove(); // remove component immediately
+        },
+        { tagName, markup }
+      );
 
-    await sleep(100);
+      await sleep(100);
 
-    expect(getConsoleErrorsAmount()).toBe(0);
+      expect(getConsoleErrorsAmount()).toBe(0);
 
-    await page.evaluate(() => console.error('test error'));
-    expect(getConsoleErrorsAmount()).toBe(1);
+      await page.evaluate(() => console.error('test error'));
+      expect(getConsoleErrorsAmount()).toBe(1);
+    });
   });
 }
