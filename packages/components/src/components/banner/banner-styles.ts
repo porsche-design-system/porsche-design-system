43540fabb5e654e7da5013c83430410a200e52ad
Michael Worm <michael.worm@mhp.com>
2024-05-13 13:06:36 +0200

    Revert "feat(components): Fix high contrast banner outline | mw | #3129"
    This reverts commit e68964991949d45e64c08179f97d96ad98a02d29.

--- packages/components/src/components/banner/banner-styles.ts
+++ packages/components/src/components/banner/banner-styles.ts
@@ -37,15 +37,13 @@ export const getComponentCss = (isOpen: boolean): string => {
           right: gridExtendedOffsetBase,
           margin: 0,
           padding: 0,
-          width: 'auto', // ua popover reset
-          height: 'auto', // ua popover reset
+          width: 'auto',
           maxWidth: '100%', // If component is wrapped in container with maxWidth
           zIndex: `var(${cssVariableZIndex},${BANNER_Z_INDEX})`,
           ...dropShadowHighStyle,
           borderRadius: borderRadiusSmall, // needed for rounded box-shadow
-          border: '0', // ua popover reset
-          outline: '0', // ua popover reset
-          overflow: 'visible', // ua popover reset
+          border: '0',
+          outline: '0',
           ...(isOpen
             ? {
                 opacity: 1,
