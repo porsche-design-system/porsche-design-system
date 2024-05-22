abbbd3b90f699e9198838d791b264c2aea55ecf9
Michael Worm <michael.worm@mhp.com>
2024-05-22 18:47:04 +0200

    fix(components): Remove banner backdrop from DOM | mw | #3129

--- packages/components/src/components/banner/banner-styles.ts
+++ packages/components/src/components/banner/banner-styles.ts
@@ -40,6 +40,9 @@ export const getComponentCss = (isOpen: boolean): string => {
           zIndex: `var(${cssVariableZIndex},${BANNER_Z_INDEX})`,
           ...dropShadowHighStyle,
           borderRadius: borderRadiusSmall, // needed for rounded box-shadow
+          '&::backdrop': {
+            display: 'none',
+          },
           ...(isOpen
             ? {
                 opacity: 1,
