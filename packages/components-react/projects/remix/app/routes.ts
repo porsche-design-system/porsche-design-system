567efec40c77c96564ca08e97bd6f05c527416cc
Henri Joss <henri.joss@mhp.com>
2024-05-23 11:53:36 +0200

    feat(components-js,react/remix): add iframe pages to remix for overview vrt | hj | #2682

--- packages/components-react/projects/remix/app/routes.ts
+++ packages/components-react/projects/remix/app/routes.ts
@@ -20,4 +20,52 @@ export const routes: Route[] = [
     name: 'Modal Page',
     path: '/modal-standalone',
   },
+  {
+    name: 'Modal Basic',
+    path: '/modal-basic',
+  },
+  {
+    name: 'Banner Basic',
+    path: '/banner-basic',
+  },
+  {
+    name: 'Banner Prefixed',
+    path: '/banner-prefixed',
+  },
+  {
+    name: 'Flyout Basic',
+    path: '/flyout-basic',
+  },
+  {
+    name: 'Flyout Prefixed',
+    path: '/flyout-prefixed',
+  },
+  {
+    name: 'Flyout Navigation Basic',
+    path: '/flyout-navigation-basic',
+  },
+  {
+    name: 'Flyout Navigation Prefixed',
+    path: '/flyout-navigation-prefixed',
+  },
+  {
+    name: 'Modal Basic',
+    path: '/modal-basic',
+  },
+  {
+    name: 'Modal Prefixed',
+    path: '/modal-prefixed',
+  },
+  {
+    name: 'Overview',
+    path: '/overview',
+  },
+  {
+    name: 'Toast Basic',
+    path: '/toast-basic',
+  },
+  {
+    name: 'Toast Prefixed',
+    path: '/toast-prefixed',
+  },
 ];
