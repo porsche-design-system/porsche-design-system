eb3b7c68832d9f6f94167fcd529377399caea69d
Michael Worm <michael.worm@mhp.com>
2024-05-29 09:52:19 +0200

    fix(components): Refactor parseJSONAttribute to omit variables | mw | #3313

--- packages/components/src/utils/json.ts
+++ packages/components/src/utils/json.ts
@@ -4,17 +4,15 @@ export const parseJSONAttribute = <T>(attribute: T | string): T => {
     return attribute;
   }
 
-  // Convert single quotes to double quotes except the ones which are escaped by backslash
-  let jsonString = attribute
-    .replace(/\\'/g, '__escaped_single_quote__')
-    .replace(/'/g, '"')
-    .replace(/__escaped_single_quote__/g, '\\\'');
-
-  // Remove string escapes except the ones followed by unicode u0027
-  jsonString = jsonString.replace(/([^\\])\\(?!u0027)/g, '$1');
-
-  // Wrap keys in double quotes
-  jsonString = jsonString.replace(/[\s"]?([\w-]+)[\s"]?:/g, '"$1":');
-
-  return JSON.parse(jsonString);
+  return JSON.parse(
+    attribute
+      // Convert single quotes to double quotes except the ones which are escaped by backslash
+      .replace(/\\'/g, '__escaped_single_quote__')
+      .replace(/'/g, '"')
+      .replace(/__escaped_single_quote__/g, '\\\'')
+      // Remove string escapes except the ones followed by unicode u0027
+      .replace(/([^\\])\\(?!u0027)/g, '$1')
+      // Wrap keys in double quotes
+      .replace(/[\s"]?([\w-]+)[\s"]?:/g, '"$1":')
+  );
 };
