d81eee424efa3b5d0a6afd82c9d1238711187519
Oliver Hook <oliver.hook@porsche.digital>
2024-06-05 11:53:54 +0200

    test(components): enhance unit tests | oh | #3190

--- packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-utils.spec.ts
+++ packages/components/src/components/segmented-control/segmented-control-item/segmented-control-item-utils.spec.ts
@@ -1,14 +1,14 @@
-import { getButtonAttributes, getIconColor } from './segmented-control-item-utils';
+import { getSegmentedControlItemAriaAttributes, getIconColor } from './segmented-control-item-utils';
 import { IconColor } from '../../icon/icon-utils';
 
 describe('getButtonAttributes()', () => {
-  it.each<Parameters<typeof getButtonAttributes>>([
-    [false, false],
-    [true, true],
-    [true, false],
-    [false, true],
+  it.each<Parameters<typeof getSegmentedControlItemAriaAttributes>>([
+    [false, false, { 'aria-label': 'Some label' }],
+    [true, true, undefined],
+    [true, false, { 'aria-label': 'Some label' }],
+    [false, true, undefined],
   ])('should return correct css for isSelected: %s and isDisabled: %s', (...args) => {
-    expect(getButtonAttributes(...args)).toMatchSnapshot();
+    expect(getSegmentedControlItemAriaAttributes(...args)).toMatchSnapshot();
   });
 });
 
