0346fb4c58647db27c08027c8fa264857f1a4d48
Michael Worm <michael.worm@mhp.com>
2024-06-10 10:35:48 +0200

    feat(components): Correctly type getIntlFromAria | mw | #3015

--- packages/components/src/utils/getIntlFromAria.ts
+++ packages/components/src/utils/getIntlFromAria.ts
@@ -1,29 +1,29 @@
-import { CarouselAriaInternationalization } from '../components/carousel/carousel-utils';
-import {
-  PaginationAriaInternationalization,
-  PaginationInternationalization,
-} from '../components/pagination/pagination-utils';
+type IntlFromAria = {
+  [key: string]: string;
+  root?: string;
+};
 
-type IntlFromAriaInput = PaginationAriaInternationalization | CarouselAriaInternationalization;
-type IntlFromAriaOutput = PaginationInternationalization | CarouselAriaInternationalization;
+type IntlFromAriaOutput<T> = (T & IntlFromAria) | Record<string, never>;
 
-export const getIntlFromAria = (aria?: IntlFromAriaInput): IntlFromAriaOutput => {
+export const getIntlFromAria = <T>(aria?: T): IntlFromAriaOutput<T> => {
   if (!aria) {
     return {};
   }
 
-  const intlObj: IntlFromAriaOutput = {};
+  const intlObj: Partial<IntlFromAria> = {};
   const translationKey = 'aria-label';
 
   for (const key of Object.keys(aria)) {
-    // { 'aria-label': 'Lorem ipsum' }
-    if (key === translationKey) {
-      intlObj.root = aria[key];
-    }
+    const value = aria[key];
 
-    // { key: { 'aria-label': 'Lorem ipsum' } }
-    intlObj[key] = aria[key][translationKey];
+    if (key === translationKey) {
+      // Direct 'aria-label' key at the root
+      intlObj.root = value;
+    } else if (typeof value === 'object' && value !== null && translationKey in value) {
+      // Nested object with 'aria-label'
+      intlObj[key] = value[translationKey];
+    }
   }
 
-  return intlObj;
+  return intlObj as IntlFromAriaOutput<T>;
 };
