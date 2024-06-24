7752a5843d0209ade24616fb11b6353717ff665d
Oliver Hook <oliver.hook@porsche.digital>
2024-06-24 08:27:45 +0200

    test(components-vue): adapt test pages | oh | #3167

--- packages/components-vue/src/pages/FlyoutMultilevelBasicPage.vue
+++ packages/components-vue/src/pages/FlyoutMultilevelBasicPage.vue
@@ -1,34 +1,34 @@
 <script setup lang="ts">
-  import { PFlyoutNavigation, PFlyoutNavigationItem } from '@porsche-design-system/components-vue';
+  import { PFlyoutMultilevel, PFlyoutMultilevelItem } from '@porsche-design-system/components-vue';
 </script>
 
 <template>
-  <PFlyoutNavigation open="true">
-    <PFlyoutNavigationItem identifier="item-1" label="Some Label">
+  <PFlyoutMultilevel open="true">
+    <PFlyoutMultilevelItem identifier="item-1" label="Some Label">
       <a href="#some-anchor">Some anchor</a>
       <a href="#some-anchor">Some anchor</a>
       <a href="#some-anchor">Some anchor</a>
       <a href="#some-anchor">Some anchor</a>
       <a href="#some-anchor">Some anchor</a>
-    </PFlyoutNavigationItem>
-    <PFlyoutNavigationItem identifier="item-2" label="Some Label">
+    </PFlyoutMultilevelItem>
+    <PFlyoutMultilevelItem identifier="item-2" label="Some Label">
       <a href="#some-anchor">Some anchor</a>
       <a href="#some-anchor">Some anchor</a>
       <a href="#some-anchor">Some anchor</a>
-    </PFlyoutNavigationItem>
-    <PFlyoutNavigationItem identifier="item-3" label="Some Label">
+    </PFlyoutMultilevelItem>
+    <PFlyoutMultilevelItem identifier="item-3" label="Some Label">
       <a href="#some-anchor">Some anchor</a>
       <a href="#some-anchor">Some anchor</a>
-    </PFlyoutNavigationItem>
-    <PFlyoutNavigationItem identifier="item-4" label="Some Label">
+    </PFlyoutMultilevelItem>
+    <PFlyoutMultilevelItem identifier="item-4" label="Some Label">
       <a href="#some-anchor">Some anchor</a>
-    </PFlyoutNavigationItem>
-    <PFlyoutNavigationItem identifier="item-5" label="Some Label">
+    </PFlyoutMultilevelItem>
+    <PFlyoutMultilevelItem identifier="item-5" label="Some Label">
       <a href="#some-anchor">Some anchor</a>
-    </PFlyoutNavigationItem>
-    <PFlyoutNavigationItem identifier="item-6" label="Some Label">
+    </PFlyoutMultilevelItem>
+    <PFlyoutMultilevelItem identifier="item-6" label="Some Label">
       <a href="#some-anchor">Some anchor</a>
       <a href="#some-anchor" class="scroll-into-view">Some anchor</a>
-    </PFlyoutNavigationItem>
-  </PFlyoutNavigation>
+    </PFlyoutMultilevelItem>
+  </PFlyoutMultilevel>
 </template>
