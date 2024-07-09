6a67050f08155dff0ba906722d53e5b3c99d0c72
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-07-09 10:21:06 +0200

    chore: re-organize npm dependencies | sas | #3362

--- packages/storefront/src/utils/stackblitz/getVueProjectAndOpenOptions.ts
+++ packages/storefront/src/utils/stackblitz/getVueProjectAndOpenOptions.ts
@@ -1,4 +1,5 @@
 import { devDependencies, dependencies } from '../../../../components-vue/package.json';
+import { devDependencies as devDependenciesRoot } from '../../../../../package.json';
 import {
   convertImportPaths,
   getExternalDependencies,
@@ -136,7 +137,7 @@ export const getDevDependencies = (): StackBlitzProjectDependencies => {
   return {
     '@vitejs/plugin-vue': devDependencies['@vitejs/plugin-vue'],
     '@vitejs/plugin-vue-jsx': devDependencies['@vitejs/plugin-vue-jsx'],
-    typescript: devDependencies['typescript'],
+    typescript: devDependenciesRoot['typescript'],
     vite: devDependencies['vite'],
     'vue-tsc': devDependencies['vue-tsc'],
   };
