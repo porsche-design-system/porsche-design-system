6c34eccb7767248d25d7285086eaa05069b30dc4
kaiszybiak <kai.szybiak@netrunners.com>
2024-07-23 17:27:43 +0200

    test(components-js): add exception to vrt test  | ks / hj | #3080

--- packages/components-js/tests/vrt/specs/common/components.vrt.ts
+++ packages/components-js/tests/vrt/specs/common/components.vrt.ts
@@ -5,7 +5,7 @@ import { getComponentMeta } from '@porsche-design-system/component-meta';
 import { schemes, themes, viewportWidthM, viewportWidths } from '@porsche-design-system/shared/testing/playwright.vrt';
 
 const components = (TAG_NAMES as unknown as TagName[])
-  .filter((tagName) => tagName !== 'p-canvas') // TODO: remove filter as soon as component becomes stable
+  .filter((tagName) => !['p-canvas', 'p-optgroup'].includes(tagName)) // TODO: remove filter as soon as component becomes stable
   // Filter out non-chunked components
   .filter((tagName) => {
     const { isChunked } = getComponentMeta(tagName);
