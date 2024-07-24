3dd2805285b7616fc9ed8cec0826b32565588f59
kaiszybiak <kai.szybiak@netrunners.com>
2024-07-24 13:35:41 +0200

    test(components-js): update partials snapshots | ks / hj | #3080

--- packages/components-js/projects/partials/tests/unit/specs/getComponentChunkLinks.spec.ts
+++ packages/components-js/projects/partials/tests/unit/specs/getComponentChunkLinks.spec.ts
@@ -20,7 +20,7 @@ describe('validation', () => {
           some-invalid-component
 
         Please use only valid component chunk names:
-          accordion, banner, button-group, button-pure, button-tile, button, canvas, carousel, checkbox-wrapper, content-wrapper, crest, display, divider, fieldset-wrapper, fieldset, flex, flyout-multilevel, flyout, grid, heading, headline, icon, inline-notification, link-pure, link-social, link-tile-model-signature, link-tile-product, link-tile, link, marque, modal, model-signature, multi-select, pagination, pin-code, popover, radio-button-wrapper, scroller, segmented-control, select-wrapper, select, spinner, stepper-horizontal, switch, table, tabs-bar, tabs, tag-dismissible, tag, text-field-wrapper, text-list, text, textarea-wrapper, toast, wordmark]
+          accordion, banner, button-group, button-pure, button-tile, button, canvas, carousel, checkbox-wrapper, content-wrapper, crest, display, divider, fieldset-wrapper, fieldset, flex, flyout-multilevel, flyout, grid, heading, headline, icon, inline-notification, link-pure, link-social, link-tile-model-signature, link-tile-product, link-tile, link, marque, modal, model-signature, multi-select, optgroup, pagination, pin-code, popover, radio-button-wrapper, scroller, segmented-control, select-wrapper, select, spinner, stepper-horizontal, switch, table, tabs-bar, tabs, tag-dismissible, tag, text-field-wrapper, text-list, text, textarea-wrapper, toast, wordmark]
       `);
   });
 });
