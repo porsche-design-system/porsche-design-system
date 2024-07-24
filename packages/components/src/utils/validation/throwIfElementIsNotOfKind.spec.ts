625dc55aa8e5a7375b8d4dc839eecfe628eddfc6
Henri Joss <henri.joss@mhp.com>
2024-07-24 15:10:53 +0200

    feat(components): add option to validate multiple tagNames | hj | #3080

--- packages/components/src/utils/validation/throwIfElementIsNotOfKind.spec.ts
+++ packages/components/src/utils/validation/throwIfElementIsNotOfKind.spec.ts
@@ -1,15 +1,32 @@
 import * as getPrefixedTagNamesUtils from '../tag-name';
-import { getPrefixedTagNames } from '../tag-name';
+import { PrefixedTagNames } from '../tag-name';
 import { throwIfElementIsNotOfKind } from './throwIfElementIsNotOfKind';
+import * as paramCaseToCamelCaseUtils from '../paramCaseToCamelCase';
 
 const host = document.createElement('div');
 const link = document.createElement('p-link');
+const option = document.createElement('p-multi-select-option');
 
 it('should call getPrefixedTagNames() with correct parameters', () => {
   const spy = jest.spyOn(getPrefixedTagNamesUtils, 'getPrefixedTagNames');
   throwIfElementIsNotOfKind(host, link, 'p-link');
 
   expect(spy).toHaveBeenCalledWith(host);
+});
+
+it('should call paramCaseToCamelCase() with correct parameters', () => {
+  const spy = jest.spyOn(paramCaseToCamelCaseUtils, 'paramCaseToCamelCase');
+  throwIfElementIsNotOfKind(host, link, 'p-link');
+
+  expect(spy).toHaveBeenCalledWith('p-link');
+});
+
+it('should call paramCaseToCamelCase() for each tagName with correct parameters', () => {
+  const spy = jest.spyOn(paramCaseToCamelCaseUtils, 'paramCaseToCamelCase');
+  throwIfElementIsNotOfKind(host, option, ['p-multi-select-option', 'p-optgroup']);
+
+  expect(spy).toHaveBeenNthCalledWith(1, 'p-multi-select-option');
+  expect(spy).toHaveBeenNthCalledWith(2, 'p-optgroup');
 });
 
 it('should call getTagName() with correct parameters', () => {
@@ -20,12 +37,27 @@ it('should call getTagName() with correct parameters', () => {
 });
 
 it('should throw error if return value of getPrefixedTagNames() !== getTagName()', () => {
-  const prefixedTagNameMock = { ...getPrefixedTagNames(host), pLink: 'p-link' };
+  const prefixedTagNameMock = { pLink: 'p-link' } as PrefixedTagNames;
   jest.spyOn(getPrefixedTagNamesUtils, 'getPrefixedTagNames').mockReturnValue(prefixedTagNameMock);
   const slot = document.createElement('a');
 
   expect(() => throwIfElementIsNotOfKind(host, slot, 'p-link')).toThrowErrorMatchingInlineSnapshot(
-    `"[Porsche Design System] child a of div has to be a p-link."`,
+    `"[Porsche Design System] child a of div has to be a p-link."`
+  );
+});
+
+it('should throw error if tagName is not included in prefixedTagNames', () => {
+  const prefixedTagNameMock = {
+    pMultiSelectOption: 'p-multi-select-option',
+    pOptgroup: 'p-optgroup',
+  } as PrefixedTagNames;
+  jest.spyOn(getPrefixedTagNamesUtils, 'getPrefixedTagNames').mockReturnValue(prefixedTagNameMock);
+  const testElement = document.createElement('a');
+
+  expect(() =>
+    throwIfElementIsNotOfKind(host, testElement, ['p-multi-select-option', 'p-optgroup'])
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"[Porsche Design System] child a of div has to be a p-multi-select-option | p-optgroup."`
   );
 });
 
@@ -33,8 +65,20 @@ it('should not throw error if slot is of kind', () => {
   expect(() => throwIfElementIsNotOfKind(host, link, 'p-link')).not.toThrow();
 });
 
+it('should not throw error if child is one of kinds', () => {
+  expect(() => throwIfElementIsNotOfKind(host, option, ['p-multi-select-option', 'p-optgroup'])).not.toThrow();
+});
+
 it('should not throw error if prefixed slot is of kind', () => {
   const prefixedHost = document.createElement('prefixed-p-link-tile-model-signature');
   const prefixedLink = document.createElement('prefixed-p-link');
   expect(() => throwIfElementIsNotOfKind(prefixedHost, prefixedLink, 'p-link')).not.toThrow();
+});
+
+it('should not throw error if prefixed child is one of kinds', () => {
+  const prefixedHost = document.createElement('prefixed-p-select');
+  const prefixedLink = document.createElement('prefixed-p-optgroup');
+  expect(() =>
+    throwIfElementIsNotOfKind(prefixedHost, prefixedLink, ['p-multi-select-option', 'p-optgroup'])
+  ).not.toThrow();
 });
--- packages/components/src/utils/validation/throwIfElementIsNotOfKind.ts
+++ packages/components/src/utils/validation/throwIfElementIsNotOfKind.ts
@@ -3,11 +3,20 @@ import { getPrefixedTagNames, getTagName } from '../tag-name';
 import { throwException } from '../log';
 import { paramCaseToCamelCase } from '../paramCaseToCamelCase';
 
-export const throwIfElementIsNotOfKind = (host: HTMLElement, element: HTMLElement, tagName: TagName): void => {
-  const prefixedTagName = getPrefixedTagNames(host)[paramCaseToCamelCase(tagName)];
+export const throwIfElementIsNotOfKind = (
+  host: HTMLElement,
+  element: HTMLElement,
+  tagNameOrNames: TagName | TagName[]
+): void => {
+  const prefixedTagNamesMap = getPrefixedTagNames(host);
+
+  const prefixedTagNames: string[] = Array.isArray(tagNameOrNames)
+    ? tagNameOrNames.map((tagName) => prefixedTagNamesMap[paramCaseToCamelCase(tagName)])
+    : [prefixedTagNamesMap[paramCaseToCamelCase(tagNameOrNames)]];
+
   const actualTagName = getTagName(element);
 
-  if (actualTagName !== prefixedTagName) {
-    throwException(`child ${actualTagName} of ${getTagName(host)} has to be a ${prefixedTagName}.`);
+  if (!prefixedTagNames.includes(actualTagName)) {
+    throwException(`child ${actualTagName} of ${getTagName(host)} has to be a ${prefixedTagNames.join(' | ')}.`);
   }
 };
