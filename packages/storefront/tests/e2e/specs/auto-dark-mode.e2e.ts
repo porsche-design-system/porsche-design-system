407fd194d99c15642cad594be4716eba67494e5f
kaiszybiak <kai.szybiak@netrunners.com>
2024-07-29 08:28:06 +0200

    test(storefront): replace ElementHandle with Locator | ks | #3086

--- packages/storefront/tests/e2e/specs/auto-dark-mode.e2e.ts
+++ packages/storefront/tests/e2e/specs/auto-dark-mode.e2e.ts
@@ -17,7 +17,7 @@ for (const [url, index] of internalUrls.map<[string, number]>((url, i) => [url, 
       (window as unknown as Window & { componentsReady: () => Promise<number> }).componentsReady()
     );
 
-    const components = await page.locator(componentsWithThemeAutoSelector);
+    const components = page.locator(componentsWithThemeAutoSelector);
 
     // TODO: "The use of ElementHandle is discouraged, use Locator objects and web-first assertions instead.", see: https://playwright.dev/docs/api/class-elementhandle
     for (const component of await components.elementHandles()) {
