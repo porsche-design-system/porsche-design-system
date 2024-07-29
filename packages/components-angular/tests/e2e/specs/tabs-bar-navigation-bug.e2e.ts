459c4b905a043caf34d8bf6f01bf4f651f83d8dd
kaiszybiak <kai.szybiak@netrunners.com>
2024-07-29 08:28:45 +0200

    test(components-angular): replace ElementHandle with Locator | ks | #3086

--- packages/components-angular/tests/e2e/specs/tabs-bar-navigation-bug.e2e.ts
+++ packages/components-angular/tests/e2e/specs/tabs-bar-navigation-bug.e2e.ts
@@ -4,23 +4,24 @@ import { goto } from '../helpers';
 test('should correctly set activeTabIndex on p-tabs-bar', async ({ page }) => {
   await goto(page, 'tabs-bar-navigation-bug');
 
-  const getTabsBarActiveTabIndex = (): Promise<number> => page.$eval('p-tabs-bar', (el) => (el as any).activeTabIndex);
-  const getHeadingText = (): Promise<string> => page.$eval('p-heading', (el) => el.textContent);
+  const getTabsBarActiveTabIndex = (): Promise<number> =>
+    page.locator('p-tabs-bar').evaluate((el) => (el as any).activeTabIndex);
+  const getHeadingText = (): Promise<string> => page.locator('p-heading').evaluate((el) => el.textContent);
 
   expect(await getTabsBarActiveTabIndex()).toBe(0);
   expect(await getHeadingText()).toBe('Bug Page 1');
 
-  const [, link2] = await page.$$('a');
+  const [, link2] = await page.locator('a').all();
   await Promise.all([page.waitForURL(page.url()), link2.click()]);
   expect(await getTabsBarActiveTabIndex()).toBe(1);
   expect(await getHeadingText()).toBe('Bug Page 2');
 
-  const [, , link3] = await page.$$('a');
+  const [, , link3] = await page.locator('a').all();
   await Promise.all([page.waitForURL(page.url()), link3.click()]);
   expect(await getTabsBarActiveTabIndex()).toBe(2);
   expect(await getHeadingText()).toBe('Bug Page 3');
 
-  const [link1] = await page.$$('a');
+  const [link1] = await page.locator('a').all();
   await Promise.all([page.waitForURL(page.url()), link1.click()]);
   expect(await getTabsBarActiveTabIndex()).toBe(0);
   expect(await getHeadingText()).toBe('Bug Page 1');
