3cdf49c912bafdbea9c9f5bac763c5cb9b161031
kaiszybiak <kaiszybiak@users.noreply.github.com>
2024-08-15 20:03:51 +0000

    Automated deployment to GitHub Pages (issue/3385): 656ef689dba5334d1a03e48a2af4d253918bd1ca

---
+++ issue/3385/js/chunk-2d0cc660.6c8ec0cd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cc660"],{"4e81":function(e,t,a){"use strict";a.r(t);var l=function(){var e=this,t=e._self._c;return t("div",{staticClass:"vmark"},[t("h2",[e._v("Table Head Cell")]),t("h3",[e._v("Properties")]),t("MetaTable",{attrs:{component:"p-table-head-cell",type:"props"}})],1)},n=[],s=a("2877"),c={},p=Object(s["a"])(c,l,n,!1,null,null,null);t["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d0cc660.6c8ec0cd.js.map
