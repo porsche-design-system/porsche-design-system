98e6d5a98915dbc68e2d55f76c2ac9f59d4e957b
Henri Joss <henri.joss@mhp.com>
2024-08-21 09:03:25 +0200

    feat(components): adjust test | hj | #3358

--- packages/components/src/components/textarea/textarea-styles.spec.ts
+++ packages/components/src/components/textarea/textarea-styles.spec.ts
@@ -3,21 +3,21 @@ import { validateCssAndMatchSnapshot } from '../../../tests/unit/helpers';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, 'none', true, 'light'],
-    [false, false, 'none', false, 'light'],
-    [false, true, 'none', true, 'light'],
-    [false, true, 'none', false, 'light'],
-    [false, false, 'success', true, 'light'],
-    [false, false, 'success', false, 'light'],
-    [false, true, 'success', true, 'light'],
-    [false, true, 'success', false, 'light'],
-    [false, false, 'error', true, 'light'],
-    [false, false, 'error', false, 'light'],
-    [false, true, 'error', true, 'light'],
-    [false, true, 'error', false, 'light'],
-    [true, true, 'error', false, 'light'],
-    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'light'],
-    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'dark'],
+    [false, false, 'none', true, 'vertical', 'light'],
+    [false, false, 'none', false, 'vertical', 'light'],
+    [false, true, 'none', true, 'vertical', 'light'],
+    [false, true, 'none', false, 'vertical', 'light'],
+    [false, false, 'success', true, 'vertical', 'light'],
+    [false, false, 'success', false, 'vertical', 'light'],
+    [false, true, 'success', true, 'vertical', 'light'],
+    [false, true, 'success', false, 'vertical', 'light'],
+    [false, false, 'error', true, 'vertical', 'light'],
+    [false, false, 'error', false, 'vertical', 'light'],
+    [false, true, 'error', true, 'vertical', 'light'],
+    [false, true, 'error', false, 'vertical', 'light'],
+    [true, true, 'error', false, 'vertical', 'light'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'vertical', 'light'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'vertical', 'dark'],
   ])('should return correct css for isDisabled: %s, hideLabel: %o, state: %s, hasCounter: %s, theme: %s', (...args) => {
     validateCssAndMatchSnapshot(getComponentCss(...args));
   });
