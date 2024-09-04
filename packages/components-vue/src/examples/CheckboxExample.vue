89c65d5bc6de7a4431c5811964a4195b7f878131
kaiszybiak <kai.szybiak@netrunners.com>
2024-09-04 23:33:50 +0200

    test(components-vue): add checkbox examples | ks | #3386

---
+++ packages/components-vue/src/examples/CheckboxExample.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import { PCheckbox, PText } from '@porsche-design-system/components-vue';
+  import { ref } from 'vue';
+
+  const lastSubmittedData = ref('');
+  const onSubmit = (e: Event) => {
+    const formData = new FormData(e.target as HTMLFormElement);
+    lastSubmittedData.value = formData.get('some-name') as string;
+  };
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <PCheckbox :name="'some-name'" :label="'Some Label'" :value="'some-value'" checked />
+    <button type="submit">Submit</button>
+  </form>
+
+  <PText>Last submitted data: {{ lastSubmittedData }}</PText>
+</template>
---
+++ packages/components-vue/src/examples/CheckboxExampleControlled.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import { PCheckbox, PText, type CheckboxUpdateEventDetail } from '@porsche-design-system/components-vue';
+  import { computed, ref } from 'vue';
+
+  const value = ref<string>('some-value');
+
+  const onUpdate = (e: CheckboxUpdateEventDetail) => {
+    console.log(e);
+    value.value = e.checked ? e.value : undefined;
+  };
+
+  const debugText = computed(() => {
+    return `Value: ${value.value}`;
+  });
+</script>
+
+<template>
+  <PCheckbox :name="'some-name'" :label="'Some Label'" checked :value="value" @update="onUpdate" />
+  <PText>{{ debugText }}</PText>
+</template>
