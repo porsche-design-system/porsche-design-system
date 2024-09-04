b99eeef8ab8aec7b9d5dfba8167182bfd86b8604
kaiszybiak <kai.szybiak@netrunners.com>
2024-09-04 23:34:47 +0200

    feat(components): update checkbox type | ks | #3386

--- packages/components/src/components/checkbox/checkbox-utils.ts
+++ packages/components/src/components/checkbox/checkbox-utils.ts
@@ -2,4 +2,4 @@ import type { FormState } from '../../utils/form/form-state';
 
 export type CheckboxState = FormState;
 
-export type CheckboxUpdateEventDetail = { name: string; value: string; checked: boolean };
+export type CheckboxUpdateEventDetail = { name: string; value?: string; checked: boolean };
