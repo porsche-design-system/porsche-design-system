3c456e422e569b1a401b16c534e462c0e3399fed
kaiszybiak <kai.szybiak@netrunners.com>
2024-09-04 20:36:31 +0200

    test(components): add unit test for checkbox | ks | #3386

---
+++ packages/components/src/components/checkbox/checkbox.spec.ts
@@ -0,0 +1,124 @@
+import { Checkbox } from './checkbox';
+import * as applyConstructableStylesheetStylesUtils from '../../utils/applyConstructableStylesheetStyle';
+import { getSlottedAnchorStyles } from '../../styles';
+import { expect } from '@jest/globals';
+import { getCheckboxRadioButtonSafariRenderingFix } from '../../utils/form/applyCheckboxRadioButtonSafariRenderingFix';
+
+jest.mock('../../utils/dom');
+
+class MockElementInternals {
+  setValidity = jest.fn();
+  setFormValue = jest.fn();
+}
+
+let mockEmit: jest.SpyInstance;
+
+const initComponent = (): Checkbox => {
+  const component = new Checkbox();
+  component.host = document.createElement('p-checkbox');
+  component.host.attachShadow({ mode: 'open' });
+  const input = document.createElement('input');
+  input.type = 'checkbox';
+  component.host.shadowRoot.appendChild(input);
+  component['input'] = input;
+  component['internals'] = new MockElementInternals() as unknown as ElementInternals;
+
+  mockEmit = jest.fn();
+
+  component.update = { emit: mockEmit } as any;
+  return component;
+};
+
+describe('connectedCallback', () => {
+  it('should call applyConstructableStylesheetStyles() with correct parameters', () => {
+    const applyConstructableStylesheetStylesSpy = jest.spyOn(
+      applyConstructableStylesheetStylesUtils,
+      'applyConstructableStylesheetStyles'
+    );
+    const component = initComponent();
+
+    component.connectedCallback();
+    expect(applyConstructableStylesheetStylesSpy).toHaveBeenCalledWith(
+      component.host,
+      getSlottedAnchorStyles,
+      getCheckboxRadioButtonSafariRenderingFix
+    );
+  });
+});
+
+describe('formResetCallback', () => {
+  const value = 'test-value';
+  const component = initComponent();
+  component.value = value;
+  const setValiditySpy = jest.spyOn(component['internals'], 'setValidity' as any);
+  const setFormValueSpy = jest.spyOn(component['internals'], 'setFormValue' as any);
+  component.formResetCallback();
+  expect(setValiditySpy).toHaveBeenCalledWith({});
+  expect(setFormValueSpy).toHaveBeenCalledWith(null);
+  expect(component.value).toBe(value);
+});
+
+describe('componentDidLoad', () => {
+  it('should call setFormValue() on componentDidLoad() if checkbox is checked', () => {
+    const component = initComponent();
+    component.value = 'test';
+    component['input'].checked = true;
+    const setFormValueSpy = jest.spyOn(component['internals'], 'setFormValue' as any);
+    component.componentDidLoad();
+    expect(setFormValueSpy).toHaveBeenCalledWith(component.value);
+  });
+
+  it('should not call setFormValue() on componentDidLoad() if checkbox is checked', () => {
+    const component = initComponent();
+    component.value = 'test';
+    const setFormValueSpy = jest.spyOn(component['internals'], 'setFormValue' as any);
+    component.componentDidLoad();
+    expect(setFormValueSpy).not.toHaveBeenCalled();
+  });
+});
+
+describe('onChange', () => {
+  it('should call setFormValue() and emit update event', () => {
+    const value = 'test-value';
+    const name = 'test-name';
+    const checked = true;
+    const component = initComponent();
+    component.name = name;
+    component.value = value;
+    const setFormValueSpy = jest.spyOn(component['internals'], 'setFormValue' as any);
+
+    const event = {
+      stopPropagation: jest.fn(),
+      stopImmediatePropagation: jest.fn(),
+      target: {
+        checked,
+      },
+    } as unknown as Event;
+
+    component['onChange'](event);
+    expect(setFormValueSpy).toHaveBeenCalledWith(value);
+    expect(mockEmit).toHaveBeenCalledWith({ name, value, checked });
+  });
+
+  it('should reset form value if checkbox is not checked', () => {
+    const value = 'test-value';
+    const name = 'test-name';
+    const checked = false;
+    const component = initComponent();
+    component.name = name;
+    component.value = value;
+    const setFormValueSpy = jest.spyOn(component['internals'], 'setFormValue' as any);
+
+    const event = {
+      stopPropagation: jest.fn(),
+      stopImmediatePropagation: jest.fn(),
+      target: {
+        checked,
+      },
+    } as unknown as Event;
+
+    component['onChange'](event);
+    expect(setFormValueSpy).toHaveBeenCalledWith(null);
+    expect(mockEmit).toHaveBeenCalledWith({ name, value: null, checked });
+  });
+});
