3aa8c3bca11ca3eb08e7faecc6051b107c032513
Michael Worm <michael.worm@mhp.com>
2024-09-11 09:07:39 +0200

    feat(utilities): Update AG Grid unit test snapshot | mw | #3458

--- packages/utilities/projects/ag-grid/tests/unit/specs/ag-grid.spec.ts
+++ packages/utilities/projects/ag-grid/tests/unit/specs/ag-grid.spec.ts
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import path from 'path';
+
 test('should match snapshot in theme light', () => {
-  const css = fs.readFileSync(path.resolve(__dirname, '../../../dist/theme-light.css'), 'utf8');
+  const css = fs.readFileSync(path.resolve(__dirname, '../../../dist/theme-pds.css'), 'utf8');
   expect(css).toMatchSnapshot();
 });
 
 test('should match snapshot in theme dark', () => {
-  const css = fs.readFileSync(path.resolve(__dirname, '../../../dist/theme-dark.css'), 'utf8');
+  const css = fs.readFileSync(path.resolve(__dirname, '../../../dist/theme-pds.css'), 'utf8');
   expect(css).toMatchSnapshot();
 });
