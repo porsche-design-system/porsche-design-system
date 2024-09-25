916c50a1f846a238e6d675588127c5b6e2d1833a
kaiszybiak <kai.szybiak@netrunners.com>
2024-09-25 19:14:45 +0200

    refactor(components): prevent background scrollbar overlaying modal scrollbar | ks | #3509

--- packages/components/src/utils/setScrollLock.ts
+++ packages/components/src/utils/setScrollLock.ts
@@ -1,3 +1,16 @@
+export const scrollLockStyles = `
+      html, body {
+        overflow: hidden; /* prevent scrolling on the background */
+      }
+      html::-webkit-scrollbar,
+      body::-webkit-scrollbar {
+        display: none; /* hide scrollbar in WebKit browsers */
+      }
+      html, body {
+        scrollbar-width: none; /* hide scrollbar in Firefox */
+      }
+    `;
+
 export const setScrollLock = (isOpen: boolean): void => {
   // This doesn't work reliably in iOS Safari with collapsed address bar where body is still scrollable
   // there are libraries like `body-scroll-lock` and `body-scroll-lock-upgrade` which advertise to handle it, but they
@@ -5,5 +18,18 @@ export const setScrollLock = (isOpen: boolean): void => {
   // https://github.com/willmcpo/body-scroll-lock/blob/master/src/bodyScrollLock.js
   // https://github.com/rick-liruixin/body-scroll-lock-upgrade/blob/develop/src/body-scroll-lock.ts
   // Since solutions where the html and body nodes are manipulated can cause all kinds of other problems, we keep this simple solution for now
-  document.body.style.overflow = isOpen ? 'hidden' : '';
+  const styleAttribute = 'data-pds-scroll-lock-styles';
+  let styleElement = document.querySelector(`style[${styleAttribute}]`);
+
+  if (!styleElement) {
+    styleElement = document.createElement('style');
+    styleElement.setAttribute(styleAttribute, '');
+    document.head.appendChild(styleElement);
+  }
+
+  if (isOpen) {
+    styleElement.innerHTML = scrollLockStyles;
+  } else {
+    document.head.removeChild(styleElement);
+  }
 };
