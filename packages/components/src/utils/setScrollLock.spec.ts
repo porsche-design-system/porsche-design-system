63f2bac7fba74677a2a7a2693e7bf12c99038abe
kaiszybiak <kai.szybiak@netrunners.com>
2024-09-25 19:15:24 +0200

    test(components): update setScrollbarLock tests | ks | #3509

--- packages/components/src/utils/setScrollLock.spec.ts
+++ packages/components/src/utils/setScrollLock.spec.ts
@@ -1,57 +1,103 @@
-import { setScrollLock } from './setScrollLock';
+import { scrollLockStyles, setScrollLock } from './setScrollLock';
 
-it('should add correct body styles on Desktop Safari for parameter isOpen: true', () => {
-  jest.spyOn(window, 'navigator', 'get').mockImplementation(
-    () =>
-      ({
-        userAgent:
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
-      }) as any
-  );
-  setScrollLock(true);
+describe('setScrollLock', () => {
+  const styleAttribute = 'data-pds-scroll-lock-styles';
 
-  expect(document.body.style.overflow).toBe('hidden');
-});
+  afterEach(() => {
+    const styleElement = document.querySelector(`style[${styleAttribute}]`);
+    if (styleElement) {
+      document.head.removeChild(styleElement);
+    }
+  });
+  it('should add correct body styles on Desktop Safari for parameter isOpen: true', () => {
+    jest.spyOn(window, 'navigator', 'get').mockImplementation(
+      () =>
+        ({
+          userAgent:
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
+        }) as any
+    );
 
-it('should add correct body styles on Desktop Chrome for parameter isOpen: true', () => {
-  jest.spyOn(window, 'navigator', 'get').mockImplementation(
-    () =>
-      ({
-        userAgent:
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-      }) as any
-  );
-  setScrollLock(true);
+    setScrollLock(true);
 
-  expect(document.body.style.overflow).toBe('hidden');
-});
+    const styleElement = document.querySelector(`style[${styleAttribute}]`);
+    expect(styleElement).not.toBeNull();
+    expect(styleElement.innerHTML).toContain(scrollLockStyles);
+  });
 
-it('should add correct body styles on Desktop Firefox for parameter isOpen: true', () => {
-  jest.spyOn(window, 'navigator', 'get').mockImplementation(
-    () =>
-      ({
-        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0',
-      }) as any
-  );
-  setScrollLock(true);
+  it('should add correct body styles on Desktop Chrome for parameter isOpen: true', () => {
+    jest.spyOn(window, 'navigator', 'get').mockImplementation(
+      () =>
+        ({
+          userAgent:
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        }) as any
+    );
+    setScrollLock(true);
 
-  expect(document.body.style.overflow).toBe('hidden');
-});
+    const styleElement = document.querySelector(`style[${styleAttribute}]`);
+    expect(styleElement).not.toBeNull();
+    expect(styleElement.innerHTML).toContain(scrollLockStyles);
+  });
 
-it('should only remove previously added body styles on Desktop Safari for parameter isOpen: false', () => {
-  jest.spyOn(window, 'navigator', 'get').mockImplementation(
-    () =>
-      ({
-        userAgent:
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
-      }) as any
-  );
+  it('should add correct body styles on Desktop Firefox for parameter isOpen: true', () => {
+    jest.spyOn(window, 'navigator', 'get').mockImplementation(
+      () =>
+        ({
+          userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0',
+        }) as any
+    );
+    setScrollLock(true);
 
-  document.body.style.display = 'flex';
-  document.body.style.top = '0px';
-  setScrollLock(true);
-  setScrollLock(false);
+    const styleElement = document.querySelector(`style[${styleAttribute}]`);
+    expect(styleElement).not.toBeNull();
+    expect(styleElement.innerHTML).toContain(scrollLockStyles);
+  });
 
-  expect(document.body.style.overflow).toBe('');
-  expect(document.body.getAttribute('style')).toBe('display: flex; top: 0px;');
+  it('should remove the previously added body styles on Desktop Safari for parameter isOpen: false', () => {
+    jest.spyOn(window, 'navigator', 'get').mockImplementation(
+      () =>
+        ({
+          userAgent:
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15',
+        }) as any
+    );
+
+    setScrollLock(true);
+    setScrollLock(false);
+
+    const styleElement = document.querySelector(`style[${styleAttribute}]`);
+    expect(styleElement).toBeNull();
+  });
+
+  it('should remove the previously added body styles on Desktop Chrome for parameter isOpen: false', () => {
+    jest.spyOn(window, 'navigator', 'get').mockImplementation(
+      () =>
+        ({
+          userAgent:
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        }) as any
+    );
+
+    setScrollLock(true);
+    setScrollLock(false);
+
+    const styleElement = document.querySelector(`style[${styleAttribute}]`);
+    expect(styleElement).toBeNull();
+  });
+
+  it('should remove the previously added body styles on Desktop Firefox for parameter isOpen: false', () => {
+    jest.spyOn(window, 'navigator', 'get').mockImplementation(
+      () =>
+        ({
+          userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/114.0',
+        }) as any
+    );
+
+    setScrollLock(true);
+    setScrollLock(false);
+
+    const styleElement = document.querySelector(`style[${styleAttribute}]`);
+    expect(styleElement).toBeNull();
+  });
 });
