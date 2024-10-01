e13140da632b54057af2614c3740cbb7eee05735
Henri Joss <henri.joss@mhp.com>
2024-10-01 13:32:24 +0200

    feat(storefront): add stackblitz as frame-src | hj | #3458

--- packages/storefront/public/getCSPMetaTag.ts
+++ packages/storefront/public/getCSPMetaTag.ts
@@ -1,5 +1,8 @@
 export const getCSPMetaTag = (): string => {
-  const cdnUrl = 'https://cdn.ui.porsche.com';
+  const cdnUrl =
+    process.env.NODE_ENV === 'production'
+      ? 'https://cdn.ui.porsche.com'
+      : 'http://localhost:8080 http://localhost:3001';
 
   const connectUrls = [
     'https://*.algolia.net',
@@ -7,14 +10,14 @@ export const getCSPMetaTag = (): string => {
     'https://registry.npmjs.org/@porsche-design-system/components-js',
   ].join(' ');
 
-  return process.env.NODE_ENV === 'production'
-    ? `
+  // TODO: Get rid of 'unsafe-inline' 'unsafe-eval' for script-src (difficult to get embedded Stackblitz example working)
+  return `
     <meta http-equiv="Content-Security-Policy" content="
       default-src 'self' ${cdnUrl};
+      frame-src https://stackblitz.com;
       style-src 'self' 'unsafe-inline' ${cdnUrl};
       script-src 'self' 'unsafe-inline' 'unsafe-eval' ${cdnUrl};
       img-src 'self' ${cdnUrl} data:;
       media-src 'self' https://porsche-design-system.github.io;
-      connect-src 'self' ${connectUrls}" />`
-    : '';
+      connect-src 'self' ${connectUrls}" />`;
 };
