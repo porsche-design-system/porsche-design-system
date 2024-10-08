bab50baaf372a8e20bd3c72666633cdcf004ecca
Henri Joss <henri.joss@mhp.com>
2024-10-08 10:50:19 +0200

    test(component-meta): include test files | hj | #3499

--- packages/component-meta/tests/unit/config/vitest.config.ts
+++ packages/component-meta/tests/unit/config/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['**/tests/unit/specs/**/*.spec.ts'],
+    include: ['**/tests/unit/specs/**/*.spec.ts', '**/src/**/*.spec.ts'],
     globals: true,
   },
 });
