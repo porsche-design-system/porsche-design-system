10e543757fe9748fa7bd5ff8cb28f4d3826c8abd
kaiszybiak <kai.szybiak@netrunners.com>
2024-11-04 12:19:07 +0100

    test(components): update unit tests for pin-code component | ks | #3455

--- packages/components/src/components/pin-code/pin-code-styles.spec.ts
+++ packages/components/src/components/pin-code/pin-code-styles.spec.ts
@@ -3,21 +3,21 @@ import { validateCssAndMatchSnapshot } from '../../../tests/unit/helpers';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, 'none', false, false, false, 1, 'light'],
-    [false, 'none', false, false, false, 2, 'light'],
-    [false, 'none', false, false, false, 3, 'light'],
-    [false, 'none', false, false, false, 4, 'light'],
-    [false, 'none', false, false, false, 5, 'light'],
-    [false, 'none', false, true, true, 6, 'light'],
-    [false, 'none', true, false, false, 1, 'light'],
-    [false, 'none', true, false, false, 2, 'light'],
-    [false, 'none', true, false, false, 3, 'light'],
-    [false, 'none', true, false, false, 4, 'light'],
-    [false, 'none', true, false, false, 5, 'light'],
-    [true, 'error', false, false, true, 6, 'dark'],
-    [{ base: true, xs: false, s: true, m: false, l: true, xl: false }, 'success', false, false, false, 4, 'light'],
+    [false, 'none', false, false, 1, 'light'],
+    [false, 'none', false, false, 2, 'light'],
+    [false, 'none', false, false, 3, 'light'],
+    [false, 'none', false, false, 4, 'light'],
+    [false, 'none', false, false, 5, 'light'],
+    [false, 'none', false, true, 6, 'light'],
+    [false, 'none', true, false, 1, 'light'],
+    [false, 'none', true, false, 2, 'light'],
+    [false, 'none', true, false, 3, 'light'],
+    [false, 'none', true, false, 4, 'light'],
+    [false, 'none', true, false, 5, 'light'],
+    [true, 'error', false, false, 6, 'dark'],
+    [{ base: true, xs: false, s: true, m: false, l: true, xl: false }, 'success', false, false, 4, 'light'],
   ])(
-    'should return correct css for hideLabel: %o, state: %s, isDisabled: %s, isLoading: %s, isWithinForm: %s, length: %s and theme: %s',
+    'should return correct css for hideLabel: %o, state: %s, isDisabled: %s, isLoading: %s, length: %s and theme: %s',
     (...args) => {
       validateCssAndMatchSnapshot(getComponentCss(...args));
     }
