20fd67e5d56c9b86f57ae8cc6bb876eb689dbd3b
Henri Joss <henri.joss@mhp.com>
2024-11-06 17:55:41 +0100

    test(components-vue): modernize eslint config | hj / sas | #housekeeping/update-dependencies-24-10-29

--- packages/components-vue/eslint.config.mjs
+++ packages/components-vue/eslint.config.mjs
@@ -1,15 +1,13 @@
 import eslintConfig from '@porsche-design-system/shared/eslint/index.js';
-// import pluginVue from 'eslint-plugin-vue'
-// import vueEslintConfigTypescript from "@vue/eslint-config-typescript";
-// import vueEslintPrettierConfig from "@vue/eslint-config-prettier";
-
-// console.log(vueEslintPluginVue.configs['flat/recommended'])
+import pluginVue from 'eslint-plugin-vue';
+import vueEslintConfigTypescript from '@vue/eslint-config-typescript';
+import vueEslintPrettierConfig from '@vue/eslint-config-prettier';
 
 export default [
   ...eslintConfig,
-  // ...pluginVue.configs['flat/recommended'],
-  // ...vueEslintConfigTypescript(),
-  // vueEslintPrettierConfig,
+  ...pluginVue.configs['flat/recommended'],
+  ...vueEslintConfigTypescript(),
+  vueEslintPrettierConfig,
   {
     ignores: ['projects/vue-wrapper/src/lib/'],
     rules: {
