f79b36360b141eca451b812ef148a184d975818a
Robert Hilscher <r.hilscher@gmail.com>
2024-11-06 09:45:23 +0100

    Make StepperHorizontal controlled, prepare preset for LinkTileProduct

---
+++ packages/components-react/projects/uxpin-wrapper/src/wrappers/StepperHorizontal/presets/0-default.jsx
@@ -0,0 +1,10 @@
+import { StepperHorizontal } from '../StepperHorizontal';
+import { StepperHorizontalItem } from '../../StepperHorizontalItem';
+
+export default (
+  <StepperHorizontal uxpId="stepper-horizontal" activeStepIndex={0}>
+    <StepperHorizontalItem uxpId="stepper-horizontal-step-1" state="current">Enter personal details</StepperHorizontalItem>
+    <StepperHorizontalItem uxpId="stepper-horizontal-step-2">Confirm e-mail</StepperHorizontalItem>
+    <StepperHorizontalItem uxpId="stepper-horizontal-step-3">Set password</StepperHorizontalItem>
+  </StepperHorizontal>
+);
