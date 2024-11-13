7c1afd60302fd00d5c8658eb5ee0c07987485ee0
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-11-13 16:56:14 +0100

    feat: apply biome lint to components-vue | sas / hj | #3569

--- packages/components-vue/src/examples/AGGridExample.vue
+++ packages/components-vue/src/examples/AGGridExample.vue
@@ -1,27 +1,28 @@
 <script setup lang="ts">
-  import 'ag-grid-enterprise';
-  import { AgGridVue } from 'ag-grid-vue3'; // Vue Data Grid Component
-  import { dataAdvanced } from '@porsche-design-system/shared';
-  import { PLinkPure } from '@porsche-design-system/components-vue';
+import 'ag-grid-enterprise';
+import { PLinkPure } from '@porsche-design-system/components-vue';
+import { dataAdvanced } from '@porsche-design-system/shared';
+import { AgGridVue } from 'ag-grid-vue3'; // Vue Data Grid Component
 
-  const theme = 'light';
+const theme = 'light';
 
-  const ImageUrlRendererer = {
-    template: `
+const ImageUrlRendererer = {
+  template: `
       <img :src="cellValue.replace('/porsche-design-system/', '/dummyasset/')" width="80" height="45" alt="" />
     `,
-    setup(props: any) {
-      return {
-        cellValue: props.params.value,
-      };
-    },
-  };
+  // biome-ignore lint/suspicious/noExplicitAny: no typing available by AG Grid
+  setup(props: any) {
+    return {
+      cellValue: props.params.value,
+    };
+  },
+};
 
-  const ButtonRenderer = {
-    components: {
-      PLinkPure,
-    },
-    template: `
+const ButtonRenderer = {
+  components: {
+    PLinkPure,
+  },
+  template: `
       <span class="cell-centered">
       <PLinkPure
         :underline="true"
@@ -33,76 +34,77 @@
       </PLinkPure>
       </span>
     `,
-    setup(props: any) {
-      return {
-        theme,
-        data: props.params.data,
-      };
-    },
-  };
+  // biome-ignore lint/suspicious/noExplicitAny: no typing available by AG Grid
+  setup(props: any) {
+    return {
+      theme,
+      data: props.params.data,
+    };
+  },
+};
 
-  const rowData = dataAdvanced.map((row, index) => ({ active: Boolean(index % 2) /* odd rows */, ...row }));
+const rowData = dataAdvanced.map((row, index) => ({ active: Boolean(index % 2) /* odd rows */, ...row }));
 
-  const columnDefs = [
-    {
-      field: 'active',
-      showDisabledCheckboxes: true,
-      width: 170,
-    },
-    {
-      field: 'imageUrl',
-      headerName: 'Image',
-      cellRenderer: ImageUrlRendererer,
-      editable: false,
-      filter: false,
-      sortable: false,
-      width: 130,
-    },
-    {
-      field: 'model',
-      editable: false,
-    },
-    {
-      field: 'date',
-      editable: false,
-    },
-    {
-      field: 'interest',
-      editable: false,
-    },
-    {
-      field: 'vin',
-      width: 250,
-      editable: false,
-    },
-    {
-      field: 'purchaseIntention',
-      editable: false,
-    },
-    {
-      field: 'status',
-      editable: false,
-    },
-    {
-      field: 'comment',
-      filter: false,
-      width: 500,
-    },
-    {
-      field: 'leadId',
-      headerName: 'More',
-      cellRenderer: ButtonRenderer,
-      editable: false,
-      sortable: false,
-      filter: false,
-    },
-  ];
+const columnDefs = [
+  {
+    field: 'active',
+    showDisabledCheckboxes: true,
+    width: 170,
+  },
+  {
+    field: 'imageUrl',
+    headerName: 'Image',
+    cellRenderer: ImageUrlRendererer,
+    editable: false,
+    filter: false,
+    sortable: false,
+    width: 130,
+  },
+  {
+    field: 'model',
+    editable: false,
+  },
+  {
+    field: 'date',
+    editable: false,
+  },
+  {
+    field: 'interest',
+    editable: false,
+  },
+  {
+    field: 'vin',
+    width: 250,
+    editable: false,
+  },
+  {
+    field: 'purchaseIntention',
+    editable: false,
+  },
+  {
+    field: 'status',
+    editable: false,
+  },
+  {
+    field: 'comment',
+    filter: false,
+    width: 500,
+  },
+  {
+    field: 'leadId',
+    headerName: 'More',
+    cellRenderer: ButtonRenderer,
+    editable: false,
+    sortable: false,
+    filter: false,
+  },
+];
 
-  // Configurations applied to all columns
-  const defaultColDef = {
-    filter: true,
-    editable: true,
-  };
+// Configurations applied to all columns
+const defaultColDef = {
+  filter: true,
+  editable: true,
+};
 </script>
 
 <template>
