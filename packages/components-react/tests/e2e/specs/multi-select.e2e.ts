32660cbbe199f87330ef4ffbac66a3a79c538f7c
kaiszybiak <kai.szybiak@netrunners.com>
2024-11-15 13:03:30 +0100

    test(components-react): update e2e tests | ks | #3455

--- packages/components-react/tests/e2e/specs/multi-select.e2e.ts
+++ packages/components-react/tests/e2e/specs/multi-select.e2e.ts
@@ -4,14 +4,20 @@ import { goto, waitForComponentsReady } from '../helpers';
 
 const getHost = (page: Page) => page.locator('p-multi-select');
 
-test('should reset multi-select value to its initial value on form reset', async ({ page }) => {
-  await goto(page, 'multi-select-example');
-  await waitForComponentsReady(page);
-  const host = getHost(page);
-  const newValue = ['b'];
-  await setProperty(host, 'value', newValue);
-  await expect(host).toHaveJSProperty('value', newValue);
+test.describe('form', () => {
+  test('should reset multi-select value to its initial value on form reset', async ({ page }) => {
+    await goto(page, 'multi-select-example');
+    await waitForComponentsReady(page);
+    const host = getHost(page);
+    const newValue = ['b'];
+    await setProperty(host, 'value', newValue);
+    await expect(host).toHaveJSProperty('value', newValue);
 
-  await page.locator('button[type="reset"]').click();
-  await expect(host).toHaveJSProperty('value', ['a']);
+    await page.locator('button[type="reset"]').click();
+    await expect(host).toHaveJSProperty('value', []);
+  });
+
+  test('should include name & value in FormData submit if updated programmatically', async ({ page }) => {
+    // TODO fix multi-select and implement test case
+  });
 });
