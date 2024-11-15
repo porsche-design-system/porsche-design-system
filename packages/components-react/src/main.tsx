3361f23a3903a0a00560c893d7c450d1dc3b48c9
Henri Joss <henri.joss@mhp.com>
2024-11-15 16:53:04 +0100

    feat(components-react): opt into v7_startTransition & v7_relativeSplatPath to resolve warnings | hj

--- packages/components-react/src/main.tsx
+++ packages/components-react/src/main.tsx
@@ -7,7 +7,12 @@ import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <App />
     </Router>
   </StrictMode>
