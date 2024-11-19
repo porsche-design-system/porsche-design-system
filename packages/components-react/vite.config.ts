5cb47f1f2df05f363a3ab7567916308c0b6734f2
Henri Joss <henri.joss@mhp.com>
2024-11-19 14:16:01 +0100

    feat(components-react): remove comments | hj

--- packages/components-react/vite.config.ts
+++ packages/components-react/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import * as partials from '@porsche-design-system/components-react/partials';
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
