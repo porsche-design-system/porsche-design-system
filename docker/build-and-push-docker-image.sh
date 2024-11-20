d52c6d7a0da99d7e86d9b26b0934a9cfa1ad6870
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-11-20 20:08:27 +0100

    feat: update docker image | sas

--- docker/build-and-push-docker-image.sh
+++ docker/build-and-push-docker-image.sh
@@ -2,7 +2,7 @@
 
 REGISTRY=ghcr.io/porsche-design-system/porsche-design-system
 IMAGE=playwright
-TAG=v1.42.1-focal-23
+TAG=v1.42.1-focal-24
 
 docker pull $REGISTRY/$IMAGE:latest
 docker build -f Dockerfile -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
