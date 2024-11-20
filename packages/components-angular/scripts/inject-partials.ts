a5e9a8ce7ce079b54b6bd6d8f7425d8d0d72b93e
Henri Joss <henri.joss@mhp.com>
2024-11-20 14:46:23 +0100

    feat(components-angular): inject partials with postbuild script | hj

--- packages/components-angular/scripts/inject-partials.ts
+++ packages/components-angular/scripts/inject-partials.ts
@@ -7,15 +7,14 @@ const indexHtmlPath = path.join(__dirname, '../dist/demo-app/index.html');
 
 fs.readFile(indexHtmlPath, 'utf-8', (err, indexHtml) => {
   if (err) {
-    console.error('Error reading index.html:', err);
-    return;
+    throw new Error('Error reading index.html:', err);
   }
 
   const updatedIndexHtml = injectPartials({}, indexHtml);
 
   fs.writeFile(indexHtmlPath, updatedIndexHtml, 'utf-8', (err) => {
     if (err) {
-      console.error('Error injecting partials into index.html:', err);
+      throw new Error('Error injecting partials into index.html:', err);
     } else {
       console.log('Partials injected into index.html successfully.');
     }
