ce81ff60f4a9e4e83bd607513c8b5960463e188d
Stephan Schröter <stephan.schroeter@porsche.digital>
2024-11-20 19:13:14 +0100

    feat: remove aws | sas

--- aws/lambda/cloud-front-s3-logs-reorganizer/index.js
+++
@@ -1,8 +0,0 @@
-const { renameAndReorganizeLogFile } = require('./utils');
-
-exports.handler = async (event) => {
-  for (const record of event.Records) {
-    const { key } = record.s3.object;
-    await renameAndReorganizeLogFile(key);
-  }
-};
