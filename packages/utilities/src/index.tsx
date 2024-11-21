cd2c52c69abb35368a200d12c7e9b569cf6df6af
Henri Joss <henri.joss@mhp.com>
2024-11-21 14:56:12 +0100

    feat(utilities): remove react app | hj

--- packages/utilities/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<StrictMode></StrictMode>);
