97fc3ab4f0e1d0ecae44841ab87dd6d7b4d0534e
kaiszybiak <kai.szybiak@netrunners.com>
2024-11-25 19:05:38 +0100

    feat(components): update optgroup-utils | ks | #3455

--- packages/components/src/components/optgroup/optgroup-utils.ts
+++ packages/components/src/components/optgroup/optgroup-utils.ts
@@ -1,17 +1,17 @@
-import type { Theme } from '../../types';
-import type { SelectOption } from '../select/select/select-utils';
-import type { MultiSelectOption } from '../multi-select/multi-select/multi-select-utils';
 import { forceUpdate } from '@stencil/core';
+import type { Theme } from '../../types';
+import type { MultiSelectOption } from '../multi-select/multi-select/multi-select-utils';
+import type { SelectOption } from '../select/select/select-utils';
 
 export type OptgroupInternalHTMLProps = {
   /** Optgroup theme (synchronized from p-select | p-multi-select) **/
   theme?: Theme;
 };
 
-// type Child = HTMLPMultiSelectOptionElement | HTMLPSelectOptionElement;
+type Child = SelectOption | MultiSelectOption;
 export const updateOptionsDisabled = (host: HTMLElement, disabled: boolean): void => {
-  Array.from(host.children).forEach((child: SelectOption | MultiSelectOption) => {
-    child.disabledParent = disabled;
+  for (const child of Array.from(host.children)) {
+    (child as Child).disabledParent = disabled ? true : (child as Child).disabled;
     forceUpdate(child);
-  });
+  }
 };
