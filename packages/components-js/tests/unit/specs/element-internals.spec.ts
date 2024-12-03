4298fef43cbdf776e2ee1f54ed0f19e25d04e911
kaiszybiak <kai.szybiak@netrunners.com>
2024-12-03 15:04:16 +0100

    test(components.js): test attach internals patch | ks | #3149

---
+++ packages/components-js/tests/unit/specs/element-internals.spec.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+/**
+ * Checks if 'attachInternals' is used without optional chaining (?.).
+ */
+const containsInvalidAttachInternals = (content: string): boolean => {
+  const regex = /\battachInternals(?!\?\.)/;
+  return regex.test(content);
+};
+
+/**
+ * Checks if 'this.internals' is accessed with a property (.) without using optional chaining (?.).
+ */
+const containsInvalidThisInternals = (content: string): boolean => {
+  const regex = /\bthis\.internals\.(?!\?\.)/;
+  return regex.test(content);
+};
+
+describe('Element Internals', () => {
+  const dirPath = './dist/components';
+
+  it('should ensure both attachInternals and this.internals use optional chaining (?.) only', () => {
+    const files = fs.readdirSync(dirPath);
+
+    files.forEach((file) => {
+      const filePath = path.join(dirPath, file);
+      const content = fs.readFileSync(filePath, 'utf-8');
+      const hasInvalidAttachInternals = containsInvalidAttachInternals(content);
+      const hasInvalidThisInternals = containsInvalidThisInternals(content);
+
+      if (hasInvalidAttachInternals || hasInvalidThisInternals) {
+        console.error(`Invalid usage found in file: ${file}`);
+      }
+
+      expect(hasInvalidAttachInternals).toBe(false);
+      expect(hasInvalidThisInternals).toBe(false);
+    });
+  });
+});
