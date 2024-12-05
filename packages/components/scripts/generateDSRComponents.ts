06ec80ac35b061e09f3bf25ea900a7473af845a0
Henri Joss <henri.joss@mhp.com>
2024-12-05 18:19:42 +0100

    feat(components): adjust dsr replacement | hj | #3547

--- packages/components/scripts/generateDSRComponents.ts
+++ packages/components/scripts/generateDSRComponents.ts
@@ -600,8 +600,8 @@ $&`
           .replace(/validateActiveIdentifier\(.*\);/g, '')
           .replace(/(inert=\{this\.props\.open \? null : )true(})/, "$1''$2") // transform true to empty string '';
           .replace(/this\.props\.primary = !activeItem \|\| activeItem\.parentElement === this\.props;/, '')
-          .replace(/this\.props\.isSecondaryDrawerVisible = !!this\.props\.activeIdentifier;/, '')
-          .replace(/this\.props\.primary/, 'this.primary');
+          .replace(/this\.props\.primary/, 'this.primary')
+          .replace(/this\.props\.isSecondaryDrawerVisible/, 'this.isSecondaryDrawerVisible');
       } else if (tagName === 'p-flyout-multilevel-item') {
         newFileContent = newFileContent
           .replace(/: Theme/g, ': any')
