b7b2b5c56f9ecdb8818db08ef34dff75e690a085
Henri Joss <henri.joss@mhp.com>
2024-12-10 15:59:15 +0100

    test(styles): add comments | hj | #2817

--- packages/utilities/projects/utilities/rollup-test-ve.config.js
+++ packages/utilities/projects/utilities/rollup-test-ve.config.js
@@ -1,14 +1,16 @@
 import { vanillaExtractPlugin } from '@vanilla-extract/rollup-plugin';
 
+// Used in order to compile some styles with vanilla-extract and use the compiled css in unit tests
 export default {
   input: 'tests/unit/specs/vanilla-extract-test.css.ts',
   plugins: [
     vanillaExtractPlugin({
+      // Use .test as class identifier
       identifiers: ({ hash }) => 'test',
     }),
   ],
   output: {
-    dir: 'tests/unit/specs/generated', // Specify the output directory
+    dir: 'tests/unit/specs/generated',
     assetFileNames({ name }) {
       return 'vanilla-extract.css';
     },
