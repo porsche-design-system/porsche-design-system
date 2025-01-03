43b98fe9c569821394d5eb7b6ddb76c0d4331844
Henri Joss <henri.joss@mhp.com>
2025-01-03 15:19:47 +0100

    feat: update playwright docker image | hj

--- docker/build-and-push-docker-image.sh
+++ docker/build-and-push-docker-image.sh
@@ -2,7 +2,7 @@
 
 REGISTRY=ghcr.io/porsche-design-system/porsche-design-system
 IMAGE=playwright
-TAG=v1.49.1-noble
+TAG=v1.49.1-jammy
 
 docker pull $REGISTRY/$IMAGE:latest
 docker build -f Dockerfile -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
