34a7a21bfcd3741ddc2f1ae2ec80acc18ae45bb6
kaiszybiak <kai.szybiak@netrunners.com>
2025-01-07 22:10:21 +0100

    test(components): verify form and name props are reflected | ks | #3149

---
+++ packages/components/tests/unit/specs/element-internals.spec.ts
@@ -0,0 +1,26 @@
+import { expect } from '@jest/globals';
+import { getComponentMeta } from '@porsche-design-system/component-meta';
+import { TAG_NAMES } from '@porsche-design-system/shared';
+
+/**
+ * Reflecting 'form' and 'name' as an attribute ensures it is properly handled in the form submission process when using ElementInternals API.
+ */
+describe('Element Internals', () => {
+  const amountOfComponentsUsingElementInternalsApi = 8;
+
+  it('should have certain amount of components', () => {
+    const componentsWithElementInternals = TAG_NAMES.filter((tagName) => getComponentMeta(tagName).hasElementInternals);
+    expect(componentsWithElementInternals.length).toBe(amountOfComponentsUsingElementInternalsApi);
+  });
+
+  TAG_NAMES.forEach((tagName) => {
+    const componentMeta = getComponentMeta(tagName);
+
+    if (componentMeta.hasElementInternals) {
+      it(`should reflect 'name' and 'form' props for component ${tagName} using ElementInternals API`, () => {
+        expect(componentMeta.propsMeta['form'].propOptions['reflect']).toBe(true);
+        expect(componentMeta.propsMeta['name'].propOptions['reflect']).toBe(true);
+      });
+    }
+  });
+});
