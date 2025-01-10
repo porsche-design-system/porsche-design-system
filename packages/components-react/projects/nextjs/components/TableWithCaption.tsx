d1e2f2fb9ebcc585e90f00256ede32d30cf62133
Henri Joss <henri.joss@mhp.com>
2025-01-10 11:26:03 +0100

    test(components-react/next-js): fix example | hj | #3402

--- packages/components-react/projects/nextjs/components/TableWithCaption.tsx
+++ packages/components-react/projects/nextjs/components/TableWithCaption.tsx
@@ -7,10 +7,11 @@ import {
   PTableHeadCell,
   PTableHeadRow,
   PTableRow,
+  type TableHeadCellSort,
   type Theme,
 } from '@porsche-design-system/components-react/ssr';
+import { dataVrt as data, headVrt as head } from '@porsche-design-system/shared/esm/data';
 import Image from 'next/image';
-import { headVrt as head, dataVrt as data } from '@porsche-design-system/shared';
 
 // NOTE: this component is duplicated and adjusted from react
 export const TableWithCaption = ({ theme }: { theme?: Theme }): JSX.Element => {
@@ -21,7 +22,7 @@ export const TableWithCaption = ({ theme }: { theme?: Theme }): JSX.Element => {
           {head.map((item, i) => (
             <PTableHeadCell
               key={i}
-              sort={item}
+              sort={item.direction as unknown as TableHeadCellSort}
               hideLabel={item.hideLabel}
               multiline={item.multiline}
               style={item.style as React.CSSProperties}
