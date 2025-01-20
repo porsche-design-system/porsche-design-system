64503e094e8551390b24f7034254a1c0d3ebb84a
Stephan Schröter <stephan.schroeter@porsche.digital>
2025-01-20 09:52:00 +0100

    feat(storefront): render video background on frontpage only | sas | #3402

--- packages/storefront/src/components/layout/Canvas.tsx
+++ packages/storefront/src/components/layout/Canvas.tsx
@@ -12,6 +12,7 @@ import {
 } from '@porsche-design-system/components-react/ssr';
 import { breakpointS } from '@porsche-design-system/components-react/styles';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { type PropsWithChildren, useState } from 'react';
 
 export const Canvas = ({ children }: PropsWithChildren) => {
@@ -98,18 +99,20 @@ export const Canvas = ({ children }: PropsWithChildren) => {
         </PText>
       </div>
 
-      <video
-        className="w-full h-full"
-        slot="background"
-        poster="/assets/bg.webp"
-        loop={true}
-        muted={true}
-        autoPlay={true}
-        playsInline={true}
-      >
-        <source src="/assets/bg.webm" type="video/webm" />
-        <source src="/assets/bg.mp4" type="video/mp4" />
-      </video>
+      {usePathname() === '/' && (
+        <video
+          className="w-full h-full"
+          slot="background"
+          poster="/assets/bg.webp"
+          loop={true}
+          muted={true}
+          autoPlay={true}
+          playsInline={true}
+        >
+          <source src="/assets/bg.webm" type="video/webm" />
+          <source src="/assets/bg.mp4" type="video/mp4" />
+        </video>
+      )}
     </PCanvas>
   );
 };
