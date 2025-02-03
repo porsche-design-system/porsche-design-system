3330ba488887194c927e9d57aacdf9dee91cb2d2
Henri Joss <henri.joss@mhp.com>
2025-02-03 12:01:32 +0100

    feat(storefront): configurator - add switch to enable/disable slots | hj | #3402

--- packages/storefront/src/components/playground/ConfigureSlots.tsx
+++ packages/storefront/src/components/playground/ConfigureSlots.tsx
@@ -1,7 +1,7 @@
-import type { HTMLTagOrComponent, PropTypeMapping } from '@/components/playground/Configurator';
-import type { SlotStories, Story, StoryState } from '@/components/playground/componentStory';
+import type { HTMLTagOrComponent } from '@/components/playground/Configurator';
+import type { ComponentSlotStory, SlotStories, Story, StoryState } from '@/components/playground/componentStory';
 import type { SlotMeta } from '@porsche-design-system/component-meta';
-import { PPopover, PSelect, PSelectOption, PTag } from '@porsche-design-system/components-react/ssr';
+import { PPopover, PSelect, PSelectOption, PSwitch } from '@porsche-design-system/components-react/ssr';
 import { capitalCase } from 'change-case';
 import React from 'react';
 
@@ -20,51 +20,55 @@ export const ConfigureSlots = <T extends HTMLTagOrComponent>({
   slotStories,
   onUpdateSlots,
 }: ConfigureSlotsProps<T>) => {
-  const amountOfConfiguredSlots = 0;
-
   return (
     <>
       <span slot="heading" className="flex gap-xs">
         Slots
-        {amountOfConfiguredSlots > 0 && (
-          <>
-            <PTag compact={true}>{amountOfConfiguredSlots}</PTag>
-            <PTag compact={true}>
-              <button type="button" onClick={() => {}}>
-                Reset all
-              </button>
-            </PTag>
-          </>
-        )}
       </span>
       <div className="flex flex-col gap-sm">
         {Object.entries(slotStories ?? {}).map(([slotName, slotExamples]) => {
           return (
-            <PSelect
-              key={slotName}
-              name={slotName}
-              // @ts-ignore
-              value={configuredSlots?.slots?.[slotName].name}
-              onUpdate={(e) =>
-                onUpdateSlots(
-                  slotName,
-                  Object.values(slotExamples).find((slot) => slot.name === e.detail.value)
-                )
-              }
-            >
-              <span slot="label" className="inline-flex gap-xs">
+            <div key={slotName} className="flex flex-col gap-sm bg-background-shading p-sm rounded-md">
+              <PSwitch
+                checked={!!configuredSlots?.slots?.[slotName as keyof ComponentSlotStory[typeof tagName]]}
+                alignLabel="start"
+                stretch={true}
+                onUpdate={(e) => onUpdateSlots(slotName, e.detail.checked ? Object.values(slotExamples)[0] : undefined)}
+              >
                 {capitalCase(slotName)}
                 <PPopover className="ms-static-xs" onClick={(e) => e.preventDefault()}>
                   {/* TODO: Fix typing */}
                   {(componentSlots as any)?.[slotName === 'default' ? '' : slotName]?.description}
                 </PPopover>
-              </span>
-              {Object.values(slotExamples ?? {}).map((slot) => (
-                <PSelectOption key={slot.name} value={slot.name}>
-                  {slot.name}
-                </PSelectOption>
-              ))}
-            </PSelect>
+              </PSwitch>
+              <PSelect
+                className="[--p-select-background-color:bg-background-shading]"
+                name={slotName}
+                // @ts-ignore
+                value={configuredSlots?.slots?.[slotName]?.name}
+                disabled={!configuredSlots?.slots?.[slotName as keyof ComponentSlotStory[typeof tagName]]}
+                hideLabel={true}
+                onUpdate={(e) =>
+                  onUpdateSlots(
+                    slotName,
+                    Object.values(slotExamples).find((slot) => slot.name === e.detail.value)
+                  )
+                }
+              >
+                <span slot="label" className="inline-flex gap-xs">
+                  {capitalCase(slotName)}
+                  <PPopover className="ms-static-xs" onClick={(e) => e.preventDefault()}>
+                    {/* TODO: Fix typing */}
+                    {(componentSlots as any)?.[slotName === 'default' ? '' : slotName]?.description}
+                  </PPopover>
+                </span>
+                {Object.values(slotExamples ?? {}).map((slot) => (
+                  <PSelectOption key={slot.name} value={slot.name}>
+                    {slot.name}
+                  </PSelectOption>
+                ))}
+              </PSelect>
+            </div>
           );
         })}
       </div>
