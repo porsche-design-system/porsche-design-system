44110980f60dbcb6b61d36fc43a7dde48879e305
kaiszybiak <kai.szybiak@netrunners.com>
2025-02-09 10:21:17 +0100

    feat(utilities): update ag-grid icon mapping | ks | #3684

--- packages/utilities/projects/ag-grid/src/parts/checkbox-styles.ts
+++ packages/utilities/projects/ag-grid/src/parts/checkbox-styles.ts
@@ -11,8 +11,11 @@ import {
   themeLightStateDisabled,
 } from '@porsche-design-system/components-js/styles';
 import { type Part, createPart } from 'ag-grid-community';
-import { checkboxCheckedShapeImage, checkboxIndeterminateShapeImage, radioCheckedShapeImage } from '../icons';
 import { PdsThemeMode } from '../types/theme-mode';
+
+export const checkboxCheckedShapeImage = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="m20.22,7.47l-1.47-1.42-9.26,9.02-4.24-4.15-1.47,1.42,5.71,5.6,10.73-10.47Z"/></svg>`;
+export const checkboxIndeterminateShapeImage = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="m20,11v2H4v-2h16Z"/></svg>`;
+export const radioCheckedShapeImage = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="12" cy="12" r="6"/></svg>`;
 
 export const checkboxStyle: Part = createPart({
   feature: 'checkboxStyle',
