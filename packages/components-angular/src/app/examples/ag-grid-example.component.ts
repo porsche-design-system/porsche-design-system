692020368cedc19392e1a6ff549f5b5e6c19bbc6
kaiszybiak <kai.szybiak@netrunners.com>
2025-02-10 13:58:08 +0100

    feat(components-angular): remove unused | ks | #3684

--- packages/components-angular/src/app/examples/ag-grid-example.component.ts
+++ packages/components-angular/src/app/examples/ag-grid-example.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { PorscheDesignSystemModule, THEME_TOKEN, Theme } from '@porsche-design-system/components-angular';
+import { PorscheDesignSystemModule, THEME_TOKEN } from '@porsche-design-system/components-angular';
 import { pdsTheme } from '@porsche-design-system/components-angular/ag-grid';
 import { type DataAdvanced, dataAdvanced } from '@porsche-design-system/shared';
 import { type ICellRendererAngularComp } from 'ag-grid-angular';
