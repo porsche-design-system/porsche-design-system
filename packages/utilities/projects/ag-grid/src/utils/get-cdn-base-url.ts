228adbce835a9feb131d019267f8c331bb397502
kaiszybiak <kai.szybiak@netrunners.com>
2025-02-10 08:48:19 +0100

    test(utilities): update tests for ag grid theme | ks | #3684

--- packages/utilities/projects/ag-grid/src/utils/get-cdn-base-url.ts
+++ packages/utilities/projects/ag-grid/src/utils/get-cdn-base-url.ts
@@ -1,2 +1,1 @@
-//@ts-ignore
-export const getCDNBaseURL = (): string => 'http://localhost:3001'; // ROLLUP_REPLACE_CDN_BASE_URL;
+export const getCDNBaseURL = (): string => ROLLUP_REPLACE_CDN_BASE_URL;
