efd0ff67161a834da652559ad70f791cafd03692
Stephan Schröter <stephan.schroeter@porsche.digital>
2025-02-12 16:47:22 +0100

    test(components-js): add thai typography vrt scenario | sas | #3725

--- packages/components-js/tests/vrt/specs/common/typography.vrt.ts
+++ packages/components-js/tests/vrt/specs/common/typography.vrt.ts
@@ -5,7 +5,7 @@ import { setupScenario } from '../../helpers';
 
 // executed in Chrome only
 test.describe('typography', () => {
-  for (const typography of ['latin', 'greek-and-coptic', 'cyril', 'middle-east', 'fallback']) {
+  for (const typography of ['latin', 'greek-and-coptic', 'cyril', 'middle-east', 'thai', 'fallback']) {
     test.skip(({ browserName }) => browserName !== 'chromium');
 
     test(`should have no visual regression for ${typography}`, async ({ page }) => {
