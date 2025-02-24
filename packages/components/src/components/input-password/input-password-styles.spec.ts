cd02717022b7e7ece88de37ebbcadf2715cd1bb4
kaiszybiak <kai.szybiak@netrunners.com>
2025-02-24 16:58:49 +0100

    test(components): update input password styles | ks | #3389

--- packages/components/src/components/input-password/input-password-styles.spec.ts
+++ packages/components/src/components/input-password/input-password-styles.spec.ts
@@ -3,24 +3,24 @@ import { getComponentCss } from './input-password-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, false, 'none', true, 'vertical', 'light'],
-    [false, true, false, 'none', true, 'vertical', 'light'],
-    [false, false, false, 'none', false, 'vertical', 'light'],
-    [false, false, true, 'none', true, 'vertical', 'light'],
-    [false, false, true, 'none', false, 'vertical', 'light'],
-    [false, false, false, 'success', true, 'vertical', 'light'],
-    [false, false, false, 'success', false, 'vertical', 'light'],
-    [false, false, true, 'success', true, 'vertical', 'light'],
-    [false, false, true, 'success', false, 'vertical', 'light'],
-    [false, false, false, 'error', true, 'vertical', 'light'],
-    [false, false, false, 'error', false, 'vertical', 'light'],
-    [false, false, true, 'error', true, 'vertical', 'light'],
-    [false, false, true, 'error', false, 'vertical', 'light'],
-    [true, false, true, 'error', false, 'vertical', 'light'],
-    [false, false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'vertical', 'light'],
-    [false, false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'vertical', 'dark'],
+    [false, false, 'none', true, 'light'],
+    [false, false, 'none', true, 'light'],
+    [false, false, 'none', false, 'light'],
+    [false, true, 'none', true, 'light'],
+    [false, true, 'none', false, 'light'],
+    [false, false, 'success', true, 'light'],
+    [false, false, 'success', false, 'light'],
+    [false, true, 'success', true, 'light'],
+    [false, true, 'success', false, 'light'],
+    [false, false, 'error', true, 'light'],
+    [false, false, 'error', false, 'light'],
+    [false, true, 'error', true, 'light'],
+    [false, true, 'error', false, 'light'],
+    [true, true, 'error', false, 'light'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'light'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, 'dark'],
   ])(
-    'should return correct css for isDisabled: %s, isReadonly: %s, hideLabel: %o, state: %s, hasCounter: %s, theme: %s',
+    'should return correct css for isDisabled: %s, hideLabel: %o, state: %s, showPasswordToggle: %s, theme: %s',
     (...args) => {
       validateCssAndMatchSnapshot(getComponentCss(...args));
     }
