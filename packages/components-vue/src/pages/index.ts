79f2006cba085b9653e63a8ed466b821789fcb58
kaiszybiak <kai.szybiak@netrunners.com>
2025-03-19 13:52:23 +0100

    test(components-vue): add e2e tests for boolean shorthand props | ks | #3779

--- packages/components-vue/src/pages/index.ts
+++ packages/components-vue/src/pages/index.ts
@@ -1,5 +1,6 @@
 export { default as EventsPage } from './EventsPage.vue';
 export { default as OverviewPage } from './OverviewPage.vue';
+export { default as BooleanShorthandPage } from './BooleanShorthandPage.vue';
 export { default as SheetBasicPage } from './SheetBasicPage.vue';
 export { default as SheetPrefixedPage } from './SheetPrefixedPage.vue';
 export { default as BannerBasicPage } from './BannerBasicPage.vue';
