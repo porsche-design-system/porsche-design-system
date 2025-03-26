b82eabc4bc1e87fdc7274e5e965e3150faa50f87
kaiszybiak <kai.szybiak@netrunners.com>
2025-03-26 06:37:39 +0100

    test(components-js|vue): update vrt specs | ks | #3779

--- packages/components-vue/tests/vrt/specs/custom/boolean-shorthand.vrt.ts
+++ packages/components-vue/tests/vrt/specs/custom/boolean-shorthand.vrt.ts
@@ -1,17 +1,15 @@
 import { expect, test } from '@playwright/test';
-import { themes, viewportWidthM } from '@porsche-design-system/shared/testing/playwright.vrt';
+import { viewportWidthM } from '@porsche-design-system/shared/testing/playwright.vrt';
 
 test.describe('Boolean Shorthand Props', () => {
   test.skip(({ browserName }) => browserName !== 'chromium');
 
-  for (const theme of themes) {
-    test(`should have no visual regression for boolean shorthand props with theme ${theme}`, async ({ page }) => {
-      await page.goto('/boolean-shorthand');
-      await page.setViewportSize({
-        width: viewportWidthM,
-        height: await page.evaluate(() => document.body.clientHeight),
-      });
-      await expect(page.locator('#app')).toHaveScreenshot(`boolean-shorthand-${viewportWidthM}-theme-${theme}.png`);
+  test(`should have no visual regression for viewport ${viewportWidthM}`, async ({ page }) => {
+    await page.goto('/boolean-shorthand');
+    await page.setViewportSize({
+      width: viewportWidthM,
+      height: await page.evaluate(() => document.body.clientHeight),
     });
-  }
+    await expect(page.locator('#app')).toHaveScreenshot(`boolean-shorthand-${viewportWidthM}.png`);
+  });
 });
