14f2ffb26b36a1171d3b2242e4a3a56ae766a980
Henri Joss <henri.joss@mhp.com>
2025-03-28 10:56:57 +0100

    feat(storefront): add display examples | hj | #3402

--- packages/storefront/src/app/components/display/display.stories.ts
+++ packages/storefront/src/app/components/display/display.stories.ts
@@ -11,3 +11,69 @@ export const displayStory: Story<'p-display'> = {
     },
   ],
 };
+
+export const displayStorySizeInherit: Story<'p-display'> = {
+  state: {
+    properties: {
+      tag: 'h3',
+      size: 'inherit',
+      style: { fontSize: '5rem' },
+    },
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-display',
+      properties,
+      children: ['The quick brown fox jumps over the lazy dog'],
+    },
+  ],
+};
+
+export const displayStorySizeResponsive: Story<'p-display'> = {
+  state: {
+    properties: {
+      tag: 'h3',
+      size: { base: 'medium', l: 'large' },
+    },
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-display',
+      properties,
+      children: ['The quick brown fox jumps over the lazy dog'],
+    },
+  ],
+};
+
+export const displayStorySemantics: Story<'p-display'> = {
+  generator: () => [
+    {
+      tag: 'p-display',
+      properties: {
+        tag: 'h3',
+      },
+      children: ['The quick brown fox jumps over the lazy dog'],
+    },
+    {
+      tag: 'p-display',
+      children: [{ tag: 'h3', children: ['The quick brown fox jumps over the lazy dog'] }],
+    },
+  ],
+};
+
+export const displayStoryColorInherit: Story<'p-display'> = {
+  state: {
+    properties: {
+      tag: 'h3',
+      color: 'inherit',
+      style: { color: 'deeppink' },
+    },
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-display',
+      properties,
+      children: ['The quick brown fox jumps over the lazy dog'],
+    },
+  ],
+};
