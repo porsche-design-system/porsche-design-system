0407d2f222fc2b0df97778dd1d8558f76a1b4ef9
Henri Joss <henri.joss@mhp.com>
2025-04-03 10:54:16 +0200

    feat(storefront): improve partial usage | hj | #3402

--- packages/storefront/src/partials/HeaderPartials.tsx
+++ packages/storefront/src/partials/HeaderPartials.tsx
@@ -1,14 +1,34 @@
 import { getFontFaceStyles, getFontLinks, getInitialStyles } from '@porsche-design-system/components-react/partials';
 import { getComponentChunkLinks, getIconLinks } from '@porsche-design-system/partials/src';
+import { prefetchDNS, preload } from 'react-dom';
 
 export const HeaderPartials = (): JSX.Element => {
+  const getHref = (href: string) => {
+    return process.env.NODE_ENV === 'production'
+      ? href
+      : href.replace('https://cdn.ui.porsche.com/porsche-design-system', 'http://localhost:3001');
+  };
+  /* preloads Porsche Next font (=> minimize FOUT) */
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  getFontLinks({ format: 'js', weights: ['regular', 'semi-bold', 'bold'] }).forEach(({ href, options }) =>
+    preload(getHref(href), options)
+  );
+  /* preloads PDS component core chunk from CDN for PDS component hydration (=> improve loading performance) */
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  getComponentChunkLinks({
+    components: ['accordion', 'button', 'canvas', 'tabs-bar', 'scroller', 'icon'],
+    format: 'js',
+  }).forEach(({ href, options }) => preload(getHref(href), options));
+  /* preloads Porsche icons (=> minimize FOUC) */
+  // biome-ignore lint/complexity/noForEach: <explanation>
+  getIconLinks({ format: 'js', icons: ['search', 'configurate', 'sidebar', 'external'] }).forEach(({ href }) =>
+    prefetchDNS(getHref(href))
+  );
+
   return process.env.NODE_ENV === 'production' ? (
     <>
       {getInitialStyles({ format: 'jsx' })}
       {getFontFaceStyles({ format: 'jsx' })}
-      {getFontLinks({ format: 'jsx', weights: ['regular', 'semi-bold', 'bold'] })}
-      {getComponentChunkLinks({ format: 'jsx', components: ['tabs-bar', 'scroller'] })}
-      {getIconLinks({ format: 'jsx', icons: ['arrow-head-left', 'arrow-head-right'] })}
     </>
   ) : (
     <>
@@ -22,11 +42,6 @@ export const HeaderPartials = (): JSX.Element => {
             .replace('https://cdn.ui.porsche.com/porsche-design-system', 'http://localhost:3001'),
         }}
       />
-      {Array.from(getFontLinks({ weights: ['regular', 'semi-bold', 'bold'] }).matchAll(/https:\/\/[a-z0-9./-]+/g))
-        .map(([url]) => url.replace('https://cdn.ui.porsche.com/porsche-design-system', 'http://localhost:3001'))
-        .map((url) => (
-          <link key={url} rel="preload" href={url} as="font" type="font/woff2" crossOrigin="" />
-        ))}
     </>
   );
 };
