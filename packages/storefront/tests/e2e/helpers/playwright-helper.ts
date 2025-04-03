d74716f2f554a8ba8e2f1c327d0def7716e0a1da
Henri Joss <henri.joss@mhp.com>
2025-04-03 15:59:38 +0200

    test(storefront): add sitemap test | hj | #3402

---
+++ packages/storefront/tests/e2e/helpers/playwright-helper.ts
@@ -0,0 +1,19 @@
+import type { ConsoleMessage, ElementHandle, Locator, Page } from '@playwright/test';
+
+const consoleMessages: ConsoleMessage[] = [];
+
+export const initConsoleObserver = (page: Page): void => {
+  consoleMessages.length = 0; // reset
+
+  page.on('console', (msg) => {
+    if (msg.type() === 'error') {
+      consoleMessages.push(msg);
+      console.log(msg.text());
+    }
+  });
+};
+export const getConsoleErrorsAmount = () => consoleMessages.length;
+
+export const getProperty = async <T>(element: Locator | ElementHandle, prop: string): Promise<T> => {
+  return element.evaluate((el, prop: string) => el[prop], prop);
+};
---
+++ packages/storefront/tests/e2e/specs/sitemap.e2e.ts
@@ -0,0 +1,14 @@
+import { expect, test } from '@playwright/test';
+import { buildSitemap, getSitemap } from '../helpers/sitemap';
+
+const ONE_MINUTE_IN_MS = 60000;
+
+// locally the run takes about 3-4 minutes, so we triple that for the pipeline
+test.setTimeout(ONE_MINUTE_IN_MS * 9999);
+
+test('should have complete sitemap.json', async ({ page }) => {
+  const oldUrls = getSitemap(); // reads fixture/sitemap.json
+  const newUrls = await buildSitemap(page); // returns new result and writes results/sitemap.json
+
+  expect(newUrls).toEqual(oldUrls);
+});
