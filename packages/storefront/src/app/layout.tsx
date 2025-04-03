9b3b9585f57159bb5c8f34a476a864b1af77df56
Henri Joss <henri.joss@mhp.com>
2025-04-03 10:43:43 +0200

    feat(storefront): add metadata & og:image tags | hj | #3402

--- packages/storefront/src/app/layout.tsx
+++ packages/storefront/src/app/layout.tsx
@@ -1,14 +1,30 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import '@/styles/globals.css';
 import { Canvas } from '@/components/layout/Canvas';
 import { Providers } from '@/components/providers/Providers';
 import { StorefrontThemeProvider } from '@/components/providers/StorefrontThemeProvider';
 import { HeaderPartials } from '@/partials/HeaderPartials';
+import { getMetaTagsAndIconLinks } from '@porsche-design-system/components-react/partials';
 
+const title = 'Porsche Design System';
+
+const { themeColor, appleWebApp, icons } = getMetaTagsAndIconLinks({
+  appTitle: title,
+  format: 'js',
+});
+
+/* injects meta theme-color. */
+export const viewport: Viewport = {
+  themeColor,
+};
+
+/* injects favicon, apple touch icons, android touch icons, etc. */
 export const metadata: Metadata = {
-  title: 'Porsche Design System',
+  title,
   description:
     'Find all the fundamental UXI guidelines and pattern-based web components to build brand driven, consistent and intuitive designs for digital Porsche products.',
+  appleWebApp,
+  icons,
 };
 
 export default function RootLayout({
@@ -20,6 +36,21 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <base href={process.env.NEXT_PUBLIC_BASE_PATH ? `/${process.env.NEXT_PUBLIC_BASE_PATH}/` : '/'} />
+        <meta property="og:image" content="/assets/og-image.png" />
+        <meta property="og:image:alt" content="Porsche Wordmark" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Porsche Design System" />
+        <meta
+          property="og:description"
+          content="Find all the fundamental UXI guidelines and pattern-based web components to build brand driven, consistent and intuitive designs for digital Porsche products."
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Porsche Design System" />
+        <meta
+          name="twitter:description"
+          content="Find all the fundamental UXI guidelines and pattern-based web components to build brand driven, consistent and intuitive designs for digital Porsche products."
+        />
+        <meta name="twitter:image" content="/assets/og-image.png" />
         <HeaderPartials />
       </head>
       <body>
