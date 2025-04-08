47a45496d110d3361e30b4ae41c8ae7fd3d31ca8
Henri Joss <henri.joss@mhp.com>
2025-04-08 15:28:47 +0200

    test(storefront): add stackblitz e2e test | hj | #3402

---
+++ packages/storefront/tests/e2e/specs/stackBlitz.e2e.ts
@@ -0,0 +1,57 @@
+import { expect, test } from '@playwright/test';
+import type { Framework } from '@porsche-design-system/shared';
+
+// TODO: Vue and Angular currently only work for released version
+const buttonFrameworkMap: Record<Framework, string> = {
+  'vanilla-js': 'Vanilla JS',
+  react: 'React',
+  angular: 'Angular',
+  vue: 'Vue',
+};
+
+const frameworks: Framework[] = ['vanilla-js', 'react'];
+
+for (const framework of frameworks) {
+  test(`should have working stackBlitz button for framework: ${framework}`, async ({ browser }) => {
+    const context = await browser.newContext({
+      // bypass captcha in headless chrome
+      userAgent:
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+    });
+    const page = await context.newPage();
+    await page.goto('/components/button/configurator');
+
+    const playground = page.locator('.playground');
+    await expect(playground.locator('p-tabs-bar.framework-select')).toBeVisible();
+    await playground.getByText(buttonFrameworkMap[framework]).click();
+    await expect(playground.getByRole('tab', { name: buttonFrameworkMap[framework] })).toHaveAttribute(
+      'aria-selected',
+      'true'
+    );
+
+    const stackBlitzButton = page.getByText('Open in Stackblitz');
+    const stackBlitzPagePromise = page.waitForEvent('popup');
+
+    await stackBlitzButton.click();
+    const stackBlitzPage = await stackBlitzPagePromise;
+    await stackBlitzPage.waitForLoadState();
+
+    // Wait for the iframe to appear on the parent page
+    await stackBlitzPage.waitForSelector('#PreviewContentWrapper iframe', {
+      state: 'attached', // Ensures the iframe is attached to the DOM
+      timeout: 90000, // Adjust timeout as needed
+    });
+
+    const iframeElement = stackBlitzPage.locator('#PreviewContentWrapper iframe');
+    const iframe = iframeElement.contentFrame();
+
+    if (iframe) {
+      const button = iframe.locator('p-button');
+      await expect(button).toBeVisible();
+    } else {
+      throw new Error('Unable to access the iframe content, possibly due to cross-origin restrictions');
+    }
+
+    await stackBlitzPage.close();
+  });
+}
