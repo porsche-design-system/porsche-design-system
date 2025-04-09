01615112a2b7efe4b451d3350bd18245bc0ca9bd
Henri Joss <henri.joss@mhp.com>
2025-04-09 15:46:10 +0200

    feat(storefront): remove unused | hj | #3402

--- packages/storefront/src/utils/formatTagName.ts
+++
@@ -1,8 +0,0 @@
-import type { TagName } from '@porsche-design-system/shared';
-
-export const formatTagName = (tagName: TagName) => {
-  return tagName
-    .replace(/^p-/, '') // Remove the `p-` prefix
-    .replace(/-/g, ' ') // Replace dashes with spaces
-    .replace(/\b\w/g, (char) => char.toUpperCase()); // Capitalize the first letter of each word
-};
