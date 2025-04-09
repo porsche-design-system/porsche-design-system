63fbc2050c900b182385c6f9b4b29d388d7b2a51
Henri Joss <henri.joss@mhp.com>
2025-04-09 15:39:50 +0200

    test(storefront): add vrt tests | hj | #3402

---
+++ packages/storefront/tests/vrt/specs/markdown.vrt.ts
@@ -0,0 +1,41 @@
+import { expect, test } from '@playwright/test';
+import { schemes, viewportWidthM, viewportWidths } from '@porsche-design-system/shared/testing/playwright.vrt';
+
+test.describe('markdown', async () => {
+  schemes.forEach((scheme) => {
+    test(`should have no visual regression for viewport ${viewportWidthM} and theme auto with prefers-color-scheme ${scheme}`, async ({
+      page,
+    }) => {
+      await page.emulateMedia({
+        colorScheme: scheme,
+      });
+      await page.goto('/markdown');
+      await page.evaluate(() =>
+        (window as unknown as Window & { componentsReady: () => Promise<number> }).componentsReady()
+      );
+      await page.focus('a[href="https://designsystem.porsche.com/"]:not([title])');
+      await page.setViewportSize({
+        width: viewportWidthM,
+        height: await page.evaluate(() => document.body.clientHeight),
+      });
+      await expect(page.locator('#main-content')).toHaveScreenshot(`markdown-${viewportWidthM}-scheme-${scheme}.png`);
+    });
+  });
+
+  viewportWidths
+    .filter((x) => x !== viewportWidthM)
+    .forEach((viewportWidth) => {
+      test(`should have no visual regression for viewport ${viewportWidth}`, async ({ page }) => {
+        await page.goto('/markdown');
+        await page.evaluate(() =>
+          (window as unknown as Window & { componentsReady: () => Promise<number> }).componentsReady()
+        );
+        await page.focus('a[href="https://designsystem.porsche.com/"]:not([title])');
+        await page.setViewportSize({
+          width: viewportWidth,
+          height: await page.evaluate(() => document.body.clientHeight),
+        });
+        await expect(page.locator('#app > main')).toHaveScreenshot(`markdown-${viewportWidth}.png`);
+      });
+    });
+});
---
+++ packages/storefront/tests/vrt/specs/views.vrt.ts
@@ -0,0 +1,60 @@
+import { expect, test } from '@playwright/test';
+import {
+  schemes,
+  viewportWidth3XL,
+  viewportWidth4XL,
+  viewportWidthM,
+  viewportWidthXXL,
+  viewportWidths,
+} from '@porsche-design-system/shared/testing/playwright.vrt';
+
+const urls = {
+  home: '/',
+  page: '/components/pagination/examples',
+} as const;
+
+for (const [name, url] of Object.entries(urls)) {
+  test.describe(name, async () => {
+    schemes.forEach((scheme) => {
+      test(`should have no visual regression for viewport ${viewportWidthM} and theme auto with prefers-color-scheme ${scheme}`, async ({
+        page,
+      }) => {
+        await page.emulateMedia({
+          colorScheme: scheme,
+        });
+        await page.goto(url);
+        await page.evaluate(() =>
+          (window as unknown as Window & { componentsReady: () => Promise<number> }).componentsReady()
+        );
+        await page.setViewportSize({
+          width: viewportWidthM,
+          height: await page.evaluate(() => document.body.clientHeight),
+        });
+        const screenshot = await page.screenshot({ fullPage: true });
+        expect(screenshot).toMatchSnapshot(`views-${name}-${viewportWidthM}-scheme-${scheme}.png`);
+      });
+    });
+
+    (
+      [
+        ...viewportWidths.filter((x) => x !== viewportWidthM),
+        viewportWidthXXL,
+        viewportWidth3XL,
+        viewportWidth4XL,
+      ] as const
+    ).forEach((viewportWidth) => {
+      test(`should have no visual regression for viewport ${viewportWidth}`, async ({ page }) => {
+        await page.goto(url);
+        await page.evaluate(() =>
+          (window as unknown as Window & { componentsReady: () => Promise<number> }).componentsReady()
+        );
+        await page.setViewportSize({
+          width: viewportWidth,
+          height: await page.evaluate(() => document.body.clientHeight),
+        });
+        const screenshot = await page.screenshot({ fullPage: true });
+        expect(screenshot).toMatchSnapshot(`views-${name}-${viewportWidth}.png`);
+      });
+    });
+  });
+}
