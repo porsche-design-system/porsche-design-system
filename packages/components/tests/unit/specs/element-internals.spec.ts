508fb1f79e0456ed8fd7758302010f38b7115516
Oliver Hook <oliver.hook@porsche.digital>
2025-04-10 10:49:52 +0200

    test(components): adapt elementInternals unit test | oh | #3764

--- packages/components/tests/unit/specs/element-internals.spec.ts
+++ packages/components/tests/unit/specs/element-internals.spec.ts
@@ -6,21 +6,21 @@ import { TAG_NAMES } from '@porsche-design-system/shared';
  * Reflecting 'form' and 'name' as an attribute ensures it is properly handled in the form submission process when using ElementInternals API.
  */
 describe('Element Internals', () => {
-  const amountOfComponentsUsingElementInternalsApi = 9;
+  const amountOfComponentsUsingElementInternalsApi = 10;
+  const componentsWithElementInternals = TAG_NAMES.filter((tagName) => getComponentMeta(tagName).hasElementInternals);
 
   it('should have certain amount of components', () => {
-    const componentsWithElementInternals = TAG_NAMES.filter((tagName) => getComponentMeta(tagName).hasElementInternals);
     expect(componentsWithElementInternals.length).toBe(amountOfComponentsUsingElementInternalsApi);
   });
 
-  TAG_NAMES.forEach((tagName) => {
-    const componentMeta = getComponentMeta(tagName);
-
-    if (componentMeta.hasElementInternals) {
+  componentsWithElementInternals
+    .filter((tagName) => !['p-select-option'].includes(tagName))
+    .forEach((tagName) => {
+      const componentMeta = getComponentMeta(tagName);
+      console.log(tagName);
       it(`should reflect 'name' and 'form' props for component ${tagName} using ElementInternals API`, () => {
         expect(componentMeta.propsMeta['form'].propOptions['reflect']).toBe(true);
         expect(componentMeta.propsMeta['name'].propOptions['reflect']).toBe(true);
       });
-    }
-  });
+    });
 });
