32371b1200aeec0738e638997df4d107ab16cba8
Henri Joss <henri.joss@mhp.com>
2025-04-16 10:56:55 +0200

    fix(storefront): fix redirect | hj | #3402

--- packages/storefront/src/components/common/Tabs.tsx
+++ packages/storefront/src/components/common/Tabs.tsx
@@ -15,7 +15,17 @@ export default function Tabs() {
 
   const tabs = useMemo(() => {
     if (pathname) {
-      const { page, tab } = getPathnameRoutes(pathname);
+      const { category, page, tab } = getPathnameRoutes(pathname);
+
+      // If the path doesn't contain a page redirect to first subpath of category
+      if (!page && category?.subPaths) {
+        redirect(`${Object.values(category.subPaths)[0].path}`);
+      }
+
+      // If the path doesn't contain a tab redirect to first tab of the page
+      if (!tab && page?.subPaths) {
+        redirect(`${Object.values(page.subPaths)[0].path}`);
+      }
 
       if (tab && page?.subPaths) {
         setTabIndex(Object.values(page.subPaths).indexOf(tab));
