de68a557a5e265a5cd742e4fc4fd91af5605ef83
Henri Joss <henri.joss@mhp.com>
2025-04-22 09:12:39 +0200

    test(storefront): adjust helper | hj | #3402

--- packages/storefront/tests/vrt/helpers/helpers.ts
+++ packages/storefront/tests/vrt/helpers/helpers.ts
@@ -2,15 +2,17 @@ import type { Page } from '@playwright/test';
 import { expect } from '@playwright/test';
 
 export const closeSidebars = async (page: Page) => {
-  const flyoutDismissButtons = page.getByRole('dialog').getByText('Dismiss flyout');
+  const flyoutStart = page.locator('.flyout-start');
+  const flyoutEnd = page.locator('.flyout-start');
 
-  const count = await flyoutDismissButtons.count();
+  await flyoutStart.evaluate((el) => {
+    (el as HTMLElement & { open: boolean }).open = false;
+  });
 
-  for (let i = 0; i < count; i++) {
-    const button = flyoutDismissButtons.nth(i);
-    if (await button.isVisible()) {
-      await button.click();
-      await expect(button).toBeHidden();
-    }
-  }
+  await flyoutEnd.evaluate((el) => {
+    (el as HTMLElement & { open: boolean }).open = false;
+  });
+
+  await expect(flyoutStart).toBeHidden();
+  await expect(flyoutEnd).toBeHidden();
 };
