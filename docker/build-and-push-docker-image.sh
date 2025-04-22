2585536206bba8861054725295893a93a4d9c59c
Henri Joss <henri.joss@mhp.com>
2025-04-22 17:03:34 +0200

    ci: update docker image | hj | #3402

--- docker/build-and-push-docker-image.sh
+++ docker/build-and-push-docker-image.sh
@@ -2,7 +2,7 @@
 
 REGISTRY=ghcr.io/porsche-design-system/porsche-design-system
 IMAGE=playwright
-TAG=v1.49.1-jammy-test-1
+TAG=v1.49.1-jammy-test-2
 
 docker pull $REGISTRY/$IMAGE:latest
 docker build -f Dockerfile -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
