ebfe441d9221f6aa03d085b82031de415374e61d
Henri Joss <henri.joss@mhp.com>
2025-05-08 15:38:38 +0200

    feat(storefront): add spacing/skeleton tailwind docs | hj | #3570

--- packages/storefront/src/app/tailwind/skeleton/examples/skeleton.stories.ts
+++ packages/storefront/src/app/tailwind/skeleton/examples/skeleton.stories.ts
@@ -3,5 +3,32 @@
 import type { Story } from '@/models/story';
 
 export const skeletonStory: Story<'div'> = {
-  generator: () => [],
+  generator: () => [
+    {
+      tag: 'div',
+      properties: { className: 'flex flex-col items-center justify-center gap-fluid-sm p-static-sm' },
+      children: [
+        {
+          tag: 'div',
+          properties: { className: 'pds-skeleton h-[200px] w-full' },
+          children: ['.pds-skeleton'],
+        },
+        {
+          tag: 'div',
+          properties: { className: 'pds-skeleton text-sm w-fit' },
+          children: ['.pds-skeleton .text-sm'],
+        },
+        {
+          tag: 'div',
+          properties: { className: 'pds-skeleton text-md w-fit' },
+          children: ['.pds-skeleton .text-md'],
+        },
+        {
+          tag: 'div',
+          properties: { className: 'pds-skeleton text-lg w-fit' },
+          children: ['.pds-skeleton .text-lg'],
+        },
+      ],
+    },
+  ],
 };
