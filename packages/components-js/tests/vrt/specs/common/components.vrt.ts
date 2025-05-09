e048e7fc7d00bd811b221fe468f085ee1e6508eb
kaiszybiak <kai.szybiak@netrunners.com>
2025-05-09 13:44:40 +0200

    test(components-js): update fixture | ks | #3388

--- packages/components-js/tests/vrt/specs/common/components.vrt.ts
+++ packages/components-js/tests/vrt/specs/common/components.vrt.ts
@@ -44,7 +44,7 @@ const revertAutoFocus = async (page: Page, component: string): Promise<void> => 
 };
 
 test('should have certain amount of components', () => {
-  expect(components.length).toBe(60);
+  expect(components.length).toBe(61);
 });
 
 for (const component of components) {
