3b29f7306db4912da76329a82ee563540e6110ac
Stephan Schröter <stephan.schroeter@porsche.digital>
2025-05-14 11:06:33 +0200

    docs(storefront): update media query tailwind | sas | #3570

--- packages/storefront/src/app/tailwind/media-query/examples/media-query.stories.ts
+++ packages/storefront/src/app/tailwind/media-query/examples/media-query.stories.ts
@@ -7,32 +7,32 @@ export const mediaQueryStory: Story<'div'> = {
     {
       tag: 'div',
       properties: {
-        className: 'flex flex-col gap-fluid-md p-fluid-md',
+        className: 'grid gap-fluid-md prose-text-sm',
       },
       children: [
         {
           tag: 'p',
           properties: {
             className:
-              "text-sm text-primary m-0 relative after:ml-1 xs:after:content-['.xs:'] sm:after:content-['.sm:'] md:after:content-['.md:'] lg:after:content-['.lg:'] xl:after:content-['.xl:'] 2xl:after:content-['.2xl:']",
+              "xs:after:content-['.xs:'] sm:after:content-['.sm:'] md:after:content-['.md:'] lg:after:content-['.lg:'] xl:after:content-['.xl:'] 2xl:after:content-['.2xl:']",
           },
-          children: ['Media Query Min:'],
+          children: ['Media Query Min: '],
         },
         {
           tag: 'p',
           properties: {
             className:
-              "text-sm text-primary m-0 relative after:ml-1 max-xs:after:content-['.max-xs:'] max-sm:after:content-['.max-sm:'] max-md:after:content-['.max-md:'] max-lg:after:content-['.max-lg:'] max-xl:after:content-['.max-xl:'] max–2xl:after:content-['.max-2xl:']",
+              "max-xs:after:content-['.max-xs:'] max-sm:after:content-['.max-sm:'] max-md:after:content-['.max-md:'] max-lg:after:content-['.max-lg:'] max-xl:after:content-['.max-xl:'] max–2xl:after:content-['.max-2xl:']",
           },
-          children: ['Media Query Max:'],
+          children: ['Media Query Max: '],
         },
         {
           tag: 'p',
           properties: {
             className:
-              "text-sm text-primary m-0 relative after:ml-1 xs:max-sm:after:content-['.xs:max-sm:'] sm:max-md:after:content-['.sm:max-md:'] md:max-lg:after:content-['.md:max-lg:'] lg:max-xl:after:content-['.lg:max-xl:'] xl:max-2xl:after:content-['.xl:max-2xl:']",
+              "xs:max-sm:after:content-['.xs:max-sm:'] sm:max-md:after:content-['.sm:max-md:'] md:max-lg:after:content-['.md:max-lg:'] lg:max-xl:after:content-['.lg:max-xl:'] xl:max-2xl:after:content-['.xl:max-2xl:']",
           },
-          children: ['Media Query Min Max:'],
+          children: ['Media Query Min Max: '],
         },
       ],
     },
