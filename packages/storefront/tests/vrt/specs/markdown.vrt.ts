2674480c076774d2423ab8abda56e2688accfbfe
Henri Joss <henri.joss@mhp.com>
2025-05-15 10:55:13 +0200

    test(storefront): improve vrt test | hj | #3570-1

--- packages/storefront/tests/vrt/specs/markdown.vrt.ts
+++ packages/storefront/tests/vrt/specs/markdown.vrt.ts
@@ -21,14 +21,6 @@ test.describe('markdown', async () => {
       await page.evaluate(() =>
         (window as unknown as Window & { componentsReady: () => Promise<number> }).componentsReady()
       );
-      const link = page.getByRole('link', { name: 'link text' });
-      await link.focus();
-      await expect(link).toBeFocused();
-
-      await page.setViewportSize({
-        width: viewportWidthM,
-        height: await page.evaluate(() => document.body.clientHeight),
-      });
       const screenshot = await page.screenshot({ fullPage: true });
       expect(screenshot).toMatchSnapshot(`markdown-${viewportWidthM}-scheme-${scheme}.png`);
     });
@@ -39,18 +31,14 @@ test.describe('markdown', async () => {
     .forEach((viewportWidth) => {
       test(`should have no visual regression for viewport ${viewportWidth}`, async ({ page }) => {
         await page.goto('/-/mdx');
-        await resetAnimations(page);
-        await page.evaluate(() =>
-          (window as unknown as Window & { componentsReady: () => Promise<number> }).componentsReady()
-        );
-        const link = page.getByRole('link', { name: 'link text' });
-        await link.focus();
-        await expect(link).toBeFocused();
         await page.setViewportSize({
           width: viewportWidth,
           height: await page.evaluate(() => document.body.clientHeight),
         });
-
+        await resetAnimations(page);
+        await page.evaluate(() =>
+          (window as unknown as Window & { componentsReady: () => Promise<number> }).componentsReady()
+        );
         await closeSidebars(page);
 
         const screenshot = await page.screenshot({ fullPage: true });
