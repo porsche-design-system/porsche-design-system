fa035ed5e15eccba19219ba0f6652fa917c605fe
Oliver Hook <oliver.hook@porsche.digital>
2025-05-20 16:49:10 +0200

    docs(storefront): add example page | oh | #3486

---
+++ packages/components-angular/src/app/examples/popover-example-controlled.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { PorscheDesignSystemModule } from '@porsche-design-system/components-angular';
+
+@Component({
+  selector: 'page-popover-example-controlled',
+  template: `
+    <p-popover [open]="isPopoverOpen" (dismiss)="onDismiss()">
+      <p-button-pure slot="button" [hideLabel]="true" [aria]="{ 'aria-expanded': 'false' }" (click)="onOpen()" icon="information">More information</p-button-pure>
+      Some additional content with some <a href="#">link</a>.
+    </p-popover>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [PorscheDesignSystemModule],
+})
+export class FlyoutExampleComponent {
+  isPopoverOpen = false;
+
+  onOpen() {
+    this.isPopoverOpen = true;
+  }
+  onDismiss() {
+    this.isPopoverOpen = false;
+  }
+}
