befbd0b646599ffe6309815bdd8c8693cd796fb0
kaiszybiak <kai.szybiak@netrunners.com>
2025-05-20 12:31:32 +0200

    ci: update docker image | ks

--- docker/build-and-push-docker-image.sh
+++ docker/build-and-push-docker-image.sh
@@ -2,7 +2,7 @@
 
 REGISTRY=ghcr.io/porsche-design-system/porsche-design-system
 IMAGE=playwright
-TAG=v1.49.1-jammy-9-6
+TAG=v1.49.1-jammy-9-7
 
 docker pull $REGISTRY/$IMAGE:latest
 docker build -f Dockerfile -t $REGISTRY/$IMAGE:$TAG -t $REGISTRY/$IMAGE:latest .
