fc0c1ba82444aa717d015170558caa6b450db0d5
Henri Joss <henri.joss@mhp.com>
2025-05-26 16:35:35 +0200

    test(components): adjust style test | hj | #3388

--- packages/components/src/components/common/input-base/input-base-styles.spec.ts
+++ packages/components/src/components/common/input-base/input-base-styles.spec.ts
@@ -1,25 +1,25 @@
 import { validateCssAndMatchSnapshot } from '../../../../tests/unit/helpers';
-import { getFunctionalComponentInputBaseStyles } from './input-base-styles';
 import { getCss } from '../../../utils';
+import { getFunctionalComponentInputBaseStyles } from './input-base-styles';
 
 describe('getFunctionalComponentInputBaseStyles()', () => {
   it.each<Parameters<typeof getFunctionalComponentInputBaseStyles>>([
-    [false, false, 'none', true, false, false, 'light'],
-    [false, false, 'none', true, false, false, 'light'],
-    [false, false, 'none', false, false, false, 'light'],
-    [false, true, 'none', true, false, false, 'light'],
-    [false, true, 'none', false, false, false, 'light'],
-    [false, false, 'success', true, false, false, 'light'],
-    [false, false, 'success', false, false, false, 'light'],
-    [false, true, 'success', true, true, true, 'light'],
-    [false, true, 'success', false, true, true, 'light', { padding: 0 }],
-    [false, false, 'error', true, false, false, 'light'],
-    [false, false, 'error', false, false, false, 'light'],
-    [false, true, 'error', true, false, false, 'light'],
-    [false, true, 'error', false, false, false, 'light', { padding: 0 }],
-    [true, true, 'error', false, false, false, 'light'],
-    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, false, false, 'light'],
-    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, false, false, 'dark'],
+    [false, false, 'none', true, false, 'light'],
+    [false, false, 'none', true, false, 'light'],
+    [false, false, 'none', false, false, 'light'],
+    [false, true, 'none', true, false, 'light'],
+    [false, true, 'none', false, false, 'light'],
+    [false, false, 'success', true, false, 'light'],
+    [false, false, 'success', false, false, 'light'],
+    [false, true, 'success', true, true, 'light'],
+    [false, true, 'success', false, true, 'light', { padding: 0 }],
+    [false, false, 'error', true, false, 'light'],
+    [false, false, 'error', false, false, 'light'],
+    [false, true, 'error', true, false, 'light'],
+    [false, true, 'error', false, false, 'light', { padding: 0 }],
+    [true, true, 'error', false, false, 'light'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, false, 'light'],
+    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, false, 'dark'],
   ])(
     'should return correct css for disabled: %s, hideLabel: %o, state: %s, button: %o, compact: %o, readOnly: %o, theme: %s, additionalInputJssStyle: %s',
     (...args) => {
