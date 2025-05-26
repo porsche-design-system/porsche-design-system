75dc25259fba35ca7abbeb7729822eb57e08a475
Stephan Schröter <stephan.schroeter@porsche.digital>
2025-05-26 14:58:02 +0200

    test(tokens): update snapshot | sas | #3866

--- packages/tokens/src/index.spec.ts
+++ packages/tokens/src/index.spec.ts
@@ -2,7 +2,7 @@ import { it, expect } from 'vitest';
 import * as fromIndex from './';
 
 it.each<keyof typeof fromIndex>(Object.keys(fromIndex) as (keyof typeof fromIndex)[])(
-  'should contain correct values for %s',
+  'should contain correct value for %s',
   (item) => {
     expect(fromIndex[item]).toMatchSnapshot();
   }
