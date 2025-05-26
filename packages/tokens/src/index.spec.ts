811e560731783fb8f0e0f81857977c3d2ea44cb1
Stephan Schröter <stephan.schroeter@porsche.digital>
2025-05-26 12:10:05 +0200

    test(tokens): add unit test | sas | #3866

---
+++ packages/tokens/src/index.spec.ts
@@ -0,0 +1,9 @@
+import { it, expect } from 'vitest';
+import * as fromIndex from './';
+
+it.each<keyof typeof fromIndex>(Object.keys(fromIndex) as (keyof typeof fromIndex)[])(
+  'should contain correct values for %s',
+  (item) => {
+    expect(fromIndex[item]).toMatchSnapshot();
+  }
+);
