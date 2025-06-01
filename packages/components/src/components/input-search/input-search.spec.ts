0f8eb35abb1c4c8a4f0ad4a42a814a12e52aa6d7
kaiszybiak <kai.szybiak@netrunners.com>
2025-06-01 22:47:14 +0200

    test(components): update input test | ks | #3390

--- packages/components/src/components/input-search/input-search.spec.ts
+++ packages/components/src/components/input-search/input-search.spec.ts
@@ -117,3 +117,41 @@ describe('componentDidRender', () => {
     );
   });
 });
+describe('connectedCallback', () => {
+  it('should assign this.initialLoading to value of this.loading', () => {
+    const component = initComponent();
+    component.loading = true;
+
+    expect(component['initialLoading']).toBe(false);
+    component.connectedCallback();
+    expect(component['initialLoading']).toBe(true);
+
+    component.loading = false;
+    component.connectedCallback();
+    expect(component['initialLoading']).toBe(false);
+  });
+});
+describe('componentWillUpdate', () => {
+  it('should assign this.initialLoading to true for this.loading = true', () => {
+    const component = initComponent();
+    component.loading = true;
+
+    expect(component['initialLoading']).toBe(false);
+    component.componentWillUpdate();
+    expect(component['initialLoading']).toBe(true);
+  });
+});
+describe('componentWillLoad', () => {
+  it('should assign this.initialLoading to value of this.loading', () => {
+    const component = initComponent();
+    component.loading = true;
+
+    expect(component['initialLoading']).toBe(false);
+    component.componentWillLoad();
+    expect(component['initialLoading']).toBe(true);
+
+    component.loading = false;
+    component.componentWillLoad();
+    expect(component['initialLoading']).toBe(false);
+  });
+});
