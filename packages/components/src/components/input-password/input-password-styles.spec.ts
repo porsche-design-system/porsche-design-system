d94b83159b24875fb97465bb53270de936e09739
Henri Joss <henri.joss@mhp.com>
2025-06-11 11:39:27 +0200

    test(components): adjust test | hj | #3390

--- packages/components/src/components/input-password/input-password-styles.spec.ts
+++ packages/components/src/components/input-password/input-password-styles.spec.ts
@@ -3,22 +3,23 @@ import { getComponentCss } from './input-password-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [false, false, 'none', true, false, false, 'light'],
-    [false, false, 'none', true, false, false, 'light'],
-    [false, false, 'none', false, false, false, 'light'],
-    [false, true, 'none', true, false, false, 'light'],
-    [false, true, 'none', false, false, false, 'light'],
-    [false, false, 'success', true, false, false, 'light'],
-    [false, false, 'success', false, false, false, 'light'],
-    [false, true, 'success', true, true, true, 'light'],
-    [false, true, 'success', false, true, true, 'light'],
-    [false, false, 'error', true, false, false, 'light'],
-    [false, false, 'error', false, false, false, 'light'],
-    [false, true, 'error', true, false, false, 'light'],
-    [false, true, 'error', false, false, false, 'light'],
-    [true, true, 'error', false, false, false, 'light'],
+    [false, false, false, 'none', true, false, false, 'light'],
+    [false, false, false, 'none', true, false, false, 'light'],
+    [false, false, false, 'none', false, false, false, 'light'],
+    [false, false, true, 'none', true, false, false, 'light'],
+    [false, false, true, 'none', false, false, false, 'light'],
+    [false, false, false, 'success', true, false, false, 'light'],
+    [false, false, false, 'success', false, false, false, 'light'],
+    [false, false, true, 'success', true, true, true, 'light'],
+    [false, false, true, 'success', false, true, true, 'light'],
+    [false, false, false, 'error', true, false, false, 'light'],
+    [false, false, false, 'error', false, false, false, 'light'],
+    [false, false, true, 'error', true, false, false, 'light'],
+    [false, false, true, 'error', false, false, false, 'light'],
+    [true, true, true, 'error', false, false, false, 'light'],
     [
       false,
+      true,
       { base: true, xs: false, s: true, m: false, l: true, xl: false },
       'none',
       true,
@@ -27,9 +28,9 @@ describe('getComponentCss()', () => {
 
       'light',
     ],
-    [false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, false, false, 'dark'],
+    [true, false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'none', true, false, false, 'dark'],
   ])(
-    'should return correct css for isDisabledOrLoading: %s, hideLabel: %o, state: %s, toggle: %o, compact: %o, readOnly: %o, theme: %s',
+    'should return correct css for disabled: %s, loading: %s, hideLabel: %o, state: %s, toggle: %o, compact: %o, readOnly: %o, theme: %s',
     (...args) => {
       validateCssAndMatchSnapshot(getComponentCss(...args));
     }
