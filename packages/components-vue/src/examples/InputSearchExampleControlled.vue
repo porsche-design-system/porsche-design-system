97d4fbbebe1410d77d7e3e3d68c3d30d227d3c54
Henri Joss <henri.joss@mhp.com>
2025-06-12 09:21:37 +0200

    feat(components-js): remove log | hj | #3390

--- packages/components-vue/src/examples/InputSearchExampleControlled.vue
+++ packages/components-vue/src/examples/InputSearchExampleControlled.vue
@@ -5,7 +5,6 @@ import { computed, ref } from 'vue';
 const value = ref<string>('');
 
 const onInput = (e: InputSearchInputEventDetail) => {
-  console.log(e);
   value.value = (e.target as HTMLInputElement).value;
 };
 
