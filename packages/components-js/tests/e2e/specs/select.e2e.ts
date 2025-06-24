c3da592ce86781118d98b326159798dc8e2b4e97
Henri Joss <henri.joss@mhp.com>
2025-06-24 14:05:48 +0200

    test(components-js): skip tests in safari | hj | #3536

--- packages/components-js/tests/e2e/specs/select.e2e.ts
+++ packages/components-js/tests/e2e/specs/select.e2e.ts
@@ -1253,75 +1253,81 @@ test.describe('filter', () => {
       await expect(options.nth(2)).toBeVisible();
     });
 
-    test('should reset filter value and show all options again after filtering and selecting an option', async ({
-      page,
-    }) => {
-      await initSelect(page, { props: { name: 'Some name', filter: true } });
-      const host = getHost(page);
-      const buttonElement = getButton(page);
-      const filterElement = getFilter(page);
-      const filterInputElement = getFilterInput(page);
-      const options = getSelectOptions(page);
-      const dropdown = getDropdown(page);
+    skipInBrowsers(['webkit'], () => {
+      test('should reset filter value and show all options again after filtering and selecting an option', async ({
+        page,
+      }) => {
+        await initSelect(page, { props: { name: 'Some name', filter: true } });
+        const host = getHost(page);
+        const buttonElement = getButton(page);
+        const filterElement = getFilter(page);
+        const filterInputElement = getFilterInput(page);
+        const options = getSelectOptions(page);
+        const dropdown = getDropdown(page);
 
-      await buttonElement.click();
+        await buttonElement.click();
 
-      await expect(dropdown).toBeVisible();
-      await expect(filterElement).toBeFocused();
-      await expect(filterInputElement).toHaveValue('');
-      await filterInputElement.fill('b');
+        await expect(dropdown).toBeVisible();
+        await expect(filterElement).toBeFocused();
+        await expect(filterInputElement).toHaveValue('');
+        await filterInputElement.fill('b');
 
-      await expect(options.nth(0)).toBeHidden();
-      await expect(options.nth(1)).toBeVisible();
-      await expect(options.nth(1)).toHaveText('b');
-      await expect(options.nth(2)).toBeHidden();
+        await expect(options.nth(0)).toBeHidden();
+        await expect(options.nth(1)).toBeVisible();
+        await expect(options.nth(1)).toHaveText('b');
+        await expect(options.nth(2)).toBeHidden();
 
-      await page.keyboard.press('ArrowDown');
-      await expect(options.nth(1)).toHaveJSProperty('highlighted', true);
-      await page.keyboard.press('Enter');
+        await page.keyboard.press('ArrowDown');
+        await expect(options.nth(1)).toHaveJSProperty('highlighted', true);
+        await page.keyboard.press('Enter');
 
-      await expect(dropdown).toBeHidden();
-      await expect(host).toHaveJSProperty('value', 'b');
-      await expect(buttonElement).toBeFocused();
+        await expect(dropdown).toBeHidden();
+        await expect(host).toHaveJSProperty('value', 'b');
+        await expect(buttonElement).toBeFocused();
 
-      await page.keyboard.press('Space');
-      await expect(filterElement).toBeFocused();
-      await expect(filterInputElement).toHaveValue('');
+        await page.keyboard.press('Space');
+        await expect(filterElement).toBeFocused();
+        await expect(filterInputElement).toHaveValue('');
 
-      await expect(options.nth(0)).toBeVisible();
-      await expect(options.nth(1)).toBeVisible();
-      await expect(options.nth(2)).toBeVisible();
+        await expect(options.nth(0)).toBeVisible();
+        await expect(options.nth(1)).toBeVisible();
+        await expect(options.nth(2)).toBeVisible();
+      });
     });
 
-    test('should reset filter value and show all options again after closing and reopening again', async ({ page }) => {
-      await initSelect(page, { props: { name: 'Some name', filter: true } });
-      const host = getHost(page);
-      const buttonElement = getButton(page);
-      const filterElement = getFilter(page);
-      const filterInputElement = getFilterInput(page);
-      const options = getSelectOptions(page);
-      const dropdown = getDropdown(page);
+    skipInBrowsers(['webkit'], () => {
+      test('should reset filter value and show all options again after closing and reopening again', async ({
+        page,
+      }) => {
+        await initSelect(page, { props: { name: 'Some name', filter: true } });
+        const host = getHost(page);
+        const buttonElement = getButton(page);
+        const filterElement = getFilter(page);
+        const filterInputElement = getFilterInput(page);
+        const options = getSelectOptions(page);
+        const dropdown = getDropdown(page);
 
-      await buttonElement.click();
+        await buttonElement.click();
 
-      await expect(dropdown).toBeVisible();
-      await expect(filterElement).toBeFocused();
-      await expect(filterInputElement).toHaveValue('');
-      await filterInputElement.fill('b');
+        await expect(dropdown).toBeVisible();
+        await expect(filterElement).toBeFocused();
+        await expect(filterInputElement).toHaveValue('');
+        await filterInputElement.fill('b');
 
-      await page.keyboard.press('Escape');
-      await expect(dropdown).toBeHidden();
-      await expect(host).toHaveJSProperty('value', undefined);
-      await expect(buttonElement).toBeFocused();
+        await page.keyboard.press('Escape');
+        await expect(dropdown).toBeHidden();
+        await expect(host).toHaveJSProperty('value', undefined);
+        await expect(buttonElement).toBeFocused();
 
-      await waitForStencilLifecycle(page);
-      await buttonElement.click();
-      await expect(filterElement).toBeFocused();
-      await expect(filterInputElement).toHaveValue('');
+        await waitForStencilLifecycle(page);
+        await buttonElement.click();
+        await expect(filterElement).toBeFocused();
+        await expect(filterInputElement).toHaveValue('');
 
-      await expect(options.nth(0)).toBeVisible();
-      await expect(options.nth(1)).toBeVisible();
-      await expect(options.nth(2)).toBeVisible();
+        await expect(options.nth(0)).toBeVisible();
+        await expect(options.nth(1)).toBeVisible();
+        await expect(options.nth(2)).toBeVisible();
+      });
     });
 
     test('should show indicator when no results are found', async ({ page }) => {
