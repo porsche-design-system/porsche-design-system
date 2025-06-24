8cc61196a276388bd41d626b89840c3425ef6be1
Henri Joss <henri.joss@mhp.com>
2025-06-24 14:14:55 +0200

    test(components-js): fix vrt focus in iframes | hj | #3536

--- packages/components-js/tests/vrt/specs/common/components.vrt.ts
+++ packages/components-js/tests/vrt/specs/common/components.vrt.ts
@@ -40,7 +40,14 @@ const revertAutoFocus = async (page: Page, component: string): Promise<void> => 
     )
   ) {
     await page.mouse.click(0, 0); // click top left corner of the page to remove focus
-    await page.waitForFunction(() => document.activeElement.tagName === 'BODY');
+    await page.waitForFunction(() => document.activeElement?.tagName === 'BODY');
+    for (const frame of page.frames()) {
+      await frame.evaluate(() => {
+        if (document.activeElement instanceof HTMLElement) {
+          document.activeElement.blur();
+        }
+      });
+    }
   }
 };
 
