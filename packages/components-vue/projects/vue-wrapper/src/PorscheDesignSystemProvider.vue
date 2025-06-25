23ddbaba55f760a23ae0aa47659b38a92d7d281b
kaiszybiak <kai.szybiak@netrunners.com>
2025-06-25 12:20:41 +0200

    feat(components-vue): update design system provider | ks | #3864

--- packages/components-vue/projects/vue-wrapper/src/PorscheDesignSystemProvider.vue
+++ packages/components-vue/projects/vue-wrapper/src/PorscheDesignSystemProvider.vue
@@ -1,34 +1,34 @@
 <script setup lang="ts">
-import {onMounted, provide, ref, watch} from 'vue';
-import { load } from '@porsche-design-system/components-js';
-import { prefixInjectionKey, themeInjectionKey } from './utils';
-import type { Theme } from './lib/types';
+  import { provide, ref, watch } from 'vue';
+  import { load } from '@porsche-design-system/components-js';
+  import { prefixInjectionKey, themeInjectionKey } from './utils';
+  import type { Theme } from './lib/types';
 
-type Props = {
-  prefix?: string;
-  cdn?: 'auto' | 'cn';
-  theme?: Theme; // since theme exists on almost every component, it is defined here kind of like a global prop
-  // other component configurations should probably go into a separate `components`, `componentProps` or `componentDefaults`
-  // property similar to https://mui.com/material-ui/customization/theme-components/
-};
+  type Props = {
+    prefix?: string;
+    cdn?: 'auto' | 'cn';
+    theme?: Theme; // since theme exists on almost every component, it is defined here kind of like a global prop
+    // other component configurations should probably go into a separate `components`, `componentProps` or `componentDefaults`
+    // property similar to https://mui.com/material-ui/customization/theme-components/
+  };
 
-const props = withDefaults(defineProps<Props>(), {
-  prefix: '',
-  theme: 'light',
-});
+  const props = withDefaults(defineProps<Props>(), {
+    prefix: '',
+    theme: 'light',
+  });
 
-const themeRef = ref<Theme>(props.theme);
+  const themeRef = ref<Theme>(props.theme);
 
-onMounted(() => {
+  watch(
+    () => props.theme,
+    (newTheme) => {
+      themeRef.value = newTheme;
+    }
+  );
+
   load(props); // runtime prefix or cdn change is not supported
-});
-
-watch(() => props.theme, (newTheme) => {
-  themeRef.value = newTheme;
-});
-
-provide(prefixInjectionKey, props.prefix);
-provide(themeInjectionKey, themeRef);
+  provide(prefixInjectionKey, props.prefix);
+  provide(themeInjectionKey, themeRef);
 </script>
 
 <template><slot /></template>
