8656ca51f974cb1084176d01e372c292a2aa5eb6
Oliver Hook <oliver.hook@porsche.digital>
2025-07-03 08:49:49 +0200

    docs(storefront): adapt ai-tag examples | oh | #3486

--- packages/storefront/src/app/components/popover/popover.stories.ts
+++ packages/storefront/src/app/components/popover/popover.stories.ts
@@ -49,7 +49,7 @@ export const popoverControlled: Story<'p-popover'> = {
   ],
 };
 
-export const popoverControlledAiTag: Story<'p-popover'> = {
+export const popoverControlledAiTagGenerated: Story<'p-popover'> = {
   state: {
     properties: {
       open: false,
@@ -93,60 +93,76 @@ export const popoverControlledAiTag: Story<'p-popover'> = {
             {
               tag: 'p-heading',
               properties: { size: 'medium', tag: 'h3' },
-              children: ['Content Credentials'],
+              children: ['Information'],
             },
             {
               tag: 'p-text',
               properties: {
-                size: 'xx-small',
-                color: 'contrast-medium',
-                className: '-mt-fluid-sm',
-              },
-              children: ['Created by Porsche AG, 5th April 2025'],
-            },
-            {
-              tag: 'p-text',
-              properties: {
-                size: 'xx-small',
+                size: 'x-small',
                 color: 'contrast-high',
               },
-              children: ['This image combines multiple elements. At least one of them was generated using an AI tool.'],
+              children: ['This [content] was generated with artificial intelligence.'],
             },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export const popoverControlledAiTagModified: Story<'p-popover'> = {
+  state: {
+    properties: {
+      open: false,
+    } as any,
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-popover',
+      properties,
+      events: {
+        // @ts-ignore
+        onDismiss: {
+          target: 'p-popover',
+          prop: 'open',
+          value: false,
+        },
+      },
+      children: [
+        {
+          tag: 'p-tag',
+          properties: { color: 'background-frosted', icon: 'ai-spark-filled', slot: 'button' },
+          children: [
             {
-              tag: 'dl',
-              properties: { className: 'prose-text-xs m-0' },
-              children: [
-                {
-                  tag: 'dt',
-                  properties: { className: 'm-0 text-contrast-medium' },
-                  children: ['Created by'],
+              tag: 'button',
+              properties: { type: 'button', 'aria-expanded': false },
+              events: {
+                onClick: {
+                  target: 'p-popover',
+                  prop: 'open',
+                  value: true,
                 },
-                {
-                  tag: 'dd',
-                  properties: { className: 'm-0 text-primary' },
-                  children: ['Porsche AG'],
-                },
-                {
-                  tag: 'dt',
-                  properties: { className: 'm-0 text-contrast-medium mt-fluid-sm' },
-                  children: ['AI tools used'],
-                },
-                {
-                  tag: 'dd',
-                  properties: { className: 'm-0 text-primary' },
-                  children: ['Adobe Firefly'],
-                },
-              ],
-            },
-            {
-              tag: 'p-button',
-              properties: {
-                type: 'button',
-                variant: 'ghost',
-                compact: true,
-                aria: { 'aria-label': 'Check content credentials' },
               },
-              children: ['Check'],
+              children: ['AI-generated'],
+            },
+          ],
+        },
+        {
+          tag: 'div',
+          properties: { className: 'xs:max-w-[220px] flex flex-col gap-fluid-sm py-static-sm' },
+          children: [
+            {
+              tag: 'p-heading',
+              properties: { size: 'medium', tag: 'h3' },
+              children: ['Information'],
+            },
+            {
+              tag: 'p-text',
+              properties: {
+                size: 'x-small',
+                color: 'contrast-high',
+              },
+              children: ['This [content] has been modified with artificial intelligence.'],
             },
           ],
         },
