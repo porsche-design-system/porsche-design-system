44109dbca4391a1cc6481cffb1342d28fb8ba781
Henri Joss <henri.joss@mhp.com>
2025-07-07 15:02:25 +0200

    test(storefront): adjust smoke test | hj / sas | #housekeeping/angular-update

--- packages/components-js/tests/smoke/unit/specs/package.smoke.ts
+++ packages/components-js/tests/smoke/unit/specs/package.smoke.ts
@@ -9,6 +9,41 @@ import componentsJsPackageJson from '../../../../dist/components-wrapper/package
 
 const nodeRequire = createRequire(import.meta.url);
 
+const packageJsonExports = {
+  './ag-grid': {
+    types: './ag-grid/esm/index.d.ts',
+    import: './ag-grid/esm/index.mjs',
+    default: './ag-grid/cjs/index.cjs',
+  },
+  './jsdom-polyfill': {
+    types: './jsdom-polyfill/index.d.ts',
+    default: './jsdom-polyfill/index.cjs',
+  },
+  './partials': {
+    types: './partials/index.d.ts',
+    module: './partials/index.js',
+    default: './partials/index.cjs',
+  },
+  './styles': {
+    sass: './styles/_index.scss',
+    types: './styles/esm/index.d.ts',
+    import: './styles/esm/index.mjs',
+    default: './styles/cjs/index.cjs',
+  },
+  './styles/vanilla-extract': {
+    types: './styles/vanilla-extract/esm/vanilla-extract/index.d.ts',
+    import: './styles/vanilla-extract/esm/vanilla-extract/index.mjs',
+    default: './styles/vanilla-extract/cjs/vanilla-extract/index.cjs',
+  },
+  './tailwindcss': {
+    style: './tailwindcss/index.css',
+  },
+  './testing': {
+    types: './testing/index.d.ts',
+    default: './testing/index.cjs',
+  },
+};
+
 describe('package content', () => {
   const componentsJsFilePath = nodeRequire.resolve('@porsche-design-system/components-js');
   const componentsJsPackageDir = path.resolve(componentsJsFilePath, '../..');
@@ -38,69 +73,39 @@ describe('package.json files', () => {
         .replace(/(wrapper\/).+\/(package\.json)/, '$1$2'); // get rid of nested folders if there are any
       const pkgJson = JSON.parse(fs.readFileSync(pathName, 'utf8'));
 
-      if (packageName === '@porsche-design-system/components-angular') {
-        // adjust and ignore stuff that is added by ng-packagr
-        pkgJson.exports['.'].default = './cjs/index.cjs';
-        pkgJson.exports['.'].import = './esm/index.mjs';
+      // adjust and ignore stuff that is ssr related
+      delete pkgJson.exports['./ssr'];
 
-        delete pkgJson.exports['.'].esm;
-        delete pkgJson.exports['.'].es2015;
-        delete pkgJson.exports['.'].es2020;
-        delete pkgJson.exports['.'].esm2020;
-        delete pkgJson.exports['.'].esm2022;
-        delete pkgJson.exports['.'].node;
-      } else {
-        // adjust and ignore stuff that is ssr related
-        delete pkgJson.exports['./ssr'];
-
-        // map `public-api` filenames to `index` because `components-js` calls it `index`
-        pkgJson.exports['.'] = Object.fromEntries(
-          Object.entries<string>(pkgJson.exports['.']).map(([key, val]) => [key, val.replace('public-api', 'index')])
-        );
-      }
+      // map `public-api` filenames to `index` because `components-js` calls it `index`
+      pkgJson.exports['.'] = Object.fromEntries(
+        Object.entries<string>(pkgJson.exports['.']).map(([key, val]) => [key, val.replace('public-api', 'index')])
+      );
 
       // check that version and exports match
       expect(pkgJson.version).toBe(componentsJsPackageJson.version);
-      expect(pkgJson.exports).toEqual({
-        './package.json': './package.json',
-        '.': {
-          types: './esm/index.d.ts',
-          import: './esm/index.mjs',
-          default: './cjs/index.cjs',
-        },
-        './ag-grid': {
-          types: './ag-grid/esm/index.d.ts',
-          import: './ag-grid/esm/index.mjs',
-          default: './ag-grid/cjs/index.cjs',
-        },
-        './jsdom-polyfill': {
-          types: './jsdom-polyfill/index.d.ts',
-          default: './jsdom-polyfill/index.cjs',
-        },
-        './partials': {
-          types: './partials/index.d.ts',
-          module: './partials/index.js',
-          default: './partials/index.cjs',
-        },
-        './styles': {
-          sass: './styles/_index.scss',
-          types: './styles/esm/index.d.ts',
-          import: './styles/esm/index.mjs',
-          default: './styles/cjs/index.cjs',
-        },
-        './styles/vanilla-extract': {
-          types: './styles/vanilla-extract/esm/vanilla-extract/index.d.ts',
-          import: './styles/vanilla-extract/esm/vanilla-extract/index.mjs',
-          default: './styles/vanilla-extract/cjs/vanilla-extract/index.cjs',
-        },
-        './tailwindcss': {
-          style: './tailwindcss/index.css',
-        },
-        './testing': {
-          types: './testing/index.d.ts',
-          default: './testing/index.cjs',
-        },
-      });
+
+      if (packageName === '@porsche-design-system/components-angular') {
+        expect(pkgJson.exports).toEqual({
+          '.': {
+            default: './fesm2022/porsche-design-system-components-angular.mjs',
+            types: './index.d.ts',
+          },
+          './package.json': {
+            default: './package.json',
+          },
+          ...packageJsonExports,
+        });
+      } else {
+        expect(pkgJson.exports).toEqual({
+          './package.json': './package.json',
+          '.': {
+            types: './esm/index.d.ts',
+            import: './esm/index.mjs',
+            default: './cjs/index.cjs',
+          },
+          ...packageJsonExports,
+        });
+      }
 
       // create temporary local package tgz package
       // inspired by https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/9c6db02a531a797d2ce7b197bca94f82a50064e7/packages/cli/src/index.ts#L152-L155
@@ -125,7 +130,8 @@ describe('package.json files', () => {
           !(
             prob.kind === 'FalseCJS' ||
             ('entrypoint' in prob &&
-              (prob.entrypoint === '.' ||
+              (prob.entrypoint === './ag-grid' ||
+                prob.entrypoint === '.' ||
                 prob.entrypoint === './styles' ||
                 prob.entrypoint === './styles/vanilla-extract' ||
                 prob.entrypoint === './tailwindcss' ||
