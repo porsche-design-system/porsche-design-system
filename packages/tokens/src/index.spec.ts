47eecff7f5d2aa4ff25cc7d57a5be30b14ba9d17
Henri Joss <henri.joss@mhp.com>
2025-07-18 09:28:39 +0200

    feat: update biome | hj | #housekeeping/update-dependencies-2

--- packages/tokens/src/index.spec.ts
+++ packages/tokens/src/index.spec.ts
@@ -4,6 +4,7 @@ import * as fromIndex from './';
 it.each<keyof typeof fromIndex>(Object.keys(fromIndex) as (keyof typeof fromIndex)[])(
   'should contain correct value for %s',
   (item) => {
+    // biome-ignore lint/performance/noDynamicNamespaceImportAccess: ok
     expect(fromIndex[item]).toMatchSnapshot();
   }
 );
