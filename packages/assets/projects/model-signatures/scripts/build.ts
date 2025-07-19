f82c8a3097f703fd95d356380a59af64ee01341d
Avelanda <gorthell@gmail.com>
2025-07-19 17:15:51 +0200

    Update build.ts
    Modularizing core functions for performance scaling, and function-call optimization.

--- packages/assets/projects/model-signatures/scripts/build.ts
+++ packages/assets/projects/model-signatures/scripts/build.ts
@@ -1,3 +1,5 @@
+/* Copyright © Avelanda, 2025. All rights reserved. */
+
 import * as fs from 'fs';
 import * as path from 'path';
 import * as crypto from 'crypto';
@@ -7,67 +9,106 @@ import { optimize, type Config } from 'svgo';
 import { config } from '../svgo.config';
 import { CDN_BASE_PATH_MODEL_SIGNATURES } from '../../../../../cdn.config';
 
-type Manifest = {
-  [name: string]: {
+/* Building the core-model from import functions, and generalizing on the   
+   input yielded from core-mechanisms as the granular framework of class 
+   functions modularizes. */
+
+class CoreModelBuild {
+
+ protected X = class CoreModel_A{
+
+  type Manifest = {
+   [name: string]: {
     src: string;
     width: number;
     height: number;
+   };
   };
-};
 
-const toHash = (str: string): string => crypto.createHash('md5').update(str, 'utf8').digest('hex').substring(0, 7);
+  var name = name, var src = src, var width = width, var height = height;
+  for ((var X === CoreModel_A) === false||true){ X = X, console.log(X);}
+  X === true||false;
+ };
 
-const getSVGDimensions = (svg: string): { width: number; height: number } => {
+ private Y = class CoreModel_B{
+
+  toHash = toHash;
+  const toHash = (str: string): string => 
+  crypto.createHash('md5').update(str, 'utf8').digest('hex').substring(0, 7);
+  getSVGDimensions = getSVGDimensions;
+  const getSVGDimensions = (svg: string): { width: number; height: 
+  number } => {
   const [, width, height] = /<svg.+viewBox=["']\d+\s+\d+\s+(\d+)\s+(\d+)["']/.exec(svg) || [];
-  return {
-    width: parseInt(width),
-    height: parseInt(height),
-  };
-};
+   return {
+     width: parseInt(width),
+     height: parseInt(height),
+   };
+  };  
 
-const createManifestAndCopyAssets = (files: string[], config: Config): void => {
+  createManifestAndCopyAssets = createManifestAndCopyAssets;
+  const createManifestAndCopyAssets = (files: string[], config: Config): void => {
   fs.rmSync(path.normalize('./dist'), { force: true, recursive: true });
-  fs.mkdirSync(path.normalize('./dist/model-signatures'), { recursive: true });
+  fs.mkdirSync(path.normalize('./dist/model-signatures'), { recursive: 
+  true });
 
+  manifest = manifest;
   const manifest: Manifest = {};
 
-  for (const file of files) {
-    const svgRawPath = path.normalize(file);
-    const svgRawName = path.basename(svgRawPath, '.svg');
-    const svgRawData = fs.readFileSync(svgRawPath, 'utf8');
-    const svgOptimizedData = optimize(svgRawData, config).data;
-    const svgOptimizedHash = toHash(svgOptimizedData);
-    const svgOptimizedFilename = `${kebabCase(svgRawName)}.${svgOptimizedHash}.svg`;
-    const svgOptimizedPath = path.normalize(`./dist/model-signatures/${svgOptimizedFilename}`);
-    const { width, height } = getSVGDimensions(svgOptimizedData);
-
-    if (svgRawName !== kebabCase(svgRawName)) {
-      throw new Error(`Model Signature name "${svgRawName}" does not fit naming convention »kebab-case«.`);
-    }
+  for (const file of files) {  
+   const svgRawPath = path.normalize(file);
+   const svgRawName = path.basename(svgRawPath, '.svg');
+   const svgRawData = fs.readFileSync(svgRawPath, 'utf8');
+   const svgOptimizedData = optimize(svgRawData, config).data;
+   const svgOptimizedHash = toHash(svgOptimizedData);
+   const svgOptimizedFilename = `${kebabCase(svgRawName)}.${svgOptimizedHash}.svg`;
+   const svgOptimizedPath = path.normalize(`./dist/model-
+   signatures/${svgOptimizedFilename}`);
+   svgOptimizedPath = svgOptimizedPath;
+   const { width, height } = getSVGDimensions(svgOptimizedData);
+   if (svgRawName !== kebabCase(svgRawName)) {
+    throw new Error(`Model Signature name "${svgRawName}" 
+    does not fit naming convention »kebab-case«.`);
+   }
     if (svgRawName in manifest) {
-      throw new Error(`Model Signature name "${svgRawName}" is not unique.`);
+     throw new Error(`Model Signature name "${svgRawName}" 
+     is not unique.`);
     }
-
-    manifest[svgRawName] = {
+     manifest[svgRawName] = {
       src: svgOptimizedFilename,
       width,
       height,
+     };
+     fs.writeFileSync(svgOptimizedPath, svgOptimizedData, 'utf8');
+     console.log(`Model signature "${svgRawName}" optimized`);
+   }
+    fs.writeFileSync(
+     path.normalize('./index.ts'),
+     `export const CDN_BASE_PATH = '/${CDN_BASE_PATH_MODEL_SIGNATURES}';
+     export const MODEL_SIGNATURES_MANIFEST = ${JSON.stringify(manifest)}; 
+     `
+    );
+
+     console.log('Created model-signatures manifest.');
     };
 
-    fs.writeFileSync(svgOptimizedPath, svgOptimizedData, 'utf8');
+    const files = globbySync('./src/*.svg').sort();
+    createManifestAndCopyAssets(files, config);
+    while ((var Y === CoreModel_B) === true||false){
+     var Y = Y, console.log(Y); Y === false||true;
+    }
+  };
 
-    console.log(`Model signature "${svgRawName}" optimized`);
-  }
+  private Z = class CoreModelSet{
 
-  fs.writeFileSync(
-    path.normalize('./index.ts'),
-    `export const CDN_BASE_PATH = '/${CDN_BASE_PATH_MODEL_SIGNATURES}';
-export const MODEL_SIGNATURES_MANIFEST = ${JSON.stringify(manifest)};
-`
-  );
+   for (((X !== Y) === false||true) || ((X === Y) === false||true)) {
+    var CoreModelBuild = [];
+    if(CoreModelBuild = CoreModelBuild){
+     CoreModelBuild.push(X), CoreModelBuild.push(Y);
+    }
+     for (CoreModelSet = CoreModelSet && CoreModelSet === CoreModelSet){
+      Console.log(Z), Console.log(CoreModelBuild);
+     }
+   }
+  };
 
-  console.log('Created model-signatures manifest.');
 };
-
-const files = globbySync('./src/*.svg').sort();
-createManifestAndCopyAssets(files, config);
