21d671ba0815c756da3205656e181aacc9f3c29d
Oliver Hook <oliver.hook@porsche.digital>
2025-07-28 16:13:11 +0200

    test(components-js): revert changes | oh | #3814

--- packages/components-js/tests/e2e/specs/core-initializer.e2e.ts
+++ packages/components-js/tests/e2e/specs/core-initializer.e2e.ts
@@ -6,10 +6,17 @@ test.describe('core-initializer', () => {
     await goto(page, 'core-initializer');
     await page.waitForFunction(() => document.querySelectorAll('p-text-field-wrapper').length === 2);
 
+    const [component1, component2] = await page.locator('p-text-field-wrapper').all();
     const getOuterHTML = (locator: Locator) => locator.evaluate((el) => el.outerHTML);
 
-    await expect
-      .poll(async () => await getOuterHTML(page.locator('p-text-field-wrapper').nth(0)))
-      .toBe(await getOuterHTML(page.locator('p-text-field-wrapper').nth(1)));
+    const component1HTML = await getOuterHTML(component1);
+    const component2HTML = await getOuterHTML(component2);
+
+    await expect.poll(async () => component1HTML).toBe(component2HTML);
+
+    if (component1HTML !== component2HTML) {
+      console.log('component1HTML', component1HTML);
+      console.log('component2HTML', component2HTML);
+    }
   });
 });
