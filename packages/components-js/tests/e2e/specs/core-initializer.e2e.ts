663172910e9fb2de2547d49b13bb7f73b02b38b2
Oliver Hook <oliver.hook@porsche.digital>
2025-07-29 13:25:23 +0200

    test(components-js): adapt e2e test for safari | oh | #3814

--- packages/components-js/tests/e2e/specs/core-initializer.e2e.ts
+++ packages/components-js/tests/e2e/specs/core-initializer.e2e.ts
@@ -6,9 +6,19 @@ test.describe('core-initializer', () => {
     await goto(page, 'core-initializer');
     await page.waitForFunction(() => document.querySelectorAll('p-text-field-wrapper').length === 2);
 
-    const locator = page.locator('p-text-field-wrapper');
-    const getOuterHTML = async (index: number) => await locator.nth(index).evaluate((el) => el.outerHTML);
+    const component1 = page.locator('p-text-field-wrapper').nth(0);
+    const component2 = page.locator('p-text-field-wrapper').nth(1);
 
-    await expect.poll(async () => await getOuterHTML(0)).toBe(await getOuterHTML(1));
+    await expect
+      .poll(async () => {
+        const component1HTML = await component1.evaluate((el) => el.outerHTML);
+        const component2HTML = await component2.evaluate((el) => el.outerHTML);
+        if (component1HTML !== component2HTML) {
+          console.log('component1HTML', component1HTML);
+          console.log('component2HTML', component2HTML);
+        }
+        return component1HTML === component2HTML;
+      })
+      .toBe(true);
   });
 });
