83cd7ade3d39d985998286e9cdb986a6c237e6da
kaiszybiak <kai.szybiak@netrunners.com>
2025-07-29 13:01:01 +0200

    docs(components): update prop docs | ks | #3906

--- packages/components/src/components/input-text/input-text.tsx
+++ packages/components/src/components/input-text/input-text.tsx
@@ -79,7 +79,7 @@ export class InputText {
   /** The text input value. */
   @Prop({ mutable: true }) public value?: string = '';
 
-  /** Provides a hint to the browser about what type of data the field expects, which can assist with autofill features (e.g., autocomplete=\"name\"). */
+  /** Provides a hint to the browser about what type of data the field expects, which can assist with autofill features (e.g., autocomplete='name'). */
   @Prop() public autoComplete?: string;
 
   /** A boolean value that, if present, makes the input field uneditable by the user, but its value will still be submitted with the form. */
@@ -94,7 +94,7 @@ export class InputText {
   /** A non-negative integer specifying the minimum number of characters required for the input's value to be considered valid. */
   @Prop() public minLength?: number;
 
-  /** A string that provides a brief hint to the user about what kind of information is expected in the field (e.g., placeholder=\"Enter your full name\"). This text is displayed when the input field is empty. */
+  /** A string that provides a brief hint to the user about what kind of information is expected in the field (e.g., placeholder='Enter your full name'). This text is displayed when the input field is empty. */
   @Prop() public placeholder?: string = '';
 
   /** A boolean value that, if present, makes the input field unusable and unclickable. The value will not be submitted with the form. */
