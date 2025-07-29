dffc559ef5c0b62e0c9fdce2df2356d4416c5d27
Oliver Hook <oliver.hook@porsche.digital>
2025-07-29 09:04:12 +0200

    test(components-js): fix e2e test in safari | oh | #3814

--- packages/components-js/tests/e2e/specs/core-initializer.e2e.ts
+++ packages/components-js/tests/e2e/specs/core-initializer.e2e.ts
@@ -6,17 +6,9 @@ test.describe('core-initializer', () => {
     await goto(page, 'core-initializer');
     await page.waitForFunction(() => document.querySelectorAll('p-text-field-wrapper').length === 2);
 
-    const [component1, component2] = await page.locator('p-text-field-wrapper').all();
-    const getOuterHTML = (locator: Locator) => locator.evaluate((el) => el.outerHTML);
+    const locator = page.locator('p-text-field-wrapper');
+    const getOuterHTML = async (index: number) => await locator.nth(index).evaluate((el) => el.outerHTML);
 
-    const component1HTML = await getOuterHTML(component1);
-    const component2HTML = await getOuterHTML(component2);
-
-    await expect.poll(async () => component1HTML).toBe(component2HTML);
-
-    if (component1HTML !== component2HTML) {
-      console.log('component1HTML', component1HTML);
-      console.log('component2HTML', component2HTML);
-    }
+    await expect.poll(async () => await getOuterHTML(0)).toBe(await getOuterHTML(1));
   });
 });
