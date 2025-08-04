5f63124aae39f020f9c48e3db2dc44b2fa84f06b
kaiszybiak <kai.szybiak@netrunners.com>
2025-08-04 17:23:53 +0200

    test(storefront): update fixtures | ks | #3909

--- packages/storefront/tests/e2e/specs/dead-link-checker.e2e.ts
+++ packages/storefront/tests/e2e/specs/dead-link-checker.e2e.ts
@@ -4,7 +4,7 @@ import { getInternalUrls } from '../helpers/sitemap';
 const internalUrls = getInternalUrls();
 
 test('should have no exponential increase in internal urls', () => {
-  expect(internalUrls.length).toBeLessThanOrEqual(467);
+  expect(internalUrls.length).toBeLessThanOrEqual(472);
 });
 
 // TODO: Add markdown anchor links check?
