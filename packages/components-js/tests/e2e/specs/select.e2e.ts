4192b7ae56c1272a8ad66bb14a7c9bf1fced21f3
Henri Joss <henri.joss@mhp.com>
2025-08-07 14:24:31 +0200

    test(components-js): adjust test | hj | #3537-follow

--- packages/components-js/tests/e2e/specs/select.e2e.ts
+++ packages/components-js/tests/e2e/specs/select.e2e.ts
@@ -1999,7 +1999,7 @@ test.describe('lifecycle', () => {
       await waitForStencilLifecycle(page);
 
       const status2 = await getLifecycleStatus(page);
-      expect(status2.componentDidUpdate['p-select-option'], 'componentDidUpdate: p-select-option').toBe(5);
+      expect(status2.componentDidUpdate['p-select-option'], 'componentDidUpdate: p-select-option').toBe(2);
       expect(status2.componentDidUpdate['p-select'], 'componentDidUpdate: p-select').toBe(2); // Keyboard actions cause update in order to update sr highlighted option text
       expect(status2.componentDidUpdate.all, 'componentDidUpdate: all').toBe(7);
     });
