800bd5c74a65fe31284a139fdce3b020f160017b
kaiszybiak <kai.szybiak@netrunners.com>
2025-08-08 16:31:22 +0200

    test(components-js): skip e2e fixture | ks | #3905

--- packages/components-js/tests/e2e/specs/default-dom.e2e.ts
+++ packages/components-js/tests/e2e/specs/default-dom.e2e.ts
@@ -14,6 +14,10 @@ const expectShadowDomToMatchSnapshot = async (host: Locator, tagName: TagName): 
 for (const tagName of TAG_NAMES.filter((x) => !INTERNAL_TAG_NAMES.includes(x))) {
   test(`should have no basic DOM regression for ${tagName}`, async ({ page, browserName }) => {
     test.fixme(browserName === 'firefox' && tagName === 'p-carousel');
+    test.skip(
+      ['firefox', 'safari'].includes(browserName) && ['p-input-date', 'p-input-time'].includes(tagName),
+      'Skipping due to differing native date/time picker rendering in Firefox and Safari'
+    );
 
     await goto(page, ''); // start page
 
