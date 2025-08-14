33373ad8509226f0c8a025296a7f7e4338d650f3
Stephan Schröter <stephan.schroeter@porsche.digital>
2025-08-14 10:57:05 +0200

    test(components-js): add a11y test for flag | sas | #3218

--- packages/components-js/tests/a11y/specs/axe-core/components.a11y.ts
+++ packages/components-js/tests/a11y/specs/axe-core/components.a11y.ts
@@ -15,7 +15,7 @@ const revertAutoFocus = async (page: Page, component: string): Promise<void> => 
   }
 };
 
-const amountOfTestableComponents = 59;
+const amountOfTestableComponents = 60;
 
 test('should have certain amount of components', () => {
   expect(components.length).toBe(amountOfTestableComponents);
