a4cd649ea4248c0b1240e02a1f7eb429f4d7b080
kaiszybiak <kai.szybiak@netrunners.com>
2025-09-02 18:21:43 +0200

    feat(components): update generate dsr script | ks | #3913

--- packages/components/scripts/generateDSRComponents.ts
+++ packages/components/scripts/generateDSRComponents.ts
@@ -289,7 +289,7 @@ import { get${componentName}Css } from '${stylesBundleImportPath}';
           "$1 hasLabel={this.props.label || namedSlotChildren.filter(({ props: { slot } }) => slot === 'label').length > 0} hasDescription={this.props.description || namedSlotChildren.filter(({ props: { slot } }) => slot === 'description').length > 0}$2"
         )
         .replace(/(this\.props)\.host/g, '$1') // general
-        .replace(/(getSegmentedControlCss)\(getItemMaxWidth\(this\.props\)/, '$1(100') // segmented-control
+        .replace(/(getSegmentedControlCss)\(getItemMaxWidth\(this\.props,\s*this\.props\.compact\)/, '$1(100') // segmented-control
         .replace(/this\.props\.getAttribute\('tabindex'\)/g, 'null') // button
         .replace(/(const\s+TagType)(\s+=)/, '$1: any$2') // fix typing for display, heading, headline, text,
         .replace(
