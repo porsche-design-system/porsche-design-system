ffe84638aae34430427e7c0af8c8c756f077755b
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-09-17 12:40:51 +0200

    feat(components): update style snapshots | ms | #4009

--- packages/components/src/components/link-tile/link-tile-styles.spec.ts
+++ packages/components/src/components/link-tile/link-tile-styles.spec.ts
@@ -3,11 +3,11 @@ import { getComponentCss } from './link-tile-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['4:3', 'default', 'semi-bold', 'dark', 'bottom', false, true, false],
-    ['1:1', 'inherit', 'regular', 'light', 'bottom', false, true, true],
-    ['3:4', 'default', 'semi-bold', 'dark', 'top', false, true, false],
-    ['16:9', 'inherit', 'regular', 'light', 'top', true, false, true],
-    ['9:16', 'default', 'semi-bold', 'dark', 'bottom', false, false, true],
+    ['4:3', 'default', 'semi-bold', 'dark', 'bottom', false, true],
+    ['1:1', 'inherit', 'regular', 'light', 'bottom', false, true],
+    ['3:4', 'default', 'semi-bold', 'dark', 'top', false, true],
+    ['16:9', 'inherit', 'regular', 'light', 'top', true, false],
+    ['9:16', 'default', 'semi-bold', 'dark', 'bottom', false, false],
     [
       { base: '1:1', xs: '4:3', s: '3:4', m: '16:9', l: '9:16', xl: '1:1' },
       { base: 'inherit', xs: 'default', s: 'inherit', m: 'default', l: 'inherit', xl: 'default' },
@@ -16,10 +16,9 @@ describe('getComponentCss()', () => {
       'top',
       { base: true, xs: false, s: true, m: false, l: true, xl: false },
       true,
-      false,
     ],
   ])(
-    'should return correct css for aspectRatio: %j, size: %j, weight: %j, background: %s, align: %s, isCompact: %j, hasGradient: %s and hasFooterText: %s',
+    'should return correct css for aspectRatio: %j, size: %j, weight: %j, background: %s, align: %s, isCompact: %j and hasGradient: %s',
     (...args) => {
       validateCssAndMatchSnapshot(getComponentCss(...args));
     }
