d38d60aa70f8b8cc54cca72bd42d99a20249dbec
kaiszybiak <kai.szybiak@netrunners.com>
2025-10-01 20:26:48 +0200

    test(components-js): update fixtures | ks | #3387

--- packages/components-js/tests/vrt/specs/states/radio-group.vrt.ts
+++ packages/components-js/tests/vrt/specs/states/radio-group.vrt.ts
@@ -97,24 +97,23 @@ const scenario = async (page: Page, theme: Theme, scheme?: PrefersColorScheme): 
     prefersColorScheme: scheme,
   });
 
-  await forceHoverState(
-    page,
-    '.hover p-radio-group:not(.force-label) p-radio-group-option:first-of-type >>> .wrapper input[type="radio"]'
-  );
+  // hover input
+  await forceHoverState(page, '.hover p-radio-group:not(.force-label) p-radio-group-option:first-of-type >>> input');
+
+  // hover label
   await forceHoverState(page, '.hover p-radio-group.force-label p-radio-group-option:first-of-type >>> label');
-  await forceFocusVisibleState(
-    page,
-    '.focus p-radio-group p-radio-group-option:first-of-type >>> .wrapper input[type="radio"]'
-  );
+  // focus
+  await forceFocusVisibleState(page, '.focus p-radio-group p-radio-group-option:first-of-type >>> input');
+
+  // focus-hover
   await forceFocusHoverState(
     page,
-    '.focus-hover p-radio-group:not(.force-label) p-radio-group-option:first-of-type >>> .wrapper input[type="radio"]'
+    '.focus-hover p-radio-group:not(.force-label) p-radio-group-option:first-of-type >>> input'
   );
   await forceFocusVisibleState(
     page,
-    '.focus-hover p-radio-group.force-label p-radio-group-option:first-of-type >>> .wrapper input[type="radio"]'
+    '.focus-hover p-radio-group.force-label p-radio-group-option:first-of-type >>> input'
   );
-  await forceHoverState(page, '.focus-hover p-radio-group.force-label p-radio-group-option:first-of-type >>> label');
 };
 
 // executed in Chrome only
