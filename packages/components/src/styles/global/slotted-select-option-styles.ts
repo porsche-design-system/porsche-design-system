803fd1df88d3ce0fae7c8f874c13782daf080a42
henrijoss <henri.joss@mhp.com>
2025-10-06 13:23:36 +0200

    fix(components): fix native option disabled styles | hj | #3999

--- packages/components/src/styles/global/slotted-select-option-styles.ts
+++ packages/components/src/styles/global/slotted-select-option-styles.ts
@@ -1,7 +1,7 @@
-import { addImportantToEachRule } from '../common-styles';
-import { getThemedColors } from '../colors';
-import type { Styles } from 'jss';
 import type { TagName } from '@porsche-design-system/shared';
+import type { Styles } from 'jss';
+import { getThemedColors } from '../colors';
+import { addImportantToEachRule } from '../common-styles';
 
 /*
  * This is a bug fix Windows when using a touch device or the native property where the <option> elements are not visible in dark mode.
@@ -9,12 +9,15 @@ import type { TagName } from '@porsche-design-system/shared';
  * These styles will be ignored in most iOS and Android browsers anyway.
  */
 export const getSlottedSelectOptionStyles = (tagName: TagName): Styles => {
-  const { primaryColor, backgroundColor } = getThemedColors('light');
+  const { primaryColor, backgroundColor, disabledColor } = getThemedColors('light');
   return {
     '@global': {
       [`${tagName} select > option`]: addImportantToEachRule({
         color: primaryColor,
         backgroundColor,
+        '&[disabled]': {
+          color: disabledColor,
+        },
       }),
     },
   };
