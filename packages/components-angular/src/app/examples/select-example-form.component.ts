2a01cc7e42f249c6b0ddd016dba6031729a5774f
henrijoss <henri.joss@mhp.com>
2025-10-13 10:57:11 +0200

    test(components-angular): add angular forms integration test | hj | #4036

--- packages/components-angular/src/app/examples/select-example-form.component.ts
+++ packages/components-angular/src/app/examples/select-example-form.component.ts
@@ -5,8 +5,8 @@ import { PorscheDesignSystemModule } from '@porsche-design-system/components-ang
 @Component({
   selector: 'page-select-example-form',
   template: `
-    <form [formGroup]="form">
-      <p-select formControlName="select">
+    <form [formGroup]="form" (ngSubmit)="onSubmit()">
+      <p-select formControlName="mySelect">
         <p-select-option value="a">Option A</p-select-option>
         <p-select-option value="b">Option B</p-select-option>
         <p-select-option value="c">Option C</p-select-option>
@@ -14,25 +14,28 @@ import { PorscheDesignSystemModule } from '@porsche-design-system/components-ang
         <p-select-option value="e">Option E</p-select-option>
         <p-select-option value="f">Option F</p-select-option>
       </p-select>
+
+      <button type="submit">Submit</button>
     </form>
 
+    <button type="button" (click)="setValue()">
+      Set Value
+    </button>
+
+    <button type="button" (click)="resetValue()">
+      Reset
+    </button>
+
     <button type="button" (click)="toggleDisabled()">
-      {{ form.controls.select.disabled ? 'Enable' : 'Disable' }} Select
+      {{ form.controls.mySelect.disabled ? 'Enable' : 'Disable' }}
     </button>
 
-    <button type="button" (click)="this.form.controls.select.reset({ value: undefined, disabled: false })">
-        Reset
-    </button>
-
-    <button type="button" (click)="this.form.controls.select.setValue('a')">
-      Set Value a
-    </button>
-
-    <pre>Touched: {{ form.controls.select.touched }}</pre>
-    <pre>Dirty: {{ form.controls.select.dirty }}</pre>
-    <pre>Disabled: {{ form.controls.select.disabled }}</pre>
-    <pre>Value: {{ form.controls.select.value }}</pre>
-    <pre>Valid: {{ form.controls.select.valid }}</pre>
+    <div>Touched: <span data-field="touched">{{ form.controls.mySelect.touched }}</span></div>
+    <div>Dirty: <span data-field="dirty">{{ form.controls.mySelect.dirty }}</span></div>
+    <div>Disabled: <span data-field="disabled">{{ form.controls.mySelect.disabled }}</span></div>
+    <div>Value: <span data-field="value">{{ form.controls.mySelect.value }}</span></div>
+    <div>Valid: <span data-field="valid">{{ form.controls.mySelect.valid }}</span></div>
+    <div [@if]="submittedValue !== undefined">Submitted: <span data-field="submitted">{{ submittedValue }}</span></div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
@@ -40,18 +43,32 @@ import { PorscheDesignSystemModule } from '@porsche-design-system/components-ang
 })
 export class SelectExampleFormComponent {
   form = new FormGroup({
-    select: new FormControl<string | undefined>(
+    mySelect: new FormControl<string | undefined>(
       { value: undefined, disabled: false },
-      { validators: Validators.required }
+      { validators: Validators.required, nonNullable: true }
     ),
   });
 
+  submittedValue: any = undefined;
+
+  setValue(): void {
+    this.form.controls.mySelect.setValue('a');
+  }
+
+  resetValue(): void {
+    this.form.controls.mySelect.reset({ value: undefined, disabled: false });
+  }
+
   toggleDisabled(): void {
-    const control = this.form.get('select')!;
+    const control = this.form.get('mySelect')!;
     if (control.disabled) {
       control.enable();
     } else {
       control.disable();
     }
+  }
+
+  onSubmit(): void {
+    this.submittedValue = JSON.stringify(this.form.value);
   }
 }
