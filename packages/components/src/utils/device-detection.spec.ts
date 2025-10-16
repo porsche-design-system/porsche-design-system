14212a282996252e3582cee8fbe59e6112fdf9a2
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-10-16 08:48:04 +0200

    feat(components/utils): update istouchdevice test | ms | #housekeeping/update-dependencies-06102025-jest

--- packages/components/src/utils/device-detection.spec.ts
+++ packages/components/src/utils/device-detection.spec.ts
@@ -1,27 +1,30 @@
 import { isTouchDevice } from './device-detection';
 
 describe('isTouchDevice()', () => {
+  const originalNavigator = window.navigator;
+  const originalOntouchstart = (window as any).ontouchstart;
+
+  afterEach(() => {
+    Object.defineProperty(window, 'navigator', { value: originalNavigator, configurable: true });
+    (window as any).ontouchstart = originalOntouchstart;
+  });
+
   it('should return true for touch device', () => {
-    jest.spyOn(window, 'window', 'get').mockImplementation(
-      () =>
-        ({
-          ontouchstart: null,
-          navigator: {
-            maxTouchPoints: 1,
-          },
-        }) as any
-    );
+    Object.defineProperty(window, 'navigator', {
+      value: { maxTouchPoints: 1 },
+      configurable: true,
+    });
+    (window as any).ontouchstart = null;
 
     expect(isTouchDevice()).toBe(true);
   });
 
   it('should return false for non touch device', () => {
-    jest.spyOn(window, 'navigator', 'get').mockImplementation(
-      () =>
-        ({
-          maxTouchPoints: 0,
-        }) as any
-    );
+    Object.defineProperty(window, 'navigator', {
+      value: { maxTouchPoints: 0 },
+      configurable: true,
+    });
+    delete (window as any).ontouchstart;
 
     expect(isTouchDevice()).toBe(false);
   });
