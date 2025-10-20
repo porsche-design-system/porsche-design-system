4db7b81392f42bec9ff6d10ec327462b2df92092
henrijoss <henri.joss@mhp.com>
2025-10-20 16:52:01 +0200

    feat(components-vue): add v-model examples | hj | #4065

---
+++ packages/components-vue/src/examples/PinCodeExampleControlledModel.vue
@@ -0,0 +1,18 @@
+<template>
+  <PPinCode v-model:value="myPinCode" :name="'myPinCode'" :label="'Some Label'"></PPinCode>
+
+  <button type="button" @click="resetValue">Reset</button>
+
+  <div>Value: <span data-field="value">{{ myPinCode }}</span></div>
+</template>
+
+<script setup lang="ts">
+import { PPinCode } from '@porsche-design-system/components-vue';
+import { ref } from 'vue';
+
+const myPinCode = ref('');
+
+const resetValue = () => {
+  myPinCode.value = '';
+};
+</script>
---
+++ packages/components-vue/src/examples/SelectExampleControlledModel.vue
@@ -0,0 +1,25 @@
+<template>
+  <PSelect v-model:value="mySelect" :name="'mySelect'" :label="'Some Label'">
+    <PSelectOption :value="'a'">Option A</PSelectOption>
+    <PSelectOption :value="'b'">Option B</PSelectOption>
+    <PSelectOption :value="'c'">Option C</PSelectOption>
+    <PSelectOption :value="'d'">Option D</PSelectOption>
+    <PSelectOption :value="'e'">Option E</PSelectOption>
+    <PSelectOption :value="'f'">Option F</PSelectOption>
+  </PSelect>
+
+  <button type="button" @click="resetValue">Reset</button>
+
+  <div>Value: <span data-field="value">{{ mySelect }}</span></div>
+</template>
+
+<script setup lang="ts">
+import { PSelect, PSelectOption } from '@porsche-design-system/components-vue';
+import { ref } from 'vue';
+
+const mySelect = ref('');
+
+const resetValue = () => {
+  mySelect.value = '';
+};
+</script>
