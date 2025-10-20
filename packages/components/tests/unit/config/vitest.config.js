5a025bd309235de61e7a52e655306d6ec64c9240
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-10-20 15:17:18 +0200

    feat(components): add vitest | ms | #4069

---
+++ packages/components/tests/unit/config/vitest.config.js
@@ -0,0 +1,28 @@
+/// <reference types="vitest" />
+
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom', // replaces `testEnvironment: 'jsdom'`
+    root: path.resolve(__dirname, '../../../'), // replaces `rootDir`
+    setupFiles: ['tests/unit/config/jest.setup.ts'], // same purpose
+    include: ['src/**/*.spec.ts', 'tests/unit/specs/**/*.spec.ts'], // replaces `testMatch`
+    exclude: ['dist'], // replaces `modulePathIgnorePatterns`
+    globals: true, // enables global test APIs like describe, it, expect
+    clearMocks: true,
+    restoreMocks: true,
+    alias: {
+      '@stencil/core': path.resolve(__dirname, '../../../tests/unit/mocks/stencil-decorator.mocks.ts'),
+    }, // replaces `moduleNameMapper`
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+  define: {
+    ROLLUP_REPLACE_IS_STAGING: JSON.stringify('production'),
+    ROLLUP_REPLACE_CDN_BASE_URL: JSON.stringify('https://cdn.ui.porsche.com/porsche-design-system'),
+  },
+});
