ff31be200fba6564964c16b17c35dfe5fd1337ec
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-10-27 13:40:40 +0100

    feat(components): removed slot documentation | ms | #4069

--- packages/components/src/components/checkbox/checkbox.tsx
+++ packages/components/src/components/checkbox/checkbox.tsx
@@ -50,10 +50,6 @@ const propTypes: PropTypes<typeof Checkbox> = {
   theme: AllowedTypes.oneOf<Theme>(THEMES),
 };
 
-/**
- * @slot {"name": "label", "description": "Shows a label. Only [phrasing content](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Content_categories#Phrasing_content) is allowed." }
- * @slot {"name": "message", "description": "Shows a state message. Only [phrasing content](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Content_categories#Phrasing_content) is allowed." }
- */
 @Component({
   tag: 'p-checkbox',
   shadow: { delegatesFocus: true },
