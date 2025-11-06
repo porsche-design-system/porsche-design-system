3a923ba3969dda47407e015675ebcfa234c010c4
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-11-06 15:43:04 +0100

    feat(components-angular): adjust test | ms | #4069-angular

--- packages/components-angular/tests/unit/specs/partials.spec.ts
+++ packages/components-angular/tests/unit/specs/partials.spec.ts
@@ -1,11 +1,24 @@
-import * as jsPartials from '@porsche-design-system/components-js/partials';
 import * as angularPartials from '@porsche-design-system/components-angular/partials';
+import * as jsPartials from '@porsche-design-system/components-js/partials';
+import { describe, expect, it } from 'vitest';
 
-describe('Angular partials', () => {
-  const angularPartialKeys = Object.keys(angularPartials).sort();
-  const jsPartialKeys = Object.keys(jsPartials).sort();
+describe('partials', () => {
+  const angularPartialKeys = Object.keys(angularPartials)
+    .filter((key) => typeof (angularPartials as any)[key] === 'function')
+    .sort();
+  const jsPartialKeys = Object.keys(jsPartials)
+    .filter((key) => typeof (jsPartials as any)[key] === 'function')
+    .sort();
 
   it('should match snapshot', () => {
     expect(angularPartialKeys).toMatchSnapshot();
+    expect(jsPartialKeys).toMatchSnapshot();
+  });
+
+  it('should reexport all functions from js partials', () => {
+    angularPartialKeys.forEach((exportName) => {
+      expect(jsPartialKeys).toContain(exportName);
+      expect(typeof (jsPartials as any)[exportName]).toBe('function');
+    });
   });
 });
