6b73122a15c257042f29b3abd84cb2a765653ce5
Oliver Hook <oliver.hook@porsche.digital>
2025-11-06 16:06:36 +0100

    test(components/-react-ssr): adapt unit test ad update snapshots | oh | #3997

--- packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.spec.ts
+++ packages/components/src/components/segmented-control/segmented-control/segmented-control-styles.spec.ts
@@ -3,11 +3,14 @@ import { getComponentCss } from './segmented-control-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    [20, 2, 'none', 'auto'],
-    [80, 1, 'none', 'light'],
-    [230, 10, 'error', 'light'],
-    [20, { base: 4, s: 3, m: 2, l: 'auto' }, 'none', 'light'],
-  ])('should return correct css for maxWidth: %s, columns: %s, state: %s and theme: %s', (...args) => {
-    validateCssAndMatchSnapshot(getComponentCss(...args));
-  });
+    [20, 2, false, false, 'none', 'auto'],
+    [80, 1, true, true, 'none', 'light'],
+    [230, 10, false, { base: true, xs: false, s: true, m: false, l: true, xl: false }, 'error', 'light'],
+    [20, { base: 4, s: 3, m: 2, l: 'auto' }, false, false, 'none', 'light'],
+  ])(
+    'should return correct css for maxWidth: %s, columns: %s, disabled: %s, hideLabel: %o, state: %s and theme: %s',
+    (...args) => {
+      validateCssAndMatchSnapshot(getComponentCss(...args));
+    }
+  );
 });
