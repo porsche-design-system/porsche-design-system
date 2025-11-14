b2b70a50d2ab13dd28bc2ab2487a9a930e00c839
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-11-14 18:24:42 +0100

    feat(assets/fallbacks): adjust configs for vitest types | ms | #4069-combined

--- packages/assets/projects/fallbacks/tests/unit/config/vitest.config.ts
+++ packages/assets/projects/fallbacks/tests/unit/config/vitest.config.ts
@@ -1,9 +1,14 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['**/src/**/*.spec.ts'],
-    globals: true,
+    root: resolve(__dirname, '../../../'),
+    include: ['**/*.spec.ts'],
     environment: 'jsdom',
+    globals: true,
+    typecheck: {
+      tsconfig: './tsconfig.test.json',
+    },
   },
 });
