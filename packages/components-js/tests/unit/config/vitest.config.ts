c1fa7da2bc04bba8610d41ee1d9c95a5d3fc7aa6
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-11-17 09:06:16 +0100

    feat(components-js): exclude projects/ and resolve @ path | ms | #4069-combined

--- packages/components-js/tests/unit/config/vitest.config.ts
+++ packages/components-js/tests/unit/config/vitest.config.ts
@@ -2,9 +2,18 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: /^@porsche-design-system\/shared\/testing$/,
+        replacement: resolve(__dirname, '../../../../shared/dist/testing/esm/index.mjs'),
+      },
+    ],
+    conditions: ['import', 'module', 'browser', 'default'],
+  },
   test: {
     root: resolve(__dirname, '../../../'),
-    include: ['**/*.spec.ts'],
+    include: ['tests/unit/specs/**/*.spec.ts', '!**/projects/**'],
     globals: true,
     environment: 'jsdom',
     typecheck: {
