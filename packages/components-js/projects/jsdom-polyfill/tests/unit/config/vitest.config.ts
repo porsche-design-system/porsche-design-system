4a56c90cb9b3bda8680e4b92297be9a3ef5cbd1b
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-11-17 09:05:45 +0100

    feat(components-js/polyfill): specified root | ms | #4069-combined

--- packages/components-js/projects/jsdom-polyfill/tests/unit/config/vitest.config.ts
+++ packages/components-js/projects/jsdom-polyfill/tests/unit/config/vitest.config.ts
@@ -1,8 +1,10 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['**/tests/unit/specs/**/*.spec.ts'],
+    root: resolve(__dirname, '../../../'),
+    include: ['**/*.spec.ts'],
     globals: true,
     environment: 'jsdom',
     setupFiles: './tests/unit/config/vitest.setup.ts',
