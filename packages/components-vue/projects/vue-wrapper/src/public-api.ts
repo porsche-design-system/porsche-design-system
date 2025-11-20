064fd2d8372ba5a69bafae890cc521b4e6f5a383
Stephan Schröter <stephan.schroeter@porsche.digital>
2025-11-20 12:15:18 +0100

    fix(components-vue): enable build | sas / hj

--- packages/components-vue/projects/vue-wrapper/src/public-api.ts
+++ packages/components-vue/projects/vue-wrapper/src/public-api.ts
@@ -1,6 +1,6 @@
 export { componentsReady } from '@porsche-design-system/components-js';
 export * from './lib/components';
 export * from './lib/types';
-export { useToastManager, themeInjectionKey, prefixInjectionKey } from './utils';
 export { default as PorscheDesignSystemProvider } from './PorscheDesignSystemProvider.vue';
 export { createPorscheDesignSystem, usePorscheDesignSystemPlugin } from './plugin';
+export { prefixInjectionKey, useToastManager } from './utils';
