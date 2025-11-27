441cfbaffd01d39aefa1ebb801194ba5dd9182be
Oliver Hook <oliver.hook@porsche.digital>
2025-11-27 08:33:01 +0100

    feat(components/-js): adapt external css var naming | oh | #4096

--- packages/components/src/styles/checkbox/checkbox-css-vars.ts
+++ packages/components/src/styles/checkbox/checkbox-css-vars.ts
@@ -1,5 +1,5 @@
 // TODO: how to define naming convention for internal and external CSS vars.
-export const cssVarInternalCheckboxScaling = '--__p-internal-checkbox-scaling';
+export const cssVarInternalCheckboxScaling = '--__checkbox-scaling';
 export const cssVarInternalCheckboxBorderColor = '--__checkbox-border-color'; // internal for now
 export const cssVarInternalCheckboxBackgroundColor = '--__checkbox-background-color'; // internal for now
 export const cssVarInternalCheckboxTextColor = '--__checkbox-text-color'; // internal for now
