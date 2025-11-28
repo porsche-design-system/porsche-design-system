ca84da6be535b4a044d34b8b22f3623107247b05
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-11-28 14:01:27 +0100

    feat(storefront): disable sitemap test | ms | #4069-combined

--- packages/storefront/tests/e2e/specs/sitemap.e2e.ts
+++ packages/storefront/tests/e2e/specs/sitemap.e2e.ts
@@ -6,7 +6,7 @@ const ONE_MINUTE_IN_MS = 60000;
 // locally the run takes about 3-4 minutes, so we triple that for the pipeline
 test.setTimeout(ONE_MINUTE_IN_MS * 13);
 
-test('should have complete sitemap.json', async ({ page }) => {
+test.skip('should have complete sitemap.json', async ({ page }) => {
   const oldUrls = getSitemap(); // reads fixture/sitemap.json
   const newUrls = await buildSitemap(page); // returns new result and writes results/sitemap.json
 
