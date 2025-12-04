529a6c16d2db8e335565b0235b3d1aef7c7796df
henrijoss <henri.joss@mhp.com>
2025-12-04 16:31:20 +0100

    feat(components-react): add global styles | hj | v4

--- packages/components-react/src/main.tsx
+++ packages/components-react/src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@porsche-design-system/shared/css/styles.css';
+import '@porsche-design-system/components-react/index.css';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
