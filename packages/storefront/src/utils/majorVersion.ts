b1ea9f11ffd790847cee49a241dab07c24c8e7f6
Stephan Schröter <stephan.schroeter@porsche.digital>
2025-12-16 18:04:18 +0100

    feat(storefront): modernize homepage | sas

--- packages/storefront/src/utils/majorVersion.ts
+++ packages/storefront/src/utils/majorVersion.ts
@@ -4,3 +4,5 @@ export const majorVersion = parseInt(
   pkgJson.dependencies['@porsche-design-system/components-react'].replace(/^\^|^~/, ''),
   10
 );
+
+export const fullVersion = pkgJson.dependencies['@porsche-design-system/components-react'];
