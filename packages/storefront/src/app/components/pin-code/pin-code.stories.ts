a5f13434eb8a9fabb53377af007614c1985d547a
Oliver Hook <oliver.hook@porsche.digital>
2026-01-21 14:29:39 +0100

    docs(storefront): add slot examples | oh | #4096

--- packages/storefront/src/app/components/pin-code/pin-code.stories.ts
+++ packages/storefront/src/app/components/pin-code/pin-code.stories.ts
@@ -13,3 +13,66 @@ export const pinCodeStory: Story<'p-pin-code'> = {
     },
   ],
 };
+
+export const pinCodeStorySlots: Story<'p-pin-code'> = {
+  generator: () => [
+    {
+      tag: 'p-pin-code',
+      properties: { state: 'error' },
+      children: [
+        {
+          tag: 'span',
+          properties: { slot: 'label', id: 'some-label-id' },
+          children: [
+            'Some label with a ',
+            {
+              tag: 'a',
+              properties: { href: 'https://designsystem.porsche.com', className: 'underline' },
+              children: ['link'],
+            },
+            ' and a "label-after" slot.',
+          ],
+        },
+        {
+          tag: 'p-popover',
+          properties: { slot: 'label-after', className: 'ms-static-xs' },
+          children: [
+            'Some Popover content with a ',
+            {
+              tag: 'a',
+              properties: { href: 'https://designsystem.porsche.com', className: 'underline' },
+              children: ['link'],
+            },
+            '.',
+          ],
+        },
+        {
+          tag: 'span',
+          properties: { slot: 'description', id: 'some-description-id' },
+          children: [
+            'Some description with a ',
+            {
+              tag: 'a',
+              properties: { href: 'https://designsystem.porsche.com', className: 'underline' },
+              children: ['link'],
+            },
+            '.',
+          ],
+        },
+        {
+          tag: 'span',
+          properties: { slot: 'message', id: 'some-message-id' },
+          children: [
+            'Some error message with a ',
+            {
+              tag: 'a',
+              properties: { href: 'https://designsystem.porsche.com', className: 'underline' },
+              children: ['link'],
+            },
+            '.',
+          ],
+        },
+      ],
+    },
+  ],
+};
