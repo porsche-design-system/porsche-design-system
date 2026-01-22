5134778bdd7c2c656063d80e7e1562631dc56ca7
Mihael Smrček <mihael.smrcek@porsche.digital>
2026-01-22 16:56:44 +0100

    fix(ag-grid): migrate old `emotions` imports to use new `tokens` imports | ms

--- packages/utilities/projects/ag-grid/src/parts/input-styles.ts
+++ packages/utilities/projects/ag-grid/src/parts/input-styles.ts
@@ -1,39 +1,40 @@
 import { createPart, type Part } from 'ag-grid-community';
 import {
-  themeDarkBackgroundBase,
-  themeDarkContrastMedium,
-  themeDarkNotificationError,
-  themeDarkPrimary,
-  themeDarkStateDisabled,
-  themeLightBackgroundBase,
-  themeLightContrastMedium,
-  themeLightNotificationError,
-  themeLightPrimary,
-  themeLightStateDisabled,
+  borderRadiusXl,
+  colorCanvasDark,
+  colorCanvasLight,
+  colorContrastMedium,
+  colorContrastMediumDark,
+  colorDisabledDark,
+  colorDisabledLight,
+  colorErrorDark,
+  colorErrorLight,
+  colorPrimaryDark,
+  colorPrimaryLight,
 } from '../styles';
 import { pdsThemeModeDark } from '../types/theme-mode';
 
 export const inputStyles: Part = createPart({
   feature: 'inputStyles',
   params: {
-    inputBorder: `1px solid ${themeLightContrastMedium}`,
-    inputInvalidBorder: `1px solid ${themeLightNotificationError}`,
-    inputDisabledBorder: `1px solid ${themeLightStateDisabled}`,
-    inputDisabledBackgroundColor: themeLightBackgroundBase,
-    inputFocusBorder: `1px solid ${themeLightPrimary}`,
+    inputBorder: `1px solid ${colorContrastMedium}`,
+    inputInvalidBorder: `1px solid ${colorErrorLight}`,
+    inputDisabledBorder: `1px solid ${colorDisabledLight}`,
+    inputDisabledBackgroundColor: colorCanvasLight,
+    inputFocusBorder: `1px solid ${colorPrimaryLight}`,
   },
   modeParams: {
     [pdsThemeModeDark]: {
-      inputBorder: `1px solid ${themeDarkContrastMedium}`,
-      inputInvalidBorder: `1px solid ${themeDarkNotificationError}`,
-      inputDisabledBorder: `1px solid ${themeDarkStateDisabled}`,
-      inputDisabledBackgroundColor: themeDarkBackgroundBase,
-      inputFocusBorder: `1px solid ${themeDarkPrimary}`,
+      inputBorder: `1px solid ${colorContrastMediumDark}`,
+      inputInvalidBorder: `1px solid ${colorErrorDark}`,
+      inputDisabledBorder: `1px solid ${colorDisabledDark}`,
+      inputDisabledBackgroundColor: colorCanvasDark,
+      inputFocusBorder: `1px solid ${colorPrimaryDark}`,
     },
   },
   css: `
     .ag-text-field-input {
-      border-radius: 12px;
+      border-radius: ${borderRadiusXl};
     }
   `,
 });
--- packages/utilities/projects/ag-grid/src/parts/toggle-button-styles.ts
+++ packages/utilities/projects/ag-grid/src/parts/toggle-button-styles.ts
@@ -1,66 +1,64 @@
-import { type Part, createPart } from 'ag-grid-community';
+import { createPart, type Part } from 'ag-grid-community';
 import {
-  borderWidthBase,
-  spacingStaticXSmall,
-  themeDarkBackgroundBase,
-  themeDarkContrastMedium,
-  themeDarkNotificationSuccess,
-  themeDarkPrimary,
-  themeDarkStateDisabled,
-  themeDarkSuccessColorDarken,
-  themeLightBackgroundBase,
-  themeLightContrastMedium,
-  themeLightNotificationSuccess,
-  themeLightPrimary,
-  themeLightStateDisabled,
-  themeLightSuccessColorDarken,
+  borderWidthRegular,
+  colorCanvasDark,
+  colorCanvasLight,
+  colorContrastMediumDark,
+  colorContrastMediumLight,
+  colorDisabledDark,
+  colorDisabledLight,
+  colorPrimaryDark,
+  colorPrimaryLight,
+  colorSuccessDark,
+  colorSuccessLight,
+  spacingStaticXs,
 } from '../styles';
 import { pdsThemeModeDark } from '../types/theme-mode';
 
 export const toggleButtonStyle: Part = createPart({
   feature: 'toggleButtonStyle',
   params: {
-    toggleButtonOnBackgroundColor: themeLightNotificationSuccess,
-    toggleButtonOffBackgroundColor: themeLightBackgroundBase,
-    toggleButtonSwitchBackgroundColor: themeLightPrimary,
-    toggleButtonOnHoverBackgroundColor: themeLightSuccessColorDarken,
-    toggleButtonOnBorderColor: themeLightNotificationSuccess,
-    toggleButtonOnHoverBorderColor: themeLightSuccessColorDarken,
-    toggleButtonOffHoverBorderColor: themeLightPrimary,
-    toggleButtonOffBorderColor: themeLightContrastMedium,
-    toggleButtonDisabledColor: themeLightStateDisabled,
+    toggleButtonOnBackgroundColor: colorSuccessLight,
+    toggleButtonOffBackgroundColor: colorCanvasLight,
+    toggleButtonSwitchBackgroundColor: colorPrimaryLight,
+    toggleButtonOnHoverBackgroundColor: colorSuccessLight,
+    toggleButtonOnBorderColor: colorSuccessLight,
+    toggleButtonOnHoverBorderColor: colorSuccessLight,
+    toggleButtonOffHoverBorderColor: colorPrimaryLight,
+    toggleButtonOffBorderColor: colorContrastMediumLight,
+    toggleButtonDisabledColor: colorDisabledLight,
   },
   modeParams: {
     [pdsThemeModeDark]: {
-      toggleButtonOnBackgroundColor: themeDarkNotificationSuccess,
-      toggleButtonOffBackgroundColor: themeDarkBackgroundBase,
-      toggleButtonSwitchBackgroundColor: themeDarkPrimary,
-      toggleButtonOnHoverBackgroundColor: themeDarkSuccessColorDarken,
-      toggleButtonOnBorderColor: themeDarkNotificationSuccess,
-      toggleButtonOnHoverBorderColor: themeDarkSuccessColorDarken,
-      toggleButtonOffHoverBorderColor: themeDarkPrimary,
-      toggleButtonOffBorderColor: themeDarkContrastMedium,
-      toggleButtonDisabledColor: themeDarkStateDisabled,
+      toggleButtonOnBackgroundColor: colorSuccessDark,
+      toggleButtonOffBackgroundColor: colorCanvasDark,
+      toggleButtonSwitchBackgroundColor: colorPrimaryDark,
+      toggleButtonOnHoverBackgroundColor: colorSuccessDark,
+      toggleButtonOnBorderColor: colorSuccessDark,
+      toggleButtonOnHoverBorderColor: colorSuccessDark,
+      toggleButtonOffHoverBorderColor: colorPrimaryDark,
+      toggleButtonOffBorderColor: colorContrastMediumDark,
+      toggleButtonDisabledColor: colorDisabledDark,
     },
   },
   css: `
     .ag-toggle-button-input-wrapper {
-        border: ${borderWidthBase} solid var(--ag-toggle-button-off-border-color);
+        border: ${borderWidthRegular} solid var(--ag-toggle-button-off-border-color);
         position: relative;
 
         &::before {
-            height: calc(var(--ag-toggle-button-height) - ${spacingStaticXSmall} * 2) !important;
-            width: calc(var(--ag-toggle-button-height) - ${spacingStaticXSmall} * 2) !important;
-            top: calc(${spacingStaticXSmall} - ${borderWidthBase}) !important;
-            left: calc(${spacingStaticXSmall} - ${borderWidthBase}) !important;
+            height: calc(var(--ag-toggle-button-height) - ${spacingStaticXs} * 2) !important;
+            width: calc(var(--ag-toggle-button-height) - ${spacingStaticXs} * 2) !important;
+            top: calc(${spacingStaticXs} - ${borderWidthRegular}) !important;
+            left: calc(${spacingStaticXs} - ${borderWidthRegular}) !important;
         }
 
         &.ag-checked {
             border-color: var(--ag-toggle-button-on-border-color);
 
             &::before {
-                --ag-toggle-button-switch-background-color: ${themeLightBackgroundBase};
-                --ag-toggle-button-on-border-color: ${themeLightBackgroundBase};
+                --ag-toggle-button-switch-background-color: ${colorCanvasLight};
+                --ag-toggle-button-on-border-color: ${colorCanvasLight};
                 left: calc(100% - var(--ag-toggle-button-height) + 6px) !important;
             }
         }
