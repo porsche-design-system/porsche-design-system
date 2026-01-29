834d69e6724769c4d1a3ac4c627ff2b0a4a94d72
henrijoss <henri.joss@mhp.com>
2026-01-29 16:59:56 +0100

    feat(styles/emotion): add new border, shadow, focus styles & deprecate old styles | hj

--- packages/styles/projects/emotion/src/border/deprecated/border.spec.ts
+++ packages/styles/projects/emotion/src/border/deprecated/border.spec.ts
@@ -1,3 +1,4 @@
+import { expect, it } from 'vitest';
 import * as fromBorder from './';
 
 it('should provide all exports', () => {
--- packages/styles/projects/emotion/src/shadow/deprecated/dropShadowHighStyle.ts
+++ packages/styles/projects/emotion/src/shadow/deprecated/dropShadowHighStyle.ts
@@ -1,5 +1,6 @@
 import { shadowLg } from '@porsche-design-system/tokens';
 
+/** @deprecated since v4.0.0, will be removed with next major release. Use boxShadow: shadowLg instead. */
 export const dropShadowHighStyle = {
   boxShadow: shadowLg,
 } as const;
