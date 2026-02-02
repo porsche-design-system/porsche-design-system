db18d386605a19e678826596de661df68e4f93bd
henrijoss <henri.joss@mhp.com>
2026-02-02 13:44:58 +0100

    test(styles/scss): update snapshots | hj

--- packages/styles/projects/scss/tests/unit/specs/_skeleton.spec.ts
+++ packages/styles/projects/scss/tests/unit/specs/_skeleton.spec.ts
@@ -5,12 +5,12 @@ import { describe, expect, it } from 'vitest';
 
 const borderVariables = fs.readFileSync(path.resolve('dist/_border.scss'), 'utf8');
 const motionVariables = fs.readFileSync(path.resolve('dist/_motion.scss'), 'utf8');
-const themeVariables = fs.readFileSync(path.resolve('dist/_theme.scss'), 'utf8');
+const themeVariables = fs.readFileSync(path.resolve('dist/_color.scss'), 'utf8');
 const skeletonMixin = fs
   .readFileSync(path.resolve('src/_skeleton.scss'), 'utf8')
   .replace(/@use.*;/g, '')
   .replace(/border\./g, '')
-  .replace(/theme\./g, '')
+  .replace(/color\./g, '')
   .replace(/motion\./g, '');
 
 describe('pds-skeleton()', () => {
