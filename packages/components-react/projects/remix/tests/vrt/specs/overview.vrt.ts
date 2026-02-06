edd9eefaecb2a0a36984c84a3455e8c078d08ebc
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-02-06 13:18:33 +0100

    test(component-react/nextjs): fix tests | sas / hj

--- packages/components-react/projects/remix/tests/vrt/specs/overview.vrt.ts
+++ packages/components-react/projects/remix/tests/vrt/specs/overview.vrt.ts
@@ -2,13 +2,13 @@ import { expect, test } from '@playwright/test';
 import { viewportWidthXXL } from '@porsche-design-system/shared/testing';
 
 test.describe('overview', async () => {
-  test(`should have no visual regression`, async ({ page }) => {
+  test('components', async ({ page }) => {
     await page.goto('/overview');
     await page.setViewportSize({
       width: viewportWidthXXL,
       height: await page.evaluate(() => document.body.clientHeight),
     });
     await page.mouse.click(0, 0); // click top left corner of the page to remove autofocus
-    await expect(page.locator('#app')).toHaveScreenshot(`overview-${viewportWidthXXL}.png`);
+    await expect(page.locator('#app')).toHaveScreenshot(`overview-components-${viewportWidthXXL}.png`);
   });
 });
--- packages/components-react/tests/vrt/specs/components/overview.vrt.ts
+++ packages/components-react/tests/vrt/specs/components/overview.vrt.ts
@@ -10,7 +10,7 @@ test.describe('overview', async () => {
       width: viewportWidthXXL,
       height: await page.evaluate(() => document.body.clientHeight),
     });
-    await page.mouse.click(0, 0);
+    await page.mouse.click(0, 0); // click top left corner of the page to remove autofocus
     await expect(page.locator('#app')).toHaveScreenshot(`overview-components-${viewportWidthXXL}.png`);
   });
 });
