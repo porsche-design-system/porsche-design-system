6abba220ec62cb423309ba3c474c4c1d76a0bd6e
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-02-18 16:52:48 +0100

    test(components): update snapshot | sas | #4160

--- packages/components/src/components/accordion/accordion-styles.spec.ts
+++ packages/components/src/components/accordion/accordion-styles.spec.ts
@@ -4,19 +4,21 @@ import { getComponentCss } from './accordion-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['start', 'frosted', false, false, false],
-    ['start', 'frosted', true, false, false],
-    ['start', 'frosted', true, true, false],
-    ['start', 'frosted', true, true, true],
-    ['end', 'frosted', true, true, true],
-    ['start', 'frosted', false, false, false],
-    ['start', 'canvas', false, false, false],
-    ['start', 'canvas', false, false, true],
-    ['start', 'canvas', false, true, true],
-    ['start', 'surface', false, true, true],
-    ['start', 'none', false, false, false],
+    ['start', 'frosted', false, false, false, false, false],
+    ['start', 'frosted', true, false, false, false, false],
+    ['start', 'frosted', true, true, false, false, false],
+    ['start', 'frosted', true, true, true, false, false],
+    ['end', 'frosted', true, true, true, false, false],
+    ['start', 'frosted', true, true, true, true, false],
+    ['start', 'frosted', true, true, true, true, true],
+    ['start', 'frosted', false, false, false, false, false],
+    ['start', 'canvas', false, false, false, false, false],
+    ['start', 'canvas', false, false, true, false, false],
+    ['start', 'canvas', false, true, true, false, false],
+    ['start', 'surface', false, true, true, false, false],
+    ['start', 'none', false, false, false, false, false],
   ])(
-    'should return correct css for alignIcon: %s, background: %s, isCompact: %s, isOpen: %s and isSticky: %s',
+    'should return correct css for alignMarker: %s, background: %s, isCompact: %s, isOpen: %s, isSticky: %s, hasSummaryBefore: %s, hasSummaryAfter: %s',
     (...args) => {
       validateCssAndMatchSnapshot(getComponentCss(...args));
     }
