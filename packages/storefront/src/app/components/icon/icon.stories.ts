31229a5930488928b59209cfa2ca9686686d1bd1
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-03-16 19:01:45 +0100

    docs(storefront): update examples for icon | sas | #4233

--- packages/storefront/src/app/components/icon/icon.stories.ts
+++ packages/storefront/src/app/components/icon/icon.stories.ts
@@ -1,9 +1,9 @@
 'use client';
 
-import type { Story } from '@/models/story';
 import { ICONS_MANIFEST } from '@porsche-design-system/assets';
 import type { PIconProps } from '@porsche-design-system/components-react/ssr';
 import { capitalCase } from 'change-case';
+import type { Story } from '@/models/story';
 
 export const iconStory: Story<'p-icon'> = {
   generator: ({ properties } = {}) => [
@@ -20,6 +20,105 @@ export const iconStoryOverview: Story<'p-icon'> = {
       tag: 'p-icon',
       properties: { name: icon as PIconProps['name'], aria: { 'aria-label': `${capitalCase(icon)} icon` } },
     })),
+};
+
+export const iconStorySize: Story<'p-icon'> = {
+  state: {
+    properties: {
+      className: 'me-static-sm',
+      name: 'highway',
+      size: 'inherit',
+      aria: { 'aria-label': 'Highway icon' },
+    },
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-text',
+      properties: { className: 'text-[48px]', size: 'inherit' },
+      children: [
+        {
+          tag: 'p-icon',
+          properties,
+        },
+        'Some text',
+      ],
+    },
+  ],
+};
+
+export const iconStorySizeCSSVar: Story<'p-icon'> = {
+  state: {
+    properties: {
+      name: 'highway',
+      className: '[--p-icon-size:48px]',
+      aria: { 'aria-label': 'Highway icon' },
+    },
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-icon',
+      properties,
+    },
+  ],
+};
+
+export const iconStoryResponsiveSize: Story<'p-icon'> = {
+  state: {
+    properties: {
+      name: 'highway',
+      size: { base: 'small', l: 'xx-large' },
+      aria: { 'aria-label': 'Highway icon' },
+    },
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-icon',
+      properties,
+    },
+  ],
+};
+
+export const iconStoryColor: Story<'p-icon'> = {
+  state: {
+    properties: {
+      className: 'me-static-sm',
+      name: 'highway',
+      color: 'inherit',
+      aria: { 'aria-label': 'Highway icon' },
+    },
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-text',
+      properties: {
+        className: 'text-[deeppink]',
+        color: 'inherit',
+      },
+      children: [
+        {
+          tag: 'p-icon',
+          properties,
+        },
+        'Some text',
+      ],
+    },
+  ],
+};
+
+export const iconStoryColorCSSVar: Story<'p-icon'> = {
+  state: {
+    properties: {
+      name: 'highway',
+      className: '[--p-icon-color:deeppink]',
+      aria: { 'aria-label': 'Highway icon' },
+    },
+  },
+  generator: ({ properties } = {}) => [
+    {
+      tag: 'p-icon',
+      properties,
+    },
+  ],
 };
 
 export const iconStoryCustom: Story<'p-icon'> = {
