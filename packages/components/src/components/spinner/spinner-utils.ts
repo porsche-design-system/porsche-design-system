474eba201445c0dec962a8853c5eadfe46cf8ef1
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-03-18 16:17:54 +0100

    feat(components): align spinner with text and heading | sas | #4245

--- packages/components/src/components/spinner/spinner-utils.ts
+++ packages/components/src/components/spinner/spinner-utils.ts
@@ -1,15 +1,21 @@
 export const SPINNER_COLORS = ['primary', 'inherit'] as const;
 export type SpinnerColor = (typeof SPINNER_COLORS)[number];
 
+/** @deprecated */
+export const SPINNER_SIZES_DEPRECATED = ['small', 'medium', 'large'] as const;
 export const SPINNER_SIZES = [
-  'xx-small',
-  'x-small',
-  'small',
-  'medium',
-  'large',
-  'x-large',
-  'xx-large',
+  '2xs',
+  'xs',
+  'sm',
+  'md',
+  'lg',
+  'xl',
+  '2xl',
+  '3xl',
+  '4xl',
+  '5xl',
   'inherit',
+  ...SPINNER_SIZES_DEPRECATED,
 ] as const;
 export type SpinnerSize = (typeof SPINNER_SIZES)[number];
 
