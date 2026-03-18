ab5cd386c3eb3010911a196f7406592c31829c17
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-03-18 21:16:06 +0100

    test(components): update snapshots | sas | #4245

--- packages/components/src/components/heading/heading-styles.spec.ts
+++ packages/components/src/components/heading/heading-styles.spec.ts
@@ -3,15 +3,15 @@ import { getComponentCss } from './heading-styles';
 
 describe('getComponentCss()', () => {
   it.each<Parameters<typeof getComponentCss>>([
-    ['xx-large', 'start', 'primary', false],
-    ['x-large', 'start', 'primary', false],
-    ['x-large', 'end', 'primary', false],
-    ['large', 'center', 'inherit', true],
-    ['medium', 'center', 'inherit', true],
-    ['small', 'center', 'inherit', true],
-    ['small', 'inherit', 'inherit', true],
-    ['inherit', 'center', 'inherit', true],
-    [{ base: 'small', xs: 'medium', s: 'large', m: 'x-large', l: 'xx-large', xl: 'large' }, 'end', 'inherit', false],
+    ['2xl', 'normal', 'start', 'primary', false],
+    ['xl', 'normal', 'start', 'primary', false],
+    ['xl', 'normal', 'end', 'primary', false],
+    ['lg', 'normal', 'center', 'inherit', true],
+    ['md', 'normal', 'center', 'inherit', true],
+    ['sm', 'normal', 'center', 'inherit', true],
+    ['sm', 'normal', 'inherit', 'inherit', true],
+    ['inherit', 'normal', 'center', 'inherit', true],
+    [{ base: 'sm', xs: 'md', s: 'lg', m: 'xl', l: '2xl', xl: 'lg' }, 'normal', 'end', 'inherit', false],
   ])('should return correct css for size: %j, align: %s, color: %s and ellipsis: %s', (...args) => {
     validateCssAndMatchSnapshot(getComponentCss(...args));
   });
