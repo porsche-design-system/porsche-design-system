66bd4f0d787d3e8ef89a5c9de2c12f99421e3ef5
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-03-18 10:09:01 +0100

    docs(storefront): update blur stories | sas

--- packages/storefront/src/app/scss/blur/examples/blur.stories.ts
+++ packages/storefront/src/app/scss/blur/examples/blur.stories.ts
@@ -32,9 +32,7 @@ export const blurScssStory: Story<'div'> = {
             className:
               'backdrop-blur-frosted bg-frosted col-1 row-1 rounded-lg m-fluid-lg p-fluid-sm grid place-items-center',
           },
-          children: [
-            'backdrop-filter: $blur-frosted; background-color: $color-frosted | $color-frosted-light | $color-frosted-dark;',
-          ],
+          children: ['backdrop-filter: $blur-frosted; background-color: $color-frosted;'],
         },
       ],
     },
