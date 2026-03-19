0adf2b813defc6d3d84c929fa04d23234aed7173
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-03-19 13:18:02 +0100

    feat(storefront): improve stylesheet section | sas | #4245

--- packages/storefront/src/app/stylesheets/css-variables/page.tsx
+++ packages/storefront/src/app/stylesheets/css-variables/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function Page() {
-  redirect('/stylesheets/color/examples');
+  redirect('/stylesheets/css-variables/introduction');
 }
