9d8b09b0013a0749a6af9edb6ec60195d975117c
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-03-19 19:23:41 +0100

    feat(ci/cd): trusted publishing for npm packages | sas

--- .github/workflows/release-npm.sh
+++ .github/workflows/release-npm.sh
@@ -16,10 +16,10 @@ publish_npmjs() {
   echo "task: [$(date)] \"publish_npmjs\" (${PACKAGE_LOCATION})"
 
   if [[ ${NPM_TAG_NAME} ]]; then
-    npm publish --tag ${NPM_TAG_NAME} --non-interactive --access public "${PACKAGE_LOCATION}"
+    npm publish --tag ${NPM_TAG_NAME} --access public "${PACKAGE_LOCATION}"
   else
     # tagged with "latest"
-    npm publish --non-interactive --access public "${PACKAGE_LOCATION}"
+    npm publish --access public "${PACKAGE_LOCATION}"
   fi
 }
 
