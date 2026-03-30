04b079e81c0e420adb998732948504c64482e9d9
henrijoss <henri.joss@mhp.com>
2026-03-30 10:44:56 +0200

    test(components-js): adjust test | hj

--- packages/components-js/tests/smoke/unit/specs/package.smoke.ts
+++ packages/components-js/tests/smoke/unit/specs/package.smoke.ts
@@ -178,6 +178,7 @@ describe('package.json files', () => {
                   prob.entrypoint === './styles' ||
                   prob.entrypoint === './styles/vanilla-extract' ||
                   prob.entrypoint.includes('tailwindcss') ||
+                  prob.entrypoint.includes('color-scheme') ||
                   prob.entrypoint.includes('font-face') ||
                   prob.entrypoint.includes('normalize') ||
                   prob.entrypoint.includes('legacy-radius') ||
