afa87bd09aef5085497a1f8cff03f8c7cac65020
henrijoss <henri.joss@mhp.com>
2026-04-09 13:48:44 +0200

    feat: switch from globby to fast-glob | hj

--- packages/components-js/tests/smoke/unit/specs/package.smoke.ts
+++ packages/components-js/tests/smoke/unit/specs/package.smoke.ts
@@ -2,7 +2,7 @@ import { createRequire } from 'node:module';
 import { type Analysis, checkPackage, createPackageFromTarballData, type Problem } from '@arethetypeswrong/core';
 import { execSync } from 'child_process';
 import * as fs from 'fs';
-import { globbySync } from 'globby';
+import { sync as globbySync } from 'fast-glob';
 import * as path from 'path';
 import { describe, expect, test } from 'vitest';
 import componentsJsPackageJson from '../../../../dist/components-wrapper/package.json';
