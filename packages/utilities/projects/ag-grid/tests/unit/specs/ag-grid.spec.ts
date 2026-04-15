4d4c7641b571036a943a2059161f8dddc544bef0
Mihael Smrček <mihael.smrcek@porsche.digital>
2026-04-15 15:49:17 +0200

    test(utilities): update css snapshot | ms | #4267

--- packages/utilities/projects/ag-grid/tests/unit/specs/ag-grid.spec.ts
+++ packages/utilities/projects/ag-grid/tests/unit/specs/ag-grid.spec.ts
@@ -13,7 +13,7 @@ describe('pdsTheme', () => {
   });
 
   it('should match the snapshot for CSS Chunk', () => {
-    const cssChunk = theme._getParamsCss();
+    const cssChunk = theme._getPerInstanceCss('ag-theme-pds');
     expect(cssChunk).toMatchSnapshot();
   });
 
