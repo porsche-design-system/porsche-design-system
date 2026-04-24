8e93384b2475aeda8dd41c21a1fac8f678987a03
henrijoss <henri.joss@mhp.com>
2026-04-24 16:40:38 +0200

    test(components): adjust test | hj

--- packages/components/src/components/table/table/table.spec.ts
+++ packages/components/src/components/table/table/table.spec.ts
@@ -3,14 +3,14 @@ import { Table } from './table';
 import * as tableUtils from './table-utils';
 
 describe('table', () => {
-  describe('componentWillLoad', () => {
+  describe('componentDidLoad', () => {
     it('should call warnIfCaptionIsMissing() with correct parameters', () => {
       const spy = vi.spyOn(tableUtils, 'warnIfCaptionIsMissing');
       const component = new Table();
       component.host = document.createElement('p-table');
       component.host.attachShadow({ mode: 'open' });
       component.caption = 'some valid caption';
-      component.componentWillLoad();
+      component.componentDidLoad();
 
       expect(spy).toHaveBeenCalledWith(component.host, 'some valid caption');
     });
