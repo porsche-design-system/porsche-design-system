733aa6606f1400415c3e1b23ecd10b73f7d47848
henrijoss <henri.joss@mhp.com>
2026-04-29 09:11:24 +0200

    feat(components): adjust dsr replacement | hj | #4319

--- packages/components/scripts/generateDSRComponents.ts
+++ packages/components/scripts/generateDSRComponents.ts
@@ -675,7 +675,8 @@ import { get${componentName}Css } from '${stylesBundleImportPath}';
           .replace(/this\.props\.disabled = disabled;/, '')
           .replace(/this\.props\.value = state;/, '')
           .replace(/formDisabledCallback\(disabled: boolean\)/, 'formDisabledCallback()')
-          .replace(/formStateRestoreCallback\(state: string\)/, 'formStateRestoreCallback()');
+          .replace(/formStateRestoreCallback\(state: string\)/, 'formStateRestoreCallback()')
+          .replace(/this\.props\.parsedValue/, 'this.parsedValue');
       } else if (tagName === 'p-canvas') {
         newFileContent = newFileContent
           .replace(
