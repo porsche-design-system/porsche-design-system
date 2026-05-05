33c0f23a05b22dd386ca7880a97387586087f2f1
Mihael Smrček <mihael.smrcek@porsche.digital>
2026-05-05 13:55:01 +0200

    feat(components/model-signature): add deprecate `size` prop message | ms, rk | #4373

--- packages/components/src/components/model-signature/model-signature.tsx
+++ packages/components/src/components/model-signature/model-signature.tsx
@@ -45,7 +45,8 @@ export class ModelSignature {
   /** Defines whether the model signature is always loaded or only loaded when it is in the viewport (this feature may not work reliably). */
   @Prop() public lazy?: boolean = false;
 
-  /** Adapts the size of the component. When set to `inherit` a CSS `width` or `height` needs to be defined on the host but not both. */
+  /**@deprecated Will be removed in the next major release.
+   * Adapts the size of the component. When set to `inherit` a CSS `width` or `height` needs to be defined on the host but not both. */
   @Prop() public size?: ModelSignatureSize = 'small';
 
   /** Adapts the color of the component. */
