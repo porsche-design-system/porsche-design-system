3c55559d03ffc8525ee5f44d0c1feb5041a56a6c
Oliver Hook <oliver.hook@porsche.digital>
2026-05-07 10:53:50 +0200

    test(components): fix test and update snapshot | oh | #4213

--- packages/components/src/components/ai-tag/ai-tag-styles.spec.ts
+++ packages/components/src/components/ai-tag/ai-tag-styles.spec.ts
@@ -1,9 +1,8 @@
 import { validateCssAndMatchSnapshot } from '../../../tests/unit/helpers';
-import type { Theme } from '../../types';
 import { getComponentCss } from './ai-tag-styles';
 
 describe('getComponentCss()', () => {
-  it.each<Theme>(['light', 'dark'])('should return correct css for theme: %s', (theme) => {
-    validateCssAndMatchSnapshot(getComponentCss(theme));
+  it('should return correct css', () => {
+    validateCssAndMatchSnapshot(getComponentCss());
   });
 });
