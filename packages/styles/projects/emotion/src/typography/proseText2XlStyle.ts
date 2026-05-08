2cdb3d41f92964482ee7b76f58ea9a0f9cd600bd
Stephan Schröter <stephan.schroeter@porsche.digital>
2026-05-08 10:48:19 +0200

    feat(styles/emotion): improve cjk support | sas | #4329

--- packages/styles/projects/emotion/src/typography/proseText2XlStyle.ts
+++ packages/styles/projects/emotion/src/typography/proseText2XlStyle.ts
@@ -1,7 +1,8 @@
 import { colorPrimary } from '../color';
-import { fontPorscheNext, fontWeightNormal, leadingNormal, typescale2Xl } from '../font';
+import { fontPorscheNext, fontWeightNormal, getCJKFontFamilyStyle, leadingNormal, typescale2Xl } from '../font';
 
 export const proseText2XlStyle = {
+  ...getCJKFontFamilyStyle(),
   font: `${fontWeightNormal} ${typescale2Xl} / ${leadingNormal} ${fontPorscheNext}`,
   color: colorPrimary,
 };
