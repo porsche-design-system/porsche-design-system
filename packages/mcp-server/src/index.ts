313564888f736f25d8be4127c5b3c1c23272db1d
Mihael Smrček <mihael.smrcek@porsche.digital>
2026-05-26 15:19:53 +0200

    chore: add shebang | ms
    Signed-off-by: Mihael Smrček <mihael.smrcek@porsche.digital>

--- packages/mcp-server/src/index.ts
+++ packages/mcp-server/src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
 import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 import { TAG_NAMES } from '@porsche-design-system/shared';
