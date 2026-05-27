af8da764a0acd85509b04fee50090efd365c2ca3
kauricre <renato.kauric.extern@porsche.digital>
2026-05-27 12:00:31 +0200

    feat(vanilla-extract-meta): project scaffold, generate script creation | rk | #4380

---
+++ packages/styles/projects/vanilla-extract/projects/vanilla-extract-meta/src/index.ts
@@ -0,0 +1,3 @@
+export type { VanillaExtractMetaTree } from './lib/vanillaExtractMeta';
+export { vanillaExtractMeta } from './lib/vanillaExtractMeta';
+export type { VanillaExtractMeta } from './types/vanilla-extract-meta';
---
+++ packages/styles/projects/vanilla-extract/projects/vanilla-extract-meta/src/types/vanilla-extract-meta.ts
@@ -0,0 +1,5 @@
+export type VanillaExtractMeta = {
+  name: string;
+  value?: string | number;
+  description: string;
+};
