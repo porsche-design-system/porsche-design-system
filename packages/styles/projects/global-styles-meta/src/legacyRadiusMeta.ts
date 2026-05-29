1cebf431ebaa3551dd06793e5f22059d290a4628
henrijoss <henri.joss@mhp.com>
2026-05-29 16:14:18 +0200

    feat(global-styles-meta): remove unnecessary information | hj | #4473

--- packages/styles/projects/global-styles-meta/src/legacyRadiusMeta.ts
+++ packages/styles/projects/global-styles-meta/src/legacyRadiusMeta.ts
@@ -5,21 +5,18 @@ import type { CssNode, CssVariableMeta } from './types';
 // radii for legacy consumers.
 export const legacyRadiusMeta = [
   {
-    name: 'small',
     type: 'radius',
     property: '--_p-legacy-radius-small',
     description: 'Private **small** legacy `border-radius` for backwards compatibility.',
     value: '4px',
   },
   {
-    name: 'medium',
     type: 'radius',
     property: '--_p-legacy-radius-medium',
     description: 'Private **medium** legacy `border-radius` for backwards compatibility.',
     value: '8px',
   },
   {
-    name: 'large',
     type: 'radius',
     property: '--_p-legacy-radius-large',
     description: 'Private **large** legacy `border-radius` for backwards compatibility.',
