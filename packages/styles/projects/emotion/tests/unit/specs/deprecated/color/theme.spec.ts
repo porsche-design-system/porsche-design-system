c91a854f61e8f7fd65a538fcf9d08c89fe3577fb
kauricre <renato.kauric.extern@porsche.digital>
2026-06-03 15:43:48 +0200

    test(emotion): move deprecated specs out of src and update snapshots | rk | #4380-1

---
+++ packages/styles/projects/emotion/tests/unit/specs/deprecated/color/theme.spec.ts
@@ -0,0 +1,13 @@
+import { expect, it } from 'vitest';
+import * as fromTheme from '../../../../../src/color/generated/deprecated';
+
+it('should provide all exports', () => {
+  expect(Object.keys(fromTheme).length).toBe(43);
+});
+
+it.each<keyof typeof fromTheme>(Object.keys(fromTheme) as (keyof typeof fromTheme)[])(
+  'should contain correct values for %s',
+  (item) => {
+    expect(fromTheme[item]).toMatchSnapshot();
+  }
+);
