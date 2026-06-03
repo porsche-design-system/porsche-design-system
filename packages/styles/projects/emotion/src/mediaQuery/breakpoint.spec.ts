0a9c2369b71b52e0accb2566172b1964e5edefb6
kauricre <renato.kauric.extern@porsche.digital>
2026-06-03 15:19:03 +0200

    test(emotion): update test files | rk | #4380-1

--- packages/styles/projects/emotion/src/mediaQuery/breakpoint.spec.ts
+++ packages/styles/projects/emotion/src/mediaQuery/breakpoint.spec.ts
@@ -1,5 +1,5 @@
 import { expect, it } from 'vitest';
-import { breakpoint } from './breakpoint';
+import { breakpoint } from './generated';
 
 it('should contain correct values for breakpoint', () => {
   expect(breakpoint).toMatchSnapshot();
