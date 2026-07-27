eba36dd5b87a1d19c271038399390b0def4188b5
kauricre <renato.kauric.extern@porsche.digital>
2026-07-27 14:12:21 +0200

    refactor(components-react): make AG Grid storefront examples self-contained | rk | #4616

--- packages/components-react/src/examples/AGGridCompactExampleStorefront.tsx
+++ packages/components-react/src/examples/AGGridCompactExampleStorefront.tsx
@@ -1,16 +1,79 @@
 import { pdsThemeCompact } from '@porsche-design-system/components-react/ag-grid';
-import { type DataAdvanced, dataAdvanced } from '@porsche-design-system/shared';
 import { AllCommunityModule, type ColDef, ModuleRegistry } from 'ag-grid-enterprise';
 import { AgGridReact } from 'ag-grid-react';
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-type ColumnDefs = DataAdvanced & {
+type Row = {
+  model: string;
+  date: string;
+  interest: string;
+  vin: string;
+  purchaseIntention: string;
+  status: string;
+  comment: string;
+  leadId: string;
+};
+
+type ColumnDefs = Row & {
   active: boolean;
 };
 
+const data: Row[] = [
+  {
+    model: '718',
+    date: '23.06.2021',
+    interest: 'New Car',
+    vin: '1FM5K7F84FGB16304',
+    purchaseIntention: '08/2021',
+    status: 'Won',
+    comment: '-',
+    leadId: '0000824402',
+  },
+  {
+    model: 'Panamera',
+    date: '19.06.2021',
+    interest: 'New Car',
+    vin: '2GCEC13T141374801',
+    purchaseIntention: '11/2021',
+    status: 'Lost',
+    comment: 'Some multiline text and a column with a min width.',
+    leadId: '0000824409',
+  },
+  {
+    model: '911',
+    date: '19.05.2021',
+    interest: 'Used Car',
+    vin: '5GAKVCKD8EJ335750',
+    purchaseIntention: '09/2021',
+    status: 'Won',
+    comment: '-',
+    leadId: '0000824408',
+  },
+  {
+    model: 'Macan',
+    date: '10.05.2021',
+    interest: 'Used Car',
+    vin: '1FMPU17L83LC09302',
+    purchaseIntention: '07/2021',
+    status: 'Lost',
+    comment: '-',
+    leadId: '0000824407',
+  },
+  {
+    model: 'Taycan',
+    date: '03.05.2021',
+    interest: 'New Car',
+    vin: 'JN1BY1AR3BM375187',
+    purchaseIntention: '05/2021',
+    status: 'Won',
+    comment: '-',
+    leadId: '0000824406',
+  },
+];
+
 export const AGGridCompactExampleStorefrontPage = () => {
-  const rowData = dataAdvanced.map((row, index) => ({ active: Boolean(index % 2) /* odd rows */, ...row }));
+  const rowData = data.map((row, index) => ({ active: Boolean(index % 2) /* odd rows */, ...row }));
 
   const columnDefs: ColDef<ColumnDefs>[] = [
     {
@@ -56,12 +119,13 @@ export const AGGridCompactExampleStorefrontPage = () => {
   };
 
   return (
-    <div style={{ height: '100vh' }}>
+    <div>
       <AgGridReact
         rowData={rowData}
         columnDefs={columnDefs}
         defaultColDef={defaultColDef}
         pagination={true}
+        domLayout="autoHeight"
         theme={pdsThemeCompact}
       />
     </div>
